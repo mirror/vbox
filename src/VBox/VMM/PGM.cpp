89ff8d837415dd5aa6da18685c3dd8850b06e84f
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2007-01-15 17:35:58 +0000

    Fixed state loading.
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@39 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/VMM/PGM.cpp
+++ src/VBox/VMM/PGM.cpp
@@ -1599,6 +1599,12 @@ static DECLCALLBACK(int) pgmR3Load(PVM pVM, PSSMHANDLE pSSM, uint32_t u32Version
         /* any memory associated with the range. */
         if (pRam->fFlags & MM_RAM_FLAGS_DYNAMIC_ALLOC)
         {
+            bool fPreAlloc = false;
+
+            rc = CFGMR3QueryBool(CFGMR3GetRoot(pVM), "PreAlloc", &fPreAlloc);
+            if (rc == VERR_CFGM_VALUE_NOT_FOUND)
+                fPreAlloc = false;
+
             for (unsigned iChunk = 0; iChunk < (pRam->cb >> PGM_DYNAMIC_CHUNK_SHIFT); iChunk++)
             {
                 uint8_t fValidChunk;
@@ -1611,9 +1617,14 @@ static DECLCALLBACK(int) pgmR3Load(PVM pVM, PSSMHANDLE pSSM, uint32_t u32Version
 
                 if (fValidChunk)
                 {
-                    rc = pgmr3PhysGrowRange(pVM, pRam->GCPhys + iChunk * PGM_DYNAMIC_CHUNK_SIZE);
-                    if (VBOX_FAILURE(rc))
-                        return rc;
+                    /* The first chunk is always present. (all chunks are present if guest ram is preallocated) */
+                    if (    iChunk != 0 
+                        && !fPreAlloc)
+                    {
+                        rc = pgmr3PhysGrowRange(pVM, pRam->GCPhys + iChunk * PGM_DYNAMIC_CHUNK_SIZE);
+                        if (VBOX_FAILURE(rc))
+                            return rc;
+                    }
 
                     Assert(pRam->pavHCChunkHC[iChunk]);
 
