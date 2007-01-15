e22d80742b45e8c34e54268cff138fe4485a0e7f
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2007-01-15 08:31:09 +0000

    Add C version of timesync daemon.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@5 cfe28804-0f27-0410-a406-dd0f0b0b656f

---
+++ src/VBox/Additions/linux/daemon/supc++.c
@@ -0,0 +1,49 @@
+/** @file
+ *
+ * supc++ -- VirtualBox Guest Additions for Linux:
+ * VirtualBox timesync daemon for Linux
+ *
+ * Support for linking a C++ static library into a C programme
+ *
+ */
+
+/*
+ * Copyright (C) 2006 InnoTek Systemberatung GmbH
+ *
+ * This file is part of VirtualBox Open Source Edition (OSE), as
+ * available from http://www.virtualbox.org. This file is free software;
+ * you can redistribute it and/or modify it under the terms of the GNU
+ * General Public License as published by the Free Software Foundation,
+ * in version 2 as it comes in the "COPYING" file of the VirtualBox OSE
+ * distribution. VirtualBox OSE is distributed in the hope that it will
+ * be useful, but WITHOUT ANY WARRANTY of any kind.
+ *
+ * If you received this file as part of a commercial VirtualBox
+ * distribution, then only the terms of your commercial VirtualBox
+ * license agreement apply instead of the previous paragraph.
+ */
+
+#include <stdio.h>
+
+/* C++ hack */
+int __gxx_personality_v0 = 0xdeadbeef;
+
+/* long long hacks (as far as i can see, gcc emits the refs to those
+   symbols, notwithstanding the fact that those aren't referenced
+   anywhere) */
+/* I will have to think of something better than that printf sometime. */
+void __divdi3 (void);
+
+void __divdi3 (void)
+{
+        printf ("__divdi3 called from %p\n", __builtin_return_address (0));
+        /* exit(1); */  /* Should we do this? */
+}
+
+void __moddi3 (void);
+
+void __moddi3 (void)
+{
+        printf ("__moddi3 called from %p\n", __builtin_return_address (0));
+        /* exit(1); */
+}
---
+++ src/VBox/Additions/linux/daemon/vboxadd_timesync.c
@@ -0,0 +1,178 @@
+/** @file
+ *
+ * VirtualBox timesync daemon for Linux
+ */
+
+/*
+ * Copyright (C) 2006 InnoTek Systemberatung GmbH
+ *
+ * This file is part of VirtualBox Open Source Edition (OSE), as
+ * available from http://www.virtualbox.org. This file is free software;
+ * you can redistribute it and/or modify it under the terms of the GNU
+ * General Public License as published by the Free Software Foundation,
+ * in version 2 as it comes in the "COPYING" file of the VirtualBox OSE
+ * distribution. VirtualBox OSE is distributed in the hope that it will
+ * be useful, but WITHOUT ANY WARRANTY of any kind.
+ *
+ * If you received this file as part of a commercial VirtualBox
+ * distribution, then only the terms of your commercial VirtualBox
+ * license agreement apply instead of the previous paragraph.
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <getopt.h>
+#include <errno.h>
+#include <sys/fcntl.h>
+#include <sys/ioctl.h>
+#include <sys/time.h>
+#include <time.h>
+#include <VBox/VBoxGuest.h>
+#include <VBox/err.h>
+
+static void usage(void)
+{
+    printf("vboxadd-timesync [--interval <seconds>]\n"
+           "                 [--daemonize]"
+           "\n");
+}
+
+static void safe_sleep(time_t seconds)
+{
+    struct timespec requestedSleepTime;
+
+    requestedSleepTime.tv_sec = seconds;
+    requestedSleepTime.tv_nsec = 0;
+
+    for (;;)
+    {
+        int err;
+        struct timespec remainingSleepTime;
+
+        err = nanosleep(&requestedSleepTime, &remainingSleepTime);
+        if (err)
+        {
+            /* if the sleep was interrupted, remember remaining sleep time */
+            if (errno == EINTR)
+            {
+                requestedSleepTime = remainingSleepTime;
+            }
+            else
+            {
+                /* not good... */
+            }
+        }
+        else
+        {
+            /* nanosleep completed and took at least the requested time */
+            break;
+        }
+    }
+}
+
+
+int main(int argc, char *argv[])
+{
+    const struct option options[] =
+    {
+        { "interval",  required_argument, NULL, 'i' },
+        { "daemonize", no_argument,       NULL, 'd' },
+        { "help",      no_argument,       NULL, 'h' },
+        { NULL,        0,                 NULL, 0   }
+    };
+    int secInterval = 10;
+    int fDaemonize = 0;
+    int c;
+    int fd;
+    VMMDevReqHostTime req;
+
+    /* command line parsing */
+    for (;;)
+    {
+        c = getopt_long(argc, argv, "", options, NULL);
+        if (c == -1)
+            break;
+        switch (c)
+        {
+            case 'i':
+            {
+                secInterval = atoi(optarg);
+                break;
+            }
+
+            case 'd':
+            {
+                fDaemonize = 1;
+                break;
+            }
+
+            case 'h':
+            {
+                usage();
+                return 0;
+            }
+
+            case ':':
+            case '?':
+            {
+                /* unrecognized option (?) or parameter missing (:) */
+                return 1;
+            }
+
+            default:
+                break;
+        }
+    }
+
+    /* open the driver */
+    fd = open(VBOXGUEST_DEVICE_NAME, O_RDWR, 0);
+    if (fd < 0)
+    {
+        printf("Error opening kernel module! rc = %d\n", errno);
+        return 1;
+    }
+
+    /* prepare the request structure */
+    vmmdevInitRequest((VMMDevRequestHeader*)&req, VMMDevReq_GetHostTime);
+
+    if (!fDaemonize)
+        printf("VirtualBox timesync daemon.\n"
+               "(C) 2005 InnoTek Systemberatung GmbH\n"
+               "\nSync interval: %d seconds.\n", secInterval);
+
+    if (fDaemonize)
+        daemon(1, 0);
+
+    do
+    {
+        /* perform VMM request */
+        if (ioctl(fd, IOCTL_VBOXGUEST_VMMREQUEST, (void*)&req) >= 0)
+        {
+            if (VBOX_SUCCESS(req.header.rc))
+            {
+                /* set the system time */
+                struct timeval tv;
+                tv.tv_sec  = req.time / (uint64_t)1000;
+                tv.tv_usec = (req.time % (uint64_t)1000) * 1000;
+                settimeofday(&tv, NULL);
+            }
+            else
+            {
+                printf("Error querying host time! header.rc = %d\n", req.header.rc);
+            }
+        }
+        else
+        {
+            printf("Error performing VMM request! errno = %d\n", errno);
+        }
+	    /* wait for the next run */
+        safe_sleep(secInterval);
+
+    } while (1);
+
+    close(fd);
+
+    return 0;
+}
