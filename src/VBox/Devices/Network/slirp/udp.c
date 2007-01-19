98cc0952c12cece64f2fb313c901ce993b37390a
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2007-01-19 16:03:20 +0000

    merge with upstream
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@184 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Devices/Network/slirp/udp.c
+++ src/VBox/Devices/Network/slirp/udp.c
@@ -207,8 +207,6 @@ udp_input(m, iphlen)
 	  /* udp_last_so = so; */
 	  so->so_laddr = ip->ip_src;
 	  so->so_lport = uh->uh_sport;
-	  so->so_faddr = ip->ip_dst; /* XXX */
-	  so->so_fport = uh->uh_dport; /* XXX */
 	  
 	  if ((so->so_iptos = udp_tos(so)) == 0)
 	    so->so_iptos = ip->ip_tos;
@@ -218,13 +216,9 @@ udp_input(m, iphlen)
 	   * and if it is, do the fork_exec() etc.
 	   */
 	}
-#ifdef VBOX
-	else
-	{
-	  so->so_faddr = ip->ip_dst; /* XXX */
-	  so->so_fport = uh->uh_dport; /* XXX */
-	}
-#endif
+
+        so->so_faddr = ip->ip_dst; /* XXX */
+        so->so_fport = uh->uh_dport; /* XXX */
 
 	iphlen += sizeof(struct udphdr);
 	m->m_len -= iphlen;
