cd650ad3c1986b6cab7b58293b38fd11a289ccb0
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2007-01-19 14:45:28 +0000

    Backed out 17600 -> breaks parsing alltogether
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@178 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Runtime/log.cpp
+++ src/VBox/Runtime/log.cpp
@@ -56,7 +56,7 @@
 *   Defined Constants And Macros                                               *
 *******************************************************************************/
 /** Ascii to lower macro. */
-#define CHLOWER(ch)     (((unsigned char)(ch) <= (unsigned char)'A') || ((unsigned char)(ch) > (unsigned char)'Z') ? (ch) : (ch) + ('a' - 'A'))
+#define CHLOWER(ch)     (((unsigned char)(ch) - (unsigned char)'A') > (unsigned char)('Z' - 'A') ? (ch) : (ch) + ('a' - 'A'))
 
 
 /*******************************************************************************
@@ -1031,8 +1031,7 @@ static unsigned rtlogGroupFlags(const char *psz)
                 if (!*psz1)
                 {
                     if (    (*psz2 >= 'a' && *psz2 <= 'z')
-                        ||  (*psz2 >= 'A' && *psz2 <= 'Z')
-                        ||  (*psz2 >= '0' && *psz2 <= '9') )
+                        ||  (*psz2 >= 'A' && *psz2 <= 'Z') )
                         break;
                     fFlags |= aFlags[i].fFlag;
                     fFound = true;
@@ -1701,7 +1700,7 @@ static DECLCALLBACK(size_t) rtLogOutput(void *pv, const char *pachChars, size_t 
         size_t cbRet = 0;
         for (;;)
         {
-#if defined(DEBUG) && defined(IN_RING3)
+            #if defined(DEBUG) && defined(IN_RING3)
             /* sanity */
             if (pLogger->offScratch >= sizeof(pLogger->achScratch))
             {
@@ -1709,7 +1708,7 @@ static DECLCALLBACK(size_t) rtLogOutput(void *pv, const char *pachChars, size_t 
                         pLogger->offScratch, (unsigned)sizeof(pLogger->achScratch));
                 AssertBreakpoint(); AssertBreakpoint();
             }
-#endif
+            #endif
 
             /* how much */
             size_t cb = sizeof(pLogger->achScratch) - pLogger->offScratch - 1;
