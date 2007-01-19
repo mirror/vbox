4273bc5a4c3dbf757a0b0c813d3d7debe73b5376
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2007-01-19 17:04:07 +0000

    Turn off sysenter/sysexit support due to IOPL problems. (#1782)
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@186 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/VMM/CPUM.cpp
+++ src/VBox/VMM/CPUM.cpp
@@ -259,7 +259,8 @@ static int cpumR3CpuIdInit(PVM pVM)
                                        //| X86_CPUID_AMD_FEATURE_EDX_MCE    - not virtualized yet.
                                        | X86_CPUID_AMD_FEATURE_EDX_CX8
                                        //| X86_CPUID_AMD_FEATURE_EDX_APIC   - set by the APIC device if present.
-                                       | X86_CPUID_AMD_FEATURE_EDX_SEP
+                                       /** @note we don't report sysenter/sysexit support due to our inability to keep the IOPL part of eflags in sync while in ring 1 (see #1757) */
+                                       //| X86_CPUID_AMD_FEATURE_EDX_SEP
                                        //| X86_CPUID_AMD_FEATURE_EDX_MTRR   - not virtualized.
                                        | X86_CPUID_AMD_FEATURE_EDX_PGE
                                        //| X86_CPUID_AMD_FEATURE_EDX_MCA    - not virtualized.
