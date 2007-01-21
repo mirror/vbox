4ca2e0a45b18dabd30a1a800e48c1c984e8ab452
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2007-01-21 21:40:14 +0000

    ...
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@216 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/HostDrivers/Support/darwin/load.sh
+++ src/VBox/HostDrivers/Support/darwin/load.sh
@@ -10,14 +10,20 @@ if [ ! -d "$DIR" ]; then
     echo "Cannot find $DIR or it's not a directory..."
     exit 1;
 fi
+if [ -n "$*" ]; then
+  OPTS="$*"
+else
+  OPTS="-t"
+fi
 
 trap "sudo chown -R `whoami` $DIR; exit 1" INT
 set -x
 sudo chown -R root:wheel "$DIR"
 sudo chmod -R o-rwx "$DIR"
-sync ; sleep 1; sync
-sudo kextload -t "$DIR"
+sync
+sudo kextload $OPTS "$DIR"
 sudo chown -R `whoami` "$DIR"
+sudo chmod 666 /dev/vboxdrv
 
 
 
