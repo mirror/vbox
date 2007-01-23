2e659888fb9102e9b92936ac8685d537e103c1bb
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2007-01-23 17:12:21 +0000

    Reversed
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@250 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/VMM/MMPhys.cpp
+++ src/VBox/VMM/MMPhys.cpp
@@ -147,7 +147,7 @@ MMR3DECL(int) MMR3PhysRegisterEx(PVM pVM, void *pvRam, RTGCPHYS GCPhys, unsigned
          * Lock the memory. (fully allocated by caller)
          */
         PMMLOCKEDMEM    pLockedMem;
-        rc = mmr3LockMem(pVM, pvRam, cb, MM_LOCKED_TYPE_PHYS, &pLockedMem, (enmType == MM_PHYS_TYPE_DYNALLOC_CHUNK) ? false : true /* fSilentFailure */);
+        rc = mmr3LockMem(pVM, pvRam, cb, MM_LOCKED_TYPE_PHYS, &pLockedMem, (enmType == MM_PHYS_TYPE_DYNALLOC_CHUNK) ? true : false /* fSilentFailure */);
         if (VBOX_SUCCESS(rc))
         {
             pLockedMem->u.phys.GCPhys = GCPhys;
