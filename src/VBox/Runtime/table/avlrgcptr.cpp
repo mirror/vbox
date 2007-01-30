d3a716cf63e618dec8f4bbc6b37258835b5b030b
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2007-01-30 23:03:57 +0000

    RTAvlrGCPtr*
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@449 cfe28804-0f27-0410-a406-dd0f0b0b656f

---
+++ src/VBox/Runtime/table/avlrgcptr.cpp
@@ -0,0 +1,78 @@
+/* $Id$ */
+/** @file
+ * InnoTek Portable Runtime - AVL tree, RTGCPTR, range, unique keys.
+ */
+
+/*
+ * Copyright (C) 2001-2004 knut st. osmundsen (bird-src-spam@anduin.net)
+ * Copyright (C) 2006 InnoTek Systemberatung GmbH
+ *
+ * This file is part of VirtualBox Open Source Edition (OSE), as
+ * available from http://www.virtualbox.org. This file is free software;
+ * you can redistribute it and/or modify it under the terms of the GNU
+ * General Public License as published by the Free Software Foundation,
+ * in version 2 as it comes in the "COPYING" file of the VirtualBox OSE
+ * distribution. VirtualBox OSE is distributed in the hope that it will
+ * be useful, but WITHOUT ANY WARRANTY of any kind.
+ *
+ * If you received this file as part of a commercial VirtualBox
+ * distribution, then only the terms of your commercial VirtualBox
+ * license agreement apply instead of the previous paragraph.
+ */
+
+#ifndef NOFILEID
+static const char szFileId[] = "Id: kAVLULInt.c,v 1.4 2003/02/13 02:02:38 bird Exp $";
+#endif
+
+/*******************************************************************************
+*   Defined Constants And Macros                                               *
+*******************************************************************************/
+/*
+ * AVL configuration.
+ */
+#define KAVL_FN(a)                  RTAvlrGCPtr##a
+#define KAVL_MAX_STACK              27  /* Up to 2^24 nodes. */
+#define KAVL_CHECK_FOR_EQUAL_INSERT 1   /* No duplicate keys! */
+#define KAVLNODECORE                AVLRGCPTRNODECORE
+#define PKAVLNODECORE               PAVLRGCPTRNODECORE
+#define PPKAVLNODECORE              PPAVLRGCPTRNODECORE
+#define KAVLKEY                     RTGCPTR
+#define PKAVLKEY                    PRTGCPTR
+#define KAVLENUMDATA                AVLRGCPTRENUMDATA
+#define PKAVLENUMDATA               PAVLRGCPTRENUMDATA
+#define PKAVLCALLBACK               PAVLRGCPTRCALLBACK
+#define KAVL_RANGE                  1
+
+
+/*
+ * AVL Compare macros
+ */
+#define KAVL_G( key1, key2)         ( (RTGCUINTPTR)(key1) >  (RTGCUINTPTR)(key2) )
+#define KAVL_E( key1, key2)         ( (RTGCUINTPTR)(key1) == (RTGCUINTPTR)(key2) )
+#define KAVL_NE(key1, key2)         ( (RTGCUINTPTR)(key1) != (RTGCUINTPTR)(key2) )
+#define KAVL_R_IS_IDENTICAL(key1B, key2B, key1E, key2E)     ( (RTGCUINTPTR)(key1B) == (RTGCUINTPTR)(key2B) && (RTGCUINTPTR)(key1E) == (RTGCUINTPTR)(key2E) )
+#define KAVL_R_IS_INTERSECTING(key1B, key2B, key1E, key2E)  ( (RTGCUINTPTR)(key1B) <= (RTGCUINTPTR)(key2E) && (RTGCUINTPTR)(key1E) >= (RTGCUINTPTR)(key2B) )
+#define KAVL_R_IS_IN_RANGE(key1B, key1E, key2)              KAVL_R_IS_INTERSECTING(key1B, key2, key1E, key2)
+
+
+
+/*******************************************************************************
+*   Header Files                                                               *
+*******************************************************************************/
+#include <iprt/avl.h>
+#include <iprt/assert.h>
+
+/*
+ * Include the code.
+ */
+#define SSToDS(ptr) ptr
+#define KMAX        RT_MAX
+#define kASSERT     Assert
+#include "avl_Base.cpp.h"
+#include "avl_Get.cpp.h"
+#include "avl_Range.cpp.h"
+#include "avl_DoWithAll.cpp.h"
+#include "avl_Destroy.cpp.h"
+#include "avl_GetBestFit.cpp.h"
+#include "avl_Enum.cpp.h"
+
