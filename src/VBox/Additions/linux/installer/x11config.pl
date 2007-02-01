17e45c51a9812881456ea1cfa6c8387d583b46c8
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2007-02-01 09:10:58 +0000

    Additions installers are not OSE
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@495 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Additions/linux/installer/x11config.pl
+++ src/VBox/Additions/linux/installer/x11config.pl
@@ -1,23 +1,14 @@
 #!/usr/bin/perl -w
 #
-#  InnoTek VirtualBox
+# InnoTek VirtualBox
 #
-#  Linux Additions X11 config update script
+# Linux Additions X11 config update script
 #
-#  Copyright (C) 2006 InnoTek Systemberatung GmbH
+# Copyright (C) 2006 InnoTek Systemberatung GmbH
 #
-#  This file is part of VirtualBox Open Source Edition (OSE), as
-#  available from http://www.virtualbox.org. This file is free software;
-#  you can redistribute it and/or modify it under the terms of the GNU
-#  General Public License as published by the Free Software Foundation,
-#  in version 2 as it comes in the "COPYING" file of the VirtualBox OSE
-#  distribution. VirtualBox OSE is distributed in the hope that it will
-#  be useful, but WITHOUT ANY WARRANTY of any kind.
+# Use only with permission.
 #
-#  If you received this file as part of a commercial VirtualBox
-#  distribution, then only the terms of your commercial VirtualBox
-#  license agreement apply instead of the previous paragraph.
-#
+
 
 my $temp="/tmp/xorg.conf";
 my @cfg_files = ("/etc/X11/xorg.conf-4", "/etc/X11/xorg.conf", "/etc/xorg.conf",
