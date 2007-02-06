7bea261270e6af245ffcb47b87883f004866ec07
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2007-02-06 04:45:46 +0000

    ASMGetTR & ASMGetGDTR
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@683 cfe28804-0f27-0410-a406-dd0f0b0b656f

---
+++ src/VBox/Runtime/win64/ASMGetGDTR.asm
@@ -0,0 +1,36 @@
+;; @file
+; InnoTek Portable Runtime - ASMGetGDTR().
+;
+
+;
+; Copyright (C) 2006 InnoTek Systemberatung GmbH
+;
+; This file is part of VirtualBox Open Source Edition (OSE), as
+; available from http://www.virtualbox.org. This file is free software;
+; you can redistribute it and/or modify it under the terms of the GNU
+; General Public License as published by the Free Software Foundation,
+; in version 2 as it comes in the "COPYING" file of the VirtualBox OSE
+; distribution. VirtualBox OSE is distributed in the hope that it will
+; be useful, but WITHOUT ANY WARRANTY of any kind.
+;
+; If you received this file as part of a commercial VirtualBox
+; distribution, then only the terms of your commercial VirtualBox
+; license agreement apply instead of the previous paragraph.
+;
+
+;*******************************************************************************
+;* Header Files                                                                *
+;*******************************************************************************
+%include "iprt/asmdefs.mac"
+
+BEGINCODE
+
+;;
+; Gets the content of the GDTR CPU register.
+; @param   rcx  pGdtr   Where to store the GDTR contents.
+;
+BEGINPROC_EXPORTED ASMGetGDTR
+        sgdt    [rcx]
+        ret
+ENDPROC ASMGetGDTR
+
