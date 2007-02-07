af009333e31ee056f11c111f6f35373027357544
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2007-02-07 09:48:27 +0000

    Fix
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@739 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/VMM/PATM/PATMA.asm
+++ src/VBox/VMM/PATM/PATMA.asm
@@ -1682,7 +1682,7 @@ PATMLookupAndCall_SearchEnd:
     mov     ebx, dword [esp - 20 - 16]               ; original guest return address
     mov     edx, dword [esp - 40 + 4 - 16]           ; duplicated patch function
     lock    or dword [ss:PATM_PENDINGACTION], PATM_ACTION_LOG_CALL
-    mov     eax, PATM_ACTION_LOG_RET
+    mov     eax, PATM_ACTION_LOG_CALL
     mov     ecx, PATM_ACTION_MAGIC
     db      0fh, 0bh        ; illegal instr (hardcoded assumption in PATMHandleIllegalInstrTrap)
     pop     edx
