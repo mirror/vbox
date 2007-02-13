5ae552cf7d485b8cfb51acbf28e3767082394baf
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2007-02-13 13:21:02 +0000

    Backed out 18568
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@867 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- include/VBox/types.h
+++ include/VBox/types.h
@@ -103,8 +103,6 @@ typedef enum VMSTATE
     VMSTATE_SAVING,
     /** The VM is suspended. */
     VMSTATE_SUSPENDED,
-    /** The VM is suspended in a state, that doesn't allow saving. */
-    VMSTATE_SUSPENDED_NOSAVE,
     /** The VM is being reset. */
     VMSTATE_RESETTING,
     /** The VM is in guru meditation over a fatal failure. */
