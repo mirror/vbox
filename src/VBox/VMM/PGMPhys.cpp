bd1f010a9d295442a583f40c8cbf9509ba3199c5
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2007-02-13 15:31:16 +0000

    Changed error message when out of memory. (no longer allowed to save the state)
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@877 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/VMM/PGMPhys.cpp
+++ src/VBox/VMM/PGMPhys.cpp
@@ -389,7 +389,7 @@ int pgmr3PhysGrowRange(PVM pVM, RTGCPHYS GCPhys)
         SUPPageFree(pvRam);
 
         LogRel(("pgmr3PhysGrowRange: out of memory. pause until the user resumes execution.\n"));
-        VMSetRuntimeError(pVM, false, "HostMemoryLow", "Unable to allocate and lock memory. The virtual machine will be paused. Please close applications to free up memory or save and close the VM.");
+        VMSetRuntimeError(pVM, false, "HostMemoryLow", "Unable to allocate and lock memory. The virtual machine will be paused. Please close applications to free up memory or close the VM.");
  
         rc = VMR3SuspendNoSave(pVM);
         AssertRC(rc);
