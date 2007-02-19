d157186e9bb183670d2201dbc56ebd34b050dd2b
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2007-02-19 17:24:30 +0000

    Fixed confusion between hyper disasm request and real mode. Also we're converting pages, not the read size.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@986 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/VMM/DBGFDisas.cpp
+++ src/VBox/VMM/DBGFDisas.cpp
@@ -170,7 +170,7 @@ static DECLCALLBACK(int32_t) dbgfR3DisasInstrRead(RTHCUINTPTR PtrSrc, uint8_t *p
                     pState->rc = VERR_INVALID_POINTER;
             }
             else if (pState->enmMode <= PGMMODE_PROTECTED)
-                pState->rc = PGMPhysGCPhys2HCPtr(pState->pVM, pState->pvPageGC, cbRead, &pState->pvPageHC);
+                pState->rc = PGMPhysGCPhys2HCPtr(pState->pVM, pState->pvPageGC, PAGE_SIZE, &pState->pvPageHC);
             else
                 pState->rc = PGMPhysGCPtr2HCPtr(pState->pVM, pState->pvPageGC, &pState->pvPageHC);
             if (VBOX_FAILURE(pState->rc))
@@ -253,8 +253,9 @@ DBGFR3DECL(int) DBGFR3DisasInstrEx(PVM pVM, RTSEL Sel, RTGCPTR GCPtr, unsigned f
     SELMSELINFO SelInfo;
     const PGMMODE enmMode = PGMGetGuestMode(pVM);
     bool fRealModeAddress = false;
-    if (    (pCtxCore && pCtxCore->eflags.Bits.u1VM == 1)
-        ||  enmMode == PGMMODE_REAL)
+    if (    !(fFlags & DBGF_DISAS_FLAGS_CURRENT_HYPER)
+        &&  (   (pCtxCore && pCtxCore->eflags.Bits.u1VM)
+             ||  enmMode == PGMMODE_REAL) )
     {   /* V86 mode or real mode - real mode addressing */
         SelInfo.GCPtrBase           = Sel * 16;
         SelInfo.cbLimit             = ~0;
