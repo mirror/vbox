29ab5c3c806bf76b3d46122f002d38ff509fdc7e
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2007-02-26 20:14:08 +0000

    removed dead code; cosmetics
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@1067 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Additions/linux/module/cmc.c
+++ src/VBox/Additions/linux/module/cmc.c
@@ -18,98 +18,33 @@
  * license agreement apply instead of the previous paragraph.
  */
 
-#if 0
-  #ifndef bool /* Linux 2.6.19 C++ nightmare */
-  #define bool bool_type
-  #define true true_type
-  #define false false_type
-  #define _Bool int
-  #define bool_cmc_c
-  #endif
-  #include <linux/autoconf.h>
-  #include <linux/version.h>
-  #if LINUX_VERSION_CODE > KERNEL_VERSION(2, 6, 0)
-  # undef ALIGN
-  #endif
-  #ifndef KBUILD_STR
-  # if LINUX_VERSION_CODE < KERNEL_VERSION(2, 6, 16)
-  #  define KBUILD_STR(s) s
-  # else
-  #  define KBUILD_STR(s) #s
-  # endif
-  #endif
-  /* Make (at least) RHEL3U5 happy. */
-  #if LINUX_VERSION_CODE < KERNEL_VERSION(2, 5, 0)
-  // # define EXPORT_SYMTAB
-  #endif
-  #include <linux/module.h>
-  #include <linux/kernel.h>
-  #include <linux/fs.h>
-  #include <linux/mm.h>
-  #include <linux/wait.h>
-  #include <linux/pci.h>
-  #include <linux/delay.h>
-  #include <linux/interrupt.h>
-  #include <linux/completion.h>
-  #include <asm/uaccess.h>
-  #ifdef bool_cmc_c
-  #undef bool
-  #undef true
-  #undef false
-  #undef _Bool
-  #undef bool_cmc_c
-  #endif
-#endif
-
 #include "the-linux-kernel.h"
-
-// What is this for?
-// bird: see comment on the other #undef.
-#undef ALIGN
 #include "vboxmod.h"
-/* #include "waitcompat.h" */
 
 /**
  * HGCM
  *
  */
 static DECLVBGL(void)
-vboxadd_hgcm_callback (
-    VMMDevHGCMRequestHeader *pHeader,
-    void *pvData,
-    uint32_t u32Data
-    )
+vboxadd_hgcm_callback (VMMDevHGCMRequestHeader *pHeader, void *pvData, uint32_t u32Data)
 {
     VBoxDevice *dev = pvData;
-
     wait_event (dev->eventq, pHeader->fu32Flags & VBOX_HGCM_REQ_DONE);
 }
 
-static int vboxadd_hgcm_connect (
-    VBoxDevice *dev,
-    VBoxGuestHGCMConnectInfo *info
-    )
+static int vboxadd_hgcm_connect (VBoxDevice *dev, VBoxGuestHGCMConnectInfo *info)
 {
-    int rc = VbglHGCMConnect (info, vboxadd_hgcm_callback, dev, 0);
-    return rc;
+    return VbglHGCMConnect (info, vboxadd_hgcm_callback, dev, 0);
 }
 
-static int vboxadd_hgcm_disconnect (
-    VBoxDevice *dev,
-    VBoxGuestHGCMDisconnectInfo *info
-    )
+static int vboxadd_hgcm_disconnect (VBoxDevice *dev, VBoxGuestHGCMDisconnectInfo *info)
 {
-    int rc = VbglHGCMDisconnect (info, vboxadd_hgcm_callback, dev, 0);
-    return rc;
+    return VbglHGCMDisconnect (info, vboxadd_hgcm_callback, dev, 0);
 }
 
-static int vboxadd_hgcm_call (
-    VBoxDevice *dev,
-    VBoxGuestHGCMCallInfo *info
-    )
+static int vboxadd_hgcm_call (VBoxDevice *dev, VBoxGuestHGCMCallInfo *info)
 {
-    int rc = VbglHGCMCall (info, vboxadd_hgcm_callback, dev, 0);
-    return rc;
+    return VbglHGCMCall (info, vboxadd_hgcm_callback, dev, 0);
 }
 
 DECLVBGL (int) vboxadd_cmc_call (void *opaque, uint32_t func, void *data)
@@ -142,27 +77,29 @@ void vboxadd_cmc_fini (void)
 DECLVBGL (int)
 vboxadd_cmc_ctl_guest_filter_mask (uint32_t or_mask, uint32_t not_mask)
 {
-        int rc;
-        VMMDevCtlGuestFilterMask *req;
+    int rc;
+    VMMDevCtlGuestFilterMask *req;
 
-        rc = VbglGRAlloc ((VMMDevRequestHeader**) &req, sizeof (*req),
-                          VMMDevReq_CtlGuestFilterMask);
+    rc = VbglGRAlloc ((VMMDevRequestHeader**) &req, sizeof (*req),
+                      VMMDevReq_CtlGuestFilterMask);
 
-        if (VBOX_FAILURE (rc)) {
-                elog ("VbglGRAlloc (CtlGuestFilterMask) failed rc=%d\n", rc);
-                return -1;
-        }
+    if (VBOX_FAILURE (rc))
+    {
+        elog ("VbglGRAlloc (CtlGuestFilterMask) failed rc=%d\n", rc);
+        return -1;
+    }
 
-        req->u32OrMask = or_mask;
-        req->u32NotMask = not_mask;
+    req->u32OrMask = or_mask;
+    req->u32NotMask = not_mask;
 
-        rc = VbglGRPerform (&req->header);
-        VbglGRFree (&req->header);
-        if (VBOX_FAILURE (rc)) {
-                elog ("VbglGRPerform (CtlGuestFilterMask) failed rc=%d\n", rc);
-                return -1;
-        }
-        return 0;
+    rc = VbglGRPerform (&req->header);
+    VbglGRFree (&req->header);
+    if (VBOX_FAILURE (rc))
+    {
+        elog ("VbglGRPerform (CtlGuestFilterMask) failed rc=%d\n", rc);
+        return -1;
+    }
+    return 0;
 }
 
 EXPORT_SYMBOL (vboxadd_cmc_call);
