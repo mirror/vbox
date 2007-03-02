fa999f7aea191e8ba3d1f31c121794090ff1343f
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2007-03-02 14:42:20 +0000

    v86 mode corrections
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@1162 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/VMM/PATM/VMMAll/PATMAll.cpp
+++ src/VBox/VMM/PATM/VMMAll/PATMAll.cpp
@@ -396,6 +396,7 @@ PATMDECL(int) PATMSysCall(PVM pVM, PCPUMCTXCORE pRegFrame, PDISCPUSTATE pCpu)
     if (pCpu->pCurInstr->opcode == OP_SYSENTER)
     {
         if (    pCtx->SysEnter.cs == 0
+            ||  pRegFrame->eflags.Bits.u1VM
             ||  (pRegFrame->cs & X86_SEL_RPL) != 3
             ||  pVM->patm.s.pfnSysEnterPatchGC == 0
             ||  pVM->patm.s.pfnSysEnterGC != (RTGCPTR)pCtx->SysEnter.eip
@@ -424,6 +425,7 @@ PATMDECL(int) PATMSysCall(PVM pVM, PCPUMCTXCORE pRegFrame, PDISCPUSTATE pCpu)
     {
         if (    pCtx->SysEnter.cs == 0
             ||  (pRegFrame->cs & X86_SEL_RPL) != 1
+            ||  pRegFrame->eflags.Bits.u1VM
             ||  !(PATMRawGetEFlags(pVM, pRegFrame) & X86_EFL_IF))
             goto end;
 
