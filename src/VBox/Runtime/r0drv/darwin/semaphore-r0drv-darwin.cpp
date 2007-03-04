abaf01e02a58129f415c11d589f98520be0b6acf
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2007-03-04 20:34:10 +0000

    Corrected bug in the multiple release event semaphore wait code. Added a todo for a scheduling race condition on smp boxes.
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@1189 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Runtime/r0drv/darwin/semaphore-r0drv-darwin.cpp
+++ src/VBox/Runtime/r0drv/darwin/semaphore-r0drv-darwin.cpp
@@ -184,6 +184,9 @@ RTDECL(int)  RTSemEventSignal(RTSEMEVENT EventSem)
         ASMAtomicDecU32(&pEventInt->cWaiters);
         ASMAtomicIncU32(&pEventInt->cWaking);
         thread_wakeup_prim((event_t)pEventInt, TRUE /* one thread */, THREAD_AWAKENED);
+        /** @todo this isn't safe. a scheduling interrupt on the other cpu while we're in here
+         * could cause the thread to be timed out before we manage to wake it up and the event
+         * ends up in the wrong state. ditto for posix signals. */
     }
     else
         ASMAtomicXchgU8(&pEventInt->fSignaled, true);
@@ -404,10 +407,7 @@ static int rtSemEventMultiWait(RTSEMEVENTMULTI EventMultiSem, unsigned cMillies,
 
     int rc;
     if (pEventMultiInt->fSignaled)
-    {
-        ASMAtomicXchgU8(&pEventMultiInt->fSignaled, false);
         rc = VINF_SUCCESS;
-    }
     else
     {
         ASMAtomicIncU32(&pEventMultiInt->cWaiters);
