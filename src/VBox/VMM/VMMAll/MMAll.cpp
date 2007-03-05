34c0991f66e624980d044ef364e8cd0dd8e8e180
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2007-03-05 18:10:30 +0000

    Pointer mess.
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@1238 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/VMM/VMMAll/MMAll.cpp
+++ src/VBox/VMM/VMMAll/MMAll.cpp
@@ -27,6 +27,7 @@
 #include <VBox/mm.h>
 #include "MMInternal.h"
 #include <VBox/vm.h>
+#include <VBox/log.h>
 #include <iprt/assert.h>
 
 
@@ -107,7 +108,7 @@ DECLINLINE(PMMLOOKUPHYPER) mmHyperLookupR0(PVM pVM, RTR0PTR R0Ptr, uint32_t *pof
 
     /** @todo fix this properly; the ring 0 pVM address differs from the R3 one. */
     if (R0Ptr >= pVM->pVMR0 && (RTR0UINTREG)R0Ptr < (RTR0UINTREG)pVM->pVMR0 + sizeof(*pVM))
-        R3Ptr = (RTR3PTR)((RTR0UINTREG)R0Ptr - (RTR0UINTREG)pVM + (RTR0UINTREG)pVM->pVMR3);
+        R3Ptr = (RTR3PTR)((RTR0UINTREG)R0Ptr - (RTR0UINTREG)pVM->pVMR0 + (RTR0UINTREG)pVM->pVMR3);
     else
         R3Ptr = (RTR3PTR)R0Ptr;
 
