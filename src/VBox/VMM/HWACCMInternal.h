3af8a4807f69bb5bc48c03e2a6da37c3d62dcfe6
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2007-03-07 12:25:24 +0000

    removed another workaround
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@1297 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/VMM/HWACCMInternal.h
+++ src/VBox/VMM/HWACCMInternal.h
@@ -105,7 +105,7 @@ __BEGIN_DECLS
 #define HWACCM_SVM_TRAP_MASK                HWACCM_VMX_TRAP_MASK
 #else
 #define HWACCM_VMX_TRAP_MASK                BIT(7) | BIT(14)
-#define HWACCM_SVM_TRAP_MASK                HWACCM_VMX_TRAP_MASK | BIT(13) /* #GP is required for the #GP in V86 workaround */
+#define HWACCM_SVM_TRAP_MASK                HWACCM_VMX_TRAP_MASK
 #endif
 /** @} */
 
