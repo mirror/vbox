c9ee01ff7ac1a3d730c2058ae555a803690ac02d
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2007-03-09 20:18:02 +0000

    filelock-posix.cpp, no pl. Made darwin copy of the locks
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@1388 cfe28804-0f27-0410-a406-dd0f0b0b656f

---
+++ src/VBox/Runtime/r3/darwin/filelock-darwin.cpp
@@ -0,0 +1,135 @@
+/* $Id$ */
+/** @file
+ * InnoTek Portable Runtime - File Locking, POSIX.
+ */
+
+/*
+ * Copyright (C) 2006 InnoTek Systemberatung GmbH
+ *
+ * This file is part of VirtualBox Open Source Edition (OSE), as
+ * available from http://www.virtualbox.org. This file is free software;
+ * you can redistribute it and/or modify it under the terms of the GNU
+ * General Public License as published by the Free Software Foundation,
+ * in version 2 as it comes in the "COPYING" file of the VirtualBox OSE
+ * distribution. VirtualBox OSE is distributed in the hope that it will
+ * be useful, but WITHOUT ANY WARRANTY of any kind.
+ *
+ * If you received this file as part of a commercial VirtualBox
+ * distribution, then only the terms of your commercial VirtualBox
+ * license agreement apply instead of the previous paragraph.
+ */
+
+
+/*******************************************************************************
+*   Header Files                                                               *
+*******************************************************************************/
+#define LOG_GROUP RTLOGGROUP_FILE
+
+#include <errno.h>
+#include <sys/types.h>
+#include <sys/ioctl.h>
+#include <sys/fcntl.hs>
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/time.h>
+
+#include <iprt/file.h>
+#include <iprt/assert.h>
+#include <iprt/string.h>
+#include <iprt/err.h>
+#include <iprt/log.h>
+#include "internal/file.h"
+#include "internal/fs.h"
+
+
+
+
+RTR3DECL(int)  RTFileLock(RTFILE File, unsigned fLock, int64_t offLock, uint64_t cbLock)
+{
+    Assert(offLock >= 0);
+
+    /* Check arguments. */
+    if (fLock & ~RTFILE_LOCK_MASK)
+    {
+        AssertMsgFailed(("Invalid fLock=%08X\n", fLock));
+        return VERR_INVALID_PARAMETER;
+    }
+
+    /*
+     * Validate offset.
+     */
+    if (    sizeof(off_t) < sizeof(cbLock)
+        &&  (    (offLock >> 32) != 0
+             ||  (cbLock >> 32) != 0
+             ||  ((offLock + cbLock) >> 32) != 0))
+    {
+        AssertMsgFailed(("64-bit file i/o not supported! offLock=%lld cbLock=%lld\n", offLock, cbLock));
+        return VERR_NOT_SUPPORTED;
+    }
+
+    /* Prepare flock structure. */
+    struct flock fl;
+    Assert(RTFILE_LOCK_WRITE);
+    fl.l_type   = (fLock & RTFILE_LOCK_WRITE) ? F_WRLCK : F_RDLCK;
+    fl.l_whence = SEEK_SET;
+    fl.l_start  = (off_t)offLock;
+    fl.l_len    = (off_t)cbLock;
+    fl.l_pid    = 0;
+
+    Assert(RTFILE_LOCK_WAIT);
+    if (fcntl(File, (fLock & RTFILE_LOCK_WAIT) ? F_SETLKW : F_SETLK, &fl) >= 0)
+        return VINF_SUCCESS;
+
+    int iErr = errno;
+    if (    iErr == EAGAIN
+        ||  iErr == EACCES)
+        return VERR_FILE_LOCK_VIOLATION;
+
+    return RTErrConvertFromErrno(iErr);
+}
+
+
+RTR3DECL(int)  RTFileChangeLock(RTFILE File, unsigned fLock, int64_t offLock, uint64_t cbLock)
+{
+    /** @todo We never returns VERR_FILE_NOT_LOCKED for now. */
+    return RTFileLock(File, fLock, offLock, cbLock);
+}
+
+
+RTR3DECL(int)  RTFileUnlock(RTFILE File, int64_t offLock, uint64_t cbLock)
+{
+    Assert(offLock >= 0);
+
+    /*
+     * Validate offset.
+     */
+    if (    sizeof(off_t) < sizeof(cbLock)
+        &&  (    (offLock >> 32) != 0
+             ||  (cbLock >> 32) != 0
+             ||  ((offLock + cbLock) >> 32) != 0))
+    {
+        AssertMsgFailed(("64-bit file i/o not supported! offLock=%lld cbLock=%lld\n", offLock, cbLock));
+        return VERR_NOT_SUPPORTED;
+    }
+
+    /* Prepare flock structure. */
+    struct flock fl;
+    fl.l_type   = F_UNLCK;
+    fl.l_whence = SEEK_SET;
+    fl.l_start  = (off_t)offLock;
+    fl.l_len    = (off_t)cbLock;
+    fl.l_pid    = 0;
+
+    if (fcntl(File, F_SETLK, &fl) >= 0)
+        return VINF_SUCCESS;
+
+    /* @todo check error codes for non existing lock. */
+    int iErr = errno;
+    if (    iErr == EAGAIN
+        ||  iErr == EACCES)
+        return VERR_FILE_LOCK_VIOLATION;
+
+    return RTErrConvertFromErrno(iErr);
+}
+
+
