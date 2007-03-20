63eb25b362a551117a325ebc17a0cad367d93271
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2007-03-20 11:51:40 +0000

    Removed obsolete todo
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@1576 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/VMM/PGMMap.cpp
+++ src/VBox/VMM/PGMMap.cpp
@@ -108,7 +108,6 @@ PGMR3DECL(int) PGMR3MapPT(PVM pVM, RTGCPTR GCPtr, size_t cb, PFNPGMRELOCATE pfnR
         pPrev = pCur;
         pCur = pCur->pNextHC;
     }
-/** @todo this needs fixing, the function must relocate on conflict, not fail! */
 
     /*
      * Check for conflicts with intermediate mappings.
