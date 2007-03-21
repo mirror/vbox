b402aaeb805c091f5320bb3922f515dad91a0482
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2007-03-21 17:08:28 +0000

    Never generate a raw trap for trap 6 faults; instruction emulation is required
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@1602 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/VMM/VMMGC/TRPMGCHandlers.cpp
+++ src/VBox/VMM/VMMGC/TRPMGCHandlers.cpp
@@ -374,7 +374,8 @@ DECLASM(int) TRPMGCTrap06Handler(PTRPM pTrpm, PCPUMCTXCORE pRegFrame)
         int rc = TRPMForwardTrap(pVM, pRegFrame, 0x6, 0, TRPM_TRAP_NO_ERRORCODE, TRPM_TRAP);
         Assert(rc == VINF_EM_RAW_GUEST_TRAP);
     }
-    return trpmGCExitTrap(pVM, VINF_EM_RAW_GUEST_TRAP, pRegFrame);
+    /* Never generate a raw trap here; it might be a monitor instruction, that requires emulation. */
+    return trpmGCExitTrap(pVM, VINF_EM_RAW_EMULATE_INSTR, pRegFrame);
 }
 
 
