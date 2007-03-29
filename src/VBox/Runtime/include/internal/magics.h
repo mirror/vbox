4475daebb5730cd8ac16f56ab303a7c286586168
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2007-03-29 18:30:31 +0000

    export it.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@1815 cfe28804-0f27-0410-a406-dd0f0b0b656f

---
+++ src/VBox/Runtime/include/internal/magics.h
@@ -0,0 +1,35 @@
+/* $Id: $ */
+/** @file
+ * InnoTek Portable Runtime - Internal Header Defining The Magic Numbers.
+ */
+
+/*
+ * Copyright (c) 2007 InnoTek Systemberatung GmbH
+ *
+ *
+ * This file is part of VirtualBox Open Source Edition (OSE), as
+ * available from http://www.virtualbox.org. This file is free software;
+ * you can redistribute it and/or modify it under the terms of the GNU
+ * General Public License as published by the Free Software Foundation,
+ * in version 2 as it comes in the "COPYING" file of the VirtualBox OSE
+ * distribution. VirtualBox OSE is distributed in the hope that it will
+ * be useful, but WITHOUT ANY WARRANTY of any kind.
+ *
+ * If you received this file as part of a commercial VirtualBox
+ * distribution, then only the terms of your commercial VirtualBox
+ * license agreement apply instead of the previous paragraph.
+ *
+ */
+
+#ifndef INCLUDED_internal_magics_h
+#define INCLUDED_internal_magics_h
+
+/** @name Magic Numbers. 
+ * @{ */
+
+/** Magic number for timer handles. (Jared Mason Diamond) */
+#define RTTIMER_MAGIC   0x19370910
+
+/** @} */
+
+#endif
