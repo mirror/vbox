86ecf0cf8c781414280d4eb1dc67d220f31c84cc
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2007-03-30 08:25:09 +0000

    Correct.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@1820 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/VMM/VMMGC/CPUMGC.cpp
+++ src/VBox/VMM/VMMGC/CPUMGC.cpp
@@ -92,9 +92,8 @@ DECLCALLBACK(int) cpumGCHandleNPAndGP(PVM pVM, PCPUMCTXCORE pRegFrame, uintptr_t
             PCPUMCTXCORE  pGstCtxCore = CPUMCTX2CORE(&pVM->cpum.s.Guest);
             uint32_t     *pEsp = (uint32_t *)pRegFrame->esp;
 
-#if 0       /** @todo r=bird: I think we're also missing the bits of the state that's already. I can't test this, so I've left the fix disabled for now. */
+            /* Sync general purpose registers */
             *pGstCtxCore = *pRegFrame;
-#endif
 
             pGstCtxCore->eip        = *pEsp++;
             pGstCtxCore->cs         = (RTSEL)*pEsp++;
