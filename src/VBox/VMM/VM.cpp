30ceeb6a9916509f3c6bddce9eb0440586a583f2
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2007-04-02 19:31:04 +0000

    X2/GIP debugging code
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@1868 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/VMM/VM.cpp
+++ src/VBox/VMM/VM.cpp
@@ -1272,6 +1272,22 @@ static DECLCALLBACK(int) vmR3PowerOff(PVM pVM)
             }
         }
 #endif
+#if 1 /* for debugging problems with the async GIP code on linux */
+        if (    g_pSUPGlobalInfoPage 
+            &&  g_pSUPGlobalInfoPage->u32Mode == SUPGIPMODE_ASYNC_TSC)
+        {
+            RTLogRelPrintf("**** Async GIP (the values should be somewhat similar) ****\n");
+            SUPGLOBALINFOPAGE GipCopy = *g_pSUPGlobalInfoPage;
+            for (unsigned i = 0; i < RT_ELEMENTS(GipCopy.aCPUs); i++)
+                if (GipCopy.aCPUs[i].u64CpuHz != 0 && GipCopy.aCPUs[i].u64CpuHz < _1T)
+                    RTLogRelPrintf("%#d: u64CpuHz=%RU64Hz u32TransactionId=%#x u64TSC=%RX64 u64NanoTS=%RX64n", 
+                                   i, 
+                                   GipCopy.aCPUs[i].u64CpuHz,
+                                   GipCopy.aCPUs[i].u32TransactionId,
+                                   GipCopy.aCPUs[i].u64TSC,
+                                   GipCopy.aCPUs[i].u64NanoTS);
+        }
+#endif 
         RTLogRelPrintf("************** End of Guest state at power off ***************\n");
     }
 
