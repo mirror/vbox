bfdbe69ab930c0abcf93528679d228b5c2d04f52
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2007-04-04 06:08:36 +0000

    attempt to fix tinderboxes: remove r20142, r20145, r20146
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@1907 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/VMM/EM.cpp
+++ src/VBox/VMM/EM.cpp
@@ -1067,11 +1067,13 @@ static int emR3RawExecuteInstructionWorker(PVM pVM, int rcGC)
                     Log(("PATCH: IF=1 -> emulate last instruction as it can't be interrupted!!\n"));
                     return emR3RawExecuteInstruction(pVM, "PATCHIR");
                 }
+#if 0 /** @note no noticable change; revisit later when we can emulate iret ourselves. */
                 else if (rcGC == VINF_PATM_PENDING_IRQ_AFTER_IRET)
                 {
                     /* special case: iret, that sets IF,  detected a pending irq/event */
                     return emR3RawExecuteInstruction(pVM, "PATCHIRET");
                 }
+#endif
                 return VINF_EM_RESCHEDULE_REM;
 
             /*
