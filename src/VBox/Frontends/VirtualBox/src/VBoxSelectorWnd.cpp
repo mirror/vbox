f2cd72a37e9e0779303ff69a1b10db23bbd3c131
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2007-04-20 08:32:07 +0000

    show pdf help file on Mac
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@2257 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Frontends/VirtualBox/src/VBoxSelectorWnd.cpp
+++ src/VBox/Frontends/VirtualBox/src/VBoxSelectorWnd.cpp
@@ -933,19 +933,23 @@ void VBoxSelectorWnd::refreshVMItem (const QUuid &aID, bool aDetails,
 
 void VBoxSelectorWnd::showHelpContents()
 {
+#ifndef VBOX_OSE
 #if defined (Q_WS_WIN32)
     QString fullHelpFilePath = qApp->applicationDirPath() + "/VirtualBox.chm";
 
     HtmlHelp (GetDesktopWindow(), fullHelpFilePath.ucs2(),
               HH_DISPLAY_TOPIC, NULL);
 #elif defined (Q_WS_X11)
-# ifndef VBOX_OSE
     QString fullProgPath = qApp->applicationDirPath();
     QProcess kchmViewer (fullProgPath + "/kchmviewer");
     kchmViewer.addArgument (fullProgPath + "/VirtualBox.chm");
     kchmViewer.launch ("");
-# endif
+#elif defined (Q_WS_MAC)
+    QProcess openApp (QString("/usr/bin/open"));
+    openApp.addArgument (qApp->applicationDirPath() + "/UserManual.pdf");
+    openApp.launch ("");
 #endif
+#endif /* VBOX_OSE */
 }
 
 // Protected members
