fc081412f5ef149595a9621865c4190d7df6adc9
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2007-04-21 00:21:08 +0000

    MMHyperR3ToGC
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@2301 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/VMM/IOM.cpp
+++ src/VBox/VMM/IOM.cpp
@@ -1190,7 +1190,7 @@ IOMR3DECL(int)  IOMR3MMIORegisterR3(PVM pVM, PPDMDEVINS pDevIns, RTGCPHYS GCPhys
         int rc = PGMR3HandlerPhysicalRegister(pVM, PGMPHYSHANDLERTYPE_MMIO, GCPhysStart, GCPhysStart + (cbRange - 1),
                                               /*IOMR3MMIOHandler*/ NULL, pRange,
                                               NULL, "IOMMMIOHandler", MMHyperR3ToR0(pVM, pRange),
-                                              NULL, "IOMMMIOHandler", MMHyperHC2GC(pVM, pRange), pszDesc);
+                                              NULL, "IOMMMIOHandler", MMHyperR3ToGC(pVM, pRange), pszDesc);
         if (VBOX_SUCCESS(rc))
         {
             if (RTAvlroGCPhysInsert(&pVM->iom.s.pTreesHC->MMIOTreeR3, &pRange->Core))
