d77ceea88c2f2ccba048a92d49ad97c0bd39a0b5
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2007-04-26 20:59:50 +0000

    watch all cpus.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@2372 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/HostDrivers/Support/testcase/tstGIP-2.cpp
+++ src/VBox/HostDrivers/Support/testcase/tstGIP-2.cpp
@@ -61,7 +61,7 @@ int main(void)
             {
                 for (unsigned iCpu = 0; iCpu < RT_ELEMENTS(g_pSUPGlobalInfoPage->aCPUs); iCpu++)
                     if (    g_pSUPGlobalInfoPage->aCPUs[iCpu].u64CpuHz > 0 
-                        &&  g_pSUPGlobalInfoPage->aCPUs[iCpu].u64CpuHz != _4G)
+                        &&  g_pSUPGlobalInfoPage->aCPUs[iCpu].u64CpuHz != _4G + 1)
                         RTPrintf("tstGIP-2: %2d/%d: %016llx %016llx %08x %d %08x %15llu %08x %08x %08x %08x %08x %08x %08x %08x (%d)\n",
                                  i, iCpu,
                                  g_pSUPGlobalInfoPage->aCPUs[iCpu].u64NanoTS,
