8323a304d95341a6af7455780401d5683c0f8dd9
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2007-05-14 09:20:44 +0000

    export to OSE
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@2615 cfe28804-0f27-0410-a406-dd0f0b0b656f

---
+++ src/VBox/Runtime/generic/RTTimeLocalNow-generic.cpp
@@ -0,0 +1,42 @@
+/* $Id $ */
+/** @file
+ * InnoTek Portable Runtime - Time, generic RTTimeLocalNow.
+ */
+
+/*
+ * Copyright (C) 2006 InnoTek Systemberatung GmbH
+ *
+ * This file is part of VirtualBox Open Source Edition (OSE), as
+ * available from http://www.virtualbox.org. This file is free software;
+ * you can redistribute it and/or modify it under the terms of the GNU
+ * General Public License as published by the Free Software Foundation,
+ * in version 2 as it comes in the "COPYING" file of the VirtualBox OSE
+ * distribution. VirtualBox OSE is distributed in the hope that it will
+ * be useful, but WITHOUT ANY WARRANTY of any kind.
+ *
+ * If you received this file as part of a commercial VirtualBox
+ * distribution, then only the terms of your commercial VirtualBox
+ * license agreement apply instead of the previous paragraph.
+ */
+
+
+/*******************************************************************************
+*   Header Files                                                               *
+*******************************************************************************/
+#include <iprt/time.h>
+
+
+RTDECL(PRTTIMESPEC) RTTimeLocalNow(PRTTIMESPEC pTime)
+{
+    int64_t i64PreDelta;
+    int64_t i64PostDelta;
+    do
+    {
+        i64PreDelta = RTTimeLocalDeltaNano();
+        RTTimeNow(pTime);
+        i64PostDelta = RTTimeLocalDeltaNano();
+    } while (i64PreDelta != i64PostDelta);
+
+    return RTTimeSpecAddNano(pTime, i64PostDelta);
+}
+
