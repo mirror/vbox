87db94d0da62b979e2c008c3f3c56695cacd9f19
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2007-05-14 19:11:40 +0000

    nls
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@2634 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Main/nls/VBoxSVC_de.ts
+++ src/VBox/Main/nls/VBoxSVC_de.ts
@@ -3,11 +3,11 @@
     <name>VirtualBox</name>
     <message>
         <source>The machine with the given UUID could not be found</source>
-        <translation type="unfinished"></translation>
+        <translation>Die Maschine mit der angegebenen UUID konnte nicht gefunden werden</translation>
     </message>
     <message>
         <source>The machine named `%ls&apos; could not be found</source>
-        <translation type="unfinished"></translation>
+        <translation>Die Maschine &quot;%ls&quot; konnte nicht gefunden werden</translation>
     </message>
 </context>
 </TS>
