c72360fef9a83bcc6f0e0f35345c1d3a41f76125
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2007-05-18 14:33:00 +0000

    One other case where we should check if any forced actions are pending before forwarding a trap or interrupt.
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@2716 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/VMM/EM.cpp
+++ src/VBox/VMM/EM.cpp
@@ -2036,6 +2036,9 @@ DECLINLINE(int) emR3RawHandleRC(PVM pVM, PCPUMCTX pCtx, int rc)
                     /** If it was successful, then we could go back to raw mode. */
                     if (TRPMR3GetGuestTrapHandler(pVM, u8Interrupt) != TRPM_INVALID_HANDLER)
                     {
+                        /* Must check pending forced actions as our IDT or GDT might be out of sync */
+                        EMR3CheckRawForcedActions(pVM);
+
                         rc = TRPMForwardTrap(pVM, CPUMCTX2CORE(pCtx), u8Interrupt, uErrorCode, enmError, TRPM_TRAP);
                         if (rc == VINF_SUCCESS /* Don't use VBOX_SUCCESS */)
                         {
