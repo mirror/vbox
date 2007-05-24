02797021db48a49bbad3855ee33db70d9d521ab2
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2007-05-24 18:48:00 +0000

    typo
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@2853 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Main/include/USBProxyService.h
+++ src/VBox/Main/include/USBProxyService.h
@@ -236,7 +236,7 @@ private:
     CFRunLoopRef mServiceRunLoopRef;
     /** The opaque value returned by DarwinSubscribeUSBNotifications. */
     void *mNotifyOpaque;
-    /** A hack to work around the problem with the usb device enumration 
+    /** A hack to work around the problem with the usb device enumeration 
      * not including newly attached devices. */
     bool mWaitABitNextTime;
 };
