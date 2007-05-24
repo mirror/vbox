25e341ff57a05017940386e19fd86ecc9190bbe8
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2007-05-24 16:26:39 +0000

    Backed out 21480
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@2847 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- include/VBox/usblib.h
+++ include/VBox/usblib.h
@@ -287,9 +287,8 @@ VBOXDDU_DECL(int) usbLibCaptureDevice(USHORT usVendorId, USHORT usProductId, USH
  * @param usVendorId        Vendor id
  * @param usProductId       Product id
  * @param usRevision        Revision
- * @param pszAddress        Device address
  */
-VBOXDDU_DECL(int) usbLibReleaseDevice(USHORT usVendorId, USHORT usProductId, USHORT usRevision, const char *pszAddress);
+VBOXDDU_DECL(int) usbLibReleaseDevice(USHORT usVendorId, USHORT usProductId, USHORT usRevision);
 
 
 /** @} */
