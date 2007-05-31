14b3c444a5d3e3a540ef46fb9dcf4ad9c0da805e
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2007-05-31 18:17:06 +0000

    Export
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@2966 cfe28804-0f27-0410-a406-dd0f0b0b656f

---
+++ src/VBox/Runtime/darwin/RTErrConvertFromDarwinIO.cpp
@@ -0,0 +1,48 @@
+/* $Id $ */
+/** @file
+ * InnoTek Portable Runtime - Convert Darwin IOKit returns codes to iprt status codes.
+ */
+
+/*
+ * Copyright (C) 2006 InnoTek Systemberatung GmbH
+ *
+ * This file is part of VirtualBox Open Source Edition (OSE), as
+ * available from http://www.virtualbox.org. This file is free software;
+ * you can redistribute it and/or modify it under the terms of the GNU
+ * General Public License as published by the Free Software Foundation,
+ * in version 2 as it comes in the "COPYING" file of the VirtualBox OSE
+ * distribution. VirtualBox OSE is distributed in the hope that it will
+ * be useful, but WITHOUT ANY WARRANTY of any kind.
+ *
+ * If you received this file as part of a commercial VirtualBox
+ * distribution, then only the terms of your commercial VirtualBox
+ * license agreement apply instead of the previous paragraph.
+ */
+
+/*******************************************************************************
+*   Header Files                                                               *
+*******************************************************************************/
+#include <IOKit/IOReturn.h>
+
+#include <iprt/err.h>
+#include <iprt/assert.h>
+
+
+RTDECL(int) RTErrConvertFromDarwinIO(int iNativeCode)
+{
+    /*
+     * 'optimzied' success case.
+     */
+    if (iNativeCode == kIOReturnSuccess)
+        return VINF_SUCCESS;
+
+    switch (iNativeCode)
+    {
+        case kIOReturnNoDevice:     return VERR_IO_BAD_UNIT;
+    }
+
+    /* unknown error. */
+    AssertMsgFailed(("Unhandled error %#x\n", iNativeCode));
+    return VERR_UNRESOLVED_ERROR;
+}
+
