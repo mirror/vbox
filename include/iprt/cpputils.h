b43b9c27099a22864bebb5e2cc9f065018ef5c1d
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2007-06-04 08:40:22 +0000

    export it.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@3008 cfe28804-0f27-0410-a406-dd0f0b0b656f

---
+++ include/iprt/cpputils.h
@@ -0,0 +1,61 @@
+/** @file
+ * innotek Portable Runtime - C++ Utilities (useful templates, defines and such).
+ */
+
+/*
+ * Copyright (C) 2006-2007 innotek GmbH
+ *
+ * This file is part of VirtualBox Open Source Edition (OSE), as
+ * available from http://www.virtualbox.org. This file is free software;
+ * you can redistribute it and/or modify it under the terms of the GNU
+ * General Public License as published by the Free Software Foundation,
+ * in version 2 as it comes in the "COPYING" file of the VirtualBox OSE
+ * distribution. VirtualBox OSE is distributed in the hope that it will
+ * be useful, but WITHOUT ANY WARRANTY of any kind.
+ *
+ * If you received this file as part of a commercial VirtualBox
+ * distribution, then only the terms of your commercial VirtualBox
+ * license agreement apply instead of the previous paragraph.
+ */
+
+#ifndef ___iprt_cpputils_h___
+#define ___iprt_cpputils_h___
+
+/** @defgroup grp_rt_cpputils   C++ Utilities
+ * @ingroup grp_rt
+ * @{
+ */
+
+/**
+ * Shortcut to |const_cast<C &>()| that automatically derives the correct
+ * type (class) for the const_cast template's argument from its own argument.
+ * Can be used to temporarily cancel the |const| modifier on the left-hand side
+ * of assignment expressions, like this:
+ * @code
+ *      const Class that;
+ *      ...
+ *      unconst (that) = some_value;
+ * @endcode
+ */
+template <class C>
+inline C &unconst (const C &that) { return const_cast <C &> (that); }
+
+
+/**
+ * Shortcut to |const_cast<C *>()| that automatically derives the correct
+ * type (class) for the const_cast template's argument from its own argument.
+ * Can be used to temporarily cancel the |const| modifier on the left-hand side
+ * of assignment expressions, like this:
+ * @code
+ *      const Class *that;
+ *      ...
+ *      unconst (that) = some_value;
+ * @endcode
+ */
+template <class C>
+inline C *unconst (const C *that) { return const_cast <C *> (that); }
+
+/** @} */
+
+#endif
+
