5ae315d817cac255649e139a2bd348edcd0884f2
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2007-06-06 12:02:34 +0000

    Dual cursor issue fixed.
    Issue description: If the GuestOS is loaded (in case of "Guest Additions" are correctly installed and activated and "Mouse integration" feature is activated too) and the mouse cursor was not moved during the guest loading process then the user can see two mouse cursors - guest's and host's, which are positioned separately. If the user moves the mouse cursor above the VM Console Viewport, both cursors will be positioned one-above-one. It looks like some dirty visual artifact.
    
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@3063 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Frontends/VirtualBox/src/VBoxConsoleView.cpp
+++ src/VBox/Frontends/VirtualBox/src/VBoxConsoleView.cpp
@@ -439,7 +439,7 @@ public:
             return E_POINTER;
 
 #if defined (Q_WS_MAC)
-        /* 
+        /*
          * Let's try the simple approach first - grab the focus.
          * Getting a window out of the dock (minimized or whatever it's called)
          * needs to be done on the GUI thread, so post it a note.
@@ -448,15 +448,15 @@ public:
         if (!mView)
             return S_OK;
 
-        ProcessSerialNumber psn = { 0, kCurrentProcess }; 
+        ProcessSerialNumber psn = { 0, kCurrentProcess };
         OSErr rc = ::SetFrontProcess (&psn);
-        if (!rc) 
+        if (!rc)
             QApplication::postEvent (mView, new QEvent ((QEvent::Type)VBoxDefs::ShowWindowEventType));
         else
         {
             /*
              * It failed for some reason, send the other process our PSN so it can try.
-             * (This is just a precaution should Mac OS X start imposing the same sensible 
+             * (This is just a precaution should Mac OS X start imposing the same sensible
              * focus stealing restrictions that other window managers implement.)
              */
             AssertMsgFailed(("SetFrontProcess -> %#x\n", rc));
@@ -467,7 +467,7 @@ public:
 #else
         /* Return the ID of the top-level console window. */
         *winId = (ULONG64) mView->topLevelWidget()->winId();
-#endif 
+#endif
 
         return S_OK;
     }
@@ -946,7 +946,11 @@ bool VBoxConsoleView::event (QEvent *e)
                     /* correct the mouse capture state and reset the cursor
                      * to the default shape if necessary */
                     if (mouse_absolute)
+                    {
+                        CMouse mouse = cconsole.GetMouse();
+                        mouse.PutMouseEventAbsolute (-1, -1, 0, 0);
                         captureMouse (false, false);
+                    }
                     else
                         viewport()->unsetCursor();
                     emitMouseStateChanged();
@@ -1107,20 +1111,20 @@ bool VBoxConsoleView::event (QEvent *e)
             /* posted OnShowWindow */
             case VBoxDefs::ShowWindowEventType:
             {
-                /* 
-                 *  Dunno what Qt3 thinks a window that has minimized to the dock 
+                /*
+                 *  Dunno what Qt3 thinks a window that has minimized to the dock
                  *  should be - it is not hidden, neither is it minimized. OTOH it is
-                 *  marked shown and visible, but not activated. This latter isn't of 
+                 *  marked shown and visible, but not activated. This latter isn't of
                  *  much help though, since at this point nothing is marked activated.
                  *  I might have overlooked something, but I'm buggered what if I know
-                 *  what. So, I'll just always show & activate the stupid window to 
+                 *  what. So, I'll just always show & activate the stupid window to
                  *  make it get out of the dock when the user wishes to show a VM.
                  */
                 topLevelWidget()->show();
                 topLevelWidget()->setActiveWindow();
                 return true;
             }
-#endif 
+#endif
             default:
                 break;
         }
