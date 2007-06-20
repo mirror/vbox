379baf5139d054f95c41cf86ae7bc525a0c8b447
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2007-06-20 09:14:10 +0000

    compile fix
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@3172 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/VMM/VMMR0/HWSVMR0.cpp
+++ src/VBox/VMM/VMMR0/HWSVMR0.cpp
@@ -1303,10 +1303,6 @@ ResumeExecution:
             Log2(("EM status from IO at %VGv %x size %d: %Vrc\n", pCtx->eip, IoExitInfo.n.u16Port, uIOSize, rc));
             break;
         }
-        /* Force instruction emulation and not a reschedule to the recompiler as that will come right back to us */
-        if (rc == VINF_EM_RESCHEDULE_REM)
-            rc = VINF_IOM_HC_IOPORT_READWRITE;
-
 #ifdef VBOX_STRICT
         if (rc == VINF_IOM_HC_IOPORT_READ)
             Assert(IoExitInfo.n.u1Type != 0);
@@ -1315,6 +1311,9 @@ ResumeExecution:
         else
             AssertMsg(VBOX_FAILURE(rc) || rc == VINF_EM_RAW_GUEST_TRAP || rc == VINF_TRPM_XCPT_DISPATCHED || rc == VINF_EM_RESCHEDULE_REM, ("%Vrc\n", rc));
 #endif 
+        /* Force instruction emulation and not a reschedule to the recompiler as that will come right back to us */
+        if (rc == VINF_EM_RESCHEDULE_REM)
+            rc = VINF_IOM_HC_IOPORT_WRITE;
         Log2(("Failed IO at %VGv %x size %d\n", pCtx->eip, IoExitInfo.n.u16Port, uIOSize));
         break;
     }
