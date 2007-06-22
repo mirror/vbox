5d60044ef6d194f481863e5e803b81c1dc4ee3a4
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2007-06-22 17:05:24 +0000

    Backed out 22205 & 22203
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@3235 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Additions/common/VBoxGuestLib/VBoxCalls.c
+++ src/VBox/Additions/common/VBoxGuestLib/VBoxCalls.c
@@ -199,13 +199,6 @@ DECLVBGL(int) vboxCallMapFolder(PVBSFCLIENT pClient, PSHFLSTRING szFolderName,
     data.delimiter.type               = VMMDevHGCMParmType_32bit;
     data.delimiter.u.value32          = RTPATH_DELIMITER;
 
-    data.fCaseSensitive.type          = VMMDevHGCMParmType_32bit;
-#if defined(__OS2__) || defined(__WIN__)
-    data.fCaseSensitive.u.value32     = 0;
-#else
-    data.fCaseSensitive.u.value32     = 1;
-#endif
-
     rc = VbglHGCMCall (pClient->handle, &data.callInfo, sizeof (data));
 
 /*    Log(("VBOXSF: VBoxSF::vboxCallMapFolder: "
