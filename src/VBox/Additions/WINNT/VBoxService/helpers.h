676f0ffa45054b9ba7eb62ed065d7099dab5e99f
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2007-06-25 07:39:42 +0000

    export files
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@3260 cfe28804-0f27-0410-a406-dd0f0b0b656f

---
+++ src/VBox/Additions/WINNT/VBoxService/helpers.h
@@ -0,0 +1,38 @@
+/** @file
+ * helpers - Guest Additions Service helper functions header
+ */
+
+/*
+ * Copyright (C) 2006-2007 innotek GmbH
+ *
+ * This file is part of VirtualBox Open Source Edition (OSE), as
+ * available from http://www.virtualbox.org. This file is free software;
+ * you can redistribute it and/or modify it under the terms of the GNU
+ * General Public License as published by the Free Software Foundation,
+ * in version 2 as it comes in the "COPYING" file of the VirtualBox OSE
+ * distribution. VirtualBox OSE is distributed in the hope that it will
+ * be useful, but WITHOUT ANY WARRANTY of any kind.
+ *
+ * If you received this file as part of a commercial VirtualBox
+ * distribution, then only the terms of your commercial VirtualBox
+ * license agreement apply instead of the previous paragraph.
+ *
+ */
+
+#ifndef __VBOXSERVICEHELPERS__H
+#define __VBOXSERVICEHELPERS__H
+
+// #define DEBUG_DISPLAY_CHANGE
+
+#ifndef DDCLOG
+void SvcDebugOut2(char *String, ...);
+# ifdef DEBUG_DISPLAY_CHANGE
+#  define DDCLOG(a) do { SvcDebugOut2 a; } while (0)
+# else
+#  define DDCLOG(a) do {} while (0)
+# endif /* DEBUG_DISPLAY_CHANGE */
+#endif /* DDCLOG */
+
+void resizeRect(RECTL *paRects, unsigned nRects, unsigned iPrimary, unsigned iResized, int NewWidth, int NewHeight);
+
+#endif /* __VBOXSERVICEHELPERS__H */
