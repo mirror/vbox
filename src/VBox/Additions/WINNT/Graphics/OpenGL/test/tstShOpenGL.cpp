c54059aa3e5e5e5c0561e5cde78f9c0b208a3098
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2007-07-02 14:29:57 +0000

    Link with opengl.lib
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@3352 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Additions/WINNT/Graphics/OpenGL/test/tstShOpenGL.cpp
+++ src/VBox/Additions/WINNT/Graphics/OpenGL/test/tstShOpenGL.cpp
@@ -21,15 +21,58 @@
 #include <GL/gl.h>
 #include <stdio.h>
 
+
+
+
+// Enable OpenGL
+
+void EnableOpenGL(HWND hWnd, HDC * hDC, HGLRC * hRC)
+{
+	PIXELFORMATDESCRIPTOR pfd;
+	int format;
+	
+	// get the device context (DC)
+	*hDC = GetDC( hWnd );
+	
+	// set the pixel format for the DC
+	ZeroMemory( &pfd, sizeof( pfd ) );
+	pfd.nSize = sizeof( pfd );
+	pfd.nVersion = 1;
+	pfd.dwFlags = PFD_DRAW_TO_WINDOW | PFD_SUPPORT_OPENGL | PFD_DOUBLEBUFFER;
+	pfd.iPixelType = PFD_TYPE_RGBA;
+	pfd.cColorBits = 24;
+	pfd.cDepthBits = 16;
+	pfd.iLayerType = PFD_MAIN_PLANE;
+	format = ChoosePixelFormat( *hDC, &pfd );
+	SetPixelFormat( *hDC, format, &pfd );
+	
+	// create and enable the render context (RC)
+	*hRC = wglCreateContext( *hDC );
+	wglMakeCurrent( *hDC, *hRC );
+	
+}
+
+// Disable OpenGL
+
+void DisableOpenGL(HWND hWnd, HDC hDC, HGLRC hRC)
+{
+	wglMakeCurrent( NULL, NULL );
+	wglDeleteContext( hRC );
+	ReleaseDC( hWnd, hDC );
+}
+
 int main(int argc, char **argv)
 {
+    HDC hdc;
+    HGLRC hglrc;
+
+    EnableOpenGL(0, &hdc, &hglrc);
+
     printf("GL_VENDOR       %s\n", glGetString(GL_VENDOR));
     printf("GL_RENDERER     %s\n", glGetString(GL_RENDERER));
     printf("GL_VERSION      %s\n", glGetString(GL_VERSION));
     printf("GL_EXTENSIONS   %s\n", glGetString(GL_EXTENSIONS));
 
-    glEnable(GL_BLEND);
-    glFlush();
+    DisableOpenGL(0, hdc, hglrc);
     return 0;
 }
-
