ce2a3e9800206cdabfc6b1b2ed4dda1fcb7638e7
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2007-07-05 09:00:44 +0000

    Window creation needs to be done in DrvSetPixelFormat
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@3438 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Additions/WINNT/Graphics/OpenGL/drv.cpp
+++ src/VBox/Additions/WINNT/Graphics/OpenGL/drv.cpp
@@ -91,8 +91,190 @@ PICDTABLE APIENTRY DrvSetContext(HDC hdc, HGLRC hglrc, void *callback)
     return &icdTable;
 }
 
+/* DrvSetPixelFormat can only be called once for each window (if hdc associated with one). */
+BOOL APIENTRY DrvSetPixelFormat(HDC hdc, int iPixelFormat)
+{
+    uint32_t cx, cy;
+    HWND hwnd;
+
+    /** check dimensions and pixel format of hdc */
+    hwnd = WindowFromDC(hdc);
+    if (hwnd)
+    {
+        RECT rect;
+
+        GetClientRect(hwnd, &rect);
+        cx = rect.right - rect.left;
+        cy = rect.bottom - rect.top;
+    }
+    else
+    {
+        /** @todo get dimenions of memory dc; a bitmap should be selected in there */
+        AssertFailed();
+    }
+
+    VBOX_OGL_GEN_SYNC_OP4_RET(BOOL, DrvSetPixelFormat, hdc, iPixelFormat, cx, cy);
+    /* Propagate error through the right channel */
+    SetLastError(glGetError());
+    return retval;
+}
+
+HGLRC APIENTRY DrvCreateContext(HDC hdc)
+{
+    VBOX_OGL_GEN_SYNC_OP1_RET(HGLRC, DrvCreateContext, hdc);
+    /* Propagate error through the right channel */
+    SetLastError(glGetError());
+    return retval;
+}
+
+HGLRC APIENTRY DrvCreateLayerContext(HDC hdc, int iLayerPlane)
+{
+    VBOX_OGL_GEN_SYNC_OP2_RET(HGLRC, DrvCreateLayerContext, hdc, iLayerPlane);
+    /* Propagate error through the right channel */
+    SetLastError(glGetError());
+    return retval;
+}
+
+BOOL APIENTRY DrvDescribeLayerPlane(HDC hdc,int iPixelFormat,
+                                    int iLayerPlane, UINT nBytes,
+                                    LPLAYERPLANEDESCRIPTOR plpd)
+{
+    VBOX_OGL_GEN_SYNC_OP5_PASS_PTR_RET(BOOL, DrvDescribeLayerPlane, hdc, iPixelFormat, iLayerPlane, nBytes, nBytes, plpd);
+    /* Propagate error through the right channel */
+    SetLastError(glGetError());
+    return retval;
+}
+
+int APIENTRY DrvGetLayerPaletteEntries(HDC hdc, int iLayerPlane,
+                                       int iStart, int cEntries,
+                                       COLORREF *pcr)
+{
+    VBOX_OGL_GEN_SYNC_OP5_PASS_PTR_RET(int, DrvGetLayerPaletteEntries, hdc, iLayerPlane, iStart, cEntries, sizeof(COLORREF)*cEntries, pcr);
+    /* Propagate error through the right channel */
+    SetLastError(glGetError());
+    return retval;
+}
+
+int APIENTRY DrvDescribePixelFormat(HDC hdc, int iPixelFormat, UINT nBytes, LPPIXELFORMATDESCRIPTOR ppfd)
+{
+    /* if ppfd == NULL, then DrvDescribelayerPlane returns the maximum nr of supported pixel formats */
+    VBOX_OGL_GEN_SYNC_OP4_PASS_PTR_RET(int, DrvDescribePixelFormat, hdc, iPixelFormat, nBytes, nBytes, ppfd);
+    /* Propagate error through the right channel */
+    SetLastError(glGetError());
+    return retval;
+}
+
+
+BOOL APIENTRY DrvDeleteContext(HGLRC hglrc)
+{
+    VBOX_OGL_GEN_SYNC_OP1_RET(BOOL, DrvDeleteContext, hglrc);
+    /* Propagate error through the right channel */
+    SetLastError(glGetError());
+    return retval;
+}
+
+BOOL APIENTRY DrvCopyContext(HGLRC hglrcSrc, HGLRC hglrcDst, UINT mask)
+{
+    VBOX_OGL_GEN_SYNC_OP3_RET(BOOL, DrvCopyContext, hglrcSrc, hglrcDst, mask);
+    /* Propagate error through the right channel */
+    SetLastError(glGetError());
+    return retval;
+}
+
+void APIENTRY DrvReleaseContext(HGLRC hglrc)
+{
+    VBOX_OGL_GEN_SYNC_OP1(DrvReleaseContext, hglrc);
+    /* Propagate error through the right channel */
+    SetLastError(glGetError());
+}
+
+BOOL APIENTRY DrvShareLists(HGLRC hglrc1, HGLRC hglrc2)
+{
+    VBOX_OGL_GEN_SYNC_OP2_RET(BOOL, DrvShareLists, hglrc1, hglrc2);
+    /* Propagate error through the right channel */
+    SetLastError(glGetError());
+    return retval;
+}
+
+int APIENTRY DrvSetLayerPaletteEntries(HDC hdc, int iLayerPlane,
+                                       int iStart, int cEntries,
+                                       CONST COLORREF *pcr)
+{
+    VBOX_OGL_GEN_SYNC_OP5_PTR_RET(int, DrvSetLayerPaletteEntries, hdc, iLayerPlane, iStart, cEntries, sizeof(COLORREF)*cEntries, pcr);
+    /* Propagate error through the right channel */
+    SetLastError(glGetError());
+    return retval;
+}
+
+
+BOOL APIENTRY DrvRealizeLayerPalette(HDC hdc, int iLayerPlane, BOOL bRealize)
+{
+    VBOX_OGL_GEN_SYNC_OP3_RET(BOOL, DrvRealizeLayerPalette, hdc, iLayerPlane, bRealize);
+    /* Propagate error through the right channel */
+    SetLastError(glGetError());
+    return retval;
+}
+
+BOOL APIENTRY DrvSwapLayerBuffers(HDC hdc, UINT fuPlanes)
+{
+    VBOX_OGL_GEN_SYNC_OP2_RET(BOOL, DrvSwapLayerBuffers, hdc, fuPlanes);
+    /* Propagate error through the right channel */
+    SetLastError(glGetError());
+    return retval;
+}
+
+BOOL APIENTRY DrvSwapBuffers(HDC hdc)
+{
+    VBOX_OGL_GEN_SYNC_OP1_RET(BOOL, DrvSwapBuffers, hdc);
+    /* Propagate error through the right channel */
+    SetLastError(glGetError());
+    return retval;
+}
+
 #ifdef VBOX_WITH_WGL_EXPORTS
 /* Test export for directly linking with vboxogl.dll */
+int WINAPI wglSetLayerPaletteEntries(HDC hdc, int iLayerPlane,
+                                     int iStart, int cEntries,
+                                     CONST COLORREF *pcr)
+{
+    return DrvSetLayerPaletteEntries(hdc, iLayerPlane, iStart, cEntries, pcr);
+}
+
+BOOL WINAPI wglSetPixelFormat(HDC hdc, int iPixelFormat)
+{
+    return DrvSetPixelFormat(hdc, iPixelFormat);
+}
+
+BOOL WINAPI wglRealizeLayerPalette(HDC hdc, int iLayerPlane, BOOL bRealize)
+{
+    return DrvRealizeLayerPalette(hdc, iLayerPlane, bRealize);
+}
+
+BOOL WINAPI wglDeleteContext(HGLRC hglrc)
+{
+    return DrvDeleteContext(hglrc);
+}
+
+BOOL WINAPI wglSwapLayerBuffers(HDC hdc, UINT fuPlanes)
+{
+    return DrvSwapLayerBuffers(hdc, fuPlanes);
+}
+
+BOOL WINAPI SwapBuffers(HDC hdc)
+{
+    return DrvSwapBuffers(hdc);
+}
+
+BOOL WINAPI wglCopyContext(HGLRC hglrcSrc, HGLRC hglrcDst, UINT mask)
+{
+    return DrvCopyContext(hglrcSrc, hglrcDst, mask);
+}
+
+BOOL WINAPI wglShareLists(HGLRC hglrc1, HGLRC hglrc2)
+{
+    return DrvShareLists(hglrc1, hglrc2);
+}
+
 BOOL WINAPI wglMakeCurrent(HDC hdc, HGLRC hglrc)
 {
     DrvSetContext(hdc, hglrc, NULL);
@@ -103,4 +285,29 @@ PROC WINAPI wglGetProcAddress(LPCSTR lpszProc)
 {
     return DrvGetProcAddress(lpszProc);
 }
+
+int WINAPI wglDescribePixelFormat(HDC hdc, int iPixelFormat, UINT nBytes, LPPIXELFORMATDESCRIPTOR ppfd)
+{
+    return DrvDescribePixelFormat(hdc, iPixelFormat, nBytes, ppfd);
+}
+
+BOOL WINAPI wglDescribeLayerPlane(HDC hdc,int iPixelFormat,
+                                  int iLayerPlane, UINT nBytes,
+                                  LPLAYERPLANEDESCRIPTOR plpd)
+{
+    return DrvDescribeLayerPlane(hdc, iPixelFormat, iLayerPlane, nBytes, plpd);
+}
+
+int WINAPI wglGetLayerPaletteEntries(HDC hdc, int iLayerPlane,
+                                     int iStart, int cEntries,
+                                     COLORREF *pcr)
+{
+    return DrvGetLayerPaletteEntries(hdc, iLayerPlane, iStart, cEntries, pcr);
+}
+
+HGLRC WINAPI wglCreateContext(HDC hdc)
+{
+    return DrvCreateContext(hdc);
+}
+
 #endif
