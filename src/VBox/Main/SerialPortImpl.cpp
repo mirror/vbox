caf9e7ec3a74c3789b0d5a5cd504d6607160afd9
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2007-07-09 09:16:44 +0000

    export
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@3501 cfe28804-0f27-0410-a406-dd0f0b0b656f

---
+++ src/VBox/Main/SerialPortImpl.cpp
@@ -0,0 +1,544 @@
+/** @file
+ *
+ * VirtualBox COM class implementation
+ */
+
+/*
+ * Copyright (C) 2006-2007 innotek GmbH
+ *
+ * This file is part of VirtualBox Open Source Edition (OSE), as
+ * available from http://www.virtualbox.org. This file is free software;
+ * you can redistribute it and/or modify it under the terms of the GNU
+ * General Public License as published by the Free Software Foundation,
+ * in version 2 as it comes in the "COPYING" file of the VirtualBox OSE
+ * distribution. VirtualBox OSE is distributed in the hope that it will
+ * be useful, but WITHOUT ANY WARRANTY of any kind.
+ *
+ * If you received this file as part of a commercial VirtualBox
+ * distribution, then only the terms of your commercial VirtualBox
+ * license agreement apply instead of the previous paragraph.
+ */
+
+#include "SerialPortImpl.h"
+#include "MachineImpl.h"
+#include "VirtualBoxImpl.h"
+#include "Logging.h"
+
+#include <iprt/string.h>
+#include <iprt/cpputils.h>
+
+// constructor / destructor
+/////////////////////////////////////////////////////////////////////////////
+
+DEFINE_EMPTY_CTOR_DTOR (SerialPort)
+
+HRESULT SerialPort::FinalConstruct()
+{
+    return S_OK;
+}
+
+void SerialPort::FinalRelease()
+{
+    uninit();
+}
+
+// public initializer/uninitializer for internal purposes only
+/////////////////////////////////////////////////////////////////////////////
+
+/**
+ *  Initializes the Serial Port object.
+ *
+ *  @param aParent  Handle of the parent object.
+ */
+HRESULT SerialPort::init (Machine *aParent, ULONG aSlot)
+{
+    LogFlowThisFunc (("aParent=%p, aSlot=%d\n", aParent, aSlot));
+
+    ComAssertRet (aParent, E_INVALIDARG);
+
+    /* Enclose the state transition NotReady->InInit->Ready */
+    AutoInitSpan autoInitSpan (this);
+    AssertReturn (autoInitSpan.isOk(), E_UNEXPECTED);
+
+    unconst (mParent) = aParent;
+    /* mPeer is left null */
+
+    mData.allocate();
+
+    /* initialize data */
+    mData->mSlot = aSlot;
+
+    /* Confirm a successful initialization */
+    autoInitSpan.setSucceeded();
+
+    return S_OK;
+}
+
+/**
+ *  Initializes the Serial Port object given another serial port object
+ *  (a kind of copy constructor). This object shares data with
+ *  the object passed as an argument.
+ *
+ *  @note This object must be destroyed before the original object
+ *  it shares data with is destroyed.
+ *
+ *  @note Locks @a aThat object for reading.
+ */
+HRESULT SerialPort::init (Machine *aParent, SerialPort *aThat)
+{
+    LogFlowThisFunc (("aParent=%p, aThat=%p\n", aParent, aThat));
+
+    ComAssertRet (aParent && aThat, E_INVALIDARG);
+
+    /* Enclose the state transition NotReady->InInit->Ready */
+    AutoInitSpan autoInitSpan (this);
+    AssertReturn (autoInitSpan.isOk(), E_UNEXPECTED);
+
+    unconst (mParent) = aParent;
+    unconst (mPeer) = aThat;
+
+    AutoCaller thatCaller (aThat);
+    AssertComRCReturnRC (thatCaller.rc());
+
+    AutoReaderLock thatLock (aThat);
+    mData.share (aThat->mData);
+
+    /* Confirm a successful initialization */
+    autoInitSpan.setSucceeded();
+
+    return S_OK;
+}
+
+/**
+ *  Initializes the guest object given another guest object
+ *  (a kind of copy constructor). This object makes a private copy of data
+ *  of the original object passed as an argument.
+ *
+ *  @note Locks @a aThat object for reading.
+ */
+HRESULT SerialPort::initCopy (Machine *aParent, SerialPort *aThat)
+{
+    LogFlowThisFunc (("aParent=%p, aThat=%p\n", aParent, aThat));
+
+    ComAssertRet (aParent && aThat, E_INVALIDARG);
+
+    /* Enclose the state transition NotReady->InInit->Ready */
+    AutoInitSpan autoInitSpan (this);
+    AssertReturn (autoInitSpan.isOk(), E_UNEXPECTED);
+
+    unconst (mParent) = aParent;
+    /* mPeer is left null */
+
+    AutoCaller thatCaller (aThat);
+    AssertComRCReturnRC (thatCaller.rc());
+
+    AutoReaderLock thatLock (aThat);
+    mData.attachCopy (aThat->mData);
+
+    /* Confirm a successful initialization */
+    autoInitSpan.setSucceeded();
+
+    return S_OK;
+}
+
+/**
+ *  Uninitializes the instance and sets the ready flag to FALSE.
+ *  Called either from FinalRelease() or by the parent when it gets destroyed.
+ */
+void SerialPort::uninit()
+{
+    LogFlowThisFunc (("\n"));
+
+    /* Enclose the state transition Ready->InUninit->NotReady */
+    AutoUninitSpan autoUninitSpan (this);
+    if (autoUninitSpan.uninitDone())
+        return;
+
+    mData.free();
+
+    unconst (mPeer).setNull();
+    unconst (mParent).setNull();
+}
+
+// public methods only for internal purposes
+////////////////////////////////////////////////////////////////////////////////
+
+/** 
+ *  @note Locks this object for writing.
+ */
+bool SerialPort::rollback()
+{
+    /* sanity */
+    AutoCaller autoCaller (this);
+    AssertComRCReturn (autoCaller.rc(), false);
+
+    AutoLock alock (this);
+
+    bool changed = false;
+
+    if (mData.isBackedUp())
+    {
+        /* we need to check all data to see whether anything will be changed
+         * after rollback */
+        changed = mData.hasActualChanges();
+        mData.rollback();
+    }
+
+    return changed;
+}
+
+/** 
+ *  @note Locks this object for writing, together with the peer object (also
+ *  for writing) if there is one.
+ */
+void SerialPort::commit()
+{
+    /* sanity */
+    AutoCaller autoCaller (this);
+    AssertComRCReturnVoid (autoCaller.rc());
+
+    /* sanity too */
+    AutoCaller thatCaller (mPeer);
+    AssertComRCReturnVoid (thatCaller.rc());
+
+    /* lock both for writing since we modify both */
+    AutoMultiLock <2> alock (this->wlock(), AutoLock::maybeWlock (mPeer));
+
+    if (mData.isBackedUp())
+    {
+        mData.commit();
+        if (mPeer)
+        {
+            /* attach new data to the peer and reshare it */
+            mPeer->mData.attach (mData);
+        }
+    }
+}
+
+/** 
+ *  @note Locks this object for writing, together with the peer object
+ *  represented by @a aThat (locked for reading).
+ */
+void SerialPort::copyFrom (SerialPort *aThat)
+{
+    AssertReturnVoid (aThat != NULL);
+
+    /* sanity */
+    AutoCaller autoCaller (this);
+    AssertComRCReturnVoid (autoCaller.rc());
+
+    /* sanity too */
+    AutoCaller thatCaller (mPeer);
+    AssertComRCReturnVoid (thatCaller.rc());
+
+    /* peer is not modified, lock it for reading */
+    AutoMultiLock <2> alock (this->wlock(), aThat->rlock());
+
+    /* this will back up current data */
+    mData.assignCopy (aThat->mData);
+}
+
+HRESULT SerialPort::loadSettings (CFGNODE aNode, ULONG aSlot)
+{
+    LogFlowThisFunc (("aMachine=%p\n", aNode));
+
+    AssertReturn (aNode, E_FAIL);
+
+    AutoCaller autoCaller (this);
+    AssertComRCReturnRC (autoCaller.rc());
+
+    AutoLock alock (this);
+
+    CFGNODE portNode = NULL;
+    CFGLDRGetChildNode (aNode, "Port", aSlot, &portNode);
+
+    /* slot number (required) */
+    /* slot unicity is guaranteed by XML Schema */
+    uint32_t uSlot = 0;
+    CFGLDRQueryUInt32 (portNode, "slot", &uSlot);
+    /* enabled (required) */
+    bool fEnabled = false;
+    CFGLDRQueryBool (portNode, "enabled", &fEnabled);
+    /* I/O base (required) */
+    uint32_t uIOBase;
+    CFGLDRQueryUInt32 (portNode, "IOBase", &uIOBase);
+    /* IRQ (required) */
+    uint32_t uIRQ;
+    CFGLDRQueryUInt32 (portNode, "IRQ", &uIRQ);
+    /* name of the pipe (required) */
+    Bstr pipe;
+    CFGLDRQueryBSTR   (portNode, "pipe", pipe.asOutParam());
+    bool fServer = true;
+    CFGLDRQueryBool   (portNode, "server", &fServer);
+
+    mData->mEnabled = fEnabled;
+    mData->mSlot    = uSlot;
+    mData->mIobase  = uIOBase;
+    mData->mIrq     = uIRQ;
+    mData->mPipe    = pipe;
+    mData->mServer  = fServer;
+
+    return S_OK;
+}
+
+HRESULT SerialPort::saveSettings (CFGNODE aNode)
+{
+    AssertReturn (aNode, E_FAIL);
+
+    AutoCaller autoCaller (this);
+    CheckComRCReturnRC (autoCaller.rc());
+
+    AutoReaderLock alock (this);
+
+    CFGNODE portNode = 0;
+    int vrc = CFGLDRAppendChildNode (aNode, "Port", &portNode);
+    ComAssertRCRet (vrc, E_FAIL);
+
+    CFGLDRSetUInt32 (portNode, "slot",    mData->mSlot);
+    CFGLDRSetBool   (portNode, "enabled", !!mData->mEnabled);
+    CFGLDRSetUInt32 (portNode, "IOBase",  mData->mIobase);
+    CFGLDRSetUInt32 (portNode, "IRQ",     mData->mIrq);
+    CFGLDRSetBSTR   (portNode, "pipe",    mData->mPipe);
+    CFGLDRSetBool   (portNode, "server",  !!mData->mServer);
+
+    return S_OK;
+}
+
+// ISerialPort properties
+/////////////////////////////////////////////////////////////////////////////
+
+STDMETHODIMP SerialPort::COMGETTER(Enabled) (BOOL *aEnabled)
+{
+    if (!aEnabled)
+        return E_POINTER;
+
+    AutoCaller autoCaller (this);
+    CheckComRCReturnRC (autoCaller.rc());
+
+    AutoReaderLock alock (this);
+
+    *aEnabled = mData->mEnabled;
+
+    return S_OK;
+}
+
+STDMETHODIMP SerialPort::COMSETTER(Enabled) (BOOL aEnabled)
+{
+    LogFlowThisFunc (("aEnabled=%RTbool\n", aEnabled));
+
+    AutoCaller autoCaller (this);
+    CheckComRCReturnRC (autoCaller.rc());
+
+    /* the machine needs to be mutable */
+    Machine::AutoMutableStateDependency adep (mParent);
+    CheckComRCReturnRC (adep.rc());
+
+    AutoLock alock (this);
+
+    if (mData->mEnabled != aEnabled)
+    {
+        mData.backup();
+        mData->mEnabled = aEnabled;
+
+        /* leave the lock before informing callbacks */
+        alock.unlock();
+
+        mParent->onSerialPortChange (this);
+    }
+
+    return S_OK;
+}
+
+STDMETHODIMP SerialPort::COMGETTER(Slot) (ULONG *aSlot)
+{
+    if (!aSlot)
+        return E_POINTER;
+
+    AutoCaller autoCaller (this);
+    CheckComRCReturnRC (autoCaller.rc());
+
+    AutoReaderLock alock (this);
+
+    *aSlot = mData->mSlot;
+
+    return S_OK;
+}
+
+STDMETHODIMP SerialPort::COMGETTER(Irq) (ULONG *aIrq)
+{
+    if (!aIrq)
+        return E_POINTER;
+
+    AutoCaller autoCaller (this);
+    CheckComRCReturnRC (autoCaller.rc());
+
+    AutoReaderLock alock (this);
+
+    *aIrq = mData->mIrq;
+
+    return S_OK;
+}
+
+STDMETHODIMP SerialPort::COMSETTER(Irq)(ULONG aIrq)
+{
+    AutoCaller autoCaller (this);
+    CheckComRCReturnRC (autoCaller.rc());
+
+    /* the machine needs to be mutable */
+    Machine::AutoMutableStateDependency adep (mParent);
+    CheckComRCReturnRC (adep.rc());
+
+    AutoLock alock (this);
+
+    HRESULT rc = S_OK;
+    bool emitChangeEvent = false;
+
+    if (mData->mIrq != aIrq)
+    {
+        mData.backup();
+        mData->mIrq = aIrq;
+        emitChangeEvent = true;
+    }
+
+    if (emitChangeEvent)
+    {
+        /* leave the lock before informing callbacks */
+        alock.unlock();
+
+        mParent->onSerialPortChange (this);
+    }
+
+    return rc;
+}
+
+STDMETHODIMP SerialPort::COMGETTER(Iobase) (ULONG *aIobase)
+{
+    if (!aIobase)
+        return E_POINTER;
+
+    AutoCaller autoCaller (this);
+    CheckComRCReturnRC (autoCaller.rc());
+
+    AutoReaderLock alock (this);
+
+    *aIobase = mData->mIobase;
+
+    return S_OK;
+}
+
+STDMETHODIMP SerialPort::COMSETTER(Iobase)(ULONG aIobase)
+{
+    AutoCaller autoCaller (this);
+    CheckComRCReturnRC (autoCaller.rc());
+
+    /* the machine needs to be mutable */
+    Machine::AutoMutableStateDependency adep (mParent);
+    CheckComRCReturnRC (adep.rc());
+
+    AutoLock alock (this);
+
+    HRESULT rc = S_OK;
+    bool emitChangeEvent = false;
+
+    if (mData->mIobase != aIobase)
+    {
+        mData.backup();
+        mData->mIobase = aIobase;
+        emitChangeEvent = true;
+    }
+
+    if (emitChangeEvent)
+    {
+        /* leave the lock before informing callbacks */
+        alock.unlock();
+
+        mParent->onSerialPortChange (this);
+    }
+
+    return rc;
+}
+
+STDMETHODIMP SerialPort::COMGETTER(Pipe) (BSTR *aPipe)
+{
+    if (!aPipe)
+        return E_POINTER;
+
+    AutoCaller autoCaller (this);
+    CheckComRCReturnRC (autoCaller.rc());
+
+    AutoReaderLock alock (this);
+
+    mData->mPipe.cloneTo (aPipe);
+
+    return S_OK;
+}
+
+STDMETHODIMP SerialPort::COMSETTER(Pipe) (INPTR BSTR aPipe)
+{
+    if (!aPipe || *aPipe == 0)
+        return E_INVALIDARG;
+
+    AutoCaller autoCaller (this);
+    CheckComRCReturnRC (autoCaller.rc());
+
+    /* the machine needs to be mutable */
+    Machine::AutoMutableStateDependency adep (mParent);
+    CheckComRCReturnRC (adep.rc());
+
+    AutoLock alock (this);
+
+    if (mData->mPipe != aPipe)
+    {
+        mData.backup();
+        mData->mPipe = aPipe;
+
+        /* leave the lock before informing callbacks */
+        alock.unlock();
+
+        return mParent->onSerialPortChange (this);
+    }
+
+    return S_OK;
+}
+
+STDMETHODIMP SerialPort::COMGETTER(Server) (BOOL *aServer)
+{
+    if (!aServer)
+        return E_POINTER;
+
+    AutoCaller autoCaller (this);
+    CheckComRCReturnRC (autoCaller.rc());
+
+    AutoReaderLock alock (this);
+
+    *aServer = mData->mServer;
+
+    return S_OK;
+}
+
+STDMETHODIMP SerialPort::COMSETTER(Server) (BOOL aServer)
+{
+    LogFlowThisFunc (("aServer=%RTbool\n", aServer));
+
+    AutoCaller autoCaller (this);
+    CheckComRCReturnRC (autoCaller.rc());
+
+    /* the machine needs to be mutable */
+    Machine::AutoMutableStateDependency adep (mParent);
+    CheckComRCReturnRC (adep.rc());
+
+    AutoLock alock (this);
+
+    if (mData->mServer != aServer)
+    {
+        mData.backup();
+        mData->mServer = aServer;
+
+        /* leave the lock before informing callbacks */
+        alock.unlock();
+
+        mParent->onSerialPortChange (this);
+    }
+
+    return S_OK;
+}
