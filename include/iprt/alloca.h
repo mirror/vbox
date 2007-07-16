56163d57c65b60187bb096e7755199c5d8a9c170
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2007-07-16 12:06:30 +0000

    The __SOMETHING__ OS and architecture #defines are now deprecated. Instead use RT_ARCH_[X86|AMD64], RT_OS_[DARWIN|FREEBSD|L4|LINUX|NETBSD|OPENBSD|OS2|SOLARIS|WINDOWS|whatever].
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@3629 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- include/iprt/alloca.h
+++ include/iprt/alloca.h
@@ -21,7 +21,6 @@
 #ifndef __iprt_alloca_h__
 #define __iprt_alloca_h__
 
-
 /*
  * If there are more difficult platforms out there, we'll do OS
  * specific #ifdefs. But for now we'll just include the headers
@@ -29,15 +28,15 @@
  * When we're in kernel territory it starts getting a bit more
  * interesting of course...
  */
-#if defined(IN_RING0) && defined(__LINUX__)
+#if defined(IN_RING0) && defined(RT_OS_LINUX)
 /* ASSUMES GNU C */
 # define alloca(cb) __builtin_alloca(cb)
 #else
 # include <stdlib.h>
-# if !defined(__DARWIN__) && !defined(__FREEBSD__)
+# if !defined(RT_OS_DARWIN) && !defined(RT_OS_FREEBSD)
 #  include <malloc.h>
 # endif
-# ifdef __SOLARIS__
+# ifdef RT_OS_SOLARIS
 #  include <alloca.h>
 # endif
 #endif
