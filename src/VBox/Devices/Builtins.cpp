01d296a04e6be4cbe3da34f68f4b2884e6b86c46
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2007-07-16 15:40:44 +0000

    Added parallel device emulation and host driver. Contributed by: Alexander Eichner
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@3648 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Devices/Builtins.cpp
+++ src/VBox/Devices/Builtins.cpp
@@ -120,6 +120,9 @@ extern "C" DECLEXPORT(int) VBoxDevicesRegister(PPDMDEVREGCB pCallbacks, uint32_t
     rc = pCallbacks->pfnRegister(pCallbacks, &g_DeviceSerialPort);
     if (VBOX_FAILURE(rc))
         return rc;
+    rc = pCallbacks->pfnRegister(pCallbacks, &g_DeviceParallelPort);
+    if (VBOX_FAILURE(rc))
+        return rc;
 
     return VINF_SUCCESS;
 }
@@ -220,6 +223,12 @@ extern "C" DECLEXPORT(int) VBoxDriversRegister(PCPDMDRVREGCB pCallbacks, uint32_
     if (VBOX_FAILURE(rc))
         return rc;
     rc = pCallbacks->pfnRegister(pCallbacks, &g_DrvChar);
+    if (VBOX_FAILURE(rc))
+        return rc;
+#endif
+
+#if defined(__LINUX__)
+    rc = pCallbacks->pfnRegister(pCallbacks, &g_DrvHostParallel);
     if (VBOX_FAILURE(rc))
         return rc;
 #endif
