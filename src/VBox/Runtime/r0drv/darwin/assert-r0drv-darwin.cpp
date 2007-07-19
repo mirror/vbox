9683a63dcdd91eb457a14d3e6182a2fed40e9b12
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2007-07-19 00:32:07 +0000

    AssertMsg1/2.
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@3706 cfe28804-0f27-0410-a406-dd0f0b0b656f

---
+++ src/VBox/Runtime/r0drv/darwin/assert-r0drv-darwin.cpp
@@ -0,0 +1,67 @@
+/* $Id$ */
+/** @file
+ * innotek Portable Runtime -  Assertion Workers, Ring-0 Drivers, Darwin.
+ */
+
+/*
+ * Copyright (C) 2007 innotek GmbH
+ *
+ * This file is part of VirtualBox Open Source Edition (OSE), as
+ * available from http://www.virtualbox.org. This file is free software;
+ * you can redistribute it and/or modify it under the terms of the GNU
+ * General Public License as published by the Free Software Foundation,
+ * in version 2 as it comes in the "COPYING" file of the VirtualBox OSE
+ * distribution. VirtualBox OSE is distributed in the hope that it will
+ * be useful, but WITHOUT ANY WARRANTY of any kind.
+ *
+ * If you received this file as part of a commercial VirtualBox
+ * distribution, then only the terms of your commercial VirtualBox
+ * license agreement apply instead of the previous paragraph.
+ */
+
+
+/*******************************************************************************
+*   Header Files                                                               *
+*******************************************************************************/
+#include "the-darwin-kernel.h"
+
+#include <iprt/assert.h>
+#include <iprt/log.h>
+#include <iprt/string.h>
+#include <iprt/stdarg.h>
+
+
+RTDECL(void) AssertMsg1(const char *pszExpr, unsigned uLine, const char *pszFile, const char *pszFunction)
+{
+#ifdef IN_GUEST_R0
+    RTLogBackdoorPrintf("\n!!Assertion Failed!!\n"
+                        "Expression: %s\n"
+                        "Location  : %s(%d) %s\n",
+                        pszExpr, pszFile, uLine, pszFunction);
+#endif
+
+    printf("\r\n!!Assertion Failed!!\r\n"
+           "Expression: %s\r\n"
+           "Location  : %s(%d) %s\r\n",
+           pszExpr, pszFile, uLine, pszFunction);
+}
+
+
+RTDECL(void) AssertMsg2(const char *pszFormat, ...)
+{
+    va_list va;
+    char    szMsg[256];
+
+#ifdef IN_GUEST_R0
+    va_start(va, pszFormat);
+    RTLogBackdoorPrintfV(pszFormat, va);
+    va_end(va);
+#endif
+
+    va_start(va, pszFormat);
+    RTStrPrintfV(szMsg, sizeof(szMsg) - 1, pszFormat, va);
+    szMsg[sizeof(szMsg) - 1] = '\0';
+    va_end(va);
+    printf("%s", szMsg);
+}
+
