b77655ac0195f0b7f56a0a39c51fe05ccb7e1fdd
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2007-07-19 19:07:48 +0000

    Double underscore cleanup.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@3726 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/recompiler/InnoTek/config-host.h
+++ src/recompiler/InnoTek/config-host.h
@@ -20,21 +20,21 @@
  */
 
 
-#if defined(__amd64__) || defined(HOST_X86_64) /* latter, for dyngen on win64. */
+#if defined(RT_ARCH_AMD64) || defined(HOST_X86_64) /* The latter, for dyngen when cross compiling (windows, l4, etc). */
 # define HOST_X86_64 1
 # define HOST_LONG_BITS 64
 #else
 # define HOST_I386 1
 # define HOST_LONG_BITS 32
-# ifdef __WIN32__
+# ifdef RT_OS_WINDOWS
 #  define CONFIG_WIN32 1
-# elif defined(__OS2__)
+# elif defined(RT_OS_OS2)
 #  define CONFIG_OS2
-# elif defined(__DARWIN__)
+# elif defined(RT_OS_DARWIN)
 #  define CONFIG_DARWIN
-# elif defined(__FREEBSD__) || defined(__NETBSD__) || defined(__OPENBSD__)
+# elif defined(RT_OS_FREEBSD) || defined(RT_OS_NETBSD) || defined(RT_OS_OPENBSD)
 /*#  define CONFIG_BSD*/
-# elif defined(__SOLARIS__)
+# elif defined(RT_OS_SOLARIS)
 /*#  define CONFIG_SUN*/
 # elif !defined(IPRT_NO_CRT)
 #  define HAVE_BYTESWAP_H 1
