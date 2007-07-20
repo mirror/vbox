add3bd5683fc89d4b88c99a90374cf9bfea08a4c
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2007-07-20 15:01:47 +0000

    Export VBoxHook
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@3743 cfe28804-0f27-0410-a406-dd0f0b0b656f

---
+++ src/VBox/Additions/WINNT/VBoxHook/testcase/tstHook.cpp
@@ -0,0 +1,33 @@
+/*
+ * Copyright (C) 2006-2007 innotek GmbH
+ *
+ * This file is part of VirtualBox Open Source Edition (OSE), as
+ * available from http://www.virtualbox.org. This file is free software;
+ * you can redistribute it and/or modify it under the terms of the GNU
+ * General Public License as published by the Free Software Foundation,
+ * in version 2 as it comes in the "COPYING" file of the VirtualBox OSE
+ * distribution. VirtualBox OSE is distributed in the hope that it will
+ * be useful, but WITHOUT ANY WARRANTY of any kind.
+ *
+ * If you received this file as part of a commercial VirtualBox
+ * distribution, then only the terms of your commercial VirtualBox
+ * license agreement apply instead of the previous paragraph.
+ */
+
+/*******************************************************************************
+*   Header Files                                                               *
+*******************************************************************************/
+#include <windows.h>
+#include <VBoxHook.h>
+#include <stdio.h>
+
+int main(int argc, char **argv)
+{
+    printf("Enabling global hook\n");
+    VBoxInstallHook(GetModuleHandle("VBoxHook.dll"));
+    getchar();
+
+    printf("Disabling global hook\n");
+    VBoxRemoveHook();
+    return 0;
+}
