f381964222d8707e1654a839e2e2c7a2e339e5a9
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2007-07-24 11:37:53 +0000

    Install hooks in main thread
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@3811 cfe28804-0f27-0410-a406-dd0f0b0b656f

---
+++ src/VBox/Additions/WINNT/VBoxService/VBoxSeamless.h
@@ -0,0 +1,38 @@
+/** @file
+ * helpers - Guest Additions Service helper functions header
+ */
+
+/*
+ * Copyright (C) 2006-2007 innotek GmbH
+ *
+ * This file is part of VirtualBox Open Source Edition (OSE), as
+ * available from http://www.virtualbox.org. This file is free software;
+ * you can redistribute it and/or modify it under the terms of the GNU
+ * General Public License as published by the Free Software Foundation,
+ * in version 2 as it comes in the "COPYING" file of the VirtualBox OSE
+ * distribution. VirtualBox OSE is distributed in the hope that it will
+ * be useful, but WITHOUT ANY WARRANTY of any kind.
+ *
+ * If you received this file as part of a commercial VirtualBox
+ * distribution, then only the terms of your commercial VirtualBox
+ * license agreement apply instead of the previous paragraph.
+ *
+ */
+
+#ifndef __VBOXSERVICESEAMLESS__H
+#define __VBOXSERVICESEAMLESS__H
+
+/* The seamless windows service prototypes */
+int                VBoxSeamlessInit     (const VBOXSERVICEENV *pEnv, void **ppInstance, bool *pfStartThread);
+unsigned __stdcall VBoxSeamlessThread   (void *pInstance);
+void               VBoxSeamlessDestroy  (const VBOXSERVICEENV *pEnv, void *pInstance);
+
+
+/* custom messages as we must install the hook from the main thread */
+#define WM_VBOX_INSTALL_SEAMLESS_HOOK               0x2001
+#define WM_VBOX_REMOVE_SEAMLESS_HOOK                0x2002
+
+void VBoxSeamlessInstallHook();
+void VBoxSeamlessRemoveHook();
+
+#endif /* __VBOXSERVICESEAMLESS__H */
