411ea74a1f4f0ad791920c8581a3c0c4617d9b75
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2007-07-25 12:24:59 +0000

    export -This line, and those below, will be ignored--
    _M   VBoxDefs.cpp
    
    
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@3832 cfe28804-0f27-0410-a406-dd0f0b0b656f

---
+++ src/VBox/Frontends/VirtualBox/src/VBoxDefs.cpp
@@ -0,0 +1,35 @@
+/** @file
+ *
+ * VBox frontends: Qt GUI ("VirtualBox"):
+ * VBoxDefs implementation
+ */
+
+/*
+ * Copyright (C) 2006-2007 innotek GmbH
+ *
+ * This file is part of VirtualBox Open Source Edition (OSE), as
+ * available from http://www.virtualbox.org. This file is free software;
+ * you can redistribute it and/or modify it under the terms of the GNU
+ * General Public License as published by the Free Software Foundation,
+ * in version 2 as it comes in the "COPYING" file of the VirtualBox OSE
+ * distribution. VirtualBox OSE is distributed in the hope that it will
+ * be useful, but WITHOUT ANY WARRANTY of any kind.
+ *
+ * If you received this file as part of a commercial VirtualBox
+ * distribution, then only the terms of your commercial VirtualBox
+ * license agreement apply instead of the previous paragraph.
+ */
+
+#include <VBoxDefs.h>
+
+const char* VBoxDefs::GUI_LastWindowPosition = "GUI/LastWindowPostion";
+const char* VBoxDefs::GUI_LastWindowPosition_Max = "max";
+const char* VBoxDefs::GUI_Fullscreen = "GUI/Fullscreen";
+const char* VBoxDefs::GUI_Seamless = "GUI/Seamless";
+const char* VBoxDefs::GUI_AutoresizeGuest = "GUI/AutoresizeGuest";
+const char* VBoxDefs::GUI_FirstRun = "GUI/FirstRun";
+const char* VBoxDefs::GUI_SaveMountedAtRuntime = "GUI/SaveMountedAtRuntime";
+const char* VBoxDefs::GUI_LastCloseAction = "GUI/LastCloseAction";
+const char* VBoxDefs::GUI_SuppressMessages = "GUI/SuppressMessages";
+const char* VBoxDefs::GUI_PermanentSharedFoldersAtRuntime = "GUI/PermanentSharedFoldersAtRuntime";
+
