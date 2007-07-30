afbf5cc230a05014b95025ae0ab9239696410a69
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2007-07-30 15:26:59 +0000

    Added copyright header
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@3924 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- include/iprt/nocrt/compiler/gcc.h
+++ include/iprt/nocrt/compiler/gcc.h
@@ -1,6 +1,22 @@
 /* A quick hack for freebsd where there are no separate location 
    for compiler specific headers like on linux, mingw, os2, ++. 
    This file will be cleaned up later...  */
+/*
+ * Copyright (C) 2006-2007 innotek GmbH
+ *
+ * This file is part of VirtualBox Open Source Edition (OSE), as
+ * available from http://www.virtualbox.org. This file is free software;
+ * you can redistribute it and/or modify it under the terms of the GNU
+ * General Public License as published by the Free Software Foundation,
+ * in version 2 as it comes in the "COPYING" file of the VirtualBox OSE
+ * distribution. VirtualBox OSE is distributed in the hope that it will
+ * be useful, but WITHOUT ANY WARRANTY of any kind.
+ *
+ * If you received this file as part of a commercial VirtualBox
+ * distribution, then only the terms of your commercial VirtualBox
+ * license agreement apply instead of the previous paragraph.
+ *
+ */
    
 #ifndef ___iprt_nocrt_compiler_gcc_h
 #define ___iprt_nocrt_compiler_gcc_h
