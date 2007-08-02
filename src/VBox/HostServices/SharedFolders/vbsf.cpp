2932e8d244c6412f938574d4ac41fefd9200cb15
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2007-08-02 12:47:09 +0000

    Backed out 23427; problem lies elsewhere
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@4004 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/HostServices/SharedFolders/vbsf.cpp
+++ src/VBox/HostServices/SharedFolders/vbsf.cpp
@@ -918,8 +918,7 @@ int vbsfCreate (SHFLCLIENTDATA *pClient, SHFLROOT root, SHFLSTRING *pPath, uint3
             }
         }
         else 
-        /* Note: In Linux we sometimes get VERR_PATH_NOT_FOUND when only the file is not present (sigh) */
-        if (rc == VERR_FILE_NOT_FOUND || rc == VERR_PATH_NOT_FOUND)
+        if (rc == VERR_FILE_NOT_FOUND)
         {
             Log2(("pParms->CreateFlags = %x\n", pParms->CreateFlags));
 
@@ -974,6 +973,13 @@ int vbsfCreate (SHFLCLIENTDATA *pClient, SHFLROOT root, SHFLSTRING *pPath, uint3
                     }
                 }
             }
+        }
+        else
+        if (rc == VERR_PATH_NOT_FOUND)
+        {
+            rc = VINF_SUCCESS;
+ 
+            pParms->Result = SHFL_PATH_NOT_FOUND;
         }
 
         if (rc == VINF_SUCCESS && pParms->Handle != SHFL_HANDLE_NIL)
