022c75943a75ccbdf5853d4ef142e7e4913fe9e4
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2007-08-07 14:13:20 +0000

    Runtime: Added basic RTEnv...(RTENV,..) API.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@4063 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- include/iprt/env.h
+++ include/iprt/env.h
@@ -77,6 +77,75 @@ RTDECL(int) RTEnvSet(const char *pszVar, const char *pszValue);
 
 /** @todo Add the missing environment APIs: safe, printf like, and various modifications. */
 
+/**
+ * Creates an empty environment block.
+ * 
+ * @returns IPRT status code. Typical error is VERR_NO_MEMORY.
+ * 
+ * @param   pEnv        Where to store the handle of the environment block.
+ */
+RTDECL(int) RTEnvCreate(PRTENV pEnv);
+
+/**
+ * Destroys an environment block.
+ *
+ * @returns IPRT status code.
+ *
+ * @param   Env     Handle of the environment block.
+ */
+RTDECL(int) RTEnvDestroy(RTENV Env);
+
+/**
+ * Creates an environment block and fill it with variables from the given
+ * environment array.
+ *
+ * @returns IPRT status code. Typical error is VERR_NO_MEMORY.
+ *
+ * @param   pEnv        Where to store the handle of the environment block.
+ * @param   apszEnv     Pointer to the NULL-terminated array of environment
+ *                      variables. If NULL, the current process' environment
+ *                      will be cloned.
+ */
+RTDECL(int) RTEnvClone(PRTENV pEnv, char const *const *apszEnv);
+
+/** @todo later */
+/*
+RTDECL(int) RTEnvCloneEx(PRTENV pEnv, const RTENV Env);
+
+RTDECL(int) RTEnvExistEx(RTENV Env, const char *pszVar);
+RTDECL(int) RTEnvSetEx(RTENV Env, const char *pszVar, const char *pszValue);
+RTDECL(int) RTEnvGetEx(RTENV Env, const char *pszVar, const char **ppszValue);
+RTDECL(int) RTEnvCountEx(RTENV Env, size_t *cbCount);
+*/
+
+/**
+ * Puts a 'variable=value' string into the environment.
+ *
+ * The supplied string must be in the current process' codepage.
+ * This function makes a copy of the supplied string.
+ * 
+ * @returns IPRT status code. Typical error is VERR_NO_MEMORY.
+ * 
+ * @param   Env                 Handle of the environment block.
+ * @param   pszVarEqualValue    The variable '=' value string. If the value and '=' is 
+ *                              omitted, the variable is removed from the environment.
+ */
+RTDECL(int) RTEnvPutEx(RTENV Env, const char *pszVarEqualValue);
+
+/**
+ * Returns a raw pointer to the array of environment variables of the given
+ * environment block where every variable is a string in format
+ * 'variable=value'.
+ *
+ * All returned strings are in the current process' codepage.
+ * 
+ * @returns Pointer to the raw array of environment variables.
+ * @returns NULL if Env is NULL or invalid.
+ * 
+ * @param   Env                 Handle of the environment block.
+ */
+RTDECL(char const *const *) RTEnvGetArray(RTENV Env);
+
 #endif /* IN_RING3 */
 
 /** @} */
