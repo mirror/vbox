63702761cda7f5a7189c7c1e1beafc2b38f0f90b
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2007-08-22 16:07:09 +0000

    Private header for windows only declarations
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@4292 cfe28804-0f27-0410-a406-dd0f0b0b656f

---
+++ src/VBox/Additions/WINNT/include/VBoxGuestInternal.h
@@ -0,0 +1,20 @@
+/** @file
+ *
+ * VBoxGuestInternal -- Private windows additions declarations
+ *
+ * Copyright (C) 2006-2007 innotek GmbH
+ *
+ * innotek GmbH confidential
+ * All rights reserved
+ */
+#ifndef __VBoxGuestInternal_h__
+#define __VBoxGuestInternal_h__
+
+/** IOCTL for VBoxGuest to enable a VRDP session */
+#define IOCTL_VBOXGUEST_ENABLE_VRDP_SESSION     IOCTL_CODE(FILE_DEVICE_UNKNOWN, 2100, METHOD_BUFFERED, FILE_WRITE_ACCESS, 0)
+
+/** IOCTL for VBoxGuest to disable a VRDP session */
+#define IOCTL_VBOXGUEST_DISABLE_VRDP_SESSION    IOCTL_CODE(FILE_DEVICE_UNKNOWN, 2101, METHOD_BUFFERED, FILE_WRITE_ACCESS, 0)
+
+
+#endif /* __VBoxGuestInternal_h__ */
