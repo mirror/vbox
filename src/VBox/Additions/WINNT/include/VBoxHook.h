1a1a13cc31d40c0a0a02916df4805c58c345d14c
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2007-08-22 09:11:10 +0000

    export property
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@4278 cfe28804-0f27-0410-a406-dd0f0b0b656f

---
+++ src/VBox/Additions/WINNT/include/VBoxHook.h
@@ -0,0 +1,27 @@
+/** @file
+ *
+ * VBoxHook -- Global windows hook dll
+ *
+ * Copyright (C) 2006-2007 innotek GmbH
+ *
+ * innotek GmbH confidential
+ * All rights reserved
+ */
+#ifndef __VBoxHook_h__
+#define __VBoxHook_h__
+
+/* custom messages as we must install the hook from the main thread */
+#define WM_VBOX_INSTALL_SEAMLESS_HOOK               0x2001
+#define WM_VBOX_REMOVE_SEAMLESS_HOOK                0x2002
+#define WM_VBOX_SEAMLESS_UPDATE                     0x2003
+
+
+#define VBOXHOOK_DLL_NAME           "VBoxHook.dll"
+
+/* Install the global message hook */
+BOOL VBoxInstallHook(HMODULE hDll, HWND hwndPostWindow);
+
+/* Remove the global message hook */
+BOOL VBoxRemoveHook();
+
+#endif /* __VBoxHook_h__ */
