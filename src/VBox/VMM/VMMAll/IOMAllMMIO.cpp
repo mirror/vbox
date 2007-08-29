9716148243ad5478562ae1c3f93bbf126731c1c0
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2007-08-29 09:02:17 +0000

    Backed out 23986
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@4416 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/VMM/VMMAll/IOMAllMMIO.cpp
+++ src/VBox/VMM/VMMAll/IOMAllMMIO.cpp
@@ -460,7 +460,17 @@ DECLINLINE(int) iomRamRead(PVM pVM, void *pDest, RTGCPTR GCSrc, uint32_t cb)
 #ifdef IN_GC
     return MMGCRamReadNoTrapHandler(pDest, GCSrc, cb);
 #else
-    return PGMPhysReadGCPtrSafe(pVM, pDest, GCSrc, cb);
+    int         rc;
+    RTGCPHYS    GCPhys;
+    RTGCUINTPTR offset;
+
+    offset = GCSrc & PAGE_OFFSET_MASK;
+
+    /** @todo optimize the loop; no need to convert the address all the time */
+    rc = PGMPhysGCPtr2GCPhys(pVM, GCSrc, &GCPhys);
+    AssertRCReturn(rc, rc);
+    PGMPhysRead(pVM, GCPhys + offset, pDest, cb);
+    return VINF_SUCCESS;
 #endif
 }
 
@@ -469,7 +479,16 @@ DECLINLINE(int) iomRamWrite(PVM pVM, RTGCPTR GCDest, void *pSrc, uint32_t cb)
 #ifdef IN_GC
     return MMGCRamWriteNoTrapHandler(GCDest, pSrc, cb);
 #else
-    return PGMPhysWriteGCPtrSafe(pVM, GCDest, pSrc, cb);
+    int         rc;
+    RTGCPHYS    GCPhys;
+    RTGCUINTPTR offset;
+
+    /** @todo optimize the loop; no need to convert the address all the time */
+    offset = GCDest & PAGE_OFFSET_MASK;
+    rc = PGMPhysGCPtr2GCPhys(pVM, GCDest, &GCPhys);
+    AssertRCReturn(rc, rc);
+    PGMPhysWrite(pVM, GCPhys + offset, pSrc, cb);
+    return VINF_SUCCESS;
 #endif
 }
 
