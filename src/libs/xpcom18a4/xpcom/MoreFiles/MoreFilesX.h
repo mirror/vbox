ebfb54e99c10b54581e2454d2160bf23130a1756
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2007-09-02 11:03:09 +0000

    remove Apple disclaimer as permitted by original disclaimer and make it MPL/GPL
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@4481 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/libs/xpcom18a4/xpcom/MoreFiles/MoreFilesX.h
+++ src/libs/xpcom18a4/xpcom/MoreFiles/MoreFilesX.h
@@ -6,83 +6,46 @@
 
 	Version:	MoreFilesX 1.0.1
 
-	Copyright:	© 1992-2002 by Apple Computer, Inc., all rights reserved.
 
-	Disclaimer:	IMPORTANT:  This Apple software is supplied to you by Apple Computer, Inc.
-				("Apple") in consideration of your agreement to the following terms, and your
-				use, installation, modification or redistribution of this Apple software
-				constitutes acceptance of these terms.  If you do not agree with these terms,
-				please do not use, install, modify or redistribute this Apple software.
-
-				In consideration of your agreement to abide by the following terms, and subject
-				to these terms, Apple grants you a personal, non-exclusive license, under Apple’s
-				copyrights in this original Apple software (the "Apple Software"), to use,
-				reproduce, modify and redistribute the Apple Software, with or without
-				modifications, in source and/or binary forms; provided that if you redistribute
-				the Apple Software in its entirety and without modifications, you must retain
-				this notice and the following text and disclaimers in all such redistributions of
-				the Apple Software.  Neither the name, trademarks, service marks or logos of
-				Apple Computer, Inc. may be used to endorse or promote products derived from the
-				Apple Software without specific prior written permission from Apple.  Except as
-				expressly stated in this notice, no other rights or licenses, express or implied,
-				are granted by Apple herein, including but not limited to any patent rights that
-				may be infringed by your derivative works or by other works in which the Apple
-				Software may be incorporated.
-
-				The Apple Software is provided by Apple on an "AS IS" basis.  APPLE MAKES NO
-				WARRANTIES, EXPRESS OR IMPLIED, INCLUDING WITHOUT LIMITATION THE IMPLIED
-				WARRANTIES OF NON-INFRINGEMENT, MERCHANTABILITY AND FITNESS FOR A PARTICULAR
-				PURPOSE, REGARDING THE APPLE SOFTWARE OR ITS USE AND OPERATION ALONE OR IN
-				COMBINATION WITH YOUR PRODUCTS.
-
-				IN NO EVENT SHALL APPLE BE LIABLE FOR ANY SPECIAL, INDIRECT, INCIDENTAL OR
-				CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
-				GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
-				ARISING IN ANY WAY OUT OF THE USE, REPRODUCTION, MODIFICATION AND/OR DISTRIBUTION
-				OF THE APPLE SOFTWARE, HOWEVER CAUSED AND WHETHER UNDER THEORY OF CONTRACT, TORT
-				(INCLUDING NEGLIGENCE), STRICT LIABILITY OR OTHERWISE, EVEN IF APPLE HAS BEEN
-				ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-
-	File Ownership:
-
-		DRI:				Apple Macintosh Developer Technical Support
-
-		Other Contact:		For bug reports, consult the following page on
-							the World Wide Web:
-								http://developer.apple.com/bugreporter/
-
-		Technology:			DTS Sample Code
-
-	Writers:
-
-		(JL)	Jim Luther
-
-	Change History (most recent first):
-
-		 <3>	 4/19/02	JL		[2853905]  Fixed #if test around header includes.
-		 <2>	 4/19/02	JL		[2853901]  Updated standard disclaimer.
-		 <1>	 1/25/02	JL		MoreFilesX 1.0
-	
-	Notes:
-		What do those arrows in the documentation for each routine mean?
-			
-			--> The parameter is an input
-			
-			<-- The parameter is an output. The pointer to the variable
-				where the output will be returned (must not be NULL).
-			
-			<** The parameter is an optional output. If it is not a
-				NULL pointer, it points to the variable where the output
-				will be returned. If it is a NULL pointer, the output will
-				not be returned and will possibly let the routine and the
-				File Manager do less work. If you don't need an optional output,
-				don't ask for it.
-			**> The parameter is an optional input. If it is not a
-				NULL pointer, it points to the variable containing the
-				input data. If it is a NULL pointer, the input is not used
-				and will possibly let the routine and the File Manager
-				do less work.
 */
+
+/* ***** BEGIN LICENSE BLOCK *****
+ * Version: MPL 1.1/GPL 2.0/LGPL 2.1
+ *
+ * The contents of this file are subject to the Mozilla Public License Version
+ * 1.1 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+ * for the specific language governing rights and limitations under the
+ * License.
+ *
+ * The Original Code is Mozilla Communicator client code, released
+ * March 31, 1998.
+ *
+ * The Initial Developer of the Original Code is
+ * Netscape Communications Corporation.
+ * Portions created by the Initial Developer are Copyright (C) 2000
+ * the Initial Developer. All Rights Reserved.
+ *
+ * Contributor(s):
+ *
+ * Alternatively, the contents of this file may be used under the terms of
+ * either of the GNU General Public License Version 2 or later (the "GPL"),
+ * or the GNU Lesser General Public License Version 2.1 or later (the "LGPL"),
+ * in which case the provisions of the GPL or the LGPL are applicable instead
+ * of those above. If you wish to allow use of your version of this file only
+ * under the terms of either the GPL or the LGPL, and not to allow others to
+ * use your version of this file under the terms of the MPL, indicate your
+ * decision by deleting the provisions above and replace them with the notice
+ * and other provisions required by the GPL or the LGPL. If you do not delete
+ * the provisions above, a recipient may use your version of this file under
+ * the terms of any one of the MPL, the GPL or the LGPL.
+ *
+ * ***** END LICENSE BLOCK ***** */
+
 
 // Modified 2006-01-23 - added this comment.
 
