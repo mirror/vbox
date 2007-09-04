b4975ae2a5012e92fe00b3eee033f174b743cbc1
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2007-09-04 11:41:37 +0000

    Reapplied fixed 24093.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@4513 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Main/include/MachineImpl.h
+++ src/VBox/Main/include/MachineImpl.h
@@ -220,6 +220,7 @@ public:
         bool operator== (const HWData &that) const;
 
         ULONG          mMemorySize;
+        ULONG          mMemoryBalloonSize;
         ULONG          mVRAMSize;
         ULONG          mMonitorCount;
         TriStateBool_T mHWVirtExEnabled;
@@ -440,6 +441,8 @@ public:
     STDMETHOD(COMSETTER(OSTypeId)) (INPTR BSTR aOSTypeId);
     STDMETHOD(COMGETTER(MemorySize))(ULONG *memorySize);
     STDMETHOD(COMSETTER(MemorySize))(ULONG memorySize);
+    STDMETHOD(COMGETTER(MemoryBalloonSize))(ULONG *memoryBalloonSize);
+    STDMETHOD(COMSETTER(MemoryBalloonSize))(ULONG memoryBalloonSize);
     STDMETHOD(COMGETTER(VRAMSize))(ULONG *memorySize);
     STDMETHOD(COMSETTER(VRAMSize))(ULONG memorySize);
     STDMETHOD(COMGETTER(MonitorCount))(ULONG *monitorCount);
