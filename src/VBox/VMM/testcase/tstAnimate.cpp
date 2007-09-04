991868c3b74360fdc69fb5f47e5e5af61613a69e
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2007-09-04 06:15:51 +0000

    grrrr
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@4508 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/VMM/testcase/tstAnimate.cpp
+++ src/VBox/VMM/testcase/tstAnimate.cpp
@@ -420,7 +420,7 @@ static DECLCALLBACK(int) cfgmR3CreateDefault(PVM pVM, void *pvUser)
     rc = CFGMR3InsertInteger(pCfg,  "IOAPIC", fIOAPIC);                         UPDATERC();
     RTUUID Uuid;
     RTUuidClear(&Uuid);
-    rc = CFGMR3InsertBytes(pCfg,    "UUID", &Uuid, sizeof(Uuid));               UPDATE_RC();
+    rc = CFGMR3InsertBytes(pCfg,    "UUID", &Uuid, sizeof(Uuid));               UPDATERC();
     /* Bios logo. */
     rc = CFGMR3InsertInteger(pCfg,  "FadeIn",               0);
     UPDATERC();
