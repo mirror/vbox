d169a7fa365a936b7c216a546ec52416b2ae5c12
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2007-09-14 16:18:20 +0000

    Added an RTErrnoConvertFromErr function to simplify error management inside the Linux Additions kernel modules
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@4808 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Additions/linux/sharedfolders/utils.c
+++ src/VBox/Additions/linux/sharedfolders/utils.c
@@ -568,7 +568,7 @@ sf_dir_read_all (struct sf_glob_info *sf_g, struct sf_inode_info *sf_i,
                                 break;
 
                         default:
-                                err = -EPROTO;
+                                err = -RTErrnoConvertFromErr(rc);
                                 LogFunc(("vboxCallDirInfo failed rc=%Vrc\n", rc));
                                 goto fail1;
                 }
