adb039c11520e8f8476fd6f946e94ff940e8882a
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2007-09-19 08:03:39 +0000

    FreeBSD patches
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@4890 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Main/include/VirtualBoxXMLUtil.h
+++ src/VBox/Main/include/VirtualBoxXMLUtil.h
@@ -31,16 +31,22 @@
 #define VBOX_XML_VERSION "1.2"
 
 /** VirtualBox XML settings version string */
-#if defined (RT_OS_WINDOWS)
-#   define VBOX_XML_PLATFORM     "windows"
+#if defined (RT_OS_DARWIN)
+#   define VBOX_XML_PLATFORM     "macosx"
+#if defined (RT_OS_FREEBSD)
+#   define VBOX_XML_PLATFORM     "freebsd"
 #elif defined (RT_OS_LINUX)
 #   define VBOX_XML_PLATFORM     "linux"
-#elif defined (RT_OS_DARWIN)
-#   define VBOX_XML_PLATFORM     "macosx"
+#elif defined (RT_OS_NETBSD)
+#   define VBOX_XML_PLATFORM     "netbsd"
+#elif defined (RT_OS_OPENBSD)
+#   define VBOX_XML_PLATFORM     "openbsd"
 #elif defined (RT_OS_OS2)
 #   define VBOX_XML_PLATFORM     "os2"
 #elif defined (RT_OS_SOLARIS)
 #   define VBOX_XML_PLATFORM     "solaris"
+#if defined (RT_OS_WINDOWS)
+#   define VBOX_XML_PLATFORM     "windows"
 #else
 #   error Unsupported platform!
 #endif
