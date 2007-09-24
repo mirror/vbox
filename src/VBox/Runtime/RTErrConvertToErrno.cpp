a97a014f1b66d5956c35b8693be865568c8345a9
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2007-09-24 14:33:11 +0000

    This wasn't a typo.
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@5009 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Runtime/RTErrConvertToErrno.cpp
+++ src/VBox/Runtime/RTErrConvertToErrno.cpp
@@ -426,7 +426,7 @@ RTDECL(int) RTErrConvertToErrno(int iErr)
 #endif
 
         default:
-            AssertMsgFailed(("Unhandled error code %Vrc\n", iErr));
+            AssertMsgFailed(("Unhandled error code %Rrc\n", iErr));
             return EPROTO;
     }
 }
