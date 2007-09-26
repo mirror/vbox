e477623d2294fcd8fdcd33daf5d9aad68f647f9f
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2007-09-26 16:08:23 +0000

    assertion was the wrong way around. typical 2am bug.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@5056 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/VMM/PDMLdr.cpp
+++ src/VBox/VMM/PDMLdr.cpp
@@ -607,9 +607,7 @@ static int pdmR3LoadR0(PVM pVM, const char *pszFilename, const char *pszName)
         /* next */
         pCur = pCur->pNext;
     }
-#if 0 /* breaks VT-x */
-    AssertReturn(!strcmp(pszName, VMMR0_MAIN_MODULE_NAME), VERR_INTERNAL_ERROR);
-#endif
+    AssertReturn(strcmp(pszName, VMMR0_MAIN_MODULE_NAME), VERR_INTERNAL_ERROR);
 
     /*
      * Find the file if not specified.
