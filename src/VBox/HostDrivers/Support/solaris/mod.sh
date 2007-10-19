31b81a6ccb8e704cc8a289b82e61d45b327d36e9
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2007-10-19 09:35:14 +0000

    New driver location: /platform/i86pc/kernel/drv
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@5379 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/HostDrivers/Support/solaris/mod.sh
+++ src/VBox/HostDrivers/Support/solaris/mod.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
+set -x
+#
+# Figure out the environment and locations.
+#
 
 # Sudo isn't native solaris, but it's very convenient...
 if test -z "$SUDO" && test "`whoami`" != "root"; then
@@ -24,28 +28,33 @@ fi
 
 DIR=$script_dir/out/$BUILD_TARGET.$BUILD_TARGET_ARCH/$BUILD_TYPE/bin/
 
+VBOXDRV_CONF_DIR=/platform/i86pc/kernel/drv
 if test "$BUILD_TARGET_ARCH" = "amd64"; then
-    VBOXDRV_INS=/usr/kernel/drv/amd64/vboxdrv
+    VBOXDRV_DIR=$VBOXDRV_CONF_DIR/amd64
 else
-    VBOXDRV_INS=/usr/kernel/drv/vboxdrv
+    VBOXDRV_DIR=$VBOXDRV_CONF_DIR
 fi
-$(SUDO) cp $DIR/vboxdrv.o $VBOXDRV_INS
-$(SUDO) cp $script_dir/src/VBox/HostDrivers/Support/solaris/vboxdrv.conf /usr/kernel/drv/vboxdrv.conf
+
+#
+# Do the job.
+#
+$SUDO cp $DIR/vboxdrv.o $VBOXDRV_DIR/vboxdrv
+$SUDO cp $script_dir/src/VBox/HostDrivers/Support/solaris/vboxdrv.conf $VBOXDRV_CONF_DIR/vboxdrv.conf
 old_id=`/usr/sbin/modinfo | grep vbox | cut -f 1 -d ' ' `
 if test -n "$old_id"; then
     echo "* unloading $old_id..."
     sync
     sync
-    $(SUDO) /usr/sbin/modunload -i $old_id
+    $SUDO /usr/sbin/modunload -i $old_id
 else
-    echo "* If it fails below, run: $(SUDO) add_drv vboxdrv"
+    echo "* If it fails below, run: $SUDO add_drv vboxdrv"
 fi
 echo "* loading vboxdrv..."
 sync
 sync
-$(SUDO) /usr/sbin/modload $VBOXDRV_INS
+$SUDO /usr/sbin/modload $VBOXDRV_DIR/vboxdrv
 /usr/sbin/modinfo | grep vboxdrv
 echo "* dmesg:"
 dmesg | tail -20
-$(SUDO) chmod a+rw /devices/pseudo/vboxdrv*
+$SUDO chmod a+rw /devices/pseudo/vboxdrv*
 
