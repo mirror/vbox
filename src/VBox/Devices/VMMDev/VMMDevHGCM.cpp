90911f865a8111d6de63f57da130acd6511f227f
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2007-10-24 09:24:56 +0000

    fixed debug build
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@5476 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Devices/VMMDev/VMMDevHGCM.cpp
+++ src/VBox/Devices/VMMDev/VMMDevHGCM.cpp
@@ -491,7 +491,7 @@ int vmmdevHGCMCall (VMMDevState *pVMMDevState, VMMDevHGCMCall *pHGCMCall, RTGCPH
                  case VMMDevHGCMParmType_PhysAddr:
                  {
                      uint32_t size = pGuestParm->u.Pointer.size;
-                     /* RTGCPHYS physAddr = pGuestParm->u.Pointer.u.physAddr; */
+                     RTGCPHYS physAddr = pGuestParm->u.Pointer.u.physAddr;
 
                      pHostParm->type = VBOX_HGCM_SVC_PARM_PTR;
                      pHostParm->u.pointer.size = size;
