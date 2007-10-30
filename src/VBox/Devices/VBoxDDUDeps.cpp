bb82a2df077f6f875a36aac259b452ce586f2632
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2007-10-30 18:17:48 +0000

    Link USBLib into VBoxDDU.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@5564 cfe28804-0f27-0410-a406-dd0f0b0b656f

---
+++ src/VBox/Devices/VBoxDDUDeps.cpp
@@ -0,0 +1,35 @@
+/* $Id$ */
+/** @file
+ * VBoxDDU - For dragging in library objects.
+ */
+
+/*
+ * Copyright (c) 2007 knut st. osmundsen <bird-src-spam@anduin.net>
+ *
+ * This file is part of VirtualBox Open Source Edition (OSE), as
+ * available from http://www.virtualbox.org. This file is free software;
+ * you can redistribute it and/or modify it under the terms of the GNU
+ * General Public License as published by the Free Software Foundation,
+ * in version 2 as it comes in the "COPYING" file of the VirtualBox OSE
+ * distribution. VirtualBox OSE is distributed in the hope that it will
+ * be useful, but WITHOUT ANY WARRANTY of any kind.
+ *
+ */
+
+/*******************************************************************************
+*   Header Files                                                               *
+*******************************************************************************/
+#ifdef VBOX_WITH_USB
+# include <VBox/usbfilter.h>
+#endif
+
+/** Just a dummy global structure containing a bunch of
+ * function pointers to code which is wanted in the link.
+ */
+PFNRT g_apfnDeps[] =
+{
+#ifdef VBOX_WITH_USB
+    (PFNRT)USBFilterInit,
+#endif
+    NULL
+};
