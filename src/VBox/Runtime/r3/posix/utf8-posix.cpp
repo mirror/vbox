631fd53a213339c4a41f5332a607fe82faf329bf
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2007-10-31 21:58:45 +0000

    Leopard build fix.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@5591 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Runtime/r3/posix/utf8-posix.cpp
+++ src/VBox/Runtime/r3/posix/utf8-posix.cpp
@@ -101,7 +101,7 @@ static int rtstrConvert(const void *pvInput, size_t cbInput, const char *pszInpu
             size_t      cbOutLeft = cbOutput2;
             const void *pvInputLeft = pvInput;
             void       *pvOutputLeft = pvOutput;
-#if defined(RT_OS_LINUX) || defined(_LIBICONV_VERSION) /* there are different opinions about the constness of the input buffer. */
+#if defined(RT_OS_LINUX) || (defined(RT_OS_DARWIN) && defined(_DARWIN_FEATURE_UNIX_CONFORMANCE)) /* there are different opinions about the constness of the input buffer. */
             if (iconv(icHandle, (char **)&pvInputLeft, &cbInLeft, (char **)&pvOutputLeft, &cbOutLeft) != (size_t)-1)
 #else
             if (iconv(icHandle, (const char **)&pvInputLeft, &cbInLeft, (char **)&pvOutputLeft, &cbOutLeft) != (size_t)-1)
