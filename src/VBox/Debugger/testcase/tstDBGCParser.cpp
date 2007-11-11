75ff3e73cfdba542d4603a9e484db6d688a60305
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2007-11-11 11:11:40 +0000

    testcase framework.
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@5685 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Debugger/testcase/tstDBGCParser.cpp
+++ src/VBox/Debugger/testcase/tstDBGCParser.cpp
@@ -28,10 +28,111 @@
 
 
 /*******************************************************************************
+*   Internal Functions                                                         *
+*******************************************************************************/
+static DECLCALLBACK(bool) tstDBGCBackInput(PDBGCBACK pBack, uint32_t cMillies);
+static DECLCALLBACK(int) tstDBGCBackRead(PDBGCBACK pBack, void *pvBuf, size_t cbBuf, size_t *pcbRead);
+static DECLCALLBACK(int) tstDBGCBackWrite(PDBGCBACK pBack, const void *pvBuf, size_t cbBuf, size_t *pcbWritten);
+
+
+/*******************************************************************************
 *   Global Variables                                                           *
 *******************************************************************************/
 /** Global error counter. */
 static unsigned g_cErrors = 0;
+/** The DBGC backend structure for use in this testcase. */
+static DBGCBACK g_tstBack =
+{
+    tstDBGCBackInput,
+    tstDBGCBackRead,
+    tstDBGCBackWrite
+};
+/** For keeping track of output prefixing. */
+static bool g_fPendingPrefix = true;
+
+
+/**
+ * Checks if there is input.
+ *
+ * @returns true if there is input ready.
+ * @returns false if there not input ready.
+ * @param   pBack       Pointer to the backend structure supplied by
+ *                      the backend. The backend can use this to find
+ *                      it's instance data.
+ * @param   cMillies    Number of milliseconds to wait on input data.
+ */
+static DECLCALLBACK(bool) tstDBGCBackInput(PDBGCBACK pBack, uint32_t cMillies)
+{
+    RTPrintf("tstDBGCParser: tstDBGCBackInput was called!\n");
+    g_cErrors++;
+    return false;
+}
+
+
+/**
+ * Read input.
+ *
+ * @returns VBox status code.
+ * @param   pBack       Pointer to the backend structure supplied by
+ *                      the backend. The backend can use this to find
+ *                      it's instance data.
+ * @param   pvBuf       Where to put the bytes we read.
+ * @param   cbBuf       Maximum nymber of bytes to read.
+ * @param   pcbRead     Where to store the number of bytes actually read.
+ *                      If NULL the entire buffer must be filled for a
+ *                      successful return.
+ */
+static DECLCALLBACK(int) tstDBGCBackRead(PDBGCBACK pBack, void *pvBuf, size_t cbBuf, size_t *pcbRead)
+{
+    RTPrintf("tstDBGCParser: tstDBGCBackRead was called!\n");
+    g_cErrors++;
+    return VERR_INTERNAL_ERROR;
+}
+
+
+/**
+ * Write (output).
+ *
+ * @returns VBox status code.
+ * @param   pBack       Pointer to the backend structure supplied by
+ *                      the backend. The backend can use this to find
+ *                      it's instance data.
+ * @param   pvBuf       What to write.
+ * @param   cbBuf       Number of bytes to write.
+ * @param   pcbWritten  Where to store the number of bytes actually written.
+ *                      If NULL the entire buffer must be successfully written.
+ */
+static DECLCALLBACK(int) tstDBGCBackWrite(PDBGCBACK pBack, const void *pvBuf, size_t cbBuf, size_t *pcbWritten)
+{
+    const char *pch = (const char *)pvBuf;
+    *pcbWritten = cbBuf;
+    while (cbBuf-- > 0)
+    {
+        if (g_fPendingPrefix)
+        {
+            RTPrintf("tstDBGCParser: OUTPUT: ");
+            g_fPendingPrefix = false;
+        }
+        if (*pch == '\n')
+            g_fPendingPrefix = true;
+        RTPrintf("%c", *pch);
+        pch++;
+    }
+    return VINF_SUCCESS;
+}
+
+
+/**
+ * Completes the output, making sure that we're in
+ * the 1 position of a new line.
+ */
+static void tstCompleteOutput(void)
+{
+    if (!g_fPendingPrefix)
+        RTPrintf("\n");
+    g_fPendingPrefix = true;
+}
+
 
 
 
@@ -44,8 +145,15 @@ int main()
     RTPrintf("tstDBGCParser: TESTING...\n");
 
     /*
-     *
+     * Create a DBGC instance.
      */
+    PDBGC pDbgc;
+    int rc = dbgcCreate(&pDbgc, &g_tstBack, 0);
+    if (RT_SUCCESS(rc))
+    {
+
+        dbgcDestroy(pDbgc);
+    }
 
 
     /*
---
+++ src/VBox/Debugger/testcase/tstDBGCStubs.cpp
@@ -0,0 +1,277 @@
+/* $Id$ */
+/** @file
+ * DBGC Testcase - Command Parser, VMM Stub Functions.
+ */
+
+/*
+ * Copyright (c) 2007 knut st. osmundsen <bird-kStuff-spam@anduin.net>
+ *
+ * This file is part of VirtualBox Open Source Edition (OSE), as
+ * available from http://www.virtualbox.org. This file is free software;
+ * you can redistribute it and/or modify it under the terms of the GNU
+ * General Public License as published by the Free Software Foundation,
+ * in version 2 as it comes in the "COPYING" file of the VirtualBox OSE
+ * distribution. VirtualBox OSE is distributed in the hope that it will
+ * be useful, but WITHOUT ANY WARRANTY of any kind.
+ *
+ */
+
+#include <VBox/err.h>
+#include <VBox/cpum.h>
+
+CPUMDECL(uint32_t) CPUMGetGuestCR3(PVM pVM)
+{
+    return 0;
+}
+
+CPUMDECL(uint32_t) CPUMGetGuestCR4(PVM pVM)
+{
+    return 0;
+}
+
+CPUMDECL(RTSEL) CPUMGetGuestCS(PVM pVM)
+{
+    return 0;
+}
+
+CPUMDECL(PCCPUMCTXCORE) CPUMGetGuestCtxCore(PVM pVM)
+{
+    return NULL;
+}
+
+CPUMDECL(uint32_t) CPUMGetGuestEIP(PVM pVM)
+{
+    return 0;
+}
+
+CPUMDECL(uint32_t) CPUMGetGuestIDTR(PVM pVM, uint16_t *pcbLimit)
+{
+    return 0;
+}
+
+CPUMDECL(CPUMMODE) CPUMGetGuestMode(PVM pVM)
+{
+    return CPUMMODE_INVALID;
+}
+
+CPUMDECL(RTSEL) CPUMGetHyperCS(PVM pVM)
+{
+    return 0xfff8;
+}
+
+CPUMDECL(PCCPUMCTXCORE) CPUMGetHyperCtxCore(PVM pVM)
+{
+    return NULL;
+}
+
+CPUMDECL(uint32_t) CPUMGetHyperEIP(PVM pVM)
+{
+    return 0;
+}
+
+CPUMDECL(int) CPUMQueryGuestCtxPtr(PVM pVM, PCPUMCTX *ppCtx)
+{
+    return VERR_INTERNAL_ERROR;
+}
+
+CPUMDECL(int) CPUMQueryHyperCtxPtr(PVM pVM, PCPUMCTX *ppCtx)
+{
+    return VERR_INTERNAL_ERROR;
+}
+
+
+#include <VBox/mm.h>
+
+MMR3DECL(int) MMR3HCPhys2HCVirt(PVM pVM, RTHCPHYS HCPhys, void **ppv)
+{
+    return VERR_INTERNAL_ERROR;
+}
+
+MMR3DECL(int) MMR3ReadGCVirt(PVM pVM, void *pvDst, RTGCPTR GCPtr, size_t cb)
+{
+    return VERR_INTERNAL_ERROR;
+}
+
+
+#include <VBox/selm.h>
+
+SELMR3DECL(int) SELMR3GetSelectorInfo(PVM pVM, RTSEL Sel, PSELMSELINFO pSelInfo)
+{
+    return VERR_INTERNAL_ERROR;
+}
+
+
+#include <VBox/pgm.h>
+
+PGMDECL(uint32_t) PGMGetHyperCR3(PVM pVM)
+{
+    return 0;
+}
+
+PGMDECL(PGMMODE) PGMGetShadowMode(PVM pVM)
+{
+    return PGMMODE_INVALID;
+}
+
+PGMDECL(int) PGMPhysGCPhys2HCPhys(PVM pVM, RTGCPHYS GCPhys, PRTHCPHYS pHCPhys)
+{
+    return VERR_INTERNAL_ERROR;
+}
+
+PGMDECL(int) PGMPhysGCPhys2HCPtr(PVM pVM, RTGCPHYS GCPhys, RTUINT cbRange, PRTHCPTR pHCPtr)
+{
+    return VERR_INTERNAL_ERROR;
+}
+
+PGMDECL(int) PGMPhysGCPtr2GCPhys(PVM pVM, RTGCPTR GCPtr, PRTGCPHYS pGCPhys)
+{
+    return VERR_INTERNAL_ERROR;
+}
+
+PGMDECL(int) PGMPhysGCPtr2HCPhys(PVM pVM, RTGCPTR GCPtr, PRTHCPHYS pHCPhys)
+{
+    return VERR_INTERNAL_ERROR;
+}
+
+PGMDECL(int) PGMPhysGCPtr2HCPtr(PVM pVM, RTGCPTR GCPtr, PRTHCPTR pHCPtr)
+{
+    return VERR_INTERNAL_ERROR;
+}
+
+PGMDECL(int) PGMPhysReadGCPhys(PVM pVM, void *pvDst, RTGCPHYS GCPhysSrc, size_t cb)
+{
+    return VERR_INTERNAL_ERROR;
+}
+
+PGMR3DECL(int) PGMR3DbgHCPtr2GCPhys(PVM pVM, RTHCPTR HCPtr, PRTGCPHYS pGCPhys)
+{
+    return VERR_INTERNAL_ERROR;
+}
+
+PGMR3DECL(int) PGMR3DbgHCPtr2HCPhys(PVM pVM, RTHCPTR HCPtr, PRTHCPHYS pHCPhys)
+{
+    return VERR_INTERNAL_ERROR;
+}
+
+
+#include <VBox/dbgf.h>
+DBGFR3DECL(void) DBGFR3AddrFromFlat(PVM pVM, PDBGFADDRESS pAddress, RTGCUINTPTR FlatPtr)
+{
+}
+
+DBGFR3DECL(int) DBGFR3AddrFromSelOff(PVM pVM, PDBGFADDRESS pAddress, RTSEL Sel, RTUINTPTR off)
+{
+    return VERR_INTERNAL_ERROR;
+}
+
+DBGFR3DECL(int) DBGFR3Attach(PVM pVM)
+{
+    return VERR_INTERNAL_ERROR;
+}
+
+DBGFR3DECL(int) DBGFR3BpClear(PVM pVM, RTUINT iBp)
+{
+    return VERR_INTERNAL_ERROR;
+}
+DBGFR3DECL(int) DBGFR3BpDisable(PVM pVM, RTUINT iBp)
+{
+    return VERR_INTERNAL_ERROR;
+}
+DBGFR3DECL(int) DBGFR3BpEnable(PVM pVM, RTUINT iBp)
+{
+    return VERR_INTERNAL_ERROR;
+}
+DBGFR3DECL(int) DBGFR3BpEnum(PVM pVM, PFNDBGFBPENUM pfnCallback, void *pvUser)
+{
+    return VERR_INTERNAL_ERROR;
+}
+DBGFR3DECL(int) DBGFR3BpSet(PVM pVM, PCDBGFADDRESS pAddress, uint64_t iHitTrigger, uint64_t iHitDisable, PRTUINT piBp)
+{
+    return VERR_INTERNAL_ERROR;
+}
+DBGFR3DECL(int) DBGFR3BpSetReg(PVM pVM, PCDBGFADDRESS pAddress, uint64_t iHitTrigger, uint64_t iHitDisable,
+                               uint8_t fType, uint8_t cb, PRTUINT piBp)
+{
+    return VERR_INTERNAL_ERROR;
+}
+DBGFR3DECL(int) DBGFR3BpSetREM(PVM pVM, PCDBGFADDRESS pAddress, uint64_t iHitTrigger, uint64_t iHitDisable, PRTUINT piBp)
+{
+    return VERR_INTERNAL_ERROR;
+}
+DBGFR3DECL(bool) DBGFR3CanWait(PVM pVM)
+{
+    return true;
+}
+DBGFR3DECL(int) DBGFR3Detach(PVM pVM)
+{
+    return VERR_INTERNAL_ERROR;
+}
+DBGFR3DECL(int) DBGFR3DisasInstrEx(PVM pVM, RTSEL Sel, RTGCPTR GCPtr, unsigned fFlags, char *pszOutput, uint32_t cchOutput, uint32_t *pcbInstr)
+{
+    return VERR_INTERNAL_ERROR;
+}
+DBGFR3DECL(int) DBGFR3EventWait(PVM pVM, unsigned cMillies, PCDBGFEVENT *ppEvent)
+{
+    return VERR_INTERNAL_ERROR;
+}
+DBGFR3DECL(int) DBGFR3Halt(PVM pVM)
+{
+    return VERR_INTERNAL_ERROR;
+}
+DBGFR3DECL(int) DBGFR3Info(PVM pVM, const char *pszName, const char *pszArgs, PCDBGFINFOHLP pHlp)
+{
+    return VERR_INTERNAL_ERROR;
+}
+DBGFR3DECL(bool) DBGFR3IsHalted(PVM pVM)
+{
+    return true;
+}
+DBGFR3DECL(int) DBGFR3LineByAddr(PVM pVM, RTGCUINTPTR Address, PRTGCINTPTR poffDisplacement, PDBGFLINE pLine)
+{
+    return VERR_INTERNAL_ERROR;
+}
+DBGFR3DECL(int) DBGFR3LogModifyDestinations(PVM pVM, const char *pszDestSettings)
+{
+    return VERR_INTERNAL_ERROR;
+}
+DBGFR3DECL(int) DBGFR3LogModifyFlags(PVM pVM, const char *pszFlagSettings)
+{
+    return VERR_INTERNAL_ERROR;
+}
+DBGFR3DECL(int) DBGFR3LogModifyGroups(PVM pVM, const char *pszGroupSettings)
+{
+    return VERR_INTERNAL_ERROR;
+}
+DBGFR3DECL(int) DBGFR3ModuleLoad(PVM pVM, const char *pszFilename, RTGCUINTPTR AddressDelta, const char *pszName, RTGCUINTPTR ModuleAddress, unsigned cbImage)
+{
+    return VERR_INTERNAL_ERROR;
+}
+DBGFR3DECL(int) DBGFR3Resume(PVM pVM)
+{
+    return VERR_INTERNAL_ERROR;
+}
+DBGFR3DECL(int) DBGFR3StackWalkBeginGuest(PVM pVM, PDBGFSTACKFRAME pFrame)
+{
+    return VERR_INTERNAL_ERROR;
+}
+DBGFR3DECL(int) DBGFR3StackWalkBeginHyper(PVM pVM, PDBGFSTACKFRAME pFrame)
+{
+    return VERR_INTERNAL_ERROR;
+}
+DBGFR3DECL(int) DBGFR3StackWalkNext(PVM pVM, PDBGFSTACKFRAME pFrame)
+{
+    return VERR_INTERNAL_ERROR;
+}
+DBGFR3DECL(int) DBGFR3Step(PVM pVM)
+{
+    return VERR_INTERNAL_ERROR;
+}
+DBGFR3DECL(int) DBGFR3SymbolByAddr(PVM pVM, RTGCUINTPTR Address, PRTGCINTPTR poffDisplacement, PDBGFSYMBOL pSymbol)
+{
+    return VERR_INTERNAL_ERROR;
+}
+DBGFR3DECL(int) DBGFR3SymbolByName(PVM pVM, const char *pszSymbol, PDBGFSYMBOL pSymbol)
+{
+    return VERR_INTERNAL_ERROR;
+}
+
