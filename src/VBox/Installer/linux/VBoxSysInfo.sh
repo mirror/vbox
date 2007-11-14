97528a98bfd79d82f6476cb3de8c12a0c6d565d5
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2007-11-14 12:29:46 +0000

    Installer (Linux): added licence information to VBoxSysInfo.sh
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@5740 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Installer/linux/VBoxSysInfo.sh
+++ src/VBox/Installer/linux/VBoxSysInfo.sh
@@ -1,5 +1,20 @@
 #!/bin/sh
 
+# @file
+#
+# Installer (Unix-like)
+# Information about the host system/Linux distribution
+
+# Copyright (C) 2006-2007 innotek GmbH
+#
+# This file is part of VirtualBox Open Source Edition (OSE), as
+# available from http://www.virtualbox.org. This file is free software;
+# you can redistribute it and/or modify it under the terms of the GNU
+# General Public License as published by the Free Software Foundation,
+# in version 2 as it comes in the "COPYING" file of the VirtualBox OSE
+# distribution. VirtualBox OSE is distributed in the hope that it will
+# be useful, but WITHOUT ANY WARRANTY of any kind.
+
 # Print information about a Linux system
 # @param distribution name of the distribution
 # @param version      version of the distribution
