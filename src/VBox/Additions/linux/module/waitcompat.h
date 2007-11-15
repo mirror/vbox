1fd212078e07b65932de6b40cb10c42733a4f9c3
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2007-11-15 03:44:07 +0000

    fixed header.
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@5753 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Additions/linux/module/waitcompat.h
+++ src/VBox/Additions/linux/module/waitcompat.h
@@ -1,19 +1,4 @@
-/** @file
- *
- */
-
-/*
- * Copyright (C) 2006-2007 innotek GmbH
- *
- * This file is part of VirtualBox Open Source Edition (OSE), as
- * available from http://www.virtualbox.org. This file is free software;
- * you can redistribute it and/or modify it under the terms of the GNU
- * General Public License as published by the Free Software Foundation,
- * in version 2 as it comes in the "COPYING" file of the VirtualBox OSE
- * distribution. VirtualBox OSE is distributed in the hope that it will
- * be useful, but WITHOUT ANY WARRANTY of any kind.
- */
-
+/* copied from linux/wait.h */
 
 #ifndef wait_event_timeout
 #define wait_event_timeout(wq, condition, timeout)              \
@@ -42,3 +27,4 @@ do {                                                            \
         remove_wait_queue(&wq, &__wait);                        \
 } while (0)
 #endif
+
