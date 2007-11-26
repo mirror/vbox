bfe0e9ca37e45208c4cb43d6fcf2deb3f9f4423d
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2007-11-26 18:45:33 +0000

    r=bird: Adjusted the RTGetOpt API a little bit.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@5843 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- include/iprt/getopt.h
+++ include/iprt/getopt.h
@@ -28,100 +28,133 @@ __BEGIN_DECLS
  * @{
  */
 
-/** @def RTGETOPT_REQUIRES_ARGUMENT
- * A command line option requires that an argument (value) follow.
- * The format of the argument can be any string, unless RTGETOPT_ARG_FORMAT_INT32
- * or RTGETOPT_ARG_FORMAT_UINT32 are also specified.
- */
-#define RTGETOPT_REQUIRES_ARGUMENT              0x0001
+/** @name RTOPTIONDEF::f
+ * @{ */
 
-/** @def RTGETOPT_ARG_FORMAT_INT32
- * To be ORed with RTGETOPT_REQUIRES_ARGUMENT; the value must be a valid
- * signed integer, or an error will be returned.
- */
-#define RTGETOPT_ARG_FORMAT_INT32               0x0002
+/** Requires no extra argument. 
+ * (Can be assumed to be 0 for ever.) */
+#define RTGETOPT_REQ_NOTHING                    0
+/** A value is required or error will be returned. */
+#define RTGETOPT_REQ_STRING                     1
+/** The value must be a valid signed 32-bit integer or an error will be returned. */
+#define RTGETOPT_REQ_INT32                      2
+/** The value must be a valid signed 32-bit integer or an error will be returned. */
+#define RTGETOPT_REQ_UINT32                     3
+/** The mask of the valid required types. */
+#define RTGETOPT_REQ_MASK                       3
+/** @} */
 
-/** @def RTGETOPT_ARG_FORMAT_UINT32
- * To be ORed with RTGETOPT_REQUIRES_ARGUMENT; the value must be a valid
- * unsigned integer, or an error will be returned.
+/**
+ * An option definition.
  */
-#define RTGETOPT_ARG_FORMAT_UINT32              0x0004
-
-typedef struct _rt_getopt_option
+typedef struct RTOPTIONDEF
 {
-    const char  *pcszLong;
-    char        cShort;
-    int         fl;
+    /** The long option.
+     * This is optional */
+    const char     *pszLong;
+    /** The short option character. 
+     * This doesn't have to be a character, it may also be a \#define or enum value if 
+     * there isn't any short version of this option. */
+    int             iShort;
+    /** The flags (RTGETOPT_*). */
+    unsigned        fFlags;
 } RTOPTIONDEF;
+/** Pointer to an option definition. */
+typedef RTOPTIONDEF *PRTOPTIONDEF;
+/** Pointer to an const option definition. */
+typedef const RTOPTIONDEF *PCRTOPTIONDEF;
 
-typedef union _rt_getopt_value
+/**
+ * Option argument union.
+ * 
+ * What ends up here depends on argument format in the option definition.
+ */
+typedef union RTOPTIONUNION
 {
-    const char          *pcsz;
-    int32_t             i;
-    uint32_t            u;
+    /** Pointer to the definition on failure or when the option doesn't take an argument.
+     * This can be NULL for some errors. */
+    PCRTOPTIONDEF   pDef;
+    /** A RTGETOPT_ARG_FORMAT_STRING option argument. */
+    const char     *psz;
+    /** A RTGETOPT_ARG_FORMAT_INT32 option argument. */
+    int32_t         i32;
+    /** A RTGETOPT_ARG_FORMAT_UINT32 option argument . */
+    uint32_t        u32;
 } RTOPTIONUNION;
+/** Pointer to an option argument union. */
+typedef RTOPTIONUNION *PRTOPTIONUNION;
 
 
 /**
  * Command line argument parser, handling both long and short options and checking
  * argument formats, if desired.
  *
- * This is to be called in a loop until it returns 0 (meaning that all arguments
- * were parsed) or a negative value (meaning that an error occured).
+ * This is to be called in a loop until it returns 0 (meaning that all options
+ * were parsed) or a negative value (meaning that an error occured). The passed in 
+ * argument vector is sorted into options and non-option arguments, such that when
+ * returning 0 the *piThis is the index of the first non-option argument.
  *
  * For example, for a program which takes the following options:
  *
  *   --optwithstring (or -s) and a string argument;
- *   --optwithint (or -i) and a signed integer argument;
+ *   --optwithint (or -i) and a 32-bit signed integer argument;
  *   --verbose (or -v) with no arguments,
  *
  * code would look something like this:
  *
  * @code
- * int main(int argc, char* argv[])
+ * int main(int argc, char *argv[])
  * {
- *      static const RTOPTIONDEF g_aOptions[]
- *            = {
- *                  { "--optwithstring",    's', RTGETOPT_REQUIRES_ARGUMENT },
- *                  { "--optwithint",       'i', RTGETOPT_REQUIRES_ARGUMENT | RTGETOPT_ARG_FORMAT_INT32 },
- *                  { "--verbose",          'v', 0 },
- *              };
+ *      static const RTOPTIONDEF g_aOptions[] = 
+ *      {
+ *          { "--optwithstring",    's', RTGETOPT_REQ_STRING },
+ *          { "--optwithint",       'i', RTGETOPT_REQ_INT32 },
+ *          { "--verbose",          'v', 0 },
+ *      };
  *
- *      int c;
+ *      int ch;
  *      int i = 1;
  *      RTOPTIONUNION ValueUnion;
- *      while ((c = RTGetOpt(argc, argv, g_aOptions, RT_ELEMENTS(g_aOptions), &i, &ValueUnion)))
+ *      while ((ch = RTGetOpt(argc, argv, g_aOptions, RT_ELEMENTS(g_aOptions), &i, &ValueUnion)))
  *      {
- *          if (c < 0) .... error
+ *          if (ch < 0) { .... error }
  *
  *          // for options that require an argument, ValueUnion has received the value
- *          switch (c)
+ *          switch (ch)
  *          {
  *              case 's': // --optwithstring or -s
- *                  // string argument, copy ValueUnion.pcsz
- *              break;
+ *                  // string argument, copy ValueUnion.psz
+ *                  break;
  *
  *              case 'i': // --optwithint or -i
- *                  // integer argument, copy ValueUnion.i
- *              break;
+ *                  // integer argument, copy ValueUnion.i32
+ *                  break;
  *
  *              case 'v': // --verbose or -v
- *                  g_optVerbose = true;
- *              break;
+ *                  g_fOptVerbose = true;
+ *                  break;
  *          }
  *      }
+ *     
+ *      while (i < argc)
+ *      {
+ *          //do stuff to argv[i].
+ *      }
+ * 
+ *      return 0;
  * }
  * @endcode
  *
- * @param argc argument count, to be copied from what comes in with main().
- * @param argv arguments array, to be copied from what comes in with main().
- * @param paOptions array of RTOPTIONDEF structures, which must specify what options are understood by the program.
- * @param cOptions number of array items passed in with paOptions.
- * @param piThis address of stack counter used internally by RTGetOpt; value must be initialized to 1 before the first call!
- * @param pValueUnion union with value; in the event of an error, pcsz member points to erroneous parameter; otherwise, for options
- *             that require an argument, this contains the value of that argument, depending on the type that is required.
+ * @param argc          argument count, to be copied from what comes in with main().
+ * @param argv          argument array, to be copied from what comes in with main().
+ *                      This may end up being modified by the option/argument sorting.
+ * @param paOptions     array of RTOPTIONDEF structures, which must specify what options are understood by the program.
+ * @param cOptions      number of array items passed in with paOptions.
+ * @param piThis        address of stack counter used internally by RTGetOpt; value must be initialized to 1 before the first call!
+ * @param pValueUnion   union with value; in the event of an error, psz member points to erroneous parameter; otherwise, for options
+ *                      that require an argument, this contains the value of that argument, depending on the type that is required.
  */
-RTR3DECL(int) RTGetOpt(int argc, char *argv[], const RTOPTIONDEF *paOptions, int cOptions, int *piThis, RTOPTIONUNION *pValueUnion);
+RTDECL(int) RTGetOpt(int argc, char *argv[], PCRTOPTIONDEF paOptions, size_t cOptions, int *piThis, PRTOPTIONUNION pValueUnion);
 
 /** @} */
 
