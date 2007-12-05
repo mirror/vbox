e369324168b76c41c8b8c5aad43d5b5019f8e8d9
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2007-12-05 17:54:38 +0000

    Added a bunch of RT_C_IS/TO macros that mimics the C (aka POSIX) locale assuming an ASCII like character set. No tables or helper code, so, it's safe to use in all contexts.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@5968 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- include/iprt/ctype.h
+++ include/iprt/ctype.h
@@ -1,5 +1,5 @@
 /** @file
- * innotek Portable Runtime - ctype.h wrapper and ascii variants.
+ * innotek Portable Runtime - ctype.h wrapper and C locale variants.
  */
 
 /*
@@ -18,13 +18,36 @@
 #define ___iprt_ctype_h
 
 #ifdef IN_RING3
-#include <ctype.h>
+# include <ctype.h>
 
 # if defined(_MSC_VER) && !defined(isblank)
 /* isblank for MSC */
 #  define isblank(ch)  ( (ch) == ' ' || (ch) == '\t' )
 # endif
 #endif /* IN_RING3 */
+
+/** @name C locale macros.
+ *
+ * @remarks The macros may reference the character more than once.
+ * @remarks Assumes a charset similar to ASCII.
+ * @remarks Can probably be optimized if someone has time.
+ * @{ */
+#define RT_C_IS_BLANK(ch)   ( (ch) == ' ' || (ch) == '\t' )
+#define RT_C_IS_ALNUM(ch)   ( RT_C_IS_DIGIT(ch) || RT_C_IS_ALPHA(ch) )
+#define RT_C_IS_ALPHA(ch)   ( RT_C_IS_LOWER(ch) || RT_C_IS_UPPER(ch) )
+#define RT_C_IS_CNTRL(ch)   ( (ch) >= 0   && (ch) <  32 )
+#define RT_C_IS_DIGIT(ch)   ( (ch) >= '0' && (ch) <= '9' )
+#define RT_C_IS_LOWER(ch)   ( (ch) >= 'a' && (ch) <= 'z' )
+#define RT_C_IS_GRAPH(ch)   ( RT_C_IS_PRINT(ch) && !RT_C_IS_BLANK(ch) )
+#define RT_C_IS_PRINT(ch)   ( (ch) >= 32  && (ch) < 127 ) /**< @todo possibly incorrect */
+#define RT_C_IS_PUNCT(ch)   ( (ch) == ',' || (ch) == '.'  || (ch) == ':'  || (ch) == ';'  || (ch) == '!'  || (ch) == '?' ) /**< @todo possibly incorrect */
+#define RT_C_IS_SPACE(ch)   ( (ch) == ' ' || (ch) == '\t' || (ch) == '\n' || (ch) == '\r' || (ch) == '\f' || (ch) == '\v' )
+#define RT_C_IS_UPPER(ch)   ( (ch) >= 'A' && (ch) <= 'Z' )
+#define RT_C_IS_XDIGIT(ch)  ( RT_C_IS_DIGIT(ch) || ((ch) >= 'a' && (ch) <= 'f') || ((ch) >= 'A' && (ch) <= 'F') )
+
+#define RT_C_TO_LOWER(ch)   ( RT_C_IS_UPPER(ch) ? (ch) + ('a' - 'A') : (ch) )
+#define RT_C_TO_UPPER(ch)   ( RT_C_IS_LOWER(ch) ? (ch) - ('a' - 'A') : (ch) )
+/** @} */
 
 #endif
 
