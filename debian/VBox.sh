05545969de5c939f86085203369e4f901876976d
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2007-12-07 15:05:06 +0000

    The Giant CDDL Dual-License Header Change.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@5999 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- debian/VBox.sh
+++ debian/VBox.sh
@@ -2,15 +2,16 @@
 #
 # innotek VirtualBox
 #
-#  Copyright (C) 2006-2007 innotek GmbH
-# 
-#  This file is part of VirtualBox Open Source Edition (OSE), as
-#  available from http://www.virtualbox.org. This file is free software;
-#  you can redistribute it and/or modify it under the terms of the GNU
-#  General Public License as published by the Free Software Foundation,
-#  in version 2 as it comes in the "COPYING" file of the VirtualBox OSE
-#  distribution. VirtualBox OSE is distributed in the hope that it will
-#  be useful, but WITHOUT ANY WARRANTY of any kind.
+# Copyright (C) 2006-2007 innotek GmbH
+#
+# This file is part of VirtualBox Open Source Edition (OSE), as
+# available from http://www.virtualbox.org. This file is free software;
+# you can redistribute it and/or modify it under the terms of the GNU
+# General Public License (GPL) as published by the Free Software
+# Foundation, in version 2 as it comes in the "COPYING" file of the
+# VirtualBox OSE distribution. VirtualBox OSE is distributed in the
+# hope that it will be useful, but WITHOUT ANY WARRANTY of any kind.
+#
 
 PATH="/usr/bin:/bin:/usr/sbin:/sbin"
 
--- src/VBox/Main/idl/doxygen.xsl
+++ src/VBox/Main/idl/doxygen.xsl
@@ -6,14 +6,14 @@
  *  generate the documentation using Doxygen.
 
      Copyright (C) 2006-2007 innotek GmbH
-
+    
      This file is part of VirtualBox Open Source Edition (OSE), as
      available from http://www.virtualbox.org. This file is free software;
      you can redistribute it and/or modify it under the terms of the GNU
-     General Public License as published by the Free Software Foundation,
-     in version 2 as it comes in the "COPYING" file of the VirtualBox OSE
-     distribution. VirtualBox OSE is distributed in the hope that it will
-     be useful, but WITHOUT ANY WARRANTY of any kind.
+     General Public License (GPL) as published by the Free Software
+     Foundation, in version 2 as it comes in the "COPYING" file of the
+     VirtualBox OSE distribution. VirtualBox OSE is distributed in the
+     hope that it will be useful, but WITHOUT ANY WARRANTY of any kind.
 -->
 
 <xsl:stylesheet version="1.0" xmlns:xsl="http://www.w3.org/1999/XSL/Transform">
