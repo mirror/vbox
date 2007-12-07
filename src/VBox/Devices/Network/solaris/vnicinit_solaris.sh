8eecf4c63e418fe670d629da2a2b7fdb1d9ce40e
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2007-12-07 14:54:22 +0000

    Fix incorrect file headers that make filemuncher.pl choke.
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@5998 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Devices/Network/solaris/vnicinit_solaris.sh
+++ src/VBox/Devices/Network/solaris/vnicinit_solaris.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 # innotek VirtualBox
+# VirtualBox VNIC setup script for Solaris hosts with Crossbow.
+#
 # Copyright (C) 2007 innotek GmbH
 #
-# VirtualBox VNIC setup script for Solaris hosts with Crossbow.
+# This file is part of VirtualBox Open Source Edition (OSE), as
+# available from http://www.virtualbox.org. This file is free software;
+# you can redistribute it and/or modify it under the terms of the GNU
+# General Public License (GPL) as published by the Free Software
+# Foundation, in version 2 as it comes in the "COPYING" file of the
+# VirtualBox OSE distribution. VirtualBox OSE is distributed in the
+# hope that it will be useful, but WITHOUT ANY WARRANTY of any kind.
 #
 
 if [ -z "$1" ]; then
