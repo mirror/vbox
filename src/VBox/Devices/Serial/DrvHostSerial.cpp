4ed5a0503d6696f2da4e2e8a8fc2b1eed94567aa
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2007-12-09 00:19:24 +0000

    First try to implement monitoring of status lines
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@6016 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Devices/Serial/DrvHostSerial.cpp
+++ src/VBox/Devices/Serial/DrvHostSerial.cpp
@@ -42,6 +42,17 @@
 # include <string.h>
 # include <unistd.h>
 # include <sys/poll.h>
+# include <sys/ioctl.h>
+
+/*
+ * TIOCM_LOOP is not defined in the above header files for some reason but in asm/termios.h.
+ * But inclusion of this file however leads to compilation errors because of redefinition of some
+ * structs. Thatswhy it is defined here until a better solution is found.
+ */
+#ifndef TIOCM_LOOP
+# define TIOCM_LOOP	0x8000
+#endif
+
 #elif defined(RT_OS_WINDOWS)
 # include <windows.h>
 #endif
@@ -72,6 +83,8 @@ typedef struct DRVHOSTSERIAL
     PPDMTHREAD                  pRecvThread;
     /** Send thread. */
     PPDMTHREAD                  pSendThread;
+    /** Status lines monitor thread. */
+    PPDMTHREAD                  pMonitorThread;
     /** Send event semephore */
     RTSEMEVENT                  SendSem;
 
@@ -653,6 +666,132 @@ static DECLCALLBACK(int) drvHostSerialWakeupRecvThread(PPDMDRVINS pDrvIns, PPDMT
 #endif
 }
 
+/* -=-=-=-=- Monitor thread -=-=-=-=- */
+
+/**
+ * Monitor thread loop.
+ *
+ * This thread monitors the status lines and notifies the device
+ * if they change.
+ *
+ * @returns VINF_SUCCESS.
+ * @param   ThreadSelf  Thread handle to this thread.
+ * @param   pvUser      User argument.
+ */
+static DECLCALLBACK(int) drvHostSerialMonitorThread(PPDMDRVINS pDrvIns, PPDMTHREAD pThread)
+{
+    PDRVHOSTSERIAL pData = PDMINS2DATA(pDrvIns, PDRVHOSTSERIAL);
+    int rc = VINF_SUCCESS;
+
+#if defined (RT_OS_LINUX)
+    unsigned uStatusLinesToCheck = 0;
+
+    uStatusLinesToCheck = TIOCM_CAR | TIOCM_RNG | TIOCM_LE | TIOCM_CTS;
+#endif
+
+    while (pThread->enmState == PDMTHREADSTATE_RUNNING)
+    {
+        PDMICHARSTATUSLINES newStatusLine = 0;
+
+#if defined(RT_OS_LINUX)
+        unsigned int statusLines;
+
+        /*
+         * Wait for status line change.
+         */
+        rc = ioctl(pData->DeviceFile, TIOCMIWAIT, &uStatusLinesToCheck);
+        if (pThread->enmState != PDMTHREADSTATE_RUNNING)
+            break;
+
+        ioctl(pData->DeviceFile, TIOCMGET, &statusLines);
+
+        if (statusLines & TIOCM_CAR)
+            newStatusLine |= PDM_ICHAR_STATUS_LINES_DCD;
+        if (statusLines & TIOCM_RNG)
+            newStatusLine |= PDM_ICHAR_STATUS_LINES_RI;
+        if (statusLines & TIOCM_LE)
+            newStatusLine |= PDM_ICHAR_STATUS_LINES_DSR;
+        if (statusLines & TIOCM_CTS)
+            newStatusLine |= PDM_ICHAR_STATUS_LINES_CTS;
+        rc = pData->pDrvCharPort->pfnNotifyStatusLinesChanged(pData->pDrvCharPort, newStatusLine);
+#endif
+    }
+
+    return VINF_SUCCESS;
+}
+
+/**
+ * Unblock the monitor thread so it can respond to a state change.
+ *
+ * @returns a VBox status code.
+ * @param     pDrvIns     The driver instance.
+ * @param     pThread     The send thread.
+ */
+static DECLCALLBACK(int) drvHostSerialWakeupMonitorThread(PPDMDRVINS pDrvIns, PPDMTHREAD pThread)
+{
+    PDRVHOSTSERIAL pData = PDMINS2DATA(pDrvIns, PDRVHOSTSERIAL);
+    int rc = VINF_SUCCESS;
+#if defined(RT_OS_LINUX)
+    /*
+     * Linux is a bit difficult as the thread is sleeping in an ioctl call.
+     * So there is no way to have a wakeup pipe.
+     * Thatswhy we set the serial device into loopback mode and change one of the
+     * modem control bits.
+     * This should make the ioctl call return.
+     */
+    rc = ioctl(pData->DeviceFile, TIOCMBIS, TIOCM_LOOP);
+    if (rc < 0)
+       AssertMsgFailed(("%s: Setting device into loopback mode failed. Cannot wake up the thread!!\n", __FUNCTION__));
+
+    rc = ioctl(pData->DeviceFile, TIOCMBIS, TIOCM_RTS);
+    if (rc < 0)
+        AssertMsgFailed(("%s: Setting bit failed. Cannot wake up thread!!\n", __FUNCTION__));
+
+    /*
+     * Set serial device into normal state.
+     */
+    rc = ioctl(pData->DeviceFile, TIOCMBIC, TIOCM_LOOP);
+    if (rc < 0)
+        AssertMsgFailed(("%s: Setting device into normal mode failed. Device is not in a working state!!\n", __FUNCTION__));
+
+#elif defined(RT_OS_WINDOWS)
+    /** @todo */
+#else
+# error adapt me!
+#endif
+
+    return rc;
+}
+
+/**
+ * Set the modem lines.
+ *
+ * @returns VBox status code
+ * @param pInterface        Pointer to the interface structure.
+ * @param RequestToSend     Set to 1 if this control line should be made active.
+ * @param DataTerminalReady Set to 1 if this control line should be made active.
+ */
+static DECLCALLBACK(int) drvHostSerialSetModemLines(PPDMICHAR pInterface, unsigned RequestToSend, unsigned DataTerminalReady)
+{
+#ifdef RT_OS_LINUX
+    PDRVHOSTSERIAL pData = PDMICHAR_2_DRVHOSTSERIAL(pInterface);
+    int modemState = 0;
+
+    if (RequestToSend)
+    {
+        modemState |= TIOCM_RTS;
+    }
+
+    if (DataTerminalReady)
+    {
+        modemState |= TIOCM_DTR;
+    }
+
+    ioctl(pData->DeviceFile, TIOCMBIS, &modemState);
+#endif
+
+    return VINF_SUCCESS;
+}
 
 /* -=-=-=-=- driver interface -=-=-=-=- */
 
@@ -686,6 +825,7 @@ static DECLCALLBACK(int) drvHostSerialConstruct(PPDMDRVINS pDrvIns, PCFGMNODE pC
     /* IChar. */
     pData->IChar.pfnWrite                   = drvHostSerialWrite;
     pData->IChar.pfnSetParameters           = drvHostSerialSetParameters;
+    pData->IChar.pfnSetModemLines           = drvHostSerialSetModemLines;
 
     /*
      * Query configuration.
@@ -810,6 +950,11 @@ static DECLCALLBACK(int) drvHostSerialConstruct(PPDMDRVINS pDrvIns, PCFGMNODE pC
     if (VBOX_FAILURE(rc))
         return PDMDrvHlpVMSetError(pDrvIns, rc, RT_SRC_POS, N_("HostSerial#%d cannot create send thread"), pDrvIns->iInstance);
 
+#if defined(RT_OS_LINUX)
+    rc = PDMDrvHlpPDMThreadCreate(pDrvIns, &pData->pMonitorThread, pData, drvHostSerialMonitorThread, drvHostSerialWakeupMonitorThread, 0, RTTHREADTYPE_IO, "Serial Monitor");
+    if (VBOX_FAILURE(rc))
+        return PDMDrvHlpVMSetError(pDrvIns, rc, RT_SRC_POS, N_("HostSerial#%d cannot create monitor thread"), pDrvIns->iInstance);
+#endif
 
     PDMDrvHlpSTAMRegisterF(pDrvIns, &pData->StatBytesWritten,    STAMTYPE_COUNTER, STAMVISIBILITY_USED, STAMUNIT_BYTES, "Nr of bytes written",         "/Devices/HostSerial%d/Written", pDrvIns->iInstance);
     PDMDrvHlpSTAMRegisterF(pDrvIns, &pData->StatBytesRead,       STAMTYPE_COUNTER, STAMVISIBILITY_USED, STAMUNIT_BYTES, "Nr of bytes read",            "/Devices/HostSerial%d/Read", pDrvIns->iInstance);
