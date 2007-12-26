e6119d72161007cb891e1eb706ed58d3d6b1eb9c
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2007-12-26 19:45:16 +0000

    stupid mistake
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@6186 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Devices/Serial/DrvHostSerial.cpp
+++ src/VBox/Devices/Serial/DrvHostSerial.cpp
@@ -622,7 +622,7 @@ static DECLCALLBACK(int) drvHostSerialRecvThread(PPDMDRVINS pDrvIns, PPDMTHREAD 
                 uint8_t uNewStatusLinesState = 0;
 
                 /* Get the new state */
-                if (GetModemStatus(pData->hDeviceFile, &dwNewStatusLinesState))
+                if (GetCommModemStatus(pData->hDeviceFile, &dwNewStatusLinesState))
                 {
                     if (dwNewStatusLinesState & MS_RLSD_ON)
                         uNewStatusLinesState |= PDM_ICHAR_STATUS_LINES_DCD;
