3b97a7be5d876e877f44eb57642d2536f9adf084
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2008-01-08 14:11:28 +0000

    Guest Additions (Linux): fixed VBOXGUEST_IOCTL_WAITEVENT ioctl in kernel module
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@6278 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Additions/linux/module/waitcompat.h
+++ src/VBox/Additions/linux/module/waitcompat.h
@@ -28,3 +28,52 @@ do {                                                            \
 } while (0)
 #endif
 
+#ifndef wait_event_interruptible_timeout
+
+/* We need to define these ones here as they only exist in kernels 2.6 and up */
+
+#define __wait_event_interruptible_timeout(wq, condition, timeout, ret)   \
+do {                                                                      \
+        int __ret = 0;                                                    \
+        if (!(condition)) {                                               \
+          wait_queue_t __wait;                                            \
+          unsigned long expire;                                           \
+          init_waitqueue_entry(&__wait, current);                         \
+                                                                          \
+          expire = timeout + jiffies;                                     \
+          add_wait_queue(&wq, &__wait);                                   \
+          for (;;) {                                                      \
+                  set_current_state(TASK_INTERRUPTIBLE);                  \
+                  if (condition)                                          \
+                          break;                                          \
+                  if (jiffies > expire) {                                 \
+                          ret = jiffies - expire;                         \
+                          break;                                          \
+                  }                                                       \
+                  if (!signal_pending(current)) {                         \
+                          schedule_timeout(timeout);                      \
+                          continue;                                       \
+                  }                                                       \
+                  ret = -ERESTARTSYS;                                     \
+                  break;                                                  \
+          }                                                               \
+          current->state = TASK_RUNNING;                                  \
+          remove_wait_queue(&wq, &__wait);                                \
+        }                                                                 \
+} while (0)
+
+/*
+   retval == 0; condition met; we're good.
+   retval < 0; interrupted by signal.
+   retval > 0; timed out.
+*/
+#define wait_event_interruptible_timeout(wq, condition, timeout)        \
+({                                                                      \
+        int __ret = 0;                                                  \
+        if (!(condition))                                               \
+                __wait_event_interruptible_timeout(wq, condition,       \
+                                                timeout, __ret);        \
+        __ret;                                                          \
+})
+
+#endif  /* wait_event_interruptible_timeout not defined */
