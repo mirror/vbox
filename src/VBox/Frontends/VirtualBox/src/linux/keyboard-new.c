ec787e514a8d43f76161bb87f83ccf9cf9a8b8e6
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2008-01-23 08:35:24 +0000

    Frontends/VirtualBox: changed the licence text at the top of the Linux keyboard driver - Ulrich, please check
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@6460 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Frontends/VirtualBox/src/linux/keyboard-new.c
+++ src/VBox/Frontends/VirtualBox/src/linux/keyboard-new.c
@@ -1,15 +1,12 @@
 /*
- * Copyright (C) 2007 innotek GmbH
+ * Copyright (C) 2008 innotek GmbH
  *
- * This file is part of VirtualBox Open Source Edition (OSE), as
- * available from http://www.virtualbox.org. This file is free software;
- * you can redistribute it and/or modify it under the terms of the GNU
- * General Public License (GPL) as published by the Free Software
- * Foundation, in version 2 as it comes in the "COPYING" file of the
- * VirtualBox OSE distribution. VirtualBox OSE is distributed in the
- * hope that it will be useful, but WITHOUT ANY WARRANTY of any kind.
+ * This library is part of VirtualBox Open Source Edition (OSE), as
  * available from http://www.virtualbox.org.
  */
+
+/* This file is from the Wine project.  It was changed by innotek GmbH
+   on 22 January 2008. */
 
 /*
  * X11 keyboard driver
