e88c70a97de4f191361051c31429a316815361b4
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2008-01-25 09:53:43 +0000

    Frontends/VirtualBox: the LGPL is now in COPYING.LIB, and fixed the headers refering to it
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@6503 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Frontends/VirtualBox/src/linux/keyboard-layouts.h
+++ src/VBox/Frontends/VirtualBox/src/linux/keyboard-layouts.h
@@ -6,31 +6,19 @@
  */
 
 /*
- * Copyright (C) 2007 innotek GmbH
+ * Copyright (C) 2008 innotek GmbH
  *
- * This file is part of VirtualBox Open Source Edition (OSE), as
- * available from http://www.virtualbox.org. This file is free software;
+ * This library is part of VirtualBox Open Source Edition (OSE), as
+ * available from http://www.virtualbox.org. This library is free software;
  * you can redistribute it and/or modify it under the terms of the GNU
- * General Public License (GPL) as published by the Free Software
- * Foundation, in version 2 as it comes in the "COPYING" file of the
- * VirtualBox OSE distribution. VirtualBox OSE is distributed in the
- * hope that it will be useful, but WITHOUT ANY WARRANTY of any kind.
- * available from http://www.virtualbox.org.
- *
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
- * version 2.1 of the License, or (at your option) any later version.
+ * Lesser General Public License as published by the Free Software
+ * Foundation, in version 2.1 as it comes in the "COPYING.LIB" file of the
+ * VirtualBox OSE distribution.
  *
  * This library is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this library; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  
-USA
  */
 
 #ifndef ___VBox_keyboard_tables_h
