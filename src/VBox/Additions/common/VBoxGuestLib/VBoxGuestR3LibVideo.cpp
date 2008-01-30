bf4081652a8234be31759c4f2cf608a1bbb6f45d
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2008-01-30 13:34:16 +0000

    {}.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@6601 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Additions/common/VBoxGuestLib/VBoxGuestR3LibVideo.cpp
+++ src/VBox/Additions/common/VBoxGuestLib/VBoxGuestR3LibVideo.cpp
@@ -141,9 +141,7 @@ VBGLR3DECL(int) VbglR3GetDisplayChangeRequest(uint32_t *pcx, uint32_t *pcy, uint
     Req.display = iDisplay;
     int rc = vbglR3GRPerform(&Req.header);
     if (RT_SUCCESS(rc))
-    {
         rc = Req.header.rc;
-    }
     if (RT_SUCCESS(rc))
     {
         *pcx = Req.xres;
