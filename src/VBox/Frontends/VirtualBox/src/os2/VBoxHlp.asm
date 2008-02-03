7930aa89de4c72c742f8036b48c54255ae1968bf
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2008-02-03 19:48:51 +0000

    FE/Qt: OS/2: Hacked BEGINCODE again as it doesn't properly work for some reason.
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@6769 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Frontends/VirtualBox/src/os2/VBoxHlp.asm
+++ src/VBox/Frontends/VirtualBox/src/os2/VBoxHlp.asm
@@ -1,4 +1,4 @@
-; $Id:$
+; $Id$
 ;; @file
 ;
 ; VBox frontends: Qt GUI ("VirtualBox"):
@@ -19,11 +19,35 @@
 ; be useful, but WITHOUT ANY WARRANTY of any kind.
 ;
 
+;; @todo BEGINCODE gives us this:
+;
+;   02-03-2008  22:19:37  SYS3175  PID 4383  TID 0001  Slot 0076
+;   D:\CODING\INNOTEK\VBOX\OUT\OS2.X86\RELEASE\BIN\VIRTUALBOX.EXE
+;   c0000005
+;   17d40000
+;   P1=00000008  P2=0000bea4  P3=XXXXXXXX  P4=XXXXXXXX  
+;   EAX=00001489  EBX=00000000  ECX=00000000  EDX=00000000
+;   ESI=00000000  EDI=00001489  
+;   DS=be7f  DSACC=00f3  DSLIM=0000003f  
+;   ES=0053  ESACC=f0f3  ESLIM=ffffffff  
+;   FS=150b  FSACC=00f3  FSLIM=00000030
+;   GS=0000  GSACC=****  GSLIM=********
+;   CS:EIP=bea7:00000000  CSACC=00f2  CSLIM=00000002
+;   SS:ESP=01d7:0000ffe8  SSACC=00f3  SSLIM=0000ffff
+;   EBP=00000000  FLG=00012202
+;
+;   VBOXHLP.DLL 0003:00000000
+;
+; Looks like the previous 'segment TEXT32 ...' definition in asmdefs.mac
+; is ignored and the segment is redefined as if it had no 'CLASS=CODE...'
+; attributes...
 
-include "iprt/asmdefs.mac"
+;%include "iprt/asmdefs.mac"
+;
+;BEGINCODE
 
+segment TEXT32 public CLASS=CODE align=16 use32 flat
 
-BEGINCODE
 extern _DLL_InitTerm
 
 ; Low-level DLL entry point - Forward to the C code.
