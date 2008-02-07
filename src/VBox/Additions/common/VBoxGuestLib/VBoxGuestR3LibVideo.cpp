a4a59e23af708f334b38882176dd624fe39a7a4d
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2008-02-07 11:25:50 +0000

    Additions/common: added a call to wait for a display change request from the host
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@6842 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Additions/common/VBoxGuestLib/VBoxGuestR3LibVideo.cpp
+++ src/VBox/Additions/common/VBoxGuestLib/VBoxGuestR3LibVideo.cpp
@@ -21,6 +21,7 @@
 *******************************************************************************/
 #include <iprt/string.h>
 #include <iprt/mem.h>
+#include <iprt/assert.h>
 
 #include "VBGLR3Internal.h"
 
@@ -142,6 +143,59 @@ VBGLR3DECL(int) VbglR3GetDisplayChangeRequest(uint32_t *pcx, uint32_t *pcy, uint
         *pcx = Req.xres;
         *pcy = Req.yres;
         *pcBits = Req.bpp;
+    }
+    return rc;
+}
+
+/**
+ * Wait for a display change request event from the host.  These events must have been
+ * activated previously using VbglR3CtlFilterMask.
+ *
+ * @returns IPRT status value
+ * @param   pcx       on success, where to return the requested display width.  0 means no
+ *                    change.
+ * @param   pcy       on success, where to return the requested display height.  0 means no
+ *                    change.
+ * @param   pcBits    on success, where to return the requested bits per pixel.  0 means no
+ *                    change.
+ * @param   piDisplay on success, where to return the index of the display to be changed.
+ */
+VBGLR3DECL(int) VbglR3DisplayChangeWaitEvent(uint32_t *pcx, uint32_t *pcy, uint32_t *pcBits,
+                                             uint32_t *piDisplay)
+{
+    VBoxGuestWaitEventInfo waitEvent;
+    int rc;
+
+#ifndef VBOX_VBGLR3_XFREE86
+    AssertPtrReturn(pcx, VERR_INVALID_PARAMETER);
+    AssertPtrReturn(pcy, VERR_INVALID_PARAMETER);
+    AssertPtrReturn(pcBits, VERR_INVALID_PARAMETER);
+    AssertPtrReturn(piDisplay, VERR_INVALID_PARAMETER);
+#endif
+    waitEvent.u32TimeoutIn = RT_INDEFINITE_WAIT;
+    waitEvent.u32EventMaskIn = VMMDEV_EVENT_DISPLAY_CHANGE_REQUEST;
+    rc = vbglR3DoIOCtl(VBOXGUEST_IOCTL_WAITEVENT, &waitEvent, sizeof(waitEvent));
+    if (RT_SUCCESS(rc))
+    {
+        /* did we get the right event? */
+        if (waitEvent.u32EventFlagsOut & VMMDEV_EVENT_DISPLAY_CHANGE_REQUEST)
+        {
+            VMMDevDisplayChangeRequest2 Req = { { 0 } };
+            vmmdevInitRequest(&Req.header, VMMDevReq_GetDisplayChangeRequest2);
+            Req.eventAck = VMMDEV_EVENT_DISPLAY_CHANGE_REQUEST;
+            int rc = vbglR3GRPerform(&Req.header);
+            if (RT_SUCCESS(rc))
+                rc = Req.header.rc;
+            if (RT_SUCCESS(rc))
+            {
+                *pcx = Req.xres;
+                *pcy = Req.yres;
+                *pcBits = Req.bpp;
+                *piDisplay = Req.display;
+            }
+        }
+        else
+            rc = VERR_TRY_AGAIN;
     }
     return rc;
 }
