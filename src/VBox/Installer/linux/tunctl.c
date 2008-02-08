e94b4c670398a43d98f2a68645e2621bc59bedf6
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2008-02-08 14:19:33 +0000

    updated tunctl
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@6866 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Installer/linux/tunctl.c
+++ src/VBox/Installer/linux/tunctl.c
@@ -9,15 +9,21 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include <pwd.h>
+#include <grp.h>
 #include <net/if.h>
 #include <sys/ioctl.h>
 #include <linux/if_tun.h>
 
+/* TUNSETGROUP appeared in 2.6.23 */
+#ifndef TUNSETGROUP
+#define TUNSETGROUP   _IOW('T', 206, int)
+#endif
+
 static void Usage(char *name)
 {
-  fprintf(stderr, "Create: %s [-b] [-u owner] [-t device-name] "
+  fprintf(stderr, "Create: %s [-b] [-u owner] [-g group] [-t device-name] "
 	  "[-f tun-clone-device]\n", name);
-  fprintf(stderr, "Delete: %s -d device-name [-f tun-clone-device]\n\n", 
+  fprintf(stderr, "Delete: %s -d device-name [-f tun-clone-device]\n\n",
 	  name);
   fprintf(stderr, "The default tun clone device is /dev/net/tun - some systems"
 	  " use\n/dev/misc/net/tun instead\n\n");
@@ -29,11 +35,13 @@ int main(int argc, char **argv)
 {
   struct ifreq ifr;
   struct passwd *pw;
-  long owner = geteuid();
+  struct group *gr;
+  uid_t owner = -1;
+  gid_t group = -1;
   int tap_fd, opt, delete = 0, brief = 0;
   char *tun = "", *file = "/dev/net/tun", *name = argv[0], *end;
 
-  while((opt = getopt(argc, argv, "bd:f:t:u:")) > 0){
+  while((opt = getopt(argc, argv, "bd:f:t:u:g:")) > 0){
     switch(opt) {
       case 'b':
         brief = 1;
@@ -58,6 +66,20 @@ int main(int argc, char **argv)
 	  Usage(name);
 	}
         break;
+      case 'g':
+	gr = getgrnam(optarg);
+	if(gr != NULL){
+	  group = gr->gr_gid;
+	  break;
+	}
+        group = strtol(optarg, &end, 0);
+	if(*end != '\0'){
+	  fprintf(stderr, "'%s' is neither a groupname nor a numeric group.\n",
+		  optarg);
+	  Usage(name);
+	}
+        break;
+
       case 't':
         tun = optarg;
         break;
@@ -90,24 +112,45 @@ int main(int argc, char **argv)
 
   if(delete){
     if(ioctl(tap_fd, TUNSETPERSIST, 0) < 0){
-      perror("TUNSETPERSIST");
+      perror("disabling TUNSETPERSIST");
       exit(1);
-    }    
+    }
     printf("Set '%s' nonpersistent\n", ifr.ifr_name);
   }
   else {
+    /* emulate behaviour prior to TUNSETGROUP */
+    if(owner == -1 && group == -1) {
+      owner = geteuid();
+    }
+
+    if(owner != -1) {
+      if(ioctl(tap_fd, TUNSETOWNER, owner) < 0){
+      	perror("TUNSETOWNER");
+      	exit(1);
+      }
+    }
+    if(group != -1) {
+      if(ioctl(tap_fd, TUNSETGROUP, group) < 0){
+      	perror("TUNSETGROUP");
+      	exit(1);
+      }
+    }
+
     if(ioctl(tap_fd, TUNSETPERSIST, 1) < 0){
-      perror("TUNSETPERSIST");
+      perror("enabling TUNSETPERSIST");
       exit(1);
     }
-    if(ioctl(tap_fd, TUNSETOWNER, owner) < 0){
-      perror("TUNSETPERSIST");
-      exit(1);
-    } 
+
     if(brief)
       printf("%s\n", ifr.ifr_name);
-    else printf("Set '%s' persistent and owned by uid %ld\n", ifr.ifr_name, 
-		owner);
+    else {
+      printf("Set '%s' persistent and owned by", ifr.ifr_name);
+      if(owner != -1)
+          printf(" uid %d", owner);
+      if(group != -1)
+          printf(" gid %d", group);
+      printf("\n");
+    }
   }
   return(0);
 }
