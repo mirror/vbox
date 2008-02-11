22bcc8eabb1f212f1732773947c27034e90f7679
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2008-02-11 18:36:27 +0000

    Fixed uninitialized rc in PGMHandlerPhysicalReset.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@6908 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/VMM/VMMAll/PGMAllHandler.cpp
+++ src/VBox/VMM/VMMAll/PGMAllHandler.cpp
@@ -828,7 +828,7 @@ PGMDECL(int)  PGMHandlerPhysicalReset(PVM pVM, RTGCPHYS GCPhys)
                 STAM_COUNTER_INC(&pVM->pgm.s.StatHandlePhysicalReset);
                 PPGMRAMRANGE pRam = pgmPhysGetRange(&pVM->pgm.s, GCPhys);
                 Assert(pRam);
-                int rc = pgmHandlerPhysicalSetRamFlagsAndFlushShadowPTs(pVM, pCur, pRam);
+                rc = pgmHandlerPhysicalSetRamFlagsAndFlushShadowPTs(pVM, pCur, pRam);
                 if (rc == VINF_PGM_GCPHYS_ALIASED)
                 {
                     pVM->pgm.s.fSyncFlags |= PGM_SYNC_CLEAR_PGM_POOL;
