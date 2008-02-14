423406479f5d00356ff14df1c93ed730d2cde3fe
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2008-02-14 14:50:04 +0000

    Moved RTTLS to iprt/types.h (from thread.h)
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@6956 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- include/iprt/types.h
+++ include/iprt/types.h
@@ -940,6 +940,15 @@ typedef RTTHREAD                                   *PRTTHREAD;
 /** Nil thread handle. */
 #define NIL_RTTHREAD                                0
 
+/** A TLS index. */
+typedef int                                         RTTLS;
+/** Pointer to a TLS index. */
+typedef RTTLS                                      *PRTTLS;
+/** Pointer to a const TLS index. */
+typedef RTTLS const                                *PCRTTLS;
+/** NIL TLS index value. */
+#define NIL_RTTLS                                   (-1)
+
 /** Handle to a simple heap. */
 typedef R3R0PTRTYPE(struct RTHEAPSIMPLEINTERNAL *)  RTHEAPSIMPLE;
 /** Pointer to a handle to a simple heap. */
