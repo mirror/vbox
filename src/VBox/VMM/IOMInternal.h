4a6388f00ddf4c18800d85e0e63df2c42e5e03a6
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2008-02-20 16:26:06 +0000

    Realigning after RTGCPHYS change.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@7053 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/VMM/IOMInternal.h
+++ src/VBox/VMM/IOMInternal.h
@@ -42,13 +42,12 @@ typedef struct IOMMMIORANGER3
 {
     /** Avl node core with GCPhys as Key and GCPhys + cbSize - 1 as KeyLast. */
     AVLROGCPHYSNODECORE         Core;
-#if HC_ARCH_BITS == 64 && GC_ARCH_BITS == 32 && !defined(RT_OS_WINDOWS)
-    uint32_t                    u32Alignment; /**< The sizeof(Core) differs. */
-#endif
     /** Start physical address. */
     RTGCPHYS                    GCPhys;
     /** Size of the range. */
     RTUINT                      cbSize;
+    /** Alignment. */
+    uint32_t                    u32Alignment;
     /** Pointer to user argument. */
     RTR3PTR                     pvUser;
     /** Pointer to device instance. */
@@ -72,13 +71,12 @@ typedef struct IOMMMIORANGER0
 {
     /** Avl node core with GCPhys as Key and GCPhys + cbSize - 1 as KeyLast. */
     AVLROGCPHYSNODECORE         Core;
-#if HC_ARCH_BITS == 64 && GC_ARCH_BITS == 32 && !defined(RT_OS_WINDOWS)
-    uint32_t                    u32Alignment; /**< The sizeof(Core) differs. */
-#endif
     /** Start physical address. */
     RTGCPHYS                    GCPhys;
     /** Size of the range. */
-    RTUINT                      cbSize;
+    uint32_t                    cbSize;
+    /** Alignment. */
+    uint32_t                    u32Alignment;
     /** Pointer to user argument. */
     RTR0PTR                     pvUser;
     /** Pointer to device instance. */
@@ -102,9 +100,6 @@ typedef struct IOMMMIORANGEGC
 {
     /** Avl node core with GCPhys as Key and GCPhys + cbSize - 1 as KeyLast. */
     AVLROGCPHYSNODECORE         Core;
-#if HC_ARCH_BITS == 64 && GC_ARCH_BITS == 32 && !defined(RT_OS_WINDOWS)
-    uint32_t                    u32Alignment; /**< The sizeof(Core) differs. */
-#endif
     /** Start physical address. */
     RTGCPHYS                    GCPhys;
     /** Size of the range. */
@@ -119,9 +114,6 @@ typedef struct IOMMMIORANGEGC
     GCPTRTYPE(PFNIOMMMIOREAD)   pfnReadCallback;
     /** Pointer to fill (memset) callback function. */
     GCPTRTYPE(PFNIOMMMIOFILL)   pfnFillCallback;
-#if HC_ARCH_BITS == 64 && GC_ARCH_BITS == 32
-    RTGCPTR                     GCPtrAlignment; /**< pszDesc is 8 byte aligned. */
-#endif 
     /** Description / Name. For easing debugging. */
     R3PTRTYPE(const char *)     pszDesc;
 } IOMMMIORANGEGC;
@@ -265,7 +257,7 @@ typedef struct IOMIOPORTRANGEGC
     GCPTRTYPE(PFNIOMIOPORTINSTRING) pfnInStrCallback;
 #if HC_ARCH_BITS == 64 && GC_ARCH_BITS == 32
     RTGCPTR                     GCPtrAlignment; /**< pszDesc is 8 byte aligned. */
-#endif 
+#endif
     /** Description / Name. For easing debugging. */
     R3PTRTYPE(const char *)     pszDesc;
 } IOMIOPORTRANGEGC;
