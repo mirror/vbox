f7b1323d673f313938c3ac41e6ac9a2243f90b7c
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2008-02-25 16:01:16 +0000

    Only while LOG_TO_BACKDOOR is defined for ring-3 guest apps.
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@7118 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Additions/x11/xclient/clipboard.cpp
+++ src/VBox/Additions/x11/xclient/clipboard.cpp
@@ -41,8 +41,9 @@
 #include "clipboard.h"
 
 /* #define DEBUG_CLIPBOARD */
-#if !defined(DEBUG_CLIPBOARD)
-  /* Disabled unnecessary debug logging in the shared clipboard. */
+#if !defined(DEBUG_CLIPBOARD) && defined(LOG_TO_BACKDOOR)
+  /* Disabled unnecessary debug logging in the shared clipboard when
+     LOG_TO_BACKDOOR is defined (to be removed). */
 # undef LogFlow
 # define LogFlow(a) do {} while (0)
 #endif
