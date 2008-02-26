0cf2bf1d88f0a447211cb6d1aff91d5d3aff1573
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2008-02-26 17:59:24 +0000

    Fixed broken build caused by non-standard compliant vcc header files.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@7156 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Devices/Storage/VmdkHDDCore.cpp
+++ src/VBox/Devices/Storage/VmdkHDDCore.cpp
@@ -633,7 +633,7 @@ static int vmdkStringUnquote(PVMDKIMAGE pImage, const char *pszStr,
     if (*pszStr++ != '"')
         return vmdkError(pImage, VERR_VDI_INVALID_HEADER, RT_SRC_POS, N_("VMDK: incorrectly quoted value in descriptor in '%s'"), pImage->pszFilename);
 
-    pszQ = strchr(pszStr, '"');
+    pszQ = (char *)strchr(pszStr, '"');
     if (pszQ == NULL)
         return vmdkError(pImage, VERR_VDI_INVALID_HEADER, RT_SRC_POS, N_("VMDK: incorrectly quoted value in descriptor in '%s'"), pImage->pszFilename);
     pszUnquoted = (char *)RTMemTmpAlloc(pszQ - pszStr + 1);
