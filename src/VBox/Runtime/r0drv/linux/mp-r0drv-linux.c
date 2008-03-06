6a33cddbd1da4de9b19adfca3ced9a1db4edea8b
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2008-03-06 17:27:51 +0000

    Enabled RTMp on linux.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@7337 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Runtime/r0drv/linux/mp-r0drv-linux.c
+++ src/VBox/Runtime/r0drv/linux/mp-r0drv-linux.c
@@ -31,6 +31,7 @@
 #include "the-linux-kernel.h"
 
 #include <iprt/mp.h>
+#include <iprt/cpuset.h>
 #include <iprt/err.h>
 #include <iprt/asm.h>
 #include "r0drv/mp-r0drv.h"
@@ -42,16 +43,139 @@ RTDECL(RTCPUID) RTMpCpuId(void)
 }
 
 
-/** 
- * Wrapper between the native linux per-cpu callbacks and PFNRTWORKER 
- * 
+RTDECL(int) RTMpCpuIdToSetIndex(RTCPUID idCpu)
+{
+    return idCpu < NR_CPUS ? idCpu : -1;
+}
+
+
+RTDECL(RTCPUID) RTMpCpuIdFromSetIndex(int iCpu)
+{
+    return iCpu < NR_CPUS ? iCpu : NIL_RTCPUID;
+}
+
+
+RTDECL(RTCPUID) RTMpGetMaxCpuId(void)
+{
+    return NR_CPUS - 1; //???
+}
+
+
+RTDECL(bool) RTMpIsCpuOnline(RTCPUID idCpu)
+{
+#ifdef CONFIG_SMP
+    if (RT_UNLIKELY(idCpu >= NR_CPUS))
+        return false;
+# ifdef cpu_online
+    return cpu_online(idCpu);
+# else /* 2.4: */
+    return cpu_online_map & RT_BIT_64(idCpu);
+# endif
+#else
+    return idCpu == RTMpCpuId();
+#endif
+}
+
+
+RTDECL(bool) RTMpDoesCpuExist(RTCPUID idCpu)
+{
+#ifdef CONFIG_SMP
+    if (RT_UNLIKELY(idCpu >= NR_CPUS))
+        return false;
+# ifdef CONFIG_HOTPLUG_CPU /* introduced & uses cpu_present */
+    return cpu_present(idCpu);
+# elif defined(cpu_possible)
+    return cpu_possible(idCpu);
+# else /* 2.4: */
+    return idCpu < (RTCPUID)smp_num_cpus;
+# endif
+#else
+    return idCpu == RTMpCpuId();
+#endif
+}
+
+
+RTDECL(PRTCPUSET) RTMpGetSet(PRTCPUSET pSet)
+{
+    RTCPUID idCpu;
+
+    RTCpuSetEmpty(pSet);
+    idCpu = RTMpGetMaxCpuId();
+    do
+    {
+        if (RTMpDoesCpuExist(idCpu))
+            RTCpuSetAdd(pSet, idCpu);
+    } while (idCpu-- > 0);
+    return pSet;
+}
+
+
+RTDECL(RTCPUID) RTMpGetCount(void)
+{
+#ifdef CONFIG_SMP
+# if defined(CONFIG_HOTPLUG_CPU) /* introduced & uses cpu_present */
+    return num_present_cpus();
+# elif defined(num_possible_cpus)
+    return num_possible_cpus();
+# elif LINUX_VERSION_CODE < KERNEL_VERSION(2, 5, 0)
+    return smp_num_cpus;
+# else
+    RTCPUSET Set;
+    RTMpGetSet(&Set);
+    return RTCpuSetCount(pSet);
+# endif
+#else
+    return 1;
+#endif
+}
+
+
+RTDECL(PRTCPUSET) RTMpGetOnlineSet(PRTCPUSET pSet)
+{
+#ifdef CONFIG_SMP
+    RTCPUID idCpu;
+
+    RTCpuSetEmpty(pSet);
+    idCpu = RTMpGetMaxCpuId();
+    do
+    {
+        if (RTMpIsCpuOnline(idCpu))
+            RTCpuSetAdd(pSet, idCpu);
+    } while (idCpu-- > 0);
+#else
+    RTCpuSetEmpty(pSet);
+    RTCpuSetAdd(pSet, RTMpCpuId());
+#endif
+    return pSet;
+}
+
+
+RTDECL(RTCPUID) RTMpGetOnlineCount(void)
+{
+#ifdef CONFIG_SMP
+# if defined(num_online_cpus)
+    return num_online_cpus();
+# else
+    RTCPUSET Set;
+    RTMpGetOnlineSet(&Set);
+    return RTCpuSetCount(pSet);
+# endif
+#else
+    return 1;
+#endif
+}
+
+
+/**
+ * Wrapper between the native linux per-cpu callbacks and PFNRTWORKER
+ *
  * @param   pvInfo      Pointer to the RTMPARGS package.
  */
 static void rtmpLinuxWrapper(void *pvInfo)
 {
     PRTMPARGS pArgs = (PRTMPARGS)pvInfo;
     ASMAtomicIncU32(&pArgs->cHits);
-    pArgs->pfnWorker(smp_process_id(), pArgs->pvUser1, pArgs->pvUser2);
+    pArgs->pfnWorker(smp_processor_id(), pArgs->pvUser1, pArgs->pvUser2);
 }
 
 
@@ -65,7 +189,7 @@ RTDECL(int) RTMpOnAll(PFNRTMPWORKER pfnWorker, void *pvUser1, void *pvUser2)
     Args.idCpu = NIL_RTCPUID;
     Args.cHits = 0;
 
-#if 1 /** @todo the proper checks for 2.6.x. */
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(2, 6, 0)
     rc = on_each_cpu(rtmpLinuxWrapper, &Args, 0 /* retry */, 1 /* wait */);
 
 #else /* older kernels */
