bcd96a032dd983d294c268d9bfe31166f84fce91
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2008-03-10 13:59:19 +0000

    Changed to OSE copyright headers for Additions/solaris.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@7404 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Additions/solaris/Installer/makepackage.sh
+++ src/VBox/Additions/solaris/Installer/makepackage.sh
@@ -3,6 +3,17 @@
 # innotek VirtualBox Solaris Guest Additions package creation script.
 # Usage:
 #       makespackage.sh $(PATH_TARGET)/install packagename
+#
+# Copyright (C) 2008 innotek GmbH
+#
+# This file is part of VirtualBox Open Source Edition (OSE), as
+# available from http://www.virtualbox.org. This file is free software;
+# you can redistribute it and/or modify it under the terms of the GNU
+# General Public License (GPL) as published by the Free Software
+# Foundation, in version 2 as it comes in the "COPYING" file of the
+# VirtualBox OSE distribution. VirtualBox OSE is distributed in the
+# hope that it will be useful, but WITHOUT ANY WARRANTY of any kind.
+#
 
 if [ -z "$2" ]; then
     echo "Usage: $0 installdir packagename"
