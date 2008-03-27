c11b1221df173b446e60b272711429f374facc12
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2008-03-27 17:58:07 +0000

    fix debug builds
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@7603 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/VMM/IOM.cpp
+++ src/VBox/VMM/IOM.cpp
@@ -391,7 +391,9 @@ PIOMIOPORTSTATS iomr3IOPortStatsCreate(PVM pVM, RTIOPORT Port, const char *pszDe
  */
 PIOMMMIOSTATS iomR3MMIOStatsCreate(PVM pVM, RTGCPHYS GCPhys, const char *pszDesc)
 {
+#ifdef DEBUG_sandervl
     AssertGCPhys32(GCPhys);
+#endif
     /* check if it already exists. */
     PIOMMMIOSTATS pStats = (PIOMMMIOSTATS)RTAvloGCPhysGet(&pVM->iom.s.pTreesHC->MMIOStatTree, GCPhys);
     if (pStats)
