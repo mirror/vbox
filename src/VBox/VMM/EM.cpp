5387c3234bd84aae88cd6ba20395a4bf6b8a80c3
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2008-04-08 13:29:54 +0000

    More logging
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@7803 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/VMM/EM.cpp
+++ src/VBox/VMM/EM.cpp
@@ -2657,7 +2657,7 @@ static int emR3HwAccExecute(PVM pVM, bool *pfFFDone)
         if (pCtx->eflags.Bits.u1VM)
             Log(("HWV86: %08X IF=%d\n", pCtx->eip, pCtx->eflags.Bits.u1IF));
         else
-            Log(("HWR%d: %08X ESP=%08X IF=%d CR0=%x\n", cpl, pCtx->eip, pCtx->esp, pCtx->eflags.Bits.u1IF, (uint32_t)pCtx->cr0));
+            Log(("HWR%d: %08X ESP=%08X IF=%d CR0=%x CR4=%x EFER=%x\n", cpl, pCtx->eip, pCtx->esp, pCtx->eflags.Bits.u1IF, (uint32_t)pCtx->cr0, (uint32_t)pCtx->cr4, (uint32_t)pCtx->msrEFER));
 #endif
 
         /*
