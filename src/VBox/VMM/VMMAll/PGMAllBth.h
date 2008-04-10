65324b8cd10367ffdaa144a7a61a3373235a0795
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2008-04-10 08:23:34 +0000

    Again
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@7859 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/VMM/VMMAll/PGMAllBth.h
+++ src/VBox/VMM/VMMAll/PGMAllBth.h
@@ -3033,6 +3033,8 @@ PGM_BTH_DECL(unsigned, AssertCR3)(PVM pVM, uint64_t cr3, uint64_t cr4, RTGCUINTP
 # if PGM_GST_TYPE == PGM_TYPE_PAE
         uint32_t        iPDSrc;
         PGSTPD          pPDSrc = pgmGstGetPaePDPtr(pPGM, (RTGCUINTPTR)GCPtr, &iPDSrc);
+        if (!pPDSrc)
+            continue;
 #endif
 
         const SHWPDE PdeDst = pPDDst->a[iPDDst];
