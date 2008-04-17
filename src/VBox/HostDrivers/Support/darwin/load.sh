f5510b857663332b09bf533de2fd3d3fee5bb39d
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2008-04-17 12:33:33 +0000

    unload and smbfs.
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@8092 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/HostDrivers/Support/darwin/load.sh
+++ src/VBox/HostDrivers/Support/darwin/load.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-
-#
+## @file
 # For development.
+#
+
 #
 # Copyright (C) 2006-2007 innotek GmbH
 #
@@ -23,9 +24,12 @@
 # terms and conditions of either the GPL or the CDDL or both.
 #
 
+DRVNAME="VBoxDrv.kext"
+BUNDLE="org.virtualbox.kext.VBoxDrv"
 
-DIR="VBoxDrv.kext"
-
+DIR=`dirname "$0"`
+DIR=`cd "$DIR" && pwd`
+DIR="$DIR/$DRVNAME"
 if [ ! -d "$DIR" ]; then
     echo "Cannot find $DIR or it's not a directory..."
     exit 1;
@@ -37,10 +41,49 @@ else
 fi
 
 trap "sudo chown -R `whoami` $DIR; exit 1" INT
-set -x
+
+# Try unload any existing instance first.
+LOADED=`kextstat -b $BUNDLE -l`
+if test -n "$LOADED"; then
+    echo "load.sh: Unloading $BUNDLE..."
+    sudo kextunload -v 6 -b $BUNDLE
+    LOADED=`kextstat -b $BUNDLE -l`
+    if test -n "$LOADED"; then
+        echo "load.sh: failed to unload $BUNDLE, see above..."
+        exit 1;
+    fi
+    echo "load.sh: Successfully unloaded $BUNDLE"
+fi
+
+set -e
+# On smbfs, this might succeed just fine but make no actual changes,
+# so we might have to temporarily copy the driver to a local directory.
 sudo chown -R root:wheel "$DIR"
+OWNER=`stat -f "%u" "$DIR"`
+if test "$OWNER" -ne 0; then
+    TMP_DIR=/tmp/loaddrv.tmp
+    echo "load.sh: chown didn't work on $DIR, using temp location $TMP_DIR/$DRVNAME"
+
+    # clean up first (no sudo rm)
+    if test -e "$TMP_DIR"; then
+        sudo chown -R `whoami` "$TMP_DIR"
+        rm -Rf "$TMP_DIR"
+    fi
+
+    # make a copy and switch over DIR
+    mkdir -p "$TMP_DIR/"
+    cp -Rp "$DIR" "$TMP_DIR/"
+    DIR="$TMP_DIR/$DRVNAME"
+
+    # retry
+    sudo chown -R root:wheel "$DIR"
+fi
 sudo chmod -R o-rwx "$DIR"
 sync
+echo "load.sh: loading $DIR..."
 sudo kextload $OPTS "$DIR"
+sync
 sudo chown -R `whoami` "$DIR"
-sudo chmod 666 /dev/vboxdrv
+#sudo chmod 666 /dev/vboxdrv
+kextstat | grep org.virtualbox.kext
+
