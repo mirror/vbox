f1f423a235ffd700715595c48488a734509a55a6
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2008-04-18 12:50:36 +0000

    Fix missing notices in some file headers.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@8139 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Additions/freebsd/vboxvfs/vboxvfs.h
+++ src/VBox/Additions/freebsd/vboxvfs/vboxvfs.h
@@ -4,15 +4,10 @@
  */
 
 /*
- * Copyright (C) 2008 Sun Microsystems Inc.
+ * Copyright (C) 2008 innotek GmbH
  *
- * This file is part of VirtualBox Open Source Edition (OSE), as
- * available from http://www.virtualbox.org. This file is free software;
- * you can redistribute it and/or modify it under the terms of the GNU
- * General Public License (GPL) as published by the Free Software
- * Foundation, in version 2 as it comes in the "COPYING" file of the
- * VirtualBox OSE distribution. VirtualBox OSE is distributed in the
- * hope that it will be useful, but WITHOUT ANY WARRANTY of any kind.
+ * innotek GmbH confidential
+ * All rights reserved
  */
 
 #ifndef ___VBOXVFS_H___
