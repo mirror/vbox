7ae844e89d05e693e1d97c50c8c72cddc02e3790
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2008-05-05 15:53:09 +0000

    Another parameter check
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@8605 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Main/SATAControllerImpl.cpp
+++ src/VBox/Main/SATAControllerImpl.cpp
@@ -298,6 +298,11 @@ STDMETHODIMP SATAController::GetIDEEmulationPort(LONG DevicePosition, LONG *aPor
 
 STDMETHODIMP SATAController::SetIDEEmulationPort(LONG DevicePosition, LONG aPortNumber)
 {
+    if ((aPortNumber < 0) || (aPortNumber >= 30))
+        return setError (E_INVALIDARG,
+            tr ("Invalid port number: %l (must be in range [%lu, %lu])"),
+                aPortCount, 0, 29);
+
     AutoCaller autoCaller (this);
     CheckComRCReturnRC (autoCaller.rc());
 
