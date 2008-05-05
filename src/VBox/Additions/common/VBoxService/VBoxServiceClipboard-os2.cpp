6a3d332096b7dc9e5886c6e2245e71c517dfc9dc
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2008-05-05 13:56:33 +0000

    AssertMsgBreakVoid -> AssertMsgBreak.
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@8580 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Additions/common/VBoxService/VBoxServiceClipboard-os2.cpp
+++ src/VBox/Additions/common/VBoxService/VBoxServiceClipboard-os2.cpp
@@ -672,7 +672,7 @@ static MRESULT EXPENTRY VBoxServiceClipboardOS2WinProc(HWND hwnd, ULONG msg, MPA
         case WM_DRAWCLIPBOARD:
             if (g_enmState == kClipboardState_SettingViewer)
                 break;
-            AssertMsgBreakVoid(g_enmState == kClipboardState_Viewer, ("g_enmState=%d\n", g_enmState));
+            AssertMsgBreak(g_enmState == kClipboardState_Viewer, ("g_enmState=%d\n", g_enmState));
             VBoxServiceClipboardOS2Poll();
             break;
 
@@ -683,7 +683,7 @@ static MRESULT EXPENTRY VBoxServiceClipboardOS2WinProc(HWND hwnd, ULONG msg, MPA
         case WM_DESTROYCLIPBOARD:
             if (g_enmState == kClipboardState_Destroying)
                 break; /* it's us doing the replacing, ignore. */
-            AssertMsgBreakVoid(g_enmState == kClipboardState_Owner, ("g_enmState=%d\n", g_enmState));
+            AssertMsgBreak(g_enmState == kClipboardState_Owner, ("g_enmState=%d\n", g_enmState));
             VBoxServiceClipboardOS2Destroyed();
             break;
 
@@ -692,7 +692,7 @@ static MRESULT EXPENTRY VBoxServiceClipboardOS2WinProc(HWND hwnd, ULONG msg, MPA
          * This is called by someone which owns the clipboard, but that's fine.
          */
         case WM_RENDERFMT:
-            AssertMsgBreakVoid(g_enmState == kClipboardState_Owner, ("g_enmState=%d\n", g_enmState));
+            AssertMsgBreak(g_enmState == kClipboardState_Owner, ("g_enmState=%d\n", g_enmState));
             VBoxServiceClipboardOS2RenderFormat(SHORT1FROMMP(mp1));
             break;
 
