7d3351a58227019ea82a19f02870e8f37948cb0d
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2008-05-05 08:34:16 +0000

    Solaris hostif: fixed vnic_init script for the changed vna.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@8555 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Devices/Network/solaris/vnicinit_solaris.sh
+++ src/VBox/Devices/Network/solaris/vnicinit_solaris.sh
@@ -41,7 +41,7 @@ if [ -z "$2" ]; then
     fi
 
     # To use a specific physical NIC, replace $phys_nic with the name of the NIC.
-    vnic_name=`/usr/lib/vna $phys_nic $mac`
+    vnic_name="vnic"`/usr/lib/vna $phys_nic $mac`
     if [ $? != 0 ]; then
         exit 1
     fi
