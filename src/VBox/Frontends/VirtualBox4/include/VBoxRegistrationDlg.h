46092cbc3abdfe0a374d4240c55dd53df3f9d61f
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2008-05-07 11:30:29 +0000

    FE/Qt4: Fixed last Qt3 support warnings and added copyright notice.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@8648 cfe28804-0f27-0410-a406-dd0f0b0b656f

---
+++ src/VBox/Frontends/VirtualBox4/include/VBoxRegistrationDlg.h
@@ -0,0 +1,82 @@
+/** @file
+ *
+ * VBox frontends: Qt4 GUI ("VirtualBox"):
+ * VBoxRegistrationDlg class declaration
+ */
+
+/*
+ * Copyright (C) 2006-2008 Sun Microsystems, Inc.
+ *
+ * This file is part of VirtualBox Open Source Edition (OSE), as
+ * available from http://www.virtualbox.org. This file is free software;
+ * you can redistribute it and/or modify it under the terms of the GNU
+ * General Public License (GPL) as published by the Free Software
+ * Foundation, in version 2 as it comes in the "COPYING" file of the
+ * VirtualBox OSE distribution. VirtualBox OSE is distributed in the
+ * hope that it will be useful, but WITHOUT ANY WARRANTY of any kind.
+ *
+ * Please contact Sun Microsystems, Inc., 4150 Network Circle, Santa
+ * Clara, CA 95054 USA or visit http://www.sun.com if you need
+ * additional information or have any questions.
+ */
+
+#ifndef __VBoxRegistrationDlg_h__
+#define __VBoxRegistrationDlg_h__
+
+#include <QIAbstractWizard.h>
+#include <VBoxRegistrationDlg.gen.h>
+#include <COMDefs.h>
+#include <QIWidgetValidator.h>
+
+/* Qt includes */
+#include <QUrl>
+
+class VBoxNetworkFramework;
+
+class VBoxRegistrationDlg : public QIAbstractWizard, public Ui::VBoxRegistrationDlg
+{
+    Q_OBJECT;
+
+public:
+
+    VBoxRegistrationDlg (VBoxRegistrationDlg **aSelf, QWidget *aParent = 0,
+                         Qt::WindowFlags aFlags = 0);
+   ~VBoxRegistrationDlg();
+
+    static bool hasToBeShown();
+
+private slots:
+
+    void accept();
+    void reject();
+    void handshake();
+    void registration();
+    void processTimeout();
+    void onNetBegin (int aStatus);
+    void onNetData (const QByteArray &aData);
+    void onNetEnd (const QByteArray &aData);
+    void onNetError (const QString &aError);
+
+    void revalidate (QIWidgetValidator *aWval);
+    void enableNext (const QIWidgetValidator *aWval);
+    void onPageShow();
+
+private:
+
+    void postRequest (const QString &aHost, const QString &aPath);
+    void abortRegisterRequest (const QString &aReason);
+    QString getPlatform();
+    void finish();
+
+    VBoxRegistrationDlg **mSelf;
+    QIWidgetValidator    *mWvalReg;
+    QUrl                  mUrl;
+    QString               mKey;
+    QTimer               *mTimeout;
+    bool                  mHandshake;
+    bool                  mSuicide;
+    VBoxNetworkFramework *mNetfw;
+};
+
+#endif // __VBoxRegistrationDlg_h__
+
