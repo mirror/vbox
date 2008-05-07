b29dd4c46212bc8ef090af1ecee9e1b66925b755
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2008-05-07 21:40:19 +0000

    ordering
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@8686 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Runtime/common/err/errmsgxpcom.cpp
+++ src/VBox/Runtime/common/err/errmsgxpcom.cpp
@@ -52,20 +52,6 @@ static const RTCOMERRMSG  g_aStatusMsgs[] =
     { "NS_ERROR_ABORT",                     "Operation aborted",                            UINT32_C(0x80004004) },
     { "NS_ERROR_FAILURE",                   "Operation failed",                             UINT32_C(0x80004005) },
     { "NS_ERROR_UNEXPECTED",                "Unexpected error",                             UINT32_C(0x8000ffff) },
-    { "E_ACCESSDENIED",                     "Access denied",                                UINT32_C(0x80070005) }, /* VirtualBox addition */
-    { "NS_ERROR_OUT_OF_MEMORY",             "Memory allocation failed",                     UINT32_C(0x8007000e) },
-    { "NS_ERROR_INVALID_ARG",               "Invalid argument value",                       UINT32_C(0x80070057) },
-    { "NS_ERROR_NOT_INITIALIZED",           "Instance not initialized",                     UINT32_C(0xc1f30001) },
-    { "NS_ERROR_ALREADY_INITIALIZED",       "Instance already initialized",                 UINT32_C(0xc1f30002) },
-
-    { "NS_ERROR_NO_AGGREGATION",            "Class does not allow aggregation",             UINT32_C(0x80040110) },
-    { "NS_ERROR_NOT_AVAILABLE",             "Resource not available",                       UINT32_C(0x80040111) },
-    { "NS_ERROR_FACTORY_NOT_REGISTERED",    "Class not registered",                         UINT32_C(0x80040154) },
-    { "NS_ERROR_FACTORY_REGISTER_AGAIN",    "Cannot be registered, try again later",        UINT32_C(0x80040155) },
-    { "NS_ERROR_FACTORY_NOT_LOADED",        "Dynamically loaded factory cannot be found",   UINT32_C(0x800401f8) },
-
-    { "NS_ERROR_FACTORY_EXISTS",            "Factory already exists",                       UINT32_C(0xc1f30100) },
-    { "NS_ERROR_FACTORY_NO_SIGNATURE_SUPPORT", "Factory does not support signatures",       UINT32_C(0xc1f30101) },
 
     { "NS_ERROR_CANNOT_CONVERT_DATA",       "Cannot convert data",                          UINT32_C(0x80010001) },
     { "NS_ERROR_OBJECT_IS_IMMUTABLE",       "Object is immutable",                          UINT32_C(0x80010002) },
@@ -73,6 +59,20 @@ static const RTCOMERRMSG  g_aStatusMsgs[] =
     { "NS_ERROR_PROXY_INVALID_IN_PARAMETER","Cannot proxy an IN parameter",                 UINT32_C(0x80010010) },
     { "NS_ERROR_PROXY_INVALID_OUT_PARAMETER", "Cannot proxy an OUT parameter",              UINT32_C(0x80010011) },
     { "NS_SUCCESS_LOSS_OF_INSIGNIFICANT_DATA", "Loss of insignificant data",                UINT32_C(0x00010001) },
+
+    { "E_ACCESSDENIED",                     "Access denied",                                UINT32_C(0x80070005) }, /* VirtualBox addition */
+    { "NS_ERROR_OUT_OF_MEMORY",             "Memory allocation failed",                     UINT32_C(0x8007000e) },
+    { "NS_ERROR_INVALID_ARG",               "Invalid argument value",                       UINT32_C(0x80070057) },
+
+    { "NS_ERROR_NO_AGGREGATION",            "Class does not allow aggregation",             UINT32_C(0x80040110) },
+    { "NS_ERROR_NOT_AVAILABLE",             "Resource not available",                       UINT32_C(0x80040111) },
+    { "NS_ERROR_FACTORY_NOT_REGISTERED",    "Class not registered",                         UINT32_C(0x80040154) },
+    { "NS_ERROR_FACTORY_REGISTER_AGAIN",    "Cannot be registered, try again later",        UINT32_C(0x80040155) },
+    { "NS_ERROR_FACTORY_NOT_LOADED",        "Dynamically loaded factory cannot be found",   UINT32_C(0x800401f8) },
+    { "NS_ERROR_FACTORY_EXISTS",            "Factory already exists",                       UINT32_C(0xc1f30100) },
+    { "NS_ERROR_FACTORY_NO_SIGNATURE_SUPPORT", "Factory does not support signatures",       UINT32_C(0xc1f30101) },
+    { "NS_ERROR_NOT_INITIALIZED",           "Instance not initialized",                     UINT32_C(0xc1f30001) },
+    { "NS_ERROR_ALREADY_INITIALIZED",       "Instance already initialized",                 UINT32_C(0xc1f30002) },
 
     { "NS_BASE_STREAM_CLOSED",              "Stream closed",                                UINT32_C(0x80470002) },
     { "NS_BASE_STREAM_OSERROR",             "Operative system stream error",                UINT32_C(0x80470003) },
