f2c9f9a899ba41b4eca86dd16c2e47ba77fa2aee
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2008-05-08 13:25:41 +0000

    r=bird: the timeout handling is busted since we're using absolute wall clock.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@8706 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Runtime/r3/posix/semeventmulti-posix.cpp
+++ src/VBox/Runtime/r3/posix/semeventmulti-posix.cpp
@@ -367,6 +367,13 @@ static int rtSemEventMultiWait(RTSEMEVENTMULTI EventMultiSem, unsigned cMillies,
         /*
          * Get current time and calc end of wait time.
          */
+        /** @todo Something is braindead here. we're getting occational timeouts after no time has
+         * elapsed on linux 2.6.23. (ata code typically)
+         *
+         * The general problem here is that we're using the realtime clock, i.e. the wall clock
+         * that is subject to ntp updates and user alteration, so we will have to compenstate
+         * for this by using RTTimeMilliTS together with the clock_gettime()/gettimeofday() call.
+         * Joy, oh joy. */
         struct timespec     ts = {0,0};
 #ifdef RT_OS_DARWIN
         struct timeval      tv = {0,0};
