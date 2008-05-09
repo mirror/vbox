ec89432c0705bcd8278443a13ccadd52716ba6d6
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2008-05-09 12:25:03 +0000

    FE/Qt4: new QIButtonDialogBox: - auto set the properties of the help button Snapshot Detail Dialog: - fixed last Qt3 support warnings - disabled auto fill background of the summary window (OSX) - added new QIButtonDialogBox - added copyright notice
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@8740 cfe28804-0f27-0410-a406-dd0f0b0b656f

---
+++ src/VBox/Frontends/VirtualBox4/src/VBoxSnapshotDetailsDlg.cpp
@@ -0,0 +1,79 @@
+/** @file
+ *
+ * VBox frontends: Qt4 GUI ("VirtualBox"):
+ * VBoxSnapshotDetailsDlg class implementation
+ */
+
+/*
+ * Copyright (C) 2006-2008 Sun Microsystems, Inc.
+ *
+ * This file is part of VirtualBox Open Source Edition (OSE), as
+ * available from http://www.virtualbox.org. This file is free software;
+ * you can redistribute it and/or modify it under the terms of the GNU
+ * General Public License (GPL) as published by the Free Software
+ * Foundation, in version 2 as it comes in the "COPYING" file of the
+ * VirtualBox OSE distribution. VirtualBox OSE is distributed in the
+ * hope that it will be useful, but WITHOUT ANY WARRANTY of any kind.
+ *
+ * Please contact Sun Microsystems, Inc., 4150 Network Circle, Santa
+ * Clara, CA 95054 USA or visit http://www.sun.com if you need
+ * additional information or have any questions.
+ */
+
+#include <VBoxSnapshotDetailsDlg.h>
+#include <VBoxGlobal.h>
+#include <VBoxProblemReporter.h>
+#include <VBoxUtils.h>
+
+/* Qt includes */
+#include <QPushButton>
+
+VBoxSnapshotDetailsDlg::VBoxSnapshotDetailsDlg (QWidget *aParent)
+    : QDialog (aParent)
+{
+    /* Apply UI decorations */
+    setupUi (this);
+
+    /* Setup mTeSummary browser. */
+    mTeSummary->viewport()->setAutoFillBackground (false);
+    mTeSummary->setFocus();
+
+    /* Setup connections */
+    connect (mLeName, SIGNAL (textChanged (const QString&)),
+             this, SLOT (onNameChanged (const QString&)));
+    connect (mButtonBox, SIGNAL (helpRequested()),
+             &vboxProblem(), SLOT (showHelpHelpDialog()));
+
+    resize (450, 450);
+}
+
+void VBoxSnapshotDetailsDlg::getFromSnapshot (const CSnapshot &aSnapshot)
+{
+    mSnapshot = aSnapshot;
+    CMachine machine = aSnapshot.GetMachine();
+
+    /* Get properties */
+    mLeName->setText (aSnapshot.GetName());
+    mTeDescription->setText (aSnapshot.GetDescription());
+
+    /* Compose summary */
+    mTeSummary->setText (
+        vboxGlobal().detailsReport (machine, false /* isNewVM */,
+                                             false /* withLinks */));
+    setWindowTitle (tr ("Details of %1 (%2)")
+                    .arg (mSnapshot.GetName()).arg (machine.GetName()));
+}
+
+void VBoxSnapshotDetailsDlg::putBackToSnapshot()
+{
+    AssertReturn (!mSnapshot.isNull(), (void) 0);
+
+    mSnapshot.SetName (mLeName->text());
+    mSnapshot.SetDescription (mTeDescription->toPlainText());
+}
+
+void VBoxSnapshotDetailsDlg::onNameChanged (const QString &aText)
+{
+    mButtonBox->button (QDialogButtonBox::Ok)->setEnabled (!aText.trimmed().isEmpty());
+}
+
