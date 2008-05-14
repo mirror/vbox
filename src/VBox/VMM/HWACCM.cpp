82cb89d22baf44736bd86f522428c811017cbb74
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2008-05-14 14:49:10 +0000

    Added a release log statement about erratum 170
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@8816 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/VMM/HWACCM.cpp
+++ src/VBox/VMM/HWACCM.cpp
@@ -425,6 +425,35 @@ HWACCMR3DECL(int) HWACCMR3InitFinalizeR0(PVM pVM)
 
         if (pVM->hwaccm.s.fInitialized == false)
         {
+            /* Erratum 170 which requires a forced TLB flush for each world switch:
+             * See http://www.amd.com/us-en/assets/content_type/white_papers_and_tech_docs/33610.pdf
+             *
+             * All BH-G1/2 and DH-G1/2 models include a fix:
+             * Athlon X2:   0x6b 1/2
+             *              0x68 1/2
+             * Athlon 64:   0x7f 1
+             *              0x6f 2
+             * Sempron:     0x7f 1/2
+             *              0x6f 2
+             *              0x6c 2
+             *              0x7c 2
+             * Turion 64:   0x68 2
+             *
+             */
+            uint32_t u32Dummy;
+            uint32_t u32Version, u32Family, u32Model, u32Stepping;
+            ASMCpuId(1, &u32Version, &u32Dummy, &u32Dummy, &u32Dummy);
+            u32Family    = ((u32Version >> 8) & 0xf) + (((u32Version >> 8) & 0xf) == 0xf ? (u32Version >> 20) & 0x7f : 0);
+            u32Model     = ((u32Version >> 4) & 0xf);
+            u32Model     = u32Model | ((u32Model == 0xf ? (u32Version >> 16) & 0x0f : 0) << 4);
+            u32Stepping  = u32Version & 0xf;
+            if (    u32Family == 0xf
+                &&  !((u32Model == 0x68 || u32Model == 0x6b || u32Model == 0x7f) &&  u32Stepping >= 1)
+                &&  !((u32Model == 0x6f || u32Model == 0x6c || u32Model == 0x7c) &&  u32Stepping >= 2))
+            {
+                LogRel(("HWACMM: AMD cpu with erratum 170 family %x model %x stepping %x\n", u32Family, u32Model, u32Stepping));
+            }
+
             LogRel(("HWACMM: cpuid 0x80000001.u32AMDFeatureECX = %VX32\n", pVM->hwaccm.s.cpuid.u32AMDFeatureECX));
             LogRel(("HWACMM: cpuid 0x80000001.u32AMDFeatureEDX = %VX32\n", pVM->hwaccm.s.cpuid.u32AMDFeatureEDX));
             LogRel(("HWACCM: SVM revision                      = %X\n", pVM->hwaccm.s.svm.u32Rev));
