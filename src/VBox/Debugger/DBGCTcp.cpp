deaac00f283091c34931ba83c5d7ce0bdc5cf76b
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2008-05-15 01:34:45 +0000

    VBOX_WITH_DEBUGGER governs the telnet debugger, not DEBUG, since the qt bit doesn't work reliably on X and OS X.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@8827 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Debugger/DBGCTcp.cpp
+++ src/VBox/Debugger/DBGCTcp.cpp
@@ -214,7 +214,7 @@ DBGDECL(int)    DBGCTcpCreate(PVM pVM, void **ppvData)
     bool fEnabled;
     int rc = CFGMR3QueryBool(pKey, "Enabled", &fEnabled);
     if (rc == VERR_CFGM_VALUE_NOT_FOUND || rc == VERR_CFGM_NO_PARENT)
-#if defined(DEBUG) && !defined(__L4ENV__) && !defined(DEBUG_dmik)
+#if defined(VBOX_WITH_DEBUGGER) && !defined(__L4ENV__) && !defined(DEBUG_dmik)
         fEnabled = true;
 #else
         fEnabled = false;
