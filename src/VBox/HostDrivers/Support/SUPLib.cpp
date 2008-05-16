ce1edf834ffb34a3f3bec8e10b87304f6492ec4e
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2008-05-16 12:43:44 +0000

    require SUPDRVIOC_VERSION >= 0x00070002 because of new vboxdrv export (RTMpCpuId()).
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@8891 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/HostDrivers/Support/SUPLib.cpp
+++ src/VBox/HostDrivers/Support/SUPLib.cpp
@@ -213,7 +213,7 @@ SUPR3DECL(int) SUPInit(PSUPDRVSESSION *ppSession /* NULL */, size_t cbReserve /*
         strcpy(CookieReq.u.In.szMagic, SUPCOOKIE_MAGIC);
         CookieReq.u.In.u32ReqVersion = SUPDRVIOC_VERSION;
         const uint32_t MinVersion = (SUPDRVIOC_VERSION & 0xffff0000) == 0x00070000
-                                  ? 0x00070001 /* need new exports */
+                                  ? 0x00070002 /* need new exports */
                                   : SUPDRVIOC_VERSION & 0xffff0000;
         CookieReq.u.In.u32MinVersion = MinVersion;
         rc = suplibOsIOCtl(SUP_IOCTL_COOKIE, &CookieReq, SUP_IOCTL_COOKIE_SIZE);
