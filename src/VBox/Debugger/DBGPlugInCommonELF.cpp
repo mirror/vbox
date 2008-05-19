74e6a1e5deddc03f2458727f02042242c7087df2
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2008-05-19 18:57:04 +0000

    gcc-4.3 warning
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@8929 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Debugger/DBGPlugInCommonELF.cpp
+++ src/VBox/Debugger/DBGPlugInCommonELF.cpp
@@ -68,7 +68,7 @@ int DBGDiggerCommonParseElf32Mod(PVM pVM, const char *pszModName, const char *ps
     AssertPtrReturn(pszModName, VERR_INVALID_POINTER);
     AssertPtrReturn(pszFilename, VERR_INVALID_POINTER);
     AssertReturn(!(fFlags & ~DBG_DIGGER_ELF_MASK), VERR_INVALID_PARAMETER);
-    AssertReturn(fFlags & (DBG_DIGGER_ELF_FUNNY_SHDRS | DBG_DIGGER_ELF_ADJUST_SYM_VALUE)
+    AssertReturn((fFlags & (DBG_DIGGER_ELF_FUNNY_SHDRS | DBG_DIGGER_ELF_ADJUST_SYM_VALUE))
                  != (DBG_DIGGER_ELF_FUNNY_SHDRS | DBG_DIGGER_ELF_ADJUST_SYM_VALUE), VERR_INVALID_PARAMETER);
     AssertPtrReturn(paShdrs, VERR_INVALID_POINTER);
     AssertPtrReturn(paSyms, VERR_INVALID_POINTER);
