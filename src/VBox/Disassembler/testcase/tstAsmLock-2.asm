010a7d4a95a56a08e7442a28cce9d8baa6aadce8
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2008-05-20 17:59:23 +0000

    ADD and ADC + a make file.
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@8968 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Disassembler/testcase/tstAsmLock-2.asm
+++ src/VBox/Disassembler/testcase/tstAsmLock-2.asm
@@ -74,8 +74,94 @@
     lock adc rcx, [0badh]
 %endif
 
+    ;
     ; ADD
+    ;
+        ; 04 ib         ADD AL, imm8
+    lock add al, byte 8
+        ; 05 i[wd]      ADD [ER]AX, immX
+    lock add ax, word 16
+    lock add eax, dword 128
+%ifdef WITH_64_BIT_TESTS
+    lock add rax, dword 256
+    lock add rax, dword 0cc90cc90h
+%endif
+        ; 80 /0 ib      ADD reg/mem8, imm8 - with reg dst
+    lock add cl, byte 8
+        ; 81 /0 i[wd]   ADD reg/memX, immX - with reg dst
+    lock add cx, word 1000h
+    lock add ecx, dword 100000h
+%ifdef WITH_64_BIT_TESTS
+    lock add rcx, dword 100000h
+%endif
+        ; 83 /0 ib      ADD reg/memX, imm8 - with reg dst
+    lock add cx, byte 07fh
+    lock add ecx, byte 07fh
+%ifdef WITH_64_BIT_TESTS
+    lock add rcx, byte 07fh
+%endif
+
+        ; 00 /r         ADD reg/mem8, reg8 - with reg dst
+    lock add cl, bl
+        ; 01 /r         ADD reg/memX, regX - with reg dst
+    lock add cx, bx
+    lock add ecx, ebx
+%ifdef WITH_64_BIT_TESTS
+    lock add rcx, rbx
+%endif
+        ; 02 /r         ADD reg8, reg/mem8
+    lock add cl, [0badh]
+        ; 03 /r         ADD regX, reg/memX
+    lock add cx, [0badh]
+    lock add ecx, [0badh]
+%ifdef WITH_64_BIT_TESTS
+    lock add rcx, [0badh]
+%endif
+
+    ;
     ; AND
+    ;
+        ; 24 ib         AND AL, imm8
+    lock and al, byte 8
+        ; 25 i[wd]      AND [ER]AX, immX
+    lock and ax, word 16
+    lock and eax, dword 128
+%ifdef WITH_64_BIT_TESTS
+    lock and rax, dword 256
+    lock and rax, dword 0cc90cc90h
+%endif
+        ; 80 /4 ib      AND reg/mem8, imm8 - with reg dst
+    lock and cl, byte 8
+        ; 81 /4 i[wd]   AND reg/memX, immX - with reg dst
+    lock and cx, word 1000h
+    lock and ecx, dword 100000h
+%ifdef WITH_64_BIT_TESTS
+    lock and rcx, dword 100000h
+%endif
+        ; 83 /4 ib      AND reg/memX, imm8 - with reg dst
+    lock and cx, byte 07fh
+    lock and ecx, byte 07fh
+%ifdef WITH_64_BIT_TESTS
+    lock and rcx, byte 07fh
+%endif
+
+        ; 20 /r         AND reg/mem8, reg8 - with reg dst
+    lock and cl, bl
+        ; 21 /r         AND reg/memX, regX - with reg dst
+    lock and cx, bx
+    lock and ecx, ebx
+%ifdef WITH_64_BIT_TESTS
+    lock and rcx, rbx
+%endif
+        ; 22 /r         AND reg8, reg/mem8
+    lock and cl, [0badh]
+        ; 23 /r         AND regX, reg/memX
+    lock and cx, [0badh]
+    lock and ecx, [0badh]
+%ifdef WITH_64_BIT_TESTS
+    lock and rcx, [0badh]
+%endif
+
     ; BTC
     ; BTR
     ; BTS
