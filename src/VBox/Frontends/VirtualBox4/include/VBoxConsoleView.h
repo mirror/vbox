3f43a7435bd6f948d2f5bfc5c677f5fa50401856
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2008-05-22 06:16:10 +0000

    Frontends/VirtualBox and VirtualBox4: whether or not the console view object should perform resize operations should depend on the state of the guest SUPPORTS_GRAPHICS capability, not on whether or not the Additions are present
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@9048 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Frontends/VirtualBox4/include/VBoxConsoleView.h
+++ src/VBox/Frontends/VirtualBox4/include/VBoxConsoleView.h
@@ -257,7 +257,7 @@ private:
     bool mIgnoreMainwndResize : 1;
     bool mAutoresizeGuest : 1;
 
-    bool mIsAdditionsActive : 1;
+    bool mGuestSupportsGraphics : 1;
 
     bool mNumLock : 1;
     bool mScrollLock : 1;
