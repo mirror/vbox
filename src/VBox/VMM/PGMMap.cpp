96b5788507b838ee7e9e6da857e93c5963c0513a
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2008-06-02 13:47:21 +0000

    Signed/unsigned mismatch. More string format specifiers
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@9303 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/VMM/PGMMap.cpp
+++ src/VBox/VMM/PGMMap.cpp
@@ -781,8 +781,8 @@ static void pgmR3MapSetPDEs(PVM pVM, PPGMMAPPING pMap, unsigned iNewPDE)
  */
 void pgmR3MapRelocate(PVM pVM, PPGMMAPPING pMapping, RTGCPTR GCPtrOldMapping, RTGCPTR GCPtrNewMapping)
 {
-    int iPDOld = GCPtrOldMapping >> X86_PD_SHIFT;
-    int iPDNew = GCPtrNewMapping >> X86_PD_SHIFT;
+    unsigned iPDOld = GCPtrOldMapping >> X86_PD_SHIFT;
+    unsigned iPDNew = GCPtrNewMapping >> X86_PD_SHIFT;
 
     Log(("PGM: Relocating %s from %VGv to %VGv\n", pMapping->pszDesc, GCPtrOldMapping, GCPtrNewMapping));
     Assert(((unsigned)iPDOld << X86_PD_SHIFT) == pMapping->GCPtr);
