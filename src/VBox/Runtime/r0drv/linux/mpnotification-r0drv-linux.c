d9f388887b1e3332883375d96d28abab2d2b1f2a
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2008-06-02 15:27:03 +0000

    props
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@9311 cfe28804-0f27-0410-a406-dd0f0b0b656f

---
+++ src/VBox/Runtime/r0drv/linux/mpnotification-r0drv-linux.c
@@ -0,0 +1,159 @@
+/* $Id$ */
+/** @file
+ * IPRT - Multiprocessor Event Notifications, Ring-0 Driver, Linux.
+ */
+
+/*
+ * Copyright (C) 2008 Sun Microsystems, Inc.
+ *
+ * This file is part of VirtualBox Open Source Edition (OSE), as
+ * available from http://www.virtualbox.org. This file is free software;
+ * you can redistribute it and/or modify it under the terms of the GNU
+ * General Public License (GPL) as published by the Free Software
+ * Foundation, in version 2 as it comes in the "COPYING" file of the
+ * VirtualBox OSE distribution. VirtualBox OSE is distributed in the
+ * hope that it will be useful, but WITHOUT ANY WARRANTY of any kind.
+ *
+ * The contents of this file may alternatively be used under the terms
+ * of the Common Development and Distribution License Version 1.0
+ * (CDDL) only, as it comes in the "COPYING.CDDL" file of the
+ * VirtualBox OSE distribution, in which case the provisions of the
+ * CDDL are applicable instead of those of the GPL.
+ *
+ * You may elect to license modified versions of this file under the
+ * terms and conditions of either the GPL or the CDDL or both.
+ *
+ * Please contact Sun Microsystems, Inc., 4150 Network Circle, Santa
+ * Clara, CA 95054 USA or visit http://www.sun.com if you need
+ * additional information or have any questions.
+ *
+ */
+
+/*******************************************************************************
+*   Header Files                                                               *
+*******************************************************************************/
+#include "the-linux-kernel.h"
+
+#include <iprt/mp.h>
+#include <iprt/err.h>
+#include <iprt/cpuset.h>
+#include "r0drv/mp-r0drv.h"
+
+
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(2, 5, 71) && defined(CONFIG_SMP)
+
+/*******************************************************************************
+*   Internal Functions                                                         *
+*******************************************************************************/
+static int rtMpNotificationLinuxCallback(struct notifier_block *pNotifierBlock, unsigned long ulNativeEvent, void *pvCpu);
+
+
+/*******************************************************************************
+*   Global Variables                                                           *
+*******************************************************************************/
+/**
+ * The notifier block we use for registering the callback.
+ */
+static struct notifier_block g_NotifierBlock =
+{
+    .notifier_call = rtMpNotificationLinuxCallback,
+    .next = NULL,
+    .priority = 0
+};
+
+#ifdef CPU_DOWN_FAILED
+/**
+ * The set of CPUs we've seen going offline recently.
+ */
+static RTCPUSET g_MpPendingOfflineSet;
+#endif
+
+
+/**
+ * The native callback.
+ *
+ * @returns 0.
+ * @param   pNotifierBlock  Pointer to g_NotifierBlock.
+ * @param   ulNativeEvent   The native event.
+ * @param   pvCpu           The cpu id cast into a pointer value.
+ */
+static int rtMpNotificationLinuxCallback(struct notifier_block *pNotifierBlock, unsigned long ulNativeEvent, void *pvCpu)
+{
+    RTCPUID idCpu = (uintptr_t)pvCpu;
+    NOREF(pNotifierBlock);
+
+    /* ASSUMES iCpu == RTCPUID */
+    switch (ulNativeEvent)
+    {
+#ifdef CPU_DOWN_FAILED
+        case CPU_DOWN_FAILED:
+# ifdef CPU_TASKS_FROZEN
+        case CPU_DOWN_FAILED_FROZEN:
+# endif
+            if (!RTCpuSetIsMember(&g_MpPendingOfflineSet, idCpu))
+                return 0;
+        /* fall thru */
+#endif
+        case CPU_ONLINE:
+# ifdef CPU_TASKS_FROZEN
+        case CPU_ONLINE_FROZEN:
+# endif
+            RTCpuSetDel(&g_MpPendingOfflineSet, idCpu);
+            rtMpNotificationDoCallbacks(RTMPEVENT_ONLINE, idCpu);
+            break;
+
+#ifdef CPU_DOWN_PREPARE
+        case CPU_DOWN_PREPARE:
+# ifdef CPU_TASKS_FROZEN
+        case CPU_DOWN_PREPARE_FROZEN:
+# endif
+#else
+        case CPU_DEAD:
+# ifdef CPU_TASKS_FROZEN
+        case CPU_DEAD_FROZEN:
+# endif
+#endif
+            rtMpNotificationDoCallbacks(RTMPEVENT_OFFLINE, idCpu);
+            RTCpuSetAdd(&g_MpPendingOfflineSet, idCpu);
+            break;
+    }
+
+    return 0;
+}
+
+
+int rtR0MpNotificationNativeInit(void *pvOS)
+{
+    int rc;
+    NOREF(pvOS);
+
+#ifdef CPU_DOWN_FAILED
+    RTCpuSetEmpty(&g_MpPendingOfflineSet);
+#endif
+
+    rc = register_cpu_notifier(&g_NotifierBlock);
+    AssertMsgReturn(!rc, ("%d\n", rc), RTErrConvertFromErrno(rc));
+    return VINF_SUCCESS;
+}
+
+
+void rtR0MpNotificationNativeTerm(void *pvOS)
+{
+    unregister_cpu_notifier(&g_NotifierBlock);
+}
+
+#else   /* Not supported / Not needed */
+
+int rtR0MpNotificationNativeInit(void *pvOS)
+{
+    NOREF(pvOS);
+    return VINF_SUCCESS;
+}
+
+void rtR0MpNotificationNativeTerm(void *pvOS)
+{
+    NOREF(pvOS);
+}
+
+#endif  /* Not supported / Not needed */
+
---
+++ src/VBox/Runtime/r0drv/solaris/mpnotification-r0drv-solaris.c
@@ -0,0 +1,79 @@
+/* $Id$ */
+/** @file
+ * IPRT - Multiprocessor Event Notifications, Ring-0 Driver, Solaris.
+ */
+
+/*
+ * Copyright (C) 2008 Sun Microsystems, Inc.
+ *
+ * This file is part of VirtualBox Open Source Edition (OSE), as
+ * available from http://www.virtualbox.org. This file is free software;
+ * you can redistribute it and/or modify it under the terms of the GNU
+ * General Public License (GPL) as published by the Free Software
+ * Foundation, in version 2 as it comes in the "COPYING" file of the
+ * VirtualBox OSE distribution. VirtualBox OSE is distributed in the
+ * hope that it will be useful, but WITHOUT ANY WARRANTY of any kind.
+ *
+ * The contents of this file may alternatively be used under the terms
+ * of the Common Development and Distribution License Version 1.0
+ * (CDDL) only, as it comes in the "COPYING.CDDL" file of the
+ * VirtualBox OSE distribution, in which case the provisions of the
+ * CDDL are applicable instead of those of the GPL.
+ *
+ * You may elect to license modified versions of this file under the
+ * terms and conditions of either the GPL or the CDDL or both.
+ *
+ * Please contact Sun Microsystems, Inc., 4150 Network Circle, Santa
+ * Clara, CA 95054 USA or visit http://www.sun.com if you need
+ * additional information or have any questions.
+ *
+ */
+
+/*******************************************************************************
+*   Header Files                                                               *
+*******************************************************************************/
+#include <sys/cpuvar.h>
+#include <iprt/mp.h>
+#include "r0drv/mp.h"
+
+
+static int rtmpNotificationNativeCallback(cpu_setup_t enmSolarisEvent, int iCpu, void *pvUser)
+{
+    NOREF(pvUser);
+
+    /* ASSUMES iCpu == RTCPUID */
+    switch (enmSolarisEvent)
+    {
+        case CPU_INIT:
+        case CPU_CONFIG:
+        case CPU_UNCONFIG:
+            break;
+
+        case CPU_ON:
+            rtMpNotificationDoCallbacks(RTMPEVENT_ONLINE, iCpu);
+            break;
+
+        case CPU_OFF:
+            rtMpNotificationDoCallbacks(RTMPEVENT_OFFLINE, iCpu);
+            break;
+
+        case CPU_CPUPART_IN:
+        case CPU_CPUPART_OUT:
+            /** @todo are these relevant? */
+            break;
+    }
+    return 0;
+}
+
+
+int rtR0MpNotificationNativeInit(void *pvOS)
+{
+    register_cpu_setup_func(rtmpNotificationNativeCallback, NULL);
+}
+
+
+void rtR0MpNotificationNativeTerm(void *pvOS)
+{
+    unregister_cpu_setup_func(rtmpNotificationNativeCallback, NULL);
+}
+
