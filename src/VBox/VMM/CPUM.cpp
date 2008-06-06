c573736cb3894a3e69f8b77fd0e9ec305b036e91
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2008-06-06 11:34:04 +0000

    Some more fixed 64 bits base values
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@9463 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/VMM/CPUM.cpp
+++ src/VBox/VMM/CPUM.cpp
@@ -824,8 +824,8 @@ static void cpumR3InfoOne(PCPUMCTX pCtx, PCCPUMCTXCORE pCtxCore, PCDBGFINFOHLP p
                 "%sgs={%04x base=%016RX64 limit=%08x flags=%08x} %scr0=%08RX64 %scr2=%08RX64\n"
                 "%sss={%04x base=%016RX64 limit=%08x flags=%08x} %scr3=%08RX64 %scr4=%08RX64\n"
                 "%sgdtr=%VGv:%04x  %sidtr=%VGv:%04x  %seflags=%08x\n"
-                "%sldtr={%04x base=%VGv limit=%08x flags=%08x}\n"
-                "%str  ={%04x base=%VGv limit=%08x flags=%08x}\n"
+                "%sldtr={%04x base=%08RX64 limit=%08x flags=%08x}\n"
+                "%str  ={%04x base=%08RX64 limit=%08x flags=%08x}\n"
                 "%sSysEnter={cs=%04llx eip=%08llx esp=%08llx}\n"
                 ,
                 pszPrefix, pCtxCore->eax, pszPrefix, pCtxCore->ebx, pszPrefix, pCtxCore->ecx, pszPrefix, pCtxCore->edx, pszPrefix, pCtxCore->esi, pszPrefix, pCtxCore->edi,
