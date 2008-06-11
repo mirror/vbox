f571e8180ffa895252f837d44eae01b43e4c2753
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2008-06-11 10:03:56 +0000

    PGMPOOLKIND_64BIT_PD_FOR_64BIT_PD is the same as PGMPOOLKIND_PAE_PD_FOR_PAE_PD.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@9596 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/VMM/VMMAll/PGMAll.cpp
+++ src/VBox/VMM/VMMAll/PGMAll.cpp
@@ -776,7 +776,7 @@ PGMDECL(int) PGMShwGetAllocLongModePDPtr(PVM pVM, RTGCUINTPTR64 GCPtr, PX86PDPAE
         AssertRCReturn(rc, rc);
 
         Assert(!(pPdpe->u & X86_PDPE_PG_MASK));
-        rc = pgmPoolAlloc(pVM, pPdptGst->a[iPdPt].u & X86_PDPE_PG_MASK, PGMPOOLKIND_64BIT_PD_FOR_64BIT_PD, PGMPOOL_IDX_PDPT, iPdPt, &pShwPage);
+        rc = pgmPoolAlloc(pVM, pPdptGst->a[iPdPt].u & X86_PDPE_PG_MASK, PGMPOOLKIND_PAE_PD_FOR_PAE_PD, PGMPOOL_IDX_PDPT, iPdPt, &pShwPage);
         if (rc == VERR_PGM_POOL_FLUSHED)
             return VINF_PGM_SYNC_CR3;
 
