0a1f7c2aa82f97dfecd887cc8fc8eb88aae66877
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2008-06-16 13:13:18 +0000

    Comment
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@9721 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/VMM/VMMAll/EMAll.cpp
+++ src/VBox/VMM/VMMAll/EMAll.cpp
@@ -2228,6 +2228,7 @@ EMDECL(int) EMInterpretRdmsr(PVM pVM, PCPUMCTXCORE pRegFrame)
  */
 static int emInterpretRdmsr(PVM pVM, PDISCPUSTATE pCpu, PCPUMCTXCORE pRegFrame, RTGCPTR pvFault, uint32_t *pcbSize)
 {
+    /* Note: the intel manual claims there's a REX version of RDMSR that's slightly different, so we play safe by completely disassembling the instruction. */
     Assert(!(pCpu->prefix & PREFIX_REX));
     return EMInterpretRdmsr(pVM, pRegFrame);
 }
