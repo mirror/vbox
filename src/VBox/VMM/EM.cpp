f74f9f85b6c760e5c196cdcfdfc743a6de3f95b6
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2008-06-16 11:52:39 +0000

    Logging change
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@9712 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/VMM/EM.cpp
+++ src/VBox/VMM/EM.cpp
@@ -2677,6 +2677,9 @@ static int emR3HwAccExecute(PVM pVM, bool *pfFFDone)
         if (pCtx->eflags.Bits.u1VM)
             Log(("HWV86: %08X IF=%d\n", pCtx->eip, pCtx->eflags.Bits.u1IF));
         else
+        if (CPUMIsGuestIn64BitCode(pVM, CPUMCTX2CORE(pCtx)))
+            Log(("HWR%d: %VGv ESP=%VGv IF=%d CR0=%x CR4=%x EFER=%x\n", cpl, pCtx->rip, pCtx->rsp, pCtx->eflags.Bits.u1IF, (uint32_t)pCtx->cr0, (uint32_t)pCtx->cr4, (uint32_t)pCtx->msrEFER));
+        else
             Log(("HWR%d: %08X ESP=%08X IF=%d CR0=%x CR4=%x EFER=%x\n", cpl, pCtx->eip, pCtx->esp, pCtx->eflags.Bits.u1IF, (uint32_t)pCtx->cr0, (uint32_t)pCtx->cr4, (uint32_t)pCtx->msrEFER));
 #endif
 
