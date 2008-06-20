1ecff181451ff6e643209b3ee6c4398bc46f699c
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2008-06-20 13:15:59 +0000

    Sigh.
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@9854 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/VMM/VMMR0/HWACCMR0A.asm
+++ src/VBox/VMM/VMMR0/HWACCMR0A.asm
@@ -70,8 +70,8 @@
     rdmsr
     push    rdx
     push    rax
-    xor     rdx, rdx
-    mov     rax, qword [xSI + %2]
+    mov     edx, dword [xSI + %2 + 4]
+    mov     eax, dword [xSI + %2]
     wrmsr
   %endmacro
 
@@ -80,7 +80,8 @@
   %macro LOADHOSTMSREX 2
     mov     rcx, %1
     rdmsr 
-    mov     qword [xSI + %2], rax
+    mov     dword [xSI + %2], eax
+    mov     dword [xSI + %2 + 4], edx
     pop     rax
     pop     rdx
     wrmsr
