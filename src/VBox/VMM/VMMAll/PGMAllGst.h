5bee42c1682b86987de64f31b1d5fe3f676b6afe
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2008-06-26 13:42:54 +0000

    Forgot to query the physical address of the CR3 cached page.
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@9952 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/VMM/VMMAll/PGMAllGst.h
+++ src/VBox/VMM/VMMAll/PGMAllGst.h
@@ -480,7 +480,8 @@ PGM_GST_DECL(int, MapCR3)(PVM pVM, RTGCPHYS GCPhysCR3)
                 AssertFailed(); /* check if we handle this properly!! */
                 return VINF_PGM_SYNC_CR3;
             }
-            pVM->pgm.s.pHCPaePML4 = (PX86PML4)PGMPOOL_PAGE_2_PTR(pPool->CTXSUFF(pVM), pVM->pgm.s.pShwAmd64CR3);
+            pVM->pgm.s.pHCPaePML4    = (PX86PML4)PGMPOOL_PAGE_2_PTR(pPool->CTXSUFF(pVM), pVM->pgm.s.pShwAmd64CR3);
+            pVM->pgm.s.HCPhysPaePML4 = pVM->pgm.s.pShwAmd64CR3->Core.Key;
 # endif
         }
         else
