e2115d2fb02928546033b7b3fdaedf35cc0fb0c4
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2008-07-02 12:28:55 +0000

    Derive CPL from cs, not ss.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@10097 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/VMM/VMMAll/CPUMAllRegs.cpp
+++ src/VBox/VMM/VMMAll/CPUMAllRegs.cpp
@@ -1818,12 +1818,12 @@ CPUMDECL(uint32_t) CPUMGetGuestCPL(PVM pVM, PCPUMCTXCORE pCtxCore)
     uint32_t cpl;
 
     if (CPUMAreHiddenSelRegsValid(pVM))
-        cpl = pCtxCore->ssHid.Attr.n.u2Dpl;
+        cpl = pCtxCore->csHid.Attr.n.u2Dpl;
     else if (RT_LIKELY(pVM->cpum.s.Guest.cr0 & X86_CR0_PE))
     {
         if (RT_LIKELY(!pCtxCore->eflags.Bits.u1VM))
         {
-            cpl = (pCtxCore->ss & X86_SEL_RPL);
+            cpl = (pCtxCore->cs & X86_SEL_RPL);
 #ifndef IN_RING0
             if (cpl == 1)
                 cpl = 0;
