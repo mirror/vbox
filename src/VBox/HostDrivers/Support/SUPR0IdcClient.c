77e2050621fdcd787de26df8027863de2d7412e3
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2008-07-04 23:31:26 +0000

    Inter-Driver Communication (IDC) interface for the support driver. The client end (SUPR0IdcClient).
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@10258 cfe28804-0f27-0410-a406-dd0f0b0b656f

---
+++ src/VBox/HostDrivers/Support/SUPR0IdcClient.c
@@ -0,0 +1,197 @@
+/* $Id$ */
+/** @file
+ * VirtualBox Support Driver - IDC Client Lib, Core.
+ */
+
+/*
+ * Copyright (C) 2008 Sun Microsystems, Inc.
+ *
+ * This file is part of VirtualBox Open Source Edition (OSE), as
+ * available from http://www.virtualbox.org. This file is free software;
+ * you can redistribute it and/or modify it under the terms of the GNU
+ * General Public License (GPL) as published by the Free Software
+ * Foundation, in version 2 as it comes in the "COPYING" file of the
+ * VirtualBox OSE distribution. VirtualBox OSE is distributed in the
+ * hope that it will be useful, but WITHOUT ANY WARRANTY of any kind.
+ *
+ * The contents of this file may alternatively be used under the terms
+ * of the Common Development and Distribution License Version 1.0
+ * (CDDL) only, as it comes in the "COPYING.CDDL" file of the
+ * VirtualBox OSE distribution, in which case the provisions of the
+ * CDDL are applicable instead of those of the GPL.
+ *
+ * You may elect to license modified versions of this file under the
+ * terms and conditions of either the GPL or the CDDL or both.
+ *
+ * Please contact Sun Microsystems, Inc., 4150 Network Circle, Santa
+ * Clara, CA 95054 USA or visit http://www.sun.com if you need
+ * additional information or have any questions.
+ */
+
+/*******************************************************************************
+*   Header Files                                                               *
+*******************************************************************************/
+#include "SUPR0IdcClientInternal.h"
+#include <VBox/err.h>
+
+
+/*******************************************************************************
+*   Global Variables                                                           *
+*******************************************************************************/
+static PSUPDRVIDCHANDLE volatile g_pMainHandle = NULL;
+
+
+/**
+ * Opens the IDC interface of the support driver.
+ *
+ * This will perform basic version negotiations and fail if the
+ * minmum requirements aren't met.
+ *
+ * @returns VBox status code.
+ * @param   pHandle         The handle structure (output).
+ * @param   uReqVersion     The requested version. Pass 0 for default.
+ * @param   uMinVersion     The minimum required version. Pass 0 for default.
+ * @param   puVersion       Where to store the version of the IDC interface. Optional.
+ */
+SUPR0DECL(int) SUPR0IdcOpen(PSUPDRVIDCHANDLE pHandle, uint32_t uReqVersion, uint32_t uMinVersion, uint32_t *puVersion)
+{
+    unsigned uDefaultMinVersion;
+    SUPDRVIDCREQCONNECT Req;
+    int rc;
+
+    /*
+     * Validate and set failure return values.
+     */
+    AssertPtrReturn(pHandle, VERR_INVALID_POINTER);
+    pHandle->s.pSession = NULL;
+    AssertPtrNullReturn(puVersion, VERR_INVALID_POINTER);
+    if (puVersion)
+        *puVersion = 0;
+    AssertReturn(!uMinVersion || (uMinVersion & UINT32_C(0xffff0000)) == (SUPDRV_IDC_VERSION & UINT32_C(0xffff0000)), VERR_INVALID_PARAMETER);
+    AssertReturn(!uReqVersion || (uReqVersion & UINT32_C(0xffff0000)) == (SUPDRV_IDC_VERSION & UINT32_C(0xffff0000)), VERR_INVALID_PARAMETER);
+
+    /*
+     * Handle default version input and enforce minimum requirements made
+     * by this library.
+     *
+     * The clients will pass defaults (0), and only in the case that some
+     * special API feature was just added will they set an actual version.
+     * So, this is the place where can easily enforce a minimum IDC version
+     * on bugs and similar. It corresponds a bit to what SUPInit is
+     * responsible for.
+     */
+    uDefaultMinVersion = SUPDRV_IDC_VERSION & UINT32_C(0xffff0000);
+    if (!uMinVersion || uMinVersion < uDefaultMinVersion)
+        uMinVersion = uDefaultMinVersion;
+    if (!uReqVersion || uReqVersion < uDefaultMinVersion)
+        uReqVersion = uDefaultMinVersion;
+
+    /*
+     * Setup the connect request packet and call the OS specific function.
+     */
+    Req.Hdr.cb = sizeof(Req);
+    Req.Hdr.rc = VERR_WRONG_ORDER;
+    Req.Hdr.pSession = NULL;
+    Req.u.In.u32MagicCookie = SUPDRVIDCREQ_CONNECT_MAGIC_COOKIE;
+    Req.u.In.uMinVersion = uMinVersion;
+    Req.u.In.uReqVersion = uReqVersion;
+    rc = supR0IdcNativeOpen(pHandle, &Req);
+    if (RT_SUCCESS(rc))
+    {
+        pHandle->s.pSession = Req.u.Out.pSession;
+        if (puVersion)
+            *puVersion = Req.u.Out.uVersion;
+
+        /*
+         * We don't really trust anyone, make sure the returned
+         * session and version values actually makes sense.
+         */
+        if (    VALID_PTR(Req.u.Out.pSession)
+            &&  Req.u.Out.uVersion >= uMinVersion
+            &&  (Req.u.Out.uVersion & UINT32_C(0xffff0000)) == (SUPDRV_IDC_VERSION & UINT32_C(0xffff0000)))
+        {
+            ASMAtomicCmpXchgPtr((void * volatile *)&g_pMainHandle, pHandle, NULL);
+            return rc;
+        }
+
+        AssertMsgFailed(("pSession=%p uVersion=0x%x\n", Req.u.Out.pSession, Req.u.Out.uVersion));
+        rc = VERR_VERSION_MISMATCH;
+        SUPR0IdcClose(pHandle);
+    }
+
+    return rc;
+}
+
+
+/**
+ * Closes a IDC connection established by SUPR0IdcOpen.
+ *
+ * @returns VBox status code.
+ * @param   pHandle     The IDC handle.
+ */
+SUPR0DECL(int) SUPR0IdcClose(PSUPDRVIDCHANDLE pHandle)
+{
+    SUPDRVIDCREQHDR Req;
+    int rc;
+
+    /*
+     * Catch closed handles and check that the session is valid.
+     */
+    AssertPtrReturn(pHandle, VERR_INVALID_POINTER);
+    if (!pHandle->s.pSession)
+        return VERR_INVALID_HANDLE;
+    AssertPtrReturn(pHandle->s.pSession, VERR_INVALID_HANDLE);
+
+    /*
+     * Create the request and hand it to the OS specific code.
+     */
+    Req.cb = sizeof(Req);
+    Req.rc = VERR_WRONG_ORDER;
+    Req.pSession = pHandle->s.pSession;
+    rc = supR0IdcNativeClose(pHandle, &Req);
+    if (RT_SUCCESS(rc))
+    {
+        pHandle->s.pSession = NULL;
+        ASMAtomicCmpXchgPtr((void * volatile *)&g_pMainHandle, NULL, pHandle);
+    }
+    return rc;
+}
+
+
+/**
+ * Get the SUPDRV session for the IDC connection.
+ *
+ * This is for use with SUPDRV and component APIs that requires a valid
+ * session handle.
+ *
+ * @returns The session handle on success, NULL if the IDC handle is invalid.
+ *
+ * @param   pHandle         The IDC handle.
+ */
+SUPR0DECL(PSUPDRVSESSION) SUPR0IdcGetSession(PSUPDRVIDCHANDLE pHandle)
+{
+    PSUPDRVSESSION pSession;
+    AssertPtrReturn(pHandle, NULL);
+    pSession = pHandle->s.pSession;
+    AssertPtrReturn(pSession, NULL);
+    return pSession;
+}
+
+
+/**
+ * Looks up a IDC handle by session.
+ *
+ * @returns The IDC handle on success, NULL on failure.
+ * @param   pSession    The session to lookup.
+ *
+ * @internal
+ */
+PSUPDRVIDCHANDLE supR0IdcGetHandleFromSession(PSUPDRVSESSION pSession)
+{
+    PSUPDRVIDCHANDLE pHandle = (PSUPDRVIDCHANDLE)ASMAtomicUoReadPtr((void * volatile *)&g_pMainHandle);
+    if (    VALID_PTR(pHandle)
+        &&  pHandle->s.pSession == pSession)
+        return pHandle;
+    return NULL;
+}
+
