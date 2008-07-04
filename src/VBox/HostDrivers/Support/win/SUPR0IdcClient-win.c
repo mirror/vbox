9af602127458d7c844500c681096b6ee9b342de5
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2008-07-04 23:31:26 +0000

    Inter-Driver Communication (IDC) interface for the support driver. The client end (SUPR0IdcClient).
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@10258 cfe28804-0f27-0410-a406-dd0f0b0b656f

---
+++ src/VBox/HostDrivers/Support/win/SUPR0IdcClient-win.c
@@ -0,0 +1,148 @@
+/* $Id$ */
+/** @file
+ * VirtualBox Support Driver - IDC Client Lib, Windows Specific Code.
+ */
+
+/*
+ * Copyright (C) 2008 Sun Microsystems, Inc.
+ *
+ * This file is part of VirtualBox Open Source Edition (OSE), as
+ * available from http://www.virtualbox.org. This file is free software;
+ * you can redistribute it and/or modify it under the terms of the GNU
+ * General Public License (GPL) as published by the Free Software
+ * Foundation, in version 2 as it comes in the "COPYING" file of the
+ * VirtualBox OSE distribution. VirtualBox OSE is distributed in the
+ * hope that it will be useful, but WITHOUT ANY WARRANTY of any kind.
+ *
+ * The contents of this file may alternatively be used under the terms
+ * of the Common Development and Distribution License Version 1.0
+ * (CDDL) only, as it comes in the "COPYING.CDDL" file of the
+ * VirtualBox OSE distribution, in which case the provisions of the
+ * CDDL are applicable instead of those of the GPL.
+ *
+ * You may elect to license modified versions of this file under the
+ * terms and conditions of either the GPL or the CDDL or both.
+ *
+ * Please contact Sun Microsystems, Inc., 4150 Network Circle, Santa
+ * Clara, CA 95054 USA or visit http://www.sun.com if you need
+ * additional information or have any questions.
+ */
+
+/*******************************************************************************
+*   Header Files                                                               *
+*******************************************************************************/
+#include "../SUPR0IdcClientInternal.h"
+#include <VBox/err.h>
+
+
+/*******************************************************************************
+*   Defined Constants And Macros                                               *
+*******************************************************************************/
+/** NT Device name. */
+#define DEVICE_NAME_NT   L"\\Device\\VBoxDrv"
+
+
+static int supR0IdcNtCallInternal(PDEVICE_OBJECT pDeviceObject, uint32_t iReq, PSUPDRVIDCREQHDR pReq)
+{
+    int                 rc;
+    IO_STATUS_BLOCK     IoStatusBlock;
+    KEVENT              Event;
+    PIRP                pIrp;
+
+    /*
+     * Build the request.
+     */
+    KeInitializeEvent(&Event, NotificationEvent, FALSE);
+    pIrp = IoBuildDeviceIoControlRequest(iReq,              /* IoControlCode */
+                                         pDeviceObject,
+                                         pReq,              /* InputBuffer */
+                                         pReq->cb,          /* InputBufferLength */
+                                         pReq,              /* OutputBuffer */
+                                         pReq->cb,          /* OutputBufferLength */
+                                         TRUE,              /* InternalDeviceIoControl (=> IRP_MJ_INTERNAL_DEVICE_CONTROL) */
+                                         &Event,            /* Event */
+                                         &IoStatusBlock);   /* IoStatusBlock */
+    if (pIrp)
+    {
+        /*
+         * Call the driver, wait for an async request to complete (should never happen).
+         */
+        NTSTATUS rcNt = IoCallDriver(pDeviceObject, pIrp);
+        if (rcNt == STATUS_PENDING)
+        {
+            rcNt = KeWaitForSingleObject(&Event,            /* Object */
+                                         Executive,         /* WaitReason */
+                                         KernelMode,        /* WaitMode */
+                                         FALSE,             /* Altertable */
+                                         NULL);             /* TimeOut */
+            rcNt = IoStatusBlock.Status;
+        }
+        if (NT_SUCCESS(rcNt))
+            rc = pReq->rc;
+        else
+            rc = RTErrConvertFromNtStatus(rcNt);
+    }
+    else
+        rc = VERR_NO_MEMORY;
+    return rc;
+}
+
+int VBOXCALL supR0IdcNativeOpen(PSUPDRVIDCHANDLE pHandle, PSUPDRVIDCREQCONNECT pReq)
+{
+    PDEVICE_OBJECT  pDeviceObject = NULL;
+    PFILE_OBJECT    pFileObject = NULL;
+    UNICODE_STRING  wszDeviceName;
+    NTSTATUS        rcNt;
+    int             rc;
+
+    /*
+     * Get the device object pointer.
+     */
+    RtlInitUnicodeString(&wszDeviceName, DEVICE_NAME_NT);
+    rcNt = IoGetDeviceObjectPointer(&wszDeviceName, FILE_ALL_ACCESS, &pFileObject, &pDeviceObject);
+    if (NT_SUCCESS(rcNt))
+    {
+        /*
+         * Make the connection call.
+         */
+        rc = supR0IdcNtCallInternal(pDeviceObject, SUPDRV_IDC_REQ_CONNECT, &pReq->Hdr);
+        if (RT_SUCCESS(rc))
+        {
+            pHandle->s.pDeviceObject = pDeviceObject;
+            pHandle->s.pFileObject = pFileObject;
+            return rc;
+        }
+
+        /* only the file object. */
+        ObDereferenceObject(pFileObject);
+    }
+    else
+        rc = RTErrConvertFromNtStatus(rcNt);
+
+    pHandle->s.pDeviceObject = NULL;
+    pHandle->s.pFileObject = NULL;
+    return rc;
+}
+
+
+int VBOXCALL supR0IdcNativeClose(PSUPDRVIDCHANDLE pHandle, PSUPDRVIDCREQHDR pReq)
+{
+    PDEVICE_OBJECT  pDeviceObject = pHandle->s.pDeviceObject;
+    PFILE_OBJECT    pFileObject   = pHandle->s.pFileObject;
+    int rc = supR0IdcNtCallInternal(pDeviceObject, SUPDRV_IDC_REQ_DISCONNECT, pReq);
+    if (RT_SUCCESS(rc))
+    {
+        pHandle->s.pDeviceObject = NULL;
+        pHandle->s.pFileObject  = NULL;
+        ObDereferenceObject(pFileObject);
+    }
+
+    return rc;
+}
+
+
+int VBOXCALL supR0IdcNativeCall(PSUPDRVIDCHANDLE pHandle, uint32_t iReq, PSUPDRVIDCREQHDR pReq)
+{
+    return supR0IdcNtCallInternal(pHandle->s.pDeviceObject, iReq, pReq);
+}
+
