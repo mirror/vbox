a2bc961ab5ff47fb932ff40b2683957eaa79cbee
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2008-07-05 17:50:22 +0000

    Expose X86_CPUID_AMD_FEATURE_EDX_MCA & X86_CPUID_AMD_FEATURE_EDX_MTRR -> watch for regressionsCPUM.cpp
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@10289 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/VMM/CPUM.cpp
+++ src/VBox/VMM/CPUM.cpp
@@ -256,9 +256,9 @@ static int cpumR3CpuIdInit(PVM pVM)
                                        //| X86_CPUID_FEATURE_EDX_APIC  - set by the APIC device if present.
                                        /** @note we don't report sysenter/sysexit support due to our inability to keep the IOPL part of eflags in sync while in ring 1 (see #1757) */
                                        //| X86_CPUID_FEATURE_EDX_SEP
-                                       //| X86_CPUID_FEATURE_EDX_MTRR  - no MTRRs.
+                                       | X86_CPUID_FEATURE_EDX_MTRR
                                        | X86_CPUID_FEATURE_EDX_PGE
-                                       //| X86_CPUID_FEATURE_EDX_MCA   - not virtualized.
+                                       | X86_CPUID_FEATURE_EDX_MCA
                                        | X86_CPUID_FEATURE_EDX_CMOV
                                        | X86_CPUID_FEATURE_EDX_PAT
                                        //| X86_CPUID_FEATURE_EDX_PSE36 - not virtualized.
@@ -303,9 +303,9 @@ static int cpumR3CpuIdInit(PVM pVM)
                                        //| X86_CPUID_AMD_FEATURE_EDX_APIC   - set by the APIC device if present.
                                        /** @note we don't report sysenter/sysexit support due to our inability to keep the IOPL part of eflags in sync while in ring 1 (see #1757) */
                                        //| X86_CPUID_AMD_FEATURE_EDX_SEP
-                                       //| X86_CPUID_AMD_FEATURE_EDX_MTRR   - not virtualized.
+                                       | X86_CPUID_AMD_FEATURE_EDX_MTRR
                                        | X86_CPUID_AMD_FEATURE_EDX_PGE
-                                       //| X86_CPUID_AMD_FEATURE_EDX_MCA    - not virtualized.
+                                       | X86_CPUID_AMD_FEATURE_EDX_MCA
                                        | X86_CPUID_AMD_FEATURE_EDX_CMOV
                                        | X86_CPUID_AMD_FEATURE_EDX_PAT
                                        //| X86_CPUID_AMD_FEATURE_EDX_PSE36  - not virtualized.
