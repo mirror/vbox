2c18cd3ab296b58ff9e8e9e58ddd66492cbaed99
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2008-07-07 14:22:29 +0000

    Removed the assertion completely.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@10331 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/VMM/VMMR0/HWSVMR0.cpp
+++ src/VBox/VMM/VMMR0/HWSVMR0.cpp
@@ -582,7 +582,6 @@ HWACCMR0DECL(int) SVMR0LoadGuestState(PVM pVM, CPUMCTX *pCtx)
         val = pCtx->cr0;
         if (!CPUMIsGuestFPUStateActive(pVM))
         {
-            Assert(!pVM->hwaccm.s.svm.fResumeVM);
             /* Always use #NM exceptions to load the FPU/XMM state on demand. */
             val |= X86_CR0_TS | X86_CR0_ET | X86_CR0_NE | X86_CR0_MP;
         }
