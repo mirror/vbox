4c66c7d45bd33c7034ca03c58841e4820c24f5d0
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2008-07-10 21:08:17 +0000

    Todo for #2993.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@10487 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- include/VBox/VBoxGuest.h
+++ include/VBox/VBoxGuest.h
@@ -1099,9 +1099,15 @@ typedef const VBGLBIGREQ *PCVBGLBIGREQ;
 
 
 #if defined(RT_OS_WINDOWS)
+# if 0 /** @todo Andy, enable this locally and make it work (may require changes to WINNT/VBoxGuest and other places that
+        * calls it). Then remove all the #else cases in the #ifdef VBOXGUEST_IOCTL_CODE test below and commit. (see #2993) */
+# define VBOXGUEST_IOCTL_CODE(Function, Size)   IOCTL_CODE(FILE_DEVICE_UNKNOWN, 2048 + (Function), METHOD_BUFFERED, FILE_WRITE_ACCESS, 0)
+# define VBOXGUEST_IOCTL_STRIP_SIZE(Code)       (Code)
+# else
   /* legacy encoding. */
 # define IOCTL_CODE(DeviceType, Function, Method, Access, DataSize_ignored) \
     ( ((DeviceType) << 16) | ((Access) << 14) | ((Function) << 2) | (Method))
+# endif
 
 #elif defined(RT_OS_OS2)
   /* No automatic buffering, size not encoded. */
