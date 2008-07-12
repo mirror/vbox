344ba1578db4497aaf5916022841f2954d5d9dd3
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2008-07-12 16:05:29 +0000

    Forgot to make the addition cpuid leafs available.
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@10571 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/VMM/CPUM.cpp
+++ src/VBox/VMM/CPUM.cpp
@@ -454,8 +454,8 @@ static int cpumR3CpuIdInit(PVM pVM)
      * is perhaps a bit crudely done as there is probably some relatively harmless
      * info too in these leaves (like words about having a constant TSC).
      */
-    if (pCPUM->aGuestCpuIdStd[0].eax > 2)
-        pCPUM->aGuestCpuIdStd[0].eax = 2;
+    if (pCPUM->aGuestCpuIdStd[0].eax > 5)
+        pCPUM->aGuestCpuIdStd[0].eax = 5;
     for (i = pCPUM->aGuestCpuIdStd[0].eax + 1; i < RT_ELEMENTS(pCPUM->aGuestCpuIdStd); i++)
         pCPUM->aGuestCpuIdStd[i] = pCPUM->GuestCpuIdDef;
 
