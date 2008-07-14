2442ce097f844fbe95e30ebe1f88fd9806851e6d
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2008-07-14 19:25:05 +0000

    Performance API enabled for Linux, Solaris and Windows. POSIX timers enabled for Linux and Solaris.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@10613 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Main/ProgressImpl.cpp
+++ src/VBox/Main/ProgressImpl.cpp
@@ -19,8 +19,6 @@
  * additional information or have any questions.
  */
 
-#include <iprt/stdint.h>
-
 #if defined (VBOX_WITH_XPCOM)
 #include <nsIServiceManager.h>
 #include <nsIExceptionService.h>
