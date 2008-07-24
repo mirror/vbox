150368ca5ab31dc814a5094c5e544f7a972d41bb
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2008-07-24 13:31:40 +0000

    Comment update
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@10853 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/VMM/HWACCM.cpp
+++ src/VBox/VMM/HWACCM.cpp
@@ -747,7 +747,7 @@ HWACCMR3DECL(bool) HWACCMR3CanExecuteGuest(PVM pVM, PCPUMCTX pCtx)
 
         /* The guest is about to complete the switch to protected mode. Wait a bit longer. */
         /* Windows XP; switch to protected mode; all selectors are marked not present in the
-         * hidden registers (possible recompiler bug) */
+         * hidden registers (possible recompiler bug; see load_seg_vm) */
         if (pCtx->csHid.Attr.n.u1Present == 0)
             return false;
         if (pCtx->ssHid.Attr.n.u1Present == 0)
