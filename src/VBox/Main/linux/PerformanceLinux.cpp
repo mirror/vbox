1e4122278574b874788798de32e9a74d77b6a497
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2008-07-25 08:10:03 +0000

    Fix for missing getpagesize()
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@10879 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Main/linux/PerformanceLinux.cpp
+++ src/VBox/Main/linux/PerformanceLinux.cpp
@@ -40,6 +40,7 @@ public:
     virtual int getRawProcessCpuLoad(RTPROCESS process, uint64_t *user, uint64_t *kernel, uint64_t *total);
 private:
     int getRawProcessStats(RTPROCESS process, uint64_t *cpuUser, uint64_t *cpuKernel, unsigned long *memPagesUsed);
+    long getPageSize() { return sysconf(_SC_PAGESIZE); };
 };
 
 // Linux Metric factory
@@ -128,8 +129,8 @@ int CollectorLinux::getProcessMemoryUsage(RTPROCESS process, unsigned long *used
     int rc = getRawProcessStats(process, &u64Tmp, &u64Tmp, &nPagesUsed);
     if (RT_SUCCESS(rc))
     {
-        Assert(getpagesize() >= 1024);
-        *used = nPagesUsed * (getpagesize() / 1024);
+        Assert(getPageSize() >= 1024);
+        *used = nPagesUsed * (getPageSize() / 1024);
     }
     return rc;
 }
