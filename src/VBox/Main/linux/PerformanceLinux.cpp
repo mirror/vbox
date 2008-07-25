f2eaad00f21ab33252d199008118b231983bfd31
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2008-07-25 08:51:57 +0000

    Another attempt to fix missing getpagesize() in RHEL3
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@10881 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Main/linux/PerformanceLinux.cpp
+++ src/VBox/Main/linux/PerformanceLinux.cpp
@@ -21,15 +21,12 @@
  * additional information or have any questions.
  */
 
+#include <unistd.h>
 #include <stdio.h>
 #include <iprt/alloc.h>
 #include <iprt/err.h>
 #include <iprt/string.h>
 #include "Performance.h"
-
-#ifndef PAGESIZE
-#define PAGESIZE _SC_PAGESIZE
-#endif
 
 namespace pm {
 
@@ -44,7 +41,7 @@ public:
     virtual int getRawProcessCpuLoad(RTPROCESS process, uint64_t *user, uint64_t *kernel, uint64_t *total);
 private:
     int getRawProcessStats(RTPROCESS process, uint64_t *cpuUser, uint64_t *cpuKernel, unsigned long *memPagesUsed);
-    long getPageSize() { return sysconf(PAGESIZE); };
+    long getPageSize() { return getpagesize(); };
 };
 
 // Linux Metric factory
