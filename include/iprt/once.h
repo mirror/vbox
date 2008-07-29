cdb31da65316e7bc50e46581e8b9acedf746bd0c
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2008-07-29 16:32:25 +0000

    IPRT: Implemented the RTOnce API.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@10940 cfe28804-0f27-0410-a406-dd0f0b0b656f

---
+++ include/iprt/once.h
@@ -0,0 +1,99 @@
+/** @file
+ * IPRT - Execute Once.
+ */
+
+/*
+ * Copyright (C) 2006-2007 Sun Microsystems, Inc.
+ *
+ * This file is part of VirtualBox Open Source Edition (OSE), as
+ * available from http://www.virtualbox.org. This file is free software;
+ * you can redistribute it and/or modify it under the terms of the GNU
+ * General Public License (GPL) as published by the Free Software
+ * Foundation, in version 2 as it comes in the "COPYING" file of the
+ * VirtualBox OSE distribution. VirtualBox OSE is distributed in the
+ * hope that it will be useful, but WITHOUT ANY WARRANTY of any kind.
+ *
+ * The contents of this file may alternatively be used under the terms
+ * of the Common Development and Distribution License Version 1.0
+ * (CDDL) only, as it comes in the "COPYING.CDDL" file of the
+ * VirtualBox OSE distribution, in which case the provisions of the
+ * CDDL are applicable instead of those of the GPL.
+ *
+ * You may elect to license modified versions of this file under the
+ * terms and conditions of either the GPL or the CDDL or both.
+ *
+ * Please contact Sun Microsystems, Inc., 4150 Network Circle, Santa
+ * Clara, CA 95054 USA or visit http://www.sun.com if you need
+ * additional information or have any questions.
+ */
+
+#ifndef ___iprt_once_h
+#define ___iprt_once_h
+
+#include <iprt/cdefs.h>
+#include <iprt/types.h>
+#include <iprt/err.h>
+
+__BEGIN_DECLS
+
+/** @defgroup grp_rt_once       RTOnce - Execute Once
+ * @ingroup grp_rt
+ * @{
+ */
+
+/**
+ * Execute once structure.
+ *
+ * This is typically a global variable that is statically initialized
+ * by RTONCE_INITIALIZER.
+ */
+typedef struct RTONCE
+{
+    /** Event semaphore that the other guys are blocking on. */
+    RTSEMEVENTMULTI volatile hEventMulti;
+    /** Reference counter for hEventMulti. */
+    int32_t volatile cEventRefs;
+    /** -1 when uninitialized, 1 when initializing (busy) and 2 when done. */
+    int32_t volatile iState;
+    /** The return code of pfnOnce. */
+    int32_t volatile rc;
+} RTONCE;
+/** Pointer to a execute once struct. */
+typedef RTONCE *PRTONCE;
+
+/** Static initializer for RTONCE variables. */
+#define RTONCE_INITIALIZER      { NIL_RTSEMEVENTMULTI, 0, -1, VERR_INTERNAL_ERROR }
+
+
+/**
+ * Callback that gets executed once.
+ *
+ * @returns IPRT style status code, RTOnce returns this.
+ *
+ * @param   pvUser1         The first user parameter.
+ * @param   pvUser2         The second user parameter.
+ */
+typedef DECLCALLBACK(int32_t) FNRTONCE(void *pvUser1, void *pvUser2);
+/** Pointer to a FNRTONCE. */
+typedef FNRTONCE *PFNRTONCE;
+
+/**
+ * Serializes execution of the pfnOnce function, making sure it's
+ * executed exactly once and that nobody returns from RTOnce before
+ * it has executed successfully.
+ *
+ * @returns IPRT like status code returned by pfnOnce.
+ *
+ * @param   pOnce           Pointer to the execute once variable.
+ * @param   pfnOnce         The function to executed once.
+ * @param   pvUser1         The first user parameter for pfnOnce.
+ * @param   pvUser2         The second user parameter for pfnOnce.
+ */
+RTDECL(int) RTOnce(PRTONCE pOnce, PFNRTONCE pfnOnce, void *pvUser1, void *pvUser2);
+
+/** @} */
+
+__END_DECLS
+
+#endif
+
---
+++ src/VBox/Runtime/common/misc/once.cpp
@@ -0,0 +1,161 @@
+/* $Id$ */
+/** @file
+ * IPRT - Execute Once.
+ */
+
+/*
+ * Copyright (C) 2007 Sun Microsystems, Inc.
+ *
+ * This file is part of VirtualBox Open Source Edition (OSE), as
+ * available from http://www.virtualbox.org. This file is free software;
+ * you can redistribute it and/or modify it under the terms of the GNU
+ * General Public License (GPL) as published by the Free Software
+ * Foundation, in version 2 as it comes in the "COPYING" file of the
+ * VirtualBox OSE distribution. VirtualBox OSE is distributed in the
+ * hope that it will be useful, but WITHOUT ANY WARRANTY of any kind.
+ *
+ * The contents of this file may alternatively be used under the terms
+ * of the Common Development and Distribution License Version 1.0
+ * (CDDL) only, as it comes in the "COPYING.CDDL" file of the
+ * VirtualBox OSE distribution, in which case the provisions of the
+ * CDDL are applicable instead of those of the GPL.
+ *
+ * You may elect to license modified versions of this file under the
+ * terms and conditions of either the GPL or the CDDL or both.
+ *
+ * Please contact Sun Microsystems, Inc., 4150 Network Circle, Santa
+ * Clara, CA 95054 USA or visit http://www.sun.com if you need
+ * additional information or have any questions.
+ */
+
+/*******************************************************************************
+*   Header Files                                                               *
+*******************************************************************************/
+#include <iprt/once.h>
+#include <iprt/semaphore.h>
+#include <iprt/thread.h>
+#include <iprt/err.h>
+#include <iprt/assert.h>
+#include <iprt/asm.h>
+
+
+
+RTDECL(int) RTOnce(PRTONCE pOnce, PFNRTONCE pfnOnce, void *pvUser1, void *pvUser2)
+{
+    /*
+     * Validate input (strict builds only).
+     */
+    AssertPtr(pOnce);
+    AssertPtr(pfnOnce);
+
+    /*
+     * Deal with the 'initialized' case first
+     */
+    int32_t iState = ASMAtomicUoReadS32(&pOnce->iState);
+    if (RT_LIKELY(iState == 2))
+        return ASMAtomicUoReadS32(&pOnce->rc);
+    AssertReturn(iState == -1 || iState == 1, VERR_INTERNAL_ERROR);
+
+    /*
+     * Do we initialize it?
+     */
+    if (iState == -1)
+    {
+        RTSEMEVENTMULTI hEventMulti;
+        int rc = RTSemEventMultiCreate(&hEventMulti);
+        if (RT_FAILURE(rc))
+            hEventMulti = NIL_RTSEMEVENTMULTI;
+
+        if (ASMAtomicCmpXchgS32(&pOnce->iState, 1, -1))
+        {
+            ASMAtomicWriteHandle(&pOnce->hEventMulti, hEventMulti);
+            ASMAtomicIncS32(&pOnce->cEventRefs);
+
+            /* do the execute once stuff. */
+            int32_t rcOnce = pfnOnce(pvUser1, pvUser2);
+
+            /* set the return code, change the state and signal any waiters. */
+            ASMAtomicWriteS32(&pOnce->rc, rcOnce);
+            ASMAtomicWriteS32(&pOnce->iState, 2);
+            if (hEventMulti != NIL_RTSEMEVENTMULTI)
+                RTSemEventMultiSignal(hEventMulti);
+
+            /* last guy destroys the semaphore. */
+            if (ASMAtomicDecS32(&pOnce->cEventRefs) == 0)
+                ASMAtomicWriteSize(&pOnce->hEventMulti, NIL_RTSEMEVENTMULTI);
+            else
+                hEventMulti = NIL_RTSEMEVENTMULTI;
+        }
+        if (hEventMulti != NIL_RTSEMEVENTMULTI)
+            RTSemEventMultiDestroy(hEventMulti);
+    }
+
+    /*
+     * Wait for it to finish initializing.
+     */
+    if (ASMAtomicReadS32(&pOnce->iState) == 1)
+    {
+        int i = 0;
+        while (ASMAtomicReadS32(&pOnce->iState) == 1)
+        {
+            bool fYieldSleep = true;
+
+            /*
+             * Take care not to increment the counter if it's 0, that indicates
+             * that RTONCE::hEventMulti isn't valid either because it's not set
+             * yet, or because it's being destroyed.
+             */
+            int32_t cEventRefs = ASMAtomicUoReadS32(&pOnce->cEventRefs);
+            while (   cEventRefs > 0
+                   && !ASMAtomicCmpXchgS32(&pOnce->cEventRefs, cEventRefs + 1, cEventRefs))
+                cEventRefs = ASMAtomicUoReadS32(&pOnce->cEventRefs);
+            if (cEventRefs > 1)
+            {
+                /*
+                 * The hEventMulti might be NIL for two reasons, see above in
+                 * the init code, if it isn't valid just do the yield/sleep thing.
+                 */
+                RTSEMEVENTMULTI hEventMulti;
+                ASMAtomicUoReadSize(&pOnce->hEventMulti, &hEventMulti);
+                if (hEventMulti != NIL_RTSEMEVENTMULTI)
+                {
+                    fYieldSleep = false;
+                    RTSemEventMultiWait(hEventMulti, RT_INDEFINITE_WAIT);
+                }
+
+                /*
+                 * Last thread cleans up.
+                 */
+                if (ASMAtomicDecS32(&pOnce->cEventRefs) == 0)
+                {
+                    ASMAtomicXchgHandle(&pOnce->hEventMulti, NIL_RTSEMEVENTMULTI, &hEventMulti);
+                    if (hEventMulti != NIL_RTSEMEVENTMULTI)
+                        RTSemEventMultiDestroy(hEventMulti);
+                }
+            }
+
+            /*
+             * If we didn't block, yield or sleep for a bit.
+             *
+             * The sleep is essential to prevent higher priority threads from spinning wildly
+             * and preventing a lower priority thread from completing the pfnOnce operation
+             * in a timely manner.
+             */
+            if (fYieldSleep)
+            {
+                if (ASMAtomicReadS32(&pOnce->iState) != 1)
+                    break;
+                if (!(++i % 8) )
+                    RTThreadSleep(1);
+                else
+                    RTThreadYield();
+            }
+        }
+    }
+
+    /*
+     * Finally, return the status code from the execute once function.
+     */
+    return ASMAtomicUoReadS32(&pOnce->rc);
+}
+
