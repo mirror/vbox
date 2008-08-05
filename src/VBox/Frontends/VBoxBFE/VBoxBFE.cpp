27a1088a176f17debc0692e9127837751e5a42a4
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2008-08-05 23:14:59 +0000

    PDMMAC -> RTMAC.
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@11158 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Frontends/VBoxBFE/VBoxBFE.cpp
+++ src/VBox/Frontends/VBoxBFE/VBoxBFE.cpp
@@ -204,7 +204,7 @@ typedef struct BFENetworkDevice
     }           enmType;    /**< The type of network driver. */
     bool        fSniff;     /**< Set if the network sniffer should be installed. */
     const char *pszSniff;   /**< Output file for the network sniffer. */
-    PDMMAC      Mac;        /**< The mac address for the device. */
+    RTMAC       Mac;        /**< The mac address for the device. */
     const char *pszName;    /**< The device name of a HIF device. The name of the internal network. */
 #ifdef RT_OS_OS2
     bool        fHaveConnectTo; /**< Whether fConnectTo is set. */
@@ -1730,7 +1730,7 @@ static DECLCALLBACK(int) vboxbfeConfigConstructor(PVM pVM, void *pvUser)
                                              !ulInstance ? 3 : ulInstance - 1 + 8); UPDATE_RC();
             rc = CFGMR3InsertInteger(pInst, "PCIFunctionNo",  0);                   UPDATE_RC();
             rc = CFGMR3InsertNode(pInst,    "Config",         &pCfg);               UPDATE_RC();
-            rc = CFGMR3InsertBytes(pCfg,    "MAC",            &g_aNetDevs[ulInstance].Mac, sizeof(PDMMAC));
+            rc = CFGMR3InsertBytes(pCfg,    "MAC",            &g_aNetDevs[ulInstance].Mac, sizeof(RTMAC));
                                                                                     UPDATE_RC();
 
             /*
