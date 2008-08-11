3cde5369713295a7321a8340af6650c8afb21a80
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2008-08-11 18:08:17 +0000

    Fe/Qt4: Changes for Virtual Disk Manager including: tab-stop fixing, some tool-tip fixing, shortcuts fixing, reworking info pane to be directly included in *.ui file, little code changes according coding style.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@11344 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Frontends/VirtualBox4/include/VBoxDiskImageManagerDlg.h
+++ src/VBox/Frontends/VirtualBox4/include/VBoxDiskImageManagerDlg.h
@@ -32,7 +32,6 @@
 
 class DiskImageItem;
 class VBoxToolBar;
-class InfoPaneLabel;
 class VBoxProgressBar;
 
 class VBoxDiskImageManagerDlg : public QIWithRetranslateUI2<QIMainDialog>,
@@ -40,24 +39,30 @@ class VBoxDiskImageManagerDlg : public QIWithRetranslateUI2<QIMainDialog>,
 {
     Q_OBJECT;
 
-    enum TabIndex { HDTab = 0,
-                    CDTab,
-                    FDTab };
+    enum TabIndex { HDTab = 0, CDTab, FDTab };
 
 public:
 
-    VBoxDiskImageManagerDlg (QWidget *aParent = NULL, Qt::WindowFlags aFlags = Qt::Dialog);
+    VBoxDiskImageManagerDlg (QWidget *aParent = 0,
+                             Qt::WindowFlags aFlags = Qt::Dialog);
 
-    void setup (int aType, bool aDoSelect, const QUuid &aTargetVMId = QUuid(), bool aRefresh = true, CMachine aMachine = NULL, const QUuid &aHdId = QUuid(), const QUuid &aCdId = QUuid(), const QUuid &aFdId = QUuid());
+    void setup (int aType, bool aDoSelect, const QUuid &aTargetVMId = QUuid(),
+                bool aRefresh = true, CMachine aMachine = 0,
+                const QUuid &aHdId = QUuid(),
+                const QUuid &aCdId = QUuid(),
+                const QUuid &aFdId = QUuid());
 
     static void showModeless (bool aRefresh = true);
 
     QUuid selectedUuid() const;
     QString selectedPath() const;
 
-    static QString composeHdToolTip (CHardDisk &aHd, VBoxMedia::Status aStatus, DiskImageItem *aItem = NULL);
-    static QString composeCdToolTip (CDVDImage &aCd, VBoxMedia::Status aStatus, DiskImageItem *aItem = NULL);
-    static QString composeFdToolTip (CFloppyImage &aFd, VBoxMedia::Status aStatus, DiskImageItem *aItem = NULL);
+    static QString composeHdToolTip (CHardDisk &aHd, VBoxMedia::Status aStatus,
+                                     DiskImageItem *aItem = 0);
+    static QString composeCdToolTip (CDVDImage &aCd, VBoxMedia::Status aStatus,
+                                     DiskImageItem *aItem = 0);
+    static QString composeFdToolTip (CFloppyImage &aFd, VBoxMedia::Status aStatus,
+                                     DiskImageItem *aItem = 0);
 
 public slots:
 
@@ -68,8 +73,6 @@ protected:
     void retranslateUi();
     virtual void closeEvent (QCloseEvent *aEvent);
     virtual bool eventFilter (QObject *aObject, QEvent *aEvent);
-    /* @todo: Currently not used (Ported from Qt3): */
-    virtual void machineStateChanged (const VBoxMachineStateChangeEvent &aEvent);
 
 private slots:
 
@@ -86,10 +89,11 @@ private slots:
     void removeImage();
     void releaseImage();
 
-    void releaseDisk (const QUuid &aMachineId, const QUuid &aItemId, VBoxDefs::DiskType aDiskType);
+    void releaseDisk (const QUuid &aMachineId, const QUuid &aItemId,
+                      VBoxDefs::DiskType aDiskType);
 
     void processCurrentChanged (int index = -1);
-    void processCurrentChanged (QTreeWidgetItem *aItem, QTreeWidgetItem *aPrevItem = NULL);
+    void processCurrentChanged (QTreeWidgetItem *aItem, QTreeWidgetItem *aPrevItem = 0);
     void processDoubleClick (QTreeWidgetItem *aItem, int aColumn);
     void showContextMenu (const QPoint &aPos);
 
@@ -99,13 +103,14 @@ private:
     VBoxDefs::DiskType currentTreeWidgetType() const;
     QTreeWidget* currentTreeWidget() const;
 
-    QTreeWidgetItem *selectedItem (const QTreeWidget *aTree) const;
-    DiskImageItem *toDiskImageItem (QTreeWidgetItem *aItem) const;
+    QTreeWidgetItem* selectedItem (const QTreeWidget *aTree) const;
+    DiskImageItem* toDiskImageItem (QTreeWidgetItem *aItem) const;
 
     void setCurrentItem (QTreeWidget *aTree, QTreeWidgetItem *aItem);
 
     void addImageToList (const QString &aSource, VBoxDefs::DiskType aDiskType);
-    DiskImageItem* createImageNode (QTreeWidget *aTree, DiskImageItem *aRoot, const VBoxMedia &aMedia) const;
+    DiskImageItem* createImageNode (QTreeWidget *aTree, DiskImageItem *aRoot,
+                                    const VBoxMedia &aMedia) const;
 
     DiskImageItem* createHdItem (QTreeWidget *aTree, const VBoxMedia &aMedia) const;
     DiskImageItem* createCdItem (QTreeWidget *aTree, const VBoxMedia &aMedia) const;
@@ -125,9 +130,9 @@ private:
 
     void clearInfoPanes();
     void prepareToRefresh (int aTotal = 0);
-    void createInfoString (InfoPaneLabel *&aInfo, QWidget* aRoot, bool aLeftRightMargin, int aRow, int aCol, int aRowSpan = 1, int aColSpan = 1) const;
 
-    void makeWarningMark (DiskImageItem *aItem, VBoxMedia::Status aStatus, VBoxDefs::DiskType aType) const;
+    void makeWarningMark (DiskImageItem *aItem, VBoxMedia::Status aStatus,
+                          VBoxDefs::DiskType aType) const;
 
     static QString DVDImageUsage (const QUuid &aId, QString &aSnapshotUsage);
     static QString FloppyImageUsage (const QUuid &aId, QString &aSnapshotUsage);
@@ -163,19 +168,6 @@ private:
     QAction     *mRemoveAction;
     QAction     *mReleaseAction;
     QAction     *mRefreshAction;
-
-    /* The grid entries in the various information panels */
-    InfoPaneLabel *mHdsPane1;
-    InfoPaneLabel *mHdsPane2;
-    InfoPaneLabel *mHdsPane3;
-    InfoPaneLabel *mHdsPane4;
-    InfoPaneLabel *mHdsPane5;
-    InfoPaneLabel *mHdsPane6;
-    InfoPaneLabel *mHdsPane7;
-    InfoPaneLabel *mCdsPane1;
-    InfoPaneLabel *mCdsPane2;
-    InfoPaneLabel *mFdsPane1;
-    InfoPaneLabel *mFdsPane2;
 
     /* Machine */
     CMachine mMachine;
