ba91cb0925659ef249d64345477b7c508c0335d0
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2008-08-12 14:02:08 +0000

    PerfAPI: Important! VM metrics are now associated with Machine instead of SessionMachine.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@11357 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Main/include/MachineImpl.h
+++ src/VBox/Main/include/MachineImpl.h
@@ -735,7 +735,7 @@ protected:
     void copyFrom (Machine *aThat);
 
 #ifdef VBOX_WITH_RESOURCE_USAGE_API
-    void registerMetrics (PerformanceCollector *aCollector, RTPROCESS pid);
+    void registerMetrics (PerformanceCollector *aCollector, Machine *aMachine, RTPROCESS pid);
     void unregisterMetrics (PerformanceCollector *aCollector);
 #endif /* VBOX_WITH_RESOURCE_USAGE_API */
 
