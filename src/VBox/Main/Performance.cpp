d1aef632d1802fbccdfe38d5c805c5397eb271b6
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2008-08-13 12:21:04 +0000

    PerfAPI: Null IN array handling changed.
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@11383 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Main/Performance.cpp
+++ src/VBox/Main/Performance.cpp
@@ -386,9 +386,9 @@ const char * AggregateMax::getName()
 Filter::Filter(ComSafeArrayIn(INPTR BSTR, metricNames),
                ComSafeArrayIn(IUnknown *, objects))
 {
-    com::SafeIfaceArray <IUnknown> objectArray(ComSafeArrayInArg(objects));
     com::SafeArray <INPTR BSTR> nameArray(ComSafeArrayInArg(metricNames));
-    if (objectArray.isNull())
+
+    if (ComSafeArrayInIsNull(objects))
     {
         if (nameArray.size())
         {
@@ -400,6 +400,8 @@ Filter::Filter(ComSafeArrayIn(INPTR BSTR, metricNames),
     }
     else
     {
+        com::SafeIfaceArray <IUnknown> objectArray(ComSafeArrayInArg(objects));
+
         for (size_t i = 0; i < objectArray.size(); ++i)
             switch (nameArray.size())
             {
