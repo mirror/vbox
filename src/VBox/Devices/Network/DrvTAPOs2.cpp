4ec07d513b28ee12a5ca0308e553036f29a70a7b
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2008-08-15 18:17:28 +0000

    DrvTAP: Increased buffer sizes to 16384 in Windows and OS/2. Increased max MTU size to 16384 in .inf file.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@11448 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Devices/Network/DrvTAPOs2.cpp
+++ src/VBox/Devices/Network/DrvTAPOs2.cpp
@@ -237,7 +237,7 @@ static DECLCALLBACK(int) drvTAPOs2ReceiveThread(PPDMDRVINS pDrvIns, PPDMTHREAD p
         /*
          * Read a frame, this will block for a while if nothing to read.
          */
-        char    abBuf[4096];
+        char    abBuf[16384];
         ULONG   Parm[2] = { ~0UL, ~0UL };   /* mysterious output */
         ULONG   cbParm = sizeof(Parm);      /* this one is actually ignored... */
         ULONG   cbBuf = sizeof(abBuf);
@@ -252,7 +252,8 @@ static DECLCALLBACK(int) drvTAPOs2ReceiveThread(PPDMDRVINS pDrvIns, PPDMTHREAD p
             &&  !Parm[0]
             &&  cbRead > 0 /* cbRead */)
         {
-            AssertMsg(cbRead <= 1536, ("cbRead=%d\n", cbRead));
+            // The following assertion was commented out to support jumbo frames.
+            //AssertMsg(cbRead <= 1536, ("cbRead=%d\n", cbRead));
 
             /*
              * Wait for the device to have some room. A return code != VINF_SUCCESS
