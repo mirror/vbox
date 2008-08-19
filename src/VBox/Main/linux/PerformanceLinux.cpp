3bfab276e76ed86fd5c54630e1eb45b93d3b023e
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2008-08-19 12:11:10 +0000

    PerfAPI: Linux implementation of CPU/MHz counter
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@11478 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Main/linux/PerformanceLinux.cpp
+++ src/VBox/Main/linux/PerformanceLinux.cpp
@@ -24,6 +24,7 @@
 #include <stdio.h>
 #include <iprt/alloc.h>
 #include <iprt/err.h>
+#include <iprt/mp.h>
 #include <iprt/param.h>
 #include <iprt/string.h>
 #include "Performance.h"
@@ -95,7 +96,14 @@ int CollectorLinux::getRawProcessCpuLoad(RTPROCESS process, uint64_t *user, uint
 
 int CollectorLinux::getHostCpuMHz(ULONG *mhz)
 {
-    return E_NOTIMPL;
+    RTCPUID nProcessors = RTMpGetCount();
+    uint64_t uTotalMHz  = 0;
+
+    for (RTCPUID i = 0; i < nProcessors; ++i)
+        uTotalMHz += RTMpGetCurFrequency(i);
+
+    *mhz = (ULONG)(uTotalMHz / nProcessors);
+    return VINF_SUCCESS;
 }
 
 int CollectorLinux::getHostMemoryUsage(ULONG *total, ULONG *used, ULONG *available)
