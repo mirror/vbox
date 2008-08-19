81d93533e4434cc2bae31d95d9e9ac14db7632ef
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2008-08-19 19:19:03 +0000

    Installer/solaris: Move the webservice start/stop script to the right place.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@11499 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Installer/solaris/makepackage.sh
+++ src/VBox/Installer/solaris/makepackage.sh
@@ -91,10 +91,10 @@ filelist_fixup prototype '$3 == "opt/VirtualBox/VBox.png=VBox.png"'             
 filelist_fixup prototype '$3 == "opt/VirtualBox/virtualbox-webservice.xml=virtualbox-webservice.xml"'   '$3 = "var/svc/manifest/application/virtualbox/webservice.xml=virtualbox-webservice.xml"'
 
 # webservice SMF start/stop script
-filelist_fixup prototype '$3 == "opt/VirtualBox/smf-vboxwebsrv.sh=smf-vboxwebsrv.sh"'                   '$3 = "var/svc/manifest/application/virtualbox/smf-vboxwebsrv=smf-vboxwebsrv.sh"'
+filelist_fixup prototype '$3 == "opt/VirtualBox/smf-vboxwebsrv.sh=smf-vboxwebsrv.sh"'                   '$3 = "opt/VirtualBox/smf-vboxwebsrv=smf-vboxwebsrv.sh"'
 
 # webservice SMF configuration file, mark as editable.
-filelist_fixup prototype '$3 == "opt/VirtualBox/etc/webservice.cfg=webservice.cfg"'   '$1 = "e"'
+filelist_fixup prototype '$3 == "opt/VirtualBox/etc/webservice.cfg=webservice.cfg"'                     '$1 = "e"'
 
 # explicitly set timestamp to shutup warning
 VBOXPKG_TIMESTAMP=vbox`date '+%Y%m%d%H%M%S'`
