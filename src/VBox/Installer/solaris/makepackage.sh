cdc90451bff21ecd4a04b9ab33d2d58b477a0574
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2008-08-19 08:48:02 +0000

    Installer/Solaris: add webservice configuration file, used by the smf start script.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@11475 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Installer/solaris/makepackage.sh
+++ src/VBox/Installer/solaris/makepackage.sh
@@ -90,6 +90,9 @@ filelist_fixup prototype '$3 == "opt/VirtualBox/VBox.png=VBox.png"'             
 # webservice SMF manifest
 filelist_fixup prototype '$3 == "opt/VirtualBox/virtualbox-webservice.xml=virtualbox-webservice.xml"'   '$3 = "var/svc/manifest/application/virtualbox/webservice.xml=virtualbox-webservice.xml"'
 
+# webservice SMF configuration file, mark as editable.
+filelist_fixup prototype '$3 == "opt/VirtualBox/etc/webservice.cfg=webservice.cfg"'   '$1 = "e"'
+
 # explicitly set timestamp to shutup warning
 VBOXPKG_TIMESTAMP=vbox`date '+%Y%m%d%H%M%S'`
 
