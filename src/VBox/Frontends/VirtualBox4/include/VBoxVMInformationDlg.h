4e776e18693ad3d2bdbb8b5ffdf55bdb5609862a
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2008-08-21 11:08:54 +0000

    Fe/Qt4: Right (values) column alignment changed to right-aligned.
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@11532 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Frontends/VirtualBox4/include/VBoxVMInformationDlg.h
+++ src/VBox/Frontends/VirtualBox4/include/VBoxVMInformationDlg.h
@@ -69,6 +69,7 @@ private:
     QString parseStatistics (const QString &aText);
     void refreshStatistics();
 
+    QString formatValue (const QString &aValueName, const QString &aValue, int aMaxSize);
     QString formatHardDisk (KStorageBus aBus, LONG aChannel, LONG aDevice, const QString &aBelongsTo);
     QString formatAdapter (ULONG aSlot, const QString &aBelongsTo);
 
