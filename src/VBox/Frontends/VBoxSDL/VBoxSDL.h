db0304199965ee257b2532cdcff642df5ab588b0
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2008-08-22 14:47:17 +0000

    typo
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@11576 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Frontends/VBoxSDL/VBoxSDL.h
+++ src/VBox/Frontends/VBoxSDL/VBoxSDL.h
@@ -48,7 +48,7 @@
 #define SDL_USER_EVENT_TIMER              (SDL_USEREVENT + 11)
 /** custom SDL user event for resetting mouse cursor */
 #define SDL_USER_EVENT_GUEST_CAP_CHANGED  (SDL_USEREVENT + 12)
-/** custom SDL user event for window resize down */
+/** custom SDL user event for window resize done */
 #define SDL_USER_EVENT_WINDOW_RESIZE_DONE (SDL_USEREVENT + 13)
 
 
