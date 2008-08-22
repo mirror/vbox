91ee6dacbe4e320ec681b3bcfecd835f10228b48
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2008-08-22 12:32:15 +0000

    Fe/Qt4: Cleaning main.cpp
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@11572 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Frontends/VirtualBox4/src/main.cpp
+++ src/VBox/Frontends/VirtualBox4/src/main.cpp
@@ -181,20 +181,7 @@ int main (int argc, char **argv)
         PlaySound (NULL, NULL, 0);
 #endif
 
-#ifndef Q_WS_MAC
-        /* some gui qt-styles has it's own different color for buttons
-         * causing tool-buttons and dropped menu displayed in
-         * different annoying color, so fixing palette button's color */
-//#warning "port me: any longer required?"
-//        QPalette pal = a.palette();
-//        pal.setColor (QPalette::Disabled, QColorGroup::Button,
-//                      pal.color (QPalette::Disabled, QColorGroup::Background));
-//        pal.setColor (QPalette::Active, QColorGroup::Button,
-//                      pal.color (QPalette::Active, QColorGroup::Background));
-//        pal.setColor (QPalette::Inactive, QColorGroup::Button,
-//                      pal.color (QPalette::Inactive, QColorGroup::Background));
-//        a.setPalette (pal);
-#else /* !Q_WS_MAC */
+#ifdef Q_WS_MAC
         ::darwinDisableIconsInMenus();
 #endif /* Q_WS_MAC */
 
