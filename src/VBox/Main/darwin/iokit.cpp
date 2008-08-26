41ac04ddaba3d8e5c01f3c91795a372c62946f41
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2008-08-26 01:09:17 +0000

    #1869: Change the interface soring so that the ones marked as 'pimary' is at the head of the list. This way the GUI default should be a bit more sensible (it selects the first entry).
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@11639 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Main/darwin/iokit.cpp
+++ src/VBox/Main/darwin/iokit.cpp
@@ -1568,7 +1568,7 @@ PDARWINETHERNIC DarwinGetEthernetControllers(void)
                             memcpy(pNew->szName, szTmp, cchName + 1);
 
                             /*
-                             * Link it into the list, keep the list sorted by the BSD name.
+                             * Link it into the list, keep the list sorted by fPrimaryIf and the BSD name.
                              */
                             if (pTail)
                             {
@@ -1577,7 +1577,8 @@ PDARWINETHERNIC DarwinGetEthernetControllers(void)
                                 {
                                     pPrev = NULL;
                                     for (PDARWINETHERNIC pCur = pHead; pCur; pPrev = pCur, pCur = pCur->pNext)
-                                        if (strcmp(pNew->szBSDName, pCur->szBSDName) >= 0)
+                                        if (    (int)pNew->fPrimaryIf - (int)pCur->fPrimaryIf >= 0
+                                            &&  strcmp(pNew->szBSDName, pCur->szBSDName) >= 0)
                                             break;
                                 }
                                 if (pPrev)
