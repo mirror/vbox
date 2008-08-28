a431a64f5f403261b5c9fbb03e8ef644ae4a1b6f
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2008-08-28 09:22:20 +0000

    export python lib to OSE
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@11746 cfe28804-0f27-0410-a406-dd0f0b0b656f

---
+++ src/libs/xpcom18a4/python/vboxxpcom.py
@@ -0,0 +1,26 @@
+#
+# Copyright (C) 2008 Sun Microsystems, Inc.
+#
+# This file is part of VirtualBox Open Source Edition (OSE), as
+# available from http://www.virtualbox.org. This file is free software;
+# you can redistribute it and/or modify it under the terms of the GNU
+# General Public License (GPL) as published by the Free Software
+# Foundation, in version 2 as it comes in the "COPYING" file of the
+# VirtualBox OSE distribution. VirtualBox OSE is distributed in the
+# hope that it will be useful, but WITHOUT ANY WARRANTY of any kind.
+# 
+# Please contact Sun Microsystems, Inc., 4150 Network Circle, Santa
+# Clara, CA 95054 USA or visit http://www.sun.com if you need
+# additional information or have any questions.
+#
+
+import xpcom
+import sys
+
+# this code overcomes somewhat unlucky feature of Python, where it searches
+# for binaries in the same place as platfom independent modules, while
+# rest of Python bindings expect _xpcom to be inside xpcom module
+# XXX: maybe implement per Python version module search
+cglue = __import__('VBoxPython')
+sys.modules['xpcom._xpcom'] = cglue
+xpcom._xpcom = cglue
