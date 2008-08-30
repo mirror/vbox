a5b4c74170d08db64837e4b0694ff5c223e020b7
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2008-08-30 00:38:44 +0000

    xpcom18a4: Yet another shot at the vtable for nsXPTCStubBase (__ZTV14nsXPTCStubBase).
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@11863 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/libs/xpcom18a4/vboxdeps.cpp
+++ src/libs/xpcom18a4/vboxdeps.cpp
@@ -20,6 +20,38 @@ uintptr_t deps[] =
     0
 };
 
+class foobardep : public nsXPTCStubBase
+{
+public:
+    NS_IMETHOD_(nsrefcnt) AddRef(void)
+    {
+        return 1;
+    }
+
+    NS_IMETHOD_(nsrefcnt) Release(void)
+    {
+        return 0;
+    }
+
+    NS_IMETHOD GetInterfaceInfo(nsIInterfaceInfo** info)
+    {
+        (void)info;
+        return 0;
+    }
+
+    // call this method and return result
+    NS_IMETHOD CallMethod(PRUint16 methodIndex, const nsXPTMethodInfo* info, nsXPTCMiniVariant* params)
+    {
+        (void)methodIndex;
+        (void)info;
+        (void)params;
+        return 0;
+    }
+
+};
+
+
+
 void foodep(void)
 {
     nsVoidHashSetSuper *a = new nsVoidHashSetSuper();
@@ -29,5 +61,9 @@ void foodep(void)
     //nsXPTCStubBase
     nsProxyEventObject *c = new nsProxyEventObject();
     c->Release();
+
+    foobardep *d = new foobardep();
+    nsXPTCStubBase *e = d;
+    e->Release();
 }
 
