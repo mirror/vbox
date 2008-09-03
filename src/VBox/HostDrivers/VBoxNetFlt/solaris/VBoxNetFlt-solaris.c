bef2041b5ea477f2c1ebfb36af070940c575b946
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2008-09-03 12:42:10 +0000

    Solaris/vboxnetflt: more todo.
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@12035 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/HostDrivers/VBoxNetFlt/solaris/VBoxNetFlt-solaris.c
+++ src/VBox/HostDrivers/VBoxNetFlt/solaris/VBoxNetFlt-solaris.c
@@ -637,6 +637,7 @@ static int VBoxNetFltSolarisModOpen(queue_t *pQueue, dev_t *pDev, int fOpenMode,
     pQueue->q_ptr = pStream;
     WR(pQueue)->q_ptr = pStream;
 
+    /** @todo Get rid of linked list maintenance; we're not a virtual DLPI device slaving for ring-3 consumers. */
     /*
      * Link it to the list of streams.
      */
