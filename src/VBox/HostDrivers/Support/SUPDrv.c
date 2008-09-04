0b564f9b4c55b733f9ae298d4d5e246b68d0523c
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2008-09-04 21:00:43 +0000

    VBoxDrv: Print the .reload command for modules as they are loaded (windows, debug builds only). You need to set the image path correctly for this short version to work.
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@12109 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/HostDrivers/Support/SUPDrv.c
+++ src/VBox/HostDrivers/Support/SUPDrv.c
@@ -3695,6 +3695,10 @@ static int supdrvIOCtl_LdrOpen(PSUPDRVDEVEXT pDevExt, PSUPDRVSESSION pSession, P
     pReq->u.Out.pvImageBase = pImage->pvImage;
     pReq->u.Out.fNeedsLoading = true;
     RTSemFastMutexRelease(pDevExt->mtxLdr);
+
+#if defined(RT_OS_WINDOWS) && defined(DEBUG)
+    SUPR0Printf("VBoxDrv: windbg> .reload /f %s=%#p\n", pImage->szName, pImage->pvImage);
+#endif
     return VINF_SUCCESS;
 }
 
