2c70263352a6374402c9b22f8669b67d235ff7cf
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2008-09-04 23:23:06 +0000

    #3076: Re-enabled the current->euid == 0 check in VBoxDrvLinuxCreate.
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@12115 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/HostDrivers/Support/linux/SUPDrv-linux.c
+++ src/VBox/HostDrivers/Support/linux/SUPDrv-linux.c
@@ -682,7 +682,7 @@ static int VBoxDrvLinuxCreate(struct inode *pInode, struct file *pFilp)
     PSUPDRVSESSION      pSession;
     Log(("VBoxDrvLinuxCreate: pFilp=%p pid=%d/%d %s\n", pFilp, RTProcSelf(), current->pid, current->comm));
 
-#if 0 /** @todo test this: #ifdef VBOX_WITH_HARDENING */
+#ifdef VBOX_WITH_HARDENING
     /*
      * Only root is allowed to access the device, enforce it!
      */
