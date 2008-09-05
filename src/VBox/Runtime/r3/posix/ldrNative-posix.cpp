02c007df45966d64a42c2177fd2119a7ca952df8
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2008-09-05 16:27:28 +0000

    IPRT/ldrNative-posix: LogRel dlopen errors so we can get the exact error on the release log.
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@12146 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Runtime/r3/posix/ldrNative-posix.cpp
+++ src/VBox/Runtime/r3/posix/ldrNative-posix.cpp
@@ -79,7 +79,7 @@ int rtldrNativeLoad(const char *pszFilename, uintptr_t *phHandle)
         *phHandle = (uintptr_t)pvMod;
         return VINF_SUCCESS;
     }
-    Log(("rtldrNativeLoad: dlopen('%s', RTLD_NOW | RTLD_LOCAL) failed: %s\n", pszFilename, dlerror()));
+    LogRel(("rtldrNativeLoad: dlopen('%s', RTLD_NOW | RTLD_LOCAL) failed: %s\n", pszFilename, dlerror()));
     return VERR_FILE_NOT_FOUND;
 }
 
