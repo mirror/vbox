b806a73ed4cd5df5d5cef52d867d26cc8e1488bd
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2008-09-10 19:32:36 +0000

    warnings
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@12371 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- include/VBox/pdmdev.h
+++ include/VBox/pdmdev.h
@@ -3054,6 +3054,11 @@ typedef struct PDMDEVINS
  */
 DECLINLINE(int) PDMDeviceDBGFStop(PPDMDEVINS pDevIns, RT_SRC_POS_DECL, const char *pszFormat, ...)
 {
+    NOREF(pDevIns);
+    NOREF(pszFormat);
+    NOREF(pszFile);
+    NOREF(iLine);
+    NOREF(pszFunction);
 #ifdef VBOX_STRICT
 # ifdef IN_RING3
     int rc;
