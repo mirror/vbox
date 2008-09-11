5c6ee44d7f906984a040382a7b093e72e7728b13
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2008-09-11 15:01:43 +0000

    We must flush the PGM pool cache if the guest mode changes; we don't always switch shadow paging mode (e.g. protected->32-bit) and shouldn't reuse the shadow page tables. (#3115 OS/2 guests)
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@12405 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/VMM/PGM.cpp
+++ src/VBox/VMM/PGM.cpp
@@ -3179,6 +3179,8 @@ PGMR3DECL(int) PGMR3ChangeMode(PVM pVM, PGMMODE enmGuestMode)
         }
 
     }
+    else
+        LogFlow(("PGMR3ChangeMode: Shadow mode remains: %s\n",  PGMGetModeName(pVM->pgm.s.enmShadowMode)));
 
     /* guest */
     if (PGM_GST_PFN(Exit, pVM))
@@ -3236,6 +3238,17 @@ PGMR3DECL(int) PGMR3ChangeMode(PVM pVM, PGMMODE enmGuestMode)
             pVM->pgm.s.enmShadowMode = PGMMODE_INVALID;
             return rc;
         }
+    }
+
+    /* We must flush the PGM pool cache if the guest mode changes; we don't always
+     * switch shadow paging mode (e.g. protected->32-bit) and shouldn't reuse
+     * the shadow page tables.
+     */
+    if (   pVM->pgm.s.CTXSUFF(pPool)
+        && pVM->pgm.s.enmGuestMode != enmGuestMode)
+    {
+        Log(("PGMR3ChangeMode: changing guest paging mode -> flush pgm pool cache!\n"));
+        pgmPoolFlushAll(pVM);
     }
 
     /*
