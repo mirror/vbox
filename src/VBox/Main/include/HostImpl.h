12c09fe794b5be99b587f0bc201dea548185ebc3
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2008-09-12 17:37:42 +0000

    installer code added to HostImpl, bugfixing in macket handing in the driver
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@12439 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Main/include/HostImpl.h
+++ src/VBox/Main/include/HostImpl.h
@@ -98,6 +98,23 @@ public:
     STDMETHOD(RemoveHostNetworkInterface) (INPTR GUIDPARAM aId,
                                            IHostNetworkInterface **aHostNetworkInterface,
                                            IProgress **aProgress);
+# ifdef VBOX_WITH_NETFLT
+    /*
+     * increments the reference to the netflt - to - aHostNetworkInterface binding reference count
+     * the exec flow:
+     * 1. if netflt is NOT installed currently - install it
+     * 2. if the NetFlt is not bound to the specified adapter - bind it
+     * 3. increase the binding reference count -
+     */
+    STDMETHOD(NetFltAcquire) (IHostNetworkInterface *aHostNetworkInterface,
+                                           IProgress **aProgress);
+
+    /*
+     * decrements the reference to the netflt - to - aHostNetworkInterface binding reference count
+     */
+    STDMETHOD(NetFltRelease) (IHostNetworkInterface *aHostNetworkInterface,
+                                           IProgress **aProgress);
+# endif /*VBOX_WITH_NETFLT*/
 #endif
     STDMETHOD(CreateUSBDeviceFilter) (INPTR BSTR aName, IHostUSBDeviceFilter **aFilter);
     STDMETHOD(InsertUSBDeviceFilter) (ULONG aPosition, IHostUSBDeviceFilter *aFilter);
