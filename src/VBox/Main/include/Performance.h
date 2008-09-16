7edfe7b67979ba1cad3f3056ef431847a7639510
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2008-09-16 19:11:15 +0000

    PerfAPI: Windows collector re-done: WMI replaced with kernel and psapi calls.
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@12513 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Main/include/Performance.h
+++ src/VBox/Main/include/Performance.h
@@ -81,28 +81,9 @@ namespace pm
 
     class CollectorHints
     {
+    public:
         typedef std::list<ProcessFlagsPair> ProcessList;
 
-        HintFlags   mHostFlags;
-        ProcessList mProcesses;
-
-        ProcessFlagsPair& findProcess(RTPROCESS process)
-        {
-            ProcessList::iterator it;
-
-            it = std::find_if(mProcesses.begin(),
-                              mProcesses.end(),
-                              std::bind2nd(std::ptr_fun(processEqual), process));
-
-            if (it != mProcesses.end())
-                return *it;
-
-            /* Not found -- add new */
-            mProcesses.push_back(ProcessFlagsPair(process, COLLECT_NONE));
-            return mProcesses.back();
-        }
-
-    public:
         CollectorHints() : mHostFlags(COLLECT_NONE) {}
         void collectHostCpuLoad() { mHostFlags |= COLLECT_CPU_LOAD; }
         void collectHostRamUsage() { mHostFlags |= COLLECT_RAM_USAGE; }
@@ -130,6 +111,29 @@ namespace pm
             processes.reserve(mProcesses.size());
             for (ProcessList::const_iterator it = mProcesses.begin(); it != mProcesses.end(); it++)
                 processes.push_back(it->first);
+        }
+        const ProcessList& getProcessFlags() const
+        {
+            return mProcesses;
+        }
+    private:
+        HintFlags   mHostFlags;
+        ProcessList mProcesses;
+
+        ProcessFlagsPair& findProcess(RTPROCESS process)
+        {
+            ProcessList::iterator it;
+
+            it = std::find_if(mProcesses.begin(),
+                              mProcesses.end(),
+                              std::bind2nd(std::ptr_fun(processEqual), process));
+
+            if (it != mProcesses.end())
+                return *it;
+
+            /* Not found -- add new */
+            mProcesses.push_back(ProcessFlagsPair(process, COLLECT_NONE));
+            return mProcesses.back();
         }
     };
 
