0fcb062d9925c4bef081c3264c8e22877ccf8834
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2008-09-19 15:58:38 +0000

    nit picking.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@12607 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Frontends/VBoxManage/VBoxManage.cpp
+++ src/VBox/Frontends/VBoxManage/VBoxManage.cpp
@@ -8027,7 +8027,7 @@ static BOOL WINAPI ctrlHandler(DWORD dwCtrlType)
         case CTRL_BREAK_EVENT:
         case CTRL_CLOSE_EVENT:
             /* Let's shut down gracefully. */
-            ASMAtomicWriteBool(&g_fKeepGoing, FALSE);
+            ASMAtomicWriteBool(&g_fKeepGoing, false);
             return TRUE;
     }
     /* Don't care about the rest -- let it die a horrible death. */
