541c0ae3d81f8753977cbd90bef8dbaca1934d23
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2008-09-25 11:48:56 +0000

    pdmR3Load3U: inverted test. grr!
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@12720 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/VMM/PDMLdr.cpp
+++ src/VBox/VMM/PDMLdr.cpp
@@ -271,7 +271,7 @@ int pdmR3LoadR3U(PUVM pUVM, const char *pszFilename, const char *pszName)
      * Allocate the module list node and initialize it.
      */
     const char *pszSuff = RTLdrGetSuff();
-    size_t      cchSuff = RTPathHaveExt(pszFilename) ? strlen(pszSuff) : 0;
+    size_t      cchSuff = RTPathHaveExt(pszFilename) ? 0 : strlen(pszSuff);
     PPDMMOD     pModule = (PPDMMOD)RTMemAllocZ(RT_OFFSETOF(PDMMOD, szFilename[cchFilename + cchSuff + 1]));
     if (!pModule)
         return VERR_NO_MEMORY;
