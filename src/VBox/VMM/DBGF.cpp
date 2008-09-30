227188d4863c991893a982c54a8ca5f867349de1
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2008-09-30 13:51:53 +0000

    DBGF: spaces.
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@12832 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/VMM/DBGF.cpp
+++ src/VBox/VMM/DBGF.cpp
@@ -238,12 +238,13 @@ bool dbgfR3WaitForAttach(PVM pVM, DBGFEVENTTYPE enmEvent)
      */
 #ifndef RT_OS_L4
 
-# if ! defined(DEBUG) || defined (DEBUG_sandervl) || defined (DEBUG_frank)
+# if !defined(DEBUG) || defined(DEBUG_sandervl) || defined(DEBUG_frank)
     int cWait = 10;
 # else
     int cWait = 150;
 # endif
-    RTStrmPrintf(g_pStdErr, "DBGF: No debugger attached, waiting %u seconds for one to attach (event=%d)\n", cWait / 10, enmEvent);
+    RTStrmPrintf(g_pStdErr, "DBGF: No debugger attached, waiting %d second%s for one to attach (event=%d)\n",
+                 cWait / 10, cWait != 10 ? "s" : "", enmEvent);
     RTStrmFlush(g_pStdErr);
     while (cWait > 0)
     {
