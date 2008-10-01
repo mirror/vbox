27db62c6d08d9db14d38e0c05de6f88804c01bc8
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2008-10-01 22:44:09 +0000

    DBGFR3Detach: Have to use RTSemPongWait, DBGFR3EventWait will check the fAttached flag and racing EMT which is clearing it.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@12881 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/VMM/DBGF.cpp
+++ src/VBox/VMM/DBGF.cpp
@@ -892,8 +892,7 @@ DBGFR3DECL(int) DBGFR3Detach(PVM pVM)
     /*
      * Wait for the OK event.
      */
-    PCDBGFEVENT pEvent;
-    rc = DBGFR3EventWait(pVM, RT_INDEFINITE_WAIT, &pEvent);
+    rc = RTSemPongWait(&pVM->dbgf.s.PingPong, RT_INDEFINITE_WAIT);
     AssertLogRelMsgRCReturn(rc, ("Wait on detach command failed, rc=%Rrc\n", rc), rc);
 
     /*
