f204c17af9a7c50bb1f370dd1e7028e11668f62e
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2008-10-07 11:05:23 +0000

    CSTAR was still restored in failure paths. (not supported on Intel CPUs)
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@13036 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/VMM/VMMR0/HWACCMR0A.asm
+++ src/VBox/VMM/VMMR0/HWACCMR0A.asm
@@ -618,7 +618,9 @@ ALIGNCODE(16)
     ;; @todo use the automatic load feature for MSRs
     LOADHOSTMSREX MSR_K8_KERNEL_GS_BASE, CPUMCTX.msrKERNELGSBASE
     LOADHOSTMSR MSR_K8_SF_MASK
+%if 0  ; not supported on Intel CPUs
     LOADHOSTMSR MSR_K8_CSTAR
+%endif
     LOADHOSTMSR MSR_K8_LSTAR
 
     ; Restore segment registers
@@ -645,7 +647,9 @@ ALIGNCODE(16)
     ;; @todo use the automatic load feature for MSRs
     LOADHOSTMSREX MSR_K8_KERNEL_GS_BASE, CPUMCTX.msrKERNELGSBASE
     LOADHOSTMSR MSR_K8_SF_MASK
+%if 0  ; not supported on Intel CPUs
     LOADHOSTMSR MSR_K8_CSTAR
+%endif
     LOADHOSTMSR MSR_K8_LSTAR
 
     ; Restore segment registers
