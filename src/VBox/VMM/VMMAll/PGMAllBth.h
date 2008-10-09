73438b365da9a49c6a7c3cf0b8db49f6ee653acb
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2008-10-09 14:24:07 +0000

    EPT: set the ignore PAT and WB cache flags in page table entries.
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@13134 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/VMM/VMMAll/PGMAllBth.h
+++ src/VBox/VMM/VMMAll/PGMAllBth.h
@@ -1427,9 +1427,11 @@ DECLINLINE(void) PGM_BTH_NAME(SyncPageWorker)(PVM pVM, PSHWPTE pPteDst, GSTPDE P
                 if (!PGM_PAGE_HAS_ACTIVE_ALL_HANDLERS(pPage))
                 {
 #if PGM_SHW_TYPE == PGM_TYPE_EPT
-                    PteDst.u           = (HCPhys & EPT_PTE_PG_MASK);
-                    PteDst.n.u1Present = 1;
-                    PteDst.n.u1Execute = 1;
+                    PteDst.u             = (HCPhys & EPT_PTE_PG_MASK);
+                    PteDst.n.u1Present   = 1;
+                    PteDst.n.u1Execute   = 1;
+                    PteDst.n.u1IgnorePAT = 1;
+                    PteDst.n.u3EMT       = VMX_EPT_MEMTYPE_WB;
                     /* PteDst.n.u1Write = 0 && PteDst.n.u1Big = 0 */
 #else
                     PteDst.u = (PteSrc.u & ~(X86_PTE_PAE_PG_MASK | X86_PTE_AVL_MASK | X86_PTE_PAT | X86_PTE_PCD | X86_PTE_PWT | X86_PTE_RW))
@@ -1473,10 +1475,12 @@ DECLINLINE(void) PGM_BTH_NAME(SyncPageWorker)(PVM pVM, PSHWPTE pPteDst, GSTPDE P
                 {
                     STAM_COUNTER_INC(&pVM->pgm.s.CTX_MID_Z(Stat,DirtyPageSkipped));
 #if PGM_SHW_TYPE == PGM_TYPE_EPT
-                    PteDst.u           = (HCPhys & EPT_PTE_PG_MASK);
-                    PteDst.n.u1Present = 1;
-                    PteDst.n.u1Write   = 1;
-                    PteDst.n.u1Execute = 1;
+                    PteDst.u             = (HCPhys & EPT_PTE_PG_MASK);
+                    PteDst.n.u1Present   = 1;
+                    PteDst.n.u1Write     = 1;
+                    PteDst.n.u1Execute   = 1;
+                    PteDst.n.u1IgnorePAT = 1;
+                    PteDst.n.u3EMT       = VMX_EPT_MEMTYPE_WB;
                     /* PteDst.n.u1Big = 0 */
 #else
                     PteDst.u = (PteSrc.u & ~(X86_PTE_PAE_PG_MASK | X86_PTE_AVL_MASK | X86_PTE_PAT | X86_PTE_PCD | X86_PTE_PWT))
