7fcd62250c6101d51f1e26251cf4044028734bd2
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2008-10-10 11:23:35 +0000

    Added clarification.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@13164 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/VMM/HWACCM.cpp
+++ src/VBox/VMM/HWACCM.cpp
@@ -854,7 +854,7 @@ VMMR3DECL(bool) HWACCMR3CanExecuteGuest(PVM pVM, PCPUMCTX pCtx)
     {
         /* VT-x will not allow high selector bases in v86 mode; fall back to the recompiler in that case. */
         if (   (   pCtx->csHid.u64Base > 0xfffff
-                && pCtx->csHid.u64Base != 0xffff0000)
+                && pCtx->csHid.u64Base != 0xffff0000 /* we can deal with the BIOS code as it's also mapped into the lower region. */)
             ||  pCtx->dsHid.u64Base > 0xfffff
             ||  pCtx->esHid.u64Base > 0xfffff
             ||  pCtx->fsHid.u64Base > 0xfffff
