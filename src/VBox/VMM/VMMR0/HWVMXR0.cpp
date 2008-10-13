578a83dd8fd77fec4c0355897f0ea3bd90ee9b5a
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2008-10-13 06:30:04 +0000

    uint64_t conversion fix
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@13193 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/VMM/VMMR0/HWVMXR0.cpp
+++ src/VBox/VMM/VMMR0/HWVMXR0.cpp
@@ -2281,11 +2281,12 @@ ResumeExecution:
         rc = VMXReadVMCS(VMX_VMCS_EXIT_PHYS_ADDR_FULL, &GCPhys);
         AssertRC(rc);
 #else
+        uint32_t val_hi;
         rc = VMXReadVMCS(VMX_VMCS_EXIT_PHYS_ADDR_FULL, &val);
         AssertRC(rc);
-        GCPhys = val;
-        rc = VMXReadVMCS(VMX_VMCS_EXIT_PHYS_ADDR_HIGH, &val);
-        GCPhys |= (val << 32ULL);      
+        rc = VMXReadVMCS(VMX_VMCS_EXIT_PHYS_ADDR_HIGH, &val_hi);
+        AssertRC(rc);
+        GCPhys = RT_MAKE_U64(val, val_hi);
 #endif
 
         Assert(((exitQualification >> 7) & 3) != 2);
