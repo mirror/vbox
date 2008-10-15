b626990fb28c878aef5eaff24ecc23002c375577
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2008-10-15 10:02:16 +0000

    Compile fix
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@13277 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/VMM/VMMR0/HWVMXR0.cpp
+++ src/VBox/VMM/VMMR0/HWVMXR0.cpp
@@ -529,9 +529,9 @@ vmx_end:
 static int VMXR0InjectEvent(PVM pVM, CPUMCTX *pCtx, uint32_t intInfo, uint32_t cbInstr, uint32_t errCode)
 {
     int         rc;
+    uint32_t    iGate = VMX_EXIT_INTERRUPTION_INFO_VECTOR(intInfo);
 
 #ifdef VBOX_STRICT
-    uint32_t    iGate = VMX_EXIT_INTERRUPTION_INFO_VECTOR(intInfo);
     if (iGate == 0xE)
         LogFlow(("VMXR0InjectEvent: Injecting interrupt %d at %VGv error code=%08x CR2=%08x intInfo=%08x\n", iGate, pCtx->rip, errCode, pCtx->cr2, intInfo));
     else
