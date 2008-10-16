2c045c5e590617f05f4f3be8ba91fbe832240bed
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2008-10-16 15:10:29 +0000

    SUPR3HardenedVerify: SharedCrOpenGL.so as optional. Removed unnecessary define.
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@13344 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/HostDrivers/Support/SUPR3HardenedVerify.cpp
+++ src/VBox/HostDrivers/Support/SUPR3HardenedVerify.cpp
@@ -108,9 +108,7 @@ static SUPINSTFILE const    g_aSupInstallFiles[] =
 
     {   kSupIFT_Dll,  kSupID_AppPrivArch,       false, "VBoxSharedClipboard" SUPLIB_DLL_SUFF },
     {   kSupIFT_Dll,  kSupID_AppPrivArch,       false, "VBoxSharedFolders" SUPLIB_DLL_SUFF },
-#ifdef VBOX_WITH_CROGL
-    {   kSupIFT_Dll,  kSupID_AppPrivArch,       false, "VBoxSharedCrOpenGL" SUPLIB_DLL_SUFF },
-#endif
+    {   kSupIFT_Dll,  kSupID_AppPrivArch,        true, "VBoxSharedCrOpenGL" SUPLIB_DLL_SUFF },
     {   kSupIFT_Dll,  kSupID_AppPrivArch,       false, "VBoxGuestPropSvc" SUPLIB_DLL_SUFF },
 
     {   kSupIFT_Exe,  kSupID_AppBin,            false, "VBoxManage" SUPLIB_EXE_SUFF },
