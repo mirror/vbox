b17d988ce64632d8c35e9dba1ce35ec951e85de3
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2008-10-17 18:40:13 +0000

    tmVirtualNanoTSRediscover: return to R3 when we start stepping (1ns). Attempt to make S10 work reliably.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@13380 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/VMM/VMMAll/TMAllVirtual.cpp
+++ src/VBox/VMM/VMMAll/TMAllVirtual.cpp
@@ -88,7 +88,15 @@ DECLEXPORT(uint64_t) tmVirtualNanoTSRediscover(PRTTIMENANOTSDATA pData)
  */
 DECLINLINE(uint64_t) tmVirtualGetRawNanoTS(PVM pVM)
 {
+#ifdef IN_RING3
     return CTXALLSUFF(pVM->tm.s.pfnVirtualGetRaw)(&CTXALLSUFF(pVM->tm.s.VirtualGetRawData));
+# else  /* !IN_RING3 */
+    uint32_t cPrevSteps = pVM->tm.s.CTXALLSUFF(VirtualGetRawData).c1nsSteps;
+    uint64_t u64 = CTXALLSUFF(pVM->tm.s.pfnVirtualGetRaw)(&CTXALLSUFF(pVM->tm.s.VirtualGetRawData));
+    if (cPrevSteps != pVM->tm.s.CTXALLSUFF(VirtualGetRawData).c1nsSteps)
+        VM_FF_SET(pVM, VM_FF_TO_R3); /* S10 hack */
+    return u64;
+# endif /* !IN_RING3 */
 }
 
 #else
@@ -219,6 +227,9 @@ static uint64_t tmVirtualGetRawNanoTS(PVM pVM)
         /* occasional - u64NanoTS is in the 'past' relative to previous returns. */
         ASMAtomicIncU32(&pVM->tm.s.CTXALLSUFF(VirtualGetRawData).c1nsSteps);
         u64NanoTS = u64PrevNanoTS + 1;
+#ifndef IN_RING3
+        VM_FF_SET(pVM, VM_FF_TO_R3); /* S10 hack */
+#endif
     }
     else if (u64PrevNanoTS)
     {
