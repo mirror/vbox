83cc627fc355824d644a0146d5162a48d18be44a
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2008-10-20 15:37:53 +0000

    Extra checks
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@13400 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/VMM/VMMAll/IOMAllMMIO.cpp
+++ src/VBox/VMM/VMMAll/IOMAllMMIO.cpp
@@ -1808,6 +1808,12 @@ VMMDECL(int)  IOMMMIOResetRegion(PVM pVM, RTGCPHYS GCPhys)
         rc = PGMShwModifyPage(pVM, (RTGCPTR)GCPhys, 1, 0, ~(uint64_t)(X86_PTE_RW|X86_PTE_P));
         AssertRC(rc);
 
+#ifdef VBOX_STRICT
+        uint64_t fFlags;
+        RTHCPHYS HCPhys;
+        rc = PGMShwGetPage(pVM, (RTGCPTR)GCPhys, &fFlags, &HCPhys);
+        Assert(rc == VERR_PAGE_NOT_PRESENT);
+#endif
         cb     -= PAGE_SIZE;
         GCPhys += PAGE_SIZE;
     }
