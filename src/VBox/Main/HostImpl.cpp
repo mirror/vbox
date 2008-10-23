be81204614df43ef7280cd05ae33ce2fab467f77
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2008-10-23 17:37:55 +0000

    win NetFlt/Main: fix for adaptor list calculation
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@13544 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Main/HostImpl.cpp
+++ src/VBox/Main/HostImpl.cpp
@@ -660,7 +660,7 @@ static bool vboxSolarisSortNICList(const ComObjPtr <HostNetworkInterface> Iface1
     Bstr Iface2Str;
     (*Iface2).COMGETTER(Name) (Iface2Str.asOutParam());
 
-    return Iface1Str < Iface2Str;    
+    return Iface1Str < Iface2Str;
 }
 
 static bool vboxSolarisSameNIC(const ComObjPtr <HostNetworkInterface> Iface1, const ComObjPtr <HostNetworkInterface> Iface2)
@@ -1255,8 +1255,14 @@ STDMETHODIMP Host::COMGETTER(NetworkInterfaces) (IHostNetworkInterfaceCollection
     Assert(hr == S_OK);
     if(hr == S_OK)
     {
-        /* for now we just get all miniports the MS_TCPIP protocol binds to */
+#ifdef VBOX_NETFLT_ONDEMAND_BIND
+        /* for the protocol-based approach for now we just get all miniports the MS_TCPIP protocol binds to */
         hr = pNc->FindComponent(L"MS_TCPIP", &pTcpIpNcc);
+#else
+        /* for the filter-based approach we get all miniports our filter (sun_VBoxNetFlt)is bound to */
+        hr = pNc->FindComponent(L"sun_VBoxNetFlt", &pTcpIpNcc);
+#endif
+
         Assert(hr == S_OK);
         if(hr == S_OK)
         {
