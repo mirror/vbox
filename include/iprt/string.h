edab116b3398560818445153189880fca9168d8a
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2008-10-24 11:31:40 +0000

    Just RTSTR_MAX.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@13549 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- include/iprt/string.h
+++ include/iprt/string.h
@@ -814,13 +814,11 @@ RTDECL(bool) RTStrSimplePatternMatch(const char *pszPattern, const char *pszStri
  * @param  pszPattern   The pattern. Special chars are '*' and '?', where the
  *                      asterisk matches zero or more characters and question
  *                      mark matches exactly one character.
- * @param  cchPattern   The pattern length. Pass RTSTR_MAX (~(size_t)0) if you
- *                      don't know the length and wish to stop at the string
- *                      terminator.
+ * @param  cchPattern   The pattern length. Pass RTSTR_MAX if you don't know the
+ *                      length and wish to stop at the string terminator.
  * @param  pszString    The string to match against the pattern.
- * @param  cchString    The string length. Pass RTSTR_MAX (~(size_t)0) if you
- *                      don't know the length and wish to match up to the string
- *                      terminator.
+ * @param  cchString    The string length. Pass RTSTR_MAX if you don't know the
+ *                      length and wish to match up to the string terminator.
  */
 RTDECL(bool) RTStrSimplePatternNMatch(const char *pszPattern, size_t cchPattern,
                                       const char *pszString, size_t cchString);
@@ -833,11 +831,11 @@ RTDECL(bool) RTStrSimplePatternNMatch(const char *pszPattern, size_t cchPattern,
  * @returns true if the string matches the pattern, otherwise false.
  *
  * @param   pszPatterns The patterns.
- * @param   cchPatterns The lengths of the patterns to use. Pass RTSTR_MAX
- *                      (~(size_t)0) to stop at the terminator.
- * @param   pszString   The string to match against the pattern.
- * @param   cchString   The string length. Pass RTSTR_MAX (~(size_t)0) stop
+ * @param   cchPatterns The lengths of the patterns to use. Pass RTSTR_MAX to
  *                      stop at the terminator.
+ * @param   pszString   The string to match against the pattern.
+ * @param   cchString   The string length. Pass RTSTR_MAX stop stop at the
+ *                      terminator.
  * @param   poffPattern Offset into the patterns string of the patttern that
  *                      matched. If no match, this will be set to RTSTR_MAX.
  *                      This is optional, NULL is fine.
