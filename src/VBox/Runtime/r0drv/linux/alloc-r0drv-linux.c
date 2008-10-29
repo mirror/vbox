62c05293668d770dff93327039eaa7f433cbb481
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2008-10-29 17:42:51 +0000

    rtMemAlloc/linux: Don't fool ourselves trying to allocate executable memory via __vmalloc on AMD64, it won't work. Better to fail straight away.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@13665 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Runtime/r0drv/linux/alloc-r0drv-linux.c
+++ src/VBox/Runtime/r0drv/linux/alloc-r0drv-linux.c
@@ -130,8 +130,10 @@ PRTMEMHDR rtMemAlloc(size_t cb, uint32_t fFlags)
             fFlags |= RTMEMHDR_FLAG_EXEC_HEAP;
         }
         else
-# endif
-            pHdr = (PRTMEMHDR)__vmalloc(cb + sizeof(*pHdr), GFP_KERNEL | __GFP_HIGHMEM, MY_PAGE_KERNEL_EXEC);
+            pHdr = NULL;
+# else  /* !RTMEMALLOC_EXEC_HEAP */
+        pHdr = (PRTMEMHDR)__vmalloc(cb + sizeof(*pHdr), GFP_KERNEL | __GFP_HIGHMEM, MY_PAGE_KERNEL_EXEC);
+# endif /* !RTMEMALLOC_EXEC_HEAP */
 
 #elif defined(PAGE_KERNEL_EXEC) && defined(CONFIG_X86_PAE)
         pHdr = (PRTMEMHDR)__vmalloc(cb + sizeof(*pHdr), GFP_KERNEL | __GFP_HIGHMEM, MY_PAGE_KERNEL_EXEC);
