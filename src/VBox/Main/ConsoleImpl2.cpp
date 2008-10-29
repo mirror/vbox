6626bf7ea42acae58fc4f94430810c85a7115084
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2008-10-29 15:26:02 +0000

    ConsoleImpl2.cpp: r=bird: QuietlyIgnoreAllPromisc will never be a default in a standard build, IgnoreAllPromisc is a maybe. Since IgnoreAllPromisc is questionsable as a default, I've disabled the code for now. (I've also moved the code since pCfg was pointing to the sniffer/nic's config, not DrvIntNet.)
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@13657 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Main/ConsoleImpl2.cpp
+++ src/VBox/Main/ConsoleImpl2.cpp
@@ -1190,11 +1190,6 @@ DECLCALLBACK(int) Console::configConstructor(PVM pVM, void *pvConsole)
 
                     const char *pszTrunk = szTrunk;
 
-                    /* Zone access controls*/
-                    zoneid_t ZoneId = getzoneid();
-                    if (ZoneId != GLOBAL_ZONEID)
-                        rc = CFGMR3InsertInteger(pCfg, "QuietlyIgnoreAllPromisc", true);   RC_CHECK();
-
 # elif defined(RT_OS_WINDOWS)
                     ComPtr<IHostNetworkInterfaceCollection> coll;
                     hrc = host->COMGETTER(NetworkInterfaces)(coll.asOutParam());    H();
@@ -1234,6 +1229,17 @@ DECLCALLBACK(int) Console::configConstructor(PVM pVM, void *pvConsole)
                     }
 # else
                     /** @todo PORTME: wireless detection */
+# endif
+
+# if defined(RT_OS_SOLARIS)
+#  if 0 /* bird: this is a bit questionable and might cause more trouble than its worth.  */
+                    /* Zone access restriction, don't allow snopping the global zone. */
+                    zoneid_t ZoneId = getzoneid();
+                    if (ZoneId != GLOBAL_ZONEID)
+                    {
+                        rc = CFGMR3InsertInteger(pCfg, "IgnoreAllPromisc", true);   RC_CHECK();
+                    }
+#  endif
 # endif
 
 #elif defined(RT_OS_WINDOWS)
