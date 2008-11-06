953e0d7b1263838fd67160340ad02886d6cc1f06
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2008-11-06 14:10:07 +0000

    MMAllHyper.cpp: Another stale CTXSUFF that had been overlooped.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@13918 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/VMM/VMMAll/MMAllHyper.cpp
+++ src/VBox/VMM/VMMAll/MMAllHyper.cpp
@@ -516,7 +516,7 @@ static PMMHYPERCHUNK mmHyperAllocChunk(PMMHYPERHEAP pHeap, uint32_t cb, unsigned
     uint32_t *pu32End = (uint32_t *)((uint8_t *)(pRet + 1) + cb);
     uint32_t *pu32EndReal = pRet->offNext
                           ? (uint32_t *)((uint8_t *)pRet + pRet->offNext)
-                          : (uint32_t *)(pHeap->CTXSUFF(pbHeap) + pHeap->cbHeap);
+                          : (uint32_t *)(pHeap->CTX_SUFF(pbHeap) + pHeap->cbHeap);
     cbFence += (uintptr_t)pu32EndReal - (uintptr_t)pu32End; Assert(!(cbFence & 0x3));
     ASMMemFill32((uint8_t *)pu32EndReal - cbFence, cbFence, MMHYPER_HEAP_STRICT_FENCE_U32);
     pu32EndReal[-1] = cbFence;
