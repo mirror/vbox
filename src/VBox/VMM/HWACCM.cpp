a62de2c50fec3fa3700cf3f69262e4f79bd40718
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2008-11-06 10:13:19 +0000

    Oops.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@13902 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/VMM/HWACCM.cpp
+++ src/VBox/VMM/HWACCM.cpp
@@ -223,7 +223,7 @@ VMMR3DECL(int) HWACCMR3InitCPU(PVM pVM)
                                          "/HWACCM/CPU%d/Exit/Reason/%02x", i, j);
                 AssertRC(rc);
             }
-            rc = STAMR3RegisterF(pVM, &pVCpu->hwaccm.s.StatExitReasonNPF, STAMTYPE_COUNTER, STAMVISIBILITY_USED, STAMUNIT_OCCURENCES, "/HWACCM/CPU%d/Exit/Reason/#NPF", "Exit reason", i);
+            rc = STAMR3RegisterF(pVM, &pVCpu->hwaccm.s.StatExitReasonNPF, STAMTYPE_COUNTER, STAMVISIBILITY_USED, STAMUNIT_OCCURENCES, "Exit reason", "/HWACCM/CPU%d/Exit/Reason/#NPF", i);
             AssertRC(rc);
         }
         pVCpu->hwaccm.s.paStatExitReasonR0 = MMHyperR3ToR0(pVM, pVCpu->hwaccm.s.paStatExitReason);
