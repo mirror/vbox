898e736376bd8b6cf5924fbc84979fb7e61753c5
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2008-11-07 16:33:20 +0000

    Even more debugger fixes.
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@13975 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/VMM/DBGFStack.cpp
+++ src/VBox/VMM/DBGFStack.cpp
@@ -357,7 +357,7 @@ VMMR3DECL(int) DBGFR3StackWalkBeginGuest(PVM pVM, PDBGFSTACKFRAME pFrame)
 
     PVMREQ pReq;
     int rc = VMR3ReqCall(pVM, VMREQDEST_ANY, &pReq, RT_INDEFINITE_WAIT, (PFNRT)dbgfR3StackWalkCtxFull, 4,
-                         pVM, pFrame, CPUMGetGuestCtxCore(pVM), true);
+                         pVM, pFrame, CPUMGetGuestCtxCoreEx(pVM, VMMGetCpu(pVM)), true); /* @todo SMP */
     if (RT_SUCCESS(rc))
         rc = pReq->iStatus;
     VMR3ReqFree(pReq);
