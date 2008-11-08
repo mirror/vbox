12150c5f7e8f6f9343a0606c2b3e5cfb36a2a6f6
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2008-11-08 18:35:21 +0000

    Solaris/vboxnetflt: don't push up messages on promisc stream even when inactive as we have no ring3 consumers.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@13983 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/HostDrivers/VBoxNetFlt/solaris/VBoxNetFlt-solaris.c
+++ src/VBox/HostDrivers/VBoxNetFlt/solaris/VBoxNetFlt-solaris.c
@@ -16,6 +16,7 @@
 #if defined(DEBUG_ramshankar) && !defined(LOG_ENABLED)
 # define LOG_ENABLED
 #endif
+
 #define LOG_GROUP LOG_GROUP_NET_FLT_DRV
 #include <VBox/log.h>
 #include <VBox/err.h>
@@ -904,8 +905,8 @@ static int VBoxNetFltSolarisModReadPut(queue_t *pQueue, mblk_t *pMsg)
                     {
                         vboxNetFltSolarisRecv(pThis, pStream, pQueue, pMsg);
                         pMsg = NULL;
-                        fSendUpstream = false;
                     }
+                    fSendUpstream = false;
                     break;
                 }
 
