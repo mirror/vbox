088a2964cf830375323f42de344443aeca10f53c
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2008-11-13 02:50:46 +0000

    mac build fixes.
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@14157 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/VMM/VMMAll/MMAllPagePool.cpp
+++ src/VBox/VMM/VMMAll/MMAllPagePool.cpp
@@ -42,8 +42,7 @@
 #endif
 
 
-
-#ifndef VBOX_WITH_2X_4GB_ADDR_SPACE_R0
+#ifndef VBOX_WITH_2X_4GB_ADDR_SPACE_IN_R0
 
 /**
  * Converts a pool physical address to a linear address.
@@ -107,7 +106,7 @@ VMMDECL(int) MMPagePhys2PageEx(PVM pVM, RTHCPHYS HCPhysPage, void **ppvPage)
     return VINF_SUCCESS;
 }
 
-#endif /* !VBOX_WITH_2X_4GB_ADDR_SPACE_R0 */
+#endif /* !VBOX_WITH_2X_4GB_ADDR_SPACE_IN_R0 */
 #ifdef IN_RING3
 
 /**
