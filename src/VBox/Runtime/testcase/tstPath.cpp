f8bb7648d36162275305a0bac0c06ae2c31690be
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2008-11-18 19:09:34 +0000

    RTPathStripFilename root fix.
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@14324 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Runtime/testcase/tstPath.cpp
+++ src/VBox/Runtime/testcase/tstPath.cpp
@@ -35,6 +35,7 @@
 #include <iprt/process.h>
 #include <iprt/initterm.h>
 #include <iprt/stream.h>
+#include <iprt/string.h>
 #include <iprt/err.h>
 #include <iprt/param.h>
 
@@ -119,7 +120,7 @@ int main()
         "\\MustDie",                    "\\from_root/dir/..",
         "\\temp",                       "\\data",
 #endif
-        };
+    };
 
     for (unsigned i = 0; i < RT_ELEMENTS(aInput); i += 2)
     {
@@ -130,12 +131,48 @@ int main()
     }
 
     /*
+     * RTPathStripFilename
+     */
+    RTPrintf("tstPath: RTPathStripFilename...\n");
+    static const char *apszStripFilenameTests[] =
+    {
+        "/usr/include///",              "/usr/include//",
+        "/usr/include/",                "/usr/include",
+        "/usr/include",                 "/usr",
+        "/usr",                         "/",
+        "usr",                          ".",
+#if defined (RT_OS_OS2) || defined (RT_OS_WINDOWS)
+        "c:/windows",                   "c:/",
+        "c:/",                          "c:/",
+        "D:",                           "D:",
+        "C:\\OS2\\DLLS",                "C:\\OS2",
+#endif
+    };
+    for (unsigned i = 0; i < RT_ELEMENTS(apszStripFilenameTests); i += 2)
+    {
+        const char *pszInput  = apszStripFilenameTests[i];
+        const char *pszExpect = apszStripFilenameTests[i + 1];
+        char szPath[RTPATH_MAX];
+        strcpy(szPath, pszInput);
+        RTPathStripFilename(szPath);
+        if (strcmp(szPath, pszExpect))
+        {
+            RTPrintf("tstPath: RTPathStripFilename failed!\n"
+                     "   input: '%s'\n"
+                     "  output: '%s'\n"
+                     "expected: '%s'\n",
+                     pszInput, szPath, pszExpect);
+            cErrors++;
+        }
+    }
+
+    /*
      * Summary.
      */
     if (!cErrors)
-        RTPrintf("tstTimer: SUCCESS\n");
+        RTPrintf("tstPath: SUCCESS\n");
     else
-        RTPrintf("tstTimer: FAILURE %d errors\n", cErrors);
+        RTPrintf("tstPath: FAILURE %d errors\n", cErrors);
     return !!cErrors;
 }
 
