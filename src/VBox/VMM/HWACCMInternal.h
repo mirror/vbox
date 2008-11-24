286654d470d36b8da33514a5dda26dd63ad1673b
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2008-11-24 16:50:00 +0000

    Compile fix
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@14532 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/VMM/HWACCMInternal.h
+++ src/VBox/VMM/HWACCMInternal.h
@@ -442,15 +442,9 @@ typedef struct HWACCMCPU
     /** Currenty shadow paging mode. */
     PGMMODE                 enmShadowMode;
 
-#ifdef VBOX_STRICT
     /** The CPU ID of the CPU currently owning the VMCS. Set in
      * HWACCMR0Enter and cleared in HWACCMR0Leave. */
     RTCPUID                 idEnteredCpu;
-#else
-# if HC_ARCH_BITS == 32
-    RTCPUID                 Alignment2;
-# endif
-#endif
 
     STAMPROFILEADV          StatEntry;
     STAMPROFILEADV          StatExit;
