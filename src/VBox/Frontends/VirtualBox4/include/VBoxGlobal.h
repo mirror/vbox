cc086121ef6c1f8f8c48a4396d9a19de7e1704ad
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2008-11-24 11:20:26 +0000

    mac build fix
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@14511 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Frontends/VirtualBox4/include/VBoxGlobal.h
+++ src/VBox/Frontends/VirtualBox4/include/VBoxGlobal.h
@@ -935,7 +935,9 @@ signals:
     void machineRegistered (const VBoxMachineRegisteredEvent &e);
     void sessionStateChanged (const VBoxSessionStateChangeEvent &e);
     void snapshotChanged (const VBoxSnapshotEvent &e);
+#ifdef VBOX_GUI_WITH_SYSTRAY
     void systrayIconChanged (const VBoxChangeGUITrayIconEvent &e);
+#endif
 
     void canShowRegDlg (bool aCanShow);
     void canShowUpdDlg (bool aCanShow);
