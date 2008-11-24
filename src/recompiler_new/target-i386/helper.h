c7ef69b14b0c945dc887407a59eef76f8032d0d8
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2008-11-24 19:25:35 +0000

    Export new recompiler to OSE
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@14542 cfe28804-0f27-0410-a406-dd0f0b0b656f

---
+++ src/recompiler_new/target-i386/helper.h
@@ -0,0 +1,247 @@
+#ifndef DEF_HELPER
+#define DEF_HELPER(ret, name, params) ret name params;
+#endif
+
+DEF_HELPER(void, helper_lock, (void))
+DEF_HELPER(void, helper_unlock, (void))
+DEF_HELPER(void, helper_write_eflags, (target_ulong t0, uint32_t update_mask))
+DEF_HELPER(target_ulong, helper_read_eflags, (void))
+#ifdef VBOX
+DEF_HELPER(void, helper_write_eflags_vme, (target_ulong t0))
+DEF_HELPER(target_ulong, helper_read_eflags_vme, (void))
+#endif
+DEF_HELPER(void, helper_divb_AL, (target_ulong t0))
+DEF_HELPER(void, helper_idivb_AL, (target_ulong t0))
+DEF_HELPER(void, helper_divw_AX, (target_ulong t0))
+DEF_HELPER(void, helper_idivw_AX, (target_ulong t0))
+DEF_HELPER(void, helper_divl_EAX, (target_ulong t0))
+DEF_HELPER(void, helper_idivl_EAX, (target_ulong t0))
+#ifdef TARGET_X86_64
+DEF_HELPER(void, helper_mulq_EAX_T0, (target_ulong t0))
+DEF_HELPER(void, helper_imulq_EAX_T0, (target_ulong t0))
+DEF_HELPER(target_ulong, helper_imulq_T0_T1, (target_ulong t0, target_ulong t1))
+DEF_HELPER(void, helper_divq_EAX, (target_ulong t0))
+DEF_HELPER(void, helper_idivq_EAX, (target_ulong t0))
+#endif
+
+DEF_HELPER(void, helper_aam, (int base))
+DEF_HELPER(void, helper_aad, (int base))
+DEF_HELPER(void, helper_aaa, (void))
+DEF_HELPER(void, helper_aas, (void))
+DEF_HELPER(void, helper_daa, (void))
+DEF_HELPER(void, helper_das, (void))
+
+DEF_HELPER(target_ulong, helper_lsl, (target_ulong selector1))
+DEF_HELPER(target_ulong, helper_lar, (target_ulong selector1))
+DEF_HELPER(void, helper_verr, (target_ulong selector1))
+DEF_HELPER(void, helper_verw, (target_ulong selector1))
+DEF_HELPER(void, helper_lldt, (int selector))
+DEF_HELPER(void, helper_ltr, (int selector))
+DEF_HELPER(void, helper_load_seg, (int seg_reg, int selector))
+DEF_HELPER(void, helper_ljmp_protected, (int new_cs, target_ulong new_eip,
+                           int next_eip_addend))
+DEF_HELPER(void, helper_lcall_real, (int new_cs, target_ulong new_eip1,
+                       int shift, int next_eip))
+DEF_HELPER(void, helper_lcall_protected, (int new_cs, target_ulong new_eip, 
+                            int shift, int next_eip_addend))
+DEF_HELPER(void, helper_iret_real, (int shift))
+DEF_HELPER(void, helper_iret_protected, (int shift, int next_eip))
+DEF_HELPER(void, helper_lret_protected, (int shift, int addend))
+DEF_HELPER(target_ulong, helper_read_crN, (int reg))
+DEF_HELPER(void, helper_write_crN, (int reg, target_ulong t0))
+DEF_HELPER(void, helper_lmsw, (target_ulong t0))
+DEF_HELPER(void, helper_clts, (void))
+DEF_HELPER(void, helper_movl_drN_T0, (int reg, target_ulong t0))
+DEF_HELPER(void, helper_invlpg, (target_ulong addr))
+
+DEF_HELPER(void, helper_enter_level, (int level, int data32, target_ulong t1))
+#ifdef TARGET_X86_64
+DEF_HELPER(void, helper_enter64_level, (int level, int data64, target_ulong t1))
+#endif
+DEF_HELPER(void, helper_sysenter, (void))
+DEF_HELPER(void, helper_sysexit, (int dflag))
+#ifdef TARGET_X86_64
+DEF_HELPER(void, helper_syscall, (int next_eip_addend))
+DEF_HELPER(void, helper_sysret, (int dflag))
+#endif
+DEF_HELPER(void, helper_hlt, (int next_eip_addend))
+DEF_HELPER(void, helper_monitor, (target_ulong ptr))
+DEF_HELPER(void, helper_mwait, (int next_eip_addend))
+DEF_HELPER(void, helper_debug, (void))
+DEF_HELPER(void, helper_raise_interrupt, (int intno, int next_eip_addend))
+DEF_HELPER(void, helper_raise_exception, (int exception_index))
+DEF_HELPER(void, helper_cli, (void))
+DEF_HELPER(void, helper_sti, (void))
+#ifdef VBOX
+DEF_HELPER(void, helper_cli_vme, (void))
+DEF_HELPER(void, helper_sti_vme, (void))
+#endif
+DEF_HELPER(void, helper_set_inhibit_irq, (void))
+DEF_HELPER(void, helper_reset_inhibit_irq, (void))
+DEF_HELPER(void, helper_boundw, (target_ulong a0, int v))
+DEF_HELPER(void, helper_boundl, (target_ulong a0, int v))
+DEF_HELPER(void, helper_rsm, (void))
+DEF_HELPER(void, helper_into, (int next_eip_addend))
+DEF_HELPER(void, helper_cmpxchg8b, (target_ulong a0))
+#ifdef TARGET_X86_64
+DEF_HELPER(void, helper_cmpxchg16b, (target_ulong a0))
+#endif
+DEF_HELPER(void, helper_single_step, (void))
+DEF_HELPER(void, helper_cpuid, (void))
+DEF_HELPER(void, helper_rdtsc, (void))
+DEF_HELPER(void, helper_rdpmc, (void))
+DEF_HELPER(void, helper_rdmsr, (void))
+DEF_HELPER(void, helper_wrmsr, (void))
+#ifdef VBOX
+DEF_HELPER(void, helper_rdtscp, (void))
+#endif
+
+DEF_HELPER(void, helper_check_iob, (uint32_t t0))
+DEF_HELPER(void, helper_check_iow, (uint32_t t0))
+DEF_HELPER(void, helper_check_iol, (uint32_t t0))
+#ifdef VBOX
+DEF_HELPER(void, helper_check_external_event, (void))
+DEF_HELPER(void, helper_dump_state, (void))
+DEF_HELPER(void, helper_sync_seg, (uint32_t t0))
+#endif
+DEF_HELPER(void, helper_outb, (uint32_t port, uint32_t data))
+DEF_HELPER(target_ulong, helper_inb, (uint32_t port))
+DEF_HELPER(void, helper_outw, (uint32_t port, uint32_t data))
+DEF_HELPER(target_ulong, helper_inw, (uint32_t port))
+DEF_HELPER(void, helper_outl, (uint32_t port, uint32_t data))
+DEF_HELPER(target_ulong, helper_inl, (uint32_t port))
+
+DEF_HELPER(void, helper_svm_check_intercept_param, (uint32_t type, uint64_t param))
+DEF_HELPER(void, helper_vmexit, (uint32_t exit_code, uint64_t exit_info_1))
+DEF_HELPER(void, helper_svm_check_io, (uint32_t port, uint32_t param, 
+                         uint32_t next_eip_addend))
+DEF_HELPER(void, helper_vmrun, (int aflag, int next_eip_addend))
+DEF_HELPER(void, helper_vmmcall, (void))
+DEF_HELPER(void, helper_vmload, (int aflag))
+DEF_HELPER(void, helper_vmsave, (int aflag))
+DEF_HELPER(void, helper_stgi, (void))
+DEF_HELPER(void, helper_clgi, (void))
+DEF_HELPER(void, helper_skinit, (void))
+DEF_HELPER(void, helper_invlpga, (int aflag))
+
+/* x86 FPU */
+
+DEF_HELPER(void, helper_flds_FT0, (uint32_t val))
+DEF_HELPER(void, helper_fldl_FT0, (uint64_t val))
+DEF_HELPER(void, helper_fildl_FT0, (int32_t val))
+DEF_HELPER(void, helper_flds_ST0, (uint32_t val))
+DEF_HELPER(void, helper_fldl_ST0, (uint64_t val))
+DEF_HELPER(void, helper_fildl_ST0, (int32_t val))
+DEF_HELPER(void, helper_fildll_ST0, (int64_t val))
+DEF_HELPER(uint32_t, helper_fsts_ST0, (void))
+DEF_HELPER(uint64_t, helper_fstl_ST0, (void))
+DEF_HELPER(int32_t, helper_fist_ST0, (void))
+DEF_HELPER(int32_t, helper_fistl_ST0, (void))
+DEF_HELPER(int64_t, helper_fistll_ST0, (void))
+DEF_HELPER(int32_t, helper_fistt_ST0, (void))
+DEF_HELPER(int32_t, helper_fisttl_ST0, (void))
+DEF_HELPER(int64_t, helper_fisttll_ST0, (void))
+DEF_HELPER(void, helper_fldt_ST0, (target_ulong ptr))
+DEF_HELPER(void, helper_fstt_ST0, (target_ulong ptr))
+DEF_HELPER(void, helper_fpush, (void))
+DEF_HELPER(void, helper_fpop, (void))
+DEF_HELPER(void, helper_fdecstp, (void))
+DEF_HELPER(void, helper_fincstp, (void))
+DEF_HELPER(void, helper_ffree_STN, (int st_index))
+DEF_HELPER(void, helper_fmov_ST0_FT0, (void))
+DEF_HELPER(void, helper_fmov_FT0_STN, (int st_index))
+DEF_HELPER(void, helper_fmov_ST0_STN, (int st_index))
+DEF_HELPER(void, helper_fmov_STN_ST0, (int st_index))
+DEF_HELPER(void, helper_fxchg_ST0_STN, (int st_index))
+DEF_HELPER(void, helper_fcom_ST0_FT0, (void))
+DEF_HELPER(void, helper_fucom_ST0_FT0, (void))
+DEF_HELPER(void, helper_fcomi_ST0_FT0, (void))
+DEF_HELPER(void, helper_fucomi_ST0_FT0, (void))
+DEF_HELPER(void, helper_fadd_ST0_FT0, (void))
+DEF_HELPER(void, helper_fmul_ST0_FT0, (void))
+DEF_HELPER(void, helper_fsub_ST0_FT0, (void))
+DEF_HELPER(void, helper_fsubr_ST0_FT0, (void))
+DEF_HELPER(void, helper_fdiv_ST0_FT0, (void))
+DEF_HELPER(void, helper_fdivr_ST0_FT0, (void))
+DEF_HELPER(void, helper_fadd_STN_ST0, (int st_index))
+DEF_HELPER(void, helper_fmul_STN_ST0, (int st_index))
+DEF_HELPER(void, helper_fsub_STN_ST0, (int st_index))
+DEF_HELPER(void, helper_fsubr_STN_ST0, (int st_index))
+DEF_HELPER(void, helper_fdiv_STN_ST0, (int st_index))
+DEF_HELPER(void, helper_fdivr_STN_ST0, (int st_index))
+DEF_HELPER(void, helper_fchs_ST0, (void))
+DEF_HELPER(void, helper_fabs_ST0, (void))
+DEF_HELPER(void, helper_fxam_ST0, (void))
+DEF_HELPER(void, helper_fld1_ST0, (void))
+DEF_HELPER(void, helper_fldl2t_ST0, (void))
+DEF_HELPER(void, helper_fldl2e_ST0, (void))
+DEF_HELPER(void, helper_fldpi_ST0, (void))
+DEF_HELPER(void, helper_fldlg2_ST0, (void))
+DEF_HELPER(void, helper_fldln2_ST0, (void))
+DEF_HELPER(void, helper_fldz_ST0, (void))
+DEF_HELPER(void, helper_fldz_FT0, (void))
+DEF_HELPER(uint32_t, helper_fnstsw, (void))
+DEF_HELPER(uint32_t, helper_fnstcw, (void))
+DEF_HELPER(void, helper_fldcw, (uint32_t val))
+DEF_HELPER(void, helper_fclex, (void))
+DEF_HELPER(void, helper_fwait, (void))
+DEF_HELPER(void, helper_fninit, (void))
+DEF_HELPER(void, helper_fbld_ST0, (target_ulong ptr))
+DEF_HELPER(void, helper_fbst_ST0, (target_ulong ptr))
+DEF_HELPER(void, helper_f2xm1, (void))
+DEF_HELPER(void, helper_fyl2x, (void))
+DEF_HELPER(void, helper_fptan, (void))
+DEF_HELPER(void, helper_fpatan, (void))
+DEF_HELPER(void, helper_fxtract, (void))
+DEF_HELPER(void, helper_fprem1, (void))
+DEF_HELPER(void, helper_fprem, (void))
+DEF_HELPER(void, helper_fyl2xp1, (void))
+DEF_HELPER(void, helper_fsqrt, (void))
+DEF_HELPER(void, helper_fsincos, (void))
+DEF_HELPER(void, helper_frndint, (void))
+DEF_HELPER(void, helper_fscale, (void))
+DEF_HELPER(void, helper_fsin, (void))
+DEF_HELPER(void, helper_fcos, (void))
+DEF_HELPER(void, helper_fstenv, (target_ulong ptr, int data32))
+DEF_HELPER(void, helper_fldenv, (target_ulong ptr, int data32))
+DEF_HELPER(void, helper_fsave, (target_ulong ptr, int data32))
+DEF_HELPER(void, helper_frstor, (target_ulong ptr, int data32))
+DEF_HELPER(void, helper_fxsave, (target_ulong ptr, int data64))
+DEF_HELPER(void, helper_fxrstor, (target_ulong ptr, int data64))
+DEF_HELPER(target_ulong, helper_bsf, (target_ulong t0))
+DEF_HELPER(target_ulong, helper_bsr, (target_ulong t0))
+
+/* MMX/SSE */
+
+DEF_HELPER(void, helper_enter_mmx, (void))
+DEF_HELPER(void, helper_emms, (void))
+DEF_HELPER(void, helper_movq, (uint64_t *d, uint64_t *s))
+
+#define SHIFT 0
+#include "ops_sse_header.h"
+#define SHIFT 1
+#include "ops_sse_header.h"
+
+DEF_HELPER(target_ulong, helper_rclb, (target_ulong t0, target_ulong t1))
+DEF_HELPER(target_ulong, helper_rclw, (target_ulong t0, target_ulong t1))
+DEF_HELPER(target_ulong, helper_rcll, (target_ulong t0, target_ulong t1))
+DEF_HELPER(target_ulong, helper_rcrb, (target_ulong t0, target_ulong t1))
+DEF_HELPER(target_ulong, helper_rcrw, (target_ulong t0, target_ulong t1))
+DEF_HELPER(target_ulong, helper_rcrl, (target_ulong t0, target_ulong t1))
+#ifdef TARGET_X86_64
+DEF_HELPER(target_ulong, helper_rclq, (target_ulong t0, target_ulong t1))
+DEF_HELPER(target_ulong, helper_rcrq, (target_ulong t0, target_ulong t1))
+#endif
+
+#ifdef VBOX
+void helper_external_event(void);
+void helper_record_call(void);
+
+/* in op_helper.c */
+void sync_seg(CPUX86State *env1, int seg_reg, int selector);
+void sync_ldtr(CPUX86State *env1, int selector);
+int  sync_tr(CPUX86State *env1, int selector);
+
+#endif
+
+#undef DEF_HELPER
---
+++ src/recompiler_new/target-i386/ops_sse_header.h
@@ -0,0 +1,346 @@
+/*
+ *  MMX/3DNow!/SSE/SSE2/SSE3/SSSE3/SSE4/PNI support
+ *
+ *  Copyright (c) 2005 Fabrice Bellard
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+/*
+ * Sun LGPL Disclaimer: For the avoidance of doubt, except that if any license choice
+ * other than GPL or LGPL is available it will apply instead, Sun elects to use only
+ * the Lesser General Public License version 2.1 (LGPLv2) at this time for any software where
+ * a choice of LGPL license versions is made available with the language indicating
+ * that LGPLv2 or any later version may be used, or where a choice of which version
+ * of the LGPL is applied is otherwise unspecified.
+ */
+#if SHIFT == 0
+#define Reg MMXReg
+#define SUFFIX _mmx
+#else
+#define Reg XMMReg
+#define SUFFIX _xmm
+#endif
+
+DEF_HELPER(void, glue(helper_psrlw, SUFFIX), (Reg *d, Reg *s))
+DEF_HELPER(void, glue(helper_psraw, SUFFIX), (Reg *d, Reg *s))
+DEF_HELPER(void, glue(helper_psllw, SUFFIX), (Reg *d, Reg *s))
+DEF_HELPER(void, glue(helper_psrld, SUFFIX), (Reg *d, Reg *s))
+DEF_HELPER(void, glue(helper_psrad, SUFFIX), (Reg *d, Reg *s))
+DEF_HELPER(void, glue(helper_pslld, SUFFIX), (Reg *d, Reg *s))
+DEF_HELPER(void, glue(helper_psrlq, SUFFIX), (Reg *d, Reg *s))
+DEF_HELPER(void, glue(helper_psllq, SUFFIX), (Reg *d, Reg *s))
+
+#if SHIFT == 1
+DEF_HELPER(void, glue(helper_psrldq, SUFFIX), (Reg *d, Reg *s))
+DEF_HELPER(void, glue(helper_pslldq, SUFFIX), (Reg *d, Reg *s))
+#endif
+
+#define SSE_HELPER_B(name, F)\
+    DEF_HELPER(void, glue(name, SUFFIX), (Reg *d, Reg *s))
+
+#define SSE_HELPER_W(name, F)\
+    DEF_HELPER(void, glue(name, SUFFIX), (Reg *d, Reg *s))
+
+#define SSE_HELPER_L(name, F)\
+    DEF_HELPER(void, glue(name, SUFFIX), (Reg *d, Reg *s))
+
+#define SSE_HELPER_Q(name, F)\
+    DEF_HELPER(void, glue(name, SUFFIX), (Reg *d, Reg *s))
+
+SSE_HELPER_B(helper_paddb, FADD)
+SSE_HELPER_W(helper_paddw, FADD)
+SSE_HELPER_L(helper_paddl, FADD)
+SSE_HELPER_Q(helper_paddq, FADD)
+
+SSE_HELPER_B(helper_psubb, FSUB)
+SSE_HELPER_W(helper_psubw, FSUB)
+SSE_HELPER_L(helper_psubl, FSUB)
+SSE_HELPER_Q(helper_psubq, FSUB)
+
+SSE_HELPER_B(helper_paddusb, FADDUB)
+SSE_HELPER_B(helper_paddsb, FADDSB)
+SSE_HELPER_B(helper_psubusb, FSUBUB)
+SSE_HELPER_B(helper_psubsb, FSUBSB)
+
+SSE_HELPER_W(helper_paddusw, FADDUW)
+SSE_HELPER_W(helper_paddsw, FADDSW)
+SSE_HELPER_W(helper_psubusw, FSUBUW)
+SSE_HELPER_W(helper_psubsw, FSUBSW)
+
+SSE_HELPER_B(helper_pminub, FMINUB)
+SSE_HELPER_B(helper_pmaxub, FMAXUB)
+
+SSE_HELPER_W(helper_pminsw, FMINSW)
+SSE_HELPER_W(helper_pmaxsw, FMAXSW)
+
+SSE_HELPER_Q(helper_pand, FAND)
+SSE_HELPER_Q(helper_pandn, FANDN)
+SSE_HELPER_Q(helper_por, FOR)
+SSE_HELPER_Q(helper_pxor, FXOR)
+
+SSE_HELPER_B(helper_pcmpgtb, FCMPGTB)
+SSE_HELPER_W(helper_pcmpgtw, FCMPGTW)
+SSE_HELPER_L(helper_pcmpgtl, FCMPGTL)
+
+SSE_HELPER_B(helper_pcmpeqb, FCMPEQ)
+SSE_HELPER_W(helper_pcmpeqw, FCMPEQ)
+SSE_HELPER_L(helper_pcmpeql, FCMPEQ)
+
+SSE_HELPER_W(helper_pmullw, FMULLW)
+#if SHIFT == 0
+SSE_HELPER_W(helper_pmulhrw, FMULHRW)
+#endif
+SSE_HELPER_W(helper_pmulhuw, FMULHUW)
+SSE_HELPER_W(helper_pmulhw, FMULHW)
+
+SSE_HELPER_B(helper_pavgb, FAVG)
+SSE_HELPER_W(helper_pavgw, FAVG)
+
+DEF_HELPER(void, glue(helper_pmuludq, SUFFIX) , (Reg *d, Reg *s))
+DEF_HELPER(void, glue(helper_pmaddwd, SUFFIX) , (Reg *d, Reg *s))
+
+DEF_HELPER(void, glue(helper_psadbw, SUFFIX) , (Reg *d, Reg *s))
+DEF_HELPER(void, glue(helper_maskmov, SUFFIX) , (Reg *d, Reg *s, target_ulong a0))
+DEF_HELPER(void, glue(helper_movl_mm_T0, SUFFIX) , (Reg *d, uint32_t val))
+#ifdef TARGET_X86_64
+DEF_HELPER(void, glue(helper_movq_mm_T0, SUFFIX) , (Reg *d, uint64_t val))
+#endif
+
+#if SHIFT == 0
+DEF_HELPER(void, glue(helper_pshufw, SUFFIX) , (Reg *d, Reg *s, int order))
+#else
+DEF_HELPER(void, helper_shufps, (Reg *d, Reg *s, int order))
+DEF_HELPER(void, helper_shufpd, (Reg *d, Reg *s, int order))
+DEF_HELPER(void, glue(helper_pshufd, SUFFIX) , (Reg *d, Reg *s, int order))
+DEF_HELPER(void, glue(helper_pshuflw, SUFFIX) , (Reg *d, Reg *s, int order))
+DEF_HELPER(void, glue(helper_pshufhw, SUFFIX) , (Reg *d, Reg *s, int order))
+#endif
+
+#if SHIFT == 1
+/* FPU ops */
+/* XXX: not accurate */
+
+#define SSE_HELPER_S(name, F)\
+    DEF_HELPER(void, helper_ ## name ## ps , (Reg *d, Reg *s))        \
+    DEF_HELPER(void, helper_ ## name ## ss , (Reg *d, Reg *s))        \
+    DEF_HELPER(void, helper_ ## name ## pd , (Reg *d, Reg *s))        \
+    DEF_HELPER(void, helper_ ## name ## sd , (Reg *d, Reg *s))
+
+SSE_HELPER_S(add, FPU_ADD)
+SSE_HELPER_S(sub, FPU_SUB)
+SSE_HELPER_S(mul, FPU_MUL)
+SSE_HELPER_S(div, FPU_DIV)
+SSE_HELPER_S(min, FPU_MIN)
+SSE_HELPER_S(max, FPU_MAX)
+SSE_HELPER_S(sqrt, FPU_SQRT)
+
+
+DEF_HELPER(void, helper_cvtps2pd, (Reg *d, Reg *s))
+DEF_HELPER(void, helper_cvtpd2ps, (Reg *d, Reg *s))
+DEF_HELPER(void, helper_cvtss2sd, (Reg *d, Reg *s))
+DEF_HELPER(void, helper_cvtsd2ss, (Reg *d, Reg *s))
+DEF_HELPER(void, helper_cvtdq2ps, (Reg *d, Reg *s))
+DEF_HELPER(void, helper_cvtdq2pd, (Reg *d, Reg *s))
+DEF_HELPER(void, helper_cvtpi2ps, (XMMReg *d, MMXReg *s))
+DEF_HELPER(void, helper_cvtpi2pd, (XMMReg *d, MMXReg *s))
+DEF_HELPER(void, helper_cvtsi2ss, (XMMReg *d, uint32_t val))
+DEF_HELPER(void, helper_cvtsi2sd, (XMMReg *d, uint32_t val))
+
+#ifdef TARGET_X86_64
+DEF_HELPER(void, helper_cvtsq2ss, (XMMReg *d, uint64_t val))
+DEF_HELPER(void, helper_cvtsq2sd, (XMMReg *d, uint64_t val))
+#endif
+
+DEF_HELPER(void, helper_cvtps2dq, (XMMReg *d, XMMReg *s))
+DEF_HELPER(void, helper_cvtpd2dq, (XMMReg *d, XMMReg *s))
+DEF_HELPER(void, helper_cvtps2pi, (MMXReg *d, XMMReg *s))
+DEF_HELPER(void, helper_cvtpd2pi, (MMXReg *d, XMMReg *s))
+DEF_HELPER(int32_t, helper_cvtss2si, (XMMReg *s))
+DEF_HELPER(int32_t, helper_cvtsd2si, (XMMReg *s))
+#ifdef TARGET_X86_64
+DEF_HELPER(int64_t, helper_cvtss2sq, (XMMReg *s))
+DEF_HELPER(int64_t, helper_cvtsd2sq, (XMMReg *s))
+#endif
+
+DEF_HELPER(void, helper_cvttps2dq, (XMMReg *d, XMMReg *s))
+DEF_HELPER(void, helper_cvttpd2dq, (XMMReg *d, XMMReg *s))
+DEF_HELPER(void, helper_cvttps2pi, (MMXReg *d, XMMReg *s))
+DEF_HELPER(void, helper_cvttpd2pi, (MMXReg *d, XMMReg *s))
+DEF_HELPER(int32_t, helper_cvttss2si, (XMMReg *s))
+DEF_HELPER(int32_t, helper_cvttsd2si, (XMMReg *s))
+#ifdef TARGET_X86_64
+DEF_HELPER(int64_t, helper_cvttss2sq, (XMMReg *s))
+DEF_HELPER(int64_t, helper_cvttsd2sq, (XMMReg *s))
+#endif
+
+DEF_HELPER(void, helper_rsqrtps, (XMMReg *d, XMMReg *s))
+DEF_HELPER(void, helper_rsqrtss, (XMMReg *d, XMMReg *s))
+DEF_HELPER(void, helper_rcpps, (XMMReg *d, XMMReg *s))
+DEF_HELPER(void, helper_rcpss, (XMMReg *d, XMMReg *s))
+DEF_HELPER(void, helper_haddps, (XMMReg *d, XMMReg *s))
+DEF_HELPER(void, helper_haddpd, (XMMReg *d, XMMReg *s))
+DEF_HELPER(void, helper_hsubps, (XMMReg *d, XMMReg *s))
+DEF_HELPER(void, helper_hsubpd, (XMMReg *d, XMMReg *s))
+DEF_HELPER(void, helper_addsubps, (XMMReg *d, XMMReg *s))
+DEF_HELPER(void, helper_addsubpd, (XMMReg *d, XMMReg *s))
+
+#define SSE_HELPER_CMP(name, F)\
+    DEF_HELPER(void, helper_ ## name ## ps , (Reg *d, Reg *s))        \
+    DEF_HELPER(void, helper_ ## name ## ss , (Reg *d, Reg *s))        \
+    DEF_HELPER(void, helper_ ## name ## pd , (Reg *d, Reg *s))        \
+    DEF_HELPER(void, helper_ ## name ## sd , (Reg *d, Reg *s))
+
+SSE_HELPER_CMP(cmpeq, FPU_CMPEQ)
+SSE_HELPER_CMP(cmplt, FPU_CMPLT)
+SSE_HELPER_CMP(cmple, FPU_CMPLE)
+SSE_HELPER_CMP(cmpunord, FPU_CMPUNORD)
+SSE_HELPER_CMP(cmpneq, FPU_CMPNEQ)
+SSE_HELPER_CMP(cmpnlt, FPU_CMPNLT)
+SSE_HELPER_CMP(cmpnle, FPU_CMPNLE)
+SSE_HELPER_CMP(cmpord, FPU_CMPORD)
+
+DEF_HELPER(void, helper_ucomiss, (Reg *d, Reg *s))
+DEF_HELPER(void, helper_comiss, (Reg *d, Reg *s))
+DEF_HELPER(void, helper_ucomisd, (Reg *d, Reg *s))
+DEF_HELPER(void, helper_comisd, (Reg *d, Reg *s))
+DEF_HELPER(uint32_t, helper_movmskps, (Reg *s))
+DEF_HELPER(uint32_t, helper_movmskpd, (Reg *s))
+#endif
+
+DEF_HELPER(uint32_t, glue(helper_pmovmskb, SUFFIX), (Reg *s))
+DEF_HELPER(void, glue(helper_packsswb, SUFFIX) , (Reg *d, Reg *s))
+DEF_HELPER(void, glue(helper_packuswb, SUFFIX) , (Reg *d, Reg *s))
+DEF_HELPER(void, glue(helper_packssdw, SUFFIX) , (Reg *d, Reg *s))
+#define UNPCK_OP(base_name, base)                               \
+    DEF_HELPER(void, glue(helper_punpck ## base_name ## bw, SUFFIX) , (Reg *d, Reg *s)) \
+    DEF_HELPER(void, glue(helper_punpck ## base_name ## wd, SUFFIX) , (Reg *d, Reg *s)) \
+    DEF_HELPER(void, glue(helper_punpck ## base_name ## dq, SUFFIX) , (Reg *d, Reg *s))
+
+UNPCK_OP(l, 0)
+UNPCK_OP(h, 1)
+
+#if SHIFT == 1
+DEF_HELPER(void, glue(helper_punpcklqdq, SUFFIX) , (Reg *d, Reg *s))
+DEF_HELPER(void, glue(helper_punpckhqdq, SUFFIX) , (Reg *d, Reg *s))
+#endif
+
+/* 3DNow! float ops */
+#if SHIFT == 0
+DEF_HELPER(void, helper_pi2fd, (MMXReg *d, MMXReg *s))
+DEF_HELPER(void, helper_pi2fw, (MMXReg *d, MMXReg *s))
+DEF_HELPER(void, helper_pf2id, (MMXReg *d, MMXReg *s))
+DEF_HELPER(void, helper_pf2iw, (MMXReg *d, MMXReg *s))
+DEF_HELPER(void, helper_pfacc, (MMXReg *d, MMXReg *s))
+DEF_HELPER(void, helper_pfadd, (MMXReg *d, MMXReg *s))
+DEF_HELPER(void, helper_pfcmpeq, (MMXReg *d, MMXReg *s))
+DEF_HELPER(void, helper_pfcmpge, (MMXReg *d, MMXReg *s))
+DEF_HELPER(void, helper_pfcmpgt, (MMXReg *d, MMXReg *s))
+DEF_HELPER(void, helper_pfmax, (MMXReg *d, MMXReg *s))
+DEF_HELPER(void, helper_pfmin, (MMXReg *d, MMXReg *s))
+DEF_HELPER(void, helper_pfmul, (MMXReg *d, MMXReg *s))
+DEF_HELPER(void, helper_pfnacc, (MMXReg *d, MMXReg *s))
+DEF_HELPER(void, helper_pfpnacc, (MMXReg *d, MMXReg *s))
+DEF_HELPER(void, helper_pfrcp, (MMXReg *d, MMXReg *s))
+DEF_HELPER(void, helper_pfrsqrt, (MMXReg *d, MMXReg *s))
+DEF_HELPER(void, helper_pfsub, (MMXReg *d, MMXReg *s))
+DEF_HELPER(void, helper_pfsubr, (MMXReg *d, MMXReg *s))
+DEF_HELPER(void, helper_pswapd, (MMXReg *d, MMXReg *s))
+#endif
+
+/* SSSE3 op helpers */
+DEF_HELPER(void, glue(helper_phaddw, SUFFIX), (Reg *d, Reg *s))
+DEF_HELPER(void, glue(helper_phaddd, SUFFIX), (Reg *d, Reg *s))
+DEF_HELPER(void, glue(helper_phaddsw, SUFFIX), (Reg *d, Reg *s))
+DEF_HELPER(void, glue(helper_phsubw, SUFFIX), (Reg *d, Reg *s))
+DEF_HELPER(void, glue(helper_phsubd, SUFFIX), (Reg *d, Reg *s))
+DEF_HELPER(void, glue(helper_phsubsw, SUFFIX), (Reg *d, Reg *s))
+DEF_HELPER(void, glue(helper_pabsb, SUFFIX), (Reg *d, Reg *s))
+DEF_HELPER(void, glue(helper_pabsw, SUFFIX), (Reg *d, Reg *s))
+DEF_HELPER(void, glue(helper_pabsd, SUFFIX), (Reg *d, Reg *s))
+DEF_HELPER(void, glue(helper_pmaddubsw, SUFFIX), (Reg *d, Reg *s))
+DEF_HELPER(void, glue(helper_pmulhrsw, SUFFIX), (Reg *d, Reg *s))
+DEF_HELPER(void, glue(helper_pshufb, SUFFIX), (Reg *d, Reg *s))
+DEF_HELPER(void, glue(helper_psignb, SUFFIX), (Reg *d, Reg *s))
+DEF_HELPER(void, glue(helper_psignw, SUFFIX), (Reg *d, Reg *s))
+DEF_HELPER(void, glue(helper_psignd, SUFFIX), (Reg *d, Reg *s))
+DEF_HELPER(void, glue(helper_palignr, SUFFIX), (Reg *d, Reg *s, int32_t shift))
+
+/* SSE4.1 op helpers */
+#if SHIFT == 1
+DEF_HELPER(void, glue(helper_pblendvb, SUFFIX), (Reg *d, Reg *s))
+DEF_HELPER(void, glue(helper_blendvps, SUFFIX), (Reg *d, Reg *s))
+DEF_HELPER(void, glue(helper_blendvpd, SUFFIX), (Reg *d, Reg *s))
+DEF_HELPER(void, glue(helper_ptest, SUFFIX), (Reg *d, Reg *s))
+DEF_HELPER(void, glue(helper_pmovsxbw, SUFFIX), (Reg *d, Reg *s))
+DEF_HELPER(void, glue(helper_pmovsxbd, SUFFIX), (Reg *d, Reg *s))
+DEF_HELPER(void, glue(helper_pmovsxbq, SUFFIX), (Reg *d, Reg *s))
+DEF_HELPER(void, glue(helper_pmovsxwd, SUFFIX), (Reg *d, Reg *s))
+DEF_HELPER(void, glue(helper_pmovsxwq, SUFFIX), (Reg *d, Reg *s))
+DEF_HELPER(void, glue(helper_pmovsxdq, SUFFIX), (Reg *d, Reg *s))
+DEF_HELPER(void, glue(helper_pmovzxbw, SUFFIX), (Reg *d, Reg *s))
+DEF_HELPER(void, glue(helper_pmovzxbd, SUFFIX), (Reg *d, Reg *s))
+DEF_HELPER(void, glue(helper_pmovzxbq, SUFFIX), (Reg *d, Reg *s))
+DEF_HELPER(void, glue(helper_pmovzxwd, SUFFIX), (Reg *d, Reg *s))
+DEF_HELPER(void, glue(helper_pmovzxwq, SUFFIX), (Reg *d, Reg *s))
+DEF_HELPER(void, glue(helper_pmovzxdq, SUFFIX), (Reg *d, Reg *s))
+DEF_HELPER(void, glue(helper_pmuldq, SUFFIX), (Reg *d, Reg *s))
+DEF_HELPER(void, glue(helper_pcmpeqq, SUFFIX), (Reg *d, Reg *s))
+DEF_HELPER(void, glue(helper_packusdw, SUFFIX), (Reg *d, Reg *s))
+DEF_HELPER(void, glue(helper_pminsb, SUFFIX), (Reg *d, Reg *s))
+DEF_HELPER(void, glue(helper_pminsd, SUFFIX), (Reg *d, Reg *s))
+DEF_HELPER(void, glue(helper_pminuw, SUFFIX), (Reg *d, Reg *s))
+DEF_HELPER(void, glue(helper_pminud, SUFFIX), (Reg *d, Reg *s))
+DEF_HELPER(void, glue(helper_pmaxsb, SUFFIX), (Reg *d, Reg *s))
+DEF_HELPER(void, glue(helper_pmaxsd, SUFFIX), (Reg *d, Reg *s))
+DEF_HELPER(void, glue(helper_pmaxuw, SUFFIX), (Reg *d, Reg *s))
+DEF_HELPER(void, glue(helper_pmaxud, SUFFIX), (Reg *d, Reg *s))
+DEF_HELPER(void, glue(helper_pmulld, SUFFIX), (Reg *d, Reg *s))
+DEF_HELPER(void, glue(helper_phminposuw, SUFFIX), (Reg *d, Reg *s))
+DEF_HELPER(void, glue(helper_roundps, SUFFIX), (Reg *d, Reg *s, uint32_t mode))
+DEF_HELPER(void, glue(helper_roundpd, SUFFIX), (Reg *d, Reg *s, uint32_t mode))
+DEF_HELPER(void, glue(helper_roundss, SUFFIX), (Reg *d, Reg *s, uint32_t mode))
+DEF_HELPER(void, glue(helper_roundsd, SUFFIX), (Reg *d, Reg *s, uint32_t mode))
+DEF_HELPER(void, glue(helper_blendps, SUFFIX), (Reg *d, Reg *s, uint32_t imm))
+DEF_HELPER(void, glue(helper_blendpd, SUFFIX), (Reg *d, Reg *s, uint32_t imm))
+DEF_HELPER(void, glue(helper_pblendw, SUFFIX), (Reg *d, Reg *s, uint32_t imm))
+DEF_HELPER(void, glue(helper_dpps, SUFFIX), (Reg *d, Reg *s, uint32_t mask))
+DEF_HELPER(void, glue(helper_dppd, SUFFIX), (Reg *d, Reg *s, uint32_t mask))
+DEF_HELPER(void, glue(helper_mpsadbw, SUFFIX), (Reg *d, Reg *s, uint32_t off))
+#endif
+
+/* SSE4.2 op helpers */
+#if SHIFT == 1
+DEF_HELPER(void, glue(helper_pcmpgtq, SUFFIX), (Reg *d, Reg *s))
+DEF_HELPER(void, glue(helper_pcmpestri, SUFFIX), (Reg *d, Reg *s, uint32_t ctl))
+DEF_HELPER(void, glue(helper_pcmpestrm, SUFFIX), (Reg *d, Reg *s, uint32_t ctl))
+DEF_HELPER(void, glue(helper_pcmpistri, SUFFIX), (Reg *d, Reg *s, uint32_t ctl))
+DEF_HELPER(void, glue(helper_pcmpistrm, SUFFIX), (Reg *d, Reg *s, uint32_t ctl))
+DEF_HELPER(target_ulong, helper_crc32,
+                (uint32_t crc1, target_ulong msg, uint32_t len))
+DEF_HELPER(target_ulong, helper_popcnt, (target_ulong n, uint32_t type))
+#endif
+
+#undef SHIFT
+#undef Reg
+#undef SUFFIX
+
+#undef SSE_HELPER_B
+#undef SSE_HELPER_W
+#undef SSE_HELPER_L
+#undef SSE_HELPER_Q
+#undef SSE_HELPER_S
+#undef SSE_HELPER_CMP
+#undef UNPCK_OP
