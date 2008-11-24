7465b5ea691f4af0837fc460305230985e98822b
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2008-11-24 03:10:33 +0000

    PGMR0DynMap: Re-enabled the code.
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@14504 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/VMM/VMMR0/PGMR0DynMap.cpp
+++ src/VBox/VMM/VMMR0/PGMR0DynMap.cpp
@@ -313,9 +313,8 @@ VMMR0DECL(void) PGMR0DynMapTerm(void)
  */
 VMMR0DECL(int) PGMR0DynMapInitVM(PVM pVM)
 {
-#ifndef DEBUG_bird
-    return VINF_SUCCESS;
-#else
+    AssertMsgReturn(!pVM->pgm.s.pvR0DynMapUsed, ("%p (pThis=%p)\n", pVM->pgm.s.pvR0DynMapUsed, g_pPGMR0DynMap), VERR_WRONG_ORDER);
+
     /*
      * Initialize the auto sets.
      */
@@ -336,8 +335,6 @@ VMMR0DECL(int) PGMR0DynMapInitVM(PVM pVM)
     /*
      * Do we need the cache? Skip the last bit if we don't.
      */
-    Assert(!pVM->pgm.s.pvR0DynMapUsed);
-    pVM->pgm.s.pvR0DynMapUsed = NULL;
     if (!HWACCMIsEnabled(pVM))
         return VINF_SUCCESS;
 
@@ -362,7 +359,6 @@ VMMR0DECL(int) PGMR0DynMapInitVM(PVM pVM)
     RTSemFastMutexRelease(pThis->hInitLock);
 
     return rc;
-#endif
 }
 
 
@@ -373,7 +369,6 @@ VMMR0DECL(int) PGMR0DynMapInitVM(PVM pVM)
  */
 VMMR0DECL(void) PGMR0DynMapTermVM(PVM pVM)
 {
-#ifdef DEBUG_bird
     /*
      * Return immediately if we're not using the cache.
      */
@@ -441,7 +436,6 @@ VMMR0DECL(void) PGMR0DynMapTermVM(PVM pVM)
         AssertLogRelMsgFailed(("pvR0DynMapUsed=%p pThis=%p\n", pVM->pgm.s.pvR0DynMapUsed, pThis));
 
     RTSemFastMutexRelease(pThis->hInitLock);
-#endif
 }
 
 
