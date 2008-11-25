2ccccf3f5b55eff242ac7d42904c9d5428aec57e
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2008-11-25 11:13:26 +0000

    Frontends/VBoxManage: Fix machinereadable variant of SATA info dumping. Convention is to use the same string as for the "VBoxManage moddifyvm" parameter if possible.
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@14564 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Frontends/VBoxManage/VBoxManageInfo.cpp
+++ src/VBox/Frontends/VBoxManage/VBoxManageInfo.cpp
@@ -485,8 +485,8 @@ HRESULT showVMInfo (ComPtr <IVirtualBox> virtualBox, ComPtr<IMachine> machine,
                 hardDisk->COMGETTER(Id)(uuid.asOutParam());
                 if (details == VMINFO_MACHINEREADABLE)
                 {
-                    RTPrintf("sata%d=\"%lS\"\n", i, filePath.raw());
-                    RTPrintf("sata%dImageUUID=\"%s\"\n", i, uuid.toString().raw());
+                    RTPrintf("sataport%d=\"%lS\"\n", i, filePath.raw());
+                    RTPrintf("SataPortImageUUID%d=\"%s\"\n", i, uuid.toString().raw());
                 }
                 else
                     RTPrintf("SATA %d:          %lS (UUID: %s)\n", i, filePath.raw(), uuid.toString().raw());
