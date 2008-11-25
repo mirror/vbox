594d855ab14f50a73980c9ad7cf0f81ee16d31cc
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2008-11-25 22:12:10 +0000

    @todo
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@14608 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/VMM/VMMR0/VMMR0.cpp
+++ src/VBox/VMM/VMMR0/VMMR0.cpp
@@ -260,6 +260,9 @@ static int vmmR0InitVM(PVM pVM, uint32_t uSvnRev)
  */
 static int vmmR0TermVM(PVM pVM)
 {
+/** @todo @bugref{1865,3202}: Make these tail onto the VM object destruction
+ *        to make sure they are *always* executed and don't leave mess behind
+ *        when the process is killed. */
 #ifdef VBOX_WITH_2X_4GB_ADDR_SPACE
     PGMR0DynMapTermVM(pVM);
 #endif
