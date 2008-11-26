f2f1cccd2aa505714c57d7b61c373ef4df92e1a6
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2008-11-26 14:09:41 +0000

    SUP, VMM: Moved the max alloc/map page count to VBox/param.h
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@14645 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/VMM/MMHyper.cpp
+++ src/VBox/VMM/MMHyper.cpp
@@ -536,8 +536,8 @@ VMMR3DECL(int) MMR3HyperMapPages(PVM pVM, void *pvR3, RTR0PTR pvR0, size_t cPage
      */
     AssertPtrReturn(pvR3, VERR_INVALID_POINTER);
     AssertPtrReturn(paPages, VERR_INVALID_POINTER);
-    AssertReturn(cPages > 0, VERR_INVALID_PARAMETER);
-    AssertReturn(cPages < 128 * _1M / PAGE_SIZE, VERR_INVALID_PARAMETER);
+    AssertReturn(cPages > 0, VERR_PAGE_COUNT_OUT_OF_RANGE);
+    AssertReturn(cPages <= VBOX_MAX_ALLOC_PAGE_COUNT, VERR_PAGE_COUNT_OUT_OF_RANGE);
     AssertPtrReturn(pszDesc, VERR_INVALID_POINTER);
     AssertReturn(*pszDesc, VERR_INVALID_PARAMETER);
     AssertPtrReturn(pGCPtr, VERR_INVALID_PARAMETER);
