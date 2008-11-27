3c618735005ffcd3caca5cf3d7dd6f15b8b055e4
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2008-11-27 02:09:04 +0000

    pgmPoolGetPage: assertion text.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@14682 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/VMM/PGMInternal.h
+++ src/VBox/VMM/PGMInternal.h
@@ -4363,7 +4363,7 @@ DECLINLINE(PPGMPOOLPAGE) pgmPoolGetPage(PPGMPOOL pPool, RTHCPHYS HCPhys)
      * Look up the page.
      */
     PPGMPOOLPAGE pPage = (PPGMPOOLPAGE)RTAvloHCPhysGet(&pPool->HCPhysTree, HCPhys & X86_PTE_PAE_PG_MASK);
-    AssertFatalMsg(pPage && pPage->enmKind != PGMPOOLKIND_FREE, ("HCPhys=%RHp pPage=%p type=%d\n", HCPhys, pPage, (pPage) ? pPage->enmKind : 0));
+    AssertFatalMsg(pPage && pPage->enmKind != PGMPOOLKIND_FREE, ("HCPhys=%RHp pPage=%p idx=%d\n", HCPhys, pPage, (pPage) ? pPage->idx : 0));
     return pPage;
 }
 
