c60b8a7acb032f00e188632a1db51e66da761301
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2008-11-27 15:16:52 +0000

    Devices/USB and Main: use hal and sysfs for Linux USB (currently disabled)
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@14711 cfe28804-0f27-0410-a406-dd0f0b0b656f

---
+++ src/VBox/Main/testcase/tstUSBLinux.h
@@ -0,0 +1,56 @@
+/* $Id $ */
+/** @file
+ * VirtualBox USB Proxy Service class, test version for Linux hosts.
+ */
+
+/*
+ * Copyright (C) 2008 Sun Microsystems, Inc.
+ *
+ * Sun Microsystems, Inc. confidential
+ * All rights reserved
+ */
+
+
+#ifndef ____H_TSTUSBLINUX
+#define ____H_TSTUSBLINUX
+
+typedef int HRESULT;
+enum { S_OK = 0, E_NOTIMPL = 1 };
+
+#include <VBox/usb.h>
+#include <VBox/usbfilter.h>
+
+#include <VBox/err.h>
+
+#ifdef VBOX_USB_WITH_SYSFS
+# include "libhal.h"
+#endif
+
+#include <stdio.h>
+/**
+ * The Linux hosted USB Proxy Service.
+ */
+class USBProxyServiceLinux
+{
+public:
+    USBProxyServiceLinux() : mLastError(VINF_SUCCESS) {}
+    HRESULT initSysfs(void);
+    PUSBDEVICE getDevicesFromSysfs(void);
+    int getLastError(void) { return mLastError; }
+
+private:
+    int start(void) { return VINF_SUCCESS; }
+    static void freeDevice(PUSBDEVICE) {}  /* We don't care about leaks in a test. */
+    int usbProbeInterfacesFromLibhal(const char *pszHalUuid, PUSBDEVICE pDev);
+    int mLastError;
+#  ifdef VBOX_USB_WITH_SYSFS
+    /** Our connection to DBus for getting information from hal.  This will be
+     * NULL if the initialisation failed. */
+    DBusConnection *mDBusConnection;
+    /** Handle to libhal. */
+    LibHalContext *mLibHalContext;
+#  endif
+};
+
+#endif /* !____H_TSTUSBLINUX */
+
