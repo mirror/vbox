3defe4a3964d8c0d3252d0b8548314fdd8116571
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2008-11-28 16:30:57 +0000

    #3285: Improve error handling API to include unique error numbers
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@14791 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Main/ConsoleImpl.cpp
+++ src/VBox/Main/ConsoleImpl.cpp
@@ -1871,7 +1871,7 @@ STDMETHODIMP Console::AdoptSavedState (INPTR BSTR aSavedStateFile)
 
     if (mMachineState != MachineState_PoweredOff &&
         mMachineState != MachineState_Aborted)
-        return setError (E_FAIL,
+        return setError (VBOX_E_INVALID_VM_STATE,
             tr ("Cannot adopt the saved machine state as the machine is "
                 "not in Powered Off or Aborted state (machine state: %d)"),
             mMachineState);
@@ -1887,7 +1887,7 @@ STDMETHODIMP Console::DiscardSavedState()
     AutoWriteLock alock (this);
 
     if (mMachineState != MachineState_Saved)
-        return setError (E_FAIL,
+        return setError (VBOX_E_INVALID_VM_STATE,
             tr ("Cannot discard the machine state as the machine is "
                 "not in the saved state (machine state: %d)"),
             mMachineState);
