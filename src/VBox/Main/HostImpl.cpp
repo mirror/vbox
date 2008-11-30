7d1e844371c206cb42d385acbba13e870d0ede2b
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2008-11-30 00:24:20 +0000

    HostImpl.cpp: asm.h
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@14819 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Main/HostImpl.cpp
+++ src/VBox/Main/HostImpl.cpp
@@ -109,6 +109,7 @@ extern "C" char *getfullrawname(char *);
 
 #include <VBox/usb.h>
 #include <VBox/x86.h>
+#include <VBox/asm.h>
 #include <VBox/err.h>
 #include <iprt/string.h>
 #include <iprt/mp.h>
