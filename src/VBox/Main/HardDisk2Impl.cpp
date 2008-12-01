7a8780040c3ee7aadccf042f89e518712b6c196d
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2008-12-01 09:40:05 +0000

    s/VBOX_E_OBJECT_IN_USE/VBOX_E_INVALID_OBJECT_STATE/g as suggested by dmik.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@14842 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Main/HardDisk2Impl.cpp
+++ src/VBox/Main/HardDisk2Impl.cpp
@@ -1860,7 +1860,7 @@ HRESULT HardDisk2::deleteStorage (ComObjPtr <Progress> *aProgress, bool aWait)
     }
 
     if (m.backRefs.size() != 0)
-        return setError (VBOX_E_OBJECT_IN_USE,
+        return setError (VBOX_E_INVALID_OBJECT_STATE,
             tr ("Hard disk '%ls' is attached to %d virtual machines"),
                 m.locationFull.raw(), m.backRefs.size());
 
@@ -2015,7 +2015,7 @@ HRESULT HardDisk2::createDiffStorage (ComObjPtr <HardDisk2> &aTarget,
 
             if (it->snapshotIds.size() == 0)
             {
-                return setError (VBOX_E_OBJECT_IN_USE,
+                return setError (VBOX_E_INVALID_OBJECT_STATE,
                     tr ("Hard disk '%ls' is attached to a virtual machine "
                         "with UUID {%RTuuid}. No differencing hard disks "
                         "based on it may be created until it is detached"),
