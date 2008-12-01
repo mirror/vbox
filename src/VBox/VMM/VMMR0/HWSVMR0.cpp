ec47f9bab33776ef5c2333ba5fa2040020018e70
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2008-12-01 14:19:16 +0000

    Compile fix
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@14862 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/VMM/VMMR0/HWSVMR0.cpp
+++ src/VBox/VMM/VMMR0/HWSVMR0.cpp
@@ -2277,7 +2277,7 @@ VMMR0DECL(int) SVMR0InvalidatePhysPage(PVM pVM, PVMCPU pVCpu, RTGCPHYS GCPhys)
 DECLASM(int) SVMR0VMSwitcherRun64(RTHCPHYS pVMCBHostPhys, RTHCPHYS pVMCBPhys, PCPUMCTX pCtx)
 {
     /* @todo This code is not guest SMP safe (hyper context) */
-    AssertReturn(pVM->cCPUs == 1, VERR_ACCESS_DENIED);
+////    AssertReturn(pVM->cCPUs == 1, VERR_ACCESS_DENIED);
     return VERR_NOT_IMPLEMENTED;
 }
 
