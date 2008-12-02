c3d573cc817a7f3ec8adc132a5f0e6b97bd31077
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2008-12-02 09:29:48 +0000

    Compile fix
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@14884 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/VMM/PATM/PATMSSM.cpp
+++ src/VBox/VMM/PATM/PATMSSM.cpp
@@ -26,6 +26,7 @@
 *******************************************************************************/
 #define LOG_GROUP LOG_GROUP_PATM
 #include <VBox/patm.h>
+#include <VBox/hwaccm.h>
 #include <VBox/stam.h>
 #include <VBox/pgm.h>
 #include <VBox/cpum.h>
