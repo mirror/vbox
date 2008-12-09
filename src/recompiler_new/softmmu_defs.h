3b6d4495c301e42f91add66ed46c2cfa4358410d
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2008-12-09 14:14:24 +0000

    oops
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@15175 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/recompiler_new/softmmu_defs.h
+++ src/recompiler_new/softmmu_defs.h
@@ -37,6 +37,21 @@ RTCCUINTREG REGPARM __ldl_cmmu(target_ulong addr, int mmu_idx);
 void REGPARM __stl_cmmu(target_ulong addr, uint32_t val, int mmu_idx);
 uint64_t REGPARM __ldq_cmmu(target_ulong addr, int mmu_idx);
 void REGPARM __stq_cmmu(target_ulong addr, uint64_t val, int mmu_idx);
+
+#ifdef REM_PHYS_ADDR_IN_TLB
+RTCCUINTREG REGPARM __ldb_vbox_phys(RTCCUINTREG addr);
+RTCCUINTREG REGPARM __ldub_vbox_phys(RTCCUINTREG addr);
+void REGPARM __stb_vbox_phys(RTCCUINTREG addr, RTCCUINTREG val);
+RTCCUINTREG REGPARM __ldw_vbox_phys(RTCCUINTREG addr);
+RTCCUINTREG REGPARM __lduw_vbox_phys(RTCCUINTREG addr);
+void REGPARM __stw_vbox_phys(RTCCUINTREG addr, RTCCUINTREG val);
+RTCCUINTREG REGPARM __ldl_vbox_phys(RTCCUINTREG addr);
+RTCCUINTREG REGPARM __ldul_vbox_phys(RTCCUINTREG addr);
+void REGPARM __stl_vbox_phys(RTCCUINTREG addr, RTCCUINTREG val);
+uint64_t REGPARM __ldq_vbox_phys(RTCCUINTREG addr);
+void REGPARM __stq_vbox_phys(RTCCUINTREG addr, uint64_t val);
+#endif
+
 #endif
 
 #endif
