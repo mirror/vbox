fef4258a2be84ff1a12f896f93f9c62b8cae1b35
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2008-12-09 17:12:22 +0000

    slirp:coding style
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@15195 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Devices/Network/slirp/slirp_state.h
+++ src/VBox/Devices/Network/slirp/slirp_state.h
@@ -136,7 +136,7 @@ typedef struct NATState
 #ifdef VBOX_WITH_SLIRP_ICMP
     struct socket icmp_socket;
     struct icmp_storage icmp_msg_head;
-#ifdef RT_OS_WINDOWS
+# ifdef RT_OS_WINDOWS
     void *pvIcmpBuffer;
     size_t szIcmpBuffer;
     /* Accordin MSDN specification IcmpParseReplies 
@@ -145,7 +145,7 @@ typedef struct NATState
     long (WINAPI * pfIcmpParseReplies)(void *, long);
     BOOL (WINAPI * pfIcmpCloseHandle)(HANDLE);
     HMODULE hmIcmpLibrary;
-#endif
+# endif
 #endif
 #if defined(VBOX_WITH_SIMPLIFIED_SLIRP_SYNC) && defined(RT_OS_WINDOWS)
 # define VBOX_SOCKET_EVENT (pData->phEvents[VBOX_SOCKET_EVENT_INDEX])
