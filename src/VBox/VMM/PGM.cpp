5ccff10d1ddac4e12a3ed91979e8650f0747ce80
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2008-12-09 14:53:53 +0000

    Argh
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@15182 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/VMM/PGM.cpp
+++ src/VBox/VMM/PGM.cpp
@@ -2918,7 +2918,7 @@ static int pgmR3ModeDataInit(PVM pVM, bool fResolveGCAndR0)
         break;
 #endif /* HC_ARCH_BITS != 32 */
 
-#if HC_ARCH_BITS == 64 || RT_OS_DARWIN
+#if HC_ARCH_BITS == 64 || defined(RT_OS_DARWIN)
     case SUPPAGINGMODE_AMD64:
     case SUPPAGINGMODE_AMD64_GLOBAL:
     case SUPPAGINGMODE_AMD64_NX:
