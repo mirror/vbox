0f48f8776c2b44caa682e3e021b0b6f19314b343
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2008-12-10 09:29:07 +0000

    #3282: API, RT generic part, updated VBoxManage list hostif.
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@15235 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Main/HostNetworkInterfaceImpl.cpp
+++ src/VBox/Main/HostNetworkInterfaceImpl.cpp
@@ -70,6 +70,40 @@ HRESULT HostNetworkInterface::init (Bstr aInterfaceName, Guid aGuid)
     return S_OK;
 }
 
+#ifdef VBOX_WITH_HOSTNETIF_API
+/**
+ * Initializes the host object.
+ *
+ * @returns COM result indicator
+ * @param   aInterfaceName name of the network interface
+ * @param   aGuid GUID of the host network interface
+ */
+HRESULT HostNetworkInterface::init (PRTNETIFINFO pIf)
+{
+//    LogFlowThisFunc (("aInterfaceName={%ls}, aGuid={%s}\n",
+//                      aInterfaceName.raw(), aGuid.toString().raw()));
+
+//    ComAssertRet (aInterfaceName, E_INVALIDARG);
+//    ComAssertRet (!aGuid.isEmpty(), E_INVALIDARG);
+    ComAssertRet (!pIf, E_INVALIDARG);
+
+    /* Enclose the state transition NotReady->InInit->Ready */
+    AutoInitSpan autoInitSpan (this);
+    AssertReturn (autoInitSpan.isOk(), E_FAIL);
+
+    unconst (mInterfaceName) = Bstr(pIf->szName);
+    unconst (mGuid) = pIf->Uuid;
+//    m.IPAddress = pIf->IPAddress;
+//    m.networkMask = pIf->IPNetMask;
+//    m.IPv6Address = composeIPv6Address(pIf->IPv6Address, pIf->IPv6NetMask);
+
+    /* Confirm a successful initialization */
+    autoInitSpan.setSucceeded();
+
+    return S_OK;
+}
+#endif
+
 // IHostNetworkInterface properties
 /////////////////////////////////////////////////////////////////////////////
 
@@ -108,4 +142,114 @@ STDMETHODIMP HostNetworkInterface::COMGETTER(Id) (OUT_GUID aGuid)
 
     return S_OK;
 }
+
+
+/**
+ * Returns the IP address of the host network interface.
+ *
+ * @returns COM status code
+ * @param   aIPAddress address of result pointer
+ */
+STDMETHODIMP HostNetworkInterface::COMGETTER(IPAddress) (ULONG *aIPAddress)
+{
+    CheckComArgOutPointerValid(aIPAddress);
+
+    AutoCaller autoCaller (this);
+    CheckComRCReturnRC (autoCaller.rc());
+
+    *aIPAddress = m.IPAddress;
+
+    return S_OK;
+}
+
+/**
+ * Returns the netwok mask of the host network interface.
+ *
+ * @returns COM status code
+ * @param   aNetworkMask address of result pointer
+ */
+STDMETHODIMP HostNetworkInterface::COMGETTER(NetworkMask) (ULONG *aNetworkMask)
+{
+    CheckComArgOutPointerValid(aNetworkMask);
+
+    AutoCaller autoCaller (this);
+    CheckComRCReturnRC (autoCaller.rc());
+
+    *aNetworkMask = m.networkMask;
+
+    return S_OK;
+}
+
+/**
+ * Returns the IP V6 address of the host network interface.
+ *
+ * @returns COM status code
+ * @param   aIPV6Address address of result pointer
+ */
+STDMETHODIMP HostNetworkInterface::COMGETTER(IPV6Address) (BSTR *aIPV6Address)
+{
+    CheckComArgOutPointerValid(aIPV6Address);
+
+    AutoCaller autoCaller (this);
+    CheckComRCReturnRC (autoCaller.rc());
+
+    m.IPV6Address.cloneTo (aIPV6Address);
+
+    return S_OK;
+}
+
+/**
+ * Returns the hardware address of the host network interface.
+ *
+ * @returns COM status code
+ * @param   aHardwareAddress address of result pointer
+ */
+STDMETHODIMP HostNetworkInterface::COMGETTER(HardwareAddress) (BSTR *aHardwareAddress)
+{
+    CheckComArgOutPointerValid(aHardwareAddress);
+
+    AutoCaller autoCaller (this);
+    CheckComRCReturnRC (autoCaller.rc());
+
+    m.hardwareAddress.cloneTo (aHardwareAddress);
+
+    return S_OK;
+}
+
+/**
+ * Returns the encapsulation protocol type of the host network interface.
+ *
+ * @returns COM status code
+ * @param   aType address of result pointer
+ */
+STDMETHODIMP HostNetworkInterface::COMGETTER(Type) (HostNetworkInterfaceType_T *aType)
+{
+    CheckComArgOutPointerValid(aType);
+
+    AutoCaller autoCaller (this);
+    CheckComRCReturnRC (autoCaller.rc());
+
+    *aType = m.type;
+
+    return S_OK;
+}
+
+/**
+ * Returns the current state of the host network interface.
+ *
+ * @returns COM status code
+ * @param   aStatus address of result pointer
+ */
+STDMETHODIMP HostNetworkInterface::COMGETTER(Status) (HostNetworkInterfaceStatus_T *aStatus)
+{
+    CheckComArgOutPointerValid(aStatus);
+
+    AutoCaller autoCaller (this);
+    CheckComRCReturnRC (autoCaller.rc());
+
+    *aStatus = m.status;
+
+    return S_OK;
+}
+
 /* vi: set tabstop=4 shiftwidth=4 expandtab: */
