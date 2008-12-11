36e39e071234ee87d0c77a200944a46e79642828
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2008-12-11 15:31:29 +0000

    REM: implement config check heuristic, to select which REM to load
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@15305 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Main/ConsoleImpl2.cpp
+++ src/VBox/Main/ConsoleImpl2.cpp
@@ -216,6 +216,27 @@ DECLCALLBACK(int) Console::configConstructor(PVM pVM, void *pvConsole)
         rc = CFGMR3InsertInteger(pHWVirtExt, "Enabled",     1);                     RC_CHECK();
     }
 
+    /* Check REM flavour we wish to use with this VM. Now we assume 64-bit guests 
+       can be only enabled if hardware acceleration is used. */
+    if (fHWVirtExEnabled)
+    {
+        PRBool fSupportsLongMode = false, fIs64BitGuest = false; 
+        ComPtr<IGuest> guest;
+        ComPtr <IGuestOSType> guestOSType;
+        Bstr osTypeId;
+        rc = pMachine->COMGETTER(OSTypeId)(osTypeId.asOutParam());                  RC_CHECK();
+        rc = virtualBox->COMGETTER(GuestOSType) (osTypeId, 
+                                                 guestOSType.asOutParam());         RC_CHECK();
+        rc = host->COMGETTER(ProcessorFeature) (ProcessorFeature_LongMode, 
+                                                &fSupportsLongMode);                RC_CHECK();
+        rc = guestOSType->COMGETTER(Is64Bit) (&fIs64BitGuest);                      RC_CHECK();
+        
+        if (fSupportsLongMode && fIs64BitGuest)
+        {
+            rc = CFGMR3InsertInteger(pRoot, "Rem64Enabled",     1);                 RC_CHECK();
+        }
+    }
+
     /* Nested paging (VT-x/AMD-V) */
     BOOL fEnableNestedPaging = false;
     hrc = pMachine->COMGETTER(HWVirtExNestedPagingEnabled)(&fEnableNestedPaging);   H();
