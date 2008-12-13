40c8c73bd08a93ad812f8117fb210da3167a70ed
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2008-12-13 07:01:33 +0000

    EMAll: Corrected PGMVerifyAccess call in the STOSDW emulation to pass the correct X86_PTE_US flag. Vista64 took a lot of REMR3EmulateInstruction fallbacks because of this.
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@15419 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/VMM/VMMAll/EMAll.cpp
+++ src/VBox/VMM/VMMAll/EMAll.cpp
@@ -1357,7 +1357,9 @@ static int emInterpretStosWD(PVM pVM, PDISCPUSTATE pCpu, PCPUMCTXCORE pRegFrame,
 
         LogFlow(("emInterpretStosWD dest=%04X:%RGv (%RGv) cbSize=%d cTransfers=%x DF=%d\n", pRegFrame->es, GCOffset, GCDest, cbSize, cTransfers, pRegFrame->eflags.Bits.u1DF));
         /* Access verification first; we currently can't recover properly from traps inside this instruction */
-        rc = PGMVerifyAccess(pVM, GCDest - ((offIncrement > 0) ? 0 : ((cTransfers-1) * cbSize)), cTransfers * cbSize, X86_PTE_RW | X86_PTE_US);
+        rc = PGMVerifyAccess(pVM, GCDest - ((offIncrement > 0) ? 0 : ((cTransfers-1) * cbSize)),
+                             cTransfers * cbSize,
+                             X86_PTE_RW | (CPUMGetGuestCPL(pVM, pRegFrame) == 3 ? X86_PTE_US : 0));
         if (rc != VINF_SUCCESS)
         {
             Log(("STOSWD will generate a trap -> recompiler, rc=%d\n", rc));
