e0fe7a118d5e6349923d66153ea1182b54507d06
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2008-12-13 12:08:04 +0000

    slirp:icmp: get rid of unused code
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@15437 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Devices/Network/slirp/socket.c
+++ src/VBox/Devices/Network/slirp/socket.c
@@ -849,12 +849,6 @@ send_icmp_to_guest(PNATState pData, char *buff, size_t len, struct socket *so, c
         ip->ip_tos = ((ip->ip_tos & 0x1E) | 0xC0);  /* high priority for errors */
     }
 
-    /* the low level expects fields to be in host format so let's convert them*/
-#if 0 /* need check on linux */
-    NTOHS(ip->ip_len);
-    NTOHS(ip->ip_off);
-    NTOHS(ip->ip_id);
-#endif
     ip->ip_src.s_addr = src;
     ip->ip_dst.s_addr = dst;
     icmp_reflect(pData, m);
