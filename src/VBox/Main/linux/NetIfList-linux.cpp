8d18e914b926d55d93032f6035e0486a3f6cd77d
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2008-12-14 13:03:29 +0000

    #3282 HostNetIf API: 32-bit Linux is operational.
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@15462 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Main/linux/NetIfList-linux.cpp
+++ src/VBox/Main/linux/NetIfList-linux.cpp
@@ -37,12 +37,88 @@
 
 #include <iprt/err.h>
 #include <list>
+#include <sys/ioctl.h>
+#include <net/if.h>
+#include <net/if_arp.h>
+#include <netinet/in.h>
 
 #include "HostNetworkInterfaceImpl.h"
 #include "netif.h"
 
 int NetIfList(std::list <ComObjPtr <HostNetworkInterface> > &list)
 {
-    return VERR_NOT_IMPLEMENTED;
+    int sock = socket(AF_INET, SOCK_DGRAM, 0);
+    if (sock >= 0)
+    {
+        char pBuffer[2048];
+        struct ifconf ifConf;
+        ifConf.ifc_len = sizeof(pBuffer);
+        ifConf.ifc_buf = pBuffer;
+        if (ioctl(sock, SIOCGIFCONF, &ifConf) >= 0)
+        {
+            for (struct ifreq *pReq = ifConf.ifc_req; (char*)pReq < pBuffer + ifConf.ifc_len; pReq++)
+            {
+                if (ioctl(sock, SIOCGIFHWADDR, pReq) >= 0)
+                {
+                    if (pReq->ifr_hwaddr.sa_family == ARPHRD_ETHER)
+                    {
+                        NETIFINFO Info;
+                        memset(&Info, 0, sizeof(Info));
+                        Info.enmType = NETIF_T_ETHERNET;
+                        /* Pick up some garbage from stack. */
+                        RTUUID uuid;
+                        Assert(sizeof(uuid) <= sizeof(*pReq));
+                        memcpy(uuid.Gen.au8Node, &pReq->ifr_hwaddr.sa_data, sizeof(uuid.Gen.au8Node));
+                        Info.Uuid = uuid;
+                        memcpy(&Info.MACAddress, pReq->ifr_hwaddr.sa_data, sizeof(Info.MACAddress));
+
+                        if (ioctl(sock, SIOCGIFADDR, pReq) >= 0)
+                            memcpy(Info.IPAddress.au8,
+                                   &((struct sockaddr_in *)&pReq->ifr_addr)->sin_addr.s_addr,
+                                   sizeof(Info.IPAddress.au8));
+
+                        if (ioctl(sock, SIOCGIFNETMASK, pReq) >= 0)
+                            memcpy(Info.IPNetMask.au8,
+                                   &((struct sockaddr_in *)&pReq->ifr_addr)->sin_addr.s_addr,
+                                   sizeof(Info.IPNetMask.au8));
+
+                        if (ioctl(sock, SIOCGIFFLAGS, pReq) >= 0)
+                            Info.enmStatus = pReq->ifr_flags & IFF_UP ? NETIF_S_UP : NETIF_S_DOWN;
+
+                        FILE *fp = fopen("/proc/net/if_inet6", "r");
+                        if (fp)
+                        {
+                            RTNETADDRIPV6 IPv6Address;
+                            unsigned uIndex, uLength, uScope, uTmp;
+                            char szName[30];
+                            while (fscanf(fp,
+                                          "%08x%08x%08x%08x"
+                                          " %02x %02x %02x %02x %20s\n",
+                                          &IPv6Address.au32[0], &IPv6Address.au32[1],
+                                          &IPv6Address.au32[2], &IPv6Address.au32[3],
+                                          &uIndex, &uLength, &uScope, &uTmp, szName) != EOF)
+                            {
+                                if (!strcmp(pReq->ifr_name, szName))
+                                {
+                                    Info.IPv6Address.au32[0] = htonl(IPv6Address.au32[0]);
+                                    Info.IPv6Address.au32[1] = htonl(IPv6Address.au32[1]);
+                                    Info.IPv6Address.au32[2] = htonl(IPv6Address.au32[2]);
+                                    Info.IPv6Address.au32[3] = htonl(IPv6Address.au32[3]);
+                                    ASMBitSetRange(&Info.IPv6NetMask, 0, uLength);
+                                }
+                            }
+                            fclose(fp);
+                        }
+                        ComObjPtr<HostNetworkInterface> IfObj;
+                        IfObj.createObject();
+                        if (SUCCEEDED(IfObj->init(Bstr(pReq->ifr_name), &Info)))
+                            list.push_back(IfObj);
+                    }
+                }
+            }
+        }
+        close(sock);
+    }
+    return VINF_SUCCESS;
 }
 
