451eb4a9cc297f32a9636d4af064ddc0082c6578
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2008-12-15 11:24:46 +0000

    warning
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@15494 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Main/MachineImpl.cpp
+++ src/VBox/Main/MachineImpl.cpp
@@ -9735,7 +9735,7 @@ void SessionMachine::discardSnapshotHandler (DiscardSnapshotTask &aTask)
                  * because it will be going to delete the child) */
 
                 /* The below assert would be nice but I don't want to move
-                 * HardDisk2::MergeChain to the header just for that
+                 * HardDisk2::MergeChain to the header just for that */
                  * Assert (!chain->isForward()); */
 
                 Assert (hd->children().size() == 1);
