d8c52eded0e02327a0827894d78004875922beb4
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2008-12-15 09:09:10 +0000

    Storage/iSCSI: fix iSCSI over IntNet
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@15484 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Main/HardDisk2Impl.cpp
+++ src/VBox/Main/HardDisk2Impl.cpp
@@ -1319,6 +1319,14 @@ STDMETHODIMP HardDisk2::FlattenTo (IHardDisk2 *aTarget, IProgress **aProgress)
     ReturnComNotImplemented();
 }
 
+STDMETHODIMP HardDisk2::Compact (IProgress **aProgress)
+{
+    AutoCaller autoCaller (this);
+    CheckComRCReturnRC (autoCaller.rc());
+
+    ReturnComNotImplemented();
+}
+
 // public methods for internal purposes only
 ////////////////////////////////////////////////////////////////////////////////
 
