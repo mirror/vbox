734dad5a236495c2647ae1348142385c8fe6614d
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2008-12-15 16:55:01 +0000

    Don't trigger unnecessary assertions
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@15519 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/VMM/VMMGC/HWACCMGCA.asm
+++ src/VBox/VMM/VMMGC/HWACCMGCA.asm
@@ -344,6 +344,12 @@ ALIGN(16)
 
 %ifdef VMX_USE_CACHED_VMCS_ACCESSES
     pop     rdi         ; pCache
+
+%ifdef DEBUG
+    mov     [rdi + VMCSCACHE.TestOut.pCache], rdi
+    mov     [rdi + VMCSCACHE.TestOut.pCtx], rsi
+%endif
+
 %endif
 
     ; Restore segment registers
@@ -358,6 +364,12 @@ ALIGN(16)
 
 %ifdef VMX_USE_CACHED_VMCS_ACCESSES
     pop     rdi         ; pCache
+
+%ifdef DEBUG
+    mov     [rdi + VMCSCACHE.TestOut.pCache], rdi
+    mov     [rdi + VMCSCACHE.TestOut.pCtx], rsi
+%endif
+
 %endif
 
     ; Restore segment registers
