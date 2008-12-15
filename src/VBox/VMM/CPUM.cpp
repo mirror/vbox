e9863fb9ef763f1200e3a6e18cbd57a776b24746
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2008-12-15 13:26:31 +0000

    SSM, CPUM: Moved the GCPtr/GCPhys sizes into the file header instead of out sourcing it to CPUM, bumped header version (warning: not forward compatible). Assumes 32-bit RTGCPTR on 32-bit hosts using the old header, this will conflict with saved states from the last few days because of 64-bit guest on 32-bit hosts changes but there is no way around that. (64-bit hosts are not affected by this latter issue as they've been using 64-bit guest pointers since 2.0.)
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@15503 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/VMM/CPUM.cpp
+++ src/VBox/VMM/CPUM.cpp
@@ -772,9 +772,6 @@ VMMR3DECL(void) CPUMR3Reset(PVM pVM)
  */
 static DECLCALLBACK(int) cpumR3Save(PVM pVM, PSSMHANDLE pSSM)
 {
-    /* Set the size of RTGCPTR for use of SSMR3Get/PutGCPtr. */
-    SSMR3SetGCPtrSize(pSSM, sizeof(RTGCPTR));
-
     /*
      * Save.
      */
@@ -928,8 +925,8 @@ static DECLCALLBACK(int) cpumR3Load(PVM pVM, PSSMHANDLE pSSM, uint32_t u32Versio
     /* Set the size of RTGCPTR for SSMR3GetGCPtr. */
     if (u32Version == CPUM_SAVED_STATE_VERSION_VER1_6)
         SSMR3SetGCPtrSize(pSSM, sizeof(RTGCPTR32));
-    else
-        SSMR3SetGCPtrSize(pSSM, sizeof(RTGCPTR));
+    else if (u32Version <= CPUM_SAVED_STATE_VERSION)
+        SSMR3SetGCPtrSize(pSSM, HC_ARCH_BITS == 32 ? sizeof(RTGCPTR32) : sizeof(RTGCPTR));
 
     /*
      * Restore.
