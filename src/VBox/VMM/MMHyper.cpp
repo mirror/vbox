a79a6410fb568037521ab547a30528e81c707405
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2008-12-15 19:32:34 +0000

    bigger heap for darwin/VT-x.
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@15538 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/VMM/MMHyper.cpp
+++ src/VBox/VMM/MMHyper.cpp
@@ -78,7 +78,7 @@ int mmR3HyperInit(PVM pVM)
     int rc = CFGMR3QueryU32(CFGMR3GetChild(CFGMR3GetRoot(pVM), "MM"), "cbHyperHeap", &cbHyperHeap);
     if (rc == VERR_CFGM_NO_PARENT || rc == VERR_CFGM_VALUE_NOT_FOUND)
         cbHyperHeap = VMMIsHwVirtExtForced(pVM)
-                    ? 384*_1K
+                    ? 640*_1K
                     : 1280*_1K;
     else if (RT_FAILURE(rc))
     {
