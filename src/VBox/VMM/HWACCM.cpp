1e1c2392ce8db72d794cdbce11158f1c9fea69ba
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2008-12-16 22:28:15 +0000

    HWACCM: Updated todo in HWACCMR3CanExecuteGuest.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@15611 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/VMM/HWACCM.cpp
+++ src/VBox/VMM/HWACCM.cpp
@@ -1253,7 +1253,8 @@ VMMR3DECL(bool) HWACCMR3CanExecuteGuest(PVM pVM, PCPUMCTX pCtx)
             /** @todo   This should (probably) be set on every excursion to the REM,
              *          however it's too risky right now. So, only apply it when we go
              *          back to REM for real mode execution. (The XP hack below doesn't
-             *          work reliably without this.) */
+             *          work reliably without this.)
+             *  Update: Implemented in EM.cpp, see #ifdef EM_NOTIFY_HWACCM.  */
             pVM->aCpus[0].hwaccm.s.fContextUseFlags |= HWACCM_CHANGED_ALL_GUEST;
 
             /* Too early for VT-x; Solaris guests will fail with a guru meditation otherwise; same for XP. */
