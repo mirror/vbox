db0cacc3ae5de3ca254fccddc13590585ee91d47
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2008-12-16 18:01:38 +0000

    VBoxManage: move converthd back to internalcommands, and a few minor cleanups.
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@15602 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Frontends/VBoxManage/VBoxManage.h
+++ src/VBox/Frontends/VBoxManage/VBoxManage.h
@@ -70,7 +70,7 @@
 #define USAGE_LOADSYMS              RT_BIT_64(29)
 #define USAGE_UNLOADSYMS            RT_BIT_64(30)
 #define USAGE_SETHDUUID             RT_BIT_64(31)
-#define USAGE_CONVERTDD             RT_BIT_64(32)
+#define USAGE_CONVERTFROMRAW        RT_BIT_64(32)
 #define USAGE_LISTPARTITIONS        RT_BIT_64(33)
 #define USAGE_CREATERAWVMDK         RT_BIT_64(34)
 #define USAGE_VM_STATISTICS         RT_BIT_64(35)
@@ -155,7 +155,7 @@ int handleModifyHardDisk(int argc, char *argv[],
 int handleCloneHardDisk(int argc, char *argv[],
                         ComPtr<IVirtualBox> virtualBox, ComPtr<ISession> session);
 int handleConvertHardDisk(int argc, char **argv);
-int handleConvertDDImage(int argc, char *argv[]);
+int handleConvertFromRaw(int argc, char *argv[]);
 int handleAddiSCSIDisk(int argc, char *argv[],
                        ComPtr <IVirtualBox> aVirtualBox, ComPtr<ISession> aSession);
 int handleShowHardDiskInfo(int argc, char *argv[],
