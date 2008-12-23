882e965c8fbc86cd6a7122c3c91b17e1f5c2f7bc
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2008-12-23 14:09:52 +0000

    Assertion only valid for windows hosts.
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@15728 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/VMM/VMMR0/HWVMXR0.cpp
+++ src/VBox/VMM/VMMR0/HWVMXR0.cpp
@@ -3651,8 +3651,9 @@ VMMR0DECL(int) VMXR0Execute64BitsHandler(PVM pVM, PVMCPU pVCpu, PCPUMCTX pCtx, R
     }
 
     VMXActivateVMCS(pVCpu->hwaccm.s.vmx.pVMCSPhys);
+#ifdef RT_OS_WINDOWS
     Assert(ASMGetFlags() & X86_EFL_IF);
-
+#endif
     return rc;
 }
 
