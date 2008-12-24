d928511c3615fb8abcd329c90660db893ad5c1d8
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2008-12-24 12:59:56 +0000

    Backed out 41313. Assert failed on Linux/Mac boxes for some reason
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@15750 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/VMM/SELM.cpp
+++ src/VBox/VMM/SELM.cpp
@@ -1565,12 +1565,11 @@ VMMR3DECL(int) SELMR3SyncTSS(PVM pVM)
                 {
                     uint32_t offRedirBitmap;
                     
-                    AssertCompile(RT_OFFSETOF(VBOXTSS, IntRedirBitmap) + sizeof(tss.IntRedirBitmap) == sizeof(VBOXTSS));
                     /* Make sure the io bitmap offset is valid; anything less than sizeof(VBOXTSS) means there's none. */
-                    if (tss.offIoBitmap < sizeof(VBOXTSS))
+                    if (tss.offIoBitmap < RT_OFFSETOF(VBOXTSS, IntRedirBitmap) + sizeof(tss.IntRedirBitmap))
                     {
                         Log(("Invalid io bitmap offset detected (%x)!\n", tss.offIoBitmap));
-                        tss.offIoBitmap = sizeof(VBOXTSS);
+                        tss.offIoBitmap = RT_OFFSETOF(VBOXTSS, IntRedirBitmap) + sizeof(tss.IntRedirBitmap);
                     }
                         
                     offRedirBitmap = tss.offIoBitmap - sizeof(tss.IntRedirBitmap);
