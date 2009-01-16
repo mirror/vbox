8985a6fdd17cff7eae9e51c944e2cd43f9e3bc05
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2009-01-16 09:19:51 +0000

    Set the limit to 0xffff when loading a segment register in real mode.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@15982 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/recompiler_new/target-i386/translate.c
+++ src/recompiler_new/target-i386/translate.c
@@ -2777,12 +2777,15 @@ DECLINLINE(void) gen_op_movl_seg_T0_vm(int seg_reg)
     tcg_gen_st_tl(cpu_T[0], cpu_env,
                   offsetof(CPUX86State,segs[seg_reg].base));
 #ifdef VBOX
-    /** @todo shouldn't we set the limit here too? (to 0xffff) */
     int flags = DESC_P_MASK | DESC_S_MASK | DESC_W_MASK;
     if (seg_reg == R_CS)
         flags |= DESC_CS_MASK;
     gen_op_movl_T0_im(flags);
     tcg_gen_st32_tl(cpu_T[0], cpu_env, offsetof(CPUX86State,segs[seg_reg].flags));
+
+    /* Set the limit to 0xffff. */
+    gen_op_movl_T0_im(0xffff);
+    tcg_gen_st32_tl(cpu_T[0], cpu_env, offsetof(CPUX86State,segs[seg_reg].limit));
 #endif
 }
 
