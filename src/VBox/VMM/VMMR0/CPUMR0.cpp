a8aab2aea45b7d0968b6488cc272567ef1e3d76f
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2009-01-20 22:35:20 +0000

    CPUMR0: forgot to remove a line.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@16106 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/VMM/VMMR0/CPUMR0.cpp
+++ src/VBox/VMM/VMMR0/CPUMR0.cpp
@@ -260,11 +260,8 @@ VMMR0DECL(int) CPUMR0SaveGuestFPU(PVM pVM, PVMCPU pVCpu, PCPUMCTX pCtx)
 #if HC_ARCH_BITS == 32 && defined(VBOX_WITH_64_BITS_GUESTS) && !defined(VBOX_WITH_HYBRID_32BIT_KERNEL)
     if (CPUMIsGuestInLongModeEx(pCtx))
     {
-if (!(pVCpu->cpum.s.fUseFlags & CPUM_SYNC_FPU_STATE))
         if (!(pVCpu->cpum.s.fUseFlags & CPUM_SYNC_FPU_STATE))
             HWACCMR0SaveFPUState(pVM, pVCpu, pCtx);
-
-
         cpumR0RestoreHostFPUState(&pVCpu->cpum.s);
     }
     else
