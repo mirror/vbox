7c1bcefca10e49d0bbceb8201728f8d54c666aed
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2009-01-20 09:59:13 +0000

    crOpenGL: tabs to spaces
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@16073 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Additions/WINNT/Graphics/crOpenGL/array/arrayspu_init.c
+++ src/VBox/Additions/WINNT/Graphics/crOpenGL/array/arrayspu_init.c
@@ -13,65 +13,65 @@ extern SPUNamedFunctionTable _cr_array_table[];
 extern SPUOptions arraySPUOptions[];
 
 static SPUFunctions array_functions = {
-	NULL, /* CHILD COPY */
-	NULL, /* DATA */
-	_cr_array_table /* THE ACTUAL FUNCTIONS */
+    NULL, /* CHILD COPY */
+    NULL, /* DATA */
+    _cr_array_table /* THE ACTUAL FUNCTIONS */
 };
 
 static SPUFunctions *arraySPUInit( int id, SPU *child, SPU *self,
-		unsigned int context_id,
-		unsigned int num_contexts )
+        unsigned int context_id,
+        unsigned int num_contexts )
 {
 
-	(void) context_id;
-	(void) num_contexts;
+    (void) context_id;
+    (void) num_contexts;
 
-	array_spu.id = id;
-	array_spu.has_child = 0;
-	if (child)
-	{
-		crSPUInitDispatchTable( &(array_spu.child) );
-		crSPUCopyDispatchTable( &(array_spu.child), &(child->dispatch_table) );
-		array_spu.has_child = 1;
-	}
-	crSPUInitDispatchTable( &(array_spu.super) );
-	crSPUCopyDispatchTable( &(array_spu.super), &(self->superSPU->dispatch_table) );
-	arrayspuSetVBoxConfiguration();
+    array_spu.id = id;
+    array_spu.has_child = 0;
+    if (child)
+    {
+        crSPUInitDispatchTable( &(array_spu.child) );
+        crSPUCopyDispatchTable( &(array_spu.child), &(child->dispatch_table) );
+        array_spu.has_child = 1;
+    }
+    crSPUInitDispatchTable( &(array_spu.super) );
+    crSPUCopyDispatchTable( &(array_spu.super), &(self->superSPU->dispatch_table) );
+    arrayspuSetVBoxConfiguration();
 
-	crStateInit();
-	array_spu.ctx = crStateCreateContext( NULL, 0, NULL );
+    crStateInit();
+    array_spu.ctx = crStateCreateContext( NULL, 0, NULL );
 #ifdef CR_ARB_vertex_buffer_object
-	array_spu.ctx->bufferobject.retainBufferData = GL_TRUE;
+    array_spu.ctx->bufferobject.retainBufferData = GL_TRUE;
 #endif
-	/* we call SetCurrent instead of MakeCurrent as the differencer
-	 * isn't setup yet anyway */
-	crStateSetCurrent( array_spu.ctx );
+    /* we call SetCurrent instead of MakeCurrent as the differencer
+     * isn't setup yet anyway */
+    crStateSetCurrent( array_spu.ctx );
 
-	return &array_functions;
+    return &array_functions;
 }
 
 static void arraySPUSelfDispatch(SPUDispatchTable *self)
 {
-	crSPUInitDispatchTable( &(array_spu.self) );
-	crSPUCopyDispatchTable( &(array_spu.self), self );
+    crSPUInitDispatchTable( &(array_spu.self) );
+    crSPUCopyDispatchTable( &(array_spu.self), self );
 }
 
 static int arraySPUCleanup(void)
 {
-	return 1;
+    return 1;
 }
 
 int SPULoad( char **name, char **super, SPUInitFuncPtr *init,
-	     SPUSelfDispatchFuncPtr *self, SPUCleanupFuncPtr *cleanup,
-	     SPUOptionsPtr *options, int *flags )
+         SPUSelfDispatchFuncPtr *self, SPUCleanupFuncPtr *cleanup,
+         SPUOptionsPtr *options, int *flags )
 {
-	*name = "array";
-	*super = "passthrough";
-	*init = arraySPUInit;
-	*self = arraySPUSelfDispatch;
-	*cleanup = arraySPUCleanup;
-	*options = arraySPUOptions;
-	*flags = (SPU_NO_PACKER|SPU_NOT_TERMINAL|SPU_MAX_SERVERS_ZERO);
-	
-	return 1;
+    *name = "array";
+    *super = "passthrough";
+    *init = arraySPUInit;
+    *self = arraySPUSelfDispatch;
+    *cleanup = arraySPUCleanup;
+    *options = arraySPUOptions;
+    *flags = (SPU_NO_PACKER|SPU_NOT_TERMINAL|SPU_MAX_SERVERS_ZERO);
+    
+    return 1;
 }
