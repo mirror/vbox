7b4c575ade506416adf7b7ba203bc3d513e6046a
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2009-01-26 10:03:46 +0000

    Devices/Serial: Data unavaiable while reading shouldn't terminate thread.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@16217 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Devices/Serial/DrvHostSerial.cpp
+++ src/VBox/Devices/Serial/DrvHostSerial.cpp
@@ -757,7 +757,7 @@ static DECLCALLBACK(int) drvHostSerialRecvThread(PPDMDRVINS pDrvIns, PPDMTHREAD 
             rc = RTFileRead(pThis->DeviceFileR, abBuffer, sizeof(abBuffer), &cbRead);
             if (RT_FAILURE(rc))
             {
-                LogRel(("HostSerial#%d: Read failed with %Rrc, terminating the worker thread.\n", pDrvIns->iInstance, rc));
+                LogRel(("HostSerial#%d: (1) Read failed with %Rrc, terminating the worker thread.\n", pDrvIns->iInstance, rc));
                 rcThread = rc;
                 break;
             }
@@ -795,7 +795,11 @@ static DECLCALLBACK(int) drvHostSerialRecvThread(PPDMDRVINS pDrvIns, PPDMTHREAD 
             rc = RTFileRead(pThis->DeviceFile, abBuffer, sizeof(abBuffer), &cbRead);
             if (RT_FAILURE(rc))
             {
-                LogRel(("HostSerial#%d: Read failed with %Rrc, terminating the worker thread.\n", pDrvIns->iInstance, rc));
+                /* don't terminate worker thread when data unavailable */
+                if (rc == VERR_TRY_AGAIN)
+                    continue;
+
+                LogRel(("HostSerial#%d: (2) Read failed with %Rrc, terminating the worker thread.\n", pDrvIns->iInstance, rc));
                 rcThread = rc;
                 break;
             }
