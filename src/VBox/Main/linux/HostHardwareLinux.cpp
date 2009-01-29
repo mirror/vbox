4646c9b4f5aca523c0c6b91b226c6ab3182bb757
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2009-01-29 11:24:18 +0000

    Main/USBProxyServiceLinux: logging and fix a burn
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@16363 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Main/linux/HostHardwareLinux.cpp
+++ src/VBox/Main/linux/HostHardwareLinux.cpp
@@ -303,6 +303,8 @@ int VBoxMainHotplugWaiter::Wait(unsigned cMillies)
     {
         connected = dbus_connection_read_write_dispatch (mContext->mConnection.get(),
                                                          cRealMillies);
+        if (mContext->mInterrupt)
+            LogFlowFunc(("wait loop interrupted\n"));
         if (cMillies != RT_INDEFINITE_WAIT)
             mContext->mInterrupt = true;
     }
@@ -318,6 +320,7 @@ int VBoxMainHotplugWaiter::Wait(unsigned cMillies)
 void VBoxMainHotplugWaiter::Interrupt()
 {
 #if defined RT_OS_LINUX && defined VBOX_WITH_DBUS
+    LogFlowFunc(("\n"));
     mContext->mInterrupt = true;
 #endif  /* defined RT_OS_LINUX && defined VBOX_WITH_DBUS */
 }
