7fd017ec6a47a68230a0f82976f31a5dfaaaa04e
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2009-02-02 18:03:07 +0000

    build fix.
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@16474 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Frontends/VirtualBox/src/VBoxConsoleView.cpp
+++ src/VBox/Frontends/VirtualBox/src/VBoxConsoleView.cpp
@@ -84,6 +84,9 @@ const int XKeyRelease = KeyRelease;
 # ifdef VBOX_WITH_HACKED_QT
 #  include "QIApplication.h"
 # endif
+# ifndef QT_MAC_USE_COCOA
+#  include <Carbon/Carbon.h>
+# endif
 # include <VBox/err.h>
 #endif /* defined (Q_WS_MAC) */
 
