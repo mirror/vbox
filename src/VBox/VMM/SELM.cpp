7f7f23e6d7097fa55eee6a653cd6000c080dfa69
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2009-02-04 10:50:15 +0000

    Adjusted assertion slightly
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@16506 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/VMM/SELM.cpp
+++ src/VBox/VMM/SELM.cpp
@@ -1487,7 +1487,7 @@ VMMR3DECL(int) SELMR3SyncTSS(PVM pVM)
 #endif
         /* The guest's TSS can span multiple pages now. We will monitor the whole thing. */
 #ifndef DEBUG_michael
-        AssertMsg((GCPtrTss >> PAGE_SHIFT) == ((GCPtrTss + sizeof(VBOXTSS) - 1) >> PAGE_SHIFT),
+        AssertMsg((GCPtrTss >> PAGE_SHIFT) == ((GCPtrTss + RT_MIN(sizeof(VBOXTSS), cbTss) - 1) >> PAGE_SHIFT),
                   ("GCPtrTss=%RGv cbTss=%#x - We assume everything is inside one page!\n", GCPtrTss, cbTss));
 #endif
 
