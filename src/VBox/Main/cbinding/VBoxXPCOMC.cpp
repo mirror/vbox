b4016907c907b127992a2205f2a99264247c3945
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2009-02-05 17:30:59 +0000

    VBoxXPCOMC.cpp: fixed the LOG_GROUP definition (must be first statement) and log.h inclusion. Removed unused streams header and review comment (has been executed).
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@16534 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Main/cbinding/VBoxXPCOMC.cpp
+++ src/VBox/Main/cbinding/VBoxXPCOMC.cpp
@@ -19,16 +19,13 @@
  * additional information or have any questions.
  */
 
-#include <iostream>
-#include <iomanip>
-
+#define LOG_GROUP LOG_GROUP_MAIN
 #include <nsMemory.h>
 #include <nsIServiceManager.h>
 #include <nsEventQueueUtils.h>
 
 #include <iprt/string.h>
-#define LOG_GROUP_MAIN
-#include <iprt/log.h>
+#include <VBox/log.h>
 
 #include "VirtualBox_XPCOM.h"
 #include "cbinding.h"
@@ -83,11 +80,6 @@ VBoxComInitialize(IVirtualBox **virtualBox, ISession **session)
 
     Session     = *session;
     Ivirtualbox = *virtualBox;
-
-/** @todo r=bird: Why is cout/cerr used unconditionally here?
- * It would be preferred to use RTPrintf/RTStrmPrintf(g_pStdErr,..). If this is
- * going to be used in real life, the cout(/RTPrintf) bits should be optional,
- * add a flag argument and define VBOXCOMINIT_FLAG_VERBOSE for the purpose. */
 
     rc = NS_InitXPCOM2(&serviceManager, nsnull, nsnull);
     if (NS_FAILED(rc))
