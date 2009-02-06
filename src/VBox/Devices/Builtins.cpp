c10b2108cdd2b1ea5a2f4f7f7bbe99329dc9e8f8
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2009-02-06 17:02:23 +0000

    initial EFI code
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@16559 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Devices/Builtins.cpp
+++ src/VBox/Devices/Builtins.cpp
@@ -93,6 +93,11 @@ extern "C" DECLEXPORT(int) VBoxDevicesRegister(PPDMDEVREGCB pCallbacks, uint32_t
     if (RT_FAILURE(rc))
         return rc;
 #endif
+#ifdef VBOX_WITH_EFI
+    rc = pCallbacks->pfnRegister(pCallbacks, &g_DeviceEFI);
+    if (RT_FAILURE(rc))
+        return rc;
+#endif
     rc = pCallbacks->pfnRegister(pCallbacks, &g_DeviceMC146818);
     if (RT_FAILURE(rc))
         return rc;
