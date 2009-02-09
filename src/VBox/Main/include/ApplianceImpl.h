c1916ab219cac26d06368c57c9e0cf44ec4dc268
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2009-02-09 16:29:25 +0000

    OVF: added count attribute to receive the current number of entries
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@16600 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Main/include/ApplianceImpl.h
+++ src/VBox/Main/include/ApplianceImpl.h
@@ -144,6 +144,7 @@ public:
     static const wchar_t *getComponentName() { return L"VirtualSystemDescription"; }
 
     /* IVirtualSystemDescription properties */
+    STDMETHOD(COMGETTER(Count))(ULONG *aCount);
 
     /* IVirtualSystemDescription methods */
     STDMETHOD(GetDescription)(ComSafeArrayOut(VirtualSystemDescriptionType_T, aTypes),
