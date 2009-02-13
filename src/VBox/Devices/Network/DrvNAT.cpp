ffc261d457a9e49367bcbe64aa7c60c06167903d
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2009-02-13 05:16:48 +0000

    NAT: avoiding "false alarms"
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@16726 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Devices/Network/DrvNAT.cpp
+++ src/VBox/Devices/Network/DrvNAT.cpp
@@ -399,7 +399,15 @@ static DECLCALLBACK(int) drvNATAsyncIoThread(PPDMDRVINS pDrvIns, PPDMTHREAD pThr
                 /* drain the pipe */
                 char ch[1];
                 size_t cbRead;
-                RTFileRead(pThis->PipeRead, &ch, 1, &cbRead);
+                /* 
+                 * drvNATSend decoupled so we don't know how many times 
+                 * device's thread sends before we've entered multiplex,
+                 * so to avoid false alarm drain pipe here to the very end
+                 *
+                 * @todo: Probably we should counter drvNATSend to count how 
+                 * deep pipe has been filed before drain
+                 */
+                while(RTFileRead(pThis->PipeRead, &ch, 1, &cbRead) > 0);
             }
             /* process _all_ outstanding requests but don't wait */
             RTReqProcess(pThis->pReqQueue, 0);
