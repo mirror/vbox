2399bfefef4763a79d990c90c946920a65d6490a
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2009-02-18 16:00:29 +0000

    VBOX_WITH_PGMPOOL_PAGING_ONLY: disabled obsolete code
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@16919 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/VMM/VMMAll/PGMAllPool.cpp
+++ src/VBox/VMM/VMMAll/PGMAllPool.cpp
@@ -1990,7 +1990,7 @@ static int pgmPoolMonitorFlush(PPGMPOOL pPool, PPGMPOOLPAGE pPage)
     return rc;
 }
 
-# ifdef PGMPOOL_WITH_MIXED_PT_CR3
+# if defined(PGMPOOL_WITH_MIXED_PT_CR3) && !defined(VBOX_WITH_PGMPOOL_PAGING_ONLY)
 
 /**
  * Set or clear the fCR3Mix attribute in a chain of monitored pages.
@@ -2111,7 +2111,7 @@ int pgmPoolMonitorUnmonitorCR3(PPGMPOOL pPool, uint16_t idxRoot)
     return rc;
 }
 
-# endif /* PGMPOOL_WITH_MIXED_PT_CR3 */
+# endif /* PGMPOOL_WITH_MIXED_PT_CR3 && !VBOX_WITH_PGMPOOL_PAGING_ONLY*/
 
 /**
  * Inserts the page into the list of modified pages.
