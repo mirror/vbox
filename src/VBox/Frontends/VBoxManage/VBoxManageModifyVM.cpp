5ab654d4139ab59e36563fcaabfb3e6ed984dd76
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2009-02-27 12:48:14 +0000

    #3551: “Main: Replace remaining collections with safe arrays” Take two. Convert HostDVDDriveCollection. This time for all plattforms.
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@17200 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Frontends/VBoxManage/VBoxManageModifyVM.cpp
+++ src/VBox/Frontends/VBoxManage/VBoxManageModifyVM.cpp
@@ -24,6 +24,7 @@
 *******************************************************************************/
 #ifndef VBOX_ONLY_DOCS
 #include <VBox/com/com.h>
+#include <VBox/com/array.h>
 #include <VBox/com/ErrorInfo.h>
 #include <VBox/com/errorprint2.h>
 #include <VBox/com/EventQueue.h>
@@ -1090,10 +1091,11 @@ int handleModifyVM(HandlerArg *a)
             {
                 ComPtr<IHost> host;
                 CHECK_ERROR(a->virtualBox, COMGETTER(Host)(host.asOutParam()));
-                ComPtr<IHostDVDDriveCollection> hostDVDs;
-                CHECK_ERROR(host, COMGETTER(DVDDrives)(hostDVDs.asOutParam()));
+                com::SafeIfaceArray <IHostDVDDrive> hostDVDs;
+                rc = host->COMGETTER(DVDDrives)(ComSafeArrayAsOutParam(hostDVDs));
+
                 ComPtr<IHostDVDDrive> hostDVDDrive;
-                rc = hostDVDs->FindByName(Bstr(dvd + 5), hostDVDDrive.asOutParam());
+                rc = host->FindHostDVDDrive(Bstr(dvd + 5), hostDVDDrive.asOutParam());
                 if (!hostDVDDrive)
                 {
                     /* 2nd try: try with the real name, important on Linux+libhal */
@@ -1104,7 +1106,7 @@ int handleModifyVM(HandlerArg *a)
                         rc = E_FAIL;
                         break;
                     }
-                    rc = hostDVDs->FindByName(Bstr(szPathReal), hostDVDDrive.asOutParam());
+                    rc = host->FindHostDVDDrive(Bstr(szPathReal), hostDVDDrive.asOutParam());
                     if (!hostDVDDrive)
                     {
                         errorArgument("Invalid host DVD drive name");
