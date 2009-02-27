7ee4104aa345e98d590c712e7e714690335957b4
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2009-02-27 12:16:54 +0000

    vboxNetAdpFactoryCreateAndConnect: inverted assertion.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@17198 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/HostDrivers/VBoxNetAdp/VBoxNetAdp.c
+++ src/VBox/HostDrivers/VBoxNetAdp/VBoxNetAdp.c
@@ -758,7 +758,7 @@ static DECLCALLBACK(int) vboxNetAdpFactoryCreateAndConnect(PINTNETTRUNKFACTORY p
 
     LogFlow(("vboxNetAdpFactoryCreateAndConnect: pszName=%p:{%s} fFlags=%#x\n", pszName, pszName, fFlags));
     Assert(pGlobals->cFactoryRefs > 0);
-    AssertMsgReturn(fFlags,
+    AssertMsgReturn(!fFlags,
                     ("%#x\n", fFlags), VERR_INVALID_PARAMETER);
 
     /*
