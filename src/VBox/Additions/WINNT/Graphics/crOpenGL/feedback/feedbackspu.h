baf2ccdef1e26ba14d941c0d1b0de051f9ddf0f7
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2009-03-03 12:23:40 +0000

    export to OSE
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@17269 cfe28804-0f27-0410-a406-dd0f0b0b656f

---
+++ src/VBox/Additions/WINNT/Graphics/crOpenGL/feedback/feedbackspu.h
@@ -0,0 +1,40 @@
+/* Copyright (c) 2001, Stanford University
+ * All rights reserved.
+ *
+ * See the file LICENSE.txt for information on redistributing this software.
+ */
+
+#ifndef FEEDBACK_SPU_H
+#define FEEDBACK_SPU_H
+
+#ifdef WINDOWS
+#define FEEDBACKSPU_APIENTRY __stdcall
+#else
+#define FEEDBACKSPU_APIENTRY
+#endif
+
+#include "cr_spu.h"
+#include "cr_timer.h"
+#include "cr_glstate.h"
+
+typedef struct {
+	int id;
+	int has_child;
+	SPUDispatchTable self, child, super;
+
+	int render_mode;
+
+	int default_viewport;
+
+	CRCurrentStatePointers current;
+} feedbackSPU;
+
+extern feedbackSPU feedback_spu;
+
+extern SPUNamedFunctionTable _cr_feedback_table[];
+
+extern SPUOptions feedbackSPUOptions[];
+
+extern void feedbackspuGatherConfiguration( void );
+
+#endif /* FEEDBACK_SPU_H */
---
+++ src/VBox/Additions/WINNT/Graphics/crOpenGL/feedback/feedbackspu_init.c
@@ -0,0 +1,80 @@
+/* Copyright (c) 2001, Stanford University
+ * All rights reserved
+ *
+ * See the file LICENSE.txt for information on redistributing this software.
+ */
+
+#include "cr_spu.h"
+#include "cr_environment.h"
+#include "cr_string.h"
+#include "cr_error.h"
+#include "cr_mem.h"
+#include "cr_server.h"
+#include "feedbackspu.h"
+#include <fcntl.h>
+#ifndef WINDOWS
+#include <unistd.h>
+#endif
+
+feedbackSPU feedback_spu;
+
+static SPUFunctions feedback_functions = {
+	NULL, /* CHILD COPY */
+	NULL, /* DATA */
+	_cr_feedback_table /* THE ACTUAL FUNCTIONS */
+};
+
+static SPUFunctions *feedbackSPUInit( int id, SPU *child, SPU *self,
+		unsigned int context_id,
+		unsigned int num_contexts )
+{
+	CRContext *ctx;
+	(void) context_id;
+	(void) num_contexts;
+
+	feedback_spu.id = id;
+	feedback_spu.has_child = 0;
+	if (child)
+	{
+		crSPUInitDispatchTable( &(feedback_spu.child) );
+		crSPUCopyDispatchTable( &(feedback_spu.child), &(child->dispatch_table) );
+		feedback_spu.has_child = 1;
+	}
+	crSPUInitDispatchTable( &(feedback_spu.super) );
+	crSPUCopyDispatchTable( &(feedback_spu.super), &(self->superSPU->dispatch_table) );
+	feedbackspuGatherConfiguration();
+
+	/* create/init default state tracker */
+	crStateInit();
+	ctx = crStateGetCurrent();
+	CRASSERT(ctx);
+	crStateSetCurrentPointers(ctx, &feedback_spu.current);
+
+	return &feedback_functions;
+}
+
+static void feedbackSPUSelfDispatch(SPUDispatchTable *self)
+{
+	crSPUInitDispatchTable( &(feedback_spu.self) );
+	crSPUCopyDispatchTable( &(feedback_spu.self), self );
+}
+
+static int feedbackSPUCleanup(void)
+{
+	return 1;
+}
+
+int SPULoad( char **name, char **super, SPUInitFuncPtr *init,
+	     SPUSelfDispatchFuncPtr *self, SPUCleanupFuncPtr *cleanup,
+	     SPUOptionsPtr *options, int *flags )
+{
+	*name = "feedback";
+	*super = "passthrough";
+	*init = feedbackSPUInit;
+	*self = feedbackSPUSelfDispatch;
+	*cleanup = feedbackSPUCleanup;
+	*options = feedbackSPUOptions;
+	*flags = (SPU_NO_PACKER|SPU_NOT_TERMINAL|SPU_MAX_SERVERS_ZERO);
+	
+	return 1;
+}
