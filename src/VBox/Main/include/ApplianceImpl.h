9a60ec1fc144a65cec5eb9633e606ed04b502cdd
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2009-03-03 15:08:59 +0000

    OVF: rename config array items to have them make sense on export as well.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@17291 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Main/include/ApplianceImpl.h
+++ src/VBox/Main/include/ApplianceImpl.h
@@ -109,8 +109,8 @@ struct VirtualSystemDescriptionEntry
     uint32_t ulIndex;                       // zero-based index of this entry within array
     VirtualSystemDescriptionType_T type;    // type of this entry
     Utf8Str strRef;                         // reference number (hard disk controllers only)
-    Utf8Str strOrig;                        // original OVF value (type-dependent)
-    Utf8Str strConfig;                      // configuration value (type-dependent)
+    Utf8Str strOvf;                         // original OVF value (type-dependent)
+    Utf8Str strVbox;                        // configuration value (type-dependent)
     Utf8Str strExtraConfig;                 // extra configuration key=value strings (type-dependent)
 };
 
@@ -153,12 +153,12 @@ public:
     /* IVirtualSystemDescription methods */
     STDMETHOD(GetDescription)(ComSafeArrayOut(VirtualSystemDescriptionType_T, aTypes),
                               ComSafeArrayOut(BSTR, aRefs),
-                              ComSafeArrayOut(BSTR, aOrigValues),
-                              ComSafeArrayOut(BSTR, aConfigValues),
+                              ComSafeArrayOut(BSTR, aOvfValues),
+                              ComSafeArrayOut(BSTR, aVboxValues),
                               ComSafeArrayOut(BSTR, aExtraConfigValues));
 
     STDMETHOD(SetFinalValues)(ComSafeArrayIn(BOOL, aEnabled),
-                              ComSafeArrayIn(IN_BSTR, aConfigValues),
+                              ComSafeArrayIn(IN_BSTR, aVboxValues),
                               ComSafeArrayIn(IN_BSTR, aExtraConfigValues));
 
     STDMETHOD(GetWarnings)(ComSafeArrayOut(BSTR, aWarnings));
