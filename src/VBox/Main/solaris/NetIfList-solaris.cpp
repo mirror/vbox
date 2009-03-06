92ca870e03195e00a9f1ad06fc18a4604e6d2c39
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2009-03-06 09:56:18 +0000

    #3656: Moved IP config function stubs from solaris, darwin and linux to Main/generic for common implementation.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@17443 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Main/solaris/NetIfList-solaris.cpp
+++ src/VBox/Main/solaris/NetIfList-solaris.cpp
@@ -381,18 +381,3 @@ int NetIfList(std::list <ComObjPtr <HostNetworkInterface> > &list)
     return VERR_NOT_IMPLEMENTED;
 }
 #endif
-
-int NetIfEnableStaticIpConfig(HostNetworkInterface * pIf, ULONG ip, ULONG mask)
-{
-    return VERR_NOT_IMPLEMENTED;
-}
-
-int NetIfEnableStaticIpConfigV6(HostNetworkInterface * pIf, IN_BSTR aIPV6Address, ULONG aIPV6MaskPrefixLength)
-{
-    return VERR_NOT_IMPLEMENTED;
-}
-
-int NetIfEnableDynamicIpConfig(HostNetworkInterface * pIf)
-{
-    return VERR_NOT_IMPLEMENTED;
-}
