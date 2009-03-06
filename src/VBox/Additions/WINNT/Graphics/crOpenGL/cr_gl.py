379294995777b08e5b726739bb37be4a997d8d1f
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2009-03-06 14:03:46 +0000

    crOpenGL: more dri related drafts
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@17472 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Additions/WINNT/Graphics/crOpenGL/cr_gl.py
+++ src/VBox/Additions/WINNT/Graphics/crOpenGL/cr_gl.py
@@ -4,7 +4,7 @@ print """
  */
 
 /*
- * Copyright (C) 2006-2007 Sun Microsystems, Inc.
+ * Copyright (C) 2008 Sun Microsystems, Inc.
  *
  * This file is part of VirtualBox Open Source Edition (OSE), as
  * available from http://www.virtualbox.org. This file is free software;
