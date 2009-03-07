fff4065bf442deebd467a2278d3c2476fb472ef9
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2009-03-07 05:45:55 +0000

    MM,PGM: Fixed page reservation, include a full set of handy pages for the time being. Fixed page counting statistics.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@17514 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- include/VBox/mm.h
+++ include/VBox/mm.h
@@ -302,6 +302,7 @@ VMMR3DECL(int)      MMR3HyperInitFinalize(PVM pVM);
 VMMR3DECL(int)      MMR3Term(PVM pVM);
 VMMR3DECL(void)     MMR3TermUVM(PUVM pUVM);
 VMMR3DECL(void)     MMR3Reset(PVM pVM);
+VMMR3DECL(int)      MMR3ReserveHandyPages(PVM pVM, uint32_t cHandyPages);
 VMMR3DECL(int)      MMR3IncreaseBaseReservation(PVM pVM, uint64_t cAddBasePages);
 VMMR3DECL(int)      MMR3AdjustFixedReservation(PVM pVM, int32_t cDeltaFixedPages, const char *pszDesc);
 VMMR3DECL(int)      MMR3UpdateShadowReservation(PVM pVM, uint32_t cShadowPages);
