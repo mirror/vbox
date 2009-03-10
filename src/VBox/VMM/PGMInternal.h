b987dd78b5f333bd44804e02277f49d4f1d23489
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2009-03-10 12:32:23 +0000

    Simple check added to make sure we don't bounce back and forth between hypervisor mappings that have caused conflicts in the past.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@17622 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/VMM/PGMInternal.h
+++ src/VBox/VMM/PGMInternal.h
@@ -363,6 +363,8 @@
 # define PGM_INVL_GUEST_TLBS()          HWACCMFlushTLB(pVM)
 #endif
 
+/** Size of the GCPtrConflict array in PGMMAPPING. */
+#define PGMMAPPING_CONFLICT_MAX         8
 
 /**
  * Structure for tracking GC Mappings.
@@ -391,11 +393,13 @@ typedef struct PGMMAPPING
     R3PTRTYPE(void *)                   pvUser;
     /** Mapping description / name. For easing debugging. */
     R3PTRTYPE(const char *)             pszDesc;
+    /** Last 8 addresses that caused conflicts. */
+    RTGCPTR                             GCPtrConflict[PGMMAPPING_CONFLICT_MAX];
+    /** Number of conflicts for this hypervisor mapping. */
+    uint32_t                            cConflicts;
     /** Number of page tables. */
     uint32_t                            cPTs;
-#if HC_ARCH_BITS != GC_ARCH_BITS || GC_ARCH_BITS == 64
-    uint32_t                            uPadding1; /**< Alignment padding. */
-#endif
+
     /** Array of page table mapping data. Each entry
      * describes one page table. The array can be longer
      * than the declared length.
