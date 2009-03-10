14c4f9d3b309d9a24482395b138b9f7530b7f08d
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2009-03-10 09:29:54 +0000

    Additions/x11: added libXcomposite stub library for linking against
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@17602 cfe28804-0f27-0410-a406-dd0f0b0b656f

---
+++ src/VBox/Additions/x11/x11stubs/libXcomposite-1.0.0/libXcomposite.c
@@ -0,0 +1,54 @@
+/* $Id$ */
+
+/** @file
+ * X.Org libXcomposite.so linker stub
+ */
+
+/*
+ * Copyright (C) 2009 Sun Microsystems, Inc.
+ *
+ * This file is part of VirtualBox Open Source Edition (OSE), as
+ * available from http://www.virtualbox.org. This file is free software;
+ * you can redistribute it and/or modify it under the terms of the GNU
+ * General Public License (GPL) as published by the Free Software
+ * Foundation, in version 2 as it comes in the "COPYING" file of the
+ * VirtualBox OSE distribution. VirtualBox OSE is distributed in the
+ * hope that it will be useful, but WITHOUT ANY WARRANTY of any kind.
+ *
+ * Please contact Sun Microsystems, Inc., 4150 Network Circle, Santa
+ * Clara, CA 95054 USA or visit http://www.sun.com if you need
+ * additional information or have any questions.
+ */
+
+#include <iprt/cdefs.h>
+#include <iprt/types.h>
+
+DECLEXPORT(void) XCompositeCreateRegionFromBorderClip(void);
+void XCompositeCreateRegionFromBorderClip(void) {}
+DECLEXPORT(void) XCompositeExtensionInfo(void);
+void XCompositeExtensionInfo(void) {}
+DECLEXPORT(void) XCompositeExtensionName(void);
+void XCompositeExtensionName(void) {}
+DECLEXPORT(void) XCompositeFindDisplay(void);
+void XCompositeFindDisplay(void) {}
+DECLEXPORT(void) XCompositeGetOverlayWindow(void);
+void XCompositeGetOverlayWindow(void) {}
+DECLEXPORT(void) XCompositeNameWindowPixmap(void);
+void XCompositeNameWindowPixmap(void) {}
+DECLEXPORT(void) XCompositeQueryExtension(void);
+void XCompositeQueryExtension(void) {}
+DECLEXPORT(void) XCompositeQueryVersion(void);
+void XCompositeQueryVersion(void) {}
+DECLEXPORT(void) XCompositeRedirectSubwindows(void);
+void XCompositeRedirectSubwindows(void) {}
+DECLEXPORT(void) XCompositeRedirectWindow(void);
+void XCompositeRedirectWindow(void) {}
+DECLEXPORT(void) XCompositeReleaseOverlayWindow(void);
+void XCompositeReleaseOverlayWindow(void) {}
+DECLEXPORT(void) XCompositeUnredirectSubwindows(void);
+void XCompositeUnredirectSubwindows(void) {}
+DECLEXPORT(void) XCompositeUnredirectWindow(void);
+void XCompositeUnredirectWindow(void) {}
+DECLEXPORT(void) XCompositeVersion(void);
+void XCompositeVersion(void) {}
+
