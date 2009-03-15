cc8d67252fb511eb5c9f7364140a0bd47721a44a
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2009-03-15 17:29:59 +0000

    OSE fix
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@17891 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Main/win/NetIfList-win.cpp
+++ src/VBox/Main/win/NetIfList-win.cpp
@@ -2324,6 +2324,9 @@ int NetIfCreateHostOnlyNetworkInterface (VirtualBox *pVBox,
                                   IHostNetworkInterface **aHostNetworkInterface,
                                   IProgress **aProgress)
 {
+#ifndef VBOX_WITH_NETFLT
+    return VERR_NOT_IMPLEMENTED;
+#else
     /* create a progress object */
     ComObjPtr <Progress> progress;
     progress.createObject();
@@ -2369,12 +2372,16 @@ int NetIfCreateHostOnlyNetworkInterface (VirtualBox *pVBox,
     }
 
     return SUCCEEDED(rc) ? VINF_SUCCESS : VERR_GENERAL_FAILURE;
+#endif
 }
 
 int NetIfRemoveHostOnlyNetworkInterface (VirtualBox *pVBox, IN_GUID aId,
                                   IHostNetworkInterface **aHostNetworkInterface,
                                   IProgress **aProgress)
 {
+#ifndef VBOX_WITH_NETFLT
+    return VERR_NOT_IMPLEMENTED;
+#else
     /* create a progress object */
     ComObjPtr <Progress> progress;
     progress.createObject();
@@ -2413,6 +2420,7 @@ int NetIfRemoveHostOnlyNetworkInterface (VirtualBox *pVBox, IN_GUID aId,
     }
 
     return SUCCEEDED(rc) ? VINF_SUCCESS : VERR_GENERAL_FAILURE;
+#endif
 }
 
 int NetIfEnableStaticIpConfig(VirtualBox *vBox, HostNetworkInterface * pIf, ULONG ip, ULONG mask)
