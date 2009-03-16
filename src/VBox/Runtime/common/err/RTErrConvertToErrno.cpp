6a9620745fcd9cfd46396beee7a8f9087c23e582
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2009-03-16 16:19:53 +0000

    Runtime: reversed r44528
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@17951 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Runtime/common/err/RTErrConvertToErrno.cpp
+++ src/VBox/Runtime/common/err/RTErrConvertToErrno.cpp
@@ -440,8 +440,7 @@ RTDECL(int) RTErrConvertToErrno(int iErr)
 #endif
 
         default:
-            if (iErr > -1000)  /* VBox private errors */
-                AssertMsgFailed(("Unhandled error code %Rrc\n", iErr));
+            AssertMsgFailed(("Unhandled error code %Rrc\n", iErr));
 #ifdef EPROTO
             return EPROTO;
 #else
