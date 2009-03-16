b5ad3dfff453c978c407446046c929cc8af97226
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2009-03-16 11:51:49 +0000

    crOpenGL: add user visible warning if host 3d acceleration is disabled
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@17921 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Additions/common/crOpenGL/load.c
+++ src/VBox/Additions/common/crOpenGL/load.c
@@ -13,6 +13,7 @@
 #include "cr_environment.h"
 #include "cr_process.h"
 #include "cr_rand.h"
+#include "cr_netserver.h"
 #include "stub.h"
 #include <stdlib.h>
 #include <string.h>
@@ -20,8 +21,6 @@
 #ifndef WINDOWS
 #include <sys/types.h>
 #include <unistd.h>
-#else
-#include "cr_netserver.h"
 #endif
 #ifdef CHROMIUM_THREADSAFE
 #include "cr_threads.h"
@@ -514,7 +513,6 @@ stubInit(void)
 
     if (stub_initialized)
         return true;
-    stub_initialized = 1;
     
     stubInitVars();
 
@@ -522,6 +520,26 @@ stubInit(void)
     app_id = crGetenv( "CR_APPLICATION_ID_NUMBER" );
 
     crNetInit( NULL, NULL );
+
+#ifndef WINDOWS
+    {
+        CRNetServer ns;
+
+        ns.name = "vboxhgcm://host:0";
+        ns.buffer_size = 1024;
+        crNetServerConnect(&ns);
+        if (!ns.conn)
+        {
+            crWarning("Failed to connect to host. Make sure 3D acceleration is enabled for this VM.");
+            return false;
+        }
+        else
+        {
+            crNetFreeConnection(ns.conn);
+        }
+    }
+#endif
+
     strcpy(response, "3 0 array 1 feedback 2 pack");
     spuchain = crStrSplit( response, " " );
     num_spus = crStrToInt( spuchain[0] );
@@ -546,7 +564,6 @@ stubInit(void)
 
     // spu chain load failed somewhere
     if (!stub.spu) {
-        stub_initialized = 0;
         return false;
     }
 
@@ -576,6 +593,7 @@ raise(SIGINT);*/
     stub.bShmInitFailed = GL_FALSE;
 #endif
 
+    stub_initialized = 1;
     return true;
 }
 
