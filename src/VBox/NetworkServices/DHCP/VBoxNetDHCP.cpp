73c8c46ff08f9e4d1d2697655871644df933eea3
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2009-03-25 21:03:41 +0000

    VBoxNetDHCP: Added 'none' as a trunk type.
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@18282 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/NetworkServices/DHCP/VBoxNetDHCP.cpp
+++ src/VBox/NetworkServices/DHCP/VBoxNetDHCP.cpp
@@ -732,7 +732,9 @@ int VBoxNetDhcp::parseArgs(int argc, char **argv)
                 m_TrunkName = Val.psz;
                 break;
             case 'T':
-                if (!strcmp(Val.psz, "whatever"))
+                if (!strcmp(Val.psz, "none"))
+                    m_enmTrunkType = kIntNetTrunkType_None;
+                else if (!strcmp(Val.psz, "whatever"))
                     m_enmTrunkType = kIntNetTrunkType_WhateverNone;
                 else if (!strcmp(Val.psz, "netflt"))
                     m_enmTrunkType = kIntNetTrunkType_NetFlt;
