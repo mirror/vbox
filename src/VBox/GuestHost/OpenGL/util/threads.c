49d212e2b6f738a83566fba6adcd395cb2f174ff
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2009-03-26 16:23:13 +0000

    crOpenGL: tabs to spaces
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@18326 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/GuestHost/OpenGL/util/threads.c
+++ src/VBox/GuestHost/OpenGL/util/threads.c
@@ -3,7 +3,7 @@
  *
  * See the file LICENSE.txt for information on redistributing this software.
  */
-	
+    
 #include <stdio.h>
 #include "cr_threads.h"
 #include "cr_error.h"
@@ -22,18 +22,18 @@
 void crInitTSDF(CRtsd *tsd, void (*destructor)(void *))
 {
 #ifdef WINDOWS
-	tsd->key = TlsAlloc();
-	if (tsd->key == 0xffffffff) {
-		crError("crInitTSD failed!");
-	}
-	(void) destructor;
+    tsd->key = TlsAlloc();
+    if (tsd->key == 0xffffffff) {
+        crError("crInitTSD failed!");
+    }
+    (void) destructor;
 #else
-	if (pthread_key_create(&tsd->key, destructor) != 0) {
-		perror(INIT_TSD_ERROR);
-		crError("crInitTSD failed!");
-	}
+    if (pthread_key_create(&tsd->key, destructor) != 0) {
+        perror(INIT_TSD_ERROR);
+        crError("crInitTSD failed!");
+    }
 #endif
-	tsd->initMagic = INIT_MAGIC;
+    tsd->initMagic = INIT_MAGIC;
 }
 
 
@@ -46,35 +46,35 @@ void crInitTSD(CRtsd *tsd)
 void crFreeTSD(CRtsd *tsd)
 {
 #ifdef WINDOWS
-	/* Windows returns true on success, 0 on failure */
-	if (TlsFree(tsd->key) == 0) {
-		crError("crFreeTSD failed!");
-	}
+    /* Windows returns true on success, 0 on failure */
+    if (TlsFree(tsd->key) == 0) {
+        crError("crFreeTSD failed!");
+    }
 #else
-	if (pthread_key_delete(tsd->key) != 0) {
-		perror(FREE_TSD_ERROR);
-		crError("crFreeTSD failed!");
-	}
+    if (pthread_key_delete(tsd->key) != 0) {
+        perror(FREE_TSD_ERROR);
+        crError("crFreeTSD failed!");
+    }
 #endif
-	tsd->initMagic = 0x0;
+    tsd->initMagic = 0x0;
 }
 
 
 /* Set thread-specific data */
 void crSetTSD(CRtsd *tsd, void *ptr)
 {
-	if (tsd->initMagic != (int) INIT_MAGIC) {
-		/* initialize this CRtsd */
-		crInitTSD(tsd);
-	}
+    if (tsd->initMagic != (int) INIT_MAGIC) {
+        /* initialize this CRtsd */
+        crInitTSD(tsd);
+    }
 #ifdef WINDOWS
-	if (TlsSetValue(tsd->key, ptr) == 0) {
-		crError("crSetTSD failed!");
-	}
+    if (TlsSetValue(tsd->key, ptr) == 0) {
+        crError("crSetTSD failed!");
+    }
 #else
-	if (pthread_setspecific(tsd->key, ptr) != 0) {
-		crError("crSetTSD failed!");
-	}
+    if (pthread_setspecific(tsd->key, ptr) != 0) {
+        crError("crSetTSD failed!");
+    }
 #endif
 }
 
@@ -83,34 +83,34 @@ void crSetTSD(CRtsd *tsd, void *ptr)
 void *crGetTSD(CRtsd *tsd)
 {
 #ifdef WINDOWS
-	void * value;
-	DWORD  err;
-	LPVOID lpMsgBuf;
+    void * value;
+    DWORD  err;
+    LPVOID lpMsgBuf;
 #endif
-	if (tsd->initMagic != (int) INIT_MAGIC) {
-		crInitTSD(tsd);
-	}
+    if (tsd->initMagic != (int) INIT_MAGIC) {
+        crInitTSD(tsd);
+    }
 #ifdef WINDOWS
-	value = TlsGetValue(tsd->key);
-	if (!value) {
-		err = GetLastError();
-		if ( err != ERROR_SUCCESS ) {
-			FormatMessage(
-        		FORMAT_MESSAGE_ALLOCATE_BUFFER | 
-        		FORMAT_MESSAGE_FROM_SYSTEM |
-        		FORMAT_MESSAGE_IGNORE_INSERTS,
-        		NULL,
-        		err,
-        		MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
-        		(LPTSTR) &lpMsgBuf,
-        		0, NULL );
-			crError("crGetTSD failed with %d: %s", err, lpMsgBuf);
-			LocalFree(lpMsgBuf);
-		}
-	}
-	return value;
+    value = TlsGetValue(tsd->key);
+    if (!value) {
+        err = GetLastError();
+        if ( err != ERROR_SUCCESS ) {
+            FormatMessage(
+                FORMAT_MESSAGE_ALLOCATE_BUFFER | 
+                FORMAT_MESSAGE_FROM_SYSTEM |
+                FORMAT_MESSAGE_IGNORE_INSERTS,
+                NULL,
+                err,
+                MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
+                (LPTSTR) &lpMsgBuf,
+                0, NULL );
+            crError("crGetTSD failed with %d: %s", err, lpMsgBuf);
+            LocalFree(lpMsgBuf);
+        }
+    }
+    return value;
 #else
-	return pthread_getspecific(tsd->key);
+    return pthread_getspecific(tsd->key);
 #endif
 }
 
@@ -120,9 +120,9 @@ void *crGetTSD(CRtsd *tsd)
 unsigned long crThreadID(void)
 {
 #ifdef WINDOWS
-	return (unsigned long) GetCurrentThreadId();
+    return (unsigned long) GetCurrentThreadId();
 #else
-	return (unsigned long) pthread_self();
+    return (unsigned long) pthread_self();
 #endif
 }
 
@@ -131,9 +131,9 @@ unsigned long crThreadID(void)
 void crInitMutex(CRmutex *mutex)
 {
 #ifdef WINDOWS
-	InitializeCriticalSection(mutex);
+    InitializeCriticalSection(mutex);
 #else
-	pthread_mutex_init(mutex, NULL);
+    pthread_mutex_init(mutex, NULL);
 #endif
 }
 
@@ -141,9 +141,9 @@ void crInitMutex(CRmutex *mutex)
 void crFreeMutex(CRmutex *mutex)
 {
 #ifdef WINDOWS
-	DeleteCriticalSection(mutex);
+    DeleteCriticalSection(mutex);
 #else
-	pthread_mutex_destroy(mutex);
+    pthread_mutex_destroy(mutex);
 #endif
 }
 
@@ -151,9 +151,9 @@ void crFreeMutex(CRmutex *mutex)
 void crLockMutex(CRmutex *mutex)
 {
 #ifdef WINDOWS
-	EnterCriticalSection(mutex);
+    EnterCriticalSection(mutex);
 #else
-	pthread_mutex_lock(mutex);
+    pthread_mutex_lock(mutex);
 #endif
 }
 
@@ -161,9 +161,9 @@ void crLockMutex(CRmutex *mutex)
 void crUnlockMutex(CRmutex *mutex)
 {
 #ifdef WINDOWS
-	LeaveCriticalSection(mutex);
+    LeaveCriticalSection(mutex);
 #else
-	pthread_mutex_unlock(mutex);
+    pthread_mutex_unlock(mutex);
 #endif
 }
 
@@ -171,13 +171,13 @@ void crUnlockMutex(CRmutex *mutex)
 void crInitCondition(CRcondition *cond)
 {
 #ifdef WINDOWS
-	/* XXX fix me */
-	(void) cond;
+    /* XXX fix me */
+    (void) cond;
 #else
-	int err = pthread_cond_init(cond, NULL);
-	if (err) {
-		crError("crInitCondition failed");
-	}
+    int err = pthread_cond_init(cond, NULL);
+    if (err) {
+        crError("crInitCondition failed");
+    }
 #endif
 }
 
@@ -185,13 +185,13 @@ void crInitCondition(CRcondition *cond)
 void crFreeCondition(CRcondition *cond)
 {
 #ifdef WINDOWS
-	/* XXX fix me */
-	(void) cond;
+    /* XXX fix me */
+    (void) cond;
 #else
-	int err = pthread_cond_destroy(cond);
-	if (err) {
-		crError("crFreeCondition error (threads waiting on the condition?)");
-	}
+    int err = pthread_cond_destroy(cond);
+    if (err) {
+        crError("crFreeCondition error (threads waiting on the condition?)");
+    }
 #endif
 }
 
@@ -202,11 +202,11 @@ void crFreeCondition(CRcondition *cond)
 void crWaitCondition(CRcondition *cond, CRmutex *mutex)
 {
 #ifdef WINDOWS
-	/* XXX fix me */
-	(void) cond;
-	(void) mutex;
+    /* XXX fix me */
+    (void) cond;
+    (void) mutex;
 #else
-	pthread_cond_wait(cond, mutex);
+    pthread_cond_wait(cond, mutex);
 #endif
 }
 
@@ -214,10 +214,10 @@ void crWaitCondition(CRcondition *cond, CRmutex *mutex)
 void crSignalCondition(CRcondition *cond)
 {
 #ifdef WINDOWS
-	/* XXX fix me */
-	(void) cond;
+    /* XXX fix me */
+    (void) cond;
 #else
-	pthread_cond_signal(cond);
+    pthread_cond_signal(cond);
 #endif
 }
 
@@ -225,40 +225,40 @@ void crSignalCondition(CRcondition *cond)
 void crInitBarrier(CRbarrier *b, unsigned int count)
 {
 #ifdef WINDOWS
-	unsigned int i;
-	for (i = 0; i < count; i++)
-		b->hEvents[i] = CreateEvent(NULL, FALSE, FALSE, NULL);
+    unsigned int i;
+    for (i = 0; i < count; i++)
+        b->hEvents[i] = CreateEvent(NULL, FALSE, FALSE, NULL);
 #else
-	b->count = count;
-	b->waiting = 0;
-	pthread_cond_init( &(b->cond), NULL );
-	pthread_mutex_init( &(b->mutex), NULL );
+    b->count = count;
+    b->waiting = 0;
+    pthread_cond_init( &(b->cond), NULL );
+    pthread_mutex_init( &(b->mutex), NULL );
 #endif
 }
 
 
 void crFreeBarrier(CRbarrier *b)
 {
-	/* XXX anything to do? */
+    /* XXX anything to do? */
 }
 
 
 void crWaitBarrier(CRbarrier *b)
 {
 #ifdef WINDOWS
-	DWORD dwEvent
-		= WaitForMultipleObjects( b->count, b->hEvents, FALSE, INFINITE );
+    DWORD dwEvent
+        = WaitForMultipleObjects( b->count, b->hEvents, FALSE, INFINITE );
 #else
-	pthread_mutex_lock( &(b->mutex) );
-	b->waiting++;
-	if (b->waiting < b->count) {
-		pthread_cond_wait( &(b->cond), &(b->mutex) );
-	}
-	else {
-		pthread_cond_broadcast( &(b->cond) );
-		b->waiting = 0;
-	}
-	pthread_mutex_unlock( &(b->mutex) );
+    pthread_mutex_lock( &(b->mutex) );
+    b->waiting++;
+    if (b->waiting < b->count) {
+        pthread_cond_wait( &(b->cond), &(b->mutex) );
+    }
+    else {
+        pthread_cond_broadcast( &(b->cond) );
+        b->waiting = 0;
+    }
+    pthread_mutex_unlock( &(b->mutex) );
 #endif
 }
 
@@ -266,9 +266,9 @@ void crWaitBarrier(CRbarrier *b)
 void crInitSemaphore(CRsemaphore *s, unsigned int count)
 {
 #ifdef WINDOWS
-	crWarning("CRsemaphore functions not implemented on Windows");
+    crWarning("CRsemaphore functions not implemented on Windows");
 #else
-	sem_init(s, 0, count);
+    sem_init(s, 0, count);
 #endif
 }
 
@@ -276,9 +276,9 @@ void crInitSemaphore(CRsemaphore *s, unsigned int count)
 void crWaitSemaphore(CRsemaphore *s)
 {
 #ifdef WINDOWS
-	/* to do */
+    /* to do */
 #else
-	sem_wait(s);
+    sem_wait(s);
 #endif
 }
 
@@ -286,9 +286,9 @@ void crWaitSemaphore(CRsemaphore *s)
 void crSignalSemaphore(CRsemaphore *s)
 {
 #ifdef WINDOWS
-	/* to do */
+    /* to do */
 #else
-	sem_post(s);
+    sem_post(s);
 #endif
 }
 
