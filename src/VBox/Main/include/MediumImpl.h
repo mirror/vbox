3c2e45520b20e1aa0c46ac7e09f49a21f63ca3dc
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2009-03-26 13:59:14 +0000

    Main: a different approach to opening images read-only for import which does not assert on empty UUIDs
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@18313 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Main/include/MediumImpl.h
+++ src/VBox/Main/include/MediumImpl.h
@@ -142,7 +142,7 @@ protected:
     virtual Utf8Str name();
 
     virtual HRESULT setLocation (CBSTR aLocation);
-    virtual HRESULT queryInfo(bool fWrite);
+    virtual HRESULT queryInfo();
 
     /**
      * Performs extra checks if the medium can be closed and returns S_OK in
