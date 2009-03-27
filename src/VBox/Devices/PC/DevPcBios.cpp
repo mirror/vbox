81a9225bda2a51218eb24ef0e95c63e9fb405e6e
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2009-03-27 06:28:50 +0000

    DevPcBios: comments.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@18373 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Devices/PC/DevPcBios.cpp
+++ src/VBox/Devices/PC/DevPcBios.cpp
@@ -55,7 +55,7 @@
          0x18
          0x30
          0x31
-    Amount of memory above 16M:
+    Amount of memory above 16M and below 4GB in 64KB units:
          0x34
          0x35
     Boot device (BOCHS bios specific):
@@ -90,7 +90,7 @@
          0x58 - 0x5f
     Number of CPUs:
          0x60
-    RAM above 4G (in 64KB units):
+    RAM above 4G in 64KB units:
          0x61 - 0x65
 @endverbatim
  *
