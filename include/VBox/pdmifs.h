7010c2497a35e8af879f67c5b1cdc18bbc74070b
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2009-03-28 02:19:08 +0000

    pdmifs.h,DevATA,ATAController,DrvHostDVD: Changed the pcbBuf parameter of PDMIBLOCK::pfnSendCmd from size_t to uint32_t pointer to avoid a bunch of size_t conversion warnings.
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@18434 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- include/VBox/pdmifs.h
+++ include/VBox/pdmifs.h
@@ -645,7 +645,7 @@ typedef struct PDMIBLOCK
      * @param   cTimeoutMillies Command timeout in milliseconds.
      * @thread  Any thread.
      */
-    DECLR3CALLBACKMEMBER(int, pfnSendCmd,(PPDMIBLOCK pInterface, const uint8_t *pbCmd, PDMBLOCKTXDIR enmTxDir, void *pvBuf, size_t *pcbBuf, uint8_t *pabSense, size_t cbSense, uint32_t cTimeoutMillies));
+    DECLR3CALLBACKMEMBER(int, pfnSendCmd,(PPDMIBLOCK pInterface, const uint8_t *pbCmd, PDMBLOCKTXDIR enmTxDir, void *pvBuf, uint32_t *pcbBuf, uint8_t *pabSense, size_t cbSense, uint32_t cTimeoutMillies));
 
     /**
      * Check if the media is readonly or not.
