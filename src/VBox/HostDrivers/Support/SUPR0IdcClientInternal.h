1e46b21c4dbab822872bee1d5031478766284b3a
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2009-03-29 02:28:22 +0000

    SUPR0IdcClientInternal.h: Shut up warning about stupid intrinsics.
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@18500 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/HostDrivers/Support/SUPR0IdcClientInternal.h
+++ src/VBox/HostDrivers/Support/SUPR0IdcClientInternal.h
@@ -41,9 +41,11 @@
 #  define _InterlockedExchangeAdd        _InterlockedExchangeAdd_StupidDDKVsCompilerCrap
 #  define _InterlockedCompareExchange    _InterlockedCompareExchange_StupidDDKVsCompilerCrap
 #  define _InterlockedAddLargeStatistic  _InterlockedAddLargeStatistic_StupidDDKVsCompilerCrap
+#  pragma warning(disable : 4163)
 __BEGIN_DECLS
 #  include <ntddk.h>
 __END_DECLS
+#  pragma warning(default : 4163)
 #  undef  _InterlockedExchange
 #  undef  _InterlockedExchangeAdd
 #  undef  _InterlockedCompareExchange
