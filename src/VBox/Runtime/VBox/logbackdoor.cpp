9dbcd92fe5f9024954857a9d9f2a533d0a05d44d
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2009-03-30 12:26:58 +0000

    bah.
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@18537 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Runtime/VBox/logbackdoor.cpp
+++ src/VBox/Runtime/VBox/logbackdoor.cpp
@@ -90,7 +90,7 @@ RTDECL(void) RTLogWriteUser(const char *pch, size_t cb)
     if (cb > 1)
         ASMOutStrU8(RTLOG_DEBUG_PORT, pau8, cb);
     else if (cb)
-        ASMOutU8(RTLOG_DEBUG_PORT, *pu8);
+        ASMOutU8(RTLOG_DEBUG_PORT, *pau8);
 }
 
 # if defined(RT_OS_LINUX) && defined(IN_MODULE)
