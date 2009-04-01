4567386f8de1bd86aa1c7a0e314ad6c3e7d4e29d
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2009-04-01 15:53:54 +0000

    oops.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@18604 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/VMM/PGMInternal.h
+++ src/VBox/VMM/PGMInternal.h
@@ -2583,7 +2583,7 @@ typedef struct PGM
      * The size of this array is important, see pgmPhysEnsureHandyPage for details.
      * (The current size of 32 pages, means 128 KB of handy memory.)
      */
-    GMMPAGEDESC                     aHandyPages[/*32*/128];
+    GMMPAGEDESC                     aHandyPages[32];
 
     /** @name Release Statistics
      * @{ */
