d024e65ffbfbb61a4aee6612745db55c1683fbbe
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2009-04-02 12:47:58 +0000

    Guest Additions/common: Compile fixes for Windows platforms.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@18634 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Additions/common/VBoxService/VBoxServiceTimeSync.cpp
+++ src/VBox/Additions/common/VBoxService/VBoxServiceTimeSync.cpp
@@ -231,12 +231,12 @@ DECLCALLBACK(int) VBoxServiceTimeSyncWorker(bool volatile *pfShutdown)
                 if (RTTimeSpecGetMilli(&AbsDrift) > MinAdjust)
                 {
                     /*
-                     * The drift is to big, we have to make adjustments. :-/
+                     * The drift is too big, we have to make adjustments. :-/
                      * If we've got adjtime around, try that first - most
                      * *NIX systems have it. Fall back on settimeofday.
                      */
 #ifdef RT_OS_WINDOWS
-                    /* just make sure it compiles for now, but later:
+                    /* Just make sure it compiles for now, but later:
                      SetSystemTimeAdjustment and fall back on SetSystemTime.
                      */
                     AssertFatalFailed();
