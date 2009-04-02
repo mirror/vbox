ed35e3211a2d1583fe5b8cfbf26efd67bb071588
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2009-04-02 19:08:14 +0000

    Devices: Clean out the VBOX_WITH_NEW_PHYS_CODE #ifdefs.
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@18663 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Devices/PC/DevPcBios.cpp
+++ src/VBox/Devices/PC/DevPcBios.cpp
@@ -1223,7 +1223,6 @@ static DECLCALLBACK(void) pcbiosReset(PPDMDEVINS pDevIns)
     if (pThis->u8IOAPIC)
         pcbiosPlantMPStable(pDevIns, pThis->au8DMIPage + VBOX_DMI_TABLE_SIZE, pThis->cCpus);
 
-#ifdef VBOX_WITH_NEW_PHYS_CODE
     /*
      * Re-shadow the LAN ROM image and make it RAM/RAM.
      *
@@ -1257,7 +1256,6 @@ static DECLCALLBACK(void) pcbiosReset(PPDMDEVINS pDevIns)
         GCPhys += PAGE_SIZE;
         cPages--;
     }
-#endif
 }
 
 
@@ -1766,7 +1764,6 @@ static DECLCALLBACK(int)  pcbiosConstruct(PPDMDEVINS pDevIns, int iInstance, PCF
 
         rc = PDMDevHlpROMRegister(pDevIns, VBOX_LANBOOT_SEG << 4, cbLanBootBinary, pu8LanBootBinary,
                                   PGMPHYS_ROM_FLAGS_SHADOWED, "Net Boot ROM");
-#ifdef VBOX_WITH_NEW_PHYS_CODE
         if (RT_SUCCESS(rc))
         {
             rc = PDMDevHlpROMProtectShadow(pDevIns, VBOX_LANBOOT_SEG << 4, cbLanBootBinary, PGMROMPROT_READ_RAM_WRITE_RAM);
@@ -1774,7 +1771,6 @@ static DECLCALLBACK(int)  pcbiosConstruct(PPDMDEVINS pDevIns, int iInstance, PCF
             rc = PDMDevHlpPhysWrite(pDevIns, VBOX_LANBOOT_SEG << 4, pu8LanBootBinary, cbLanBootBinary);
             AssertRCReturn(rc, rc);
         }
-#endif
     }
 
     rc = CFGMR3QueryU8Def(pCfgHandle, "DelayBoot", &pThis->uBootDelay, 0);
