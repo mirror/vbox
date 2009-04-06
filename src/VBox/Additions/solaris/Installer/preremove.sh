3c9b0ffed12eb88f627be244fd86f180dd277f23
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2009-04-06 11:23:32 +0000

    Additions/Solaris/Installer: during uninstall move rather than copy while restoring libGL.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@18748 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Additions/solaris/Installer/preremove.sh
+++ src/VBox/Additions/solaris/Installer/preremove.sh
@@ -47,12 +47,12 @@ echo "Restoring Xorg..."
 # Restore crogl symlink mess
 # 32-bit crogl opengl library replacement
 if test -f "/usr/lib/VBoxOGL.so" && test -f "/usr/X11/lib/mesa/libGL_original_.so.1"; then
-    cp -f /usr/X11/lib/mesa/libGL_original_.so.1 /usr/X11/lib/mesa/libGL.so.1
+    mv -f /usr/X11/lib/mesa/libGL_original_.so.1 /usr/X11/lib/mesa/libGL.so.1
 fi
 
 # 64-bit crogl opengl library replacement
 if test -f "/usr/lib/amd64/VBoxOGL.so" && test -f "/usr/X11/lib/mesa/amd64/libGL_original_.so.1"; then
-    cp -f /usr/X11/lib/mesa/amd64/libGL_original_.so.1 /usr/X11/lib/mesa/amd64/libGL.so.1
+    mv -f /usr/X11/lib/mesa/amd64/libGL_original_.so.1 /usr/X11/lib/mesa/amd64/libGL.so.1
 fi
 
 
