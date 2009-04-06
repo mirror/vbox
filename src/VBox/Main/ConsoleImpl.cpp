0a802d2d15ca3cbc940ad9104c1e0e1c8d6ac293
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2009-04-06 13:36:45 +0000

    ConsoleImpl.cpp/powerUpThread: Deregister the error VMSetError callback since task will die soon after.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@18755 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Main/ConsoleImpl.cpp
+++ src/VBox/Main/ConsoleImpl.cpp
@@ -6498,6 +6498,10 @@ DECLCALLBACK (int) Console::powerUpThread (RTTHREAD Thread, void *pvUser)
                 HRESULT rc2 = console->powerDown();
                 AssertComRC (rc2);
             }
+
+            /* Deregister the VMSetError callback. */
+            VMR3AtErrorDeregister(pVM, task->mSetVMErrorCallback, task.get());
+            /** @todo register another VMSetError callback? */
         }
         else
         {
