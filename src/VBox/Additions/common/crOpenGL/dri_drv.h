ad0e6753a52544a48032d58206c6066f9cd34174
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2009-04-14 11:01:05 +0000

    crOpenGL: exported the remaining Additions bits to OSE
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@18882 cfe28804-0f27-0410-a406-dd0f0b0b656f

---
+++ src/VBox/Additions/common/crOpenGL/dri_drv.h
@@ -0,0 +1,24 @@
+/* $Id$ */
+
+/** @file
+ *
+ * VirtualBox guest OpenGL DRI header
+ */
+
+/*
+ * Copyright (C) 2009 Sun Microsystems, Inc.
+ *
+ * Sun Microsystems, Inc. confidential
+ * All rights reserved
+ */
+
+#ifndef ___CROPENGL_DRIDRV_H
+#define ___CROPENGL_DRIDRV_H
+
+#include "src/mesa/main/mtypes.h"
+#include "src/mesa/drivers/dri/common/dri_util.h"
+#include "src/mesa/glapi/dispatch.h"
+#include "src/mesa/main/dd.h"
+
+#endif /* ___CROPENGL_DRIDRV_H */
+
