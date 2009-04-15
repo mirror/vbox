d0d6f08e4abd353a32d6e8a651cf9a573805635a
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2009-04-15 08:16:44 +0000

    XPCOM: an additional hint to check the permissions of the /tmp directory
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@18899 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/libs/xpcom18a4/ipc/ipcd/daemon/src/ipcdUnix.cpp
+++ src/libs/xpcom18a4/ipc/ipcd/daemon/src/ipcdUnix.cpp
@@ -130,13 +130,23 @@ static Status AcquireDaemonLock(const char *baseDir)
 
     if (st.st_uid != getuid() && st.st_uid != geteuid())
     {
-        printf("Wrong owner (%d) of '%s'.\n", st.st_uid, baseDir);
+        printf("Wrong owner (%d) of '%s'", st.st_uid, baseDir);
+        if (   !stat("/tmp", &st)
+            && (st.st_mode & 07777) != 01777)
+            printf(" -- check /tmp permissions (%o should be 1777)\n",
+                    st.st_mode & 07777);
+        printf(".\n");
         return ELockFileOwner;
     }
 
     if (st.st_mode != (S_IRUSR | S_IWUSR | S_IXUSR | S_IFDIR))
     {
-        printf("Wrong mode (%o) of '%s'.\n", st.st_mode, baseDir);
+        printf("Wrong mode (%o) of '%s'", st.st_mode, baseDir);
+        if (   !stat("/tmp", &st)
+            && (st.st_mode & 07777) != 01777)
+            printf(" -- check /tmp permissions (%o should be 1777)\n",
+                    st.st_mode & 07777);
+        printf(".\n");
         return ELockFileOwner;
     }
 #endif
