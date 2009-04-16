c0be56ce259ef01e8b7bea1adc041269aa2c4dd9
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2009-04-16 23:41:48 +0000

    Main/NetIf-freebsd.cpp: export.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@18971 cfe28804-0f27-0410-a406-dd0f0b0b656f

---
+++ src/VBox/Main/freebsd/NetIf-freebsd.cpp
@@ -0,0 +1,37 @@
+/* $Id$ */
+/** @file
+ * Main - NetIfList, FreeBSD implementation.
+ */
+
+/*
+ * Copyright (C) 2008 Sun Microsystems, Inc.
+ *
+ * This file is part of VirtualBox Open Source Edition (OSE), as
+ * available from http://www.virtualbox.org. This file is free software;
+ * you can redistribute it and/or modify it under the terms of the GNU
+ * General Public License (GPL) as published by the Free Software
+ * Foundation, in version 2 as it comes in the "COPYING" file of the
+ * VirtualBox OSE distribution. VirtualBox OSE is distributed in the
+ * hope that it will be useful, but WITHOUT ANY WARRANTY of any kind.
+ *
+ * Please contact Sun Microsystems, Inc., 4150 Network Circle, Santa
+ * Clara, CA 95054 USA or visit http://www.sun.com if you need
+ * additional information or have any questions.
+ */
+
+
+
+/*******************************************************************************
+*   Header Files                                                               *
+*******************************************************************************/
+#define LOG_GROUP LOG_GROUP_MAIN
+
+#include "HostNetworkInterfaceImpl.h"
+#include "netif.h"
+#include "Logging.h"
+
+int NetIfList(std::list <ComObjPtr <HostNetworkInterface> > &list)
+{
+    /** @todo implement */
+    return VERR_NOT_IMPLEMENTED;
+}
