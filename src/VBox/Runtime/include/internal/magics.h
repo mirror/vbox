d1fab8a6ced3dac36da85032b3c90a4e816fa415
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2009-04-21 02:02:36 +0000

    IPRT/fileaio-linux.cpp: A quick code review and cleanup:   * RTFileAioCtxWait was a bit weird, tried to fix it.   * Added magics to the handle structures and checks for them in the IPRT tradition.   * Also follow the tradition for internal names.   * Limit variable scope where possible.   * The typical style adjustments.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@19040 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Runtime/include/internal/magics.h
+++ src/VBox/Runtime/include/internal/magics.h
@@ -40,6 +40,12 @@
 #define RTDIR_MAGIC                 0x19291112
 /** The value of RTDIR::u32Magic after RTDirClose().  */
 #define RTDIR_MAGIC_DEAD            0x19950829
+/** The value of RTFILEAIOCTXINT::u32Magic. (Howard Phillips Lovecraft) */
+#define RTFILEAIOCTX_MAGIC          0x18900820
+/** The value of RTFILEAIOCTXINT::u32Magic after RTFileAioCtxDestroy(). */
+#define RTFILEAIOCTX_MAGIC_DEAD     0x19370315
+/** The value of RTFILEAIOREQINT::u32Magic. (Stephen Edwin King)  */
+#define RTFILEAIOREQ_MAGIC          0x19470921
 /** Magic number for RTHANDLETABLEINT::u32Magic. (Hitomi Kanehara) */
 #define RTHANDLETABLE_MAGIC         0x19830808
 /** Magic number for RTHEAPSIMPLEINTERNAL::u32Magic. (Kyoichi Katayama) */
