ce144cc77725736c4eb2b485b87499e528d8936b
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2009-04-23 13:53:01 +0000

    Bumped saved state version
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@19142 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/VMM/PATM/PATMInternal.h
+++ src/VBox/VMM/PATM/PATMInternal.h
@@ -39,7 +39,8 @@
 /* Last version to use SSMR3Put/GetMem */
 # define PATM_SSM_VERSION_GETPUTMEM          53
 #else
-# define PATM_SSM_VERSION                    54
+# define PATM_SSM_VERSION                    55
+# define PATM_SSM_VERSION_FIXUP_HACK         54
 # define PATM_SSM_VERSION_FIXUP_HACK         54
 # define PATM_SSM_VERSION_VER16              53
 #endif
