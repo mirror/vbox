fcc8e65ae46aee6eda6f178ad966378b5ca0526e
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2009-04-27 14:29:37 +0000

    Fe/Qt: style fix
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@19215 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Frontends/VirtualBox/src/VBoxGlobal.cpp
+++ src/VBox/Frontends/VirtualBox/src/VBoxGlobal.cpp
@@ -695,8 +695,8 @@ static VBoxDefs::RenderMode vboxGetRenderMode (const char *aModeStr)
 # ifdef RT_ARCH_X86
     /* Quartz2DMode doesn't refresh correctly on 32-bit Snow Leopard, use image mode. */
     char szRelease[80];
-    if (    RT_SUCCESS(RTSystemQueryOSInfo(RTSYSOSINFO_RELEASE, szRelease, sizeof(szRelease)))
-        &&  !strncmp(szRelease, "10.", 3))
+    if (    RT_SUCCESS (RTSystemQueryOSInfo (RTSYSOSINFO_RELEASE, szRelease, sizeof (szRelease)))
+        &&  !strncmp (szRelease, "10.", 3))
         mode = VBoxDefs::QImageMode;
 # endif
 #elif (defined (Q_WS_WIN32) || defined (Q_WS_PM) || defined (Q_WS_X11)) && defined (VBOX_GUI_USE_QIMAGE)
