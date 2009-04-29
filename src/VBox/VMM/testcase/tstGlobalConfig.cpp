2c07fabab9706e33054cd244ca8dcbb7db4285ec
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2009-04-29 11:51:55 +0000

    Reapplied 46658 + fix
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@19257 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/VMM/testcase/tstGlobalConfig.cpp
+++ src/VBox/VMM/testcase/tstGlobalConfig.cpp
@@ -106,7 +106,7 @@ int main(int argc, char **argv)
     if (RT_SUCCESS(rc))
     {
         Req.pSession = pSession;
-        rc = SUPCallVMMR0Ex(NIL_RTR0PTR, enmOp, 0, &Req.Hdr);
+        rc = SUPCallVMMR0Ex(NIL_RTR0PTR, 0 /* VCPU 0 */, enmOp, 0, &Req.Hdr);
         if (RT_SUCCESS(rc))
         {
             if (enmOp == VMMR0_DO_GCFGM_QUERY_VALUE)
