19d756cba3e129cc27827731a73818f94c102ece
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2009-05-01 17:27:28 +0000

    EM: Fixed FF mixup causing cpu specific requests to be ignored.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@19298 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/VMM/EM.cpp
+++ src/VBox/VMM/EM.cpp
@@ -3421,7 +3421,7 @@ static int emR3ForcedActions(PVM pVM, PVMCPU pVCpu, int rc)
         /*
          * Requests from other threads.
          */
-        if (VM_FF_IS_PENDING_EXCEPT(pVM, VM_FF_REQUEST, VM_FF_PGM_NO_MEMORY))
+        if (VMCPU_FF_IS_PENDING_EXCEPT(pVCpu, VMCPU_FF_REQUEST, VM_FF_PGM_NO_MEMORY))
         {
             rc2 = VMR3ReqProcessU(pVM->pUVM, (VMREQDEST)pVCpu->idCpu);
             if (rc2 == VINF_EM_OFF || rc2 == VINF_EM_TERMINATE)
