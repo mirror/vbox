a4cd30ee9a78832be844f0f7443dd56cfad579ed
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2009-05-05 17:39:03 +0000

    SUPDrv: expose RTMpPokeCpu, minor version incremented.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@19394 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/HostDrivers/Support/SUPDrvIOC.h
+++ src/VBox/HostDrivers/Support/SUPDrvIOC.h
@@ -187,7 +187,7 @@ typedef SUPREQHDR *PSUPREQHDR;
  *          - Remove SUPR0PageAlloc in favor of SUPR0PageAllocEx, removing
  *            and renaming the related IOCtls too.
  */
-#define SUPDRV_IOC_VERSION                              0x000b0000
+#define SUPDRV_IOC_VERSION                              0x000b0001
 
 /** SUP_IOCTL_COOKIE. */
 typedef struct SUPCOOKIE
