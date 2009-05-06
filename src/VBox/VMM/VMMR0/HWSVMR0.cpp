d3e5b40d43e420762ef880def595e553a255d89a
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2009-05-06 19:46:33 +0000

    HWACCM: play safe and only change the state to VMCPUSTATE_STARTED if it's VMCPUSTATE_STARTED_EXEC.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@19460 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/VMM/VMMR0/HWSVMR0.cpp
+++ src/VBox/VMM/VMMR0/HWSVMR0.cpp
@@ -2130,7 +2130,7 @@ end:
         rc = VINF_EM_RAW_EMULATE_INSTR;
 
     /* Just set the correct state here instead of trying to catch every goto above. */
-    VMCPU_SET_STATE(pVCpu, VMCPUSTATE_STARTED);
+    VMCPU_CMPXCHG_STATE(pVCpu, VMCPUSTATE_STARTED, VMCPUSTATE_STARTED_EXEC);
 
     STAM_PROFILE_ADV_STOP(&pVCpu->hwaccm.s.StatExit1, x);
     return rc;
