24854f327f9191ca14187b255196eb3472df4827
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2009-05-07 17:37:36 +0000

    PDMR3VMMDevHeapAlloc: disabled annoying assertion.
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@19493 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/VMM/PDM.cpp
+++ src/VBox/VMM/PDM.cpp
@@ -1409,7 +1409,12 @@ VMMR3DECL(int) PDMR3UnregisterVMMDevHeap(PVM pVM, RTGCPHYS GCPhys)
  */
 VMMR3DECL(int) PDMR3VMMDevHeapAlloc(PVM pVM, unsigned cbSize, RTR3PTR *ppv)
 {
+#ifdef DEBUG_bird
+    if (cbSize && cbSize <= pVM->pdm.s.cbVMMDevHeapLeft)
+        return VERR_NO_MEMORY;
+#else
     AssertReturn(cbSize && cbSize <= pVM->pdm.s.cbVMMDevHeapLeft, VERR_NO_MEMORY);
+#endif
 
     Log(("PDMR3VMMDevHeapAlloc %x\n", cbSize));
 
