2805e72009eb2fdc078d7e551b05b2ccf9124d08
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2009-05-07 12:21:38 +0000

    Solaris/VBoxNetFlt: redundant prototypes.
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@19483 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/HostDrivers/VBoxNetFlt/solaris/VBoxNetFlt-solaris.c
+++ src/VBox/HostDrivers/VBoxNetFlt/solaris/VBoxNetFlt-solaris.c
@@ -131,20 +131,6 @@ static int VBoxNetFltSolarisModClose(queue_t *pQueue, int fFile, cred_t *pCred);
 static int VBoxNetFltSolarisModReadPut(queue_t *pQueue, mblk_t *pMsg);
 static int VBoxNetFltSolarisModWritePut(queue_t *pQueue, mblk_t *pMsg);
 
-/**
- * OS specific hooks invoked from common VBoxNetFlt ring-0.
- */
-/** @todo r=bird: What are these doing here? */
-bool vboxNetFltPortOsIsPromiscuous(PVBOXNETFLTINS pThis);
-void vboxNetFltPortOsGetMacAddress(PVBOXNETFLTINS pThis, PRTMAC pMac);
-bool vboxNetFltPortOsIsHostMac(PVBOXNETFLTINS pThis, PCRTMAC pMac);
-void vboxNetFltPortOsSetActive(PVBOXNETFLTINS pThis, bool fActive);
-int vboxNetFltOsDisconnectIt(PVBOXNETFLTINS pThis);
-int vboxNetFltOsConnectIt(PVBOXNETFLTINS pThis);
-void vboxNetFltOsDeleteInstance(PVBOXNETFLTINS pThis);
-int vboxNetFltOsInitInstance(PVBOXNETFLTINS pThis, void *pvContext);
-int vboxNetFltOsPreInitInstance(PVBOXNETFLTINS pThis);
-
 
 /*******************************************************************************
 *   Structures and Typedefs                                                    *
