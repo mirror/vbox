f32fe721a863a123e6b30411fd1cfff8b58aaad6
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2009-05-07 19:37:20 +0000

    Fixed r47022 regression
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@19504 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/VMM/PGMInternal.h
+++ src/VBox/VMM/PGMInternal.h
@@ -2871,7 +2871,8 @@ typedef PGMCPU *PPGMCPU;
 /** Check guest mapping in SyncCR3. */
 #define PGM_SYNC_MAP_CR3                        RT_BIT(3)
 /** Clear the page pool (a light weight flush). */
-#define PGM_GLOBAL_SYNC_CLEAR_PGM_POOL          RT_BIT(8)
+#define PGM_GLOBAL_SYNC_CLEAR_PGM_POOL_BIT      8
+#define PGM_GLOBAL_SYNC_CLEAR_PGM_POOL          RT_BIT(PGM_GLOBAL_SYNC_CLEAR_PGM_POOL_BIT)
 /** @} */
 
 
