ca6704218414d878130eff1b2024bbe67068cd87
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2009-05-07 17:45:25 +0000

    VM: Name the EMTs differently if there are more than one.
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@19495 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/VMM/VM.cpp
+++ src/VBox/VMM/VM.cpp
@@ -446,8 +446,9 @@ static int vmR3CreateUVM(uint32_t cCpus, PUVM *ppUVM)
                          */
                         for (i = 0; i < cCpus; i++)
                         {
-                            rc = RTThreadCreate(&pUVM->aCpus[i].vm.s.ThreadEMT, vmR3EmulationThread, &pUVM->aCpus[i], _1M,
-                                                RTTHREADTYPE_EMULATION, RTTHREADFLAGS_WAITABLE, "EMT");
+                            rc = RTThreadCreateF(&pUVM->aCpus[i].vm.s.ThreadEMT, vmR3EmulationThread, &pUVM->aCpus[i], _1M,
+                                                 RTTHREADTYPE_EMULATION, RTTHREADFLAGS_WAITABLE,
+                                                 cCpus > 1 ? "EMT-%u" : "EMT", i);
                             if (RT_FAILURE(rc))
                                 break;
 
