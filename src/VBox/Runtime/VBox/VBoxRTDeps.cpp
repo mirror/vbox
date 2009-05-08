5a6a2ab1ba6f1e0f0fffb366f239e17c44fe2edd
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2009-05-08 23:10:04 +0000

    VboxRTDeps.cpp: openssl stuff for darwin.
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@19555 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Runtime/VBox/VBoxRTDeps.cpp
+++ src/VBox/Runtime/VBox/VBoxRTDeps.cpp
@@ -36,6 +36,12 @@
 #include <iprt/assert.h>
 #ifdef VBOX_WITH_LIBXML2_IN_VBOXRT
 # include <libxml/xmlmodule.h>
+# include <libxml/globals.h>
+# include <openssl/md5.h>
+# include <openssl/rc4.h>
+# include <openssl/pem.h>
+# include <openssl/x509.h>
+# include <openssl/rsa.h>
 #endif
 
 
@@ -48,6 +54,14 @@ PFNRT g_VBoxRTDeps[] =
     (PFNRT)SUPPageLock,
 #ifdef VBOX_WITH_LIBXML2_IN_VBOXRT
     (PFNRT)xmlModuleOpen,
+    (PFNRT)MD5_Init,
+    (PFNRT)RC4,
+    (PFNRT)RC4_set_key,
+    (PFNRT)PEM_read_bio_X509,
+    (PFNRT)PEM_read_bio_PrivateKey,
+    (PFNRT)X509_free,
+    (PFNRT)i2d_X509,
+    (PFNRT)RSA_generate_key,
 #endif
     (PFNRT)RTAssertShouldPanic
 };
