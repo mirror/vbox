1752a318e55012d0c3da1032647b8e2c77c0ada8
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2009-05-11 14:33:04 +0000

    Do not lock IFramebuffer while it is being resized (xTracker #3902).
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@19588 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Main/DisplayImpl.cpp
+++ src/VBox/Main/DisplayImpl.cpp
@@ -318,11 +318,6 @@ int Display::handleDisplayResize (unsigned uScreenId, uint32_t bpp, void *pvVRAM
         return VINF_VGA_RESIZE_IN_PROGRESS;
     }
 
-    /* The framebuffer is locked in the state.
-     * The lock is kept, because the framebuffer is in undefined state.
-     */
-    maFramebuffers[uScreenId].pFramebuffer->Lock();
-
     int rc = callFramebufferResize (maFramebuffers[uScreenId].pFramebuffer, uScreenId,
                                     pixelFormat, pvVRAM, bpp, cbLine, w, h);
     if (rc == VINF_VGA_RESIZE_IN_PROGRESS)
@@ -416,12 +411,6 @@ void Display::handleResizeCompletedEMT (void)
         /* Inform VRDP server about the change of display parameters. */
         LogFlowFunc (("Calling VRDP\n"));
         mParent->consoleVRDPServer()->SendResize();
-
-        if (!pFBInfo->pFramebuffer.isNull())
-        {
-            /* Unlock framebuffer after evrything is done. */
-            pFBInfo->pFramebuffer->Unlock();
-        }
     }
 }
 
