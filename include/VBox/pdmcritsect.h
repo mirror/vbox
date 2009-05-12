09ff48874600a2d5c50a624f497bb5d8a973a453
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2009-05-12 08:48:13 +0000

    Cleanup
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@19595 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- include/VBox/pdmcritsect.h
+++ include/VBox/pdmcritsect.h
@@ -58,20 +58,20 @@ typedef PDMCRITSECT *PPDMCRITSECT;
 /** Pointer to a const PDM critical section. */
 typedef const PDMCRITSECT *PCPDMCRITSECT;
 
-VMMR3DECL(int)  PDMR3CritSectInit(PVM pVM, PPDMCRITSECT pCritSect, const char *pszName);
-VMMDECL(int)    PDMCritSectEnter(PPDMCRITSECT pCritSect, int rcBusy);
-VMMDECL(int)    PDMCritSectTryEnter(PPDMCRITSECT pCritSect);
-VMMR3DECL(int)  PDMR3CritSectEnterEx(PPDMCRITSECT pCritSect, bool fCallHost);
-VMMDECL(void)   PDMCritSectLeave(PPDMCRITSECT pCritSect);
-VMMDECL(bool)   PDMCritSectIsOwner(PCPDMCRITSECT pCritSect);
-VMMDECL(bool)   PDMCritSectIsLocked(PCPDMCRITSECT pCritSect);
-VMMDECL(bool)   PDMCritSectIsOwnerEx(PCPDMCRITSECT pCritSect, VMCPUID idCpu);
-VMMDECL(bool)   PDMCritSectIsInitialized(PCPDMCRITSECT pCritSect);
-VMMDECL(uint32_t) PDMCritSectGetRecursion(PCPDMCRITSECT pCritSect);
-VMMR3DECL(int)  PDMR3CritSectScheduleExitEvent(PPDMCRITSECT pCritSect, RTSEMEVENT EventToSignal);
-VMMR3DECL(int)  PDMR3CritSectDelete(PPDMCRITSECT pCritSect);
-VMMDECL(int)    PDMR3CritSectTerm(PVM pVM);
-VMMR3DECL(void) PDMR3CritSectFF(PVMCPU pVCpu);
+VMMR3DECL(int)      PDMR3CritSectInit(PVM pVM, PPDMCRITSECT pCritSect, const char *pszName);
+VMMDECL(int)        PDMCritSectEnter(PPDMCRITSECT pCritSect, int rcBusy);
+VMMDECL(int)        PDMCritSectTryEnter(PPDMCRITSECT pCritSect);
+VMMR3DECL(int)      PDMR3CritSectEnterEx(PPDMCRITSECT pCritSect, bool fCallHost);
+VMMDECL(void)       PDMCritSectLeave(PPDMCRITSECT pCritSect);
+VMMDECL(bool)       PDMCritSectIsOwner(PCPDMCRITSECT pCritSect);
+VMMDECL(bool)       PDMCritSectIsLocked(PCPDMCRITSECT pCritSect);
+VMMDECL(bool)       PDMCritSectIsOwnerEx(PCPDMCRITSECT pCritSect, VMCPUID idCpu);
+VMMDECL(bool)       PDMCritSectIsInitialized(PCPDMCRITSECT pCritSect);
+VMMDECL(uint32_t)   PDMCritSectGetRecursion(PCPDMCRITSECT pCritSect);
+VMMR3DECL(int)      PDMR3CritSectScheduleExitEvent(PPDMCRITSECT pCritSect, RTSEMEVENT EventToSignal);
+VMMR3DECL(int)      PDMR3CritSectDelete(PPDMCRITSECT pCritSect);
+VMMDECL(int)        PDMR3CritSectTerm(PVM pVM);
+VMMR3DECL(void)     PDMR3CritSectFF(PVMCPU pVCpu);
 VMMR3DECL(uint32_t) PDMR3CritSectCountOwned(PVM pVM, char *pszNames, size_t cbNames);
 
 /** @} */
