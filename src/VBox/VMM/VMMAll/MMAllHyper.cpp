e48180383d3dc06466408e7918d6857e7dbe904e
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2009-05-13 15:06:00 +0000

    Protect the MM hypervisor heap with a critical section.
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@19663 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/VMM/VMMAll/MMAllHyper.cpp
+++ src/VBox/VMM/VMMAll/MMAllHyper.cpp
@@ -151,6 +151,54 @@ static int mmHyperFree(PMMHYPERHEAP pHeap, PMMHYPERCHUNK pChunk);
 static void mmHyperHeapCheck(PMMHYPERHEAP pHeap);
 #endif
 
+/**
+ * Locks the hypervisor heap.
+ * This might call back to Ring-3 in order to deal with lock contention in GC and R3.
+ *
+ * @param   pVM     The VM handle.
+ */
+VMMDECL(int) MMHyperLock(PVM pVM)
+{
+    PMMHYPERHEAP pHeap = pVM->mm.s.CTX_SUFF(pHyperHeap);
+
+#ifdef IN_RING3
+    if (!PDMCritSectIsInitialized(&pHeap->Lock))
+        return VINF_SUCCESS;     /* early init */
+
+    int rc = PDMCritSectEnter(&pHeap->Lock, VERR_INTERNAL_ERROR);
+#else
+    Assert(PDMCritSectIsInitialized(&pHeap->Lock));
+    int rc = PDMCritSectEnter(&pHeap->Lock, VERR_GENERAL_FAILURE);
+    if (rc == VERR_GENERAL_FAILURE)
+    {
+# ifdef IN_RC
+        rc = VMMGCCallHost(pVM, VMMCALLHOST_MMHYPER_LOCK, 0);
+# else
+        rc = VMMR0CallHost(pVM, VMMCALLHOST_MMHYPER_LOCK, 0);
+# endif
+    }
+#endif
+    AssertRC(rc);
+    return rc;
+}
+
+
+/**
+ * Unlocks the hypervisor heap.
+ *
+ * @param   pVM     The VM handle.
+ */
+VMMDECL(void) MMHyperUnlock(PVM pVM)
+{
+    PMMHYPERHEAP pHeap = pVM->mm.s.CTX_SUFF(pHyperHeap);
+
+#ifdef IN_RING3
+    if (!PDMCritSectIsInitialized(&pHeap->Lock))
+        return;     /* early init */
+#endif
+    Assert(PDMCritSectIsInitialized(&pHeap->Lock));
+    PDMCritSectLeave(&pHeap->Lock);
+}
 
 /**
  * Allocates memory in the Hypervisor (RC VMM) area.
@@ -165,9 +213,8 @@ static void mmHyperHeapCheck(PMMHYPERHEAP pHeap);
  * @param   enmTag      The statistics tag.
  * @param   ppv         Where to store the address to the allocated
  *                      memory.
- * @remark  This is assumed not to be used at times when serialization is required.
  */
-VMMDECL(int) MMHyperAlloc(PVM pVM, size_t cb, unsigned uAlignment, MMTAG enmTag, void **ppv)
+static int mmHyperAllocInternal(PVM pVM, size_t cb, unsigned uAlignment, MMTAG enmTag, void **ppv)
 {
     AssertMsg(cb >= 8, ("Hey! Do you really mean to allocate less than 8 bytes?! cb=%d\n", cb));
 
@@ -281,7 +328,21 @@ VMMDECL(int) MMHyperAlloc(PVM pVM, size_t cb, unsigned uAlignment, MMTAG enmTag,
     return VERR_MM_HYPER_NO_MEMORY;
 }
 
+/**
+ * Wrapper for mmHyperAllocInternal
+ */
+VMMDECL(int) MMHyperAlloc(PVM pVM, size_t cb, unsigned uAlignment, MMTAG enmTag, void **ppv)
+{
+    int rc;
 
+    rc = MMHyperLock(pVM);
+    AssertRCReturn(rc, rc);
+
+    rc = mmHyperAllocInternal(pVM, cb, uAlignment, enmTag, ppv);
+
+    MMHyperUnlock(pVM);
+    return rc;
+}
 
 /**
  * Allocates a chunk of memory from the specified heap.
@@ -704,7 +765,7 @@ static void mmR3HyperStatRegisterOne(PVM pVM, PMMHYPERSTAT pStat)
  * @param   pv          The memory to free.
  * @remark  Try avoid free hyper memory.
  */
-VMMDECL(int) MMHyperFree(PVM pVM, void *pv)
+static int mmHyperFreeInternal(PVM pVM, void *pv)
 {
     Log2(("MMHyperFree: pv=%p\n", pv));
     if (!pv)
@@ -757,7 +818,7 @@ VMMDECL(int) MMHyperFree(PVM pVM, void *pv)
     AssertMsgReturn(pHeap->u32Magic == MMHYPERHEAP_MAGIC,
                     ("%p: u32Magic=%#x\n", pv, pHeap->u32Magic),
                     VERR_INVALID_POINTER);
-Assert(pHeap == pVM->mm.s.CTX_SUFF(pHyperHeap));
+    Assert(pHeap == pVM->mm.s.CTX_SUFF(pHyperHeap));
 
     /* Some more verifications using additional info from pHeap. */
     AssertMsgReturn((uintptr_t)pChunk + offPrev >= (uintptr_t)pHeap->CTX_SUFF(pbHeap),
@@ -843,6 +904,23 @@ Assert(pHeap == pVM->mm.s.CTX_SUFF(pHyperHeap));
         pStat->cFailures++;
 #endif
 
+    return rc;
+}
+
+
+/**
+ * Wrapper for mmHyperFreeInternal
+ */
+VMMDECL(int) MMHyperFree(PVM pVM, void *pv)
+{
+    int rc;
+
+    rc = MMHyperLock(pVM);
+    AssertRCReturn(rc, rc);
+
+    rc = mmHyperFreeInternal(pVM, pv);
+
+    MMHyperUnlock(pVM);
     return rc;
 }
 
