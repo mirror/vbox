2f293e087aac5608435faeae72e34024d2843592
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2009-05-20 11:50:09 +0000

    Python: copyrights, removed stalled code
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@19859 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Frontends/VBoxShell/shellcommon.py
+++ src/VBox/Frontends/VBoxShell/shellcommon.py
@@ -1,3 +1,19 @@
+#
+# Copyright (C) 2009 Sun Microsystems, Inc.
+#
+# This file is part of VirtualBox Open Source Edition (OSE), as
+# available from http://www.virtualbox.org. This file is free software;
+# you can redistribute it and/or modify it under the terms of the GNU
+# General Public License (GPL) as published by the Free Software
+# Foundation, in version 2 as it comes in the "COPYING" file of the
+# VirtualBox OSE distribution. VirtualBox OSE is distributed in the
+# hope that it will be useful, but WITHOUT ANY WARRANTY of any kind.
+#
+# Please contact Sun Microsystems, Inc., 4150 Network Circle, Santa
+# Clara, CA 95054 USA or visit http://www.sun.com if you need
+# additional information or have any questions.
+#
+#
 import traceback
 import sys
 import pdb
@@ -498,12 +514,7 @@ def runCommand(ctx, cmd):
 def interpret(ctx):
     vbox = ctx['vb']
     print "Running VirtualBox version %s" %(vbox.version)
-
-    # MSCOM doesn't work with collector yet
-    if ctx['type'] != 'MSCOM':
-       ctx['perf'] = PerfCollector(vbox)
-    else:
-       ctx['perf'] = None
+    ctx['perf'] = PerfCollector(vbox)
 
     autoCompletion(commands, ctx)
 
