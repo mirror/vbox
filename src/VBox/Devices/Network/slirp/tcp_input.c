0c18b4c64031e1a0187f29c4590fe2ed9a577409
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2009-05-22 05:06:46 +0000

    NAT: attempt to fix (workaround) #3894.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@19899 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Devices/Network/slirp/tcp_input.c
+++ src/VBox/Devices/Network/slirp/tcp_input.c
@@ -296,6 +296,14 @@ tcp_input(PNATState pData, register struct mbuf *m, int iphlen, struct socket *i
 
         so->so_m = 0;
         ti = so->so_ti;
+		/* @todo (r -vvl) clarify why it might happens */
+		if (ti == NULL)
+		{
+			LogRel(("NAT: ti is null. can't do any reseting connection actions\n"));
+			/* mbuf should be cleared in sofree called from tcp_close */
+			tcp_close(pData, tp);
+			return;
+		}
         tiwin = ti->ti_win;
         tiflags = ti->ti_flags;
 
