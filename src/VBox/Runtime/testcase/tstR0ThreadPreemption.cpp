22838a3dfb4ca5d07894a9088c2fbcc688aa2a6d
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2009-05-23 15:46:49 +0000

    tstR0ThreadPreemption.cpp: use thousand separators to make the ns and loop count numbers readable.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@19943 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Runtime/testcase/tstR0ThreadPreemption.cpp
+++ src/VBox/Runtime/testcase/tstR0ThreadPreemption.cpp
@@ -109,12 +109,12 @@ DECLEXPORT(int) TSTR0ThreadPreemptionSrvReqHandler(PSUPDRVSESSION pSession, uint
                     } while (   !fPending
                              && cNanosElapsed < UINT64_C(60)*1000U*1000U*1000U);
                     if (!fPending)
-                        RTStrPrintf(pszErr, cchErr, "!Preempt not pending after %llu loops / %llu ns",
+                        RTStrPrintf(pszErr, cchErr, "!Preempt not pending after %'llu loops / %'llu ns",
                                     cLoops, cNanosElapsed);
                     else if (cLoops == 1)
                         RTStrPrintf(pszErr, cchErr, "!cLoops=0\n");
                     else
-                        RTStrPrintf(pszErr, cchErr, "RTThreadPreemptIsPending returned true after %llu loops / %llu ns",
+                        RTStrPrintf(pszErr, cchErr, "RTThreadPreemptIsPending returned true after %'llu loops / %'llu ns",
                                     cLoops, cNanosElapsed);
                 }
                 else
