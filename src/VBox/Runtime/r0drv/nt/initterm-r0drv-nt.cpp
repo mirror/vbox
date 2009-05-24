dba489c47d358ab5438eeb262b1eccdbecd94e77
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2009-05-24 16:21:24 +0000

    IPRT/r0drv-nt: Cleaning up the preemption hacks; XP SP2 is done.
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@19969 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Runtime/r0drv/nt/initterm-r0drv-nt.cpp
+++ src/VBox/Runtime/r0drv/nt/initterm-r0drv-nt.cpp
@@ -32,9 +32,10 @@
 *   Header Files                                                               *
 *******************************************************************************/
 #include "the-nt-kernel.h"
-#include <iprt/err.h>
 #include <iprt/assert.h>
+#include <iprt/err.h>
 #include <iprt/mp.h>
+#include <iprt/string.h>
 #include "internal/initterm.h"
 #include "internal-r0drv-nt.h"
 
@@ -49,12 +50,20 @@
  * Once we start caring about this, we'll simply let the native MP event callback
  * and update this variable as CPUs comes online. (The code is done already.)
  */
-RTCPUSET g_rtMpNtCpuSet;
+RTCPUSET                    g_rtMpNtCpuSet;
 
 /** ExSetTimerResolution, introduced in W2K. */
-PFNMYEXSETTIMERRESOLUTION g_pfnrtNtExSetTimerResolution;
+PFNMYEXSETTIMERRESOLUTION   g_pfnrtNtExSetTimerResolution;
 /** KeFlushQueuedDpcs, introduced in XP. */
-PFNMYKEFLUSHQUEUEDDPCS g_pfnrtNtKeFlushQueuedDpcs;
+PFNMYKEFLUSHQUEUEDDPCS      g_pfnrtNtKeFlushQueuedDpcs;
+
+/** Offset of the _KPRCB::QuantumEnd field. 0 if not found. */
+uint32_t                    g_offrtNtPbQuantumEnd;
+/** Size of the _KPRCB::QuantumEnd field. 0 if not found. */
+uint32_t                    g_cbrtNtPbQuantumEnd;
+/** Offset of the _KPRCB::DpcQueueDepth field. 0 if not found. */
+uint32_t                    g_offrtNtPbDpcQueueDepth;
+
 
 
 int rtR0InitNative(void)
@@ -75,6 +84,74 @@ int rtR0InitNative(void)
 
     RtlInitUnicodeString(&RoutineName, L"KeFlushQueuedDpcs");
     g_pfnrtNtKeFlushQueuedDpcs = (PFNMYKEFLUSHQUEUEDDPCS)MmGetSystemRoutineAddress(&RoutineName);
+
+    /*
+     * Get some info that might come in handy below.
+     */
+    ULONG MajorVersion = 0;
+    ULONG MinorVersion = 0;
+    ULONG BuildNumber  = 0;
+    PsGetVersion(&MajorVersion, &MinorVersion, &BuildNumber, NULL);
+
+    KIRQL OldIrql;
+    KeRaiseIrql(DISPATCH_LEVEL, &OldIrql); /* make sure we stay on the same cpu */
+
+    union
+    {
+        uint32_t auRegs[4];
+        char szVendor[4*3+1];
+    } u;
+    ASMCpuId(0, &u.auRegs[3], &u.auRegs[0], &u.auRegs[2], &u.auRegs[1]);
+    u.szVendor[4*3] = '\0';
+
+    /*
+     * Try find _KPRCB::QuantumEnd and possibly also _KPRCB::DpcQueueDepth.
+     */
+    __try
+    {
+        /* HACK ALERT! The offsets are from poking around in windbg. */
+#if defined(RT_ARCH_X86)
+        PKPCR    pPcr   = (PKPCR)__readfsdword(RT_OFFSETOF(KPCR,SelfPcr));
+        uint8_t *pbPrcb = (uint8_t *)pPcr->Prcb;
+
+        if (    BuildNumber == 2600                             /* XP SP2 */
+            &&  !memcmp(&pbPrcb[0x900], &u.szVendor[0], 4*3))
+        {
+            g_offrtNtPbQuantumEnd    = 0x88c;
+            g_cbrtNtPbQuantumEnd     = 4;
+            g_offrtNtPbDpcQueueDepth = 0x870;
+        }
+        /** @todo more */
+        //pbQuantumEnd = (uint8_t volatile *)pPcr->Prcb + 0x1a41;
+
+#elif defined(RT_ARCH_AMD64)
+        PKPCR    pPcr   = (PKPCR)__readgsqword(RT_OFFSETOF(KPCR,Self));
+        uint8_t *pbPrcb = (uint8_t *)pPcr->CurrentPrcb;
+        /** @todo proper detection! */
+        if (pbPrcb[0x3375] <= 1)
+        {
+            g_offrtNtPbQuantumEnd    = 0x3375;
+            g_cbrtNtPbQuantumEnd     = 1;
+            g_offrtNtPbDpcQueueDepth = 0;
+        }
+
+#else
+# error "port me"
+#endif
+    }
+    __except(EXCEPTION_EXECUTE_HANDLER)
+    {
+        g_offrtNtPbQuantumEnd    = 0;
+        g_cbrtNtPbQuantumEnd     = 0;
+        g_offrtNtPbDpcQueueDepth = 0;
+    }
+
+    KeLowerIrql(OldIrql);
+
+#ifndef IN_GUEST /** @todo fix above for all Nt versions. */
+    if (!g_offrtNtPbQuantumEnd && !g_offrtNtPbDpcQueueDepth)
+        DbgPrint("IPRT: Neither _KPRCB::QuantumEnd nor _KPRCB::DpcQueueDepth was not found!\n");
+#endif
 
     return VINF_SUCCESS;
 }
--- src/VBox/Runtime/r0drv/nt/thread-r0drv-nt.cpp
+++ src/VBox/Runtime/r0drv/nt/thread-r0drv-nt.cpp
@@ -38,6 +38,8 @@
 #include <iprt/assert.h>
 #include <iprt/asm.h>
 
+#include "internal-r0drv-nt.h"
+
 __BEGIN_DECLS
 NTSTATUS NTAPI ZwYieldExecution(void);
 __END_DECLS
@@ -91,27 +93,57 @@ RTDECL(bool) RTThreadPreemptIsPending(RTTHREAD hThread)
 {
     Assert(hThread == NIL_RTTHREAD);
 
-    uint8_t volatile *pbQuantumEnd;
-    RTCCUINTREG       fSavedFlags  = ASMIntDisableFlags();
+    /*
+     * Read the globals and check if they are useful.
+     */
+    uint32_t const offQuantumEnd     = g_offrtNtPbQuantumEnd;
+    uint32_t const cbQuantumEnd      = g_cbrtNtPbQuantumEnd;
+    uint32_t const offDpcQueueDepth  = g_offrtNtPbDpcQueueDepth;
+    if (!offQuantumEnd && !cbQuantumEnd && !offDpcQueueDepth)
+        return false;
+    Assert((offQuantumEnd && cbQuantumEnd) || (!offQuantumEnd && !cbQuantumEnd));
 
-#if   defined(RT_ARCH_X86)
-    /* HACK ALERT! The offset is from ks386.inc. */
-    PKPCR pPcr = (PKPCR)__readfsdword(RT_OFFSETOF(KPCR,SelfPcr));
-    pbQuantumEnd = (uint8_t volatile *)pPcr->Prcb + 0x1a41;
+    /*
+     * Disable interrupts so we won't be messed around.
+     */
+    bool            fPending;
+    RTCCUINTREG     fSavedFlags  = ASMIntDisableFlags();
+
+#ifdef RT_ARCH_X86
+    PKPCR       pPcr   = (PKPCR)__readfsdword(RT_OFFSETOF(KPCR,SelfPcr));
+    uint8_t    *pbPrcb = (uint8_t *)pPcr->Prcb;
 
 #elif defined(RT_ARCH_AMD64)
     /* HACK ALERT! The offset is from windbg/vista64. */
-    PKPCR pPcr = (PKPCR)__readgsqword(RT_OFFSETOF(KPCR,Self));
-    pbQuantumEnd = (uint8_t volatile *)pPcr->CurrentPrcb + 0x3375;
+    PKPCR       pPcr   = (PKPCR)__readgsqword(RT_OFFSETOF(KPCR,Self));
+    uint8_t    *pbPrcb = (uint8_t *)pPcr->CurrentPrcb;
 
 #else
 # error "port me"
 #endif
-    uint8_t QuantumEnd = *pbQuantumEnd;
-    ASMSetFlags(fSavedFlags);
 
-    AssertMsg(QuantumEnd == FALSE || QuantumEnd == TRUE, ("%x\n", QuantumEnd));
-    return QuantumEnd == TRUE;
+    /* Check QuantumEnd. */
+    if (cbQuantumEnd == 1)
+    {
+        uint8_t volatile *pbQuantumEnd = (uint8_t volatile *)(pbPrcb + offQuantumEnd);
+        fPending = *pbQuantumEnd == TRUE;
+    }
+    else if (cbQuantumEnd == sizeof(uint32_t))
+    {
+        uint32_t volatile *pu32QuantumEnd = (uint32_t volatile *)(pbPrcb + offQuantumEnd);
+        fPending = *pu32QuantumEnd != 0;
+    }
+
+    /* Check DpcQueueDepth. */
+    if (    !fPending
+        &&  offDpcQueueDepth)
+    {
+        uint32_t volatile *pu32DpcQueueDepth = (uint32_t volatile *)(pbPrcb + offDpcQueueDepth);
+        fPending = *pu32DpcQueueDepth > 0;
+    }
+
+    ASMSetFlags(fSavedFlags);
+    return fPending;
 }
 
 
