c6e9b1f029e4ca6a0ccd995741abee4549d925c8
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2009-05-26 14:49:55 +0000

    Main: sketched 3D hardware support test in IHost
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@20042 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Main/HostImpl.cpp
+++ src/VBox/Main/HostImpl.cpp
@@ -110,6 +110,9 @@ extern "C" char *getfullrawname(char *);
 # include "darwin/iokit.h"
 #endif
 
+//#ifdef VBOX_WITH_CROGL
+//# include "cr_spu.h"
+//#endif /* VBOX_WITH_CROGL */
 
 #include <iprt/asm.h>
 #include <iprt/string.h>
@@ -248,6 +251,20 @@ HRESULT Host::init (VirtualBox *aParent)
                 fVTxAMDVSupported = true;
         }
     }
+
+    f3DAccelerationSupported = true;
+    /* Test for 3D hardware acceleration support */
+//    f3DAccelerationSupported = false;
+//#ifdef VBOX_WITH_CROGL
+//    SPU *spu;
+//    spu = crSPULoad(NULL, 0, "render", NULL, NULL);
+//    if (spu)
+//    {
+//        crSPUUnloadChain(spu);
+//        f3DAccelerationSupported = true;
+//    }
+//
+//#endif /* VBOX_WITH_CROGL */
 
     setReady(true);
     return S_OK;
@@ -1222,6 +1239,18 @@ STDMETHODIMP Host::COMGETTER(UTCTime)(LONG64 *aUTCTime)
     CHECK_READY();
     RTTIMESPEC now;
     *aUTCTime = RTTimeSpecGetMilli(RTTimeNow(&now));
+    return S_OK;
+}
+
+STDMETHODIMP Host::COMGETTER(Acceleration3DAvailable)(BOOL *aSupported)
+{
+    CheckComArgOutPointerValid(aSupported);
+
+    AutoWriteLock alock(this);
+    CHECK_READY();
+
+    *aSupported = f3DAccelerationSupported;
+
     return S_OK;
 }
 
