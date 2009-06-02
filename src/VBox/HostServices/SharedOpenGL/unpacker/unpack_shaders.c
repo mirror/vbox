4227ecdd51f3ce5650ef4ae5c4811ce0d26ae7d6
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2009-06-02 07:37:31 +0000

    crOpenGL: tabs to spaces
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@20175 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/HostServices/SharedOpenGL/unpacker/unpack_shaders.c
+++ src/VBox/HostServices/SharedOpenGL/unpacker/unpack_shaders.c
@@ -22,18 +22,18 @@
 
 void crUnpackExtendBindAttribLocation(void)
 {
-	GLuint program   = READ_DATA(8, GLuint);
-	GLuint index     = READ_DATA(12, GLuint);
+    GLuint program   = READ_DATA(8, GLuint);
+    GLuint index     = READ_DATA(12, GLuint);
     const char *name = DATA_POINTER(16, const char);
 
-	cr_unpackDispatch.BindAttribLocation(program, index, name);
+    cr_unpackDispatch.BindAttribLocation(program, index, name);
 }
 
 void crUnpackExtendShaderSource(void)
 {
     GLint *length = NULL;
-	GLuint shader = READ_DATA(8, GLuint);
-	GLsizei count = READ_DATA(12, GLsizei);
+    GLuint shader = READ_DATA(8, GLuint);
+    GLsizei count = READ_DATA(12, GLsizei);
     GLint hasNonLocalLen = READ_DATA(16, GLsizei);
     GLint *pLocalLength = DATA_POINTER(20, GLint);
     char **ppStrings = NULL;
@@ -59,99 +59,99 @@ void crUnpackExtendShaderSource(void)
         }
     }
 
-	cr_unpackDispatch.ShaderSource(shader, count, ppStrings, length ? length : pLocalLength);
+    cr_unpackDispatch.ShaderSource(shader, count, ppStrings, length ? length : pLocalLength);
     crFree(ppStrings);
 }
 
 void crUnpackExtendUniform1fv(void)
 {
-	GLint location = READ_DATA(8, GLint);
-	GLsizei count = READ_DATA(12, GLsizei);
-	const GLfloat *value = DATA_POINTER(16, const GLfloat);
-	cr_unpackDispatch.Uniform1fv(location, count, value);
+    GLint location = READ_DATA(8, GLint);
+    GLsizei count = READ_DATA(12, GLsizei);
+    const GLfloat *value = DATA_POINTER(16, const GLfloat);
+    cr_unpackDispatch.Uniform1fv(location, count, value);
 }
 
 void crUnpackExtendUniform1iv(void)
 {
-	GLint location = READ_DATA(8, GLint);
-	GLsizei count = READ_DATA(12, GLsizei);
+    GLint location = READ_DATA(8, GLint);
+    GLsizei count = READ_DATA(12, GLsizei);
     const GLint *value = DATA_POINTER(16, const GLint);
-	cr_unpackDispatch.Uniform1iv(location, count, value);
+    cr_unpackDispatch.Uniform1iv(location, count, value);
 }
 
 void crUnpackExtendUniform2fv(void)
 {
-	GLint location = READ_DATA(8, GLint);
-	GLsizei count = READ_DATA(12, GLsizei);
-	const GLfloat *value = DATA_POINTER(16, const GLfloat);
-	cr_unpackDispatch.Uniform2fv(location, count, value);
+    GLint location = READ_DATA(8, GLint);
+    GLsizei count = READ_DATA(12, GLsizei);
+    const GLfloat *value = DATA_POINTER(16, const GLfloat);
+    cr_unpackDispatch.Uniform2fv(location, count, value);
 }
 
 void crUnpackExtendUniform2iv(void)
 {
-	GLint location = READ_DATA(8, GLint);
-	GLsizei count = READ_DATA(12, GLsizei);
+    GLint location = READ_DATA(8, GLint);
+    GLsizei count = READ_DATA(12, GLsizei);
     const GLint *value = DATA_POINTER(16, const GLint);
-	cr_unpackDispatch.Uniform2iv(location, count, value);
+    cr_unpackDispatch.Uniform2iv(location, count, value);
 }
 
 void crUnpackExtendUniform3fv(void)
 {
-	GLint location = READ_DATA(8, GLint);
-	GLsizei count = READ_DATA(12, GLsizei);
-	const GLfloat *value = DATA_POINTER(16, const GLfloat);
-	cr_unpackDispatch.Uniform3fv(location, count, value);
+    GLint location = READ_DATA(8, GLint);
+    GLsizei count = READ_DATA(12, GLsizei);
+    const GLfloat *value = DATA_POINTER(16, const GLfloat);
+    cr_unpackDispatch.Uniform3fv(location, count, value);
 }
 
 void crUnpackExtendUniform3iv(void)
 {
-	GLint location = READ_DATA(8, GLint);
-	GLsizei count = READ_DATA(12, GLsizei);
+    GLint location = READ_DATA(8, GLint);
+    GLsizei count = READ_DATA(12, GLsizei);
     const GLint *value = DATA_POINTER(16, const GLint);
-	cr_unpackDispatch.Uniform3iv(location, count, value);
+    cr_unpackDispatch.Uniform3iv(location, count, value);
 }
 
 void crUnpackExtendUniform4fv(void)
 {
-	GLint location = READ_DATA(8, GLint);
-	GLsizei count = READ_DATA(12, GLsizei);
-	const GLfloat *value = DATA_POINTER(16, const GLfloat);
-	cr_unpackDispatch.Uniform4fv(location, count, value);
+    GLint location = READ_DATA(8, GLint);
+    GLsizei count = READ_DATA(12, GLsizei);
+    const GLfloat *value = DATA_POINTER(16, const GLfloat);
+    cr_unpackDispatch.Uniform4fv(location, count, value);
 }
 
 void crUnpackExtendUniform4iv(void)
 {
-	GLint location = READ_DATA(8, GLint);
-	GLsizei count = READ_DATA(12, GLsizei);
+    GLint location = READ_DATA(8, GLint);
+    GLsizei count = READ_DATA(12, GLsizei);
     const GLint *value = DATA_POINTER(16, const GLint);
-	cr_unpackDispatch.Uniform4iv(location, count, value);
+    cr_unpackDispatch.Uniform4iv(location, count, value);
 }
 
 void crUnpackExtendUniformMatrix2fv(void)
 {
-	GLint location = READ_DATA(8, GLint);
-	GLsizei count = READ_DATA(12, GLsizei);
-	GLboolean transpose = READ_DATA(16, GLboolean);
-	const GLfloat *value = DATA_POINTER(16+sizeof(GLboolean), const GLfloat);
-	cr_unpackDispatch.UniformMatrix2fv(location, count, transpose, value);
+    GLint location = READ_DATA(8, GLint);
+    GLsizei count = READ_DATA(12, GLsizei);
+    GLboolean transpose = READ_DATA(16, GLboolean);
+    const GLfloat *value = DATA_POINTER(16+sizeof(GLboolean), const GLfloat);
+    cr_unpackDispatch.UniformMatrix2fv(location, count, transpose, value);
 }
 
 void crUnpackExtendUniformMatrix3fv(void)
 {
-	GLint location = READ_DATA(8, GLint);
-	GLsizei count = READ_DATA(12, GLsizei);
-	GLboolean transpose = READ_DATA(16, GLboolean);
-	const GLfloat *value = DATA_POINTER(16+sizeof(GLboolean), const GLfloat);
-	cr_unpackDispatch.UniformMatrix3fv(location, count, transpose, value);
+    GLint location = READ_DATA(8, GLint);
+    GLsizei count = READ_DATA(12, GLsizei);
+    GLboolean transpose = READ_DATA(16, GLboolean);
+    const GLfloat *value = DATA_POINTER(16+sizeof(GLboolean), const GLfloat);
+    cr_unpackDispatch.UniformMatrix3fv(location, count, transpose, value);
 }
 
 void crUnpackExtendUniformMatrix4fv(void)
 {
-	GLint location = READ_DATA(8, GLint);
-	GLsizei count = READ_DATA(12, GLsizei);
-	GLboolean transpose = READ_DATA(16, GLboolean);
-	const GLfloat *value = DATA_POINTER(16+sizeof(GLboolean), const GLfloat);
-	cr_unpackDispatch.UniformMatrix4fv(location, count, transpose, value);
+    GLint location = READ_DATA(8, GLint);
+    GLsizei count = READ_DATA(12, GLsizei);
+    GLboolean transpose = READ_DATA(16, GLboolean);
+    const GLfloat *value = DATA_POINTER(16+sizeof(GLboolean), const GLfloat);
+    cr_unpackDispatch.UniformMatrix4fv(location, count, transpose, value);
 }
 
 #endif /* #ifdef CR_OPENGL_VERSION_2_0 */
