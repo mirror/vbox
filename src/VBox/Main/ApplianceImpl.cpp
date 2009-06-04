1895f7aff08d7e163a943359afcf91c0e690d7b2
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2009-06-04 10:17:28 +0000

    OVF: fix win burns
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@20261 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Main/ApplianceImpl.cpp
+++ src/VBox/Main/ApplianceImpl.cpp
@@ -1944,7 +1944,7 @@ DECLCALLBACK(int) Appliance::taskThreadImportMachines(RTTHREAD /* aThread */, vo
             /* We need HWVirt & IO-APIC if more than one CPU is requested */
             if (tmpCount > 1)
             {
-                rc = pNewMachine->COMSETTER(HWVirtExEnabled)(TRUE);
+                rc = pNewMachine->COMSETTER(HWVirtExEnabled)(TSBool_True);
                 if (FAILED(rc)) throw rc;
 
                 fEnableIOApic = true;
