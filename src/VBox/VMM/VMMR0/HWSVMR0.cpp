365b391a66adef556a303843fbd77ac909ee33b6
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2009-06-05 12:21:54 +0000

    Correction
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@20320 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/VMM/VMMR0/HWSVMR0.cpp
+++ src/VBox/VMM/VMMR0/HWSVMR0.cpp
@@ -1776,7 +1776,7 @@ ResumeExecution:
                             &&  cbOp == 6)
                         {
                             RTGCPTR  oldEip   = pCtx->eip;
-                            RTGCPTR  GCPtrTpr = Cpu.param1.parval;
+                            RTGCPTR  GCPtrTpr = Cpu.param1.disp32;
                             uint32_t uMmioReg = Cpu.param2.base.reg_gen;
 
                             /* Found:
@@ -1789,8 +1789,8 @@ ResumeExecution:
                             if (    rc == VINF_SUCCESS
                                 &&  Cpu.pCurInstr->opcode == OP_MOV
                                 &&  Cpu.param1.flags == USE_REG_GEN32
-                                &&  Cpu.param2.flags == USE_IMMEDIATE32
-                                &&  Cpu.param2.parval == GCPtrTpr
+                                &&  Cpu.param2.flags == USE_DISPLACEMENT32
+                                &&  Cpu.param2.disp32 == GCPtrTpr
                                 &&  cbOp == 5)
                             {
                                 /* mov new_reg, uMmioReg */
