61d1033023de5c3cc9d37c8647fa6b19ce518f11
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2009-06-08 06:43:48 +0000

    NAT: introduce slirp's interface to configure socket's and sbuf defaults
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@20379 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Devices/Network/slirp/tcp_subr.c
+++ src/VBox/Devices/Network/slirp/tcp_subr.c
@@ -518,7 +518,7 @@ tcp_connect(PNATState pData, struct socket *inso)
     if (cVerbose > 0)
         LogRel(("NAT: old socket rcv size: %dKB\n", opt / 1024));
     /* @todo (r-vvl) make it configurable (via extra data) */
-    opt = 64 * _1K;
+    opt = pData->socket_rcv;
     status = setsockopt(s, SOL_SOCKET, SO_RCVBUF, (char *)&opt, sizeof(int));
     if (status < 0)
     {
@@ -534,7 +534,7 @@ tcp_connect(PNATState pData, struct socket *inso)
     }
     if (cVerbose > 0)
         LogRel(("NAT: old socket snd size: %dKB\n", opt / 1024));
-    opt = 64 * _1K;
+    opt = pData->socket_rcv;
     status = setsockopt(s, SOL_SOCKET, SO_SNDBUF, (char *)&opt, sizeof(int));
     if (status < 0)
     {
