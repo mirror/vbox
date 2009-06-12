66418d39d31bbf620b13bd5520fe8f77f67b9449
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2009-06-12 12:48:23 +0000

    OSE header
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@20508 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Additions/WINNT/Graphics/Wine/wined3d/glsl_shader.c
+++ src/VBox/Additions/WINNT/Graphics/Wine/wined3d/glsl_shader.c
@@ -22,6 +22,15 @@
  */
 
 /*
+ * Sun LGPL Disclaimer: For the avoidance of doubt, except that if any license choice
+ * other than GPL or LGPL is available it will apply instead, Sun elects to use only
+ * the Lesser General Public License version 2.1 (LGPLv2) at this time for any software where
+ * a choice of LGPL license versions is made available with the language indicating
+ * that LGPLv2 or any later version may be used, or where a choice of which version
+ * of the LGPL is applied is otherwise unspecified.
+ */
+
+/*
  * D3D shader asm has swizzles on source parameters, and write masks for
  * destination parameters. GLSL uses swizzles for both. The result of this is
  * that for example "mov dst.xw, src.zyxw" becomes "dst.xw = src.zw" in GLSL.
