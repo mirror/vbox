26bb3e861a2d5c222493c116069eba5a1b06911f
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2009-06-13 20:18:20 +0000

    rtR0MemObjNativeAllocLow/solaris: removed misplaced assertion (it belongs in vbi_lowmem_alloc, not here).
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@20526 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Runtime/r0drv/solaris/vbi/memobj-r0drv-solaris.c
+++ src/VBox/Runtime/r0drv/solaris/vbi/memobj-r0drv-solaris.c
@@ -139,7 +139,6 @@ int rtR0MemObjNativeAllocLow(PPRTR0MEMOBJINTERNAL ppMem, size_t cb, bool fExecut
         rtR0MemObjDelete(&pMemSolaris->Core);
         return VERR_NO_LOW_MEMORY;
     }
-    Assert(phys < (uint64_t)1 << 32);
     pMemSolaris->Core.pv = virtAddr;
     pMemSolaris->handle = NULL;
     *ppMem = &pMemSolaris->Core;
