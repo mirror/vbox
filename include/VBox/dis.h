8676777cff8de52d5c7ab8b66951328af7622dc6
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2009-06-13 21:13:55 +0000

    dis.h: DISCPUSTATE_PADDING_SIZE correction for 32-bit windows.
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@20537 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- include/VBox/dis.h
+++ include/VBox/dis.h
@@ -519,7 +519,7 @@ typedef DISCPUSTATE const *PCDISCPUSTATE;
 
 /** The storage padding sufficient to hold the largest DISCPUSTATE in all
  * contexts (R3, R0 and RC). Used various places in the VMM internals.   */
-#define DISCPUSTATE_PADDING_SIZE    (HC_ARCH_BITS == 64 ? 0x1a0 : 0x160)
+#define DISCPUSTATE_PADDING_SIZE    (HC_ARCH_BITS == 64 ? 0x1a0 : 0x180)
 
 /** Opcode. */
 #pragma pack(4)
