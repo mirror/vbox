2d26920e0b6da9a482f5c87d56688ddc42bde00e
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2009-06-17 09:34:43 +0000

    Updated comments
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@20655 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/VMM/VMMR0/HWSVMR0.cpp
+++ src/VBox/VMM/VMMR0/HWSVMR0.cpp
@@ -975,7 +975,9 @@ ResumeExecution:
     }
 
     /* TPR caching using CR8 is only available in 64 bits mode or with 32 bits guests when X86_CPUID_AMD_FEATURE_ECX_CR8L is supported. */
-    /* Note: we can't do this in LoadGuestState as PDMApicGetTPR can jump back to ring 3 (lock)!!!!!!!! */
+    /* Note: we can't do this in LoadGuestState as PDMApicGetTPR can jump back to ring 3 (lock)!!!!!!!! 
+     * @todo query and update the TPR only when it could have been changed (mmio access)
+     */
     if (pVM->hwaccm.s.fHasIoApic)
     {
         bool fPending;
