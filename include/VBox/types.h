ddadd64e552f2463461a70230244a7f401c67690
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2009-06-23 08:05:46 +0000

    Introduced VMCPUID_OTHER
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@20804 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- include/VBox/types.h
+++ include/VBox/types.h
@@ -108,6 +108,9 @@ typedef VMCPUID *PVMCPUID;
 /** Any virtual CPU.
  * Intended for scheduling a VM request or some other task. */
 #define VMCPUID_ANY         UINT32_C(0xfffffff4)
+/** Any virtual CPU except the current one.
+ * Intended for scheduling a VM request or some other task. */
+#define VMCPUID_OTHER       UINT32_C(0xfffffff5)
 /** The NIL value. */
 #define NIL_VMCPUID         UINT32_C(0xfffffffd)
 /** @} */
