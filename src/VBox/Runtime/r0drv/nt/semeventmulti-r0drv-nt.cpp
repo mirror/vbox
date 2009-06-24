c7ef12c8710db5b40aa736510f048d7d66f7fda9
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2009-06-24 09:08:48 +0000

    Call KeWaitForSingleObject with UserMode for event semaphores
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@20883 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Runtime/r0drv/nt/semeventmulti-r0drv-nt.cpp
+++ src/VBox/Runtime/r0drv/nt/semeventmulti-r0drv-nt.cpp
@@ -147,12 +147,17 @@ static int rtSemEventMultiWait(RTSEMEVENTMULTI EventMultiSem, unsigned cMillies,
      */
     NTSTATUS rcNt;
     if (cMillies == RT_INDEFINITE_WAIT)
-        rcNt = KeWaitForSingleObject(&pThis->Event, Executive, KernelMode, fInterruptible, NULL);
+        rcNt = KeWaitForSingleObject(&pThis->Event, Executive, UserMode, fInterruptible, NULL);
     else
     {
-        LARGE_INTEGER Timeout;
-        Timeout.QuadPart = -(int64_t)cMillies * 10000;
-        rcNt = KeWaitForSingleObject(&pThis->Event, Executive, KernelMode, fInterruptible, &Timeout);
+        /* Can't use the stack here as the wait is UserMode. */
+        PLARGE_INTEGER pTimeout = (PLARGE_INTEGER)RTMemAlloc(sizeof(*pTimeout));
+        if (!pTimeout)
+            return VERR_NO_MEMORY;
+
+        pTimeout->QuadPart = -(int64_t)cMillies * 10000;
+        rcNt = KeWaitForSingleObject(&pThis->Event, Executive, UserMode, fInterruptible, pTimeout);
+        RTMemFree(pTimeout);
     }
     switch (rcNt)
     {
