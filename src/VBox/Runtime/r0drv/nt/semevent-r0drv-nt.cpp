2ef49ce614334dd893c3282b0c375bd11cfd2bba
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2009-06-24 09:08:48 +0000

    Call KeWaitForSingleObject with UserMode for event semaphores
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@20883 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Runtime/r0drv/nt/semevent-r0drv-nt.cpp
+++ src/VBox/Runtime/r0drv/nt/semevent-r0drv-nt.cpp
@@ -137,15 +137,23 @@ static int rtSemEventWait(RTSEMEVENT EventSem, unsigned cMillies, bool fInterrup
 
     /*
      * Wait for it.
+     *
+     * We default to UserMode here as the waits might be aborted due to process termination. 
+     * @todo As far as I can tell this is currently safe as all calls are made on behalf of user threads.
      */
     NTSTATUS rcNt;
     if (cMillies == RT_INDEFINITE_WAIT)
-        rcNt = KeWaitForSingleObject(&pEventInt->Event, Executive, KernelMode, fInterruptible, NULL);
+        rcNt = KeWaitForSingleObject(&pEventInt->Event, Executive, UserMode, fInterruptible, NULL);
     else
     {
-        LARGE_INTEGER Timeout;
-        Timeout.QuadPart = -(int64_t)cMillies * 10000;
-        rcNt = KeWaitForSingleObject(&pEventInt->Event, Executive, KernelMode, fInterruptible, &Timeout);
+        /* Can't use the stack here as the wait is UserMode. */
+        PLARGE_INTEGER pTimeout = (PLARGE_INTEGER)RTMemAlloc(sizeof(*pTimeout));
+        if (!pTimeout)
+            return VERR_NO_MEMORY;
+
+        pTimeout->QuadPart = -(int64_t)cMillies * 10000;
+        rcNt = KeWaitForSingleObject(&pEventInt->Event, Executive, UserMode, fInterruptible, pTimeout);
+        RTMemFree(pTimeout);
     }
     switch (rcNt)
     {
