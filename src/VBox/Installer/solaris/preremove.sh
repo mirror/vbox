0afe86791bfcfc70bbb4756753e8f3b24e3e3dd0
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2009-06-25 19:43:14 +0000

    Solaris/Installer: Remove files created during python install.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@20955 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Installer/solaris/preremove.sh
+++ src/VBox/Installer/solaris/preremove.sh
@@ -67,6 +67,9 @@ if test "$currentzone" = "global"; then
     if test -h "/dev/vboxusbmon" || test -f "/dev/vboxusbmon"; then
         rm -f /dev/vboxusbmon
     fi
+
+    # remove files installed by Python build
+    rm -rf /opt/VirtualBox/sdk/installer/build
 fi
 
 echo "Done."
