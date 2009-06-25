b53148e4048fc6263deba93a238deab2118231ac
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2009-06-25 16:29:14 +0000

    SUPDrvInternal.h: comment cleanup.
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@20954 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/HostDrivers/Support/SUPDrvInternal.h
+++ src/VBox/HostDrivers/Support/SUPDrvInternal.h
@@ -554,8 +554,7 @@ typedef struct SUPDRVSESSION
  */
 typedef struct SUPDRVDEVEXT
 {
-    /** Spinlock to serialize the initialization,
-     * usage counting and destruction of the IDT entry override and objects. */
+    /** Spinlock to serialize the initialization, usage counting and objects. */
     RTSPINLOCK                      Spinlock;
 
     /** List of registered objects. Protected by the spinlock. */
