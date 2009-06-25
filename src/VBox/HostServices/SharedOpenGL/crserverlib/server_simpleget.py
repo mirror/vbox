e40739c92fa92a46e5918e0d1065c3d14963feed
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2009-06-25 09:18:46 +0000

    crOpenGL: fix memory corruption when querying for supported texture compression formats
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@20916 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/HostServices/SharedOpenGL/crserverlib/server_simpleget.py
+++ src/VBox/HostServices/SharedOpenGL/crserverlib/server_simpleget.py
@@ -26,15 +26,29 @@ funcs = [ 'GetIntegerv', 'GetFloatv', 'GetDoublev', 'GetBooleanv' ]
 types = [ 'GLint', 'GLfloat', 'GLdouble', 'GLboolean' ]
 
 for index in range(len(funcs)):
-	func_name = funcs[index]
-	params = apiutil.Parameters(func_name)
-	print 'void SERVER_DISPATCH_APIENTRY crServerDispatch%s( %s )' % ( func_name, apiutil.MakeDeclarationString(params))
-	print '{'
-	print '\t%s *get_values;' % types[index]
-	print '\tint tablesize = __numValues( pname ) * sizeof(%s);' % types[index]
-	print '\t(void) params;'
-	print '\tget_values = (%s *) crAlloc( tablesize );' % types[index]
-	print '\tcr_server.head_spu->dispatch_table.%s( pname, get_values );' % func_name
-	print '\tcrServerReturnValue( get_values, tablesize );'
-	print '\tcrFree(get_values);'
-	print '}\n'
+    func_name = funcs[index]
+    params = apiutil.Parameters(func_name)
+    print 'void SERVER_DISPATCH_APIENTRY crServerDispatch%s( %s )' % ( func_name, apiutil.MakeDeclarationString(params))
+    print '{'
+    print '\t%s *get_values;' % types[index]
+    print '\tint tablesize;'
+    print """
+    #ifdef CR_ARB_texture_compression
+    if (GL_COMPRESSED_TEXTURE_FORMATS_ARB == pname)
+    {
+        GLint numtexfmts = 0;
+        cr_server.head_spu->dispatch_table.GetIntegerv(GL_NUM_COMPRESSED_TEXTURE_FORMATS_ARB, &numtexfmts);
+        tablesize = numtexfmts * sizeof(%s);
+    }
+    else
+    #endif
+    {
+        tablesize = __numValues( pname ) * sizeof(%s);
+    }
+    """ % (types[index], types[index])
+    print '\t(void) params;'
+    print '\tget_values = (%s *) crAlloc( tablesize );' % types[index]
+    print '\tcr_server.head_spu->dispatch_table.%s( pname, get_values );' % func_name
+    print '\tcrServerReturnValue( get_values, tablesize );'
+    print '\tcrFree(get_values);'
+    print '}\n'
