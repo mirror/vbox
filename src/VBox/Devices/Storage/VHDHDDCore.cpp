16555cc68cba6d4d21dd8632245e597fc55c68a2
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2009-06-28 19:07:20 +0000

    VHD:      * Fix calculation of block bitmap bit to test or set
           Explanation: The code to avoid unaligned accesses for bt calculates the wrong bit to test.
                        If sector 0 in the block bitmap should be tested we have to test bit 7 in the first
                        byte the pointer of the data block bitmap points to.
                        The current code however would test the 31th bit which is wrong.
                        It would be correct to add the skipped bytes as number of bits to the bit position
                        like iBitInByte = ((8-1) - (cBlockBitmapEntry % 8)) + ((cBlockBitmapEntry / 8) & 3)*8
                        to ensure aligned access but I doubt that the additional instructions outweigh the
                        the slower memory access. So we restore the old behavior here and accept unaligned accesses.
    
         * On 64bit Windows systems we have to add 8 bytes instead of 4 because BT might read 8 bytes from the memory operand
    
         Many thanks to Huihong Luo for pointing that out!
    
    
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@21006 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Devices/Storage/VHDHDDCore.cpp
+++ src/VBox/Devices/Storage/VHDHDDCore.cpp
@@ -222,12 +222,16 @@ static void vhdTime2RtTime(PRTTIMESPEC pRtTimeStamp, uint32_t u32VhdTimeStamp)
 }
 
 /**
- * Internal: Allocates the block bitmap rounding up to the next 32bit boundary.
+ * Internal: Allocates the block bitmap rounding up to the next 32bit or 64bit boundary.
  *           Can be freed with RTMemFree. The memory is zeroed.
  */
 DECLINLINE(uint8_t *)vhdBlockBitmapAllocate(PVHDIMAGE pImage)
 {
-    return (uint8_t *)RTMemAllocZ(RT_ALIGN_T(pImage->cbDataBlockBitmap, 4, uint32_t));
+#ifdef RT_ARCH_AMD64
+    return (uint8_t *)RTMemAllocZ(RT_ALIGN_64(pImage->cbDataBlockBitmap, 8));
+#else
+    return (uint8_t *)RTMemAllocZ(RT_ALIGN_32(pImage->cbDataBlockBitmap, 4));
+#endif
 }
 
 /**
@@ -916,7 +920,7 @@ DECLINLINE(bool) vhdBlockBitmapSectorContainsData(PVHDIMAGE pImage, uint32_t cBl
      * The index of the bit in the byte of the data block bitmap.
      * The most signifcant bit stands for a lower sector number.
      */
-    uint8_t  iBitInByte = (32-1) - (cBlockBitmapEntry % 32);
+    uint8_t  iBitInByte = (8-1) - (cBlockBitmapEntry % 8);
     uint8_t *puBitmap = pImage->pu8Bitmap + iBitmap;
 
     AssertMsg(puBitmap < (pImage->pu8Bitmap + pImage->cbDataBlockBitmap),
@@ -936,7 +940,7 @@ DECLINLINE(void) vhdBlockBitmapSectorSet(PVHDIMAGE pImage, uint32_t cBlockBitmap
      * The index of the bit in the byte of the data block bitmap.
      * The most signifcant bit stands for a lower sector number.
      */
-    uint8_t  iBitInByte = (32-1) - (cBlockBitmapEntry % 32);
+    uint8_t  iBitInByte = (8-1) - (cBlockBitmapEntry % 8);
     uint8_t  *puBitmap  = pImage->pu8Bitmap + iBitmap;
 
     AssertMsg(puBitmap < (pImage->pu8Bitmap + pImage->cbDataBlockBitmap),
