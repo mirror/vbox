6db1495461e4d5173cd73f0413a8a8c9ca3a167c
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2009-07-01 04:55:22 +0000

    NAT: use constant
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@21112 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Devices/Network/slirp/ctl.h
+++ src/VBox/Devices/Network/slirp/ctl.h
@@ -3,6 +3,7 @@
 #define CTL_ALIAS       2
 #define CTL_DNS         3
 #define CTL_TFTP        4
+#define CTL_GUEST       15
 #define CTL_BROADCAST   255
 
 #define CTL_CHECK(x, ctl) (((x) & ~pData->netmask) == (ctl))
