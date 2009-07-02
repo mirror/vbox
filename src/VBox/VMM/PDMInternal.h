eac184b06949fc88901e4aadda7cb32e60fdf411
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2009-07-02 13:52:46 +0000

    Backed out 49450. Not necessary
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@21161 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/VMM/PDMInternal.h
+++ src/VBox/VMM/PDMInternal.h
@@ -229,10 +229,8 @@ typedef struct PDMCRITSECTINT
     STAMCOUNTER                     StatContentionRZUnlock;
     /** R3 lock contention. */
     STAMCOUNTER                     StatContentionR3;
-#ifdef VBOX_WITH_STATISTICS
     /** Profiling the time the section is locked. */
     STAMPROFILEADV                  StatLocked;
-#endif
 } PDMCRITSECTINT;
 typedef PDMCRITSECTINT *PPDMCRITSECTINT;
 
