d0af5e4fb7895190ce6256b5c0c5fcf6754b1e30
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2009-07-06 15:35:22 +0000

    oops.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@21261 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Additions/common/VBoxGuestLib/HGCM.cpp
+++ src/VBox/Additions/common/VBoxGuestLib/HGCM.cpp
@@ -115,7 +115,7 @@ void vbglHGCMHandleFree (struct VBGLHGCMHANDLEDATA *pHandle)
     return;
 }
 
-DECLVBGL(int) VbglR0HGCMInternalConnect (VBGLHGCMHANDLE *pHandle, VBoxGuestHGCMConnectInfo *pData)
+DECLVBGL(int) VbglHGCMConnect (VBGLHGCMHANDLE *pHandle, VBoxGuestHGCMConnectInfo *pData)
 {
     int rc;
     struct VBGLHGCMHANDLEDATA *pHandleData;
