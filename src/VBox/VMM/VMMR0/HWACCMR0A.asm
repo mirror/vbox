380c3fcb11a592b3aae4dd0fc16cf2636bbd2c8f
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2009-07-14 10:15:40 +0000

    Fixed broken SVMR0InvlpgA
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@21570 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/VMM/VMMR0/HWACCMR0A.asm
+++ src/VBox/VMM/VMMR0/HWACCMR0A.asm
@@ -923,11 +923,7 @@ BEGINPROC SVMR0InvlpgA
     mov     rax, rdi
     mov     rcx, rsi
  %else
-    ; from http://www.cs.cmu.edu/~fp/courses/15213-s06/misc/asm64-handout.pdf:
-    ; ``Perhaps unexpectedly, instructions that move or generate 32-bit register
-    ;   values also set the upper 32 bits of the register to zero. Consequently
-    ;   there is no need for an instruction movzlq.''
-    mov     eax, ecx
+    mov     rax, rcx
     mov     rcx, rdx
  %endif
 %else
@@ -952,10 +948,6 @@ BEGINPROC SVMR0InvlpgA
     movzx   rax, edi
     mov     ecx, esi
  %else
-    ; from http://www.cs.cmu.edu/~fp/courses/15213-s06/misc/asm64-handout.pdf:
-    ; ``Perhaps unexpectedly, instructions that move or generate 32-bit register
-    ;   values also set the upper 32 bits of the register to zero. Consequently
-    ;   there is no need for an instruction movzlq.''
     mov     eax, ecx
     mov     ecx, edx
  %endif
