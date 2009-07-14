cc39d318c58fa92d1371edfe43d4a7a975b015cb
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2009-07-14 15:50:00 +0000

    Make sure we turn off X86_CR4_VME if we are in emulated real mode (VT-x)
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@21587 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/VMM/VMMR0/HWVMXR0.cpp
+++ src/VBox/VMM/VMMR0/HWVMXR0.cpp
@@ -1510,12 +1510,17 @@ VMMR0DECL(int) VMXR0LoadGuestState(PVM pVM, PVMCPU pVCpu, PCPUMCTX pCtx)
             val &= ~X86_CR4_PAE;
         }
 
+        /* Turn off VME if we're in emulated real mode. */
+        if (CPUMIsGuestInRealModeEx(pCtx))
+            val &= ~X86_CR4_VME;
+
         rc |= VMXWriteVMCS64(VMX_VMCS64_GUEST_CR4,            val);
         Log2(("Guest CR4 %08x\n", val));
         /* CR4 flags owned by the host; if the guests attempts to change them, then
          * the VM will exit.
          */
         val =   0
+              | X86_CR4_VME
               | X86_CR4_PAE
               | X86_CR4_PGE
               | X86_CR4_PSE
