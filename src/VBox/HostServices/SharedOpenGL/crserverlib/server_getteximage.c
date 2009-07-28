10465e455a911c18accd2c865f49e809a360cd6f
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2009-07-28 12:06:03 +0000

    crOpenGL: tabs to spaces
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@21832 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/HostServices/SharedOpenGL/crserverlib/server_getteximage.c
+++ src/VBox/HostServices/SharedOpenGL/crserverlib/server_getteximage.c
@@ -13,32 +13,31 @@
 
 void SERVER_DISPATCH_APIENTRY
 crServerDispatchGetTexImage(GLenum target, GLint level, GLenum format,
-														GLenum type, GLvoid * pixels)
+                            GLenum type, GLvoid * pixels)
 {
-	GLsizei width, height, depth, size;
-	GLvoid *buffer = NULL;
+    GLsizei width, height, depth, size;
+    GLvoid *buffer = NULL;
 
-	cr_server.head_spu->dispatch_table.GetTexLevelParameteriv(target, level, GL_TEXTURE_WIDTH, &width);
-	cr_server.head_spu->dispatch_table.GetTexLevelParameteriv(target, level, GL_TEXTURE_HEIGHT, &height);
-	cr_server.head_spu->dispatch_table.GetTexLevelParameteriv(target, level, GL_TEXTURE_DEPTH, &depth);
+    cr_server.head_spu->dispatch_table.GetTexLevelParameteriv(target, level, GL_TEXTURE_WIDTH, &width);
+    cr_server.head_spu->dispatch_table.GetTexLevelParameteriv(target, level, GL_TEXTURE_HEIGHT, &height);
+    cr_server.head_spu->dispatch_table.GetTexLevelParameteriv(target, level, GL_TEXTURE_DEPTH, &depth);
 
-	size = crTextureSize(format, type, width, height, depth);
+    size = crTextureSize(format, type, width, height, depth);
 
-	if (size && (buffer = crAlloc(size))) {
-		/* Note, the other pixel PACK parameters (default values) should
-		 * be OK at this point.
-		 */
-		cr_server.head_spu->dispatch_table.PixelStorei(GL_PACK_ALIGNMENT, 1);
-		cr_server.head_spu->dispatch_table.GetTexImage(target, level, format,
-																									 type, buffer);
-		crServerReturnValue( buffer, size );
-		crFree(buffer);
-	}
-	else {
-		/* need to return _something_ to avoid blowing up */
-		GLuint dummy = 0;
-		crServerReturnValue( (GLvoid *) &dummy, sizeof(dummy) );
-	}
+    if (size && (buffer = crAlloc(size))) {
+        /* Note, the other pixel PACK parameters (default values) should
+         * be OK at this point.
+         */
+        cr_server.head_spu->dispatch_table.PixelStorei(GL_PACK_ALIGNMENT, 1);
+        cr_server.head_spu->dispatch_table.GetTexImage(target, level, format, type, buffer);
+        crServerReturnValue( buffer, size );
+        crFree(buffer);
+    }
+    else {
+        /* need to return _something_ to avoid blowing up */
+        GLuint dummy = 0;
+        crServerReturnValue( (GLvoid *) &dummy, sizeof(dummy) );
+    }
 }
 
 
@@ -46,25 +45,24 @@ crServerDispatchGetTexImage(GLenum target, GLint level, GLenum format,
 
 void SERVER_DISPATCH_APIENTRY
 crServerDispatchGetCompressedTexImageARB(GLenum target, GLint level,
-																				 GLvoid *img)
+                                         GLvoid *img)
 {
-	GLint size;
-	GLvoid *buffer=NULL;
+    GLint size;
+    GLvoid *buffer=NULL;
 
-	cr_server.head_spu->dispatch_table.GetTexLevelParameteriv(target, level, GL_TEXTURE_COMPRESSED_IMAGE_SIZE, &size);
+    cr_server.head_spu->dispatch_table.GetTexLevelParameteriv(target, level, GL_TEXTURE_COMPRESSED_IMAGE_SIZE, &size);
 
-	if (size && (buffer = crAlloc(size))) {
-		/* XXX the pixel PACK parameter should be OK at this point */
-		cr_server.head_spu->dispatch_table.GetCompressedTexImageARB(target, level,
-																																buffer);
-		crServerReturnValue( buffer, size );
-		crFree(buffer);
-	}
-	else {
-		/* need to return _something_ to avoid blowing up */
-		GLuint dummy = 0;
-		crServerReturnValue( (GLvoid *) &dummy, sizeof(dummy) );
-	}
+    if (size && (buffer = crAlloc(size))) {
+        /* XXX the pixel PACK parameter should be OK at this point */
+        cr_server.head_spu->dispatch_table.GetCompressedTexImageARB(target, level, buffer);
+        crServerReturnValue( buffer, size );
+        crFree(buffer);
+    }
+    else {
+        /* need to return _something_ to avoid blowing up */
+        GLuint dummy = 0;
+        crServerReturnValue( (GLvoid *) &dummy, sizeof(dummy) );
+    }
 }
 
 #endif /* CR_ARB_texture_compression */
