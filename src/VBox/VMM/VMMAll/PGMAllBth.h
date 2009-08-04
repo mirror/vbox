aecc6b8bb83aae7fe94db295d9ed7e6741efe7f2
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2009-08-04 16:47:38 +0000

    Workaround for fatal cached page table free.
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@21966 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/VMM/VMMAll/PGMAllBth.h
+++ src/VBox/VMM/VMMAll/PGMAllBth.h
@@ -4416,7 +4416,8 @@ PGM_BTH_DECL(int, MapCR3)(PVMCPU pVCpu, RTGCPHYS GCPhysCR3)
 #  endif
 
     /* Clean up the old CR3 root. */
-    if (pOldShwPageCR3)
+    if (    pOldShwPageCR3 
+        &&  pOldShwPageCR3 != pNewShwPageCR3    /* @todo can happen due to incorrect syncing between REM & PGM; find the real cause */)
     {
         Assert(pOldShwPageCR3->enmKind != PGMPOOLKIND_FREE);
 #  ifndef PGM_WITHOUT_MAPPINGS
