86701d7222c8ba1feebd1be7cb784c0f320b96cf
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2009-08-10 10:24:29 +0000

    AHCI: Fix following regressions from 3.0.4     * BSOD when rebooting Windows XP (#4710)     * Hang during boot if a hard disk is attached to port 1 (#4695 and #4739)
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@22124 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Devices/Storage/DevAHCI.cpp
+++ src/VBox/Devices/Storage/DevAHCI.cpp
@@ -1514,7 +1514,7 @@ static int HbaInterruptStatus_w(PAHCI ahci, uint32_t iReg, uint32_t u32Value)
             unsigned i = 0;
 
             /* Check if the cleared ports have a interrupt status bit set. */
-            while (u32Value > 0)
+            while ((u32Value > 0) && (i < AHCI_MAX_NR_PORTS_IMPL))
             {
                 if (u32Value & 0x01)
                 {
@@ -1816,8 +1816,6 @@ static void ahciPortSwReset(PAHCIPort pAhciPort)
     if (pAhciPort->pDrvBase)
     {
         pAhciPort->regCMD |= AHCI_PORT_CMD_CPS; /* Indicate that there is a device on that port */
-        /* We received a COMINIT signal */
-        pAhciPort->regTFD  |= ATA_STAT_BUSY;
 
         if (pAhciPort->fPoweredOn)
         {
