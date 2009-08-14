e5fff3190334956e2d508ec54fc68b9c89fb6d1c
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2009-08-14 02:14:56 +0000

    NAT: name counter fix
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@22251 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Devices/Network/DrvNAT.cpp
+++ src/VBox/Devices/Network/DrvNAT.cpp
@@ -969,7 +969,7 @@ static DECLCALLBACK(int) drvNATConstruct(PPDMDRVINS pDrvIns, PCFGMNODE pCfgHandl
                               " queue", "/Drivers/NAT%u/QueuePacketDropped", pDrvIns->iInstance);
         PDMDrvHlpSTAMRegisterF(pDrvIns, &pThis->StatConsumerFalse, STAMTYPE_COUNTER,
                               STAMVISIBILITY_ALWAYS, STAMUNIT_COUNT, "counting PDM consumer false"
-                              " queue", "/Drivers/NAT%u/QueuePacketDropped", pDrvIns->iInstance);
+                              " queue", "/Drivers/NAT%u/PDMConsumerFalse", pDrvIns->iInstance);
 #endif
 
         int rc2 = drvNATConstructRedir(pDrvIns->iInstance, pThis, pCfgHandle, Network);
