88e3e003bf573e43310c2b69ab345fadfd1462eb
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2009-08-18 10:29:29 +0000

    some Windows warnings in Main
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@22317 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Main/ConsoleImpl2.cpp
+++ src/VBox/Main/ConsoleImpl2.cpp
@@ -1898,7 +1898,7 @@ DECLCALLBACK(int) Console::configConstructor(PVM pVM, void *pvConsole)
             Log(("Set VBoxGuestPropSvc property store\n"));
         }
     }
-    catch(std::bad_alloc &e)
+    catch(std::bad_alloc & /*e*/)
     {
         return VERR_NO_MEMORY;
     }
@@ -2304,7 +2304,7 @@ DECLCALLBACK(int) Console::configConstructor(PVM pVM, void *pvConsole)
                 H();
             }
             Guid hostIFGuid(str);
-            STR_FREE(str);
+            STR_FREE();
 
             INetCfg              *pNc;
             ComPtr<INetCfgComponent> pAdaptorComponent;
@@ -2618,7 +2618,7 @@ DECLCALLBACK(int) Console::configConstructor(PVM pVM, void *pvConsole)
                 H();
             }
             Guid hostIFGuid(str);
-            STR_FREE(str);
+            STR_FREE();
 
             INetCfg *pNc;
             ComPtr<INetCfgComponent> pAdaptorComponent;
