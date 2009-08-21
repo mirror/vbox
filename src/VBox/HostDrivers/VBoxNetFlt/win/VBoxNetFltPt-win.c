fbf8c444d56d27922be6fc83c35502c1a4a93f1a
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2009-08-21 15:11:58 +0000

    netflt/win: fix for proper cleaning of rx packet array on power change
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@22389 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/HostDrivers/VBoxNetFlt/win/VBoxNetFltPt-win.c
+++ src/VBox/HostDrivers/VBoxNetFlt/win/VBoxNetFltPt-win.c
@@ -969,16 +969,6 @@ vboxNetFltWinPtQueueReceivedPacket(
     do{
         RTSpinlockAcquire(pNetFlt->hSpinlock, &Tmp);
 
-        if(vboxNetFltWinGetPowerState(&pAdapt->PTState) != NdisDeviceStateD0
-                || vboxNetFltWinGetPowerState(&pAdapt->MPState) != NdisDeviceStateD0
-                || vboxNetFltWinGetOpState(&pAdapt->PTState) > kVBoxNetDevOpState_Initialized
-                || vboxNetFltWinGetOpState(&pAdapt->MPState) > kVBoxNetDevOpState_Initialized)
-        {
-            RTSpinlockRelease(pNetFlt->hSpinlock, &Tmp);
-            bReturn = true;
-            break;
-        }
-
         Assert(pAdapt->cReceivedPacketCount < MAX_RECEIVE_PACKET_ARRAY_SIZE);
 
         /*
@@ -989,12 +979,22 @@ vboxNetFltWinPtQueueReceivedPacket(
         pAdapt->aReceivedPackets[pAdapt->cReceivedPacketCount] = Packet;
         pAdapt->cReceivedPacketCount++;
 
+        /* check the device state */
+        if(vboxNetFltWinGetPowerState(&pAdapt->PTState) != NdisDeviceStateD0
+                || vboxNetFltWinGetPowerState(&pAdapt->MPState) != NdisDeviceStateD0
+                || vboxNetFltWinGetOpState(&pAdapt->PTState) > kVBoxNetDevOpState_Initialized
+                || vboxNetFltWinGetOpState(&pAdapt->MPState) > kVBoxNetDevOpState_Initialized)
+        {
+            /* we need to return all packets */
+            bReturn = true;
+        }
+
         /*
          *  If our receive packet array is full, or the miniport below indicated the packets
          *  with resources, do the indicatin now.
          */
 
-        if ((pAdapt->cReceivedPacketCount == MAX_RECEIVE_PACKET_ARRAY_SIZE) || DoIndicate)
+        if ((pAdapt->cReceivedPacketCount == MAX_RECEIVE_PACKET_ARRAY_SIZE) || DoIndicate || bReturn)
         {
             NdisMoveMemory(PacketArray,
                            pAdapt->aReceivedPackets,
@@ -1005,7 +1005,11 @@ vboxNetFltWinPtQueueReceivedPacket(
              * So other thread can queue the received packets
              */
             pAdapt->cReceivedPacketCount = 0;
-            DoIndicate = TRUE;
+
+            if(!bReturn)
+            {
+                DoIndicate = TRUE;
+            }
         }
         RTSpinlockRelease(pNetFlt->hSpinlock, &Tmp);
     } while(0);
