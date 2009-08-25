1bd83805717932e0b9caef2199941cf1a6e77ef6
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2009-08-25 15:37:30 +0000

    Windows guest display driver HGSMI: removed unused code, corrected a typo and log.
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@22440 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Additions/WINNT/Graphics/Display/vbox.c
+++ src/VBox/Additions/WINNT/Graphics/Display/vbox.c
@@ -347,8 +347,6 @@ static void vboxHGSMIBufferSubmit (PPDEV ppdev, void *p)
     DISPDBG((0, "VBoxDISP::vboxHGSMIBufferSubmit: offset 0x%x\n", offBuffer));
 
     ppdev->pfnHGSMIGHCommandPost(ppdev->hMpHGSMI, offBuffer);
-//    ASMOutU16 (VBE_DISPI_IOPORT_INDEX, VBE_DISPI_INDEX_VBVA_GUEST);
-//    ASMOutU32 (VBE_DISPI_IOPORT_DATA, offBuffer);
 }
 
 static BOOL vboxVBVAInformHost (PPDEV ppdev, BOOL bEnable)
@@ -520,7 +518,7 @@ static void vboxHwBufferFlush (PPDEV ppdev)
 {
     /* Issue the flush command. */
     void *p = HGSMIHeapAlloc (&ppdev->hgsmiDisplayHeap,
-                              sizeof (VBVA_FLUSH),
+                              sizeof (VBVAFLUSH),
                               HGSMI_CH_VBVA,
                               VBVA_FLUSH);
     if (!p)
@@ -671,7 +669,7 @@ void VBoxProcessDisplayInfo (PPDEV ppdev)
                                   VBVA_INFO_SCREEN);
         if (!p)
         {
-            DISPDBG((0, "VBoxDISP::vboxHwBufferFlush: HGSMIHeapAlloc failed\n"));
+            DISPDBG((0, "VBoxDISP::VBoxProcessDisplayInfo: HGSMIHeapAlloc failed\n"));
         }
         else
         {
