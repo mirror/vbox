03f4426601ef56daa6abc883af7e7a566119aabf
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2009-08-26 08:32:32 +0000

    ConsoleImpl: meAttachmentType shouldn't be static
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@22455 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Main/ConsoleImpl.cpp
+++ src/VBox/Main/ConsoleImpl.cpp
@@ -242,7 +242,10 @@ Console::Console()
     , mAudioSniffer (NULL)
     , mVMStateChangeCallbackDisabled (false)
     , mMachineState (MachineState_PoweredOff)
-{}
+{
+    for (ULONG slot = 0; slot < SchemaDefs::NetworkAdapterCount; slot ++)
+        meAttachmentType[slot] = NetworkAttachmentType_Null;
+}
 
 Console::~Console()
 {}
@@ -7678,10 +7681,5 @@ const PDMDRVREG Console::DrvStatusReg =
     /* u32EndVersion */
     PDM_DRVREG_VERSION
 };
-
-/**
- * Initializing the attachment type for the network adapters
- */
-NetworkAttachmentType_T Console::meAttachmentType[] = {};
 
 /* vi: set tabstop=4 shiftwidth=4 expandtab: */
