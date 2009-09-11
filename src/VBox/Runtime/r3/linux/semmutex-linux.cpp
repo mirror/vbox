96a92c800fefefa571bc08c42f154f235807eb8c
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2009-09-11 13:30:27 +0000

    i64Diff not u64Diff
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@22958 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Runtime/r3/linux/semmutex-linux.cpp
+++ src/VBox/Runtime/r3/linux/semmutex-linux.cpp
@@ -231,14 +231,14 @@ static int rtsemMutexRequest(RTSEMMUTEX MutexSem, unsigned cMillies, bool fAutoR
             /* adjust the relative timeout */
             if (pTimeout)
             {
-                int64_t u64Diff = u64End - RTTimeSystemNanoTS();
-                if (u64Diff < 1000)
+                int64_t i64Diff = u64End - RTTimeSystemNanoTS();
+                if (i64Diff < 1000)
                 {
                     rc = VERR_TIMEOUT;
                     break;
                 }
-                ts.tv_sec  = u64Diff / 1000000000;
-                ts.tv_nsec = u64Diff % 1000000000;
+                ts.tv_sec  = i64Diff / 1000000000;
+                ts.tv_nsec = i64Diff % 1000000000;
             }
         }
 
