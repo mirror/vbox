0aaee75cc0331dc12f6d8f0be179f03aff585920
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2009-09-11 06:34:12 +0000

    NAT: bsd mbuf changesest (misc.h)
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@22943 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Devices/Network/slirp/misc.h
+++ src/VBox/Devices/Network/slirp/misc.h
@@ -74,4 +74,39 @@ void add_emu (char *);
 void fd_nonblock (int);
 void fd_block (int);
 
+#ifdef VBOX_WITH_SLIRP_BSD_MBUF
+/* UVM interface */
+#define UMA_ALIGN_PTR       (1 << 0)
+#define UMA_ZONE_REFCNT     (1 << 1)
+#define UMA_ZONE_MAXBUCKET  (1 << 2)
+#define UMA_ZONE_ZINIT      (1 << 3)
+#define UMA_SLAB_KERNEL     (1 << 4)
+#define UMA_ZFLAG_FULL      (1 << 5)
+
+struct uma_zone;
+typedef struct uma_zone *uma_zone_t;
+typedef void *(*uma_alloc_t)(uma_zone_t, int, u_int8_t *, int);
+typedef void (*uma_free_t)(void *, int, u_int8_t);
+
+typedef int (*ctor_t)(PNATState, void *, int, void *, int);
+typedef void (*dtor_t)(PNATState, void *, int, void *);
+typedef int (*zinit_t)(PNATState, void *, int, int);
+typedef void (*zfini_t)(PNATState, void *, int);
+uma_zone_t uma_zcreate(PNATState, char *, size_t, ctor_t, dtor_t, zinit_t, zfini_t, int, int);
+uma_zone_t uma_zsecond_create(char *, ctor_t, dtor_t, zinit_t, zfini_t, uma_zone_t);
+void uma_zone_set_max(uma_zone_t, int);
+void uma_zone_set_allocf(uma_zone_t, uma_alloc_t);
+void uma_zone_set_freef(uma_zone_t, uma_free_t);
+
+uint32_t *uma_find_refcnt(uma_zone_t, void *);
+void *uma_zalloc(uma_zone_t, int);
+void *uma_zalloc_arg(uma_zone_t, void *, int);
+void uma_zfree(uma_zone_t, void *);
+void uma_zfree_arg(uma_zone_t, void *, void *);
+int uma_zone_exhausted_nolock(uma_zone_t);
+void zone_drain(uma_zone_t);
+
+void slirp_null_arg_free(void *, void *);
+#endif
+
 #endif
