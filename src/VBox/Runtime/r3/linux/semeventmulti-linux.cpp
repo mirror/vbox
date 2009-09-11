e853e0f7c278051a0dfa008f66c7c6582bdd6963
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2009-09-11 12:33:33 +0000

    Runtime/semevent: stupid typos; no RT_UNLIKELY
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@22954 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Runtime/r3/linux/semeventmulti-linux.cpp
+++ src/VBox/Runtime/r3/linux/semeventmulti-linux.cpp
@@ -30,7 +30,7 @@
 
 
 #include <features.h>
-#if __GLIBC_PREREQ(2,6)
+#if __GLIBC_PREREQ(2,6) && 0
 
 /*
  * glibc 2.6 fixed a serious bug in the mutex implementation. We wrote this
@@ -219,7 +219,7 @@ static int rtSemEventMultiWait(RTSEMEVENTMULTI EventMultiSem, unsigned cMillies,
     struct timespec ts;
     struct timespec tsEnd;
     struct timespec *pTimeout = NULL;
-    if (RT_UNLIKELY(cMillies != RT_INDEFINITE_WAIT))
+    if (cMillies != RT_INDEFINITE_WAIT)
     {
         ts.tv_sec  = cMillies / 1000;
         ts.tv_nsec = (cMillies % 1000) * 1000000;
@@ -249,16 +249,16 @@ static int rtSemEventMultiWait(RTSEMEVENTMULTI EventMultiSem, unsigned cMillies,
             ||  ASMAtomicCmpXchgS32(&pThis->iState, 1, 0))
         {
             /* adjust the relative timeout */
-            if (RT_UNLIKELY(pTimeout))
+            if (pTimeout)
             {
                 clock_gettime(CLOCK_REALTIME, &ts);
                 ts.tv_nsec = tsEnd.tv_nsec - ts.tv_nsec;
-                ts.tv_sec  = tsEnd.tv_nsec - ts.tv_sec;
+                ts.tv_sec  = tsEnd.tv_sec - ts.tv_sec;
                 if (ts.tv_nsec < 0)
                 {
                     ts.tv_nsec += 1000000000; /* not correct if ts.tv_sec is negative but we
                                                  leave on negative timeouts in any case */
-                    ts.tv_nsec--;
+                    ts.tv_sec--;
                 }
                 /* don't wait for less than 1 microsecond */
                 if (   ts.tv_sec < 0
