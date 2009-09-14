a7b3c1cd0f5c83bfe5a0421dffe07a016558bd6f
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2009-09-14 13:12:36 +0000

    Wrong free function called for UVM
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@23001 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/VMM/VM.cpp
+++ src/VBox/VMM/VM.cpp
@@ -516,7 +516,7 @@ static int vmR3CreateUVM(uint32_t cCpus, PUVM *ppUVM)
         }
         RTTlsFree(pUVM->vm.s.idxTLS);
     }
-    RTMemFree(pUVM);
+    RTMemPageFree(pUVM);
     return rc;
 }
 
@@ -2143,7 +2143,7 @@ static void vmR3DestroyUVM(PUVM pUVM, uint32_t cMilliesEMTWait)
     RTTlsFree(pUVM->vm.s.idxTLS);
 
     ASMAtomicUoWriteU32(&pUVM->u32Magic, UINT32_MAX);
-    RTMemFree(pUVM);
+    RTMemPageFree(pUVM);
 
     RTLogFlush(NULL);
 }
