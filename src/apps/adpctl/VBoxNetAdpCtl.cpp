8976266d78851929913ccc14637d7efe2d3b464d
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2009-09-15 23:21:32 +0000

    VBoxNetAdpCtl.cpp: Fix for execv/dup2 failure path in the child.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@23048 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/apps/adpctl/VBoxNetAdpCtl.cpp
+++ src/apps/adpctl/VBoxNetAdpCtl.cpp
@@ -129,50 +129,51 @@ static bool removeAddresses(char *pszAdapterName)
 
     if (pid == 0)
     {
+        /* child */
         close(fds[0]);
         close(STDOUT_FILENO);
         rc = dup2(fds[1], STDOUT_FILENO);
         if (rc >= 0)
             execve(VBOXADPCTL_IFCONFIG_PATH, argv, envp);
+        return false;
     }
-    else
+
+    /* parent */
+    close(fds[1]);
+    FILE *fp = fdopen(fds[0], "r");
+    if (!fp)
+        return false;
+
+    int cAddrs;
+    for (cAddrs = 0; cAddrs < MAX_ADDRESSES && fgets(szBuf, sizeof(szBuf), fp);)
     {
-        close(fds[1]);
-        FILE *fp = fdopen(fds[0], "r");
-        if (!fp)
-            return false;
-
-        int cAddrs;
-        for (cAddrs = 0; cAddrs < MAX_ADDRESSES && fgets(szBuf, sizeof(szBuf), fp);)
-        {
-            int cbSkipWS = strspn(szBuf, " \t");
+        int cbSkipWS = strspn(szBuf, " \t");
 #if 0 /* Don't use this! assert() breaks the mac build. Use IPRT or be a rectangular building thing. */
-            assert(cbSkipWS < 20);
+        assert(cbSkipWS < 20);
 #endif
-            char *pszWord = strtok(szBuf + cbSkipWS, " ");
-            /* We are concerned with IPv6 address lines only. */
-            if (!pszWord || strcmp(pszWord, "inet6"))
-                continue;
+        char *pszWord = strtok(szBuf + cbSkipWS, " ");
+        /* We are concerned with IPv6 address lines only. */
+        if (!pszWord || strcmp(pszWord, "inet6"))
+            continue;
 #ifdef RT_OS_LINUX
-            pszWord = strtok(NULL, " ");
-            /* Skip "addr:". */
-            if (!pszWord || strcmp(pszWord, "addr:"))
-                continue;
+        pszWord = strtok(NULL, " ");
+        /* Skip "addr:". */
+        if (!pszWord || strcmp(pszWord, "addr:"))
+            continue;
 #endif
-            pszWord = strtok(NULL, " ");
-            /* Skip link-local addresses. */
-            if (!pszWord || !strncmp(pszWord, "fe80", 4))
-                continue;
-            strncpy(aszAddresses[cAddrs++], pszWord, MAX_ADDRLEN-1);
-        }
-        fclose(fp);
+        pszWord = strtok(NULL, " ");
+        /* Skip link-local addresses. */
+        if (!pszWord || !strncmp(pszWord, "fe80", 4))
+            continue;
+        strncpy(aszAddresses[cAddrs++], pszWord, MAX_ADDRLEN-1);
+    }
+    fclose(fp);
 
-        for (int i = 0; i < cAddrs; i++)
-        {
-            if (executeIfconfig(pszAdapterName, "inet6",
-                                VBOXADPCTL_DEL_CMD, aszAddresses[i]) != EXIT_SUCCESS)
-                return false;
-        }
+    for (int i = 0; i < cAddrs; i++)
+    {
+        if (executeIfconfig(pszAdapterName, "inet6",
+                            VBOXADPCTL_DEL_CMD, aszAddresses[i]) != EXIT_SUCCESS)
+            return false;
     }
 
     return true;
@@ -190,12 +191,12 @@ int doIOCtl(unsigned long uCmd, void *pData)
     int rc = ioctl(fd, uCmd, pData);
     if (rc == -1)
     {
-        perror("VBoxNetAdpCtl: ioctl failed for " VBOXNETADP_CTL_DEV_NAME); 
+        perror("VBoxNetAdpCtl: ioctl failed for " VBOXNETADP_CTL_DEV_NAME);
         rc = ADPCTLERR_IOCTL_FAILED;
     }
-    
+
     close(fd);
- 
+
     return rc;
 }
 
