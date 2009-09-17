d038af4c6d408bd8de9d1c224d8df93f43ecc83a
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2009-09-17 11:35:58 +0000

    The VERR_PAGE_TABLE_NOT_PRESENT problem occurs on uniprocessor VMs too. Extend the workaround.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@23085 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/VMM/VMMAll/PGMAll.cpp
+++ src/VBox/VMM/VMMAll/PGMAll.cpp
@@ -459,8 +459,7 @@ VMMDECL(int) PGMTrap0eHandler(PVMCPU pVCpu, RTGCUINT uErr, PCPUMCTXCORE pRegFram
 
 # ifdef IN_RING0
     /* Note: hack alert for difficult to reproduce problem. */
-    if (    pVM->cCpus > 1
-        &&  rc == VERR_PAGE_TABLE_NOT_PRESENT)
+    if (rc == VERR_PAGE_TABLE_NOT_PRESENT)
     {
         Log(("WARNING: Unexpected VERR_PAGE_TABLE_NOT_PRESENT for page fault at %RGv error code %x (rip=%RGv)\n", pvFault, uErr, pRegFrame->rip));
         rc = VINF_SUCCESS;
