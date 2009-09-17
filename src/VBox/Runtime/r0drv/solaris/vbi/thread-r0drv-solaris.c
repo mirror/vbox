07830a573b40b6a0cae6e08ef7115b50f796366a
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2009-09-17 09:07:03 +0000

    Solaris/thread-r0drv-solaris: try with IPL/PIL 0 instead of 1.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@23076 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Runtime/r0drv/solaris/vbi/thread-r0drv-solaris.c
+++ src/VBox/Runtime/r0drv/solaris/vbi/thread-r0drv-solaris.c
@@ -119,7 +119,7 @@ RTDECL(void) RTThreadPreemptDisable(PRTTHREADPREEMPTSTATE pState)
 
     vbi_preempt_disable();
 
-    pState->uOldPil = splr(ipltospl(1));
+    pState->uOldPil = splr(ipltospl(0));
     Assert(pState->uOldPil != UINT32_MAX);
     RT_ASSERT_PREEMPT_CPUID_DISABLE(pState);
 }
