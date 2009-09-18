ec68ffa77aac48a57f54c070a93fcfb08a445c99
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2009-09-18 16:05:46 +0000

    VBoxTray: Modified logic for checking the host version. Now checks in every case (empty, value!=0) but "0".
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@23143 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Additions/WINNT/VBoxTray/VBoxHostVersion.cpp
+++ src/VBox/Additions/WINNT/VBoxTray/VBoxHostVersion.cpp
@@ -64,14 +64,18 @@ int VBoxCheckHostVersion ()
     if (RT_FAILURE(rc))    
     {
         if (rc == VERR_NOT_FOUND)
-            rc = VERR_NOT_SUPPORTED; /* If we don't find the value above this is not critical */
+            rc = VINF_SUCCESS; /* If we don't find the value above we do the check by default */
         else
             Log(("VBoxTray: Could not read check host version flag! rc = %d\n", rc));
     }
     else
     {
-        if (pszCheckHostVersion && atoi(pszCheckHostVersion) <= 0)
+        /* Only don't do the check if we have a valid "0" in it */
+        if (   atoi(pszCheckHostVersion) == 0
+            && strlen(pszCheckHostVersion))
+        {
             rc = VERR_NOT_SUPPORTED;
+        }
         VbglR3GuestPropReadValueFree(pszCheckHostVersion);
     }
     if (rc == VERR_NOT_SUPPORTED)            
