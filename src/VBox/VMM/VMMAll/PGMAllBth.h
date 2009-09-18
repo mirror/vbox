65a92cf2a2f7b13fc580e48adf5afe120f67b92e
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2009-09-18 09:32:17 +0000

    Noisy logging
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@23116 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/VMM/VMMAll/PGMAllBth.h
+++ src/VBox/VMM/VMMAll/PGMAllBth.h
@@ -1634,7 +1634,7 @@ DECLINLINE(void) PGM_BTH_NAME(SyncPageWorker)(PVMCPU pVCpu, PSHWPTE pPteDst, GST
         /*
          * Page not-present.
          */
-        LogFlow(("SyncPageWorker: page not present in Pte\n"));
+        Log2(("SyncPageWorker: page not present in Pte\n"));
 #ifdef PGMPOOL_WITH_USER_TRACKING
         /* Keep user track up to date. */
         if (pPteDst->n.u1Present)
