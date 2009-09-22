ba35ceb468ef905d77581e0b514d571f9f49632c
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2009-09-22 09:36:23 +0000

    Trying to fix alignment issues
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@23205 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Devices/Network/DrvIntNet.cpp
+++ src/VBox/Devices/Network/DrvIntNet.cpp
@@ -89,6 +89,14 @@ typedef struct DRVINTNET
     /** Set if data transmission should start immediately and deactivate
      * as late as possible. */
     bool                    fActivateEarlyDeactivateLate;
+    bool                    bAlignment[6];
+
+#ifdef LOG_ENABLED
+    /** The nano ts of the last transfer. */
+    uint64_t                u64LastTransferTS;
+    /** The nano ts of the last receive. */
+    uint64_t                u64LastReceiveTS;
+#endif
 
 #ifdef VBOX_WITH_STATISTICS
     /** Profiling packet transmit runs. */
@@ -97,12 +105,6 @@ typedef struct DRVINTNET
     STAMPROFILEADV          StatReceive;
 #endif /* VBOX_WITH_STATISTICS */
 
-#ifdef LOG_ENABLED
-    /** The nano ts of the last transfer. */
-    uint64_t                u64LastTransferTS;
-    /** The nano ts of the last receive. */
-    uint64_t                u64LastReceiveTS;
-#endif
     /** The network name. */
     char                    szNetwork[INTNET_MAX_NETWORK_NAME];
 } DRVINTNET, *PDRVINTNET;
