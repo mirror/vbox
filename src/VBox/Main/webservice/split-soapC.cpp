d266d2465bf5faefc1a0d7f0998464e879d790f6
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2009-09-23 16:18:54 +0000

    fix OSE exports
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@23264 cfe28804-0f27-0410-a406-dd0f0b0b656f

---
+++ src/VBox/Main/webservice/split-soapC.cpp
@@ -0,0 +1,186 @@
+/** @file
+ * File splitter: splits soapC.cpp into manageable pieces. It is somewhat
+ * intelligent and avoids splitting inside functions or similar places.
+ */
+
+/*
+ * Copyright (C) 2009 Sun Microsystems, Inc.
+ *
+ * This file is part of VirtualBox Open Source Edition (OSE), as
+ * available from http://www.virtualbox.org. This file is free software;
+ * you can redistribute it and/or modify it under the terms of the GNU
+ * General Public License (GPL) as published by the Free Software
+ * Foundation, in version 2 as it comes in the "COPYING" file of the
+ * VirtualBox OSE distribution. VirtualBox OSE is distributed in the
+ * hope that it will be useful, but WITHOUT ANY WARRANTY of any kind.
+ *
+ * Please contact Sun Microsystems, Inc., 4150 Network Circle, Santa
+ * Clara, CA 95054 USA or visit http://www.sun.com if you need
+ * additional information or have any questions.
+ */
+
+#include <sys/types.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <limits.h>
+
+
+int main(int argc, char *argv[])
+{
+    int rc = 0;
+    FILE *pFileIn = NULL;
+    FILE *pFileOut = NULL;
+    char *pBuffer = NULL;
+
+    do
+    {
+        if (argc != 4)
+        {
+            fprintf(stderr, "split-soapC: Must be started with exactly three arguments,\n"
+                            "1) the input file, 2) the directory where to put the output files and\n"
+                            "3) the number chunks to create.\n");
+            rc = 2;
+            break;
+        }
+
+        char *pEnd = NULL;
+        unsigned long cChunk = strtoul(argv[3], &pEnd, 0);
+        if (cChunk == ULONG_MAX || cChunk == 0 || !argv[3] || *pEnd)
+        {
+            fprintf(stderr, "split-soapC: Given argument \"%s\" is not a valid chunk count.\n", argv[3]);
+            rc = 2;
+            break;
+        }
+
+        pFileIn = fopen(argv[1], "rb");
+        if (!pFileIn)
+        {
+            fprintf(stderr, "split-soapC: Cannot open file \"%s\" for reading.\n", argv[1]);
+            rc = 2;
+            break;
+        }
+        fseek(pFileIn, 0, SEEK_END);
+        long cbFileIn = ftell(pFileIn);
+        rewind(pFileIn);
+
+        if (!(pBuffer = (char*)malloc(cbFileIn + 1)))
+        {
+            fprintf(stderr, "split-soapC: Failed to allocate %ld bytes.\n", cbFileIn);
+            rc = 2;
+            break;
+        }
+
+        if (fread(pBuffer, 1, cbFileIn, pFileIn) != cbFileIn)
+        {
+            fprintf(stderr, "split-soapC: Failed to read %ld bytes from input file.\n", cbFileIn);
+            rc = 2;
+            break;
+        }
+        pBuffer[cbFileIn] = '\0';
+
+        const char *pLine = pBuffer;
+        unsigned long cbChunk = cbFileIn / cChunk;
+        unsigned long cFiles = 0;
+        unsigned long uLimit = 0;
+        unsigned long cbWritten = 0;
+        unsigned long cIfNesting = 0;
+        unsigned long cBraceNesting = 0;
+        bool fJustZero = false;
+
+        do
+        {
+            if (!pFileOut)
+            {
+                /* construct output filename */
+                char szFilename[1024];
+                sprintf(szFilename, "%s/soapC-%u.cpp", argv[2], ++cFiles);
+                szFilename[sizeof(szFilename)-1] = '\0';
+                printf("info: soapC-%u.cpp\n", cFiles);
+
+                /* create output file */
+                if (!(pFileOut = fopen(szFilename, "wb")))
+                {
+                    fprintf(stderr, "split-soapC: Failed to open file \"%s\" for writing\n", szFilename);
+                    rc = 2;
+                    break;
+                }
+                if (cFiles > 1)
+                    fprintf(pFileOut, "#include \"soapH.h\"%s\n",
+#ifdef RT_OS_WINDOWS
+                                      "\r"
+#else /* !RT_OS_WINDOWS */
+                                      ""
+#endif /* !RT_OS_WINDOWS */
+                           );
+                uLimit += cbChunk;
+            }
+
+            /* find begin of next line and print current line */
+            const char *pNextLine = strchr(pLine, '\n');
+            size_t cbLine;
+            if (pNextLine)
+            {
+                pNextLine++;
+                cbLine = pNextLine - pLine;
+            }
+            else
+                cbLine = strlen(pLine);
+            if (fwrite(pLine, 1, cbLine, pFileOut) != cbLine)
+            {
+                fprintf(stderr, "split-soapC: Failed to write to output file\n");
+                rc = 2;
+                break;
+            }
+            cbWritten += cbLine;
+
+            /* process nesting depth information */
+            if (!strncmp(pLine, "#if", 3))
+                cIfNesting++;
+            else if (!strncmp(pLine, "#endif", 6))
+            {
+                cIfNesting--;
+                if (!cBraceNesting && !cIfNesting)
+                    fJustZero = true;
+            }
+            else
+            {
+                for (const char *p = pLine; p < pLine + cbLine; p++)
+                {
+                    if (*p == '{')
+                        cBraceNesting++;
+                    else if (*p == '}')
+                    {
+                        cBraceNesting--;
+                        if (!cBraceNesting && !cIfNesting)
+                            fJustZero = true;
+                    }
+                }
+            }
+
+            /* start a new output file if necessary and possible */
+            if (cbWritten >= uLimit && cIfNesting == 0 && fJustZero && cFiles < cChunk)
+            {
+                fclose(pFileOut);
+                pFileOut = NULL;
+            }
+                
+            if (rc)
+                break;
+
+            fJustZero = false;
+            pLine = pNextLine;
+        } while (pLine);
+
+        printf("split-soapC: Created %lu files.\n", cFiles);
+    } while (0);
+
+    if (pBuffer)
+        free(pBuffer);
+    if (pFileIn)
+        fclose(pFileIn);
+    if (pFileOut)
+        fclose(pFileOut);
+
+    return rc;
+}
