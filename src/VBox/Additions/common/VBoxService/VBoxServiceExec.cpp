f04e315f80908f74c188f4af4590284b4660f0f7
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2009-09-23 09:10:07 +0000

    VBoxService: Exec: Also let host know the status if successful.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@23247 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Additions/common/VBoxService/VBoxServiceExec.cpp
+++ src/VBox/Additions/common/VBoxService/VBoxServiceExec.cpp
@@ -440,14 +440,14 @@ DECLCALLBACK(int) VBoxServiceExecWorker(bool volatile *pfShutdown)
                     VBoxServiceVerbose(1, "Exec: Stopping sysprep processing (rc=%Rrc)\n", rc);
                     fSysprepDone = true;
                 }
+            }
 
-                /* Let the host know what went wrong (but only if we got a value) */
-                if (rc != VERR_NOT_FOUND)
-                {
-                    rc = VbglR3GuestPropWriteValueF(g_uExecGuestPropSvcClientID, "/VirtualBox/HostGuest/SysprepVBoxRC", "%d", rc);
-                    if (RT_FAILURE(rc))
-                        VBoxServiceError("Exec: Failed to write SysprepVBoxRC: rc=%Rrc\n", rc);
-                }
+            /* Let the host know what happend (but only if we got a guest property value) */
+            if (rc != VERR_NOT_FOUND)
+            {
+                rc = VbglR3GuestPropWriteValueF(g_uExecGuestPropSvcClientID, "/VirtualBox/HostGuest/SysprepVBoxRC", "%d", rc);
+                if (RT_FAILURE(rc))
+                    VBoxServiceError("Exec: Failed to write SysprepVBoxRC: rc=%Rrc\n", rc);
             }
         }
 
