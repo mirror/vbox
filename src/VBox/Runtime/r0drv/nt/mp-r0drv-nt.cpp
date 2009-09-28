f6a9687cfc221bdbaa4bed6e47db7573049a9c3a
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2009-09-28 12:41:24 +0000

    Compile fix
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@23370 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Runtime/r0drv/nt/mp-r0drv-nt.cpp
+++ src/VBox/Runtime/r0drv/nt/mp-r0drv-nt.cpp
@@ -231,7 +231,7 @@ static int rtMpCall(PFNRTMPWORKER pfnWorker, void *pvUser1, void *pvUser2, RT_NT
 
 #ifdef IPRT_TARGET_NT4
     KAFFINITY Mask;
-    /* g_pfnrtNt* do not present on NT anyway. */
+    /* g_pfnrtNt* are not present on NT anyway. */
     return VERR_NOT_SUPPORTED;
 #else
     KAFFINITY Mask = KeQueryActiveProcessors();
@@ -348,7 +348,7 @@ static VOID rtMpNtPokeCpuDummy(IN PKDPC Dpc, IN PVOID DeferredContext, IN PVOID 
 }
 
 
-extern "C" HalRequestIpi(KAFFINITY TargetSet);
+extern "C" void HalRequestIpi(KAFFINITY TargetSet);
 
 RTDECL(int) RTMpPokeCpu(RTCPUID idCpu)
 {
