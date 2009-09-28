628a6d01b4b10ad973bb6903ab3d2d3268763382
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2009-09-28 14:01:11 +0000

    no trailing ';' in STAM_GET_TS macro
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@23380 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- include/VBox/stam.h
+++ include/VBox/stam.h
@@ -58,7 +58,7 @@ RT_C_DECLS_BEGIN
 #ifdef __GNUC__
 # if defined(RT_ARCH_X86)
    /* This produces optimal assembler code for x86 but does not work for AMD64 ('A' means 'either rax or rdx') */
-#  define STAM_GET_TS(u64) __asm__ __volatile__ ("rdtsc\n\t" : "=A" (u64));
+#  define STAM_GET_TS(u64) __asm__ __volatile__ ("rdtsc\n\t" : "=A" (u64))
 # elif defined(RT_ARCH_AMD64)
 #  define STAM_GET_TS(u64) \
     do { uint64_t low; uint64_t high; \
