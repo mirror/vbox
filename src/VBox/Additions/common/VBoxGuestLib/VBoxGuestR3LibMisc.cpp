95e608e93df4986c89f65e61f5968cca4b29a5b2
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2009-10-16 22:55:57 +0000

    VBoxClient/VBoxTray/VBgl: Tabs.
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@23838 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Additions/common/VBoxGuestLib/VBoxGuestR3LibMisc.cpp
+++ src/VBox/Additions/common/VBoxGuestLib/VBoxGuestR3LibMisc.cpp
@@ -201,7 +201,7 @@ VBGLR3DECL(int) VbglR3GetAdditionsVersion(char **ppszVer, char **ppszRev)
     HKEY hKey;
     LONG r;
 
-	/* Check the new path first. */
+    /* Check the new path first. */
     r = RegOpenKeyEx(HKEY_LOCAL_MACHINE, "SOFTWARE\\Sun\\VirtualBox Guest Additions", 0, KEY_READ, &hKey);
 #ifdef RT_ARCH_AMD64
     if (r != ERROR_SUCCESS)
@@ -233,19 +233,19 @@ VBGLR3DECL(int) VbglR3GetAdditionsVersion(char **ppszVer, char **ppszRev)
         char *pszVer = (char*)RTMemAlloc(dwSize);
         if (pszVer)
         {
-			if (ERROR_SUCCESS == RegQueryValueEx(hKey, "Version", NULL, &dwType, (BYTE*)(LPCTSTR)pszVer, &dwSize))
-				ppszVer = pszVer;
-		}
+            if (ERROR_SUCCESS == RegQueryValueEx(hKey, "Version", NULL, &dwType, (BYTE*)(LPCTSTR)pszVer, &dwSize))
+                ppszVer = pszVer;
+        }
         /* Revision. */
         if (ppszRev)
         {
-			dwSize = 32;
-			char pszRev = (char*)RTMemAlloc(dwSize);
-			if (ERROR_SUCCESS == RegQueryValueEx(hKey, "Revision", NULL, &dwType, (BYTE*)(LPCTSTR)pszRev, &dwSize))
-				ppszRev = pszRev;
-		}
+            dwSize = 32;
+            char pszRev = (char*)RTMemAlloc(dwSize);
+            if (ERROR_SUCCESS == RegQueryValueEx(hKey, "Revision", NULL, &dwType, (BYTE*)(LPCTSTR)pszRev, &dwSize))
+                ppszRev = pszRev;
+        }
     }
-	rc = RTErrConvertFromWin32(r);
+    rc = RTErrConvertFromWin32(r);
 
     if (NULL != hKey)
         RegCloseKey(hKey);
