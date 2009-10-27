5916450138e26ba5e8608cf8694c38cf5d8b1293
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2009-10-27 10:54:06 +0000

    Storage/VMDK: fix bug in handling split images, causing failure to read/write.
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@24112 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Devices/Storage/VmdkHDDCore.cpp
+++ src/VBox/Devices/Storage/VmdkHDDCore.cpp
@@ -4039,7 +4039,7 @@ static int vmdkFindExtent(PVMDKIMAGE pImage, uint64_t offSector,
         if (offSector < pImage->pExtents[i].cNominalSectors)
         {
             pExtent = &pImage->pExtents[i];
-            *puSectorInExtent = offSector + pImage->pExtents[i].uSectorOffset;
+            *puSectorInExtent = offSector;
             break;
         }
         offSector -= pImage->pExtents[i].cNominalSectors;
@@ -5808,9 +5808,9 @@ static int vmdkAsyncRead(void *pvBackendData, uint64_t uOffset, size_t cbRead,
             goto out;
         }
 
-        /* Clip write range to remain in this extent. */
+        /* Clip read range to remain in this extent. */
         cbToRead = RT_MIN(cbRead, VMDK_SECTOR2BYTE(pExtent->uSectorOffset + pExtent->cNominalSectors - uSectorExtentRel));
-        /* Clip write range to remain into current data segment. */
+        /* Clip read range to remain into current data segment. */
         cbToRead = RT_MIN(cbToRead, cbLeftInCurrentSegment);
 
         switch (pExtent->enmType)
