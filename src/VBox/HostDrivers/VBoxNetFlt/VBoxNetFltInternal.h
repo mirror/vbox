bade876c29301d29903f8e6fb57151941c022a7a
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2009-10-28 02:46:59 +0000

    VBoxNetFlt/FreeBSD: Fix locking problems. Contributed by Fredrik Lindberg
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@24123 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/HostDrivers/VBoxNetFlt/VBoxNetFltInternal.h
+++ src/VBox/HostDrivers/VBoxNetFlt/VBoxNetFltInternal.h
@@ -206,6 +206,14 @@ typedef struct VBOXNETFLTINS
             hook_p output;
             /** Original interface flags */
             unsigned int flags;
+            /** Input queue */
+            struct ifqueue inq;
+            /** Output queue */
+            struct ifqueue outq;
+            /** Input task */
+            struct task tskin;
+            /** Output task */
+            struct task tskout;
             /** The MAC address of the interface. */
             RTMAC Mac;
             /** @} */
@@ -240,6 +248,8 @@ typedef struct VBOXNETFLTINS
         uint8_t abPadding[1024];
 # endif
 #elif defined(RT_OS_LINUX)
+        uint8_t abPadding[320];
+#elif defined(RT_OS_FREEBSD)
         uint8_t abPadding[320];
 #else
         uint8_t abPadding[128];
