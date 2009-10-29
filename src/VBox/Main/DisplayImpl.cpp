38b2f5e3b5f624392a41cc91add6d0ab4947c850
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2009-10-29 15:46:38 +0000

    2d accel: proper handle the 2d command when framebuffer in null
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@24165 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Main/DisplayImpl.cpp
+++ src/VBox/Main/DisplayImpl.cpp
@@ -2339,20 +2339,22 @@ void Display::handleVHWACommandProcess(PPDMIDISPLAYCONNECTOR pInterface, PVBOXVH
     {
         IFramebuffer *pFramebuffer = maFramebuffers[id].pFramebuffer;
 
-        // if there is no framebuffer, this call is not interesting
         if (pFramebuffer == NULL)
-            return;
-
-        pFramebuffer->Lock();
-
-        HRESULT hr = pFramebuffer->ProcessVHWACommand((BYTE*)pCommand);
-        if(FAILED(hr))
         {
-            rc = (hr == E_NOTIMPL) ? VERR_NOT_IMPLEMENTED : VERR_GENERAL_FAILURE;
+            pFramebuffer->Lock();
+
+            HRESULT hr = pFramebuffer->ProcessVHWACommand((BYTE*)pCommand);
+            if(FAILED(hr))
+            {
+                rc = (hr == E_NOTIMPL) ? VERR_NOT_IMPLEMENTED : VERR_GENERAL_FAILURE;
+            }
+
+            pFramebuffer->Unlock();
         }
-
-        pFramebuffer->Unlock();
-
+        else
+        {
+            rc = VERR_NOT_IMPLEMENTED;
+        }
     }
     else
     {
