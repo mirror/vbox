b664d7f7156b5656fb63c1efc4a747592f7d2816
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2009-11-04 10:44:03 +0000

    VBoxGuestR3LibCredentials.cpp: File header, use RT_ZERO(), deal with RTStrDupEx failures, corrected the *Retrieve docs.
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@24311 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Additions/common/VBoxGuestLib/VBoxGuestR3LibCredentials.cpp
+++ src/VBox/Additions/common/VBoxGuestLib/VBoxGuestR3LibCredentials.cpp
@@ -1,4 +1,4 @@
-/* $Id: */
+/* $Id$ */
 /** @file
  * VBoxGuestR3Lib - Ring-3 Support Library for VirtualBox guest additions, user credentials.
  */
@@ -33,58 +33,55 @@
  * Checks whether user credentials are available to the guest or not.
  *
  * @returns true if credentials are available, false if not (or error occured).
- *
  */
 VBGLR3DECL(bool) VbglR3CredentialsAreAvailable(void)
 {
-    int rc;
-    VMMDevCredentials vmmreqCredentials;
+    VMMDevCredentials Req;
+    RT_ZERO(Req);
+    vmmdevInitRequest((VMMDevRequestHeader*)&Req, VMMDevReq_QueryCredentials);
+    Req.u32Flags |= VMMDEV_CREDENTIALS_QUERYPRESENCE;
 
-    memset(&vmmreqCredentials, 0, sizeof(vmmreqCredentials));
-
-    vmmdevInitRequest((VMMDevRequestHeader*)&vmmreqCredentials, VMMDevReq_QueryCredentials);
-    vmmreqCredentials.u32Flags |= VMMDEV_CREDENTIALS_QUERYPRESENCE;
-    rc = vbglR3GRPerform(&vmmreqCredentials.header);
-
-    bool fAvailable = false;
-    if (   RT_SUCCESS(rc)
-        && ((vmmreqCredentials.u32Flags & VMMDEV_CREDENTIALS_PRESENT) != 0))
-    {
-        fAvailable = true;
-    }
-    return fAvailable;
+    int rc = vbglR3GRPerform(&Req.header);
+    return RT_SUCCESS(rc)
+        && (Req.u32Flags & VMMDEV_CREDENTIALS_PRESENT) != 0;
 }
 
 
 /**
- * Retrieves user credentials to log into a guest OS.
+ * Retrieves and clears the user credentials for logging into the guest OS.
  *
  * @returns IPRT status value
- * @param   ppszUser        Receives pointer of allocated user name string. NULL is accepted.
+ * @param   ppszUser        Receives pointer of allocated user name string.
  *                          The returned pointer must be freed using RTStrFree().
- * @param   ppszPassword    Receives pointer of allocated user password string. NULL is accepted.
+ * @param   ppszPassword    Receives pointer of allocated user password string.
  *                          The returned pointer must be freed using RTStrFree().
- * @param   ppszDomain      Receives pointer of allocated domain name string. NULL is accepted.
+ * @param   ppszDomain      Receives pointer of allocated domain name string.
  *                          The returned pointer must be freed using RTStrFree().
  */
 VBGLR3DECL(int) VbglR3CredentialsRetrieve(char **ppszUser, char **ppszPassword, char **ppszDomain)
 {
-    int rc;
-    VMMDevCredentials vmmreqCredentials;
+    VMMDevCredentials Req;
+    RT_ZERO(Req);
+    vmmdevInitRequest((VMMDevRequestHeader*)&Req, VMMDevReq_QueryCredentials);
+    Req.u32Flags |= VMMDEV_CREDENTIALS_READ | VMMDEV_CREDENTIALS_CLEAR;
 
-    memset(&vmmreqCredentials, 0, sizeof(vmmreqCredentials));
-
-    vmmdevInitRequest((VMMDevRequestHeader*)&vmmreqCredentials, VMMDevReq_QueryCredentials);
-    vmmreqCredentials.u32Flags |= VMMDEV_CREDENTIALS_READ;
-    vmmreqCredentials.u32Flags |= VMMDEV_CREDENTIALS_CLEAR;
-    rc = vbglR3GRPerform(&vmmreqCredentials.header);
-
+    int rc = vbglR3GRPerform(&Req.header);
     if (RT_SUCCESS(rc))
     {
-        /** @todo error checking */
-        rc = RTStrDupEx(ppszUser, vmmreqCredentials.szUserName);
-        rc = RTStrDupEx(ppszPassword, vmmreqCredentials.szPassword);
-        rc = RTStrDupEx(ppszDomain, vmmreqCredentials.szDomain);
+        rc = RTStrDupEx(ppszUser, Req.szUserName);
+        if (RT_SUCCESS(rc))
+        {
+            rc = RTStrDupEx(ppszPassword, Req.szPassword);
+            if (RT_SUCCESS(rc))
+            {
+                rc = RTStrDupEx(ppszDomain, Req.szDomain);
+                if (RT_SUCCESS(rc))
+                    return VINF_SUCCESS;
+
+                RTStrFree(*ppszPassword);
+            }
+            RTStrFree(*ppszUser);
+        }
     }
     return rc;
 }
