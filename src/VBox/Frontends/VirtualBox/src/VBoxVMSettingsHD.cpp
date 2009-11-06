e9c272aa1366dddcd0c17f2e6f196e9c7afd18e0
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2009-11-06 00:38:48 +0000

    FE/Qt4: Storage Settings UI: Disabling passthrough for other than IDE controller's devices.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@24421 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Frontends/VirtualBox/src/VBoxVMSettingsHD.cpp
+++ src/VBox/Frontends/VirtualBox/src/VBoxVMSettingsHD.cpp
@@ -2066,8 +2066,10 @@ void VBoxVMSettingsHD::getInformation()
 
                 /* Getting Passthrough state */
                 bool isHostDrive = mStorageModel->data (index, StorageModel::R_AttIsHostDrive).toBool();
-                mCbPassthrough->setEnabled (isHostDrive);
-                mCbPassthrough->setChecked (isHostDrive && mStorageModel->data (index, StorageModel::R_AttIsPassthrough).toBool());
+                /* Passthrough is currently available only for the IDE controller DVD device */
+                bool isIdeCtr = mStorageModel->data (index.parent(), StorageModel::R_CtrBusType).value <KStorageBus>() == KStorageBus_IDE;
+                mCbPassthrough->setEnabled (isHostDrive && isIdeCtr);
+                mCbPassthrough->setChecked (isHostDrive && isIdeCtr && mStorageModel->data (index, StorageModel::R_AttIsPassthrough).toBool());
 
                 /* Update optional widgets visibility */
                 updateAdditionalObjects (device);
