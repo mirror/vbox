d9639ad2c65ae2d725119a5f88b6618113a8eb53
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2009-11-06 13:39:28 +0000

    ConsoleImpl: should be pcnet and not e1000
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@24441 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Main/ConsoleImpl.cpp
+++ src/VBox/Main/ConsoleImpl.cpp
@@ -3148,7 +3148,7 @@ HRESULT Console::onNetworkAdapterChange(INetworkAdapter *aNetworkAdapter, BOOL c
             {
                 case NetworkAdapterType_Am79C970A:
                 case NetworkAdapterType_Am79C973:
-                    pszAdapterName = "e1000";
+                    pszAdapterName = "pcnet";
                     break;
 #ifdef VBOX_WITH_E1000
                 case NetworkAdapterType_I82540EM:
