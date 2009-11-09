906ee8cd0949f978e1b160a8720dfc9b24156ddf
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2009-11-09 10:00:12 +0000

    HWACCMR0.cpp: Disabled deadly assertion.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@24482 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/VMM/VMMR0/HWACCMR0.cpp
+++ src/VBox/VMM/VMMR0/HWACCMR0.cpp
@@ -359,7 +359,9 @@ VMMR0DECL(int) HWACCMR0Init(void)
                 if (RT_SUCCESS(rc))
                     rc = hwaccmR0CheckCpuRcArray(aRc, RT_ELEMENTS(aRc), &idCpu);
 
+#if 0
                 AssertMsgRC(rc, ("HWACCMR0InitCPU failed for cpu %d with rc=%d\n", idCpu, rc));
+#endif
 
                 if (RT_SUCCESS(rc))
                 {
