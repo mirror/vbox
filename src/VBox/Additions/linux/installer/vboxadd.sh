fd4c17de6bf52fbf8326f64947ac3c7fc6448bf8
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2009-11-10 00:00:54 +0000

    Additions/linux/installer: typo
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@24532 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Additions/linux/installer/vboxadd.sh
+++ src/VBox/Additions/linux/installer/vboxadd.sh
@@ -394,7 +394,7 @@ cleanup()
              echo "$line" | grep -q installed > /dev/null; then
             version=`echo "$line" | sed "s/$mod,\([^,]*\)[,:].*/\1/;t;d"`
             echo "  removing module $mod version $version"
-            $DKMS remove -m $mod -v $version --all 2>/dev/null
+            $DKMS remove -m $mod -v $version --all >/dev/null
           fi
         done
       done
