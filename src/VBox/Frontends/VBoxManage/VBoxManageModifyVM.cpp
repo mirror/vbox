8ded9e23574de97769c2038727582bec8d276c7a
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2009-11-11 17:09:45 +0000

    EFI: Main work for additional firmware types and presence checks
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@24599 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Frontends/VBoxManage/VBoxManageModifyVM.cpp
+++ src/VBox/Frontends/VBoxManage/VBoxManageModifyVM.cpp
@@ -301,9 +301,17 @@ int handleModifyVM(HandlerArg *a)
 
             case MODIFYVM_FIRMWARE:
             {
-                if (!strcmp(ValueUnion.psz, "efi"))
+                if (!strcmp(ValueUnion.psz, "efi") || !strcmp(ValueUnion.psz, "efi32"))
                 {
                     CHECK_ERROR(machine, COMSETTER(FirmwareType)(FirmwareType_EFI));
+                }
+                else if (!strcmp(ValueUnion.psz, "efi64"))
+                {
+                    CHECK_ERROR(machine, COMSETTER(FirmwareType)(FirmwareType_EFI64));
+                }
+                else if (!strcmp(ValueUnion.psz, "efidual"))
+                {
+                    CHECK_ERROR(machine, COMSETTER(FirmwareType)(FirmwareType_EFIDUAL));
                 }
                 else if (!strcmp(ValueUnion.psz, "bios"))
                 {
