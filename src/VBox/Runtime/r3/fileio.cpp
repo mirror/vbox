c127ec14b7d1bc55e1897fc607ef89e6bd9fb6b4
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2009-11-14 20:14:49 +0000

    Please, don't do this.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@24653 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Runtime/r3/fileio.cpp
+++ src/VBox/Runtime/r3/fileio.cpp
@@ -149,9 +149,7 @@ int rtFileRecalcAndValidateFlags(uint32_t *pfOpen)
     switch (fOpen & RTFILE_O_ACTION_MASK)
     {
         case 0: /* temporarily */
-#ifdef DEBUG_bird
             AssertMsgFailed(("Missing RTFILE_O_OPEN/CREATE*! (continuable assertion)\n"));
-#endif
             fOpen |= RTFILE_O_OPEN;
             break;
         case RTFILE_O_OPEN:
@@ -168,9 +166,7 @@ int rtFileRecalcAndValidateFlags(uint32_t *pfOpen)
     switch (fOpen & RTFILE_O_DENY_MASK)
     {
         case 0: /* temporarily */
-#ifdef DEBUG_bird
             AssertMsgFailed(("Missing RTFILE_O_DENY_*! (continuable assertion)\n"));
-#endif
             fOpen |= RTFILE_O_DENY_NONE;
             break;
         case RTFILE_O_DENY_NONE:
