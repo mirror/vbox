ec01e0ec57610cc4346f0161b151d83a4900960e
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2009-11-14 23:41:50 +0000

    Moved RTStrVersionToUInt32 out of strtonum.cpp.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@24662 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Runtime/common/string/strtonum.cpp
+++ src/VBox/Runtime/common/string/strtonum.cpp
@@ -32,7 +32,6 @@
 /*******************************************************************************
 *   Header Files                                                               *
 *******************************************************************************/
-#include <iprt/mem.h>
 #include <iprt/string.h>
 #include "internal/iprt.h"
 
@@ -91,87 +90,6 @@ int main()
     return 0;
 }
 */
-
-
-/**
- * Converts a string representation of a version number to an unsigned number.
- *
- * @returns iprt status code.
- *          Warnings are used to indicate convertion problems.
- * @retval  VWRN_NUMBER_TOO_BIG
- * @retval  VWRN_TRAILING_CHARS
- * @retval  VWRN_TRAILING_SPACES
- * @retval  VINF_SUCCESS
- * @retval  VERR_NO_MEMORY
- * @retval  VERR_NO_DIGITS
- *
- * @param   pszValue    Pointer to the string value.
- * @param   pu32        Where to store the converted number.
- *
- * @todo    r=bird: The returned value isn't really suitable for comparing two
- *          version strings.  Try see which result you get when converting
- *          "3.0.14" and "3.1.0" and comparing the values.  The way to fix this
- *          deficiency would be to convert the individual parts and dividing the
- *          return value into sections: bits 31:24 FirstNumber; 23:16 Second;
- *          15:8 Third; 7:0 Forth.  It would probably be a good idea to use a
- *          64-bit return value instead of a 32-bit one, so there is room for
- *          revision number when found.
- *
- *          Actually, because of the above, the kind of API I had in mind was
- *          int RTStrVersionCompare(const char *pszVer1, const char *pszVer2).
- *          It wouldn't try convert input to numbers, just do a parallel parse.
- *          This would allow easy handling beta/alpha/++ indicators and any
- *          number of dots and dashes.
- */
-RTDECL(int) RTStrVersionToUInt32(const char *pszVer, uint32_t *pu32)
-{
-    const char *psz = pszVer;
-    AssertPtr(pu32);
-    AssertPtr(psz);
-
-    char *pszNew = (char*)RTMemAllocZ((strlen(pszVer) + 1) * sizeof(char));
-    if (pszNew == NULL)
-        return VERR_NO_MEMORY;
-
-    unsigned    i            = 0;
-    bool        fLastInvalid = false;
-    while (    psz
-           && *psz != '\0')
-    {
-        if (fLastInvalid)
-        {
-            if (   *psz == '-'
-                || *psz == '_')
-                fLastInvalid = false;
-        }
-        else
-        {
-            if (RT_C_IS_DIGIT(*psz))
-                pszNew[i++] = *psz;
-            else if (   *psz != '.'
-                     && i == 0)
-                fLastInvalid = true;
-        }
-        psz++;
-    }
-    pszNew[i] = '\0';
-
-    /* Convert final number string to number */
-    int rc;
-    if (fLastInvalid)
-    {
-        *pu32 = 0;
-        rc = VERR_NO_DIGITS;
-    }
-    else
-    {
-        rc = RTStrToUInt32Ex(pszNew, NULL /*pszNext*/, 10 /*uBase*/, pu32);
-        if (rc != VINF_SUCCESS)
-            *pu32 = 0;
-    }
-    RTStrFree(pszNew);
-    return rc;
-}
 
 
 /**
