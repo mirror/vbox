74e625c586aff83c3e0fb756b115fb0ad2b574d6
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2009-11-14 22:36:32 +0000

    iprt/ministring: bird review - addressed object state after throwing std::bad_alloc. Because of the cleanup() + copyFrom() approach to changing the string, there is no way to preserve the original string value without rewriting the code fundamentally. I would strongly recommend doing so.  (The rewrite is to not cleanup() first, but use RTMemRealloc() to extend the buffer.)
    Left a few review @todos in the startsWith, endsWith & contains 
    implementations.  They are related and all depends on a policy decision wrt
    matching empty strings.
    
    
    
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@24656 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- include/iprt/ministring_cpp.h
+++ include/iprt/ministring_cpp.h
@@ -1,6 +1,5 @@
 /** @file
- * VirtualBox mini C++ string class. This is a base for both Utf8Str and
- * other places where IPRT may want to use a lean C++ string class.
+ * IPRT - Mini C++ string class.
  */
 
 /*
@@ -40,20 +39,21 @@ namespace iprt
 {
 
 /**
- *  "MiniString" is a small C++ string class that does not depend on anything
- *  else except IPRT memory management functions. This is used as the base of
- *  both the Utf8Str class that COM uses as well as C++ code in IPRT that
- *  prefers to have a string class, like in xml.cpp.
+ * @brief Mini C++ string class.
  *
- *  Semantics are like in std::string, except it can do a lot less.
- *
- *  Much of the code in here used to be in com::Utf8Str so that com::Utf8Str
- *  can now derive from MiniString and only contain code that is COM-specific,
- *  such as com::Bstr conversions. Compared to the old Utf8Str though, MiniString
- *  always knows the length of its member string and the size of the buffer
- *  so it can use memcpy() instead of strdup().
+ * "MiniString" is a small C++ string class that does not depend on anything
+ * else except IPRT memory management functions.  Semantics are like in
+ * std::string, except it can do a lot less.
  */
-
+#ifdef VBOX
+ /** @remarks Much of the code in here used to be in com::Utf8Str so that
+  *          com::Utf8Str can now derive from MiniString and only contain code
+  *          that is COM-specific, such as com::Bstr conversions.  Compared to
+  *          the old Utf8Str though, MiniString always knows the length of its
+  *          member string and the size of the buffer so it can use memcpy()
+  *          instead of strdup().
+  */
+#endif
 class RT_DECL_CLASS MiniString
 {
 public:
@@ -68,9 +68,13 @@ public:
     }
 
     /**
-     * Creates a copy of another MiniString. This allocates
-     * s.length() + 1 bytes for the new instance.
-     * @param s
+     * Creates a copy of another MiniString.
+     *
+     * This allocates s.length() + 1 bytes for the new instance.
+     *
+     * @param   s               The source string.
+     *
+     * @throws  std::bad_alloc
      */
     MiniString(const MiniString &s)
     {
@@ -78,9 +82,13 @@ public:
     }
 
     /**
-     * Creates a copy of another MiniString. This allocates
-     * strlen(pcsz) + 1 bytes for the new instance.
-     * @param pcsz
+     * Creates a copy of another MiniString.
+     *
+     * This allocates strlen(pcsz) + 1 bytes for the new instance.
+     *
+     * @param   pcsz            The source string.
+     *
+     * @throws  std::bad_alloc
      */
     MiniString(const char *pcsz)
     {
@@ -96,11 +104,14 @@ public:
     }
 
     /**
-     * Returns the length of the member string, which is equal to
-     * strlen(c_str()). In other words, this does not count UTF-8 characters
-     * but returns the number of bytes.     This is always cached
-     * so calling this is cheap and requires no strlen() invocation.
-     * @return
+     * String length in bytes.
+     *
+     * Returns the length of the member string, which is equal to strlen(c_str()).
+     * In other words, this does not count unicode codepoints but returns the number
+     * of bytes.  This is always cached so calling this is cheap and requires no
+     * strlen() invocation.
+     *
+     * @returns m_cbLength.
      */
     size_t length() const
     {
@@ -108,9 +119,12 @@ public:
     }
 
     /**
-     * Returns the number of bytes allocated in the internal string buffer,
-     * which is at least length() + 1 if length() > 0.
-     * @return
+     * The allocated buffer size (in bytes).
+     *
+     * Returns the number of bytes allocated in the internal string buffer, which is
+     * at least length() + 1 if length() > 0.
+     *
+     * @returns m_cbAllocated.
      */
     size_t capacity() const
     {
@@ -118,24 +132,33 @@ public:
     }
 
     /**
+     * Make sure at that least cb of buffer space is reserved.
+     *
      * Requests that the contained memory buffer have at least cb bytes allocated.
      * This may expand or shrink the string's storage, but will never truncate the
-     * contained string. In other words, cb will be ignored if it's smaller than
+     * contained string.  In other words, cb will be ignored if it's smaller than
      * length() + 1.
-     * @param cb new minimum size of member memory buffer
+     *
+     * @param   cb              New minimum size (in bytes) of member memory buffer.
+     *
+     * @throws  std::bad_alloc  On allocation error.  The object is left unchanged.
      */
     void reserve(size_t cb)
     {
-        if (    (cb != m_cbAllocated)
-             && (cb > m_cbLength + 1)
+        if (    cb != m_cbAllocated
+             && cb > m_cbLength + 1
            )
         {
-            m_psz = (char*)RTMemRealloc(m_psz, cb);
+            char *pszNew = (char*)RTMemRealloc(m_psz, cb);
+            if (RT_LIKELY(pszNew))
+            {
+                m_psz = pszNew;
+                m_cbAllocated = cb;
+            }
 #ifdef RT_EXCEPTIONS_ENABLED
-            if (!m_psz)
+            else
                 throw std::bad_alloc();
 #endif
-            m_cbAllocated = cb;
         }
     }
 
@@ -148,31 +171,34 @@ public:
     }
 
     /**
-     *  Returns a non-const raw pointer that allows to modify the string directly.
-     *  @warning
-     *      1)  Be sure not to modify data beyond the allocated memory! Call
-     *          capacity() to find out how large that buffer is.
-     *      2)  After any operation that modifies the length of the string,
-     *          you _must_ call MiniString::jolt(), or subsequent copy operations
-     *          may go nowhere. Better not use mutableRaw() at all.
+     * Returns a non-const raw pointer that allows to modify the string directly.
+     *
+     * @warning
+     *      -# Be sure not to modify data beyond the allocated memory! Call
+     *         capacity() to find out how large that buffer is.
+     *      -# After any operation that modifies the length of the string,
+     *         you _must_ call MiniString::jolt(), or subsequent copy operations
+     *         may go nowhere.  Better not use mutableRaw() at all.
      */
-    char* mutableRaw()
+    char *mutableRaw()
     {
         return m_psz;
     }
 
     /**
+     * Clean up after using mutableRaw.
+     *
      * Intended to be called after something has messed with the internal string
-     * buffer (e.g. after using mutableRaw() or Utf8Str::asOutParam()). Resets
-     * the internal lengths correctly. Otherwise subsequent copy operations may
-     * go nowhere.
+     * buffer (e.g. after using mutableRaw() or Utf8Str::asOutParam()).  Resets the
+     * internal lengths correctly.  Otherwise subsequent copy operations may go
+     * nowhere.
      */
     void jolt()
     {
         if (m_psz)
         {
             m_cbLength = strlen(m_psz);
-            m_cbAllocated = m_cbLength + 1;
+            m_cbAllocated = m_cbLength + 1; /* (Required for the Utf8Str::asOutParam case) */
         }
         else
         {
@@ -183,10 +209,15 @@ public:
 
     /**
      * Assigns a copy of pcsz to "this".
-     * @param pcsz
-     * @return
+     *
+     * @param   pcsz            The source string.
+     *
+     * @throws  std::bad_alloc  On allocation failure.  The object is left describing
+     *             a NULL string.
+     *
+     * @returns Reference to the object.
      */
-    MiniString& operator=(const char *pcsz)
+    MiniString &operator=(const char *pcsz)
     {
         if (m_psz != pcsz)
         {
@@ -198,10 +229,15 @@ public:
 
     /**
      * Assigns a copy of s to "this".
-     * @param s
-     * @return
+     *
+     * @param   s               The source string.
+     *
+     * @throws  std::bad_alloc  On allocation failure.  The object is left describing
+     *             a NULL string.
+     *
+     * @returns Reference to the object.
      */
-    MiniString& operator=(const MiniString &s)
+    MiniString &operator=(const MiniString &s)
     {
         if (this != &s)
         {
@@ -211,16 +247,37 @@ public:
         return *this;
     }
 
-    MiniString& append(const MiniString &that);
-    MiniString& append(char c);
+    /**
+     * Appends the string "that" to "this".
+     *
+     * @param   that            The string to append.
+     *
+     * @throws  std::bad_alloc  On allocation error.  The object is left unchanged.
+     *
+     * @returns Reference to the object.
+     */
+    MiniString &append(const MiniString &that);
 
     /**
-     * Returns the byte at the given index, or a null byte if
-     * the index is not smaller than length(). This does _not_
-     * count UTF-8 characters but simply points into the
-     * member C string.
-     * @param i
-     * @return
+     * Appends the given character to "this".
+     *
+     * @param   c               The character to append.
+     *
+     * @throws  std::bad_alloc  On allocation error.  The object is left unchanged.
+     *
+     * @returns Reference to the object.
+     */
+    MiniString &append(char c);
+
+    /**
+     * Index operator.
+     *
+     * Returns the byte at the given index, or a null byte if the index is not
+     * smaller than length().  This does _not_ count codepoints but simply points
+     * into the member C string.
+     *
+     * @param   i       The index into the string buffer.
+     * @returns char at the index or null.
      */
     inline char operator[](size_t i) const
     {
@@ -231,32 +288,38 @@ public:
 
     /**
      * Returns the contained string as a C-style const char* pointer.
-     * @return
+     *
+     * @returns const pointer to C-style string.
      */
-    inline const char* c_str() const
+    inline const char *c_str() const
     {
         return m_psz;
     }
 
     /**
      * Like c_str(), for compatibility with lots of VirtualBox Main code.
-     * @return
+     *
+     * @returns const pointer to C-style string.
      */
-    inline const char* raw() const
+    inline const char *raw() const
     {
         return m_psz;
     }
 
     /**
-     * Returns true if the member string has no length. This states nothing about
+     * Emptry string or not?
+     *
+     * Returns true if the member string has no length.  This states nothing about
      * how much memory might be allocated.
-     * @return
+     *
+     * @returns true if empty, false if not.
      */
     bool isEmpty() const
     {
         return length() == 0;
     }
 
+    /** Case sensitivity selector. */
     enum CaseSensitivity
     {
         CaseSensitive,
@@ -289,53 +352,81 @@ public:
         return compare(that.m_psz, cs);
     }
 
+    /** @name Comparison operators.
+     * @{  */
     bool operator==(const MiniString &that) const { return !compare(that); }
     bool operator!=(const MiniString &that) const { return !!compare(that); }
-    bool operator<(const MiniString &that) const { return compare(that) < 0; }
-    bool operator>(const MiniString &that) const { return compare(that) > 0; }
+    bool operator<( const MiniString &that) const { return compare(that) < 0; }
+    bool operator>( const MiniString &that) const { return compare(that) > 0; }
 
-    bool operator==(const char *that) const { return !compare(that); }
-    bool operator!=(const char *that) const { return !!compare(that); }
-    bool operator<(const char *that) const { return compare(that) < 0; }
-    bool operator>(const char *that) const { return compare(that) > 0; }
+    bool operator==(const char *that) const       { return !compare(that); }
+    bool operator!=(const char *that) const       { return !!compare(that); }
+    bool operator<( const char *that) const       { return compare(that) < 0; }
+    bool operator>( const char *that) const       { return compare(that) > 0; }
+    /** @} */
 
+    /** Max string offset value.
+     *
+     * When returned by a method, this indicates failure.  When taken as input,
+     * typically a default, it means all the way to the string terminator.
+     */
     static const size_t npos;
 
     /**
+     * Find the given substring.
+     *
      * Looks for pcszFind in "this" starting at "pos" and returns its position,
-     * counting from the beginning of "this" at 0. Returns npos if not found.
+     * counting from the beginning of "this" at 0.
+     *
+     * @param   pcszFind        The substring to find.
+     * @param   pos             The (byte) offset into the string buffer to start
+     *                          searching.
+     *
+     * @returns 0 based position of pcszFind. npos if not found.
      */
     size_t find(const char *pcszFind, size_t pos = 0) const;
 
     /**
-     * Returns a substring of "this" as a new Utf8Str. Works exactly like
-     * its equivalent in std::string except that this interprets pos and n
-     * as UTF-8 codepoints instead of bytes. With the default parameters "0"
-     * and "npos", this always copies the entire string.
-     * @param pos Index of first codepoint to copy from "this", counting from 0.
-     * @param n Number of codepoints to copy, starting with the one at "pos".
+     * Returns a substring of "this" as a new Utf8Str.
+     *
+     * Works exactly like its equivalent in std::string except that this interprets
+     * pos and n as unicode codepoints instead of bytes.  With the default
+     * parameters "0" and "npos", this always copies the entire string.
+     *
+     * @param   pos             Index of first unicode codepoint to copy from
+     *                          "this", counting from 0.
+     * @param   n               Number of unicode codepoints to copy, starting with
+     *                          the one at "pos".  The copying will stop if the null
+     *                          terminator is encountered before n codepoints have
+     *                          been copied.
+     *
+     * @remarks This works on code points, not bytes!
      */
     iprt::MiniString substr(size_t pos = 0, size_t n = npos) const;
 
     /**
      * Returns true if "this" ends with "that".
-     * @param that
-     * @param cs
-     * @return
+     *
+     * @param   that    Suffix to test for.
+     * @param   cs      Case sensitivity selector.
+     * @returns true if match, false if mismatch.
      */
     bool endsWith(const iprt::MiniString &that, CaseSensitivity cs = CaseSensitive) const;
 
     /**
      * Returns true if "this" begins with "that".
-     * @return
+     * @param   that    Prefix to test for.
+     * @param   cs      Case sensitivity selector.
+     * @returns true if match, false if mismatch.
      */
     bool startsWith(const iprt::MiniString &that, CaseSensitivity cs = CaseSensitive) const;
 
     /**
      * Returns true if "this" contains "that" (strstr).
-     * @param that
-     * @param cs
-     * @return
+     *
+     * @param   that    Substring to look for.
+     * @param   cs      Case sensitivity selector.
+     * @returns true if match, false if mismatch.
      */
     bool contains(const iprt::MiniString &that, CaseSensitivity cs = CaseSensitive) const;
 
@@ -363,15 +454,17 @@ public:
 
     /**
      * Attempts to convert the member string into an unsigned 64-bit integer.
-     * @return IPRT error code.
-     * @param i Output buffer.
+     *
+     * @param   i       Where to return the value on success.
+     * @returns IPRT error code, see RTStrToInt64.
      */
     int toInt(uint64_t &i) const;
 
     /**
      * Attempts to convert the member string into an unsigned 32-bit integer.
-     * @return IPRT error code.
-     * @param i Output buffer.
+     *
+     * @param   i       Where to return the value on success.
+     * @returns IPRT error code, see RTStrToInt32.
      */
     int toInt(uint32_t &i) const;
 
@@ -383,8 +476,8 @@ protected:
     operator bool() const { return false; }
 
     /**
-     *  Destructor implementation, also used to clean up in operator=()
-     *  before assigning a new string.
+     * Destructor implementation, also used to clean up in operator=() before
+     * assigning a new string.
      */
     void cleanup()
     {
@@ -398,29 +491,38 @@ protected:
     }
 
     /**
-     * Protected internal helper.
-     * copyFrom() unconditionally sets the members to a copy of the
-     * given other strings and makes no assumptions about previous
-     * contents. Can therefore be used both in copy constructors,
-     * when member variables have no defined value, and in assignments
-     * after having called cleanup().
+     * Protected internal helper for copy a string that completely ignors the
+     * current object state.
+     *
+     * copyFrom() unconditionally sets the members to a copy of the given other
+     * strings and makes no assumptions about previous contents. Can therefore be
+     * used both in copy constructors, when member variables have no defined value,
+     * and in assignments after having called cleanup().
      *
      * This variant copies from another MiniString and is fast since
      * the length of source string is known.
      *
-     * @param s
+     * @param   s               The source string.
+     *
+     * @throws  std::bad_alloc  On allocation failure. The object is left describing
+     *             a NULL string.
      */
     void copyFrom(const MiniString &s)
     {
         if ((m_cbLength = s.m_cbLength))
         {
             m_cbAllocated = m_cbLength + 1;
-            m_psz = (char*)RTMemAlloc(m_cbAllocated);
+            m_psz = (char *)RTMemAlloc(m_cbAllocated);
+            if (RT_LIKELY(m_psz))
+                memcpy(m_psz, s.m_psz, m_cbAllocated);      // include 0 terminator
+            else
+            {
+                m_cbLength = 0;
+                m_cbAllocated = 0;
 #ifdef RT_EXCEPTIONS_ENABLED
-            if (!m_psz)
                 throw std::bad_alloc();
 #endif
-            memcpy(m_psz, s.m_psz, m_cbAllocated);      // include 0 terminator
+            }
         }
         else
         {
@@ -430,13 +532,18 @@ protected:
     }
 
     /**
-     * Protected internal helper.
+     * Protected internal helper for copy a string that completely ignors the
+     * current object state.
+     *
      * See copyFrom() above.
      *
      * This variant copies from a C string and needs to call strlen()
      * on it. It's therefore slower than the one above.
      *
-     * @param pcsz
+     * @param   pcsz            The source string.
+     *
+     * @throws  std::bad_alloc  On allocation failure. The object is left describing
+     *             a NULL string.
      */
     void copyFrom(const char *pcsz)
     {
@@ -444,12 +551,17 @@ protected:
         {
             m_cbLength = strlen(pcsz);
             m_cbAllocated = m_cbLength + 1;
-            m_psz = (char*)RTMemAlloc(m_cbAllocated);
+            m_psz = (char *)RTMemAlloc(m_cbAllocated);
+            if (RT_LIKELY(m_psz))
+                memcpy(m_psz, pcsz, m_cbAllocated);     // include 0 terminator
+            else
+            {
+                m_cbLength = 0;
+                m_cbAllocated = 0;
 #ifdef RT_EXCEPTIONS_ENABLED
-            if (!m_psz)
                 throw std::bad_alloc();
 #endif
-            memcpy(m_psz, pcsz, m_cbAllocated);      // include 0 terminator
+            }
         }
         else
         {
@@ -459,11 +571,12 @@ protected:
         }
     }
 
-    char    *m_psz;
-    size_t  m_cbLength;                     // strlen(m_psz)
-    size_t  m_cbAllocated;                  // size of buffer that m_psz points to; at least m_cbLength + 1
+    char    *m_psz;                     /**< The string buffer. */
+    size_t  m_cbLength;                 /**< strlen(m_psz) - i.e. no terminator included. */
+    size_t  m_cbAllocated;              /**< Size of buffer that m_psz points to; at least m_cbLength + 1. */
 };
 
 } // namespace iprt
 
 #endif
+
