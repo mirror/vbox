489844ed3d0954ef88ffdb6b203d580b9ac3ae5d
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2009-11-23 16:07:22 +0000

    PGMSavedState: refinement.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@24876 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/VMM/PGMSavedState.cpp
+++ src/VBox/VMM/PGMSavedState.cpp
@@ -1823,7 +1823,7 @@ static DECLCALLBACK(int)  pgmR3LiveVote(PVM pVM, PSSMHANDLE pSSM, uint32_t uPass
      */
     if (    cDirtyPagesShort <= cDirtyPagesLong
         &&  (   cDirtyNow    <= cDirtyPagesShort
-             || cDirtyNow - cDirtyPagesShort < cDirtyPagesShort / 8
+             || cDirtyNow - cDirtyPagesShort < RT_MIN(cDirtyPagesShort / 8, 16)
             )
        )
     {
