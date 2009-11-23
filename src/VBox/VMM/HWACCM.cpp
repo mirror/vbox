046a4853da222e1efb78c3780982a4aafa029eb2
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2009-11-23 13:39:12 +0000

    Again
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@24870 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/VMM/HWACCM.cpp
+++ src/VBox/VMM/HWACCM.cpp
@@ -2617,7 +2617,9 @@ static DECLCALLBACK(int) hwaccmR3Load(PVM pVM, PSSMHANDLE pSSM, uint32_t uVersio
     /* Recheck all VCPUs if we can go staight into hwaccm execution mode. */
     for (VMCPUID i = 0; i < pVM->cCpus; i++)
     {
-        HWACCMR3CanExecuteGuest(pVM, &pVM->aCpus[i]);
+        PVMCPU pVCpu = &pVM->aCpus[i];
+
+        HWACCMR3CanExecuteGuest(pVM, CPUMQueryGuestCtxPtr(pVCpu));
     }
     return VINF_SUCCESS;
 }
