92277a880e4a5ecfdd475efdf9d2355fb72be61a
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2009-12-01 13:17:23 +0000

    DevRTC: Fixed incorrect weekday calculation (two days off).
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@25127 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Devices/PC/DevRTC.cpp
+++ src/VBox/Devices/PC/DevRTC.cpp
@@ -808,7 +808,7 @@ static DECLCALLBACK(int)  rtcInitComplete(PPDMDEVINS pDevIns)
     Tm.tm_year = Time.i32Year - 1900;
     Tm.tm_mon  = Time.u8Month - 1;
     Tm.tm_mday = Time.u8MonthDay;
-    Tm.tm_wday = (Time.u8WeekDay - 1 + 7) % 7; /* 0 = monday -> sunday */
+    Tm.tm_wday = (Time.u8WeekDay + 1 + 7) % 7; /* 0 = monday -> sunday */
     Tm.tm_yday = Time.u16YearDay - 1;
     Tm.tm_hour = Time.u8Hour;
     Tm.tm_min  = Time.u8Minute;
