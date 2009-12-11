adf6a1e78cfe1454def30cb51162793d07758e6c
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2009-12-11 11:09:15 +0000

    tstSupLoadModule: stripped off some more code.
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@25321 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/HostDrivers/Support/testcase/tstSupLoadModule.cpp
+++ src/VBox/HostDrivers/Support/testcase/tstSupLoadModule.cpp
@@ -41,32 +41,7 @@
 #include <iprt/message.h>
 #include <iprt/path.h>
 #include <iprt/stream.h>
-#include <iprt/string.h>
 
-
-/**
- * Makes a path to a file in the executable directory.
- */
-static char *ExeDirFile(char *pszFile, const char *pszArgv0, const char *pszFilename)
-{
-    char   *psz;
-    char   *psz2;
-
-    strcpy(pszFile, pszArgv0);
-    psz = strrchr(pszFile, '/');
-    psz2 = strrchr(pszFile, '\\');
-    if (psz < psz2)
-        psz = psz2;
-    if (!psz)
-        psz = strrchr(pszFile, ':');
-    if (!psz)
-    {
-        strcpy(pszFile, "./");
-        psz = &pszFile[1];
-    }
-    strcpy(psz + 1, "VMMR0.r0");
-    return pszFile;
-}
 
 int main(int argc, char **argv)
 {
