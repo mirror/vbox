cf1fd8e1d7531ec998f68a3039b231208892114c
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2009-12-11 11:03:44 +0000

    tstSupLoadModule: new suplib testcase.
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@25320 cfe28804-0f27-0410-a406-dd0f0b0b656f

---
+++ src/VBox/HostDrivers/Support/testcase/tstSupLoadModule.cpp
@@ -0,0 +1,130 @@
+/* $Id$ */
+/** @file
+ * SUP Testcase - Test SUPR3LoadModule.
+ */
+
+/*
+ * Copyright (C) 2006-2009 Sun Microsystems, Inc.
+ *
+ * This file is part of VirtualBox Open Source Edition (OSE), as
+ * available from http://www.virtualbox.org. This file is free software;
+ * you can redistribute it and/or modify it under the terms of the GNU
+ * General Public License (GPL) as published by the Free Software
+ * Foundation, in version 2 as it comes in the "COPYING" file of the
+ * VirtualBox OSE distribution. VirtualBox OSE is distributed in the
+ * hope that it will be useful, but WITHOUT ANY WARRANTY of any kind.
+ *
+ * The contents of this file may alternatively be used under the terms
+ * of the Common Development and Distribution License Version 1.0
+ * (CDDL) only, as it comes in the "COPYING.CDDL" file of the
+ * VirtualBox OSE distribution, in which case the provisions of the
+ * CDDL are applicable instead of those of the GPL.
+ *
+ * You may elect to license modified versions of this file under the
+ * terms and conditions of either the GPL or the CDDL or both.
+ *
+ * Please contact Sun Microsystems, Inc., 4150 Network Circle, Santa
+ * Clara, CA 95054 USA or visit http://www.sun.com if you need
+ * additional information or have any questions.
+ */
+
+
+/*******************************************************************************
+*   Header Files                                                               *
+*******************************************************************************/
+#include <VBox/sup.h>
+#include <VBox/err.h>
+
+#include <iprt/getopt.h>
+#include <iprt/initterm.h>
+#include <iprt/mem.h>
+#include <iprt/message.h>
+#include <iprt/path.h>
+#include <iprt/stream.h>
+#include <iprt/string.h>
+
+
+/**
+ * Makes a path to a file in the executable directory.
+ */
+static char *ExeDirFile(char *pszFile, const char *pszArgv0, const char *pszFilename)
+{
+    char   *psz;
+    char   *psz2;
+
+    strcpy(pszFile, pszArgv0);
+    psz = strrchr(pszFile, '/');
+    psz2 = strrchr(pszFile, '\\');
+    if (psz < psz2)
+        psz = psz2;
+    if (!psz)
+        psz = strrchr(pszFile, ':');
+    if (!psz)
+    {
+        strcpy(pszFile, "./");
+        psz = &pszFile[1];
+    }
+    strcpy(psz + 1, "VMMR0.r0");
+    return pszFile;
+}
+
+int main(int argc, char **argv)
+{
+    /*
+     * Init.
+     */
+    int rc = RTR3InitAndSUPLib();
+    if (RT_FAILURE(rc))
+    {
+        RTMsgError("RTR3InitAndSUPLib failed with rc=%Rrc\n", rc);
+        return 1;
+    }
+
+    /* 
+     * Process arguments.
+     */
+    static const RTGETOPTDEF s_aOptions[] =
+    {
+        { "--help",             'h', 0 }
+    };
+
+    int ch;
+    RTGETOPTUNION ValueUnion;
+    RTGETOPTSTATE GetState;
+    RTGetOptInit(&GetState, argc, argv, s_aOptions, RT_ELEMENTS(s_aOptions), 1, 0);
+    while ((ch = RTGetOpt(&GetState, &ValueUnion)))
+    {
+        switch (ch)
+        {
+            case VINF_GETOPT_NOT_OPTION:
+            {
+                void *pvImageBase;
+                rc = SUPR3LoadModule(ValueUnion.psz, RTPathFilename(ValueUnion.psz), &pvImageBase);
+                if (RT_FAILURE(rc))
+                {
+                    RTMsgError("%Rrc when attempting to load '%s'\n", rc, ValueUnion.psz);
+                    return 1;
+                }
+                RTPrintf("Loaded '%s' at %p\n", ValueUnion.psz, pvImageBase);
+
+                rc = SUPR3FreeModule(pvImageBase);
+                if (RT_FAILURE(rc))
+                {
+                    RTMsgError("%Rrc when attempting to load '%s'\n", rc, ValueUnion.psz);
+                    return 1;
+                }
+                break;
+            }
+
+            case 'h':
+                RTPrintf("%s [mod1 [mod2...]]\n");
+                return 1;
+
+            default:
+                return RTGetOptPrintError(ch, &ValueUnion);
+        }
+    }
+
+    return 0;
+}
+
