d5fa72b1e98a9eeae84b91c8b939b977a76425d2
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2010-01-05 15:40:42 +0000

    nitpicking.
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@25657 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Runtime/r3/win/semevent-win.cpp
+++ src/VBox/Runtime/r3/win/semevent-win.cpp
@@ -204,8 +204,8 @@ RTDECL(int)   RTSemEventWaitNoResume(RTSEMEVENT EventSem, unsigned cMillies)
             AssertMsgFailed(("%u\n", rc));
         case WAIT_FAILED:
         {
-            AssertMsgFailed(("Wait on EventSem %p failed, rc=%d lasterr=%d\n", pThis, rc, GetLastError()));
             int rc2 = RTErrConvertFromWin32(GetLastError());
+            AssertMsgFailed(("Wait on EventSem %p failed, rc=%d lasterr=%d\n", pThis, rc, GetLastError()));
             if (rc2)
                 return rc2;
 
