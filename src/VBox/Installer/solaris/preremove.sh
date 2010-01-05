5831c43092e66c7ad45990748861db67aa6da7e9
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2010-01-05 09:36:37 +0000

    Solaris/Installer: fixed explicit unload of USB client driver.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@25646 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Installer/solaris/preremove.sh
+++ src/VBox/Installer/solaris/preremove.sh
@@ -19,7 +19,7 @@
 
 currentzone=`zonename`
 if test "$currentzone" = "global"; then
-    echo "Removing VirtualBox drivers and services..."
+    echo "Removing VirtualBox services and drivers..."
     /opt/VirtualBox/vboxconfig.sh --preremove
     if test "$?" -eq 0; then
         echo "Done."
