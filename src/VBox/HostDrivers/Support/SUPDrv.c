34f9c2cf72bb6f8797daedab74f30f612d23d50b
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2010-01-12 10:14:31 +0000

    SUPDrv.c: Enabled assertion that was temporarily disabled for me.
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@25744 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/HostDrivers/Support/SUPDrv.c
+++ src/VBox/HostDrivers/Support/SUPDrv.c
@@ -507,9 +507,7 @@ void VBOXCALL supdrvDeleteDevExt(PSUPDRVDEVEXT pDevExt)
      */
     /* objects. */
     pObj = pDevExt->pObjs;
-#if !defined(DEBUG_bird) || !defined(RT_OS_LINUX) /* breaks unloading, temporary, remove me! */
     Assert(!pObj);                      /* (can trigger on forced unloads) */
-#endif
     pDevExt->pObjs = NULL;
     while (pObj)
     {
