4bfd807c6f369cc436a37b25172c4327641cbd4a
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2010-01-15 13:16:29 +0000

    tstTimeSpec.cpp: Converted to RTTest and added conversion tests.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@25856 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Runtime/testcase/tstTimeSpec.cpp
+++ src/VBox/Runtime/testcase/tstTimeSpec.cpp
@@ -31,8 +31,14 @@
 /*******************************************************************************
 *   Header Files                                                               *
 *******************************************************************************/
+#if !defined(RT_OS_WINDOWS)
+# define RTTIME_INCL_TIMEVAL
+# define RTTIME_INCL_TIMESPEC
+# include <time.h>
+#endif
 #include <iprt/time.h>
-#include <iprt/stream.h>
+
+#include <iprt/test.h>
 #include <iprt/string.h>
 
 
@@ -57,30 +63,24 @@ char *ToString(PRTTIME pTime)
     return szBuf;
 }
 
-#define CHECK_NZ(expr)  do { if (!(expr)) { RTPrintf("tstTimeSpec: FAILURE at line %d: %#x\n", __LINE__, #expr); return 1; } } while (0)
+#define CHECK_NZ(expr) do { if (!(expr)) { RTTestIFailed("at line %d: %#x\n", __LINE__, #expr); return RTTestSummaryAndDestroy(hTest); } } while (0)
 
 #define TEST_NS(ns) do {\
         CHECK_NZ(RTTimeExplode(&T1, RTTimeSpecSetNano(&Ts1, ns))); \
-        RTPrintf("tstTimeSpec: %RI64 ns - %s\n", ns, ToString(&T1)); \
+        RTTestIPrintf(RTTESTLVL_ALWAYS, "%RI64 ns - %s\n", ns, ToString(&T1)); \
         CHECK_NZ(RTTimeImplode(&Ts2, &T1)); \
         if (!RTTimeSpecIsEqual(&Ts2, &Ts1)) \
-        { \
-            RTPrintf("tstTimeSpec: FAILURE - %RI64 != %RI64\n", RTTimeSpecGetNano(&Ts2), RTTimeSpecGetNano(&Ts1)); \
-            RTPrintf("             line no %d\n", __LINE__); \
-            cErrors++; \
-        } \
+            RTTestIFailed("FAILURE - %RI64 != %RI64, line no. %d\n", \
+                          RTTimeSpecGetNano(&Ts2), RTTimeSpecGetNano(&Ts1), __LINE__); \
     } while (0)
 
 #define TEST_SEC(sec) do {\
         CHECK_NZ(RTTimeExplode(&T1, RTTimeSpecSetSeconds(&Ts1, sec))); \
-        RTPrintf("tstTimeSpec: %RI64 sec - %s\n", sec, ToString(&T1)); \
+        RTTestIPrintf(RTTESTLVL_ALWAYS, "%RI64 sec - %s\n", sec, ToString(&T1)); \
         CHECK_NZ(RTTimeImplode(&Ts2, &T1)); \
         if (!RTTimeSpecIsEqual(&Ts2, &Ts1)) \
-        { \
-            RTPrintf("tstTimeSpec: FAILURE - %RI64 != %RI64\n", RTTimeSpecGetNano(&Ts2), RTTimeSpecGetNano(&Ts1)); \
-            RTPrintf("             line no %d\n", __LINE__); \
-            cErrors++; \
-        } \
+                RTTestIFailed("FAILURE - %RI64 != %RI64, line no. %d\n", \
+                              RTTimeSpecGetNano(&Ts2), RTTimeSpecGetNano(&Ts1), __LINE__); \
     } while (0)
 
 #define CHECK_TIME(pTime, _i32Year, _u8Month, _u8MonthDay, _u8Hour, _u8Minute, _u8Second, _u32Nanosecond, _u16YearDay, _u8WeekDay, _offUTC, _fFlags)\
@@ -98,15 +98,13 @@ char *ToString(PRTTIME pTime)
             ||  (pTime)->fFlags != (_fFlags) \
             ) \
         { \
-            RTPrintf("tstTimeSpec: FAILURE - %s\n" \
-                     "                    != %04d-%02d-%02dT%02u-%02u-%02u.%09u [YD%u WD%u UO%d F%#x]\n", \
-                     ToString(pTime), (_i32Year), (_u8Month), (_u8MonthDay), (_u8Hour), (_u8Minute), \
-                     (_u8Second), (_u32Nanosecond), (_u16YearDay), (_u8WeekDay), (_offUTC), (_fFlags)); \
-            RTPrintf("             line no %d\n", __LINE__); \
-            cErrors++; \
+            RTTestIFailed("   %s ; line no %d\n" \
+                          "!= %04d-%02d-%02dT%02u-%02u-%02u.%09u [YD%u WD%u UO%d F%#x]\n", \
+                          ToString(pTime), __LINE__, (_i32Year), (_u8Month), (_u8MonthDay), (_u8Hour), (_u8Minute), \
+                          (_u8Second), (_u32Nanosecond), (_u16YearDay), (_u8WeekDay), (_offUTC), (_fFlags)); \
         } \
         else \
-            RTPrintf("          => %s\n", ToString(pTime)); \
+            RTTestIPrintf(RTTESTLVL_ALWAYS, "=> %s\n", ToString(pTime)); \
     } while (0)
 
 #define SET_TIME(pTime, _i32Year, _u8Month, _u8MonthDay, _u8Hour, _u8Minute, _u8Second, _u32Nanosecond, _u16YearDay, _u8WeekDay, _offUTC, _fFlags)\
@@ -122,48 +120,55 @@ char *ToString(PRTTIME pTime)
         (pTime)->u32Nanosecond = (_u32Nanosecond); \
         (pTime)->offUTC = (_offUTC); \
         (pTime)->fFlags = (_fFlags); \
-        RTPrintf("tstTimeSpec: %s\n", ToString(pTime)); \
+        RTTestIPrintf(RTTESTLVL_ALWAYS, "   %s\n", ToString(pTime)); \
     } while (0)
 
 
 int main()
 {
-    unsigned    cErrors = 0;
-    RTTIMESPEC  Now;
-    RTTIMESPEC  Ts1;
-    RTTIMESPEC  Ts2;
-    RTTIME      T1;
-    RTTIME      T2;
+    RTTIMESPEC      Now;
+    RTTIMESPEC      Ts1;
+    RTTIMESPEC      Ts2;
+    RTTIME          T1;
+    RTTIME          T2;
+#ifdef RTTIME_INCL_TIMEVAL
+    struct timeval  Tv1;
+    struct timeval  Tv2;
+    struct timespec Tsp1;
+    struct timespec Tsp2;
+#endif
+    RTTEST          hTest;
+
+    int rc = RTTestInitAndCreate("tstRTTimeSpec", &hTest);
+    if (rc)
+        return rc;
 
     /*
      * Simple test with current time.
      */
+    RTTestSub(hTest, "Current time (UTC)");
     CHECK_NZ(RTTimeNow(&Now));
     CHECK_NZ(RTTimeExplode(&T1, &Now));
-    RTPrintf("tstTimeSpec: %RI64 ns - %s\n", RTTimeSpecGetNano(&Now), ToString(&T1));
+    RTTestIPrintf(RTTESTLVL_ALWAYS, "   %RI64 ns - %s\n", RTTimeSpecGetNano(&Now), ToString(&T1));
     CHECK_NZ(RTTimeImplode(&Ts1, &T1));
     if (!RTTimeSpecIsEqual(&Ts1, &Now))
-    {
-        RTPrintf("tstTimeSpec: FAILURE - %RI64 != %RI64\n", RTTimeSpecGetNano(&Ts1), RTTimeSpecGetNano(&Now));
-        cErrors++;
-    }
+        RTTestIFailed("%RI64 != %RI64\n", RTTimeSpecGetNano(&Ts1), RTTimeSpecGetNano(&Now));
 
     /*
      * Simple test with current local time.
      */
+    RTTestSub(hTest, "Current time (local)");
     CHECK_NZ(RTTimeLocalNow(&Now));
     CHECK_NZ(RTTimeExplode(&T1, &Now));
-    RTPrintf("tstTimeSpec: %RI64 ns - %s\n", RTTimeSpecGetNano(&Now), ToString(&T1));
+    RTTestIPrintf(RTTESTLVL_ALWAYS, "   %RI64 ns - %s\n", RTTimeSpecGetNano(&Now), ToString(&T1));
     CHECK_NZ(RTTimeImplode(&Ts1, &T1));
     if (!RTTimeSpecIsEqual(&Ts1, &Now))
-    {
-        RTPrintf("tstTimeSpec: FAILURE - %RI64 != %RI64\n", RTTimeSpecGetNano(&Ts1), RTTimeSpecGetNano(&Now));
-        cErrors++;
-    }
+        RTTestIFailed("%RI64 != %RI64\n", RTTimeSpecGetNano(&Ts1), RTTimeSpecGetNano(&Now));
 
     /*
      * Some simple tests with fixed dates (just checking for smoke).
      */
+    RTTestSub(hTest, "Smoke");
     TEST_NS(INT64_C(0));
     CHECK_TIME(&T1, 1970,01,01, 00,00,00,        0,   1, 3, 0, RTTIME_FLAGS_TYPE_UTC | RTTIME_FLAGS_COMMON_YEAR);
     TEST_NS(INT64_C(86400000000000));
@@ -177,6 +182,7 @@ int main()
     /*
      * Test the limits.
      */
+    RTTestSub(hTest, "Extremes");
     TEST_NS(INT64_MAX);
     TEST_NS(INT64_MIN);
     TEST_SEC(1095379198);
@@ -192,16 +198,14 @@ int main()
     /*
      * Test normalization (UTC).
      */
+    RTTestSub(hTest, "Normalization (UTC)");
     /* simple */
     CHECK_NZ(RTTimeNow(&Now));
     CHECK_NZ(RTTimeExplode(&T1, &Now));
     T2 = T1;
     CHECK_NZ(RTTimeNormalize(&T1));
     if (memcmp(&T1, &T2, sizeof(T1)))
-    {
-        RTPrintf("tstTimeSpec: FAILURE - simple normalization failed\n");
-        cErrors++;
-    }
+        RTTestIFailed("simple normalization failed\n");
     CHECK_NZ(RTTimeImplode(&Ts1, &T1));
     CHECK_NZ(RTTimeSpecIsEqual(&Ts1, &Now));
 
@@ -307,12 +311,96 @@ int main()
     CHECK_TIME(&T1, 2007,12,06, 02,15,23,        1, 340, 3, 0, RTTIME_FLAGS_TYPE_UTC | RTTIME_FLAGS_COMMON_YEAR);
 
     /*
+     * Convertsions.
+     */
+#define CHECK_NSEC(Ts1, T2) \
+    do { \
+        RTTIMESPEC TsTmp; \
+        RTTESTI_CHECK_MSG( RTTimeSpecGetNano(&(Ts1)) == RTTimeSpecGetNano(RTTimeImplode(&TsTmp, &(T2))), \
+                          ("line %d: %RI64, %RI64\n", __LINE__, \
+                           RTTimeSpecGetNano(&(Ts1)),   RTTimeSpecGetNano(RTTimeImplode(&TsTmp, &(T2)))) ); \
+    } while (0)
+    RTTestSub(hTest, "Conversions, positive");
+    SET_TIME(&T1, 1980,01,01, 00,00,00,        0,   1, 1, 0, RTTIME_FLAGS_TYPE_UTC | RTTIME_FLAGS_LEAP_YEAR);
+    RTTESTI_CHECK(RTTimeSpecSetDosSeconds(&Ts2,    0) == &Ts2);
+    RTTESTI_CHECK(RTTimeSpecGetDosSeconds(&Ts2) == 0);
+    CHECK_NSEC(Ts2, T1);
+
+    SET_TIME(&T1, 1980,01,01, 00,00,00,        0,   1, 1, 0, RTTIME_FLAGS_TYPE_UTC | RTTIME_FLAGS_LEAP_YEAR);
+    RTTESTI_CHECK(RTTimeSpecSetNtTime(&Ts2,    INT64_C(119600064000000000)) == &Ts2);
+    RTTESTI_CHECK(RTTimeSpecGetNtTime(&Ts2) == INT64_C(119600064000000000));
+    CHECK_NSEC(Ts2, T1);
+
+    SET_TIME(&T1, 1970,01,01, 00,00,01,        0,   1, 3, 0, RTTIME_FLAGS_TYPE_UTC | RTTIME_FLAGS_COMMON_YEAR);
+    RTTESTI_CHECK(RTTimeSpecSetSeconds(&Ts2,    1) == &Ts2);
+    RTTESTI_CHECK(RTTimeSpecGetSeconds(&Ts2) == 1);
+    CHECK_NSEC(Ts2, T1);
+
+    SET_TIME(&T1, 1970,01,01, 00,00,01,        0,   1, 3, 0, RTTIME_FLAGS_TYPE_UTC | RTTIME_FLAGS_COMMON_YEAR);
+    RTTESTI_CHECK(RTTimeSpecSetMilli(&Ts2,    1000) == &Ts2);
+    RTTESTI_CHECK(RTTimeSpecGetMilli(&Ts2) == 1000);
+    CHECK_NSEC(Ts2, T1);
+
+    SET_TIME(&T1, 1970,01,01, 00,00,01,        0,   1, 3, 0, RTTIME_FLAGS_TYPE_UTC | RTTIME_FLAGS_COMMON_YEAR);
+    RTTESTI_CHECK(RTTimeSpecSetMicro(&Ts2,    1000000) == &Ts2);
+    RTTESTI_CHECK(RTTimeSpecGetMicro(&Ts2) == 1000000);
+    CHECK_NSEC(Ts2, T1);
+
+    SET_TIME(&T1, 1970,01,01, 00,00,01,        0,   1, 3, 0, RTTIME_FLAGS_TYPE_UTC | RTTIME_FLAGS_COMMON_YEAR);
+    RTTESTI_CHECK(RTTimeSpecSetNano(&Ts2,    1000000000) == &Ts2);
+    RTTESTI_CHECK(RTTimeSpecGetNano(&Ts2) == 1000000000);
+    CHECK_NSEC(Ts2, T1);
+
+#ifdef RTTIME_INCL_TIMEVAL
+    SET_TIME(&T1, 1970,01,01, 00,00,01,     5000,   1, 3, 0, RTTIME_FLAGS_TYPE_UTC | RTTIME_FLAGS_COMMON_YEAR);
+    Tv1.tv_sec  = 1;
+    Tv1.tv_usec = 5;
+    RTTESTI_CHECK(RTTimeSpecSetTimeval(&Ts2, &Tv1) == &Ts2);
+    RTTESTI_CHECK(RTTimeSpecGetMicro(&Ts2) == 1000005);
+    CHECK_NSEC(Ts2, T1);
+    RTTESTI_CHECK(RTTimeSpecGetTimeval(&Ts2, &Tv2) == &Tv2);
+    RTTESTI_CHECK(Tv1.tv_sec == Tv2.tv_sec); RTTESTI_CHECK(Tv1.tv_usec == Tv2.tv_usec);
+#endif
+
+#ifdef RTTIME_INCL_TIMESPEC
+    SET_TIME(&T1, 1970,01,01, 00,00,01,        5,   1, 3, 0, RTTIME_FLAGS_TYPE_UTC | RTTIME_FLAGS_COMMON_YEAR);
+    Tsp1.tv_sec  = 1;
+    Tsp1.tv_nsec = 5;
+    RTTESTI_CHECK(RTTimeSpecSetTimespec(&Ts2, &Tsp1) == &Ts2);
+    RTTESTI_CHECK(RTTimeSpecGetNano(&Ts2) == 1000000005);
+    CHECK_NSEC(Ts2, T1);
+    RTTESTI_CHECK(RTTimeSpecGetTimespec(&Ts2, &Tsp2) == &Tsp2);
+    RTTESTI_CHECK(Tsp1.tv_sec == Tsp2.tv_sec); RTTESTI_CHECK(Tsp1.tv_nsec == Tsp2.tv_nsec);
+#endif
+
+
+    RTTestSub(hTest, "Conversions, negative");
+
+#ifdef RTTIME_INCL_TIMEVAL
+    SET_TIME(&T1, 1969,12,31, 23,59,58,999995000, 365, 2, 0, RTTIME_FLAGS_TYPE_UTC | RTTIME_FLAGS_COMMON_YEAR);
+    Tv1.tv_sec  = -2;
+    Tv1.tv_usec = 999995;
+    RTTESTI_CHECK(RTTimeSpecSetTimeval(&Ts2, &Tv1) == &Ts2);
+    RTTESTI_CHECK_MSG(RTTimeSpecGetMicro(&Ts2) == -1000005, ("%RI64\n", RTTimeSpecGetMicro(&Ts2)));
+    CHECK_NSEC(Ts2, T1);
+    RTTESTI_CHECK(RTTimeSpecGetTimeval(&Ts2, &Tv2) == &Tv2);
+    RTTESTI_CHECK(Tv1.tv_sec == Tv2.tv_sec); RTTESTI_CHECK(Tv1.tv_usec == Tv2.tv_usec);
+#endif
+
+#ifdef RTTIME_INCL_TIMESPEC
+    SET_TIME(&T1, 1969,12,31, 23,59,58,999999995, 365, 2, 0, RTTIME_FLAGS_TYPE_UTC | RTTIME_FLAGS_COMMON_YEAR);
+    Tsp1.tv_sec  = -2;
+    Tsp1.tv_nsec = 999999995;
+    RTTESTI_CHECK(RTTimeSpecSetTimespec(&Ts2, &Tsp1) == &Ts2);
+    RTTESTI_CHECK_MSG(RTTimeSpecGetNano(&Ts2) == -1000000005, ("%RI64\n", RTTimeSpecGetMicro(&Ts2)));
+    CHECK_NSEC(Ts2, T1);
+    RTTESTI_CHECK(RTTimeSpecGetTimespec(&Ts2, &Tsp2) == &Tsp2);
+    RTTESTI_CHECK(Tsp1.tv_sec == Tsp2.tv_sec); RTTESTI_CHECK(Tsp1.tv_nsec == Tsp2.tv_nsec);
+#endif
+
+    /*
      * Summary
      */
-    if (!cErrors)
-        RTPrintf("tstTimeSpec: SUCCESS\n");
-    else
-        RTPrintf("tstTimeSpec: FAILURE - %d errors\n", cErrors);
-    return cErrors ? 1 : 0;
+    return RTTestSummaryAndDestroy(hTest);
 }
 
