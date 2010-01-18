ceb01dba6f7952b292e159dff429a82169c4ea69
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2010-01-18 15:31:35 +0000

    ATA: ataIOPortReadStr1: deal with the unlikely case where no data (or not enough for the read length operation) is available; go back to ring 3.
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@25897 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Devices/Storage/DevATA.cpp
+++ src/VBox/Devices/Storage/DevATA.cpp
@@ -5387,6 +5387,13 @@ PDMBOTHCBDECL(int) ataIOPortReadStr1(PPDMDEVINS pDevIns, void *pvUser, RTIOPORT 
 
         cTransAvailable = (s->iIOBufferPIODataEnd - s->iIOBufferPIODataStart) / cb;
 #ifndef IN_RING3
+        /* Deal with the unlikely case where no data (or not enough for the read length operation) is available; go back to ring 3. */
+        if (!cTransAvailable)
+        {
+            PDMCritSectLeave(&pCtl->lock);
+            return VINF_IOM_HC_IOPORT_READ;
+        }
+
         /* The last transfer unit cannot be handled in GC, as it involves thread communication. */
         cTransAvailable--;
 #endif /* !IN_RING3 */
