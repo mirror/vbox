258bfeae646d07f4579dd2e22dab15a4d5cf95fe
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2010-01-20 07:41:00 +0000

    Main/ApplianceImpl: fixed incorrect throw
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@25919 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Main/ApplianceImpl.cpp
+++ src/VBox/Main/ApplianceImpl.cpp
@@ -935,9 +935,8 @@ int Appliance::readFS(TaskImportOVF *pTask)
         char *pszDigest;
         int vrc = RTSha1Digest(pTask->locInfo.strPath.c_str(), &pszDigest);
         if (RT_FAILURE(vrc))
-            throw setError(VBOX_E_FILE_ERROR,
-                           tr("Couldn't calculate SHA1 digest for file '%s' (%Rrc)"),
-                           RTPathFilename(pTask->locInfo.strPath.c_str()), vrc);
+            throw OVFLogicError(tr("Couldn't calculate SHA1 digest for file '%s' (%Rrc)"),
+                                RTPathFilename(pTask->locInfo.strPath.c_str()), vrc);
         m->strOVFSHA1Digest = pszDigest;
         RTStrFree(pszDigest);
     }
