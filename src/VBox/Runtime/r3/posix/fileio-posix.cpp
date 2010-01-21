b77419ce27e83dfc804112dd2d6b802a9b77e034
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2010-01-21 10:20:58 +0000

    Runtime/RTFileIoCtl: fixed prototype (iRequest => ulRequest)
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@25953 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Runtime/r3/posix/fileio-posix.cpp
+++ src/VBox/Runtime/r3/posix/fileio-posix.cpp
@@ -586,9 +586,9 @@ RTR3DECL(int)  RTFileFlush(RTFILE File)
 }
 
 
-RTR3DECL(int) RTFileIoCtl(RTFILE File, int iRequest, void *pvData, unsigned cbData, int *piRet)
+RTR3DECL(int) RTFileIoCtl(RTFILE File, unsigned long ulRequest, void *pvData, unsigned cbData, int *piRet)
 {
-    int rc = ioctl((int)File, iRequest, pvData);
+    int rc = ioctl((int)File, ulRequest, pvData);
     if (piRet)
         *piRet = rc;
     return rc >= 0 ? VINF_SUCCESS : RTErrConvertFromErrno(errno);
