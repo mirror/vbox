303d8dff43ab5faf116fc051fb47713fb561a595
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2010-01-24 20:51:35 +0000

    fix OSE
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@25988 cfe28804-0f27-0410-a406-dd0f0b0b656f

---
+++ src/VBox/Additions/x11/x11stubs/libXdamage-1.1.0/libXdamage.c
@@ -0,0 +1,37 @@
+/* $Id$ */
+
+/** @file
+ * X.Org libXdamage.so linker stub
+ */
+
+/*
+ * Copyright (C) 2010 Sun Microsystems, Inc.
+ *
+ * This file is part of VirtualBox Open Source Edition (OSE), as
+ * available from http://www.virtualbox.org. This file is free software;
+ * you can redistribute it and/or modify it under the terms of the GNU
+ * General Public License (GPL) as published by the Free Software
+ * Foundation, in version 2 as it comes in the "COPYING" file of the
+ * VirtualBox OSE distribution. VirtualBox OSE is distributed in the
+ * hope that it will be useful, but WITHOUT ANY WARRANTY of any kind.
+ *
+ * Please contact Sun Microsystems, Inc., 4150 Network Circle, Santa
+ * Clara, CA 95054 USA or visit http://www.sun.com if you need
+ * additional information or have any questions.
+ */
+
+#include <iprt/cdefs.h>
+#include <iprt/types.h>
+
+#define DECLXSTUB(func) \
+    DECLEXPORT(void) func(void); \
+    void func(void) {}
+
+DECLXSTUB(XDamageCreate)
+DECLXSTUB(XDamageFindDisplay)
+DECLXSTUB(XDamageQueryExtension)
+DECLXSTUB(XDamageSubtract)
+DECLXSTUB(XDamageAdd)
+DECLXSTUB(XDamageDestroy)
+DECLXSTUB(XDamageExtensionInfo)
+DECLXSTUB(XDamageQueryVersion)
