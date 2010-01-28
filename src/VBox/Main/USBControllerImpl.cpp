db69727f2300ee7df57b5a2c9559dd04048435c5
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2010-01-28 13:59:20 +0000

    Main: fix locking order violation when loading USB filters
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@26084 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Main/USBControllerImpl.cpp
+++ src/VBox/Main/USBControllerImpl.cpp
@@ -610,14 +610,12 @@ STDMETHODIMP USBController::RemoveDeviceFilter(ULONG aPosition,
  *
  *  @param aMachineNode <Machine> node.
  *
- *  @note Locks this object for writing.
+ *  @note Does not lock "this" as Machine::loadHardware, which calls this, does not lock either.
  */
 HRESULT USBController::loadSettings(const settings::USBController &data)
 {
     AutoCaller autoCaller(this);
     AssertComRCReturnRC(autoCaller.rc());
-
-    AutoWriteLock alock(this COMMA_LOCKVAL_SRC_POS);
 
     /* Note: we assume that the default values for attributes of optional
      * nodes are assigned in the Data::Data() constructor and don't do it
