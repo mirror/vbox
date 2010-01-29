5d9714dc56c8761bbf45027723f69c6ffa2d647e
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2010-01-29 09:12:27 +0000

    Branding: right fix for r57057
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@26093 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- include/VBox/version.h
+++ include/VBox/version.h
@@ -30,6 +30,9 @@
 #ifndef ___VBox_version_h
 #define ___VBox_version_h
 
+/** Product info */
+#include <product-generated.h>
+
 #ifndef RC_INVOKED
 # include <version-generated.h>
 
@@ -68,16 +71,6 @@
 # define VBOX_FULL_VERSION_GET_BUILD(uFullVer)  ( ((uFullVer)      ) & 0xffffU )
 
 #endif /* !RC_INVOKED */
-
-/** Vendor name */
-#ifndef VBOX_VENDOR
-# define VBOX_VENDOR                     "Sun Microsystems, Inc."
-#endif
-
-/** Product name */
-#ifndef VBOX_PRODUCT
-# define VBOX_PRODUCT                    "Sun VirtualBox"
-#endif
 
 /** @name Prefined strings for Windows resource files
  *
