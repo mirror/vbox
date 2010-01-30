4b1341c262b66683bb9d2c74f7df3b1743e5a2c1
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2010-01-30 01:34:37 +0000

    PGMPhys.cpp: Count ROM pages correctly. Should fix most of the trouble in #4457.
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@26105 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/VMM/PGMPhys.cpp
+++ src/VBox/VMM/PGMPhys.cpp
@@ -2239,7 +2239,7 @@ VMMR3DECL(int) PGMR3PhysRomRegister(PVM pVM, PPDMDEVINS pDevIns, RTGCPHYS GCPhys
     /*
      * Update the base memory reservation if necessary.
      */
-    uint32_t cExtraBaseCost = fRamExists ? cPages : 0;
+    uint32_t cExtraBaseCost = fRamExists ? 0 : cPages;
     if (fFlags & PGMPHYS_ROM_FLAGS_SHADOWED)
         cExtraBaseCost += cPages;
     if (cExtraBaseCost)
