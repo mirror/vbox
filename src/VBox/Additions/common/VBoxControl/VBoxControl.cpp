56c9077d14fc1cec849a6978dba6b7fae7fd005b
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2010-02-03 14:55:20 +0000

    Branding: build fix
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@26199 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Additions/common/VBoxControl/VBoxControl.cpp
+++ src/VBox/Additions/common/VBoxControl/VBoxControl.cpp
@@ -1347,7 +1347,7 @@ int main(int argc, char **argv)
     if (showlogo)
         RTPrintf(VBOX_PRODUCT " Guest Additions Command Line Management Interface Version "
                  VBOX_VERSION_STRING "\n"
-                 "(C) 2008-" VBOX_C_YEAR " " VBOX_VENDER "\n"
+                 "(C) 2008-" VBOX_C_YEAR " " VBOX_VENDOR "\n"
                  "All rights reserved.\n\n");
     if (dohelp)
         usage();
