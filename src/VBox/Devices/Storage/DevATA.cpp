2565613b4de3667e38bcba3cbbca8d8ce5cdd0a0
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2010-02-05 13:45:22 +0000

    DevATA: Initializers... wonder if all compilers will like this.
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@26285 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Devices/Storage/DevATA.cpp
+++ src/VBox/Devices/Storage/DevATA.cpp
@@ -683,11 +683,10 @@ static const PSourceSinkFunc g_apfnSourceSinkFuncs[ATAFN_SS_MAX] =
 };
 
 
-static const ATARequest g_ataDMARequest = { ATA_AIO_DMA, };
-static const ATARequest g_ataPIORequest = { ATA_AIO_PIO, };
-static const ATARequest g_ataResetARequest = { ATA_AIO_RESET_ASSERTED, };
-static const ATARequest g_ataResetCRequest = { ATA_AIO_RESET_CLEARED, };
-
+static const ATARequest g_ataDMARequest    = { ATA_AIO_DMA,            { { 0, 0, 0, 0, 0 } } };
+static const ATARequest g_ataPIORequest    = { ATA_AIO_PIO,            { { 0, 0, 0, 0, 0 } } };
+static const ATARequest g_ataResetARequest = { ATA_AIO_RESET_ASSERTED, { { 0, 0, 0, 0, 0 } } };
+static const ATARequest g_ataResetCRequest = { ATA_AIO_RESET_CLEARED,  { { 0, 0, 0, 0, 0 } } };
 
 static void ataAsyncIOClearRequests(PATACONTROLLER pCtl)
 {
