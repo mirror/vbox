b0ae9ff1ec9ac93f870d0b5a89bf2f5bc4b1f213
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2010-02-08 16:17:35 +0000

    SMC: read OSK from the device
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@26331 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Main/ConsoleImpl2.cpp
+++ src/VBox/Main/ConsoleImpl2.cpp
@@ -157,6 +157,24 @@ static int findEfiRom(IVirtualBox* vbox, FirmwareType_T aFirmwareType, Utf8Str& 
     return S_OK;
 }
 
+static int getSmcDeviceKey(IMachine* pMachine, BSTR * aKey)
+{
+    int rc;
+
+# if defined(RT_OS_DARWIN)
+    char aKeyBuf[65];
+
+    rc = DarwinSmcKey(aKeyBuf, sizeof aKeyBuf);
+    if (SUCCEEDED(rc))
+    {
+        Bstr(aKeyBuf).detachTo(aKey);
+        return rc;
+    }
+#endif
+
+    return pMachine->GetExtraData(Bstr("VBoxInternal2/SmcDeviceKey"), aKey);
+}
+
 /**
  *  Construct the VM configuration tree (CFGM).
  *
@@ -539,7 +557,7 @@ DECLCALLBACK(int) Console::configConstructor(PVM pVM, void *pvConsole)
         rc = CFGMR3InsertNode(pDev,     "0", &pInst);                        RC_CHECK();
         rc = CFGMR3InsertInteger(pInst, "Trusted",   1);     /* boolean */   RC_CHECK();
         rc = CFGMR3InsertNode(pInst,    "Config", &pCfg);                    RC_CHECK();
-        rc = pMachine->GetExtraData(Bstr("VBoxInternal2/SmcDeviceKey"), tmpStr2.asOutParam()); RC_CHECK();
+        rc = getSmcDeviceKey(pMachine,   tmpStr2.asOutParam());              RC_CHECK();
         rc = CFGMR3InsertString(pCfg,   "DeviceKey", Utf8Str(tmpStr2).raw());RC_CHECK();
     }
 
