5cc14d0417c75c712c3824ff475c658a642c4b56
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2010-02-12 08:17:33 +0000

    Fixed regression
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@26455 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Runtime/r0drv/nt/memobj-r0drv-nt.cpp
+++ src/VBox/Runtime/r0drv/nt/memobj-r0drv-nt.cpp
@@ -349,7 +349,7 @@ static int rtR0MemObjNativeAllocContEx(PPRTR0MEMOBJINTERNAL ppMem, size_t cb, bo
     PHYSICAL_ADDRESS PhysAddrHighest, PhysAddrLowest, PhysAddrBoundary;
     PhysAddrHighest.QuadPart  = PhysHighest;
     PhysAddrLowest.QuadPart   = 0;
-    PhysAddrBoundary.QuadPart = uAlignment;
+    PhysAddrBoundary.QuadPart = (uAlignment == PAGE_SIZE) ? 0 : uAlignment;
     void *pv = MmAllocateContiguousMemorySpecifyCache(cb, PhysAddrLowest, PhysAddrHighest, PhysAddrBoundary, MmCached);
     if (!pv)
         return VERR_NO_MEMORY;
