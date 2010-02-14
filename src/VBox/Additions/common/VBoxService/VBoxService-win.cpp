d5e62d72193b0fa6e31681fe712865589846edb2
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2010-02-14 07:32:29 +0000

    Misc svn:eol-style correction and some manual cleanup.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@26489 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Additions/common/VBoxService/VBoxService-win.cpp
+++ src/VBox/Additions/common/VBoxService/VBoxService-win.cpp
@@ -128,7 +128,7 @@ BOOL VBoxServiceWinSetStatus(DWORD dwStatus, DWORD dwCheckPoint)
     SERVICE_STATUS ss;
     ss.dwServiceType              = SERVICE_WIN32_OWN_PROCESS;
     ss.dwCurrentState             = g_rcWinService;
-    ss.dwControlsAccepted	      = SERVICE_ACCEPT_STOP | SERVICE_ACCEPT_SHUTDOWN;
+    ss.dwControlsAccepted         = SERVICE_ACCEPT_STOP | SERVICE_ACCEPT_SHUTDOWN;
     ss.dwWin32ExitCode            = NO_ERROR;
     ss.dwServiceSpecificExitCode  = 0; /* Not used */
     ss.dwCheckPoint               = dwCheckPoint;
