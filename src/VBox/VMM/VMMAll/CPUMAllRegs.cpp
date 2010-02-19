f46d8fb685339e7d72381c7cfd0af2b1c34bc73b
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2010-02-19 13:16:11 +0000

    CPUM: better cache sharing descriptor - L2 cache is shared amongst all cores
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@26649 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/VMM/VMMAll/CPUMAllRegs.cpp
+++ src/VBox/VMM/VMMAll/CPUMAllRegs.cpp
@@ -1121,7 +1121,7 @@ VMMDECL(void) CPUMGetGuestCpuId(PVMCPU pVCpu, uint32_t iLeaf, uint32_t *pEax, ui
             case 2:
                 level = 2;
                 type = 3;
-                sharing = 1;
+                sharing = cores; /* our L2 cache is modelled as shared between all cores */ 
                 linesize = 64;
                 associativity = 24;
                 sets = 4096;
