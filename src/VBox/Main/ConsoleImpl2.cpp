44343319a42f64e6c5a1eef7fce20b425c62572d
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2010-02-22 16:38:15 +0000

    Main: Transfer tablet/mouse setting to USB device.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@26681 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Main/ConsoleImpl2.cpp
+++ src/VBox/Main/ConsoleImpl2.cpp
@@ -1924,15 +1924,23 @@ DECLCALLBACK(int) Console::configConstructor(PVM pVM, void *pvConsole)
             rc = CFGMR3InsertString(pCfg,   "Format", "VDI");                       RC_CHECK();
 # endif
 
-            /* Virtual USB Mouse*/
+            /* Virtual USB Mouse/Tablet */
             PointingHidType_T aPointingHid;
             hrc = pMachine->COMGETTER(PointingHidType)(&aPointingHid);               H();
-            if (aPointingHid == PointingHidType_USBMouse)
+            if (aPointingHid == PointingHidType_USBMouse || aPointingHid == PointingHidType_USBTablet)
             {
                 rc = CFGMR3InsertNode(pUsbDevices, "HidMouse", &pDev);               RC_CHECK();
                 rc = CFGMR3InsertNode(pDev,     "0", &pInst);                        RC_CHECK();
                 rc = CFGMR3InsertNode(pInst,    "Config", &pCfg);                    RC_CHECK();
 
+                if (aPointingHid == PointingHidType_USBTablet)
+                {
+                    rc = CFGMR3InsertInteger(pCfg, "Absolute", 1);                   RC_CHECK();
+                }
+                else
+                {
+                    rc = CFGMR3InsertInteger(pCfg, "Absolute", 0);                   RC_CHECK();
+                }
                 rc = CFGMR3InsertNode(pInst,    "LUN#0", &pLunL0);                   RC_CHECK();
                 rc = CFGMR3InsertString(pLunL0, "Driver",        "MouseQueue");      RC_CHECK();
                 rc = CFGMR3InsertNode(pLunL0,   "Config", &pCfg);                    RC_CHECK();
