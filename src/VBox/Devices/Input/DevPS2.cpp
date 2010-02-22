a1d362a4d0f2c628cdbc17cdf821457a88012143
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2010-02-22 22:25:22 +0000

    Devices/PS/2: do not report that the touchscreen is being touched, as that may be treated as button 1 being held down
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@26690 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Devices/Input/DevPS2.cpp
+++ src/VBox/Devices/Input/DevPS2.cpp
@@ -853,7 +853,7 @@ static void kbd_mouse_send_abs_packet(KBDState *s, bool fToCmdQueue)
 
     LogRel3(("%s: cx1=%d, cy1=%d, fButtons=0x%x\n", __PRETTY_FUNCTION__,
              cx1, cy1, fButtons));
-    b[0] = 4 /* Screen is being touched */ | fButtons;
+    b[0] = fButtons;
     Assert((b[0] & 0xf8) == 0);
     kbd_queue(s, b[0], aux);
     b[1] = ((cy1 << 2) & 0xc0) | (cx1 >> 6);
