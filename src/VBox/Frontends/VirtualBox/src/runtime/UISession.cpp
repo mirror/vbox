d25ffb07fe325094a0e9a27ea005ae2cbff41c7c
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2010-02-26 12:52:11 +0000

    FE/Qt4: new core: This is COM referenced counted. No need to delete it manually.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@26842 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Frontends/VirtualBox/src/runtime/UISession.cpp
+++ src/VBox/Frontends/VirtualBox/src/runtime/UISession.cpp
@@ -570,8 +570,6 @@ UISession::~UISession()
 
     /* Unregister console callback: */
     m_session.GetConsole().UnregisterCallback(m_callback);
-    delete m_pCallback;
-    m_pCallback = 0;
 }
 
 bool UISession::setPause(bool fOn)
