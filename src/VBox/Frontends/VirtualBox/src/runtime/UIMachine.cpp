3b27708679348d06bdaad6d82e86b83e2b5fe8ee
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2010-03-02 17:16:38 +0000

    FE/Qt4: new core: not Cocoa specific
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@26962 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Frontends/VirtualBox/src/runtime/UIMachine.cpp
+++ src/VBox/Frontends/VirtualBox/src/runtime/UIMachine.cpp
@@ -30,9 +30,7 @@
 #include "UIMachineWindow.h"
 
 #ifdef Q_WS_MAC
-# ifdef QT_MAC_USE_COCOA
-#  include <Carbon/Carbon.h>
-# endif /* QT_MAC_USE_COCOA */
+# include <Carbon/Carbon.h>
 #endif /* Q_WS_MAC */
 
 class UIVisualState : public QObject
