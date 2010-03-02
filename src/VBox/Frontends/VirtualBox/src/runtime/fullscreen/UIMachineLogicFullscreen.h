a68f5827620ff7a9994580e1d23b9d15997caffd
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2010-03-02 11:00:50 +0000

    FE/Qt4: new core: Presentation mode belongs to the machine logic not a particular window.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@26937 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Frontends/VirtualBox/src/runtime/fullscreen/UIMachineLogicFullscreen.h
+++ src/VBox/Frontends/VirtualBox/src/runtime/fullscreen/UIMachineLogicFullscreen.h
@@ -59,6 +59,12 @@ private:
     void cleanupMachineWindow();
     void cleanupActionConnections() {}
 
+#ifdef Q_WS_MAC
+# ifdef QT_MAC_USE_COCOA
+    void setPresentationModeEnabled(bool fEnabled);
+# endif /* QT_MAC_USE_COCOA */
+#endif /* Q_WS_MAC */
+
     /* Friend classes: */
     friend class UIMachineLogic;
 };
