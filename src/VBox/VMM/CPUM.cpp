1af11a53260dc59c4aa4d79adc41f7fbe6f496bb
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2010-03-03 13:57:20 +0000

    HTT doesn't make sense in single cpu configs
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@26990 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/VMM/CPUM.cpp
+++ src/VBox/VMM/CPUM.cpp
@@ -567,7 +567,8 @@ static int cpumR3CpuIdInit(PVM pVM)
     {
         /* If CPUID Fn0000_0001_EDX[HTT] = 1 then LogicalProcessorCount is the number of threads per CPU core times the number of CPU cores per processor */
         pCPUM->aGuestCpuIdStd[1].ebx |= (pVM->cCpus << 16);
-        pCPUM->aGuestCpuIdStd[1].edx |= X86_CPUID_FEATURE_EDX_HTT;  /* necessary for hyper-threading *or* multi-core CPUs */
+        if (pVM->cCpus > 1)
+            pCPUM->aGuestCpuIdStd[1].edx |= X86_CPUID_FEATURE_EDX_HTT;  /* necessary for hyper-threading *or* multi-core CPUs */
     }
 #endif
 
