95f3503121e2861df8af4d8ae8f0116d3fc4c987
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2010-03-07 16:55:06 +0000

    memobj-r0drv-freebsd.c: Major rewrite again. Removed most of the vm_object_t usage and changed the Phys/PhysNC allocators again. Seems to work this time but only tested on FreeBSD 9-CURRENT amd64 so far, so be careful
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@27146 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Runtime/r0drv/freebsd/the-freebsd-kernel.h
+++ src/VBox/Runtime/r0drv/freebsd/the-freebsd-kernel.h
@@ -64,6 +64,8 @@
 #include <vm/vm_kern.h>
 #include <vm/vm_param.h>        /* KERN_SUCCESS ++ */
 #include <vm/vm_page.h>
+#include <vm/vm_phys.h>         /* vm_phys_alloc_* */
+#include <sys/vmmeter.h>        /* cnt */
 #include <sys/resourcevar.h>
 #include <machine/cpu.h>
 
