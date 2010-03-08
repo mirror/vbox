d754e2c24d4d0e0516903f0a145062c28d1e579b
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2010-03-08 15:57:34 +0000

    Compile fix
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@27173 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/VMM/PGMPhys.cpp
+++ src/VBox/VMM/PGMPhys.cpp
@@ -3274,7 +3274,7 @@ VMMR3DECL(int) PGMR3PhysAllocateLargeHandyPage(PVM pVM, RTGCPHYS GCPhys)
             /*
              * Clear the pages.
              */
-            STAM_PROFILE_START(&pVM->pgm.s.StatClearLargePage, a);
+            STAM_PROFILE_START(&pVM->pgm.s.StatClearLargePage, b);
             for (unsigned i = 0; i < _2M/PAGE_SIZE; i++)
             {
                 ASMMemZeroPage(pv);
@@ -3306,12 +3306,12 @@ VMMR3DECL(int) PGMR3PhysAllocateLargeHandyPage(PVM pVM, RTGCPHYS GCPhys)
 
                 Log3(("PGMR3PhysAllocateLargePage: idPage=%#x HCPhys=%RGp\n", idPage, HCPhys));
             }
-            STAM_PROFILE_STOP(&pVM->pgm.s.StatClearLargePage, a);
+            STAM_PROFILE_STOP(&pVM->pgm.s.StatClearLargePage, b);
 
             /* Flush all TLBs. */
             PGM_INVL_ALL_VCPU_TLBS(pVM);
             PGMPhysInvalidatePageMapTLB(pVM);
-       }
+        }
         pVM->pgm.s.cLargeHandyPages = 0;
     }
 
