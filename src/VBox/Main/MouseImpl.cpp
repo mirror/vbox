90b883c9ae90f471b2a26bbbc3f01b42bae9e0de
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2010-03-08 23:07:52 +0000

    Main/MouseImpl.cpp: updated a todo
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@27189 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Main/MouseImpl.cpp
+++ src/VBox/Main/MouseImpl.cpp
@@ -536,10 +536,7 @@ void Mouse::sendMouseCapsNotifications(void)
     mParent->onMouseCapabilityChange(fAbsDev || fVMMDevCanAbs, fRelDev,
                                      fVMMDevNeedsHostCursor);
     /** @todo if this gets called during device initialisation we get an
-     * error due to VMMDev not being initialised yet.  This is currently
-     * worked around in DevPS2 (which doesn't need the capability anyway of
-     * course), but it seems to me that Main should be initialised before
-     * the devices are. */
+     * error due to VMMDev not being initialised yet. */
     if (fAbsDev && !(uHostCaps & VMMDEV_MOUSE_HOST_HAS_ABS_DEV))
         uHostCaps |= VMMDEV_MOUSE_HOST_HAS_ABS_DEV;
     if (!fAbsDev && (uHostCaps & VMMDEV_MOUSE_HOST_HAS_ABS_DEV))
