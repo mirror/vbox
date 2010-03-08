65529c31dacb309d5c752197795e1c20e3a141ed
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2010-03-08 17:39:02 +0000

    Alignment
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@27184 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/VMM/PGMInternal.h
+++ src/VBox/VMM/PGMInternal.h
@@ -2782,6 +2782,7 @@ typedef struct PGM
     uint32_t                        cWriteLockedPages;  /**< The number of write locked pages. */
     uint32_t                        cReadLockedPages;   /**< The number of read locked pages. */
     uint32_t                        cBalloonedPages;    /**< The number of ballooned pages. */
+    uint32_t                        aAlignment4[1];
 
     /** The number of times we were forced to change the hypervisor region location. */
     STAMCOUNTER                     cRelocations;
