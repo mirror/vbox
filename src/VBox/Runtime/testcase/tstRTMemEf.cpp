1aeb6f0c53fdaaa5f8a823114f54e8f73bc5afbd
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2010-03-11 10:15:46 +0000

    Runtime/testcase: basic electric fence allocation testcase
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@27269 cfe28804-0f27-0410-a406-dd0f0b0b656f

---
+++ src/VBox/Runtime/testcase/tstRTMemEf.cpp
@@ -0,0 +1,87 @@
+/* $Id$ */
+/** @file
+ * IPRT - Testcase for the RTMemEf* functions.
+ */
+
+/*
+ * Copyright (C) 2010 Sun Microsystems, Inc.
+ *
+ * This file is part of VirtualBox Open Source Edition (OSE), as
+ * available from http://www.virtualbox.org. This file is free software;
+ * you can redistribute it and/or modify it under the terms of the GNU
+ * General Public License (GPL) as published by the Free Software
+ * Foundation, in version 2 as it comes in the "COPYING" file of the
+ * VirtualBox OSE distribution. VirtualBox OSE is distributed in the
+ * hope that it will be useful, but WITHOUT ANY WARRANTY of any kind.
+ *
+ * The contents of this file may alternatively be used under the terms
+ * of the Common Development and Distribution License Version 1.0
+ * (CDDL) only, as it comes in the "COPYING.CDDL" file of the
+ * VirtualBox OSE distribution, in which case the provisions of the
+ * CDDL are applicable instead of those of the GPL.
+ *
+ * You may elect to license modified versions of this file under the
+ * terms and conditions of either the GPL or the CDDL or both.
+ *
+ * Please contact Sun Microsystems, Inc., 4150 Network Circle, Santa
+ * Clara, CA 95054 USA or visit http://www.sun.com if you need
+ * additional information or have any questions.
+ */
+
+/*******************************************************************************
+*   Header Files                                                               *
+*******************************************************************************/
+#include <iprt/mem.h>
+#include <iprt/stream.h>
+#include <iprt/initterm.h>
+#include <iprt/string.h>
+
+
+/*******************************************************************************
+*   Global Variables                                                           *
+*******************************************************************************/
+static unsigned g_cErrors = 0;
+
+
+static int tstMemAllocEfAccess()
+{
+    int32_t v;
+
+    /* Trivial alloc fence test - allocate a single word and access both
+     * the word after the allocated block and the word before. One of them
+     * will crash no matter whether the fence is at the bottom or on top. */
+    int32_t *p = (int32_t *)RTMemEfAlloc(sizeof(int32_t));
+    RTPrintf("tstRTMemAllocEfAccess: allocated int32_t at %#p\n");
+    RTPrintf("tstRTMemAllocEfAccess: triggering buffer overrun...\n");
+    v = *(p + 1);
+    RTPrintf("tstRTMemAllocEfAccess: triggering buffer underrun...\n");
+    v = *(p - 1);
+
+    /* Reaching this is a severe error. */
+    return VERR_GENERAL_FAILURE;
+}
+
+int main()
+{
+    RTR3Init();
+    RTPrintf("tstRTMemEf: TESTING...\n");
+
+#define CHECK_EXPR(expr) \
+    do { bool const f = !!(expr); if (!f) { RTPrintf("tstRTMemEf(%d): %s!\n", __LINE__, #expr); g_cErrors++; } } while (0)
+
+    /*
+     * Some simple stuff.
+     */
+    {
+        CHECK_EXPR(tstMemAllocEfAccess());
+    }
+
+    /*
+     * Summary.
+     */
+    if (!g_cErrors)
+        RTPrintf("tstMemAutoPtr: SUCCESS\n");
+    else
+        RTPrintf("tstMemAutoPtr: FAILED - %d errors\n", g_cErrors);
+    return !!g_cErrors;
+}
