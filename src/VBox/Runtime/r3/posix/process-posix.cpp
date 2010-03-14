c3c4a7e6dd7a9da3a7a9708ef738e924f9e3daa7
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2010-03-14 18:37:17 +0000

    process-posix.cpp: copy & past thing in commented out code.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@27349 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Runtime/r3/posix/process-posix.cpp
+++ src/VBox/Runtime/r3/posix/process-posix.cpp
@@ -119,7 +119,7 @@ RTR3DECL(int)   RTProcCreateEx(const char *pszExec, const char * const *papszArg
 
                 case RTHANDLETYPE_SOCKET:
                     aStdFds[i] = paHandles[i]->u.hSocket != NIL_RTSOCKET
-                               ? (int)paHandles[i]->u.hSocket //RTPipeToNative(paHandles[i]->u.hPipe)
+                               ? (int)paHandles[i]->u.hSocket //RTTcpToNative(paHandles[i]->u.hSocket)
                                : -2 /* close it */;
                     break;
 
