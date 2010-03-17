116ce24b56e190460d5d41aa3db355a28b946ab8
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2010-03-17 16:09:33 +0000

    FE/Qt4: New running VM core: some cleanup.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@27455 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Frontends/VirtualBox/src/runtime/seamless/UIMachineViewSeamless.cpp
+++ src/VBox/Frontends/VirtualBox/src/runtime/seamless/UIMachineViewSeamless.cpp
@@ -181,14 +181,12 @@ bool UIMachineViewSeamless::event(QEvent *pEvent)
         }
 
         case VBoxDefs::ResizeEventType:
-        case VBoxDefs::RepaintEventType:
         {
-            /* Unlock on guest resize & repaint events: */
+            /* Unlock after processing guest resize event: */
             bool fResult = UIMachineView::event(pEvent);
             if (m_pSyncBlocker && m_pSyncBlocker->isRunning())
                 m_pSyncBlocker->quit();
             return fResult;
-            break;
         }
 
         default:
