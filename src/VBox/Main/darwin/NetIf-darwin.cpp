17e520296de05ba4a57dbf40c82fd37f4e95fe4f
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2010-03-19 18:37:21 +0000

    Net-OSX: remove debugging code
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@27550 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Main/darwin/NetIf-darwin.cpp
+++ src/VBox/Main/darwin/NetIf-darwin.cpp
@@ -409,25 +409,12 @@ int NetIfList(std::list <ComObjPtr<HostNetworkInterface> > &list)
 
             ComObjPtr<HostNetworkInterface> IfObj;
             IfObj.createObject();
-            try
-            {
-#ifdef DEBUG
-                printf("IF '%s'\n", pNew->szName);
-#endif /* DEBUG */
-                if (SUCCEEDED(IfObj->init(Bstr(pNew->szName), enmType, pNew)))
-                    /* Make sure the default interface gets to the beginning. */
-                    if (pIfMsg->ifm_index == u16DefaultIface)
-                        list.push_front(IfObj);
-                    else
-                        list.push_back(IfObj);
-            }
-            catch(...)
-            {
-#ifdef DEBUG
-                printf("Throw happens\n");
-#endif /* DEBUG */
-            }
-
+            if (SUCCEEDED(IfObj->init(Bstr(pNew->szName), enmType, pNew)))
+                /* Make sure the default interface gets to the beginning. */
+                if (pIfMsg->ifm_index == u16DefaultIface)
+                    list.push_front(IfObj);
+                else
+                    list.push_back(IfObj);
         }
         RTMemFree(pNew);
     }
