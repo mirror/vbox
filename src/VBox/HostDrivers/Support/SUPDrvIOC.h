1a680b637cf0620cd565464885774e3b935143c1
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2010-03-23 03:22:27 +0000

    SUPDrvIOC.h: More sparc (just to make the VBoxRT deps build).
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@27617 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/HostDrivers/Support/SUPDrvIOC.h
+++ src/VBox/HostDrivers/Support/SUPDrvIOC.h
@@ -42,9 +42,9 @@
  * The SUP_IOCTL_FLAG macro is used to separate requests from 32-bit
  * and 64-bit processes.
  */
-#ifdef RT_ARCH_AMD64
+#if defined(RT_ARCH_AMD64) || defined(RT_ARCH_SPARC64)
 # define SUP_IOCTL_FLAG     128
-#elif defined(RT_ARCH_X86)
+#elif defined(RT_ARCH_X86) || defined(RT_ARCH_SPARC)
 # define SUP_IOCTL_FLAG     0
 #else
 # error "dunno which arch this is!"
