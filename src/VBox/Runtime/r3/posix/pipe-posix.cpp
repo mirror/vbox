ea14561055ce37257cc4f575e62dac5ab932296d
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2010-03-25 21:41:33 +0000

    RTPipeFromNative/posix: Solaris creates bi-directional pipes so we have to allow for O_RDWR too.
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@27721 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Runtime/r3/posix/pipe-posix.cpp
+++ src/VBox/Runtime/r3/posix/pipe-posix.cpp
@@ -198,7 +198,9 @@ RTDECL(int)  RTPipeFromNative(PRTPIPE phPipe, RTHCINTPTR hNativePipe, uint32_t f
 
     int fFd = fcntl(hNative, F_GETFL, 0);
     AssertReturn(fFd != -1, VERR_INVALID_HANDLE);
-    AssertMsgReturn((fFd & O_ACCMODE) == (fFlags & RTPIPE_N_READ ? O_RDONLY : O_WRONLY), ("%#x\n", fFd), VERR_INVALID_HANDLE);
+    AssertMsgReturn(   (fFd & O_ACCMODE) == (fFlags & RTPIPE_N_READ ? O_RDONLY : O_WRONLY)
+                    || (fFd & O_ACCMODE) == O_RDWR /* Solaris creates bi-directional pipes. */
+                    , ("%#x\n", fFd), VERR_INVALID_HANDLE);
 
     /*
      * Create the handle.
