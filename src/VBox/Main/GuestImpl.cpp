8e76e89e296c4e6969d0e739cb113461f178e017
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2010-03-26 13:45:10 +0000

    Guest Control: Update (Main, Host Service, Testcase).
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@27741 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Main/GuestImpl.cpp
+++ src/VBox/Main/GuestImpl.cpp
@@ -31,6 +31,10 @@
 #include "Logging.h"
 
 #include <VBox/VMMDev.h>
+#ifdef VBOX_WITH_GUEST_CONTROL
+# include <VBox/HostServices/GuestControlSvc.h>
+# include <VBox/com/array.h>
+#endif
 #include <iprt/cpp/utils.h>
 
 // defines
@@ -307,6 +311,56 @@ STDMETHODIMP Guest::SetStatistic(ULONG aCpuId, GuestStatisticType_T aStatistic, 
     return S_OK;
 }
 
+HRESULT Guest::prepareExecuteArgs(ComSafeArrayIn(IN_BSTR, aArguments),
+                                  char **ppszArgv, uint32_t *pcbList, uint32_t *pcArgs)
+{
+    com::SafeArray<IN_BSTR> args(ComSafeArrayInArg(aArguments));
+    char *pszArgs;
+    const char *pszCurArg;
+
+    HRESULT rc = S_OK;
+    int vrc = VINF_SUCCESS;
+
+    for (unsigned i = 0; i < args.size(); ++i)
+    {
+        if (i > 0) /* Insert space as delimiter. */
+            vrc = RTStrAAppendN(&pszArgs, " ", 1);
+        if (RT_SUCCESS(vrc))
+        {
+            pszCurArg = Utf8Str(args[i]).raw();
+            vrc = RTStrAAppendN(&pszArgs, pszCurArg, strlen(pszCurArg));
+        }
+        if (RT_FAILURE(vrc))
+            break;
+    }
+
+    if (RT_SUCCESS(rc))
+    {
+        *ppszArgv = pszArgs;
+        *pcArgs = args.size();
+        *pcbList = strlen(pszArgs) + 1; /* Include zero termination. */
+    }
+    else
+    {
+        rc = setError(E_UNEXPECTED,
+                              tr("The service call failed with the error %Rrc"),
+                              vrc);
+    }
+    return rc;
+}
+
+HRESULT Guest::prepareExecuteEnv(ComSafeArrayIn(IN_BSTR, aEnvironment),
+                                 void **ppvList, uint32_t *pcbList, uint32_t *pcEnv)
+{
+    com::SafeArray<IN_BSTR> env(ComSafeArrayInArg(aEnvironment));
+    const char *pszCurEnv;
+    for (unsigned i = 0; i < env.size(); ++i)
+    {
+    }
+
+    return S_OK;
+}
+
 STDMETHODIMP Guest::ExecuteProgram(IN_BSTR aExecName, ULONG aFlags,
                                    ComSafeArrayIn(IN_BSTR, aArguments), ComSafeArrayIn(IN_BSTR, aEnvironment),
                                    IN_BSTR aStdIn, IN_BSTR aStdOut, IN_BSTR aStdErr,
@@ -316,10 +370,83 @@ STDMETHODIMP Guest::ExecuteProgram(IN_BSTR aExecName, ULONG aFlags,
 #ifndef VBOX_WITH_GUEST_CONTROL
     ReturnComNotImplemented();
 #else  /* VBOX_WITH_GUEST_CONTROL */
+    using namespace guestControl;
+
     CheckComArgStrNotEmptyOrNull(aExecName);
     CheckComArgOutPointerValid(aPID);
-    return E_NOTIMPL;
-#endif
+    /* Flags are not supported at the moment. */
+    if (aFlags != 0)
+        return E_INVALIDARG;
+
+    HRESULT rc = S_OK;
+
+    try
+    {
+        AutoCaller autoCaller(this);
+        if (FAILED(autoCaller.rc())) return autoCaller.rc();
+
+        AutoWriteLock alock(this COMMA_LOCKVAL_SRC_POS);
+       
+        /* Just be on the safe side when calling another process. */
+        alock.leave();
+     
+        HRESULT rc = E_UNEXPECTED;
+        using namespace guestControl;
+    
+        int vrc = VINF_SUCCESS; 
+        Utf8Str Utf8ExecName(aExecName);
+
+        /* Prepare arguments. */
+        char *pszArgs;
+        uint32_t cNumArgs;
+        uint32_t cbArgs;
+        rc = prepareExecuteArgs(aArguments,
+                                &pszArgs, &cbArgs, &cNumArgs);
+        if (SUCCEEDED(rc))
+        {
+            /* Prepare environment. */
+            /** @todo */
+            if (SUCCEEDED(rc))
+            {
+                Utf8Str Utf8StdIn(aStdIn);
+                Utf8Str Utf8StdOut(aStdOut);
+                Utf8Str Utf8StdErr(aStdErr);
+                Utf8Str Utf8UserName(aUserName);
+                Utf8Str Utf8Password(aPassword);
+            
+                /* Call the stub which does the actual work. */
+                if (RT_SUCCESS(vrc))
+                {
+                    VBOXHGCMSVCPARM paParms[13];
+                    uint32_t cParms = 13;
+
+                    /* Forward the information to the VMM device. */
+                    AssertPtr(mParent);
+                    VMMDev *vmmDev = mParent->getVMMDev();
+                    if (vmmDev)
+                    {
+                        vrc = vmmDev->hgcmHostCall("VBoxGuestControlSvc", HOST_EXEC_CMD,
+                                                   cParms, &paParms[0]);
+                    }
+                }
+                //RTMemFree(pszEnv);
+            }
+            RTStrFree(pszArgs);
+        }
+        if (RT_SUCCESS(vrc))
+            rc = S_OK;
+        else
+            rc = setError(E_UNEXPECTED,
+                          tr("The service call failed with the error %Rrc"),
+                          vrc);
+    }
+    catch (std::bad_alloc &)
+    {
+        rc = E_OUTOFMEMORY;
+    };
+
+    return rc;    
+#endif /* VBOX_WITH_GUEST_CONTROL */
 }
 
 // public methods only for internal purposes
