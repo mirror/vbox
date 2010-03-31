1237d05504d70a42330992f55649dc42cc656d2f
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2010-03-31 09:28:45 +0000

    export the guest control stuff to OSE
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@27865 cfe28804-0f27-0410-a406-dd0f0b0b656f

---
+++ src/VBox/Additions/common/VBoxGuestLib/VBoxGuestR3LibGuestCtrl.cpp
@@ -0,0 +1,186 @@
+/* $Id$ */
+/** @file
+ * VBoxGuestR3Lib - Ring-3 Support Library for VirtualBox guest additions, guest control.
+ */
+
+/*
+ * Copyright (C) 2010 Sun Microsystems, Inc.
+ *
+ * This file is part of VirtualBox Open Source Edition (OSE), as
+ * available from http://www.virtualbox.org. This file is free software;
+ * you can redistribute it and/or modify it under the terms of the GNU
+ * General Public License (GPL) as published by the Free Software
+ * Foundation, in version 2 as it comes in the "COPYING" file of the
+ * VirtualBox OSE distribution. VirtualBox OSE is distributed in the
+ * hope that it will be useful, but WITHOUT ANY WARRANTY of any kind.
+ *
+ * The contents of this file may alternatively be used under the terms
+ * of the Common Development and Distribution License Version 1.0
+ * (CDDL) only, as it comes in the "COPYING.CDDL" file of the
+ * VirtualBox OSE distribution, in which case the provisions of the
+ * CDDL are applicable instead of those of the GPL.
+ *
+ * You may elect to license modified versions of this file under the
+ * terms and conditions of either the GPL or the CDDL or both.
+ *
+ * Please contact Sun Microsystems, Inc., 4150 Network Circle, Santa
+ * Clara, CA 95054 USA or visit http://www.sun.com if you need
+ * additional information or have any questions.
+ */
+
+
+/*******************************************************************************
+*   Header Files                                                               *
+*******************************************************************************/
+#include <iprt/string.h>
+#include <iprt/mem.h>
+#include <iprt/assert.h>
+#include <iprt/cpp/autores.h>
+#include <iprt/stdarg.h>
+#include <VBox/log.h>
+#include <VBox/HostServices/GuestControlSvc.h>
+
+#include "VBGLR3Internal.h"
+
+
+/*******************************************************************************
+*   Structures and Typedefs                                                    *
+*******************************************************************************/
+
+using namespace guestControl;
+
+/**
+ * Connects to the guest control service.
+ *
+ * @returns VBox status code
+ * @param   pu32ClientId    Where to put the client id on success. The client id
+ *                          must be passed to all the other calls to the service.
+ */
+VBGLR3DECL(int) VbglR3GuestCtrlConnect(uint32_t *pu32ClientId)
+{
+    VBoxGuestHGCMConnectInfo Info;
+    Info.result = VERR_WRONG_ORDER;
+    Info.Loc.type = VMMDevHGCMLoc_LocalHost_Existing;
+    RT_ZERO(Info.Loc.u);
+    strcpy(Info.Loc.u.host.achName, "VBoxGuestControlSvc");
+    Info.u32ClientID = UINT32_MAX;  /* try make valgrid shut up. */
+
+    int rc = vbglR3DoIOCtl(VBOXGUEST_IOCTL_HGCM_CONNECT, &Info, sizeof(Info));
+    if (RT_SUCCESS(rc))
+    {
+        rc = Info.result;
+        if (RT_SUCCESS(rc))
+            *pu32ClientId = Info.u32ClientID;
+    }
+    return rc;
+}
+
+
+/**
+ * Disconnect from the guest control service.
+ *
+ * @returns VBox status code.
+ * @param   u32ClientId     The client id returned by VbglR3GuestCtrlConnect().
+ */
+VBGLR3DECL(int) VbglR3GuestCtrlDisconnect(uint32_t u32ClientId)
+{
+    VBoxGuestHGCMDisconnectInfo Info;
+    Info.result = VERR_WRONG_ORDER;
+    Info.u32ClientID = u32ClientId;
+
+    int rc = vbglR3DoIOCtl(VBOXGUEST_IOCTL_HGCM_DISCONNECT, &Info, sizeof(Info));
+    if (RT_SUCCESS(rc))
+        rc = Info.result;
+    return rc;
+}
+
+
+/**
+ * Gets a host message.
+ *
+ * This will block until a message becomes available.
+ *
+ * @returns VBox status code.
+ * @param   u32ClientId     The client id returned by VbglR3GuestCtrlConnect().
+ * @param   puMsg           Where to store the message id.
+ * @param   puNumParms      Where to store the number  of parameters which will be received
+ *                          in a second call to the host.
+ */
+VBGLR3DECL(int) VbglR3GuestCtrlGetHostMsg(uint32_t u32ClientId, uint32_t *puMsg, uint32_t *puNumParms)
+{
+    VBoxGuestCtrlHGCMMsgType Msg;
+
+    Msg.hdr.result = VERR_WRONG_ORDER;
+    Msg.hdr.u32ClientID = u32ClientId;
+    Msg.hdr.u32Function = GUEST_GET_HOST_MSG; /* Tell the host we want our next command. */
+    Msg.hdr.cParms = 2;
+
+    VbglHGCMParmUInt32Set(&Msg.msg, 0);
+    VbglHGCMParmUInt32Set(&Msg.num_parms, 0);
+
+    int rc = vbglR3DoIOCtl(VBOXGUEST_IOCTL_HGCM_CALL(sizeof(Msg)), &Msg, sizeof(Msg));
+    if (RT_SUCCESS(rc))
+    {
+        rc = Msg.hdr.result;
+        if (RT_SUCCESS(rc))
+        {
+            rc = VbglHGCMParmUInt32Get(&Msg.msg, puMsg);
+            if (RT_SUCCESS(rc))
+                rc = VbglHGCMParmUInt32Get(&Msg.num_parms, puNumParms);
+            /* Ok, so now we know what message type and how much parameters there are. */
+        }
+    }
+    return rc;
+}
+
+
+/**
+ * Allocates and gets host data, based on the message id.
+ *
+ * This will block until data becomes available.
+ *
+ * @returns VBox status code.
+ * @param   u32ClientId     The client id returned by VbglR3GuestCtrlConnect().
+ * @param   ppvData    
+ * @param   uNumParms   
+ */
+VBGLR3DECL(int) VbglR3GuestCtrlGetHostCmdExec(uint32_t u32ClientId, uint32_t uNumParms,
+                                              char    *pszCmd,      uint32_t cbCmd, 
+                                              uint32_t *puFlags,
+                                              char *pszArgs,        uint32_t cbArgs,  uint32_t *puNumArgs, 
+                                              char *pszEnv,         uint32_t *pcbEnv, uint32_t *puNumEnvVars,
+                                              char *pszStdIn,       uint32_t cbStdIn,
+                                              char *pszStdOut,      uint32_t cbStdOut,
+                                              char *pszStdErr,      uint32_t cbStdErr,
+                                              char *pszUser,        uint32_t cbUser,
+                                              char *pszPassword,    uint32_t cbPassword,
+                                              uint32_t *puTimeLimit)                                             
+{
+    VBoxGuestCtrlHGCMMsgExecCmd Msg;
+        
+    Msg.hdr.result = VERR_WRONG_ORDER;
+    Msg.hdr.u32ClientID = u32ClientId;
+    Msg.hdr.u32Function = GUEST_GET_HOST_MSG_DATA; /* Tell the host we want the actual data of a command. */
+    Msg.hdr.cParms = uNumParms;
+
+    VbglHGCMParmPtrSet(&Msg.cmd, pszCmd, cbCmd);
+    VbglHGCMParmUInt32Set(&Msg.flags, 0);
+    VbglHGCMParmUInt32Set(&Msg.num_args, 0);
+    VbglHGCMParmPtrSet(&Msg.args, pszArgs, cbArgs);
+    VbglHGCMParmUInt32Set(&Msg.num_env, 0);
+    VbglHGCMParmPtrSet(&Msg.env, pszEnv, *pcbEnv);
+    VbglHGCMParmPtrSet(&Msg.std_in, pszStdIn, cbStdIn);
+    VbglHGCMParmPtrSet(&Msg.std_out, pszStdOut, cbStdOut);
+    VbglHGCMParmPtrSet(&Msg.std_err, pszStdErr, cbStdErr);
+    VbglHGCMParmPtrSet(&Msg.username, pszUser, cbUser);
+    VbglHGCMParmPtrSet(&Msg.password, pszPassword, cbPassword);
+    VbglHGCMParmUInt32Set(&Msg.timeout, 0);
+
+    int rc = vbglR3DoIOCtl(VBOXGUEST_IOCTL_HGCM_CALL(sizeof(Msg)), &Msg, sizeof(Msg));
+    if (RT_SUCCESS(rc))
+    {
+        rc = Msg.hdr.result; 
+    }
+    return rc;
+}
+
---
+++ src/VBox/Additions/common/VBoxService/VBoxServiceControl.cpp
@@ -0,0 +1,245 @@
+
+/* $Id$ */
+/** @file
+ * VBoxServiceControl - Host-driven Guest Control.
+ */
+
+/*
+ * Copyright (C) 2010 Sun Microsystems, Inc.
+ *
+ * This file is part of VirtualBox Open Source Edition (OSE), as
+ * available from http://www.virtualbox.org. This file is free software;
+ * you can redistribute it and/or modify it under the terms of the GNU
+ * General Public License (GPL) as published by the Free Software
+ * Foundation, in version 2 as it comes in the "COPYING" file of the
+ * VirtualBox OSE distribution. VirtualBox OSE is distributed in the
+ * hope that it will be useful, but WITHOUT ANY WARRANTY of any kind.
+ *
+ * Please contact Sun Microsystems, Inc., 4150 Network Circle, Santa
+ * Clara, CA 95054 USA or visit http://www.sun.com if you need
+ * additional information or have any questions.
+ */
+
+
+/*******************************************************************************
+*   Header Files                                                               *
+*******************************************************************************/
+#include <iprt/assert.h>
+#include <iprt/ctype.h>
+#include <iprt/env.h>
+#include <iprt/file.h>
+#include <iprt/mem.h>
+#include <iprt/path.h>
+#include <iprt/param.h>
+#include <iprt/process.h>
+#include <iprt/string.h>
+#include <iprt/semaphore.h>
+#include <iprt/thread.h>
+#include <VBox/version.h>
+#include <VBox/VBoxGuestLib.h>
+#include <VBox/HostServices/GuestControlSvc.h>
+#include "VBoxServiceInternal.h"
+#include "VBoxServiceUtils.h"
+
+using namespace guestControl;
+
+/*******************************************************************************
+*   Global Variables                                                           *
+*******************************************************************************/
+/** The control interval (millseconds). */
+uint32_t g_ControlInterval = 0;
+/** The semaphore we're blocking on. */
+static RTSEMEVENTMULTI  g_hControlEvent = NIL_RTSEMEVENTMULTI;
+/** The guest property service client ID. */
+static uint32_t         g_GuestControlSvcClientID = 0;
+
+/** @copydoc VBOXSERVICE::pfnPreInit */
+static DECLCALLBACK(int) VBoxServiceControlPreInit(void)
+{
+    return VINF_SUCCESS;
+}
+
+
+/** @copydoc VBOXSERVICE::pfnOption */
+static DECLCALLBACK(int) VBoxServiceControlOption(const char **ppszShort, int argc, char **argv, int *pi)
+{
+    int rc = -1;
+    if (ppszShort)
+        /* no short options */;
+    else if (!strcmp(argv[*pi], "--control-interval"))
+        rc = VBoxServiceArgUInt32(argc, argv, "", pi,
+                                  &g_ControlInterval, 1, UINT32_MAX - 1);
+    return rc;
+}
+
+
+/** @copydoc VBOXSERVICE::pfnInit */
+static DECLCALLBACK(int) VBoxServiceControlInit(void)
+{
+    /*
+     * If not specified, find the right interval default.
+     * Then create the event sem to block on.
+     */
+    if (!g_ControlInterval)
+        g_ControlInterval = 1000;
+
+    int rc = RTSemEventMultiCreate(&g_hControlEvent);
+    AssertRCReturn(rc, rc);
+
+    rc = VbglR3GuestCtrlConnect(&g_GuestControlSvcClientID);
+    if (RT_SUCCESS(rc))
+        VBoxServiceVerbose(3, "Control: Service Client ID: %#x\n", g_GuestControlSvcClientID);
+    else
+    {
+        VBoxServiceError("Control: Failed to connect to the guest control service! Error: %Rrc\n", rc);
+        RTSemEventMultiDestroy(g_hControlEvent);
+        g_hControlEvent = NIL_RTSEMEVENTMULTI;
+    }
+
+    return rc;
+}
+
+int VBoxServiceControlHandleCmdExec(uint32_t u32ClientId, uint32_t uNumParms)
+{
+    VBoxServiceVerbose(3, "VBoxServiceControlHandleCmdExec: Called uNumParms=%ld\n", uNumParms);
+
+    char szCmd[_1K];
+    uint32_t uFlags;
+    char szArgs[_1K];
+    uint32_t uNumArgs;
+    char szEnv[_64K];
+    uint32_t cbEnv = sizeof(szEnv);
+    uint32_t uNumEnvVars;
+    char szStdIn[_1K];
+    char szStdOut[_1K];
+    char szStdErr[_1K];
+    char szUser[128];
+    char szPassword[128];
+    uint32_t uTimeLimitMS;
+
+    int rc = VbglR3GuestCtrlGetHostCmdExec(u32ClientId, uNumParms,
+                                           szCmd, sizeof(szCmd),
+                                           &uFlags,
+                                           szArgs, sizeof(szArgs), &uNumArgs,
+                                           szEnv, &cbEnv, &uNumEnvVars,
+                                           szStdIn, sizeof(szStdIn),
+                                           szStdOut, sizeof(szStdOut),
+                                           szStdErr, sizeof(szStdErr),
+                                           szUser, sizeof(szUser),
+                                           szPassword, sizeof(szPassword),
+                                           &uTimeLimitMS);
+    return 0;
+}
+
+
+/** @copydoc VBOXSERVICE::pfnWorker */
+DECLCALLBACK(int) VBoxServiceControlWorker(bool volatile *pfShutdown)
+{
+    /*
+     * Tell the control thread that it can continue
+     * spawning services.
+     */
+    RTThreadUserSignal(RTThreadSelf());
+    Assert(g_GuestControlSvcClientID > 0);
+
+    int rc = VINF_SUCCESS;
+
+    /*
+     * Execution loop.
+     *
+     * @todo
+     */
+    for (;;)
+    {
+        uint32_t uMsg;
+        uint32_t uNumParms;
+        rc = VbglR3GuestCtrlGetHostMsg(g_GuestControlSvcClientID, &uMsg, &uNumParms);
+        if (RT_SUCCESS(rc))
+        {
+            switch(uMsg)
+            {
+                case GETHOSTMSG_EXEC_CMD:
+                    rc = VBoxServiceControlHandleCmdExec(g_GuestControlSvcClientID, uNumParms);
+                    break;
+
+                default:
+                    VBoxServiceVerbose(3, "VBoxServiceControlWorker: Unsupported message from host! Msg=%ld\n", uMsg);
+                    /* Don't terminate here; just wait for the next message. */
+                    break;
+            }
+        }
+    
+        /*
+         * Block for a while.
+         *
+         * The event semaphore takes care of ignoring interruptions and it
+         * allows us to implement service wakeup later.
+         */
+        if (*pfShutdown)
+            break;
+        int rc2 = RTSemEventMultiWait(g_hControlEvent, g_ControlInterval);
+        if (*pfShutdown)
+            break;
+        if (rc2 != VERR_TIMEOUT && RT_FAILURE(rc2))
+        {
+            VBoxServiceError("VBoxServiceControlWorker: RTSemEventMultiWait failed; rc2=%Rrc\n", rc2);
+            rc = rc2;
+            break;
+        }
+    }
+
+    RTSemEventMultiDestroy(g_hControlEvent);
+    g_hControlEvent = NIL_RTSEMEVENTMULTI;
+    return rc;
+}
+
+
+/** @copydoc VBOXSERVICE::pfnStop */
+static DECLCALLBACK(void) VBoxServiceControlStop(void)
+{
+    /** @todo Later, figure what to do if we're in RTProcWait(). it's a very
+     *        annoying call since doesn't support timeouts in the posix world. */
+    RTSemEventMultiSignal(g_hControlEvent);
+}
+
+
+/** @copydoc VBOXSERVICE::pfnTerm */
+static DECLCALLBACK(void) VBoxServiceControlTerm(void)
+{
+    /* Nothing here yet. */
+    VbglR3GuestCtrlDisconnect(g_GuestControlSvcClientID);
+    g_GuestControlSvcClientID = 0;
+
+    if (g_hControlEvent != NIL_RTSEMEVENTMULTI)
+    {
+        RTSemEventMultiDestroy(g_hControlEvent);
+        g_hControlEvent = NIL_RTSEMEVENTMULTI;
+    }
+}
+
+
+/**
+ * The 'vminfo' service description.
+ */
+VBOXSERVICE g_Control =
+{
+    /* pszName. */
+    "control",
+    /* pszDescription. */
+    "Host-driven Guest Control",
+    /* pszUsage. */
+   "[--control-interval <ms>]"
+    ,
+    /* pszOptions. */
+    "    --control-interval   Specifies the interval at which to check for\n"
+    "                         new ocntrol commands. The default is 1000 ms.\n"
+    ,
+    /* methods */
+    VBoxServiceControlPreInit,
+    VBoxServiceControlOption,
+    VBoxServiceControlInit,
+    VBoxServiceControlWorker,
+    VBoxServiceControlStop,
+    VBoxServiceControlTerm
+};
+
