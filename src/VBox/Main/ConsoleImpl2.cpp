9d8541b82aafbb5614842c7071cff4f4f60153e1
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2010-03-31 08:24:00 +0000

    Backed out r59458 here too
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@27853 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Main/ConsoleImpl2.cpp
+++ src/VBox/Main/ConsoleImpl2.cpp
@@ -2203,7 +2203,6 @@ DECLCALLBACK(int) Console::configConstructor(PVM pVM, void *pvConsole)
         rc = CFGMR3InsertInteger(pCfg,  "HpetEnabled", fHpetEnabled);               RC_CHECK();
         rc = CFGMR3InsertInteger(pCfg,  "SmcEnabled", fSmcEnabled);                 RC_CHECK();
         rc = CFGMR3InsertInteger(pCfg,  "ShowRtc",    fExtProfile);                 RC_CHECK();
-        rc = CFGMR3InsertInteger(pCfg,  "ShowNic",    fExtProfile);                 RC_CHECK();
 
         rc = CFGMR3InsertInteger(pCfg,  "ShowCpu", fShowCpu);                       RC_CHECK();
         rc = CFGMR3InsertInteger(pCfg,  "CpuHotPlug", fCpuHotPlug);                 RC_CHECK();
@@ -3552,3 +3551,4 @@ int configSetGlobalPropertyFlags(VMMDev * const pVMMDev,
     return VERR_NOT_SUPPORTED;
 #endif /* !VBOX_WITH_GUEST_CONTROL */
 }
+
