a99ccd99086d77dea211cc1a721794aedf4cecee
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2010-04-02 20:05:49 +0000

    VBoxGuest (common): Ballooning review, untested: Don't use VERR_NO_PHYS_MEMORY for flagging ring-3 handling as the status was added with RTR0MemObjAllocPhysNC in mind. Just put the whole balloonning stuff behind a mutex, ring-3 is not thrusted period.  Renamed functions and corrected log comments to match the current IOCtl names. Also fixed missing NIL initialization of the spinlock members - my own blunder.
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@27967 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Additions/common/VBoxGuest/VBoxGuestInternal.h
+++ src/VBox/Additions/common/VBoxGuest/VBoxGuestInternal.h
@@ -81,6 +81,8 @@ typedef VBOXGUESTWAITLIST *PVBOXGUESTWAITLIST;
  */
 typedef struct VBOXGUESTMEMBALLOON
 {
+    /** Mutext protecting the members below from concurrent access.. */
+    RTSEMFASTMUTEX              hMtx;
     /** The current number of chunks in the balloon. */
     uint32_t                    cChunks;
     /** The maximum number of chunks in the balloon (typically the amount of guest
@@ -89,8 +91,10 @@ typedef struct VBOXGUESTMEMBALLOON
     /** This is true if we are using RTR0MemObjAllocPhysNC() / RTR0MemObjGetPagePhysAddr()
      * and false otherwise. */
     bool                        fUseKernelAPI;
-    /** The owner of the balloon which is the first process using the balloon API. */
-    PVBOXGUESTSESSION volatile  pOwner;
+    /** The current owner of the balloon.
+     * This is automatically assigned to the first session using the ballooning
+     * API and first released when the session closes. */
+    PVBOXGUESTSESSION           pOwner;
     /** The pointer to the array of memory objects holding the chunks of the
      *  balloon.  This array is cMaxChunks in size when present. */
     PRTR0MEMOBJ                 paMemObj;
