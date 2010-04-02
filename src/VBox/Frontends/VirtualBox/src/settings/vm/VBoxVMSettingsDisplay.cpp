4da7cb8a8ca9a64d21e1998a534cb2210280cdf1
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2010-04-02 12:51:03 +0000

    2d: enable for multi-monitor (to be tested for non-win hosts)
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@27957 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Frontends/VirtualBox/src/settings/vm/VBoxVMSettingsDisplay.cpp
+++ src/VBox/Frontends/VirtualBox/src/settings/vm/VBoxVMSettingsDisplay.cpp
@@ -300,15 +300,12 @@ void VBoxVMSettingsDisplay::checkMultiMonitorReqs()
     int cVal = mSlMonitors->value();
     if (cVal > 1)
     {
-#ifdef VBOX_WITH_VIDEOHWACCEL
-        mCb2DVideo->setChecked(false);
-#endif /* VBOX_WITH_VIDEOHWACCEL */
 #ifdef Q_WS_MAC
         mCb3D->setChecked(false);
 #endif
     }
 #ifdef VBOX_WITH_VIDEOHWACCEL
-    mCb2DVideo->setEnabled(cVal == 1 && VBoxGlobal::isAcceleration2DVideoAvailable());
+    mCb2DVideo->setEnabled(VBoxGlobal::isAcceleration2DVideoAvailable());
 #endif /* VBOX_WITH_VIDEOHWACCEL */
 #ifdef Q_WS_MAC
     mCb3D->setEnabled(cVal == 1 && vboxGlobal().virtualBox().GetHost().GetAcceleration3DAvailable());
