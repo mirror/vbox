ff0457771b14bb227aad4e4538ee4c489d3d9bee
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2010-04-07 17:08:08 +0000

    Runtime: Generic S/G buffer handling. For VBoxHDD but might become useful for other parts too.
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@28053 cfe28804-0f27-0410-a406-dd0f0b0b656f

---
+++ include/iprt/sg.h
@@ -0,0 +1,190 @@
+/** @file
+ * IPRT - S/G buffer handling.
+ */
+
+/*
+ * Copyright (C) 2010 Sun Microsystems, Inc.
+ *
+ * This file is part of VirtualBox Open Source Edition (OSE), as
+ * available from http://www.virtualbox.org. This file is free software;
+ * you can redistribute it and/or modify it under the terms of the GNU
+ * General Public License (GPL) as published by the Free Software
+ * Foundation, in version 2 as it comes in the "COPYING" file of the
+ * VirtualBox OSE distribution. VirtualBox OSE is distributed in the
+ * hope that it will be useful, but WITHOUT ANY WARRANTY of any kind.
+ *
+ * The contents of this file may alternatively be used under the terms
+ * of the Common Development and Distribution License Version 1.0
+ * (CDDL) only, as it comes in the "COPYING.CDDL" file of the
+ * VirtualBox OSE distribution, in which case the provisions of the
+ * CDDL are applicable instead of those of the GPL.
+ *
+ * You may elect to license modified versions of this file under the
+ * terms and conditions of either the GPL or the CDDL or both.
+ *
+ * Please contact Sun Microsystems, Inc., 4150 Network Circle, Santa
+ * Clara, CA 95054 USA or visit http://www.sun.com if you need
+ * additional information or have any questions.
+ */
+
+#ifndef ___iprt_sg_h
+#define ___iprt_sg_h
+
+#include <iprt/types.h>
+
+RT_C_DECLS_BEGIN
+
+/**
+ * A S/G entry.
+ */
+typedef struct RTSGSEG
+{
+    /** Pointer to the segment buffer. */
+    void   *pvSeg;
+    /** Size of the segment buffer. */
+    size_t  cbSeg;
+} RTSGSEG;
+/** Pointer to a S/G entry. */
+typedef RTSGSEG *PRTSGSEG;
+/** Pointer to a const S/G entry. */
+typedef const RTSGSEG *PCRTSGSEG;
+/** Pointer to a S/G entry pointer. */
+typedef PRTSGSEG *PPRTSGSEG;
+
+/**
+ * A S/G buffer.
+ *
+ * The members should be treated as private.
+ */
+typedef struct RTSGBUF
+{
+    /** Pointer to the scatter/gather array. */
+    PCRTSGSEG pcaSeg;
+    /** Number of segments. */
+    unsigned  cSeg;
+    /** Current segment we are in. */
+    unsigned  idxSeg;
+    /** Pointer to the current segment start. */
+    void     *pvSegCurr;
+    /** Number of bytes left in the current buffer. */
+    size_t    cbSegLeft;
+} RTSGBUF;
+/** Pointer to a S/G entry. */
+typedef RTSGBUF *PRTSGBUF;
+/** Pointer to a const S/G entry. */
+typedef const RTSGBUF *PCRTSGBUF;
+/** Pointer to a S/G entry pointer. */
+typedef PRTSGBUF *PPRTSGBUF;
+
+/**
+ * Initialize a S/G buffer structure.
+ *
+ * @returns nothing.
+ * @param   pSgBuf    Pointer to the S/G buffer to initialize.
+ * @param   pcaSeg    Pointer to the start of the segment array.
+ * @param   cSeg      Number of segments in the array.
+ */
+RTDECL(void) RTSgBufInit(PRTSGBUF pSgBuf, PCRTSGSEG pcaSeg, unsigned cSeg);
+
+/**
+ * Resets the internal buffer position of the S/G buffer to the beginning.
+ *
+ * @returns nothing.
+ * @param   pSgBuf    The S/G buffer to reset.
+ */
+RTDECL(void) RTSgBufReset(PRTSGBUF pSgBuf);
+
+/**
+ * Clones a given S/G buffer.
+ *
+ * @returns nothing.
+ * @param   pSgBufNew    The new S/G buffer to clone to.
+ * @param   pSgBufOld    The source S/G buffer to clone from.
+ *
+ * @note This is only a shallow copy. Both S/G buffers will point to the
+ *       same segment array.
+ */
+RTDECL(void) RTSgBufClone(PRTSGBUF pSgBufNew, PCRTSGBUF pSgBufOld);
+
+/**
+ * Copy data between two S/G buffers.
+ *
+ * @returns The number of bytes copied.
+ * @param   pSgBufDst    The destination S/G buffer.
+ * @param   pSgBufSrc    The source S/G buffer.
+ * @param   cbCopy       Number of bytes to copy.
+ *
+ * @note This operation advances the internal buffer pointer of both S/G buffers.
+ */
+RTDECL(size_t) RTSgBufCopy(PRTSGBUF pSgBufDst, PRTSGBUF pSgBufSrc, size_t cbCopy);
+
+/**
+ * Compares the content of two S/G buffers.
+ *
+ * @returns Whatever memcmp returns.
+ * @param   pSgBuf1      First S/G buffer.
+ * @param   pSgBuf2      Second S/G buffer.
+ * @param   cbCmp        How many bytes to compare.
+ *
+ * @note This operation doesn't change the internal position of the S/G buffers.
+ */
+RTDECL(int) RTSgBufCmp(PCRTSGBUF pSgBuf1, PCRTSGBUF pSgBuf2, size_t cbCmp);
+
+/**
+ * Fills an S/G buf with a constant byte.
+ *
+ * @returns The number of actually filled bytes.
+ *          Can be less than than cbSet if the end of the S/G buffer was reached.
+ * @param   pSgBuf       The S/G buffer.
+ * @param   ubFill       The byte to fill the buffer with.
+ * @param   cbSet        How many bytes to set.
+ *
+ * @note This operation advances the internal buffer pointer of the S/G buffer.
+ */
+RTDECL(size_t) RTSgBufSet(PRTSGBUF pSgBuf, uint8_t ubFill, size_t cbSet);
+
+/**
+ * Copies data from an S/G buffer into a given non scattered buffer.
+ *
+ * @returns Number of bytes copied.
+ * @param   pSgBuf       The S/G buffer to copy from.
+ * @param   pvBuf        Buffer to copy the data into.
+ * @param   cbCopy       How many bytes to copy.
+ *
+ * @note This operation advances the internal buffer pointer of the S/G buffer.
+ */
+RTDECL(size_t) RTSgBufCopyToBuf(PRTSGBUF pSgBuf, void *pvBuf, size_t cbCopy);
+
+/**
+ * Copies data from a non scattered buffer into an S/G buffer.
+ *
+ * @returns Number of bytes copied.
+ * @param   pSgBuf       The S/G buffer to copy to.
+ * @param   pvBuf        Buffer to copy the data into.
+ * @param   cbCopy       How many bytes to copy.
+ *
+ * @note This operation advances the internal buffer pointer of the S/G buffer.
+ */
+RTDECL(size_t) RTSgBufCopyFromBuf(PRTSGBUF pSgBuf, void *pvBuf, size_t cbCopy);
+
+/**
+ * Constructs a new segment array starting from the current position
+ * and describing the given number of bytes.
+ *
+ * @returns Number of bytes the array describes.
+ * @param   pSgBuf      The S/G buffer.
+ * @param   paSeg       The unitialized segment array.
+ * @param   pcSeg       The number of segments the given array has.
+ *                      This will hold the actual number of entries needed upon return.
+ * @param   cbData      Number of bytes the new array should describe.
+ *
+ * @note This operation advances the internal buffer pointer of the S/G buffer.
+ */
+RTDECL(size_t) RTSgBufSegArrayCreate(PRTSGBUF pSgBuf, PRTSGSEG paSeg, unsigned *pcSeg, size_t cbData);
+
+RT_C_DECLS_END
+
+/** @} */
+
+#endif
+
---
+++ src/VBox/Runtime/common/misc/sg.cpp
@@ -0,0 +1,282 @@
+/* $Id$ */
+/** @file
+ * IPRT - S/G buffer handling.
+ */
+
+/*
+ * Copyright (C) 2010 Sun Microsystems, Inc.
+ *
+ * This file is part of VirtualBox Open Source Edition (OSE), as
+ * available from http://www.virtualbox.org. This file is free software;
+ * you can redistribute it and/or modify it under the terms of the GNU
+ * General Public License (GPL) as published by the Free Software
+ * Foundation, in version 2 as it comes in the "COPYING" file of the
+ * VirtualBox OSE distribution. VirtualBox OSE is distributed in the
+ * hope that it will be useful, but WITHOUT ANY WARRANTY of any kind.
+ *
+ * The contents of this file may alternatively be used under the terms
+ * of the Common Development and Distribution License Version 1.0
+ * (CDDL) only, as it comes in the "COPYING.CDDL" file of the
+ * VirtualBox OSE distribution, in which case the provisions of the
+ * CDDL are applicable instead of those of the GPL.
+ *
+ * You may elect to license modified versions of this file under the
+ * terms and conditions of either the GPL or the CDDL or both.
+ *
+ * Please contact Sun Microsystems, Inc., 4150 Network Circle, Santa
+ * Clara, CA 95054 USA or visit http://www.sun.com if you need
+ * additional information or have any questions.
+ */
+
+
+/*******************************************************************************
+*   Header Files                                                               *
+*******************************************************************************/
+#include <iprt/sg.h>
+#include <iprt/string.h>
+#include <iprt/assert.h>
+
+
+static void *sgBufGet(PRTSGBUF pSgBuf, size_t *pcbData)
+{
+    size_t cbData = RT_MIN(*pcbData, pSgBuf->cbSegLeft);
+    void *pvBuf = pSgBuf->pvSegCurr;
+
+    pSgBuf->cbSegLeft -= cbData;
+
+    /* Advance to the next segment if required. */
+    if (!pSgBuf->cbSegLeft)
+    {
+        pSgBuf->idxSeg++;
+
+        if (RT_UNLIKELY(pSgBuf->idxSeg == pSgBuf->cSeg))
+        {
+            pSgBuf->cbSegLeft = 0;
+            pSgBuf->pvSegCurr = NULL;
+        }
+        else
+        {
+            pSgBuf->pvSegCurr = pSgBuf->pcaSeg[pSgBuf->idxSeg].pvSeg;
+            pSgBuf->cbSegLeft = pSgBuf->pcaSeg[pSgBuf->idxSeg].cbSeg;
+        }
+
+        *pcbData = cbData;
+    }
+    else
+        pSgBuf->pvSegCurr = (void *)((uintptr_t)pSgBuf->pvSegCurr + cbData);
+
+    return pvBuf;
+}
+
+
+RTDECL(void) RTSgBufInit(PRTSGBUF pSgBuf, PCRTSGSEG pcaSeg, unsigned cSeg)
+{
+    AssertPtrReturnVoid(pSgBuf);
+    AssertPtrReturnVoid(pcaSeg);
+    AssertReturnVoid(cSeg > 0);
+
+    pSgBuf->pcaSeg    = pcaSeg;
+    pSgBuf->cSeg      = cSeg;
+    pSgBuf->idxSeg    = 0;
+    pSgBuf->pvSegCurr = pcaSeg[0].pvSeg;
+    pSgBuf->cbSegLeft = pcaSeg[0].cbSeg;
+}
+
+
+RTDECL(void) RTSgBufReset(PRTSGBUF pSgBuf)
+{
+    AssertPtrReturnVoid(pSgBuf);
+
+    pSgBuf->idxSeg    = 0;
+    pSgBuf->pvSegCurr = pSgBuf->pcaSeg[0].pvSeg;
+    pSgBuf->cbSegLeft = pSgBuf->pcaSeg[0].cbSeg;
+}
+
+
+RTDECL(void) RTSgBufClone(PRTSGBUF pSgBufTo, PCRTSGBUF pSgBufFrom)
+{
+    AssertPtrReturnVoid(pSgBufTo);
+    AssertPtrReturnVoid(pSgBufFrom);
+
+    pSgBufTo->pcaSeg    = pSgBufFrom->pcaSeg;
+    pSgBufTo->cSeg      = pSgBufFrom->cSeg;
+    pSgBufTo->idxSeg    = pSgBufFrom->idxSeg;
+    pSgBufTo->pvSegCurr = pSgBufFrom->pvSegCurr;
+    pSgBufTo->cbSegLeft = pSgBufFrom->cbSegLeft;
+}
+
+
+RTDECL(size_t) RTSgBufCopy(PRTSGBUF pSgBufDst, PRTSGBUF pSgBufSrc, size_t cbCopy)
+{
+    AssertPtrReturn(pSgBufDst, 0);
+    AssertPtrReturn(pSgBufSrc, 0);
+
+    size_t cbLeft = cbCopy;
+
+    while (cbLeft)
+    {
+        size_t cbThisCopy = RT_MIN(RT_MIN(pSgBufDst->cbSegLeft, cbLeft), pSgBufSrc->cbSegLeft);
+        size_t cbTmp = cbThisCopy;
+        void *pvBufDst;
+        void *pvBufSrc;
+
+        if (!cbCopy)
+            break;
+
+        pvBufDst = sgBufGet(pSgBufDst, &cbTmp);
+        Assert(cbTmp == cbThisCopy);
+        pvBufSrc = sgBufGet(pSgBufDst, &cbTmp);
+        Assert(cbTmp == cbThisCopy);
+
+        memcpy(pvBufDst, pvBufSrc, cbThisCopy);
+
+        cbLeft -= cbThisCopy;
+    }
+
+    return cbCopy - cbLeft;
+}
+
+
+RTDECL(int) RTSgBufCmp(PCRTSGBUF pSgBuf1, PCRTSGBUF pSgBuf2, size_t cbCmp)
+{
+    AssertPtrReturn(pSgBuf1, 0);
+    AssertPtrReturn(pSgBuf2, 0);
+
+    size_t cbLeft = cbCmp;
+    RTSGBUF SgBuf1;
+    RTSGBUF SgBuf2;
+
+    /* Set up the temporary buffers */
+    RTSgBufClone(&SgBuf1, pSgBuf1);
+    RTSgBufClone(&SgBuf2, pSgBuf2);
+
+    while (cbLeft)
+    {
+        size_t cbThisCmp = RT_MIN(RT_MIN(SgBuf1.cbSegLeft, cbLeft), SgBuf2.cbSegLeft);
+        size_t cbTmp = cbThisCmp;
+        void *pvBuf1;
+        void *pvBuf2;
+
+        if (!cbCmp)
+            break;
+
+        pvBuf1 = sgBufGet(&SgBuf1, &cbTmp);
+        Assert(cbTmp == cbThisCmp);
+        pvBuf2 = sgBufGet(&SgBuf2, &cbTmp);
+        Assert(cbTmp == cbThisCmp);
+
+        int rc = memcmp(pvBuf1, pvBuf1, cbThisCmp);
+        if (rc)
+            return rc;
+
+        cbLeft -= cbThisCmp;
+    }
+
+    return 0;
+}
+
+
+RTDECL(size_t) RTSgBufSet(PRTSGBUF pSgBuf, uint8_t ubFill, size_t cbSet)
+{
+    AssertPtrReturn(pSgBuf, 0);
+
+    size_t cbLeft = cbSet;
+
+    while (cbLeft)
+    {
+        size_t cbThisSet = cbLeft;
+        void *pvBuf = sgBufGet(pSgBuf, &cbThisSet);
+
+        if (!cbThisSet)
+            break;
+
+        memset(pvBuf, ubFill, cbThisSet);
+
+        cbLeft -= cbThisSet;
+    }
+
+    return cbSet - cbLeft;
+}
+
+
+RTDECL(size_t) RTSgBufCopyToBuf(PRTSGBUF pSgBuf, void *pvBuf, size_t cbCopy)
+{
+    AssertPtrReturn(pSgBuf, 0);
+    AssertPtrReturn(pvBuf, 0);
+
+    size_t cbLeft = cbCopy;
+
+    while (cbLeft)
+    {
+        size_t cbThisCopy = cbLeft;
+        void *pvSrc = sgBufGet(pSgBuf, &cbThisCopy);
+
+        if (!cbThisCopy)
+            break;
+
+        memcpy(pvBuf, pvSrc, cbThisCopy);
+
+        cbLeft -= cbThisCopy;
+        pvBuf = (void *)((uintptr_t)pvBuf + cbThisCopy);
+    }
+
+    return cbCopy - cbLeft;
+}
+
+
+RTDECL(size_t) RTSgBufCopyFromBuf(PRTSGBUF pSgBuf, void *pvBuf, size_t cbCopy)
+{
+    AssertPtrReturn(pSgBuf, 0);
+    AssertPtrReturn(pvBuf, 0);
+
+    size_t cbLeft = cbCopy;
+
+    while (cbLeft)
+    {
+        size_t cbThisCopy = cbLeft;
+        void *pvDst = sgBufGet(pSgBuf, &cbThisCopy);
+
+        if (!cbThisCopy)
+            break;
+
+        memcpy(pvDst, pvBuf, cbThisCopy);
+
+        cbLeft -= cbThisCopy;
+        pvBuf = (void *)((uintptr_t)pvBuf + cbThisCopy);
+    }
+
+    return cbCopy - cbLeft;
+}
+
+
+RTDECL(size_t) RTSgBufSegArrayCreate(PRTSGBUF pSgBuf, PRTSGSEG paSeg, unsigned *pcSeg, size_t cbData)
+{
+    AssertPtrReturn(pSgBuf, 0);
+    AssertPtrReturn(paSeg, 0);
+    AssertPtrReturn(pcSeg, 0);
+
+    size_t   cb = 0;
+    unsigned cSeg = 0;
+
+    while (   cbData
+           && cSeg < *pcSeg)
+    {
+        size_t  cbThisSeg = cbData;
+        void   *pvSeg     = NULL;
+
+        pvSeg = sgBufGet(pSgBuf, &cbThisSeg);
+
+        AssertMsg(cbThisSeg <= cbData, ("Impossible!\n"));
+
+        paSeg[cSeg].cbSeg = cbThisSeg;
+        paSeg[cSeg].pvSeg = pvSeg;
+        cSeg++;
+        cbData -= cbThisSeg;
+        cb     += cbThisSeg;
+    }
+
+    *pcSeg = cSeg;
+
+    return cb;
+}
+
