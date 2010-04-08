5136fe60b116d1957547f3efc2d2b66e522b4963
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2010-04-08 20:35:17 +0000

    DrvDiskIntegrity: Support for async I/O
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@28114 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Devices/Storage/DrvDiskIntegrity.cpp
+++ src/VBox/Devices/Storage/DrvDiskIntegrity.cpp
@@ -31,6 +31,7 @@
 #include <iprt/avl.h>
 #include <iprt/mem.h>
 #include <iprt/message.h>
+#include <iprt/sg.h>
 
 #include "Builtins.h"
 
@@ -38,6 +39,25 @@
 /*******************************************************************************
 *   Structures and Typedefs                                                    *
 *******************************************************************************/
+
+/**
+ * async I/O request.
+ */
+typedef struct DRVDISKAIOREQ
+{
+    /** Flag whether this is a read or write request. */
+    bool      fRead;
+    /** Start offset. */
+    uint64_t  off;
+    /** Transfer size. */
+    size_t    cbTransfer;
+    /** Segment array. */
+    PCRTSGSEG paSeg;
+    /** Number of array entries. */
+    unsigned  cSeg;
+    /** User argument */
+    void     *pvUser;
+} DRVDISKAIOREQ, *PDRVDISKAIOREQ;
 
 /**
  * I/O log entry.
@@ -84,108 +104,70 @@ typedef struct DRVDISKINTEGRITY
     /** Our media interface */
     PDMIMEDIA               IMedia;
 
+    /** Pointer to the media async driver below us.
+     * This is NULL if the media is not mounted. */
+    PPDMIMEDIAASYNC         pDrvMediaAsync;
+    /** Our media async interface */
+    PDMIMEDIAASYNC          IMediaAsync;
+
+    /** The async media port interface above. */
+    PPDMIMEDIAASYNCPORT     pDrvMediaAsyncPort;
+    /** Our media async port interface */
+    PDMIMEDIAASYNCPORT      IMediaAsyncPort;
+
     /** AVL tree containing the disk blocks to check. */
     PAVLRFOFFTREE           pTreeSegments;
 } DRVDISKINTEGRITY, *PDRVDISKINTEGRITY;
 
 
-/* -=-=-=-=- IMedia -=-=-=-=- */
-
-/** Makes a PDRVDISKINTEGRITY out of a PPDMIMEDIA. */
-#define PDMIMEDIA_2_DRVDISKINTEGRITY(pInterface)        ( (PDRVDISKINTEGRITY)((uintptr_t)pInterface - RT_OFFSETOF(DRVDISKINTEGRITY, IMedia)) )
-
-/*******************************************************************************
-*   Media interface methods                                                    *
-*******************************************************************************/
-
-/** @copydoc PDMIMEDIA::pfnRead */
-static DECLCALLBACK(int) drvdiskintRead(PPDMIMEDIA pInterface,
-                                        uint64_t off, void *pvBuf, size_t cbRead)
+/**
+ * Allocate a new I/O request.
+ *
+ * @returns New I/O request.
+ * @param   fRead         Flag whether this is a read or a write.
+ * @param   off           Start offset.
+ * @param   paSeg         Segment array.
+ * @param   cSeg          Number of segments.
+ * @param   cbTransfer    Number of bytes to transfer.
+ * @param   pvUser        User argument.
+ */
+static PDRVDISKAIOREQ drvdiskintIoReqAlloc(bool fRead, uint64_t off, PCRTSGSEG paSeg,
+                                           unsigned cSeg, size_t cbTransfer, void *pvUser)
 {
-    PDRVDISKINTEGRITY pThis = PDMIMEDIA_2_DRVDISKINTEGRITY(pInterface);
-    int rc = pThis->pDrvMedia->pfnRead(pThis->pDrvMedia, off, pvBuf, cbRead);
-    if (RT_FAILURE(rc))
-        return rc;
+    PDRVDISKAIOREQ pIoReq = (PDRVDISKAIOREQ)RTMemAlloc(sizeof(DRVDISKAIOREQ));
 
-    Assert(off % 512 == 0);
-    Assert(cbRead % 512 == 0);
-
-    /* Compare read data */
-    size_t cbLeft   = cbRead;
-    RTFOFF offCurr  = (RTFOFF)off;
-    uint8_t *pbBuf  = (uint8_t *)pvBuf;
-
-    while (cbLeft)
+    if (RT_LIKELY(pIoReq))
     {
-        PDRVDISKSEGMENT pSeg = (PDRVDISKSEGMENT)RTAvlrFileOffsetRangeGet(pThis->pTreeSegments, offCurr);
-        size_t cbRange  = 0;
-        bool fCmp       = false;
-        unsigned offSeg = 0;
-
-        if (!pSeg)
-        {
-            /* Get next segment */
-            pSeg = (PDRVDISKSEGMENT)RTAvlrFileOffsetGetBestFit(pThis->pTreeSegments, offCurr, true);
-            if (!pSeg)
-            {
-                /* No data in the tree for this read. Assume everything is ok. */
-                cbRange = cbLeft;
-            }
-            else if (offCurr + (RTFOFF)cbLeft <= pSeg->Core.Key)
-                cbRange = cbLeft;
-            else
-                cbRange = pSeg->Core.Key - offCurr;
-        }
-        else
-        {
-            fCmp    = true;
-            offSeg  = offCurr - pSeg->Core.Key;
-            cbRange = RT_MIN(cbLeft, (size_t)(pSeg->Core.KeyLast + 1 - offCurr));
-        }
-
-        if (   fCmp
-            && memcmp(pbBuf, pSeg->pbSeg + offSeg, cbRange))
-        {
-            unsigned offWrong = 0;
-            for (offWrong = 0; offWrong < cbRange; offWrong++)
-                if (pbBuf[offWrong] != pSeg->pbSeg[offSeg + offWrong])
-                {
-                    /* Corrupted disk, print I/O log entry of the last write which accessed this range. */
-                    uint32_t cSector = (offSeg + offWrong) / 512;
-                    AssertMsg(cSector < pSeg->cIoLogEntries, ("Internal bug!\n"));
-
-                    RTMsgError("Corrupted disk at offset %llu (%u bytes in the current read buffer)!\n",
-                               offCurr + offWrong, offWrong);
-                    RTMsgError("Last write to this sector started at offset %llu with %u bytes (%u references to this log entry)\n",
-                               pSeg->apIoLog[cSector]->off,
-                               pSeg->apIoLog[cSector]->cbWrite,
-                               pSeg->apIoLog[cSector]->cRefs);
-                    RTAssertDebugBreak();
-                }
-        }
-
-        offCurr += cbRange;
-        cbLeft  -= cbRange;
-        pbBuf   += cbRange;
+        pIoReq->fRead      = fRead;
+        pIoReq->off        = off;
+        pIoReq->cbTransfer = cbTransfer;
+        pIoReq->paSeg      = paSeg;
+        pIoReq->cSeg       = cSeg;
+        pIoReq->pvUser     = pvUser;
     }
 
-    return rc;
+    return pIoReq;
 }
 
-/** @copydoc PDMIMEDIA::pfnWrite */
-static DECLCALLBACK(int) drvdiskintWrite(PPDMIMEDIA pInterface,
-                                         uint64_t off, const void *pvBuf,
-                                         size_t cbWrite)
+/**
+ * Record a successful write to the virtual disk.
+ *
+ * @returns VBox status code.
+ * @param   pThis    Disk integrity driver instance data.
+ * @param   paSeg    Segment array of the write to record.
+ * @param   cSeg     Number of segments.
+ * @param   off      Start offset.
+ * @param   cbWrite  Number of bytes to record.
+ */
+static int drvdiskintWriteRecord(PDRVDISKINTEGRITY pThis, PCRTSGSEG paSeg, unsigned cSeg,
+                                 uint64_t off, size_t cbWrite)
 {
-    PDRVDISKINTEGRITY pThis = PDMIMEDIA_2_DRVDISKINTEGRITY(pInterface);
-    int rc = pThis->pDrvMedia->pfnWrite(pThis->pDrvMedia, off, pvBuf, cbWrite);
-    if (RT_FAILURE(rc))
-        return rc;
+    int rc = VINF_SUCCESS;
 
     /* Update the segments */
     size_t cbLeft   = cbWrite;
     RTFOFF offCurr  = (RTFOFF)off;
-    uint8_t *pbBuf  = (uint8_t *)pvBuf;
+    RTSGBUF SgBuf;
     PIOLOGENT pIoLogEnt = (PIOLOGENT)RTMemAllocZ(sizeof(IOLOGENT));
     if (!pIoLogEnt)
         return VERR_NO_MEMORY;
@@ -193,6 +175,8 @@ static DECLCALLBACK(int) drvdiskintWrite(PPDMIMEDIA pInterface,
     pIoLogEnt->off     = off;
     pIoLogEnt->cbWrite = cbWrite;
     pIoLogEnt->cRefs   = 0;
+
+    RTSgBufInit(&SgBuf, paSeg, cSeg);
 
     while (cbLeft)
     {
@@ -242,7 +226,7 @@ static DECLCALLBACK(int) drvdiskintWrite(PPDMIMEDIA pInterface,
         if (fSet)
         {
             AssertPtr(pSeg);
-            memcpy(pSeg->pbSeg + offSeg, pbBuf, cbRange);
+            RTSgBufCopyToBuf(&SgBuf, pSeg->pbSeg + offSeg, cbRange);
 
             /* Update the I/O log pointers */
             Assert(offSeg % 512 == 0);
@@ -268,12 +252,202 @@ static DECLCALLBACK(int) drvdiskintWrite(PPDMIMEDIA pInterface,
                 offSeg += 512;
             }
         }
+        else
+            RTSgBufAdvance(&SgBuf, cbRange);
 
         offCurr += cbRange;
         cbLeft  -= cbRange;
-        pbBuf   += cbRange;
     }
 
+    return rc;
+}
+
+/**
+ * Verifies a read request.
+ *
+ * @returns VBox status code.
+ * @param   pThis    Disk integrity driver instance data.
+ * @param   paSeg    Segment array of the containing the data buffers to verify.
+ * @param   cSeg     Number of segments.
+ * @param   off      Start offset.
+ * @param   cbWrite  Number of bytes to verify.
+ */
+static int drvdiskintReadVerify(PDRVDISKINTEGRITY pThis, PCRTSGSEG paSeg, unsigned cSeg,
+                                uint64_t off, size_t cbRead)
+{
+    int rc = VINF_SUCCESS;
+
+    Assert(off % 512 == 0);
+    Assert(cbRead % 512 == 0);
+
+    /* Compare read data */
+    size_t cbLeft   = cbRead;
+    RTFOFF offCurr  = (RTFOFF)off;
+    RTSGBUF SgBuf;
+
+    RTSgBufInit(&SgBuf, paSeg, cSeg);
+
+    while (cbLeft)
+    {
+        PDRVDISKSEGMENT pSeg = (PDRVDISKSEGMENT)RTAvlrFileOffsetRangeGet(pThis->pTreeSegments, offCurr);
+        size_t cbRange  = 0;
+        bool fCmp       = false;
+        unsigned offSeg = 0;
+
+        if (!pSeg)
+        {
+            /* Get next segment */
+            pSeg = (PDRVDISKSEGMENT)RTAvlrFileOffsetGetBestFit(pThis->pTreeSegments, offCurr, true);
+            if (!pSeg)
+            {
+                /* No data in the tree for this read. Assume everything is ok. */
+                cbRange = cbLeft;
+            }
+            else if (offCurr + (RTFOFF)cbLeft <= pSeg->Core.Key)
+                cbRange = cbLeft;
+            else
+                cbRange = pSeg->Core.Key - offCurr;
+        }
+        else
+        {
+            fCmp    = true;
+            offSeg  = offCurr - pSeg->Core.Key;
+            cbRange = RT_MIN(cbLeft, (size_t)(pSeg->Core.KeyLast + 1 - offCurr));
+        }
+
+        if (fCmp)
+        {
+            RTSGSEG Seg;
+            RTSGBUF SgBufCmp;
+
+            Seg.cbSeg = cbRange;
+            Seg.pvSeg = pSeg->pbSeg + offSeg;
+
+            RTSgBufInit(&SgBufCmp, &Seg, 1);
+            if (RTSgBufCmp(&SgBuf, &SgBufCmp, cbRange))
+            {
+                unsigned offWrong = 0;
+#if 0
+                for (offWrong = 0; offWrong < cbRange; offWrong++)
+                    if (pbBuf[offWrong] != pSeg->pbSeg[offSeg + offWrong])
+#endif
+                    {
+                        /* Corrupted disk, print I/O log entry of the last write which accessed this range. */
+                        uint32_t cSector = (offSeg + offWrong) / 512;
+                        AssertMsg(cSector < pSeg->cIoLogEntries, ("Internal bug!\n"));
+
+                        RTMsgError("Corrupted disk at offset %llu (%u bytes in the current read buffer)!\n",
+                                   offCurr + offWrong, offWrong);
+                        RTMsgError("Last write to this sector started at offset %llu with %u bytes (%u references to this log entry)\n",
+                                   pSeg->apIoLog[cSector]->off,
+                                   pSeg->apIoLog[cSector]->cbWrite,
+                                   pSeg->apIoLog[cSector]->cRefs);
+                        RTAssertDebugBreak();
+                    }
+            }
+        }
+
+        offCurr += cbRange;
+        cbLeft  -= cbRange;
+        RTSgBufAdvance(&SgBuf, cbRange);
+    }
+
+    return rc;
+}
+
+/* -=-=-=-=- IMedia -=-=-=-=- */
+
+/** Makes a PDRVDISKINTEGRITY out of a PPDMIMEDIA. */
+#define PDMIMEDIA_2_DRVDISKINTEGRITY(pInterface)        ( (PDRVDISKINTEGRITY)((uintptr_t)pInterface - RT_OFFSETOF(DRVDISKINTEGRITY, IMedia)) )
+/** Makes a PDRVDISKINTEGRITY out of a PPDMIMEDIAASYNC. */
+#define PDMIMEDIAASYNC_2_DRVDISKINTEGRITY(pInterface)   ( (PDRVDISKINTEGRITY)((uintptr_t)pInterface - RT_OFFSETOF(DRVDISKINTEGRITY, IMediaAsync)) )
+
+/*******************************************************************************
+*   Media interface methods                                                    *
+*******************************************************************************/
+
+/** @copydoc PDMIMEDIA::pfnRead */
+static DECLCALLBACK(int) drvdiskintRead(PPDMIMEDIA pInterface,
+                                        uint64_t off, void *pvBuf, size_t cbRead)
+{
+    PDRVDISKINTEGRITY pThis = PDMIMEDIA_2_DRVDISKINTEGRITY(pInterface);
+    int rc = pThis->pDrvMedia->pfnRead(pThis->pDrvMedia, off, pvBuf, cbRead);
+    if (RT_FAILURE(rc))
+        return rc;
+
+    /* Verify the read. */
+    RTSGSEG Seg;
+    Seg.cbSeg = cbRead;
+    Seg.pvSeg = pvBuf;
+    return drvdiskintReadVerify(pThis, &Seg, 1, off, cbRead);
+}
+
+/** @copydoc PDMIMEDIA::pfnWrite */
+static DECLCALLBACK(int) drvdiskintWrite(PPDMIMEDIA pInterface,
+                                         uint64_t off, const void *pvBuf,
+                                         size_t cbWrite)
+{
+    PDRVDISKINTEGRITY pThis = PDMIMEDIA_2_DRVDISKINTEGRITY(pInterface);
+    int rc = pThis->pDrvMedia->pfnWrite(pThis->pDrvMedia, off, pvBuf, cbWrite);
+    if (RT_FAILURE(rc))
+        return rc;
+
+    /* Record the write. */
+    RTSGSEG Seg;
+    Seg.cbSeg = cbWrite;
+    Seg.pvSeg = (void *)pvBuf;
+    return drvdiskintWriteRecord(pThis, &Seg, 1, off, cbWrite);
+}
+
+static DECLCALLBACK(int) drvdiskintStartRead(PPDMIMEDIAASYNC pInterface, uint64_t uOffset,
+                                             PCRTSGSEG paSeg, unsigned cSeg,
+                                             size_t cbRead, void *pvUser)
+{
+     LogFlow(("%s: uOffset=%#llx paSeg=%#p cSeg=%u cbRead=%d\n pvUser=%#p", __FUNCTION__,
+             uOffset, paSeg, cSeg, cbRead, pvUser));
+    PDRVDISKINTEGRITY pThis = PDMIMEDIAASYNC_2_DRVDISKINTEGRITY(pInterface);
+    PDRVDISKAIOREQ pIoReq = drvdiskintIoReqAlloc(true, uOffset, paSeg, cSeg, cbRead, pvUser);
+    AssertPtr(pIoReq);
+
+    int rc = pThis->pDrvMediaAsync->pfnStartRead(pThis->pDrvMediaAsync, uOffset, paSeg, cSeg,
+                                                 cbRead, pIoReq);
+    if (rc == VINF_VD_ASYNC_IO_FINISHED)
+    {
+        /* Verify the read now. */
+        int rc2 = drvdiskintReadVerify(pThis, paSeg, cSeg, uOffset, cbRead);
+        AssertRC(rc2);
+        RTMemFree(pIoReq);
+    }
+    else if (RT_FAILURE(rc) && rc != VERR_VD_ASYNC_IO_IN_PROGRESS)
+        RTMemFree(pIoReq);
+
+    LogFlow(("%s: returns %Rrc\n", __FUNCTION__, rc));
+    return rc;
+}
+
+static DECLCALLBACK(int) drvdiskintStartWrite(PPDMIMEDIAASYNC pInterface, uint64_t uOffset,
+                                              PCRTSGSEG paSeg, unsigned cSeg,
+                                              size_t cbWrite, void *pvUser)
+{
+     LogFlow(("%s: uOffset=%#llx paSeg=%#p cSeg=%u cbWrite=%d\n pvUser=%#p", __FUNCTION__,
+             uOffset, paSeg, cSeg, cbWrite, pvUser));
+    PDRVDISKINTEGRITY pThis = PDMIMEDIAASYNC_2_DRVDISKINTEGRITY(pInterface);
+    PDRVDISKAIOREQ pIoReq = drvdiskintIoReqAlloc(false, uOffset, paSeg, cSeg, cbWrite, pvUser);
+    AssertPtr(pIoReq);
+
+    int rc = pThis->pDrvMediaAsync->pfnStartWrite(pThis->pDrvMediaAsync, uOffset, paSeg, cSeg,
+                                                  cbWrite, pIoReq);
+    if (rc == VINF_VD_ASYNC_IO_FINISHED)
+    {
+        /* Verify the read now. */
+        int rc2 = drvdiskintWriteRecord(pThis, paSeg, cSeg, uOffset, cbWrite);
+        AssertRC(rc2);
+        RTMemFree(pIoReq);
+    }
+    else if (RT_FAILURE(rc) && rc != VERR_VD_ASYNC_IO_IN_PROGRESS)
+        RTMemFree(pIoReq);
+
+    LogFlow(("%s: returns %Rrc\n", __FUNCTION__, rc));
     return rc;
 }
 
@@ -337,6 +511,30 @@ static DECLCALLBACK(int) drvdiskintGetUuid(PPDMIMEDIA pInterface, PRTUUID pUuid)
     return pThis->pDrvMedia->pfnGetUuid(pThis->pDrvMedia, pUuid);
 }
 
+/* -=-=-=-=- IMediaAsyncPort -=-=-=-=- */
+
+/** Makes a PDRVBLOCKASYNC out of a PPDMIMEDIAASYNCPORT. */
+#define PDMIMEDIAASYNCPORT_2_DRVDISKINTEGRITY(pInterface)    ( (PDRVDISKINTEGRITY((uintptr_t)pInterface - RT_OFFSETOF(DRVDISKINTEGRITY, IMediaAsyncPort))) )
+
+static DECLCALLBACK(int) drvdiskintAsyncTransferCompleteNotify(PPDMIMEDIAASYNCPORT pInterface, void *pvUser)
+{
+    PDRVDISKINTEGRITY pThis = PDMIMEDIAASYNCPORT_2_DRVDISKINTEGRITY(pInterface);
+    PDRVDISKAIOREQ pIoReq = (PDRVDISKAIOREQ)pvUser;
+    int rc = VINF_SUCCESS;
+
+    if (pIoReq->fRead)
+        rc = drvdiskintReadVerify(pThis, pIoReq->paSeg, pIoReq->cSeg, pIoReq->off, pIoReq->cbTransfer);
+    else
+        rc = drvdiskintWriteRecord(pThis, pIoReq->paSeg, pIoReq->cSeg, pIoReq->off, pIoReq->cbTransfer);
+
+    AssertRC(rc);
+
+    rc = pThis->pDrvMediaAsyncPort->pfnTransferCompleteNotify(pThis->pDrvMediaAsyncPort, pIoReq->pvUser);
+    RTMemFree(pIoReq);
+
+    return rc;
+}
+
 /* -=-=-=-=- IBase -=-=-=-=- */
 
 /**
@@ -349,6 +547,7 @@ static DECLCALLBACK(void *)  drvdiskintQueryInterface(PPDMIBASE pInterface, cons
 
     PDMIBASE_RETURN_INTERFACE(pszIID, PDMIBASE, &pDrvIns->IBase);
     PDMIBASE_RETURN_INTERFACE(pszIID, PDMIMEDIA, &pThis->IMedia);
+    PDMIBASE_RETURN_INTERFACE(pszIID, PDMIMEDIAASYNCPORT, pThis->pDrvMediaAsync ? &pThis->IMediaAsyncPort : NULL);
     return NULL;
 }
 
@@ -415,6 +614,13 @@ static DECLCALLBACK(int) drvdiskintConstruct(PPDMDRVINS pDrvIns, PCFGMNODE pCfg,
     pThis->IMedia.pfnBiosSetLCHSGeometry = drvdiskintBiosSetLCHSGeometry;
     pThis->IMedia.pfnGetUuid             = drvdiskintGetUuid;
 
+    /* IMediaAsync */
+    pThis->IMediaAsync.pfnStartRead      = drvdiskintStartRead;
+    pThis->IMediaAsync.pfnStartWrite     = drvdiskintStartWrite;
+
+    /* IMediaAsyncPort. */
+    pThis->IMediaAsyncPort.pfnTransferCompleteNotify  = drvdiskintAsyncTransferCompleteNotify;
+
     /*
      * Try attach driver below and query it's media interface.
      */
@@ -429,7 +635,12 @@ static DECLCALLBACK(int) drvdiskintConstruct(PPDMDRVINS pDrvIns, PCFGMNODE pCfg,
         return PDMDRV_SET_ERROR(pDrvIns, VERR_PDM_MISSING_INTERFACE_BELOW,
                                 N_("No media or async media interface below"));
 
-    /** Create the AVL tree. */
+    pThis->pDrvMediaAsync = PDMIBASE_QUERY_INTERFACE(pBase, PDMIMEDIAASYNC);
+
+    /* Try to attach async media port interface above.*/
+    pThis->pDrvMediaAsyncPort = PDMIBASE_QUERY_INTERFACE(pDrvIns->pUpBase, PDMIMEDIAASYNCPORT);
+
+    /* Create the AVL tree. */
     pThis->pTreeSegments = (PAVLRFOFFTREE)RTMemAllocZ(sizeof(AVLRFOFFTREE));
     if (!pThis->pTreeSegments)
         rc = VERR_NO_MEMORY;
