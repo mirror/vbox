dd23d4589b920b6f48cfc3d4babcec4d81556921
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2010-04-14 07:23:28 +0000

    2d: disable Assert
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@28282 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Main/DisplayImpl.cpp
+++ src/VBox/Main/DisplayImpl.cpp
@@ -3376,7 +3376,9 @@ void Display::handleVHWACommandProcess(PPDMIDISPLAYCONNECTOR pInterface, PVBOXVH
     if (id < mcMonitors)
     {
         IFramebuffer *pFramebuffer = maFramebuffers[id].pFramebuffer;
+#ifdef DEBUG_misha
         Assert (pFramebuffer);
+#endif
 
         if (pFramebuffer != NULL)
         {
