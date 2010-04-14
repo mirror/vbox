6d03fc9807b4783bf23ed7e505c9a81d9762dacf
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2010-04-14 06:40:48 +0000

    VBoxNetFlt/linux: 2.6.22 that is
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@28281 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/HostDrivers/VBoxNetFlt/linux/VBoxNetFlt-linux.c
+++ src/VBox/HostDrivers/VBoxNetFlt/linux/VBoxNetFlt-linux.c
@@ -893,7 +893,7 @@ static bool vboxNetFltLinuxCanForwardAsGso(PVBOXNETFLTINS pThis, struct sk_buff 
     /*
      * skb_gso_segment does the following. Do we need to do it as well?
      */
-#if LINUX_VERSION_CODE >= KERNEL_VERSION(2, 6, 23)
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(2, 6, 22)
     skb_reset_mac_header(pSkb);
     pSkb->mac_len = pSkb->network_header - pSkb->mac_header;
 #else
