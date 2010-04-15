b6617c13b8891611dcd0e553016d1f15ebce62da
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2010-04-15 17:35:45 +0000

    LsiLogic: Don't use RTMemFree on memory allocated with RTMemPageAlloc
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@28381 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Devices/Storage/DevLsiLogicSCSI.cpp
+++ src/VBox/Devices/Storage/DevLsiLogicSCSI.cpp
@@ -1431,7 +1431,7 @@ static int lsilogicScatterGatherListAllocate(PLSILOGICTASKSTATE pTaskState, uint
     if (pTaskState->cbBufferUnaligned < cbUnaligned)
     {
         if (pTaskState->pvBufferUnaligned)
-            RTMemFree(pTaskState->pvBufferUnaligned);
+            RTMemPageFree(pTaskState->pvBufferUnaligned, pTaskState->cbBufferUnaligned);
 
         Log(("%s: Allocating buffer for unaligned segments cbUnaligned=%u\n", __FUNCTION__, cbUnaligned));
 
@@ -1486,7 +1486,7 @@ static void lsilogicScatterGatherListDestroy(PLSILOGICSCSI pLsiLogic, PLSILOGICT
         RTMemFree(pTaskState->pSGListHead);
         RTMemFree(pTaskState->paSGEntries);
         if (pTaskState->pvBufferUnaligned)
-            RTMemFree(pTaskState->pvBufferUnaligned);
+            RTMemPageFree(pTaskState->pvBufferUnaligned, pTaskState->cbBufferUnaligned);
         pTaskState->cSGListSize = 0;
         pTaskState->cSGInfoSize = 0;
         pTaskState->cSGInfoEntries = 0;
