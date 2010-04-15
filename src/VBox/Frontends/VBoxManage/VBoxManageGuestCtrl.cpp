92eb417ab873221e736673ceb33569d88e72f7c7
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2010-04-15 12:37:18 +0000

    Guest Control/VBoxManage: Syntax.
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@28355 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Frontends/VBoxManage/VBoxManageGuestCtrl.cpp
+++ src/VBox/Frontends/VBoxManage/VBoxManageGuestCtrl.cpp
@@ -63,7 +63,7 @@ void usageGuestControl(void)
 {
     RTPrintf("VBoxManage guestcontrol     execute <vmname>|<uuid>\n"
              "                            <path to program> [--arguments \"<arguments>\"] [--environment \"NAME=VALUE NAME=VALUE\"]\n"
-             "                            [--flags <flags>] [--timeout <msec>] [--username <name> [--password <password>]\n"
+             "                            [--flags <flags>] [--timeout <msec>] [--username <name> [--password <password>]]\n"
              "                            [--verbose] [--wait-for exit]\n"
              "\n");
 }
