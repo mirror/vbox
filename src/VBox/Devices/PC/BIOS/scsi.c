0f71913df957b2f5127e3f593126548a3525ed23
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2010-04-20 15:50:42 +0000

    BIOS: It is a good idea to initialize variables before using them, not after.
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@28530 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Devices/PC/BIOS/scsi.c
+++ src/VBox/Devices/PC/BIOS/scsi.c
@@ -187,6 +187,7 @@ int scsi_read_sectors(device_id, count, lba, segment, offset)
     if (device_id > BX_MAX_SCSI_DEVICES)
         BX_PANIC("scsi_read_sectors: device_id out of range %d\n", device_id);
 
+    ebda_seg = read_word(0x0040, 0x000E);
     // Reset count of transferred data
     write_word(ebda_seg, &EbdaData->ata.trsfsectors,0);
     write_dword(ebda_seg, &EbdaData->ata.trsfbytes,0L);
@@ -203,7 +204,6 @@ int scsi_read_sectors(device_id, count, lba, segment, offset)
     write_byte(get_SS(), aCDB + 8, (uint8_t)(count));
     write_byte(get_SS(), aCDB + 9, 0);
 
-    ebda_seg = read_word(0x0040, 0x000E);
     io_base = read_word(ebda_seg, &EbdaData->scsi.devices[device_id].io_base);
     target_id = read_byte(ebda_seg, &EbdaData->scsi.devices[device_id].target_id);
 
@@ -241,6 +241,7 @@ int scsi_write_sectors(device_id, count, lba, segment, offset)
     if (device_id > BX_MAX_SCSI_DEVICES)
         BX_PANIC("scsi_write_sectors: device_id out of range %d\n", device_id);
 
+    ebda_seg = read_word(0x0040, 0x000E);
     // Reset count of transferred data
     write_word(ebda_seg, &EbdaData->ata.trsfsectors,0);
     write_dword(ebda_seg, &EbdaData->ata.trsfbytes,0L);
@@ -257,7 +258,6 @@ int scsi_write_sectors(device_id, count, lba, segment, offset)
     write_byte(get_SS(), aCDB + 8, (uint8_t)(count));
     write_byte(get_SS(), aCDB + 9, 0);
 
-    ebda_seg = read_word(0x0040, 0x000E);
     io_base = read_word(ebda_seg, &EbdaData->scsi.devices[device_id].io_base);
     target_id = read_byte(ebda_seg, &EbdaData->scsi.devices[device_id].target_id);
 
