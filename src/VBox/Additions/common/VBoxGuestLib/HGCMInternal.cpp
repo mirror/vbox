1afb19c132f1c6f78706538458aa991921b86e6b
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2010-04-20 12:38:47 +0000

    VBoxGuest: Hrmpf!
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@28518 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Additions/common/VBoxGuestLib/HGCMInternal.cpp
+++ src/VBox/Additions/common/VBoxGuestLib/HGCMInternal.cpp
@@ -906,7 +906,7 @@ DECLR0VBGL(int) VbglR0HGCMInternalCall(VBoxGuestHGCMCallInfo *pCallInfo, uint32_
             else
             {
                 if (   rc != VERR_INTERRUPTED
-                    || rc != VERR_TIMEOUT)
+                    && rc != VERR_TIMEOUT)
                     LogRel(("VbglR0HGCMInternalCall: vbglR0HGCMInternalDoCall failed. rc=%Rrc\n", rc));
             }
 
