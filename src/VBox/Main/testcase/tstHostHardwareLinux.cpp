7f7f2b65e19cb379105621c5e81ed5304a8fffc4
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2010-04-21 10:16:57 +0000

    Main: add support for polling for USB devices on Linux hosts without usbfs or hal
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@28553 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Main/testcase/tstHostHardwareLinux.cpp
+++ src/VBox/Main/testcase/tstHostHardwareLinux.cpp
@@ -28,6 +28,7 @@
 #include <iprt/initterm.h>
 #include <iprt/param.h>
 #include <iprt/stream.h>
+#include <iprt/thread.h>
 #include <iprt/linux/sysfs.h>
 
 #include <iprt/cdefs.h>
@@ -36,6 +37,28 @@
 #include <errno.h>
 #include <string.h>
 #include <stdlib.h>
+
+void doHotplugEvent(VBoxMainHotplugWaiter *waiter, RTMSINTERVAL cMillies)
+{
+    while (true)
+    {
+        int rc = waiter->Wait (cMillies);
+        if (rc == VERR_TRY_AGAIN)
+        {
+            RTThreadYield();
+            continue;
+        }
+        if (rc == VERR_TIMEOUT)
+            continue;
+        if (RT_FAILURE(rc))
+        {
+            RTPrintf("Failed!\n");
+            exit(1);
+        }
+        if (RT_SUCCESS(rc))
+            break;
+    }
+}
 
 int main()
 {
@@ -131,17 +154,9 @@ int main()
     VBoxMainHotplugWaiter waiter;
     RTPrintf ("Waiting for hotplug events.  Note that DBus often seems to deliver duplicate events in close succession.\n");
     RTPrintf ("Waiting for a hotplug event for five seconds...\n");
-    if (RT_FAILURE(waiter.Wait (5000)))
-    {
-        RTPrintf("Failed!\n");
-        exit(1);
-    }
+    doHotplugEvent(&waiter, 5000);
     RTPrintf ("Waiting for a hotplug event, Ctrl-C to abort...\n");
-    if (RT_FAILURE(waiter.Wait(RT_INDEFINITE_WAIT)))
-    {
-        RTPrintf("Failed!\n");
-        exit(1);
-    }
+    doHotplugEvent(&waiter, RT_INDEFINITE_WAIT);
 #endif  /* VBOX_USB_WITH_SYSFS */
     return 0;
 }
