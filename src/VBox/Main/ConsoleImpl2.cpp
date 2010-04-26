45ccb55e01cf942ff7177488753ed67172c49d65
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2010-04-26 16:47:24 +0000

    Main: Use a different PCI slot for the SAS LsiLogic controller
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@28769 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Main/ConsoleImpl2.cpp
+++ src/VBox/Main/ConsoleImpl2.cpp
@@ -1232,9 +1232,9 @@ DECLCALLBACK(int) Console::configConstructor(PVM pVM, void *pvConsole)
 
             case StorageControllerType_LsiLogicSas:
             {
-                rc = CFGMR3InsertInteger(pCtlInst, "PCIDeviceNo",          21);                 RC_CHECK();
-                Assert(!afPciDeviceNo[21]);
-                afPciDeviceNo[21] = true;
+                rc = CFGMR3InsertInteger(pCtlInst, "PCIDeviceNo",          22);                 RC_CHECK();
+                Assert(!afPciDeviceNo[22]);
+                afPciDeviceNo[22] = true;
                 rc = CFGMR3InsertInteger(pCtlInst, "PCIFunctionNo",        0);                  RC_CHECK();
 
                 rc = CFGMR3InsertString(pCfg,  "ControllerType", "SAS1068");                    RC_CHECK();
