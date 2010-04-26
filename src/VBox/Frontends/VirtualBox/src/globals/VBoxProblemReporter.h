8e85825ba4da6518d41a2da80843e03951bfde16
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2010-04-26 15:30:22 +0000

    really back out accidental previous commit
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@28758 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Frontends/VirtualBox/src/globals/VBoxProblemReporter.h
+++ src/VBox/Frontends/VirtualBox/src/globals/VBoxProblemReporter.h
@@ -5,7 +5,7 @@
  */
 
 /*
- * Copyright (C) 2006-2010 Sun Microsystems, Inc.
+ * Copyright (C) 2006-2008 Sun Microsystems, Inc.
  *
  * This file is part of VirtualBox Open Source Edition (OSE), as
  * available from http://www.virtualbox.org. This file is free software;
@@ -214,10 +214,6 @@ public:
 
     bool askAboutSnapshotRestoring (const QString &aSnapshotName);
     bool askAboutSnapshotDeleting (const QString &aSnapshotName);
-    bool askAboutSnapshotDeletingFreeSpace (const QString &aSnapshotName,
-                                            const QString &aTargetImageName,
-                                            const QString &aTargetImageMaxSize,
-                                            const QString &aTargetFilesystemFree);
     void cannotRestoreSnapshot (const CConsole &aConsole, const QString &aSnapshotName);
     void cannotRestoreSnapshot (const CProgress &aProgress, const QString &aSnapshotName);
     void cannotDeleteSnapshot (const CConsole &aConsole, const QString &aSnapshotName);
