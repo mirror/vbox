46f591787968853644dc78ae653522832bb4d4ee
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2010-04-26 16:34:05 +0000

    filemuncher run
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@28767 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Frontends/VBoxHeadless/FramebufferVNC.h
+++ src/VBox/Frontends/VBoxHeadless/FramebufferVNC.h
@@ -4,9 +4,9 @@
  */
 
 /*
- * Copyright (C) 2006-2007 Sun Microsystems, Inc.
- *
  * Contributed by Ivo Smits <Ivo@UFO-Net.nl>
+ *
+ * Copyright (C) 2006-2007 Sun Microsystems, Inc.
  *
  * This file is part of VirtualBox Open Source Edition (OSE), as
  * available from http://www.virtualbox.org. This file is free software;
