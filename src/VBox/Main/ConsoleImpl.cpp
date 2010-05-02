7d3e29318c2d77f22d16c5b143613f96372d08ee
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2010-05-02 19:36:11 +0000

    ConsoleImpl: Implemented VBOX_E_DONT_CALL_AGAIN for IConsoleCallback.
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@28959 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Main/ConsoleImpl.cpp
+++ src/VBox/Main/ConsoleImpl.cpp
@@ -241,6 +241,140 @@ struct VMSaveTask : public VMProgressTask
     ComPtr<IProgress> mServerProgress;
 };
 
+// ConsoleCallbackRegistration
+////////////////////////////////////////////////////////////////////////////////
+
+/**
+ * Registered IConsoleCallback, used by Console::CallbackList and
+ * Console::mCallbacks.
+ *
+ * In addition to keeping the interface pointer this also keeps track of the
+ * methods that asked to not be called again.  The latter is for reducing
+ * unnecessary IPC.
+ */
+class ConsoleCallbackRegistration
+{
+public:
+    ComPtr<IConsoleCallback> ptrICb;
+    /** Bitmap of disabled callback methods, that is methods that has return
+     * VBOX_E_DONT_CALL_AGAIN. */
+    uint32_t                    bmDisabled;
+    /** Callback bit indexes (for bmDisabled). */
+    typedef enum
+    {
+        kOnMousePointerShapeChange = 0,
+        kOnMouseCapabilityChange,
+        kOnKeyboardLedsChange,
+        kOnStateChange,
+        kOnAdditionsStateChange,
+        kOnNetworkAdapterChange,
+        kOnSerialPortChange,
+        kOnParallelPortChange,
+        kOnStorageControllerChange,
+        kOnMediumChange,
+        kOnCPUChange,
+        kOnVRDPServerChange,
+        kOnRemoteDisplayInfoChange,
+        kOnUSBControllerChange,
+        kOnUSBDeviceStateChange,
+        kOnSharedFolderChange,
+        kOnRuntimeError,
+        kOnCanShowWindow,
+        kOnShowWindow
+    } CallbackBit;
+
+    ConsoleCallbackRegistration(IConsoleCallback *pIConsoleCallback)
+        : ptrICb(pIConsoleCallback), bmDisabled(0)
+    {
+        /* nothing */
+    }
+
+    ~ConsoleCallbackRegistration()
+    {
+       /* nothing */
+    }
+
+    /** Equal operator for std::find. */
+    bool operator==(const ConsoleCallbackRegistration &rThat) const
+    {
+        return this->ptrICb == rThat.ptrICb;
+    }
+
+    /**
+     * Checks if the callback is wanted, i.e. if the method hasn't yet returned
+     * VBOX_E_DONT_CALL_AGAIN.
+     * @returns @c true if it is wanted, @c false if not.
+     * @param   enmBit      The callback, be sure to get this one right!
+     */
+    inline bool isWanted(CallbackBit enmBit) const
+    {
+        return !ASMBitTest(&bmDisabled, enmBit);
+    }
+
+    /**
+     * Called in response to VBOX_E_DONT_CALL_AGAIN.
+     * @param   enmBit      The callback, be sure to get this one right!
+     */
+    inline void setDontCallAgain(CallbackBit enmBit)
+    {
+        ASMAtomicBitSet(&bmDisabled, enmBit);
+    }
+
+    /**
+     * Handle a callback return code, picking up VBOX_E_DONT_CALL_AGAIN.
+     *
+     * @returns hrc or S_OK if VBOX_E_DONT_CALL_AGAIN.
+     * @param   enmBit      The callback, be sure to get this one right!
+     * @param   hrc         The status code returned by the callback.
+     */
+    inline HRESULT handleResult(CallbackBit enmBit, HRESULT hrc)
+    {
+        if (hrc == VBOX_E_DONT_CALL_AGAIN)
+        {
+            setDontCallAgain(enmBit);
+            hrc = S_OK;
+        }
+        return hrc;
+    }
+};
+
+/**
+ * Macro for iterating the callback list (Console::mCallbacks) and invoking the
+ * given method on each entry.
+ *
+ * This handles VBOX_E_DONT_CALL_AGAIN as well as removing dead interfaces
+ * which.  This makes the code a big and clunky, thus this macro.  It may make
+ * debugging and selective logging a bit of a pain, but duplicating this code
+ * some seventeen times is much more of a pain.
+ *
+ * The caller must hold the console object lock - read or write, we don't care.
+ * The caller must be a Console method - the console members accessible thru the
+ * 'this' pointer.
+ *
+ * @param   CallbackMethod      The callback method, like OnKeyboardLedsChange.
+ * @param   Args                The method arguments enclosed in parentheses.
+ */
+#define CONSOLE_DO_CALLBACKS(CallbackMethod, Args) \
+    do \
+    { \
+        CallbackList::iterator it = this->mCallbacks.begin(); \
+        while (it != this->mCallbacks.end()) \
+        { \
+            if (it->isWanted(ConsoleCallbackRegistration::k ## CallbackMethod)) \
+            { \
+                HRESULT hrc = it->ptrICb-> CallbackMethod Args; \
+                hrc = it->handleResult(ConsoleCallbackRegistration::k ## CallbackMethod, hrc); \
+                if (FAILED_DEAD_INTERFACE(hrc)) \
+                { \
+                    it = this->mCallbacks.erase(it); \
+                    continue; \
+                } \
+            } \
+            ++it; \
+        } \
+    } while (0)
+
+
 // constructor / destructor
 /////////////////////////////////////////////////////////////////////////////
 
@@ -2621,11 +2755,7 @@ Console::CreateSharedFolder(IN_BSTR aName, IN_BSTR aHostPath, BOOL aWritable)
     mSharedFolders.insert(std::make_pair(aName, sharedFolder));
 
     /* notify console callbacks after the folder is added to the list */
-    {
-        CallbackList::iterator it = mCallbacks.begin();
-        while (it != mCallbacks.end())
-            (*it++)->OnSharedFolderChange(Scope_Session);
-    }
+    CONSOLE_DO_CALLBACKS(OnSharedFolderChange,(Scope_Session));
 
     return rc;
 }
@@ -2682,11 +2812,7 @@ STDMETHODIMP Console::RemoveSharedFolder(IN_BSTR aName)
     mSharedFolders.erase(aName);
 
     /* notify console callbacks after the folder is removed to the list */
-    {
-        CallbackList::iterator it = mCallbacks.begin();
-        while (it != mCallbacks.end())
-            (*it++)->OnSharedFolderChange(Scope_Session);
-    }
+    CONSOLE_DO_CALLBACKS(OnSharedFolderChange,(Scope_Session));
 
     return rc;
 }
@@ -3379,11 +3505,7 @@ HRESULT Console::onNetworkAdapterChange(INetworkAdapter *aNetworkAdapter, BOOL c
 
     /* notify console callbacks on success */
     if (SUCCEEDED(rc))
-    {
-        CallbackList::iterator it = mCallbacks.begin();
-        while (it != mCallbacks.end())
-            (*it++)->OnNetworkAdapterChange(aNetworkAdapter);
-    }
+        CONSOLE_DO_CALLBACKS(OnNetworkAdapterChange,(aNetworkAdapter));
 
     LogFlowThisFunc(("Leaving rc=%#x\n", rc));
     return rc;
@@ -3599,11 +3721,7 @@ HRESULT Console::onSerialPortChange(ISerialPort *aSerialPort)
 
     /* notify console callbacks on success */
     if (SUCCEEDED(rc))
-    {
-        CallbackList::iterator it = mCallbacks.begin();
-        while (it != mCallbacks.end())
-            (*it++)->OnSerialPortChange(aSerialPort);
-    }
+        CONSOLE_DO_CALLBACKS(OnSerialPortChange,(aSerialPort));
 
     LogFlowThisFunc(("Leaving rc=%#x\n", rc));
     return rc;
@@ -3637,11 +3755,7 @@ HRESULT Console::onParallelPortChange(IParallelPort *aParallelPort)
 
     /* notify console callbacks on success */
     if (SUCCEEDED(rc))
-    {
-        CallbackList::iterator it = mCallbacks.begin();
-        while (it != mCallbacks.end())
-            (*it++)->OnParallelPortChange(aParallelPort);
-    }
+        CONSOLE_DO_CALLBACKS(OnParallelPortChange,(aParallelPort));
 
     LogFlowThisFunc(("Leaving rc=%#x\n", rc));
     return rc;
@@ -3675,11 +3789,7 @@ HRESULT Console::onStorageControllerChange()
 
     /* notify console callbacks on success */
     if (SUCCEEDED(rc))
-    {
-        CallbackList::iterator it = mCallbacks.begin();
-        while (it != mCallbacks.end())
-            (*it++)->OnStorageControllerChange();
-    }
+        CONSOLE_DO_CALLBACKS(OnStorageControllerChange,());
 
     LogFlowThisFunc(("Leaving rc=%#x\n", rc));
     return rc;
@@ -3713,11 +3823,7 @@ HRESULT Console::onMediumChange(IMediumAttachment *aMediumAttachment, BOOL aForc
 
     /* notify console callbacks on success */
     if (SUCCEEDED(rc))
-    {
-        CallbackList::iterator it = mCallbacks.begin();
-        while (it != mCallbacks.end())
-            (*it++)->OnMediumChange(aMediumAttachment);
-    }
+        CONSOLE_DO_CALLBACKS(OnMediumChange,(aMediumAttachment));
 
     LogFlowThisFunc(("Leaving rc=%#x\n", rc));
     return rc;
@@ -3754,11 +3860,7 @@ HRESULT Console::onCPUChange(ULONG aCPU, BOOL aRemove)
 
     /* notify console callbacks on success */
     if (SUCCEEDED(rc))
-    {
-        CallbackList::iterator it = mCallbacks.begin();
-        while (it != mCallbacks.end())
-            (*it++)->OnCPUChange(aCPU, aRemove);
-    }
+        CONSOLE_DO_CALLBACKS(OnCPUChange,(aCPU, aRemove));
 
     LogFlowThisFunc(("Leaving rc=%#x\n", rc));
     return rc;
@@ -3819,11 +3921,7 @@ HRESULT Console::onVRDPServerChange()
 
     /* notify console callbacks on success */
     if (SUCCEEDED(rc))
-    {
-        CallbackList::iterator it = mCallbacks.begin();
-        while (it != mCallbacks.end())
-            (*it++)->OnVRDPServerChange();
-    }
+        CONSOLE_DO_CALLBACKS(OnVRDPServerChange,());
 
     return rc;
 }
@@ -3838,9 +3936,7 @@ void Console::onRemoteDisplayInfoChange()
 
     AutoReadLock alock(this COMMA_LOCKVAL_SRC_POS);
 
-    CallbackList::iterator it = mCallbacks.begin();
-    while (it != mCallbacks.end())
-        (*it++)->OnRemoteDisplayInfoChange();
+    CONSOLE_DO_CALLBACKS(OnRemoteDisplayInfoChange,());
 }
 
 
@@ -3878,11 +3974,7 @@ HRESULT Console::onUSBControllerChange()
 
     /* notify console callbacks on success */
     if (SUCCEEDED(rc))
-    {
-        CallbackList::iterator it = mCallbacks.begin();
-        while (it != mCallbacks.end())
-            (*it++)->OnUSBControllerChange();
-    }
+        CONSOLE_DO_CALLBACKS(OnUSBControllerChange,());
 
     return rc;
 }
@@ -3906,10 +3998,10 @@ HRESULT Console::onSharedFolderChange(BOOL aGlobal)
     /* notify console callbacks on success */
     if (SUCCEEDED(rc))
     {
-        CallbackList::iterator it = mCallbacks.begin();
-        while (it != mCallbacks.end())
-            (*it++)->OnSharedFolderChange(aGlobal ? (Scope_T)Scope_Global
-                                                  : (Scope_T)Scope_Machine);
+        if (aGlobal)
+            CONSOLE_DO_CALLBACKS(OnSharedFolderChange,(Scope_Global));
+        else
+            CONSOLE_DO_CALLBACKS(OnSharedFolderChange,(Scope_Machine));
     }
 
     return rc;
@@ -4585,10 +4677,7 @@ void Console::onMousePointerShapeChange(bool fVisible, bool fAlpha,
 
     mCallbackData.mpsc.valid = true;
 
-    CallbackList::iterator it = mCallbacks.begin();
-    while (it != mCallbacks.end())
-        (*it++)->OnMousePointerShapeChange(fVisible, fAlpha, xHot, yHot,
-                                           width, height, (BYTE *) pShape);
+    CONSOLE_DO_CALLBACKS(OnMousePointerShapeChange,(fVisible, fAlpha, xHot, yHot, width, height, (BYTE *) pShape));
 
 #if 0
     LogFlowThisFuncLeave();
@@ -4615,12 +4704,7 @@ void Console::onMouseCapabilityChange(BOOL supportsAbsolute, BOOL supportsRelati
     mCallbackData.mcc.needsHostCursor = needsHostCursor;
     mCallbackData.mcc.valid = true;
 
-    CallbackList::iterator it = mCallbacks.begin();
-    while (it != mCallbacks.end())
-    {
-        Log2(("Console::onMouseCapabilityChange: calling %p\n", (void*)*it));
-        (*it++)->OnMouseCapabilityChange(supportsAbsolute, supportsRelative, needsHostCursor);
-    }
+    CONSOLE_DO_CALLBACKS(OnMouseCapabilityChange,(supportsAbsolute, supportsRelative, needsHostCursor));
 }
 
 /**
@@ -4632,10 +4716,7 @@ void Console::onStateChange(MachineState_T machineState)
     AssertComRCReturnVoid(autoCaller.rc());
 
     AutoReadLock alock(this COMMA_LOCKVAL_SRC_POS);
-
-    CallbackList::iterator it = mCallbacks.begin();
-    while (it != mCallbacks.end())
-        (*it++)->OnStateChange(machineState);
+    CONSOLE_DO_CALLBACKS(OnStateChange,(machineState));
 }
 
 /**
@@ -4647,10 +4728,7 @@ void Console::onAdditionsStateChange()
     AssertComRCReturnVoid(autoCaller.rc());
 
     AutoReadLock alock(this COMMA_LOCKVAL_SRC_POS);
-
-    CallbackList::iterator it = mCallbacks.begin();
-    while (it != mCallbacks.end())
-        (*it++)->OnAdditionsStateChange();
+    CONSOLE_DO_CALLBACKS(OnAdditionsStateChange,());
 }
 
 /**
@@ -4687,9 +4765,7 @@ void Console::onKeyboardLedsChange(bool fNumLock, bool fCapsLock, bool fScrollLo
     mCallbackData.klc.scrollLock = fScrollLock;
     mCallbackData.klc.valid = true;
 
-    CallbackList::iterator it = mCallbacks.begin();
-    while (it != mCallbacks.end())
-        (*it++)->OnKeyboardLedsChange(fNumLock, fCapsLock, fScrollLock);
+    CONSOLE_DO_CALLBACKS(OnKeyboardLedsChange,(fNumLock, fCapsLock, fScrollLock));
 }
 
 /**
@@ -4702,10 +4778,7 @@ void Console::onUSBDeviceStateChange(IUSBDevice *aDevice, bool aAttached,
     AssertComRCReturnVoid(autoCaller.rc());
 
     AutoReadLock alock(this COMMA_LOCKVAL_SRC_POS);
-
-    CallbackList::iterator it = mCallbacks.begin();
-    while (it != mCallbacks.end())
-        (*it++)->OnUSBDeviceStateChange(aDevice, aAttached, aError);
+    CONSOLE_DO_CALLBACKS(OnUSBDeviceStateChange,(aDevice, aAttached, aError));
 }
 
 /**
@@ -4717,10 +4790,7 @@ void Console::onRuntimeError(BOOL aFatal, IN_BSTR aErrorID, IN_BSTR aMessage)
     AssertComRCReturnVoid(autoCaller.rc());
 
     AutoReadLock alock(this COMMA_LOCKVAL_SRC_POS);
-
-    CallbackList::iterator it = mCallbacks.begin();
-    while (it != mCallbacks.end())
-        (*it++)->OnRuntimeError(aFatal, aErrorID, aMessage);
+    CONSOLE_DO_CALLBACKS(OnRuntimeError,(aFatal, aErrorID, aMessage));
 }
 
 /**
@@ -4738,19 +4808,26 @@ HRESULT Console::onShowWindow(BOOL aCheck, BOOL *aCanShow, ULONG64 *aWinId)
     AssertComRCReturnRC(autoCaller.rc());
 
     AutoReadLock alock(this COMMA_LOCKVAL_SRC_POS);
-
-    HRESULT rc = S_OK;
     CallbackList::iterator it = mCallbacks.begin();
-
     if (aCheck)
     {
         while (it != mCallbacks.end())
         {
-            BOOL canShow = FALSE;
-            rc = (*it++)->OnCanShowWindow(&canShow);
-            AssertComRC(rc);
-            if (FAILED(rc) || !canShow)
-                return rc;
+            if (it->isWanted(ConsoleCallbackRegistration::kOnCanShowWindow))
+            {
+                BOOL canShow = FALSE;
+                HRESULT hrc = it->ptrICb->OnCanShowWindow(&canShow);
+                hrc = it->handleResult(ConsoleCallbackRegistration::kOnCanShowWindow, hrc);
+                if (FAILED_DEAD_INTERFACE(hrc))
+                {
+                    it = this->mCallbacks.erase(it);
+                    continue;
+                }
+                AssertComRC(hrc);
+                if (FAILED(hrc) || !canShow)
+                    return hrc;
+            }
+            ++it;
         }
         *aCanShow = TRUE;
     }
@@ -4758,15 +4835,25 @@ HRESULT Console::onShowWindow(BOOL aCheck, BOOL *aCanShow, ULONG64 *aWinId)
     {
         while (it != mCallbacks.end())
         {
-            ULONG64 winId = 0;
-            rc = (*it++)->OnShowWindow(&winId);
-            AssertComRC(rc);
-            if (FAILED(rc))
-                return rc;
-            /* only one callback may return non-null winId */
-            Assert(*aWinId == 0 || winId == 0);
-            if (*aWinId == 0)
-                *aWinId = winId;
+            if (it->isWanted(ConsoleCallbackRegistration::kOnShowWindow))
+            {
+                ULONG64 winId = 0;
+                HRESULT hrc = it->ptrICb->OnShowWindow(&winId);
+                hrc = it->handleResult(ConsoleCallbackRegistration::kOnCanShowWindow, hrc);
+                if (FAILED_DEAD_INTERFACE(hrc))
+                {
+                    it = this->mCallbacks.erase(it);
+                    continue;
+                }
+                AssertComRC(hrc);
+                if (FAILED(hrc))
+                    return hrc;
+
+                /* only one callback may return non-null winId */
+                Assert(*aWinId == 0 || winId == 0);
+                if (*aWinId == 0)
+                    *aWinId = winId;
+            }
         }
     }
 
