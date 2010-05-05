8537905da5dd13e5c73149d72d1060183183f904
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2010-05-05 02:26:10 +0000

    NAT: reserves space for ether header before any manipulation with mbuf.
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@29050 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Devices/Network/slirp/tftp.c
+++ src/VBox/Devices/Network/slirp/tftp.c
@@ -156,6 +156,7 @@ static int tftp_send_oack(PNATState pData,
     memset(m->m_data, 0, m->m_size);
     m->m_data += if_maxlinkhdr;
 #else
+    m->m_data += if_maxlinkhdr;
     m->m_pkthdr.header = mtod(m, void *);
 #endif
     tp = (void *)m->m_data;
@@ -205,6 +206,7 @@ static int tftp_send_error(PNATState pData,
     memset(m->m_data, 0, m->m_size);
     m->m_data += if_maxlinkhdr;
 #else
+    m->m_data += if_maxlinkhdr;
     m->m_pkthdr.header = mtod(m, void *);
 #endif
     tp = (void *)m->m_data;
@@ -261,9 +263,10 @@ static int tftp_send_data(PNATState pData,
     memset(m->m_data, 0, m->m_size);
     m->m_data += if_maxlinkhdr;
 #else
+    m->m_data += if_maxlinkhdr;
     m->m_pkthdr.header = mtod(m, void *);
 #endif
-    tp = (void *)m->m_data;
+    tp = mtod(m, void *);
     m->m_data += sizeof(struct udpiphdr);
 
     tp->tp_op = RT_H2N_U16_C(TFTP_DATA);
