8be7d6b500b987966eb6476f3688ceb226973a6a
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2010-05-06 12:55:49 +0000

    Main/Medium: add forgotten medium registry save when deleting the storage representation of a medium.
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@29149 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Main/MediumImpl.cpp
+++ src/VBox/Main/MediumImpl.cpp
@@ -3907,13 +3907,23 @@ HRESULT Medium::deleteStorage(ComObjPtr<Progress> *aProgress,
         }
 
         /* try to remove from the list of known hard disks before performing
-         * actual deletion (we favor the consistency of the media registry in
-         * the first place which would have been broken if
-         * unregisterWithVirtualBox() failed after we successfully deleted the
-         * storage) */
-        rc = unregisterWithVirtualBox(pfNeedsSaveSettings);
+         * actual deletion (we favor the consistency of the media registry
+         * which would have been broken if unregisterWithVirtualBox() failed
+         * after we successfully deleted the storage) */
+        bool fNeedsSaveSettings = false;
+        rc = unregisterWithVirtualBox(&fNeedsSaveSettings);
         if (FAILED(rc))
             throw rc;
+        // no longer need lock, and below we might need the VirtualBox lock.
+        multilock.release();
+        if (fNeedsSaveSettings)
+        {
+            AutoWriteLock vboxlock(m->pVirtualBox COMMA_LOCKVAL_SRC_POS);
+            m->pVirtualBox->saveSettings();
+        }
+        // always set it to false because the medium registry is up to date
+        if (pfNeedsSaveSettings)
+            *pfNeedsSaveSettings = false;
 
         if (aProgress != NULL)
         {
