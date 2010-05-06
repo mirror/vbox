cba7c632c2958bf01408dc41c2a690d4af43ba16
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2010-05-06 10:40:30 +0000

    Runtime/Aio: Fix flush API. The fFlush flag needs to be resetted
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@29129 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Runtime/r3/posix/fileaio-posix.cpp
+++ src/VBox/Runtime/r3/posix/fileaio-posix.cpp
@@ -395,6 +395,7 @@ DECLINLINE(int) rtFileAioReqPrepareTransfer(RTFILEAIOREQ hReq, RTFILE hFile,
     Assert(cbTransfer > 0);
 
     memset(&pReqInt->AioCB, 0, sizeof(struct aiocb));
+    pReqInt->fFlush               = false;
     pReqInt->AioCB.aio_lio_opcode = uTransferDirection;
     pReqInt->AioCB.aio_fildes     = (int)hFile;
     pReqInt->AioCB.aio_offset     = off;
@@ -435,7 +436,11 @@ RTDECL(int) RTFileAioReqPrepareFlush(RTFILEAIOREQ hReq, RTFILE hFile, void *pvUs
 
     pReqInt->fFlush           = true;
     pReqInt->AioCB.aio_fildes = (int)hFile;
+    pReqInt->AioCB.aio_offset = 0;
+    pReqInt->AioCB.aio_nbytes = 0;
+    pReqInt->AioCB.aio_buf    = NULL;
     pReqInt->pvUser           = pvUser;
+    pReqInt->Rc               = VERR_FILE_AIO_IN_PROGRESS;
     RTFILEAIOREQ_SET_STATE(pReqInt, PREPARED);
 
     return VINF_SUCCESS;
