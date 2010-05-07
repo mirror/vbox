dddaa6df347a480bf07333923b156b492faa7b6f
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2010-05-07 07:36:09 +0000

    non working callback sample
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@29183 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/libs/xpcom18a4/java/tests/TestVBox.java
+++ src/libs/xpcom18a4/java/tests/TestVBox.java
@@ -16,6 +16,29 @@ import org.virtualbox.*;
 
 public class TestVBox
 {
+
+    public static class VBoxCallbacks implements IVirtualBoxCallback
+    {
+        public void onGuestPropertyChange(String machineId, String name, String value, String flags) {}
+        public void onSnapshotChange(String machineId, String snapshotId) {}
+        public void onSnapshotDeleted(String machineId, String snapshotId) {}
+        public void onSnapshotTaken(String machineId, String snapshotId) {}
+        /** @todo is there a type SessionState instead of long? */
+        public void onSessionStateChange(String machineId, long state) {}
+        public void onMachineRegistered(String machineId, boolean registered) {}
+        /** @todo long -> MediumType */
+        public void onMediumRegistered(String mediumId, long mediumType, boolean registered) {}
+        public void onExtraDataChange(String machineId, String key, String value) {}
+        public boolean onExtraDataCanChange(String machineId, String key, String value, String[] error) { return true; }
+        public void onMachineDataChange(String machineId) {}
+        /** @todo long -> MachineState */
+        public void onMachineStateChange(String machineId, long state) { System.out.println("onMachineStateChange -- VM: " + machineId + ", state: " + state); };
+        
+        /** @todo ugly reimplementation of queryInterface, should have base class to derive from */
+        public nsISupports queryInterface(String iid) { return org.mozilla.xpcom.Mozilla.queryInterface(this, iid); }
+    };
+	
+	
     public static void main(String[] args)
     {
         VirtualBoxManager mgr = VirtualBoxManager.getInstance(null);
@@ -44,6 +67,9 @@ public class TestVBox
                 }
             }
 
+//            VBoxCallbacks vboxCallbacks = new VBoxCallbacks();
+//            vbox.registerCallback(vboxCallbacks);
+
             /* do something silly, start the first VM in the list */
             String m = machs[0].getName();
             System.out.println("\nAttempting to start VM '" + m + "'");
@@ -56,6 +82,8 @@ public class TestVBox
             {
                 System.out.println("cannot start machine "+m);
             }
+            
+//            vbox.unregisterCallback(vboxCallbacks);
         }
         catch (Throwable e)
         {
