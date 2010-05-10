3025caa4539a0d53b0c2ca83e78dea00b1fc5376
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2010-05-10 14:48:40 +0000

    Linux Additions/installer: fixed autorun.sh header
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@29305 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Additions/linux/installer/autorun.sh
+++ src/VBox/Additions/linux/installer/autorun.sh
@@ -1,13 +1,18 @@
 #!/bin/sh
 #
-# Sun VirtualBox
 # VirtualBox Guest Additions installation script for Linux
+#
 
 #
-# Copyrign Microsystems, Inc.
+# Copyright (C) 2009-2010 Oracle Corporation
 #
-# Sun Microsystems, Inc.
-# All rights reserved
+# This file is part of VirtualBox Open Source Edition (OSE), as
+# available from http://www.virtualbox.org. This file is free software;
+# you can redistribute it and/or modify it under the terms of the GNU
+# General Public License (GPL) as published by the Free Software
+# Foundation, in version 2 as it comes in the "COPYING" file of the
+# VirtualBox OSE distribution. VirtualBox OSE is distributed in the
+# hope that it will be useful, but WITHOUT ANY WARRANTY of any kind.
 #
 
 PATH=$PATH:/bin:/sbin:/usr/sbin
