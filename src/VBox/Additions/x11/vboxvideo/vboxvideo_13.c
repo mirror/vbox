db43a495838c415632450bc869241a58c1beed3f
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2010-05-11 16:56:15 +0000

    header
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@29379 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Additions/x11/vboxvideo/vboxvideo_13.c
+++ src/VBox/Additions/x11/vboxvideo/vboxvideo_13.c
@@ -1,10 +1,10 @@
+/* $Id: $ */
 /** @file
- *
  * Linux Additions X11 graphics driver
  */
 
 /*
- * Copyright (C) 2006-2007 Oracle Corporation
+ * Copyright (C) 2006-2010 Oracle Corporation
  *
  * This file is part of VirtualBox Open Source Edition (OSE), as
  * available from http://www.virtualbox.org. This file is free software;
