ff71050cfa34c1cb89e5b6ab6b85de019dd159e2
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2010-05-11 10:52:38 +0000

    FE/Qt: New running VM core: cosmetics.
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@29339 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Frontends/VirtualBox/src/runtime/normal/UIMachineWindowNormal.cpp
+++ src/VBox/Frontends/VirtualBox/src/runtime/normal/UIMachineWindowNormal.cpp
@@ -567,9 +567,9 @@ void UIMachineWindowNormal::loadWindowSettings()
         if (machineView())
 #if defined(Q_WS_MAC) || defined(Q_WS_WIN)
             machineView()->normalizeGeometry(true);
-#else /* Q_WS_MAC */
+#else /* Q_WS_MAC || Q_WS_WIN */
             QTimer::singleShot(0, machineView(), SLOT(sltNormalizeGeometry()));
-#endif /* Q_WS_MAC */
+#endif /* Q_WS_MAC || Q_WS_WIN */
     }
 
     /* Load availability settings: */
