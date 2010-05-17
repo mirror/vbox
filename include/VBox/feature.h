6120295cc8907a8e6b47bfca67fcc90a6575b7e2
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2010-05-17 15:26:56 +0000

    Introduced feature.h
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@29567 cfe28804-0f27-0410-a406-dd0f0b0b656f

---
+++ include/VBox/feature.h
@@ -0,0 +1,36 @@
+/** @file
+ * Misc. features
+ */
+
+/*
+ * Copyright (C) 2006-2007 Oracle Corporation
+ *
+ * This file is part of VirtualBox Open Source Edition (OSE), as
+ * available from http://www.virtualbox.org. This file is free software;
+ * you can redistribute it and/or modify it under the terms of the GNU
+ * General Public License (GPL) as published by the Free Software
+ * Foundation, in version 2 as it comes in the "COPYING" file of the
+ * VirtualBox OSE distribution. VirtualBox OSE is distributed in the
+ * hope that it will be useful, but WITHOUT ANY WARRANTY of any kind.
+ *
+ * The contents of this file may alternatively be used under the terms
+ * of the Common Development and Distribution License Version 1.0
+ * (CDDL) only, as it comes in the "COPYING.CDDL" file of the
+ * VirtualBox OSE distribution, in which case the provisions of the
+ * CDDL are applicable instead of those of the GPL.
+ *
+ * You may elect to license modified versions of this file under the
+ * terms and conditions of either the GPL or the CDDL or both.
+ */
+
+#ifndef ___VBox_feature_h
+#define ___VBox_feature_h
+
+/* This must match GMMR0Init; currently we only support page fusion on all 64-bit hosts except Mac OS X */
+#if HC_ARCH_BITS == 64 && (defined(RT_OS_WINDOWS) || defined(RT_OS_SOLARIS) || defined(RT_OS_LINUX) || defined(RT_OS_FREEBSD))
+#define VBOX_WITH_PAGE_SHARING
+#endif
+
+
+#endif
+
