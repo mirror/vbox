967fb4746fc91ea1189f26a0cbab48144d9b7cf7
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2010-05-17 12:37:33 +0000

    Main/Snapshot: fix lock order violation when restoring a snapshot
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@29540 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Main/SnapshotImpl.cpp
+++ src/VBox/Main/SnapshotImpl.cpp
@@ -1702,14 +1702,6 @@ void SessionMachine::restoreSnapshotHandler(RestoreSnapshotTask &aTask)
         return;
     }
 
-    AutoWriteLock alock(this COMMA_LOCKVAL_SRC_POS);
-
-    /* discard all current changes to mUserData (name, OSType etc.) (note that
-     * the machine is powered off, so there is no need to inform the direct
-     * session) */
-    if (mData->flModifications)
-        rollback(false /* aNotify */);
-
     HRESULT rc = S_OK;
 
     bool stateRestored = false;
@@ -1717,6 +1709,14 @@ void SessionMachine::restoreSnapshotHandler(RestoreSnapshotTask &aTask)
 
     try
     {
+        AutoWriteLock alock(this COMMA_LOCKVAL_SRC_POS);
+
+        /* Discard all current changes to mUserData (name, OSType etc.).
+         * Note that the machine is powered off, so there is no need to inform
+         * the direct session. */
+        if (mData->flModifications)
+            rollback(false /* aNotify */);
+
         /* Delete the saved state file if the machine was Saved prior to this
          * operation */
         if (aTask.machineStateBackup == MachineState_Saved)
