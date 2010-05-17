e620218c079750137619737f65656ea6aa5307d6
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2010-05-17 10:59:21 +0000

    FE/Qt: New running VM core: More strict using of closing() signal in appropriate places.
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@29526 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Frontends/VirtualBox/src/VBoxVMInformationDlg.cpp
+++ src/VBox/Frontends/VirtualBox/src/VBoxVMInformationDlg.cpp
@@ -48,7 +48,6 @@ void VBoxVMInformationDlg::createInformationDlg(UIMachineWindow *pMachineWindow)
         VBoxVMInformationDlg *id = new VBoxVMInformationDlg(pMachineWindow, Qt::Window);
         id->centerAccording (pMachineWindow->machineWindow());
         // TODO_NEW_CORE: this seems not necessary, cause we set WA_DeleteOnClose.
-        connect (vboxGlobal().mainWindow(), SIGNAL (closing()), id, SLOT (close()));
         id->setAttribute (Qt::WA_DeleteOnClose);
         mSelfArray [machine.GetName()] = id;
     }
@@ -145,7 +144,6 @@ void VBoxVMInformationDlg::createInformationDlg (const CSession &aSession, VBoxC
         /* Creating new information dialog if there is no one existing */
         VBoxVMInformationDlg *id = new VBoxVMInformationDlg (aConsole, aSession, Qt::Window);
         id->centerAccording (aConsole);
-        connect (vboxGlobal().mainWindow(), SIGNAL (closing()), id, SLOT (close()));
         id->setAttribute (Qt::WA_DeleteOnClose);
         mSelfArray [machine.GetName()] = id;
     }
