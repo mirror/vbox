cb0897a1cc8774d0fa85f03ef81e69e0c5a8b7e0
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2010-05-18 09:00:39 +0000

    process-posix: use ''crypt_r()'' instead of ''crypt()''
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@29602 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Runtime/r3/posix/process-posix.cpp
+++ src/VBox/Runtime/r3/posix/process-posix.cpp
@@ -39,6 +39,7 @@
 #include <fcntl.h>
 #include <signal.h>
 #if defined(RT_OS_LINUX)
+# include <crypt.h>
 # include <pwd.h>
 # include <shadow.h>
 #endif
@@ -92,7 +93,8 @@ static int rtCheckCredentials(const char *pszUser, const char *pszPasswd, gid_t 
     if (spwd)
         pw->pw_passwd = spwd->sp_pwdp;
 
-    char *pszEncPasswd = crypt(pszPasswd, pw->pw_passwd);
+    struct crypt_data data;
+    char *pszEncPasswd = crypt_r(pszPasswd, pw->pw_passwd, &data);
     if (strcmp(pszEncPasswd, pw->pw_passwd))
         return VERR_PERMISSION_DENIED;
 
