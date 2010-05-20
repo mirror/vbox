d885816761f04beddebe79872065347b793e6723
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2010-05-20 15:06:55 +0000

    Compile fix
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@29702 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/VMM/VMMAll/PGMAllPool.cpp
+++ src/VBox/VMM/VMMAll/PGMAllPool.cpp
@@ -4035,7 +4035,7 @@ static void pgmPoolTracDerefGCPhys(PPGMPOOL pPool, PPGMPOOLPAGE pPage, RTHCPHYS 
  */
 void pgmPoolTracDerefGCPhysHint(PPGMPOOL pPool, PPGMPOOLPAGE pPage, RTHCPHYS HCPhys, RTGCPHYS GCPhysHint, uint16_t iPte)
 {
-    RTHCPHYS HCPhysExpected = 0xDEADBEEFDEADBEEF;
+    RTHCPHYS HCPhysExpected = 0xDEADBEEFDEADBEEFULL;
 
     Log4(("pgmPoolTracDerefGCPhysHint %RHp %RGp\n", HCPhys, GCPhysHint));
 
