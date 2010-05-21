f5e8e506b7afcbb455577c93f64a8753e41b8049
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2010-05-21 11:56:03 +0000

    FE/Qt4: fix burns (pageIds >= Qt 4.5)
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@29723 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Frontends/VirtualBox/src/extensions/QIWizard.cpp
+++ src/VBox/Frontends/VirtualBox/src/extensions/QIWizard.cpp
@@ -162,9 +162,9 @@ void QIWizard::resizeAccordingLabelWidth(int iLabelsWidth)
 
 void QIWizard::retranslateAllPages()
 {
-    QList<int> idList = pageIds();
-    for(int i=0; i < idList.size(); ++i)
-        static_cast<QIWizardPage*>(page(idList.at(i)))->retranslateUi();
+    QList<QIWizardPage*> pages = findChildren<QIWizardPage*>();
+    for(int i=0; i < pages.size(); ++i)
+        static_cast<QIWizardPage*>(pages.at((i)))->retranslateUi();
 }
 
 QIWizardPage::QIWizardPage()
