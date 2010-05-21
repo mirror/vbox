dbfbf3031dfc0c302e0fe1fa7f5c4940524d859d
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2010-05-21 08:45:15 +0000

    Typo.
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@29718 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Runtime/r3/win/process-win.cpp
+++ src/VBox/Runtime/r3/win/process-win.cpp
@@ -322,7 +322,7 @@ static int rtProcCreateAsUserHlp(PRTUTF16 pwszUser, PRTUTF16 pwszPassword, PRTUT
          * - If valid get the correct VBoxTray instance corresponding to that
          *   user. This of course is only possible if that user is logged in (over
          *   physical console or terminal services).
-         * - We we found the user's VBoxTray app, use and modify the token to
+         * - If we found the user's VBoxTray app, use and modify the token to
          *   use it in order to allow the newly started process acess the user's
          *   desktop. If there's no VBoxTray app we cannot display the started
          *   process (but run it without UI).
