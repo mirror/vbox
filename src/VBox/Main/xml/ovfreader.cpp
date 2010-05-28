2bc130a250a2d5487bc2b566b470e6a43764ee3e
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2010-05-28 19:17:22 +0000

    OVF: fix broken OVF 0.9 import
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@29876 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Main/xml/ovfreader.cpp
+++ src/VBox/Main/xml/ovfreader.cpp
@@ -85,7 +85,9 @@ void OVFReader::LoopThruSections(const xml::ElementNode *pReferencesElem,
         const char *pcszElemName = pElem->getName();
         const char *pcszTypeAttr = "";
         const xml::AttributeNode *pTypeAttr;
-        if ((pTypeAttr = pElem->findAttribute("type")))
+        if (    ((pTypeAttr = pElem->findAttribute("xsi:type")))
+             || ((pTypeAttr = pElem->findAttribute("type")))
+           )
             pcszTypeAttr = pTypeAttr->getValue();
 
         if (    (!strcmp(pcszElemName, "DiskSection"))
@@ -96,7 +98,7 @@ void OVFReader::LoopThruSections(const xml::ElementNode *pReferencesElem,
         {
             HandleDiskSection(pReferencesElem, pElem);
         }
-       else if (    (!strcmp(pcszElemName, "NetworkSection"))
+        else if (    (!strcmp(pcszElemName, "NetworkSection"))
                   || (    (!strcmp(pcszElemName, "Section"))
                        && (!strcmp(pcszTypeAttr, "ovf:NetworkSection_Type"))
                      )
