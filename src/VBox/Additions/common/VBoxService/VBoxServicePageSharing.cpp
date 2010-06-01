d61b3790915d41f77e7066ef5478f90cd57a4b75
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2010-06-01 15:51:14 +0000

    Compile fix
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@29962 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Additions/common/VBoxService/VBoxServicePageSharing.cpp
+++ src/VBox/Additions/common/VBoxService/VBoxServicePageSharing.cpp
@@ -382,7 +382,7 @@ void VBoxServicePageSharingInspectGuest()
         for (unsigned i = 0; i < pSystemModules->NumberOfModules; i++)
         {
             /* User-mode modules seem to have no flags set; skip them as we detected them above. */
-            if (pSystemModules->Modules[i].flags == 0)
+            if (pSystemModules->Modules[i].Flags == 0)
                 continue;
 
             char *pszDot = strrchr(pSystemModules->Modules[i].FullPathName, '.');
@@ -435,7 +435,7 @@ void VBoxServicePageSharingInspectGuest()
                     VBoxServiceVerbose(3, "\n\n   KERNEL  MODULE NAME:     %s",     pModule->Info.szModule );
                     VBoxServiceVerbose(3, "\n     executable     = %s",             pModule->Info.szExePath );
                     VBoxServiceVerbose(3, "\n     base address   = 0x%08X", (DWORD) pModule->Info.modBaseAddr );
-                    VBoxServiceVerbose(3, "\n     flags          = 0x%08X",         pSystemModules->Modules[i].flags);
+                    VBoxServiceVerbose(3, "\n     flags          = 0x%08X",         pSystemModules->Modules[i].Flags);
                     VBoxServiceVerbose(3, "\n     base size      = %d",             pModule->Info.modBaseSize );
 
                     pRec = &pModule->Core;
