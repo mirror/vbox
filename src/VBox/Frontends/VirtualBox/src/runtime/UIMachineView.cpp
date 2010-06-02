e1746e95f2375169634f8af239ae56e417f2a655
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2010-06-02 12:45:07 +0000

    FE/Qt: 5003: Activate VM window on mouse-hovering (addition to r62276 for x11).
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@29988 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Frontends/VirtualBox/src/runtime/UIMachineView.cpp
+++ src/VBox/Frontends/VirtualBox/src/runtime/UIMachineView.cpp
@@ -1081,7 +1081,15 @@ bool UIMachineView::eventFilter(QObject *pWatched, QEvent *pEvent)
 
                 /* Check if we should activate window under cursor: */
                 if (machineWindowWrapper()->machineWindow() != QApplication::activeWindow())
+                {
+                    /* Activating hovered machine window: */
                     machineWindowWrapper()->machineWindow()->activateWindow();
+#ifdef Q_WS_X11
+                    /* On X11 its not enough to just activate window if you
+                     * want to raise it also, so we will make it separately: */
+                    machineWindowWrapper()->machineWindow()->raise();
+#endif /* Q_WS_X11 */
+                }
 
                 /* This event should be also processed using next 'case': */
             }
