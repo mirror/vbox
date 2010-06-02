660317e2085123991a85ab54b25d06363df89c52
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2010-06-02 11:41:49 +0000

    Fronrends/VBoxManage: automatically resume VM if savestate failed
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@29979 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Frontends/VBoxManage/VBoxManageControlVM.cpp
+++ src/VBox/Frontends/VBoxManage/VBoxManageControlVM.cpp
@@ -162,7 +162,12 @@ int handleControlVM(HandlerArg *a)
             CHECK_ERROR_BREAK(console, Pause());
 
             ComPtr<IProgress> progress;
-            CHECK_ERROR_BREAK(console, SaveState(progress.asOutParam()));
+            CHECK_ERROR(console, SaveState(progress.asOutParam()));
+            if (FAILED(rc))
+            {
+                console->Resume();
+                break;
+            }
 
             rc = showProgress(progress);
             if (FAILED(rc))
@@ -176,6 +181,7 @@ int handleControlVM(HandlerArg *a)
                 {
                     RTPrintf("Error: failed to save machine state. No error message available!\n");
                 }
+                console->Resume();
             }
         }
         else if (!strcmp(a->argv[1], "acpipowerbutton"))
