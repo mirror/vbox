f29d6e87ac327c6931039efe142bcd2d4157f62e
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2010-06-03 14:51:57 +0000

    FE/Qt: finally prevent a Window resize event during normal startup
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@30014 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Frontends/VirtualBox/src/runtime/normal/UIMachineWindowNormal.cpp
+++ src/VBox/Frontends/VirtualBox/src/runtime/normal/UIMachineWindowNormal.cpp
@@ -540,8 +540,20 @@ void UIMachineWindowNormal::loadWindowSettings()
 
         if (ok /* if previous parameters were read correctly */)
         {
-            m_normalGeometry = QRect(x, y, w, h);
-            setGeometry(m_normalGeometry);
+            if (machine.GetState() == KMachineState_Saved)
+            {
+                /* restore from a saved state: restore window size and position */
+                m_normalGeometry = QRect(x, y, w, h);
+                setGeometry(m_normalGeometry);
+            }
+            else
+            {
+                /* not restored from a saved state: restore only the last position */
+                move(x, y);
+                if (machineView())
+                    machineView()->normalizeGeometry(false /* adjust position? */);
+
+            }
 
             /* Maximize if needed: */
             if (max)
