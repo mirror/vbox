ee9dc8d895885ced78f968a5c5a804970883f47d
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2010-06-07 13:58:35 +0000

    VMMGuruMeditation.cpp: Enable ring-0 callstack everywhere. (untested)
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@30073 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/VMM/VMMGuruMeditation.cpp
+++ src/VBox/VMM/VMMGuruMeditation.cpp
@@ -362,15 +362,14 @@ VMMR3DECL(void) VMMR3FatalDump(PVM pVM, PVMCPU pVCpu, int rcErr)
 # endif
 #endif
 
-#if HC_ARCH_BITS == 32
                     /* Callstack. */
                     DBGFADDRESS pc;
                     pc.fFlags    = DBGFADDRESS_FLAGS_RING0 | DBGFADDRESS_FLAGS_VALID;
-# if HC_ARCH_BITS == 64
+#if HC_ARCH_BITS == 64
                     pc.FlatPtr   = pc.off = pVCpu->vmm.s.CallRing3JmpBufR0.rip;
-# else
+#else
                     pc.FlatPtr   = pc.off = pVCpu->vmm.s.CallRing3JmpBufR0.eip;
-# endif
+#endif
                     pc.Sel       = DBGF_SEL_FLAT;
 
                     DBGFADDRESS ebp;
@@ -424,7 +423,6 @@ VMMR3DECL(void) VMMR3FatalDump(PVM pVM, PVMCPU pVCpu, int rcErr)
                         }
                         DBGFR3StackWalkEnd(pFirstFrame);
                     }
-#endif /* defined(RT_OS_WINDOWS) && HC_ARCH_BITS == 32 */
 
                     /* raw stack */
                     pHlp->pfnPrintf(pHlp,
