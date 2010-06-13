4bf1b8b370b4b0cc559da1428a3b674284e7ceb7
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2010-06-13 20:59:43 +0000

    Linux shared folders: coding style
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@30175 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Additions/linux/sharedfolders/vfsmod.h
+++ src/VBox/Additions/linux/sharedfolders/vfsmod.h
@@ -29,51 +29,57 @@
 #include "VBoxCalls.h"
 #include "vbsfmount.h"
 
+#define DIR_BUFFER_SIZE (16*_1K)
+
 /* per-shared folder information */
-struct sf_glob_info {
-        VBSFMAP map;
-        struct nls_table *nls;
-        int ttl;
-        int uid;
-        int gid;
-        int dmode;
-        int fmode;
-        int dmask;
-        int fmask;
+struct sf_glob_info
+{
+    VBSFMAP map;
+    struct nls_table *nls;
+    int ttl;
+    int uid;
+    int gid;
+    int dmode;
+    int fmode;
+    int dmask;
+    int fmask;
 #if LINUX_VERSION_CODE >= KERNEL_VERSION(2, 6, 0)
-        struct backing_dev_info bdi;
+    struct backing_dev_info bdi;
 #endif
 };
 
 /* per-inode information */
-struct sf_inode_info {
-        /* which file */
-        SHFLSTRING *path;
-        /* some information was changed, update data on next revalidate */
-        int force_restat;
-        /* directory content changed, update the whole directory on next sf_getdent */
-        int force_reread;
-        /* file structure, only valid between open() and release() */
-        struct file *file;
-        /* handle valid if a file was created with sf_create_aux until it will
-         * be opened with sf_reg_open() */
-        SHFLHANDLE handle;
+struct sf_inode_info
+{
+    /* which file */
+    SHFLSTRING *path;
+    /* some information was changed, update data on next revalidate */
+    int force_restat;
+    /* directory content changed, update the whole directory on next sf_getdent */
+    int force_reread;
+    /* file structure, only valid between open() and release() */
+    struct file *file;
+    /* handle valid if a file was created with sf_create_aux until it will
+     * be opened with sf_reg_open() */
+    SHFLHANDLE handle;
 };
 
 struct sf_dir_info {
         struct list_head info_list;
 };
 
-struct sf_dir_buf {
-        size_t nb_entries;
-        size_t free_bytes;
-        size_t used_bytes;
-        void *buf;
-        struct list_head head;
+struct sf_dir_buf
+{
+    size_t cEntries;
+    size_t cbFree;
+    size_t cbUsed;
+    void   *buf;
+    struct list_head head;
 };
 
-struct sf_reg_info {
-        SHFLHANDLE handle;
+struct sf_reg_info
+{
+    SHFLHANDLE handle;
 };
 
 /* globals */
@@ -89,26 +95,26 @@ extern struct address_space_operations sf_reg_aops;
 
 extern void sf_init_inode(struct sf_glob_info *sf_g, struct inode *inode,
                           RTFSOBJINFO *info);
-extern int sf_stat(const char *caller, struct sf_glob_info *sf_g,
-                   SHFLSTRING *path, RTFSOBJINFO *result, int ok_to_fail);
-extern int sf_inode_revalidate(struct dentry *dentry);
+extern int  sf_stat(const char *caller, struct sf_glob_info *sf_g,
+                    SHFLSTRING *path, RTFSOBJINFO *result, int ok_to_fail);
+extern int  sf_inode_revalidate(struct dentry *dentry);
 #if LINUX_VERSION_CODE >= KERNEL_VERSION(2, 6, 0)
-extern int sf_getattr(struct vfsmount *mnt, struct dentry *dentry,
-                      struct kstat *kstat);
-extern int sf_setattr(struct dentry *dentry, struct iattr *iattr);
+extern int  sf_getattr(struct vfsmount *mnt, struct dentry *dentry,
+                       struct kstat *kstat);
+extern int  sf_setattr(struct dentry *dentry, struct iattr *iattr);
 #endif
-extern int sf_path_from_dentry(const char *caller, struct sf_glob_info *sf_g,
-                               struct sf_inode_info *sf_i, struct dentry *dentry,
-                               SHFLSTRING **result);
-extern int sf_nlscpy(struct sf_glob_info *sf_g,
-                     char *name, size_t name_bound_len,
-                     const unsigned char *utf8_name, size_t utf8_len);
+extern int  sf_path_from_dentry(const char *caller, struct sf_glob_info *sf_g,
+                                struct sf_inode_info *sf_i, struct dentry *dentry,
+                                SHFLSTRING **result);
+extern int  sf_nlscpy(struct sf_glob_info *sf_g,
+                      char *name, size_t name_bound_len,
+                      const unsigned char *utf8_name, size_t utf8_len);
 extern void sf_dir_info_free(struct sf_dir_info *p);
 extern void sf_dir_info_empty(struct sf_dir_info *p);
 extern struct sf_dir_info *sf_dir_info_alloc(void);
-extern int sf_dir_read_all(struct sf_glob_info *sf_g, struct sf_inode_info *sf_i,
-                           struct sf_dir_info *sf_d, SHFLHANDLE handle);
-extern int sf_init_backing_dev(struct sf_glob_info *sf_g, const char *name);
+extern int  sf_dir_read_all(struct sf_glob_info *sf_g, struct sf_inode_info *sf_i,
+                            struct sf_dir_info *sf_d, SHFLHANDLE handle);
+extern int  sf_init_backing_dev(struct sf_glob_info *sf_g, const char *name);
 extern void sf_done_backing_dev(struct sf_glob_info *sf_g);
 
 #if LINUX_VERSION_CODE < KERNEL_VERSION(2, 6, 0)
