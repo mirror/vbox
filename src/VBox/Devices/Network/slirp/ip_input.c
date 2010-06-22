38b21bbc2a74af2cf42472112cff2c912026900a
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2010-06-22 02:39:23 +0000

    NAT: Don't m_free mbuf just after send, it's responsible of recv threads to release mbuf.
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@30350 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Devices/Network/slirp/ip_input.c
+++ src/VBox/Devices/Network/slirp/ip_input.c
@@ -120,7 +120,7 @@ ip_input(PNATState pData, struct mbuf *m)
     {
         int rc;
         STAM_PROFILE_START(&pData->StatALIAS_input, b);
-        rc = LibAliasIn(select_alias(pData, m), mtod(m, char *), m->m_len);
+        rc = LibAliasIn(select_alias(pData, m), mtod(m, char *), m_length(m, NULL));
         STAM_PROFILE_STOP(&pData->StatALIAS_input, b);
         Log2(("NAT: LibAlias return %d\n", rc));
         if (m->m_len != RT_N2H_U16(ip->ip_len))
@@ -195,7 +195,8 @@ ip_input(PNATState pData, struct mbuf *m)
     if (ip->ip_ttl==0 || ip->ip_ttl == 1)
     {
         icmp_error(pData, m, ICMP_TIMXCEED, ICMP_TIMXCEED_INTRANS, 0, "ttl");
-        goto bad;
+        /* don't let this mbuf to be freed */
+        return;
     }
 
     ip->ip_ttl--;
