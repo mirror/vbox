2271ae8d66e4a497b1294ef70889f87cc837fc7b
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2010-06-23 17:30:40 +0000

    NAT: missed ''m_freem'' calls right after ''icmp_error'' (see xTracker/#5060).
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@30402 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Devices/Network/slirp/socket.c
+++ src/VBox/Devices/Network/slirp/socket.c
@@ -793,6 +793,7 @@ sorecvfrom(PNATState pData, struct socket *so)
 
             Log2((" rx error, tx icmp ICMP_UNREACH:%i\n", code));
             icmp_error(pData, so->so_m, ICMP_UNREACH, code, 0, strerror(errno));
+            m_freem(pData, so->so_m);
             so->so_m = NULL;
         }
         else
@@ -1341,6 +1342,7 @@ sorecvfrom_icmp_win(PNATState pData, struct socket *so)
             case IP_DEST_PORT_UNREACHABLE:
                 code = (code != ~0 ? code : ICMP_UNREACH_PORT);
                 icmp_error(pData, so->so_m, ICMP_UNREACH, code, 0, "Error occurred!!!");
+                m_freem(pData, so->so_m);
                 so->so_m = NULL;
                 break;
             case IP_SUCCESS: /* echo replied */
@@ -1460,6 +1462,7 @@ static void sorecvfrom_icmp_unix(PNATState pData, struct socket *so)
         LogRel((" udp icmp rx errno = %d-%s\n",
                     errno, strerror(errno)));
         icmp_error(pData, so->so_m, ICMP_UNREACH, code, 0, strerror(errno));
+        m_freem(pData, so->so_m);
         so->so_m = NULL;
         Log(("sorecvfrom_icmp_unix: 1 - step can't read IP datagramm \n"));
         return;
