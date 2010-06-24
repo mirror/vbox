2a37a7d506b4bb148a5bf194b4629ed7d7a7ddd9
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2010-06-24 23:31:36 +0000

    vboxadd.sh: addressed bashism (==) introduced in r62583.
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@30443 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Additions/linux/installer/vboxadd.sh
+++ src/VBox/Additions/linux/installer/vboxadd.sh
@@ -173,7 +173,7 @@ test_sane_kernel_dir()
     KERN_DIR="/lib/modules/$KERN_VER/build"
     if [ -d "$KERN_DIR" ]; then
         KERN_REL=`make -sC $KERN_DIR --no-print-directory kernelrelease || true`
-        if [ "x$KERN_REL" == "x$KERN_VER" ]; then
+        if [ "x$KERN_REL" = "x$KERN_VER" ]; then
             return 0
         fi
     fi
