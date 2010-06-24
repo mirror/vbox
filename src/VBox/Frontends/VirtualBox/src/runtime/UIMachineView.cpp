b39b52685f8793ba5023f77ae0148a81fa6a3470
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2010-06-24 04:11:27 +0000

    FE/Qt: First try to fix mac build for r63034.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@30409 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Frontends/VirtualBox/src/runtime/UIMachineView.cpp
+++ src/VBox/Frontends/VirtualBox/src/runtime/UIMachineView.cpp
@@ -1779,7 +1779,7 @@ void UIMachineView::darwinGrabKeyboardEvents(bool fGrab)
     {
         /* Disable mouse and keyboard event compression/delaying to make sure we *really* get all of the events. */
         ::CGSetLocalEventsSuppressionInterval(0.0);
-        setMouseCoalescingEnabled(false);
+        machineLogic()->mouseHandler()->setMouseCoalescingEnabled(false);
 
         /* Register the event callback/hook and grab the keyboard. */
         UICocoaApplication::instance()->registerForNativeEvents(RT_BIT_32(10) | RT_BIT_32(11) | RT_BIT_32(12) /* NSKeyDown  | NSKeyUp | | NSFlagsChanged */,
