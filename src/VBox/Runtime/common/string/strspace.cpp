0b398edfe4539aaedb3c791d1bca9a352f52e7c8
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2010-07-05 12:53:59 +0000

    strspace.cpp: Fixed bug in sdbm() resulting in wrong cchString values (+1). Return true when inserting into the collision list, only return false for identical strings.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@30611 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Runtime/common/string/strspace.cpp
+++ src/VBox/Runtime/common/string/strspace.cpp
@@ -83,7 +83,7 @@
    experimenting with different constants, and turns out to be a prime.
    this is one of the algorithms used in berkeley db (see sleepycat) and
    elsewhere. */
-inline uint32_t sdbm(const char *str, size_t *pcch)
+DECLINLINE(uint32_t) sdbm(const char *str, size_t *pcch)
 {
     uint8_t *pu8 = (uint8_t *)str;
     uint32_t hash = 0;
@@ -92,7 +92,7 @@ inline uint32_t sdbm(const char *str, size_t *pcch)
     while ((c = *pu8++))
         hash = c + (hash << 6) + (hash << 16) - hash;
 
-    *pcch = (uintptr_t)pu8 - (uintptr_t)str;
+    *pcch = (uintptr_t)pu8 - (uintptr_t)str - 1;
     return hash;
 }
 
@@ -119,7 +119,7 @@ RTDECL(bool) RTStrSpaceInsert(PRTSTRSPACE pStrSpace, PRTSTRSPACECORE pStr)
             return false;
     pStr->pList = pMatch->pList;
     pMatch->pList = pStr;
-    return false;
+    return true;
 }
 RT_EXPORT_SYMBOL(RTStrSpaceInsert);
 
