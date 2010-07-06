a60d8912e4c754ab59a6de29b9cb3c5c59ee83ea
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2010-07-06 14:57:01 +0000

    FE/Qt: New running VM core: little fix for specific keyboard-handlers.
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@30675 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Frontends/VirtualBox/src/runtime/normal/UIKeyboardHandlerNormal.cpp
+++ src/VBox/Frontends/VirtualBox/src/runtime/normal/UIKeyboardHandlerNormal.cpp
@@ -57,32 +57,29 @@ bool UIKeyboardHandlerNormal::eventFilter(QObject *pWatchedObject, QEvent *pEven
                 /* Get key-event: */
                 QKeyEvent *pKeyEvent = static_cast<QKeyEvent*>(pEvent);
                 /* Process Host+Home as menu-bar activator: */
-                if (isHostKeyPressed() && pEvent->type() == QEvent::KeyPress)
+                if (isHostKeyPressed() && pKeyEvent->key() == Qt::Key_Home)
                 {
-                    if (pKeyEvent->key() == Qt::Key_Home)
+                    /* Trying to get menu-bar: */
+                    QMenuBar *pMenuBar = qobject_cast<QMainWindow*>(m_windows[uScreenId]->machineWindow())->menuBar();
+                    /* If menu-bar is present and have actions: */
+                    if (pMenuBar && !pMenuBar->actions().isEmpty())
                     {
-                        /* Trying to get menu-bar: */
-                        QMenuBar *pMenuBar = qobject_cast<QMainWindow*>(m_windows[uScreenId]->machineWindow())->menuBar();
-                        /* If menu-bar is present and have actions: */
-                        if (pMenuBar && !pMenuBar->actions().isEmpty())
+                        /* If 'active' action is NOT chosen: */
+                        if (!pMenuBar->activeAction())
+                            /* Set first menu-bar action as 'active': */
+                            pMenuBar->setActiveAction(pMenuBar->actions()[0]);
+                        /* If 'active' action is chosen: */
+                        if (pMenuBar->activeAction())
                         {
-                            /* If 'active' action is NOT chosen: */
-                            if (!pMenuBar->activeAction())
-                                /* Set first menu-bar action as 'active': */
-                                pMenuBar->setActiveAction(pMenuBar->actions()[0]);
-                            /* If 'active' action is chosen: */
-                            if (pMenuBar->activeAction())
-                            {
-                                /* Activate 'active' menu-bar action: */
-                                pMenuBar->activeAction()->activate(QAction::Trigger);
+                            /* Activate 'active' menu-bar action: */
+                            pMenuBar->activeAction()->activate(QAction::Trigger);
 #ifdef Q_WS_WIN
-                                /* Windows host needs separate 'focus set'
-                                 * to let menubar operate while popped up: */
-                                pMenuBar->setFocus();
+                            /* Windows host needs separate 'focus set'
+                             * to let menubar operate while popped up: */
+                            pMenuBar->setFocus();
 #endif /* Q_WS_WIN */
-                                /* Filter-out this event: */
-                                return true;
-                            }
+                            /* Filter-out this event: */
+                            return true;
                         }
                     }
                 }
