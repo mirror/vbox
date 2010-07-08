062fd1af1b92feadee0e5bfaabe61949e8771656
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2010-07-08 11:50:58 +0000

    VBoxManage: Show additional guest (additions) information.
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@30734 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Frontends/VBoxManage/VBoxManageInfo.cpp
+++ src/VBox/Frontends/VBoxManage/VBoxManageInfo.cpp
@@ -1905,10 +1905,49 @@ HRESULT showVMInfo (ComPtr<IVirtualBox> virtualBox,
         }
     }
 
-    ULONG guestVal;
+
     if (details != VMINFO_MACHINEREADABLE)
         RTPrintf("Guest:\n\n");
 
+    ComPtr<IGuest> guest;
+    rc = console->COMGETTER(Guest)(guest.asOutParam());
+    if (RT_SUCCESS(rc))
+    {
+        Bstr guestString;
+        rc = guest->COMGETTER(OSTypeId)(guestString.asOutParam());
+        if (   SUCCEEDED(rc)
+            && !guestString.isEmpty())
+        {
+            if (details == VMINFO_MACHINEREADABLE)
+                RTPrintf("GuestOSType=\"%lS\"\n", guestString.raw());
+            else
+                RTPrintf("OS type:                             %lS\n", guestString.raw());
+        }
+
+        BOOL guestFlag;
+        rc = guest->COMGETTER(AdditionsActive)(&guestFlag);
+        if (SUCCEEDED(rc))
+        {
+            if (details == VMINFO_MACHINEREADABLE)
+                RTPrintf("GuestAdditionsActive=%s\n", guestFlag ? "on" : "off");
+            else
+                RTPrintf("Additions active:                    %s\n", guestFlag ? "yes" : "no");
+        }
+
+        if (details == VMINFO_FULL)
+        {
+            rc = guest->COMGETTER(AdditionsVersion)(guestString.asOutParam());
+            if (SUCCEEDED(rc))
+            {
+                if (details == VMINFO_MACHINEREADABLE)
+                    RTPrintf("GuestAdditionsAPIVersion=\"%lS\"\n", guestString.raw());
+                else
+                    RTPrintf("Additions API version:               %lS\n\n", guestString.raw());
+            }
+        }
+    }
+
+    ULONG guestVal;
     rc = machine->COMGETTER(MemoryBalloonSize)(&guestVal);
     if (SUCCEEDED(rc))
     {
