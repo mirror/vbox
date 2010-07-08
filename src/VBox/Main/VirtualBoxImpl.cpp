ee56a5f09d0c12af666a503e7731995b05907e0b
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2010-07-08 13:29:21 +0000

    windows build fix
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@30742 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Main/VirtualBoxImpl.cpp
+++ src/VBox/Main/VirtualBoxImpl.cpp
@@ -2377,7 +2377,7 @@ HRESULT VirtualBox::startSVCHelperClient(bool aPrivileged,
                              0, RTTHREADTYPE_MAIN_WORKER,
                              RTTHREADFLAGS_WAITABLE, "SVCHelper");
     if (RT_FAILURE(vrc))
-        return setError(E_FAIL; "Could not create SVCHelper thread (%Rrc)", vrc);
+        return setError(E_FAIL, "Could not create SVCHelper thread (%Rrc)", vrc);
 
     /* d is now owned by SVCHelperClientThread(), so release it */
     d.release();
