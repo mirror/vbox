997d0469261e0b76b262e3732a559ac7fc0cad1c
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2010-07-14 11:51:26 +0000

    Sigh
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@30821 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/VMM/PGMPhys.cpp
+++ src/VBox/VMM/PGMPhys.cpp
@@ -3280,7 +3280,7 @@ static DECLCALLBACK(int) pgmR3PhysChunkUnmapCandidateCallback(PAVLLU32NODECORE p
             /*
              * Check that it's not in any of the TLBs.
              */
-            PVM pVM = (pvUser)
+            PVM pVM = (PVM)pvUser;
             for (unsigned i = 0; i < RT_ELEMENTS(pVM->pgm.s.ChunkR3Map.Tlb.aEntries); i++)
                 if (pVM->pgm.s.ChunkR3Map.Tlb.aEntries[i].pChunk == pChunk)
                 {
