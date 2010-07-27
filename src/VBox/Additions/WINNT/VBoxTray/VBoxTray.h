db5b0e15fb7807d58c725b56b10b16103c3d2b8d
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2010-07-27 15:53:05 +0000

    VBoxTray: move display WDDM/XPDM abstraction API to a separate file
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@31145 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Additions/WINNT/VBoxTray/VBoxTray.h
+++ src/VBox/Additions/WINNT/VBoxTray/VBoxTray.h
@@ -32,9 +32,8 @@
 #include <VBox/VBoxGuest.h> /** @todo use the VbglR3 interface! */
 #include <VBox/VBoxGuestLib.h>
 #include <VBoxDisplay.h>
-#ifdef VBOXWDDM
-# include <d3dkmthk.h>
-#endif
+
+#include "VBoxDispIf.h"
 
 #define WM_VBOX_RESTORED                WM_APP + 1
 #define WM_VBOX_CHECK_VRDP              WM_APP + 2
@@ -43,60 +42,6 @@
 
 #define ID_TRAYICON                     2000
 
-typedef enum
-{
-    VBOXDISPIF_MODE_UNKNOWN  = 0,
-    VBOXDISPIF_MODE_XPDM_NT4 = 1,
-    VBOXDISPIF_MODE_XPDM
-#ifdef VBOXWDDM
-    , VBOXDISPIF_MODE_WDDM
-#endif
-} VBOXDISPIF_MODE;
-/* display driver interface abstraction for XPDM & WDDM
- * with WDDM we can not use ExtEscape to communicate with our driver
- * because we do not have XPDM display driver any more, i.e. escape requests are handled by cdd
- * that knows nothing about us
- * NOTE: DispIf makes no checks whether the display driver is actually a VBox driver,
- * it just switches between using different backend OS API based on the VBoxDispIfSwitchMode call
- * It's caller's responsibility to initiate it to work in the correct mode */
-typedef struct VBOXDISPIF
-{
-    VBOXDISPIF_MODE enmMode;
-    /* with WDDM the approach is to call into WDDM miniport driver via PFND3DKMT API provided by the GDI,
-     * The PFND3DKMT is supposed to be used by the OpenGL ICD according to MSDN, so this approach is a bit hacky */
-    union
-    {
-        struct
-        {
-            LONG (WINAPI * pfnChangeDisplaySettingsEx)(LPCSTR lpszDeviceName, LPDEVMODE lpDevMode, HWND hwnd, DWORD dwflags, LPVOID lParam);
-        } xpdm;
-#ifdef VBOXWDDM
-        struct
-        {
-            /* open adapter */
-            PFND3DKMT_OPENADAPTERFROMHDC pfnD3DKMTOpenAdapterFromHdc;
-            PFND3DKMT_OPENADAPTERFROMGDIDISPLAYNAME pfnD3DKMTOpenAdapterFromGdiDisplayName;
-            /* close adapter */
-            PFND3DKMT_CLOSEADAPTER pfnD3DKMTCloseAdapter;
-            /* escape */
-            PFND3DKMT_ESCAPE pfnD3DKMTEscape;
-            /* auto resize support */
-            PFND3DKMT_INVALIDATEACTIVEVIDPN pfnD3DKMTInvalidateActiveVidPn;
-        } wddm;
-#endif
-    } modeData;
-} VBOXDISPIF, *PVBOXDISPIF;
-typedef const struct VBOXDISPIF *PCVBOXDISPIF;
-
-/* initializes the DispIf
- * Initially the DispIf is configured to work in XPDM mode
- * call VBoxDispIfSwitchMode to switch the mode to WDDM */
-DWORD VBoxDispIfInit(PVBOXDISPIF pIf);
-DWORD VBoxDispIfSwitchMode(PVBOXDISPIF pIf, VBOXDISPIF_MODE enmMode, VBOXDISPIF_MODE *penmOldMode);
-DECLINLINE(VBOXDISPIF_MODE) VBoxDispGetMode(PVBOXDISPIF pIf) { return pIf->enmMode; }
-DWORD VBoxDispIfTerm(PVBOXDISPIF pIf);
-DWORD VBoxDispIfEscape(PCVBOXDISPIF const pIf, PVBOXDISPIFESCAPE pEscape, int cbData);
-DWORD VBoxDispIfResize(PCVBOXDISPIF const pIf, ULONG Id, DWORD Width, DWORD Height, DWORD BitsPerPixel);
 
 /* The environment information for services. */
 typedef struct _VBOXSERVICEENV
