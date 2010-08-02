5c874bf5e8f631a4b35f7ffc1542a9ca2826222e
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2010-08-02 09:11:22 +0000

    RTNETCSUMTYPE -> PDMNETCSUMTYPE. Assert on invalid parameters.
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@31272 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/NetworkServices/NetLib/VBoxNetUDP.cpp
+++ src/VBox/NetworkServices/NetLib/VBoxNetUDP.cpp
@@ -122,7 +122,7 @@ void *VBoxNetUDPMatch(PINTNETBUF pBuf, unsigned uDstPort, PCRTMAC pDstMac, uint3
      * are set correctly (they are usually set to 0).
      */
     if (pGso)
-        PDMNetGsoPrepForDirectUse(pGso, (void *)pvFrame, cbFrame, RTNETCSUMTYPE_NONE);
+        PDMNetGsoPrepForDirectUse(pGso, (void *)pvFrame, cbFrame, PDMNETCSUMTYPE_NONE);
 
     /*
      * IP validation and matching.
