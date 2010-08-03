4a16946a9412d3ce5c78b2edfd2b4d7834932210
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2010-08-03 12:00:45 +0000

    FE/Qt4: show the nested paging setting when VT-x/AMD-V is enabled only
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@31330 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Frontends/VirtualBox/src/selector/UIVMDesktop.cpp
+++ src/VBox/Frontends/VirtualBox/src/selector/UIVMDesktop.cpp
@@ -505,15 +505,15 @@ void UIDetailsPagePrivate::sltUpdateSystem()
             QStringList accel;
             if (m_vbox.GetHost().GetProcessorFeature(KProcessorFeature_HWVirtEx))
             {
-
                 /* VT-x/AMD-V */
                 if (m_machine.GetHWVirtExProperty(KHWVirtExPropertyType_Enabled))
+                {
                     accel << tr("VT-x/AMD-V", "details report");
 
-                /* Nested Paging */
-                if (m_machine.GetHWVirtExProperty(KHWVirtExPropertyType_NestedPaging))
-                    accel << tr("Nested Paging", "details report");
-
+                    /* Nested Paging (only when hw virt is enabled) */
+                    if (m_machine.GetHWVirtExProperty(KHWVirtExPropertyType_NestedPaging))
+                        accel << tr("Nested Paging", "details report");
+                }
             }
             /* PAE/NX */
             if (m_machine.GetCPUProperty(KCPUPropertyType_PAE))
