cd4a898610d05a7225dfeb6b76830f06dd8f800f
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2010-08-04 12:37:50 +0000

    SharedFolders/AutoMount: Added support for dynamic mount point prefix.
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@31355 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- include/VBox/VBoxGuestLib.h
+++ include/VBox/VBoxGuestLib.h
@@ -530,6 +530,7 @@ VBGLR3DECL(int)     VbglR3SharedFolderGetMappings(uint32_t u32ClientId, bool fAu
                                                   PVBGLR3SHAREDFOLDERMAPPING *ppaMappings, uint32_t *pcMappings);
 VBGLR3DECL(void)    VbglR3SharedFolderFreeMappings(PVBGLR3SHAREDFOLDERMAPPING paMappings);
 VBGLR3DECL(int)     VbglR3SharedFolderGetName(uint32_t  u32ClientId,uint32_t u32Root, char **ppszName);
+VBGLR3DECL(int)     VbglR3SharedFolderGetMountPrefix(char **ppszPrefix);
 /** @}  */
 # endif /* VBOX_WITH_SHARED_FOLDERS defined */
 
