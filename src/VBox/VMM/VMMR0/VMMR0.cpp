aca661c2046b847273ca11f575e72329ea8ebc6c
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2010-08-05 13:36:39 +0000

    VMMR0: Set VMCPU::hNativeThreadR0 in GVMMR0CreateVM and GVMMR0RegisterVCpu.
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@31407 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/VMM/VMMR0/VMMR0.cpp
+++ src/VBox/VMM/VMMR0/VMMR0.cpp
@@ -963,10 +963,7 @@ static int vmmR0EntryExWorker(PVM pVM, VMCPUID idCpu, VMMR0OPERATION enmOperatio
                 return VERR_INVALID_PARAMETER;
 
             PVMCPU pVCpu = &pVM->aCpus[idCpu];
-
-            /* Initialize the r0 native thread handle on the fly. */
-            if (pVCpu->hNativeThreadR0 == NIL_RTNATIVETHREAD) 
-                pVCpu->hNativeThreadR0 = RTThreadNativeSelf(); 
+            Assert(pVCpu->hNativeThreadR0 == RTThreadNativeSelf());
 
 # ifdef DEBUG_sandervl
             /* Make sure that log flushes can jump back to ring-3; annoying to get an incomplete log (this is risky though as the code doesn't take this into account). */
