ee405297c28a7a9db560e4806cd737446baac83a
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2010-08-10 16:53:48 +0000

    build fix
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@31548 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Frontends/VirtualBox/src/settings/vm/VBoxVMSettingsPortForwardingDlg.cpp
+++ src/VBox/Frontends/VirtualBox/src/settings/vm/VBoxVMSettingsPortForwardingDlg.cpp
@@ -165,7 +165,7 @@ public:
     PortEditor(QWidget *pParent = 0) : QSpinBox(pParent)
     {
         setFrame(false);
-        setRange(0, pow(2, 8 * sizeof(ushort)) - 1);
+        setRange(0, (1 << (8 * sizeof(ushort))) - 1);
     }
 
 private:
