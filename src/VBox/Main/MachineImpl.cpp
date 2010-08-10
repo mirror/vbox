75de6554502d7afac6c5564fd712ec3b68bf6ba8
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2010-08-10 16:39:05 +0000

    build fix
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@31546 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Main/MachineImpl.cpp
+++ src/VBox/Main/MachineImpl.cpp
@@ -6010,7 +6010,7 @@ bool Machine::checkForSpawnFailure()
      * error and finalize session spawning */
     rc = setError(E_FAIL,
                   tr("The virtual machine '%ls' has terminated unexpectedly during startup"),
-                  getName().raw());
+                  getName().c_str());
 #else
 
     /* PID not yet initialized, skip check. */
