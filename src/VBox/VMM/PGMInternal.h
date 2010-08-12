b3240f07d1ffde07525d686ef9a2420765292719
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2010-08-12 15:22:39 +0000

    Timer init changes
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@31606 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/VMM/PGMInternal.h
+++ src/VBox/VMM/PGMInternal.h
@@ -3140,10 +3140,11 @@ typedef struct PGM
     STAMCOUNTER                     StatLargePageRecheck;               /**< The number of times we rechecked a disabled large page.*/
     /** @} */
 
-#ifdef VBOX_STRICT
+#ifdef DEBUG_sandervl
     PTMTIMERR3                      pPhysWritesCountTimer;
+    uint32_t                        u32PhysWriteCountTimerInterval;
     bool                            fCountingPhysWrites;
-    uint8_t                         u8Alignment[7];
+    uint8_t                         u8Alignment[3];
 
     STAMCOUNTER                     StatR3FTPhysPageWrite;              /**< R3: The number of times a physical page was written to (FT stats) */
     STAMCOUNTER                     StatRZFTPhysPageWrite;              /**< RC/R0: The number of times a physical page was written to (FT stats) */
