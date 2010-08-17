f7701e99f0d4755a1db9b2033588b5128301dee2
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2010-08-17 13:43:49 +0000

    Terminology
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@31731 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Main/xml/Settings.cpp
+++ src/VBox/Main/xml/Settings.cpp
@@ -3112,11 +3112,11 @@ void MachineConfigFile::readMachine(const xml::ElementNode &elmMachine)
                 Utf8Str strFaultToleranceSate;
                 if (pelmMachineChild->getAttributeValue("state", strFaultToleranceSate))
                 {
-                    if (strFaultToleranceSate == "source")
-                        machineUserData.enmFaultToleranceState = FaultToleranceState_Source;
+                    if (strFaultToleranceSate == "master")
+                        machineUserData.enmFaultToleranceState = FaultToleranceState_Master;
                     else
-                    if (strFaultToleranceSate == "target")
-                        machineUserData.enmFaultToleranceState = FaultToleranceState_Target;
+                    if (strFaultToleranceSate == "clone")
+                        machineUserData.enmFaultToleranceState = FaultToleranceState_Clone;
                     else
                         machineUserData.enmFaultToleranceState = FaultToleranceState_Inactive;
                 }               
@@ -4041,11 +4041,11 @@ void MachineConfigFile::buildMachineXML(xml::ElementNode &elmMachine,
         case FaultToleranceState_Inactive:
             pelmFaultTolerance->setAttribute("state", "inactive");
             break;
-        case FaultToleranceState_Source:
-            pelmFaultTolerance->setAttribute("state", "source");
+        case FaultToleranceState_Master:
+            pelmFaultTolerance->setAttribute("state", "master");
             break;
-        case FaultToleranceState_Target:
-            pelmFaultTolerance->setAttribute("state", "target");
+        case FaultToleranceState_Clone:
+            pelmFaultTolerance->setAttribute("state", "clone");
             break;
         }
 
