b698b125aea777b5fe21e7b2bb9cefe2461a7b4b
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2010-08-18 12:11:09 +0000

    missing copyright hdr
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@31757 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Additions/WINNT/Graphics/Wine/vbox/VBoxWineEx.h
+++ src/VBox/Additions/WINNT/Graphics/Wine/vbox/VBoxWineEx.h
@@ -1,3 +1,17 @@
+/** @file
+ *
+ * VBox extension to Wine D3D
+ *
+ * Copyright (C) 2010 Oracle Corporation
+ *
+ * This file is part of VirtualBox Open Source Edition (OSE), as
+ * available from http://www.virtualbox.org. This file is free software;
+ * you can redistribute it and/or modify it under the terms of the GNU
+ * General Public License (GPL) as published by the Free Software
+ * Foundation, in version 2 as it comes in the "COPYING" file of the
+ * VirtualBox OSE distribution. VirtualBox OSE is distributed in the
+ * hope that it will be useful, but WITHOUT ANY WARRANTY of any kind.
+ */
 #ifndef ___VBoxWineEx_h__
 #define ___VBoxWineEx_h__
 
