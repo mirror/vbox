c111636120369e67cc1d5419724a6da4296a3af5
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2010-08-23 12:11:12 +0000

    fix OSE
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@31856 cfe28804-0f27-0410-a406-dd0f0b0b656f

---
+++ src/VBox/Installer/win/InstallHelper/VBoxCommon.cpp
@@ -0,0 +1,49 @@
+/* $Id: */
+/** @file
+* VBoxCommon - Misc helper routines for install helper.
+*/
+
+/*
+ * Copyright (C) 2006-2010 Oracle Corporation
+ *
+ * This file is part of VirtualBox Open Source Edition (OSE), as
+ * available from http://www.virtualbox.org. This file is free software;
+ * you can redistribute it and/or modify it under the terms of the GNU
+ * General Public License (GPL) as published by the Free Software
+ * Foundation, in version 2 as it comes in the "COPYING" file of the
+ * VirtualBox OSE distribution. VirtualBox OSE is distributed in the
+ * hope that it will be useful, but WITHOUT ANY WARRANTY of any kind.
+ */
+
+#include <windows.h>
+#include <tchar.h>
+#include <stdio.h>
+
+#include <msi.h>
+#include <msiquery.h>
+
+UINT VBoxGetProperty(MSIHANDLE a_hModule, TCHAR* a_pszName, TCHAR* a_pValue, DWORD a_dwSize)
+{
+    UINT uiRet = ERROR_SUCCESS;
+    DWORD dwBuffer = 0;
+
+    uiRet = MsiGetProperty(a_hModule, a_pszName, TEXT(""), &dwBuffer);
+    if (ERROR_MORE_DATA == uiRet)
+    {
+        ++dwBuffer;     /* On output does not include terminating null, so add 1. */
+
+        if (dwBuffer > a_dwSize)
+            return ERROR_MORE_DATA;
+
+        ZeroMemory(a_pValue, a_dwSize);
+        uiRet = MsiGetProperty(a_hModule, a_pszName, a_pValue, &dwBuffer);
+    }
+
+    return uiRet;
+}
+
+UINT VBoxSetProperty(MSIHANDLE a_hModule, TCHAR* a_pszName, TCHAR* a_pValue)
+{
+    return MsiSetProperty(a_hModule, a_pszName, a_pValue);
+}
+
---
+++ src/VBox/Installer/win/InstallHelper/VBoxCommon.h
@@ -0,0 +1,24 @@
+/* $Id: */
+/** @file
+* VBoxCommon - Misc helper routines for install helper.
+*/
+
+/*
+ * Copyright (C) 2006-2010 Oracle Corporation
+ *
+ * This file is part of VirtualBox Open Source Edition (OSE), as
+ * available from http://www.virtualbox.org. This file is free software;
+ * you can redistribute it and/or modify it under the terms of the GNU
+ * General Public License (GPL) as published by the Free Software
+ * Foundation, in version 2 as it comes in the "COPYING" file of the
+ * VirtualBox OSE distribution. VirtualBox OSE is distributed in the
+ * hope that it will be useful, but WITHOUT ANY WARRANTY of any kind.
+ */
+
+#ifndef __VBoxInstallHelper_Common_h__
+#define __VBoxInstallHelper_Common_h__
+
+UINT VBoxGetProperty(MSIHANDLE a_hModule, TCHAR* a_pszName, TCHAR* a_pValue, DWORD a_dwSize);
+UINT VBoxSetProperty(MSIHANDLE a_hModule, TCHAR* a_pszName, TCHAR* a_pValue);
+
+#endif /* __VBoxInstallHelper_Common_h__ */
