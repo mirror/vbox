0678bd9a6acef193c49f884075473f0bb6013d4b
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2010-08-24 07:22:23 +0000

    OSE header fixes
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@31885 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Main/win/samples/run.bat
+++ src/VBox/Main/win/samples/run.bat
@@ -1,13 +1,14 @@
 @echo off
-rem Copyright (C) 2010 Oracle Corporation
-rem
-rem This file is part of VirtualBox Open Source Edition (OSE), as
-rem available from http://www.virtualbox.org. This file is free software;
-rem you can redistribute it and/or modify it under the terms of the GNU
-rem General Public License (GPL) as published by the Free Software
-rem Foundation, in version 2 as it comes in the "COPYING" file of the
-rem VirtualBox OSE distribution. VirtualBox OSE is distributed in the
-rem hope that it will be useful, but WITHOUT ANY WARRANTY of any kind.
+REM  Copyright (C) 2010 Oracle Corporation
+REM
+REM  This file is part of VirtualBox Open Source Edition (OSE), as
+REM  available from http://www.virtualbox.org. This file is free software;
+REM  you can redistribute it and/or modify it under the terms of the GNU
+REM  General Public License (GPL) as published by the Free Software
+REM  Foundation, in version 2 as it comes in the "COPYING" file of the
+REM  VirtualBox OSE distribution. VirtualBox OSE is distributed in the
+REM  hope that it will be useful, but WITHOUT ANY WARRANTY of any kind.
+REM
 
 set JAVA_HOME=c:\jdk
 set JAVA=%JAVA_HOME%\bin\java
