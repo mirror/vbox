c205de9039d3576ef7c6980600954938dacbbfac
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2010-08-27 16:58:26 +0000

    crOpenGL: move define out of wrong ifdef
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@32060 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/GuestHost/OpenGL/include/chromium.h
+++ src/VBox/GuestHost/OpenGL/include/chromium.h
@@ -683,11 +683,10 @@ extern void APIENTRY glZPixCR(GLsizei width, GLsizei height, GLenum format,
 #define GL_REAL_VERSION    0x8B24
 #define GL_REAL_RENDERER   0x8B25
 #define GL_REAL_EXTENSIONS 0x8B26
+#endif
 
 /*Global resource ids sharing*/
 #define GL_SHARE_CONTEXT_RESOURCES_CR 0x8B27
-#endif
-
 
 /**********************************************************************/
 /*****                Chromium-specific API                       *****/
