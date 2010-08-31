1b9c9ca487e6de050fd505807767f04e88a5a518
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2010-08-31 14:47:09 +0000

    PCI work: more docs, exact device config
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@32152 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Devices/Bus/DevPciIch9.cpp
+++ src/VBox/Devices/Bus/DevPciIch9.cpp
@@ -78,11 +78,8 @@ typedef struct
 /**
  * PCI Globals - This is the host-to-pci bridge and the root bus.
  */
-typedef struct PCIGLOBALS
+typedef struct
 {
-    /** I/O APIC usage flag */
-    bool                fUseIoApic;
-
     /** R3 pointer to the device instance. */
     PPDMDEVINSR3        pDevInsR3;
     /** R0 pointer to the device instance. */
@@ -94,27 +91,209 @@ typedef struct PCIGLOBALS
     uint32_t            Alignment0;
 #endif
 
-    /** PCI bus which is attached to the host-to-PCI bridge. */
-    PCIBUS              PciBus;
+    /** PIC irq levels */
+    volatile uint32_t   uaPicPciIrqLevels[PCI_IRQ_PINS];
+    /** I/O APIC irq levels */
+    volatile uint32_t   uaPciApicIrqLevels[PCI_APIC_IRQ_PINS];
 
-} PCIGLOBALS;
-/** Pointer to per VM data. */
-typedef PCIGLOBALS *PPCIGLOBALS;
+    /** ACPI IRQ level */
+    uint32_t            uAcpiIrqLevel;
+    /** .. and number */
+    uint32_t            uAcpiIrq;
+
+    /** Config register. */
+    uint32_t            uConfigReg;
+
+    /** I/O APIC usage flag */
+    bool                fUseIoApic;
+
+
+    /** PCI bus which is attached to the host-to-PCI bridge. */
+    PCIBUS              aPciBus;
+
+} PCIGLOBALS, *PPCIGLOBALS;
+
 
 /*******************************************************************************
  *   Defined Constants And Macros                                              *
  *******************************************************************************/
+
+/** @def VBOX_ICH9PCI_SAVED_STATE_VERSION
+ * Saved state version of the ICH9 PCI bus device.
+ */
+#define VBOX_ICH9PCI_SAVED_STATE_VERSION 1
 
 /** Converts a bus instance pointer to a device instance pointer. */
 #define PCIBUS_2_DEVINS(pPciBus)        ((pPciBus)->CTX_SUFF(pDevIns))
 /** Converts a device instance pointer to a PCIGLOBALS pointer. */
 #define DEVINS_2_PCIGLOBALS(pDevIns)    ((PPCIGLOBALS)(PDMINS_2_DATA(pDevIns, PPCIGLOBALS)))
 /** Converts a device instance pointer to a PCIBUS pointer. */
-#define DEVINS_2_PCIBUS(pDevIns)        ((PPCIBUS)(&PDMINS_2_DATA(pDevIns, PPCIGLOBALS)->PciBus))
+#define DEVINS_2_PCIBUS(pDevIns)        ((PPCIBUS)(&PDMINS_2_DATA(pDevIns, PPCIGLOBALS)->aPciBus))
 
 #ifndef VBOX_DEVICE_STRUCT_TESTCASE
 
+RT_C_DECLS_BEGIN
+
+PDMBOTHCBDECL(void) ich9pciSetIrq(PPDMDEVINS pDevIns, PPCIDEVICE pPciDev, int iIrq, int iLevel);
+PDMBOTHCBDECL(void) ich9pcibridgeSetIrq(PPDMDEVINS pDevIns, PPCIDEVICE pPciDev, int iIrq, int iLevel);
+PDMBOTHCBDECL(int)  ich9pciIOPortAddressWrite(PPDMDEVINS pDevIns, void *pvUser, RTIOPORT Port, uint32_t u32, unsigned cb);
+PDMBOTHCBDECL(int)  ich9pciIOPortAddressRead(PPDMDEVINS pDevIns, void *pvUser, RTIOPORT Port, uint32_t *pu32, unsigned cb);
+PDMBOTHCBDECL(int)  ich9pciIOPortDataWrite(PPDMDEVINS pDevIns, void *pvUser, RTIOPORT Port, uint32_t u32, unsigned cb);
+PDMBOTHCBDECL(int)  ich9pciIOPortDataRead(PPDMDEVINS pDevIns, void *pvUser, RTIOPORT Port, uint32_t *pu32, unsigned cb);
+
 #ifdef IN_RING3
+DECLINLINE(PPCIDEVICE) ich9pciFindBridge(PPCIBUS pBus, uint8_t iBus);
+#endif
+
+RT_C_DECLS_END
+
+
+PDMBOTHCBDECL(void) ich9pciSetIrq(PPDMDEVINS pDevIns, PPCIDEVICE pPciDev, int iIrq, int iLevel)
+{
+    return;
+}
+
+PDMBOTHCBDECL(void) ich9pcibridgeSetIrq(PPDMDEVINS pDevIns, PPCIDEVICE pPciDev, int iIrq, int iLevel)
+{
+    return;
+}
+
+PDMBOTHCBDECL(int)  ich9pciIOPortAddressWrite(PPDMDEVINS pDevIns, void *pvUser, RTIOPORT Port, uint32_t u32, unsigned cb)
+{
+    return 0;
+}
+
+PDMBOTHCBDECL(int)  ich9pciIOPortAddressRead(PPDMDEVINS pDevIns, void *pvUser, RTIOPORT Port, uint32_t *pu32, unsigned cb)
+{
+    return 0;
+}
+
+PDMBOTHCBDECL(int)  ich9pciIOPortDataWrite(PPDMDEVINS pDevIns, void *pvUser, RTIOPORT Port, uint32_t u32, unsigned cb)
+{
+    return 0;
+}
+
+PDMBOTHCBDECL(int)  ich9pciIOPortDataRead(PPDMDEVINS pDevIns, void *pvUser, RTIOPORT Port, uint32_t *pu32, unsigned cb)
+{
+    return 0;
+}
+
+#ifdef IN_RING3
+DECLINLINE(PPCIDEVICE) ich9pciFindBridge(PPCIBUS pBus, uint8_t iBus);
+#endif
+
+#ifdef IN_RING3
+
+static DECLCALLBACK(int) ich9pciRegister(PPDMDEVINS pDevIns, PPCIDEVICE pPciDev, const char *pszName, int iDev)
+{
+    return 0;
+}
+
+static DECLCALLBACK(int) ich9pcibridgeRegister(PPDMDEVINS pDevIns, PPCIDEVICE pPciDev, const char *pszName, int iDev)
+{
+    return 0;
+}
+
+static DECLCALLBACK(int) ich9pciIORegionRegister(PPDMDEVINS pDevIns, PPCIDEVICE pPciDev, int iRegion, uint32_t cbRegion, PCIADDRESSSPACE enmType, PFNPCIIOREGIONMAP pfnCallback)
+{
+    return 0;
+}
+
+static DECLCALLBACK(void) ich9pciSetConfigCallbacks(PPDMDEVINS pDevIns, PPCIDEVICE pPciDev, PFNPCICONFIGREAD pfnRead, PPFNPCICONFIGREAD ppfnReadOld,
+                                                    PFNPCICONFIGWRITE pfnWrite, PPFNPCICONFIGWRITE ppfnWriteOld)
+{
+}
+
+/**
+ * Saves a state of the PCI device.
+ *
+ * @returns VBox status code.
+ * @param   pDevIns         Device instance of the PCI Bus.
+ * @param   pPciDev         Pointer to PCI device.
+ * @param   pSSM            The handle to save the state to.
+ */
+static DECLCALLBACK(int) pciGenericSaveExec(PPDMDEVINS pDevIns, PPCIDEVICE pPciDev, PSSMHANDLE pSSM)
+{
+    return SSMR3PutMem(pSSM, &pPciDev->config[0], sizeof(pPciDev->config));
+}
+
+static int pciR3CommonSaveExec(PPCIBUS pBus, PSSMHANDLE pSSM)
+{
+    /*
+     * Iterate thru all the devices.
+     */
+    for (uint32_t i = 0; i < RT_ELEMENTS(pBus->devices); i++)
+    {
+        PPCIDEVICE pDev = pBus->devices[i];
+        if (pDev)
+        {
+            SSMR3PutU32(pSSM, i);
+            SSMR3PutMem(pSSM, pDev->config, sizeof(pDev->config));
+
+            int rc = SSMR3PutS32(pSSM, pDev->Int.s.uIrqPinState);
+            if (RT_FAILURE(rc))
+                return rc;
+        }
+    }
+    return SSMR3PutU32(pSSM, UINT32_MAX); /* terminator */
+}
+
+static DECLCALLBACK(int) pciR3SaveExec(PPDMDEVINS pDevIns, PSSMHANDLE pSSM)
+{
+    PPCIBUS pThis = PDMINS_2_DATA(pDevIns, PPCIBUS);
+    return pciR3CommonSaveExec(pThis, pSSM);
+}
+
+
+static DECLCALLBACK(int) pcibridgeR3SaveExec(PPDMDEVINS pDevIns, PSSMHANDLE pSSM)
+{
+    PPCIBUS pThis = PDMINS_2_DATA(pDevIns, PPCIBUS);
+    return pciR3CommonSaveExec(pThis, pSSM);
+}
+
+static DECLCALLBACK(int) pciR3CommonLoadExec(PPCIBUS pBus, PSSMHANDLE pSSM, uint32_t uVersion, uint32_t uPass)
+{
+    return 0;
+}
+
+/**
+ * Loads a saved PCI device state.
+ *
+ * @returns VBox status code.
+ * @param   pDevIns         Device instance of the PCI Bus.
+ * @param   pPciDev         Pointer to PCI device.
+ * @param   pSSM            The handle to the saved state.
+ */
+static DECLCALLBACK(int) pciGenericLoadExec(PPDMDEVINS pDevIns, PPCIDEVICE pPciDev, PSSMHANDLE pSSM)
+{
+    return SSMR3GetMem(pSSM, &pPciDev->config[0], sizeof(pPciDev->config));
+}
+
+static DECLCALLBACK(int) pciR3LoadExec(PPDMDEVINS pDevIns, PSSMHANDLE pSSM, uint32_t uVersion, uint32_t uPass)
+{
+    PPCIBUS pThis = PDMINS_2_DATA(pDevIns, PPCIBUS);
+    if (uVersion > VBOX_ICH9PCI_SAVED_STATE_VERSION)
+        return VERR_SSM_UNSUPPORTED_DATA_UNIT_VERSION;
+    return pciR3CommonLoadExec(pThis, pSSM, uVersion, uPass);
+}
+
+static DECLCALLBACK(int) pcibridgeR3LoadExec(PPDMDEVINS pDevIns, PSSMHANDLE pSSM, uint32_t uVersion, uint32_t uPass)
+{
+    PPCIBUS pThis = PDMINS_2_DATA(pDevIns, PPCIBUS);
+    if (uVersion > VBOX_ICH9PCI_SAVED_STATE_VERSION)
+        return VERR_SSM_UNSUPPORTED_DATA_UNIT_VERSION;
+    return pciR3CommonLoadExec(pThis, pSSM, uVersion, uPass);
+}
+
+static DECLCALLBACK(int) ich9pciFakePCIBIOS(PPDMDEVINS pDevIns)
+{
+    return 0;
+}
+
+static int ich9pciRegisterInternal(PPCIBUS pBus, int iDev, PPCIDEVICE pPciDev, const char *pszName)
+{
+    return VINF_SUCCESS;
+}
 
 static DECLCALLBACK(int) ich9pciConstruct(PPDMDEVINS pDevIns,
                                           int        iInstance,
@@ -152,10 +331,71 @@ static DECLCALLBACK(int) ich9pciConstruct(PPDMDEVINS pDevIns,
     Log(("PCI: fUseIoApic=%RTbool fGCEnabled=%RTbool fR0Enabled=%RTbool\n", fUseIoApic, fGCEnabled, fR0Enabled));
 
     /*
-     * Init data and register the PCI bus.
+     * Init data.
      */
     PPCIGLOBALS pGlobals = PDMINS_2_DATA(pDevIns, PPCIGLOBALS);
+    PPCIBUS     pBus     = &pGlobals->aPciBus;
+    /* Zero out everything */
+    memset(pGlobals, 0, sizeof(*pGlobals));
+    /* And fill values */
     pGlobals->fUseIoApic          = fUseIoApic;
+    pGlobals->pDevInsR3 = pDevIns;
+    pGlobals->pDevInsR0 = PDMDEVINS_2_R0PTR(pDevIns);
+    pGlobals->pDevInsRC = PDMDEVINS_2_RCPTR(pDevIns);
+
+    pGlobals->aPciBus.pDevInsR3 = pDevIns;
+    pGlobals->aPciBus.pDevInsR0 = PDMDEVINS_2_R0PTR(pDevIns);
+    pGlobals->aPciBus.pDevInsRC = PDMDEVINS_2_RCPTR(pDevIns);
+    pGlobals->aPciBus.papBridgesR3 = (PPCIDEVICE *)PDMDevHlpMMHeapAllocZ(pDevIns, sizeof(PPCIDEVICE) * RT_ELEMENTS(pGlobals->aPciBus.devices));
+
+    /*
+     * Register bus
+     */
+    PDMPCIBUSREG PciBusReg;
+    PciBusReg.u32Version              = PDM_PCIBUSREG_VERSION;
+    PciBusReg.pfnRegisterR3           = ich9pciRegister;
+    PciBusReg.pfnIORegionRegisterR3   = ich9pciIORegionRegister;
+    PciBusReg.pfnSetConfigCallbacksR3 = ich9pciSetConfigCallbacks;
+    PciBusReg.pfnSetIrqR3             = ich9pciSetIrq;
+    PciBusReg.pfnSaveExecR3           = pciGenericSaveExec;
+    PciBusReg.pfnLoadExecR3           = pciGenericLoadExec;
+    PciBusReg.pfnFakePCIBIOSR3        = ich9pciFakePCIBIOS;
+    PciBusReg.pszSetIrqRC             = fGCEnabled ? "ich9pciSetIrq" : NULL;
+    PciBusReg.pszSetIrqR0             = fR0Enabled ? "ich9pciSetIrq" : NULL;
+    rc = PDMDevHlpPCIBusRegister(pDevIns, &PciBusReg, &pBus->pPciHlpR3);
+    if (RT_FAILURE(rc))
+        return PDMDEV_SET_ERROR(pDevIns, rc,
+                                N_("Failed to register ourselves as a PCI Bus"));
+    if (pBus->pPciHlpR3->u32Version != PDM_PCIHLPR3_VERSION)
+        return PDMDevHlpVMSetError(pDevIns, VERR_VERSION_MISMATCH, RT_SRC_POS,
+                                   N_("PCI helper version mismatch; got %#x expected %#x"),
+                                   pBus->pPciHlpR3->u32Version, PDM_PCIHLPR3_VERSION);
+
+    pBus->pPciHlpRC = pBus->pPciHlpR3->pfnGetRCHelpers(pDevIns);
+    pBus->pPciHlpR0 = pBus->pPciHlpR3->pfnGetR0Helpers(pDevIns);
+
+    /*
+     * Fill in PCI configs and add them to the bus.
+     */
+
+    /**
+     * We emulate 82801IB ICH9 IO chip used in Q35,
+     * see http://ark.intel.com/Product.aspx?id=31892
+     *
+     * Stepping   S-Spec   Top Marking
+     *
+     *   A2        SLA9M    NH82801IB
+     */
+    PCIDevSetVendorId(  &pBus->PciDev, 0x8086); /* Intel */
+    PCIDevSetDeviceId(  &pBus->PciDev, 0x244e); /* Desktop */
+    PCIDevSetRevisionId(&pBus->PciDev,   0x92); /* rev. A2 */
+    PCIDevSetClassSub(  &pBus->PciDev,   0x00); /* Host/PCI bridge */
+    PCIDevSetClassBase( &pBus->PciDev,   0x06); /* bridge */
+    PCIDevSetHeaderType(&pBus->PciDev,   0x00);
+
+    pBus->PciDev.pDevIns              = pDevIns;
+    pBus->PciDev.Int.s.fRequestedDevFn= true;
+    ich9pciRegisterInternal(pBus, 0, &pBus->PciDev, "i82801");
 
     return VINF_SUCCESS;
 }
