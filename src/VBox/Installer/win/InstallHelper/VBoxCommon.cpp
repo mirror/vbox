38aaedce7f6d8a2b1921328bb8a1e7f8eff3ace1
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2010-08-31 08:44:09 +0000

    more nits.
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@32112 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Installer/win/InstallHelper/VBoxCommon.cpp
+++ src/VBox/Installer/win/InstallHelper/VBoxCommon.cpp
@@ -1,10 +1,10 @@
-/* $Id: */
+/* $Id$ */
 /** @file
-* VBoxCommon - Misc helper routines for install helper.
-*/
+ * VBoxCommon - Misc helper routines for install helper.
+ */
 
 /*
- * Copyright (C) 2006-2010 Oracle Corporation
+ * Copyright (C) 2008-2010 Oracle Corporation
  *
  * This file is part of VirtualBox Open Source Edition (OSE), as
  * available from http://www.virtualbox.org. This file is free software;
