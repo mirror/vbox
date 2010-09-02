b55ef7622f6a8a3839ae035fef292ef0c5857f76
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2010-09-02 13:22:21 +0000

    Runtime: Our own ldrELF32,64.h headers. Renamed ldrElf.h->ldrELF.h.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@32197 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Runtime/include/internal/ldrELF32.h
+++ src/VBox/Runtime/include/internal/ldrELF32.h
@@ -1,165 +1,179 @@
-/*-
- * Copyright (c) 1996-1998 John D. Polstra.
- * All rights reserved.
+/* $Id$ */
+/** @file
+ * IPRT - ELF 32-bit header.
+ */
+
+/*
+ * Copyright (C) 2010 Oracle Corporation
  *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
- * 1. Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer.
- * 2. Redistributions in binary form must reproduce the above copyright
- *    notice, this list of conditions and the following disclaimer in the
- *    documentation and/or other materials provided with the distribution.
- *
- * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
- * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
- * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
- * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
- * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
- * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
- * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
- * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
- * SUCH DAMAGE.
+ * This file is part of VirtualBox Open Source Edition (OSE), as
+ * available from http://www.virtualbox.org. This file is free software;
+ * you can redistribute it and/or modify it under the terms of the GNU
+ * General Public License (GPL) as published by the Free Software
+ * Foundation, in version 2 as it comes in the "COPYING" file of the
+ * VirtualBox OSE distribution. VirtualBox OSE is distributed in the
+ * hope that it will be useful, but WITHOUT ANY WARRANTY of any kind.
  */
 
 #ifndef ___internal_ldrELF32_h
 #define ___internal_ldrELF32_h
 
+#include <iprt/assert.h>
+
 #include "ldrELFCommon.h"
 
 /*
- * ELF definitions common to all 32-bit architectures.
+ * ELF 32 standard types.
  */
+typedef uint32_t        Elf32_Addr;
+typedef uint16_t        Elf32_Half;
+typedef uint32_t        Elf32_Off;
+typedef int32_t         Elf32_Sword;
+typedef uint32_t        Elf32_Word;
 
-typedef uint32_t	Elf32_Addr;
-typedef uint16_t	Elf32_Half;
-typedef uint32_t	Elf32_Off;
-typedef int32_t		Elf32_Sword;
-typedef uint32_t	Elf32_Word;
-typedef uint32_t	Elf32_Size;
-typedef Elf32_Off	Elf32_Hashelt;
+/*
+ * Ensure type size correctness in accordance to                                                             .
+ * Portable Format Specification (for ELF), Version 1.1, fig 1-2.                                            .
+ */
+AssertCompileSize(Elf32_Addr,   4);
+AssertCompileSize(Elf32_Half,   2);
+AssertCompileSize(Elf32_Off,    4);
+AssertCompileSize(Elf32_Sword,  4);
+AssertCompileSize(Elf32_Word,   4);
+
+/*
+ * ELF 32 non-standard types for convenience.
+ */
+typedef Elf32_Word      Elf32_Size;
+typedef Elf32_Word      Elf32_Hashelt;
 
 /*
  * ELF header.
  */
-
-typedef struct Elf32_Ehdr {
-	unsigned char	e_ident[EI_NIDENT];	/* File identification. */
-	Elf32_Half	e_type;		/* File type. */
-	Elf32_Half	e_machine;	/* Machine architecture. */
-	Elf32_Word	e_version;	/* ELF format version. */
-	Elf32_Addr	e_entry;	/* Entry point. */
-	Elf32_Off	e_phoff;	/* Program header file offset. */
-	Elf32_Off	e_shoff;	/* Section header file offset. */
-	Elf32_Word	e_flags;	/* Architecture-specific flags. */
-	Elf32_Half	e_ehsize;	/* Size of ELF header in bytes. */
-	Elf32_Half	e_phentsize;	/* Size of program header entry. */
-	Elf32_Half	e_phnum;	/* Number of program header entries. */
-	Elf32_Half	e_shentsize;	/* Size of section header entry. */
-	Elf32_Half	e_shnum;	/* Number of section header entries. */
-	Elf32_Half	e_shstrndx;	/* Section name strings section. */
+typedef struct
+{
+    unsigned char e_ident[16];      /* ELF identification. */
+    Elf32_Half    e_type;           /* Object file type. */
+    Elf32_Half    e_machine;        /* Machine type. */
+    Elf32_Word    e_version;        /* Object file version. */
+    Elf32_Addr    e_entry;          /* Entry point address. */
+    Elf32_Off     e_phoff;          /* Program header offset. */
+    Elf32_Off     e_shoff;          /* Section header offset. */
+    Elf32_Word    e_flags;          /* Processor-specific flags. */
+    Elf32_Half    e_ehsize;         /* ELF header size. */
+    Elf32_Half    e_phentsize;      /* Size of program header entries. */
+    Elf32_Half    e_phnum;          /* Number of program headers. */
+    Elf32_Half    e_shentsize;      /* Size of section header entries. */
+    Elf32_Half    e_shnum;          /* Number of section headers. */
+    Elf32_Half    e_shstrndx;       /* Section name string table index. */
 } Elf32_Ehdr;
 
 /*
  * Section header.
  */
-
-typedef struct {
-	Elf32_Word	sh_name;	/* Section name (index into the
-					   section header string table). */
-	Elf32_Word	sh_type;	/* Section type. */
-	Elf32_Word	sh_flags;	/* Section flags. */
-	Elf32_Addr	sh_addr;	/* Address in memory image. */
-	Elf32_Off	sh_offset;	/* Offset in file. */
-	Elf32_Size	sh_size;	/* Size in bytes. */
-	Elf32_Word	sh_link;	/* Index of a related section. */
-	Elf32_Word	sh_info;	/* Depends on section type. */
-	Elf32_Size	sh_addralign;	/* Alignment in bytes. */
-	Elf32_Size	sh_entsize;	/* Size of each entry in section. */
+typedef struct
+{
+    Elf32_Word    sh_name;          /* Section name. */
+    Elf32_Word    sh_type;          /* Section type. */
+    Elf32_Word    sh_flags;         /* Section attributes. */
+    Elf32_Addr    sh_addr;          /* Virtual address in memory. */
+    Elf32_Off     sh_offset;        /* Offset in file. */
+    Elf32_Word    sh_size;          /* Size of section. */
+    Elf32_Word    sh_link;          /* Link to other section. */
+    Elf32_Word    sh_info;          /* Miscellaneous information. */
+    Elf32_Word    sh_addralign;     /* Address alignment boundary. */
+    Elf32_Word    sh_entsize;       /* Size of entries, if section has table. */
 } Elf32_Shdr;
+
 
 /*
  * Program header.
  */
-
-typedef struct {
-	Elf32_Word	p_type;		/* Entry type. */
-	Elf32_Off	p_offset;	/* File offset of contents. */
-	Elf32_Addr	p_vaddr;	/* Virtual address in memory image. */
-	Elf32_Addr	p_paddr;	/* Physical address (not used). */
-	Elf32_Size	p_filesz;	/* Size of contents in file. */
-	Elf32_Size	p_memsz;	/* Size of contents in memory. */
-	Elf32_Word	p_flags;	/* Access permission flags. */
-	Elf32_Size	p_align;	/* Alignment in memory and file. */
+typedef struct
+{
+    Elf32_Word    p_type;           /* Type of segment. */
+    Elf32_Off     p_offset;         /* Offset in file. */
+    Elf32_Addr    p_vaddr;          /* Virtual address in memory. */
+    Elf32_Addr    p_paddr;          /* Physical address (reserved). */
+    Elf32_Word    p_filesz;         /* Size of segment in file. */
+    Elf32_Word    p_memsz;          /* Size of segment in memory. */
+    Elf32_Word    p_flags;          /* Segment attributes. */
+    Elf32_Word    p_align;          /* Alignment of segment. */
 } Elf32_Phdr;
 
-/*
- * Note header. (added by Ramshankar)
- */
 
-typedef struct {
-	Elf32_Word	n_namesz;	/* length of note's name */
-	Elf32_Word	n_descsz;	/* length of note's desc */
-	Elf32_Word	n_type;		/* type of note */
+/*
+ * Note header.
+ */
+typedef struct
+{
+    Elf32_Word    n_namesz;         /* Length of note's name. */
+    Elf32_Word    n_descsz;         /* Length of note's description. */
+    Elf32_Word    n_type;           /* Type of note. */
 } Elf32_Nhdr;
 
-/*
- * Dynamic structure.  The ".dynamic" section contains an array of them.
- */
 
-typedef struct {
-	Elf32_Sword	d_tag;		/* Entry type. */
-	union {
-		Elf32_Size	d_val;	/* Integer value. */
-		Elf32_Addr	d_ptr;	/* Address value. */
-	} d_un;
+/*
+ * Symbol table entry.
+ */
+typedef struct
+{
+    Elf32_Word    st_name;          /* Symbol name. */
+    Elf32_Addr    st_value;         /* Symbol value. */
+    Elf32_Word    st_size;          /* Size associated with symbol. */
+    unsigned char st_info;          /* Type and binding attributes. */
+    unsigned char st_other;         /* Reserved. */
+    Elf32_Half    st_shndx;         /* Section header table index. */
+} Elf32_Sym;
+
+
+/*
+ * Relocations.
+ */
+typedef struct
+{
+    Elf32_Addr    r_offset;         /* Location to be relocated. */
+    Elf32_Word    r_info;           /* Symbol index and type of relocation. */
+} Elf32_Rel;
+
+typedef struct
+{
+    Elf32_Addr    r_offset;         /* Location to be relocated. */
+    Elf32_Word    r_info;           /* Symbol index and type of relocation. */
+    Elf32_Sword   r_addend;         /* Constant part of expression. */
+} Elf32_Rela;
+
+/*
+ * Dynamic section entry.
+ * ".dynamic" section contains an array of this.
+ */
+typedef struct
+{
+    Elf32_Sword   d_tag;            /* Type of entry. */
+    union
+    {
+        Elf32_Word    d_val;        /* Integer value. */
+        Elf32_Addr    d_ptr;        /* Virtual address value. */
+    } d_un;
 } Elf32_Dyn;
 
 /*
- * Relocation entries.
+ * Helper macros.
  */
+/** The symbol's type. */
+#define ELF32_ST_TYPE(info)         ((info) & 0xF)
+/** The symbol's binding. */
+#define ELF32_ST_BIND(info)         ((info) >> 4)
+/** Make st_info. given binding and type. */
+#define ELF32_ST_INFO(bind, type)   (((bind) << 4) + ((type) & 0xf))
 
-/* Relocations that don't need an addend field. */
-typedef struct {
-	Elf32_Addr	r_offset;	/* Location to be relocated. */
-	Elf32_Word	r_info;		/* Relocation type and symbol index. */
-} Elf32_Rel;
+/** Relocation type. */
+#define ELF32_R_TYPE(info)          ((unsigned char)(info))
+/** Relocation symbol index. */
+#define ELF32_R_SYM(info)           ((info) >> 8)
+/** Make r_info given the symbol index and type.  */
+#define ELF32_R_INFO(sym, type)     (((sym) << 8) + (unsigned char)(type))
 
-/* Relocations that need an addend field. */
-typedef struct {
-	Elf32_Addr	r_offset;	/* Location to be relocated. */
-	Elf32_Word	r_info;		/* Relocation type and symbol index. */
-	Elf32_Sword	r_addend;	/* Addend. */
-} Elf32_Rela;
 
-/* Macros for accessing the fields of r_info. */
-#define ELF32_R_SYM(info)	((info) >> 8)
-#define ELF32_R_TYPE(info)	((unsigned char)(info))
-
-/* Macro for constructing r_info from field values. */
-#define ELF32_R_INFO(sym, type)	(((sym) << 8) + (unsigned char)(type))
-
-/*
- * Symbol table entries.
- */
-
-typedef struct {
-	Elf32_Word	st_name;	/* String table index of name. */
-	Elf32_Addr	st_value;	/* Symbol value. */
-	Elf32_Size	st_size;	/* Size of associated object. */
-	unsigned char	st_info;	/* Type and binding information. */
-	unsigned char	st_other;	/* Reserved (not used). */
-	Elf32_Half	st_shndx;	/* Section index of symbol. */
-} Elf32_Sym;
-
-/* Macros for accessing the fields of st_info. */
-#define ELF32_ST_BIND(info)		((info) >> 4)
-#define ELF32_ST_TYPE(info)		((info) & 0xf)
-
-/* Macro for constructing st_info from field values. */
-#define ELF32_ST_INFO(bind, type)	(((bind) << 4) + ((type) & 0xf))
-
-#endif
+#endif /* ___internal_ldrELF32_h */
 
--- src/VBox/Runtime/include/internal/ldrELF64.h
+++ src/VBox/Runtime/include/internal/ldrELF64.h
@@ -1,179 +1,178 @@
-/*-
- * Copyright (c) 1996-1998 John D. Polstra.
- * All rights reserved.
+/* $Id$ */
+/** @file
+ * IPRT - ELF 64-bit header.
+ */
+
+/*
+ * Copyright (C) 2010 Oracle Corporation
  *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
- * 1. Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer.
- * 2. Redistributions in binary form must reproduce the above copyright
- *    notice, this list of conditions and the following disclaimer in the
- *    documentation and/or other materials provided with the distribution.
- *
- * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
- * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
- * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
- * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
- * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
- * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
- * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
- * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
- * SUCH DAMAGE.
+ * This file is part of VirtualBox Open Source Edition (OSE), as
+ * available from http://www.virtualbox.org. This file is free software;
+ * you can redistribute it and/or modify it under the terms of the GNU
+ * General Public License (GPL) as published by the Free Software
+ * Foundation, in version 2 as it comes in the "COPYING" file of the
+ * VirtualBox OSE distribution. VirtualBox OSE is distributed in the
+ * hope that it will be useful, but WITHOUT ANY WARRANTY of any kind.
  */
 
 #ifndef ___internal_ldrELF64_h
 #define ___internal_ldrELF64_h
 
+#include <iprt/assert.h>            /* Added by Ramshankar */
+
 #include "ldrELFCommon.h"
 
 /*
- * ELF definitions common to all 64-bit architectures.
+ * ELF 64 standard types.
  */
-
-typedef uint64_t	Elf64_Addr;
-typedef uint32_t	Elf64_Half;
-typedef uint64_t	Elf64_Off;
-typedef int64_t		Elf64_Sword;
-typedef uint64_t	Elf64_Word;
-typedef uint64_t	Elf64_Size;
-typedef uint16_t	Elf64_Quarter;
+typedef uint64_t        Elf64_Addr;
+typedef uint64_t        Elf64_Off;
+typedef uint16_t        Elf64_Half;
+typedef uint32_t        Elf64_Word;
+typedef int32_t         Elf64_Sword;
+typedef uint64_t        Elf64_Xword;
+typedef int64_t         Elf64_Sxword;
 
 /*
- * Types of dynamic symbol hash table bucket and chain elements.
- *
- * This is inconsistent among 64 bit architectures, so a machine dependent
- * typedef is required.
+ * Ensure type size correctness in accordance to ELF-64 Object File Format, Version 1.5 Draft 2, p2.
  */
-
-#ifdef __alpha__
-typedef Elf64_Off	Elf64_Hashelt;
-#else
-typedef Elf64_Half	Elf64_Hashelt;
-#endif
+AssertCompileSize(Elf64_Addr,    8);
+AssertCompileSize(Elf64_Off,     8);
+AssertCompileSize(Elf64_Half,    2);
+AssertCompileSize(Elf64_Word,    4);
+AssertCompileSize(Elf64_Sword,   4);
+AssertCompileSize(Elf64_Xword,   8);
+AssertCompileSize(Elf64_Sxword,  8);
 
 /*
- * ELF header.
+ * ELF 64 non-standard types for convenience.
  */
+typedef Elf64_Xword     Elf64_Size;
+typedef Elf64_Word      Elf64_Hashelt;
 
-typedef struct {
-	unsigned char	e_ident[EI_NIDENT];	/* File identification. */
-	Elf64_Quarter	e_type;		/* File type. */
-	Elf64_Quarter	e_machine;	/* Machine architecture. */
-	Elf64_Half	e_version;	/* ELF format version. */
-	Elf64_Addr	e_entry;	/* Entry point. */
-	Elf64_Off	e_phoff;	/* Program header file offset. */
-	Elf64_Off	e_shoff;	/* Section header file offset. */
-	Elf64_Half	e_flags;	/* Architecture-specific flags. */
-	Elf64_Quarter	e_ehsize;	/* Size of ELF header in bytes. */
-	Elf64_Quarter	e_phentsize;	/* Size of program header entry. */
-	Elf64_Quarter	e_phnum;	/* Number of program header entries. */
-	Elf64_Quarter	e_shentsize;	/* Size of section header entry. */
-	Elf64_Quarter	e_shnum;	/* Number of section header entries. */
-	Elf64_Quarter	e_shstrndx;	/* Section name strings section. */
+/*
+ * ELF Header.
+ */
+typedef struct
+{
+    unsigned char e_ident[16];      /* ELF identification. */
+    Elf64_Half    e_type;           /* Object file type. */
+    Elf64_Half    e_machine;        /* Machine type. */
+    Elf64_Word    e_version;        /* Object file version. */
+    Elf64_Addr    e_entry;          /* Entry point address. */
+    Elf64_Off     e_phoff;          /* Program header offset. */
+    Elf64_Off     e_shoff;          /* Section header offset. */
+    Elf64_Word    e_flags;          /* Processor-specific flags. */
+    Elf64_Half    e_ehsize;         /* ELF header size. */
+    Elf64_Half    e_phentsize;      /* Size of program header entry. */
+    Elf64_Half    e_phnum;          /* Number of program header entries. */
+    Elf64_Half    e_shentsize;      /* Size of section header entry. */
+    Elf64_Half    e_shnum;          /* Number of section header entries. */
+    Elf64_Half    e_shstrndx;       /* Section name string table index. */
 } Elf64_Ehdr;
 
 /*
  * Section header.
  */
-
-typedef struct {
-	Elf64_Half	sh_name;	/* Section name (index into the
-					   section header string table). */
-	Elf64_Half	sh_type;	/* Section type. */
-	Elf64_Size	sh_flags;	/* Section flags. */
-	Elf64_Addr	sh_addr;	/* Address in memory image. */
-	Elf64_Off	sh_offset;	/* Offset in file. */
-	Elf64_Size	sh_size;	/* Size in bytes. */
-	Elf64_Half	sh_link;	/* Index of a related section. */
-	Elf64_Half	sh_info;	/* Depends on section type. */
-	Elf64_Size	sh_addralign;	/* Alignment in bytes. */
-	Elf64_Size	sh_entsize;	/* Size of each entry in section. */
+typedef struct
+{
+    Elf64_Word    sh_name;          /* Section name. */
+    Elf64_Word    sh_type;          /* Section type. */
+    Elf64_Xword   sh_flags;         /* Section attributes. */
+    Elf64_Addr    sh_addr;          /* Virtual address in memory. */
+    Elf64_Off     sh_offset;        /* Offset in file. */
+    Elf64_Xword   sh_size;          /* Size of section. */
+    Elf64_Word    sh_link;          /* Link to other section. */
+    Elf64_Word    sh_info;          /* Miscellaneous information. */
+    Elf64_Xword   sh_addralign;     /* Address alignment boundary. */
+    Elf64_Xword   sh_entsize;       /* Size of entries, if section has table. */
 } Elf64_Shdr;
 
 /*
  * Program header.
  */
-
-typedef struct {
-	Elf64_Half	p_type;		/* Entry type. */
-	Elf64_Half	p_flags;	/* Access permission flags. */
-	Elf64_Off	p_offset;	/* File offset of contents. */
-	Elf64_Addr	p_vaddr;	/* Virtual address in memory image. */
-	Elf64_Addr	p_paddr;	/* Physical address (not used). */
-	Elf64_Size	p_filesz;	/* Size of contents in file. */
-	Elf64_Size	p_memsz;	/* Size of contents in memory. */
-	Elf64_Size	p_align;	/* Alignment in memory and file. */
+typedef struct
+{
+    Elf64_Word    p_type;           /* Type of segment. */
+    Elf64_Word    p_flags;          /* Segment attributes. */
+    Elf64_Off     p_offset;         /* Offset in file. */
+    Elf64_Addr    p_vaddr;          /* Virtual address in memory. */
+    Elf64_Addr    p_paddr;          /* Physical address (reserved). */
+    Elf64_Xword   p_filesz;         /* Size of segment in file. */
+    Elf64_Xword   p_memsz;          /* Size of segment in memory. */
+    Elf64_Xword   p_align;          /* Alignment of segment. */
 } Elf64_Phdr;
 
 /*
- * Note header. (added by Ramshankar)
+ * Note header.
  */
-
-typedef struct {
-	Elf64_Word	n_namesz;	/* length of note's name */
-	Elf64_Word	n_descsz;	/* length of note's desc */
-	Elf64_Word	n_type;		/* type of note */
+typedef struct
+{
+    Elf64_Word    n_namesz;         /* Length of note's name. */
+    Elf64_Word    n_descsz;         /* Length of note's description. */
+    Elf64_Word    n_type;           /* Type of note. */
 } Elf64_Nhdr;
 
+/*
+ * Symbol table entry.
+ */
+typedef struct
+{
+    Elf64_Word    st_name;          /* Symbol name. */
+    unsigned char st_info;          /* Type and binding attributes. */
+    unsigned char st_other;         /* Reserved. */
+    Elf64_Half    st_shndx;         /* Section header table index. */
+    Elf64_Addr    st_value;         /* Symbol value. */
+    Elf64_Xword   st_size;          /* Size associated with symbol. */
+} Elf64_Sym;
 
 /*
- * Dynamic structure.  The ".dynamic" section contains an array of them.
+ * Relocations.
  */
+typedef struct
+{
+    Elf64_Addr    r_offset;         /* Location to be relocated. */
+    Elf64_Xword   r_info;           /* Symbol index and type of relocation. */
+} Elf64_Rel;
 
-typedef struct {
-	Elf64_Size	d_tag;		/* Entry type. */
-	union {
-		Elf64_Size	d_val;	/* Integer value. */
-		Elf64_Addr	d_ptr;	/* Address value. */
-	} d_un;
+typedef struct
+{
+    Elf64_Addr    r_offset;         /* Location to be relocated. */
+    Elf64_Xword   r_info;           /* Symbol index and type of relocation. */
+    Elf64_Sxword  r_addend;         /* Constant part of expression. */
+} Elf64_Rela;
+
+/*
+ * Dynamic section entry.
+ * ".dynamic" section contains an array of this.
+ */
+typedef struct
+{
+    Elf64_Sxword  d_tag;            /* Type of entry. */
+    union
+    {
+        Elf64_Xword   d_val;        /* Integer value. */
+        Elf64_Addr    d_ptr;        /* Virtual address value. */
+    } d_un;
 } Elf64_Dyn;
 
 /*
- * Relocation entries.
+ * Helper macros.
  */
+/** The symbol's type. */
+#define ELF64_ST_TYPE(info)         ((info) & 0xF)
+/** The symbol's binding. */
+#define ELF64_ST_BIND(info)         ((info) >> 4)
+/** Make st_info. given binding and type. */
+#define ELF64_ST_INFO(bind, type)   (((bind) << 4) + ((type) & 0xf))
 
-/* Relocations that don't need an addend field. */
-typedef struct {
-	Elf64_Addr	r_offset;	/* Location to be relocated. */
-	Elf64_Size	r_info;		/* Relocation type and symbol index. */
-} Elf64_Rel;
+/** Relocation type. */
+#define ELF64_R_TYPE(info)          ((unsigned char)(info))
+/** Relocation symbol index. */
+#define ELF64_R_SYM(info)           ((info) >> 32)
+/** Make r_info given the symbol index and type.  */
+#define ELF64_R_INFO(sym, type)     (((sym) << 32) + (unsigned char)(type))
 
-/* Relocations that need an addend field. */
-typedef struct {
-	Elf64_Addr	r_offset;	/* Location to be relocated. */
-	Elf64_Size	r_info;		/* Relocation type and symbol index. */
-	Elf64_Off	r_addend;	/* Addend. */
-} Elf64_Rela;
 
-/* Macros for accessing the fields of r_info. */
-#define ELF64_R_SYM(info)	((info) >> 32)
-#define ELF64_R_TYPE(info)	((unsigned char)(info))
-
-/* Macro for constructing r_info from field values. */
-#define ELF64_R_INFO(sym, type)	(((sym) << 32) + (unsigned char)(type))
-
-/*
- * Symbol table entries.
- */
-
-typedef struct {
-	Elf64_Half	st_name;	/* String table index of name. */
-	unsigned char	st_info;	/* Type and binding information. */
-	unsigned char	st_other;	/* Reserved (not used). */
-	Elf64_Quarter	st_shndx;	/* Section index of symbol. */
-	Elf64_Addr	st_value;	/* Symbol value. */
-	Elf64_Size	st_size;	/* Size of associated object. */
-} Elf64_Sym;
-
-/* Macros for accessing the fields of st_info. */
-#define ELF64_ST_BIND(info)		((info) >> 4)
-#define ELF64_ST_TYPE(info)		((info) & 0xf)
-
-/* Macro for constructing st_info from field values. */
-#define ELF64_ST_INFO(bind, type)	(((bind) << 4) + ((type) & 0xf))
-
-#endif
+#endif /* ___internal_ldrELF64_h */
 
