ae7ed016ff633a74071fdca76c39d11896a431e4
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2010-09-09 12:08:48 +0000

    PCI: typos, first functional version
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@32342 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Devices/Builtins.cpp
+++ src/VBox/Devices/Builtins.cpp
@@ -59,6 +59,9 @@ extern "C" DECLEXPORT(int) VBoxDevicesRegister(PPDMDEVREGCB pCallbacks, uint32_t
     rc = pCallbacks->pfnRegister(pCallbacks, &g_DevicePCI);
     if (RT_FAILURE(rc))
         return rc;
+    rc = pCallbacks->pfnRegister(pCallbacks, &g_DevicePciIch9);
+    if (RT_FAILURE(rc))
+        return rc;
     rc = pCallbacks->pfnRegister(pCallbacks, &g_DevicePcArch);
     if (RT_FAILURE(rc))
         return rc;
@@ -164,6 +167,9 @@ extern "C" DECLEXPORT(int) VBoxDevicesRegister(PPDMDEVREGCB pCallbacks, uint32_t
         return rc;
 #endif
     rc = pCallbacks->pfnRegister(pCallbacks, &g_DevicePCIBridge);
+    if (RT_FAILURE(rc))
+        return rc;
+    rc = pCallbacks->pfnRegister(pCallbacks, &g_DevicePciIch9Bridge);
     if (RT_FAILURE(rc))
         return rc;
 #ifdef VBOX_WITH_LSILOGIC
