b5e8e6182ae9687df72ad75fb2ac34aea04dcf91
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2010-09-16 15:42:51 +0000

    Runtime: fix burn
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@32569 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Runtime/common/checksum/RTSha1Digest.cpp
+++ src/VBox/Runtime/common/checksum/RTSha1Digest.cpp
@@ -40,7 +40,7 @@
 #include <openssl/sha.h>
 
 
-RTR3DECL(int) RTSha1Digest(void* pvBuf, size_t cbBuf, char **ppszDigest, FNRTPROGRESS pfnProgressCallback, void *pvUser)
+RTR3DECL(int) RTSha1Digest(void* pvBuf, size_t cbBuf, char **ppszDigest, PFNRTPROGRESS pfnProgressCallback, void *pvUser)
 {
     /* Validate input */
     AssertPtrReturn(pvBuf, VERR_INVALID_POINTER);
