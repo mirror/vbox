ba9c1f1838f3d6c51f4b9eda20ad30db6d907fc5
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2010-09-16 11:30:20 +0000

    Solaris/Installer: vboxconfig.sh, checkinstall.sh cleanup.
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@32549 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Installer/solaris/checkinstall.sh
+++ src/VBox/Installer/solaris/checkinstall.sh
@@ -16,9 +16,20 @@
 # hope that it will be useful, but WITHOUT ANY WARRANTY of any kind.
 #
 
+infoprint()
+{
+    echo 1>&2 "$1"
+}
+
+errorprint()
+{
+    echo 1>&2 "## $1"
+}
+
 abort_error()
 {
-    echo "## Please close all VirtualBox processes and re-run this installer."
+    errorprint "Please close all VirtualBox processes and re-run this installer."
+    errorprint "Note: It can take up to 10 seconds for all VirtualBox & related processes to close."
     exit 1
 }
 
@@ -27,18 +38,11 @@ if test "x${BASEDIR}" != "x/"; then
     exit 0
 fi
 
-# Check if VBoxSVC is currently running
-VBOXSVC_PID=`ps -eo pid,fname | grep VBoxSVC | grep -v grep | awk '{ print $1 }'`
-if test ! -z "$VBOXSVC_PID" && test "$VBOXSVC_PID" -ge 0; then
-    echo "## VirtualBox's VBoxSVC (pid $VBOXSVC_PID) still appears to be running."
-    abort_error
-fi
-
 # Check if the Zone Access service is holding open vboxdrv, if so stop & remove it
 servicefound=`svcs -H "svc:/application/virtualbox/zoneaccess" 2> /dev/null | grep '^online'`
 if test ! -z "$servicefound"; then
-    echo "## VirtualBox's zone access service appears to still be running."
-    echo "## Halting & removing zone access service..."
+    infoprint "VirtualBox's zone access service appears to still be running."
+    infoprint "Halting & removing zone access service..."
     /usr/sbin/svcadm disable -s svc:/application/virtualbox/zoneaccess
     # Don't delete the service, handled by manifest class action
     # /usr/sbin/svccfg delete svc:/application/virtualbox/zoneaccess
@@ -47,11 +51,25 @@ fi
 # Check if the Web service is running, if so stop & remove it
 servicefound=`svcs -H "svc:/application/virtualbox/webservice" 2> /dev/null | grep '^online'`
 if test ! -z "$servicefound"; then
-    echo "## VirtualBox web service appears to still be running."
-    echo "## Halting & removing webservice..."
+    infoprint "VirtualBox web service appears to still be running."
+    infoprint "Halting & removing webservice..."
     /usr/sbin/svcadm disable -s svc:/application/virtualbox/webservice
     # Don't delete the service, handled by manifest class action
     # /usr/sbin/svccfg delete svc:/application/virtualbox/webservice
+fi
+
+# Check if VBoxSVC is currently running
+VBOXSVC_PID=`ps -eo pid,fname | grep VBoxSVC | grep -v grep | awk '{ print $1 }'`
+if test ! -z "$VBOXSVC_PID" && test "$VBOXSVC_PID" -ge 0; then
+    errorprint "VirtualBox's VBoxSVC (pid $VBOXSVC_PID) still appears to be running."
+    abort_error
+fi
+
+# Check if VBoxNetDHCP is currently running
+VBOXNETDHCP_PID=`ps -eo pid,fname | grep VBoxNetDHCP | grep -v grep | awk '{ print $1 }'`
+if test ! -z "VBOXNETDHCP_PID" && test "VBOXNETDHCP_PID" -ge 0; then
+    errorprint "VirtualBox's VBoxNetDHCP (pid $VBOXNETDHCP_PID) still appears to be running."
+    abort_error
 fi
 
 # Check if vboxnet is still plumbed, if so try unplumb it
@@ -59,21 +77,21 @@ BIN_IFCONFIG=`which ifconfig 2> /dev/null`
 if test -x "$BIN_IFCONFIG"; then
     vboxnetup=`$BIN_IFCONFIG vboxnet0 >/dev/null 2>&1`
     if test "$?" -eq 0; then
-        echo "## VirtualBox NetAdapter is still plumbed"
-        echo "## Trying to remove old NetAdapter..."
+        infoprint "VirtualBox NetAdapter is still plumbed"
+        infoprint "Trying to remove old NetAdapter..."
         $BIN_IFCONFIG vboxnet0 unplumb
         if test "$?" -ne 0; then
-            echo "## VirtualBox NetAdapter 'vboxnet0' couldn't be unplumbed (probably in use)."
+            errorprint "VirtualBox NetAdapter 'vboxnet0' couldn't be unplumbed (probably in use)."
             abort_error
         fi
     fi
     vboxnetup=`$BIN_IFCONFIG vboxnet0 inet6 >/dev/null 2>&1`
     if test "$?" -eq 0; then
-        echo "## VirtualBox NetAdapter (Ipv6) is still plumbed"
-        echo "## Trying to remove old NetAdapter..."
+        infoprint "VirtualBox NetAdapter (Ipv6) is still plumbed"
+        infoprint "Trying to remove old NetAdapter..."
         $BIN_IFCONFIG vboxnet0 inet6 unplumb
         if test "$?" -ne 0; then
-            echo "## VirtualBox NetAdapter 'vboxnet0' IPv6 couldn't be unplumbed (probably in use)."
+            errorprint "VirtualBox NetAdapter 'vboxnet0' IPv6 couldn't be unplumbed (probably in use)."
             abort_error
         fi
     fi
