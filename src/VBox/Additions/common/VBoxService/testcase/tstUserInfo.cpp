5b538eaaa06aab36c9f7877b0f8d12ecda509cb5
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2010-09-20 12:00:44 +0000

    VBoxService: Added a simple testcase for guest user environment.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@32634 cfe28804-0f27-0410-a406-dd0f0b0b656f

---
+++ src/VBox/Additions/common/VBoxService/testcase/tstUserInfo.cpp
@@ -0,0 +1,81 @@
+/* $Id$ */
+/** @file
+ * Test case for correct user environment.
+ */
+
+/*
+ * Copyright (C) 2010 Oracle Corporation
+ *
+ * This file is part of VirtualBox Open Source Edition (OSE), as
+ * available from http://www.virtualbox.org. This file is free software;
+ * you can redistribute it and/or modify it under the terms of the GNU
+ * General Public License (GPL) as published by the Free Software
+ * Foundation, in version 2 as it comes in the "COPYING" file of the
+ * VirtualBox OSE distribution. VirtualBox OSE is distributed in the
+ * hope that it will be useful, but WITHOUT ANY WARRANTY of any kind.
+ */
+
+
+/*******************************************************************************
+*   Header Files                                                               *
+*******************************************************************************/
+#ifdef RT_OS_WINDOWS
+# include <windows.h>
+# include <tchar.h>
+# include <stdio.h>
+# include <Shlobj.h>
+#endif
+
+#include <iprt/initterm.h>
+#include <iprt/path.h>
+#include <iprt/stream.h>
+#include <iprt/string.h>
+#include <VBox/log.h>
+#include <VBox/version.h>
+#include <VBox/VBoxGuestLib.h>
+
+
+int main()
+{
+    /*
+     * Init globals and such.
+     */
+    RTR3Init();
+
+    int rc = VbglR3Init();
+    if (RT_FAILURE(rc))
+    {
+        printf("VbglR3Init failed with rc=%Rrc.\n", rc);
+        return -1;
+    }
+#ifdef RT_OS_WINDOWS
+    TCHAR szPath[MAX_PATH];
+    HRESULT hRes = SHGetFolderPath(0, CSIDL_APPDATA, 0, 0, szPath);
+
+    if (SUCCEEDED(hRes))
+    {
+        RTPrintf("SHGetFolderPathW (CSIDL_APPDATA) = %ls\n", szPath);
+        hRes = SHGetFolderPath(0, CSIDL_PERSONAL, 0, 0, szPath);
+        if (SUCCEEDED(hRes))
+        {
+            RTPrintf("SHGetFolderPathW (CSIDL_PERSONAL) = %ls\n", szPath);
+        }
+        else
+            RTPrintf("SHGetFolderPathW (CSIDL_PERSONAL) returned error: 0x%x", hRes);
+    }
+    else
+        RTPrintf("SHGetFolderPathW (CSIDL_APPDATA) returned error: 0x%x", hRes);
+
+    if (FAILED(hRes))
+        rc = RTErrConvertFromWin32(hRes);
+
+    if (RT_SUCCESS(rc))
+    {
+        /* Dump env bits. */
+        RTPrintf("Environment:\n\n");
+        RTPrintf("APPDATA = %s\n", getenv("APPDATA"));
+    }
+#endif
+    return VINF_SUCCESS(rc) ? 0 : -1;
+}
+
