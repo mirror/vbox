48413e09d062debe66b999b2c41e4df57ccca7be
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2010-09-23 11:18:51 +0000

    iprt: build fixes
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@32708 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Runtime/r0drv/solaris/vbi/alloc-r0drv-solaris.c
+++ src/VBox/Runtime/r0drv/solaris/vbi/alloc-r0drv-solaris.c
@@ -4,7 +4,7 @@
  */
 
 /*
- * Copyright (C) 2006-2007 Oracle Corporation
+ * Copyright (C) 2006-2010 Oracle Corporation
  *
  * This file is part of VirtualBox Open Source Edition (OSE), as
  * available from http://www.virtualbox.org. This file is free software;
@@ -33,6 +33,7 @@
 #include <iprt/mem.h>
 
 #include <iprt/assert.h>
+#include <iprt/err.h>
 #include <iprt/log.h>
 #include <iprt/param.h>
 #include <iprt/thread.h>
