4e800ee880bb3f747954d9961f5fd8ad96de9d90
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2010-10-05 11:43:35 +0000

    Introducing RTThreadGetExecutionTimeMilli
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@32905 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Runtime/r3/win/thread-win.cpp
+++ src/VBox/Runtime/r3/win/thread-win.cpp
@@ -222,3 +222,19 @@ RTR3DECL(uint64_t) RTThreadGetAffinity(void)
     return RTErrConvertFromWin32(iLastError);
 }
 
+
+RTR3DECL(int) RTThreadGetExecutionTimeMilli(RTTHREAD hThread, uint64_t *pKernelTime, uint64_t *pUserTime)
+{
+    uint64_t u64CreationTime, u64ExitTime, u64KernelTime, u64UserTime;
+
+    if (GetThreadTimes(hThread, (LPFILETIME)&u64CreationTime, (LPFILETIME)&u64ExitTime, (LPFILETIME)&u64KernelTime, (LPFILETIME)&u64UserTime))
+    {
+        *pKernelTime = u64KernelTime / 10000;    /* GetThreadTimes returns time in 100 ns units */
+        *pUserTime   = u64UserTime / 10000;    /* GetThreadTimes returns time in 100 ns units */
+        return VINF_SUCCESS;
+    }
+
+    int iLastError = GetLastError();
+    AssertMsgFailed(("GetThreadTimes failed, LastError=%d\n", iLastError));
+    return RTErrConvertFromWin32(iLastError);
+}
