334cd2cdfa356ce5a1f5699bcdb6e6a26226c19c
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2010-10-06 14:39:02 +0000

    tstSemEvent -> tstRTSemEventMulti; removed tstSems.
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@32949 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Runtime/testcase/tstRTSemEventMulti.cpp
+++ src/VBox/Runtime/testcase/tstRTSemEventMulti.cpp
@@ -1,10 +1,10 @@
 /* $Id$ */
 /** @file
- * IPRT Testcase - Event Semaphore Test.
+ * IPRT Testcase - Multiple Release Event Semaphores.
  */
 
 /*
- * Copyright (C) 2009 Oracle Corporation
+ * Copyright (C) 2009-2010 Oracle Corporation
  *
  * This file is part of VirtualBox Open Source Edition (OSE), as
  * available from http://www.virtualbox.org. This file is free software;
