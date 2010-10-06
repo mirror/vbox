d1304f2a19e7f0f121d8a85fd3c854b7015164c3
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2010-10-06 06:42:27 +0000

    Main/HostImpl: Solaris DVD enumeration fix for USB CD/DVD drives.
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@32931 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Main/HostImpl.cpp
+++ src/VBox/Main/HostImpl.cpp
@@ -2004,11 +2004,14 @@ static int solarisWalkDeviceNodeForDVD(di_node_t Node, void *pvArg)
 {
     PSOLARISDVD *ppDrives = (PSOLARISDVD *)pvArg;
 
-    char *pszClass = NULL;
-    if (   di_prop_lookup_strings(DDI_DEV_T_ANY, Node, "class", &pszClass) > 0
-        && !strcmp(pszClass, "scsi"))                                                   /* SCSI */
+    /*
+     * Check for removable media instead of "SCSI" so that we also include USB CD-ROMs.
+     * As unfortunately the Solaris drivers only export these common properties.
+     */
+    int *pInt = NULL;
+    if (   di_prop_lookup_ints(DDI_DEV_T_ANY, Node, "removable-media", &pInt) > 0
+        && *pInt == 1)                                                                  /* Removable Media */
     {
-        int *pInt = NULL;
         if (di_prop_lookup_ints(DDI_DEV_T_ANY, Node, "inquiry-device-type", &pInt) > 0
             && (   *pInt == DTYPE_RODIRECT                                              /* CDROM */
                 || *pInt == DTYPE_OPTICAL))                                             /* Optical Drive */
