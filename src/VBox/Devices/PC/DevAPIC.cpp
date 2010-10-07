4d1d2dd699042dccd96ae9b21004ae3317c844f9
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2010-10-07 15:16:54 +0000

    DevAPIC.cpp: The count_shift reset value shall be 1 not 0, because that is what divide_conf translates to (see Figure 10-10 in intel manual 3A).
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@32985 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Devices/PC/DevAPIC.cpp
+++ src/VBox/Devices/PC/DevAPIC.cpp
@@ -1104,7 +1104,7 @@ static void apic_init_ipi(APICDeviceInfo* dev, APICState *s)
     s->esr = 0;
     memset(s->icr, 0, sizeof(s->icr));
     s->divide_conf = 0;
-    s->count_shift = 0;
+    s->count_shift = 1;
     s->initial_count = 0;
     s->initial_count_load_time = 0;
     s->next_time = 0;
