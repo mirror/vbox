5b472234beb85f9c870dd58d83898fdafa1f454c
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2010-10-25 11:15:56 +0000

    Network/SrvIntNetR0: log release VERR_INTNET_INCOMPATIBLE_TRUNK failures.
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@33410 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Devices/Network/SrvIntNetR0.cpp
+++ src/VBox/Devices/Network/SrvIntNetR0.cpp
@@ -5202,7 +5202,11 @@ static int intnetR0OpenNetwork(PINTNET pIntNet, PSUPDRVSESSION pSession, const c
                     rc = VERR_INTNET_INCOMPATIBLE_FLAGS;
             }
             else
+            {
                 rc = VERR_INTNET_INCOMPATIBLE_TRUNK;
+                LogRel(("intnetR0OpenNetwork failed. rc=%d pCur->szTrunk=%s pszTrunk=%s pCur->enmTrunkType=%d enmTrunkType=%d\n",
+                        rc, pCur->szTrunk, pszTrunk, pCur->enmTrunkType, enmTrunkType));
+            }
 
             LogFlow(("intnetR0OpenNetwork: returns %Rrc *ppNetwork=%p\n", rc, *ppNetwork));
             return rc;
