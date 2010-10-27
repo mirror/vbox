8b65c4901ea21b548769cf2ad810223c2e5af13d
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2010-10-27 09:04:30 +0000

    Additional checks
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@33486 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Devices/Storage/ATAController.cpp
+++ src/VBox/Devices/Storage/ATAController.cpp
@@ -4705,7 +4705,17 @@ int ataControllerIOPortReadStr1(PAHCIATACONTROLLER pCtl, RTIOPORT Port, RTGCPTR 
 
         PPDMDEVINS pDevIns = pCtl->CTX_SUFF(pDevIns);
         rc = PGMPhysSimpleDirtyWriteGCPtr(PDMDevHlpGetVMCPU(pDevIns), GCDst, s->CTX_SUFF(pbIOBuffer) + s->iIOBufferPIODataStart, cbTransfer);
+#ifndef IN_RING3
+        /* Paranoia. */
+        if (RT_FAILURE(rc))
+        {
+            PDMCritSectLeave(&pCtl->lock);
+            AssertFailed();
+            return VINF_IOM_HC_IOPORT_READ;
+        }
+#else
         Assert(rc == VINF_SUCCESS);
+#endif
 
         if (cbTransfer)
             Log3(("%s: addr=%#x val=%.*Rhxs\n", __FUNCTION__, Port, cbTransfer, s->CTX_SUFF(pbIOBuffer) + s->iIOBufferPIODataStart));
@@ -4758,10 +4768,11 @@ int ataControllerIOPortWriteStr1(PAHCIATACONTROLLER pCtl, RTIOPORT Port, RTGCPTR
         PPDMDEVINS pDevIns = pCtl->CTX_SUFF(pDevIns);
         rc = PGMPhysSimpleReadGCPtr(PDMDevHlpGetVMCPU(pDevIns), s->CTX_SUFF(pbIOBuffer) + s->iIOBufferPIODataStart, GCSrc, cbTransfer);
 #ifndef IN_RING3
-        /* This can fail in RC if the page table is not present for example. */
+        /* Paranoia. */
         if (RT_FAILURE(rc))
         {
             PDMCritSectLeave(&pCtl->lock);
+            AssertFailed();
             return VINF_IOM_HC_IOPORT_WRITE;
         }
 #else
