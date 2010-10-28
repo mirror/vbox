6089950782921af38859bb3cb6cf1a930d30bba2
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2010-10-28 15:30:47 +0000

    IPRT/isofs: Fixed crash when parsing invalid .ISO file.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@33566 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Runtime/r3/isofs.cpp
+++ src/VBox/Runtime/r3/isofs.cpp
@@ -416,7 +416,8 @@ static int rtIsoFsResolvePath(PRTISOFSFILE pFile, const char *pszPath, uint32_t 
         if (!RTStrCmp(pszTemp, ".")) /* Root directory? Use first node! */
         {
             pNode = RTListNodeGetFirst(&pFile->listPaths, RTISOFSPATHTABLEENTRY, Node);
-            bFound = true;
+            if (pNode)
+                bFound = true;
         }
         else
         {
@@ -432,9 +433,12 @@ static int rtIsoFsResolvePath(PRTISOFSFILE pFile, const char *pszPath, uint32_t 
         }
         if (bFound)
         {
+            AssertPtr(pNode);
             *puSector = pNode->header.sector_dir_table;
             rc = VINF_SUCCESS;
         }
+        else
+            rc = VERR_FILE_NOT_FOUND;
         RTStrFree(pszTemp);
     }
     else
