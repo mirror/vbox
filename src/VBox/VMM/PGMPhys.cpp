0b5cd1ddfab1970429ecffddc902f71a411652f5
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2010-10-28 10:13:34 +0000

    Bump the timeout count to 10
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@33544 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/VMM/PGMPhys.cpp
+++ src/VBox/VMM/PGMPhys.cpp
@@ -3927,7 +3927,7 @@ VMMR3DECL(int) PGMR3PhysAllocateLargeHandyPage(PVM pVM, RTGCPHYS GCPhys)
 
         if (u64TimeStamp2 - u64TimeStamp1 > 100)
         {
-            if (++cTimeOut > 5)
+            if (++cTimeOut > 10)
             {
                 /* If repeated attempts to allocate a large page takes more than 100 ms, then we fall back to normal 4k pages.
                  * E.g. Vista 64 tries to move memory around, which takes a huge amount of time.
