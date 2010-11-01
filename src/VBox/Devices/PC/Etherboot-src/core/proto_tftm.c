8d9083544b9d0e51652f5716ad03b78208b38eaf
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2010-11-01 14:18:11 +0000

    *: rebrand Sun (L)GPL disclaimers
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@33656 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Devices/PC/Etherboot-src/core/proto_tftm.c
+++ src/VBox/Devices/PC/Etherboot-src/core/proto_tftm.c
@@ -32,8 +32,8 @@
 ***************************************************************************/
 
 /*
- * Sun GPL Disclaimer: For the avoidance of doubt, except that if any license choice
- * other than GPL or LGPL is available it will apply instead, Sun elects to use only
+ * Oracle GPL Disclaimer: For the avoidance of doubt, except that if any license choice
+ * other than GPL or LGPL is available it will apply instead, Oracle elects to use only
  * the General Public License version 2 (GPLv2) at this time for any software where
  * a choice of GPL license versions is made available with the language indicating
  * that GPLv2 or any later version may be used, or where a choice of which version
