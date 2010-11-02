0831cb4ae0c1e5c5f3facb7832413273d32fc281
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2010-11-02 18:21:28 +0000

    comment update
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@33704 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Runtime/common/log/logrel.cpp
+++ src/VBox/Runtime/common/log/logrel.cpp
@@ -1,10 +1,10 @@
 /* $Id$ */
 /** @file
- * Runtime VBox - Logger.
+ * Runtime VBox - Release Logger.
  */
 
 /*
- * Copyright (C) 2006-2007 Oracle Corporation
+ * Copyright (C) 2006-2010 Oracle Corporation
  *
  * This file is part of VirtualBox Open Source Edition (OSE), as
  * available from http://www.virtualbox.org. This file is free software;
