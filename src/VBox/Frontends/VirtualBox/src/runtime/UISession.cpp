09cfe1ca76066bb9ca489b4c0b0500682f663916
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2010-11-08 14:02:49 +0000

    FE/Qt: Put progress-bar error-warning into prevent-auto-close guard block.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@33844 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Frontends/VirtualBox/src/runtime/UISession.cpp
+++ src/VBox/Frontends/VirtualBox/src/runtime/UISession.cpp
@@ -206,10 +206,6 @@ void UISession::powerUp()
     else
         vboxProblem().showModalProgressDialog(progress, machine.GetName(), mainMachineWindow());
 
-    /* Allow further auto-closing: */
-    if (uimachine()->machineLogic())
-        uimachine()->machineLogic()->setPreventAutoClose(false);
-
     /* Check for a progress failure: */
     if (progress.GetResultCode() != 0)
     {
@@ -218,6 +214,10 @@ void UISession::powerUp()
         QTimer::singleShot(0, this, SLOT(sltCloseVirtualSession()));
         return;
     }
+
+    /* Allow further auto-closing: */
+    if (uimachine()->machineLogic())
+        uimachine()->machineLogic()->setPreventAutoClose(false);
 
     /* Check if we missed a really quick termination after successful startup, and process it if we did: */
     if (isTurnedOff())
