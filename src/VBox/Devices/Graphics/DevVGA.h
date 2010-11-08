8bde85fc3150a6df062cb8fbfcb524a243322bd1
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2010-11-08 15:37:09 +0000

    VGA: 64-bit build fix
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@33855 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Devices/Graphics/DevVGA.h
+++ src/VBox/Devices/Graphics/DevVGA.h
@@ -399,6 +399,7 @@ typedef struct VGAState {
 
     /** Retrace emulation state */
     bool                        fRealRetrace;
+    bool                        Padding5[HC_ARCH_BITS == 64 ? 7 : 3];
     vga_retrace_s               retrace_state;
 
 #ifdef VBE_NEW_DYN_LIST
@@ -408,7 +409,7 @@ typedef struct VGAState {
     uint16_t                    cbVBEExtraData;
     /** The VBE BIOS current memory address. */
     uint16_t                    u16VBEExtraAddress;
-    uint16_t                    Padding5[2];
+    uint16_t                    Padding6[2];
 #endif
     /** Current logo data offset. */
     uint32_t                    offLogoData;
@@ -438,7 +439,7 @@ typedef struct VGAState {
     uint16_t                    cLogoPalEntries;
     /** Clear screen flag. */
     uint8_t                     fLogoClearScreen;
-    uint8_t                     Padding6[7];
+    uint8_t                     Padding7[7];
     /** Palette data. */
     uint32_t                    au32LogoPalette[256];
     /** The VGA BIOS ROM data. */
@@ -452,12 +453,12 @@ typedef struct VGAState {
     /** Base port in the assigned PCI I/O space. */
     RTIOPORT                    IOPortBase;
 #ifdef VBOX_WITH_WDDM
-    uint8_t                     Padding7[2];
+    uint8_t                     Padding8[2];
     /* specifies guest driver caps, i.e. whether it can handle IRQs from the adapter,
      * the way it can handle async HGSMI command completion, etc. */
     uint32_t                    fGuestCaps;
 #else
-    uint8_t                     Padding7[6];
+    uint8_t                     Padding8[6];
 #endif
 #endif /* VBOX_WITH_HGSMI */
 } VGAState;
