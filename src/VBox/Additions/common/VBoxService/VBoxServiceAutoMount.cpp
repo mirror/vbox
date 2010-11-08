6205cf9dd2ccbf62988aa7ec9b9340a16515b003
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2010-11-08 15:55:05 +0000

    Another fix.
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@33858 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Additions/common/VBoxService/VBoxServiceAutoMount.cpp
+++ src/VBox/Additions/common/VBoxService/VBoxServiceAutoMount.cpp
@@ -114,9 +114,9 @@ static bool VBoxServiceAutoMountShareIsMounted(const char *pszShare,
         mnttab mntTab;
         while ((getmntent(pFh, &mntTab)))
         {
-            if (!RTStrICmp(mntTab->mnt_special, pszShare))
+            if (!RTStrICmp(mntTab.mnt_special, pszShare))
             {
-                fMounted = RTStrPrintf(pszMountPoint, cbMountPoint, "%s", pMntEnt->mnt_mountp)
+                fMounted = RTStrPrintf(pszMountPoint, cbMountPoint, "%s", mntTab.mnt_mountp)
                          ? true : false;
                 break;
             }
