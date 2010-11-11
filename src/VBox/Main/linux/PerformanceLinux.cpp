75c08bb047e3f17338938516395044327b6a3b49
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2010-11-11 13:57:03 +0000

    Grrr
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@33992 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Main/linux/PerformanceLinux.cpp
+++ src/VBox/Main/linux/PerformanceLinux.cpp
@@ -193,7 +193,7 @@ int CollectorLinux::getRawProcessStats(RTPROCESS process, uint64_t *cpuUser, uin
 
     if (f)
     {
-        if (fscanf(f, "%d %80s %c %d %d %d %d %d %u %lu %lu %lu %lu %u %u "
+        if (fscanf(f, "%d %79s %c %d %d %d %d %d %u %lu %lu %lu %lu %u %u "
                       "%ld %ld %ld %ld %ld %ld %llu %lu %u",
                    &pid2, buf, &c, &iTmp, &iTmp, &iTmp, &iTmp, &iTmp, &uTmp,
                    &ulTmp, &ulTmp, &ulTmp, &ulTmp, &u32user, &u32kernel,
