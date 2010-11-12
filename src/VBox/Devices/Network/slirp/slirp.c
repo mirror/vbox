409b8f393e2a686df839b9ff464861ad4b353c10
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2010-11-12 19:04:49 +0000

    NAT: warning.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@34043 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Devices/Network/slirp/slirp.c
+++ src/VBox/Devices/Network/slirp/slirp.c
@@ -1549,7 +1549,7 @@ void if_encap(PNATState pData, uint16_t eth_proto, struct mbuf *m, int flags)
     m->m_data -= ETH_HLEN;
     m->m_len += ETH_HLEN;
     eh = mtod(m, struct ethhdr *);
-    mlen = m->m_len;//m_length(m, NULL);
+    mlen = m->m_len;
 
     if (memcmp(eh->h_source, special_ethaddr, ETH_ALEN) != 0)
     {
