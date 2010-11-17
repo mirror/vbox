e15ae0b37170bd9fa6b3982973d43e452ce04f7e
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2010-11-17 20:46:42 +0000

    Storage: a few warnings and a typo
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@34147 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Storage/VHD.cpp
+++ src/VBox/Storage/VHD.cpp
@@ -2150,7 +2150,6 @@ static int vhdSetComment(void *pBackendData, const char *pszComment)
     else
         rc = VERR_VD_NOT_OPENED;
 
-out:
     LogFlowFunc(("returns %Rrc\n", rc));
     return rc;
 }
@@ -2885,7 +2884,7 @@ static int vhdResize(void *pBackendData, uint64_t cbSize,
         {
             /* Calculate how many sectors nee to be relocated. */
             uint64_t cbOverlapping = offStartDataNew - offStartDataOld;
-            unsigned cBlocksReloc = cbOverlapping / cbBlock;
+            unsigned cBlocksReloc = (unsigned)(cbOverlapping / cbBlock);
             if (cbOverlapping % cbBlock)
                 cBlocksReloc++;
 
