46faf2ab1fd7d3ffc8797ed0c0811326523b9279
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2010-11-17 20:05:36 +0000

    Additions/Solaris/Virtio: export to OSE
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@34143 cfe28804-0f27-0410-a406-dd0f0b0b656f

---
+++ src/VBox/Additions/solaris/Virtio/VirtioPci-solaris.h
@@ -0,0 +1,21 @@
+/* $Id$ */
+/** @file
+ * VirtualBox Guest Additions: Virtio Driver for Solaris, PCI Hypervisor Interface.
+ */
+
+/*
+ * Copyright (C) 2010 Oracle Corporation
+ *
+ * Oracle Corporation confidential
+ * All rights reserved
+ */
+
+#ifndef ___VirtioPCI_solaris_h
+#define ___VirtioPCI_solaris_h
+
+#include "Virtio-solaris.h"
+
+extern VIRTIOHYPEROPS g_VirtioHyperOpsPci;
+
+#endif
+
