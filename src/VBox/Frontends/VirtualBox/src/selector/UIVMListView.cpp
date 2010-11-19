a64f5cc8bdd3dfa93568ec6fcc0154bafbc178e3
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2010-11-19 14:49:32 +0000

    FE/Qt4: tmp; create the QPixmap ourself
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@34202 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Frontends/VirtualBox/src/selector/UIVMListView.cpp
+++ src/VBox/Frontends/VirtualBox/src/selector/UIVMListView.cpp
@@ -596,10 +596,16 @@ QPixmap UIVMListView::dragPixmap(const QModelIndex &index) const
     p.setCompositionMode(QPainter::CompositionMode_DestinationIn);
     p.fillRect(image.rect(), QColor(0, 0, 0, 177));
     p.end();
+    QPixmap i1(s);
+    i1.fill(Qt::transparent);
+    QPainter p1(&i1);
+    p1.drawImage(image.rect(), image);
+    p1.end();
 #ifdef DEBUG
     image.save("test.png", "PNG");
+    i1.save("test_pixmap.png", "PNG");
 #endif
-    return QPixmap::fromImage(image);
+    return i1;//QPixmap::fromImage(image);
 }
 
 QModelIndex UIVMListView::moveCursor(QAbstractItemView::CursorAction cursorAction, Qt::KeyboardModifiers modifiers)
