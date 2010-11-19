e77cbed14c7358e4ba79cb4d63f78200661e7d40
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2010-11-19 13:18:50 +0000

    dbgfR3CoreWrite: Must use PGMPhysSimpleReadGCPhys because PGMPhysRead has sideffects on read access handled pages and runs into trouble with MMIO.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@34197 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/VMM/DBGFCoreWrite.cpp
+++ src/VBox/VMM/DBGFCoreWrite.cpp
@@ -447,19 +447,20 @@ static int dbgfR3CoreWriteWorker(PVM pVM, PDBGFCOREDATA pDbgfData, RTFILE hFile)
 
         /*
          * Write page-by-page of this memory range.
+         *
+         * The read function may fail on MMIO ranges, we write these as zero
+         * pages for now (would be nice to have the VGA bits there though).
          */
         uint64_t cbMemRange  = GCPhysEnd - GCPhysStart + 1;
         uint64_t cPages      = cbMemRange >> PAGE_SHIFT;
         for (uint64_t iPage = 0; iPage < cPages; iPage++)
         {
             uint8_t abPage[PAGE_SIZE];
-            rc = PGMPhysRead(pVM, GCPhysStart + (iPage << PAGE_SHIFT), abPage, sizeof(abPage));
+            rc = PGMPhysSimpleReadGCPhys(pVM, abPage, GCPhysStart + (iPage << PAGE_SHIFT),  sizeof(abPage));
             if (RT_FAILURE(rc))
             {
-                /*
-                 * For some reason this failed, write out a zero page instead.
-                 */
-                LogRel((DBGFLOG_NAME ": PGMPhysRead failed for iRange=%u iPage=%u. rc=%Rrc. Ignoring...\n", iRange, iPage, rc));
+                if (rc != VERR_PGM_PHYS_PAGE_RESERVED)
+                    LogRel((DBGFLOG_NAME ": PGMPhysRead failed for iRange=%u iPage=%u. rc=%Rrc. Ignoring...\n", iRange, iPage, rc));
                 RT_ZERO(abPage);
             }
 
