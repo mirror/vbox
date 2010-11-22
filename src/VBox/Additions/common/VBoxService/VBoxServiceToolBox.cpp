06ebb092782ef372cc25cd9fedad172e7bb4b87f
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2010-11-22 15:17:43 +0000

    oops.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@34249 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Additions/common/VBoxService/VBoxServiceToolBox.cpp
+++ src/VBox/Additions/common/VBoxService/VBoxServiceToolBox.cpp
@@ -353,7 +353,7 @@ int VBoxServiceToolboxMain(int argc, char **argv)
      *  and IPRT status code.  That doesn't cut it.  The RTEXITCODE part should
      *  be returned separately from the handled-or-unhandled bit.
      *
-     *  Also, please change VBoxServiceToolboxCat and VBoxServiceToolboxCat to
+     *  Also, please change VBoxServiceToolboxCat and VBoxServiceToolboxMkDir to
      *  return RTEXITCODE and use RTMsg* like RTZipTarCmd (and later
      *  RTZipGzipCmd). */
     int rc = VERR_NOT_FOUND;
@@ -367,7 +367,7 @@ int VBoxServiceToolboxMain(int argc, char **argv)
         else if (   !strcmp(argv[0], "mkdir")
                  || !strcmp(argv[0], "vbox_mkdir"))
         {
-            rc = VBoxServiceToolboxCat(argc, argv);
+            rc = VBoxServiceToolboxMkDir(argc, argv);
         }
     }
 
