16f546b18abec38e00c809fab03e822b24613d50
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2010-11-24 12:45:20 +0000

    Added VBoxRes.dll for shared resources which are presented to the running (guest) OS.
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@34314 cfe28804-0f27-0410-a406-dd0f0b0b656f

---
+++ src/VBox/Installer/win/Resources/resource.h
@@ -0,0 +1,19 @@
+/* $Id$ */
+/** @file
+ * resource.h - resource header file.
+ */
+
+/*
+ * Copyright (C) 2010 Oracle Corporation
+ *
+ * Oracle Corporation confidential
+ * All rights reserved
+ */
+
+#define IDI_VIRTUALBOX 							101
+
+#define IDI_FILETYPE_VBOX 					201
+#define IDI_FILETYPE_VBOX_EXTPACK 	202
+
+#define IDI_FILETYPE_VBOX_OVF			 	301
+#define IDI_FILETYPE_VBOX_OVA			 	302
