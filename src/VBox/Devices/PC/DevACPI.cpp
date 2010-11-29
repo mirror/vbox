9b290dbe09bb2bf0f86fe9b66961c0d48697b67f
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2010-11-29 19:33:17 +0000

    ACPI: allow to configure SCI if not yet configured (reset)
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@34486 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Devices/PC/DevACPI.cpp
+++ src/VBox/Devices/PC/DevACPI.cpp
@@ -2413,7 +2413,7 @@ static void acpiPciConfigWrite(PPCIDEVICE pPciDev, uint32_t Address, uint32_t u3
     Log2(("acpi: PCI config write: 0x%x -> 0x%x (%d)\n", u32Value, Address, cb));
 
 
-    if (Address == VBOX_PCI_INTERRUPT_LINE)
+    if (Address == VBOX_PCI_INTERRUPT_LINE && PCIDevGetInterruptLine(pPciDev) != 0)
     {
         Log(("acpi: ignore interrupt line settings: %x, we'll use hardcoded value %x\n", u32Value, PCIDevGetInterruptLine(pPciDev)));
         return;
