fa215836be9b732166cc4163c7aea7f66a66de0d
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2010-11-29 19:40:38 +0000

    one more
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@34489 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Devices/PC/DevLPC.cpp
+++ src/VBox/Devices/PC/DevLPC.cpp
@@ -258,7 +258,7 @@ static DECLCALLBACK(int) lpcConstruct(PPDMDEVINS pDevIns, int iInstance, PCFGMNO
     pThis->dev.config[0x4e] = 0x03;
     pThis->dev.config[0x4f] = 0x00;
 
-    /* 60h–63h PIRQ[n]_ROUT PIRQ[A-D] Routing Control */
+    /* 60h-63h PIRQ[n]_ROUT PIRQ[A-D] Routing Control */
     pThis->dev.config[0x60] = 0x0b; /* PCI A -> IRQ 11 */
     pThis->dev.config[0x61] = 0x09; /* PCI B -> IRQ 9  */
     pThis->dev.config[0x62] = 0x0b; /* PCI C -> IRQ 11 */
