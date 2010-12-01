4a14359527b71fe38e4026e30090dd1747063251
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2010-12-01 14:23:21 +0000

    deb/rpm: adapted USB udev rules according to r68230; icons and mime (rpm needs some more work)
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@34572 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Installer/linux/VBoxCreateUSBNode.sh
+++ src/VBox/Installer/linux/VBoxCreateUSBNode.sh
@@ -19,18 +19,22 @@ esac
 bus=`expr "$2" '/' 128 + 1`
 device=`expr "$2" '%' 128 + 1`
 class="$3"
+group="$4"
 if test "$class" -eq 9; then
   exit 0
 fi
 devdir="`printf "/dev/vboxusb/%.3d" $bus`"
 devpath="`printf "/dev/vboxusb/%.3d/%.3d" $bus $device`"
 if test "$do_remove" -eq 0; then
+  if test -z "$group"; then
+    group="vboxusers"
+  fi
   mkdir /dev/vboxusb -m 0750 2>/dev/null
-  chown root:vboxusers /dev/vboxusb 2>/dev/null
+  chown root:$group /dev/vboxusb 2>/dev/null
   mkdir "$devdir" -m 0750 2>/dev/null
-  chown root:vboxusers "$devdir" 2>/dev/null
+  chown root:$group "$devdir" 2>/dev/null
   mknod "$devpath" c $1 $2 -m 0660 2>/dev/null
-  chown root:vboxusers "$devpath" 2>/dev/null
+  chown root:$group "$devpath" 2>/dev/null
 else
   rm -f "$devpath"
 fi
