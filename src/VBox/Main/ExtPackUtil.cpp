90b7c3d909539b523cd8728bf0acef0725feb30f
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2010-12-10 17:52:01 +0000

    Moved and extended RTVfsIoStrmValidateUtf8Encoding.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@34967 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Main/ExtPackUtil.cpp
+++ src/VBox/Main/ExtPackUtil.cpp
@@ -802,7 +802,9 @@ static int VBoxExtPackValidateStandardFile(const char *pszAdjName, RTVFSOBJTYPE 
                 rc = RTVfsMemorizeIoStreamAsFile(hVfsIos, RTFILE_O_READ, &hVfsFile);
                 if (RT_SUCCESS(rc))
                 {
-                    rc = RTVfsIoStrmValidateUtf8Encoding(hVfsIos);
+                    rc = RTVfsIoStrmValidateUtf8Encoding(hVfsIos,
+                                                         RTVFS_VALIDATE_UTF8_BY_RTC_3629 | RTVFS_VALIDATE_UTF8_NO_NULL,
+                                                         NULL);
                     if (RT_SUCCESS(rc))
                     {
                         /*
