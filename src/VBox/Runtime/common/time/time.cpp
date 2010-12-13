3a7e90022f773a0904c8ba34479f9b393504cb8e
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2010-12-13 12:58:47 +0000

    Backed out r68812, r68813 and r68816 - issue will be resolved with correct CRT usage instead.
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@34999 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Runtime/common/time/time.cpp
+++ src/VBox/Runtime/common/time/time.cpp
@@ -33,7 +33,6 @@
 #include "internal/iprt.h"
 
 #include <iprt/string.h>
-#include <iprt/asm-math.h>
 #include <iprt/assert.h>
 #include "internal/time.h"
 
@@ -272,8 +271,8 @@ RTDECL(PRTTIME) RTTimeExplode(PRTTIME pTime, PCRTTIMESPEC pTimeSpec)
      */
     pTime->fFlags = RTTIME_FLAGS_TYPE_UTC;
     i64Div = pTimeSpec->i64NanosecondsRelativeToUnixEpoch;
-    i32Rem = (int32_t)ASMModU64ByU32RetU32(i64Div, 1000000000);
-    i64Div = (int64_t)ASMDivS64ByS32RetS32(i64Div, 1000000000);
+    i32Rem = (int32_t)(i64Div % 1000000000);
+    i64Div /= 1000000000;
     if (i32Rem < 0)
     {
         i32Rem += 1000000000;
@@ -282,7 +281,7 @@ RTDECL(PRTTIME) RTTimeExplode(PRTTIME pTime, PCRTTIMESPEC pTimeSpec)
     pTime->u32Nanosecond = i32Rem;
 
     /* second */
-    i32Rem = (int32_t)ASMModU64ByU32RetU32(i64Div, 60);
+    i32Rem = (int32_t)(i64Div % 60);
     i64Div /= 60;
     if (i32Rem < 0)
     {
