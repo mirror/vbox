3eef1232ad95f294b023912cfb29d4bac0b7ba1d
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2010-12-13 11:36:38 +0000

    Main/NATEngine: return proper error info if a NAT rule cannot be registered
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@34992 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Main/NATEngineImpl.cpp
+++ src/VBox/Main/NATEngineImpl.cpp
@@ -286,12 +286,13 @@ NATEngine::AddRedirect(IN_BSTR aName, NATProtocol_T aProto, IN_BSTR aBindIp, USH
     for (it = mNATRules.begin(); it != mNATRules.end(); ++it)
     {
         r = it->second;
-        if (   it->first == name
-            || (   r.strHostIP == Utf8Str(aBindIp)
-                && r.u16HostPort == aHostPort))
-        {
-            return E_INVALIDARG;
-        }
+        if (it->first == name)
+            return setError(E_INVALIDARG,
+                            tr("A NAT rule of this name does already exist"));
+        if (   r.strHostIP == Utf8Str(aBindIp)
+            && r.u16HostPort == aHostPort)
+            return setError(E_INVALIDARG,
+                            tr("A NAT rule for this host port and this host IP does already exist"));
     }
 
     r.strName = name.c_str();
