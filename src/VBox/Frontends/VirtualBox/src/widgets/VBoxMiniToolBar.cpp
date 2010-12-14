105cc1271f05dae539a41fdb45c5751e7e6b02f8
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2010-12-14 11:21:12 +0000

    FE/Qt: Mini-toolbar auto-fill-background property to make it use own background color instead of parent's color.
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@35064 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Frontends/VirtualBox/src/widgets/VBoxMiniToolBar.cpp
+++ src/VBox/Frontends/VirtualBox/src/widgets/VBoxMiniToolBar.cpp
@@ -125,6 +125,9 @@ VBoxMiniToolBar::VBoxMiniToolBar(QWidget *pParent, Alignment alignment, bool fAc
 
     /* Enable mouse-tracking for this & children allowing to get mouse-move events: */
     setMouseTrackingEnabled(m_fAutoHide);
+
+    /* Auto-fill background to make it use own color: */
+    setAutoFillBackground(true);
 }
 
 /* Appends passed menus into internal menu-list */
