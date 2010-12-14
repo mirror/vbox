0f067f183e5aef5b49479377eb079c5a04628f51
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2010-12-14 12:27:11 +0000

    Installer/solaris: shift mime/desktop update around to avoid confusing installer output
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@35069 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Installer/solaris/vboxconfig.sh
+++ src/VBox/Installer/solaris/vboxconfig.sh
@@ -795,16 +795,6 @@ postinstall()
             fi
         fi
 
-        if test -d $PKG_INSTALL_ROOT/usr/share/icons; then
-            infoprint "Installing MIME types and icons"
-            if test "$REMOTEINST" -eq 0; then
-                /usr/bin/update-mime-database /usr/share/mime 2>/dev/null
-                /usr/bin/update-desktop-database -q 2>/dev/null
-            else
-                subprint "Skipped for targetted installs."
-            fi
-        fi
-
         # Enable Zone access service for non-remote installs, other services (Webservice) are delivered disabled by the manifest class action
         if test "$REMOTEINST" -eq 0; then
             servicefound=`$BIN_SVCS -a | grep "virtualbox/zoneaccess" | grep "disabled" 2>/dev/null`
@@ -815,6 +805,19 @@ postinstall()
                 else
                     subprint "Loading Zone access service  ...FAILED."
                 fi
+            fi
+        fi
+
+        # Update mime and desktop databases to get the right menu entries
+        # and icons. There is still some delay until the GUI picks it up,
+        # but that cannot be helped.
+        if test -d $PKG_INSTALL_ROOT/usr/share/icons; then
+            infoprint "Installing MIME types and icons"
+            if test "$REMOTEINST" -eq 0; then
+                /usr/bin/update-mime-database /usr/share/mime 2>/dev/null
+                /usr/bin/update-desktop-database -q 2>/dev/null
+            else
+                subprint "Skipped for targetted installs."
             fi
         fi
 
