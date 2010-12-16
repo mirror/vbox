2b003e4b553d9dff20b39246a1f215841e7bb27f
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2010-12-16 21:59:30 +0000

    Additions/x11/vboxvideo: fail in less places if /dev/vboxguest is not available
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@35207 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Additions/x11/vboxvideo/vboxvideo.h
+++ src/VBox/Additions/x11/vboxvideo/vboxvideo.h
@@ -258,8 +258,9 @@ extern Bool vboxGetDisplayChangeRequest(ScrnInfoPtr pScrn, uint32_t *pcx,
 extern Bool vboxHostLikesVideoMode(ScrnInfoPtr pScrn, uint32_t cx, uint32_t cy, uint32_t cBits);
 extern Bool vboxSaveVideoMode(ScrnInfoPtr pScrn, uint32_t cx, uint32_t cy, uint32_t cBits);
 extern Bool vboxRetrieveVideoMode(ScrnInfoPtr pScrn, uint32_t *pcx, uint32_t *pcy, uint32_t *pcBits);
-extern void vboxGetPreferredMode(ScrnInfoPtr pScrn, uint32_t *pcx,
-                                 uint32_t *pcy, uint32_t *pcBits);
+extern void vboxGetPreferredMode(ScrnInfoPtr pScrn, uint32_t iScreen,
+                                 uint32_t *pcx, uint32_t *pcy,
+                                 uint32_t *pcBits);
 extern void vboxWriteHostModes(ScrnInfoPtr pScrn, DisplayModePtr pCurrent);
 extern void vboxAddModes(ScrnInfoPtr pScrn, uint32_t cxInit,
                          uint32_t cyInit);
