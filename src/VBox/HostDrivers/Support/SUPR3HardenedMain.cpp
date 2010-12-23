51f6367bc6088bdc34854c0b2fdbc30bc9aab53a
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2010-12-23 10:10:31 +0000

    SUPR3HardenedMain.cpp: next attempt at properly updating privileges on Solaris
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@35307 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/HostDrivers/Support/SUPR3HardenedMain.cpp
+++ src/VBox/HostDrivers/Support/SUPR3HardenedMain.cpp
@@ -648,36 +648,44 @@ static void supR3HardenedMainGrabCapabilites(void)
 
 # elif defined(RT_OS_SOLARIS)
     /*
-     * Add net_icmpaccess privilege to permitted, effective and inheritable
-     * privileges before dropping root privileges. Skip this hacky code for
-     * real root, as it removes lots of privileges due to the harcoded set.
+     * Add net_icmpaccess privilege to effective privileges and limit
+     * permitted privileges before completely dropping root privileges.
+     * This requires dropping root privileges temporarily to get the normal
+     * user's privileges.
      */
-    if (getuid() != 0)
+    seteuid(g_uid);
+    priv_set_t *pPrivEffective = priv_allocset();
+    priv_set_t *pPrivNewEffective = priv_allocset();
+    priv_set_t *pPrivNewPermitted = priv_allocset();
+    if (pPrivEffective && pPrivNewEffective && pPrivNewPermitted)
     {
-        priv_set_t *pPrivSet = priv_str_to_set("basic", ",", NULL);
-        if (pPrivSet)
+        int rc = getppriv(PRIV_EFFECTIVE, pPrivEffective);
+        seteuid(0);
+        if (!rc)
         {
-            priv_addset(pPrivSet, PRIV_NET_ICMPACCESS);
-            int rc = setppriv(PRIV_SET, PRIV_INHERITABLE, pPrivSet);
+            priv_copyset(pPrivEffective, pPrivNewEffective);
+            priv_copyset(pPrivEffective, pPrivNewPermitted);
+            rc = priv_addset(pPrivNewEffective, PRIV_NET_ICMPACCESS);
             if (!rc)
             {
-                rc = setppriv(PRIV_SET, PRIV_PERMITTED, pPrivSet);
-                if (!rc)
-                {
-                    rc = setppriv(PRIV_SET, PRIV_EFFECTIVE, pPrivSet);
-                    if (rc)
-                        supR3HardenedError(rc, false, "SUPR3HardenedMain: failed to set effective privilege set.\n");
-                }
-                else
-                    supR3HardenedError(rc, false, "SUPR3HardenedMain: failed to set permitted privilege set.\n");
+                /* Order is important, as one can't set a privilege which is
+                 * not in the permitted privilege set. We limit ourselves as
+                 * much as possible after grabbing the extra privileges. */
+                rc = setppriv(PRIV_SET, PRIV_EFFECTIVE, pPrivNewEffective);
+                if (rc)
+                    supR3HardenedError(rc, false, "SUPR3HardenedMain: failed to set effective privilege set.\n");
+                rc = setppriv(PRIV_SET, PRIV_PERMITTED, pPrivNewPermitted);
+                if (rc)
+                    supR3HardenedError(rc, false, "SUPR3HardenedMain: failed to set effective privilege set.\n");
             }
             else
-                supR3HardenedError(rc, false, "SUPR3HardenedMain: failed to set inheritable privilege set.\n");
-
-            priv_freeset(pPrivSet);
+                supR3HardenedError(rc, false, "SUPR3HardenedMain: failed to add NET_ICMPACCESS privilege.\n");
         }
-        else
-            supR3HardenedError(-1, false, "SUPR3HardenedMain: failed to get basic privilege set.\n");
+    }
+    else
+    {
+        /* for memory allocation failures just continue */
+        seteuid(0);
     }
 # endif
 }
