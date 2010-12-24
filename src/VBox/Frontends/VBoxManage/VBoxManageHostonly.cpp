06067a7ce4f13f156bbfeece3950a0bb88ed1fc2
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2010-12-24 14:44:54 +0000

    VBoxManage: host-only creation and removal enabled for all platforms except Solaris (#5439)
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@35316 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Frontends/VBoxManage/VBoxManageHostonly.cpp
+++ src/VBox/Frontends/VBoxManage/VBoxManageHostonly.cpp
@@ -44,7 +44,7 @@
 #ifndef VBOX_ONLY_DOCS
 using namespace com;
 
-#if defined(RT_OS_WINDOWS) && defined(VBOX_WITH_NETFLT)
+#if defined(VBOX_WITH_NETFLT) && (defined(RT_OS_WINDOWS) || defined(RT_OS_DARWIN) || defined(RT_OS_LINUX) || defined(RT_OS_FREEBSD))
 static int handleCreate(HandlerArg *a, int iStart, int *pcProcessed)
 {
 //    if (a->argc - iStart < 1)
@@ -296,7 +296,7 @@ int handleHostonlyIf(HandlerArg *a)
 //            else
 //                break;
         }
-#if defined(RT_OS_WINDOWS) && defined(VBOX_WITH_NETFLT)
+#if defined(VBOX_WITH_NETFLT) && (defined(RT_OS_WINDOWS) || defined(RT_OS_DARWIN) || defined(RT_OS_LINUX) || defined(RT_OS_FREEBSD))
         else if (strcmp(a->argv[i], "create") == 0)
         {
             int cProcessed;
