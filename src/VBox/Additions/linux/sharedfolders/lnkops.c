7af93b8fdc98e5b983e20fd19b5bccc748b338e8
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2011-01-11 08:49:44 +0000

    Additions/Linux/sharedfolders: OSE header fixes
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@35472 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Additions/linux/sharedfolders/lnkops.c
+++ src/VBox/Additions/linux/sharedfolders/lnkops.c
@@ -1,3 +1,21 @@
+/** @file
+ *
+ * vboxsf -- VirtualBox Guest Additions for Linux:
+ * Operations for symbolic links.
+ */
+
+/*
+ * Copyright (C) 2010-2011 Oracle Corporation
+ *
+ * This file is part of VirtualBox Open Source Edition (OSE), as
+ * available from http://www.virtualbox.org. This file is free software;
+ * you can redistribute it and/or modify it under the terms of the GNU
+ * General Public License (GPL) as published by the Free Software
+ * Foundation, in version 2 as it comes in the "COPYING" file of the
+ * VirtualBox OSE distribution. VirtualBox OSE is distributed in the
+ * hope that it will be useful, but WITHOUT ANY WARRANTY of any kind.
+ */
+
 #include "vfsmod.h"
 
 #if LINUX_VERSION_CODE >= KERNEL_VERSION(2, 6, 0)
