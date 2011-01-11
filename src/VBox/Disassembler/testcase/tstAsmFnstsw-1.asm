1ae7349eac87b21794610464b4096f10b521f726
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2011-01-11 09:08:30 +0000

    Disassembler/testcase: added OSE headers
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@35474 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Disassembler/testcase/tstAsmFnstsw-1.asm
+++ src/VBox/Disassembler/testcase/tstAsmFnstsw-1.asm
@@ -1,3 +1,24 @@
+; $Id: tstAsmLock-1.asm 60692 2010-04-27 08:22:32Z umoeller $
+;; @file
+; Disassembly testcase - Valid fnstsw* instructitons.
+;
+; This is a build test, that means it will be assembled, disassembled,
+; then the disassembly output will be assembled and the new binary will
+; compared with the original.
+;
+
+;
+; Copyright (C) 2008-2011 Oracle Corporation
+;
+; This file is part of VirtualBox Open Source Edition (OSE), as
+; available from http://www.virtualbox.org. This file is free software;
+; you can redistribute it and/or modify it under the terms of the GNU
+; General Public License (GPL) as published by the Free Software
+; Foundation, in version 2 as it comes in the "COPYING" file of the
+; VirtualBox OSE distribution. VirtualBox OSE is distributed in the
+; hope that it will be useful, but WITHOUT ANY WARRANTY of any kind.
+;
+
 %include "tstAsm.mac"
     BITS TEST_BITS
 
