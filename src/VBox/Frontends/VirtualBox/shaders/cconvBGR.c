db30bb7d4bf1cacb6a67660116cc28a4d9887844
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2011-01-11 09:02:07 +0000

    FE/Qt: OSE headers for shaders
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@35473 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Frontends/VirtualBox/shaders/cconvBGR.c
+++ src/VBox/Frontends/VirtualBox/shaders/cconvBGR.c
@@ -1,3 +1,22 @@
+/* $Id$ */
+/** @file
+ *
+ * VBox frontends: Qt4 GUI ("VirtualBox"):
+ * Shader programming for VBoxFBOverlay.
+ */
+
+/*
+ * Copyright (C) 2009-2011 Oracle Corporation
+ *
+ * This file is part of VirtualBox Open Source Edition (OSE), as
+ * available from http://www.virtualbox.org. This file is free software;
+ * you can redistribute it and/or modify it under the terms of the GNU
+ * General Public License (GPL) as published by the Free Software
+ * Foundation, in version 2 as it comes in the "COPYING" file of the
+ * VirtualBox OSE distribution. VirtualBox OSE is distributed in the
+ * hope that it will be useful, but WITHOUT ANY WARRANTY of any kind.
+ */
+
 #extension GL_ARB_texture_rectangle : enable
 uniform sampler2DRect uSrcTex;
 void vboxCConv()
