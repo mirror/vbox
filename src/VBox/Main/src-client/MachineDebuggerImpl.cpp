e22c29869168ff021b1346234b96b3d30251e662
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2011-01-12 14:50:03 +0000

    MachineDebugger::GetRegister: Added missing setError calls.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@35504 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Main/src-client/MachineDebuggerImpl.cpp
+++ src/VBox/Main/src-client/MachineDebuggerImpl.cpp
@@ -1160,6 +1160,14 @@ STDMETHODIMP MachineDebugger::GetRegister(ULONG a_idCpu, IN_BSTR a_bstrName, BST
                     hrc = E_OUTOFMEMORY;
                 }
             }
+            else if (vrc == VERR_DBGF_REGISTER_NOT_FOUND)
+                hrc = setError(E_FAIL, tr("Register '%s' was not found"), strName.c_str());
+            else if (vrc == VERR_INVALID_CPU_ID)
+                hrc = setError(E_FAIL, tr("Invalid CPU ID: %u"), a_idCpu);
+            else
+                hrc = setError(VBOX_E_VM_ERROR,
+                               tr("DBGFR3RegNmQuery failed with rc=%Rrc querying register '%s' with default cpu set to %u"),
+                               vrc, strName.c_str(), a_idCpu);
         }
     }
 
