3ce7b3a3d83b2e9061e132e01b9cffc38640b663
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2011-01-13 15:42:16 +0000

    VBoxExtPackHelperApp.cpp: Fixed issue with non-ascii characters in the argument list on windows that prevent installing extension packs residing in directory with non-ascii names.
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@35542 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Main/src-helper-apps/VBoxExtPackHelperApp.cpp
+++ src/VBox/Main/src-helper-apps/VBoxExtPackHelperApp.cpp
@@ -1854,7 +1854,35 @@ extern "C" int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPST
 {
     g_hInstance = hInstance;
     NOREF(hPrevInstance); NOREF(nShowCmd); NOREF(lpCmdLine);
-    return main(__argc, __argv);
+
+    int rc = RTR3Init();
+    if (RT_FAILURE(rc))
+        return RTMsgInitFailure(rc);
+
+    LPWSTR pwszCmdLine = GetCommandLineW();
+    if (!pwszCmdLine)
+        return RTMsgErrorExit(RTEXITCODE_FAILURE, "GetCommandLineW failed");
+
+    char *pszCmdLine;
+    rc = RTUtf16ToUtf8(pwszCmdLine, &pszCmdLine); /* leaked */
+    if (RT_FAILURE(rc))
+        return RTMsgErrorExit(RTEXITCODE_FAILURE, "Failed to convert the command line: %Rrc", rc);
+
+    int    cArgs;
+    char **papszArgs;
+    rc = RTGetOptArgvFromString(&papszArgs, &cArgs, pszCmdLine, NULL);
+    if (RT_SUCCESS(rc))
+    {
+
+        rc = main(cArgs, papszArgs);
+
+        RTGetOptArgvFree(papszArgs);
+    }
+    else
+        rc = RTMsgErrorExit(RTEXITCODE_FAILURE, "RTGetOptArgvFromString failed: %Rrc", rc);
+    RTStrFree(pszCmdLine);
+
+    return rc;
 }
 #endif
 
