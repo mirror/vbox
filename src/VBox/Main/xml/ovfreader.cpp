8516e9b3947cb4401ad78a36e5fa9fe2da89c457
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2011-01-14 14:16:22 +0000

    Main/OVF: optimization
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@35566 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Main/xml/ovfreader.cpp
+++ src/VBox/Main/xml/ovfreader.cpp
@@ -750,11 +750,11 @@ void OVFReader::HandleVirtualSystemContent(const xml::ElementNode *pelmVirtualSy
                         i.strAddressOnParent.toInt(vd.ulAddressOnParent);
                         // ovf://disk/lamp
                         // 123456789012345
-                        if (i.strHostResource.substr(0, 11) == "ovf://disk/")
+                        if (i.strHostResource.startsWith("ovf://disk/"))
                             vd.strDiskId = i.strHostResource.substr(11);
-                        else if (i.strHostResource.substr(0, 10) == "ovf:/disk/")
+                        else if (i.strHostResource.startsWith("ovf:/disk/"))
                             vd.strDiskId = i.strHostResource.substr(10);
-                        else if (i.strHostResource.substr(0, 6) == "/disk/")
+                        else if (i.strHostResource.startsWith("/disk/"))
                             vd.strDiskId = i.strHostResource.substr(6);
 
                         if (    !(vd.strDiskId.length())
