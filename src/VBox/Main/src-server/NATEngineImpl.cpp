715a6a127b62622e6a4d88761b02f8000c983daa
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2011-01-16 16:16:19 +0000

    Main/NAT: adds protocol comparison in unique rule validator. (vbox/8094).
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@35577 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Main/src-server/NATEngineImpl.cpp
+++ src/VBox/Main/src-server/NATEngineImpl.cpp
@@ -290,7 +290,8 @@ NATEngine::AddRedirect(IN_BSTR aName, NATProtocol_T aProto, IN_BSTR aBindIp, USH
             return setError(E_INVALIDARG,
                             tr("A NAT rule of this name already exists"));
         if (   r.strHostIP == Utf8Str(aBindIp)
-            && r.u16HostPort == aHostPort)
+            && r.u16HostPort == aHostPort
+            && r.proto == aProto)
             return setError(E_INVALIDARG,
                             tr("A NAT rule for this host port and this host IP already exists"));
     }
