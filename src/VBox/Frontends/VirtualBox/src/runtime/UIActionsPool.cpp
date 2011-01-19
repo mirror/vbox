594506496492ff76415ea19bd2dd911d4425eb21
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2011-01-19 16:32:11 +0000

    FE/Qt4: upps
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@35635 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Frontends/VirtualBox/src/runtime/UIActionsPool.cpp
+++ src/VBox/Frontends/VirtualBox/src/runtime/UIActionsPool.cpp
@@ -1292,7 +1292,7 @@ bool UIActionsPool::processHotKey(const QKeySequence &key)
                  * corresponding to the key sequence, so that the pointer to
                  * menu data posted along with the event will remain valid in
                  * the event handler, at least until the main window is closed. */
-//                QApplication::postEvent(this, new ActivateActionEvent(pAction));
+                QApplication::postEvent(this, new ActivateActionEvent(pAction));
                 return true;
             }
         }
