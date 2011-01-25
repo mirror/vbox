561c06695e6bc66f3a6583d136f079db8a7139f3
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2011-01-25 12:10:31 +0000

    Argument fix.
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@35705 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Additions/WINNT/Installer/VBoxDrvInst.cpp
+++ src/VBox/Additions/WINNT/Installer/VBoxDrvInst.cpp
@@ -1038,7 +1038,7 @@ int __cdecl _tmain(int argc, _TCHAR *argv[])
             _TCHAR szINF[_MAX_PATH] = { 0 }; /* Complete path to INF file.*/
             if (   (   !_tcsicmp(argv[2], _T("install"))
                     || !_tcsicmp(argv[2], _T("uninstall")))
-                && argc == 4)
+                && argc >= 4)
             {
                 if (OSinfo.dwMajorVersion < 5)
                 {
