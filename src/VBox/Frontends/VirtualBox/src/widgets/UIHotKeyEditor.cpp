3f3ace4e3109b09261ced2b7bc0ce6d7b72bd695
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2011-01-27 11:11:20 +0000

    FE/Qt: Try to fix X11 build for r69678.
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@35732 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Frontends/VirtualBox/src/widgets/UIHotKeyEditor.cpp
+++ src/VBox/Frontends/VirtualBox/src/widgets/UIHotKeyEditor.cpp
@@ -39,6 +39,8 @@
 #ifdef Q_WS_X11
 # include <QX11Info>
 # include <X11/Xutil.h>
+# include <X11/Xlib.h>
+# include <X11/keysymdef.h>
 # include "XKeyboard.h"
 # ifdef KeyPress
    const int XKeyPress = KeyPress;
