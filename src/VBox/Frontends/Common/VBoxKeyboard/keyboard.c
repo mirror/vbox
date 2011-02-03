d70a2cb3f12599ca528c6818ee3437e1c795e95e
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2011-02-03 16:01:27 +0000

    FE/Common/VBoxKeyboard: hopefully support the Canadian multilingual keyboard
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@35842 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Frontends/Common/VBoxKeyboard/keyboard.c
+++ src/VBox/Frontends/Common/VBoxKeyboard/keyboard.c
@@ -118,6 +118,8 @@ unsigned X11DRV_KeyEvent(Display *display, KeyCode code)
             scan = 0x39;
         else if (keysym == 0xFE03)          /* ISO level3 shift, aka AltGr */
             scan = 0x138;
+        else if (keysym == 0xFE11)          /* ISO level5 shift, R-Ctrl on */
+            scan = 0x11d;                   /* Canadian multilingual layout */
     }
     if (keyc2scan[code])
         scan = keyc2scan[code];
