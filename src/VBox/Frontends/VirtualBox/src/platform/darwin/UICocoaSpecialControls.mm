4adb479a0eaf727b46bff8ddd2e26ea831061725
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2011-02-07 14:14:57 +0000

    FE/Qt4-OSX: fix some warning in 64-bit
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@35874 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Frontends/VirtualBox/src/platform/darwin/UICocoaSpecialControls.mm
+++ src/VBox/Frontends/VirtualBox/src/platform/darwin/UICocoaSpecialControls.mm
@@ -37,8 +37,8 @@
 #include <QKeyEvent>
 #include <QMacCocoaViewContainer>
 
-/* 
- * Private interfaces 
+/*
+ * Private interfaces
  */
 @interface UIButtonTargetPrivate: NSObject
 {
@@ -70,7 +70,7 @@
 -(id)initWithObject2:(UICocoaSearchField*)object;
 @end
 
-#if MAC_OS_X_VERSION_MIN_ALLOWED >= MAC_OS_X_VERSION_10_6
+#if MAC_OS_X_VERSION_MAX_ALLOWED >= MAC_OS_X_VERSION_10_6
 @interface UISearchFieldDelegatePrivate: NSObject<NSTextFieldDelegate>
 #else
 @interface UISearchFieldDelegatePrivate: NSObject
@@ -78,7 +78,7 @@
 {}
 @end
 
-/* 
+/*
  * Implementation of the private interfaces
  */
 @implementation UIButtonTargetPrivate
@@ -121,7 +121,7 @@
     return self;
 }
 
-- (void)dealloc 
+- (void)dealloc
 {
     [mBGColor release];
     [super dealloc];
@@ -221,7 +221,7 @@
 @end
 
 
-/* 
+/*
  * Helper functions
  */
 NSRect darwinCenterRectVerticalTo(NSRect aRect, const NSRect& aToRect)
@@ -230,8 +230,8 @@ NSRect darwinCenterRectVerticalTo(NSRect aRect, const NSRect& aToRect)
     return aRect;
 }
 
-/* 
- * Public classes 
+/*
+ * Public classes
  */
 UICocoaWrapper::UICocoaWrapper(QWidget *pParent /* = 0 */)
     : QWidget(pParent)
