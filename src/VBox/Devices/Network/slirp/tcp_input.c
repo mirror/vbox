13c24fc99563e26e74c4f4d401bcc39c12704963
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2011-02-07 04:26:47 +0000

    NAT/tcp: similar to r69862 case on tcp.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@35860 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Devices/Network/slirp/tcp_input.c
+++ src/VBox/Devices/Network/slirp/tcp_input.c
@@ -789,6 +789,7 @@ findso:
                     m->m_len  += sizeof(struct tcpiphdr)+off-sizeof(struct tcphdr);
                     *ip = save_ip;
                     icmp_error(pData, m, ICMP_UNREACH, code, 0, strerror(errno));
+                    m_freem(pData, m);
                     tp->t_socket->so_m = NULL;
                 }
                 tp = tcp_close(pData, tp);
