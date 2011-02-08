4b61fd5d73ac24f9d24ea1d4d139ae4b6d5cee47
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2011-02-08 09:25:24 +0000

    Burn fix.
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@35891 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Frontends/VBoxManage/VBoxManageInfo.cpp
+++ src/VBox/Frontends/VBoxManage/VBoxManageInfo.cpp
@@ -2058,7 +2058,7 @@ HRESULT showVMInfo(ComPtr<IVirtualBox> virtualBox,
                 /* Print information about important Guest Additions parts: */
                 /** @todo Add a makeFacilityStatusStr() to translate facility states into a human readable string! */
                 AdditionsFacilityStatus faStatus;
-                LONGLONG lLastUpdatedMS = 0;
+                LONG64 lLastUpdatedMS = 0;
                 char szLastUpdated[32];
                 rc = guest->GetFacilityStatus(AdditionsFacilityType_VBoxGuestDriver, &lLastUpdatedMS, &faStatus);
                 if (SUCCEEDED(rc))
