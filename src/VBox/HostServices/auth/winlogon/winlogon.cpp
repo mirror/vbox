94f801f19c26b3335a26a409cef8c4bbb660d290
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2011-02-11 13:04:47 +0000

    VBoxAuth samples: updated to the new generic auth interface.
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@35943 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/HostServices/auth/winlogon/winlogon.cpp
+++ src/VBox/HostServices/auth/winlogon/winlogon.cpp
@@ -1,12 +1,11 @@
 /** @file
  *
- * VBox Remote Desktop Protocol:
- * External Authentication Library:
+ * VirtualBox External Authentication Library:
  * Windows Logon Authentication.
  */
 
 /*
- * Copyright (C) 2006-2010 Oracle Corporation
+ * Copyright (C) 2006-2011 Oracle Corporation
  *
  * This file is part of VirtualBox Open Source Edition (OSE), as
  * available from http://www.virtualbox.org. This file is free software;
