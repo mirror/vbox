2bba43f99b6c5def80086bd8570fc27d14f8ab68
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2011-02-16 13:30:53 +0000

    Two cases where CPUM_CHANGED_GLOBAL_TLB_FLUSH was missing, causing stale TLB entries and mayhem.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@35994 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/recompiler/VBoxRecompiler.c
+++ src/recompiler/VBoxRecompiler.c
@@ -1933,9 +1933,18 @@ REMR3DECL(int)  REMR3State(PVM pVM, PVMCPU pVCpu)
      */
     fFlags = CPUMR3RemEnter(pVCpu, &uCpl);
     LogFlow(("CPUMR3RemEnter %x %x\n", fFlags, uCpl));
-    if (fFlags & (  CPUM_CHANGED_CR4  | CPUM_CHANGED_CR3  | CPUM_CHANGED_CR0
-                  | CPUM_CHANGED_GDTR | CPUM_CHANGED_IDTR | CPUM_CHANGED_LDTR
-                  | CPUM_CHANGED_FPU_REM | CPUM_CHANGED_SYSENTER_MSR | CPUM_CHANGED_CPUID))
+    if (fFlags & (  CPUM_CHANGED_GLOBAL_TLB_FLUSH
+                  | CPUM_CHANGED_CR4
+                  | CPUM_CHANGED_CR0
+                  | CPUM_CHANGED_CR3
+                  | CPUM_CHANGED_GDTR
+                  | CPUM_CHANGED_IDTR
+                  | CPUM_CHANGED_SYSENTER_MSR
+                  | CPUM_CHANGED_LDTR
+                  | CPUM_CHANGED_CPUID
+                  | CPUM_CHANGED_FPU_REM
+                 )
+        )
     {
         if (fFlags & CPUM_CHANGED_GLOBAL_TLB_FLUSH)
         {
