6f71083ce5af8c5156c1e03ab04a64ef4d63033e
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2011-02-16 14:24:09 +0000

    crOpenGL: fix guest compiz clipping with forced fbo
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@35998 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/HostServices/SharedOpenGL/crserverlib/server_muralfbo.c
+++ src/VBox/HostServices/SharedOpenGL/crserverlib/server_muralfbo.c
@@ -339,27 +339,12 @@ void crServerPresentFBO(CRMuralInfo *mural)
             /* rect in window relative coords */
             crServerTransformRect(&rectwr, &rect, -mural->gX, -mural->gY);
 
-#if 0 /*@todo recheck with compiz on guests, as we could sometimes have main compiz windows without visible rects*/
             if (!mural->pVisibleRects)
             {
-                tmppixels = crAlloc(4*(rect.x2-rect.x1)*(rect.y2-rect.y1));
-                if (!tmppixels)
+                /*we don't get any rects info for guest compiz windows, so we treat windows as visible unless explicitly received 0 visible rects*/
+                if (!mural->bReceivedRects)
                 {
-                    crWarning("Out of memory in crServerPresentFBO");
-                    crFree(pixels);
-                    return;
-                }
-
-                crServerCopySubImage(tmppixels, pixels, &rectwr, mural->fboWidth, mural->fboHeight);
-                /*Note: pfnPresentFBO would free tmppixels*/
-                cr_server.pfnPresentFBO(tmppixels, i, rect.x1-cr_server.screen[i].x, rect.y1-cr_server.screen[i].y, rect.x2-rect.x1, rect.y2-rect.y1);
-            }
-#endif
-            for (j=0; j<mural->cVisibleRects; ++j)
-            {
-                if (crServerIntersectRect(&rectwr, (CRrecti*) &mural->pVisibleRects[4*j], &sectr))
-                {
-                    tmppixels = crAlloc(4*(sectr.x2-sectr.x1)*(sectr.y2-sectr.y1));
+                    tmppixels = crAlloc(4*(rect.x2-rect.x1)*(rect.y2-rect.y1));
                     if (!tmppixels)
                     {
                         crWarning("Out of memory in crServerPresentFBO");
@@ -367,12 +352,32 @@ void crServerPresentFBO(CRMuralInfo *mural)
                         return;
                     }
 
-                    crServerCopySubImage(tmppixels, pixels, &sectr, mural->fboWidth, mural->fboHeight);
+                    crServerCopySubImage(tmppixels, pixels, &rectwr, mural->fboWidth, mural->fboHeight);
                     /*Note: pfnPresentFBO would free tmppixels*/
-                    cr_server.pfnPresentFBO(tmppixels, i,
-                                            sectr.x1+mural->gX-cr_server.screen[i].x,
-                                            sectr.y1+mural->gY-cr_server.screen[i].y,
-                                            sectr.x2-sectr.x1, sectr.y2-sectr.y1);
+                    cr_server.pfnPresentFBO(tmppixels, i, rect.x1-cr_server.screen[i].x, rect.y1-cr_server.screen[i].y, rect.x2-rect.x1, rect.y2-rect.y1);
+                }
+            }
+            else
+            {
+                for (j=0; j<mural->cVisibleRects; ++j)
+                {
+                    if (crServerIntersectRect(&rectwr, (CRrecti*) &mural->pVisibleRects[4*j], &sectr))
+                    {
+                        tmppixels = crAlloc(4*(sectr.x2-sectr.x1)*(sectr.y2-sectr.y1));
+                        if (!tmppixels)
+                        {
+                            crWarning("Out of memory in crServerPresentFBO");
+                            crFree(pixels);
+                            return;
+                        }
+
+                        crServerCopySubImage(tmppixels, pixels, &sectr, mural->fboWidth, mural->fboHeight);
+                        /*Note: pfnPresentFBO would free tmppixels*/
+                        cr_server.pfnPresentFBO(tmppixels, i,
+                                                sectr.x1+mural->gX-cr_server.screen[i].x,
+                                                sectr.y1+mural->gY-cr_server.screen[i].y,
+                                                sectr.x2-sectr.x1, sectr.y2-sectr.y1);
+                    }
                 }
             }
         }
