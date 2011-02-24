6d67bff542c522609af11adc09669091dff9e90b
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2011-02-24 15:02:10 +0000

    Main/src-server/PerformanceSolaris: Tweak free memory accounting to include ZFS arc c_min.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@36071 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Main/src-server/solaris/PerformanceSolaris.cpp
+++ src/VBox/Main/src-server/solaris/PerformanceSolaris.cpp
@@ -25,6 +25,7 @@
 #include <kstat.h>
 #include <sys/sysinfo.h>
 #include <sys/time.h>
+#include <syslog.h>
 
 #include <iprt/err.h>
 #include <iprt/string.h>
@@ -84,7 +85,8 @@ CollectorSolaris::CollectorSolaris()
 
 CollectorSolaris::~CollectorSolaris()
 {
-    kstat_close(mKC);
+    if (mKC)
+        kstat_close(mKC);
 }
 
 int CollectorSolaris::getRawHostCpuLoad(uint64_t *user, uint64_t *kernel, uint64_t *idle)
@@ -190,7 +192,22 @@ int CollectorSolaris::getHostMemoryUsage(ULONG *total, ULONG *used, ULONG *avail
         if (mZFSCache)
         {
             if ((kn = (kstat_named_t *)kstat_data_lookup(mZFSCache, "size")))
-                *available += (kn->value.ul / 1024);
+            {
+                ulong_t ulSize = kn->value.ul;
+
+                if ((kn = (kstat_named_t *)kstat_data_lookup(mZFSCache, "c_min")))
+                {
+                    /*
+                     * Account for ZFS minimum arc cache size limit.
+                     * "c_min" is the target minimum size of the ZFS cache, and not the hard limit. It's possible
+                     * for "size" to shrink below "c_min" (e.g: during boot & high memory consumption).
+                     */
+                    ulong_t ulMin = kn->value.ul;
+                    *available += ulSize > ulMin ? ulSize - ulMin : 0;
+                }
+                else
+                    Log(("kstat_data_lookup(c_min) ->%d\n", errno));
+            }
             else
                 Log(("kstat_data_lookup(size) -> %d\n", errno));
         }
@@ -208,6 +225,7 @@ int CollectorSolaris::getHostMemoryUsage(ULONG *total, ULONG *used, ULONG *avail
 
     return rc;
 }
+
 int CollectorSolaris::getProcessMemoryUsage(RTPROCESS process, ULONG *used)
 {
     int rc = VINF_SUCCESS;
@@ -242,5 +260,4 @@ int CollectorSolaris::getProcessMemoryUsage(RTPROCESS process, ULONG *used)
     return rc;
 }
 
-}
 
