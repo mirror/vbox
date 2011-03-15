70ecc102d7d2262237ab6248e40a6b0f142233ec
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2011-03-15 13:22:02 +0000

    Never complete tasks on the submitting thread using the callback, this can cause problems if the caller doesn't expect it (like VD)
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@36285 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/VMM/VMMR3/PDMAsyncCompletionFile.cpp
+++ src/VBox/VMM/VMMR3/PDMAsyncCompletionFile.cpp
@@ -369,7 +369,6 @@ int pdmacFileEpTaskInitiate(PPDMASYNCCOMPLETIONTASK pTask,
                             PCRTSGSEG paSegments, size_t cSegments,
                             size_t cbTransfer, PDMACTASKFILETRANSFER enmTransfer)
 {
-    int rc = VINF_SUCCESS;
     PPDMASYNCCOMPLETIONENDPOINTFILE pEpFile = (PPDMASYNCCOMPLETIONENDPOINTFILE)pEndpoint;
     PPDMASYNCCOMPLETIONTASKFILE pTaskFile = (PPDMASYNCCOMPLETIONTASKFILE)pTask;
     PPDMACEPFILEMGR pAioMgr = pEpFile->pAioMgr;
@@ -398,13 +397,7 @@ int pdmacFileEpTaskInitiate(PPDMASYNCCOMPLETIONTASK pTask,
 
     AssertMsg(!cbTransfer, ("Incomplete transfer %u bytes left\n", cbTransfer));
 
-    if (ASMAtomicReadS32(&pTaskFile->cbTransferLeft) == 0
-        && !ASMAtomicXchgBool(&pTaskFile->fCompleted, true))
-        pdmR3AsyncCompletionCompleteTask(pTask, pTaskFile->rc, false);
-    else
-        rc = VINF_AIO_TASK_PENDING;
-
-    return rc;
+    return VINF_AIO_TASK_PENDING;
 }
 
 /**
