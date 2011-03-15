4830fc219645fb9a04caecc39cf8348217f2af49
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2011-03-15 10:47:13 +0000

    VBoxHeadless: Fix for dropping VRDP connection on guest bootup after a hard VM reset.
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@36280 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Frontends/VBoxHeadless/VBoxHeadless.cpp
+++ src/VBox/Frontends/VBoxHeadless/VBoxHeadless.cpp
@@ -212,24 +212,25 @@ public:
                         Bstr value;
                         gpcev->COMGETTER(Value)(value.asOutParam());
                         Utf8Str utf8Value = value;
-                        if (utf8Value == "true")
+
+                        if (!mfNoLoggedInUsers) /* Only if the property really changes. */
                         {
-                            if (!mfNoLoggedInUsers) /* Only if the property really changes. */
+                            if (   utf8Value == "true"
+                                /* Guest property got deleted due to hard reset,
+                                 * so it has no value anymore. */
+                                || utf8Value.isEmpty())
                             {
                                 mfNoLoggedInUsers = true;
                                 fDropConnection = true;
                             }
                         }
-                        /* Guest property got deleted due to hard reset,
-                         * so it has no value anymore. */
-                        else if (utf8Value.isEmpty())
-                        {
-                            fDropConnection = true;
-                        }
-                        else
-                        {
+                        else if (utf8Value == "false")
                             mfNoLoggedInUsers = false;
-                        }
+                        /* Guest property got deleted due to hard reset,
+                         * take the shortcut without touching the mfNoLoggedInUsers
+                         * state. */
+                        else if (utf8Value.isEmpty())
+                            fDropConnection = true;
 
                         if (fDropConnection)
                         {
