906528ead5489cf7ab1abaedd7e980581d99ea27
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2011-03-23 16:35:46 +0000

    HGCMInternal.cpp: RTR0MemObjSize does the alignment assertions now.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@36373 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Additions/common/VBoxGuestLib/HGCMInternal.cpp
+++ src/VBox/Additions/common/VBoxGuestLib/HGCMInternal.cpp
@@ -399,9 +399,7 @@ static int vbglR0HGCMInternalPreprocessCall(VBoxGuestHGCMCallInfo const *pCallIn
 
                     if (VBGLR0_CAN_USE_PHYS_PAGE_LIST())
                     {
-                        size_t const cbObj = RTR0MemObjSize(hObj);
-                        Assert(cbObj == RT_ALIGN_Z(cbObj, PAGE_SIZE));
-                        size_t const cPages = cbObj >> PAGE_SHIFT;
+                        size_t const cPages = RTR0MemObjSize(hObj) >> PAGE_SHIFT;
                         *pcbExtra += RT_OFFSETOF(HGCMPageListInfo, aPages[cPages]);
                     }
                 }
