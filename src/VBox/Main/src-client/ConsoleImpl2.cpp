f0d9e0c3aa715c440ad4d8e943fee1e9f6123bca
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2011-03-25 21:21:01 +0000

    ConsoleImpl2.cpp: Set PGM/PciPassThrough=1.
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@36443 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Main/src-client/ConsoleImpl2.cpp
+++ src/VBox/Main/src-client/ConsoleImpl2.cpp
@@ -511,15 +511,19 @@ static HRESULT attachRawPciDevices(BusAssignmentManager* BusMgr,
     {
         InsertConfigNode(pDevices,     "pciraw",  &pPciDevs);
 
+        PCFGMNODE pRoot = CFGMR3GetParent(pDevices); Assert(pRoot);
+
+        /* Tell PGM to tell GPciRaw about guest mappings. */
+        CFGMR3InsertNode(pRoot, "PGM", NULL);
+        InsertConfigInteger(CFGMR3GetChild(pRoot, "PGM"), "PciPassThrough", 1);
+
         /*
          * Currently, using IOMMU needed for PCI passthrough
          * requires RAM preallocation.
-         * @todo: check if we can lift this requirement
          */
-        PCFGMNODE pRoot = CFGMR3GetParent(pDevices);
-        Assert(pRoot);
+        /** @todo: check if we can lift this requirement */
         CFGMR3RemoveValue(pRoot, "RamPreAlloc");
-        CFGMR3InsertInteger(pRoot, "RamPreAlloc",    1);
+        InsertConfigInteger(pRoot, "RamPreAlloc", 1);
     }
 
     for (size_t iDev = 0; iDev < assignments.size(); iDev++)
