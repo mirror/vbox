d681f4b24cfd32ec69c193623d7a9cd6078645e6
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2011-03-25 13:01:01 +0000

    VBoxRecompiler.c: VMM needs msrEFER in sync when calling PGMChangeMode. See #5611.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@36430 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/recompiler/VBoxRecompiler.c
+++ src/recompiler/VBoxRecompiler.c
@@ -1847,9 +1847,9 @@ void remR3ChangeCpuMode(CPUState *env)
     if ((env->cr[4] ^ pCtx->cr4) & X86_CR4_VME)
         VMCPU_FF_SET(env->pVCpu, VMCPU_FF_SELM_SYNC_TSS);
     pCtx->cr4 = env->cr[4];
-
 #ifdef TARGET_X86_64
     efer = env->efer;
+    pCtx->msrEFER = efer;
 #else
     efer = 0;
 #endif
