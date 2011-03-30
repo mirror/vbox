baf97cbe2d633ff0f107fcc596a227aff3f03c06
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2011-03-30 18:39:29 +0000

    Main/src-server/xpcom: build fix for some gcc versions
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@36478 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Main/src-server/xpcom/server.cpp
+++ src/VBox/Main/src-server/xpcom/server.cpp
@@ -4,7 +4,7 @@
  */
 
 /*
- * Copyright (C) 2006-2010 Oracle Corporation
+ * Copyright (C) 2006-2011 Oracle Corporation
  *
  * This file is part of VirtualBox Open Source Edition (OSE), as
  * available from http://www.virtualbox.org. This file is free software;
@@ -858,7 +858,7 @@ int main(int argc, char **argv)
 
     const nsModuleComponentInfo VirtualBoxInfo = {
         "VirtualBox component",
-        CLSID_VirtualBox,
+        NS_VIRTUALBOX_CID,
         NS_VIRTUALBOX_CONTRACTID,
         VirtualBoxConstructor, // constructor function
         NULL, // registration function
