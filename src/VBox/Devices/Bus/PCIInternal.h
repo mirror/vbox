38818ff886e89313c6f3d1cc81ec6110ccac11b3
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2011-04-05 13:56:34 +0000

    PCI: better logging, try to play nice with MSI
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@36562 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Devices/Bus/PCIInternal.h
+++ src/VBox/Devices/Bus/PCIInternal.h
@@ -87,7 +87,10 @@ enum {
      * This one is set by MsixInit().  */
     PCIDEV_FLAG_MSIX_CAPABLE       = 1<<4,
     /** Flag if device represents real physical device in passthrough mode. */
-    PCIDEV_FLAG_PASSTHROUGH        = 1<<5
+    PCIDEV_FLAG_PASSTHROUGH        = 1<<5,
+    /** Flag whether the device is capable of MSI using 64-bit address.  */
+    PCIDEV_FLAG_MSI64_CAPABLE      = 1<<6
+    
 };
 
 /**
