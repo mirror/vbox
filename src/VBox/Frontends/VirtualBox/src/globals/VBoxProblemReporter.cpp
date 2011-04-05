e385faef45145071f188d43ccff09331f19a206a
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2011-04-05 13:02:12 +0000

    FE/Qt: Spelling fix for r70984.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@36558 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Frontends/VirtualBox/src/globals/VBoxProblemReporter.cpp
+++ src/VBox/Frontends/VirtualBox/src/globals/VBoxProblemReporter.cpp
@@ -2085,9 +2085,9 @@ void VBoxProblemReporter::warnAboutCannotCreateMachineFolder(QWidget *pParent, c
 {
     QFileInfo fi(strFolderName);
     message(pParent ? pParent : mainWindowShown(), Critical,
-            tr("<p>Cannot create the machine folder <b>%1</b> in the parent directory <nobr><b>%2</b>.</nobr></p>"
-               "<p>Please check what the parent directory is really exists and "
-               "you have the permissions required to do so.</p>").arg(fi.fileName()).arg(fi.absolutePath()));
+            tr("<p>Cannot create the machine folder <b>%1</b> in the parent folder <nobr><b>%2</b>.</nobr></p>"
+               "<p>Please check that the parent really exists and that you have permissions to create the machine folder.</p>")
+               .arg(fi.fileName()).arg(fi.absolutePath()));
 }
 
 /**
