9486e5aebd9e1e2b51327f1f9523cc28c98db9fb
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2011-04-12 12:09:05 +0000

    Frontend/VBoxManage: fix error message printing when the bootable option is wrong
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@36652 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Frontends/VBoxManage/VBoxManageStorageController.cpp
+++ src/VBox/Frontends/VBoxManage/VBoxManageStorageController.cpp
@@ -4,7 +4,7 @@
  */
 
 /*
- * Copyright (C) 2006-2010 Oracle Corporation
+ * Copyright (C) 2006-2011 Oracle Corporation
  *
  * This file is part of VirtualBox Open Source Edition (OSE), as
  * available from http://www.virtualbox.org. This file is free software;
@@ -1035,7 +1035,7 @@ int handleStorageController(HandlerArg *a)
                 }
                 else
                 {
-                    errorArgument("Invalid --bootable argument '%s'", pszHostIOCache);
+                    errorArgument("Invalid --bootable argument '%s'", pszBootable);
                     rc = E_FAIL;
                 }
             }
