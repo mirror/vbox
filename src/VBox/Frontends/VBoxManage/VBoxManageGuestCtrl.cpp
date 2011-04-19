4cbadb1c0a1efec2fe7512a61b0248805ffff8e7
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2011-04-19 12:35:37 +0000

    VBoxManageGuestCtrl: Skip empty arguments.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@36726 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Frontends/VBoxManage/VBoxManageGuestCtrl.cpp
+++ src/VBox/Frontends/VBoxManage/VBoxManageGuestCtrl.cpp
@@ -496,6 +496,9 @@ static int handleCtrlExecProgram(ComPtr<IGuest> guest, HandlerArg *pArg)
 
             case VINF_GETOPT_NOT_OPTION:
             {
+                if (!strlen(ValueUnion.psz))
+                    continue;
+
                 if (args.size() == 0 && Utf8Cmd.isEmpty())
                     Utf8Cmd = ValueUnion.psz;
                 args.push_back(Bstr(ValueUnion.psz).raw());
