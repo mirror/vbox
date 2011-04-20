50a0b5f65304009350bbac8661fb097e4779a4ef
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2011-04-20 13:23:41 +0000

    Additions/linux/installer: X.Org Server crashes on Natty if we call udevadm trigger for the input subsystem... forgot the uninstaller
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@36752 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Additions/linux/installer/vboxadd-x11.sh
+++ src/VBox/Additions/linux/installer/vboxadd-x11.sh
@@ -636,7 +636,7 @@ EOF
     # Remove other files
     rm /etc/hal/fdi/policy/90-vboxguest.fdi 2>/dev/null
     rm /etc/udev/rules.d/70-xorg-vboxmouse.rules 2>/dev/null
-    udevadm trigger --action=change --subsystem-match=misc --subsystem-match=input 2>/dev/null
+    udevadm trigger --action=change --subsystem-match=misc 2>/dev/null
     rm /usr/lib/X11/xorg.conf.d/50-vboxmouse.conf 2>/dev/null
     rm /usr/share/X11/xorg.conf.d/50-vboxmouse.conf 2>/dev/null
     rm /usr/share/xserver-xorg/pci/vboxvideo.ids 2>/dev/null
