41c1741373f04a8fc723ef8a9a38f4282437d3be
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2011-04-21 22:42:33 +0000

    Additions/x11/seamless: no more std::autoptr or std::vector, use the new STL-vector-in-C instead - forgotten
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@36808 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Additions/x11/VBoxClient/testcase/tstSeamlessX11.cpp
+++ src/VBox/Additions/x11/VBoxClient/testcase/tstSeamlessX11.cpp
@@ -14,11 +14,11 @@
  * hope that it will be useful, but WITHOUT ANY WARRANTY of any kind.
  */
 
-#include <iostream>
 #include <stdlib.h> /* exit() */
 
 #include <iprt/initterm.h>
 #include <iprt/semaphore.h>
+#include <iprt/stream.h>
 #include <VBox/VBoxGuestLib.h>
 
 #include "../seamless.h"
@@ -27,28 +27,27 @@ static RTSEMEVENT eventSem;
 
 int VbglR3SeamlessSendRects(uint32_t cRects, PRTRECT pRects)
 {
-    std::cout << "Received rectangle update (" << cRects << " rectangles):" << std::endl;
+    RTPrintf("Received rectangle update (%u rectangles):\n", cRects);
     for (unsigned i = 0; i < cRects; ++i)
     {
-        std::cout << "  xLeft: " << pRects[i].xLeft << "  yTop: " << pRects[i].yTop
-                  << "  xRight: " << pRects[i].xRight << "  yBottom: " << pRects[i].yBottom
-                  << std::endl;
+        RTPrintf("  xLeft: %d  yTop: %d  xRight: %d  yBottom: %d\n",
+                 pRects[i].xLeft, pRects[i].yTop, pRects[i].xRight,
+                 pRects[i].yBottom);
     }
     return true;
 }
 
 int VbglR3SeamlessSetCap(bool bState)
 {
-    std::cout << (bState ? "Seamless capability set" : "Seamless capability unset")
-              << std::endl;
+    RTPrintf("%s\n", bState ? "Seamless capability set"
+                            : "Seamless capability unset");
     return true;
 }
 
 int VbglR3CtlFilterMask(uint32_t u32OrMask, uint32_t u32NotMask)
 {
-    std::cout << "IRQ filter mask changed.  Or mask: 0x" << std::hex << u32OrMask
-              << ".  Not mask: 0x" << u32NotMask << std::dec
-              << std::endl;
+    RTPrintf("IRQ filter mask changed.  Or mask: 0x%x.  Not mask: 0x%x\n",
+             u32OrMask, u32NotMask);
     return true;
 }
 
@@ -88,74 +87,45 @@ int vboxClientXLibErrorHandler(Display *pDisplay, XErrorEvent *pError)
     if (pError->error_code == BadWindow)
     {
         /* This can be triggered if a guest application destroys a window before we notice. */
-        std::cout << "ignoring BadAtom error and returning" << std::endl;
+        RTPrintf("ignoring BadAtom error and returning\n");
         return 0;
     }
     XGetErrorText(pDisplay, pError->error_code, errorText, sizeof(errorText));
-    std::cout << "An X Window protocol error occurred: " << errorText << std::endl
-              << "  Request code: " << int(pError->request_code) << std::endl
-              << "  Minor code: " << int(pError->minor_code) << std::endl
-              << "  Serial number of the failed request: " << int(pError->serial)
-              << std::endl;
-    std::cout << std::endl << "exiting." << std::endl;
+    RTPrintf("An X Window protocol error occurred: %s\n"
+             "  Request code: %d\n"
+             "  Minor code: %d\n"
+             "  Serial number of the failed request: %d\n\n"
+             "exiting.\n",
+             errorText, (int)pError->request_code, (int)pError->minor_code,
+             (int)pError->serial);
     exit(1);
 }
 
 int main( int argc, char **argv)
 {
     int rc = VINF_SUCCESS;
-    std::string sTmp;
+    char ach[2];
 
     RTR3Init();
-    std::cout << "VirtualBox guest additions X11 seamless mode testcase" << std::endl;
+    RTPrintf("VirtualBox guest additions X11 seamless mode testcase\n");
     if (0 == XInitThreads())
     {
-        std::cout << "Failed to initialise X11 threading, exiting." << std::endl;
+        RTPrintf("Failed to initialise X11 threading, exiting.\n");
         exit(1);
     }
     /* Set an X11 error handler, so that we don't die when we get unavoidable errors. */
     XSetErrorHandler(vboxClientXLibErrorHandler);
-    std::cout << std::endl << "Press <Enter> to exit..." << std::endl;
+    RTPrintf("\nPress <Enter> to exit...\n");
     RTSemEventCreate(&eventSem);
     /** Our instance of the seamless class. */
     VBoxGuestSeamless seamless;
-    try
+    LogRel(("Starting seamless Guest Additions...\n"));
+    rc = seamless.init();
+    if (rc != VINF_SUCCESS)
     {
-        LogRel(("Starting seamless Guest Additions...\n"));
-        rc = seamless.init();
-        if (rc != VINF_SUCCESS)
-        {
-            std::cout << "Failed to initialise seamless Additions, rc = " << rc << std::endl;
-        }
+        RTPrintf("Failed to initialise seamless Additions, rc = %d\n", rc);
     }
-    catch (std::exception e)
-    {
-        std::cout << "Failed to initialise seamless Additions - caught exception: " << e.what()
-                  << std::endl;
-        rc = VERR_UNRESOLVED_ERROR;
-    }
-    catch (...)
-    {
-        std::cout << "Failed to initialise seamless Additions - caught unknown exception.\n"
-                  << std::endl;
-        rc = VERR_UNRESOLVED_ERROR;
-    }
-    std::getline(std::cin, sTmp);
-    try
-    {
-        seamless.uninit();
-    }
-    catch (std::exception e)
-    {
-        std::cout << "Error shutting down seamless Additions - caught exception: " << e.what()
-                  << std::endl;
-        rc = VERR_UNRESOLVED_ERROR;
-    }
-    catch (...)
-    {
-        std::cout << "Error shutting down seamless Additions - caught unknown exception.\n"
-                  << std::endl;
-        rc = VERR_UNRESOLVED_ERROR;
-    }
+    RTStrmGetLine(g_pStdIn, ach, sizeof(ach));
+    seamless.uninit();
     return rc;
 }
