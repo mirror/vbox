e0479f260018d2ef3571f2a8d06ad51c4d81a616
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2011-04-26 08:53:35 +0000

    3D for VRDP: initial commit, build fix (xTracker 5565).
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@36846 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/HostServices/SharedOpenGL/crserver/crservice.cpp
+++ src/VBox/HostServices/SharedOpenGL/crserver/crservice.cpp
@@ -1387,15 +1387,15 @@ static DECLCALLBACK(int) svcHostCall (void *, uint32_t u32Function, uint32_t cPa
 
                     if (RT_SUCCESS(rc))
                     {
-                        CROutputRedirect or;
-                        or.pvContext = pOutputRedirect->pvContext;
-                        or.CRORBegin = pOutputRedirect->H3DORBegin;
-                        or.CRORGeometry = pOutputRedirect->H3DORGeometry;
-                        or.CRORVisibleRegion = pOutputRedirect->H3DORVisibleRegion;
-                        or.CRORFrame = pOutputRedirect->H3DORFrame;
-                        or.CROREnd = pOutputRedirect->H3DOREnd;
-                        or.CRORContextProperty = pOutputRedirect->H3DORContextProperty;
-                        rc = crVBoxServerOutputRedirectSet(&or);
+                        CROutputRedirect outputRedirect;
+                        outputRedirect.pvContext = pOutputRedirect->pvContext;
+                        outputRedirect.CRORBegin = pOutputRedirect->H3DORBegin;
+                        outputRedirect.CRORGeometry = pOutputRedirect->H3DORGeometry;
+                        outputRedirect.CRORVisibleRegion = pOutputRedirect->H3DORVisibleRegion;
+                        outputRedirect.CRORFrame = pOutputRedirect->H3DORFrame;
+                        outputRedirect.CROREnd = pOutputRedirect->H3DOREnd;
+                        outputRedirect.CRORContextProperty = pOutputRedirect->H3DORContextProperty;
+                        rc = crVBoxServerOutputRedirectSet(&outputRedirect);
                     }
                 }
             }
