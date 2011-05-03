7b6b3e9e96783b028eafcf08049b2f852ee0dd11
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2011-05-03 06:29:12 +0000

    FE/Qt: 4989: Lazy init VM settings dialog: UISettingsCache template added.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@36915 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Frontends/VirtualBox/src/settings/UISettingsDefs.h
+++ src/VBox/Frontends/VirtualBox/src/settings/UISettingsDefs.h
@@ -19,6 +19,10 @@
 #ifndef __UISettingsDefs_h__
 #define __UISettingsDefs_h__
 
+/* Qt includes: */
+#include <QPair>
+#include <QMap>
+
 /* VBox includes: */
 #include "COMDefs.h"
 
@@ -37,6 +41,140 @@ namespace UISettingsDefs
     /* Machine state => Settings dialog type converter: */
     SettingsDialogType machineStateToSettingsDialogType(KMachineState machineState);
 }
+
+/* Template to operate settings cache item: */
+template <class CacheData> class UISettingsCache
+{
+public:
+
+    /* Creates empty cache item: */
+    UISettingsCache() { m_value = qMakePair(CacheData(), CacheData()); }
+
+    /* Returns the NON-modifiable REFERENCE to the initial cached data: */
+    const CacheData& base() const { return m_value.first; }
+    /* Returns the NON-modifiable REFERENCE to the current cached data: */
+    const CacheData& data() const { return m_value.second; }
+
+    /* We assume that old cache item was removed if
+     * initial data was set but current data was NOT set.
+     * Returns 'true' if that cache item was removed: */
+    virtual bool wasRemoved() const { return base() != CacheData() && data() == CacheData(); }
+
+    /* We assume that new cache item was created if
+     * initial data was NOT set but current data was set.
+     * Returns 'true' if that cache item was created: */
+    virtual bool wasCreated() const { return base() == CacheData() && data() != CacheData(); }
+
+    /* We assume that old cache item was updated if
+     * current and initial data were both set and not equal to each other.
+     * Returns 'true' if that cache item was updated: */
+    virtual bool wasUpdated() const { return base() != CacheData() && data() != CacheData() && data() != base(); }
+
+    /* We assume that old cache item was actually changed if
+     * 1. this item was removed or
+     * 2. this item was created or
+     * 3. this item was updated.
+     * Returns 'true' if that cache item was actually changed: */
+    virtual bool wasChanged() const { return wasRemoved() || wasCreated() || wasUpdated(); }
+
+    /* Set initial cache item data: */
+    void cacheInitialData(const CacheData &initialData) { m_value.first = initialData; }
+    /* Set current cache item data: */
+    void cacheCurrentData(const CacheData &currentData) { m_value.second = currentData; }
+
+    /* Reset the current data to be empty: */
+    void reset() { m_value.second = CacheData(); }
+
+    /* Reset the initial and the current data to be both empty: */
+    void clear() { m_value.first = CacheData(); m_value.second = CacheData(); }
+
+private:
+
+    /* Data: */
+    QPair<CacheData, CacheData> m_value;
+};
+
+/* Template to operate settings cache item with children: */
+template <class ParentCacheData, class ChildCacheData> class UISettingsCachePool : public UISettingsCache<ParentCacheData>
+{
+public:
+
+    /* Typedefs: */
+    typedef QMap<QString, ChildCacheData> UISettingsCacheChildMap;
+    typedef QMapIterator<QString, ChildCacheData> UISettingsCacheChildIterator;
+
+    /* Creates empty cache item: */
+    UISettingsCachePool() : UISettingsCache<ParentCacheData>() {}
+
+    /* Returns the modifiable REFERENCE to the particular child cached data.
+     * If child with such key or index is NOT present,
+     * both those methods will create the new one to return: */
+    ChildCacheData& child(const QString &strChildKey) { return m_children[strChildKey]; }
+    ChildCacheData& child(int iIndex) { return child(indexToKey(iIndex)); }
+
+    /* Returns the NON-modifiable COPY to the particular child cached data.
+     * If child with such key or index is NOT present,
+     * both those methods will create the new one to return: */
+    const ChildCacheData child(const QString &strChildKey) const { return m_children[strChildKey]; }
+    const ChildCacheData child(int iIndex) const { return child(indexToKey(iIndex)); }
+
+    /* Children count: */
+    int childCount() const { return m_children.size(); }
+
+    /* We assume that old cache item was updated if
+     * current and initial data were both set and not equal to each other.
+     * Takes into account all the children.
+     * Returns 'true' if that cache item was updated: */
+    bool wasUpdated() const
+    {
+        /* First of all, cache item is considered to be updated if parent data was updated: */
+        bool fWasUpdated = UISettingsCache<ParentCacheData>::wasUpdated();
+        /* If parent data was NOT updated but also was NOT created or removed too (e.j. was NOT changed at all),
+         * we have to check children too: */
+        if (!fWasUpdated && !UISettingsCache<ParentCacheData>::wasRemoved() && !UISettingsCache<ParentCacheData>::wasCreated())
+        {
+            for (int iChildIndex = 0; !fWasUpdated && iChildIndex < childCount(); ++iChildIndex)
+                if (child(iChildIndex).wasChanged())
+                    fWasUpdated = true;
+        }
+        return fWasUpdated;
+    }
+
+    /* Reset the current data to be empty.
+     * Perform the same for all the children: */
+    void reset()
+    {
+        UISettingsCache<ParentCacheData>::reset();
+        for (int iChildIndex = 0; iChildIndex < childCount(); ++iChildIndex)
+            child(iChildIndex).reset();
+    }
+
+    /* Reset the initial and the current data to be both empty.
+     * Perform the same for all the children: */
+    void clear()
+    {
+        UISettingsCache<ParentCacheData>::clear();
+        for (int iChildIndex = 0; iChildIndex < childCount(); ++iChildIndex)
+            child(iChildIndex).clear();
+    }
+
+private:
+
+    QString indexToKey(int iIndex) const
+    {
+        UISettingsCacheChildIterator childIterator(m_children);
+        for (int iChildIndex = 0; childIterator.hasNext(); ++iChildIndex)
+        {
+            childIterator.next();
+            if (iChildIndex == iIndex)
+                return childIterator.key();
+        }
+        return QString::number(iIndex);
+    }
+
+    /* Children: */
+    UISettingsCacheChildMap m_children;
+};
 
 #endif /* __UISettingsDefs_h__ */
 
