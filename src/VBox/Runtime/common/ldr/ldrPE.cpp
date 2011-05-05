14f1278a5bb4be17d6de345543e9799fc287882f
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2011-05-05 08:59:43 +0000

    more gcc-4.6 -Werror=int-to-pointer-cast fixes
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@36969 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Runtime/common/ldr/ldrPE.cpp
+++ src/VBox/Runtime/common/ldr/ldrPE.cpp
@@ -315,7 +315,7 @@ static DECLCALLBACK(int) rtldrPEResolveImports32(PRTLDRMODPE pModPe, const void 
                 rc = pfnGetImport(&pModPe->Core, pszModName, PE_RVA2TYPE(pvBitsR, (char*)(uintptr_t)pThunk->u1.AddressOfData + 2, const char *),
                                   ~0, &Value, pvUser);
                 Log4((RT_SUCCESS(rc) ? "RTLdrPE:  %RTptr %s\n" : "RTLdrPE:  %08RX32 %s rc=%Rrc\n",
-                      (uint32_t)Value, PE_RVA2TYPE(pvBitsR, (char*)pThunk->u1.AddressOfData + 2, const char *), rc));
+                      (uint32_t)Value, PE_RVA2TYPE(pvBitsR, (char*)(uintptr_t)pThunk->u1.AddressOfData + 2, const char *), rc));
             }
             else
             {
