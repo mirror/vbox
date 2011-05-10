a9d2fcaf08a1757810aa707b4b1521048a4df22b
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2011-05-10 08:20:35 +0000

    Guest Additions: Also report extended version information to the host to "/VirtualBox/GuestAdd/VersionEx".
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@37019 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Additions/common/VBoxService/VBoxServiceVMInfo.cpp
+++ src/VBox/Additions/common/VBoxService/VBoxServiceVMInfo.cpp
@@ -189,15 +189,19 @@ static void vboxserviceVMInfoWriteFixedProperties(void)
      * Retrieve version information about Guest Additions and installed files (components).
      */
     char *pszAddVer;
+    char *pszAddVerEx;
     char *pszAddRev;
-    rc = VbglR3GetAdditionsVersion(&pszAddVer, &pszAddRev);
+    rc = VbglR3GetAdditionsVersion(&pszAddVer, &pszAddVerEx, &pszAddRev);
     VBoxServiceWritePropF(g_uVMInfoGuestPropSvcClientID, "/VirtualBox/GuestAdd/Version",
                           "%s", RT_FAILURE(rc) ? "" : pszAddVer);
+    VBoxServiceWritePropF(g_uVMInfoGuestPropSvcClientID, "/VirtualBox/GuestAdd/VersionEx",
+                          "%s", RT_FAILURE(rc) ? "" : pszAddVerEx);
     VBoxServiceWritePropF(g_uVMInfoGuestPropSvcClientID, "/VirtualBox/GuestAdd/Revision",
                           "%s", RT_FAILURE(rc) ? "" : pszAddRev);
     if (RT_SUCCESS(rc))
     {
         RTStrFree(pszAddVer);
+        RTStrFree(pszAddVerEx);
         RTStrFree(pszAddRev);
     }
 
