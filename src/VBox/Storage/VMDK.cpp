f6bd280ad216c5504679f6d3b62cf98059a4bd47
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2011-05-16 11:01:03 +0000

    VD,VMDK: Enable debug logging for release builds
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@37100 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Storage/VMDK.cpp
+++ src/VBox/Storage/VMDK.cpp
@@ -19,6 +19,8 @@
 *   Header Files                                                               *
 *******************************************************************************/
 #define LOG_GROUP LOG_GROUP_VD_VMDK
+#define RT_STRICT
+#define LOG_ENABLED
 #include <VBox/vd-plugin.h>
 #include <VBox/err.h>
 
@@ -6622,7 +6624,7 @@ static unsigned vmdkGetOpenFlags(void *pBackendData)
 /** @copydoc VBOXHDDBACKEND::pfnSetOpenFlags */
 static int vmdkSetOpenFlags(void *pBackendData, unsigned uOpenFlags)
 {
-    LogFlowFunc(("pBackendData=%#p\n uOpenFlags=%#x", pBackendData, uOpenFlags));
+    LogFlowFunc(("pBackendData=%#p uOpenFlags=%#x\n", pBackendData, uOpenFlags));
     PVMDKIMAGE pImage = (PVMDKIMAGE)pBackendData;
     int rc;
 
