54afd02a31a9ba10ad6ef7784eba98ea538a41ae
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2011-05-17 11:26:00 +0000

    Installer/linux: refactored udev rule installation
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@37124 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Installer/linux/testcase/tstInstallerLinux.sh
+++ src/VBox/Installer/linux/testcase/tstInstallerLinux.sh
@@ -19,7 +19,7 @@
 
 CERRS=0
 
-echo "Testing udev rule generation for the \".run\" installer"
+echo "Testing udev rule generation"
 
 setup_test_input_install_udev ".run, udev-59" 59
 
@@ -31,7 +31,7 @@ SUBSYSTEM=="usb_device", ACTION=="remove", RUN+="/opt/VirtualBox/VBoxCreateUSBNo
 SUBSYSTEM=="usb", ACTION=="remove", ENV{DEVTYPE}=="usb_device", RUN+="/opt/VirtualBox/VBoxCreateUSBNode.sh --remove $major $minor"
 UDEV_END`
 
-install_udev_output="`install_udev_run vboxusers 0660 /opt/VirtualBox`"
+install_udev_output="`install_udev vboxusers 0660 /opt/VirtualBox`"
 case "$install_udev_output" in
     "$udev_59_rules") ;;
     *)
@@ -51,7 +51,7 @@ udev_55_rules=`cat <<'UDEV_END'
 KERNEL=="vboxdrv", NAME="vboxdrv", OWNER="root", GROUP="vboxusers", MODE="0660"
 UDEV_END`
 
-install_udev_output="`install_udev_run vboxusers 0660 /opt/VirtualBox`"
+install_udev_output="`install_udev vboxusers 0660 /opt/VirtualBox`"
 case "$install_udev_output" in
     "$udev_55_rules") ;;
     *)
@@ -71,7 +71,7 @@ udev_54_rules=`cat <<'UDEV_END'
 KERNEL="vboxdrv", NAME="vboxdrv", OWNER="root", GROUP="root", MODE="0600"
 UDEV_END`
 
-install_udev_output="`install_udev_run root 0600 /usr/lib/virtualbox`"
+install_udev_output="`install_udev root 0600 /usr/lib/virtualbox`"
 case "$install_udev_output" in
     "$udev_54_rules") ;;
     *)
@@ -84,89 +84,5 @@ case "$install_udev_output" in
 esac
 
 cleanup_test_input_install_udev
-
-echo "Testing udev rule generation for the \"package\" installer"
-
-setup_test_input_install_udev "package, udev-59" 59
-
-udev_59_rules=`cat <<'UDEV_END'
-KERNEL=="vboxdrv", NAME="vboxdrv", OWNER="root", GROUP="root", MODE="0600"
-SUBSYSTEM=="usb_device", ACTION=="add", RUN+="/usr/share/virtualbox/VBoxCreateUSBNode.sh $major $minor $attr{bDeviceClass} vboxusers"
-SUBSYSTEM=="usb", ACTION=="add", ENV{DEVTYPE}=="usb_device", RUN+="/usr/share/virtualbox/VBoxCreateUSBNode.sh $major $minor $attr{bDeviceClass} vboxusers"
-SUBSYSTEM=="usb_device", ACTION=="remove", RUN+="/usr/share/virtualbox/VBoxCreateUSBNode.sh --remove $major $minor"
-SUBSYSTEM=="usb", ACTION=="remove", ENV{DEVTYPE}=="usb_device", RUN+="/usr/share/virtualbox/VBoxCreateUSBNode.sh --remove $major $minor"
-UDEV_END`
-
-install_udev_output="`install_udev_package vboxusers`"
-case "$install_udev_output" in
-    "$udev_59_rules") ;;
-    *)
-        echo "Bad output for udev version 59.  Expected:"
-        echo "$udev_59_rules"
-        echo "Actual:"
-        echo "$install_udev_output"
-        CERRS="`expr "$CERRS" + 1`"
-        ;;
-esac
-
-cleanup_test_input_install_udev
-
-setup_test_input_install_udev "package, udev-55" 55
-
-udev_55_rules=`cat <<'UDEV_END'
-KERNEL=="vboxdrv", NAME="vboxdrv", OWNER="root", GROUP="root", MODE="0600"
-UDEV_END`
-
-install_udev_output="`install_udev_package vboxusers`"
-case "$install_udev_output" in
-    "$udev_55_rules") ;;
-    *)
-        echo "Bad output for udev version 55.  Expected:"
-        echo "$udev_55_rules"
-        echo "Actual:"
-        echo "$install_udev_output"
-        CERRS="`expr "$CERRS" + 1`"
-        ;;
-esac
-
-cleanup_test_input_install_udev
-
-setup_test_input_install_udev "package, udev-54" 54
-
-udev_54_rules=`cat <<'UDEV_END'
-KERNEL="vboxdrv", NAME="vboxdrv", OWNER="root", GROUP="root", MODE="0600"
-UDEV_END`
-
-install_udev_output="`install_udev_package root`"
-case "$install_udev_output" in
-    "$udev_54_rules") ;;
-    *)
-        echo "Bad output for udev version 54.  Expected:"
-        echo "$udev_54_rules"
-        echo "Actual:"
-        echo "$install_udev_output"
-        CERRS="`expr "$CERRS" + 1`"
-        ;;
-esac
-
-cleanup_test_input_install_udev
-
-setup_test_input_install_udev "package, no udev" 54
-INSTALL_NO_UDEV=1
-
-install_udev_output="`install_udev_package root`"
-case "$install_udev_output" in
-    "") ;;
-    *)
-        echo "Bad output for udev version 54.  Expected:"
-        echo "$udev_54_rules"
-        echo "Actual:"
-        echo "$install_udev_output"
-        CERRS="`expr "$CERRS" + 1`"
-        ;;
-esac
-
-cleanup_test_input_install_udev
-INSTALL_NO_UDEV=
 
 echo "Done.  Error count $CERRS."
