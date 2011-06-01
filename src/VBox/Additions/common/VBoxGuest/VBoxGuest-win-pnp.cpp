6472994e9437d151080eb8365277c14a7a698239
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2011-06-01 12:46:14 +0000

    VBoxGuest/win: comments v2
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@37291 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Additions/common/VBoxGuest/VBoxGuest-win-pnp.cpp
+++ src/VBox/Additions/common/VBoxGuest/VBoxGuest-win-pnp.cpp
@@ -245,7 +245,7 @@ NTSTATUS vboxguestwinPnP(PDEVICE_OBJECT pDevObj, PIRP pIrp)
                 rc = IoCallDriver(pDevExt->win.s.pNextLowerDriver, pIrp);
                 Log(("VBoxGuest::vboxguestwinGuestPnp: QUERY_REMOVE_DEVICE: Next lower driver replied rc = 0x%x\n", rc));
 
-                /* we must not do anything here after doing IoSkip & CallDriver
+                /* we must not do anything the IRP after doing IoSkip & CallDriver
                  * since the driver below us will complete (or already have completed) the IRP.
                  * I.e. just return the status we got from IoCallDriver */
                 return rc;
@@ -340,7 +340,7 @@ NTSTATUS vboxguestwinPnP(PDEVICE_OBJECT pDevObj, PIRP pIrp)
                 rc = IoCallDriver(pDevExt->win.s.pNextLowerDriver, pIrp);
                 Log(("VBoxGuest::vboxguestwinGuestPnp: QUERY_STOP_DEVICE: Next lower driver replied rc = 0x%x\n", rc));
 
-                /* we must not do anything here after doing IoSkip & CallDriver
+                /* we must not do anything with the IRP after doing IoSkip & CallDriver
                  * since the driver below us will complete (or already have completed) the IRP.
                  * I.e. just return the status we got from IoCallDriver */
                 return rc;
