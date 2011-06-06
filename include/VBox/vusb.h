3f92b1844513bea3e463987bc9e9986abb2fa3a9
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2011-06-06 05:18:46 +0000

    USB: device qualifier descriptor (from spec).
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@37325 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- include/VBox/vusb.h
+++ include/VBox/vusb.h
@@ -121,6 +121,30 @@ typedef VUSBDESCDEVICE *PVUSBDESCDEVICE;
 /** Pointer to a const USB device descriptor. */
 typedef const VUSBDESCDEVICE *PCVUSBDESCDEVICE;
 
+/**
+ * USB device qualifier (from spec)
+ */
+struct VUSBDEVICEQUALIFIER
+{
+    uint8_t bLength;
+    uint8_t bDescriptorType;
+    uint8_t bcdUsb;
+    uint8_t bDeviceClass;
+    uint8_t bDeviceSubClass;
+    uint8_t bDeviceProtocol;
+    uint8_t bMaxPacketSize0;
+    uint8_t idVendor;
+    uint8_t idProduct;
+    uint8_t bcdDevice;
+    uint8_t iManufacturer;
+    uint8_t iProduct;
+    uint8_t iSerialNumber;
+    uint8_t bNumConfigurations;
+};
+
+typedef struct VUSBDEVICEQUALIFIER VUSBDEVICEQUALIFIER;
+typedef VUSBDEVICEQUALIFIER *PVUSBDEVICEQUALIFIER;
+
 
 /**
  * USB configuration descriptor (from spec).
