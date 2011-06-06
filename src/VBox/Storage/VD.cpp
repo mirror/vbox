e02e02949972c825b6cc60025bb353b8b40b8c9b
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2011-06-06 15:43:39 +0000

    VD: Remove left over defines for logging and assertions
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@37329 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Storage/VD.cpp
+++ src/VBox/Storage/VD.cpp
@@ -19,8 +19,6 @@
 *   Header Files                                                               *
 *******************************************************************************/
 #define LOG_GROUP LOG_GROUP_VD
-#define RT_STRICT
-#define LOG_ENABLED
 #include <VBox/vd.h>
 #include <VBox/err.h>
 #include <VBox/sup.h>
