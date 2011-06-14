b6c03210351f5773f8c4a9d5e91b53165eedb213
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2011-06-14 20:09:32 +0000

    HostServices/SharedClipboard: setter for headless mode in service, ignored at present
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@37455 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/HostServices/SharedClipboard/VBoxClipboard.h
+++ src/VBox/HostServices/SharedClipboard/VBoxClipboard.h
@@ -73,6 +73,7 @@ void vboxSvcClipboardReportMsg (VBOXCLIPBOARDCLIENTDATA *pClient, uint32_t u32Ms
 
 void vboxSvcClipboardCompleteReadData(VBOXCLIPBOARDCLIENTDATA *pClient, int rc, uint32_t cbActual);
 
+bool vboxSvcClipboardGetHeadless(void);
 
 /*
  * Platform dependent functions.
@@ -90,5 +91,10 @@ int vboxClipboardReadData (VBOXCLIPBOARDCLIENTDATA *pClient, uint32_t u32Format,
 void vboxClipboardWriteData (VBOXCLIPBOARDCLIENTDATA *pClient, void *pv, uint32_t cb, uint32_t u32Format);
 
 int vboxClipboardSync (VBOXCLIPBOARDCLIENTDATA *pClient);
+
+/* Host unit testing interface */
+#ifdef UNIT_TEST
+uint32_t testClipSvcGetMode(void);
+#endif
 
 #endif /* __VBOXCLIPBOARD__H */
