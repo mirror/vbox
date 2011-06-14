a3d2a7759211552c92670c49529fd4ac148ee905
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2011-06-14 16:44:47 +0000

    VBoxManage: help item
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@37450 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Frontends/VBoxManage/VBoxManageHelp.cpp
+++ src/VBox/Frontends/VBoxManage/VBoxManageHelp.cpp
@@ -334,6 +334,7 @@ void printUsage(USAGECATEGORY u64Cmd, PRTSTREAM pStrm)
         RTStrmPrintf(pStrm,
                      "VBoxManage clonevm          <uuid>|<name>\n"
                      "                            [--snapshot <uuid>|<name>]\n"
+                     "                            [--mode machine|machineandchilds|all]\n"
                      "                            [--name <name>]\n"
                      "                            [--basefolder <basefolder>]\n"
                      "                            [--uuid <uuid>]\n"
