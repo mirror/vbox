9d15a56282c7254667f3c81fb43101284856c4eb
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2011-06-16 12:25:36 +0000

    Main-CloneVM: forgot to cleanup the header file
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@37492 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Main/include/MachineImpl.h
+++ src/VBox/Main/include/MachineImpl.h
@@ -839,17 +839,6 @@ protected:
     static DECLCALLBACK(int) deleteThread(RTTHREAD Thread, void *pvUser);
     HRESULT deleteTaskWorker(DeleteTask &task);
 
-    struct CloneVMTask;
-    HRESULT cloneCreateMachineList(const ComPtr<ISnapshot> &pSnapshot, RTCList< ComObjPtr<Machine> > &machineList) const;
-    settings::Snapshot cloneFindSnapshot(settings::MachineConfigFile *pMCF, const settings::SnapshotsList &snl, const Guid &id) const;
-    void cloneUpdateStorageLists(settings::StorageControllersList &sc, const Bstr &bstrOldId, const Bstr &bstrNewId) const;
-    void cloneUpdateSnapshotStorageLists(settings::SnapshotsList &sl, const Bstr &bstrOldId, const Bstr &bstrNewId) const;
-    void cloneUpdateStateFile(settings::SnapshotsList &snl, const Guid &id, const Utf8Str &strFile) const;
-    static int cloneCopyStateFileProgress(unsigned uPercentage, void *pvUser);
-
-    static DECLCALLBACK(int) cloneVMThread(RTTHREAD Thread, void *pvUser);
-    HRESULT cloneVMTaskWorker(CloneVMTask *pTask);
-
 #ifdef VBOX_WITH_GUEST_PROPS
     HRESULT getGuestPropertyFromService(IN_BSTR aName, BSTR *aValue,
                                         LONG64 *aTimestamp, BSTR *aFlags) const;
