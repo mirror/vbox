836fa00b74bcaec09bd4be54d75e0ea6eb9b8a30
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2011-06-17 10:17:38 +0000

    Dev*: Timer locking fixes.
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@37526 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Devices/PC/DevHPET.cpp
+++ src/VBox/Devices/PC/DevHPET.cpp
@@ -1252,7 +1252,10 @@ static DECLCALLBACK(void) hpetReset(PPDMDEVINS pDevIns)
     HpetState *pThis = PDMINS_2_DATA(pDevIns, HpetState *);
     LogFlow(("hpetReset:\n"));
 
-    pThis->u64HpetConfig = 0;
+    /*
+     * The timers first.
+     */
+    TMTimerLock(pThis->aTimers[0].pTimerR3, VERR_IGNORED);
     for (unsigned i = 0; i < HPET_NUM_TIMERS; i++)
     {
         HpetTimer *pHpetTimer = &pThis->aTimers[i];
@@ -1274,6 +1277,12 @@ static DECLCALLBACK(void) hpetReset(PPDMDEVINS pDevIns)
         pHpetTimer->u8Wrap     = 0;
         pHpetTimer->u64Cmp     = hpetInvalidValue(pHpetTimer);
     }
+    TMTimerUnlock(pThis->aTimers[0].pTimerR3);
+
+    /*
+     * The HPET state.
+     */
+    pThis->u64HpetConfig  = 0;
     pThis->u64HpetCounter = 0;
     pThis->u64HpetOffset  = 0;
 
@@ -1289,7 +1298,9 @@ static DECLCALLBACK(void) hpetReset(PPDMDEVINS pDevIns)
     pThis->u64Capabilities = (u32Vendor << 16) | u32Caps;
     pThis->u64Capabilities |= ((uint64_t)(pThis->fIch9 ? HPET_CLK_PERIOD_ICH9 : HPET_CLK_PERIOD) << 32);
 
-    /* Notify PIT/RTC devices */
+    /*
+     * Notify the PIT/RTC devices.
+     */
     if (pThis->pHpetHlpR3)
         pThis->pHpetHlpR3->pfnSetLegacyMode(pDevIns, false /*fActive*/);
 }
--- src/VBox/Devices/PC/DevPit-i8254.cpp
+++ src/VBox/Devices/PC/DevPit-i8254.cpp
@@ -976,7 +976,9 @@ static DECLCALLBACK(int) pitLoadExec(PPDMDEVINS pDevIns, PSSMHANDLE pSSM, uint32
             TMR3TimerLoad(s->CTX_SUFF(pTimer), pSSM);
             LogRel(("PIT: mode=%d count=%#x (%u) - %d.%02d Hz (ch=%d) (restore)\n",
                     s->mode, s->count, s->count, PIT_FREQ / s->count, (PIT_FREQ * 100 / s->count) % 100, i));
+            PDMCritSectEnter(&pThis->CritSect, VERR_IGNORED);
             TMTimerSetFrequencyHint(s->CTX_SUFF(pTimer), PIT_FREQ / s->count);
+            PDMCritSectLeave(&pThis->CritSect);
         }
         pThis->channels[i].cRelLogEntries = 0;
     }
