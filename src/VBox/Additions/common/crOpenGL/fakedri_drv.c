a0d6bd50ceb5d20a4f406a3f1f983ffbd19ff75f
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2011-06-22 16:26:29 +0000

    crOpenGL: fix dri driver loading for ubuntu oneiric
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@37594 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Additions/common/crOpenGL/fakedri_drv.c
+++ src/VBox/Additions/common/crOpenGL/fakedri_drv.c
@@ -37,10 +37,10 @@
 
 //@todo this could be different...
 #ifdef RT_ARCH_AMD64
-# define DRI_DEFAULT_DRIVER_DIR "/usr/lib64/dri:/usr/lib/dri"
+# define DRI_DEFAULT_DRIVER_DIR "/usr/lib64/dri:/usr/lib/dri:/usr/lib/x86_64-linux-gnu/dri"
 # define DRI_XORG_DRV_DIR "/usr/lib/xorg/modules/drivers/"
 #else
-# define DRI_DEFAULT_DRIVER_DIR "/usr/lib/dri"
+# define DRI_DEFAULT_DRIVER_DIR "/usr/lib/dri:/usr/lib/i386-linux-gnu/dri"
 # define DRI_XORG_DRV_DIR "/usr/lib/xorg/modules/drivers/"
 #endif
 
