3caec938877f26531850b57e943b6fcb024245d2
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2011-06-27 11:09:36 +0000

    e1000: applied e1kRegRead fix suggested by Vasily
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@37644 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Devices/Network/DevE1000.cpp
+++ src/VBox/Devices/Network/DevE1000.cpp
@@ -4273,12 +4273,8 @@ static int e1kRegRead(E1KSTATE *pState, uint32_t uOffset, void *pv, uint32_t cb)
             //pState->fDelayInts = false;
             //pState->iStatIntLost += pState->iStatIntLostOne;
             //pState->iStatIntLostOne = 0;
-            rc = s_e1kRegMap[index].pfnRead(pState, uOffset & 0xFFFFFFFC, index, &u32) & mask;
-            /** r=vasily
-             * Looks like a typo, probably expected smth. like:
-             * rc = s_e1kRegMap[index].pfnRead(pState, uOffset & 0xFFFFFFFC, index, &u32);
-             * u32 &= mask;
-             */
+            rc = s_e1kRegMap[index].pfnRead(pState, uOffset & 0xFFFFFFFC, index, &u32);
+            u32 &= mask;
             //e1kCsLeave(pState);
             e1kMutexRelease(pState);
             E1kLog2(("%s At %08X read  %s          from %s (%s)\n",
