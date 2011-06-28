69feed09c4dc8c76496d5b75fa78e7a55b1f6abb
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2011-06-28 11:41:49 +0000

    eliminate unneeded include file
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@37662 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Main/src-server/solaris/NetIf-solaris.cpp
+++ src/VBox/Main/src-server/solaris/NetIf-solaris.cpp
@@ -4,7 +4,7 @@
  */
 
 /*
- * Copyright (C) 2008 Oracle Corporation
+ * Copyright (C) 2008-2011 Oracle Corporation
  *
  * This file is part of VirtualBox Open Source Edition (OSE), as
  * available from http://www.virtualbox.org. This file is free software;
@@ -37,7 +37,6 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include <stropts.h>
-#include <errno.h>
 #include <limits.h>
 #include <stdio.h>
 #include <libdevinfo.h>
--- src/VBox/Main/testcase/tstHostHardwareLinux.cpp
+++ src/VBox/Main/testcase/tstHostHardwareLinux.cpp
@@ -6,7 +6,7 @@
  */
 
 /*
- * Copyright (C) 2008 Oracle Corporation
+ * Copyright (C) 2008-2011 Oracle Corporation
  *
  * This file is part of VirtualBox Open Source Edition (OSE), as
  * available from http://www.virtualbox.org. This file is free software;
@@ -31,7 +31,6 @@
 #include <iprt/cdefs.h>
 #include <iprt/types.h>
 
-#include <errno.h>
 #include <string.h>
 #include <stdlib.h>
 
