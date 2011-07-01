3f3f802c4fa8bf1d8f2e40c2309719dc21f862c2
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2011-07-01 10:45:05 +0000

    Devices/VGA: make sure that physical resolution is properly bounded by virtual resolution
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@37725 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Devices/Graphics/DevVGA.cpp
+++ src/VBox/Devices/Graphics/DevVGA.cpp
@@ -1950,7 +1950,8 @@ static void vga_get_resolution(VGAState *s, int *pwidth, int *pheight)
 #ifdef CONFIG_BOCHS_VBE
     if (s->vbe_regs[VBE_DISPI_INDEX_ENABLE] & VBE_DISPI_ENABLED) {
         width = s->vbe_regs[VBE_DISPI_INDEX_XRES];
-        height = s->vbe_regs[VBE_DISPI_INDEX_YRES];
+        height = RT_MIN(s->vbe_regs[VBE_DISPI_INDEX_YRES],
+                        s->vbe_regs[VBE_DISPI_INDEX_VIRT_HEIGHT]);
     } else
 #endif
     {
