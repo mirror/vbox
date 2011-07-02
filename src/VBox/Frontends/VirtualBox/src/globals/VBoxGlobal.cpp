3362b7007114722074393c3ff02bbe4429b3b2ae
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2011-07-02 13:24:54 +0000

    wddm: fix max VRAM size in VM settings GUI
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@37735 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Frontends/VirtualBox/src/globals/VBoxGlobal.cpp
+++ src/VBox/Frontends/VirtualBox/src/globals/VBoxGlobal.cpp
@@ -3868,7 +3868,7 @@ quint64 VBoxGlobal::requiredVideoMemory(const QString &strGuestOSTypeId, int cMo
     {
        /* Windows guests need offscreen VRAM too for graphics acceleration features: */
 #ifdef VBOX_WITH_CRHGSMI
-       if (strGuestOSTypeId == "WindowsVista" || strGuestOSTypeId == "Windows7")
+       if (isWddmCompatibleOsType(strGuestOSTypeId))
        {
            /* wddm mode, there are two surfaces for each screen: shadow & primary */
            needMBytes *= 3;
@@ -4437,6 +4437,12 @@ quint64 VBoxGlobal::required3DWddmOffscreenVideoMemory(const QString &strGuestOS
     quint64 cbSize = VBoxGlobal::requiredVideoMemory(strGuestOSTypeId, 1); /* why not cMonitors? */
     cbSize += 64 * _1M;
     return cbSize;
+}
+
+/* static */
+bool VBoxGlobal::isWddmCompatibleOsType(const QString &strGuestOSTypeId)
+{
+    return strGuestOSTypeId.startsWith("WindowsVista") || strGuestOSTypeId.startsWith("Windows7") || strGuestOSTypeId.startsWith("Windows2008");
 }
 #endif /* VBOX_WITH_CRHGSMI */
 
