81199a33837a90a378e45d64b29668b51f7e4962
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2011-07-05 14:13:19 +0000

    Solaris/Installer: plumb and network link up separately.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@37784 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Installer/solaris/vboxconfig.sh
+++ src/VBox/Installer/solaris/vboxconfig.sh
@@ -811,7 +811,8 @@ postinstall()
 
             # plumb and configure vboxnet0 for non-remote installs
             if test "$REMOTEINST" -eq 0; then
-                $BIN_IFCONFIG vboxnet0 plumb up
+                $BIN_IFCONFIG vboxnet0 plumb
+                $BIN_IFCONFIG vboxnet0 up
                 if test "$?" -eq 0; then
                     $BIN_IFCONFIG vboxnet0 192.168.56.1 netmask 255.255.255.0 up
 
