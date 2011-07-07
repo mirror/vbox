4947a4d309661848785d187074eae760a801f059
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2011-07-07 14:18:26 +0000

    gen-slickedit-workspace.sh: fixed the file header.
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@37821 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- tools/bin/gen-slickedit-workspace.cmd
+++ tools/bin/gen-slickedit-workspace.cmd
@@ -5,11 +5,17 @@ rem Windows NT batch script for launching gen-slickedit-workspace.sh
 rem
 
 rem
-rem Copyright (C) 2009 Oracle Corporation
+rem Copyright (C) 2009-2011 Oracle Corporation
 rem
-rem Oracle Corporation confidential
-rem All rights reserved
+rem This file is part of VirtualBox Open Source Edition (OSE), as
+rem available from http://www.virtualbox.org. This file is free software;
+rem you can redistribute it and/or modify it under the terms of the GNU
+rem General Public License (GPL) as published by the Free Software
+rem Foundation, in version 2 as it comes in the "COPYING" file of the
+rem VirtualBox OSE distribution. VirtualBox OSE is distributed in the
+rem hope that it will be useful, but WITHOUT ANY WARRANTY of any kind.
 rem
+
 
 setlocal ENABLEEXTENSIONS
 setlocal
