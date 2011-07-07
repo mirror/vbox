c659f38dc9e3acb004b055e69c8e781ac9a2af22
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2011-07-07 20:21:33 +0000

    Additions/VBoxService: automount broken on Solaris
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@37826 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Additions/common/VBoxService/VBoxServiceAutoMount.cpp
+++ src/VBox/Additions/common/VBoxService/VBoxServiceAutoMount.cpp
@@ -4,7 +4,7 @@
  */
 
 /*
- * Copyright (C) 2010 Oracle Corporation
+ * Copyright (C) 2010-2011 Oracle Corporation
  *
  * This file is part of VirtualBox Open Source Edition (OSE), as
  * available from http://www.virtualbox.org. This file is free software;
@@ -264,7 +264,7 @@ static int VBoxServiceAutoMountSharedFolder(const char *pszShareName, const char
         int r = mount(pszShareName,
                       pszMountPoint,
                       flags,
-                      "vboxsf",
+                      "vboxfs",
                       NULL,                     /* char *dataptr */
                       0,                        /* int datalen */
                       NULL,                     /* char *optptr */
