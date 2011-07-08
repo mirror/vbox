2693a5ba9f1c9a7f94da4630612b17d749eb533c
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2011-07-08 10:13:18 +0000

    Additions/VBoxService: fix automounting Solaris Shared folders.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@37832 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Additions/common/VBoxService/VBoxServiceAutoMount.cpp
+++ src/VBox/Additions/common/VBoxService/VBoxServiceAutoMount.cpp
@@ -260,15 +260,19 @@ static int VBoxServiceAutoMountSharedFolder(const char *pszShareName, const char
     if (RT_SUCCESS(rc))
     {
 #ifdef RT_OS_SOLARIS
-        int flags = 0; /* No flags used yet. */
+        char achOptBuf[MAX_MNTOPT_STR] = { '\0', };
+        int flags = 0;
+        if (pOpts->ronly)
+            flags |= MS_RDONLY;
+        RTStrPrintf(achOptBuf, sizeof(achOptBuf), "uid=%d,gid=%d", pOpts->uid, pOpts->gid);
         int r = mount(pszShareName,
                       pszMountPoint,
-                      flags,
+                      flags | MS_OPTIONSTR,
                       "vboxfs",
                       NULL,                     /* char *dataptr */
                       0,                        /* int datalen */
-                      NULL,                     /* char *optptr */
-                      0);                       /* int optlen */
+                      achOptBuf,
+                      sizeof(achOptBuf));
         if (r == 0)
         {
             VBoxServiceVerbose(0, "VBoxServiceAutoMountWorker: Shared folder \"%s\" was mounted to \"%s\"\n", pszShareName, pszMountPoint);
