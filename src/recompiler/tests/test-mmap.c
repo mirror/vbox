116651e5883de4273d2d5b056354d23e1421e7ff
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2011-07-08 08:35:47 +0000

    header fixes
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@37829 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/recompiler/tests/test-mmap.c
+++ src/recompiler/tests/test-mmap.c
@@ -22,6 +22,15 @@
  * along with this program; if not, see <http://www.gnu.org/licenses/>.
  */
 
+/*
+ * Oracle GPL Disclaimer: For the avoidance of doubt, except that if any license choice
+ * other than GPL or LGPL is available it will apply instead, Oracle elects to use only
+ * the General Public License version 2 (GPLv2) at this time for any software where
+ * a choice of GPL license versions is made available with the language indicating
+ * that GPLv2 or any later version may be used, or where a choice of which version
+ * of the GPL is applied is otherwise unspecified.
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
