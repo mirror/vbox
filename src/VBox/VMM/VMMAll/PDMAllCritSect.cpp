0bb469470c329309cbab55f321111151570fde51
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2011-07-18 22:40:50 +0000

    PDMCritSectEnter: Fixed rcBusy=VINF_SUCCESS case. Only affected non-strict builds, so it was kinda hard to notice, though there should've been occational VERR_SEM_BUSY gurus. Weird.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@38042 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/VMM/VMMAll/PDMAllCritSect.cpp
+++ src/VBox/VMM/VMMAll/PDMAllCritSect.cpp
@@ -319,28 +319,12 @@ DECL_FORCE_INLINE(int) pdmCritSectEnter(PPDMCRITSECT pCritSect, int rcBusy, PCRT
  */
 VMMDECL(int) PDMCritSectEnter(PPDMCRITSECT pCritSect, int rcBusy)
 {
-    int rc;
-#ifndef IN_RING3
-    if (rcBusy == VINF_SUCCESS)
-    {
-# ifndef PDMCRITSECT_STRICT
-        rc = pdmCritSectEnter(pCritSect, VERR_SEM_BUSY, NULL);
-# else
-        RTLOCKVALSRCPOS SrcPos = RTLOCKVALSRCPOS_INIT_NORMAL_API();
-        rc = pdmCritSectEnter(pCritSect, VERR_SEM_BUSY, &SrcPos);
-# endif
-    }
-    else
-#endif /* !IN_RING3 */
-    {
 #ifndef PDMCRITSECT_STRICT
-        rc = pdmCritSectEnter(pCritSect, rcBusy, NULL);
+    return pdmCritSectEnter(pCritSect, rcBusy, NULL);
 #else
-        RTLOCKVALSRCPOS SrcPos = RTLOCKVALSRCPOS_INIT_NORMAL_API();
-        rc = pdmCritSectEnter(pCritSect, rcBusy, &SrcPos);
+    RTLOCKVALSRCPOS SrcPos = RTLOCKVALSRCPOS_INIT_NORMAL_API();
+    return pdmCritSectEnter(pCritSect, rcBusy, &SrcPos);
 #endif
-    }
-    return rc;
 }
 
 
