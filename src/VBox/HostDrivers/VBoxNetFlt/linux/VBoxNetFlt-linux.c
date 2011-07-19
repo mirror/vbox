d9d8b3ff669ff9a8bccea2e9d4b2c00f42a0e2b5
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2011-07-19 08:50:19 +0000

    netflt: fix for regression introduced in r72926
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@38054 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/HostDrivers/VBoxNetFlt/linux/VBoxNetFlt-linux.c
+++ src/VBox/HostDrivers/VBoxNetFlt/linux/VBoxNetFlt-linux.c
@@ -962,14 +962,12 @@ static void vboxNetFltLinuxHookDev(PVBOXNETFLTINS pThis, struct net_device *pDev
     if (!pOverride)
         return;
     pOverride->pOrgOps              = pDev->OVR_OPS;
+    /* We only need to save ethtool_ops structure if it is present (#5712) */
+    if (VALID_PTR(pDev->OVR_OPS))
+        pOverride->Ops              = *pDev->OVR_OPS;
 # if LINUX_VERSION_CODE < KERNEL_VERSION(2, 6, 29)
-    /**
-     * There is no need to save ethtool_ops structure since we only modify
-     * the pointer itself and the structure is optional (#5712).
-     */
     pOverride->pfnStartXmit         = pDev->hard_start_xmit;
 # else /* LINUX_VERSION_CODE >= KERNEL_VERSION(2, 6, 29) */
-    pOverride->Ops                  = *pDev->OVR_OPS;
     pOverride->Ops.ndo_start_xmit   = vboxNetFltLinuxStartXmitFilter;
 # endif /* LINUX_VERSION_CODE >= KERNEL_VERSION(2, 6, 29) */
     pOverride->u32Magic             = VBOXNETDEVICEOPSOVERRIDE_MAGIC;
