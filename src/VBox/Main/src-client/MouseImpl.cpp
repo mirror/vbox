37059d95813c8660f16cf6dc59ccff4464910fcd
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2011-07-29 15:42:26 +0000

    main/mouse: correct abs mouse pointer calculation for windows guests (public bugs #7566, #9321)
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@38242 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Main/src-client/MouseImpl.cpp
+++ src/VBox/Main/src-client/MouseImpl.cpp
@@ -508,8 +508,8 @@ HRESULT Mouse::convertDisplayRes(LONG x, LONG y, int32_t *pcX, int32_t *pcY,
         int32_t x1, y1, x2, y2;
         /* Takes the display lock */
         pDisplay->getFramebufferDimensions(&x1, &y1, &x2, &y2);
-        *pcX = x1 != x2 ? (x - 1 - x1) * MOUSE_RANGE_UPPER / (x2 - x1) : 0;
-        *pcY = y1 != y2 ? (y - 1 - y1) * MOUSE_RANGE_UPPER / (y2 - y1) : 0;
+        *pcX = x1 < x2 ? (x - 1 - x1) * MOUSE_RANGE_UPPER / (x2 - x1 - 1) : 0;
+        *pcY = y1 < y2 ? (y - 1 - y1) * MOUSE_RANGE_UPPER / (y2 - y1 - 1) : 0;
         if (   *pcX < MOUSE_RANGE_LOWER || *pcX > MOUSE_RANGE_UPPER
             || *pcY < MOUSE_RANGE_LOWER || *pcY > MOUSE_RANGE_UPPER)
             if (pfValid)
