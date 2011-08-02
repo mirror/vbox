dd7699d563ce64025f27ff5e709781d0e8f3f125
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2011-08-02 11:14:44 +0000

    Main/DisplayImpl: document an internal API
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@38275 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Main/src-client/DisplayImpl.cpp
+++ src/VBox/Main/src-client/DisplayImpl.cpp
@@ -973,6 +973,11 @@ void Display::handleDisplayUpdate (unsigned uScreenId, int x, int y, int w, int 
     }
 }
 
+/**
+ * Returns the upper left and lower right corners of the virtual framebuffer.
+ * The lower right is "exclusive" (i.e. first pixel beyond the framebuffer),
+ * and the origin is (0, 0), not (1, 1) like the GUI returns.
+ */
 void Display::getFramebufferDimensions(int32_t *px1, int32_t *py1,
                                        int32_t *px2, int32_t *py2)
 {
