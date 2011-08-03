9dc4aec9c9ea3d61b9db65d688b5e31a6ed4f069
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2011-08-03 11:16:08 +0000

    xpcom build fix.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@38296 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Main/include/GuestDirEntryImpl.h
+++ src/VBox/Main/include/GuestDirEntryImpl.h
@@ -1,6 +1,6 @@
+/* $Id$ */
 /** @file
- *
- * VirtualBox COM class implementation
+ * VirtualBox Main - interface for guest directory entries, VBoxC.
  */
 
 /*
@@ -23,38 +23,42 @@
 class Guest;
 class GuestProcessStreamBlock;
 
+/**
+ * A directory entry.
+ */
 class ATL_NO_VTABLE GuestDirEntry :
     public VirtualBoxBase,
     VBOX_SCRIPTABLE_IMPL(IGuestDirEntry)
 {
 public:
+    /** @name COM and internal init/term/mapping cruft.
+     * @{ */
     VIRTUALBOXBASE_ADD_ERRORINFO_SUPPORT(GuestDirEntry, IGuestDirEntry)
-
     DECLARE_NOT_AGGREGATABLE(GuestDirEntry)
-
     DECLARE_PROTECT_FINAL_CONSTRUCT()
-
     BEGIN_COM_MAP(GuestDirEntry)
         VBOX_DEFAULT_INTERFACE_ENTRIES(IGuestDirEntry)
     END_COM_MAP()
-
     DECLARE_EMPTY_CTOR_DTOR(GuestDirEntry)
 
-    // Public initializer/uninitializer for internal purposes only
     HRESULT init(Guest *aParent, GuestProcessStreamBlock &streamBlock);
-    void uninit();
-
+    void    uninit();
     HRESULT FinalConstruct();
-    void FinalRelease();
+    void    FinalRelease();
+    /** @}  */
 
-    // IGuestDirEntry properties
+    /** @name IGuestDirEntry properties
+     * @{ */
     STDMETHOD(COMGETTER(NodeId))(LONG64 *aNodeId);
     STDMETHOD(COMGETTER(Name))(BSTR *aName);
     STDMETHOD(COMGETTER(Type))(GuestDirEntryType_T *aType);
+    /** @}  */
 
 public:
-    // Public internal methods
+    /** @name Public internal methods
+     * @{ */
     static GuestDirEntryType_T fileTypeToEntryType(const char *pszFileType);
+    /** @}  */
 
 private:
     struct Data
@@ -68,5 +72,5 @@ private:
     } mData;
 };
 
-#endif // ____H_GUESTDIRENTRYIMPL
+#endif /* !____H_GUESTDIRENTRYIMPL */
 
