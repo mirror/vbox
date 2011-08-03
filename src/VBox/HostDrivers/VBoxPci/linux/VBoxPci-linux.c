c99d60922b51c36e164110baba88020d051e11a5
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2011-08-03 11:35:54 +0000

    VBoxPci/linux: adapt to new location/name of header file in Linux 3.1. Contributed by Larry Finger.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@38299 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/HostDrivers/VBoxPci/linux/VBoxPci-linux.c
+++ src/VBox/HostDrivers/VBoxPci/linux/VBoxPci-linux.c
@@ -35,7 +35,11 @@
 #ifdef VBOX_WITH_IOMMU
 #include <linux/dmar.h>
 #include <linux/intel-iommu.h>
-#include <asm/amd_iommu.h>
+#if LINUX_VERSION_CODE < KERNEL_VERSION(3, 1, 0)
+# include <asm/amd_iommu.h>
+#else
+# include <linux/amd-iommu.h>
+#endif
 #endif
 
 
