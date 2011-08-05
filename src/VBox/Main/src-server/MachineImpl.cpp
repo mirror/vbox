f91357d1731f3365f7e8c60892db11f20875e133
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2011-08-05 15:18:40 +0000

    Main/MachineImpl: Allow hotplugging of CD/DVD drives
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@38329 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Main/src-server/MachineImpl.cpp
+++ src/VBox/Main/src-server/MachineImpl.cpp
@@ -3456,10 +3456,6 @@ STDMETHODIMP Machine::AttachDevice(IN_BSTR aControllerName,
                         tr("Controller '%ls' does not support hotplugging"),
                         aControllerName);
 
-    if (fHotplug && aType == DeviceType_DVD)
-        return setError(VBOX_E_INVALID_VM_STATE,
-                        tr("Attaching a DVD drive while the VM is running is not supported"));
-
     // check that the port and device are not out of range
     rc = ctl->checkPortAndDeviceValid(aControllerPort, aDevice);
     if (FAILED(rc)) return rc;
@@ -3891,10 +3887,6 @@ STDMETHODIMP Machine::DetachDevice(IN_BSTR aControllerName, LONG aControllerPort
         return setError(VBOX_E_OBJECT_NOT_FOUND,
                         tr("No storage device attached to device slot %d on port %d of controller '%ls'"),
                         aDevice, aControllerPort, aControllerName);
-
-    if (fHotplug && pAttach->getType() == DeviceType_DVD)
-        return setError(VBOX_E_INVALID_VM_STATE,
-                        tr("Detaching a DVD drive while the VM is running is not supported"));
 
     /*
      * The VM has to detach the device before we delete any implicit diffs.
