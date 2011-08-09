d08eddc3c43e6ccc345cd5a6a746bba099819f32
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2011-08-09 11:58:57 +0000

    Drain more keys (Ctrl-Alt-Del generates more than bytes for key up). This whole routine is wrong anyway.
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@38376 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Devices/PC/BIOS/rombios.c
+++ src/VBox/Devices/PC/BIOS/rombios.c
@@ -1840,12 +1840,12 @@ keyboard_init()
     while ( (inb(0x64) & 0x02) && (--max>0)) outb(0x80, 0x00);
 
     /* flush incoming keys */
-    max=4;
+    max=8;
     while (--max > 0) {
         outb(0x80, 0x00);
         if (inb(0x64) & 0x01) {
             inb(0x60);
-            max = 4;
+            max = 8;
             }
         }
 
