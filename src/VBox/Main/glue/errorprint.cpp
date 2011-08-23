e0cf8ae62cae481085ec3b1b20d77c2dbc4f493a
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2011-08-23 15:07:55 +0000

    Main/glue/ErrorInfo: print _all_ error messages, not only the last one; add glue code for printing errors from a IProgress object
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@38509 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Main/glue/errorprint.cpp
+++ src/VBox/Main/glue/errorprint.cpp
@@ -29,17 +29,49 @@
 namespace com
 {
 
-void GluePrintErrorInfo(com::ErrorInfo &info)
+void GluePrintErrorInfo(const com::ErrorInfo &info)
 {
-    Utf8Str str = Utf8StrFmt("%ls\n"
-                             "Details: code %Rhrc (0x%RX32), component %ls, interface %ls, callee %ls\n"
-                             ,
-                             info.getText().raw(),
-                             info.getResultCode(),
-                             info.getResultCode(),
-                             info.getComponent().raw(),
-                             info.getInterfaceName().raw(),
-                             info.getCalleeName().raw());
+    bool haveResultCode = false;
+#if defined (RT_OS_WIN)
+    haveResultCode = info.isFullAvailable();
+    bool haveComponent = true;
+    bool haveInterfaceID = true;
+#else /* defined (RT_OS_WIN) */
+    haveResultCode = true;
+    bool haveComponent = info.isFullAvailable();
+    bool haveInterfaceID = info.isFullAvailable();
+#endif
+
+    Utf8Str str;
+    RTCList<Utf8Str> comp;
+
+    Bstr bstrDetailsText = info.getText();
+    if (!bstrDetailsText.isEmpty())
+        str = Utf8StrFmt("%ls\n",
+                         bstrDetailsText.raw());
+    if (haveResultCode)
+        comp.append(Utf8StrFmt("code %Rhrc (0x%RX32)",
+                               info.getResultCode(),
+                               info.getResultCode()));
+    if (haveComponent)
+        comp.append(Utf8StrFmt("component %ls",
+                               info.getComponent().raw()));
+    if (haveInterfaceID)
+        comp.append(Utf8StrFmt("interface %ls",
+                               info.getInterfaceName().raw()));
+    if (!info.getCalleeName().isEmpty())
+        comp.append(Utf8StrFmt("callee %ls",
+                               info.getCalleeName().raw()));
+
+    if (comp.size() > 0)
+    {
+        str += "Details: ";
+        for (size_t i = 0; i < comp.size() - 1; ++i)
+            str += comp.at(i) + ", ";
+        str += comp.last();
+        str += "\n";
+    }
+
     // print and log
     RTMsgError("%s", str.c_str());
     Log(("ERROR: %s", str.c_str()));
@@ -51,11 +83,11 @@ void GluePrintErrorContext(const char *pcszContext, const char *pcszSourceFile, 
     // which we don't want to see printed:
     Utf8Str strFilename(RTPathFilename(pcszSourceFile));
     Utf8Str str = Utf8StrFmt("Context: \"%s\" at line %d of file %s\n",
-                                pcszContext,
-                                ulLine,
-                                strFilename.c_str());
+                             pcszContext,
+                             ulLine,
+                             strFilename.c_str());
     // print and log
-    RTStrmPrintf(g_pStdErr, "%s", str.c_str());
+    RTMsgError("%s", str.c_str());
     Log(("%s", str.c_str()));
 }
 
@@ -67,21 +99,66 @@ void GluePrintRCMessage(HRESULT rc)
     Log(("ERROR: %s", str.c_str()));
 }
 
+static void glueHandleComErrorInternal(com::ErrorInfo &info,
+                                       const char *pcszContext,
+                                       HRESULT rc,
+                                       const char *pcszSourceFile,
+                                       uint32_t ulLine)
+{
+    const com::ErrorInfo *pInfo = &info;
+    do
+    {
+        if (pInfo->isFullAvailable() || pInfo->isBasicAvailable())
+            GluePrintErrorInfo(*pInfo);
+        else
+#if defined (RT_OS_WIN)
+            GluePrintRCMessage(rc);
+#else /* defined (RT_OS_WIN) */
+            GluePrintRCMessage(pInfo->getResultCode());
+#endif
+        pInfo = pInfo->getNext();
+        /* If there is more than one error, separate them visually. */
+        if (pInfo)
+            RTMsgError("--------\n");
+    }
+    while(pInfo);
+
+    GluePrintErrorContext(pcszContext, pcszSourceFile, ulLine);
+}
+
 void GlueHandleComError(ComPtr<IUnknown> iface,
                         const char *pcszContext,
                         HRESULT rc,
                         const char *pcszSourceFile,
                         uint32_t ulLine)
 {
-    // if we have full error info, print something nice, and start with the actual error message
+    /* If we have full error info, print something nice, and start with the
+     * actual error message. */
     com::ErrorInfo info(iface, COM_IIDOF(IUnknown));
-    if (info.isFullAvailable() || info.isBasicAvailable())
-        GluePrintErrorInfo(info);
-    else
-        GluePrintRCMessage(rc);
-    GluePrintErrorContext(pcszContext, pcszSourceFile, ulLine);
+
+    glueHandleComErrorInternal(info,
+                               pcszContext,
+                               rc,
+                               pcszSourceFile,
+                               ulLine);
+
 }
 
+void GlueHandleComErrorProgress(ComPtr<IProgress> progress,
+                                const char *pcszContext,
+                                HRESULT rc,
+                                const char *pcszSourceFile,
+                                uint32_t ulLine)
+{
+    /* Get the error info out of the progress object. */
+    ProgressErrorInfo ei(progress);
+
+    glueHandleComErrorInternal(ei,
+                               pcszContext,
+                               rc,
+                               pcszSourceFile,
+                               ulLine);
+}
 
 } /* namespace com */
 
