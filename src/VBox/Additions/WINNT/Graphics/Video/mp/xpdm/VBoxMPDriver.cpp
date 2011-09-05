d43e19fd7e01d5b26273701aa188c032eab5fe37
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2011-09-05 13:12:08 +0000

    Windows guest XPDM video miniport: removed unnecessary code from ResetHW callback.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@38635 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Additions/WINNT/Graphics/Video/mp/xpdm/VBoxMPDriver.cpp
+++ src/VBox/Additions/WINNT/Graphics/Video/mp/xpdm/VBoxMPDriver.cpp
@@ -586,6 +586,8 @@ VBoxDrvResetHW(PVOID HwDeviceExtension, ULONG Columns, ULONG Rows)
         VideoPortWritePortUshort((PUSHORT)VBE_DISPI_IOPORT_INDEX, VBE_DISPI_INDEX_ENABLE);
         VideoPortWritePortUshort((PUSHORT)VBE_DISPI_IOPORT_DATA, VBE_DISPI_DISABLED);
 
+#if 0
+        /* ResetHW is not the place to do such cleanup. See MSDN. */
         if (pExt->u.primary.pvReqFlush != NULL)
         {
             VbglGRFree((VMMDevRequestHeader *)pExt->u.primary.pvReqFlush);
@@ -595,6 +597,7 @@ VBoxDrvResetHW(PVOID HwDeviceExtension, ULONG Columns, ULONG Rows)
         VbglTerminate();
 
         VBoxFreeDisplaysHGSMI(VBoxCommonFromDeviceExt(pExt));
+#endif
     }
     else
     {
@@ -602,7 +605,8 @@ VBoxDrvResetHW(PVOID HwDeviceExtension, ULONG Columns, ULONG Rows)
     }
 
     LOGF_LEAVE();
-    return TRUE;
+    /* Tell the system to use VGA BIOS to set the text video mode. */
+    return FALSE;
 }
 
 #ifdef VBOX_WITH_VIDEOHWACCEL
