a18ce1f3dbc36dac0bd9b93ed45565d839268a4a
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2011-09-06 15:04:56 +0000

    Wddm/3D: fix incorrect window border blurs under aero on ati cards
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@38660 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Additions/WINNT/Graphics/Video/mp/wddm/VBoxMPWddm.cpp
+++ src/VBox/Additions/WINNT/Graphics/Video/mp/wddm/VBoxMPWddm.cpp
@@ -3732,7 +3732,7 @@ DxgkDdiEscape(
                     PVBOXDISPIFESCAPE_DBGPRINT pDbgPrint = (PVBOXDISPIFESCAPE_DBGPRINT)pEscapeHdr;
                     /* ensure the last char is \0*/
                     *((uint8_t*)pDbgPrint + pEscape->PrivateDriverDataSize - 1) = '\0';
-#ifdef DEBUG_misha
+#if defined(DEBUG_misha) || defined(DEBUG_leo)
                     DbgPrint("%s", pDbgPrint->aStringBuf);
 #else
                     LOGREL(("%s", pDbgPrint->aStringBuf));
