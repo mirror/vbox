43725b98186f4bdc97738f80fc8cdb280df044dd
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2011-09-21 12:28:27 +0000

    VBoxDbgConsole: Fixed busted text handling wrt selections and cursor positioning.
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@38813 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Debugger/VBoxDbgConsole.h
+++ src/VBox/Debugger/VBoxDbgConsole.h
@@ -53,9 +53,12 @@ public:
      * This differs from QTextEdit::append() in that it won't start on a new paragraph
      * unless the previous char was a newline ('\n').
      *
-     * @param   rStr        The text string to append.
+     * @param   rStr        The text string to append. 
+     * @param   fClearSelection     Whether to clear selected text before appending. 
+     *                              If @c false the selection and window position
+     *                              are preserved.
      */
-    virtual void appendText(const QString &rStr);
+    virtual void appendText(const QString &rStr, bool fClearSelection);
 
 protected:
     /** The current line (paragraph) number. */
