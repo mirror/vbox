885f8bfc4e76402bccee10706a5bbf656fa934c4
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2011-09-29 14:29:44 +0000

    Additions/solaris/SharedFolders: readonly MMAP support.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@38901 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Additions/solaris/SharedFolders/vboxfs_vnode.h
+++ src/VBox/Additions/solaris/SharedFolders/vboxfs_vnode.h
@@ -60,9 +60,6 @@ typedef struct sfnode {
 	uint64_t	sf_stat_time;	/* last-modified time of sf_stat */
 	sffs_dirents_t	*sf_dir_list;	/* list of entries for this directory */
 	sffs_stats_t	*sf_dir_stats;	/* file attrs for the above entries */
-#if 0
-    volatile uint64_t	sf_mapcnt;  /* number of mapped pages */
-#endif
 } sfnode_t;
 
 #define VN2SFN(vp) ((sfnode_t *)(vp)->v_data)
