a1424fc13ec6eef75aa19c1f91e29ef8e41f82cc
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2011-10-05 20:45:46 +0000

    FE/Qt: incorrect comment
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@38948 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Frontends/VirtualBox/src/runtime/seamless/UIMachineViewSeamless.cpp
+++ src/VBox/Frontends/VirtualBox/src/runtime/seamless/UIMachineViewSeamless.cpp
@@ -202,8 +202,7 @@ bool UIMachineViewSeamless::eventFilter(QObject *pWatched, QEvent *pEvent)
                 if (pResizeEvent->size() != workingArea().size())
                     break;
 
-                /* Set the "guest needs to resize" hint.
-                 * This hint is acted upon when (and only when) the autoresize property is "true": */
+                /* Set the "guest needs to resize" hint. */
                 m_fShouldWeDoResize = uisession()->isGuestSupportsGraphics();
                 if (m_fShouldWeDoResize)
                     QTimer::singleShot(0, this, SLOT(sltPerformGuestResize()));
