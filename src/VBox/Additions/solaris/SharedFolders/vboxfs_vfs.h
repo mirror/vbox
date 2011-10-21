3e9cbad3ec7c3c9fea1e9eb8f7077e06803cc212
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2011-10-21 09:25:27 +0000

    Additions/solaris/SharedFolders: implemented dmode, fmode, dmask, fmask, tabs.
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@39067 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Additions/solaris/SharedFolders/vboxfs_vfs.h
+++ src/VBox/Additions/solaris/SharedFolders/vboxfs_vfs.h
@@ -39,10 +39,10 @@ typedef struct sffs_data {
 	vnode_t		*sf_rootnode;	/* of vnode of the root directory */
 	uid_t		sf_uid;		/* owner of all shared folders */
 	gid_t		sf_gid;		/* group of all shared folders */
-	mode_t      sf_dmode;   /* mode of all directories */
-	mode_t      sf_fmode;   /* mode of all files */
-	mode_t      sf_dmask;   /* mask of all directories */
-	mode_t      sf_fmask;   /* mask of all files */
+    mode_t		sf_dmode;   /* mode of all directories */
+    mode_t		sf_fmode;   /* mode of all files */
+    mode_t		sf_dmask;   /* mask of all directories */
+    mode_t		sf_fmask;   /* mask of all files */
 	int  		sf_stat_ttl;	/* ttl for stat caches (in ms) */
 	int  		sf_fsync;	/* whether to honor fsync or not */
 	char		*sf_share_name;
