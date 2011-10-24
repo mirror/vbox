4b888db4845e282a973c91c521990dfe1e19ab48
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2011-10-24 10:05:00 +0000

    alloc-ef-cpp.cpp: Always compile the stuff, so that fencing depends on linking with this library.
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@39087 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Runtime/r3/alloc-ef-cpp.cpp
+++ src/VBox/Runtime/r3/alloc-ef-cpp.cpp
@@ -29,9 +29,6 @@
 *******************************************************************************/
 #include "alloc-ef.h"
 
-#if defined(RTALLOC_EFENCE_CPP) \
- || (defined(RTMEM_WRAP_TO_EF_APIS) && !defined(RTMEM_NO_WRAP_TO_EF_APIS)) /* rest of the file */
-
 #include <iprt/asm.h>
 #include <new>
 
@@ -119,4 +116,4 @@ void RT_EF_CDECL operator delete[](void *pv, const std::nothrow_t &) throw()
     rtR3MemFree("delete[] nothrow", RTMEMTYPE_DELETE_ARRAY, pv, ASMReturnAddress(), NULL, 0, NULL);
 }
 
-#endif /* RTALLOC_EFENCE_CPP || RTMEM_WRAP_TO_EF_APIS */
+
