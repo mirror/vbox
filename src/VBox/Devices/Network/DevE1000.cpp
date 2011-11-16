d09a2425ac813255b064b8ab17ed85abacbb4bbb
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2011-11-16 13:34:38 +0000

    e1000: fix for unresolved memmove on Windows
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@39332 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Devices/Network/DevE1000.cpp
+++ src/VBox/Devices/Network/DevE1000.cpp
@@ -3207,11 +3207,8 @@ static void e1kTransmitFrame(E1KSTATE* pState, bool fOnWorkerThread)
     {
         E1kLog3(("%s Inserting VLAN tag %08x\n",
             INSTANCE(pState), RT_BE2H_U16(VET) | (RT_BE2H_U16(pState->u16VTagTCI) << 16)));
-        /** @todo: nocrt_memmove does not get resolved, fix it! */
-#ifndef RT_OS_WINDOWS
         memmove((uint8_t*)pSg->aSegs[0].pvSeg + 16, (uint8_t*)pSg->aSegs[0].pvSeg + 12, cbFrame - 12);
         *((uint32_t*)pSg->aSegs[0].pvSeg + 3) = RT_BE2H_U16(VET) | (RT_BE2H_U16(pState->u16VTagTCI) << 16);
-#endif
     }
     /* Update the stats */
     E1K_INC_CNT32(TPT);
