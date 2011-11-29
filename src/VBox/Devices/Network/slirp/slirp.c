50b4a6f1ae58592e5a9a252f2097cd1b6ef209a3
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2011-11-29 07:00:19 +0000

    NAT: bump SoMaxConnection default up to 10 (xTracker/5983).
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@39444 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Devices/Network/slirp/slirp.c
+++ src/VBox/Devices/Network/slirp/slirp.c
@@ -604,7 +604,7 @@ int slirp_init(PNATState *ppData, uint32_t u32NetAddr, uint32_t u32Netmask,
     pData->socket_snd = 64 * _1K;
     tcp_sndspace = 64 * _1K;
     tcp_rcvspace = 64 * _1K;
-    pData->soMaxConn = 1; /* historical value */
+    pData->soMaxConn = 10;
 
 #ifdef RT_OS_WINDOWS
     {
