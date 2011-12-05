ab00997bdfd2844634e6b908f19687e3c1b8975c
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2011-12-05 15:37:54 +0000

    Temporarily enabled buffering when composing the release log headering.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@39531 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Main/src-client/ConsoleImpl.cpp
+++ src/VBox/Main/src-client/ConsoleImpl.cpp
@@ -6115,6 +6115,7 @@ HRESULT Console::consoleInitReleaseLog(const ComPtr<IMachine> aMachine)
     if (RT_SUCCESS(vrc))
     {
         RTLogSetGroupLimit(pReleaseLogger, 32768);
+        bool fOldBuffered = RTLogRelSetBuffering(true /*fBuffered*/);
 
         /* some introductory information */
         RTTIMESPEC timeSpec;
@@ -6176,7 +6177,8 @@ HRESULT Console::consoleInitReleaseLog(const ComPtr<IMachine> aMachine)
         RTLogRelSetDefaultInstance(pReleaseLogger);
         hrc = S_OK;
 
-        /* Explicitly flush the log in case of VBOX_RELEASE_LOG=buffered. */
+        /* Restore the buffering setting and xplicitly flush the log. */
+        RTLogRelSetBuffering(fOldBuffered);
         RTLogFlush(pReleaseLogger);
     }
     else
