d412aa0fadcca571fd4075179007717277c9d1ba
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2011-12-05 12:59:05 +0000

    wddm: vboxWddmSwapchainCtxEscape fix
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@39528 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Additions/WINNT/Graphics/Video/mp/wddm/VBoxMPMisc.cpp
+++ src/VBox/Additions/WINNT/Graphics/Video/mp/wddm/VBoxMPMisc.cpp
@@ -413,6 +413,14 @@ NTSTATUS vboxWddmSwapchainCtxEscape(PVBOXMP_DEVEXT pDevExt, PVBOXWDDM_CONTEXT pC
     do {
         if (pSwapchainInfo->SwapchainInfo.cAllocs)
         {
+            /* ensure we do not overflow the 32bit buffer size value */
+            if (VBOXWDDM_ARRAY_MAXELEMENTSU32(PVBOXWDDM_ALLOCATION) < pSwapchainInfo->SwapchainInfo.cAllocs)
+            {
+                WARN(("number of allocations passed in too big (%d), max is (%d)", pSwapchainInfo->SwapchainInfo.cAllocs, VBOXWDDM_ARRAY_MAXELEMENTSU32(PVBOXWDDM_ALLOCATION)));
+                Status = STATUS_INVALID_PARAMETER;
+                break;
+            }
+
             apAlloc = (PVBOXWDDM_ALLOCATION *)vboxWddmMemAlloc(sizeof (PVBOXWDDM_ALLOCATION) * pSwapchainInfo->SwapchainInfo.cAllocs);
             Assert(apAlloc);
             if (!apAlloc)
