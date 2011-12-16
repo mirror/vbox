b461660e2df6caf9c01e11ddc2498ee026364b7a
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2011-12-16 00:19:35 +0000

    reqpool: fixed RTREQPOOLSTAT_REQUESTS_PROCESSED.
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@39636 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Runtime/testcase/tstRTReqPool.cpp
+++ src/VBox/Runtime/testcase/tstRTReqPool.cpp
@@ -78,8 +78,11 @@ static void test1(void)
     RTTESTI_CHECK(RTReqRelease(hReq) == 0);
     RTTESTI_CHECK(RTReqPoolGetStat(hPool, RTREQPOOLSTAT_REQUESTS_FREE) == 1);
 
+    RTTESTI_CHECK(RTReqPoolGetStat(hPool, RTREQPOOLSTAT_REQUESTS_PROCESSED) == 0);
     RTTESTI_CHECK_RC(RTReqPoolCallWait(hPool, (PFNRT)RTThreadSleep, 1, (RTMSINTERVAL)0), VINF_SUCCESS);
     RTTESTI_CHECK_RC(RTReqPoolCallWait(hPool, (PFNRT)RTThreadSleep, 1, (RTMSINTERVAL)2), VINF_SUCCESS);
+    RTTESTI_CHECK_RC(RTReqPoolCallWait(hPool, (PFNRT)RTThreadSleep, 1, (RTMSINTERVAL)3), VINF_SUCCESS);
+    RTTESTI_CHECK(RTReqPoolGetStat(hPool, RTREQPOOLSTAT_REQUESTS_PROCESSED) > 1);
     RTTESTI_CHECK(RTReqPoolGetStat(hPool, RTREQPOOLSTAT_THREADS) == 1);
 
     /* Use no wait requests to maximize the number of worker threads. */
