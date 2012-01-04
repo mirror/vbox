5fcb4851ef21671fb1628ed51d2491ce1ba614d2
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2012-01-04 22:43:51 +0000

    Runtime/r0drv: get the physical address of Linux kernel kmap mappings too.  Make it work on RHEL3.
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@39703 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Runtime/r0drv/linux/memobj-r0drv-linux.c
+++ src/VBox/Runtime/r0drv/linux/memobj-r0drv-linux.c
@@ -817,7 +817,8 @@ static struct page *rtR0MemObjLinuxVirtToPage(void *pv)
     }
 #endif
 
-#if LINUX_VERSION_CODE >= KERNEL_VERSION(2, 5, 5)
+/* As usual, RHEL 3 had pte_offset_map earlier. */
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(2, 5, 5) || defined(pte_offset_map)
     pEntry = pte_offset_map(&u.Middle, ulAddr);
 #else
     pEntry = pte_offset(&u.Middle, ulAddr);
