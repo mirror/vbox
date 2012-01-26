4a472900167af92440ef4656ef917fc3bb4b861c
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2012-01-26 18:05:46 +0000

    typo.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@39888 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Frontends/VBoxManage/VBoxManageMisc.cpp
+++ src/VBox/Frontends/VBoxManage/VBoxManageMisc.cpp
@@ -1010,7 +1010,7 @@ int handleSharedFolder(HandlerArg *a)
         }
     }
     else
-        return errorSyntax(USAGE_SETPROPERTY, "Invalid parameter '%s'", Utf8Str(a->argv[0]).c_str());
+        return errorSyntax(USAGE_SHAREDFOLDER, "Invalid parameter '%s'", Utf8Str(a->argv[0]).c_str());
 
     return 0;
 }
