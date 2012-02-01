cd1e95b8021544f4ed3f7df8507b51204ffc7f14
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2012-02-01 14:36:44 +0000

    tstTimerLR: typo
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@39935 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Runtime/testcase/tstTimerLR.cpp
+++ src/VBox/Runtime/testcase/tstTimerLR.cpp
@@ -215,7 +215,7 @@ int main()
         rc = RTTimerLRCreateEx(&hTimerLR, aTests[0].uMilliesInterval * (uint64_t)1000000, 0, TimerLRCallback, NULL);
         if (RT_FAILURE(rc))
         {
-            RTPrintf("RTTimerLRCreateEX(,%u*1M,,,) -> %d\n", aTests[i].uMilliesInterval, rc);
+            RTPrintf("RTTimerLRCreateEX(,%u*1M,,,) -> %d\n", aTests[0].uMilliesInterval, rc);
             cErrors++;
             continue;
         }
