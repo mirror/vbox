a2978b409873d26463458e7c3d7fe9cb02a7012e
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2012-02-06 22:30:18 +0000

    Build fix.
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@40012 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Frontends/VBoxBalloonCtrl/VBoxWatchdog.cpp
+++ src/VBox/Frontends/VBoxBalloonCtrl/VBoxWatchdog.cpp
@@ -452,7 +452,7 @@ static int machineDestroy(const Bstr &strUuid)
         vecMembers.erase(itMember);
 
         serviceLogVerbose(("Group \"%ls\" has %ld machines left\n",
-                           itGroup->first, vecMembers.size()));
+                           itGroup->first.raw(), vecMembers.size()));
         if (!vecMembers.size())
             g_mapGroup.erase(itGroup);
     }
@@ -713,7 +713,7 @@ static RTEXITCODE watchdogMain(HandlerArg *a)
              * Do the actual work.
              */
 
-            int rc = RTCritSectEnter(&g_csMachines);
+            rc = RTCritSectEnter(&g_csMachines);
             if (RT_SUCCESS(rc))
             {
                 for (unsigned j = 0; j < RT_ELEMENTS(g_aModules); j++)
