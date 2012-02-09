4bb83f25d0f47cb49c86eba6dba27da46058da7b
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2012-02-09 09:41:51 +0000

    VBoxClient/hostversion: uninitialized variable
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@40047 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Additions/x11/VBoxClient/hostversion.cpp
+++ src/VBox/Additions/x11/VBoxClient/hostversion.cpp
@@ -46,7 +46,7 @@ public:
         int rc;
 # ifdef VBOX_WITH_DBUS
         DBusConnection *conn;
-        DBusMessage* msg;
+        DBusMessage* msg = NULL;
         conn = dbus_bus_get (DBUS_BUS_SESSON, NULL);
         if (conn == NULL)
         {
@@ -64,8 +64,8 @@ public:
                 LogRel(("Could not create D-BUS message!\n"));
                 rc = VERR_INVALID_HANDLE;
             }
-        else
-        rc = VINF_SUCCESS;
+            else
+                rc = VINF_SUCCESS;
         }
         if (RT_SUCCESS(rc))
         {
@@ -115,7 +115,7 @@ public:
                 dbus_message_unref(reply);
             }
             if (dbus_error_is_set(&err))
-            dbus_error_free(&err);
+                dbus_error_free(&err);
         }
         if (msg != NULL)
             dbus_message_unref(msg);
