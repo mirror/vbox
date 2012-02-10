1f4695ecd78fb637c6a4f8f63b0c2634aed1fb94
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2012-02-10 14:04:56 +0000

    samples/clienttest.php: Fixed return value.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@40062 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Main/webservice/samples/php/clienttest.php
+++ src/VBox/Main/webservice/samples/php/clienttest.php
@@ -59,7 +59,7 @@ foreach ($machines as $machine)
             $display = $console->display;
             list($screenWidth, $screenHeight, $screenBpp) = $display->getScreenResolution(0 /* First screen */);             
 
-            array($imageraw) = $display->takeScreenShotToArray(0 /* First screen */, $screenWidth, $screenHeight);
+            $imageraw = $display->takeScreenShotToArray(0 /* First screen */, $screenWidth, $screenHeight);
             echo "Screenshot size: " . sizeof($imageraw) . "\n";
             
             $filename = 'screenshot.png';
