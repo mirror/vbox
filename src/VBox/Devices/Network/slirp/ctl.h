a4c3e848d2f4f54afe712036e8c60cc1f4dbaf2d
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2012-02-28 19:07:42 +0000

    NAT: CTL_CHECK(x) verify whether address belong to NAT network either.
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@40279 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Devices/Network/slirp/ctl.h
+++ src/VBox/Devices/Network/slirp/ctl.h
@@ -24,4 +24,5 @@
 #define CTL_GUEST       15
 #define CTL_BROADCAST   255
 
-#define CTL_CHECK(x, ctl) (((x) & ~pData->netmask) == (ctl))
+#define CTL_CHECK(x, ctl) (((x) & ~pData->netmask) == (ctl) \
+                           && (((x) & ~pData->netmask) == (pData->special_addr.s_addr & ~pData->netmask)))
