82fa05ffdc5378f63567a7252bc19e470e81f332
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2012-02-29 05:00:37 +0000

    NAT: special_addr is already network address.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@40285 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Devices/Network/slirp/ctl.h
+++ src/VBox/Devices/Network/slirp/ctl.h
@@ -25,4 +25,4 @@
 #define CTL_BROADCAST   255
 
 #define CTL_CHECK(x, ctl) ((RT_N2H_U32((x)) & ~pData->netmask) == (ctl) \
-                           && (((x) & ~pData->netmask) == (pData->special_addr.s_addr & ~pData->netmask)))
+                           && (((x) & ~pData->netmask) == pData->special_addr.s_addr))
