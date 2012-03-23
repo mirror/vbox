560854ac79353333c1f121d1a1c16e9bd7a07c6b
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2012-03-23 22:09:54 +0000

    coding in progress... -> laptop.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@40600 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/HostDrivers/Support/SUPDrv-dtrace.cpp
+++ src/VBox/HostDrivers/Support/SUPDrv-dtrace.cpp
@@ -32,10 +32,39 @@
 #include "SUPDrvInternal.h"
 
 #include <VBox/err.h>
+#include <VBox/VBoxTpG.h>
 #include <iprt/assert.h>
 
 #include <sys/dtrace.h>
 
+
+/*******************************************************************************
+*   Structures and Typedefs                                                    *
+*******************************************************************************/
+/**
+ * Data for a provider.
+ */
+typedef struct SUPDRVDTPROVIDER
+{
+    /** The entry in the provider list for this image. */
+    RTLISTNODE          ListEntry;
+
+    /** The provider descriptor. */
+    PVTGDESCPROVIDER    pDesc;
+    /** The VTG header. */
+    PVTGOBJHDR          pHdr;
+
+    /** Pointer to the image this provider resides in.  NULL if it's a
+     * driver. */
+    PSUPDRVLDRIMAGE     pImage;
+    /** The session this provider is associated with if registered via
+     * SUPR0VtgRegisterDrv.  NULL if pImage is set. */ 
+    PSUPDRVSESSION      pSession;
+    /** The module name. */
+    const char         *pszModName;
+} SUPDRVDTPROVIDER;
+/** Pointer to the data for a provider. */
+typedef SUPDRVDTPROVIDER *PSUPDRVDTPROVIDER;
 
 
 /*******************************************************************************
@@ -44,7 +73,7 @@
 static void     supdrvDTracePOps_ProvideModule(void *pvProv, struct modctl *pMod);
 static int      supdrvDTracePOps_Enable(void *pvProv, dtrace_id_t idProbe, void *pvProbe);
 static void     supdrvDTracePOps_Disable(void *pvProv, dtrace_id_t idProbe, void *pvProbe);
-static void     supdrvDTracePOps_GetArgDesc(void *pvProv, dtrace_id_t idProbe, void *pvProbe, 
+static void     supdrvDTracePOps_GetArgDesc(void *pvProv, dtrace_id_t idProbe, void *pvProbe,
                                             dtrace_argdesc_t *pArgDesc);
 /*static uint64_t supdrvDTracePOps_GetArgVal(void *pvProv, dtrace_id_t idProbe, void *pvProbe,
                                            int iArg, int cFrames);*/
@@ -56,7 +85,7 @@ static void     supdrvDTracePOps_Destroy(void *pvProv, dtrace_id_t idProbe, void
 *   Global Variables                                                           *
 *******************************************************************************/
 /** The default provider attributes. */
-static dtrace_pattr_t g_DefProvAttrs = 
+static dtrace_pattr_t g_DefProvAttrs =
 {   /*                       .dtat_name,                .dtat_data,                .dtat_class */
     /* .dtpa_provider = */ { DTRACE_STABILITY_UNSTABLE, DTRACE_STABILITY_UNSTABLE, DTRACE_CLASS_ISA },
     /* .dtpa_mod      = */ { DTRACE_STABILITY_UNSTABLE, DTRACE_STABILITY_UNSTABLE, DTRACE_CLASS_ISA },
@@ -65,10 +94,10 @@ static dtrace_pattr_t g_DefProvAttrs =
     /* .dtpa_args     = */ { DTRACE_STABILITY_UNSTABLE, DTRACE_STABILITY_UNSTABLE, DTRACE_CLASS_ISA },
 };
 
-/** 
- * DTrace provider method table. 
- */ 
-static const dtrace_pops_t g_SupDrvDTraceProvOps = 
+/**
+ * DTrace provider method table.
+ */
+static const dtrace_pops_t g_SupDrvDTraceProvOps =
 {
     /* .dtps_provide         = */ NULL,
     /* .dtps_provide_module  = */ supdrvDTracePOps_ProvideModule,
@@ -83,10 +112,153 @@ static const dtrace_pops_t g_SupDrvDTraceProvOps =
 };
 
 
+#define VERR_SUPDRV_VTG_MAGIC           (-3704)
+#define VERR_SUPDRV_VTG_BITS            (-3705)
+#define VERR_SUPDRV_VTG_RESERVED        (-3705)
+#define VERR_SUPDRV_VTG_BAD_PTR         (-3706)
+#define VERR_SUPDRV_VTG_TOO_FEW         (-3707)
+#define VERR_SUPDRV_VTG_TOO_MUCH        (-3708)
+#define VERR_SUPDRV_VTG_NOT_MULTIPLE    (-3709)
+
+
+/** 
+ * Validates the VTG data.
+ *  
+ * @returns VBox status code.
+ * @param   pVtgHdr             The VTG object header of the data to validate.
+ * @param   cbVtgObj            The size of the VTG object.
+ * @param   pbImage             The image base. For validating the probe 
+ *                              locations.
+ * @param   cbImage             The image size to go with @a pbImage.
+ */
+static int supdrvVtgValidate(PVTGOBJHDR pVtgHdr, size_t cbVtgObj, uint8_t *pbImage, size_t cbImage)
+{
+    /*
+     * The header.
+     */
+    if (!memcmp(pVtgHdr->szMagic, VTGOBJHDR_MAGIC, sizeof(pVtgHdr->szMagic)))
+        return VERR_SUPDRV_VTG_MAGIC;
+    if (pVtgHdr->cBits != ARCH_BITS)
+        return VERR_SUPDRV_VTG_BITS;
+    if (pVtgHdr->u32Reserved0)
+        return VERR_SUPDRV_VTG_RESERVED;
+
+#define MY_VALIDATE_PTR(p, cb, cMin, cMax, cbUnit) \
+    do { \
+        if (   (cb) >= cbVtgObj \
+            || (uintptr_t)(p) - (uintptr_t)pVtgHdr < cbVtgObj - (cb) ) \
+            return VERR_SUPDRV_VTG_BAD_PTR; \
+        if ((cb) <  (cMin) * (cbUnit)) \
+            return VERR_SUPDRV_VTG_TOO_FEW; \
+        if ((cb) >= (cMax) * (cbUnit)) \
+            return VERR_SUPDRV_VTG_TOO_MUCH; \
+        if ((cb) / (cbUnit) * (cbUnit) != (cb)) \
+            return VERR_SUPDRV_VTG_NOT_MULTIPLE; \
+    } while (0)
+
+    MY_VALIDATE_PTR(pVtgHdr->paProviders,       pVtgHdr->cbProviders,    1,   16, sizeof(VTGDESCPROVIDER));
+    MY_VALIDATE_PTR(pVtgHdr->paProbes,          pVtgHdr->cbProbes,       1, _32K, sizeof(VTGDESCPROBE));
+    MY_VALIDATE_PTR(pVtgHdr->pafProbeEnabled,   pVtgHdr->cbProbeEnabled, 1, _32K, sizeof(bool));
+    MY_VALIDATE_PTR(pVtgHdr->pachStrTab,        pVtgHdr->cbStrTab,       4,  _1M,  sizeof(char));
+    MY_VALIDATE_PTR(pVtgHdr->paArgLists,        pVtgHdr->cbArgLists,     0, _32K, sizeof(uint32_t));
+#undef MY_VALIDATE_PTR
+
+    if (!RT_VALID_PTR(pVtgHdr->paProbLocs))
+        return VERR_SUPDRV_VTG_BAD_PTR;
+    if (!RT_VALID_PTR(pVtgHdr->paProbLocsEnd))
+        return VERR_SUPDRV_VTG_BAD_PTR;
+    if ((uintptr_t)pVtgHdr->paProbLocsEnd - (uintptr_t)pVtgHdr->paProbLocs < sizeof(VTGPROBELOC))
+        return VERR_SUPDRV_VTG_TOO_FEW;
+    if ((uintptr_t)pVtgHdr->paProbLocsEnd - (uintptr_t)pVtgHdr->paProbLocs > sizeof(VTGPROBELOC) * _128K)
+        return VERR_SUPDRV_VTG_TOO_MUCH;
+    if (   ((uintptr_t)pVtgHdr->paProbLocsEnd - (uintptr_t)pVtgHdr->paProbLocs) 
+           / sizeof(VTGPROBELOC) * sizeof(VTGPROBELOC)
+        != (uintptr_t)pVtgHdr->paProbLocsEnd - (uintptr_t)pVtgHdr->paProbLocs)
+        return VERR_SUPDRV_VTG_NOT_MULTIPLE;
+    if (pbImage && cbImage)
+    {
+        if ((uintptr_t)pVtgHdr->paProbLocs - (uintptr_t)pbImage >= cbImage)
+            return VERR_SUPDRV_VTG_BAD_PTR;
+        if ((uintptr_t)pVtgHdr->paProbLocsEnd - (uintptr_t)pbImage > cbImage)
+            return VERR_SUPDRV_VTG_BAD_PTR;
+    }
+
+    /*
+     * Validate the providers.
+     */
+
+
+    return VINF_SUCCESS;
+}
+
+
+/**
+ * Registers the VTG tracepoint providers of a driver.
+ *  
+ * @returns VBox status code. 
+ * @param   pszName             The driver name.
+ * @param   pVtgHdr             The VTG header. 
+ * @param   pImage              The image if applicable. 
+ * @param   pSession            The session if applicable. 
+ * @param   pszModName          The module name. 
+ */
+static int supdrvVtgRegister(PSUPDRVDEVEXT pDevExt, PVTGOBJHDR pVtgHdr, PSUPDRVLDRIMAGE pImage, PSUPDRVSESSION pSession,
+                             const char *pszModName)
+{
+    /*
+     * Validate input.
+     */
+    AssertPtrReturn(pDevExt, VERR_INVALID_POINTER);
+    AssertPtrReturn(pVtgHdr, VERR_INVALID_POINTER);
+    AssertPtrNullReturn(pImage, VERR_INVALID_POINTER);
+    AssertPtrNullReturn(pSession, VERR_INVALID_POINTER);
+    AssertPtrReturn(pszModName, VERR_INVALID_POINTER);
+    AssertReturn((pImage == NULL) != (pSession != NULL), VERR_INVALID_PARAMETER);
+
+
+
+    /*
+     *
+     */
+
+}
+
+
+
+/**
+ * Registers the VTG tracepoint providers of a driver.
+ *  
+ * @returns VBox status code. 
+ * @param   pSession            The support driver session handle. 
+ * @param   pVtgHdr             The VTG header.
+ * @param   pszName             The driver name.
+ */
+SUPR0DECL(int) SUPR0VtgRegisterDrv(PSUPDRVSESSION pSession, PVTGOBJHDR pVtgHdr, const char *pszName)
+{
+    AssertReturn(SUP_IS_SESSION_VALID(pSession), NULL);
+    AssertPtrReturn(pszName, VERR_INVALID_POINTER);
+    AssertPtrReturn(pVtgHdr, VERR_INVALID_POINTER);
+
+    return supdrvVtgRegister(pSession->pDevExt, pVtgHdr, NULL, pSession, pszName);
+}
+
+
+
+/**
+ * Deregister the VTG tracepoint providers of a driver.
+ *  
+ * @param   pSession            The support driver session handle. 
+ * @param   pVtgHdr             The VTG header.
+ */
+SUPR0DECL(void) SUPR0VtgDeregisterDrv(PSUPDRVSESSION pSession, PVTGOBJHDR pVtgHdr)
+{
+}
+
+
 
 /**
  * Early module initialization hook.
- *  
+ *
  * @returns VBox status code.
  * @param   pDevExt             The device extension structure.
  */
@@ -102,7 +274,7 @@ int supdrvDTraceInit(PSUPDRVDEVEXT pDevExt)
 
 /**
  * Late module termination hook.
- *  
+ *
  * @returns VBox status code.
  * @param   pDevExt             The device extension structure.
  */
@@ -118,14 +290,14 @@ int supdrvDTraceTerm(PSUPDRVDEVEXT pDevExt)
 
 /**
  * Module loading hook, called before calling into the module.
- *  
+ *
  * @returns VBox status code.
  * @param   pDevExt             The device extension structure.
  */
 int supdrvDTraceModuleLoading(PSUPDRVDEVEXT pDevExt, PSUPDRVLDRIMAGE pImage)
 {
     /*
-     * Check for DTrace probes in the module, register a new provider for them 
+     * Check for DTrace probes in the module, register a new provider for them
      * if found.
      */
 
@@ -134,15 +306,15 @@ int supdrvDTraceModuleLoading(PSUPDRVDEVEXT pDevExt, PSUPDRVLDRIMAGE pImage)
 
 
 /**
- * Module unloading hook, called after execution in the module 
+ * Module unloading hook, called after execution in the module
  * have ceased.
- *  
+ *
  * @returns VBox status code.
  * @param   pDevExt             The device extension structure.
  */
 int supdrvDTraceModuleUnloading(PSUPDRVDEVEXT pDevExt, PSUPDRVLDRIMAGE pImage)
 {
-    /* 
+    /*
      * Undo what we did in supdrvDTraceModuleLoading.
      */
     return VINF_SUCCESS;
@@ -181,7 +353,7 @@ static void     supdrvDTracePOps_Disable(void *pvProv, dtrace_id_t idProbe, void
 /**
  * @callback_method_impl{dtrace_pops_t,dtps_getargdesc}
  */
-static void     supdrvDTracePOps_GetArgDesc(void *pvProv, dtrace_id_t idProbe, void *pvProbe, 
+static void     supdrvDTracePOps_GetArgDesc(void *pvProv, dtrace_id_t idProbe, void *pvProbe,
                                             dtrace_argdesc_t *pArgDesc)
 {
     pArgDesc->dtargd_ndx = DTRACE_ARGNONE;
