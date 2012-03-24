29c1dc3be5d672d7f66129c67b7c60428be64552
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2012-03-24 22:07:33 +0000

    Init the dtrace bits...
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@40611 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/HostDrivers/Support/SUPDrv.c
+++ src/VBox/HostDrivers/Support/SUPDrv.c
@@ -463,62 +463,73 @@ int VBOXCALL supdrvInitDevExt(PSUPDRVDEVEXT pDevExt, size_t cbSession)
                     rc = supdrvGipCreate(pDevExt);
                     if (RT_SUCCESS(rc))
                     {
-                        pDevExt->u32Cookie = BIRD;  /** @todo make this random? */
-                        pDevExt->cbSession = (uint32_t)cbSession;
+#ifdef VBOX_WITH_DTRACE_R0DRV
+                        rc = supdrvDTraceInit(pDevExt);
+                        if (RT_SUCCESS(rc))
+#endif
+                        {
 
-                        /*
-                         * Fixup the absolute symbols.
-                         *
-                         * Because of the table indexing assumptions we'll have a little #ifdef orgy
-                         * here rather than distributing this to OS specific files. At least for now.
-                         */
+                            pDevExt->u32Cookie = BIRD;  /** @todo make this random? */
+                            pDevExt->cbSession = (uint32_t)cbSession;
+
+                            /*
+                             * Fixup the absolute symbols.
+                             *
+                             * Because of the table indexing assumptions we'll have a little #ifdef orgy
+                             * here rather than distributing this to OS specific files. At least for now.
+                             */
 #ifdef RT_OS_DARWIN
 # if ARCH_BITS == 32
-                        if (SUPR0GetPagingMode() >= SUPPAGINGMODE_AMD64)
-                        {
-                            g_aFunctions[0].pfn = (void *)1;                    /* SUPR0AbsIs64bit */
-                            g_aFunctions[1].pfn = (void *)0x80;                 /* SUPR0Abs64bitKernelCS - KERNEL64_CS, seg.h */
-                            g_aFunctions[2].pfn = (void *)0x88;                 /* SUPR0Abs64bitKernelSS - KERNEL64_SS, seg.h */
-                            g_aFunctions[3].pfn = (void *)0x88;                 /* SUPR0Abs64bitKernelDS - KERNEL64_SS, seg.h */
-                        }
-                        else
-                            g_aFunctions[0].pfn = g_aFunctions[1].pfn = g_aFunctions[2].pfn = g_aFunctions[4].pfn = (void *)0;
-                        g_aFunctions[4].pfn = (void *)0x08;                     /* SUPR0AbsKernelCS - KERNEL_CS, seg.h */
-                        g_aFunctions[5].pfn = (void *)0x10;                     /* SUPR0AbsKernelSS - KERNEL_DS, seg.h */
-                        g_aFunctions[6].pfn = (void *)0x10;                     /* SUPR0AbsKernelDS - KERNEL_DS, seg.h */
-                        g_aFunctions[7].pfn = (void *)0x10;                     /* SUPR0AbsKernelES - KERNEL_DS, seg.h */
-                        g_aFunctions[8].pfn = (void *)0x10;                     /* SUPR0AbsKernelFS - KERNEL_DS, seg.h */
-                        g_aFunctions[9].pfn = (void *)0x48;                     /* SUPR0AbsKernelGS - CPU_DATA_GS, seg.h */
+                            if (SUPR0GetPagingMode() >= SUPPAGINGMODE_AMD64)
+                            {
+                                g_aFunctions[0].pfn = (void *)1;                    /* SUPR0AbsIs64bit */
+                                g_aFunctions[1].pfn = (void *)0x80;                 /* SUPR0Abs64bitKernelCS - KERNEL64_CS, seg.h */
+                                g_aFunctions[2].pfn = (void *)0x88;                 /* SUPR0Abs64bitKernelSS - KERNEL64_SS, seg.h */
+                                g_aFunctions[3].pfn = (void *)0x88;                 /* SUPR0Abs64bitKernelDS - KERNEL64_SS, seg.h */
+                            }
+                            else
+                                g_aFunctions[0].pfn = g_aFunctions[1].pfn = g_aFunctions[2].pfn = g_aFunctions[4].pfn = (void *)0;
+                            g_aFunctions[4].pfn = (void *)0x08;                     /* SUPR0AbsKernelCS - KERNEL_CS, seg.h */
+                            g_aFunctions[5].pfn = (void *)0x10;                     /* SUPR0AbsKernelSS - KERNEL_DS, seg.h */
+                            g_aFunctions[6].pfn = (void *)0x10;                     /* SUPR0AbsKernelDS - KERNEL_DS, seg.h */
+                            g_aFunctions[7].pfn = (void *)0x10;                     /* SUPR0AbsKernelES - KERNEL_DS, seg.h */
+                            g_aFunctions[8].pfn = (void *)0x10;                     /* SUPR0AbsKernelFS - KERNEL_DS, seg.h */
+                            g_aFunctions[9].pfn = (void *)0x48;                     /* SUPR0AbsKernelGS - CPU_DATA_GS, seg.h */
 # else /* 64-bit darwin: */
-                        g_aFunctions[0].pfn = (void *)1;                        /* SUPR0AbsIs64bit */
-                        g_aFunctions[1].pfn = (void *)(uintptr_t)ASMGetCS();    /* SUPR0Abs64bitKernelCS */
-                        g_aFunctions[2].pfn = (void *)(uintptr_t)ASMGetSS();    /* SUPR0Abs64bitKernelSS */
-                        g_aFunctions[3].pfn = (void *)0;                        /* SUPR0Abs64bitKernelDS */
-                        g_aFunctions[4].pfn = (void *)(uintptr_t)ASMGetCS();    /* SUPR0AbsKernelCS */
-                        g_aFunctions[5].pfn = (void *)(uintptr_t)ASMGetSS();    /* SUPR0AbsKernelSS */
-                        g_aFunctions[6].pfn = (void *)0;                        /* SUPR0AbsKernelDS */
-                        g_aFunctions[7].pfn = (void *)0;                        /* SUPR0AbsKernelES */
-                        g_aFunctions[8].pfn = (void *)0;                        /* SUPR0AbsKernelFS */
-                        g_aFunctions[9].pfn = (void *)0;                        /* SUPR0AbsKernelGS */
+                            g_aFunctions[0].pfn = (void *)1;                        /* SUPR0AbsIs64bit */
+                            g_aFunctions[1].pfn = (void *)(uintptr_t)ASMGetCS();    /* SUPR0Abs64bitKernelCS */
+                            g_aFunctions[2].pfn = (void *)(uintptr_t)ASMGetSS();    /* SUPR0Abs64bitKernelSS */
+                            g_aFunctions[3].pfn = (void *)0;                        /* SUPR0Abs64bitKernelDS */
+                            g_aFunctions[4].pfn = (void *)(uintptr_t)ASMGetCS();    /* SUPR0AbsKernelCS */
+                            g_aFunctions[5].pfn = (void *)(uintptr_t)ASMGetSS();    /* SUPR0AbsKernelSS */
+                            g_aFunctions[6].pfn = (void *)0;                        /* SUPR0AbsKernelDS */
+                            g_aFunctions[7].pfn = (void *)0;                        /* SUPR0AbsKernelES */
+                            g_aFunctions[8].pfn = (void *)0;                        /* SUPR0AbsKernelFS */
+                            g_aFunctions[9].pfn = (void *)0;                        /* SUPR0AbsKernelGS */
 
 # endif
 #else  /* !RT_OS_DARWIN */
 # if ARCH_BITS == 64
-                        g_aFunctions[0].pfn = (void *)1;                        /* SUPR0AbsIs64bit */
-                        g_aFunctions[1].pfn = (void *)(uintptr_t)ASMGetCS();    /* SUPR0Abs64bitKernelCS */
-                        g_aFunctions[2].pfn = (void *)(uintptr_t)ASMGetSS();    /* SUPR0Abs64bitKernelSS */
-                        g_aFunctions[3].pfn = (void *)(uintptr_t)ASMGetDS();    /* SUPR0Abs64bitKernelDS */
+                            g_aFunctions[0].pfn = (void *)1;                        /* SUPR0AbsIs64bit */
+                            g_aFunctions[1].pfn = (void *)(uintptr_t)ASMGetCS();    /* SUPR0Abs64bitKernelCS */
+                            g_aFunctions[2].pfn = (void *)(uintptr_t)ASMGetSS();    /* SUPR0Abs64bitKernelSS */
+                            g_aFunctions[3].pfn = (void *)(uintptr_t)ASMGetDS();    /* SUPR0Abs64bitKernelDS */
 # else
-                        g_aFunctions[0].pfn = g_aFunctions[1].pfn = g_aFunctions[2].pfn = g_aFunctions[4].pfn = (void *)0;
+                            g_aFunctions[0].pfn = g_aFunctions[1].pfn = g_aFunctions[2].pfn = g_aFunctions[4].pfn = (void *)0;
 # endif
-                        g_aFunctions[4].pfn = (void *)(uintptr_t)ASMGetCS();    /* SUPR0AbsKernelCS */
-                        g_aFunctions[5].pfn = (void *)(uintptr_t)ASMGetSS();    /* SUPR0AbsKernelSS */
-                        g_aFunctions[6].pfn = (void *)(uintptr_t)ASMGetDS();    /* SUPR0AbsKernelDS */
-                        g_aFunctions[7].pfn = (void *)(uintptr_t)ASMGetES();    /* SUPR0AbsKernelES */
-                        g_aFunctions[8].pfn = (void *)(uintptr_t)ASMGetFS();    /* SUPR0AbsKernelFS */
-                        g_aFunctions[9].pfn = (void *)(uintptr_t)ASMGetGS();    /* SUPR0AbsKernelGS */
+                            g_aFunctions[4].pfn = (void *)(uintptr_t)ASMGetCS();    /* SUPR0AbsKernelCS */
+                            g_aFunctions[5].pfn = (void *)(uintptr_t)ASMGetSS();    /* SUPR0AbsKernelSS */
+                            g_aFunctions[6].pfn = (void *)(uintptr_t)ASMGetDS();    /* SUPR0AbsKernelDS */
+                            g_aFunctions[7].pfn = (void *)(uintptr_t)ASMGetES();    /* SUPR0AbsKernelES */
+                            g_aFunctions[8].pfn = (void *)(uintptr_t)ASMGetFS();    /* SUPR0AbsKernelFS */
+                            g_aFunctions[9].pfn = (void *)(uintptr_t)ASMGetGS();    /* SUPR0AbsKernelGS */
 #endif /* !RT_OS_DARWIN */
-                        return VINF_SUCCESS;
+                            return VINF_SUCCESS;
+                        }
+
+#ifdef VBOX_WITH_DTRACE_R0DRV
+                        supdrvGipDestroy(pDevExt);
+#endif
                     }
 
 #ifdef SUPDRV_USE_MUTEX_FOR_GIP
@@ -610,6 +621,10 @@ void VBOXCALL supdrvDeleteDevExt(PSUPDRVDEVEXT pDevExt)
 
     /* kill the GIP. */
     supdrvGipDestroy(pDevExt);
+
+#ifdef VBOX_WITH_DTRACE_R0DRV
+    supdrvDTraceTerm(pDevExt);
+#endif
 
 #ifdef SUPDRV_WITH_RELEASE_LOGGER
     /* destroy the loggers. */
