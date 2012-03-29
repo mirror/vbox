c08993598fc3932cc3b3d181e9f5f6774618b078
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2012-03-29 14:12:11 +0000

    crOpenGL: proper handling disabled 3D case for HGSMI
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@40708 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/GuestHost/OpenGL/util/vboxhgcm.c
+++ src/VBox/GuestHost/OpenGL/util/vboxhgcm.c
@@ -2332,12 +2332,10 @@ static int crVBoxHGSMIDoConnect( CRConnection *conn )
     VBOXCRHGSMIPROFILE_FUNC_PROLOGUE();
 
     pClient = _crVBoxHGSMIClientGet(conn);
-    CRASSERT(pClient);
     if (pClient)
-    {
         rc = VBoxCrHgsmiCtlConGetClientID(pClient->pHgsmi, &conn->u32ClientID);
-        CRASSERT(RT_SUCCESS(rc));
-    }
+    else
+        rc = VERR_GENERAL_FAILURE;
 
     VBOXCRHGSMIPROFILE_FUNC_EPILOGUE();
 
