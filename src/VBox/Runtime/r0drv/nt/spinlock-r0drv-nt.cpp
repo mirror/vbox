68f93498f6bbaf231b0c20a5e6234f252673db5e
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2012-04-07 19:32:51 +0000

    Seems to work.
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@40817 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Runtime/r0drv/nt/spinlock-r0drv-nt.cpp
+++ src/VBox/Runtime/r0drv/nt/spinlock-r0drv-nt.cpp
@@ -4,7 +4,7 @@
  */
 
 /*
- * Copyright (C) 2006-2007 Oracle Corporation
+ * Copyright (C) 2006-2012 Oracle Corporation
  *
  * This file is part of VirtualBox Open Source Edition (OSE), as
  * available from http://www.virtualbox.org. This file is free software;
@@ -80,9 +80,6 @@ typedef struct RTSPINLOCKINTERNAL
     /** The NT spinlock structure. */
     KSPIN_LOCK          Spinlock;
 } RTSPINLOCKINTERNAL, *PRTSPINLOCKINTERNAL;
-
-
-This code has changed and need testing!;
 
 
 RTDECL(int)  RTSpinlockCreate(PRTSPINLOCK pSpinlock, uint32_t fFlags, const char *pszName)
