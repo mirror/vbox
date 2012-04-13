30a6c0db91f9c234b71d2493643c7ef4a70f0760
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2012-04-13 14:05:26 +0000

    Runtime/generic/RTMpGetArraySize-generic: shadow decl warning.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@40902 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Runtime/generic/RTMpGetArraySize-generic.cpp
+++ src/VBox/Runtime/generic/RTMpGetArraySize-generic.cpp
@@ -51,7 +51,7 @@ RTDECL(uint32_t) RTMpGetArraySize(void)
         RTCPUSET    CpuSet;
         uint32_t    cCpus1 = RTCpuLastIndex(RTMpGetSet(&CpuSet)) + 1;
         uint32_t    cCpus2 = RTMpGetCount();
-        uint32_t    cCpus  = RT_MAX(cCpus1, cCpus2);
+        cCpus              = RT_MAX(cCpus1, cCpus2);
         ASMAtomicCmpXchgU32(&s_cMaxCpus, cCpus, 0);
         return cCpus;
     }
