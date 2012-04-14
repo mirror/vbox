7cc9a7e50bb7402a3e32a16d2e4cd10973c3eed5
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2012-04-14 07:01:24 +0000

    FreeBSD/NetIf: Add support for IFT_L2VLAN interfaces when enumerating interfaces (Thanks to Landon Fuller)
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@40910 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Main/src-server/freebsd/NetIf-freebsd.cpp
+++ src/VBox/Main/src-server/freebsd/NetIf-freebsd.cpp
@@ -268,7 +268,7 @@ int NetIfList(std::list <ComObjPtr<HostNetworkInterface> > &list)
             pNext += pIfAddrMsg->ifam_msglen;
         }
 
-        if (pSdl->sdl_type == IFT_ETHER)
+        if (pSdl->sdl_type == IFT_ETHER || pSdl->sdl_type == IFT_L2VLAN)
         {
             struct ifreq IfReq;
             strcpy(IfReq.ifr_name, pNew->szShortName);
@@ -373,7 +373,7 @@ int NetIfGetConfigByName(PNETIFINFO pInfo)
             pNext += pIfAddrMsg->ifam_msglen;
         }
 
-        if (!fSkip && pSdl->sdl_type == IFT_ETHER)
+        if (!fSkip && (pSdl->sdl_type == IFT_ETHER || pSdl->sdl_type == IFT_L2VLAN))
         {
             size_t cbNameLen = pSdl->sdl_nlen + 1;
             memcpy(pInfo->MACAddress.au8, LLADDR(pSdl), sizeof(pInfo->MACAddress.au8));
