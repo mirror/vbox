1b116118ac578e68eaee6467733f8080657e0b91
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2012-04-14 17:04:18 +0000

    duh + device ids
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@40925 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/VMM/dtrace/int-1.d
+++ src/VBox/VMM/dtrace/int-1.d
@@ -30,11 +30,11 @@ vboxvmm*:::pdm-irq-high,vboxvmm*:::pdm-irq-hilo
 vboxvmm*:::pdm-irq-get
 /g_aStarts[args[1]] > 0 && args[1] != 0/
 {
-    @interrupts[args[3]] = count();
+    @interrupts[args[3], args[2]] = count();
     /*printf("get:  tag=%#x src=%d %llx - %llx = %llx\n", args[1], args[2], timestamp, g_aStarts[args[1]], timestamp - g_aStarts[args[1]]);*/
-    @dispavg[args[3]]  = avg(timestamp - g_aStarts[args[1]]);
-    @dispmax[args[3]]  = max(timestamp - g_aStarts[args[1]]);
-    @dispmin[args[3]]  = min(timestamp - g_aStarts[args[1]]);
+    @dispavg[args[3], args[2]]  = avg(timestamp - g_aStarts[args[1]]);
+    @dispmax[args[3], args[2]]  = max(timestamp - g_aStarts[args[1]]);
+    @dispmin[args[3], args[2]]  = min(timestamp - g_aStarts[args[1]]);
     g_aStarts[args[1]] = 0;
     g_cHits++;
 }
@@ -53,7 +53,7 @@ END
     printf("Average dispatch latency:");
     printa(@dispavg);
     printf("Minimax dispatch latency:");
-    printa(@dispmax);
-    printf("Maximum dispatch latency:");
     printa(@dispmin);
+    printf("Maximum dispatch latency:");
+    printa(@dispmax);
 }
