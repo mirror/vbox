71cd5fc951305b20ea1cdb59695b5bfdeea613c2
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2012-04-19 07:42:43 +0000

    Frontends/VBoxManage: fix help message syntax for "createhd"
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@40984 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Frontends/VBoxManage/VBoxManageHelp.cpp
+++ src/VBox/Frontends/VBoxManage/VBoxManageHelp.cpp
@@ -524,8 +524,8 @@ void printUsage(USAGECATEGORY u64Cmd, PRTSTREAM pStrm)
     if (u64Cmd & USAGE_CREATEHD)
         RTStrmPrintf(pStrm,
                      "VBoxManage createhd         --filename <filename>\n"
-                     "                            --diffparent <uuid>|<filename>\n"
                      "                            [--size <megabytes>|--sizebyte <bytes>]\n"
+                     "                            [--diffparent <uuid>|<filename>\n"
                      "                            [--format VDI|VMDK|VHD] (default: VDI)\n"
                      "                            [--variant Standard,Fixed,Split2G,Stream,ESX]\n"
                      "\n");
