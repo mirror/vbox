25187d801490a7d359c416fbcc439cf6aee73a0e
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2012-04-20 21:13:11 +0000

    VBoxCOM: proper uninitialization even if com::Initialize failed very early
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@41015 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Main/glue/EventQueue.cpp
+++ src/VBox/Main/glue/EventQueue.cpp
@@ -237,16 +237,18 @@ int EventQueue::init()
 /* static */
 int EventQueue::uninit()
 {
-    Assert(sMainQueue);
-    /* Must process all events to make sure that no NULL event is left
-     * after this point. It would need to modify the state of sMainQueue. */
+    if (sMainQueue)
+    {
+        /* Must process all events to make sure that no NULL event is left
+         * after this point. It would need to modify the state of sMainQueue. */
 #ifdef RT_OS_DARWIN /* Do not process the native runloop, the toolkit may not be ready for it. */
-    sMainQueue->mEventQ->ProcessPendingEvents();
+        sMainQueue->mEventQ->ProcessPendingEvents();
 #else
-    sMainQueue->processEventQueue(0);
+        sMainQueue->processEventQueue(0);
 #endif
-    delete sMainQueue;
-    sMainQueue = NULL;
+        delete sMainQueue;
+        sMainQueue = NULL;
+    }
     return VINF_SUCCESS;
 }
 
