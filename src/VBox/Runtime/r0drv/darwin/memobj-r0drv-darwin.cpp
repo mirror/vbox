7b2fe10bfe7a453392c6839ff0b65214dc7fd8a8
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2012-04-25 15:10:41 +0000

    memobj-r0drv-darwin.cpp: Use rtR0MemObjNativeProtect to set RTMEM_PROT_EXEC when executable memory is requested.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@41053 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Runtime/r0drv/darwin/memobj-r0drv-darwin.cpp
+++ src/VBox/Runtime/r0drv/darwin/memobj-r0drv-darwin.cpp
@@ -502,9 +502,20 @@ static int rtR0MemObjNativeAllocWorker(PPRTR0MEMOBJINTERNAL ppMem, size_t cb,
                             AssertMsgFailed(("enmType=%d\n", enmType));
                     }
 
-                    pMemDarwin->pMemDesc = pMemDesc;
-                    *ppMem = &pMemDarwin->Core;
-                    return VINF_SUCCESS;
+#if 1 /* Experimental code. */
+                    if (fExecutable)
+                        rc = rtR0MemObjNativeProtect(&pMemDarwin->Core, 0, cb, RTMEM_PROT_READ | RTMEM_PROT_WRITE | RTMEM_PROT_EXEC);
+                    else
+#endif
+                        rc = VINF_SUCCESS;
+                    if (RT_SUCCESS(rc))
+                    {
+                        pMemDarwin->pMemDesc = pMemDesc;
+                        *ppMem = &pMemDarwin->Core;
+                        return VINF_SUCCESS;
+                    }
+
+                    rtR0MemObjDelete(&pMemDarwin->Core);
                 }
 
                 if (enmType == RTR0MEMOBJTYPE_PHYS_NC)
