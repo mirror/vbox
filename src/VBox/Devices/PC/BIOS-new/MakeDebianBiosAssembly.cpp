ee1437a2cd7e945eb980e017a8ff8c2127779d65
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2012-04-27 14:42:40 +0000

    Kick off for stupid work that just needs doing.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@41088 cfe28804-0f27-0410-a406-dd0f0b0b656f

---
+++ src/VBox/Devices/PC/BIOS-new/MakeDebianBiosAssembly.cpp
@@ -0,0 +1,46 @@
+/* $Id$ */
+/** @file
+ * MakeDebianBiosAssembly - Generate Assembly Source for Debian Minded Distros.
+ */
+
+/*
+ * Copyright (C) 2012 Oracle Corporation
+ *
+ * This file is part of VirtualBox Open Source Edition (OSE), as
+ * available from http://www.virtualbox.org. This file is free software;
+ * you can redistribute it and/or modify it under the terms of the GNU
+ * General Public License (GPL) as published by the Free Software
+ * Foundation, in version 2 as it comes in the "COPYING" file of the
+ * VirtualBox OSE distribution. VirtualBox OSE is distributed in the
+ * hope that it will be useful, but WITHOUT ANY WARRANTY of any kind.
+ */
+
+
+/*******************************************************************************
+*   Header Files                                                               *
+*******************************************************************************/
+#include <iprt/getopt.h>
+#include <iprt/initterm.h>
+#include <iprt/mem.h>
+#include <iprt/message.h>
+#include <iprt/string.h>
+#include <iprt/stream.h>
+
+
+/*******************************************************************************
+*   Global Variables                                                           *
+*******************************************************************************/
+
+
+
+
+int main(int argc, char **argv)
+{
+    int rc = RTR3InitExe(argc, &argv, 0);
+    if (RT_FAILURE(rc))
+        return RTMsgInitFailure(rc);
+
+    RTMsgError("Not implemented\n");
+    return RTEXITCODE_FAILURE;
+}
+
