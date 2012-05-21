da3aff194ee7369f730716fe3442a8edd2985c50
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2012-05-21 13:11:24 +0000

    Main: smartcard support: update.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@41368 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Main/src-client/UsbCardReader.cpp
+++ src/VBox/Main/src-client/UsbCardReader.cpp
@@ -149,13 +149,13 @@ static DECLCALLBACK(void) drvCardReaderCmdStatus(PUSBCARDREADER pThis,
 }
 
 static DECLCALLBACK(void) drvCardReaderCmdConnect(PUSBCARDREADER pThis,
-                                                  const char *pcszCardReaderName,
                                                   void *pvUser,
+                                                  const char *pcszCardReaderName,
                                                   uint32_t u32ShareMode,
                                                   uint32_t u32PreferredProtocols)
 {
-    LogFlowFunc(("ENTER: pcszCardReaderName:%s, pvUser:%p, u32ShareMode:%RX32, u32PreferredProtocols:%RX32\n",
-                 pcszCardReaderName, pvUser, u32ShareMode, u32PreferredProtocols));
+    LogFlowFunc(("ENTER: pvUser:%p, pcszCardReaderName:%s, u32ShareMode:%RX32, u32PreferredProtocols:%RX32\n",
+                 pvUser, pcszCardReaderName, u32ShareMode, u32PreferredProtocols));
 
     UsbCardReader *pUsbCardReader = pThis->pUsbCardReader;
     if (!pUsbCardReader)
