5180f692db0a8c981c87787979febede3b520ff5
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2012-05-22 07:51:07 +0000

    recompiler: VBOX_DEBUG_PHYS logging fix.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@41385 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/recompiler/VBoxRecompiler.c
+++ src/recompiler/VBoxRecompiler.c
@@ -3792,7 +3792,7 @@ void remR3PhysWriteU64(RTGCPHYS DstGCPhys, uint64_t val)
     PGMR3PhysWriteU64(cpu_single_env->pVM, DstGCPhys, val);
     STAM_PROFILE_ADV_STOP(&gStatMemWrite, a);
 #ifdef VBOX_DEBUG_PHYS
-    LogRel(("writeu64: %llx -> %08x\n", val, (uint32_t)SrcGCPhys));
+    LogRel(("writeu64: %llx -> %08x\n", val, (uint32_t)DstGCPhys));
 #endif
 }
 
