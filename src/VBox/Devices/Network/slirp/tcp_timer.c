4fe1ab320613813d7597e78bb3c7075268224bfa
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2012-05-22 18:05:05 +0000

    NAT: don't waste time on tcp connections without connection information.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@41408 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Devices/Network/slirp/tcp_timer.c
+++ src/VBox/Devices/Network/slirp/tcp_timer.c
@@ -157,8 +157,16 @@ struct tcpcb *
 tcp_timers(PNATState pData, register struct tcpcb *tp, int timer)
 {
     register int rexmt;
+    int fUninitiolizedTemplate = 0;
 
     LogFlowFunc(("ENTER: tp:%R[tcpcb793], timer:%d\n", tp, timer));
+    fUninitiolizedTemplate = RT_BOOL((   tp->t_template.ti_src.s_addr == INADDR_ANY
+                                          || tp->t_template.ti_dst.s_addr == INADDR_ANY));
+    if (fUninitiolizedTemplate)
+    {
+        tp = tcp_drop(pData, tp, 0);
+        return tp;
+    }
 
     switch (timer)
     {
