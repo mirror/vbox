8ff4ecd0febe5ae93a277a4d325563e2de2bfe64
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2012-05-24 14:47:36 +0000

    PGM: Enabled the A20 masking.
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@41437 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/VMM/include/PGMInternal.h
+++ src/VBox/VMM/include/PGMInternal.h
@@ -2522,7 +2522,7 @@ DECLINLINE(void *) pgmPoolMapPageStrict(PPGMPOOLPAGE a_pPage)
 
 /** @name A20 gate macros
  * @{ */
-/*#define PGM_WITH_A20*/
+#define PGM_WITH_A20
 #ifdef PGM_WITH_A20
 # define PGM_A20_IS_ENABLED(a_pVCpu)                        ((a_pVCpu)->pgm.s.fA20Enabled)
 # define PGM_A20_APPLY(a_pVCpu, a_GCPhys)                   ((a_GCPhys) & (a_pVCpu)->pgm.s.GCPhysA20Mask)
