80d8cca54c28346a291d4a9eedab4edaffbf2675
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2012-05-24 11:51:53 +0000

    Use proper format specifier.
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@41431 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Devices/Network/DevE1000.cpp
+++ src/VBox/Devices/Network/DevE1000.cpp
@@ -6598,7 +6598,7 @@ static DECLCALLBACK(void) e1kInfo(PPDMDEVINS pDevIns, PCDBGFINFOHLP pHlp, const 
     /*
      * Show info.
      */
-    pHlp->pfnPrintf(pHlp, "E1000 #%d: port=%RTiop mmio=%RX32 mac-cfg=%RTmac %s%s%s\n",
+    pHlp->pfnPrintf(pHlp, "E1000 #%d: port=%RTiop mmio=%RGp mac-cfg=%RTmac %s%s%s\n",
                     pDevIns->iInstance, pState->addrIOPort, pState->addrMMReg,
                     &pState->macConfigured, g_Chips[pState->eChip].pcszName,
                     pState->fGCEnabled ? " GC" : "", pState->fR0Enabled ? " R0" : "");
