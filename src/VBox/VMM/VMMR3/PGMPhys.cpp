f806534525b7ae0312e46e2353f363668bc76831
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2012-05-28 22:30:31 +0000

    PGMR3PhysSetA20: Drop todo.
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@41466 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/VMM/VMMR3/PGMPhys.cpp
+++ src/VBox/VMM/VMMR3/PGMPhys.cpp
@@ -3717,7 +3717,6 @@ VMMDECL(void) PGMR3PhysSetA20(PVMCPU pVCpu, bool fEnable)
 #ifdef VBOX_WITH_REM
         REMR3A20Set(pVCpu->pVMR3, pVCpu, fEnable);
 #endif
-        /** @todo we're not handling this correctly for VT-x / AMD-V. See #2911 */
 #ifdef PGM_WITH_A20
         pVCpu->pgm.s.fSyncFlags |= PGM_SYNC_UPDATE_PAGE_BIT_VIRTUAL;
         VMCPU_FF_SET(pVCpu, VMCPU_FF_PGM_SYNC_CR3);
