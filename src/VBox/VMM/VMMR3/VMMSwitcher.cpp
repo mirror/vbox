6459b4042630abab37e18c07db485f6a26737366
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2012-06-13 10:12:23 +0000

    build fix
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@41679 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/VMM/VMMR3/VMMSwitcher.cpp
+++ src/VBox/VMM/VMMR3/VMMSwitcher.cpp
@@ -838,10 +838,10 @@ static void vmmR3SwitcherGenericRelocate(PVM pVM, PVMMSWITCHERDEF pSwitcher, RTR
                 if (RT_SUCCESS(rc))
                 {
                     Cpu.uInstrAddr += uBase - (uintptr_t)pu8CodeR3;
-                    rc = DISFormatYasmEx(&Cpu, szDisas, sizeof(szDisas),
-                                         DIS_FMT_FLAGS_ADDR_LEFT | DIS_FMT_FLAGS_BYTES_LEFT | DIS_FMT_FLAGS_BYTES_SPACED
-                                         | DIS_FMT_FLAGS_RELATIVE_BRANCH,
-                                         NULL, NULL);
+                    DISFormatYasmEx(&Cpu, szDisas, sizeof(szDisas),
+                                    DIS_FMT_FLAGS_ADDR_LEFT | DIS_FMT_FLAGS_BYTES_LEFT | DIS_FMT_FLAGS_BYTES_SPACED
+                                    | DIS_FMT_FLAGS_RELATIVE_BRANCH,
+                                    NULL, NULL);
                 }
                 if (RT_SUCCESS(rc))
                     RTLogPrintf("  %04x: %s\n", offCode, szDisas);
