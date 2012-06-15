cc7ff7497535cf9dbf47f9e78752f82a15ab7fbc
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2012-06-15 11:38:51 +0000

    iPXE: save a tiny bit more ROM space by ripping out the embedded image support, use it as the compiled in PXE ROM, but stick to Etherboot for OSE to avoid breakage until iPXE is included in the tree
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@41752 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Devices/build/VBoxDD2.cpp
+++ src/VBox/Devices/build/VBoxDD2.cpp
@@ -1,4 +1,4 @@
-/* $Id $ */
+/* $Id$ */
 /** @file
  * VBoxDD2 - Built-in drivers & devices part 2.
  *
@@ -6,7 +6,7 @@
  */
 
 /*
- * Copyright (C) 2006-2010 Oracle Corporation
+ * Copyright (C) 2006-2012 Oracle Corporation
  *
  * This file is part of VirtualBox Open Source Edition (OSE), as
  * available from http://www.virtualbox.org. This file is free software;
