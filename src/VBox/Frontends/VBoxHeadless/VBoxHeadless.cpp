4b1bdefc8ba425a04c6dd837e25a00875904ca93
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2012-06-28 12:05:20 +0000

    VBoxHeadless: Type error. Corrected.
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@41959 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Frontends/VBoxHeadless/VBoxHeadless.cpp
+++ src/VBox/Frontends/VBoxHeadless/VBoxHeadless.cpp
@@ -42,7 +42,7 @@ using namespace com;
 #include <VBox/err.h>
 #include <VBox/VBoxVideo.h>
 
-#ifdef VBOX_WITH_VIDEO_REC ||
+#ifdef VBOX_WITH_VIDEO_REC 
 #include <cstdlib>
 #include <cerrno>
 #include "VBoxHeadless.h"
