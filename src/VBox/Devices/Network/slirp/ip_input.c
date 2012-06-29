795b0615dd5acbc758f2ccc234e39ee4bd5f183e
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2012-06-29 03:23:45 +0000

    NAT: NTOHS -> RT_N2H_U16
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@41967 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Devices/Network/slirp/ip_input.c
+++ src/VBox/Devices/Network/slirp/ip_input.c
@@ -165,15 +165,15 @@ ip_input(PNATState pData, struct mbuf *m)
     /*
      * Convert fields to host representation.
      */
-    NTOHS(ip->ip_len);
+    RT_N2H_U16(ip->ip_len);
     if (ip->ip_len < hlen)
     {
         ipstat.ips_badlen++;
         goto bad_free_m;
     }
 
-    NTOHS(ip->ip_id);
-    NTOHS(ip->ip_off);
+    RT_N2H_U16(ip->ip_id);
+    RT_N2H_U16(ip->ip_off);
 
     /*
      * Check that the amount of data in the buffers
