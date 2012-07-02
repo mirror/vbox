5c96560ebb30e574bf138bca6b7c5a7b5b1ea110
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2012-07-02 12:23:44 +0000

    DevHPET: parfait hint
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@41983 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Devices/PC/DevHPET.cpp
+++ src/VBox/Devices/PC/DevHPET.cpp
@@ -442,7 +442,8 @@ static int hpetTimerRegRead32(HpetState const *pThis, uint32_t iTimerNo, uint32_
 {
     Assert(PDMCritSectIsOwner(&pThis->csLock));
 
-    if (iTimerNo >= HPET_CAP_GET_TIMERS(pThis->u32Capabilities))
+    if (   iTimerNo >= RT_ELEMENTS(pThis->aTimers) /* parfait */
+        || iTimerNo >= HPET_CAP_GET_TIMERS(pThis->u32Capabilities))
     {
         static unsigned s_cOccurences = 0;
         if (s_cOccurences++ < 10)
@@ -510,7 +511,8 @@ static int hpetTimerRegWrite32(HpetState *pThis, uint32_t iTimerNo, uint32_t iTi
 {
     Assert(!PDMCritSectIsOwner(&pThis->csLock) || TMTimerIsLockOwner(pThis->aTimers[0].CTX_SUFF(pTimer)));
 
-    if (iTimerNo >= HPET_CAP_GET_TIMERS(pThis->u32Capabilities))
+    if (   iTimerNo >= RT_ELEMENTS(pThis->aTimers) /* parfait */
+        || iTimerNo >= HPET_CAP_GET_TIMERS(pThis->u32Capabilities))
     {
         static unsigned s_cOccurences = 0;
         if (s_cOccurences++ < 10)
