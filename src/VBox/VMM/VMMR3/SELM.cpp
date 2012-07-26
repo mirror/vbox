627f4966cd7387ea311bf673d7dd097bcb0f75b9
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2012-07-26 16:24:26 +0000

    try to fix the burns
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@42418 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/VMM/VMMR3/SELM.cpp
+++ src/VBox/VMM/VMMR3/SELM.cpp
@@ -1296,7 +1296,7 @@ static VBOXSTRICTRC selmR3UpdateSegmentRegisters(PVM pVM, PVMCPU pVCpu)
         if (Sel & (X86_SEL_MASK | X86_SEL_LDT))
         {
             /* Get the shadow descriptor entry corresponding to this. */
-            static X86DESC const s_NotPresentDesc = { { 0 } };
+            static X86DESC const s_NotPresentDesc = { { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 } };
             PCX86DESC pDesc;
             if (!(Sel & X86_SEL_LDT))
             {
