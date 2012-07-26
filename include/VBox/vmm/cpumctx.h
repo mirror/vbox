f85243dd874e89dd533463a71def2a91c213becb
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2012-07-26 15:29:29 +0000

    build fix
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@42415 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- include/VBox/vmm/cpumctx.h
+++ include/VBox/vmm/cpumctx.h
@@ -84,7 +84,7 @@ typedef struct CPUMSELREG
 # define CPUMSELREG_ARE_HIDDEN_PARTS_VALID(a_pVCpu, a_pSelReg) \
     (   ((a_pSelReg)->fFlags & CPUMSELREG_FLAGS_VALID) \
      && (   (a_pSelReg)->ValidSel == (a_pSelReg)->Sel \
-         || (   (a_pVCpu) != NULL \
+         || (   (a_pVCpu) /*!= NULL*/ \
              && (a_pSelReg)->ValidSel == ((a_pSelReg)->Sel & X86_SEL_MASK_RPL) \
              && ((a_pSelReg)->Sel      & X86_SEL_RPL) == 1 \
              && ((a_pSelReg)->ValidSel & X86_SEL_RPL) == 0 \
