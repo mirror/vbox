4506d82d992e30f1f61642bb7b3a206d74880fd1
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2012-07-30 15:14:23 +0000

    trpmGCExitTrap: Check for VMCPU_FF_SELM_SYNC_GDT, VMCPU_FF_SELM_SYNC_LDT and VMCPU_FF_SELM_SYNC_TSS if rc is VINF_SUCCESS and return with VINF_SELM_SYNC_GDT to ring-3. Seen when cr4.VME is changed and a trap is forwarded shortly afterwards. The FF remains set and causes TRPM to assert in the forwarding code (debug only).
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@42450 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/VMM/VMMRC/TRPMRCHandlers.cpp
+++ src/VBox/VMM/VMMRC/TRPMRCHandlers.cpp
@@ -188,7 +188,9 @@ static int trpmGCExitTrap(PVM pVM, PVMCPU pVCpu, int rc, PCPUMCTXCORE pRegFrame)
         &&  (   VM_FF_ISPENDING(pVM, VM_FF_TM_VIRTUAL_SYNC | VM_FF_REQUEST | VM_FF_PGM_NO_MEMORY | VM_FF_PDM_DMA)
              || VMCPU_FF_ISPENDING(pVCpu, VMCPU_FF_TIMER | VMCPU_FF_TO_R3 | VMCPU_FF_INTERRUPT_APIC | VMCPU_FF_INTERRUPT_PIC
                                           | VMCPU_FF_REQUEST | VMCPU_FF_PGM_SYNC_CR3 | VMCPU_FF_PGM_SYNC_CR3_NON_GLOBAL
-                                          | VMCPU_FF_PDM_CRITSECT)
+                                          | VMCPU_FF_PDM_CRITSECT
+                                          | VMCPU_FF_SELM_SYNC_GDT | VMCPU_FF_SELM_SYNC_LDT | VMCPU_FF_SELM_SYNC_TSS
+                                   )
             )
        )
     {
@@ -215,6 +217,9 @@ static int trpmGCExitTrap(PVM pVM, PVMCPU pVCpu, int rc, PCPUMCTXCORE pRegFrame)
         else if (   VM_FF_ISPENDING(pVM, VM_FF_REQUEST)
                  || VMCPU_FF_ISPENDING(pVCpu, VMCPU_FF_REQUEST))
             rc = VINF_EM_PENDING_REQUEST;
+        /* Pending GDT/LDT/TSS sync. */
+        else if (VMCPU_FF_ISPENDING(pVCpu, VMCPU_FF_SELM_SYNC_GDT | VMCPU_FF_SELM_SYNC_LDT | VMCPU_FF_SELM_SYNC_TSS))
+            rc = VINF_SELM_SYNC_GDT;
         /* Pending interrupt: dispatch it. */
         else if (    VMCPU_FF_ISPENDING(pVCpu, VMCPU_FF_INTERRUPT_APIC | VMCPU_FF_INTERRUPT_PIC)
                  && !VMCPU_FF_ISSET(pVCpu, VMCPU_FF_INHIBIT_INTERRUPTS)
