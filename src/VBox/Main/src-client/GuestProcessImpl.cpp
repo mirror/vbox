efcf86f76e4448c97ad2ce5813b98f4dec36bc16
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2012-08-01 13:05:51 +0000

    build fix
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@42508 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Main/src-client/GuestProcessImpl.cpp
+++ src/VBox/Main/src-client/GuestProcessImpl.cpp
@@ -1143,7 +1143,7 @@ int GuestProcess::startProcessAsync(void)
 
 DECLCALLBACK(int) GuestProcess::startProcessThread(RTTHREAD Thread, void *pvUser)
 {
-    LogFlowFuncEnter(("pvUser=%p\n", pvUser));
+    LogFlowFunc(("pvUser=%p\n", pvUser));
 
     std::auto_ptr<GuestProcessStartTask> pTask(static_cast<GuestProcessStartTask*>(pvUser));
     AssertPtr(pTask.get());
