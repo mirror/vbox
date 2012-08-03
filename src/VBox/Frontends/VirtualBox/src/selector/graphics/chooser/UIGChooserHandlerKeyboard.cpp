b0ae0b5d1fbc686b2c13b98c25643f40f7aa4ba0
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2012-08-03 14:39:35 +0000

    FE/Qt: 6234: Support for VM groups: Using space key to expand/collapse chosen group.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@42583 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Frontends/VirtualBox/src/selector/graphics/chooser/UIGChooserHandlerKeyboard.cpp
+++ src/VBox/Frontends/VirtualBox/src/selector/graphics/chooser/UIGChooserHandlerKeyboard.cpp
@@ -23,6 +23,7 @@
 /* GUI incluedes: */
 #include "UIGChooserHandlerKeyboard.h"
 #include "UIGChooserModel.h"
+#include "UIGChooserItemGroup.h"
 
 UIGChooserHandlerKeyboard::UIGChooserHandlerKeyboard(UIGChooserModel *pParent)
     : QObject(pParent)
@@ -231,6 +232,27 @@ bool UIGChooserHandlerKeyboard::handleKeyPress(QKeyEvent *pEvent) const
             model()->activate();
             /* And filter out that event: */
             return true;
+        }
+        case Qt::Key_Space:
+        {
+            /* If there is a focus item: */
+            if (UIGChooserItem *pFocusItem = model()->focusItem())
+            {
+                /* Of the group type: */
+                if (pFocusItem->type() == UIGChooserItemType_Group)
+                {
+                    /* Toggle that group: */
+                    UIGChooserItemGroup *pGroupItem = pFocusItem->toGroupItem();
+                    if (pGroupItem->closed())
+                        pGroupItem->open();
+                    else
+                        pGroupItem->close();
+                    /* Filter that event out: */
+                    return true;
+                }
+            }
+            /* Pass event to other items: */
+            return false;
         }
         default:
             break;
