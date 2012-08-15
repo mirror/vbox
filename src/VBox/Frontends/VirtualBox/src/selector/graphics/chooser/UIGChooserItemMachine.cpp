a0922b346a3ca83eda23c75bde760c1322933fd3
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2012-08-15 15:52:16 +0000

    FE/Qt: 6234: Support for VM groups: Highlighted chooser-items (selected) are now using highlighted-text color for text-painting routines.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@42828 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Frontends/VirtualBox/src/selector/graphics/chooser/UIGChooserItemMachine.cpp
+++ src/VBox/Frontends/VirtualBox/src/selector/graphics/chooser/UIGChooserItemMachine.cpp
@@ -542,7 +542,7 @@ void UIGChooserItemMachine::paintBackground(QPainter *pPainter, const QRect &rec
     if (model()->selectionList().contains(this))
     {
         /* Highlight color: */
-        QColor highlight = pal.color(QPalette::Active, QPalette::Highlight);
+        QColor highlight = pal.color(QPalette::Highlight);
 
         /* Calculate top rectangle: */
         QRect tRect = rect;
@@ -655,6 +655,13 @@ void UIGChooserItemMachine::paintMachineInfo(QPainter *pPainter, const QStyleOpt
     QSize snapshotNameSize = data(MachineItemData_SnapshotNameSize).toSize();
     QSize machineStatePixmapSize = data(MachineItemData_StatePixmapSize).toSize();
     QSize machineStateTextSize = data(MachineItemData_StateTextSize).toSize();
+
+    /* Update palette: */
+    if (model()->selectionList().contains(this))
+    {
+        QPalette pal = palette();
+        pPainter->setPen(pal.color(QPalette::HighlightedText));
+    }
 
     /* Calculate indents: */
     int iLeftColumnIndent = iMargin;
