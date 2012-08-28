351809bfe53d93cee6d736aab1454eb81e559c83
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2012-08-28 13:33:33 +0000

    PDMDriver.cpp: Don't assert when trying to attach driver at a configured LUN without any driver attached. Happens when there is no network driver attached to a NIC.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@43037 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/VMM/VMMR3/PDMDriver.cpp
+++ src/VBox/VMM/VMMR3/PDMDriver.cpp
@@ -799,9 +799,10 @@ int pdmR3DrvInstantiate(PVM pVM, PCFGMNODE pNode, PPDMIBASE pBaseInterface, PPDM
     }
     else
     {
-        AssertMsgFailed(("Query for string value of \"Driver\" -> %Rrc\n", rc));
         if (rc == VERR_CFGM_VALUE_NOT_FOUND)
             rc = VERR_PDM_CFG_MISSING_DRIVER_NAME;
+        else
+            AssertMsgFailed(("Query for string value of \"Driver\" -> %Rrc\n", rc));
     }
     return rc;
 }
