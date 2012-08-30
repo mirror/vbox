61e5ebaccf6c5aa5f9944c8231d3df20f29a38f7
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2012-08-30 16:18:42 +0000

    whitespace
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@43114 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Devices/EFI/Firmware2/VBoxPkg/env.cmd
+++ src/VBox/Devices/EFI/Firmware2/VBoxPkg/env.cmd
@@ -6,15 +6,15 @@ REM  * Environment Setup Script for VBoxPkg + EDK2.
 REM  */
 
 REM /*
-REM  Copyright (C) 2009 Oracle Corporation
+REM Copyright (C) 2009 Oracle Corporation
 REM
-REM  This file is part of VirtualBox Open Source Edition (OSE), as
-REM  available from http://www.virtualbox.org. This file is free software;
-REM  you can redistribute it and/or modify it under the terms of the GNU
-REM  General Public License (GPL) as published by the Free Software
-REM  Foundation, in version 2 as it comes in the "COPYING" file of the
-REM  VirtualBox OSE distribution. VirtualBox OSE is distributed in the
-REM  hope that it will be useful, but WITHOUT ANY WARRANTY of any kind.
+REM This file is part of VirtualBox Open Source Edition (OSE), as
+REM available from http://www.virtualbox.org. This file is free software;
+REM you can redistribute it and/or modify it under the terms of the GNU
+REM General Public License (GPL) as published by the Free Software
+REM Foundation, in version 2 as it comes in the "COPYING" file of the
+REM VirtualBox OSE distribution. VirtualBox OSE is distributed in the
+REM hope that it will be useful, but WITHOUT ANY WARRANTY of any kind.
 REM
 REM */
 
