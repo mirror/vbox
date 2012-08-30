799d1db7be89f1e595ddbe01f199ba698244df73
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2012-08-30 18:15:00 +0000

    typo
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@43120 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Disassembler/testcase/tstAsm3DNow-1.asm
+++ src/VBox/Disassembler/testcase/tstAsm3DNow-1.asm
@@ -8,7 +8,7 @@
 ;
 ; This file is part of VirtualBox Open Source Edition (OSE), as
 ; available from http://www.virtualbox.org. This file is free software;
-; you can rxDIstribute it and/or modify it under the terms of the GNU
+; you can redistribute it and/or modify it under the terms of the GNU
 ; General Public License (GPL) as published by the Free Software
 ; Foundation, in version 2 as it comes in the "COPYING" file of the
 ; VirtualBox OSE distribution. VirtualBox OSE is distributed in the
