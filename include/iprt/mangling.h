0681803513657b433a373495cfee55b4daa27694
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2012-09-11 11:25:43 +0000

    mangling.h: typo.
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@43291 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- include/iprt/mangling.h
+++ include/iprt/mangling.h
@@ -48,15 +48,15 @@
  * Stable functions (alphabetical order):
  */
 # define ASMAtomicCmpXchgExU64                          RT_MANGLER(ASMAtomicCmpXchgExU64)   /* not-some-systems... */
-# define ASMAtomicCmpXchgExU64_EndProc                  RT_MANGLER(ASMAtomicCmpXchgExU64_EndProc
+# define ASMAtomicCmpXchgExU64_EndProc                  RT_MANGLER(ASMAtomicCmpXchgExU64_EndProc)
 # define ASMAtomicCmpXchgU64                            RT_MANGLER(ASMAtomicCmpXchgU64)     /* not-some-systems... */
-# define ASMAtomicCmpXchgU64_EndProc                    RT_MANGLER(ASMAtomicCmpXchgU64_EndProc
+# define ASMAtomicCmpXchgU64_EndProc                    RT_MANGLER(ASMAtomicCmpXchgU64_EndProc)
 # define ASMAtomicReadU64                               RT_MANGLER(ASMAtomicReadU64)        /* not-some-systems... */
-# define ASMAtomicReadU64_EndProc                       RT_MANGLER(ASMAtomicReadU64_EndProc
+# define ASMAtomicReadU64_EndProc                       RT_MANGLER(ASMAtomicReadU64_EndProc)
 # define ASMAtomicUoReadU64                             RT_MANGLER(ASMAtomicUoReadU64)      /* not-some-systems... */
-# define ASMAtomicUoReadU64_EndProc                     RT_MANGLER(ASMAtomicUoReadU64_EndProc
+# define ASMAtomicUoReadU64_EndProc                     RT_MANGLER(ASMAtomicUoReadU64_EndProc)
 # define ASMAtomicXchgU64                               RT_MANGLER(ASMAtomicXchgU64)        /* not-some-systems... */
-# define ASMAtomicXchgU64_EndProc                       RT_MANGLER(ASMAtomicXchgU64_EndProc
+# define ASMAtomicXchgU64_EndProc                       RT_MANGLER(ASMAtomicXchgU64_EndProc)
 # define RTAssertAreQuiet                               RT_MANGLER(RTAssertAreQuiet)
 # define RTAssertMayPanic                               RT_MANGLER(RTAssertMayPanic)
 # define RTAssertMsg1                                   RT_MANGLER(RTAssertMsg1)
@@ -1315,7 +1315,7 @@
 # define RTStrPrintfV                                   RT_MANGLER(RTStrPrintfV)
 # define RTStrPrintHexBytes                             RT_MANGLER(RTStrPrintHexBytes)
 # define RTStrPurgeEncoding                             RT_MANGLER(RTStrPurgeEncoding)
-# define RTStrPurgeComplementSet                        RT_MANGLER(RTStrPurgeComplimentSet)
+# define RTStrPurgeComplementSet                        RT_MANGLER(RTStrPurgeComplementSet)
 # define RTStrPutCpInternal                             RT_MANGLER(RTStrPutCpInternal)
 # define RTStrReallocTag                                RT_MANGLER(RTStrReallocTag)
 # define RTStrSimplePatternMatch                        RT_MANGLER(RTStrSimplePatternMatch)
