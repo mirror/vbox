8d675c68683c8fb50d91e40332fed374619a0f87
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2012-09-20 12:18:39 +0000

    Installer/linux: less sed for generating installer bits.
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@43365 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Installer/linux/vboxballoonctrl-service.sh
+++ src/VBox/Installer/linux/vboxballoonctrl-service.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 #
-# VirtualBox web service API daemon init script.
+# VirtualBox balloon control daemon init script.
 #
-# Copyright (C) 2006-2012 Oracle Corporation
+# Copyright (C) 2006-2011 Oracle Corporation
 #
 # This file is part of VirtualBox Open Source Edition (OSE), as
 # available from http://www.virtualbox.org. This file is free software;
@@ -14,64 +14,60 @@
 #
 
 # chkconfig: 35 35 65
-# description: VirtualBox web service API
+# description: VirtualBox balloon control daemon
 #
 ### BEGIN INIT INFO
-# Provides:       vboxweb-service
+# Provides:       vboxballoonctrl-service
 # Required-Start: vboxdrv
 # Required-Stop:  vboxdrv
 # Default-Start:  2 3 4 5
 # Default-Stop:   0 1 6
-# Description:    VirtualBox web service API
+# Description:    VirtualBox balloon control daemon
 ### END INIT INFO
 
 PATH=$PATH:/bin:/sbin:/usr/sbin
-DEBIAN=%DEBIAN%
-NOLSB=%NOLSB%
 
-[ -f /lib/lsb/init-functions ] || NOLSB=yes
+[ -f /lib/lsb/init-functions -a -f /etc/debian_release ] || NOLSB=yes
 [ -f /etc/vbox/vbox.cfg ] && . /etc/vbox/vbox.cfg
 
 if [ -n "$INSTALL_DIR" ]; then
-    binary="$INSTALL_DIR/vboxwebsrv"
-    vboxmanage="$INSTALL_DIR/VBoxManage"
+    binary="$INSTALL_DIR/VBoxBalloonCtrl"
 else
-    binary="/usr/lib/%PACKAGE%/vboxwebsrv"
-    vboxmanage="/usr/lib/%PACKAGE%/VBoxManage"
+    binary="/usr/lib/virtualbox/VBoxBalloonCtrl"
 fi
 
 # silently exit if the package was uninstalled but not purged,
-# applies to Debian packages only
-[ -z "$DEBIAN" -o -x $binary ] || exit 0
+# applies to Debian packages only (but shouldn't hurt elsewhere)
+[ ! -f /etc/debian_release -o -x $binary ] || exit 0
 
-[ -r /etc/default/%PACKAGE% ] && . /etc/default/%PACKAGE%
+[ -r /etc/default/virtualbox ] && . /etc/default/virtualbox
 
 system=unknown
 if [ -f /etc/redhat-release ]; then
     system=redhat
-    PIDFILE="/var/lock/subsys/vboxweb-service"
+    PIDFILE="/var/lock/subsys/vboxballoonctrl-service"
 elif [ -f /etc/SuSE-release ]; then
     system=suse
-    PIDFILE="/var/lock/subsys/vboxweb-service"
+    PIDFILE="/var/lock/subsys/vboxballoonctrl-service"
 elif [ -f /etc/debian_version ]; then
     system=debian
-    PIDFILE="/var/run/vboxweb-service"
+    PIDFILE="/var/run/vboxballoonctrl-service"
 elif [ -f /etc/gentoo-release ]; then
     system=gentoo
-    PIDFILE="/var/run/vboxweb-service"
+    PIDFILE="/var/run/vboxballoonctrl-service"
 elif [ -f /etc/arch-release ]; then
      system=arch
-     PIDFILE="/var/run/vboxweb-service"
+     PIDFILE="/var/run/vboxballoonctrl-service"
 elif [ -f /etc/slackware-version ]; then
     system=slackware
-    PIDFILE="/var/run/vboxweb-service"
+    PIDFILE="/var/run/vboxballoonctrl-service"
 elif [ -f /etc/lfs-release ]; then
     system=lfs
-    PIDFILE="/var/run/vboxweb-service.pid"
+    PIDFILE="/var/run/vboxballoonctrl-service.pid"
 else
     system=other
     if [ -d /var/run -a -w /var/run ]; then
-        PIDFILE="/var/run/vboxweb-service"
+        PIDFILE="/var/run/vboxballoonctrl-service"
     fi
 fi
 
@@ -281,54 +277,31 @@ vboxdrvrunning() {
 
 check_single_user() {
     if [ -n "$2" ]; then
-        fail_msg "VBOXWEB_USER must not contain multiple users!"
+        fail_msg "VBOXBALLOONCTRL_USER must not contain multiple users!"
         exit 1
     fi
 }
 
 start() {
     if ! test -f $PIDFILE; then
-        [ -z "$VBOXWEB_USER" ] && exit 0
-        begin_msg "Starting VirtualBox web service";
-        check_single_user $VBOXWEB_USER
+        [ -z "$VBOXBALLOONCTRL_USER" ] && exit 0
+        begin_msg "Starting VirtualBox balloon control service";
+        check_single_user $VBOXBALLOONCTRL_USER
         vboxdrvrunning || {
             fail_msg "VirtualBox kernel module not loaded!"
             exit 0
         }
         PARAMS="--background"
-        [ -n "$VBOXWEB_HOST" ]           && PARAMS="$PARAMS -H $VBOXWEB_HOST"
-        [ -n "$VBOXWEB_PORT" ]           && PARAMS="$PARAMS -p $VBOXWEB_PORT"
-        [ -n "$VBOXWEB_SSL_KEYFILE" ]    && PARAMS="$PARAMS -s -K $VBOXWEB_SSL_KEYFILE"
-        [ -n "$VBOXWEB_SSL_PASSWORDFILE" ] && PARAMS="$PARAMS -a $VBOXWEB_SSL_PASSWORDFILE"
-        [ -n "$VBOXWEB_SSL_CACERT" ]     && PARAMS="$PARAMS -c $VBOXWEB_SSL_CACERT"
-        [ -n "$VBOXWEB_SSL_CAPATH" ]     && PARAMS="$PARAMS -C $VBOXWEB_SSL_CAPATH"
-        [ -n "$VBOXWEB_SSL_DHFILE" ]     && PARAMS="$PARAMS -D $VBOXWEB_SSL_DHFILE"
-        [ -n "$VBOXWEB_SSL_RANDFILE" ]   && PARAMS="$PARAMS -r $VBOXWEB_SSL_RANDFILE"
-        [ -n "$VBOXWEB_TIMEOUT" ]        && PARAMS="$PARAMS -t $VBOXWEB_TIMEOUT"
-        [ -n "$VBOXWEB_CHECK_INTERVAL" ] && PARAMS="$PARAMS -i $VBOXWEB_CHECK_INTERVAL"
-        [ -n "$VBOXWEB_THREADS" ]        && PARAMS="$PARAMS -T $VBOXWEB_THREADS"
-        [ -n "$VBOXWEB_KEEPALIVE" ]      && PARAMS="$PARAMS -k $VBOXWEB_KEEPALIVE"
-        [ -n "$VBOXWEB_AUTHENTICATION" ] && PARAMS="$PARAMS -A $VBOXWEB_AUTHENTICATION"
-        [ -n "$VBOXWEB_LOGFILE" ]        && PARAMS="$PARAMS -F $VBOXWEB_LOGFILE"
-        [ -n "$VBOXWEB_ROTATE" ]         && PARAMS="$PARAMS -R $VBOXWEB_ROTATE"
-        [ -n "$VBOXWEB_LOGSIZE" ]        && PARAMS="$PARAMS -S $VBOXWEB_LOGSIZE"
-        [ -n "$VBOXWEB_LOGINTERVAL" ]    && PARAMS="$PARAMS -I $VBOXWEB_LOGINTERVAL"
-        # set authentication method + password hash
-        if [ -n "$VBOXWEB_AUTH_LIBRARY" ]; then
-            su - "$VBOXWEB_USER" -c "$vboxmanage setproperty websrvauthlibrary \"$VBOXWEB_AUTH_LIBRARY\""
-            if [ $? -ne 0 ]; then
-                fail_msg "Error $? setting webservice authentication library to $VBOXWEB_AUTH_LIBRARY"
-            fi
-        fi
-        if [ -n "$VBOXWEB_AUTH_PWHASH" ]; then
-            su - "$VBOXWEB_USER" -c "$vboxmanage setextradata global \"VBoxAuthSimple/users/$VBOXWEB_USER\" \"$VBOXWEB_AUTH_PWHASH\""
-            if [ $? -ne 0 ]; then
-                fail_msg "Error $? setting webservice password hash"
-            fi
-        fi
+        [ -n "$VBOXBALLOONCTRL_INTERVAL" ]      && PARAMS="$PARAMS --interval $VBOXBALLOONCTRL_INTERVAL"
+        [ -n "$VBOXBALLOONCTRL_INCREMENT" ]     && PARAMS="$PARAMS --balloon-inc $VBOXBALLOONCTRL_INCREMENT"
+        [ -n "$VBOXBALLOONCTRL_DECREMENT" ]     && PARAMS="$PARAMS --balloon-dec $VBOXBALLOONCTRL_DECREMENT"
+        [ -n "$VBOXBALLOONCTRL_LOWERLIMIT" ]    && PARAMS="$PARAMS --balloon-lower-limit $VBOXBALLOONCTRL_LOWERLIMIT"
+        [ -n "$VBOXBALLOONCTRL_ROTATE" ]        && PARAMS="$PARAMS - $VBOXBALLOONCTRL_ROTATE"
+        [ -n "$VBOXBALLOONCTRL_LOGSIZE" ]       && PARAMS="$PARAMS -S $VBOXBALLOONCTRL_LOGSIZE"
+        [ -n "$VBOXBALLOONCTRL_LOGINTERVAL" ]   && PARAMS="$PARAMS -I $VBOXBALLOONCTRL_LOGINTERVAL"
         # prevent inheriting this setting to VBoxSVC
         unset VBOX_RELEASE_LOG_DEST
-        start_daemon $VBOXWEB_USER $binary $PARAMS > /dev/null 2>&1
+        start_daemon $VBOXBALLOONCTRL_USER $binary $PARAMS > /dev/null 2>&1
         # ugly: wait until the final process has forked
         sleep .1
         PID=`pidof $binary 2>/dev/null`
@@ -346,7 +319,7 @@ start() {
 
 stop() {
     if test -f $PIDFILE; then
-        begin_msg "Stopping VirtualBox web service";
+        begin_msg "Stopping VirtualBox balloon control service";
         killproc $binary
         RETVAL=$?
         if ! pidof $binary > /dev/null 2>&1; then
@@ -364,7 +337,7 @@ restart() {
 }
 
 status() {
-    echo -n "Checking for VBox Web Service"
+    echo -n "Checking for VBox balloon control service"
     if [ -f $PIDFILE ]; then
         echo " ...running"
     else
