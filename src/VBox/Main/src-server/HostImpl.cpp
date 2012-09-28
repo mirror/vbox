e41385545f8f52fb5f5a769e587f4583b6e281a0
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2012-09-28 18:00:07 +0000

    Main/Metrics: Fixed SEGFAULT on attempt to configure host interface (#6439)
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@43470 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Main/src-server/HostImpl.cpp
+++ src/VBox/Main/src-server/HostImpl.cpp
@@ -2856,10 +2856,13 @@ HRESULT Host::updateNetIfList()
     }
     /* At this point listCopy will contain newly discovered interfaces only. */
     for (itNew = listCopy.begin(); itNew != listCopy.end(); ++itNew)
-    {
-        (*itNew)->setVirtualBox(m->pParent);
         (*itNew)->registerMetrics(aCollector, this);
-    }
+    /*
+     * Need to set the references to VirtualBox object in all interface objects
+     * (see @bugref{6439}).
+     */
+    for (itNew = list.begin(); itNew != list.end(); ++itNew)
+        (*itNew)->setVirtualBox(m->pParent);
     m->llNetIfs = list;
     return S_OK;
 #else
