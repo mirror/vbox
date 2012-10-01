4f67d6f8a5272be6cf63d0c5bc9ff0eea78c27e2
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2012-10-01 10:15:57 +0000

    FE/Qt: More suitable NLS use for r81029.
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@43484 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Frontends/VirtualBox/src/VBoxVMInformationDlg.cpp
+++ src/VBox/Frontends/VirtualBox/src/VBoxVMInformationDlg.cpp
@@ -543,8 +543,8 @@ void VBoxVMInformationDlg::refreshStatistics()
             if (!attachments.isEmpty() && busType != KStorageBus_Floppy)
             {
                 QString header = "<tr><td></td><td colspan=2><nobr>%1</nobr></td></tr>";
-                QString strControllerNameWrapper = QApplication::translate("UIMachineSettingsStorage", "%1: %2", "controller name wrapper");
-                storageStat += header.arg(strControllerNameWrapper.arg(QApplication::translate("UIMachineSettingsStorage", "Controller"), controller.GetName()));
+                QString strControllerName = QApplication::translate("UIMachineSettingsStorage", "Controller: %1");
+                storageStat += header.arg(strControllerName.arg(controller.GetName()));
                 int scsiIndex = 0;
                 foreach (const CMediumAttachment &attachment, attachments)
                 {
