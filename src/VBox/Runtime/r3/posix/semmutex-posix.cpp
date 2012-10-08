10972196a9325e90f4ccfb22046746f30be1950f
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2012-10-08 05:21:36 +0000

    RT: build fix.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@43559 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Runtime/r3/posix/semmutex-posix.cpp
+++ src/VBox/Runtime/r3/posix/semmutex-posix.cpp
@@ -274,9 +274,9 @@ DECL_FORCE_INLINE(int) rtSemMutexRequest(RTSEMMUTEX hMutexSem, RTMSINTERVAL cMil
     }
     else
     {
+        struct timespec     ts = {0,0};
 #if defined(RT_OS_DARWIN) || defined(RT_OS_HAIKU)
 
-        struct timespec     ts = {0,0};
         struct timeval      tv = {0,0};
         gettimeofday(&tv, NULL);
         ts.tv_sec = tv.tv_sec;
