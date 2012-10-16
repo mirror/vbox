787cb316feac8895431e7855625dc7d8cda8137f
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2012-10-16 16:25:26 +0000

    VBoxSCSI: Slightly extended interface to allow error reporting.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@43659 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Devices/Storage/DevLsiLogicSCSI.cpp
+++ src/VBox/Devices/Storage/DevLsiLogicSCSI.cpp
@@ -2109,7 +2109,7 @@ static DECLCALLBACK(int) lsilogicDeviceSCSIRequestCompleted(PPDMISCSIPORT pInter
     {
         if (RT_UNLIKELY(pTaskState->fBIOS))
         {
-            int rc = vboxscsiRequestFinished(&pLsiLogic->VBoxSCSI, pSCSIRequest);
+            int rc = vboxscsiRequestFinished(&pLsiLogic->VBoxSCSI, pSCSIRequest, rcCompletion);
             AssertMsgRC(rc, ("Finishing BIOS SCSI request failed rc=%Rrc\n", rc));
         }
         else
@@ -3727,7 +3727,7 @@ static int lsilogicPrepareBIOSSCSIRequest(PLSILOGICSCSI pLsiLogic)
 
     memcpy(pLsiLogic->VBoxSCSI.pBuf, &ScsiInquiryData, 5);
 
-    rc = vboxscsiRequestFinished(&pLsiLogic->VBoxSCSI, &pTaskState->PDMScsiRequest);
+    rc = vboxscsiRequestFinished(&pLsiLogic->VBoxSCSI, &pTaskState->PDMScsiRequest, SCSI_STATUS_OK);
     AssertMsgRCReturn(rc, ("Finishing BIOS SCSI request failed rc=%Rrc\n", rc), rc);
 
     RTMemCacheFree(pLsiLogic->hTaskCache, pTaskState);
@@ -5229,7 +5229,7 @@ static DECLCALLBACK(int) lsilogicConstruct(PPDMDEVINS pDevIns, int iInstance, PC
     if (fBootable)
     {
         if (pThis->enmCtrlType == LSILOGICCTRLTYPE_SCSI_SPI)
-            rc = PDMDevHlpIOPortRegister(pDevIns, LSILOGIC_BIOS_IO_PORT, 3, NULL,
+            rc = PDMDevHlpIOPortRegister(pDevIns, LSILOGIC_BIOS_IO_PORT, 4, NULL,
                                          lsilogicIsaIOPortWrite, lsilogicIsaIOPortRead,
                                          lsilogicIsaIOPortWriteStr, lsilogicIsaIOPortReadStr,
                                          "LsiLogic BIOS");
