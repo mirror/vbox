a74c864ecd74992df07381061762bce5e1932c89
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2012-10-18 19:39:16 +0000

    Installer/linux: rewrite of the headless X.Org start-up script and a unit test.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@43688 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Installer/linux/VBoxHeadlessXOrg.sh
+++ src/VBox/Installer/linux/VBoxHeadlessXOrg.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #
-# VirtualBox X Server auto-start script.
+# VirtualBox X Server auto-start service.
 #
 # Copyright (C) 2012 Oracle Corporation
 #
@@ -16,23 +16,19 @@
 PATH=$PATH:/bin:/sbin:/usr/sbin
 
 ## Start one or several X servers in the background for use with headless
-# rendering.  We only support X.Org Server at the moment.  Starting the X
-# servers is managed by dropping one or more files xorg.conf.<n> into the
-# directory ${HEADLESS_X_ORG_CONFIGURATION_DIRECTORY} - the default value
-# below can be overridden in the configuration file.  We will attempt to start
-# an X server process for each configuration file using display number <n>.
-# For usage and options see the usage() function below this comment block.
+# rendering.  For details, options and configuration see the usage() function
+# further down.
 #
 # I have tried to follow the best practices I could find for writing a Linux
-# daemon process (and doing it in shell script) which should work well with
+# service (and doing it in shell script) which should work well with
 # traditional and modern service systems using minimal init or service files. 
 # In our case this boils down to:
 #  * Start with a single command line, stop using one of ${EXIT_SIGNALS} below.
 #  * Stopping with a signal can be done safely using the pid stored in the
-#    pid-file and our (presumably unique) command name.  For this reason we only
-#    support running one instance of the service though.
-#  * Support starting in the foreground for systems with proper service control.
-#  * Support backgrounding with a pid-file for other systems.
+#    pid-file and our (presumably unique) command name.  For this reason we
+#    only support running one instance of the service though.
+#  * Start in the foreground.  Systems without proper service control can take
+#    care of the backgrounding in the init script.
 #  * Clean up all sub-processes (X servers) ourselves when we are stopped
 #    cleanly and don't provide any other way to clean them up automatically (in
 #    case we are stopped uncleanly) as we don't know of a generic safe way to
@@ -41,55 +37,13 @@ PATH=$PATH:/bin:/sbin:/usr/sbin
 #    potentially have to be run as root, so that we could run all processes
 #    using a service-specific user account and just terminate all processes
 #    run by that user to clean up.)
-#  * For this reason, our pid-file only contains the pid of the master process.
-#  * To simplify system service and start-up message handling, we write
-#    Debian-conform progress information to standard output (what processes we
-#    are starting, information if we do something slow) and errors to standard
-#    error.  This should allow us to write pretty generic init/startup scripts
-#    for different distributions which produce more-or-less correct output for
-#    the system they run on.
 
-## Print usage information for the service script.
-usage() {
-  cat << EOF
-Usage:
-
-  $(basename "${SCRIPT_NAME}") [<options>]
-
-Do any system-wide set-up required to properly run the copy of VirtualBox
-residing in the current directory.  If no options are specified, everything
-except --no-udev-and --log-file is assumed.
-
-Options:
-
-  -d|--daemonize)        Detach fron the terminal and  continue running in the
-                         background.
-
-  -l|--log-folder)       Create log files in this folder.
-
-  -p|--pidfile <name>    Specify the name of the file to save the pids of child
-                         processes in.  Pass in an empty string to disable
-                         pid-file creation.
-
-  -q|--quiet)            Do not produce unnecessary console output.  We still
-                         show a banner if the command line arguments are
-                         invalid.
-
-  --quick)               Intended for internal use.  Skip certain checks and
-                         actions at start-up and print the command line
-                         arguments to standard output.
-
-  --help|--usage         Print this text.
-EOF
-}
-
-## Configuration file name.
+## Default configuration file name.
 # Don't use vbox.cfg as that is currently automatically created and deleted.
 # Don't use /etc/default/virtualbox as that is Debian policy only and not very
 # nice.  Let's try to use this in future and phase out the other two.
 ## @todo Should we be using /etc/virtualbox instead of /etc/vbox?
 CONFIGURATION_FILE=/etc/vbox/vbox.conf
-
 ## The name of this script.
 SCRIPT_NAME="$0"
 ## Command line we were called with.
@@ -98,22 +52,72 @@ SCRIPT_COMMAND_LINE="$0 $@"
 SERVICE_NAME="vboxheadlessxorg"
 ## The descriptive service name.
 SERVICE_LONG_NAME="VBoxHeadless X Server service"
-## Timeout in seconds when shutting down the service.
-SERVICE_SHUTDOWN_TIMEOUT=5
 ## Signals and conditions which may be used to terminate the service.
 EXIT_SIGNALS="EXIT HUP INT QUIT ABRT TERM"
+## The default X server configuration directory.
+DEFAULT_CONFIGURATION_FOLDER="/etc/vbox/headlessxorg.conf.d"
 
-# Default settings values, override in the configuration file.
-## The directory where the configuration files for the X servers are dropped.
-HEADLESS_X_ORG_CONFIGURATION_DIRECTORY=/etc/vbox/headlessxorg.conf.d
-## The path to the pidfile for the service.
-HEADLESS_X_ORG_PID_FILE="/var/run/${SERVICE_NAME}.pid"
-## The default log folder
+## Print usage information for the service script.
+## @todo Perhaps we should support some of the configuration file options from
+#        the command line.  Opinions welcome.
+## @todo Possibly extract this information for the user manual.
+usage() {
+  cat << EOF
+Usage:
+
+  $(basename "${SCRIPT_NAME}") [<options>]
+
+Start one or several X servers in the background for use with headless
+rendering.  We only support X.Org Server at the moment.  Starting the X servers
+is managed by dropping one or more files xorg.conf.<n> into a configuration
+directory (by default ${DEFAULT_CONFIGURATION_FOLDER} - this can be overridden
+in the configuration file (see below).  We will attempt to start an X server
+process for each configuration file using display number <n>.
+
+Options:
+
+  -c|--conf-file)        Specify an alternative locations for the configuration
+                         file.  The default location is \"${CONFIGURATION_FILE}\".
+
+  --help|--usage         Print this text.
+
+The optional configuration file should contain a series of lines of the form
+"KEY=value".  It will be read in as a command shell sub-script.  Here is the
+current list of possible key settings with a short explanation.
+
+  HEADLESS_X_ORG_CONFIGURATION_FOLDER
+    The folder where the configuration files for the X servers are to be found.
+
+  HEADLESS_X_ORG_LOG_FOLDER
+    The default log folder.
+
+  HEADLESS_X_ORG_LOG_FILE
+    The default log file.
+
+  HEADLESS_X_ORG_SERVER_COMMAND
+    The default X server start-up command, containing the variables "${screen}"
+    for the screen number, "${conf_file}" for the configuration file path and
+    ${log_file} for the log file.  The variables must be quoted for expansion
+    at evaluation time, e.g.
+      HEADLESS_X_ORG_SERVER_COMMAND="Xorg :\${screen} [...] > \"\${log_file}\"
+
+  HEADLESS_X_ORG_SERVER_LOG_FILE_TEMPLATE
+    The default X server log file name, containing the variable "${screen}" for
+    the screen number.  The variable must be quoted for expansion at evaluation
+    time, e.g.
+      HEADLESS_X_ORG_SERVER_LOG_FILE_TEMPLATE="Xorg.\${screen}.log"
+EOF
+}
+
+# Default configuration.
+HEADLESS_X_ORG_CONFIGURATION_FOLDER="${DEFAULT_CONFIGURATION_FOLDER}"
 HEADLESS_X_ORG_LOG_FOLDER="/var/log/${SERVICE_NAME}"
+HEADLESS_X_ORG_LOG_FILE="${SERVICE_NAME}.log"
+HEADLESS_X_ORG_SERVER_COMMAND="Xorg :\${screen} -config \"\${conf_file}\" -logverbose 0 -logfile /dev/null -verbose 7 > \"\${log_file}\" 2>&1"
+HEADLESS_X_ORG_SERVER_LOG_FILE_TEMPLATE="Xorg.\${screen}.log"
 
 ## The function definition at the start of every non-trivial shell script!
 abort() {
-  stop 2>/dev/null
   ## $@, ... Error text to output to standard error in printf format.
   printf "$@" >&2
   exit 1
@@ -121,21 +125,9 @@ abort() {
 
 ## Milder version of abort, when we can't continue because of a valid condition.
 abandon() {
-  stop 2>/dev/null
   ## $@, ... Text to output to standard error in printf format.
   printf "$@" >&2
   exit 0
-}
-
-banner() {
-  cat << EOF
-${VBOX_PRODUCT} VBoxHeadless X Server start-up service Version ${VBOX_VERSION_STRING}
-(C) 2005-${VBOX_C_YEAR} ${VBOX_VENDOR}
-All rights reserved.
-
-EOF
-[ -n "${QUICK}" ] &&
-  printf "Internal command line: ${SCRIPT_COMMAND_LINE}\n\n"
 }
 
 abort_usage() {
@@ -143,108 +135,83 @@ abort_usage() {
   abort "$@"
 }
 
-# Change to the directory where the script is located.
-MY_DIR="$(dirname "${SCRIPT_NAME}")"
-MY_DIR=`cd "${MY_DIR}" && pwd`
-[ -d "${MY_DIR}" ] ||
-  abort "Failed to change to directory ${MY_DIR}.\n"
+# Print a banner message
+banner() {
+  cat << EOF
+${VBOX_PRODUCT} VBoxHeadless X Server start-up service Version ${VBOX_VERSION_STRING}
+(C) 2005-${VBOX_C_YEAR} ${VBOX_VENDOR}
+All rights reserved.
 
-# Get installation configuration
-[ -r /etc/vbox/vbox.cfg ] || abort "/etc/vbox/vbox.cfg not found.\n"
-. /etc/vbox/vbox.cfg
+EOF
+}
 
-[ -r scripts/generated.sh ] ||
-  abort "${LOG_FILE}" "Failed to find installation information in ${MY_DIR}.\n"
-. scripts/generated.sh
+# Get the directory where the script is located.
+VBOX_FOLDER="$(dirname "${SCRIPT_NAME}")"
+VBOX_FOLDER=$(cd "${VBOX_FOLDER}" && pwd)
+[ -d "${VBOX_FOLDER}" ] ||
+  abort "Failed to change to directory ${VBOX_FOLDER}.\n"
+# And change to the root directory so we don't hold any other open.
+cd /
 
-[ -r /etc/vbox/vbox.conf ] && . /etc/vbox/vbox.conf
+[ -r "${VBOX_FOLDER}/scripts/generated.sh" ] ||
+  abort "${LOG_FILE}" "Failed to find installation information in ${VBOX_FOLDER}.\n"
+. "${VBOX_FOLDER}/scripts/generated.sh"
 
 # Parse our arguments.
 while [ "$#" -gt 0 ]; do
   case $1 in
-    -d|--daemonize)
-      DAEMONIZE=true
-      ;;
-    -l|--log-folder)
+    -c|--conf-file)
       [ "$#" -gt 1 ] ||
-        {
-          banner
-          abort "%s requires at least one argument.\n" "$1"
-        }
-      HEADLESS_X_ORG_LOG_FOLDER="$2"
+      {
+        banner
+        abort "%s requires at least one argument.\n" "$1"
+      }
+      CONFIGURATION_FILE="$2"
       shift
-      ;;
-    -p|--pidfile)
-      [ "$#" -gt 1 ] ||
-        {
-          banner
-          abort "%s requires at least one argument.\n" "$1"
-        }
-      HEADLESS_X_ORG_PID_FILE="$2"
-      shift
-      ;;
-    -q|--quiet)
-      QUIET=true
-      ;;
-    --quick)
-      QUICK=true
       ;;
     --help|--usage)
+      banner
       usage
       exit 0
       ;;
     *)
-      {
-        banner
-        abort_usage "Unknown argument $1.\n"
-      }
+      banner
+      abort_usage "Unknown argument $1.\n"
       ;;
   esac
   shift
 done
 
-[ -z "${QUIET}" ] && banner
+[ -r "${CONFIGURATION_FILE}" ] && . "${CONFIGURATION_FILE}"
 
-if [ -z "${QUICK}" ]; then
-  [ -f "${HEADLESS_X_ORG_PID_FILE}" ] &&
-    ps -p "$(cat "${HEADLESS_X_ORG_PID_FILE}")" -o comm |
-      grep -q "${SERVICE_NAME}" &&
-    abort "The service appears to be already running.\n"
+# If something fails here we will catch it when we create the directory.
+[ -e "${HEADLESS_X_ORG_LOG_FOLDER}" ] &&
+  [ -d "${HEADLESS_X_ORG_LOG_FOLDER}" ] &&
+  rm -rf "${HEADLESS_X_ORG_LOG_FOLDER}.old" 2> /dev/null &&
+mv "${HEADLESS_X_ORG_LOG_FOLDER}" "${HEADLESS_X_ORG_LOG_FOLDER}.old" 2> /dev/null
+mkdir -p "${HEADLESS_X_ORG_LOG_FOLDER}" 2>/dev/null ||
+{
+  banner
+  abort "Failed to create log folder \"${HEADLESS_X_ORG_LOG_FOLDER}\".\n"
+}
+exec > "${HEADLESS_X_ORG_LOG_FOLDER}/${HEADLESS_X_ORG_LOG_FILE}" 2>&1
 
-  PIDFILE_DIRECTORY="$(dirname "${HEADLESS_X_ORG_PID_FILE}")"
-  { ! [ -d "${PIDFILE_DIRECTORY}" ] || ! [ -w "${PIDFILE_DIRECTORY}" ]; } &&
-    abort "Can't write to pid-file directory \"${PIDFILE_DIRECTORY}\".\n"
-
-  # If something fails here we will catch it when we create the directory.
-  [ -e "${HEADLESS_X_ORG_LOG_FOLDER}" ] &&
-    [ -d "${HEADLESS_X_ORG_LOG_FOLDER}" ] &&
-    rm -rf "${HEADLESS_X_ORG_LOG_FOLDER}.old" 2> /dev/null &&
-    mv "${HEADLESS_X_ORG_LOG_FOLDER}" "${HEADLESS_X_ORG_LOG_FOLDER}.old" 2> /dev/null
-  mkdir -p "${HEADLESS_X_ORG_LOG_FOLDER}" 2>/dev/null ||
-    abort "Failed to create log folder \"${HEADLESS_X_ORG_LOG_FOLDER}\".\n"
-fi # -z "${QUICK}"
-
-# Double background from shell script, disconnecting all standard streams, to
-# daemonise.  This may fail if someone has connected something to another file
-# descriptor.  This is intended (see e.g. fghack in the daemontools package).
-if [ -n "${DAEMONIZE}" ]; then
-  ("${SCRIPT_NAME}" --quick -p "${HEADLESS_X_ORG_PID_FILE}" < /dev/null > "${HEADLESS_X_ORG_LOG_FOLDER}/${SERVICE_NAME}.log" 2>&1 &) &
-  ## @todo wait for the servers to start accepting connections before exiting.
-  exit 0
-fi
+banner
 
 X_SERVER_PIDS=""
-trap "kill \${X_SERVER_PIDS} 2>/dev/null; exit 0" ${EXIT_SIGNALS}
+trap "kill \${X_SERVER_PIDS} 2>/dev/null" ${EXIT_SIGNALS}
 space=""  # Hack to put spaces between the pids but not before or after.
-for file in "${HEADLESS_X_ORG_CONFIGURATION_DIRECTORY}"/xorg.conf.*; do
-  filename="$(basename "${file}")"
-  expr "${filename}" : "xorg.conf.[0-9]*" > /dev/null ||
-    { stop; abort "Badly formed file name \"${file}\".\n"; }
-  screen="${filename##*.}"
-  Xorg ":${screen}" -config "${file}" -logverbose 0 -logfile /dev/null -verbose 7 > "${HEADLESS_X_ORG_LOG_FOLDER}/Xorg.${screen}.log" 2>&1 &
+for conf_file in "${HEADLESS_X_ORG_CONFIGURATION_FOLDER}"/*; do
+  [ x"${conf_file}" = x"${HEADLESS_X_ORG_CONFIGURATION_FOLDER}/*" ] &&
+    ! [ -e "${conf_file}" ] &&
+    abort "No configuration files found.\n"
+  filename="$(basename "${conf_file}")"
+  screen="$(expr "${filename}" : "xorg\.conf\.\(.*\)")"
+  [ 0 -le "${screen}" ] 2>/dev/null ||
+    abort "Badly formed file name \"${conf_file}\".\n"
+  log_file="${HEADLESS_X_ORG_LOG_FOLDER}/$(eval echo "${HEADLESS_X_ORG_SERVER_LOG_FILE_TEMPLATE}")"
+  eval "${HEADLESS_X_ORG_SERVER_COMMAND}" "&"
   X_SERVER_PIDS="${X_SERVER_PIDS}${space}$!"
   space=" "
 done
-[ -n "${HEADLESS_X_ORG_PID_FILE}" ] &&
-  echo "$$" > "${HEADLESS_X_ORG_PID_FILE}"
 wait
