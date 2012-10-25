246490c9278ddc3c0ab8a0b88bb16c37f0d83f14
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2012-10-25 13:57:24 +0000

    Main/Snapshot: only check free space on filesystems when the target uses file backing
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@43740 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Main/src-server/SnapshotImpl.cpp
+++ src/VBox/Main/src-server/SnapshotImpl.cpp
@@ -2500,22 +2500,35 @@ void SessionMachine::deleteSnapshotHandler(DeleteSnapshotTask &aTask)
                 uint32_t pu32Serial = 0;
                 ComObjPtr<Medium> pSource_local = it_md->mpSource;
                 ComObjPtr<Medium> pTarget_local = it_md->mpTarget;
+                ComPtr<IMediumFormat> pTargetFormat;
 
-                int vrc = RTFsQuerySerial(pTarget_local->getLocationFull().c_str(), &pu32Serial);
-                if (RT_FAILURE(vrc))
-                {
-                    rc = setError(E_FAIL,
-                                      tr(" Impossible merging with '%s'. Can't get storage UID."),
-                                      pTarget_local->getLocationFull().c_str());
+                rc = pTarget_local->COMGETTER(MediumFormat)(pTargetFormat.asOutParam());
+                if (FAILED(rc))
                     throw rc;
+                ULONG uTargetCaps = 0;
+                rc = pTargetFormat->COMGETTER(Capabilities)(&uTargetCaps);
+                if (FAILED(rc))
+                    throw rc;
+
+                if (uTargetCaps & MediumFormatCapabilities_File)
+                {
+                    int vrc = RTFsQuerySerial(pTarget_local->getLocationFull().c_str(), &pu32Serial);
+                    if (RT_FAILURE(vrc))
+                    {
+                        rc = setError(E_FAIL,
+                                      tr("Impossible merging with '%s'. Can't get storage UID"),
+                                      pTarget_local->getLocationFull().c_str());
+                        throw rc;
+                    }
+
+                    pSource_local->COMGETTER(Size)((LONG64*)&diskSize);
+
+                    /* store needed free space in multimap */
+                    neededStorageFreeSpace.insert(std::make_pair(pu32Serial,diskSize));
+                    /* linking storage UID with snapshot path, it is a helper container (just for easy finding needed path) */
+                    serialMapToStoragePath.insert(std::make_pair(pu32Serial,pTarget_local->getLocationFull().c_str()));
                 }
 
-                pSource_local->COMGETTER(Size)((LONG64*)&diskSize);
-
-                /* store needed free space in multimap */
-                neededStorageFreeSpace.insert(std::make_pair(pu32Serial,diskSize));
-                /* linking storage UID with snapshot path, it is a helper container (just for easy finding needed path) */
-                serialMapToStoragePath.insert(std::make_pair(pu32Serial,pTarget_local->getLocationFull().c_str()));
                 ++it_md;
             }
 
