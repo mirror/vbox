e0a103f3fbd08bae8f2339bb29bb7b22e097ca6c
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2012-11-13 12:06:36 +0000

    VBoxManage: Fix converting disk images from raw files
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@43862 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Frontends/VBoxManage/VBoxManageDisk.cpp
+++ src/VBox/Frontends/VBoxManage/VBoxManageDisk.cpp
@@ -797,14 +797,15 @@ RTEXITCODE handleConvertFromRaw(int argc, char *argv[])
                 break;
 
             case 'm':   // --variant
-                MediumVariant_T DiskVariant;
+            {
+                MediumVariant_T DiskVariant = MediumVariant_Standard;
                 rc = parseDiskVariant(ValueUnion.psz, &DiskVariant);
                 if (RT_FAILURE(rc))
                     return errorArgument("Invalid hard disk variant '%s'", ValueUnion.psz);
                 /// @todo cleaner solution than assuming 1:1 mapping?
                 uImageFlags = (unsigned)DiskVariant;
                 break;
-
+            }
             case VINF_GETOPT_NOT_OPTION:
                 if (!srcfilename)
                 {
