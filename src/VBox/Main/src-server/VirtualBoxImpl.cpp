02c00ea2efe224a966530f74cfc3cc226f79eadd
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2012-11-17 14:00:44 +0000

    Fix attaching a hard disk by UUID through VBoxManage (4.2 regression, see public #11209)
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@43906 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Main/src-server/VirtualBoxImpl.cpp
+++ src/VBox/Main/src-server/VirtualBoxImpl.cpp
@@ -1939,6 +1939,7 @@ STDMETHODIMP VirtualBox::OpenMedium(IN_BSTR aLocation,
     AutoCaller autoCaller(this);
     if (FAILED(autoCaller.rc())) return autoCaller.rc();
 
+    Guid id(aLocation);
     ComObjPtr<Medium> pMedium;
 
     // have to get write lock as the whole find/update sequence must be done
@@ -1951,9 +1952,12 @@ STDMETHODIMP VirtualBox::OpenMedium(IN_BSTR aLocation,
     switch (deviceType)
     {
         case DeviceType_HardDisk:
-            rc = findHardDiskByLocation(aLocation,
-                                   false, /* aSetError */
-                                   &pMedium);
+            if (!id.isEmpty())
+                rc = findHardDiskById(id, false /* setError */, &pMedium);
+            else
+                rc = findHardDiskByLocation(aLocation,
+                                            false, /* aSetError */
+                                            &pMedium);
         break;
 
         case DeviceType_Floppy:
