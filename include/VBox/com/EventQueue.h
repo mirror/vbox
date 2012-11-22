edf35726e1d7878287d369e9b753f724870685cd
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2012-11-22 17:00:42 +0000

    Main/glue: bad_alloc handling for event queue, todos.
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@43943 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- include/VBox/com/EventQueue.h
+++ include/VBox/com/EventQueue.h
@@ -4,7 +4,7 @@
  */
 
 /*
- * Copyright (C) 2006-2010 Oracle Corporation
+ * Copyright (C) 2006-2012 Oracle Corporation
  *
  * This file is part of VirtualBox Open Source Edition (OSE), as
  * available from http://www.virtualbox.org. This file is free software;
@@ -88,7 +88,7 @@ class EventQueue
 public:
 
     EventQueue();
-    ~EventQueue();
+    ~EventQueue(); /** @todo r=andy Why not virtual? */
 
     BOOL postEvent(Event *event);
     int processEventQueue(RTMSINTERVAL cMsTimeout);
