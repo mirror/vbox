3e47bb82a551b74c71d4a1c77a4a28b6aab6131b
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2012-11-23 09:38:06 +0000

    OpenGL: warnings and a bugfix
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@43945 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/HostServices/SharedOpenGL/crserverlib/server_blitter.cpp
+++ src/VBox/HostServices/SharedOpenGL/crserverlib/server_blitter.cpp
@@ -439,7 +439,7 @@ void CrBltBlitTexMural(PCR_BLITTER pBlitter, CR_BLITTER_TEXTURE *pSrc, const RTR
 
 void CrBltBlitTexTex(PCR_BLITTER pBlitter, CR_BLITTER_TEXTURE *pSrc, const RTRECT *pSrcRect, CR_BLITTER_TEXTURE *pDst, const RTRECT *pDstRect, uint32_t cRects, uint32_t fFlags)
 {
-    RTRECTSIZE DstSize = {pDst->width, pDst->height};
+    RTRECTSIZE DstSize = {(uint32_t)pDst->width, (uint32_t)pDst->height};
 
     cr_server.head_spu->dispatch_table.BindFramebufferEXT(GL_DRAW_FRAMEBUFFER, pBlitter->idFBO);
 
