f236ab2605077d57b074843fb7f7f73803b27cb0
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2012-12-02 16:18:09 +0000

    build fix
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@44005 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Devices/PC/DevAPIC.cpp
+++ src/VBox/Devices/PC/DevAPIC.cpp
@@ -164,38 +164,6 @@
 
 #define DEBUG_APIC
 
-/* APIC Local Vector Table */
-#define APIC_LVT_TIMER   0
-#define APIC_LVT_THERMAL 1
-#define APIC_LVT_PERFORM 2
-#define APIC_LVT_LINT0   3
-#define APIC_LVT_LINT1   4
-#define APIC_LVT_ERROR   5
-#define APIC_LVT_NB      6
-
-/* APIC delivery modes */
-#define APIC_DM_FIXED   0
-#define APIC_DM_LOWPRI  1
-#define APIC_DM_SMI     2
-#define APIC_DM_NMI     4
-#define APIC_DM_INIT    5
-#define APIC_DM_SIPI    6
-#define APIC_DM_EXTINT  7
-
-/* APIC destination mode */
-#define APIC_DESTMODE_FLAT      0xf
-#define APIC_DESTMODE_CLUSTER   0x0
-
-#define APIC_TRIGGER_EDGE  0
-#define APIC_TRIGGER_LEVEL 1
-
-#define APIC_LVT_TIMER_PERIODIC         (1<<17)
-#define APIC_LVT_MASKED                 (1<<16)
-#define APIC_LVT_LEVEL_TRIGGER          (1<<15)
-#define APIC_LVT_REMOTE_IRR             (1<<14)
-#define APIC_INPUT_POLARITY             (1<<13)
-#define APIC_SEND_PENDING               (1<<12)
-
 #define ESR_ILLEGAL_ADDRESS (1 << 7)
 
 #define APIC_SV_ENABLE (1 << 8)
