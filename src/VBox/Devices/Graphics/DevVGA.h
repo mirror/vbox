bc27f5af617a4c72cd72497a8d22e897e0940fd1
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2012-12-06 17:06:05 +0000

    Undid unwanted changes.
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@44056 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Devices/Graphics/DevVGA.h
+++ src/VBox/Devices/Graphics/DevVGA.h
@@ -310,10 +310,7 @@ typedef struct VGAState {
     bool                        fRemappedVGA;
     /** Whether to render the guest VRAM to the framebuffer memory. False only for some LFB modes. */
     bool                        fRenderVRAM;
-    /** Indicates "simple" planar write access to VGA aperture. */
-    bool                        fSimplePlanarWrite;
-    /** Current "simple" write plane. */
-    uint8_t                     uPassthruPlane;
+    bool                        Padding1[2];
 
     /** The physical address the VRAM was assigned. */
     RTGCPHYS                    GCPhysVRAM;
@@ -328,10 +325,6 @@ typedef struct VGAState {
     STAMPROFILE                 StatR3MemoryWrite;
     STAMCOUNTER                 StatMapPage;            /**< Counts IOMMMIOMapMMIO2Page calls.  */
     STAMCOUNTER                 StatUpdateDisp;         /**< Counts vgaPortUpdateDisplay calls.  */
-    STAMCOUNTER                 StatChgDWState;
-    STAMCOUNTER                 StatChgDWPlane;
-    STAMCOUNTER                 StatRZOptWrite;
-    STAMCOUNTER                 StatR3OptWrite;
 
     /* Keep track of ring 0 latched accesses to the VGA MMIO memory. */
     uint64_t                    u64LastLatchedAccess;
