de0965a9c0e99ca3ad55b008d1c94c1b1b940286
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2012-12-14 14:51:03 +0000

    Runtime/sg.cpp: Increase assertion limit to 32Mb as some guests might transfer more than 5MB in one S/G buffer
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@44135 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Runtime/common/misc/sg.cpp
+++ src/VBox/Runtime/common/misc/sg.cpp
@@ -46,7 +46,7 @@ static void *sgBufGet(PRTSGBUF pSgBuf, size_t *pcbData)
         return NULL;
     }
 
-    AssertReleaseMsg(      pSgBuf->cbSegLeft <= 5 * _1M
+    AssertReleaseMsg(      pSgBuf->cbSegLeft <= 32 * _1M
                      &&    (uintptr_t)pSgBuf->pvSegCur                     >= (uintptr_t)pSgBuf->paSegs[pSgBuf->idxSeg].pvSeg
                      &&    (uintptr_t)pSgBuf->pvSegCur + pSgBuf->cbSegLeft <= (uintptr_t)pSgBuf->paSegs[pSgBuf->idxSeg].pvSeg + pSgBuf->paSegs[pSgBuf->idxSeg].cbSeg,
                      ("pSgBuf->idxSeg=%d pSgBuf->cSegs=%d pSgBuf->pvSegCur=%p pSgBuf->cbSegLeft=%zd pSgBuf->paSegs[%d].pvSeg=%p pSgBuf->paSegs[%d].cbSeg=%zd\n",
