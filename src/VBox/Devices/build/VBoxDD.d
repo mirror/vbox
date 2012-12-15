08e127250ec44d34b69c70b2183a41c34fa8aa5c
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2012-12-15 14:31:17 +0000

    AHCI: Add dtrace probes
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@44138 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Devices/build/VBoxDD.d
+++ src/VBox/Devices/build/VBoxDD.d
@@ -21,6 +21,11 @@ provider vboxdd
     probe hgcmcall__completed__req(void *pvCmd, int rc);
     probe hgcmcall__completed__emt(void *pvCmd, int rc);
     probe hgcmcall__completed__done(void *pvCmd, unsigned int idFunction, unsigned int idClient, int rc);
+
+    probe ahci__req__submit(void *pvReq, int iTxDir, uint64_t offStart, uint32_t cbXfer);
+    probe ahci__req__submit__timestamp(void *pvReq, uint64_t tsStart);
+    probe ahci__req__completed(void *pvReq, int rcReq, int iState, uint64_t offStart, uint32_t cbXfer);
+    probe ahci__req__completed__timestamp(void *pvReq, uint64_t tsEnd);
 };
 
 #pragma D attributes Evolving/Evolving/Common provider vboxdd provider
