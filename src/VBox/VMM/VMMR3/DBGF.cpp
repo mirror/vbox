c13a6909358d55d3c21ec880612f0e93b6afe39d
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2013-01-27 23:20:07 +0000

    Build fix
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@44403 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/VMM/VMMR3/DBGF.cpp
+++ src/VBox/VMM/VMMR3/DBGF.cpp
@@ -1053,9 +1053,9 @@ VMMR3DECL(bool) DBGFR3IsHalted(PUVM pUVM)
  */
 VMMR3DECL(bool) DBGFR3CanWait(PUVM pUVM)
 {
-    UVM_ASSERT_VALID_EXT_RETURN(pUVM, VERR_INVALID_VM_HANDLE);
+    UVM_ASSERT_VALID_EXT_RETURN(pUVM, false);
     PVM pVM = pUVM->pVM;
-    VM_ASSERT_VALID_EXT_RETURN(pVM, VERR_INVALID_VM_HANDLE);
+    VM_ASSERT_VALID_EXT_RETURN(pVM, false);
     AssertReturn(pVM->dbgf.s.fAttached, false);
 
     return RTSemPongShouldWait(&pVM->dbgf.s.PingPong);
