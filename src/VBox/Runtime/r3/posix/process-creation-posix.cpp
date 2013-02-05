0aceab22f2aff566a3e0b4473819d190479311b2
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2013-02-05 16:10:29 +0000

    Runtime/posix/RTProcCreateEx: unblock all signals for the new child process
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@44549 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Runtime/r3/posix/process-creation-posix.cpp
+++ src/VBox/Runtime/r3/posix/process-creation-posix.cpp
@@ -475,6 +475,10 @@ RTR3DECL(int)   RTProcCreateEx(const char *pszExec, const char * const *papszArg
                 Assert(rc == 0);
             }
 # endif
+            sigset_t sigmask;
+            sigemptyset(&sigmask);
+            rc = posix_spawnattr_setsigmask(&Attr, &sigmask);
+            Assert(rc == 0);
 
             /* File changes. */
             posix_spawn_file_actions_t  FileActions;
@@ -583,6 +587,13 @@ RTR3DECL(int)   RTProcCreateEx(const char *pszExec, const char * const *papszArg
                 }
             }
 #endif
+
+            /*
+             * Unset the signal mask.
+             */
+            sigset_t sigmask;
+            sigemptyset(&sigmask);
+            sigprocmask(SIG_SETMASK, &sigmask, NULL);
 
             /*
              * Apply changes to the standard file descriptor and stuff.
