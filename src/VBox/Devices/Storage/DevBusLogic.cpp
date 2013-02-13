a99c5de6ca7993090ca4a52ea12f6e474c32e385
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2013-02-13 18:03:11 +0000

    BusLogic: Also copy data buffer out if transfer direction is unknown.
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@44672 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Devices/Storage/DevBusLogic.cpp
+++ src/VBox/Devices/Storage/DevBusLogic.cpp
@@ -1402,7 +1402,8 @@ static int buslogicR3DataBufferAlloc(PBUSLOGICTASKSTATE pTaskState)
                 return VERR_NO_MEMORY;
 
             /* Copy the data if needed */
-            if (pTaskState->CommandControlBlockGuest.c.uDataDirection == BUSLOGIC_CCB_DIRECTION_OUT)
+            if (   (pTaskState->CommandControlBlockGuest.c.uDataDirection == BUSLOGIC_CCB_DIRECTION_OUT)
+                || (pTaskState->CommandControlBlockGuest.c.uDataDirection == BUSLOGIC_CCB_DIRECTION_UNKNOWN))
             {
                 cScatterGatherGCLeft = cbDataCCB / pTaskState->cbSGEntry;
                 GCPhysAddrScatterGatherCurrent = u32PhysAddrCCB;
