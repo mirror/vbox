e5acf7a3b6c7d47c2a95fe734a752b5d0ce5fbb7
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2013-02-14 20:00:24 +0000

    DevLsiLogicSCSI.cpp: No default critical section needed according to Alexander.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@44697 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Devices/Storage/DevLsiLogicSCSI.cpp
+++ src/VBox/Devices/Storage/DevLsiLogicSCSI.cpp
@@ -5102,7 +5102,11 @@ static DECLCALLBACK(int) lsilogicR3Construct(PPDMDEVINS pDevIns, int iInstance, 
 
     /*
      * Create critical sections protecting the reply post and free queues.
+     * Note! We do our own syncronization, so NOP the default crit sect for the device.
      */
+    rc = PDMDevHlpSetDeviceCritSect(pDevIns, PDMDevHlpCritSectGetNop(pDevIns));
+    AssertRCReturn(rc, rc);
+
     rc = PDMDevHlpCritSectInit(pDevIns, &pThis->ReplyFreeQueueCritSect, RT_SRC_POS, "%sRFQ", szDevTag);
     if (RT_FAILURE(rc))
         return PDMDEV_SET_ERROR(pDevIns, rc, N_("LsiLogic: cannot create critical section for reply free queue"));
