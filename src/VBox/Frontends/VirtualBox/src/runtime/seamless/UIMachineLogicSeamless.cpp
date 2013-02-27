fb8f2072d1d72c3d0bbd0a29b49f4ea9d3d7af6d
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2013-02-27 17:26:33 +0000

    FE/Qt: Update for r83978: Multi-screen support: Create machine-windows for disabled guest-displays too.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@44846 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Frontends/VirtualBox/src/runtime/seamless/UIMachineLogicSeamless.cpp
+++ src/VBox/Frontends/VirtualBox/src/runtime/seamless/UIMachineLogicSeamless.cpp
@@ -127,7 +127,7 @@ void UIMachineLogicSeamless::prepareMachineWindows()
     m_pScreenLayout->update();
 
     /* Create machine window(s): */
-    for (int cScreenId = 0; cScreenId < m_pScreenLayout->guestScreenCount(); ++cScreenId)
+    for (uint cScreenId = 0; cScreenId < session().GetMachine().GetMonitorCount(); ++cScreenId)
         addMachineWindow(UIMachineWindow::create(this, cScreenId));
 
     /* Connect screen-layout change handler: */
