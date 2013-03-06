bd681c91ff14279f665be3bd3d55f590525e378c
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2013-03-06 17:25:24 +0000

    Additions/linux: recent RedHat-based systems support vboxvideo auto-detection.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@44936 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Additions/linux/installer/vboxadd-x11.sh
+++ src/VBox/Additions/linux/installer/vboxadd-x11.sh
@@ -331,12 +331,12 @@ setup()
         1.13.* )
             xserver_version="X.Org Server 1.13"
             vboxvideo_src=vboxvideo_drv_113.so
-            test "$system" = "redhat" || setupxorgconf=""
+            setupxorgconf=""
             ;;
         1.12.* )
             xserver_version="X.Org Server 1.12"
             vboxvideo_src=vboxvideo_drv_112.so
-            test "$system" = "redhat" || setupxorgconf=""
+            setupxorgconf=""
             ;;
         1.11.* )
             xserver_version="X.Org Server 1.11"
@@ -351,7 +351,7 @@ setup()
         1.9.* )
             xserver_version="X.Org Server 1.9"
             vboxvideo_src=vboxvideo_drv_19.so
-            # Fedora 14 and later patched out vboxvideo detection
+            # Fedora 14 to 16 patched out vboxvideo detection
             test "$system" = "redhat" || setupxorgconf=""
             ;;
         1.8.* )
