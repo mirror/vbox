216f2539d6149ea258e9c6fff5035c6a4a0ae589
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2013-03-20 15:18:21 +0000

    VMM/VMMR0, VMMR3: HM bits, don't lose CR3 and PAE PDPTE updates. Fixes Win7 64-bit to boot.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@45106 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/VMM/VMMR3/EM.cpp
+++ src/VBox/VMM/VMMR3/EM.cpp
@@ -1422,7 +1422,7 @@ int emR3HighPriorityPostForcedActions(PVM pVM, PVMCPU pVCpu, int rc)
     /* Update PAE PDPEs. This must be done *after* PGMUpdateCR3() and used only by the Nested Paging case for HM. */
     if (VMCPU_FF_ISPENDING(pVCpu, VMCPU_FF_HM_UPDATE_PAE_PDPES))
     {
-        Assert(CPUMIsGuestInPAEMode(pVCpu));
+        /* The paging mode might no longer be PAE but we still need to update this, otherwise we lose updates in HM. */
         PX86PDPE pPdpes = HMGetPaePdpes(pVCpu);
         AssertPtr(pPdpes);
 
