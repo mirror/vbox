5d7a4d6fa8f6fd871e7929a4a843b91315365966
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2013-04-03 14:40:35 +0000

    RTCritSectRw: Two fixes.
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@45309 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Runtime/generic/critsectrw-generic.cpp
+++ src/VBox/Runtime/generic/critsectrw-generic.cpp
@@ -407,7 +407,7 @@ RTDECL(int) RTCritSectRwLeaveShared(PRTCRITSECTRW pThis)
             c--;
 
             if (   c > 0
-                || (u64State & RTCSRW_CNT_RD_MASK) == 0)
+                || (u64State & RTCSRW_CNT_WR_MASK) == 0)
             {
                 /* Don't change the direction. */
                 u64State &= ~RTCSRW_CNT_RD_MASK;
@@ -554,21 +554,34 @@ static int rtCritSectRwEnterExcl(PRTCRITSECTRW pThis, PCRTLOCKVALSRCPOS pSrcPos,
     if (!fDone)
     {
         /*
+         * If only trying, undo the above writer incrementation and return.
+         */
+        if (fTryOnly)
+        {
+            for (;;)
+            {
+                u64OldState = u64State = ASMAtomicReadU64(&pThis->u64State);
+                uint64_t c = (u64State & RTCSRW_CNT_WR_MASK) >> RTCSRW_CNT_WR_SHIFT; Assert(c > 0);
+                c--;
+                u64State &= ~RTCSRW_CNT_WR_MASK;
+                u64State |= c << RTCSRW_CNT_WR_SHIFT;
+                if (ASMAtomicCmpXchgU64(&pThis->u64State, u64State, u64OldState))
+                    break;
+            }
+            return VERR_SEM_BUSY;
+        }
+
+        /*
          * Wait for our turn.
          */
         for (uint32_t iLoop = 0; ; iLoop++)
         {
             int rc;
 #ifdef RTCRITSECTRW_STRICT
-            if (!fTryOnly)
-            {
-                if (hThreadSelf == NIL_RTTHREAD)
-                    hThreadSelf = RTThreadSelfAutoAdopt();
-                rc = RTLockValidatorRecExclCheckBlocking(pThis->pValidatorWrite, hThreadSelf, pSrcPos, true,
-                                                         RT_INDEFINITE_WAIT, RTTHREADSTATE_RW_WRITE, false);
-            }
-            else
-                rc = VINF_SUCCESS;
+            if (hThreadSelf == NIL_RTTHREAD)
+                hThreadSelf = RTThreadSelfAutoAdopt();
+            rc = RTLockValidatorRecExclCheckBlocking(pThis->pValidatorWrite, hThreadSelf, pSrcPos, true,
+                                                     RT_INDEFINITE_WAIT, RTTHREADSTATE_RW_WRITE, false);
             if (RT_SUCCESS(rc))
 #else
             RTTHREAD hThreadSelf = RTThreadSelf();
