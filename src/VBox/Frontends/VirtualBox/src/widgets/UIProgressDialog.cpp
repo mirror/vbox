59f857992b7672ff015523ee7a1c497ea088c5e8
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2013-04-04 10:54:15 +0000

    FE/Qt: Progress-dialog: Protect against crash on self-destructing in own event-loop.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@45335 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Frontends/VirtualBox/src/widgets/UIProgressDialog.cpp
+++ src/VBox/Frontends/VirtualBox/src/widgets/UIProgressDialog.cpp
@@ -152,8 +152,19 @@ int UIProgressDialog::run(int cRefreshInterval)
             QApplication::setOverrideCursor(QCursor(Qt::WaitCursor));
 #endif /* Q_WS_MAC */
 
-        /* Enter the modal loop, but don't show the window immediately */
-        exec(false);
+        /* Create a local event-loop: */
+        {
+            /* Guard ourself for the case
+             * we destroyed ourself in our event-loop: */
+            QPointer<UIProgressDialog> guard = this;
+
+            /* Enter the modal loop, but don't show the window immediately: */
+            exec(false);
+
+            /* Are we still valid? */
+            if (guard.isNull())
+                return Rejected;
+        }
 
         /* Kill refresh timer */
         killTimer(id);
