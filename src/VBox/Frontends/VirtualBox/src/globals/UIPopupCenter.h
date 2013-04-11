a1840f8172a42e78ee4ed079a34d030305f222e4
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2013-04-11 17:16:00 +0000

    FE/Qt: Popup-center: Implementing main framework stuff (part 2).
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@45490 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Frontends/VirtualBox/src/globals/UIPopupCenter.h
+++ src/VBox/Frontends/VirtualBox/src/globals/UIPopupCenter.h
@@ -52,9 +52,8 @@ public:
     static void destroy();
 
     /* API: Main message function, used directly only in exceptional cases: */
-    void message(QWidget *pParent,
+    void message(QWidget *pParent, const QString &strId,
                  const QString &strMessage, const QString &strDetails,
-                 const char *pcszAutoConfirmId = 0,
                  int iButton1 = 0, int iButton2 = 0, int iButton3 = 0,
                  const QString &strButtonText1 = QString(),
                  const QString &strButtonText2 = QString(),
@@ -62,21 +61,18 @@ public:
 
     /* API: Wrapper to 'message' function.
      * Provides single OK button: */
-    void error(QWidget *pParent,
-               const QString &strMessage, const QString &strDetails,
-               const char *pcszAutoConfirmId = 0) const;
+    void error(QWidget *pParent, const QString &strId,
+               const QString &strMessage, const QString &strDetails) const;
 
     /* API: Wrapper to 'error' function.
      * Omits details: */
-    void alert(QWidget *pParent,
-               const QString &strMessage,
-               const char *pcszAutoConfirmId = 0) const;
+    void alert(QWidget *pParent, const QString &strId,
+               const QString &strMessage) const;
 
     /* API: Wrapper to 'message' function.
      * Omits details, provides two or three buttons: */
-    void question(QWidget *pParent,
+    void question(QWidget *pParent, const QString &strId,
                   const QString &strMessage,
-                  const char *pcszAutoConfirmId = 0,
                   int iButton1 = 0, int iButton2 = 0, int iButton3 = 0,
                   const QString &strButtonText1 = QString(),
                   const QString &strButtonText2 = QString(),
@@ -84,20 +80,21 @@ public:
 
     /* API: Wrapper to 'question' function,
      * Question providing two buttons (OK and Cancel by default): */
-    void questionBinary(QWidget *pParent,
+    void questionBinary(QWidget *pParent, const QString &strId,
                         const QString &strMessage,
-                        const char *pcszAutoConfirmId = 0,
                         const QString &strOkButtonText = QString(),
                         const QString &strCancelButtonText = QString()) const;
 
     /* API: Wrapper to 'question' function,
      * Question providing three buttons (Yes, No and Cancel by default): */
-    void questionTrinary(QWidget *pParent,
+    void questionTrinary(QWidget *pParent, const QString &strId,
                          const QString &strMessage,
-                         const char *pcszAutoConfirmId = 0,
                          const QString &strChoice1ButtonText = QString(),
                          const QString &strChoice2ButtonText = QString(),
                          const QString &strCancelButtonText = QString()) const;
+
+    /* API: Runtime UI stuff: */
+    void remindAboutMouseIntegration(bool fSupportsAbsolute) const;
 
 private slots:
 
@@ -111,11 +108,10 @@ private:
     ~UIPopupCenter();
 
     /* Helper: Popup-box stuff: */
-    void showPopupBox(QWidget *pParent,
+    void showPopupBox(QWidget *pParent, const QString &strId,
                       const QString &strMessage, const QString &strDetails,
                       int iButton1, int iButton2, int iButton3,
-                      const QString &strButtonText1, const QString &strButtonText2, const QString &strButtonText3,
-                      const QString &strAutoConfirmId) const;
+                      const QString &strButtonText1, const QString &strButtonText2, const QString &strButtonText3) const;
 
     /* Variables: */
     mutable QMap<QString, QPointer<UIPopupPane> > m_popups;
@@ -160,23 +156,30 @@ private slots:
 
 private:
 
+    /* Helpers: Prepare/cleanup stuff: */
+    void prepare();
+    void cleanup();
+
     /* Handler: Event-filter stuff: */
     bool eventFilter(QObject *pWatched, QEvent *pEvent);
 
     /* Handlers: Event stuff: */
     virtual void showEvent(QShowEvent *pEvent);
     virtual void polishEvent(QShowEvent *pEvent);
+    virtual void keyPressEvent(QKeyEvent *pEvent);
 
-    /* Helpers: Move/resize stuff: */
-    void moveAccordingParent();
-    void resizeAccordingParent();
+    /* Helper: Adjust stuff: */
+    void adjustAccordingParent();
 
     /* Helpers: Prepare stuff: */
     void prepareContent();
     QPushButton* createButton(int iButton);
 
-    /* Helper: */
+    /* Helper: Complete stuff: */
     void done(int iButtonCode);
+
+    /* Helper: Parent stuff: */
+    static int parentStatusBarHeight(QWidget *pParent);
 
     /* Variables: */
     bool m_fPolished;
@@ -184,12 +187,60 @@ private:
     QString m_strMessage, m_strDetails;
     int m_iButton1, m_iButton2, m_iButton3;
     QString m_strButtonText1, m_strButtonText2, m_strButtonText3;
+    int m_iButtonEsc;
+    const int m_iParentStatusBarHeight;
 
     /* Widgets: */
     QVBoxLayout *m_pMainLayout;
+    QVBoxLayout *m_pFrameLayout;
     QIRichTextLabel *m_pTextPane;
-    QPushButton *m_pButton1, *m_pButton2, *m_pButton3;
     QIDialogButtonBox *m_pButtonBox;
+    QPushButton *m_pButton1, *m_pButton2, *m_pButton3;
+};
+
+/* Popup-pane frame prototype class: */
+class UIPopupPaneFrame : public QWidget
+{
+    Q_OBJECT;
+    Q_PROPERTY(int opacity READ opacity WRITE setOpacity);
+
+signals:
+
+    /* Notifiers: Hover-machine stuff: */
+    void sigHoverEnter();
+    void sigHoverLeave();
+
+public:
+
+    /* Constructor/destructor: */
+    UIPopupPaneFrame(QWidget *pParent = 0);
+    ~UIPopupPaneFrame();
+
+private:
+
+    /* Helpers: Prepare/cleanup stuff: */
+    void prepare();
+    void cleanup();
+
+    /* Handlers: Event stuff: */
+    bool eventFilter(QObject *pWatched, QEvent *pEvent);
+    void paintEvent(QPaintEvent *pEvent);
+
+    /* Property: Hover-machine stuff: */
+    int opacity() const { return m_iOpacity; }
+    void setOpacity(int iOpacity) { m_iOpacity = iOpacity; update(); }
+
+    /* Static helper: Animation stuff: */
+    static void installPropertyAnimation(QWidget *pParent, const QByteArray &strPropertyName,
+                                         int iStartValue, int iFinalValue, int iAnimationDuration,
+                                         const char *pSignalForward, const char *pSignalBackward);
+
+    /* Hover-machine stuff: */
+    bool m_fHovered;
+    int m_iDefaultOpacity;
+    int m_iHoveredOpacity;
+    int m_iOpacity;
+    int m_iHoverAnimationDuration;
 };
 
 #endif /* __UIPopupCenter_h__ */
