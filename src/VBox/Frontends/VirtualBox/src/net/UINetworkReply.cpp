4e7b3c018cd7d2088d881146abfe6702631f7248
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2013-04-16 12:53:47 +0000

    FE/Qt: Fixing compile warning in r85060.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@45570 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Frontends/VirtualBox/src/net/UINetworkReply.cpp
+++ src/VBox/Frontends/VirtualBox/src/net/UINetworkReply.cpp
@@ -381,13 +381,13 @@ int UINetworkReplyPrivateThread::downloadCertificatePca3(RTHTTP pHttp, QFile &fi
 int UINetworkReplyPrivateThread::verifyCertificatePca3G5(RTHTTP pHttp, QByteArray &certificate)
 {
     /* PCA 3G5 secure hash algorithm 1: */
-    const char baSha1PCA3G5[] =
+    const unsigned char baSha1PCA3G5[] =
     {
         0x4e, 0xb6, 0xd5, 0x78, 0x49, 0x9b, 0x1c, 0xcf, 0x5f, 0x58,
         0x1e, 0xad, 0x56, 0xbe, 0x3d, 0x9b, 0x67, 0x44, 0xa5, 0xe5
     };
     /* PCA 3G5 secure hash algorithm 512: */
-    const char baSha512PCA3G5[] =
+    const unsigned char baSha512PCA3G5[] =
     {
         0xd4, 0xf8, 0x10, 0x54, 0x72, 0x77, 0x0a, 0x2d,
         0xe3, 0x17, 0xb3, 0xcf, 0xed, 0x61, 0xae, 0x5c,
@@ -398,8 +398,8 @@ int UINetworkReplyPrivateThread::verifyCertificatePca3G5(RTHTTP pHttp, QByteArra
         0x44, 0xeb, 0x2c, 0x74, 0x25, 0x9e, 0x5d, 0xfb,
         0xd2, 0x6b, 0xa8, 0x9a, 0xf0, 0xb3, 0x6a, 0x01
     };
-    QByteArray pca3G5sha1 = QByteArray::fromRawData(baSha1PCA3G5, sizeof(baSha1PCA3G5));
-    QByteArray pca3G5sha512 = QByteArray::fromRawData(baSha512PCA3G5, sizeof(baSha512PCA3G5));
+    QByteArray pca3G5sha1 = QByteArray::fromRawData((const char *)baSha1PCA3G5, sizeof(baSha1PCA3G5));
+    QByteArray pca3G5sha512 = QByteArray::fromRawData((const char *)baSha512PCA3G5, sizeof(baSha512PCA3G5));
 
     /* Verify certificate: */
     return verifyCertificate(pHttp, certificate, pca3G5sha1, pca3G5sha512);
@@ -409,13 +409,13 @@ int UINetworkReplyPrivateThread::verifyCertificatePca3G5(RTHTTP pHttp, QByteArra
 int UINetworkReplyPrivateThread::verifyCertificatePca3(RTHTTP pHttp, QByteArray &certificate)
 {
     /* PCA 3 secure hash algorithm 1: */
-    const char baSha1PCA3[] =
+    const unsigned char baSha1PCA3[] =
     {
         0xa1, 0xdb, 0x63, 0x93, 0x91, 0x6f, 0x17, 0xe4, 0x18, 0x55,
         0x09, 0x40, 0x04, 0x15, 0xc7, 0x02, 0x40, 0xb0, 0xae, 0x6b
     };
     /* PCA 3 secure hash algorithm 512: */
-    const char baSha512PCA3[] =
+    const unsigned char baSha512PCA3[] =
     {
         0xbb, 0xf7, 0x8a, 0x19, 0x9f, 0x37, 0xee, 0xa2,
         0xce, 0xc8, 0xaf, 0xe3, 0xd6, 0x22, 0x54, 0x20,
@@ -426,8 +426,8 @@ int UINetworkReplyPrivateThread::verifyCertificatePca3(RTHTTP pHttp, QByteArray 
         0x77, 0xa5, 0x40, 0x22, 0x18, 0x12, 0xcb, 0x3d,
         0x0a, 0x67, 0x83, 0x87, 0xc5, 0x45, 0xc4, 0x99
     };
-    QByteArray pca3sha1 = QByteArray::fromRawData(baSha1PCA3, sizeof(baSha1PCA3));
-    QByteArray pca3sha512 = QByteArray::fromRawData(baSha512PCA3, sizeof(baSha512PCA3));
+    QByteArray pca3sha1 = QByteArray::fromRawData((const char *)baSha1PCA3, sizeof(baSha1PCA3));
+    QByteArray pca3sha512 = QByteArray::fromRawData((const char *)baSha512PCA3, sizeof(baSha512PCA3));
 
     /* Verify certificate: */
     return verifyCertificate(pHttp, certificate, pca3sha1, pca3sha512);
