102b4f9fe31235687c0ba907f21e2eb8ab67dcb5
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2013-04-16 15:01:53 +0000

    FE/Qt: Tune r85065 to Win host.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@45574 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Frontends/VirtualBox/src/net/UINetworkReply.cpp
+++ src/VBox/Frontends/VirtualBox/src/net/UINetworkReply.cpp
@@ -289,8 +289,13 @@ int UINetworkReplyPrivateThread::checkCertificates(RTHTTP pHttp, const QString &
     {
         /* Parse the file content: */
         QString strData(file.readAll());
+#ifdef Q_WS_WIN
+        QRegExp regExp("(-{5}BEGIN CERTIFICATE-{5}[\\s\\S\\r\\n]+-{5}END CERTIFICATE-{5})\\r\\n"
+                       "(-{5}BEGIN CERTIFICATE-{5}[\\s\\S\\r\\n]+-{5}END CERTIFICATE-{5})");
+#else /* Q_WS_WIN */
         QRegExp regExp("(-{5}BEGIN CERTIFICATE-{5}[\\s\\S\\n]+-{5}END CERTIFICATE-{5})\\n"
                        "(-{5}BEGIN CERTIFICATE-{5}[\\s\\S\\n]+-{5}END CERTIFICATE-{5})");
+#endif /* !Q_WS_WIN */
         regExp.setMinimal(true);
         int iIndex = regExp.indexIn(strData);
         if (iIndex == -1)
@@ -487,7 +492,11 @@ int UINetworkReplyPrivateThread::saveCertificate(QFile &file, const QByteArray &
 
     /* Add 'new-line' character: */
     if (RT_SUCCESS(rc))
+#ifdef Q_WS_WIN
+        rc = file.write("\r\n") != -1 ? VINF_SUCCESS : VERR_WRITE_ERROR;
+#else /* Q_WS_WIN */
         rc = file.write("\n") != -1 ? VINF_SUCCESS : VERR_WRITE_ERROR;
+#endif /* !Q_WS_WIN */
 
     /* Return result-code: */
     return rc;
