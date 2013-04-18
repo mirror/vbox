bdd93c74094518bad35ada3b5903b5de4bcb743d
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2013-04-18 10:11:03 +0000

    Additions/linux/drm: better distinction betwen Debian's 3.2.0-4 kernel with drm backported from Linux 3.4 against other kernels like Ubuntu 3.2.0-40
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@45603 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Additions/linux/drm/vboxvideo_drm.c
+++ src/VBox/Additions/linux/drm/vboxvideo_drm.c
@@ -81,7 +81,7 @@
 
 # ifndef RHEL_RELEASE_CODE
 #  if LINUX_VERSION_CODE >= KERNEL_VERSION(3, 2, 39) && LINUX_VERSION_CODE < KERNEL_VERSION(3, 3, 0)
-#   ifdef DRM_SWITCH_POWER_ON
+#   if defined(DRM_MODE_OBJECT_PLANE) && defined(DRM_IOCTL_MODE_ADDFB2)
 #    define DRM_DEBIAN_34ON32
 #   endif
 #  endif
