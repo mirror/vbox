f99a8f0f831db07352c5d285da8c40e678c42583
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2013-04-19 08:22:59 +0000

    rtDirOpenCommon: We don't need to resolve symbolic links in the path, just making it absolute. Speeds things up on windows (ntBldSymDb 1.9x times faster).
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@45630 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Runtime/r3/dir.cpp
+++ src/VBox/Runtime/r3/dir.cpp
@@ -533,7 +533,7 @@ static int rtDirOpenCommon(PRTDIR *ppDir, const char *pszPath, const char *pszFi
     if (!pszFilter)
     {
         cbFilter = cucFilter0 = 0;
-        rc = RTPathReal(pszPath, szRealPath, sizeof(szRealPath) - 1);
+        rc = RTPathAbs(pszPath, szRealPath, sizeof(szRealPath) - 1);
     }
     else
     {
@@ -547,7 +547,7 @@ static int rtDirOpenCommon(PRTDIR *ppDir, const char *pszPath, const char *pszFi
             if (!pszTmp)
                 return VERR_NO_MEMORY;
             pszTmp[pszFilter - pszPath] = '\0';
-            rc = RTPathReal(pszTmp, szRealPath, sizeof(szRealPath) - 1);
+            rc = RTPathAbs(pszTmp, szRealPath, sizeof(szRealPath) - 1);
             RTStrFree(pszTmp);
         }
         else
