b15113c66ec99b2f4ecd5c7f9d7e79d7e85ba043
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2013-04-23 10:26:14 +0000

    VBoxTray/VBoxSeamless: Fix for the issue - A small rectangular bar appears over the start button for Vista and above in seamless mode when mouse is hovered over the start button.. Solution: There is difference in the rectangle size of "Start" button and its shadow as reported by the API GetWindowRect. So , adjusting the size of the "Start" button to match the shadow.
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@45676 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Additions/WINNT/VBoxTray/VBoxSeamless.cpp
+++ src/VBox/Additions/WINNT/VBoxTray/VBoxSeamless.cpp
@@ -154,11 +154,29 @@ BOOL CALLBACK VBoxEnumFunc(HWND hwnd, LPARAM lParam)
     /* Only visible windows that are present on the desktop are interesting here */
     if (GetWindowRect(hwnd, &rectWindow))
     {
-        rectVisible = rectWindow;
-
         char szWindowText[256];
         szWindowText[0] = 0;
+        OSVERSIONINFO OSinfo;
+        HWND hStart = NULL;
         GetWindowText(hwnd, szWindowText, sizeof(szWindowText));
+        OSinfo.dwOSVersionInfoSize = sizeof (OSinfo);
+        GetVersionEx (&OSinfo);
+        if (OSinfo.dwMajorVersion >= 6)
+        {
+            hStart = ::FindWindowEx(GetDesktopWindow(), NULL, "Button", "Start");
+            if (  hwnd == hStart && szWindowText != NULL
+                && !(strcmp(szWindowText, "Start"))
+               )
+            {
+                /* for vista and above. To solve the issue of small bar above
+                 * the Start button when mouse is hovered over the start button in seamless mode.
+                 * Difference of 7 is observed in Win 7 platform between the dimensionsof rectangle with Start title and its shadow.
+                 */
+                rectWindow.top += 7;
+                rectWindow.bottom -=7;
+            }
+        }
+        rectVisible = rectWindow;
 
 #ifdef LOG_ENABLED
         DWORD pid = 0;
