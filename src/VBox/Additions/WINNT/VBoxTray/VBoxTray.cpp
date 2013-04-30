92125beed67ced6107332945c7cf23cee2b3f2d4
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2013-04-30 17:25:37 +0000

    VBoxTray/seamless: RDS check timer interval
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@45847 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Additions/WINNT/VBoxTray/VBoxTray.cpp
+++ src/VBox/Additions/WINNT/VBoxTray/VBoxTray.cpp
@@ -1089,7 +1089,7 @@ static int vboxStInit(HWND hWnd)
                 WARN(("VBoxTray: WTSRegisterSessionNotification failed, error = %08X\n", dwErr));
                 if (dwErr == RPC_S_INVALID_BINDING)
                 {
-                    gVBoxSt.idDelayedInitTimer = SetTimer(gVBoxSt.hWTSAPIWnd, TIMERID_VBOXTRAY_ST_DELAYED_INIT_TIMER, 500, (TIMERPROC)NULL);
+                    gVBoxSt.idDelayedInitTimer = SetTimer(gVBoxSt.hWTSAPIWnd, TIMERID_VBOXTRAY_ST_DELAYED_INIT_TIMER, 2000, (TIMERPROC)NULL);
                     rc = VINF_SUCCESS;
                 }
                 else
