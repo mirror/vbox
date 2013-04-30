5b269bf588178cb7d6e1577625aea065f07dbeb6
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2013-04-30 11:56:26 +0000

    Spelling.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@45836 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/VMM/VMMAll/PGMAllBth.h
+++ src/VBox/VMM/VMMAll/PGMAllBth.h
@@ -991,7 +991,7 @@ PGM_BTH_DECL(int, Trap0eHandler)(PVMCPU pVCpu, RTGCUINT uErr, PCPUMCTXCORE pRegF
                  * The Netware WP0+RO+US hack.
                  *
                  * Netware sometimes(/always?) runs with WP0.  It has been observed doing
-                 * accessive write accesses to pages which are mapped with US=1 and RW=0
+                 * excessive write accesses to pages which are mapped with US=1 and RW=0
                  * while WP=0.  This causes a lot of exits and extremely slow execution.
                  * To avoid trapping and emulating every write here, we change the shadow
                  * page table entry to map it as US=0 and RW=1 until user mode tries to
