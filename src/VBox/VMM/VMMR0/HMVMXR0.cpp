203f5cbe69ab5fad568ac194c5c560aef408e4a4
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2013-05-07 14:53:55 +0000

    VMM/HMVMXR0: Easier to read when reading huge logs.
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@45937 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/VMM/VMMR0/HMVMXR0.cpp
+++ src/VBox/VMM/VMMR0/HMVMXR0.cpp
@@ -5595,7 +5595,7 @@ static void hmR0VmxTRPMTrapToPendingEvent(PVMCPU pVCpu)
 
     rc = TRPMResetTrap(pVCpu);
     AssertRC(rc);
-    Log(("Converting TRPM trap: u32IntrInfo=%#RX32 enmTrpmEvent=%d cbInstr=%u uErrCode=%#RX32 GCPtrFaultAddress=%#RGv\n",
+    Log(("TRPM->HM event: u32IntrInfo=%#RX32 enmTrpmEvent=%d cbInstr=%u uErrCode=%#RX32 GCPtrFaultAddress=%#RGv\n",
          u32IntrInfo, enmTrpmEvent, cbInstr, uErrCode, GCPtrFaultAddress));
     hmR0VmxSetPendingEvent(pVCpu, u32IntrInfo, cbInstr, uErrCode, GCPtrFaultAddress);
 }
@@ -5640,7 +5640,7 @@ static void hmR0VmxPendingEventToTRPMTrap(PVMCPU pVCpu)
             break;
     }
 
-    Log(("Converting pending HM event to TRPM trap uVector=%#x enmTrapType=%d\n", uVector, enmTrapType));
+    Log(("HM event->TRPM: uVector=%#x enmTrapType=%d\n", uVector, enmTrapType));
     int rc = TRPMAssertTrap(pVCpu, uVector, enmTrapType);
     AssertRC(rc);
 
