de6769351ba478d0a6cc591598a72e6ba0c8a37e
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2013-05-23 15:16:35 +0000

    FE/Qt: Animation framework rework.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@46242 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Frontends/VirtualBox/src/globals/UIAnimationFramework.cpp
+++ src/VBox/Frontends/VirtualBox/src/globals/UIAnimationFramework.cpp
@@ -26,42 +26,73 @@
 /* GUI includes: */
 #include "UIAnimationFramework.h"
 
-QStateMachine* UIAnimationFramework::installPropertyAnimation(QWidget *pTarget, const char *pszPropertyName,
-                                                              const char *pszValuePropertyNameStart, const char *pszValuePropertyNameFinal,
-                                                              const char *pSignalForward, const char *pSignalBackward,
-                                                              bool fReversive /*= false*/, int iAnimationDuration /*= 300*/)
+/* static */
+UIAnimation* UIAnimation::installPropertyAnimation(QWidget *pTarget, const char *pszPropertyName,
+                                                   const char *pszValuePropertyNameStart, const char *pszValuePropertyNameFinal,
+                                                   const char *pszSignalForward, const char *pszSignalReverse,
+                                                   bool fReverse /*= false*/, int iAnimationDuration /*= 300*/)
 {
-    /* State-machine: */
-    QStateMachine *pStateMachine = new QStateMachine(pTarget);
-    /* State-machine 'start' state: */
-    QState *pStateStart = new QState(pStateMachine);
-    /* State-machine 'final' state: */
-    QState *pStateFinal = new QState(pStateMachine);
+    /* Return newly created animation-machine: */
+    return new UIAnimation(pTarget, pszPropertyName,
+                           pszValuePropertyNameStart, pszValuePropertyNameFinal,
+                           pszSignalForward, pszSignalReverse,
+                           fReverse, iAnimationDuration);
+}
 
-    /* State-machine 'forward' animation: */
-    QPropertyAnimation *pForwardAnimation = new QPropertyAnimation(pTarget, pszPropertyName, pStateMachine);
-    pForwardAnimation->setEasingCurve(QEasingCurve(QEasingCurve::InOutCubic));
-    pForwardAnimation->setDuration(iAnimationDuration);
-    pForwardAnimation->setStartValue(pTarget->property(pszValuePropertyNameStart));
-    pForwardAnimation->setEndValue(pTarget->property(pszValuePropertyNameFinal));
-    /* State-machine 'backward' animation: */
-    QPropertyAnimation *pBackwardAnimation = new QPropertyAnimation(pTarget, pszPropertyName, pStateMachine);
-    pBackwardAnimation->setEasingCurve(QEasingCurve(QEasingCurve::InOutCubic));
-    pBackwardAnimation->setDuration(iAnimationDuration);
-    pBackwardAnimation->setStartValue(pTarget->property(pszValuePropertyNameFinal));
-    pBackwardAnimation->setEndValue(pTarget->property(pszValuePropertyNameStart));
+UIAnimation::UIAnimation(QWidget *pParent, const char *pszPropertyName,
+                         const char *pszValuePropertyNameStart, const char *pszValuePropertyNameFinal,
+                         const char *pszSignalForward, const char *pszSignalReverse,
+                         bool fReverse, int iAnimationDuration)
+    : QObject(pParent)
+    , m_pszPropertyName(pszPropertyName)
+    , m_pszValuePropertyNameStart(pszValuePropertyNameStart), m_pszValuePropertyNameFinal(pszValuePropertyNameFinal)
+    , m_pszSignalForward(pszSignalForward), m_pszSignalReverse(pszSignalReverse)
+    , m_fReverse(fReverse), m_iAnimationDuration(iAnimationDuration)
+    , m_pAnimationMachine(0), m_pForwardAnimation(0), m_pReverseAnimation(0)
+{
+    /* Prepare: */
+    prepare();
+}
 
-    /* State-machine state transitions: */
-    QSignalTransition *pDefaultToHovered = pStateStart->addTransition(pTarget, pSignalForward, pStateFinal);
-    pDefaultToHovered->addAnimation(pForwardAnimation);
-    QSignalTransition *pHoveredToDefault = pStateFinal->addTransition(pTarget, pSignalBackward, pStateStart);
-    pHoveredToDefault->addAnimation(pBackwardAnimation);
+void UIAnimation::prepare()
+{
+    /* Prepare animation-machine: */
+    m_pAnimationMachine = new QStateMachine(this);
+    /* Create 'start' and 'final' states: */
+    QState *pStateStart = new QState(m_pAnimationMachine);
+    QState *pStateFinal = new QState(m_pAnimationMachine);
 
-    /* Initial state is 'start': */
-    pStateMachine->setInitialState(!fReversive ? pStateStart : pStateFinal);
-    /* Start hover-machine: */
-    pStateMachine->start();
-    /* Return machine: */
-    return pStateMachine;
+    /* Prepare 'forward' animation: */
+    m_pForwardAnimation = new QPropertyAnimation(parent(), m_pszPropertyName, m_pAnimationMachine);
+    m_pForwardAnimation->setEasingCurve(QEasingCurve(QEasingCurve::InOutCubic));
+    m_pForwardAnimation->setDuration(m_iAnimationDuration);
+    /* Prepare 'reverse' animation: */
+    m_pReverseAnimation = new QPropertyAnimation(parent(), m_pszPropertyName, m_pAnimationMachine);
+    m_pReverseAnimation->setEasingCurve(QEasingCurve(QEasingCurve::InOutCubic));
+    m_pReverseAnimation->setDuration(m_iAnimationDuration);
+
+    /* Prepare state-transitions: */
+    QSignalTransition *pStartToFinal = pStateStart->addTransition(parent(), m_pszSignalForward, pStateFinal);
+    pStartToFinal->addAnimation(m_pForwardAnimation);
+    QSignalTransition *pFinalToStart = pStateFinal->addTransition(parent(), m_pszSignalReverse, pStateStart);
+    pFinalToStart->addAnimation(m_pReverseAnimation);
+
+    /* Fetch animation-borders: */
+    update();
+
+    /* Choose initial state: */
+    m_pAnimationMachine->setInitialState(!m_fReverse ? pStateStart : pStateFinal);
+    /* Start animation-machine: */
+    m_pAnimationMachine->start();
+}
+
+void UIAnimation::update()
+{
+    /* Update 'forward' animation: */
+    m_pForwardAnimation->setStartValue(parent()->property(m_pszValuePropertyNameStart));
+    m_pForwardAnimation->setEndValue(parent()->property(m_pszValuePropertyNameFinal));
+    /* Update 'reverse' animation: */
+    m_pReverseAnimation->setStartValue(parent()->property(m_pszValuePropertyNameFinal));
+    m_pReverseAnimation->setEndValue(parent()->property(m_pszValuePropertyNameStart));
 }
 
