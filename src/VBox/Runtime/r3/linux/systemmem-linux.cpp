fb1dd4688183836ad65c019fc10ba6f2ff1fe477
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2013-05-30 12:03:16 +0000

    Runtime/linux/RTSystemQueryAvailableRam: also consider the cached memory
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@46324 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Runtime/r3/linux/systemmem-linux.cpp
+++ src/VBox/Runtime/r3/linux/systemmem-linux.cpp
@@ -33,7 +33,9 @@
 
 #include <iprt/err.h>
 #include <iprt/assert.h>
+#include <iprt/string.h>
 
+#include <stdio.h>
 #include <errno.h>
 
 /* Satisfy compiller warning */
@@ -61,12 +63,44 @@ RTDECL(int) RTSystemQueryAvailableRam(uint64_t *pcb)
 {
     AssertPtrReturn(pcb, VERR_INVALID_POINTER);
 
+    FILE *pFile = fopen("/proc/meminfo", "r");
+    if (pFile)
+    {
+        int rc = VERR_NOT_FOUND;
+        uint64_t cbTotal = 0;
+        uint64_t cbFree = 0;
+        uint64_t cbBuffers = 0;
+        uint64_t cbCached = 0;
+        char sz[256];
+        while (fgets(sz, sizeof(sz), pFile))
+        {
+            if (!strncmp(sz, RT_STR_TUPLE("MemTotal:")))
+                rc = RTStrToUInt64Ex(RTStrStripL(&sz[sizeof("MemTotal:")]), NULL, 0, &cbTotal);
+            else if (!strncmp(sz, RT_STR_TUPLE("MemFree:")))
+                rc = RTStrToUInt64Ex(RTStrStripL(&sz[sizeof("MemFree:")]), NULL, 0, &cbFree);
+            else if (!strncmp(sz, RT_STR_TUPLE("Buffers:")))
+                rc = RTStrToUInt64Ex(RTStrStripL(&sz[sizeof("Buffers:")]), NULL, 0, &cbBuffers);
+            else if (!strncmp(sz, RT_STR_TUPLE("Cached:")))
+                rc = RTStrToUInt64Ex(RTStrStripL(&sz[sizeof("Cached:")]), NULL, 0, &cbCached);
+            if (RT_FAILURE(rc))
+                break;
+        }
+        fclose(pFile);
+        if (RT_SUCCESS(rc))
+        {
+            *pcb = (cbFree + cbBuffers + cbCached) * _1K;
+            return VINF_SUCCESS;
+        }
+    }
+    /*
+     * Fallback (e.g. /proc not mapped) to sysinfo. Less accurat because there
+     * is no information about the cached memory. 'Cached:' from above is only
+     * accessible through proc :-(
+     */
     struct sysinfo info;
     int rc = sysinfo(&info);
     if (rc == 0)
     {
-        /** @todo Actually this is not quite correct. We would also need to add the
-         * cached RAM but this information is not available in sysinfo. */
         *pcb = ((uint64_t)info.freeram + info.bufferram) * info.mem_unit;
         return VINF_SUCCESS;
     }
