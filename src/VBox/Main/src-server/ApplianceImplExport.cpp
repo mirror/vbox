43a029f05baca747204eb0009bb90c0e819d2b70
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2013-05-31 09:53:13 +0000

    Main/OVF: added a sanity check for the size of OVF filenames to prevent creating invalid appliances if the VM name is too long
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@46337 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Main/src-server/ApplianceImplExport.cpp
+++ src/VBox/Main/src-server/ApplianceImplExport.cpp
@@ -323,8 +323,8 @@ STDMETHODIMP Machine::ExportTo(IAppliance *aAppliance, IN_BSTR location, IVirtua
             Utf8Str strLocation;
             LONG64  llSize = 0;
 
-            if (deviceType == DeviceType_HardDisk
-                 && pMedium)
+            if (   deviceType == DeviceType_HardDisk
+                && pMedium)
             {
                 Bstr bstrLocation;
                 rc = pMedium->COMGETTER(Location)(bstrLocation.asOutParam());
@@ -347,6 +347,9 @@ STDMETHODIMP Machine::ExportTo(IAppliance *aAppliance, IN_BSTR location, IVirtua
 
                 Utf8Str strTargetName = Utf8Str(locInfo.strPath).stripPath().stripExt();
                 strTargetVmdkName = Utf8StrFmt("%s-disk%d.vmdk", strTargetName.c_str(), ++pAppliance->m->cDisks);
+                if (strTargetVmdkName.length() > RTTAR_NAME_MAX)
+                    throw setError(VBOX_E_NOT_SUPPORTED,
+                                tr("Cannot attach disk '%s' -- file name too long"), strTargetVmdkName.c_str());
 
                 // force reading state, or else size will be returned as 0
                 MediumState_T ms;
@@ -356,8 +359,8 @@ STDMETHODIMP Machine::ExportTo(IAppliance *aAppliance, IN_BSTR location, IVirtua
                 rc = pBaseMedium->COMGETTER(Size)(&llSize);
                 if (FAILED(rc)) throw rc;
             }
-            else if (deviceType == DeviceType_DVD
-                      && pMedium)
+            else if (   deviceType == DeviceType_DVD
+                     && pMedium)
             {
                 Bstr bstrLocation;
                 rc = pMedium->COMGETTER(Location)(bstrLocation.asOutParam());
@@ -380,6 +383,9 @@ STDMETHODIMP Machine::ExportTo(IAppliance *aAppliance, IN_BSTR location, IVirtua
 
                 Utf8Str strTargetName = Utf8Str(locInfo.strPath).stripPath().stripExt();
                 strTargetVmdkName = Utf8StrFmt("%s-disk%d.iso", strTargetName.c_str(), ++pAppliance->m->cDisks);
+                if (strTargetVmdkName.length() > RTTAR_NAME_MAX)
+                    throw setError(VBOX_E_NOT_SUPPORTED,
+                                tr("Cannot attach image '%s' -- file name too long"), strTargetVmdkName.c_str());
 
                 // force reading state, or else size will be returned as 0
                 MediumState_T ms;
