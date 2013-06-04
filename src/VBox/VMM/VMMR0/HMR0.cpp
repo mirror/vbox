2195572b939080f66dc12cd51e49fcf96a16583b
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2013-06-04 13:01:04 +0000

    VMM/HMVMXR0: Added VMFUNC feature recognition.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@46379 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/VMM/VMMR0/HMR0.cpp
+++ src/VBox/VMM/VMMR0/HMR0.cpp
@@ -135,6 +135,7 @@ static struct
             uint64_t                vmx_cr4_fixed0;
             uint64_t                vmx_cr4_fixed1;
             uint64_t                vmx_vmcs_enum;
+            uint64_t                vmx_vmfunc;
             uint64_t                vmx_ept_vpid_caps;
         } msr;
         /* Last instruction error */
@@ -428,6 +429,9 @@ static int hmR0InitIntel(uint32_t u32FeaturesECX, uint32_t u32FeaturesEDX)
                     {
                         g_HvmR0.vmx.msr.vmx_ept_vpid_caps = ASMRdMsr(MSR_IA32_VMX_EPT_VPID_CAP);
                     }
+
+                    if (g_HvmR0.vmx.msr.vmx_proc_ctls2.n.allowed1 & VMX_VMCS_CTRL_PROC_EXEC2_VMFUNC)
+                        g_HvmR0.vmx.msr.vmx_vmfunc        = ASMRdMsr(MSR_IA32_VMX_VMFUNC);
                 }
 
                 if (!g_HvmR0.vmx.fUsingSUPR0EnableVTx)
@@ -1252,6 +1256,7 @@ VMMR0_INT_DECL(int) HMR0InitVM(PVM pVM)
     pVM->hm.s.vmx.msr.vmx_cr4_fixed0    = g_HvmR0.vmx.msr.vmx_cr4_fixed0;
     pVM->hm.s.vmx.msr.vmx_cr4_fixed1    = g_HvmR0.vmx.msr.vmx_cr4_fixed1;
     pVM->hm.s.vmx.msr.vmx_vmcs_enum     = g_HvmR0.vmx.msr.vmx_vmcs_enum;
+    pVM->hm.s.vmx.msr.vmx_vmfunc        = g_HvmR0.vmx.msr.vmx_vmfunc;
     pVM->hm.s.vmx.msr.vmx_ept_vpid_caps = g_HvmR0.vmx.msr.vmx_ept_vpid_caps;
     pVM->hm.s.svm.msrHwcr               = g_HvmR0.svm.msrHwcr;
     pVM->hm.s.svm.u32Rev                = g_HvmR0.svm.u32Rev;
