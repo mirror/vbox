d2d4e09a7e195de37a5fa3a7e17320b29f4d4146
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2013-06-07 08:26:18 +0000

    VBoxTray/VBoxGuest: better caps tracking logging
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@46428 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Additions/common/VBoxGuest/VBoxGuest.cpp
+++ src/VBox/Additions/common/VBoxGuest/VBoxGuest.cpp
@@ -2543,6 +2543,8 @@ static int VBoxGuestCommonGuestCapsAcquire(PVBOXGUESTDEVEXT pDevExt, PVBOXGUESTS
 {
     uint32_t fSetCaps = 0;
 
+    LogRel(("VBoxGuest: VBoxGuestCommonGuestCapsAcquire: pSession(0x%p), OR(0x%x), NOT(0x%x), flags(0x%x)\n", pSession, fOrMask, fNotMask, enmFlags));
+
     if (!VBoxGuestCommonGuestCapsValidateValues(fOrMask))
     {
         LogRel(("invalid fOrMask 0x%x\n", fOrMask));
@@ -2617,6 +2619,8 @@ static int VBoxGuestCommonGuestCapsAcquire(PVBOXGUESTDEVEXT pDevExt, PVBOXGUESTS
     int rc = VBoxGuestSetGuestCapabilities(fSessionOrCaps, fSessionNotCaps);
     if (!RT_SUCCESS(rc))
     {
+        LogRel(("VBoxGuest: VBoxGuestCommonGuestCapsAcquire: VBoxGuestSetGuestCapabilities failed, rc %d\n", rc));
+
         /* Failure branch
          * this is generally bad since e.g. failure to release the caps may result in other sessions not being able to use it
          * so we are not trying to restore the caps back to their values before the VBoxGuestCommonGuestCapsAcquire call,
