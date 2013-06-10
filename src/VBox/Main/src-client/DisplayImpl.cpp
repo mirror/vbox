dd4049a29c7e392161f8f924b6a8bc986884c714
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2013-06-10 15:19:26 +0000

    build fix
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@46471 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Main/src-client/DisplayImpl.cpp
+++ src/VBox/Main/src-client/DisplayImpl.cpp
@@ -2676,9 +2676,9 @@ STDMETHODIMP Display::TakeScreenShotPNGToArray(ULONG aScreenId, ULONG width, ULO
 STDMETHODIMP Display::EnableVideoCaptureScreens(ComSafeArrayIn(BOOL, aScreens))
 {
 #ifdef VBOX_WITH_VPX
-    com::SafeArray<LONG> Screens(ComSafeArrayInArg(aScreens));
+    com::SafeArray<BOOL> Screens(ComSafeArrayInArg(aScreens));
     for (unsigned i = 0; i < Screens.size(); i++)
-        maVideoRecEnabled[i] = aScreens[i] ? true : false;
+        maVideoRecEnabled[i] = Screens[i];
     return S_OK;
 #else
     return E_NOTIMPL;
