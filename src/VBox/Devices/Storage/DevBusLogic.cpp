ef0c8d0696a5f551b8185a5e6de60db895d4e5ca
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2013-06-11 16:02:55 +0000

    Improved logging/debug output.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@46501 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Devices/Storage/DevBusLogic.cpp
+++ src/VBox/Devices/Storage/DevBusLogic.cpp
@@ -1015,7 +1015,8 @@ static void buslogicSetInterrupt(PBUSLOGIC pBusLogic, bool fSuppressIrq, uint8_t
  */
 static void buslogicClearInterrupt(PBUSLOGIC pBusLogic)
 {
-    LogFlowFunc(("pBusLogic=%#p\n", pBusLogic));
+    LogFlowFunc(("pBusLogic=%#p, clearing %#02x (pending %#02x)\n", 
+                 pBusLogic, pBusLogic->regInterrupt, pBusLogic->uPendingIntr));
     pBusLogic->regInterrupt = 0;
     PDMDevHlpPCISetIrq(pBusLogic->CTX_SUFF(pDevIns), 0, 0);
     /* If there's another pending interrupt, report it now. */
@@ -3486,7 +3487,7 @@ static DECLCALLBACK(void) buslogicR3Info(PPDMDEVINS pDevIns, PCDBGFINFOHLP pHlp,
             }
 
             /* Incoming mailbox, 32-bit format. */
-            GCMailbox = pThis->GCPhysAddrMailboxOutgoingBase;
+            GCMailbox = pThis->GCPhysAddrMailboxOutgoingBase + (pThis->cMailbox * sizeof(Mailbox32));
             pHlp->pfnPrintf(pHlp, " Outgoing mailbox entries (32-bit) at %08X:\n", (uint32_t)GCMailbox);
             for (i = 0; i < pThis->cMailbox; ++i)
             {
