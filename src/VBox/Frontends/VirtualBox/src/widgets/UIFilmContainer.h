75a6bdcb9bda107b9a52909d1e853f93e58af730
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2013-06-13 14:55:11 +0000

    FE/Qt: VM settings: Display page: Video tab: Multi-screen support.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@46536 cfe28804-0f27-0410-a406-dd0f0b0b656f

---
+++ src/VBox/Frontends/VirtualBox/src/widgets/UIFilmContainer.h
@@ -0,0 +1,101 @@
+/** @file
+ *
+ * VBox frontends: Qt4 GUI ("VirtualBox"):
+ * UIFilmContainer class declaration
+ */
+
+/*
+ * Copyright (C) 2013 Oracle Corporation
+ *
+ * This file is part of VirtualBox Open Source Edition (OSE), as
+ * available from http://www.virtualbox.org. This file is free software;
+ * you can redistribute it and/or modify it under the terms of the GNU
+ * General Public License (GPL) as published by the Free Software
+ * Foundation, in version 2 as it comes in the "COPYING" file of the
+ * VirtualBox OSE distribution. VirtualBox OSE is distributed in the
+ * hope that it will be useful, but WITHOUT ANY WARRANTY of any kind.
+ */
+
+#ifndef __UIFilmContainer_h__
+#define __UIFilmContainer_h__
+
+/* Qt includes: */
+#include <QWidget>
+
+/* GUI includes: */
+#include "QIWithRetranslateUI.h"
+
+/* Other VBox includes: */
+#include <VBox/com/com.h>
+
+/* Forward declarations: */
+class QScrollArea;
+class UIFilm;
+class QVBoxLayout;
+class QCheckBox;
+
+/* Transparent QScrollArea container for UIFilm widgets: */
+class UIFilmContainer : public QWidget
+{
+    Q_OBJECT;
+
+public:
+
+    /* Constructor: */
+    UIFilmContainer(QWidget *pParent = 0);
+
+    /* API: Value stuff: */
+    QVector<BOOL> value() const;
+    void setValue(const QVector<BOOL> &value);
+
+private:
+
+    /* Helpers: Prepare stuff: */
+    void prepare();
+    void prepareLayout();
+    void prepareScroller();
+
+    /* Variables: */
+    QVBoxLayout *m_pMainLayout;
+    QScrollArea *m_pScroller;
+    QList<UIFilm*> m_widgets;
+};
+
+/* QWidget item for UIFilmContainer: */
+class UIFilm : public QIWithRetranslateUI<QWidget>
+{
+    Q_OBJECT;
+
+public:
+
+    /* Constructor: */
+    UIFilm(int iScreenIndex, BOOL fEnabled, QWidget *pParent = 0);
+
+    /* API: Cehck-box stuff: */
+    bool checked() const;
+
+protected:
+
+    /* Helper: Translate stuff: */
+    void retranslateUi();
+
+private:
+
+    /* Helper: Prepare stuff: */
+    void prepare();
+    void prepareLayout();
+    void prepareCheckBox();
+
+    /* Handler: Layut stuff: */
+    QSize minimumSizeHint() const;
+
+    /* Handlers: Paint stuff: */
+    void paintEvent(QPaintEvent *pEvent);
+
+    /* Variables: */
+    int m_iScreenIndex;
+    BOOL m_fWasEnabled;
+    QVBoxLayout *m_pMainLayout;
+    QCheckBox *m_pCheckBox;
+};
+#endif /* __UIFilmContainer_h__ */
