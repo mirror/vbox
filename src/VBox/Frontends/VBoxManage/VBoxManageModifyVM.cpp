846b1d9037afec418596d838ef0001bcbd5997ea
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2013-06-19 17:41:54 +0000

    Frontends/VBoxManage: automatically convert the video capture filename to an absolute path
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@46685 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Frontends/VBoxManage/VBoxManageModifyVM.cpp
+++ src/VBox/Frontends/VBoxManage/VBoxManageModifyVM.cpp
@@ -2495,7 +2495,20 @@ int handleModifyVM(HandlerArg *a)
             }
             case MODIFYVM_VCP_FILENAME:
             {
-                Bstr bstr(ValueUnion.psz);
+                Bstr bstr;
+                /* empty string will fall through, leaving bstr empty */
+                if (*ValueUnion.psz)
+                {
+                    char szVCFileAbs[RTPATH_MAX] = "";
+                    int vrc = RTPathAbs(ValueUnion.psz, szVCFileAbs, sizeof(szVCFileAbs));
+                    if (RT_FAILURE(vrc))
+                    {
+                        errorArgument("Cannot convert filename \"%s\" to absolute path\n", ValueUnion.psz);
+                        rc = E_FAIL;
+                        break;
+                    }
+                    bstr = szVCFileAbs;
+                }
                 CHECK_ERROR(machine, COMSETTER(VideoCaptureFile)(bstr.raw()));
                 break;
             }
