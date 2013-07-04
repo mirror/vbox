36c4e5667d7f4d390520c29fc620d5a4429d1f74
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2013-07-04 16:16:52 +0000

    FE/Qt: Popup-center: Few more messages for popup-center.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@46984 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Frontends/VirtualBox/src/globals/UIPopupCenter.cpp
+++ src/VBox/Frontends/VirtualBox/src/globals/UIPopupCenter.cpp
@@ -23,6 +23,7 @@
 #include "UIMachineWindow.h"
 #include "QIMessageBox.h"
 #include "VBoxGlobal.h"
+#include "UIHostComboEditor.h"
 
 /* Other VBox includes: */
 #include <VBox/sup.h>
@@ -375,29 +376,78 @@ QString UIPopupCenter::popupStackID(QWidget *pParent)
     return pParent->metaObject()->className();
 }
 
+void UIPopupCenter::cannotSendACPIToMachine(QWidget *pParent)
+{
+    alert(pParent,  "cannotSendACPIToMachine",
+          QApplication::translate("UIMessageCenter", "You are trying to shut down the guest with the ACPI power button. "
+                                                     "This is currently not possible because the guest does not support software shutdown."));
+}
+
+void UIPopupCenter::remindAboutAutoCapture(QWidget *pParent)
+{
+    alert(pParent, "remindAboutAutoCapture",
+          QApplication::translate("UIMessageCenter", "<p>You have the <b>Auto capture keyboard</b> option turned on. "
+                                                     "This will cause the Virtual Machine to automatically <b>capture</b> "
+                                                     "the keyboard every time the VM window is activated and make it "
+                                                     "unavailable to other applications running on your host machine: "
+                                                     "when the keyboard is captured, all keystrokes (including system ones "
+                                                     "like Alt-Tab) will be directed to the VM.</p>"
+                                                     "<p>You can press the <b>host key</b> at any time to <b>uncapture</b> the "
+                                                     "keyboard and mouse (if it is captured) and return them to normal "
+                                                     "operation. The currently assigned host key is shown on the status bar "
+                                                     "at the bottom of the Virtual Machine window, next to the&nbsp;"
+                                                     "<img src=:/hostkey_16px.png/>&nbsp;icon. This icon, together "
+                                                     "with the mouse icon placed nearby, indicate the current keyboard "
+                                                     "and mouse capture state.</p>") +
+          QApplication::translate("UIMessageCenter", "<p>The host key is currently defined as <b>%1</b>.</p>",
+                                                     "additional message box paragraph")
+                                                     .arg(UIHostCombo::toReadableString(vboxGlobal().settings().hostCombo())),
+          true);
+}
+
 void UIPopupCenter::remindAboutMouseIntegration(QWidget *pParent, bool fSupportsAbsolute)
 {
     if (fSupportsAbsolute)
     {
-        alert(pParent, QString("remindAboutMouseIntegration"),
-              tr("<p>The Virtual Machine reports that the guest OS supports <b>mouse pointer integration</b>. "
-                 "This means that you do not need to <i>capture</i> the mouse pointer to be able to use it in your guest OS -- "
-                 "all mouse actions you perform when the mouse pointer is over the Virtual Machine's display "
-                 "are directly sent to the guest OS. If the mouse is currently captured, it will be automatically uncaptured.</p>"
-                 "<p>The mouse icon on the status bar will look like&nbsp;<img src=:/mouse_seamless_16px.png/>&nbsp;to inform you "
-                 "that mouse pointer integration is supported by the guest OS and is currently turned on.</p>"
-                 "<p><b>Note</b>: Some applications may behave incorrectly in mouse pointer integration mode. "
-                 "You can always disable it for the current session (and enable it again) "
-                 "by selecting the corresponding action from the menu bar.</p>"),
+        alert(pParent, "remindAboutMouseIntegration",
+              QApplication::translate("UIMessageCenter", "<p>The Virtual Machine reports that the guest OS supports <b>mouse pointer integration</b>. "
+                                                         "This means that you do not need to <i>capture</i> the mouse pointer to be able to use it in your guest OS -- "
+                                                         "all mouse actions you perform when the mouse pointer is over the Virtual Machine's display "
+                                                         "are directly sent to the guest OS. If the mouse is currently captured, it will be automatically uncaptured.</p>"
+                                                         "<p>The mouse icon on the status bar will look like&nbsp;<img src=:/mouse_seamless_16px.png/>&nbsp;to inform you "
+                                                         "that mouse pointer integration is supported by the guest OS and is currently turned on.</p>"
+                                                         "<p><b>Note</b>: Some applications may behave incorrectly in mouse pointer integration mode. "
+                                                         "You can always disable it for the current session (and enable it again) "
+                                                         "by selecting the corresponding action from the menu bar.</p>"),
               true);
     }
     else
     {
-        alert(pParent, QString("remindAboutMouseIntegration"),
-              tr("<p>The Virtual Machine reports that the guest OS does not support <b>mouse pointer integration</b> "
-                 "in the current video mode. You need to capture the mouse (by clicking over the VM display "
-                 "or pressing the host key) in order to use the mouse inside the guest OS.</p>"),
+        alert(pParent, "remindAboutMouseIntegration",
+              QApplication::translate("UIMessageCenter", "<p>The Virtual Machine reports that the guest OS does not support <b>mouse pointer integration</b> "
+                                                         "in the current video mode. You need to capture the mouse (by clicking over the VM display "
+                                                         "or pressing the host key) in order to use the mouse inside the guest OS.</p>"),
               true);
     }
+}
+
+void UIPopupCenter::remindAboutPausedVMInput(QWidget *pParent)
+{
+    alert(pParent, "remindAboutPausedVMInput",
+          QApplication::translate("UIMessageCenter", "<p>The Virtual Machine is currently in the <b>Paused</b> state and not able to see any keyboard or mouse input. "
+                                                     "If you want to continue to work inside the VM, you need to resume it by selecting the corresponding action "
+                                                     "from the menu bar.</p>"),
+          true);
+}
+
+void UIPopupCenter::remindAboutGuestAdditionsAreNotActive(QWidget *pParent)
+{
+    alert(pParent, "remindAboutGuestAdditionsAreNotActive",
+          QApplication::translate("UIMessageCenter", "<p>The VirtualBox Guest Additions do not appear to be available on this virtual machine, "
+                                                     "and shared folders cannot be used without them. To use shared folders inside the virtual machine, "
+                                                     "please install the Guest Additions if they are not installed, or re-install them if they are "
+                                                     "not working correctly, by selecting <b>Install Guest Additions</b> from the <b>Devices</b> menu. "
+                                                     "If they are installed but the machine is not yet fully started then shared folders will be available once it is.</p>"),
+          true);
 }
 
