3b293343352dbc8f331ea6183e5c7f219c597211
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2013-07-05 15:34:09 +0000

    IPRT/RTSystemQueryOSInfo-win.cpp: Must be sizeof(OSVERSIONINFOEX).
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@47011 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Runtime/r3/win/RTSystemQueryOSInfo-win.cpp
+++ src/VBox/Runtime/r3/win/RTSystemQueryOSInfo-win.cpp
@@ -308,8 +308,8 @@ static int rtSystemWinQueryOSVersion(RTSYSOSINFO enmInfo, char *pszInfo, size_t 
      * ASSUMES OSVERSIONINFOEX starts with the exact same layout as OSVERSIONINFO (safe).
      */
     OSVERSIONINFOEX OSInfoEx;
-    memset(&OSInfoEx, '\0', sizeof(OSInfoEx));
-    OSInfoEx.dwOSVersionInfoSize = sizeof(OSVERSIONINFO);
+    RT_ZERO(OSInfoEx);
+    OSInfoEx.dwOSVersionInfoSize = sizeof(OSVERSIONINFOEX);
     if (!GetVersionEx((LPOSVERSIONINFO) &OSInfoEx))
     {
         DWORD err = GetLastError();
@@ -318,10 +318,10 @@ static int rtSystemWinQueryOSVersion(RTSYSOSINFO enmInfo, char *pszInfo, size_t 
     }
 
     /* Get extended version info for 2000 and later. */
-    if (    OSInfoEx.dwPlatformId == VER_PLATFORM_WIN32_NT
-        &&  OSInfoEx.dwMajorVersion >= 5)
+    if (   OSInfoEx.dwPlatformId == VER_PLATFORM_WIN32_NT
+        && OSInfoEx.dwMajorVersion >= 5)
     {
-        ZeroMemory(&OSInfoEx, sizeof(OSInfoEx));
+        RT_ZERO(OSInfoEx);
         OSInfoEx.dwOSVersionInfoSize = sizeof(OSVERSIONINFOEX);
         if (!GetVersionEx((LPOSVERSIONINFO) &OSInfoEx))
         {
