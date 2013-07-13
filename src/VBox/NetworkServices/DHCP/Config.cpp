3c1ba9f68e29fd46dbcf865c2f38114b39bb2609
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2013-07-13 11:44:12 +0000

    VBoxNetDHCP: cosmetics.
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@47129 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/NetworkServices/DHCP/Config.cpp
+++ src/VBox/NetworkServices/DHCP/Config.cpp
@@ -512,7 +512,6 @@ int NetworkManager::offer4Session(Session& session)
     opt.cbRawOpt = 1;
     client->rawOptions.push_back(opt);
 
-    /* XXX: here should be condition MUST NOT on DHCPINFORM */
     opt.u8OptId = RTNET_DHCP_OPT_LEASE_TIME;
     *(uint32_t *)opt.au8RawOpt = RT_H2N_U32(ConfigurationManager::getConfigurationManager()->getLeaseTime());
     opt.cbRawOpt = sizeof(RTNETADDRIPV4);
@@ -564,8 +563,13 @@ int NetworkManager::ack(Session& session)
     opt.cbRawOpt = 1;
     client->rawOptions.push_back(opt);
 
+    /*
+     * XXX: lease time should be conditional. If on dhcprequest then tim should be provided,
+     * else on dhcpinform it mustn't.
+     */
     opt.u8OptId = RTNET_DHCP_OPT_LEASE_TIME;
-    *(uint32_t *)opt.au8RawOpt = RT_H2N_U32(ConfigurationManager::getConfigurationManager()->getLeaseTime());
+    *(uint32_t *)opt.au8RawOpt = 
+      RT_H2N_U32(ConfigurationManager::getConfigurationManager()->getLeaseTime());
     opt.cbRawOpt = sizeof(RTNETADDRIPV4);
     client->rawOptions.push_back(opt);
 
