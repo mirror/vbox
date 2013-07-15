280cd2085e4de7df971095921196bdbac15f56eb
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2013-07-15 11:08:03 +0000

    FE/Qt: General Icon Update: Message-center icons.
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@47153 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Frontends/VirtualBox/src/globals/UIIconPool.cpp
+++ src/VBox/Frontends/VirtualBox/src/globals/UIIconPool.cpp
@@ -159,8 +159,7 @@ QIcon UIIconPool::defaultIcon(UIDefaultIcon def, const QWidget *pWidget /* = 0 *
         {
             icon = pStyle->standardIcon(QStyle::SP_DialogCancelButton, 0, pWidget);
             if (icon.isNull())
-                icon = iconSet(":/delete_16px.png",
-                               ":/delete_dis_16px.png");
+                icon = iconSet(":/cancel_16px.png");
             break;
         }
         case DialogHelpIcon:
