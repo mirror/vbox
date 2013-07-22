115a2a67f7a1d9a9fd953ac527325e921e553b3a
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2013-07-22 14:31:56 +0000

    DIS: *fence todo.
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@47306 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Disassembler/DisasmTables.cpp
+++ src/VBox/Disassembler/DisasmTables.cpp
@@ -2698,6 +2698,8 @@ const DISOPCODE g_aMapX86_Group15_mod11_rm000[8] =
     INVALID_OPCODE_MOD_RM(0x),
     INVALID_OPCODE_MOD_RM(0x),
     INVALID_OPCODE_MOD_RM(0x),
+    /** @todo mfence + lfence + sfence instructions doesn't not require rm=0,
+     *        they work for any RM value. See bs2-cpu-instr-1.asm for details. */
     OP("lfence",             IDX_ParseModFence,  0,          0,          OP_LFENCE,  OP_PARM_NONE,       OP_PARM_NONE,   OP_PARM_NONE,   DISOPTYPE_HARMLESS),
     OP("mfence",             IDX_ParseModFence,  0,          0,          OP_MFENCE,  OP_PARM_NONE,       OP_PARM_NONE,   OP_PARM_NONE,   DISOPTYPE_HARMLESS),
     OP("sfence",             IDX_ParseModFence,  0,          0,          OP_SFENCE,  OP_PARM_NONE,       OP_PARM_NONE,   OP_PARM_NONE,   DISOPTYPE_HARMLESS),
