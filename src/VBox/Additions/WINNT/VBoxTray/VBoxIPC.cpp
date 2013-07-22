b5db3497c742bf7b6b2a101f79c1d0f30e561b1b
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2013-07-22 12:14:26 +0000

    VBoxTray/IPC: Only process commands w/ header version 0 for now.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@47296 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Additions/WINNT/VBoxTray/VBoxIPC.cpp
+++ src/VBox/Additions/WINNT/VBoxTray/VBoxIPC.cpp
@@ -294,11 +294,13 @@ static DECLCALLBACK(int) vboxIPCSessionThread(RTTHREAD hThread, void *pvSession)
             VBOXTRAYIPCHEADER ipcHdr;
             rc = RTLocalIpcSessionRead(hSession, &ipcHdr, sizeof(ipcHdr),
                                        NULL /* Exact read, blocking */);
-            bool fRejected = false;
+            bool fRejected = false; /* Reject current command? */
             if (RT_SUCCESS(rc))
-                fRejected = ipcHdr.uMagic != VBOXTRAY_IPC_HDR_MAGIC;
+                fRejected =    ipcHdr.uMagic != VBOXTRAY_IPC_HDR_MAGIC
+                            || ipcHdr.uHdrVersion != 0; /* We only know version 0 commands for now. */
 
-            if (RT_SUCCESS(rc))
+            if (   !fRejected
+                && RT_SUCCESS(rc))
             {
                 switch (ipcHdr.uMsgType)
                 {
