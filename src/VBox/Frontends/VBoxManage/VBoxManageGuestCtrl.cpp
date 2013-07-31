b6e2b622e3d6914a195816048de2765e9caac678
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2013-07-31 13:32:13 +0000

    FE/VBoxManage/GuestCtrl: Also close guest session on overall failure.
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@47492 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Frontends/VBoxManage/VBoxManageGuestCtrl.cpp
+++ src/VBox/Frontends/VBoxManage/VBoxManageGuestCtrl.cpp
@@ -904,7 +904,7 @@ static RTEXITCODE handleCtrlExecProgram(ComPtr<IGuest> pGuest, HandlerArg *pArg)
 
                     if (fVerbose)
                     {
-                        RTPrintf("Process '%s' (PID: %ul) %s\n",
+                        RTPrintf("Process '%s' (PID: %ld) %s\n",
                                  strCmd.c_str(), uPID,
                                  fWaitForExit ? "started" : "started (detached)");
                     }
@@ -1021,7 +1021,8 @@ static RTEXITCODE handleCtrlExecProgram(ComPtr<IGuest> pGuest, HandlerArg *pArg)
      * process to exit. Otherwise we wouldn't have any chance to
      * access and/or kill detached guest process lateron.
      */
-    if (fWaitForExit)
+    if (   fWaitForExit
+        || rcExit != EXITCODEEXEC_SUCCESS)
     {
         if (fVerbose)
             RTPrintf("Closing guest session ...\n");
