799ee11de0eeb5462e07a34104cc2137d5ab7e9b
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2013-08-05 10:22:24 +0000

    build fix
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@47540 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/HostDrivers/Support/SUPDrv.c
+++ src/VBox/HostDrivers/Support/SUPDrv.c
@@ -994,10 +994,11 @@ static void supdrvCloseSession(PSUPDRVDEVEXT pDevExt, PSUPDRVSESSION pSession)
  */
 uint32_t VBOXCALL supdrvSessionRetain(PSUPDRVSESSION pSession)
 {
+    uint32_t cRefs;
     AssertPtrReturn(pSession, UINT32_MAX);
     AssertReturn(SUP_IS_SESSION_VALID(pSession), UINT32_MAX);
 
-    uint32_t cRefs = ASMAtomicIncU32(&pSession->cRefs);
+    cRefs = ASMAtomicIncU32(&pSession->cRefs);
     AssertMsg(cRefs > 1 && cRefs < _1M, ("%#x %p\n", cRefs, pSession));
     return cRefs;
 }
@@ -1010,10 +1011,11 @@ uint32_t VBOXCALL supdrvSessionRetain(PSUPDRVSESSION pSession)
  */
 uint32_t VBOXCALL supdrvSessionRelease(PSUPDRVSESSION pSession)
 {
+    uint32_t cRefs;
     AssertPtrReturn(pSession, UINT32_MAX);
     AssertReturn(SUP_IS_SESSION_VALID(pSession), UINT32_MAX);
 
-    uint32_t cRefs = ASMAtomicDecU32(&pSession->cRefs);
+    cRefs = ASMAtomicDecU32(&pSession->cRefs);
     AssertMsg(cRefs < _1M, ("%#x %p\n", cRefs, pSession));
     if (cRefs == 0)
         supdrvCloseSession(pSession->pDevExt, pSession);
