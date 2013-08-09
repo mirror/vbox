5643c01735dbf7d0f1f03d7a15114fe8c32cc902
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2013-08-09 13:12:19 +0000

    Runtime/r0drv/nt: fix Windows r0drv timers
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@47637 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Runtime/r0drv/nt/timer-r0drv-nt.cpp
+++ src/VBox/Runtime/r0drv/nt/timer-r0drv-nt.cpp
@@ -121,7 +121,11 @@ static void _stdcall rtTimerNtSimpleCallback(IN PKDPC pDpc, IN PVOID pvUser, IN 
      */
     if (    !ASMAtomicUoReadBool(&pTimer->fSuspended)
         &&  pTimer->u32Magic == RTTIMER_MAGIC)
+    {
+        if (!pTimer->u64NanoInterval)
+            ASMAtomicWriteBool(&pTimer->fSuspended, true);
         pTimer->pfnTimer(pTimer, pTimer->pvUser, ++pTimer->aSubTimers[0].iTick);
+    }
 
     NOREF(pDpc); NOREF(SystemArgument1); NOREF(SystemArgument2);
 }
@@ -154,7 +158,11 @@ static void _stdcall rtTimerNtOmniSlaveCallback(IN PKDPC pDpc, IN PVOID pvUser, 
      */
     if (    !ASMAtomicUoReadBool(&pTimer->fSuspended)
         &&  pTimer->u32Magic == RTTIMER_MAGIC)
+    {
+        if (!pTimer->u64NanoInterval)
+            ASMAtomicWriteBool(&pTimer->fSuspended, true);
         pTimer->pfnTimer(pTimer, pTimer->pvUser, ++pSubTimer->iTick);
+    }
 
     NOREF(pDpc); NOREF(SystemArgument1); NOREF(SystemArgument2);
 }
@@ -199,6 +207,8 @@ static void _stdcall rtTimerNtOmniMasterCallback(IN PKDPC pDpc, IN PVOID pvUser,
                 &&  iCpuSelf != iCpu)
                 KeInsertQueueDpc(&pTimer->aSubTimers[iCpu].NtDpc, 0, 0);
 
+        if (!pTimer->u64NanoInterval)
+            ASMAtomicWriteBool(&pTimer->fSuspended, true);
         pTimer->pfnTimer(pTimer, pTimer->pvUser, ++pSubTimer->iTick);
     }
 
@@ -237,9 +247,12 @@ RTDECL(int) RTTimerStart(PRTTIMER pTimer, uint64_t u64First)
 
     LARGE_INTEGER DueTime;
     DueTime.QuadPart = -(int64_t)(u64First / 100); /* Relative, NT time. */
-    if (DueTime.QuadPart)
+    if (!DueTime.QuadPart)
         DueTime.QuadPart = -1;
 
+    unsigned cSubTimers = pTimer->fOmniTimer ? pTimer->cSubTimers : 1;
+    for (unsigned iCpu = 0; iCpu < cSubTimers; iCpu++)
+        pTimer->aSubTimers[iCpu].iTick = 0;
     ASMAtomicWriteBool(&pTimer->fSuspended, false);
     KeSetTimerEx(&pTimer->NtTimer, DueTime, ulInterval, pMasterDpc);
     return VINF_SUCCESS;
