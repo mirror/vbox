5e67f575edc3fbf9b9d177689c33187913097f06
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2013-08-13 14:41:55 +0000

    FE/VBoxManage/VBoxManageGuestCtrl.cpp: Fixed error message on guest session close.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@47696 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Frontends/VBoxManage/VBoxManageGuestCtrl.cpp
+++ src/VBox/Frontends/VBoxManage/VBoxManageGuestCtrl.cpp
@@ -3357,6 +3357,7 @@ static RTEXITCODE handleCtrlSessionClose(ComPtr<IGuest> guest, HandlerArg *pArg)
     do
     {
         bool fSessionFound = false;
+        size_t cSessionsHandled = 0;
 
         SafeIfaceArray <IGuestSession> collSessions;
         CHECK_ERROR_BREAK(guest, COMGETTER(Sessions)(ComSafeArrayAsOutParam(collSessions)));
@@ -3385,6 +3386,8 @@ static RTEXITCODE handleCtrlSessionClose(ComPtr<IGuest> guest, HandlerArg *pArg)
 
             if (fSessionFound)
             {
+                cSessionsHandled++;
+
                 Assert(!pSession.isNull());
                 if (fVerbose)
                     RTPrintf("Closing guest session ID=#%RU32 \"%s\" ...\n",
@@ -3397,7 +3400,7 @@ static RTEXITCODE handleCtrlSessionClose(ComPtr<IGuest> guest, HandlerArg *pArg)
             }
         }
 
-        if (!fSessionFound)
+        if (!cSessionsHandled)
         {
             RTPrintf("No guest session(s) found\n");
             rc = E_ABORT; /* To set exit code accordingly. */
