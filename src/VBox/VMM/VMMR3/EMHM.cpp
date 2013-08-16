819814f591119f7178bf699f658334ffd7caac16
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2013-08-16 09:08:02 +0000

    build fix
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@47790 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/VMM/VMMR3/EMHM.cpp
+++ src/VBox/VMM/VMMR3/EMHM.cpp
@@ -202,7 +202,7 @@ static int emR3HmExecuteInstructionWorker(PVM pVM, PVMCPU pVCpu, int rcRC)
 #endif
     {
 #ifdef VBOX_WITH_REM
-        STAM_PROFILE_START(&pVCpu->em.s.StatREMEmu, a);
+        STAM_PROFILE_START(&pVCpu->em.s.StatREMEmu, b);
 # ifndef VBOX_WITH_FIRST_IEM_STEP
         Log(("EMINS[rem]: %04x:%RGv RSP=%RGv\n", pCtx->cs.Sel, (RTGCPTR)pCtx->rip, (RTGCPTR)pCtx->rsp));
 # elif defined(DEBUG_bird)
@@ -216,7 +216,7 @@ static int emR3HmExecuteInstructionWorker(PVM pVM, PVMCPU pVCpu, int rcRC)
 
         rc = REMR3EmulateInstruction(pVM, pVCpu);
         EMRemUnlock(pVM);
-        STAM_PROFILE_STOP(&pVCpu->em.s.StatREMEmu, a);
+        STAM_PROFILE_STOP(&pVCpu->em.s.StatREMEmu, b);
 #else  /* !VBOX_WITH_REM */
         NOREF(pVM);
 #endif /* !VBOX_WITH_REM */
