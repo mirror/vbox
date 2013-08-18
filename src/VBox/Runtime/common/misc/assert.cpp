3b7de06c0049e882b3bc0e9933e5a673ab48d614
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2013-08-18 00:00:44 +0000

    Missed one RTLogCom call.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@47827 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Runtime/common/misc/assert.cpp
+++ src/VBox/Runtime/common/misc/assert.cpp
@@ -247,9 +247,11 @@ static void rtAssertMsg2Worker(bool fInitial, const char *pszFormat, va_list va)
 
 #else  /* !IN_RING0 */
 # if !defined(IN_RING3) && !defined(LOG_NO_COM)
+#  if 0 /* Enable this iff you have a COM port and really want this debug info. */
         va_copy(vaCopy, va);
         RTLogComPrintfV(pszFormat, vaCopy);
         va_end(vaCopy);
+#  endif
 # endif
 
         PRTLOGGER pLog = RTLogRelDefaultInstance();
