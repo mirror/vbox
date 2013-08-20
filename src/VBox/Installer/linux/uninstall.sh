73d70cc551c5187ce3daa664d5a3991df7d8163a
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2013-08-20 10:07:59 +0000

    deb/rpm: fixes for VBoxNetLwipNAT
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@47884 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Installer/linux/uninstall.sh
+++ src/VBox/Installer/linux/uninstall.sh
@@ -57,6 +57,8 @@ stop_init_script vboxweb-service
 check_running
 # Terminate VBoxNetDHCP if running
 terminate_proc VBoxNetDHCP
+# Terminate VBoxNetLwipNAT if running
+terminate_proc VBoxNetLwipNAT
 delrunlevel vboxballoonctrl-service > /dev/null 2>&1
 remove_init_script vboxballoonctrl-service
 delrunlevel vboxautostart-service > /dev/null 2>&1
@@ -98,6 +100,7 @@ rm -f \
   /usr/bin/VBoxBalloonCtrl \
   /usr/bin/VBoxAutostart \
   /usr/bin/VBoxNetDHCP \
+  /usr/bin/VBoxNetLwipNAT \
   /usr/bin/vboxwebsrv \
   /usr/bin/VBoxAddIF \
   /usr/bin/VBoxDeleteIf \
