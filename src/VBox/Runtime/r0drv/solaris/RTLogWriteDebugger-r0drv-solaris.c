167f85f0f30c31293dd08656a418e52e7a252422
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2013-08-24 12:04:57 +0000

    Solaris/LogRel: Update comment and some minor cosmetics.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@48042 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Runtime/r0drv/solaris/RTLogWriteDebugger-r0drv-solaris.c
+++ src/VBox/Runtime/r0drv/solaris/RTLogWriteDebugger-r0drv-solaris.c
@@ -45,8 +45,11 @@ RTDECL(void) RTLogWriteDebugger(const char *pch, size_t cb)
 {
     if (pch[cb] != '\0')
         AssertBreakpoint();
-    if (!RTThreadPreemptIsEnabled(NIL_RTTHREAD))    /** @todo this will change when preemptions hook are implemented. */
+
+    /* cmn_err() acquires adaptive mutexes. Not preemption safe, see @bugref{6657}. */
+    if (!RTThreadPreemptIsEnabled(NIL_RTTHREAD))
         return;
+
     if (    !g_frtSolSplSetsEIF
 #if defined(RT_ARCH_AMD64) || defined(RT_ARCH_X86)
         ||  ASMIntAreEnabled()
@@ -54,7 +57,10 @@ RTDECL(void) RTLogWriteDebugger(const char *pch, size_t cb)
 /* PORTME: Check if interrupts are enabled, if applicable. */
 #endif
         )
+    {
         cmn_err(CE_CONT, pch);
+    }
+
     return;
 }
 
