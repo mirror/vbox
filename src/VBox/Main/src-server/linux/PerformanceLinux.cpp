e81952a28addb031eb94e4a2a04d2d8ce03a9ccf
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2013-08-26 10:30:35 +0000

    Main/Performance: fixed path to VBoxVolInfo
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@48055 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Main/src-server/linux/PerformanceLinux.cpp
+++ src/VBox/Main/src-server/linux/PerformanceLinux.cpp
@@ -516,7 +516,8 @@ void CollectorLinux::addRaidDisks(const char *pcszDevice, DiskList& listDisks)
 void CollectorLinux::addVolumeDependencies(const char *pcszVolume, DiskList& listDisks)
 {
     char szVolInfo[RTPATH_MAX];
-    int rc = RTPathExecDir(szVolInfo, sizeof(szVolInfo) - sizeof("/" VBOXVOLINFO_NAME " ") - strlen(pcszVolume));
+    int rc = RTPathAppPrivateArch(szVolInfo, 
+                                  sizeof(szVolInfo) - sizeof("/" VBOXVOLINFO_NAME " ") - strlen(pcszVolume));
     if (RT_FAILURE(rc))
     {
         LogRel(("VolInfo: Failed to get program path, rc=%Rrc\n", rc));
