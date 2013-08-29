87793a7baa25d51da990e10ab1d37c716ea2a7e7
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2013-08-29 12:04:40 +0000

    Runtime/r0drv/solaris: Fix race with specific timers and enabled tstRTR0Timer testing of it.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@48148 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Runtime/testcase/tstRTR0TimerDriver.cpp
+++ src/VBox/Runtime/testcase/tstRTR0TimerDriver.cpp
@@ -60,27 +60,19 @@ int main(int argc, char **argv)
     /*
      * Standard timers.
      */
-#  ifndef RT_OS_SOLARIS // one-shot timers currently not supported on Solaris
     RTR3TestR0SimpleTest(TSTRTR0TIMER_ONE_SHOT_BASIC,       "Basic one shot");
-#  endif
     RTR3TestR0SimpleTest(TSTRTR0TIMER_PERIODIC_BASIC,       "Basic periodic");
     if (RTTestErrorCount(g_hTest) == 0)
     {
 #  if 1
-#   ifndef RT_OS_SOLARIS // one-shot timers currently not supported on Solaris
         RTR3TestR0SimpleTest(TSTRTR0TIMER_ONE_SHOT_RESTART, "Restart one shot from callback");
         RTR3TestR0SimpleTest(TSTRTR0TIMER_ONE_SHOT_DESTROY, "Destroy one shot from callback");
-#   endif
         RTR3TestR0SimpleTest(TSTRTR0TIMER_PERIODIC_CSSD_LOOPS, "Create-start-stop-destroy loops");
         for (uint32_t i = 0; i <= 7; i++)
             RTR3TestR0SimpleTestWithArg(TSTRTR0TIMER_PERIODIC_CHANGE_INTERVAL, i, "Change interval from callback, variation %u", i);
 #  endif
-#   ifndef RT_OS_SOLARIS // one-shot timers currently not supported on Solaris
         RTR3TestR0SimpleTest(TSTRTR0TIMER_ONE_SHOT_SPECIFIC, "One shot cpu specific");
-#   endif
-#   ifndef RT_OS_SOLARIS // periodic specific timers have currently a bug on Solaris
         RTR3TestR0SimpleTest(TSTRTR0TIMER_PERIODIC_SPECIFIC, "Periodic cpu specific");
-#   endif
         RTR3TestR0SimpleTest(TSTRTR0TIMER_PERIODIC_OMNI, "Periodic omni timer");
     }
 # endif
@@ -107,9 +99,7 @@ int main(int argc, char **argv)
 #   ifndef RT_OS_SOLARIS // one-shot timers currently not supported on Solaris
         RTR3TestR0SimpleTest(TSTRTR0TIMER_ONE_SHOT_SPECIFIC_HIRES, "One shot hires cpu specific");
 #   endif
-#   ifndef RT_OS_SOLARIS // periodic specific timers have currently a bug on Solaris
         RTR3TestR0SimpleTest(TSTRTR0TIMER_PERIODIC_SPECIFIC_HIRES, "Periodic hires cpu specific");
-#   endif
         RTR3TestR0SimpleTest(TSTRTR0TIMER_PERIODIC_OMNI, "Periodic omni hires timer");
     }
 # endif
