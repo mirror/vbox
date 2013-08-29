d464558398ec7e2ec6f3dccab18bbac3a354140e
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2013-08-29 16:35:14 +0000

    Main: phase out the use of the XPCOM glue (wasn't ever used), and rename tstVBoxAPILinux to tstVBoxAPIXPCOM to match the content libs/xpcom: strip down the standalone XPCOM glue code and disable building it as so far no one uses it
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@48164 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Main/src-server/xpcom/server.cpp
+++ src/VBox/Main/src-server/xpcom/server.cpp
@@ -4,7 +4,7 @@
  */
 
 /*
- * Copyright (C) 2004-2012 Oracle Corporation
+ * Copyright (C) 2004-2013 Oracle Corporation
  *
  * This file is part of VirtualBox Open Source Edition (OSE), as
  * available from http://www.virtualbox.org. This file is free software;
@@ -19,10 +19,6 @@
 #include <ipcCID.h>
 
 #include <nsIComponentRegistrar.h>
-
-#ifdef XPCOM_GLUE
-# include <nsXPCOMGlue.h>
-#endif
 
 #include <nsEventQueueUtils.h>
 #include <nsGenericFactory.h>
