2ab6d3222812619d802cd35f2127b073bb38fefb
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2013-08-30 14:59:33 +0000

    VMM: Thread-context hooks logging.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@48198 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/VMM/VMMR3/VMM.cpp
+++ src/VBox/VMM/VMMR3/VMM.cpp
@@ -535,6 +535,13 @@ VMMR3_INT_DECL(int) VMMR3InitR0(PVM pVM)
         if (RT_SUCCESS(rc))
             rc = VERR_IPE_UNEXPECTED_INFO_STATUS;
     }
+
+    /* Log whether thread-context hooks are used (on Linux this can depend on how the kernel is configured). */
+    if (pVM->aCpus[0].vmm.s.hR0ThreadCtx != NIL_RTTHREADCTX)
+        LogRel(("VMM: Thread-context hooks enabled!\n"));
+    else
+        LogRel(("VMM: Thread-context hooks unavailable."));
+
     return rc;
 }
 
