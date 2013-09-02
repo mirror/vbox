7e68bd2d796771dad857bf6102c733ace603baf0
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2013-09-02 16:02:22 +0000

    log-vbox: set logging preferences.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@48232 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Runtime/VBox/log-vbox.cpp
+++ src/VBox/Runtime/VBox/log-vbox.cpp
@@ -482,8 +482,8 @@ RTDECL(PRTLOGGER) RTLogDefaultInit(void)
         RTLogFlags(pLogger, "enabled unbuffered");
         pLogger->fDestFlags |= RTLOGDEST_DEBUGGER;
 # endif
-# if defined(DEBUG_leo) /* Guest ring-0 as well */
-        RTLogGroupSettings(pLogger, "+drv_mouse.e.l.f+drv_miniport.e.l.f+drv_display.e.l.f");
+# if defined(DEBUG_michael) && defined(IN_GUEST)
+        RTLogGroupSettings(pLogger, "+all..e.l.f");
         RTLogFlags(pLogger, "enabled unbuffered");
         pLogger->fDestFlags |= RTLOGDEST_DEBUGGER;
 # endif
