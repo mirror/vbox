6a7a163bd48b864eb88492fad39a1767b592b76f
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2013-09-04 16:15:32 +0000

    FE/Qt: Medium-enumerator framework.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@48273 cfe28804-0f27-0410-a406-dd0f0b0b656f

---
+++ src/VBox/Frontends/VirtualBox/src/medium/UIMediumEnumerator.h
@@ -0,0 +1,90 @@
+/** @file
+ *
+ * VBox frontends: Qt GUI ("VirtualBox"):
+ * UIMediumEnumerator class declaration
+ */
+
+/*
+ * Copyright (C) 2013 Oracle Corporation
+ *
+ * This file is part of VirtualBox Open Source Edition (OSE), as
+ * available from http://www.virtualbox.org. This file is free software;
+ * you can redistribute it and/or modify it under the terms of the GNU
+ * General Public License (GPL) as published by the Free Software
+ * Foundation, in version 2 as it comes in the "COPYING" file of the
+ * VirtualBox OSE distribution. VirtualBox OSE is distributed in the
+ * hope that it will be useful, but WITHOUT ANY WARRANTY of any kind.
+ */
+
+#ifndef __UIMediumEnumerator_h__
+#define __UIMediumEnumerator_h__
+
+/* Qt includes: */
+#include <QObject>
+
+/* GUI includes: */
+#include "UIMedium.h"
+
+/* Forward declarations: */
+class UIThreadPool;
+class UITask;
+
+/* Medium-enumerator prototype.
+ * Manages access to medium information using thread-pool interface. */
+class UIMediumEnumerator : public QObject
+{
+    Q_OBJECT;
+
+signals:
+
+    /* Notifiers: Medium-operations stuff: */
+    void sigMediumCreated(QString strMediumID);
+    void sigMediumUpdated(QString strMediumID);
+    void sigMediumDeleted(QString strMediumID);
+
+    /* Notifiers: Medium-enumeration stuff: */
+    void sigMediumEnumerationStarted();
+    void sigMediumEnumerated(QString strMediumID);
+    void sigMediumEnumerationFinished();
+
+public:
+
+    /* Constructor/destructor: */
+    UIMediumEnumerator(ulong uWorkerCount = 3, ulong uWorkerTimeout = 5000);
+    ~UIMediumEnumerator();
+
+    /* API: Medium-access stuff: */
+    QList<QString> mediumIDs() const;
+    UIMedium medium(const QString &strMediumID);
+    void createMedium(const UIMedium &medium);
+    void updateMedium(const UIMedium &medium);
+    void deleteMedium(const QString &strMediumID);
+
+    /* API: Medium-enumeration stuff: */
+    bool isMediumEnumerationInProgress() const { return m_fMediumEnumerationInProgress; }
+    void enumerateMediums();
+
+private slots:
+
+    /* Handler: Machine stuff: */
+    void sltHandleMachineUpdate(QString strMachineID);
+
+    /* Handler: Medium-enumeration stuff: */
+    void sltHandleMediumEnumerationTaskComplete(UITask *pTask);
+
+private:
+
+    /* Helpers: Medium-enumeration stuff: */
+    void createMediumEnumerationTask(const UIMedium &medium);
+    void addNullMediumToMap(UIMediumMap &mediums);
+    void addMediumsToMap(const CMediumVector &inputMediums, UIMediumMap &outputMediums, UIMediumType mediumType);
+    void addHardDisksToMap(const CMediumVector &inputMediums, UIMediumMap &outputMediums);
+
+    /* Variables: */
+    UIThreadPool *m_pThreadPool;
+    bool m_fMediumEnumerationInProgress;
+    QList<UITask*> m_tasks;
+    UIMediumMap m_mediums;
+};
+
+#endif /* __UIMediumEnumerator_h__ */
