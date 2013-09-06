6f28874ad25b45e60422750698e77212813a2abf
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2013-09-06 03:16:32 +0000

    Main/DNS: general host specific DNS and Co handling added. Darwin implementation has self-update mechanism, for other hosts it's in todo list.
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@48330 cfe28804-0f27-0410-a406-dd0f0b0b656f

---
+++ src/VBox/Main/src-server/HostDnsService.cpp
@@ -0,0 +1,105 @@
+/* $Id$
+/** @file
+ * Base class fo Host DNS & Co services.
+ */
+
+/*
+ * Copyright (C) 2013 Oracle Corporation
+ *
+ * This file is part of VirtualBox Open Source Edition (OSE), as
+ * available from http://www.virtualbox.org. This file is free software;
+ * you can redistribute it and/or modify it under the terms of the GNU
+ * General Public License (GPL) as published by the Free Software
+ * Foundation, in version 2 as it comes in the "COPYING" file of the
+ * VirtualBox OSE distribution. VirtualBox OSE is distributed in the
+ * hope that it will be useful, but WITHOUT ANY WARRANTY of any kind.
+ */
+
+#include <VBox/com/array.h>
+#include <VBox/com/ptr.h>
+#include <VBox/com/string.h>
+
+#include "HostDnsService.h"
+#include <iprt/thread.h>
+#include <iprt/semaphore.h>
+
+HostDnsService::HostDnsService(){}
+
+HostDnsService::~HostDnsService () 
+{
+    int rc = RTCritSectDelete(&m_hCritSect);
+    AssertRC(rc);
+}
+
+
+HRESULT HostDnsService::init (void) 
+{
+    int rc = RTCritSectInit(&m_hCritSect);
+    AssertRCReturn(rc, E_FAIL);
+    return S_OK;
+}
+
+HRESULT HostDnsService::start()
+{
+    return S_OK;
+}
+
+void HostDnsService::stop()
+{
+}
+
+HRESULT HostDnsService::update()
+{
+    return S_OK;
+}
+
+STDMETHODIMP HostDnsService::COMGETTER(NameServers)(ComSafeArrayOut(BSTR, aNameServers))
+{
+    RTCritSectEnter(&m_hCritSect);
+    com::SafeArray<BSTR> nameServers(m_llNameServers.size());
+
+    Utf8StrListIterator it;
+    int i = 0;
+    for (it = m_llNameServers.begin(); it != m_llNameServers.end(); ++it, ++i)
+        (*it).cloneTo(&nameServers[i]);
+
+    nameServers.detachTo(ComSafeArrayOutArg(aNameServers));
+
+    RTCritSectLeave(&m_hCritSect);
+
+    return S_OK;
+}
+
+
+STDMETHODIMP HostDnsService::COMGETTER(DomainName)(BSTR *aDomainName)
+{
+    RTCritSectEnter(&m_hCritSect);
+
+    m_DomainName.cloneTo(aDomainName);
+
+    RTCritSectLeave(&m_hCritSect);
+
+    return S_OK;
+}
+
+
+STDMETHODIMP HostDnsService::COMGETTER(SearchStrings)(ComSafeArrayOut(BSTR, aSearchStrings))
+{
+    RTCritSectEnter(&m_hCritSect);
+
+    com::SafeArray<BSTR> searchString(m_llSearchStrings.size());
+
+    Utf8StrListIterator it;
+    int i = 0;
+    for (it = m_llSearchStrings.begin(); it != m_llSearchStrings.end(); ++it, ++i)
+        (*it).cloneTo(&searchString[i]);
+
+
+    searchString.detachTo(ComSafeArrayOutArg(aSearchStrings));
+
+    RTCritSectLeave(&m_hCritSect);
+
+    return S_OK;
+}
+
+
---
+++ src/VBox/Main/src-server/HostDnsServiceResolvConf.cpp
@@ -0,0 +1,133 @@
+/* -*- indent-tabs-mode: nil; -*- */
+#include <VBox/com/string.h>
+#include <VBox/com/ptr.h>
+
+#include "HostDnsService.h"
+
+#ifdef RT_OS_OS2
+# include <sys/socket.h>
+typedef int socklen_t;
+#endif
+
+#include <stdio.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+
+
+#include <iprt/assert.h>
+#include <iprt/err.h>
+#include <iprt/file.h>
+
+#include <VBox/log.h>
+
+
+static int fileGets(RTFILE File, void *pvBuf, size_t cbBufSize, size_t *pcbRead)
+{
+    size_t cbRead;
+    char bTest;
+    int rc = VERR_NO_MEMORY;
+    char *pu8Buf = (char *)pvBuf;
+    *pcbRead = 0;
+
+    while (   RT_SUCCESS(rc = RTFileRead(File, &bTest, 1, &cbRead))
+           && (pu8Buf - (char *)pvBuf) >= 0   
+           && (size_t)(pu8Buf - (char *)pvBuf) < cbBufSize)
+    {
+        if (cbRead == 0)
+            return VERR_EOF;
+
+        if (bTest == '\r' || bTest == '\n')
+        {
+            *pu8Buf = 0;
+            return VINF_SUCCESS;
+        }
+        *pu8Buf = bTest;
+         pu8Buf++;
+        (*pcbRead)++;
+    }
+    return rc;
+}
+
+
+HostDnsServiceResolvConf::HostDnsServiceResolvConf(const char* aResolvConfFilename)
+{
+    m_ResolvConfFilename = com::Utf8Str(aResolvConfFilename);
+}
+
+
+HostDnsServiceResolvConf::~HostDnsServiceResolvConf()
+{
+    m_ResolvConfFilename.setNull();
+    RTFileClose(m_ResolvConfFile);
+}
+
+HRESULT HostDnsServiceResolvConf::init()
+{
+    HRESULT hrc;
+
+    int rc = RTFileOpen(&m_ResolvConfFile, m_ResolvConfFilename.c_str(), 
+                        RTFILE_O_READ | RTFILE_O_OPEN | RTFILE_O_DENY_NONE);
+    AssertRCReturn(rc, E_FAIL);
+
+
+    hrc = HostDnsService::init();
+    AssertComRCReturn(hrc, hrc);
+
+
+
+    hrc = update();
+    AssertComRCReturn(hrc, hrc);
+
+    return S_OK;
+}
+
+
+HRESULT HostDnsServiceResolvConf::update()
+{
+    char buff[256];
+    char buff2[256];
+    int cNameserversFound = 0;
+    bool fWarnTooManyDnsServers = false;
+    struct in_addr tmp_addr;
+    int rc;
+    size_t bytes;
+
+    while (    RT_SUCCESS(rc = fileGets(m_ResolvConfFile, buff, sizeof(buff), &bytes))
+            && rc != VERR_EOF)
+    {
+        if (   cNameserversFound == 4
+            && !fWarnTooManyDnsServers
+            && sscanf(buff, "nameserver%*[ \t]%255s", buff2) == 1)
+        {
+            fWarnTooManyDnsServers = true;
+            LogRel(("NAT: too many nameservers registered.\n"));
+        }
+        if (   sscanf(buff, "nameserver%*[ \t]%255s", buff2) == 1
+            && cNameserversFound < 4) /* Unix doesn't accept more than 4 name servers*/
+        {
+            if (!inet_aton(buff2, &tmp_addr))
+                continue;
+
+	    m_llNameServers.push_back(com::Utf8Str(buff2));
+
+            cNameserversFound++;
+        }
+        if ((!strncmp(buff, "domain", 6) || !strncmp(buff, "search", 6)))
+        {
+            char *tok;
+            char *saveptr;
+
+            tok = strtok_r(&buff[6], " \t\n", &saveptr);
+	    
+	    if (tok != NULL)
+		m_DomainName = com::Utf8Str(tok);
+        }
+    }
+
+    if (!cNameserversFound)
+        return E_FAIL;
+
+    return S_OK;
+
+}
