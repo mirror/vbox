00a41acc29f0fd376e85e31f121384f0c2b1cc30
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2013-09-06 11:38:20 +0000

    inverted #ifdefs.
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@48349 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Main/src-client/win/dllmain.cpp
+++ src/VBox/Main/src-client/win/dllmain.cpp
@@ -50,7 +50,7 @@ END_OBJECT_MAP()
 #endif
 
 
-#ifndef WITH_MANUAL_CLEANUP
+#ifdef WITH_MANUAL_CLEANUP
 /** Type library GUIDs to clean up manually. */
 static const char * const g_apszTypelibGuids[] =
 {
@@ -77,7 +77,7 @@ static const char * const g_apszTypelibVersions[] =
 /*******************************************************************************
 *   Internal Functions                                                         *
 *******************************************************************************/
-#ifndef WITH_MANUAL_CLEANUP
+#ifdef WITH_MANUAL_CLEANUP
 static void removeOldMess(void);
 #endif
 
@@ -134,13 +134,13 @@ STDAPI DllRegisterServer(void)
 STDAPI DllUnregisterServer(void)
 {
     HRESULT hrc = _Module.UnregisterServer(TRUE);
-#ifndef WITH_MANUAL_CLEANUP
+#ifdef WITH_MANUAL_CLEANUP
     removeOldMess();
 #endif
     return hrc;
 }
 
-#ifndef WITH_MANUAL_CLEANUP
+#ifdef WITH_MANUAL_CLEANUP
 
 /**
  * Checks if the typelib GUID is one of the ones we wish to clean up.
