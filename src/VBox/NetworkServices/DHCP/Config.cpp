0cd499031ba3927880a73cb9e47479b0efbdece5
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2013-09-11 05:04:47 +0000

    DHCP: moves implementation from header to source file.
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@48420 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/NetworkServices/DHCP/Config.cpp
+++ src/VBox/NetworkServices/DHCP/Config.cpp
@@ -42,7 +42,39 @@ static ConfigurationManager *g_ConfigurationManager = ConfigurationManager::getC
 
 static NetworkManager *g_NetworkManager = NetworkManager::getNetworkManager();
 
+int BaseConfigEntity::match(Client& client, BaseConfigEntity **cfg)
+{
+        int iMatch = (m_criteria && m_criteria->check(client)? m_MatchLevel: 0);
+        if (m_children.empty())
+        {
+            if (iMatch > 0)
+            {
+                *cfg = this;
+                return iMatch;
+            }
+        }
+        else
+        {
+            *cfg = this;
+            /* XXX: hack */
+            BaseConfigEntity *matching = this;
+            int matchingLevel = m_MatchLevel;
 
+            for (std::vector<BaseConfigEntity *>::iterator it = m_children.begin();
+                 it != m_children.end();
+                 ++it)
+            {
+                iMatch = (*it)->match(client, &matching);
+                if (iMatch > matchingLevel)
+                {
+                    *cfg = matching;
+                    matchingLevel = iMatch;
+                }
+            }
+            return matchingLevel;
+        }
+        return iMatch;
+}
 
 /* Client */
 
@@ -350,6 +382,53 @@ HostConfigEntity *ConfigurationManager::addHost(NetworkConfigEntity* pCfg,
     id++;
 
     return new HostConfigEntity(address, strname, pCfg, criteria);
+}
+
+int ConfigurationManager::addToAddressList(uint8_t u8OptId, RTNETADDRIPV4& address)
+{
+    switch(u8OptId)
+    {
+        case RTNET_DHCP_OPT_DNS:
+            m_nameservers.push_back(address);
+            break;
+        case RTNET_DHCP_OPT_ROUTERS:
+            m_routers.push_back(address);
+            break;
+        default:
+            Log(("dhcp-opt: list (%d) unsupported\n", u8OptId));
+    }
+    return VINF_SUCCESS;
+}
+
+int ConfigurationManager::flushAddressList(uint8_t u8OptId)
+{
+    switch(u8OptId)
+    {
+        case RTNET_DHCP_OPT_DNS:
+            m_nameservers.clear();
+            break;
+        case RTNET_DHCP_OPT_ROUTERS:
+            m_routers.clear();
+            break;
+        default:
+            Log(("dhcp-opt: list (%d) unsupported\n", u8OptId));
+    }
+    return VINF_SUCCESS;
+}
+
+const Ipv4AddressContainer& ConfigurationManager::getAddressList(uint8_t u8OptId)
+{
+    switch(u8OptId)
+    {
+        case RTNET_DHCP_OPT_DNS:
+            return m_nameservers;
+
+        case RTNET_DHCP_OPT_ROUTERS:
+            return m_routers;
+
+    }
+    /* XXX: Grrr !!! */
+    return m_empty;
 }
 
 /**
