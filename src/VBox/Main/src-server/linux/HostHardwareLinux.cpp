49003f8f9c79474fc2f805ec54c781092983b82a
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2013-09-11 11:32:46 +0000

    Main: remove unnecessary includes
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@48424 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Main/src-server/linux/HostHardwareLinux.cpp
+++ src/VBox/Main/src-server/linux/HostHardwareLinux.cpp
@@ -6,7 +6,7 @@
  */
 
 /*
- * Copyright (C) 2008-2011 Oracle Corporation
+ * Copyright (C) 2008-2013 Oracle Corporation
  *
  * This file is part of VirtualBox Open Source Edition (OSE), as
  * available from http://www.virtualbox.org. This file is free software;
@@ -37,7 +37,6 @@
 #include <iprt/param.h>
 #include <iprt/path.h>
 #include <iprt/string.h>
-#include <iprt/thread.h>  /* for RTThreadSleep() */
 
 #include <linux/cdrom.h>
 #include <linux/fd.h>
