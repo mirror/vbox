05aa6eb96bee53c7884d76d6f8b44ee2bd9962c6
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2013-09-13 14:17:58 +0000

    VMM/HMVMXR0: Try restore state properly on assertions in longjmps contexts. Reevaluate FPU state after injecting #NM. Other cosmetics.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@48473 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/VMM/VMMRZ/VMMRZ.cpp
+++ src/VBox/VMM/VMMRZ/VMMRZ.cpp
@@ -86,8 +86,7 @@ VMMRZDECL(int) VMMRZCallRing3(PVM pVM, PVMCPU pVCpu, VMMCALLRING3 enmOperation, 
     pVM->vmm.s.pfnRCToHost(VINF_VMM_CALL_HOST);
 #else
     int rc;
-    if (   pVCpu->vmm.s.pfnCallRing3CallbackR0
-        && enmOperation != VMMCALLRING3_VM_R0_ASSERTION)
+    if (pVCpu->vmm.s.pfnCallRing3CallbackR0)
     {
         rc = pVCpu->vmm.s.pfnCallRing3CallbackR0(pVCpu, enmOperation, pVCpu->vmm.s.pvCallRing3CallbackUserR0);
         if (RT_FAILURE(rc))
