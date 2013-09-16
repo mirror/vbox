647f7596acce4775a336c2c53cb6c67754d611cf
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2013-09-16 15:42:47 +0000

    NAT/Slirp:slirp_init: prevent loading host's DNS details in case of host resolver.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@48493 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Devices/Network/slirp/slirp.c
+++ src/VBox/Devices/Network/slirp/slirp.c
@@ -352,8 +352,13 @@ int slirp_init(PNATState *ppData, uint32_t u32NetAddr, uint32_t u32Netmask,
 
     /* set default addresses */
     inet_aton("127.0.0.1", &loopback_addr);
-    rc = slirpInitializeDnsSettings(pData);
-    AssertRCReturn(rc, VINF_NAT_DNS);
+    
+    if (!fUseHostResolver)
+    {
+        rc = slirpInitializeDnsSettings(pData);
+        AssertRCReturn(rc, VINF_NAT_DNS);
+    }
+
     rc = slirpTftpInit(pData);
     AssertRCReturn(rc, VINF_NAT_DNS);
 
