812f19897cc8c77701caac7c1483a795bf05da94
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2013-09-20 12:57:03 +0000

    FE/Qt: Machine settings / Display page / Video-capture tab: Use whats-this property instead of tool-tip one for *screens* control.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@48583 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Frontends/VirtualBox/src/widgets/UIFilmContainer.cpp
+++ src/VBox/Frontends/VirtualBox/src/widgets/UIFilmContainer.cpp
@@ -140,7 +140,7 @@ void UIFilm::retranslateUi()
 {
     /* Translate check-box: */
     m_pCheckBox->setText(QApplication::translate("UIMachineSettingsDisplay", "Screen %1").arg(m_iScreenIndex + 1));
-    m_pCheckBox->setToolTip(QApplication::translate("UIMachineSettingsDisplay", "Enable video recording for screen %1.").arg(m_iScreenIndex + 1));
+    m_pCheckBox->setWhatsThis(QApplication::translate("UIMachineSettingsDisplay", "Enable video recording for screen %1.").arg(m_iScreenIndex + 1));
 }
 
 void UIFilm::prepare()
