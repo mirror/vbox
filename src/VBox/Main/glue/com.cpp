de994fd23a8b110e1bf5a61d39f936017b28c162
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2013-09-24 12:21:27 +0000

    Main: Added KBUILD_TYPE to the release log.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@48654 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Main/glue/com.cpp
+++ src/VBox/Main/glue/com.cpp
@@ -66,7 +66,7 @@ char szXdgConfigHome[RTPATH_MAX] = "";
  * create a folder corresponding to the last in the list (the least
  * legacy) if none do.
  */
-const char *const apcszUserHome[] = 
+const char *const apcszUserHome[] =
 #ifdef RT_OS_DARWIN
 { "Library/VirtualBox" };
 #elif defined RT_OS_WINDOWS
@@ -296,6 +296,7 @@ static void vboxHeaderFooter(PRTLOGGER pReleaseLogger, RTLOGPHASE enmPhase, PFNR
                    g_pszLogEntity, VBOX_VERSION_STRING, RTBldCfgRevision(),
                    RTBldCfgTargetDotArch(), __DATE__, __TIME__, szTmp);
 
+            pfnLog(pReleaseLogger, "Build Type: %s\n", KBUILD_TYPE);
             int vrc = RTSystemQueryOSInfo(RTSYSOSINFO_PRODUCT, szTmp, sizeof(szTmp));
             if (RT_SUCCESS(vrc) || vrc == VERR_BUFFER_OVERFLOW)
                 pfnLog(pReleaseLogger, "OS Product: %s\n", szTmp);
