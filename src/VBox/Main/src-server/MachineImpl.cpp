a006f5f27524c782a525c3843ad3e7271e00862a
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2013-09-26 14:11:23 +0000

    SessionMachine::init: lockWrite(LOCKVAL_SRC_POS_DECL).
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@48713 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Main/src-server/MachineImpl.cpp
+++ src/VBox/Main/src-server/MachineImpl.cpp
@@ -12782,7 +12782,7 @@ HRESULT SessionMachine::init(Machine *aMachine)
             {
                 aMachine->lockHandle()->unlockWrite();
                 mParent->natNetworkRefInc(name.raw());
-                aMachine->lockHandle()->lockWrite();
+                aMachine->lockHandle()->lockWrite(LOCKVAL_SRC_POS_DECL);
             }
         }
     }
