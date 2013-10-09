9c6c83f88a7559dfe367dbf591647ceca4ef2613
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2013-10-09 07:05:31 +0000

    Main/NetworkAdapterImpl.cpp: Avoids lock drops and re-acquirings on NAT Networking changes in NetworkAdapter::COMSETTER(NATNetwork)(...) and NetworkAdapter::COMSETTER(AttachmentType)(...)
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@48989 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Main/include/NetworkAdapterImpl.h
+++ src/VBox/Main/include/NetworkAdapterImpl.h
@@ -154,7 +154,7 @@ private:
     void generateMACAddress();
     HRESULT updateMacAddress(Utf8Str aMacAddress);
     void updateBandwidthGroup(BandwidthGroup *aBwGroup);
-    HRESULT checkAndSwitchFromNatNetworking();
+    HRESULT checkAndSwitchFromNatNetworking(IN_BSTR aNatNetworkName);
     HRESULT switchToNatNetworking(IN_BSTR aNatNetworkName);
 
     Machine * const     mParent;
