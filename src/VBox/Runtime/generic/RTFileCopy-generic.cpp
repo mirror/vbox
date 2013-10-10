30a7d57cd1bcbf50d3a3d27db7ace17e9fd59574
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2013-10-10 16:43:53 +0000

    removed bad docs.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@49034 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Runtime/generic/RTFileCopy-generic.cpp
+++ src/VBox/Runtime/generic/RTFileCopy-generic.cpp
@@ -4,7 +4,7 @@
  */
 
 /*
- * Copyright (C) 2006-2010 Oracle Corporation
+ * Copyright (C) 2006-2013 Oracle Corporation
  *
  * This file is part of VirtualBox Open Source Edition (OSE), as
  * available from http://www.virtualbox.org. This file is free software;
@@ -32,16 +32,6 @@
 #include "internal/iprt.h"
 
 
-/**
- * Copies a file.
- *
- * @returns VERR_ALREADY_EXISTS if the destination file exists.
- * @returns VBox Status code.
- *
- * @param   pszSrc      The path to the source file.
- * @param   pszDst      The path to the destination file.
- *                      This file will be created.
- */
 RTDECL(int) RTFileCopy(const char *pszSrc, const char *pszDst)
 {
     return RTFileCopyEx(pszSrc, pszDst, 0, NULL, NULL);
