1246febd3b3f05eaf3a76ed901936cc122274020
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2013-10-15 23:08:23 +0000

    Fix TAB/spaces in previous.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@49125 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/NetworkServices/NAT/VBoxNetLwipNAT.cpp
+++ src/VBox/NetworkServices/NAT/VBoxNetLwipNAT.cpp
@@ -300,7 +300,7 @@ STDMETHODIMP VBoxNetLwipNAT::HandleEvent(VBoxEventType_T aEventType,
                 goto port_forward_done;
             }
 
-	    r.Pfr.fPfrIPv6 = fIPv6FW;
+            r.Pfr.fPfrIPv6 = fIPv6FW;
 
             switch (proto)
             {
