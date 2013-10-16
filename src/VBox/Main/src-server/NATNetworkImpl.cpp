bebb25b64dba9ba9e7c05e45accb52063693651e
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2013-10-16 12:03:12 +0000

    Main/NATNetworkImpl.cpp: update comment in  NATNetwork::COMSETTER(Network)(IN_BSTR) and NATNetwork::COMSETTER(IPv6Prefix) (IN_BSTR) about ignoring new value if there're registered port-forwarding rules. in TODO part of the comment how to resolve the conflict with different internal representation (guest address should be offset from network id, insted direct address, albeit additional check against network bounds should be added then).
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@49131 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Main/src-server/NATNetworkImpl.cpp
+++ src/VBox/Main/src-server/NATNetworkImpl.cpp
@@ -372,7 +372,10 @@ STDMETHODIMP NATNetwork::COMSETTER(Network)(IN_BSTR aIPv4NetworkCidr)
     if (aIPv4NetworkCidr == m->IPv4NetworkCidr)
         return S_OK;
         
-    /* silently ignore network cidr update */
+    /* silently ignore network cidr update for now. 
+     * todo: keep internally guest address of port forward rule 
+     * as offset from network id.
+     */
     if (m->mapName2PortForwardRule4.empty())
     {
 
@@ -444,7 +447,9 @@ STDMETHODIMP NATNetwork::COMSETTER(IPv6Prefix) (IN_BSTR aIPv6Prefix)
     if (aIPv6Prefix == m->IPv6Prefix)
         return S_OK;
 
-    /* silently ignore network cidr update */
+    /* silently ignore network IPv6 prefix update. 
+     * todo: see similar todo in NATNetwork::COMSETTER(Network)(IN_BSTR)
+     */
     if (m->mapName2PortForwardRule6.empty())
     {
 
