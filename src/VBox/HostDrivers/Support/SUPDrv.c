29442686b4b52fc08318d4b9a600032c4c3232b8
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2013-10-18 12:19:17 +0000

    SUPDrv: Doxygen for r90051.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@49179 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/HostDrivers/Support/SUPDrv.c
+++ src/VBox/HostDrivers/Support/SUPDrv.c
@@ -3372,6 +3372,7 @@ SUPR0DECL(void) SUPR0ResumeVTxOnCpu(bool fSuspended)
  * @retval  VERR_VMX_NO_VMX
  * @retval  VERR_VMX_MSR_SMX_VMXON_DISABLED
  * @retval  VERR_VMX_MSR_VMXON_DISABLED
+ * @retval  VERR_VMX_MSR_LOCKING_FAILED
  * @retval  VERR_SVM_NO_SVM
  * @retval  VERR_SVM_DISABLED
  * @retval  VERR_UNSUPPORTED_CPU if not identifiable as an AMD, Intel or VIA
