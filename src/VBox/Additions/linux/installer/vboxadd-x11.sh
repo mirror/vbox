9a6ae5d467058d9d03ca430d1fea88c23311d224
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2013-10-20 15:06:35 +0000

    Additions/linux: fix X11 driver installation for old 1.5 X.Org servers.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@49196 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Additions/linux/installer/vboxadd-x11.sh
+++ src/VBox/Additions/linux/installer/vboxadd-x11.sh
@@ -375,9 +375,12 @@ setup()
             xserver_version="X.Org Server 1.5"
             vboxvideo_src=vboxvideo_drv_15.so
             vboxmouse_src=vboxmouse_drv_15.so
-            # SUSE with X.Org 1.5 is another special case, and is also
-            # handled specially
-            test "$system" = "suse" && automouse=""
+            # Historical note: SUSE with X.Org Server 1.5 disabled automatic
+            # mouse configuration and was handled specially.  However since our
+            # kernel driver seems to have problems with X.Org Server 1.5 anyway
+            # we just create an X.Org configuration file and use the user space
+            # one generally, no longer just for SUSE.
+            automouse=""
             ;;
         1.4.* )
             xserver_version="X.Org Server 1.4"
@@ -498,8 +501,8 @@ $generated
 
 EOF
         cat << EOF
-You may need to restart the hal service and the Window System (or just restart
-the guest system) to enable the Guest Additions.
+You may need to restart the the Window System (or just restart the guest system)
+to enable the Guest Additions.
 
 EOF
     fi
