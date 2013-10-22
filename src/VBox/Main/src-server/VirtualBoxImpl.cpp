e843bacfac0fd6c8c146d2a1dfe0eaaa08d02b71
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2013-10-22 06:25:04 +0000

    VirtualBox.cpp: VirtualBox::natNetworkRefInc and VirtualBox::natNetworkRefDec don't annoy anyone with assertion anymore, instead functions return -1.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@49219 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Main/src-server/VirtualBoxImpl.cpp
+++ src/VBox/Main/src-server/VirtualBoxImpl.cpp
@@ -3110,7 +3110,7 @@ int VirtualBox::natNetworkRefInc(IN_BSTR aNetworkName)
     {
         ComPtr<INATNetwork> nat;
         HRESULT rc = FindNATNetworkByName(aNetworkName, nat.asOutParam());
-        AssertComRCReturn(rc, -1);
+        if (FAILED(rc)) return -1;
 
         rc = nat->Start(Bstr("whatever").raw());
         if (SUCCEEDED(rc))
@@ -3140,7 +3140,7 @@ int VirtualBox::natNetworkRefDec(IN_BSTR aNetworkName)
     {
         ComPtr<INATNetwork> nat;
         HRESULT rc = FindNATNetworkByName(aNetworkName, nat.asOutParam());
-        AssertComRCReturn(rc, -1);
+        if (FAILED(rc)) return -1;
 
         rc = nat->Stop();
         if (SUCCEEDED(rc))
