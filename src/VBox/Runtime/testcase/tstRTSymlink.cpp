6c23c3cc1e9af0411eaa36f845bba6ad6b9efb13
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2013-10-29 10:33:52 +0000

    Runtime/testcase: adapted tstRTSymlink to r89826
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@49318 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Runtime/testcase/tstRTSymlink.cpp
+++ src/VBox/Runtime/testcase/tstRTSymlink.cpp
@@ -47,6 +47,21 @@ static void test1Worker(RTTEST hTest, const char *pszBaseDir,
     char    szPath1[RTPATH_MAX];
     char    szPath2[RTPATH_MAX];
     size_t  cchTarget = strlen(pszTarget);
+    char    szPath3[RTPATH_MAX];
+
+    RTStrCopy(szPath3, sizeof(szPath3), pszTarget);
+
+#ifdef RT_OS_WINDOWS
+    /* see RTSymlinkCreate in symlink-win.cpp */
+    char c;
+    char *psz = szPath3;
+    while ((c = *psz) != '\0')
+    {
+        if (c == '/')
+            *psz = '\\';
+        psz++;
+    }
+#endif
 
     /* Create it.*/
     RTTESTI_CHECK_RC_OK_RETV(RTPathJoin(szPath1, sizeof(szPath1), pszBaseDir, "tstRTSymlink-link-1"));
@@ -61,19 +76,19 @@ static void test1Worker(RTTEST hTest, const char *pszBaseDir,
     memset(szPath2, 0xff, sizeof(szPath2));
     szPath2[sizeof(szPath2) - 1] = '\0';
     RTTESTI_CHECK_RC(RTSymlinkRead(szPath1, szPath2, sizeof(szPath2), 0), VINF_SUCCESS);
-    RTTESTI_CHECK_MSG(strcmp(szPath2, pszTarget) == 0, ("got=\"%s\" expected=\"%s\"", szPath2, pszTarget));
+    RTTESTI_CHECK_MSG(strcmp(szPath2, szPath3) == 0, ("got=\"%s\" expected=\"%s\"", szPath2, szPath3));
 
     memset(szPath2, 0xff, sizeof(szPath2));
     szPath2[sizeof(szPath2) - 1] = '\0';
     RTTESTI_CHECK_RC(RTSymlinkRead(szPath1, szPath2, cchTarget + 1, 0), VINF_SUCCESS);
-    RTTESTI_CHECK_MSG(strcmp(szPath2, pszTarget) == 0, ("got=\"%s\" expected=\"%s\"", szPath2, pszTarget));
+    RTTESTI_CHECK_MSG(strcmp(szPath2, szPath3) == 0, ("got=\"%s\" expected=\"%s\"", szPath2, szPath3));
 
     memset(szPath2, 0xff, sizeof(szPath2));
     szPath2[sizeof(szPath2) - 1] = '\0';
     RTTESTI_CHECK_RC(RTSymlinkRead(szPath1, szPath2, cchTarget, 0), VERR_BUFFER_OVERFLOW);
-    RTTESTI_CHECK_MSG(   strncmp(szPath2, pszTarget, cchTarget - 1) == 0
+    RTTESTI_CHECK_MSG(   strncmp(szPath2, szPath3, cchTarget - 1) == 0
                       && szPath2[cchTarget - 1] == '\0',
-                      ("got=\"%s\" expected=\"%.*s\"", szPath2, cchTarget - 1, pszTarget));
+                      ("got=\"%s\" expected=\"%.*s\"", szPath2, cchTarget - 1, szPath3));
 
     /* Other APIs that have to handle symlinks carefully. */
     int rc;
