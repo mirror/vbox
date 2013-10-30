c98004335e75291e2a2601a8f9603a0694ec96a6
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2013-10-30 17:37:09 +0000

    export to OSE
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@49339 cfe28804-0f27-0410-a406-dd0f0b0b656f

---
+++ src/VBox/NetworkServices/NetLib/shared_ptr.h
@@ -0,0 +1,81 @@
+#ifndef __SHARED_PTR_H__
+#define __SHARED_PTR_H__
+
+#ifdef __cplusplus
+template<typename T> 
+class SharedPtr
+{
+    struct imp 
+    {
+        imp(T *pTrg = NULL, int cnt = 1): ptr(pTrg),refcnt(cnt){}
+        ~imp() { if (ptr) delete ptr;}
+
+        T *ptr;
+        int refcnt;
+    };
+
+    
+    public:
+    SharedPtr(T *t = NULL):p(NULL)
+    {
+        p = new imp(t);
+    }
+    
+    ~SharedPtr()
+    {
+        p->refcnt--;
+
+        if (p->refcnt == 0)
+            delete p;
+    }
+
+
+    SharedPtr(const SharedPtr& rhs)
+    {
+        p = rhs.p;
+        p->refcnt++;
+    }
+
+    const SharedPtr& operator= (const SharedPtr& rhs)
+    {
+        if (p == rhs.p) return *this;
+        
+        p->refcnt--;
+        if (p->refcnt == 0) 
+            delete p;
+
+        p = rhs.p;
+        p->refcnt++;
+
+        return *this;
+    }
+
+
+    T *get() const
+    {
+        return p->ptr;
+    }
+
+    
+    T *operator->()
+    {
+        return p->ptr;
+    }
+
+    
+    const T*operator->() const
+    {
+        return p->ptr;
+    }
+
+
+    int use_count()
+    {
+        return p->refcnt;
+    }
+
+    private:
+    imp *p;
+};
+#endif
+#endif
