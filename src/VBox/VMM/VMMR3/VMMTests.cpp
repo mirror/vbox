05cbeab85e30995e3b4697ffad697223a2a10779
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2013-11-02 23:42:45 +0000

    testing...
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@49370 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/VMM/VMMR3/VMMTests.cpp
+++ src/VBox/VMM/VMMR3/VMMTests.cpp
@@ -158,8 +158,10 @@ static int vmmR3DoMsrQuickReport(PVM pVM, PRTSTREAM pReportStrm, bool fWithCpuId
         vmmR3ReportMsrRange(pVM, 0x40000000, 0x00012000, pReportStrm, &cMsrsFound),
         vmmR3ReportMsrRange(pVM, 0x80000000, 0x00012000, pReportStrm, &cMsrsFound),
 //        vmmR3ReportMsrRange(pVM, 0xc0000000, 0x00102000, pReportStrm, &cMsrsFound),
-        vmmR3ReportMsrRange(pVM, 0xc0000000, 0x00010000, pReportStrm, &cMsrsFound),
-        vmmR3ReportMsrRange(pVM, 0xc0010000, 0x00002000, pReportStrm, &cMsrsFound),
+//        vmmR3ReportMsrRange(pVM, 0xc0000000, 0x00010000, pReportStrm, &cMsrsFound),
+RTStrmFlush(g_pStdOut); RTThreadSleep(16000);
+        vmmR3ReportMsrRange(pVM, 0xc0010000, 0x00001000, pReportStrm, &cMsrsFound),
+RTStrmFlush(g_pStdOut);
     };
     int rc = VINF_SUCCESS;
     for (unsigned i = 0; i < RT_ELEMENTS(aRc); i++)
