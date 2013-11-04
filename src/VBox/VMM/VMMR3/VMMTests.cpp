a0deadc5206d6add7d0b4ea374a213f89e841ac6
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2013-11-04 04:37:06 +0000

    testing...
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@49376 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/VMM/VMMR3/VMMTests.cpp
+++ src/VBox/VMM/VMMR3/VMMTests.cpp
@@ -161,6 +161,7 @@ static int vmmR3DoMsrQuickReport(PVM pVM, PRTSTREAM pReportStrm, bool fWithCpuId
 //      { 0xc0000000, 0x00022000 },
         { 0xc0000000, 0x00010000 },
         { 0xc0010000, 0x00001040 },
+        { 0xc0011040, 0x00000040 }, /* should cause trouble... */
     };
     uint32_t cMsrsFound = 0;
     int rc = VINF_SUCCESS;
