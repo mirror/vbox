0690b4724adc250c005c34ce42ffe53c97d39b91
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2013-11-20 07:35:28 +0000

    DHCP/Config.h reverts occidental commit in r90758.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@49565 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/NetworkServices/DHCP/Config.h
+++ src/VBox/NetworkServices/DHCP/Config.h
@@ -502,6 +502,10 @@ class NetworkManager
 public:
     static NetworkManager *getNetworkManager();
 
+    int offer4Client(const Client& lease, uint32_t u32Xid, uint8_t *pu8ReqList, int cReqList);
+    int ack(const Client& lease, uint32_t u32Xid, uint8_t *pu8ReqList, int cReqList);
+    int nak(const Client& lease, uint32_t u32Xid);
+
     const RTNETADDRIPV4& getOurAddress() const;
     const RTNETADDRIPV4& getOurNetmask() const;
     const RTMAC& getOurMac() const;
@@ -521,10 +525,6 @@ public:
 private:
     NetworkManager();
     ~NetworkManager();
-
-    int offer4Client(const Client& lease, uint32_t u32Xid, uint8_t *pu8ReqList, int cReqList);
-    int ack(const Client& lease, uint32_t u32Xid, uint8_t *pu8ReqList, int cReqList);
-    int nak(const Client& lease, uint32_t u32Xid);
 
     int prepareReplyPacket4Client(const Client& client, uint32_t u32Xid);
     int doReply(const Client& client, const std::vector<RawOption>& extra);
