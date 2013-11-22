40670f9d07f7ea861c8c5a8728b2a9bc6c5d5dd5
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2013-11-22 16:19:16 +0000

    Main/GuestCtrl: Todo, logging.
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@49630 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Main/src-client/GuestSessionImplTasks.cpp
+++ src/VBox/Main/src-client/GuestSessionImplTasks.cpp
@@ -599,7 +599,7 @@ int SessionTaskCopyFrom::Run(void)
      * Note: There will be races between querying file size + reading the guest file's
      *       content because we currently *do not* lock down the guest file when doing the
      *       actual operations.
-     ** @todo Implement guest file locking!
+     ** @todo Use the IGuestFile API for locking down the file on the guest!
      */
     GuestFsObjData objData; int guestRc;
     int rc = pSession->fileQueryInfoInternal(Utf8Str(mSource), objData, &guestRc);
