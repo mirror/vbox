b868e1f5b59c9471e82ff2da3c5ccdc61008ae58
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2013-11-26 16:24:05 +0000

    VMM/HMVMXR0: Comment.
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@49667 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/VMM/VMMR0/HMVMXR0.cpp
+++ src/VBox/VMM/VMMR0/HMVMXR0.cpp
@@ -5862,7 +5862,7 @@ static int hmR0VmxSaveGuestLazyMsrs(PVMCPU pVCpu, PCPUMCTX pMixedCtx)
     }
     else
     {
-        /* Darwin 32-bit/PAE kernels. */
+        /* Darwin 32-bit/PAE kernel or 64-bit host running 32-bit guest. */
         pVCpu->hm.s.vmx.fUpdatedGuestState |= HMVMX_UPDATED_GUEST_LAZY_MSRS;
     }
 #else   /* HC_ARCH_BITS == 32 */
