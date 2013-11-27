9768d83595bc6ad3ae8c3c33ad6d7e26a449bfa7
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2013-11-27 17:41:15 +0000

    VBoxNetAdp/Darwin: Submit a dummy packet when host-only interface gets created (#7046)
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@49686 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/HostDrivers/VBoxNetAdp/darwin/VBoxNetAdp-darwin.cpp
+++ src/VBox/HostDrivers/VBoxNetAdp/darwin/VBoxNetAdp-darwin.cpp
@@ -232,6 +232,58 @@ static errno_t vboxNetAdpDarwinBpfSend(ifnet_t pIface, u_int32_t uLinkType, mbuf
     return 0;
 }
 
+
+/**
+ * Constructs and submits a dummy packet to ifnet_input(). This is a workaround
+ * for "stuck dock icon" issue. When the first packet goes through the interface
+ * DLIL grabs a reference to the thread that submits the packet and holds it
+ * until the interface is destroyed. By submitting this dummy we make DLIL grab
+ * the thread of a non-GUI process.
+ *
+ * Most of this function was copied from vboxNetFltDarwinMBufFromSG().
+ *
+ * @returns VBox status code.
+ * @param   pIfNet      The interface that will hold the reference to the calling
+ *                      thread. We submit dummy as if it was coming from this interface.
+ */
+static int vboxNetSendDummy(ifnet_t pIfNet)
+{
+    int rc = 0;
+    size_t cbTotal = 50; /* No Ethernet header */
+    mbuf_how_t How = MBUF_WAITOK;
+
+    mbuf_t pPkt = NULL;
+    errno_t err = mbuf_allocpacket(How, cbTotal, NULL, &pPkt);
+    if (!err)
+    {
+        /* Skip zero sized memory buffers (paranoia). */
+        mbuf_t pCur = pPkt;
+        while (pCur && !mbuf_maxlen(pCur))
+            pCur = mbuf_next(pCur);
+        Assert(pCur);
+
+        /* Set the required packet header attributes. */
+        mbuf_pkthdr_setlen(pPkt, cbTotal);
+        mbuf_pkthdr_setheader(pPkt, mbuf_data(pCur));
+
+        mbuf_setlen(pCur, cbTotal);
+        memset(mbuf_data(pCur), 0, cbTotal);
+
+        mbuf_pkthdr_setrcvif(pPkt, pIfNet); /* will crash without this. */
+
+        errno_t err = ifnet_input(pIfNet, pPkt, NULL);
+        if (err)
+        {
+            rc = RTErrConvertFromErrno(err);
+            mbuf_freem(pPkt);
+        }
+    }
+    else
+        rc = RTErrConvertFromErrno(err);
+
+    return rc;
+}
+
 int vboxNetAdpOsCreate(PVBOXNETADP pThis, PCRTMAC pMACAddress)
 {
     int rc;
@@ -294,6 +346,7 @@ int vboxNetAdpOsCreate(PVBOXNETADP pThis, PCRTMAC pMACAddress)
             if (!err)
             {
                 ifnet_set_mtu(pThis->u.s.pIface, VBOXNETADP_MTU);
+                vboxNetSendDummy(pThis->u.s.pIface);
                 return VINF_SUCCESS;
             }
             else
