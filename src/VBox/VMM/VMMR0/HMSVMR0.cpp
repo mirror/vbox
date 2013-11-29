6593e5842cfb80f5bfb2c248bb0e1e422bbc2614
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2013-11-29 11:04:07 +0000

    VMM/HMSVMR0: Added bugref comment.
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@49720 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/VMM/VMMR0/HMSVMR0.cpp
+++ src/VBox/VMM/VMMR0/HMSVMR0.cpp
@@ -5011,13 +5011,13 @@ HMSVM_EXIT_DECL hmR0SvmExitXcptMF(PVMCPU pVCpu, PCPUMCTX pCtx, PSVMTRANSIENT pSv
 
     if (!(pCtx->cr0 & X86_CR0_NE))
     {
-        PVM         pVM  = pVCpu->CTX_SUFF(pVM);
-        PDISSTATE   pDis = &pVCpu->hm.s.DisState;
-        unsigned    cbOp;
+        PVM       pVM  = pVCpu->CTX_SUFF(pVM);
+        PDISSTATE pDis = &pVCpu->hm.s.DisState;
+        unsigned  cbOp;
         int rc = EMInterpretDisasCurrent(pVM, pVCpu, pDis, &cbOp);
         if (RT_SUCCESS(rc))
         {
-            /* Convert a #MF into a FERR -> IRQ 13. */
+            /* Convert a #MF into a FERR -> IRQ 13. See @bugref{6117}. */
             rc = PDMIsaSetIrq(pVCpu->CTX_SUFF(pVM), 13, 1, 0 /*uTagSrc*/);
             if (RT_SUCCESS(rc))
                 pCtx->rip += cbOp;
