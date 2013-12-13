3aaa2758d2982d2de7039a6a63350d5548f745a8
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2013-12-13 00:34:26 +0000

    avoid the strcmp dependency here.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@49892 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Frontends/VirtualBox/src/hardenedmain.cpp
+++ src/VBox/Frontends/VirtualBox/src/hardenedmain.cpp
@@ -4,7 +4,7 @@
  */
 
 /*
- * Copyright (C) 2008-2010 Oracle Corporation
+ * Copyright (C) 2008-2013 Oracle Corporation
  *
  * This file is part of VirtualBox Open Source Edition (OSE), as
  * available from http://www.virtualbox.org. This file is free software;
@@ -15,8 +15,28 @@
  * hope that it will be useful, but WITHOUT ANY WARRANTY of any kind.
  */
 
-#include <string.h>
 #include <VBox/sup.h>
+
+
+/**
+ * No CRT on windows, so cook our own strcmp.
+ *
+ * @returns See man strcmp.
+ * @param   psz1                The first string.
+ * @param   psz2                The second string.
+ */
+static int MyStrCmp(const char *psz1, const char *psz2)
+{
+    for (;;)
+    {
+        char ch1 = *psz1++;
+        char ch2 = *psz2++;
+        if (ch1 != ch2)
+            return ch1 < ch2 ? -1 : 1;
+        if (!ch1)
+            return 0;
+    }
+}
 
 
 int main(int argc, char **argv, char **envp)
@@ -29,8 +49,8 @@ int main(int argc, char **argv, char **envp)
         /* NOTE: the check here must match the corresponding check for the
          * options to start a VM in main.cpp and VBoxGlobal.cpp exactly,
          * otherwise there will be weird error messages. */
-        if (   !::strcmp(argv[i], "--startvm")
-            || !::strcmp(argv[i], "-startvm"))
+        if (   !MyStrCmp(argv[i], "--startvm")
+            || !MyStrCmp(argv[i], "-startvm"))
         {
             fFlags &= ~SUPSECMAIN_FLAGS_DONT_OPEN_DEV;
             break;
