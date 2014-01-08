36cfb3ef199a24f00bc1d2f92e1718f55d16b070
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2014-01-08 15:34:00 +0000

    EHCI: Fix race between frame thread and completion callback which resulted in inconsistent queue heads and hanging transfers. Make the maximum framerate configurable for easier testing, the default is still 1KHz but can be set to the real frequency of 8KHz
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@50034 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Devices/testcase/tstDeviceStructSizeRC.cpp
+++ src/VBox/Devices/testcase/tstDeviceStructSizeRC.cpp
@@ -1086,6 +1086,7 @@ int main()
     GEN_CHECK_OFF(EHCI, hSemEventFrame);
     GEN_CHECK_OFF(EHCI, fBusStarted);
     GEN_CHECK_OFF(EHCI, CsIrq);
+    GEN_CHECK_OFF(EHCI, uFrameRateDefault);
     GEN_CHECK_OFF(EHCI, nsWait);
     GEN_CHECK_OFF(EHCI, CritSect);
 # endif /* VBOX_WITH_EHCI_IMPL */
