3ee2c1fa3cf97e140d30213619bcc98aad9c8a2a
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2014-01-10 17:47:57 +0000

    Solaris/Installer: Fix typo in vboxconfig.sh which breaks S12.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@50054 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Installer/solaris/vboxconfig.sh
+++ src/VBox/Installer/solaris/vboxconfig.sh
@@ -259,7 +259,7 @@ get_sysinfo()
                     # the build number.
                     BRANCH_VERSION=STR_KERN_MAJOR
                     HOST_OS_MAJORVERSION=`echo "$BRANCH_VERSION" | cut -f2 -d'-' | cut -f1,2 -d'.'`
-                    if test "HOST_OS_MAJORVERSION" = "5.12"; then
+                    if test "$HOST_OS_MAJORVERSION" = "5.12"; then
                         HOST_OS_MINORVERSION=`echo "$BRANCH_VERSION" | cut -f2 -d'-' | cut -f6 -d'.'`
                         return 0
                     else
