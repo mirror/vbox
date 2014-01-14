c9b14442625d84218b14cf9b12912b644d43907f
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2014-01-14 09:36:56 +0000

    Runtime/r0drv/linux: Use __GFP_NOWARN for our memory allocations as the callers are handling out-of-memory conditions correctly. Warnings are especially not helpful when trying to allocate 2M contiguous pages as we come back trying to allocate separate pages in this case.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@50077 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Runtime/r0drv/linux/alloc-r0drv-linux.c
+++ src/VBox/Runtime/r0drv/linux/alloc-r0drv-linux.c
@@ -180,7 +180,7 @@ static PRTMEMHDR rtR0MemAllocExecVmArea(size_t cb)
 
     for (iPage = 0; iPage < cPages; iPage++)
     {
-        papPages[iPage] = alloc_page(GFP_KERNEL | __GFP_HIGHMEM);
+        papPages[iPage] = alloc_page(GFP_KERNEL | __GFP_HIGHMEM | __GFP_NOWARN);
         if (!papPages[iPage])
             break;
     }
@@ -390,15 +390,15 @@ RTR0DECL(void *) RTMemContAlloc(PRTCCPHYS pPhys, size_t cb)
     cOrder = CalcPowerOf2Order(cPages);
 #if (defined(RT_ARCH_AMD64) || defined(CONFIG_X86_PAE)) && defined(GFP_DMA32)
     /* ZONE_DMA32: 0-4GB */
-    paPages = alloc_pages(GFP_DMA32, cOrder);
+    paPages = alloc_pages(GFP_DMA32 | __GFP_NOWARN, cOrder);
     if (!paPages)
 #endif
 #ifdef RT_ARCH_AMD64
         /* ZONE_DMA; 0-16MB */
-        paPages = alloc_pages(GFP_DMA, cOrder);
+        paPages = alloc_pages(GFP_DMA | __GFP_NOWARN, cOrder);
 #else
         /* ZONE_NORMAL: 0-896MB */
-        paPages = alloc_pages(GFP_USER, cOrder);
+        paPages = alloc_pages(GFP_USER | __GFP_NOWARN, cOrder);
 #endif
     if (paPages)
     {
