9a840369f6ae099bc979ef7ea80dc524c2976d02
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2014-01-20 14:09:00 +0000

    DevVGA: fix regression from r91599
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@50119 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Devices/Graphics/DevVGA.cpp
+++ src/VBox/Devices/Graphics/DevVGA.cpp
@@ -2357,12 +2357,11 @@ static int vga_draw_graphic(PVGASTATE pThis, bool full_update, bool fFailOnResiz
          * Can happen if the resize request was ignored by the driver.
          */
         if (   pThis->pDrv->cx != (uint32_t)width
-            || pThis->pDrv->cy != (uint32_t)height
-            || pThis->pDrv->cBits != (uint32_t)bits)
+            || pThis->pDrv->cy != (uint32_t)height)
         {
-            Log(("Framebuffer mismatch: vga %dx%d@%d, drv %dx%d@%d!!!\n",
-                 width, height, bits,
-                 pThis->pDrv->cx, pThis->pDrv->cy, pThis->pDrv->cBits));
+            LogRel(("Framebuffer mismatch: vga %dx%d, drv %dx%d!!!\n",
+                    width, height,
+                    pThis->pDrv->cx, pThis->pDrv->cy));
             return VINF_SUCCESS;
         }
     }
