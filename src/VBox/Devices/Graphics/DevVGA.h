34ee5dc7a893559c6522e8fa6aa62239e6e66ece
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2014-01-20 17:07:43 +0000

    32-bit build fix
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@50126 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Devices/Graphics/DevVGA.h
+++ src/VBox/Devices/Graphics/DevVGA.h
@@ -323,6 +323,9 @@ typedef struct VGAState {
         R3PTRTYPE(void *)           p3dState;
         /** R3 Opaque pointer to a copy of the first 32k of the framebuffer before switching into svga mode. */
         R3PTRTYPE(void *)           pFrameBufferBackup;
+#if HC_ARCH_BITS == 32
+        uint32_t                    Padding3;
+#endif
         /** Guest physical address of the FIFO memory range. */
         RTGCPHYS                    GCPhysFIFO;
         /** Size in bytes of the FIFO memory range. */
