53b29f7a73b9dac8033bd0efba75d9003e7b4c3a
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2014-01-20 08:20:28 +0000

    Additions/linux: cosmetic: report missing X11 as "skipping", not "failure".
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@50107 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Additions/linux/installer/vboxadd-x11.sh
+++ src/VBox/Additions/linux/installer/vboxadd-x11.sh
@@ -316,7 +316,11 @@ setup()
     fi
 
     test -z "$x_version" -o -z "$modules_dir" &&
-        fail "Could not find the X.Org or XFree86 Window System."
+        {
+            echo
+            echo "Could not find the X.Org or XFree86 Window System, skipping."
+            exit 0
+        }
 
     echo
     # openSUSE 10.3 shipped X.Org 7.2 with X.Org Server 1.3, but didn't
