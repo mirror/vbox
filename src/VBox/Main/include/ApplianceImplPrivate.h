71bdcdbdc5e738d8e396aed149d0a726a7ca30c3
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2014-01-24 09:34:27 +0000

    header fixes
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@50215 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Main/include/ApplianceImplPrivate.h
+++ src/VBox/Main/include/ApplianceImplPrivate.h
@@ -2,7 +2,8 @@
  * VirtualBox Appliance private data definitions
  */
 
-/* Copyright (C) 2006-2013 Oracle Corporation
+/*
+ * Copyright (C) 2006-2013 Oracle Corporation
  *
  * This file is part of VirtualBox Open Source Edition (OSE), as
  * available from http://www.virtualbox.org. This file is free software;
