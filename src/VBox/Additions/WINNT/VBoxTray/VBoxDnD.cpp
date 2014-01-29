caf68e6ec8721829e712880f8aff80d2baaf00fc
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2014-01-29 11:20:15 +0000

    Build fix; guest->host support not enabled by default yet.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@50266 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Additions/WINNT/VBoxTray/VBoxDnD.cpp
+++ src/VBox/Additions/WINNT/VBoxTray/VBoxDnD.cpp
@@ -228,10 +228,11 @@ int VBoxDnDWnd::Thread(RTTHREAD hThread, void *pvUser)
 
         } while (RT_SUCCESS(rc));
 
+#ifdef VBOX_WITH_DRAG_AND_DROP_GH
         int rc2 = pThis->UnregisterAsDropTarget();
         if (RT_SUCCESS(rc))
             rc = rc2;
-
+#endif
         OleUninitialize();
     }
 
