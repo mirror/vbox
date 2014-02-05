ee80711ec1ecf06d8f3df0b5b565e3379da68e89
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2014-02-05 09:19:01 +0000

    VboxNetSendDummy() -> VBoxNetSendDummy().
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@50322 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- include/VBox/VBoxNetSend.h
+++ include/VBox/VBoxNetSend.h
@@ -57,7 +57,7 @@ RT_C_DECLS_BEGIN
  * @param   pIfNet      The interface that will hold the reference to the calling
  *                      thread. We submit dummy as if it was coming from this interface.
  */
-inline int VboxNetSendDummy(ifnet_t pIfNet)
+inline int VBoxNetSendDummy(ifnet_t pIfNet)
 {
     int rc = 0;
     size_t cbTotal = 50; /* No Ethernet header */
