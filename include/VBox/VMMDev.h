e8be783ffa87d1e5cd5c5ca89f655e3b122e9f1e
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2014-02-19 16:59:18 +0000

    VMMDev.h: missing sanity mask for guest capabilities.
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@50511 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- include/VBox/VMMDev.h
+++ include/VBox/VMMDev.h
@@ -508,6 +508,8 @@ AssertCompileSize(VMMDevReqGuestCapabilities2, 24+8);
  * request sets this automatically, but VMMDevReq_SetGuestCapabilities does
  * not. */
 #define VMMDEV_GUEST_SUPPORTS_GRAPHICS                      RT_BIT_32(2)
+/** The mask of valid events, for sanity checking. */
+#define VMMDEV_GUEST_CAPABILITIES                           UINT32_C(0x00000007)
 /** @} */
 
 
