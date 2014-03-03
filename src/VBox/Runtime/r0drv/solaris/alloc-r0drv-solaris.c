9a3969bc446bdf1dd0a6b9b9c32316af4dd6a787
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2014-03-03 14:01:47 +0000

    Runtime/r0drv/solaris: Parameter comment.
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@50667 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Runtime/r0drv/solaris/alloc-r0drv-solaris.c
+++ src/VBox/Runtime/r0drv/solaris/alloc-r0drv-solaris.c
@@ -185,7 +185,7 @@ RTR0DECL(void *) RTMemContAlloc(PRTCCPHYS pPhys, size_t cb)
 
     /* Allocate physically contiguous (< 4GB) page-aligned memory. */
     uint64_t uPhys;
-    void *pvMem = rtR0SolMemAlloc((uint64_t)_4G - 1, &uPhys, cb, PAGESIZE, true);
+    void *pvMem = rtR0SolMemAlloc((uint64_t)_4G - 1, &uPhys, cb, PAGESIZE, true /* fContig */);
     if (RT_UNLIKELY(!pvMem))
     {
         LogRel(("RTMemContAlloc failed to allocate %u bytes\n", cb));
