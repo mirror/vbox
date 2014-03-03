227229ab162feb5802a0e62c452ce3c7d1950487
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2014-03-03 10:34:30 +0000

    Removed unnecessarily verbose logging.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@50663 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Devices/PC/BIOS/keyboard.c
+++ src/VBox/Devices/PC/BIOS/keyboard.c
@@ -474,7 +474,6 @@ void BIOSCALL int09_function(uint16_t ES, uint16_t DI, uint16_t SI, uint16_t BP,
 
     default:
         if (scancode & 0x80) {
-            BX_INFO("KBD: int09: release(?): %02X\n", scancode);
             /* Set ack/resend flags if appropriate. */
             if (scancode == 0xFA) {
                 flag = read_byte(0x0040, 0x97) | 0x10;
@@ -594,7 +593,6 @@ void BIOSCALL int16_function(volatile kbd_regs_t r)
     led_flags   = read_byte(0x0040, 0x97);
     if ((((shift_flags >> 4) & 0x07) ^ (led_flags & 0x07)) != 0) {
         int_disable();    //@todo: interrupts should be disabled already??
-        BX_INFO("KBD: int16: Setting LEDs\n");
         outb(0x60, 0xed);
         while ((inb(0x64) & 0x01) == 0) outb(0x80, 0x21);
         if ((inb(0x60) == 0xfa)) {
@@ -605,7 +603,6 @@ void BIOSCALL int16_function(volatile kbd_regs_t r)
                 outb(0x80, 0x21);
             inb(0x60);
             write_byte(0x0040, 0x97, led_flags);
-            BX_INFO("KBD: int16: LEDs set\n");
         }
         int_enable();
     }
