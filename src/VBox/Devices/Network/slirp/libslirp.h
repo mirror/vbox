7465b8ea8f0667659de34d67bb3f74125589c3e6
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2014-03-04 13:00:59 +0000

    NAT: disabled OSX-specific code (regressions)
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@50676 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Devices/Network/slirp/libslirp.h
+++ src/VBox/Devices/Network/slirp/libslirp.h
@@ -101,7 +101,7 @@ void slirp_set_somaxconn(PNATState pData, int iSoMaxConn);
  * Main API has primitives for listening DNS change event since 4.3.
  */
 #if (defined(RT_OS_DARWIN))
-# define HAVE_NOTIFICATION_FOR_DNS_UPDATE 1
+# define HAVE_NOTIFICATION_FOR_DNS_UPDATE 0
 #else
 # define HAVE_NOTIFICATION_FOR_DNS_UPDATE 0
 #endif
