f7db03fa3907104eb2262e130fec87d6119efd7e
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2014-03-07 10:11:38 +0000

    Main/Console: when changing a medium or network attachment, suspend the VM before doing the actual VMR3Req* call. Doing this in the EM context has bad side effects, see @bugref{4744} and @bugref{7283}. Also, don't dump the complete device instance but dump only the affected LUN.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@50723 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Main/src-client/ConsoleImpl2.cpp
+++ src/VBox/Main/src-client/ConsoleImpl2.cpp
@@ -2193,7 +2193,8 @@ int Console::configConstructorInner(PUVM pUVM, PVM pVM, AutoWriteLock *pAlock)
                                             false /* fForceUnmount */,
                                             false /* fHotplug */,
                                             pUVM,
-                                            paLedDevType);
+                                            paLedDevType,
+                                            NULL /* ppLunL0 */);
                 if (RT_FAILURE(rc))
                     return rc;
             }
@@ -3495,7 +3496,8 @@ int Console::configMediumAttachment(PCFGMNODE pCtlInst,
                                     bool fForceUnmount,
                                     bool fHotplug,
                                     PUVM pUVM,
-                                    DeviceType_T *paLedDevType)
+                                    DeviceType_T *paLedDevType,
+                                    PCFGMNODE *ppLunL0)
 {
     // InsertConfig* throws
     try
@@ -3609,6 +3611,8 @@ int Console::configMediumAttachment(PCFGMNODE pCtlInst,
         }
 
         InsertConfigNode(pCtlInst, Utf8StrFmt("LUN#%u", uLUN).c_str(), &pLunL0);
+        if (ppLunL0)
+            *ppLunL0 = pLunL0;
 
         PCFGMNODE pCfg = CFGMR3GetChild(pCtlInst, "Config");
         if (pCfg)
