41ca7a133be336b13783f681e130bb8d46fe8556
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2014-03-18 09:35:35 +0000

    FE/Qt: Runtime UI: Mouse-handling: Do not try to activate hovered machine-window instantly, it's causing races and conflicts with Apple HIG.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@50807 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Frontends/VirtualBox/src/runtime/UIMouseHandler.cpp
+++ src/VBox/Frontends/VirtualBox/src/runtime/UIMouseHandler.cpp
@@ -21,6 +21,7 @@
 #include <QDesktopWidget>
 #include <QMouseEvent>
 #include <QTouchEvent>
+#include <QTimer>
 
 /* GUI includes: */
 #include "VBoxGlobal.h"
@@ -596,8 +597,8 @@ bool UIMouseHandler::eventFilter(QObject *pWatched, QEvent *pEvent)
                         m_windows.values().contains(pWatchedWidget->window()) &&
                         QApplication::activeWindow() != pWatchedWidget->window())
                     {
-                        /* Activating hovered machine window: */
-                        pWatchedWidget->window()->activateWindow();
+                        /* Activating hovered machine-window in 300msec: */
+                        QTimer::singleShot(300, pWatchedWidget->window(), SLOT(sltActivateWindow()));
 #ifdef Q_WS_X11
                         /* On X11 its not enough to just activate window if you
                          * want to raise it also, so we will make it separately: */
