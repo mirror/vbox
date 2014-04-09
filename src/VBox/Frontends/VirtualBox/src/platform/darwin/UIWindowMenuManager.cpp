93c5a045a4d85514bdc626c70b2f2592f1f3d486
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2014-04-09 13:45:54 +0000

    FE/Qt: UIWindowMenuManager: Comments: Fixing some mess from r66238 and r67272.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@51012 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Frontends/VirtualBox/src/platform/darwin/UIWindowMenuManager.cpp
+++ src/VBox/Frontends/VirtualBox/src/platform/darwin/UIWindowMenuManager.cpp
@@ -15,12 +15,6 @@
  * Foundation, in version 2 as it comes in the "COPYING" file of the
  * VirtualBox OSE distribution. VirtualBox OSE is distributed in the
  * hope that it will be useful, but WITHOUT ANY WARRANTY of any kind.
- * available from hm_regWindowsp://www.virtualbox.org. This file is free software;
- * you can redistribute it and/or modify it under the terms of the GNU
- * General Public License (GPL) as published by the Free Software
- * Foundation, in version 2 as it comes in the "COPYING" file of the
- * VirtualBox OSE distribution. VirtualBox OSE is distributed in the
- * hope that it will be useful, but WITHOUT ANY WARRANTY of any kind.
  */
 
 /* Local includes */
