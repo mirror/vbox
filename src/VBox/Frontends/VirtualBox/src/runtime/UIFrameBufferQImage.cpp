f8093b3127f5129f8f4f03f38a1986fe85611e7c
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2014-04-24 11:35:48 +0000

    FE/Qt: Build fix for r93441.
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@51134 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Frontends/VirtualBox/src/runtime/UIFrameBufferQImage.cpp
+++ src/VBox/Frontends/VirtualBox/src/runtime/UIFrameBufferQImage.cpp
@@ -317,7 +317,9 @@ void UIFrameBufferQImage::drawImageRect(QPainter &painter, const QImage &image, 
         /* Scale sub-pixmap (2nd copy involved): */
         subPixmap = subPixmap.scaled(subPixmap.size() * dBackingScaleFactor,
                                      Qt::IgnoreAspectRatio, Qt::FastTransformation);
+# ifdef Q_WS_MAC
         subPixmap.setDevicePixelRatio(dBackingScaleFactor);
+# endif /* Q_WS_MAC */
     }
 
     /* Draw sub-pixmap: */
@@ -329,7 +331,9 @@ void UIFrameBufferQImage::drawImageRect(QPainter &painter, const QImage &image, 
         /* Create scaled-sub-image (1st copy involved): */
         QImage scaledSubImage = subImage.scaled(subImage.size() * dBackingScaleFactor,
                                                 Qt::IgnoreAspectRatio, Qt::FastTransformation);
+# ifdef Q_WS_MAC
         scaledSubImage.setDevicePixelRatio(dBackingScaleFactor);
+# endif /* Q_WS_MAC */
         /* Directly draw scaled-sub-image: */
         painter.drawImage(rect.x(), rect.y(), scaledSubImage);
     }
