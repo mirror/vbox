67b135798f92fdf4911f515f18ff5d0d6da0e842
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2014-05-28 12:25:36 +0000

    extended todo a little.
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@51445 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Main/src-helper-apps/VBoxExtPackHelperApp.cpp
+++ src/VBox/Main/src-helper-apps/VBoxExtPackHelperApp.cpp
@@ -294,7 +294,7 @@ static RTEXITCODE SetExtPackPermissions(const char *pszDir)
      if (RT_FAILURE(rc))
          return RTMsgErrorExit(RTEXITCODE_FAILURE, "Failed to set directory permissions: %Rrc ('%s')", rc, pszDir);
 #else
-        /** @todo  */
+     /** @todo TrustedInstaller? */
 #endif
 
     return RTEXITCODE_SUCCESS;
