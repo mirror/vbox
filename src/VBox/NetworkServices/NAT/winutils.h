ab42b74b5b4fe421810a145b25d7ab81763f61be
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2014-06-10 00:41:42 +0000

    NAT/Net: On Windows provide all "Berkeley" WSAE* error codes with their unixy names.  They conflict with "POSIX supplement" error codes from <errno.h>, but we don't expect to ever encounter the latter in the proxy code.  Note that this change doesn't redefine short names for low-numbered WSAE* codes that would conflict with regular CRT - those will be dealt with separately.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@51583 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/NetworkServices/NAT/winutils.h
+++ src/VBox/NetworkServices/NAT/winutils.h
@@ -41,13 +41,70 @@
 /**
  * "Windows Sockets Error Codes" obtained with WSAGetLastError().
  * http://msdn.microsoft.com/en-us/library/windows/desktop/ms740668(v=vs.85).aspx
+ *
+ * This block of error codes from <winsock2.h> conflicts with "POSIX
+ * supplement" error codes from <errno.h>, but we don't expect to ever
+ * encounter the latter in the proxy code, so redefine them to their
+ * unixy names.
  */
+#  undef  EWOULDBLOCK
+#  define EWOULDBLOCK           WSAEWOULDBLOCK
+#  undef  EINPROGRESS
+#  define EINPROGRESS           WSAEINPROGRESS
+#  undef  EALREADY
+#  define EALREADY              WSAEALREADY
+#  undef  ENOTSOCK
+#  define ENOTSOCK              WSAENOTSOCK
+#  undef  EDESTADDRREQ
+#  define EDESTADDRREQ          WSAEDESTADDRREQ
 #  undef  EMSGSIZE
 #  define EMSGSIZE              WSAEMSGSIZE
+#  undef  EPROTOTYPE
+#  define EPROTOTYPE            WSAEPROTOTYPE
+#  undef  ENOPROTOOPT
+#  define ENOPROTOOPT           WSAENOPROTOOPT
+#  undef  EPROTONOSUPPORT
+#  define EPROTONOSUPPORT       WSAEPROTONOSUPPORT
+#  undef  ESOCKTNOSUPPORT
+#  define ESOCKTNOSUPPORT       WSAESOCKTNOSUPPORT
+#  undef  EOPNOTSUPP
+#  define EOPNOTSUPP            WSAEOPNOTSUPP
+#  undef  EPFNOSUPPORT
+#  define EPFNOSUPPORT          WSAEPFNOSUPPORT
+#  undef  EAFNOSUPPORT
+#  define EAFNOSUPPORT          WSAEAFNOSUPPORT
+#  undef  EADDRINUSE
+#  define EADDRINUSE            WSAEADDRINUSE
+#  undef  EADDRNOTAVAIL
+#  define EADDRNOTAVAIL         WSAEADDRNOTAVAIL
 #  undef  ENETDOWN
 #  define ENETDOWN              WSAENETDOWN
 #  undef  ENETUNREACH
 #  define ENETUNREACH           WSAENETUNREACH
+#  undef  ENETRESET
+#  define ENETRESET             WSAENETRESET
+#  undef  ECONNABORTED
+#  define ECONNABORTED          WSAECONNABORTED
+#  undef  ECONNRESET
+#  define ECONNRESET            WSAECONNRESET
+#  undef  ENOBUFS
+#  define ENOBUFS               WSAENOBUFS
+#  undef  EISCONN
+#  define EISCONN               WSAEISCONN
+#  undef  ENOTCONN
+#  define ENOTCONN              WSAENOTCONN
+#  undef  ESHUTDOWN
+#  define ESHUTDOWN             WSAESHUTDOWN
+#  undef  ETOOMANYREFS
+#  define ETOOMANYREFS          WSAETOOMANYREFS
+#  undef  ETIMEDOUT
+#  define ETIMEDOUT             WSAETIMEDOUT
+#  undef  ECONNREFUSED
+#  define ECONNREFUSED          WSAECONNREFUSED
+#  undef  ELOOP
+#  define ELOOP                 WSAELOOP
+#  undef  ENAMETOOLONG
+#  define ENAMETOOLONG          WSAENAMETOOLONG
 #  undef  EHOSTDOWN
 #  define EHOSTDOWN             WSAEHOSTDOWN
 #  undef  EHOSTUNREACH
