e6bf219ed011f12b7a67c0198664218dec1a06b4
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2014-06-12 11:40:18 +0000

    Old 4NT versions cannot handle ProgramFiles(x86), changed the test.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@51611 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Main/src-all/win/comregister.cmd
+++ src/VBox/Main/src-all/win/comregister.cmd
@@ -94,8 +94,8 @@ REM
 REM Check for 64-bitness.
 REM
 set fIs64BitWindows=0
-if not "%ProgramW6432%x"      == "x" set fIs64BitWindows=1
-if not "%ProgramFiles(x86)%x" == "x" set fIs64BitWindows=1
+if not "%ProgramW6432%x" == "x" set fIs64BitWindows=1
+if exist "%windir\syswow64\kernel32.dll" set fIs64BitWindows=1
 
 REM
 REM Figure out the Windows version as the proxy stub requires 6.0 or later (at least for 64-bit).
