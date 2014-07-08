b56c3041f5bf6c618174eb7c83f8e5cd55e590f7
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2014-07-08 07:38:54 +0000

    Additions/WDDM: additional checks
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@51920 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Additions/WINNT/Graphics/Video/mp/wddm/VBoxMPWddm.cpp
+++ src/VBox/Additions/WINNT/Graphics/Video/mp/wddm/VBoxMPWddm.cpp
@@ -48,7 +48,8 @@ PVOID vboxWddmMemAlloc(IN SIZE_T cbSize)
 PVOID vboxWddmMemAllocZero(IN SIZE_T cbSize)
 {
     PVOID pvMem = vboxWddmMemAlloc(cbSize);
-    memset(pvMem, 0, cbSize);
+    if (pvMem)
+        memset(pvMem, 0, cbSize);
     return pvMem;
 }
 
@@ -2321,6 +2322,11 @@ NTSTATUS APIENTRY DxgkDdiCreateDevice(
     vboxVDbgBreakFv();
 
     PVBOXWDDM_DEVICE pDevice = (PVBOXWDDM_DEVICE)vboxWddmMemAllocZero(sizeof (VBOXWDDM_DEVICE));
+    if (!pDevice)
+    {
+        WARN(("vboxWddmMemAllocZero failed for WDDM device structure"));
+        return STATUS_NO_MEMORY;
+    }
     pCreateDevice->hDevice = pDevice;
     if (pCreateDevice->Flags.SystemDevice)
         pDevice->enmType = VBOXWDDM_DEVICE_TYPE_SYSTEM;
