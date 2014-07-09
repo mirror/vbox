ca3ab68d630da2995b07ac3b10e87dc70203738b
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2014-07-09 11:51:56 +0000

    FE/Qt: Fix for r94855.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@51952 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Frontends/VirtualBox/src/extradata/UIExtraDataManager.cpp
+++ src/VBox/Frontends/VirtualBox/src/extradata/UIExtraDataManager.cpp
@@ -1749,7 +1749,6 @@ QStringList UIExtraDataManagerWindow::knownExtraDataKeys()
            << GUI_MachineWindowIcons << GUI_MachineWindowNamePostfix
 #endif /* !Q_WS_MAC */
            << GUI_LastNormalWindowPosition << GUI_LastScaleWindowPosition
-           << GUI_Geometry_State_Max
            << GUI_RestrictedRuntimeMenus
 #ifdef Q_WS_MAC
            << GUI_RestrictedRuntimeApplicationMenuActions
