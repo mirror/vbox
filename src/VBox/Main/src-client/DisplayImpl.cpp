58c7d5bb24f72e42bd7472f022eac868e4aa577d
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2014-07-11 10:51:20 +0000

    Main: fix mode switch issue
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@51984 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Main/src-client/DisplayImpl.cpp
+++ src/VBox/Main/src-client/DisplayImpl.cpp
@@ -2396,6 +2396,31 @@ STDMETHODIMP Display::DetachFramebuffer(ULONG aScreenId)
 
     pFBInfo->pFramebuffer.setNull();
 
+    alock.release();
+
+#if defined(VBOX_WITH_HGCM) && defined(VBOX_WITH_CROGL)
+    Console::SafeVMPtrQuiet ptrVM(mParent);
+    if (ptrVM.isOk())
+    {
+        BOOL fIs3DEnabled = FALSE;
+        mParent->i_machine()->COMGETTER(Accelerate3DEnabled)(&fIs3DEnabled);
+
+        if (fIs3DEnabled)
+        {
+            VBOXCRCMDCTL_HGCM data;
+            RT_ZERO(data);
+            data.Hdr.enmType = VBOXCRCMDCTL_TYPE_HGCM;
+            data.Hdr.u32Function = SHCRGL_HOST_FN_SCREEN_CHANGED;
+
+            data.aParms[0].type = VBOX_HGCM_SVC_PARM_32BIT;
+            data.aParms[0].u.uint32 = aScreenId;
+
+            int vrc = crCtlSubmitSync(&data.Hdr, sizeof(data));
+            AssertRC(vrc);
+        }
+    }
+#endif /* defined(VBOX_WITH_HGCM) && defined(VBOX_WITH_CROGL) */
+
     return S_OK;
 }
 
@@ -3648,7 +3673,7 @@ int Display::crViewportNotify(ULONG aScreenId, ULONG x, ULONG y, ULONG width, UL
     pData->aParms[4].type = VBOX_HGCM_SVC_PARM_32BIT;
     pData->aParms[4].u.uint32 = height;
 
-    return crCtlSubmitSyncIfHasDataForScreen(aScreenId, &pData->Hdr, cbData);
+    return crCtlSubmitSyncIfHasDataForScreen(aScreenId, &pData->Hdr, (uint32_t)cbData);
 }
 #endif
 
