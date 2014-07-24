3204ba949d6c9fa276981d0f24798d254ded4344
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2014-07-24 16:49:08 +0000

    build fix
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@52173 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/HostDrivers/Support/win/SUPR3HardenedMain-win.cpp
+++ src/VBox/HostDrivers/Support/win/SUPR3HardenedMain-win.cpp
@@ -1732,7 +1732,6 @@ DECLINLINE(PIMAGE_NT_HEADERS) supR3HardNtPuChFindNtHeaders(uint8_t *pbBuf, size_
 }
 
 
-#ifdef DEBUG
 static uint32_t supR3HardNtPuChFindFirstDiff(void const *pvBuf1, void const *pvBuf2, size_t cbBuf)
 {
     uint8_t const *pabBuf1 = (uint8_t const *)pvBuf1;
@@ -1742,7 +1741,6 @@ static uint32_t supR3HardNtPuChFindFirstDiff(void const *pvBuf1, void const *pvB
         off++;
     return off;
 }
-#endif
 
 
 static NTSTATUS supR3HardNtPuChRestoreImageBits(PSUPR3HARDNTPUCH pThis, PVOID pvChildAddr,
