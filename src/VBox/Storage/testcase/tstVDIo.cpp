eb57bc63e35cc8e02b3b821358ff61c7c939ce58
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2014-08-19 08:23:56 +0000

    tstVDIo: Disable builtin tests on 32bit hosts, the address space is too small
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@52407 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Storage/testcase/tstVDIo.cpp
+++ src/VBox/Storage/testcase/tstVDIo.cpp
@@ -2810,6 +2810,11 @@ static void tstVDIoScriptRun(const char *pcszFilename)
  */
 static void tstVDIoRunBuiltinTests(void)
 {
+    /* 32bit hosts are excluded because of the 4GB address space. */
+#ifdef HC_ARCH_BITS == 32
+    RTStrmPrintf(g_pStdErr, "tstVDIo: Running on a 32bit host is not supported for the builtin tests, skipping\n");
+    return;
+#else
     /*
      * We need quite a bit of RAM for the builtin tests. Skip it if there
      * is not enough free RAM available.
@@ -2831,6 +2836,7 @@ static void tstVDIoRunBuiltinTests(void)
         AssertPtr(pszScript);
         tstVDIoScriptExec(pszScript);
     }
+#endif
 }
 
 /**
