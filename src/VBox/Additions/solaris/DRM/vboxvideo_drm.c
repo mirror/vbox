35eda764c23752ef1932728bc513582fdaab43ff
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2014-08-21 13:17:56 +0000

    warning
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@52441 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Additions/solaris/DRM/vboxvideo_drm.c
+++ src/VBox/Additions/solaris/DRM/vboxvideo_drm.c
@@ -184,7 +184,6 @@ static int VBoxVideoSolarisAttach(dev_info_t *pDip, ddi_attach_cmd_t enmCmd)
     LogFlow((DEVICE_NAME ":VBoxVideoSolarisAttach pDip=%p enmCmd=%d\n", pDip, enmCmd));
     cmn_err(CE_NOTE, DEVICE_NAME ":attach\n");
 
-    int rc = -1;
     switch (enmCmd)
     {
         case DDI_ATTACH:
