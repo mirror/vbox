6a88fd9998796b244f0761a6f7edc8645ffb5124
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2014-09-01 09:05:45 +0000

    Devices/Samples/DrvStorageFilter.cpp: adapt to deal properly with optional functions
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@52547 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Devices/Samples/DrvStorageFilter.cpp
+++ src/VBox/Devices/Samples/DrvStorageFilter.cpp
@@ -1,10 +1,10 @@
 /* $Id$ */
 /** @file
- * VBox Sample Driver.
+ * VBox storage filter driver sample.
  */
 
 /*
- * Copyright (C) 2012 Oracle Corporation
+ * Copyright (C) 2012-2014 Oracle Corporation
  *
  * This file is part of VirtualBox Open Source Edition (OSE), as
  * available from http://www.virtualbox.org. This file is free software;
@@ -339,6 +339,11 @@ static DECLCALLBACK(int) drvStorageFlt_Construct(PPDMDRVINS pDrvIns, PCFGMNODE p
     pThis->pIMediaAsyncBelow = PDMIBASE_QUERY_INTERFACE(pIBaseBelow, PDMIMEDIAASYNC);
 
     AssertLogRelReturn(pThis->pIMediaBelow, VERR_PDM_MISSING_INTERFACE_BELOW);
+
+    if (!pThis->pIMediaBelow->pfnDiscard)
+        pThis->IMedia.pfnDiscard = NULL;
+    if (!pThis->pIMediaAsyncBelow || !pThis->pIMediaAsyncBelow->pfnStartDiscard)
+        pThis->IMediaAsync.pfnStartDiscard = NULL;
 
     return VINF_SUCCESS;
 }
