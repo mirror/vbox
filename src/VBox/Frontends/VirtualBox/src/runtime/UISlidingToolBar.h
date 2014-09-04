94d3526f07296d0b4d0ef296e8f0e55440ba7fc4
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2014-09-04 13:12:10 +0000

    FE/Qt: Non Mac OS X build fix for r95868.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@52594 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Frontends/VirtualBox/src/runtime/UISlidingToolBar.h
+++ src/VBox/Frontends/VirtualBox/src/runtime/UISlidingToolBar.h
@@ -92,8 +92,10 @@ private:
     virtual void showEvent(QShowEvent *pEvent);
     /** Close event handler. */
     virtual void closeEvent(QCloseEvent *pEvent);
+#ifdef Q_WS_MAC
     /** Common event handler. */
     virtual bool event(QEvent *pEvent);
+#endif /* Q_WS_MAC */
 
     /** Defines mdi-sub-window geometry. */
     void setWidgetGeometry(const QRect &rect);
