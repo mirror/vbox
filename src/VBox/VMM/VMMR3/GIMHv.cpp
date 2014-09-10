ec97fe90cf2f08085b373d76f453086d023710de
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2014-09-10 17:29:24 +0000

    VMM/GIM/Hyper-V: Busted -- Query nested paging info., MSR bitmap info. too early.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@52685 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/VMM/VMMR3/GIMHv.cpp
+++ src/VBox/VMM/VMMR3/GIMHv.cpp
@@ -112,6 +112,8 @@ VMMR3_INT_DECL(int) GIMR3HvInit(PVM pVM)
                          | GIM_HV_HINT_RELAX_TIME_CHECKS;
 
         /* Hypervisor capabilities; features used by the hypervisor. */
+        /** @todo This is currently busted as it's too early. See @bugref{7270}
+         *        comment 130. */
         pHv->uHyperCaps  = HMIsNestedPagingActive(pVM)   ? GIM_HV_HOST_FEAT_NESTED_PAGING : 0;
         pHv->uHyperCaps |= HMAreMsrBitmapsAvailable(pVM) ? GIM_HV_HOST_FEAT_MSR_BITMAP : 0;
     }
