1658ef368daaf0cee70d760755f80fd52fb6e653
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2014-09-25 11:16:12 +0000

    Additions/WINNT/VBoxMMR: typo, thanks PVS
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@52857 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Additions/WINNT/VBoxMMR/tsmfhook.cpp
+++ src/VBox/Additions/WINNT/VBoxMMR/tsmfhook.cpp
@@ -1257,9 +1257,7 @@ ReadTSMF(uint32_t u32ChannelHandle, uint32_t u32HGCMClientId, uint32_t u32SizeAv
         pChannel->PushBuffer(pBuffer);
     }
     else
-    {
-        delete [] pBuffer;
-    }
+        delete pBuffer;
 }
 
 DECLCALLBACK(int)
