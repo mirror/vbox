170be48be4416bde21520a0aa0bd7b0e4a7c259d
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2014-10-01 15:37:10 +0000

    FE/Qt: NLS: Forgot mnemonics for r96348.
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@52918 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Frontends/VirtualBox/src/wizards/UIWizard.cpp
+++ src/VBox/Frontends/VirtualBox/src/wizards/UIWizard.cpp
@@ -103,11 +103,11 @@ void UIWizard::retranslateUi()
     switch (m_mode)
     {
         case WizardMode_Basic:
-            setButtonText(QWizard::CustomButton1, tr("Expert Mode"));
+            setButtonText(QWizard::CustomButton1, tr("&Expert Mode"));
             button(QWizard::CustomButton1)->setToolTip(tr("Switch to <nobr><b>Expert Mode</b></nobr>, a one-page dialog for experienced users."));
             break;
         case WizardMode_Expert:
-            setButtonText(QWizard::CustomButton1, tr("Guided Mode"));
+            setButtonText(QWizard::CustomButton1, tr("&Guided Mode"));
             button(QWizard::CustomButton1)->setToolTip(tr("Switch to <nobr><b>Guided Mode</b></nobr>, a step-by-step dialog with detailed explanations."));
             break;
         default: AssertMsgFailed(("Invalid mode: %d", m_mode)); break;
