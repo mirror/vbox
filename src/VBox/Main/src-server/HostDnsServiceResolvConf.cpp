f301ee134516af90d964718da7d05a360a843894
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2014-10-02 13:26:56 +0000

    HostDnsServiceResolvConf: copyright header
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@52932 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Main/src-server/HostDnsServiceResolvConf.cpp
+++ src/VBox/Main/src-server/HostDnsServiceResolvConf.cpp
@@ -1,3 +1,20 @@
+/* $Id$ */
+/** @file
+ * Base class fo Host DNS & Co services.
+ */
+
+/*
+ * Copyright (C) 2014 Oracle Corporation
+ *
+ * This file is part of VirtualBox Open Source Edition (OSE), as
+ * available from http://www.virtualbox.org. This file is free software;
+ * you can redistribute it and/or modify it under the terms of the GNU
+ * General Public License (GPL) as published by the Free Software
+ * Foundation, in version 2 as it comes in the "COPYING" file of the
+ * VirtualBox OSE distribution. VirtualBox OSE is distributed in the
+ * hope that it will be useful, but WITHOUT ANY WARRANTY of any kind.
+ */
+
 /* -*- indent-tabs-mode: nil; -*- */
 #include <VBox/com/string.h>
 #include <VBox/com/ptr.h>
