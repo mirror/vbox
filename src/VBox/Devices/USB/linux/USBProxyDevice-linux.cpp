83bc176c8e59853757a60bc728d66b3dc036cc85
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2014-10-05 14:31:20 +0000

    USBProxyDevice-linux: Hold the critical section while splitting the URB to avoid races while modifying the lists
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@52946 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Devices/USB/linux/USBProxyDevice-linux.cpp
+++ src/VBox/Devices/USB/linux/USBProxyDevice-linux.cpp
@@ -1543,8 +1543,9 @@ static DECLCALLBACK(int) usbProxyLinuxUrbQueue(PUSBPROXYDEV pProxyDev, PVUSBURB 
         if (    errno == EINVAL
             &&  pUrb->cbData >= 8*_1K)
         {
+            rc = usbProxyLinuxUrbQueueSplit(pProxyDev, pUrbLnx, pUrb);
             RTCritSectLeave(&pDevLnx->CritSect);
-            return usbProxyLinuxUrbQueueSplit(pProxyDev, pUrbLnx, pUrb);
+            return rc;
         }
 
         Log(("usb-linux: Queue URB %p -> %d!!! type=%d ep=%#x buffer_length=%#x cTries=%d\n",
