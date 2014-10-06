6338a55517cd1e88a903ca97ef186a683c86afd6
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2014-10-06 20:16:16 +0000

    Another sanity check.
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@52965 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/HostDrivers/Support/SUPR3HardenedMain.cpp
+++ src/VBox/HostDrivers/Support/SUPR3HardenedMain.cpp
@@ -1774,11 +1774,18 @@ DECLHIDDEN(int) SUPR3HardenedMain(const char *pszProgName, uint32_t fFlags, int 
 
     /*
      * The next steps are only taken if we actually need to access the support
-     * driver.  (Already done by early VM process init.)
+     * driver.  (Already done by early process init.)
      */
     if (!(fFlags & SUPSECMAIN_FLAGS_DONT_OPEN_DEV))
     {
 #ifdef RT_OS_WINDOWS
+        /*
+         * Windows: Must have done early process init if we get here.
+         */
+        if (!g_fSupEarlyProcessInit)
+            supR3HardenedFatalMsg("SUPR3HardenedMain", kSupInitOp_Integrity, VERR_WRONG_ORDER,
+                                  "Early process init was somehow skipped.");
+
         /*
          * Windows: The second respawn.  This time we make a special arrangement
          * with vboxdrv to monitor access to the new process from its inception.
