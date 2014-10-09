e50de3dde427bce66df41280d379b21d19dcb68c
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2014-10-09 06:11:47 +0000

    FE/Qt: UIVMInfoDialog.cpp do not show the guest monitor number for single monitor
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@53006 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Frontends/VirtualBox/src/UIVMInfoDialog.cpp
+++ src/VBox/Frontends/VirtualBox/src/UIVMInfoDialog.cpp
@@ -663,7 +663,9 @@ void UIVMInfoDialog::refreshStatistics()
         for (ULONG iScreen = 0; iScreen < cGuestScreens; ++iScreen)
         {
             QString strLabel(tr("Screen Resolution"));
-            strLabel += QString(" %1").arg(iScreen + 1);
+            /* The screen number makes sense only if there are multiple monitors in the guest: */
+            if (cGuestScreens > 1)
+                strLabel += QString(" %1").arg(iScreen + 1);
             strResult += formatValue(strLabel, aResolutions[iScreen], iMaxLength);
         }
         strResult += formatValue(tr("VM Uptime"), strUptime, iMaxLength);
