a61a280162cc0d20891352ae9259f247815cac02
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2014-10-16 18:43:26 +0000

    Runtime/r3: do not use a hard-coded stack size in rtSchedRunThread(), and PTHREAD_CREATE_JOINABLE is the default anyway.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@53081 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Runtime/r3/linux/sched-linux.cpp
+++ src/VBox/Runtime/r3/linux/sched-linux.cpp
@@ -311,40 +311,23 @@ static void rtSchedNativeRestore(PSAVEDPRIORITY pSave)
 static int rtSchedRunThread(void *(*pfnThread)(void *pvArg), void *pvArg)
 {
     /*
-     * Setup thread attributes.
+     * Create the thread.
      */
-    pthread_attr_t  ThreadAttr;
-    int rc = pthread_attr_init(&ThreadAttr);
+    pthread_t Thread;
+    int rc = pthread_create(&Thread, NULL, pfnThread, pvArg);
     if (!rc)
     {
-        rc = pthread_attr_setdetachstate(&ThreadAttr, PTHREAD_CREATE_JOINABLE);
-        if (!rc)
+        /*
+         * Wait for the thread to finish.
+         */
+        void *pvRet = (void *)-1;
+        do
         {
-            rc = pthread_attr_setstacksize(&ThreadAttr, 128*1024);
-            if (!rc)
-            {
-                /*
-                 * Create the thread.
-                 */
-                pthread_t Thread;
-                rc = pthread_create(&Thread, &ThreadAttr, pfnThread, pvArg);
-                if (!rc)
-                {
-                    /*
-                     * Wait for the thread to finish.
-                     */
-                    void *pvRet = (void *)-1;
-                    do
-                    {
-                        rc = pthread_join(Thread, &pvRet);
-                    } while (errno == EINTR);
-                    if (rc)
-                        return RTErrConvertFromErrno(rc);
-                    return (int)(uintptr_t)pvRet;
-                }
-            }
-        }
-        pthread_attr_destroy(&ThreadAttr);
+            rc = pthread_join(Thread, &pvRet);
+        } while (errno == EINTR);
+        if (rc)
+            return RTErrConvertFromErrno(rc);
+        return (int)(uintptr_t)pvRet;
     }
     return RTErrConvertFromErrno(rc);
 }
