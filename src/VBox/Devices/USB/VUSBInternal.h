8ed4a37aa7e51efae6963dba080f6b7a5ede32a3
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2014-11-04 18:13:30 +0000

    Alignment.
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@53211 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Devices/USB/VUSBInternal.h
+++ src/VBox/Devices/USB/VUSBInternal.h
@@ -225,7 +225,7 @@ typedef struct VUSBDEV
     bool volatile       fWokenUp;
 #if HC_ARCH_BITS == 32
     /** Align the size to a 8 byte boundary. */
-    bool                afAlignment0[6];
+    bool                afAlignment0[2];
 #endif
 } VUSBDEV;
 AssertCompileSizeAlignment(VUSBDEV, 8);
