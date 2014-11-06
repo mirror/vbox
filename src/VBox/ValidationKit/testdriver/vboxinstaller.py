90a3efd122b8e9541a96b594bbd70a60a72ded2b
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2014-11-06 20:00:11 +0000

    Validation Kit: run network cleanup script on Server 2008 hosts as well
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@53259 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/ValidationKit/testdriver/vboxinstaller.py
+++ src/VBox/ValidationKit/testdriver/vboxinstaller.py
@@ -785,7 +785,7 @@ class VBoxInstallerTestDriver(TestDriverBase):
             reporter.addLogFile(sLogFile, 'log/uninstaller', "Verbose MSI uninstallation log file");
 
         # TEMPORARY HACK - START
-        if fRc and utils.getHostOsVersion() in ['8', '8.1', '9', '2008ServerR2', '2012Server']:
+        if fRc and utils.getHostOsVersion() in ['8', '8.1', '9', '2008Server', '2008ServerR2', '2012Server']:
             reporter.log('Peforming extra NDIS cleanup...');
             sMagicScript = os.path.abspath(os.path.join(g_ksValidationKitDir, 'testdriver', 'win-vbox-net-uninstall.ps1'));
             fRc2, _ = self._sudoExecuteSync(['powershell.exe', '-Command', 'set-executionpolicy unrestricted']);
