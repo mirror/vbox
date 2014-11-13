cbfeb3604f63c1ef951a67e288ca90005ce3121b
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2014-11-13 10:06:29 +0000

    NDIS6/NetLwf: Use new component id (oracle_VBoxNetLwf) instead of old sun_VBoxNetFlt
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@53318 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Main/src-server/HostImpl.cpp
+++ src/VBox/Main/src-server/HostImpl.cpp
@@ -670,8 +670,13 @@ HRESULT Host::getNetworkInterfaces(std::vector<ComPtr<IHostNetworkInterface> > &
         /* for the protocol-based approach for now we just get all miniports the MS_TCPIP protocol binds to */
         hr = pNc->FindComponent(L"MS_TCPIP", &pTcpIpNcc);
 #    else
-        /* for the filter-based approach we get all miniports our filter (sun_VBoxNetFlt)is bound to */
-        hr = pNc->FindComponent(L"sun_VBoxNetFlt", &pTcpIpNcc);
+        /* for the filter-based approach we get all miniports our filter (oracle_VBoxNetLwf)is bound to */
+        hr = pNc->FindComponent(L"oracle_VBoxNetLwf", &pTcpIpNcc);
+        if (hr != S_OK)
+        {
+            /* fall back to NDIS5 miniport lookup (sun_VBoxNetFlt) */
+            hr = pNc->FindComponent(L"sun_VBoxNetFlt", &pTcpIpNcc);
+        }
 #     ifndef VBOX_WITH_HARDENING
         if (hr != S_OK)
         {
@@ -735,7 +740,7 @@ HRESULT Host::getNetworkInterfaces(std::vector<ComPtr<IHostNetworkInterface> > &
         }
         else
         {
-            LogRel(("failed to get the sun_VBoxNetFlt component, error (0x%x)\n", hr));
+            LogRel(("failed to get the oracle_VBoxNetLwf(sun_VBoxNetFlt) component, error (0x%x)\n", hr));
         }
 
         VBoxNetCfgWinReleaseINetCfg(pNc, FALSE);
