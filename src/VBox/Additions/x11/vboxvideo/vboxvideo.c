be0d3007d10a4ddd4b4de78cf80e2aeb34607532
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2014-11-18 09:55:52 +0000

    Additions/x11/vboxvideo: do not disable the screen on DPMS low power.
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@53345 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Additions/x11/vboxvideo/vboxvideo.c
+++ src/VBox/Additions/x11/vboxvideo/vboxvideo.c
@@ -288,7 +288,7 @@ vbox_crtc_dpms(xf86CrtcPtr crtc, int mode)
     VBOXPtr pVBox = VBOXGetRec(crtc->scrn);
     unsigned cDisplay = (uintptr_t)crtc->driver_private;
     TRACE_LOG("cDisplay=%u, mode=%i\n", cDisplay, mode);
-    pVBox->pScreens[cDisplay].afDisabled = (mode != DPMSModeOn);
+    pVBox->pScreens[cDisplay].afDisabled = (mode == DPMSModeOff);
     /* Don't fiddle with the hardware if we are switched
      * to a virtual terminal. */
     if (!crtc->scrn->vtSema) {
