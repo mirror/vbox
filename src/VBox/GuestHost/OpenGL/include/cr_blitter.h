f525536639c998e8bc9cbd76c20ffdb79de3037e
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2014-12-21 16:26:37 +0000

    Some style cleanups (no actual changes).
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@53590 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/GuestHost/OpenGL/include/cr_blitter.h
+++ src/VBox/GuestHost/OpenGL/include/cr_blitter.h
@@ -1,10 +1,10 @@
 /* $Id$ */
-
 /** @file
- * Blitter API
+ * Blitter API.
  */
+
 /*
- * Copyright (C) 2013 Oracle Corporation
+ * Copyright (C) 2013-2014 Oracle Corporation
  *
  * This file is part of VirtualBox Open Source Edition (OSE), as
  * available from http://www.virtualbox.org. This file is free software;
@@ -14,14 +14,23 @@
  * VirtualBox OSE distribution. VirtualBox OSE is distributed in the
  * hope that it will be useful, but WITHOUT ANY WARRANTY of any kind.
  */
+
 #ifndef ___cr_blitter_h__
 #define ___cr_blitter_h__
 
 #include <iprt/cdefs.h>
 #include <iprt/asm.h>
-#include "cr_spu.h"
+#include <iprt/string.h>
 #include "cr_vreg.h"
+#ifdef IN_VMSVGA3D
+# include <iprt/assert.h>
+typedef struct TODO_VMSVGA3D_DISPATCH_TABLE SPUDispatchTable;
+# include <OpenGL/OpenGL.h>
+#else
+# include "cr_spu.h"
+#endif
 
+/** @todo r=bird: VBOXBLITTERDECL makes no sense. */
 #ifndef IN_RING0
 # define VBOXBLITTERDECL(_type) DECLEXPORT(_type)
 #else
@@ -37,16 +46,23 @@ typedef struct CR_BLITTER_IMG
     GLuint width, height;
     GLuint bpp;
     GLuint pitch;
-} CR_BLITTER_IMG, *PCR_BLITTER_IMG;
+} CR_BLITTER_IMG;
+typedef CR_BLITTER_IMG *PCR_BLITTER_IMG;
+typedef CR_BLITTER_IMG const *PCCR_BLITTER_IMG;
 
-VBOXBLITTERDECL(void) CrMClrFillImgRect(CR_BLITTER_IMG *pDst, const RTRECT *pCopyRect, uint32_t u32Color);
-VBOXBLITTERDECL(void) CrMClrFillImg(CR_BLITTER_IMG *pImg, uint32_t cRects, const RTRECT *pRects, uint32_t u32Color);
-VBOXBLITTERDECL(void) CrMBltImgRect(const CR_BLITTER_IMG *pSrc, const RTPOINT *pSrcDataPoint, bool fSrcInvert, const RTRECT *pCopyRect, CR_BLITTER_IMG *pDst);
-VBOXBLITTERDECL(void) CrMBltImg(const CR_BLITTER_IMG *pSrc, const RTPOINT *pPos, uint32_t cRects, const RTRECT *pRects, CR_BLITTER_IMG *pDst);
-VBOXBLITTERDECL(void) CrMBltImgRectScaled(const CR_BLITTER_IMG *pSrc, const RTPOINT *pPos, bool fSrcInvert, const RTRECT *pCopyRect, float strX, float strY, CR_BLITTER_IMG *pDst);
-VBOXBLITTERDECL(void) CrMBltImgScaled(const CR_BLITTER_IMG *pSrc, const RTRECTSIZE *pSrcRectSize, const RTRECT *pDstRect, uint32_t cRects, const RTRECT *pRects, CR_BLITTER_IMG *pDst);
+VBOXBLITTERDECL(void) CrMClrFillImgRect(PCR_BLITTER_IMG pDst, PCRTRECT pCopyRect, uint32_t u32Color);
+VBOXBLITTERDECL(void) CrMClrFillImg(PCR_BLITTER_IMG pImg, uint32_t cRects, PCRTRECT pRects, uint32_t u32Color);
+VBOXBLITTERDECL(void) CrMBltImgRect(PCCR_BLITTER_IMG pSrc, PCRTPOINT pSrcDataPoint, bool fSrcInvert, PCRTRECT pCopyRect,
+                                    PCR_BLITTER_IMG pDst);
+VBOXBLITTERDECL(void) CrMBltImg(PCCR_BLITTER_IMG pSrc, PCRTPOINT pPos, uint32_t cRects, PCRTRECT pRects, PCR_BLITTER_IMG pDst);
+VBOXBLITTERDECL(void) CrMBltImgRectScaled(PCCR_BLITTER_IMG pSrc, PCRTPOINT pPos, bool fSrcInvert, PCRTRECT pCopyRect,
+                                          float strX, float strY, PCR_BLITTER_IMG pDst);
+VBOXBLITTERDECL(void) CrMBltImgScaled(PCCR_BLITTER_IMG pSrc, PCRTRECTSIZE pSrcRectSize, PCRTRECT pDstRect, uint32_t cRects,
+                                      PCRTRECT pRects, PCR_BLITTER_IMG pDst);
 
-/* GLSL Cache */
+/*
+ * GLSL Cache
+ */
 typedef struct CR_GLSL_CACHE
 {
     int iGlVersion;
@@ -54,35 +70,41 @@ typedef struct CR_GLSL_CACHE
     GLuint uNoAlpha2DRectProg;
     SPUDispatchTable *pDispatch;
 } CR_GLSL_CACHE;
+typedef CR_GLSL_CACHE *PCR_GLSL_CACHE;
+typedef CR_GLSL_CACHE const *PCCR_GLSL_CACHE;
 
-DECLINLINE(void) CrGlslInit(CR_GLSL_CACHE *pCache, SPUDispatchTable *pDispatch)
+DECLINLINE(void) CrGlslInit(PCR_GLSL_CACHE pCache, SPUDispatchTable *pDispatch)
 {
-    memset(pCache, 0, sizeof (*pCache));
+    RT_ZERO(*pCache);
     pCache->pDispatch = pDispatch;
 }
 
-DECLINLINE(bool) CrGlslIsInited(const CR_GLSL_CACHE *pCache)
+DECLINLINE(bool) CrGlslIsInited(PCCR_GLSL_CACHE pCache)
 {
     return !!pCache->pDispatch;
 }
 
 /* clients should set proper context before calling these funcs */
-VBOXBLITTERDECL(bool) CrGlslIsSupported(CR_GLSL_CACHE *pCache);
-VBOXBLITTERDECL(int) CrGlslProgGenAllNoAlpha(CR_GLSL_CACHE *pCache);
-VBOXBLITTERDECL(int) CrGlslProgGenNoAlpha(CR_GLSL_CACHE *pCache, GLenum enmTexTarget);
-VBOXBLITTERDECL(int) CrGlslProgUseGenNoAlpha(CR_GLSL_CACHE *pCache, GLenum enmTexTarget);
-VBOXBLITTERDECL(int) CrGlslProgUseNoAlpha(const CR_GLSL_CACHE *pCache, GLenum enmTexTarget);
-VBOXBLITTERDECL(void) CrGlslProgClear(const CR_GLSL_CACHE *pCache);
-VBOXBLITTERDECL(bool) CrGlslNeedsCleanup(const CR_GLSL_CACHE *pCache);
-VBOXBLITTERDECL(void) CrGlslCleanup(CR_GLSL_CACHE *pCache);
-VBOXBLITTERDECL(void) CrGlslTerm(CR_GLSL_CACHE *pCache);
+VBOXBLITTERDECL(bool) CrGlslIsSupported(PCR_GLSL_CACHE pCache);
+VBOXBLITTERDECL(int)  CrGlslProgGenAllNoAlpha(PCR_GLSL_CACHE pCache);
+VBOXBLITTERDECL(int)  CrGlslProgGenNoAlpha(PCR_GLSL_CACHE pCache, GLenum enmTexTarget);
+VBOXBLITTERDECL(int)  CrGlslProgUseGenNoAlpha(PCR_GLSL_CACHE pCache, GLenum enmTexTarget);
+VBOXBLITTERDECL(int)  CrGlslProgUseNoAlpha(PCCR_GLSL_CACHE pCache, GLenum enmTexTarget);
+VBOXBLITTERDECL(void) CrGlslProgClear(PCCR_GLSL_CACHE pCache);
+VBOXBLITTERDECL(bool) CrGlslNeedsCleanup(PCCR_GLSL_CACHE pCache);
+VBOXBLITTERDECL(void) CrGlslCleanup(PCR_GLSL_CACHE pCache);
+VBOXBLITTERDECL(void) CrGlslTerm(PCR_GLSL_CACHE pCache);
 
-/* BLITTER */
+/*
+ * BLITTER
+ */
 typedef struct CR_BLITTER_BUFFER
 {
     GLuint cbBuffer;
-    GLvoid * pvBuffer;
-} CR_BLITTER_BUFFER, *PCR_BLITTER_BUFFER;
+    GLvoid *pvBuffer;
+} CR_BLITTER_BUFFER;
+typedef CR_BLITTER_BUFFER *PCR_BLITTER_BUFFER;
+typedef CR_BLITTER_BUFFER const *PCCR_BLITTER_BUFFER;
 
 typedef union CR_BLITTER_FLAGS
 {
@@ -99,11 +121,12 @@ typedef union CR_BLITTER_FLAGS
         uint32_t Reserved            : 24;
     };
     uint32_t Value;
-} CR_BLITTER_FLAGS, *PCR_BLITTER_FLAGS;
+} CR_BLITTER_FLAGS;
 
 struct CR_BLITTER;
 
-typedef DECLCALLBACK(int) FNCRBLT_BLITTER(struct CR_BLITTER *pBlitter, const VBOXVR_TEXTURE *pSrc, const RTRECT *paSrcRect, const RTRECTSIZE *pDstSize, const RTRECT *paDstRect, uint32_t cRects, uint32_t fFlags);
+typedef DECLCALLBACK(int) FNCRBLT_BLITTER(struct CR_BLITTER *pBlitter, PCVBOXVR_TEXTURE pSrc, PCRTRECT paSrcRect,
+                                          PCRTRECTSIZE pDstSize, PCRTRECT paDstRect, uint32_t cRects, uint32_t fFlags);
 typedef FNCRBLT_BLITTER *PFNCRBLT_BLITTER;
 
 typedef struct CR_BLITTER_SPUITEM
@@ -115,13 +138,17 @@ typedef struct CR_BLITTER_SPUITEM
 typedef struct CR_BLITTER_CONTEXT
 {
     CR_BLITTER_SPUITEM Base;
-} CR_BLITTER_CONTEXT, *PCR_BLITTER_CONTEXT;
+} CR_BLITTER_CONTEXT;
+typedef CR_BLITTER_CONTEXT *PCR_BLITTER_CONTEXT;
+typedef CR_BLITTER_CONTEXT const *PCCR_BLITTER_CONTEXT;
 
 typedef struct CR_BLITTER_WINDOW
 {
     CR_BLITTER_SPUITEM Base;
     GLuint width, height;
-} CR_BLITTER_WINDOW, *PCR_BLITTER_WINDOW;
+} CR_BLITTER_WINDOW;
+typedef CR_BLITTER_WINDOW *PCR_BLITTER_WINDOW;
+typedef CR_BLITTER_WINDOW const *PCCR_BLITTER_WINDOW;
 
 typedef struct CR_BLITTER
 {
@@ -136,20 +163,23 @@ typedef struct CR_BLITTER
     CR_BLITTER_CONTEXT CtxInfo;
     int32_t i32MakeCurrentUserData;
     SPUDispatchTable *pDispatch;
-    const CR_GLSL_CACHE *pGlslCache;
+    PCCR_GLSL_CACHE pGlslCache;
     CR_GLSL_CACHE LocalGlslCache;
-} CR_BLITTER, *PCR_BLITTER;
+} CR_BLITTER;
+typedef CR_BLITTER *PCR_BLITTER;
+typedef CR_BLITTER const *PCCR_BLITTER;
 
 DECLINLINE(GLboolean) CrBltIsInitialized(PCR_BLITTER pBlitter)
 {
     return !!pBlitter->pDispatch;
 }
 
-VBOXBLITTERDECL(int) CrBltInit(PCR_BLITTER pBlitter, const CR_BLITTER_CONTEXT *pCtxBase, bool fCreateNewCtx, bool fForceDrawBlt, const CR_GLSL_CACHE *pShaders, SPUDispatchTable *pDispatch);
+VBOXBLITTERDECL(int)  CrBltInit(PCR_BLITTER pBlitter, PCCR_BLITTER_CONTEXT pCtxBase, bool fCreateNewCtx,
+                                bool fForceDrawBlt, PCCR_GLSL_CACHE pShaders, SPUDispatchTable *pDispatch);
 
 VBOXBLITTERDECL(void) CrBltTerm(PCR_BLITTER pBlitter);
 
-VBOXBLITTERDECL(int) CrBltCleanup(PCR_BLITTER pBlitter);
+VBOXBLITTERDECL(int)  CrBltCleanup(PCR_BLITTER pBlitter);
 
 DECLINLINE(GLboolean) CrBltSupportsTexTex(PCR_BLITTER pBlitter)
 {
@@ -177,8 +207,9 @@ DECLINLINE(void) CrBltSetMakeCurrentUserData(PCR_BLITTER pBlitter, int32_t i32Ma
     pBlitter->i32MakeCurrentUserData = i32MakeCurrentUserData;
 }
 
-VBOXBLITTERDECL(int) CrBltMuralSetCurrentInfo(PCR_BLITTER pBlitter, const CR_BLITTER_WINDOW *pMural);
-DECLINLINE(const CR_BLITTER_WINDOW *) CrBltMuralGetCurrentInfo(PCR_BLITTER pBlitter)
+VBOXBLITTERDECL(int) CrBltMuralSetCurrentInfo(PCR_BLITTER pBlitter, PCCR_BLITTER_WINDOW pMural);
+
+DECLINLINE(PCCR_BLITTER_WINDOW) CrBltMuralGetCurrentInfo(PCR_BLITTER pBlitter)
 {
     return &pBlitter->CurrentMural;
 }
@@ -187,12 +218,14 @@ VBOXBLITTERDECL(void) CrBltCheckUpdateViewport(PCR_BLITTER pBlitter);
 
 VBOXBLITTERDECL(void) CrBltLeave(PCR_BLITTER pBlitter);
 VBOXBLITTERDECL(int) CrBltEnter(PCR_BLITTER pBlitter);
-VBOXBLITTERDECL(void) CrBltBlitTexMural(PCR_BLITTER pBlitter, bool fBb, const VBOXVR_TEXTURE *pSrc, const RTRECT *paSrcRects, const RTRECT *paDstRects, uint32_t cRects, uint32_t fFlags);
-VBOXBLITTERDECL(void) CrBltBlitTexTex(PCR_BLITTER pBlitter, const VBOXVR_TEXTURE *pSrc, const RTRECT *pSrcRect, const VBOXVR_TEXTURE *pDst, const RTRECT *pDstRect, uint32_t cRects, uint32_t fFlags);
-VBOXBLITTERDECL(int) CrBltImgGetTex(PCR_BLITTER pBlitter, const VBOXVR_TEXTURE *pSrc, GLenum enmFormat, CR_BLITTER_IMG *pDst);
+VBOXBLITTERDECL(void) CrBltBlitTexMural(PCR_BLITTER pBlitter, bool fBb, PCVBOXVR_TEXTURE pSrc, PCRTRECT paSrcRects,
+                                        PCRTRECT paDstRects, uint32_t cRects, uint32_t fFlags);
+VBOXBLITTERDECL(void) CrBltBlitTexTex(PCR_BLITTER pBlitter, PCVBOXVR_TEXTURE pSrc, PCRTRECT pSrcRect, PCVBOXVR_TEXTURE pDst,
+                                      PCRTRECT pDstRect, uint32_t cRects, uint32_t fFlags);
+VBOXBLITTERDECL(int) CrBltImgGetTex(PCR_BLITTER pBlitter, PCVBOXVR_TEXTURE pSrc, GLenum enmFormat, PCR_BLITTER_IMG pDst);
 
-VBOXBLITTERDECL(int) CrBltImgGetMural(PCR_BLITTER pBlitter, bool fBb, CR_BLITTER_IMG *pDst);
-VBOXBLITTERDECL(void) CrBltImgFree(PCR_BLITTER pBlitter, CR_BLITTER_IMG *pDst);
+VBOXBLITTERDECL(int) CrBltImgGetMural(PCR_BLITTER pBlitter, bool fBb, PCR_BLITTER_IMG pDst);
+VBOXBLITTERDECL(void) CrBltImgFree(PCR_BLITTER pBlitter, PCR_BLITTER_IMG pDst);
 VBOXBLITTERDECL(void) CrBltPresent(PCR_BLITTER pBlitter);
 /* */
 struct CR_TEXDATA;
@@ -211,7 +244,7 @@ typedef union CR_TEXDATA_FLAGS
         uint32_t Reserved            : 28;
     };
     uint32_t Value;
-} CR_TEXDATA_FLAGS, *PCR_TEXDATA_FLAGS;
+} CR_TEXDATA_FLAGS;
 
 
 typedef struct CR_TEXDATA
@@ -227,9 +260,11 @@ typedef struct CR_TEXDATA
     /*dtor*/
     PFNCRTEXDATA_RELEASED pfnTextureReleased;
     struct CR_TEXDATA *pScaledCache;
-} CR_TEXDATA, *PCR_TEXDATA;
+} CR_TEXDATA;
+typedef CR_TEXDATA *PCR_TEXDATA;
+typedef CR_TEXDATA const *PCCR_TEXDATA;
 
-DECLINLINE(void) CrTdInit(PCR_TEXDATA pTex, const VBOXVR_TEXTURE *pVrTex, PCR_BLITTER pBlitter, PFNCRTEXDATA_RELEASED pfnTextureReleased)
+DECLINLINE(void) CrTdInit(PCR_TEXDATA pTex, PCVBOXVR_TEXTURE pVrTex, PCR_BLITTER pBlitter, PFNCRTEXDATA_RELEASED pfnTextureReleased)
 {
     memset(pTex, 0, sizeof (*pTex));
     pTex->Tex = *pVrTex;
@@ -238,12 +273,12 @@ DECLINLINE(void) CrTdInit(PCR_TEXDATA pTex, const VBOXVR_TEXTURE *pVrTex, PCR_BL
     pTex->pfnTextureReleased = pfnTextureReleased;
 }
 
-DECLINLINE(const VBOXVR_TEXTURE*) CrTdTexGet(const CR_TEXDATA *pTex)
+DECLINLINE(PCVBOXVR_TEXTURE) CrTdTexGet(PCCR_TEXDATA pTex)
 {
     return &pTex->Tex;
 }
 
-DECLINLINE(PCR_BLITTER) CrTdBlitterGet(CR_TEXDATA *pTex)
+DECLINLINE(PCR_BLITTER) CrTdBlitterGet(PCR_TEXDATA pTex)
 {
     return pTex->pBlitter;
 }
@@ -251,57 +286,68 @@ DECLINLINE(PCR_BLITTER) CrTdBlitterGet(CR_TEXDATA *pTex)
 DECLINLINE(int) CrTdBltEnter(PCR_TEXDATA pTex)
 {
     int rc;
-	if (pTex->Flags.Entered)
-		return VERR_INVALID_STATE;
-	rc = CrBltEnter(pTex->pBlitter);
+    if (pTex->Flags.Entered)
+        return VERR_INVALID_STATE;
+    rc = CrBltEnter(pTex->pBlitter);
+#ifdef IN_VMSVGA3D
+    AssertRCReturn(rc, rc);
+#else
     if (!RT_SUCCESS(rc))
     {
         WARN(("CrBltEnter failed rc %d", rc));
         return rc;
     }
-	pTex->Flags.Entered = 1;
-	return VINF_SUCCESS;
+#endif
+    pTex->Flags.Entered = 1;
+    return VINF_SUCCESS;
 }
 
 DECLINLINE(bool) CrTdBltIsEntered(PCR_TEXDATA pTex)
 {
-	return pTex->Flags.Entered;
+    return pTex->Flags.Entered;
 }
 
 DECLINLINE(void) CrTdBltLeave(PCR_TEXDATA pTex)
 {
-	if (!pTex->Flags.Entered)
-	{
-		WARN(("invalid Blt Leave"));
-		return;
-	}
+#ifdef IN_VMSVGA3D
+    AssertReturnVoid(pTex->Flags.Entered);
+#else
+    if (!pTex->Flags.Entered)
+    {
+        WARN(("invalid Blt Leave"));
+        return;
+    }
+#endif
 
-	CrBltLeave(pTex->pBlitter);
+    CrBltLeave(pTex->pBlitter);
 
-	pTex->Flags.Entered = 0;
+    pTex->Flags.Entered = 0;
 }
 
 /* the CrTdBltXxx calls are done with the entered blitter */
-/* acquire the texture data, returns the cached data in case it is cached.
- * the data remains cached in the CR_TEXDATA object until it is discarded with CrTdBltDataFree or CrTdBltDataCleanup.
- * */
-VBOXBLITTERDECL(int) CrTdBltDataAcquire(PCR_TEXDATA pTex, GLenum enmFormat, bool fInverted, const CR_BLITTER_IMG**ppImg);
+/** Acquire the texture data, returns the cached data in case it is cached.
+ * The data remains cached in the CR_TEXDATA object until it is discarded with
+ * CrTdBltDataFree or CrTdBltDataCleanup. */
+VBOXBLITTERDECL(int) CrTdBltDataAcquire(PCR_TEXDATA pTex, GLenum enmFormat, bool fInverted, PCCR_BLITTER_IMG *ppImg);
 
-VBOXBLITTERDECL(int) CrTdBltDataAcquireScaled(PCR_TEXDATA pTex, GLenum enmFormat, bool fInverted, uint32_t width, uint32_t height, const CR_BLITTER_IMG**ppImg);
+VBOXBLITTERDECL(int) CrTdBltDataAcquireScaled(PCR_TEXDATA pTex, GLenum enmFormat, bool fInverted,
+                                              uint32_t width, uint32_t height, PCCR_BLITTER_IMG *ppImg);
 
-VBOXBLITTERDECL(int) CrTdBltDataReleaseScaled(PCR_TEXDATA pTex, const CR_BLITTER_IMG *pImg);
+VBOXBLITTERDECL(int) CrTdBltDataReleaseScaled(PCR_TEXDATA pTex, PCCR_BLITTER_IMG pImg);
 
 VBOXBLITTERDECL(void) CrTdBltScaleCacheMoveTo(PCR_TEXDATA pTex, PCR_TEXDATA pDstTex);
 
-/* release the texture data, the data remains cached in the CR_TEXDATA object until it is discarded with CrTdBltDataFree or CrTdBltDataCleanup */
+/** Release the texture data, the data remains cached in the CR_TEXDATA object
+ * until it is discarded with CrTdBltDataFree or CrTdBltDataCleanup. */
 VBOXBLITTERDECL(int) CrTdBltDataRelease(PCR_TEXDATA pTex);
-/* discard the texture data cached with previous CrTdBltDataAcquire.
- * Must be called wit data released (CrTdBltDataRelease) */
+/** Discard the texture data cached with previous CrTdBltDataAcquire.
+ * Must be called wit data released (CrTdBltDataRelease). */
 VBOXBLITTERDECL(int) CrTdBltDataFree(PCR_TEXDATA pTex);
 VBOXBLITTERDECL(int) CrTdBltDataFreeNe(PCR_TEXDATA pTex);
 VBOXBLITTERDECL(void) CrTdBltDataInvalidateNe(PCR_TEXDATA pTex);
-/* does same as CrTdBltDataFree, and in addition cleans up.
- * this is kind of a texture destructor, which clients should call on texture object destruction, e.g. from the PFNCRTEXDATA_RELEASED callback */
+/** Does same as CrTdBltDataFree, and in addition cleans up.
+ * This is kind of a texture destructor, which clients should call on texture object destruction,
+ * e.g. from the PFNCRTEXDATA_RELEASED callback. */
 VBOXBLITTERDECL(int) CrTdBltDataCleanup(PCR_TEXDATA pTex);
 
 VBOXBLITTERDECL(int) CrTdBltDataCleanupNe(PCR_TEXDATA pTex);
@@ -320,11 +366,12 @@ DECLINLINE(uint32_t) CrTdRelease(PCR_TEXDATA pTex)
             pTex->pfnTextureReleased(pTex);
         else
             CrTdBltDataCleanupNe(pTex);
-	}
+    }
 
     return cRefs;
 }
 
 RT_C_DECLS_END
 
-#endif /* #ifndef ___cr_blitter_h__ */
+#endif
+
