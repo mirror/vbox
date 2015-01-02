95c49192f431f730b515fa1983e2d3472157c93a
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2015-01-02 12:16:22 +0000

    VBoxDTrace: Made dtrace.c compile on linux. (r12)
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@53642 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/ExtPacks/VBoxDTrace/include/VBoxDTraceTypes.h
+++ src/VBox/ExtPacks/VBoxDTrace/include/VBoxDTraceTypes.h
@@ -36,6 +36,8 @@
 
 RT_C_DECLS_BEGIN
 
+struct modctl;
+
 typedef unsigned char           uchar_t;
 typedef unsigned int            uint_t;
 typedef uintptr_t               ulong_t;
@@ -57,12 +59,19 @@ typedef struct VBoxDtCred
     RTGID                   cr_sgid;
     zoneid_t                cr_zone;
 } cred_t;
-#define PRIV_POLICY_ONLY(a_pCred, a_uPriv, a_fAll) (true)
-#define crgetuid(a_pCred)       ((a_pCred)->cr_uid)
-#define crgetzoneid(a_pCred)    ((a_pCred)->cr_zone)
+#define PRIV_POLICY_ONLY(a_pCred, a_uPriv, a_fAll)  (true)
+#define priv_isequalset(a, b)                       (true)
+#define crgetuid(a_pCred)                           ((a_pCred)->cr_uid)
+#define crgetzoneid(a_pCred)                        ((a_pCred)->cr_zone)
+#define crhold(a_pCred)                             VBoxDtCredHold
+#define crfree(a_pCred)                             VBoxDtCredFree
+void VBoxDtCredHold(struct VBoxDtCred *pCred);
+void VBoxDtCredFree(struct VBoxDtCred *pCred);
 
 
-typedef struct VBoxDtCyclicId  *cyclic_id_t;
+typedef struct RTTIMER  *cyclic_id_t;
+#define CYCLIC_NONE                                 ((struct RTTIMER *)NULL)
+#define cyclic_remove(a_hTimer)                     RTTimerDestroy(a_hTimer)
 
 typedef struct VBoxDtThread
 {
@@ -80,6 +89,7 @@ extern kthread_t *VBoxDtGetCurrentThread(void);
 typedef struct VBoxDtProcess
 {
     uint32_t                p_flag;
+    RTPROCESS               p_pid;
     struct dtrace_helpers  *p_dtrace_helpers;
 } proc_t;
 proc_t *VBoxDtGetCurrentProc(void);
@@ -88,9 +98,7 @@ proc_t *VBoxDtGetCurrentProc(void);
 #define SNOCD                   RT_BIT(0)
 
 
-typedef struct VBoxDtDevInfo    dev_info_t;
 typedef struct VBoxDtTaskQueue  taskq_t;
-typedef struct VBoxDtMemCache   kmem_cache_t;
 
 typedef struct VBoxDtMutex
 {
@@ -99,6 +107,7 @@ typedef struct VBoxDtMutex
 #define mutex_enter             VBoxDtMutexEnter
 #define mutex_exit              VBoxDtMutexExit
 #define MUTEX_HELD(a_pMtx)      VBoxDtMutexIsOwner(a_pMtx)
+#define MUTEX_NOT_HELD(a_pMtx)  (!VBoxDtMutexIsOwner(a_pMtx))
 #define mod_lock                g_DummyMtx
 #define cpu_lock                g_DummyMtx
 void VBoxDtMutexEnter(struct VBoxDtMutex *pMtx);
@@ -156,15 +165,18 @@ void VBoxDtUPrintfV(const char *pszFormat, va_list va);
 
 typedef RTUID                   uid_t;
 typedef RTPROCESS               pid_t;
-typedef RTDEV                   dev_t;
 #endif
 
+#define B_FALSE                 (0)
+#define B_TRUE                  (1)
 #define NANOSEC                 RT_NS_1SEC
 #define MILLISEC                RT_MS_1SEC
 #define NBBY                    8
 #define NCPU                    RTCPUSET_MAX_CPUS
 #define P2ROUNDUP(uWhat, uAlign)    ( ((uWhat) + (uAlign) - 1) & ~(uAlign - 1) )
+#define IS_P2ALIGNED(uWhat, uAlign) ( !((uWhat) & ((uAlign) - 1)) )
 #define	roundup(uWhat, uUnit)	    ( ( (uWhat) + ((uUnit) - 1)) / (uUnit) * (uUnit) )
+#define MIN(a1, a2)             RT_MIN(a1, a2)
 
 #define CPU_ON_INTR(a_pCpu)     (false)
 
@@ -193,6 +205,11 @@ uintptr_t VBoxDtGetKernelBase(void);
  * This is usually signed -> unsigned type changes that avoids a whole lot of
  * comparsion warnings. */
 #define VBDTTYPE(a_VBox, a_Org) a_VBox
+/** Mark missing void in a parameter list. */
+#define VBDTVOID                void
+/** Mark missing static in a function definition. */
+#define VBDTSTATIC              static
+#define VBDTUNASS(a_Value)      = a_Value
 
 /*
  * Memory allocation wrappers.
@@ -206,6 +223,20 @@ void *VBoxDtKMemAlloc(size_t cbMem, uint32_t fFlags);
 void *VBoxDtKMemAllocZ(size_t cbMem, uint32_t fFlags);
 void  VBoxDtKMemFree(void *pvMem, size_t cbMem);
 
+
+typedef struct VBoxDtMemCache   kmem_cache_t;
+#define kmem_cache_create       VBoxDtKMemCacheCreate
+#define kmem_cache_destroy      VBoxDtKMemCacheDestroy
+#define kmem_cache_alloc        VBoxDtKMemCacheAlloc
+#define kmem_cache_free         VBoxDtKMemCacheFree
+struct VBoxDtMemCache *VBoxDtKMemCacheCreate(const char *pszName, size_t cbBuf, size_t cbAlign,
+                                             PFNRT pfnCtor, PFNRT pfnDtor, PFNRT pfnReclaim,
+                                             void *pvUser, void *pvVM, uint32_t fFlags);
+void  VBoxDtKMemCacheDestroy(struct VBoxDtMemCache *pCache);
+void *VBoxDtKMemCacheAlloc(struct VBoxDtMemCache *pCache, uint32_t fFlags);
+void  VBoxDtKMemCacheFree(struct VBoxDtMemCache *pCache, void *pvMem);
+
+
 typedef struct VBoxDtVMem
 {
     size_t                  cbTodo;
@@ -213,8 +244,15 @@ typedef struct VBoxDtVMem
 } vmem_t;
 #define VM_SLEEP                RT_BIT(0)
 #define VM_BESTFIT              RT_BIT(1)
+#define VMC_IDENTIFIER          RT_BIT(16)
+#define vmem_create             VBoxDtVMemCreate
+#define vmem_destroy            VBoxDtVMemDestroy
 #define vmem_alloc              VBoxDtVMemAlloc
 #define vmem_free               VBoxDtVMemFree
+struct VBoxDtVMem *VBoxDtVMemCreate(const char *pszName, void *pvBase, size_t cb, size_t cbUnit,
+                                    PFNRT pfnAlloc, PFNRT pfnFree, struct VBoxDtVMem *pSrc,
+                                    size_t cbQCacheMax, uint32_t fFlags);
+void  VBoxDtVMemDestroy(struct VBoxDtVMem *pVMemArena);
 void *VBoxDtVMemAlloc(struct VBoxDtVMem *pVMemArena, size_t cbMem, uint32_t fFlags);
 void  VBoxDtVMemFree(struct VBoxDtVMem *pVMemArena, void *pvMem, size_t cbMem);
 
@@ -228,12 +266,70 @@ void  VBoxDtVMemFree(struct VBoxDtVMem *pVMemArena, void *pvMem, size_t cbMem);
 #define ENOMEM                  (12)
 #define ENOSPC                  (28)
 #define ENOENT                  (2)
+#define EFAULT                  (14)
+#define E2BIG                   (7)
+#define EACCES                  (13)
+#define EALREADY                (114)
+#define ENXIO                   (6)
+#define EAGAIN                  (11)
+#define ENOTTY                  (25)
+#define ESRCH                   (3)
 
 /*
  * string
  */
 #define bcopy(a_pSrc, a_pDst, a_cb) memmove(a_pDst, a_pSrc, a_cb)
 #define bzero(a_pDst, a_cb)         RT_BZERO(a_pDst, a_cb)
+#define bcmp(a_p1, a_p2, a_cb)      memcmp(a_p1, a_p2, a_cb)
+#define snprintf                    RTStrPrintf
+
+/*
+ * Copy In/Out
+ */
+#define copyin                      VBoxDtCopyIn
+#define copyout                     VBoxDtCopyOut
+int  VBoxDtCopyIn(void const *pvUser, void *pvDst, size_t cb);
+int  VBoxDtCopyOut(void const *pvSrc, void *pvUser, size_t cb);
+
+/*
+ * Device numbers.
+ */
+typedef uint64_t                    dev_t;
+typedef uint32_t                    major_t;
+typedef uint32_t                    minor_t;
+#define makedevice(a_Maj, a_Min)    RT_MAKE_U64(a_Min, a_Maj)
+#define getemajor(a_Dev)            RT_HIDWORD(a_Dev)
+#define geteminor(a_Dev)            RT_LODWORD(a_Dev)
+#define getminor(a_Dev)             RT_LODWORD(a_Dev)
+
+/*
+ * DDI
+ */
+#define DDI_SUCCESS                 (0)
+#define DDI_FAILURE                 (-1)
+#define ddi_soft_state_init         VBoxDtDdiSoftStateInit
+#define ddi_soft_state_fini         VBoxDtDdiSoftStateTerm
+#define ddi_soft_state_zalloc       VBoxDtDdiSoftStateAllocZ
+#define ddi_get_soft_state          VBoxDtDdiSoftStateGet
+#define ddi_soft_state_free         VBoxDtDdiSoftStateFree
+int   VBoxDtDdiSoftStateInit(void **ppvSoftStates, size_t cbSoftState, uint32_t cMaxItems);
+int   VBoxDtDdiSoftStateTerm(void **ppvSoftStates);
+int   VBoxDtDdiSoftStateAllocZ(void *pvSoftStates, RTDEV uMinor);
+int   VBoxDtDdiSoftStateFree(void *pvSoftStates, RTDEV uMinor);
+void *VBoxDtDdiSoftStateGet(void *pvSoftStates, RTDEV uMinor);
+
+typedef struct VBoxDtDevInfo        dev_info_t;
+typedef enum { DDI_ATT_CMD_DUMMY }  ddi_attach_cmd_t;
+typedef enum { DDI_DETACH, DDI_SUSPEND }  ddi_detach_cmd_t;
+#define ddi_driver_major            VBoxDtDdiDriverMajor
+#define ddi_report_dev              VBoxDtDdiReportDev
+major_t VBoxDtDdiDriverMajor(struct VBoxDtDevInfo *pDevInfo);
+void    VBoxDtDdiReportDev(struct VBoxDtDevInfo *pDevInfo);
+
+/*
+ * CTF - probably needs to be ported wholesale or smth.
+ */
+#define CTF_MODEL_NATIVE            1
 
 RT_C_DECLS_END
 #endif
