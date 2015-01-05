881402cc3e32b9ebd4259019b136ecd755453f98
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2015-01-05 13:22:50 +0000

    Missing files are in place.
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@53737 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Devices/Graphics/DevVGA-SVGA3d-ogl.cpp
+++ src/VBox/Devices/Graphics/DevVGA-SVGA3d-ogl.cpp
@@ -832,7 +832,6 @@ int vmsvga3dInit(PVGASTATE pThis)
     AssertCompile(GL_TRUE == 1);
     AssertCompile(GL_FALSE == 0);
 
-#if 0 // FIXME
     /*
      * Load and resolve imports from the external shared library.
      */
@@ -843,7 +842,6 @@ int vmsvga3dInit(PVGASTATE pThis)
         LogRel(("VMSVGA3d: Error loading VBoxSVGA3D and resolving necessary functions: %Rrc - %s\n", rc, ErrInfo.Core.pszMsg));
         return rc;
     }
-#endif
 
     /*
      * Allocate the state.
