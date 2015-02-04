6744168382923830ad269aef5186f1c92bcc80ee
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2015-02-04 07:20:23 +0000

    FE/Qt: do not suppress key release events when the host key is held down.
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@54073 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Frontends/VirtualBox/src/runtime/UIKeyboardHandler.cpp
+++ src/VBox/Frontends/VirtualBox/src/runtime/UIKeyboardHandler.cpp
@@ -1482,12 +1482,13 @@ bool UIKeyboardHandler::keyEvent(int iKey, uint8_t uScan, int fFlags, ulong uScr
      * 2. if currently released key releases host-combo too.
      * Using that rule, we are NOT sending to the guest:
      * 1. the last key-press of host-combo,
-     * 2. all keys pressed while the host-combo being held. */
+     * 2. all keys pressed while the host-combo being held (but we still send releases). */
     LONG aCodesBuffer[16];
     LONG *pCodes = aCodesBuffer;
     uint uCodesCount = 0;
     if ((!m_bIsHostComboPressed && !isHostComboStateChanged) ||
-        ( m_bIsHostComboPressed &&  isHostComboStateChanged))
+        ( m_bIsHostComboPressed &&  isHostComboStateChanged) ||
+        !(fFlags & KeyPressed))
     {
         /* Special flags handling (KeyPrint): */
         if (fFlags & KeyPrint)
