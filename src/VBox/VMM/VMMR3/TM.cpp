b704d3b538172a4fc845e7b2b7fee249a4c75852
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2015-02-05 14:08:06 +0000

    VMM/TM: build fix.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@54087 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/VMM/VMMR3/TM.cpp
+++ src/VBox/VMM/VMMR3/TM.cpp
@@ -3456,25 +3456,6 @@ static DECLCALLBACK(void) tmR3InfoClocks(PVM pVM, PCDBGFINFOHLP pHlp, const char
 
 
 /**
- * Gets the descriptive TM TSC mode name.
- *
- * @returns The name.
- * @param   pVM      Pointer to the VM.
- */
-static const char *tmR3GetTSCModeName(PVM pVM)
-{
-    Assert(pVM);
-    switch (pVM->tm.s.enmTSCMode)
-    {
-        case TMTSCMODE_REAL_TSC_OFFSET:    return "RealTscOffset";
-        case TMTSCMODE_VIRT_TSC_EMULATED:  return "VirtTscEmulated";
-        case TMTSCMODE_DYNAMIC:            return "Dynamic";
-        default:                           return "???";
-    }
-}
-
-
-/**
  * Gets the descriptive TM TSC mode name given the enum value.
  *
  * @returns The name.
@@ -3489,5 +3470,18 @@ static const char *tmR3GetTSCModeNameEx(TMTSCMODE enmMode)
         case TMTSCMODE_DYNAMIC:            return "Dynamic";
         default:                           return "???";
     }
+}
+
+
+/**
+ * Gets the descriptive TM TSC mode name.
+ *
+ * @returns The name.
+ * @param   pVM      Pointer to the VM.
+ */
+static const char *tmR3GetTSCModeName(PVM pVM)
+{
+    Assert(pVM);
+    return tmR3GetTSCModeNameEx(pVM->tm.s.enmTSCMode);
 }
 
