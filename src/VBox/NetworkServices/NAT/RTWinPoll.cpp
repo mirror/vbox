1acec08bed5993effc924715ac4734c5ced51116
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2015-02-10 11:22:04 +0000

    NAT/Network: Add missing Id/@file/copyright headers.
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@54124 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/NetworkServices/NAT/RTWinPoll.cpp
+++ src/VBox/NetworkServices/NAT/RTWinPoll.cpp
@@ -1,4 +1,19 @@
-/* -*- indent-tabs-mode: nil; -*- */
+/* $Id$ */
+/** @file
+ * NAT Network - poll(2) implementation for winsock.
+ */
+
+/*
+ * Copyright (C) 2013-2014 Oracle Corporation
+ *
+ * This file is part of VirtualBox Open Source Edition (OSE), as
+ * available from http://www.virtualbox.org. This file is free software;
+ * you can redistribute it and/or modify it under the terms of the GNU
+ * General Public License (GPL) as published by the Free Software
+ * Foundation, in version 2 as it comes in the "COPYING" file of the
+ * VirtualBox OSE distribution. VirtualBox OSE is distributed in the
+ * hope that it will be useful, but WITHOUT ANY WARRANTY of any kind.
+ */
 #define LOG_GROUP LOG_GROUP_NAT_SERVICE
 
 #include <iprt/asm.h>
