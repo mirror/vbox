26f83f27f5f1bf057718eb299ba37430bd3ca811
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2015-02-10 18:03:20 +0000

    FE/Qt: VirtualBox event-handler cleanup/rework (step 1).
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@54131 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Frontends/VirtualBox/src/selector/UIVirtualBoxEventHandler.cpp
+++ src/VBox/Frontends/VirtualBox/src/selector/UIVirtualBoxEventHandler.cpp
@@ -4,7 +4,7 @@
  */
 
 /*
- * Copyright (C) 2010-2012 Oracle Corporation
+ * Copyright (C) 2010-2015 Oracle Corporation
  *
  * This file is part of VirtualBox Open Source Edition (OSE), as
  * available from http://www.virtualbox.org. This file is free software;
@@ -37,7 +37,7 @@ UIVirtualBoxEventHandler *UIVirtualBoxEventHandler::m_pInstance = 0;
 UIVirtualBoxEventHandler* UIVirtualBoxEventHandler::instance()
 {
     if (!m_pInstance)
-        m_pInstance = new UIVirtualBoxEventHandler();
+        m_pInstance = new UIVirtualBoxEventHandler;
     return m_pInstance;
 }
 
@@ -53,12 +53,19 @@ void UIVirtualBoxEventHandler::destroy()
 
 UIVirtualBoxEventHandler::UIVirtualBoxEventHandler()
 {
-//    RTPrintf("Self add: %RTthrd\n", RTThreadSelf());
-    const CVirtualBox &vbox = vboxGlobal().virtualBox();
+    /* Create Main-event listener instance: */
     ComObjPtr<UIMainEventListenerImpl> pListener;
     pListener.createObject();
-    pListener->init(new UIMainEventListener(), this);
+    pListener->init(new UIMainEventListener, this);
     m_mainEventListener = CEventListener(pListener);
+
+    /* Get VirtualBox: */
+    const CVirtualBox vbox = vboxGlobal().virtualBox();
+    AssertWrapperOk(vbox);
+    /* Get event-source: */
+    CEventSource eventSource = vbox.GetEventSource();
+    AssertWrapperOk(eventSource);
+    /* Register listener for expected event-types: */
     QVector<KVBoxEventType> events;
     events
         << KVBoxEventType_OnMachineStateChanged
@@ -68,34 +75,28 @@ UIVirtualBoxEventHandler::UIVirtualBoxEventHandler()
         << KVBoxEventType_OnSnapshotTaken
         << KVBoxEventType_OnSnapshotDeleted
         << KVBoxEventType_OnSnapshotChanged;
+    eventSource.RegisterListener(m_mainEventListener, events, TRUE);
+    AssertWrapperOk(eventSource);
 
-    vbox.GetEventSource().RegisterListener(m_mainEventListener, events, TRUE);
-    AssertWrapperOk(vbox);
-
+    /* Prepare connections: */
     connect(pListener->getWrapped(), SIGNAL(sigMachineStateChange(QString, KMachineState)),
             this, SIGNAL(sigMachineStateChange(QString, KMachineState)),
             Qt::QueuedConnection);
-
     connect(pListener->getWrapped(), SIGNAL(sigMachineDataChange(QString)),
             this, SIGNAL(sigMachineDataChange(QString)),
             Qt::QueuedConnection);
-
     connect(pListener->getWrapped(), SIGNAL(sigMachineRegistered(QString, bool)),
             this, SIGNAL(sigMachineRegistered(QString, bool)),
             Qt::QueuedConnection);
-
     connect(pListener->getWrapped(), SIGNAL(sigSessionStateChange(QString, KSessionState)),
             this, SIGNAL(sigSessionStateChange(QString, KSessionState)),
             Qt::QueuedConnection);
-
     connect(pListener->getWrapped(), SIGNAL(sigSnapshotTake(QString, QString)),
             this, SIGNAL(sigSnapshotTake(QString, QString)),
             Qt::QueuedConnection);
-
     connect(pListener->getWrapped(), SIGNAL(sigSnapshotDelete(QString, QString)),
             this, SIGNAL(sigSnapshotDelete(QString, QString)),
             Qt::QueuedConnection);
-
     connect(pListener->getWrapped(), SIGNAL(sigSnapshotChange(QString, QString)),
             this, SIGNAL(sigSnapshotChange(QString, QString)),
             Qt::QueuedConnection);
@@ -103,6 +104,13 @@ UIVirtualBoxEventHandler::UIVirtualBoxEventHandler()
 
 UIVirtualBoxEventHandler::~UIVirtualBoxEventHandler()
 {
-    const CVirtualBox &vbox = vboxGlobal().virtualBox();
-    vbox.GetEventSource().UnregisterListener(m_mainEventListener);
+    /* Get VirtualBox: */
+    const CVirtualBox vbox = vboxGlobal().virtualBox();
+    AssertWrapperOk(vbox);
+    /* Get event-source: */
+    CEventSource eventSource = vbox.GetEventSource();
+    AssertWrapperOk(eventSource);
+    /* Unregister listener: */
+    eventSource.UnregisterListener(m_mainEventListener);
 }
+
--- src/VBox/Frontends/VirtualBox/src/selector/UIVirtualBoxEventHandler.h
+++ src/VBox/Frontends/VirtualBox/src/selector/UIVirtualBoxEventHandler.h
@@ -3,7 +3,7 @@
  */
 
 /*
- * Copyright (C) 2010-2012 Oracle Corporation
+ * Copyright (C) 2010-2015 Oracle Corporation
  *
  * This file is part of VirtualBox Open Source Edition (OSE), as
  * available from http://www.virtualbox.org. This file is free software;
@@ -14,41 +14,55 @@
  * hope that it will be useful, but WITHOUT ANY WARRANTY of any kind.
  */
 
-#ifndef __UIVirtualBoxEventHandler_h__
-#define __UIVirtualBoxEventHandler_h__
+#ifndef ___UIVirtualBoxEventHandler_h___
+#define ___UIVirtualBoxEventHandler_h___
 
 /* COM includes: */
 #include "COMEnums.h"
 #include "CEventListener.h"
 
-class UIVirtualBoxEventHandler: public QObject
+/** Active event handler singleton for the CVirtualBox event-source. */
+class UIVirtualBoxEventHandler : public QObject
 {
     Q_OBJECT;
 
-public:
-    static UIVirtualBoxEventHandler* instance();
-    static void destroy();
-
 signals:
-    /* VirtualBox main signals */
+
+    /** Notifies listeners about @a state change event for the machine with @a strId. */
     void sigMachineStateChange(QString strId, KMachineState state);
+    /** Notifies listeners about data change event for the machine with @a strId. */
     void sigMachineDataChange(QString strId);
+    /** Notifies listeners about machine with @a strId was @a fRegistered. */
     void sigMachineRegistered(QString strId, bool fRegistered);
+    /** Notifies listeners about @a state change event for the session of the machine with @a strId. */
     void sigSessionStateChange(QString strId, KSessionState state);
+    /** Notifies listeners about snapshot with @a strSnapshotId was taken for the machine with @a strId. */
     void sigSnapshotTake(QString strId, QString strSnapshotId);
+    /** Notifies listeners about snapshot with @a strSnapshotId was deleted for the machine with @a strId. */
     void sigSnapshotDelete(QString strId, QString strSnapshotId);
+    /** Notifies listeners about snapshot with @a strSnapshotId was changed for the machine with @a strId. */
     void sigSnapshotChange(QString strId, QString strSnapshotId);
 
+public:
+
+    /** Returns singleton instance created by the factory. */
+    static UIVirtualBoxEventHandler* instance();
+    /** Destroys singleton instance created by the factory. */
+    static void destroy();
+
 private:
+
+    /** Constructor. */
     UIVirtualBoxEventHandler();
+    /** Destructor. */
     ~UIVirtualBoxEventHandler();
 
-    /* Private member vars */
+    /** Holds the static singleton instance. */
     static UIVirtualBoxEventHandler *m_pInstance;
+    /** Holds the COM event-listener instance. */
     CEventListener m_mainEventListener;
 };
 
 #define gVBoxEvents UIVirtualBoxEventHandler::instance()
 
-#endif /* !__UIVirtualBoxEventHandler_h__ */
-
+#endif /* !___UIVirtualBoxEventHandler_h___ */
