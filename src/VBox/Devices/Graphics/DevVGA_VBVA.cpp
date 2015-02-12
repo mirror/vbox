a5ca783800eccb518b5655632d30d85b9fe80ab7
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2015-02-12 09:59:06 +0000

    DevVGA_VBVA: fix
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@54168 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Devices/Graphics/DevVGA_VBVA.cpp
+++ src/VBox/Devices/Graphics/DevVGA_VBVA.cpp
@@ -2626,7 +2626,7 @@ static int vbvaSendModeHintWorker(PVGASTATE pThis, uint32_t cx, uint32_t cy,
     /** @note See Display::setVideoModeHint: "It is up to the guest to decide
      *  whether the hint is valid. Therefore don't do any VRAM sanity checks
      *  here! */
-    if (iDisplay > RT_MIN(pThis->cMonitors, RT_ELEMENTS(pCtx->aModeHints)))
+    if (iDisplay >= RT_MIN(pThis->cMonitors, RT_ELEMENTS(pCtx->aModeHints)))
         return VERR_OUT_OF_RANGE;
     pCtx->aModeHints[iDisplay].magic    = VBVAMODEHINT_MAGIC;
     pCtx->aModeHints[iDisplay].cx       = cx;
