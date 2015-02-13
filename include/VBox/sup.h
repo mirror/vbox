7d959ac761b2a027325a6a5931065931fc8821b1
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2015-02-13 14:46:25 +0000

    warning
    git-svn-id: http://www.virtualbox.org/svn/vbox/trunk@54198 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- include/VBox/sup.h
+++ include/VBox/sup.h
@@ -1648,6 +1648,7 @@ DECLINLINE(uint64_t) SUPReadTsc(void)
     {
         uint64_t u64Tsc = UINT64_MAX;
         int rc = SUPGetTsc(&u64Tsc, NULL /* pidApic */);
+        NOREF(rc);
 #ifdef DEBUG_ramshankar
         AssertRC(rc);
 #endif
