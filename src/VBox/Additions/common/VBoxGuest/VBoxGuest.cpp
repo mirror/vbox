0e1e05bfed6e05c26a2facf2cc163409ce03dadc
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2015-02-17 15:29:40 +0000

    VBoxGuest: Reverted r92886 as it does not make any sense (RTLogFlush if buffered? Different env vars for DEBUG doesn't make anywhere, and the env.var. is ignored in ring-0 anyway).
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@54239 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Additions/common/VBoxGuest/VBoxGuest.cpp
+++ src/VBox/Additions/common/VBoxGuest/VBoxGuest.cpp
@@ -971,26 +971,13 @@ int VBoxGuestInitDevExt(PVBOXGUESTDEVEXT pDevExt, uint16_t IOPortBase,
      * Create the release log.
      */
     static const char * const s_apszGroups[] = VBOX_LOGGROUP_NAMES;
-    RTUINT fFlags =   RTLOGFLAGS_PREFIX_TIME | RTLOGFLAGS_PREFIX_TID
-                    | RTLOGFLAGS_PREFIX_THREAD | RTLOGFLAGS_PREFIX_TIME_PROG;
     PRTLOGGER pRelLogger;
-    rc = RTLogCreate(&pRelLogger, fFlags, "all",
-#ifdef DEBUG
-                     "VBOXGUEST_LOG",
-#else
-                     "VBOXGUEST_RELEASE_LOG",
-#endif
-                     RT_ELEMENTS(s_apszGroups), s_apszGroups,
+    rc = RTLogCreate(&pRelLogger, 0 /*fFlags*/, "all", "VBOXGUEST_RELEASE_LOG", RT_ELEMENTS(s_apszGroups), s_apszGroups,
                      RTLOGDEST_STDOUT | RTLOGDEST_DEBUGGER, NULL);
     if (RT_SUCCESS(rc))
-    {
         RTLogRelSetDefaultInstance(pRelLogger);
-
-        /* Explicitly flush the log in case of VBOXGUEST_RELEASE_LOG=buffered. */
-        RTLogFlush(pRelLogger);
-    }
     /** @todo Add native hook for getting logger config parameters and setting
-     *        them. On Linux we use the module parameter stuff (see vboxguestLinuxModInit). */
+     *        them.  On linux we should use the module parameter stuff... */
 #endif
 
     /*
@@ -1356,8 +1343,8 @@ static int VBoxGuestCommonIOCtl_CancelAllWaitEvents(PVBOXGUESTDEVEXT pDevExt, PV
 void VBoxGuestCloseSession(PVBOXGUESTDEVEXT pDevExt, PVBOXGUESTSESSION pSession)
 {
     unsigned i; NOREF(i);
-    LogFlowFunc(("pSession=%p proc=%RTproc (%d) r0proc=%p\n",
-                 pSession, pSession->Process, (int)pSession->Process, (uintptr_t)pSession->R0Process)); /** @todo %RTr0proc */
+    LogFlow(("VBoxGuestCloseSession: pSession=%p proc=%RTproc (%d) r0proc=%p\n",
+             pSession, pSession->Process, (int)pSession->Process, (uintptr_t)pSession->R0Process)); /** @todo %RTr0proc */
 
     RTSpinlockAcquire(pDevExt->SessionSpinlock);
     RTListNodeRemove(&pSession->ListNode);
@@ -1374,7 +1361,7 @@ void VBoxGuestCloseSession(PVBOXGUESTDEVEXT pDevExt, PVBOXGUESTSESSION pSession)
             Info.result = 0;
             Info.u32ClientID = pSession->aHGCMClientIds[i];
             pSession->aHGCMClientIds[i] = 0;
-            LogFlowFunc(("Disconnecting client ID=%#RX32\n", Info.u32ClientID));
+            Log(("VBoxGuestCloseSession: disconnecting client id %#RX32\n", Info.u32ClientID));
             VbglR0HGCMInternalDisconnect(&Info, VBoxGuestHGCMAsyncWaitCallback, pDevExt, RT_INDEFINITE_WAIT);
         }
 #endif
