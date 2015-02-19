9c7c6132d57b678de3c7623a74bd432eac31533f
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2015-02-19 20:41:49 +0000

    HostDrivers/Support: not static.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@54312 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/HostDrivers/Support/SUPDrv.c
+++ src/VBox/HostDrivers/Support/SUPDrv.c
@@ -6002,7 +6002,7 @@ static int supdrvTscDeltaThreadButchered(PSUPDRVDEVEXT pDevExt, bool fSpinlockHe
 static DECLCALLBACK(int) supdrvTscDeltaThread(RTTHREAD hThread, void *pvUser)
 {
     PSUPDRVDEVEXT     pDevExt = (PSUPDRVDEVEXT)pvUser;
-    static uint32_t   cTimesMeasured = 0;
+    uint32_t          cTimesMeasured = 0;
     uint32_t          cConsecutiveTimeouts = 0;
     int               rc = VERR_INTERNAL_ERROR_2;
     for (;;)
