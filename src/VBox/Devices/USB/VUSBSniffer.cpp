7ee1516f9edf196fd82d73e9f24fe4d792a9d5f2
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2015-02-27 23:27:41 +0000

    In case we need it later.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@54568 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Devices/USB/VUSBSniffer.cpp
+++ src/VBox/Devices/USB/VUSBSniffer.cpp
@@ -46,6 +46,7 @@
 /** Block type for the interface descriptor block. */
 #define DUMPFILE_IDB_BLOCK_TYPE       UINT32_C(0x00000001)
 /** USB link type. */
+#define DUMPFILE_IDB_LINK_TYPE_USB_LINUX        UINT16_C(189)
 #define DUMPFILE_IDB_LINK_TYPE_USB_LINUX_MMAPED UINT16_C(220)
 
 /** Block type for an enhanced packet block. */
