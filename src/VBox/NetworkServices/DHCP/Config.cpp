c4d7f4ef967f9623ad1f5504b245c192bf45d18a
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2015-02-27 02:38:08 +0000

    VBoxNetDHCP: Use map::end() instead of map::cend() to cater to older STL.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@54542 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/NetworkServices/DHCP/Config.cpp
+++ src/VBox/NetworkServices/DHCP/Config.cpp
@@ -1219,7 +1219,7 @@ int NetworkManager::processParameterReqList(const Client& client, const uint8_t 
             default:
                 {
                     DhcpOptionMap::const_iterator it = OptMap.find((int)u8Req);
-                    if (it == OptMap.cend())
+                    if (it == OptMap.end())
                     {
                         Log(("opt: %d is ignored\n", u8Req));
                         fIgnore = true;
