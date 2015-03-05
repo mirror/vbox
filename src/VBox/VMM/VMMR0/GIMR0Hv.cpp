4287dd604403da9f2877e7f7d5b80a3e5d165a3d
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2015-03-05 16:11:18 +0000

    VMM/GIM: build fix.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@54656 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/VMM/VMMR0/GIMR0Hv.cpp
+++ src/VBox/VMM/VMMR0/GIMR0Hv.cpp
@@ -91,7 +91,7 @@ static void gimR0HvPageFree(PRTR0MEMOBJ pMemObj, PRTR0PTR ppVirt, PRTHCPHYS pHCP
  * @param   u64Offset   The computed TSC offset.
  * @thread  EMT.
  */
-VMM_INT_DECL(int) GIMR0HvUpdateParavirtTsc(PVM pVM, uint64_t u64Offset)
+VMM_INT_DECL(int) gimR0HvUpdateParavirtTsc(PVM pVM, uint64_t u64Offset)
 {
     Assert(GIMIsEnabled(pVM));
     bool fHvTscEnabled = MSR_GIM_HV_REF_TSC_IS_ENABLED(pVM->gim.s.u.Hv.u64TscPageMsr);
@@ -137,7 +137,7 @@ VMM_INT_DECL(int) GIMR0HvUpdateParavirtTsc(PVM pVM, uint64_t u64Offset)
  * @returns VBox status code.
  * @param   pVM     Pointer to the VM.
  */
-VMMR0_INT_DECL(int) GIMR0HvInitVM(PVM pVM)
+VMMR0_INT_DECL(int) gimR0HvInitVM(PVM pVM)
 {
     AssertPtr(pVM);
     Assert(GIMIsEnabled(pVM));
@@ -156,7 +156,7 @@ VMMR0_INT_DECL(int) GIMR0HvInitVM(PVM pVM)
  * @returns VBox status code.
  * @param   pVM     Pointer to the VM.
  */
-VMMR0_INT_DECL(int) GIMR0HvTermVM(PVM pVM)
+VMMR0_INT_DECL(int) gimR0HvTermVM(PVM pVM)
 {
     AssertPtr(pVM);
     Assert(GIMIsEnabled(pVM));
