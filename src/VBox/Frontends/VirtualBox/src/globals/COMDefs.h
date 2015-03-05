65f114c3a5a6d092d091be59caa1ab0c72773c63
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2015-03-05 11:21:53 +0000

    FE/Qt: Re-applying stuff reverted in r98784.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@54644 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Frontends/VirtualBox/src/globals/COMDefs.h
+++ src/VBox/Frontends/VirtualBox/src/globals/COMDefs.h
@@ -97,14 +97,6 @@ class XPCOMEventQSocketListener;
 
 #endif /* !defined(VBOX_WITH_XPCOM) */
 
-
-/* VirtualBox interfaces declarations */
-#if !defined(VBOX_WITH_XPCOM)
-    #include <VirtualBox.h>
-#else /* !defined(VBOX_WITH_XPCOM) */
-    #include <VirtualBox_XPCOM.h>
-#endif /* !defined(VBOX_WITH_XPCOM) */
-
 /////////////////////////////////////////////////////////////////////////////
 
 class CVirtualBoxErrorInfo;
@@ -636,14 +628,14 @@ public:
     {
         clear();
         mIface = that.mIface;
-        this->addref(ptr());
+        this->addref((IUnknown*)ptr());
     }
 
     CInterface(I *aIface)
     {
         clear();
         setPtr(aIface);
-        this->addref(aIface);
+        this->addref((IUnknown*)aIface);
     }
 
     virtual ~CInterface()
@@ -696,13 +688,13 @@ public:
 #endif
         /* be aware of self assignment */
         I* amIface = ptr();
-        this->addref(aIface);
-        this->release(amIface);
+        this->addref((IUnknown*)aIface);
+        this->release((IUnknown*)amIface);
         if (aIface)
         {
             amIface = NULL;
             B::mRC = aIface->QueryInterface(COM_IIDOF(I), (void **)&amIface);
-            this->release(aIface);
+            this->release((IUnknown*)aIface);
             setPtr(amIface);
         }
         else
@@ -719,8 +711,8 @@ public:
         Assert(!mDead);
 #endif
         /* be aware of self assignment */
-        this->addref(aIface);
-        this->release(ptr());
+        this->addref((IUnknown*)aIface);
+        this->release((IUnknown*)ptr());
         setPtr(aIface);
         B::mRC = S_OK;
     };
@@ -731,7 +723,7 @@ public:
 #ifdef DEBUG
        Assert(!mDead);
 #endif
-       this->release(ptr());
+       this->release((IUnknown*)ptr());
        setPtr(NULL);
     }
 
--- src/VBox/Frontends/VirtualBox/src/wizards/newvm/UIWizardNewVMPageBasic3.cpp
+++ src/VBox/Frontends/VirtualBox/src/wizards/newvm/UIWizardNewVMPageBasic3.cpp
@@ -38,6 +38,12 @@
 
 #endif /* !VBOX_WITH_PRECOMPILED_HEADERS */
 
+/* VirtualBox interface declarations: */
+#ifndef VBOX_WITH_XPCOM
+# include "VirtualBox.h"
+#else /* !VBOX_WITH_XPCOM */
+# include "VirtualBox_XPCOM.h"
+#endif /* VBOX_WITH_XPCOM */
 
 UIWizardNewVMPage3::UIWizardNewVMPage3()
 {
