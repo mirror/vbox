a5a3b44540b2df5e101be7eb7015a0797e721f43
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2015-03-17 10:46:28 +0000

    more places for SMAP handling
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@54804 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Runtime/r0drv/linux/mpnotification-r0drv-linux.c
+++ src/VBox/Runtime/r0drv/linux/mpnotification-r0drv-linux.c
@@ -153,7 +153,9 @@ static int rtMpNotificationLinuxCallback(struct notifier_block *pNotifierBlock, 
 # if defined(CPU_TASKS_FROZEN) && defined(CPU_ONLINE_FROZEN)
         case CPU_ONLINE_FROZEN:
 # endif
+            stac();
             rtMpNotificationDoCallbacks(RTMPEVENT_ONLINE, idCpu);
+            clac();
             break;
 
 # ifdef CPU_DOWN_PREPARE
@@ -161,7 +163,9 @@ static int rtMpNotificationLinuxCallback(struct notifier_block *pNotifierBlock, 
 #  if defined(CPU_TASKS_FROZEN) && defined(CPU_DOWN_PREPARE_FROZEN)
         case CPU_DOWN_PREPARE_FROZEN:
 #  endif
+            stac();
             rtMpNotificationDoCallbacks(RTMPEVENT_OFFLINE, idCpu);
+            clac();
             break;
 # endif
     }
