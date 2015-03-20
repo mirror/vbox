4722910d86033723296efe710b65c3e16381810a
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2015-03-20 23:50:18 +0000

    SUPR0QueryVTCaps: Copy & past wrt unimpeded guest execution.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@54889 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/HostDrivers/Support/SUPDrv.cpp
+++ src/VBox/HostDrivers/Support/SUPDrv.cpp
@@ -1,4 +1,4 @@
-/* $Id$ */
+p/* $Id$ */
 /** @file
  * VBoxDrv - The VirtualBox Support Driver - Common code.
  */
@@ -3946,7 +3946,7 @@ SUPR0DECL(int) SUPR0QueryVTCaps(PSUPDRVSESSION pSession, uint32_t *pfCaps)
                         if (vtCaps.n.allowed1 & VMX_VMCS_CTRL_PROC_EXEC2_EPT)
                             *pfCaps |= SUPVTCAPS_NESTED_PAGING;
                         if (vtCaps.n.allowed1 & VMX_VMCS_CTRL_PROC_EXEC2_UNRESTRICTED_GUEST)
-                            *pfCaps |= SUPVTCAPS_NESTED_PAGING;
+                            *pfCaps |= SUPVTCAPS_VTX_UNRESTRICTED_GUEST;
                     }
                 }
             }
