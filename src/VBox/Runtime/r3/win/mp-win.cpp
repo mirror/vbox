f10e4356bee2601c6c2fe84ff10d60e4968d450e
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2015-03-24 17:40:16 +0000

    Runtime/r3/win: GetLogicalProcessorInformation is useless for querying the number of cores before Vista.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@54931 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Runtime/r3/win/mp-win.cpp
+++ src/VBox/Runtime/r3/win/mp-win.cpp
@@ -100,13 +100,26 @@ RTDECL(RTCPUID) RTMpGetCoreCount(void)
 {
     /*
      * Resolve the API dynamically (one try) as it requires XP w/ sp3 or later.
-     */
+     */    
     typedef BOOL (WINAPI *PFNGETLOGICALPROCINFO)(PSYSTEM_LOGICAL_PROCESSOR_INFORMATION, PDWORD);
     static PFNGETLOGICALPROCINFO s_pfnGetLogicalProcInfo = (PFNGETLOGICALPROCINFO)~(uintptr_t)0;
     if (s_pfnGetLogicalProcInfo == (PFNGETLOGICALPROCINFO)~(uintptr_t)0)
         s_pfnGetLogicalProcInfo = (PFNGETLOGICALPROCINFO)RTLdrGetSystemSymbol("kernel32.dll", "GetLogicalProcessorInformation");
 
-    if (s_pfnGetLogicalProcInfo)
+    /*
+     * Sadly, on XP and Server 2003, even if the API is present, it does not tell us
+     * how many physical cores there are (any package will look like a single core).
+     * That is worse than not using the API at all, so just skip it unless it's Vista+.
+     */
+    bool fIsVistaOrLater = false;
+    OSVERSIONINFOEX OSInfoEx = { 0 };
+    OSInfoEx.dwOSVersionInfoSize = sizeof(OSVERSIONINFOEX);
+    if (   GetVersionEx((LPOSVERSIONINFO) &OSInfoEx)
+        && (OSInfoEx.dwPlatformId == VER_PLATFORM_WIN32_NT)
+        && (OSInfoEx.dwMajorVersion >= 6))
+        fIsVistaOrLater = true;
+
+    if (s_pfnGetLogicalProcInfo && fIsVistaOrLater)
     {
         /*
          * Query the information. This unfortunately requires a buffer, so we
