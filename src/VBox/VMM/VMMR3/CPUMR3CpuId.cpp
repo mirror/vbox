04db72d425e315fe01b94511cf98c2b32242dbd2
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2015-03-26 07:59:06 +0000

    CPUMR3CpuId: Fixed typos. Thank you Jung-uk Kim.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@54958 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/VMM/VMMR3/CPUMR3CpuId.cpp
+++ src/VBox/VMM/VMMR3/CPUMR3CpuId.cpp
@@ -157,15 +157,15 @@ VMMR3DECL(CPUMMICROARCH) CPUMR3CpuIdDetermineMicroarchEx(CPUMCPUVENDOR enmVendor
             case 0x06:
                 switch (bModel)
                 {
-                    case  0: kCpumMicroarch_AMD_K7_Palomino;
-                    case  1: kCpumMicroarch_AMD_K7_Palomino;
-                    case  2: kCpumMicroarch_AMD_K7_Palomino;
-                    case  3: kCpumMicroarch_AMD_K7_Spitfire;
-                    case  4: kCpumMicroarch_AMD_K7_Thunderbird;
-                    case  6: kCpumMicroarch_AMD_K7_Palomino;
-                    case  7: kCpumMicroarch_AMD_K7_Morgan;
-                    case  8: kCpumMicroarch_AMD_K7_Thoroughbred;
-                    case 10: kCpumMicroarch_AMD_K7_Barton; /* Thorton too. */
+                    case  0: return kCpumMicroarch_AMD_K7_Palomino;
+                    case  1: return kCpumMicroarch_AMD_K7_Palomino;
+                    case  2: return kCpumMicroarch_AMD_K7_Palomino;
+                    case  3: return kCpumMicroarch_AMD_K7_Spitfire;
+                    case  4: return kCpumMicroarch_AMD_K7_Thunderbird;
+                    case  6: return kCpumMicroarch_AMD_K7_Palomino;
+                    case  7: return kCpumMicroarch_AMD_K7_Morgan;
+                    case  8: return kCpumMicroarch_AMD_K7_Thoroughbred;
+                    case 10: return kCpumMicroarch_AMD_K7_Barton; /* Thorton too. */
                 }
                 return kCpumMicroarch_AMD_K7_Unknown;
             case 0x0f:
