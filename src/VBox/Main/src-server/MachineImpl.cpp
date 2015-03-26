67dd50bafa493afffe1c8ec2346747925680dc73
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2015-03-26 14:51:46 +0000

    MediumImpl: be a bit more verbose if locking of the media failed
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@54966 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Main/src-server/MachineImpl.cpp
+++ src/VBox/Main/src-server/MachineImpl.cpp
@@ -14135,7 +14135,7 @@ HRESULT SessionMachine::i_lockMedia()
         if (FAILED(rc))
         {
             mrc = setError(rc,
-                           tr("Locking of attached media failed"));
+                           tr("Locking of attached media failed. A possible reason is that one of the media is attached to a running VM"));
         }
     }
 
