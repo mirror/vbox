6ae7e4af3d26c83ffdf17ff8cade88874e80a95c
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2015-03-31 13:32:14 +0000

    PDM/DrvAudio.cpp: Fixed a potential crash.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@55032 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Devices/Audio/DrvAudio.cpp
+++ src/VBox/Devices/Audio/DrvAudio.cpp
@@ -394,9 +394,11 @@ int drvAudioDestroyHstOut(PDRVAUDIO pThis, PPDMAUDIOHSTSTRMOUT pHstStrmOut)
         }
     }
     else
+    {
+        LogFlowFunc(("[%s] Still is being used, rc=%Rrc\n", pHstStrmOut->MixBuf.pszName, rc));
         rc = VERR_ACCESS_DENIED;
+    }
 
-    LogFlowFunc(("[%s] Still is being used, rc=%Rrc\n", pHstStrmOut->MixBuf.pszName, rc));
     return rc;
 }
 
@@ -972,9 +974,11 @@ int drvAudioDestroyHstIn(PDRVAUDIO pThis, PPDMAUDIOHSTSTRMIN pHstStrmIn)
         }
     }
     else
+    {
+        LogFlowFunc(("[%s] Still is being used, rc=%Rrc\n", pHstStrmIn->MixBuf.pszName, rc));
         rc = VERR_ACCESS_DENIED;
+    }
 
-    LogFlowFunc(("[%s] Still is being used, rc=%Rrc\n", pHstStrmIn->MixBuf.pszName, rc));
     return rc;
 }
 
