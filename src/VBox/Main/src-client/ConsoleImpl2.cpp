2d3bf9ff37a2a969a98cd812f8927815ec19a8ec
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2015-03-31 10:31:10 +0000

    ConsoleImpl2: fixed possible memory leak in error case
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@55022 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Main/src-client/ConsoleImpl2.cpp
+++ src/VBox/Main/src-client/ConsoleImpl2.cpp
@@ -5640,7 +5640,7 @@ int configSetGlobalPropertyFlags(VMMDev * const pVMMDev,
         {
             for (unsigned i = 0; RT_SUCCESS(rc) && i < cProps; ++i)
             {
-                AssertPtrReturn(namesOut[i], VERR_INVALID_PARAMETER);
+                AssertPtrBreakStmt(namesOut[i], rc = VERR_INVALID_PARAMETER);
                 rc = RTUtf16ToUtf8(namesOut[i], &papszNames[i]);
                 if (RT_FAILURE(rc))
                     break;
