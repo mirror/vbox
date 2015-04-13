5dbcc0d60e0a75f11e10ba1351779c08ac80be74
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2015-04-13 22:14:23 +0000

    WDDM: command buffer size adjustment.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@55227 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Additions/WINNT/Graphics/Video/mp/wddm/VBoxMPWddm.cpp
+++ src/VBox/Additions/WINNT/Graphics/Video/mp/wddm/VBoxMPWddm.cpp
@@ -900,16 +900,12 @@ static NTSTATUS vboxWddmSetupDisplaysNew(PVBOXMP_DEVEXT pDevExt)
                             - VBoxCommonFromDeviceExt(pDevExt)->cbMiniportHeap
                             - VBVA_ADAPTER_INFORMATION_SIZE;
 
-    ULONG cbCmdVbva = cbAvailable / 2;
-    ULONG cbCmdVbvaApprox = VBOXCMDVBVA_BUFFERSIZE(4096);
-    if (cbCmdVbvaApprox > cbCmdVbva)
-    {
-        WARN(("too few VRAM memory %d, cmdVbva %d, while approximately needed %d, trying to adjust", cbAvailable, cbCmdVbva, cbCmdVbvaApprox));
-        cbCmdVbva = cbCmdVbvaApprox;
-    }
+    /* Size of the VBVA buffer which is used to pass VBOXCMDVBVA_* commands to the host.
+     * Estimate max 4KB per command.
+     */
+    ULONG cbCmdVbva = VBOXCMDVBVA_BUFFERSIZE(4096);
 
-    cbCmdVbva = VBOXWDDM_ROUNDBOUND(cbCmdVbva, 0x1000);
-    if (cbCmdVbva > cbAvailable - 0x1000)
+    if (cbCmdVbva >= cbAvailable)
     {
         WARN(("too few VRAM memory fatal, %d, requested for CmdVbva %d", cbAvailable, cbCmdVbva));
         return STATUS_UNSUCCESSFUL;
