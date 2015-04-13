719413f76827676e5f960c0e53339e88ddf5433a
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2015-04-13 20:23:31 +0000

    Additions/x11/vboxvideo: allow setting HasVT property early.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@55226 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Additions/x11/vboxvideo/vboxvideo.c
+++ src/VBox/Additions/x11/vboxvideo/vboxvideo.c
@@ -967,6 +967,8 @@ static void updateHasVTProperty(ScrnInfoPtr pScrn, Bool hasVT)
                              FALSE);
     if (property_name == BAD_RESOURCE)
         FatalError("Failed to retrieve \"HAS_VT\" atom\n");
+    if (ROOT_WINDOW(pScrn) == NULL)
+        return;
     ChangeWindowProperty(ROOT_WINDOW(pScrn), property_name, XA_INTEGER, 32,
                          PropModeReplace, 1, &value, TRUE);
 }
