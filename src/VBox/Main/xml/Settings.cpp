488dc3d3bbd85fba7c987225a37abc568d854e28
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2015-04-14 08:48:28 +0000

    Main/xml/Settings: also bump the settings to 1.15 if there are USB storage controllers
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@55232 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Main/xml/Settings.cpp
+++ src/VBox/Main/xml/Settings.cpp
@@ -5503,6 +5503,7 @@ void MachineConfigFile::bumpSettingsVersionIfNeeded()
              * Check whether the hotpluggable flag of all storage devices differs
              * from the default for old settings.
              * AHCI ports are hotpluggable by default every other device is not.
+             * Also check if there are USB storage controllers.
              */
             for (StorageControllersList::const_iterator it = storageMachine.llStorageControllers.begin();
                  it != storageMachine.llStorageControllers.end();
@@ -5510,6 +5511,13 @@ void MachineConfigFile::bumpSettingsVersionIfNeeded()
             {
                 bool fSettingsBumped = false;
                 const StorageController &sctl = *it;
+
+                if (sctl.controllerType == StorageControllerType_USB)
+                {
+                    m->sv = SettingsVersion_v1_15;
+                    fSettingsBumped = true;
+                    break;
+                }
 
                 for (AttachedDevicesList::const_iterator it2 = sctl.llAttachedDevices.begin();
                      it2 != sctl.llAttachedDevices.end();
@@ -5533,6 +5541,9 @@ void MachineConfigFile::bumpSettingsVersionIfNeeded()
                     return;
             }
 
+            /*
+             * Check if there is an xHCI (USB3) USB controller.
+             */
             for (USBControllerList::const_iterator it = hardwareMachine.usbSettings.llUSBControllers.begin();
                  it != hardwareMachine.usbSettings.llUSBControllers.end();
                  ++it)
