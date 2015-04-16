ac587a03f0f8581f319eaa7c1942a34a4c8f4aec
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2015-04-16 10:44:47 +0000

    HMR0A.asm: Fixed some unused code.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@55300 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/VMM/VMMR0/HMR0A.asm
+++ src/VBox/VMM/VMMR0/HMR0A.asm
@@ -1521,7 +1521,7 @@ BITS 64
     mov     rsi, [rsp + 20h + 1Ch]      ; pVMCBPhys
     mov     edx, [rsp + 20h + 24h]      ; pCtx
     mov     ecx, [rsp + 20h + 28h]      ; pVM
-    mov     r8,  [rsp + 20h + 2Ch]      ; pVCpu
+    mov     r8d, [rsp + 20h + 2Ch]      ; pVCpu
     call    NAME(SVMR0VMRun_64)
     add     esp, 20h
     jmp far [.fpthunk32 wrt rip]
@@ -1562,8 +1562,8 @@ BITS 64
     mov     rdi, [rbp + 20h + 14h]      ; pVMCBHostPhys
     mov     rsi, [rbp + 20h + 1Ch]      ; pVMCBPhys
     mov     edx, [rbp + 20h + 24h]      ; pCtx
-    mov     ecx, [rsp + 20h + 20h]      ; pVM
-    mov     r8,  [rsp + 20h + 24h]      ; pVCpu
+    mov     ecx, [rsp + 20h + 28h]      ; pVM
+    mov     r8d, [rsp + 20h + 2Ch]      ; pVCpu
     call    NAME(SVMR0VMRun64_64)
     add     esp, 20h
     jmp far [.fpthunk32 wrt rip]
