8931a9be792d572a91423f56789fd1fc9cb07508
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2015-04-20 12:06:38 +0000

    FE/Qt: Runtime UI: Mouse-handler: Get the actual mouse-buttons from the QApplication instead of the invalid going from the mouse-events.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@55336 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Frontends/VirtualBox/src/runtime/UIMouseHandler.cpp
+++ src/VBox/Frontends/VirtualBox/src/runtime/UIMouseHandler.cpp
@@ -560,10 +560,10 @@ bool UIMouseHandler::eventFilter(QObject *pWatched, QEvent *pEvent)
                     UIGrabMouseEvent *pDeltaEvent = static_cast<UIGrabMouseEvent*>(pEvent);
                     QPoint p = QPoint(pDeltaEvent->xDelta() + m_lastMousePos.x(),
                                       pDeltaEvent->yDelta() + m_lastMousePos.y());
-                    if (mouseEvent(pDeltaEvent->mouseEventType(), uScreenId,
-                                   m_viewports[uScreenId]->mapFromGlobal(p), p,
-                                   pDeltaEvent->buttons(),
-                                   pDeltaEvent->wheelDelta(), pDeltaEvent->orientation()))
+                    /* Old Qt versions had a bug where mouse-buttons coming with the event were not quite valid.
+                     * For now we will be asking the QApplication for the valid button combination instead. */
+                    if (mouseEvent(pDeltaEvent->mouseEventType(), uScreenId, m_viewports[uScreenId]->mapFromGlobal(p), p,
+                                   QApplication::mouseButtons(), pDeltaEvent->wheelDelta(), pDeltaEvent->orientation()))
                         return true;
                     break;
                 }
@@ -638,9 +638,10 @@ bool UIMouseHandler::eventFilter(QObject *pWatched, QEvent *pEvent)
                 {
                     QMouseEvent *pMouseEvent = static_cast<QMouseEvent*>(pEvent);
                     m_iLastMouseWheelDelta = 0;
-                    if (mouseEvent(pMouseEvent->type(), uScreenId,
-                                   pMouseEvent->pos(), pMouseEvent->globalPos(),
-                                   pMouseEvent->buttons(), 0, Qt::Horizontal))
+                    /* Old Qt versions had a bug where mouse-buttons coming with the event were not quite valid.
+                     * For now we will be asking the QApplication for the valid button combination instead. */
+                    if (mouseEvent(pMouseEvent->type(), uScreenId, pMouseEvent->pos(), pMouseEvent->globalPos(),
+                                   QApplication::mouseButtons(), 0, Qt::Horizontal))
                         return true;
                     break;
                 }
@@ -666,17 +667,10 @@ bool UIMouseHandler::eventFilter(QObject *pWatched, QEvent *pEvent)
                         iDelta = m_iLastMouseWheelDelta;
                         m_iLastMouseWheelDelta = m_iLastMouseWheelDelta % 120;
                     }
-                    if (mouseEvent(pWheelEvent->type(), uScreenId,
-                                   pWheelEvent->pos(), pWheelEvent->globalPos(),
-#ifdef QT_MAC_USE_COCOA
-                                   /* Qt Cocoa is buggy. It always reports a left button pressed when the
-                                    * mouse wheel event occurs. A workaround is to ask the application which
-                                    * buttons are pressed currently: */
-                                   QApplication::mouseButtons(),
-#else /* QT_MAC_USE_COCOA */
-                                   pWheelEvent->buttons(),
-#endif /* !QT_MAC_USE_COCOA */
-                                   iDelta, pWheelEvent->orientation()))
+                    /* Old Qt versions had a bug where mouse-buttons coming with the event were not quite valid.
+                     * For now we will be asking the QApplication for the valid button combination instead. */
+                    if (mouseEvent(pWheelEvent->type(), uScreenId, pWheelEvent->pos(), pWheelEvent->globalPos(),
+                                   QApplication::mouseButtons(), iDelta, pWheelEvent->orientation()))
                         return true;
                     break;
                 }
