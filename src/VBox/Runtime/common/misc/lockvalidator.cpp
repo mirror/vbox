5b3b1eb31f93dddbe45c0adfdf6c14e497cbbab7
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2015-04-27 12:52:19 +0000

    lockvalidator.cpp: Added a couple of todos wrt deleting owned locks.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@55446 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Runtime/common/misc/lockvalidator.cpp
+++ src/VBox/Runtime/common/misc/lockvalidator.cpp
@@ -2999,6 +2999,9 @@ RTDECL(void) RTLockValidatorRecExclDelete(PRTLOCKVALRECEXCL pRec)
 
     rtLockValidatorSerializeDestructEnter();
 
+    /** @todo Check that it's not on our stack first.  Need to make it
+     *        configurable whether deleting a owned lock is acceptable? */
+
     ASMAtomicWriteU32(&pRec->Core.u32Magic, RTLOCKVALRECEXCL_MAGIC_DEAD);
     ASMAtomicWriteHandle(&pRec->hThread, NIL_RTTHREAD);
     RTLOCKVALCLASS hClass;
@@ -3466,6 +3469,9 @@ RTDECL(int)  RTLockValidatorRecSharedCreate(PRTLOCKVALRECSHRD *ppRec, RTLOCKVALC
 RTDECL(void) RTLockValidatorRecSharedDelete(PRTLOCKVALRECSHRD pRec)
 {
     Assert(pRec->Core.u32Magic == RTLOCKVALRECSHRD_MAGIC);
+
+    /** @todo Check that it's not on our stack first.  Need to make it
+     *        configurable whether deleting a owned lock is acceptable? */
 
     /*
      * Flip it into table realloc mode and take the destruction lock.
