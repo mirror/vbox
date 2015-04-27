3beebac2551a89a301315d657286b535fb688306
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2015-04-27 12:09:42 +0000

    ConsoleImpl: removed unnecessary local variable.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@55443 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Main/src-client/ConsoleImpl.cpp
+++ src/VBox/Main/src-client/ConsoleImpl.cpp
@@ -3017,8 +3017,6 @@ HRESULT Console::removeSharedFolder(const com::Utf8Str &aName)
 {
     LogFlowThisFunc(("Entering for '%s'\n", aName.c_str()));
 
-    Utf8Str strName(aName);
-
     AutoWriteLock alock(this COMMA_LOCKVAL_SRC_POS);
 
     /// @todo see @todo in AttachUSBDevice() about the Paused state
@@ -3050,20 +3048,20 @@ HRESULT Console::removeSharedFolder(const com::Utf8Str &aName)
          * folder. */
 
         /* first, remove the given folder */
-        rc = i_removeSharedFolder(strName);
+        rc = i_removeSharedFolder(aName);
         if (FAILED(rc)) return rc;
 
         /* first, remove the machine or the global folder if there is any */
         SharedFolderDataMap::const_iterator it;
-        if (i_findOtherSharedFolder(strName, it))
+        if (i_findOtherSharedFolder(aName, it))
         {
-            rc = i_createSharedFolder(strName, it->second);
+            rc = i_createSharedFolder(aName, it->second);
             /* don't check rc here because we need to remove the console
              * folder from the collection even on failure */
         }
     }
 
-    m_mapSharedFolders.erase(strName);
+    m_mapSharedFolders.erase(aName);
 
     /* Notify console callbacks after the folder is removed from the list. */
     alock.release();
