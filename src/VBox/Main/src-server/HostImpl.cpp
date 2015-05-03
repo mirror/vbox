6fbf5a45c1e7243a706fe1b8e986a5828ee87b10
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2015-05-03 05:02:16 +0000

    Main/Linux/HostPower: add DBus-based host suspend and resume notifications, only if VBOX_WITH_DBUS defined.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@55616 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Main/src-server/HostImpl.cpp
+++ src/VBox/Main/src-server/HostImpl.cpp
@@ -303,7 +303,7 @@ HRESULT Host::init(VirtualBox *aParent)
 
 #if defined(RT_OS_WINDOWS)
     m->pHostPowerService = new HostPowerServiceWin(m->pParent);
-#elif defined(RT_OS_LINUX)
+#elif defined(RT_OS_LINUX) && defined(VBOX_WITH_DBUS)
     m->pHostPowerService = new HostPowerServiceLinux(m->pParent);
 #elif defined(RT_OS_DARWIN)
     m->pHostPowerService = new HostPowerServiceDarwin(m->pParent);
