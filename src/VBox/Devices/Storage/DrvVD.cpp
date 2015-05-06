06da6789526a9974cda94058836d8517013d6570
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2015-05-06 08:30:56 +0000

    DrvVD: rc is not part of the format string anymore
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@55686 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Devices/Storage/DrvVD.cpp
+++ src/VBox/Devices/Storage/DrvVD.cpp
@@ -3109,7 +3109,7 @@ static DECLCALLBACK(int) drvvdConstruct(PPDMDRVINS pDrvIns, PCFGMNODE pCfg, uint
         {
            rc = PDMDrvHlpVMSetError(pDrvIns, rc, RT_SRC_POS,
                                     N_("Failed to open image '%s' in %s mode"), pszName,
-                                    (uOpenFlags & VD_OPEN_FLAGS_READONLY) ? "read-only" : "read-write", rc);
+                                    (uOpenFlags & VD_OPEN_FLAGS_READONLY) ? "read-only" : "read-write");
            break;
         }
 
