1a6dd73167a86259b9b8699889ae61e1f2655e3f
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2015-05-07 18:05:20 +0000

    CPUM.cpp/info: Missed on place where CPUMCTX_XSAVE_C_PTR should be applied.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@55736 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/VMM/VMMR3/CPUM.cpp
+++ src/VBox/VMM/VMMR3/CPUM.cpp
@@ -1865,8 +1865,7 @@ static void cpumR3InfoOne(PVM pVM, PCPUMCTX pCtx, PCCPUMCTXCORE pCtxCore, PCDBGF
 
                 if (pCtx->fXStateMask & XSAVE_C_OPMASK)
                 {
-                    PCX86XSAVEOPMASK pOpMask;
-                    pOpMask = (PCX86XSAVEOPMASK)((uint8_t *)pCtx->CTX_SUFF(pXState) + pCtx->aoffXState[XSAVE_C_OPMASK_BIT]);
+                    PCX86XSAVEOPMASK pOpMask = CPUMCTX_XSAVE_C_PTR(pCtx, XSAVE_C_OPMASK_BIT, PCX86XSAVEOPMASK);
                     for (unsigned i = 0; i < RT_ELEMENTS(pOpMask->aKRegs); i += 4)
                         pHlp->pfnPrintf(pHlp, "%sK%u=%016RX64  %sK%u=%016RX64  %sK%u=%016RX64  %sK%u=%016RX64\n",
                                         pszPrefix, i + 0, pOpMask->aKRegs[i + 0],
