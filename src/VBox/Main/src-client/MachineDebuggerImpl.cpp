12a5c55e1a49852091852a414aedfa3b990541e7
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2015-05-07 00:19:10 +0000

    MachineDebuggerImpl.cpp: Fixed the getRegisters method, missing vector resizing before filling. Also fixed unnecessary Bstr conversion.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@55702 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Main/src-client/MachineDebuggerImpl.cpp
+++ src/VBox/Main/src-client/MachineDebuggerImpl.cpp
@@ -1194,15 +1194,17 @@ HRESULT MachineDebugger::getRegisters(ULONG aCpuId, std::vector<com::Utf8Str> &a
                 {
                     try
                     {
-
+                        aValues.resize(cRegs);
+                        aNames.resize(cRegs);
                         for (uint32_t iReg = 0; iReg < cRegs; iReg++)
                         {
-                            Bstr bstrValue;
-
-                            hrc = formatRegisterValue(&bstrValue, &paRegs[iReg].Val, paRegs[iReg].enmType);
-                            AssertComRC(hrc);
+                            char szHex[160];
+                            szHex[159] = szHex[0] = '\0';
+                            ssize_t cch = DBGFR3RegFormatValue(szHex, sizeof(szHex), &paRegs[iReg].Val,
+                                                               paRegs[iReg].enmType, true /*fSpecial*/);
+                            Assert(cch > 0);
                             aNames[iReg] = Utf8Str(paRegs[iReg].pszName);
-                            aValues[iReg] = Utf8Str(bstrValue);
+                            aValues[iReg] = Utf8Str(szHex);
                         }
                     }
                     catch (std::bad_alloc)
