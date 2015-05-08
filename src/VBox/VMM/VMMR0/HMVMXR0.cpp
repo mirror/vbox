6b94ef743f8dee797946d6d85f220737e1f3417e
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2015-05-08 14:29:27 +0000

    VMM/HMVMXR0: oops! not meant to commit this!
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@55757 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/VMM/VMMR0/HMVMXR0.cpp
+++ src/VBox/VMM/VMMR0/HMVMXR0.cpp
@@ -780,14 +780,14 @@ static int hmR0VmxEnterRootMode(PVM pVM, RTHCPHYS HCPhysCpuPage, void *pvCpuPage
     RTCCUINTREG uOldCr4 = SUPR0ChangeCR4(X86_CR4_VMXE, ~0);
 
     /* Enter VMX root mode. */
-    int rc = VMXEnable(HCPhysCpuPage+5);
+    int rc = VMXEnable(HCPhysCpuPage);
     if (RT_FAILURE(rc))
     {
         if (!(uOldCr4 & X86_CR4_VMXE))
             SUPR0ChangeCR4(0, ~X86_CR4_VMXE);
 
         if (pVM)
-            pVM->hm.s.vmx.HCPhysVmxEnableError = HCPhysCpuPage+5;
+            pVM->hm.s.vmx.HCPhysVmxEnableError = HCPhysCpuPage;
     }
 
     /* Restore interrupts. */
