3d3d1248ce6c3b276b0cceaba4b2021b13948398
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2015-05-08 20:45:20 +0000

    DevATA: missing break
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@55776 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Devices/Storage/DevATA.cpp
+++ src/VBox/Devices/Storage/DevATA.cpp
@@ -2328,6 +2328,7 @@ static bool atapiReadDVDStructureSS(ATADevState *s)
 
                         /* 4 byte header + 4 byte data */
                         uASC = (4 + 4);
+                        break;
 
                     case 0x03: /* BCA information - invalid field for no BCA info */
                         uASC = -SCSI_ASC_INV_FIELD_IN_CMD_PACKET;
