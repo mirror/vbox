94e865358a0ca3be3acb3ae193755b9bc46274ee
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2015-05-12 10:10:36 +0000

    FE/Qt: Selector UI: Details pane: VM Preview: Make sure acquired guest-screen resolution is valid.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@55819 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Frontends/VirtualBox/src/selector/graphics/details/UIGMachinePreview.cpp
+++ src/VBox/Frontends/VirtualBox/src/selector/graphics/details/UIGMachinePreview.cpp
@@ -250,6 +250,13 @@ void UIGMachinePreview::sltRecreatePreview()
                     ULONG uGuestWidth, uGuestHeight, uBpp;
                     KGuestMonitorStatus monitorStatus = KGuestMonitorStatus_Enabled;
                     display.GetScreenResolution(0, uGuestWidth, uGuestHeight, uBpp, iOriginX, iOriginY, monitorStatus);
+                    if (uGuestWidth == 0 || uGuestHeight == 0)
+                    {
+                        AssertMsgFailed(("Acquired guest screen resolution is %dx%d\n",
+                                         uGuestWidth, uGuestHeight));
+                        break;
+                    }
+
                     double dAspectRatio = (double)uGuestWidth / uGuestHeight;
                     /* Look for the best aspect-ratio preset preset: */
                     preset = bestAspectRatioPreset(dAspectRatio, m_ratios);
@@ -514,6 +521,10 @@ UIGMachinePreview::AspectRatioPreset UIGMachinePreview::bestAspectRatioPreset(co
 /* static */
 QSize UIGMachinePreview::imageAspectRatioSize(const QSize &hostSize, const QSize &guestSize)
 {
+    /* Make sure host-size and guest-size are valid: */
+    AssertReturn(!hostSize.isNull(), QSize());
+    AssertReturn(!guestSize.isNull(), hostSize);
+
     /* Calculate host/guest aspect-ratio: */
     const double dHostAspectRatio = (double)hostSize.width() / hostSize.height();
     const double dGuestAspectRatio = (double)guestSize.width() / guestSize.height();
