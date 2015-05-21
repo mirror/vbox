5d78cd9cd8269ca7cd40c9f23295ddd08c109cc9
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2015-05-21 12:10:07 +0000

    pylint...
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@56001 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/ValidationKit/testdriver/vboxwrappers.py
+++ src/VBox/ValidationKit/testdriver/vboxwrappers.py
@@ -2249,7 +2249,7 @@ class SessionWrapper(TdTaskBase):
                and self.oVM.state is vboxcon.MachineState_Running:
                 self.o.console.pause();
             if self.fpApiVer >= 5.0:
-                (oProgressCom, _id) = self.o.machine.takeSnapshot(sName, sDescription, True);
+                (oProgressCom, _) = self.o.machine.takeSnapshot(sName, sDescription, True);
             else:
                 oProgressCom = self.o.console.takeSnapshot(sName, sDescription);
             oProgress = ProgressWrapper(oProgressCom, self.oVBoxMgr, self.oTstDrv, 'Take Snapshot %s' % (sName));
