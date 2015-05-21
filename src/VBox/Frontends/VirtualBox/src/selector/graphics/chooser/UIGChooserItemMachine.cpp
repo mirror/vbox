55027c37cffbf5e25eb394d043fa0ddc900130d4
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2015-05-21 16:56:59 +0000

    FE/Qt: Selector UI: Details pane: Fixing crash on try to remove multi-selected VM group with VM item in it.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@56012 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Frontends/VirtualBox/src/selector/graphics/chooser/UIGChooserItemMachine.cpp
+++ src/VBox/Frontends/VirtualBox/src/selector/graphics/chooser/UIGChooserItemMachine.cpp
@@ -160,11 +160,14 @@ void UIGChooserItemMachine::enumerateMachineItems(const QList<UIGChooserItem*> &
         {
             /* Get the iterated machine-item: */
             UIGChooserItemMachine *pMachineItem = pItem->toMachineItem();
-            /* Skip if this item is already enumerated but we need unique: */
+            /* Skip if exactly this item is already enumerated: */
+            if (ol.contains(pMachineItem))
+                continue;
+            /* Skip if item with same ID is already enumerated but we need unique: */
             if ((iEnumerationFlags & UIGChooserItemMachineEnumerationFlag_Unique) &&
                 contains(ol, pMachineItem))
                 continue;
-            /* Skip if ths item is accessible and we no need it: */
+            /* Skip if this item is accessible and we no need it: */
             if ((iEnumerationFlags & UIGChooserItemMachineEnumerationFlag_Inaccessible) &&
                 pMachineItem->accessible())
                 continue;
