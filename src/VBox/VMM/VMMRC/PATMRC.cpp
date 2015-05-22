317b982dda621eabf94af5aeec156a4a3016a439
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2015-05-22 21:05:29 +0000

    PATMRC.cpp: docs
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@56045 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/VMM/VMMRC/PATMRC.cpp
+++ src/VBox/VMM/VMMRC/PATMRC.cpp
@@ -45,19 +45,11 @@
 
 
 /**
- * \#PF Virtual Handler callback for Guest access a page monitored by PATM
+ * @callback_method_impl{FNPGMRZPHYSPFHANDLER,
+ *      PATM all access handler callback.}
  *
- * @returns VBox status code (appropriate for trap handling and GC return).
- * @param   pVM         Pointer to the VM.
- * @param   pVCpu       Pointer to the cross context CPU context for the
- *                      calling EMT.
- * @param   uErrorCode  CPU Error code.
- * @param   pRegFrame   Trap register frame.
- * @param   pvFault     The fault address (cr2).
- * @param   pvRange     The base address of the handled virtual range.
- * @param   offRange    The offset of the access into this range.
- *                      (If it's a EIP range this is the EIP, if not it's pvFault.)
- * @param   pvUser      The physical address of the guest page being monitored.
+ * @remarks The @a pvUser argument is the base address of the page being
+ *          monitored.
  */
 DECLEXPORT(VBOXSTRICTRC) patmRCVirtPagePfHandler(PVM pVM, PVMCPU pVCpu, RTGCUINT uErrorCode, PCPUMCTXCORE pRegFrame,
                                                  RTGCPTR pvFault, RTGCPTR pvRange, uintptr_t offRange, void *pvUser)
