10fb8fc84f4d1e7a123121ebb5231e851e75349e
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2015-05-27 16:12:54 +0000

    FE/Qt: VM settings: Display page: Forgot something in r100632.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@56108 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Frontends/VirtualBox/src/widgets/UIFilmContainer.cpp
+++ src/VBox/Frontends/VirtualBox/src/widgets/UIFilmContainer.cpp
@@ -145,7 +145,7 @@ void UIFilm::retranslateUi()
 {
     /* Translate check-box: */
     m_pCheckBox->setText(QApplication::translate("UIMachineSettingsDisplay", "Screen %1").arg(m_iScreenIndex + 1));
-    m_pCheckBox->setWhatsThis(QApplication::translate("UIMachineSettingsDisplay", "Enable video recording for screen %1.").arg(m_iScreenIndex + 1));
+    m_pCheckBox->setWhatsThis(QApplication::translate("UIMachineSettingsDisplay", "When checked, enables video recording for screen %1.").arg(m_iScreenIndex + 1));
 }
 
 void UIFilm::prepare()
