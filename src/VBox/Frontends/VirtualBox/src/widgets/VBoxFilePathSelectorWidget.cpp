79b5d767612289274250db7e212acf44edc0c41b
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2015-05-29 13:34:18 +0000

    FE/Qt: Action-pools: Doh! The NLS articles in few other places.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@56152 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Frontends/VirtualBox/src/widgets/VBoxFilePathSelectorWidget.cpp
+++ src/VBox/Frontends/VirtualBox/src/widgets/VBoxFilePathSelectorWidget.cpp
@@ -316,7 +316,7 @@ void VBoxFilePathSelectorWidget::retranslateUi()
     {
         case Mode_Folder:
             setItemData (SelectId,
-                         tr ("Displays window to select a different folder."),
+                         tr ("Displays a window to select a different folder."),
                          Qt::ToolTipRole);
             setItemData (ResetId,
                          tr ("Resets the folder path to the default value."),
@@ -325,7 +325,7 @@ void VBoxFilePathSelectorWidget::retranslateUi()
         case Mode_File_Open:
         case Mode_File_Save:
             setItemData (SelectId,
-                         tr ("Displays window to select a different file."),
+                         tr ("Displays a window to select a different file."),
                          Qt::ToolTipRole);
             setItemData (ResetId,
                          tr ("Resets the file path to the default value."),
