a079239913b5de43b3dcd3299b4747b91db75817
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2015-05-29 11:14:29 +0000

    SrvIntNetR0: rename intnetR0NetworkAddrCacheDeleteUnlocked s/Unlocked/Locked/ to conform to existing naming customs.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@56146 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Devices/Network/SrvIntNetR0.cpp
+++ src/VBox/Devices/Network/SrvIntNetR0.cpp
@@ -1150,10 +1150,10 @@ DECLINLINE(void) intnetR0IfAddrCacheDelete(PINTNETIF pIf, PINTNETADDRCACHE pCach
  * @param   cbAddr          The address size (optimization).
  * @param   pszMsg          Log message.
  */
-DECLINLINE(void) intnetR0NetworkAddrCacheDeleteUnlocked(PINTNETNETWORK pNetwork,
-                                                        PCRTNETADDRU pAddr, INTNETADDRTYPE enmType,
-                                                        uint8_t const cbAddr,
-                                                        const char *pszMsg)
+DECLINLINE(void) intnetR0NetworkAddrCacheDeleteLocked(PINTNETNETWORK pNetwork,
+                                                      PCRTNETADDRU pAddr, INTNETADDRTYPE enmType,
+                                                      uint8_t const cbAddr,
+                                                      const char *pszMsg)
 {
     uint32_t iIf = pNetwork->MacTab.cEntries;
     while (iIf--)
@@ -1184,7 +1184,7 @@ DECLINLINE(void) intnetR0NetworkAddrCacheDelete(PINTNETNETWORK pNetwork, PCRTNET
 {
     RTSpinlockAcquire(pNetwork->hAddrSpinlock);
 
-    intnetR0NetworkAddrCacheDeleteUnlocked(pNetwork, pAddr, enmType, cbAddr, pszMsg);
+    intnetR0NetworkAddrCacheDeleteLocked(pNetwork, pAddr, enmType, cbAddr, pszMsg);
 
     RTSpinlockRelease(pNetwork->hAddrSpinlock);
 }
@@ -5550,7 +5550,7 @@ static DECLCALLBACK(void) intnetR0NetworkNotifyHostAddress(PINTNETTRUNKSWPORT pS
         intnetR0NetworkBlacklistAdd(pNetwork, pAddr, enmType);
 
         /* kick out any guest that uses it */
-        intnetR0NetworkAddrCacheDeleteUnlocked(pNetwork, pAddr, enmType, cbAddr, "tif/host");
+        intnetR0NetworkAddrCacheDeleteLocked(pNetwork, pAddr, enmType, cbAddr, "tif/host");
     }
     else                /* address deleted from one of host interfaces */
     {
