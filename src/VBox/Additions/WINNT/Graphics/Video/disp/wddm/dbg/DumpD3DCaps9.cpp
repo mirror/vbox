72e7a535992b47c57d9d3ac10f37481a6486f523
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2015-06-04 09:17:32 +0000

    warning
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@56234 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Additions/WINNT/Graphics/Video/disp/wddm/dbg/DumpD3DCaps9.cpp
+++ src/VBox/Additions/WINNT/Graphics/Video/disp/wddm/dbg/DumpD3DCaps9.cpp
@@ -27,7 +27,7 @@
 #define DUMP_STRCASE(_t) \
         case _t: { Log(("%s", #_t"")); break; }
 #define DUMP_STRCASE_DEFAULT_DWORD(_dw) \
-        default: { Log(("0x%08", (_dw))); break; }
+        default: { Log(("0x%08x", (_dw))); break; }
 
 #define DUMP_STRIF_INIT(_ps, _t) \
         const char * _pSep = (_ps); \
