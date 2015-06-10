68ba1b0b07472ae773d0b1fe7367e9ec82c9f8cb
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2015-06-10 11:34:34 +0000

    build fix
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@56336 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/bldprogs/scmsubversion.cpp
+++ src/bldprogs/scmsubversion.cpp
@@ -756,15 +756,15 @@ static void scmSvnTryResolveFunctions(void)
                 }
                 if (RT_SUCCESS(rc))
                 {
-                    apr_status_t rc = g_pfnAprInitialize();
-                    if (rc == 0)
+                    apr_status_t rcApr = g_pfnAprInitialize();
+                    if (rcApr == 0)
                     {
                         ScmVerbose(NULL, 1, "Found subversion APIs.\n");
                         g_fSvnFunctionPointersValid = true;
                     }
                     else
                     {
-                        ScmVerbose(NULL, 0, "apr_initialize failed: %#x (%d)\n", rc, rc);
+                        ScmVerbose(NULL, 0, "apr_initialize failed: %#x (%d)\n", rcApr, rcApr);
                         AssertMsgFailed(("%#x (%d)\n", rc, rc));
                     }
                     return;
