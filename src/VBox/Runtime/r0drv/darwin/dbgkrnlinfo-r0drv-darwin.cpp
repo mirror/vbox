fb00dc09f910aaaeff14bb6a9f0782f657240473
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2015-06-30 12:06:07 +0000

    dbgkrnlinfo-r0drv-darwin.cpp: Replaced vm_map_wire with vm_map_unwire as the former is resolved differently on 10.11, it seems.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@56699 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Runtime/r0drv/darwin/dbgkrnlinfo-r0drv-darwin.cpp
+++ src/VBox/Runtime/r0drv/darwin/dbgkrnlinfo-r0drv-darwin.cpp
@@ -453,7 +453,7 @@ static int rtR0DbgKrnlDarwinCheckStandardSymbols(RTDBGKRNLINFOINT *pThis)
         KNOWN_ENTRY(ostype),
         KNOWN_ENTRY(panic),
         KNOWN_ENTRY(strprefix),
-        //KNOWN_ENTRY(sysctlbyname), - we get kernel_sysctlbyname from the kernel.
+        //KNOWN_ENTRY(sysctlbyname), - we get kernel_sysctlbyname from the 10.10+ kernels.
         KNOWN_ENTRY(vsscanf),
         KNOWN_ENTRY(page_mask),
 
@@ -520,7 +520,7 @@ static int rtR0DbgKrnlDarwinCheckStandardSymbols(RTDBGKRNLINFOINT *pThis)
         KNOWN_ENTRY(vm_map),
         KNOWN_ENTRY(vm_protect),
         KNOWN_ENTRY(vm_region),
-        KNOWN_ENTRY(vm_map_wire),
+        KNOWN_ENTRY(vm_map_unwire), /* vm_map_wire has an alternative symbol, vm_map_wire_external, in 10.11  */
         KNOWN_ENTRY(PE_kputc),
         KNOWN_ENTRY(kernel_map),
         KNOWN_ENTRY(kernel_pmap),
