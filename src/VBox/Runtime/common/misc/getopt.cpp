2a0be8617b6acf46a76aa09d811acffbaa5950d5
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2015-06-30 13:25:36 +0000

    RTGetOptPrintError: Don't crash on VERR_GETOP_TINVALID_ARGUMENT_FORMAT if pDef is NULL.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@56700 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Runtime/common/misc/getopt.cpp
+++ src/VBox/Runtime/common/misc/getopt.cpp
@@ -739,7 +739,7 @@ RTDECL(RTEXITCODE) RTGetOptPrintError(int ch, PCRTGETOPTUNION pValueUnion)
     }
     else if (ch == VERR_GETOPT_UNKNOWN_OPTION)
         RTMsgError("Unknown option: '%s'", pValueUnion->psz);
-    else if (ch == VERR_GETOPT_INVALID_ARGUMENT_FORMAT)
+    else if (pValueUnion->pDef && ch == VERR_GETOPT_INVALID_ARGUMENT_FORMAT)
         /** @todo r=klaus not really ideal, as the value isn't available */
         RTMsgError("The value given '%s' has an invalid format.", pValueUnion->pDef->pszLong);
     else if (pValueUnion->pDef)
