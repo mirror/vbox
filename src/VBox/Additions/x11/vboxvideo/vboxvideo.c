a109ee58cc106855b631cd7b7db9d1536e76d752
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2015-07-03 13:16:04 +0000

    Additions/x11/vboxvideo: add standard modes back to the RANDR 1.2+ driver.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@56784 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Additions/x11/vboxvideo/vboxvideo.c
+++ src/VBox/Additions/x11/vboxvideo/vboxvideo.c
@@ -558,16 +558,22 @@ static DisplayModePtr
 vbox_output_get_modes (xf86OutputPtr output)
 {
     unsigned cIndex = 0;
-    DisplayModePtr pModes = NULL, pMode;
+    DisplayModePtr pModes = NULL;
     ScrnInfoPtr pScrn = output->scrn;
     VBOXPtr pVBox = VBOXGetRec(pScrn);
 
     TRACE_ENTRY();
     uint32_t iScreen = (uintptr_t)output->driver_private;
-    pMode = vbox_output_add_mode(pVBox, &pModes, NULL,
-                                 RT_CLAMP(pVBox->pScreens[iScreen].aPreferredSize.cx, VBOX_VIDEO_MIN_SIZE, VBOX_VIDEO_MAX_VIRTUAL),
-                                 RT_CLAMP(pVBox->pScreens[iScreen].aPreferredSize.cy, VBOX_VIDEO_MIN_SIZE, VBOX_VIDEO_MAX_VIRTUAL),
-                                 TRUE, FALSE);
+    vbox_output_add_mode(pVBox, &pModes, NULL,
+                         RT_CLAMP(pVBox->pScreens[iScreen].aPreferredSize.cx, VBOX_VIDEO_MIN_SIZE, VBOX_VIDEO_MAX_VIRTUAL),
+                         RT_CLAMP(pVBox->pScreens[iScreen].aPreferredSize.cy, VBOX_VIDEO_MIN_SIZE, VBOX_VIDEO_MAX_VIRTUAL),
+                         TRUE, FALSE);
+    vbox_output_add_mode(pVBox, &pModes, NULL, 1600, 1200, FALSE, FALSE);
+    vbox_output_add_mode(pVBox, &pModes, NULL, 1440, 1050, FALSE, FALSE);
+    vbox_output_add_mode(pVBox, &pModes, NULL, 1280, 960,  FALSE, FALSE);
+    vbox_output_add_mode(pVBox, &pModes, NULL, 1024, 768,  FALSE, FALSE);
+    vbox_output_add_mode(pVBox, &pModes, NULL, 800,  600,  FALSE, FALSE);
+    vbox_output_add_mode(pVBox, &pModes, NULL, 640,  480,  FALSE, FALSE);
     TRACE_EXIT();
     return pModes;
 }
