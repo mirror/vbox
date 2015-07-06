06c8bac9fecfd9c1e2a3a98114b10cd04d0f0180
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2015-07-06 17:07:36 +0000

    NAT: dnsproxy_answer: make sure the answer is really for our query, to enforce socket/query correspondence.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@56826 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Devices/Network/slirp/dnsproxy/dnsproxy.c
+++ src/VBox/Devices/Network/slirp/dnsproxy/dnsproxy.c
@@ -534,9 +534,9 @@ dnsproxy_answer(PNATState pData, struct socket *so, struct mbuf *m)
         return;
     }
 
+    /* find corresponding query (XXX: but see below) */
     query = hash_find_request(pData, *((unsigned short *)buf));
 
-    /* find corresponding query */
     if (query == NULL)
     {
         /* XXX: if we haven't found anything for this request ...
@@ -547,6 +547,19 @@ dnsproxy_answer(PNATState pData, struct socket *so, struct mbuf *m)
         Log2(("NAT: query wasn't found\n"));
         return;
     }
+
+    /*
+     * XXX: The whole hash thing is pretty meaningless right now since
+     * we use a separate socket for each request, so we already know
+     * the answer.
+     *
+     * If the answer is not what we expect it to be, then it's
+     * probably a stray or malicious reply and we'd better not free a
+     * query owned by some other socket - that would cause
+     * use-after-free later on.
+     */
+    if (query != so->so_timeout_arg)
+        return;
 
     so->so_timeout = NULL;
     so->so_timeout_arg = NULL;
