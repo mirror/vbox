d4cdd9e26bf9ad519b9e15b4ba9420386e0f9855
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2015-07-09 13:11:57 +0000

    VMM/GIM: comment clarification and typo.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@56897 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/VMM/VMMAll/GIMAllKvm.cpp
+++ src/VBox/VMM/VMMAll/GIMAllKvm.cpp
@@ -370,8 +370,8 @@ VMM_INT_DECL(int) gimKvmXcptUD(PVMCPU pVCpu, PCPUMCTX pCtx, PDISCPUSTATE pDis)
     if (!pDis)
     {
         /*
-         * Disassemble the instruction at RIP to figure out if it's the Intel
-         * VMCALL instruction and if so, handle it as a hypercall.
+         * Disassemble the instruction at RIP to figure out if it's the Intel VMCALL instruction
+         * or the AMD VMMCALL instruction and if so, handle it as a hypercall.
          */
         DISCPUSTATE Dis;
         rc = EMInterpretDisasCurrent(pVM, pVCpu, &Dis, NULL /* pcbInstr */);
@@ -403,7 +403,7 @@ VMM_INT_DECL(int) gimKvmXcptUD(PVMCPU pVCpu, PCPUMCTX pCtx, PDISCPUSTATE pDis)
             /*
              * Perform the hypercall and update RIP.
              *
-             * For HM, we can simply resume guest execution without perform the hypercall now and
+             * For HM, we can simply resume guest execution without performing the hypercall now and
              * do it on the next VMCALL/VMMCALL exit handler on the patched instruction.
              *
              * For raw-mode we need to do this now anyway. So we do it here regardless with an added
