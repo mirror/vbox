de335a5018fe5bb57dde5a71897c46cd0f5440cc
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2015-07-17 14:24:47 +0000

    HGSMIHost.cpp: Use AssertLogRelRCReturn instead of AssertRCReturn when loading state so we can pinpoint errors more easily.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@56972 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Devices/Graphics/HGSMI/HGSMIHost.cpp
+++ src/VBox/Devices/Graphics/HGSMI/HGSMIHost.cpp
@@ -1337,12 +1337,12 @@ int HGSMIHostLoadStateExec (PHGSMIINSTANCE pIns, PSSMHANDLE pSSM, uint32_t u32Ve
     }
 
     rc = SSMR3GetU32(pSSM, &off);
-    AssertRCReturn(rc, rc);
+    AssertLogRelRCReturn(rc, rc);
     pIns->pHGFlags = (off != HGSMIOFFSET_VOID) ? (HGSMIHOSTFLAGS*)HGSMIOffsetToPointer (&pIns->area, off) : NULL;
 
     HGSMIHOSTHEAP hHeap = pIns->hostHeap;
     rc = SSMR3GetU32(pSSM, &off);
-    AssertRCReturn(rc, rc);
+    AssertLogRelRCReturn(rc, rc);
     if(off != HGSMIOFFSET_VOID)
     {
         /* There is a saved heap. */
@@ -1359,7 +1359,7 @@ int HGSMIHostLoadStateExec (PHGSMIINSTANCE pIns, PSSMHANDLE pSSM, uint32_t u32Ve
         SSMR3GetU32(pSSM, &cbHeap);
         uint64_t oldMem;
         rc = SSMR3GetU64(pSSM, &oldMem);
-        AssertRCReturn(rc, rc);
+        AssertLogRelRCReturn(rc, rc);
 
         if (RT_SUCCESS(rc))
         {
