666e32f5b3a55bb71d09a485f01bf93a869e8071
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2015-07-18 00:18:26 +0000

    gccplugin.cpp: a wee bit further.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@56975 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/bldprogs/gccplugin.cpp
+++ src/bldprogs/gccplugin.cpp
@@ -53,9 +53,10 @@ int plugin_is_GPL_compatible;
 #endif
 
 
-/** For use with messages. */
-#define MY_LOC(a_hPreferred, a_pState) \
-    (DECL_P(a_hPreferred) ? DECL_SOURCE_LOCATION(a_hPreferred) : gimple_location((a_pState)->hStmt))
+/** For use with messages.
+ * @todo needs some more work... Actually, seems we're a bit handicapped by
+ *       working on gimplified stuff. */
+#define MY_LOC(a_hPreferred, a_pState) EXPR_LOC_OR_LOC(a_hPreferred, (a_pState)->hFmtLoc)
 
 
 /*******************************************************************************
@@ -67,6 +68,7 @@ typedef struct MYCHECKSTATE
     gimple      hStmt;
     long        iFmt;
     long        iArgs;
+    location_t  hFmtLoc;
 } MYCHECKSTATE;
 /** Pointer to my checker state. */
 typedef MYCHECKSTATE *PMYCHECKSTATE;
@@ -201,9 +203,76 @@ static bool             MyPassGateCallback(void)
 }
 
 
-static void MyCheckFormatCString(PMYCHECKSTATE pState, const char *pszFmt, location_t hFmtLoc)
+#define MYSTATE_FMT_FILE(a_pState)      LOCATION_FILE((a_pState)->hFmtLoc)
+#define MYSTATE_FMT_LINE(a_pState)      LOCATION_LINE((a_pState)->hFmtLoc)
+#define MYSTATE_FMT_COLUMN(a_pState)    LOCATION_COLUMN((a_pState)->hFmtLoc)
+
+
+/**
+ * Checks that @a iFmtArg isn't present or a valid final dummy argument.
+ *
+ * Will issue warning/error if there are more arguments at @a iFmtArg.
+ *
+ * @param   pState              The format string checking state.
+ * @param   iArg                The index of the end of arguments, this is
+ *                              relative to MYCHECKSTATE::iArgs.
+ */
+void MyCheckFinalArg(PMYCHECKSTATE pState, unsigned iArg)
 {
-    dprintf("checker2: \"%s\"\n", pszFmt);
+    dprintf("MyCheckFinalArg: iArg=%u iArgs=%ld cArgs=%u\n", iArg, pState->iArgs, gimple_call_num_args(pState->hStmt));
+    if (pState->iArgs > 0)
+    {
+        iArg += pState->iArgs - 1;
+        unsigned cArgs = gimple_call_num_args(pState->hStmt);
+        if (iArg == cArgs)
+        { /* fine */ }
+        else if (iArg < cArgs)
+        {
+            tree hArg = gimple_call_arg(pState->hStmt, iArg);
+            if (cArgs - iArg > 1)
+                warning_at(MY_LOC(hArg, pState), 0, "%u extra arguments not consumed by format string", cArgs - iArg);
+            else if (   TREE_CODE(hArg) != INTEGER_CST
+                     || TREE_INT_CST(hArg).fits_shwi()
+                     || TREE_INT_CST(hArg).to_shwi() != -99) /* ignore final dummy argument: ..., -99); */
+                warning_at(MY_LOC(hArg, pState), 0, "one extra argument not consumed by format string");
+        }
+        /* This should be handled elsewhere, but just in case. */
+        else if (iArg - 1 == cArgs)
+            warning_at(pState->hFmtLoc, 0, "one argument too few");
+        else
+            warning_at(pState->hFmtLoc, 0, "%u arguments too few", iArg - cArgs);
+    }
+}
+
+
+/**
+ * Does the actual format string checking.
+ *
+ * @todo    Move this to different file common to both GCC and CLANG later.
+ *
+ * @param   pState              The format string checking state.
+ * @param   pszFmt              The format string.
+ */
+void MyCheckFormatCString(PMYCHECKSTATE pState, const char *pszFmt)
+{
+    dprintf("checker2: \"%s\" at %s:%d col %d\n", pszFmt,
+            MYSTATE_FMT_FILE(pState), MYSTATE_FMT_LINE(pState), MYSTATE_FMT_COLUMN(pState));
+    unsigned iArg = 0;
+    for (;;)
+    {
+        /*
+         * Skip to the next argument.
+         */
+        char ch;
+        while ((ch = *pszFmt++) != '%')
+            if (ch == '\0')
+            {
+                MyCheckFinalArg(pState, iArg);
+                return;
+            }
+
+
+    }
 }
 
 
@@ -213,13 +282,12 @@ static void MyCheckFormatCString(PMYCHECKSTATE pState, const char *pszFmt, locat
  * This will attempt to result @a hFmtArg into a string literal which it then
  * passes on to MyCheckFormatString for the actual analyzis.
  *
- * @param   pState              The format string check state.
+ * @param   pState              The format string checking state.
  * @param   hFmtArg             The format string node.
  */
-DECL_NO_INLINE(static, void) MyCheckFormatWorker(PMYCHECKSTATE pState, tree hFmtArg)
+DECL_NO_INLINE(static, void) MyCheckFormatNonRecursive(PMYCHECKSTATE pState, tree hFmtArg)
 {
     dprintf("checker: hFmtArg=%p %s\n", hFmtArg, tree_code_name[TREE_CODE(hFmtArg)]);
-    location_t hFmtLoc = MY_LOC(hFmtArg, pState);
 
     /*
      * Try resolve variables into constant strings.
@@ -235,7 +303,7 @@ DECL_NO_INLINE(static, void) MyCheckFormatWorker(PMYCHECKSTATE pState, tree hFmt
      * Fend off NULLs.
      */
     if (integer_zerop(hFmtArg))
-        warning_at(hFmtLoc, 0, "Format string should not be NULL");
+        warning_at(MY_LOC(hFmtArg, pState), 0, "Format string should not be NULL");
     /*
      * Need address expression to get any further.
      */
@@ -243,7 +311,7 @@ DECL_NO_INLINE(static, void) MyCheckFormatWorker(PMYCHECKSTATE pState, tree hFmt
         dprintf("checker1: Not address expression (%s)\n", tree_code_name[TREE_CODE(hFmtArg)]);
     else
     {
-        hFmtLoc = EXPR_LOC_OR_LOC(hFmtArg, hFmtLoc);
+        pState->hFmtLoc = EXPR_LOC_OR_LOC(hFmtArg, pState->hFmtLoc);
         hFmtArg = TREE_OPERAND(hFmtArg, 0);
 
         /*
@@ -286,7 +354,7 @@ DECL_NO_INLINE(static, void) MyCheckFormatWorker(PMYCHECKSTATE pState, tree hFmt
         if (TREE_CODE(hFmtArg) != STRING_CST)
             dprintf("checker1: Not string literal (%s)\n", tree_code_name[TREE_CODE(hFmtArg)]);
         else if (TYPE_MAIN_VARIANT(TREE_TYPE(TREE_TYPE(hFmtArg))) != char_type_node)
-            warning_at(hFmtLoc, 0, "expected 'char' type string literal");
+            warning_at(pState->hFmtLoc, 0, "expected 'char' type string literal");
         else
         {
             /*
@@ -299,15 +367,15 @@ DECL_NO_INLINE(static, void) MyCheckFormatWorker(PMYCHECKSTATE pState, tree hFmt
             if (hArraySize != NULL_TREE)
             {
                 if (TREE_CODE(hArraySize) != INTEGER_CST)
-                    warning_at(hFmtLoc, 0, "Expected integer array size (not %s)", tree_code_name[TREE_CODE(hArraySize)]);
+                    warning_at(pState->hFmtLoc, 0, "Expected integer array size (not %s)", tree_code_name[TREE_CODE(hArraySize)]);
                 else if (!TREE_INT_CST(hArraySize).fits_shwi())
-                    warning_at(hFmtLoc, 0, "Unexpected integer overflow in array size constant");
+                    warning_at(pState->hFmtLoc, 0, "Unexpected integer overflow in array size constant");
                 else
                 {
                     HOST_WIDE_INT cbArray = TREE_INT_CST(hArraySize).to_shwi();
                     if (   cbArray <= 0
                         || cbArray != (int)cbArray)
-                        warning_at(hFmtLoc, 0, "Unexpected integer array size constant value: %ld", cbArray);
+                        warning_at(pState->hFmtLoc, 0, "Unexpected integer array size constant value: %ld", cbArray);
                     else if (cchFmt > cbArray)
                     {
                         dprintf("checker1: cchFmt=%d => cchFmt=%ld (=cbArray)\n", cchFmt, cbArray);
@@ -333,12 +401,12 @@ DECL_NO_INLINE(static, void) MyCheckFormatWorker(PMYCHECKSTATE pState, tree hFmt
              */
             if (   cchFmt < 1
                 || pszFmt[cchFmt - 1] != '\0')
-                warning_at(hFmtLoc, 0, "Unterminated format string (cchFmt=%d)", cchFmt);
+                warning_at(pState->hFmtLoc, 0, "Unterminated format string (cchFmt=%d)", cchFmt);
             /*
              * Call worker to check the actual string.
              */
             else
-                MyCheckFormatCString(pState, pszFmt, hFmtLoc);
+                MyCheckFormatCString(pState, pszFmt);
         }
     }
 }
@@ -347,9 +415,9 @@ DECL_NO_INLINE(static, void) MyCheckFormatWorker(PMYCHECKSTATE pState, tree hFmt
 /**
  * Deal recursively with special format string constructs.
  *
- * This will call MyCheckFormatWorker to validate each format string.
+ * This will call MyCheckFormatNonRecursive to validate each format string.
  *
- * @param   pState              The format string check state.
+ * @param   pState              The format string checking state.
  * @param   hFmtArg             The format string node.
  */
 static void MyCheckFormatRecursive(PMYCHECKSTATE pState, tree hFmtArg)
@@ -377,7 +445,7 @@ static void MyCheckFormatRecursive(PMYCHECKSTATE pState, tree hFmtArg)
      * We're good, hand it to the non-recursive worker.
      */
     else
-        MyCheckFormatWorker(pState, hFmtArg);
+        MyCheckFormatNonRecursive(pState, hFmtArg);
 }
 
 
@@ -442,9 +510,10 @@ static unsigned int     MyPassExecuteCallback(void)
                      */
                     tree const hAttrArgs = TREE_VALUE(hAttr);
                     MYCHECKSTATE State;
-                    State.iFmt  = TREE_INT_CST(TREE_VALUE(hAttrArgs)).to_shwi();
-                    State.iArgs = TREE_INT_CST(TREE_VALUE(TREE_CHAIN(hAttrArgs))).to_shwi();
-                    State.hStmt = hStmt;
+                    State.iFmt    = TREE_INT_CST(TREE_VALUE(hAttrArgs)).to_shwi();
+                    State.iArgs   = TREE_INT_CST(TREE_VALUE(TREE_CHAIN(hAttrArgs))).to_shwi();
+                    State.hStmt   = hStmt;
+                    State.hFmtLoc = gimple_location(hStmt);
                     dprintf("     %s() __iprt_format__(iFmt=%ld, iArgs=%ld)\n",
                             DECL_NAME(hFnDecl) ? IDENTIFIER_POINTER(DECL_NAME(hFnDecl)) : "<unamed>", State.iFmt, State.iArgs);
 
