ef00b76d8817a6a5466e17221e82e456d799a603
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2015-07-30 11:54:06 +0000

    VMM/HM: Avoid duplicate line when a VT-x feature must be set.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@57127 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/VMM/VMMR3/HM.cpp
+++ src/VBox/VMM/VMMR3/HM.cpp
@@ -280,11 +280,14 @@ static const char * const g_apszAmdVExitReasons[MAX_EXITREASON_STAT] =
 #define HMVMX_REPORT_FEATURE(allowed1, disallowed0, featflag) \
     do { \
         if ((allowed1) & (featflag)) \
-            LogRel(("HM:   " #featflag "\n")); \
+        { \
+            if ((disallowed0) & (featflag)) \
+                LogRel(("HM:   " #featflag " (must be set)\n")); \
+            else \
+                LogRel(("HM:   " #featflag "\n")); \
+        } \
         else \
             LogRel(("HM:   " #featflag " (must be cleared)\n")); \
-        if ((disallowed0) & (featflag)) \
-            LogRel(("HM:   " #featflag " (must be set)\n")); \
     } while (0)
 
 #define HMVMX_REPORT_ALLOWED_FEATURE(allowed1, featflag) \
