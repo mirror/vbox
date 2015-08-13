c124be70a9750d14eb5fa77b29bab78d9d376c1f
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2015-08-13 11:46:45 +0000

    build fix
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@57318 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Runtime/r0drv/linux/the-linux-kernel.h
+++ src/VBox/Runtime/r0drv/linux/the-linux-kernel.h
@@ -382,11 +382,9 @@ DECLINLINE(unsigned long) msecs_to_jiffies(unsigned int cMillies)
  * The AMD 64 switch_to in macro in arch/x86/include/asm/switch_to.h stopped
  * restoring flags.
  * @{ */
-# define IPRT_X86_EFL_AC                    RT_BIT(18)
-#ifdef CONFIG_X86_SMAP
-# include <iprt/asm-amd64-x86.h>
-#endif
 #if defined(CONFIG_X86_SMAP) || defined(RT_STRICT) || defined(IPRT_WITH_EFLAGS_AC_PRESERVING)
+# include <iprt/asm-amd64-x86.h>
+# define IPRT_X86_EFL_AC                    RT_BIT(18)
 # define IPRT_LINUX_SAVE_EFL_AC()           RTCCUINTREG fSavedEfl = ASMGetFlags()
 # define IPRT_LINUX_RESTORE_EFL_AC()        ASMSetFlags(fSavedEfl)
 # define IPRT_LINUX_RESTORE_EFL_ONLY_AC()   ASMSetFlags((ASMGetFlags() & ~IPRT_X86_EFL_AC) | (fSavedEfl & IPRT_X86_EFL_AC))
