4401868f59160ce153aaa98a6db91b6154d3db9e
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2015-08-21 13:50:47 +0000

    FE/Qt: Fixed a small coding guidline.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@57497 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Frontends/VirtualBox/src/UIVMLogViewer.cpp
+++ src/VBox/Frontends/VirtualBox/src/UIVMLogViewer.cpp
@@ -679,14 +679,14 @@ void UIVMLogViewer::saveSettings()
     /* Save window geometry: */
     {
         /* Save geometry: */
-        const QRect save_geometry = geometry();
+        const QRect saveGeometry = geometry();
 #ifdef Q_WS_MAC
-        gEDataManager->setLogWindowGeometry(save_geometry, ::darwinIsWindowMaximized(this));
+        gEDataManager->setLogWindowGeometry(saveGeometry, ::darwinIsWindowMaximized(this));
 #else /* Q_WS_MAC */
-        gEDataManager->setLogWindowGeometry(save_geometry, isMaximized());
+        gEDataManager->setLogWindowGeometry(saveGeometry, isMaximized());
 #endif /* !Q_WS_MAC */
         LogRel(("GUI: UIVMLogViewer: Geometry saved as: %dx%d @ %dx%d\n",
-                save_geometry.x(), save_geometry.y(), save_geometry.width(), save_geometry.height()));
+                saveGeometry.x(), saveGeometry.y(), saveGeometry.width(), saveGeometry.height()));
     }
 }
 
