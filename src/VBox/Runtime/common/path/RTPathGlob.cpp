7935a356ea08412c08f065479327e9f2dc5f1463
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2015-09-10 11:29:40 +0000

    RTPathGlob: fix for a another stupid bug.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@57686 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Runtime/common/path/RTPathGlob.cpp
+++ src/VBox/Runtime/common/path/RTPathGlob.cpp
@@ -2062,8 +2062,8 @@ RTDECL(int) RTPathGlob(const char *pszPattern, uint32_t fFlags, PPCRTPATHGLOBENT
     int rc = RTPathParse(pszPattern, pParsed, cbParsed, RTPATH_STR_F_STYLE_HOST);
     if (rc == VERR_BUFFER_OVERFLOW)
     {
-        RTMemTmpFree(pParsed);
         cbParsed = RT_OFFSETOF(RTPATHPARSED, aComps[pParsed->cComps + 1]);
+        RTMemTmpFree(pParsed);
         pParsed = (PRTPATHPARSED)RTMemTmpAlloc(cbParsed);
         AssertReturn(pParsed, VERR_NO_MEMORY);
 
