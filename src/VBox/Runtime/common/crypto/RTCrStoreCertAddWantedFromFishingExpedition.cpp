b6e27558269210fa679cdf2dd595281371f0effd
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2015-09-10 19:20:16 +0000

    RTCrStoreCertAddWantedFromFishingExpedition.cpp: Solaris 11 update.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@57697 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Runtime/common/crypto/RTCrStoreCertAddWantedFromFishingExpedition.cpp
+++ src/VBox/Runtime/common/crypto/RTCrStoreCertAddWantedFromFishingExpedition.cpp
@@ -148,6 +148,7 @@ RTDECL(int) RTCrStoreCertAddWantedFromFishingExpedition(RTCRSTORE hStore, uint32
 #endif
 #ifdef RT_OS_SOLARIS
             PREFIX_UNIXROOT "/usr/java/jre/lib/security/cacerts",
+            PREFIX_UNIXROOT "/usr/jdk/instances/*/jre/lib/security/cacerts",
 #endif
 #ifdef RT_OS_WINDOWS
             "${AllProgramFiles}/Git/bin/curl-ca-bundle.crt",
