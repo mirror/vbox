370cfac3e8fd632550c3be5b87fe7beb6b71334f
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2015-09-14 08:27:42 +0000

    Windows build fix
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@57730 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Additions/common/VBoxGuestLib/VBoxGuestR3LibDaemonize.cpp
+++ src/VBox/Additions/common/VBoxGuestLib/VBoxGuestR3LibDaemonize.cpp
@@ -51,7 +51,6 @@
 # include <errno.h>
 #endif
 
-#include <iprt/file.h>
 #include <iprt/process.h>
 #include <iprt/string.h>
 #include "VBGLR3Internal.h"
@@ -250,77 +249,3 @@ VBGLR3DECL(int) VbglR3Daemonize(bool fNoChDir, bool fNoClose, bool fRespawn, uns
     return VINF_SUCCESS;
 #endif
 }
-
-
-/**
- * Creates a PID File and returns the open file descriptor.
- *
- * On DOS based system, file sharing (deny write) is used for locking the PID
- * file.
- *
- * On Unix-y systems, an exclusive advisory lock is used for locking the PID
- * file since the file sharing support is usually missing there.
- *
- * This API will overwrite any existing PID Files without a lock on them, on the
- * assumption that they are stale files which an old process did not properly
- * clean up.
- *
- * @returns IPRT status code.
- * @param   pszPath  The path and filename to create the PID File under
- * @param   phFile   Where to store the file descriptor of the open (and locked
- *                   on Unix-y systems) PID File. On failure, or if another
- *                   process owns the PID File, this will be set to NIL_RTFILE.
- */
-VBGLR3DECL(int) VbglR3PidFile(const char *pszPath, PRTFILE phFile)
-{
-    AssertPtrReturn(pszPath, VERR_INVALID_PARAMETER);
-    AssertPtrReturn(phFile, VERR_INVALID_PARAMETER);
-    *phFile = NIL_RTFILE;
-
-    RTFILE hPidFile;
-    int rc = RTFileOpen(&hPidFile, pszPath,
-                        RTFILE_O_READWRITE | RTFILE_O_OPEN_CREATE | RTFILE_O_DENY_WRITE
-                        | (0644 << RTFILE_O_CREATE_MODE_SHIFT));
-    if (RT_SUCCESS(rc))
-    {
-#if !defined(RT_OS_WINDOWS) && !defined(RT_OS_OS2)
-        /** @todo using size 0 for locking means lock all on Posix.
-         * We should adopt this as our convention too, or something
-         * similar. */
-        rc = RTFileLock(hPidFile, RTFILE_LOCK_WRITE, 0, 0);
-        if (RT_FAILURE(rc))
-            RTFileClose(hPidFile);
-        else
-#endif
-        {
-            char szBuf[256];
-            size_t cbPid = RTStrPrintf(szBuf, sizeof(szBuf), "%d\n",
-                                       RTProcSelf());
-            RTFileWrite(hPidFile, szBuf, cbPid, NULL);
-            *phFile = hPidFile;
-        }
-    }
-    return rc;
-}
-
-
-/**
- * Close and remove an open PID File.
- *
- * @param  pszPath  The path to the PID File,
- * @param  hFile    The handle for the file. NIL_RTFILE is ignored as usual.
- */
-VBGLR3DECL(void) VbglR3ClosePidFile(const char *pszPath, RTFILE hFile)
-{
-    AssertPtrReturnVoid(pszPath);
-    if (hFile != NIL_RTFILE)
-    {
-#if defined(RT_OS_WINDOWS) || defined(RT_OS_OS2)
-        RTFileWriteAt(hFile, 0, "-1", 2, NULL);
-#else
-        RTFileDelete(pszPath);
-#endif
-        RTFileClose(hFile);
-    }
-}
-
