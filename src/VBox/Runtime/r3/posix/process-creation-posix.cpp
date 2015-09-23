80282f6cf786c3c8f6a3a8f353674e95484bbdb6
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2015-09-23 13:59:12 +0000

    process-creation-posix.cpp: darwin build fix.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@57871 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Runtime/r3/posix/process-creation-posix.cpp
+++ src/VBox/Runtime/r3/posix/process-creation-posix.cpp
@@ -291,11 +291,11 @@ static int rtProcPosixAdjustProfileEnvFromChild(RTENV hEnvToUse, uint32_t fFlags
         && (!(fFlags & RTPROC_FLAGS_ENV_CHANGE_RECORD) || RTEnvExistEx(hEnv, "TMPDIR")) )
     {
         char szValue[_4K];
-        rc = confstr(_SC_DARWIN_USER_TEMP_DIR, szValue, sizeof(szValue));
-        if (rc > 0 && rc < sizeof(szValue))
+        size_t cbNeeded = confstr(_CS_DARWIN_USER_TEMP_DIR, szValue, sizeof(szValue));
+        if (cbNeeded > 0 && cbNeeded < sizeof(szValue))
         {
-            char *pszTmp
-            rc = RTStrCurrentCPToUtf8(&pszTmp, achBuf);
+            char *pszTmp;
+            rc = RTStrCurrentCPToUtf8(&pszTmp, szValue);
             if (RT_SUCCESS(rc))
             {
                 rc = RTEnvSetEx(hEnvToUse, "TMPDIR", pszTmp);
@@ -372,10 +372,10 @@ static int rtProcPosixCreateProfileEnv(PRTENV phEnvToUse, const char *pszUser)
 #ifdef RT_OS_DARWIN
                         if (RT_SUCCESS(rc) && !pszUserFree)
                         {
-                            rc = confstr(_SC_DARWIN_USER_TEMP_DIR, achBuf, sizeof(achBuf));
-                            if (rc > 0 && rc < sizeof(achBuf))
+                            size_t cbNeeded = confstr(_CS_DARWIN_USER_TEMP_DIR, achBuf, sizeof(achBuf));
+                            if (cbNeeded > 0 && cbNeeded < sizeof(achBuf))
                             {
-                                char *pszTmp
+                                char *pszTmp;
                                 rc = RTStrCurrentCPToUtf8(&pszTmp, achBuf);
                                 if (RT_SUCCESS(rc))
                                 {
