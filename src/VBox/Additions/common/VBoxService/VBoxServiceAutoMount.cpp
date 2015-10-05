78ccb6e7552146fde434544e8a0b8ae6992bcc32
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2015-10-05 20:56:02 +0000

    VBoxService: build fix
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@58030 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Additions/common/VBoxService/VBoxServiceAutoMount.cpp
+++ src/VBox/Additions/common/VBoxService/VBoxServiceAutoMount.cpp
@@ -331,10 +331,9 @@ static int vbsvcAutoMountSharedFolder(const char *pszShareName, const char *pszM
                       &mntinf);
         if (r == 0)
         {
-            VGSvcVerbose(0, "vbsvcAutoMountWorker: Shared folder '%s' was mounted to '%s'\n",
-                               pszShareName, pszMountPoint);
+            VGSvcVerbose(0, "vbsvcAutoMountWorker: Shared folder '%s' was mounted to '%s'\n", pszShareName, pszMountPoint);
 
-            r = vbsfmount_complete(pszShareName, pszMountPoint, flags, pOpts);
+            r = vbsfmount_complete(pszShareName, pszMountPoint, fFlags, pOpts);
             switch (r)
             {
                 case 0: /* Success. */
@@ -386,7 +385,7 @@ static int vbsvcAutoMountSharedFolder(const char *pszShareName, const char *pszM
                     /* We checked before that we have enough space */
                     strcpy(mntinf.name, pszMountPoint + cchCWD);
                 }
-                r = mount(NULL, pszMountPoint, "vboxsf", flags, &mntinf);
+                r = mount(NULL, pszMountPoint, "vboxsf", fFlags, &mntinf);
             }
             if (errno == EPROTO)
             {
@@ -400,7 +399,7 @@ static int vbsvcAutoMountSharedFolder(const char *pszShareName, const char *pszM
                 mntinf_old.uid = mntinf.uid;
                 mntinf_old.gid = mntinf.gid;
                 mntinf_old.ttl = mntinf.ttl;
-                r = mount(NULL, pszMountPoint, "vboxsf", flags, &mntinf_old);
+                r = mount(NULL, pszMountPoint, "vboxsf", fFlags, &mntinf_old);
             }
             if (r == -1) /* Was there some error from one of the tries above? */
             {
