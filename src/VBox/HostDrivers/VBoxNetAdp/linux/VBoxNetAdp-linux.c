98efa241846a4e29bdef4e8170aeb48d422fb118
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2015-10-13 16:45:15 +0000

    VBoxNetAdp/linux: followup fix to r103374 - ethtool_cmd_speed_set() made its debut in 2.6.27
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@58222 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/HostDrivers/VBoxNetAdp/linux/VBoxNetAdp-linux.c
+++ src/VBox/HostDrivers/VBoxNetAdp/linux/VBoxNetAdp-linux.c
@@ -193,7 +193,11 @@ static int vboxNetAdpEthGetSettings(struct net_device *pNetDev, struct ethtool_c
 {
     cmd->supported      = 0;
     cmd->advertising    = 0;
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(2, 6, 27)
     ethtool_cmd_speed_set(cmd, SPEED_10);
+#else
+    cmd->speed          = SPEED_10;
+#endif
     cmd->duplex         = DUPLEX_FULL;
     cmd->port           = PORT_TP;
     cmd->phy_address    = 0;
