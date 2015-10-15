00b1e36b1044dddcf84f5e5e54cac22155d19752
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2015-10-15 10:21:38 +0000

    Storage/RAW: fix conversion to raw (it previously overwrote the converted data with zeroes due to accidentally removed offAccess updating)
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@58258 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Storage/RAW.cpp
+++ src/VBox/Storage/RAW.cpp
@@ -638,10 +638,21 @@ static DECLCALLBACK(int) rawRead(void *pBackendData, uint64_t uOffset, size_t cb
     int rc = VINF_SUCCESS;
     PRAWIMAGE pImage = (PRAWIMAGE)pBackendData;
 
+    /* For sequential access do not allow to go back. */
+    if (   pImage->uOpenFlags & VD_OPEN_FLAGS_SEQUENTIAL
+        && uOffset < pImage->offAccess)
+    {
+        *pcbActuallyRead = 0;
+        return VERR_INVALID_PARAMETER;
+    }
+
     rc = vdIfIoIntFileReadUser(pImage->pIfIo, pImage->pStorage, uOffset,
                                pIoCtx, cbRead);
     if (RT_SUCCESS(rc))
+    {
         *pcbActuallyRead = cbRead;
+        pImage->offAccess = uOffset + cbRead;
+    }
 
     return rc;
 }
@@ -654,6 +665,16 @@ static DECLCALLBACK(int) rawWrite(void *pBackendData, uint64_t uOffset, size_t c
     int rc = VINF_SUCCESS;
     PRAWIMAGE pImage = (PRAWIMAGE)pBackendData;
 
+    /* For sequential access do not allow to go back. */
+    if (   pImage->uOpenFlags & VD_OPEN_FLAGS_SEQUENTIAL
+        && uOffset < pImage->offAccess)
+    {
+        *pcbWriteProcess = 0;
+        *pcbPostRead = 0;
+        *pcbPreRead  = 0;
+        return VERR_INVALID_PARAMETER;
+    }
+
     rc = vdIfIoIntFileWriteUser(pImage->pIfIo, pImage->pStorage, uOffset,
                                 pIoCtx, cbWrite, NULL, NULL);
     if (RT_SUCCESS(rc))
@@ -661,6 +682,7 @@ static DECLCALLBACK(int) rawWrite(void *pBackendData, uint64_t uOffset, size_t c
         *pcbWriteProcess = cbWrite;
         *pcbPostRead = 0;
         *pcbPreRead  = 0;
+        pImage->offAccess = uOffset + cbWrite;
     }
 
     return rc;
