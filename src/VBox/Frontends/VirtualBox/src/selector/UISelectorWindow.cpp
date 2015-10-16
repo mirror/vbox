b23f17e754fe1175719d95562f08b0db6c8ade7a
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2015-10-16 16:47:45 +0000

    FE/Qt: 8026: Selector UI: Pause state synchronization in selector UI context menu when VM is paused/unpaused from VM's window menu(Machine->Pause).
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@58286 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Frontends/VirtualBox/src/selector/UISelectorWindow.cpp
+++ src/VBox/Frontends/VirtualBox/src/selector/UISelectorWindow.cpp
@@ -1808,12 +1808,20 @@ void UISelectorWindow::updateActionsAppearance()
     UIVMItem *pFirstStartedAction = 0;
     foreach (UIVMItem *pSelectedItem, items)
         if (UIVMItem::isItemStarted(pSelectedItem))
+        {
             pFirstStartedAction = pSelectedItem;
-    /* Update the Pause/Resume action appearance: */
+            break;
+        }
+    /* Update the group Pause/Resume action appearance: */
     actionPool()->action(UIActionIndexST_M_Group_T_Pause)->blockSignals(true);
     actionPool()->action(UIActionIndexST_M_Group_T_Pause)->setChecked(pFirstStartedAction && UIVMItem::isItemPaused(pFirstStartedAction));
     actionPool()->action(UIActionIndexST_M_Group_T_Pause)->retranslateUi();
     actionPool()->action(UIActionIndexST_M_Group_T_Pause)->blockSignals(false);
+    /* Update the machine Pause/Resume action appearance: */
+    actionPool()->action(UIActionIndexST_M_Machine_T_Pause)->blockSignals(true);
+    actionPool()->action(UIActionIndexST_M_Machine_T_Pause)->setChecked(pFirstStartedAction && UIVMItem::isItemPaused(pFirstStartedAction));
+    actionPool()->action(UIActionIndexST_M_Machine_T_Pause)->retranslateUi();
+    actionPool()->action(UIActionIndexST_M_Machine_T_Pause)->blockSignals(false);
 
 #ifdef QT_MAC_USE_COCOA
     /* Avoid bug in Qt Cocoa which results in showing a "more arrow" on size-hint changes: */
