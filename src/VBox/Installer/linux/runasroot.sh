36a420928af1161e1cf6a609f50804f6d2810423
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2015-10-21 08:59:00 +0000

    Installer/linux (guest): get rid of more auto-generation.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@58351 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Installer/linux/runasroot.sh
+++ src/VBox/Installer/linux/runasroot.sh
@@ -16,7 +16,37 @@
 # hope that it will be useful, but WITHOUT ANY WARRANTY of any kind.
 #
 
-#include sh-utils.sh
+# Deal with differing "which" semantics
+mywhich() {
+    which "$1" 2>/dev/null | grep -v "no $1"
+}
+
+# Get the name and execute switch for a useful terminal emulator
+#
+# Sets $gxtpath to the emulator path or empty
+# Sets $gxttitle to the "title" switch for that emulator
+# Sets $gxtexec to the "execute" switch for that emulator
+# May clobber $gtx*
+# Calls mywhich
+getxterm() {
+    # gnome-terminal uses -e differently to other emulators
+    for gxti in "konsole --title -e" "gnome-terminal --title -x" "xterm -T -e"; do
+        set $gxti
+        gxtpath="`mywhich $1`"
+        case "$gxtpath" in ?*)
+            gxttitle=$2
+            gxtexec=$3
+            return
+            ;;
+        esac
+    done
+}
+
+# Quotes its argument by inserting '\' in front of every character save
+# for 'A-Za-z0-9/'.  Prints the result to stdout.
+quotify() {
+    echo "$1" | sed -e 's/\([^a-zA-Z0-9/]\)/\\\1/g'
+}
 
 ostype=`uname -s`
 if test "$ostype" != "Linux" && test "$ostype" != "SunOS" ; then
