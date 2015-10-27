0303825e1dc9bb180e8e31326ba0dda79ce709bf
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2015-10-27 16:11:52 +0000

    VBoxManage: don't crash on 'control addencpassword'
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@58434 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Frontends/VBoxManage/VBoxManageControlVM.cpp
+++ src/VBox/Frontends/VBoxManage/VBoxManageControlVM.cpp
@@ -1683,15 +1683,20 @@ RTEXITCODE handleControlVM(HandlerArg *a)
                 break;
             }
 
-            if (   strcmp(a->argv[4], "--removeonsuspend")
-                || (   strcmp(a->argv[5], "yes")
-                    && strcmp(a->argv[5], "no")))
+            BOOL fRemoveOnSuspend = FALSE;
+            if (a->argc == 6)
             {
-                errorSyntax(USAGE_CONTROLVM, "Invalid parameters");
-                break;
+                if (   strcmp(a->argv[4], "--removeonsuspend")
+                    || (   strcmp(a->argv[5], "yes")
+                        && strcmp(a->argv[5], "no")))
+                {
+                    errorSyntax(USAGE_CONTROLVM, "Invalid parameters");
+                    break;
+                }
+                if (!strcmp(a->argv[5], "yes"))
+                    fRemoveOnSuspend = TRUE;
             }
 
-            BOOL fRemoveOnSuspend = FALSE;
             Bstr bstrPwId(a->argv[2]);
             Utf8Str strPassword;
 
@@ -1711,10 +1716,6 @@ RTEXITCODE handleControlVM(HandlerArg *a)
                     break;
                 }
             }
-
-            if (   a->argc == 6
-                && !strcmp(a->argv[5], "yes"))
-                fRemoveOnSuspend = TRUE;
 
             CHECK_ERROR_BREAK(console, AddDiskEncryptionPassword(bstrPwId.raw(), Bstr(strPassword).raw(), fRemoveOnSuspend));
         }
