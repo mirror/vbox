37e7625126e0f1f95336ee76edb5d3294138e741
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2015-10-27 14:37:07 +0000

    linux/load.sh: It's not acceptable to assert that loadall.sh and load.sh can't be mixed. Switch back to bash and restore the ability to pass arguments to make. Try autodetect the make job number from /proc/cpuinfo.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@58428 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/HostDrivers/linux/load.sh
+++ src/VBox/HostDrivers/linux/load.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 ## @file
 # For development, builds and loads all the host drivers.
 #
@@ -15,26 +15,42 @@
 # hope that it will be useful, but WITHOUT ANY WARRANTY of any kind.
 #
 
-# The below is GNU-specific.  See VBox.sh for the longer Solaris/OS X version.
-TARGET=`readlink -e -- "${0}"` || exit 1
+TARGET=`readlink -e -- "${0}"` || exit 1  # The GNU-specific way.
 MY_DIR="${TARGET%/[!/]*}"
 
 set -e
-if ! test   `echo /etc/udev/rules.d/*-vboxdrv.rules` \
-          = "/etc/udev/rules.d/*-vboxdrv.rules"; then
-    echo "You can not use this script while you have a version of VirtualBox installed."
-    echo "If you are running from the build directory you may have installed using"
-    echo "loadall.sh.  You may wish to re-run that."
-    ## @todo Any one who needs different behaviour should decide what and do it.
+# bird: This is not acceptable.
+#if ! test   "`echo /etc/udev/rules.d/*-vboxdrv.rules`" \
+#          = "/etc/udev/rules.d/*-vboxdrv.rules"; then
+#    echo "You can not use this script while you have a version of VirtualBox installed."
+#    echo "If you are running from the build directory you may have installed using"
+#    echo "loadall.sh.  You may wish to re-run that."
+#    ## @todo Any one who needs different behaviour should decide what and do it.
+#    exit 1
+#fi
+
+# Parse parameters.
+OPT_UNLOAD_ONLY=
+if [ ${#} -ge 1 -a "${1}" = "-u" ]; then
+    OPT_UNLOAD_ONLY=yes
+    shift
+fi
+if [ ${#} -ge 1 -a '(' "${1}" = "-h"  -o  "${1}" = "--help" ')' ]; then
+    echo "usage: load.sh [-u] [make arguments]"
     exit 1
 fi
-test ${#} -eq 0 ||
-    if ! test ${#} -eq 1 || ! test "x${1}" = x-u; then
-        echo "Usage: load.sh [-u]"
-        exit 1
-    fi
-sudo "${MY_DIR}/vboxdrv.sh" stop
-test ${#} -eq 0 || exit 0
-make -C "${MY_DIR}/src/vboxdrv" "$@"
-echo "Installing SUPDrv (aka VBoxDrv/vboxdrv)"
-sudo /sbin/insmod "${MY_DIR}/src/vboxdrv/vboxdrv.ko"
+
+# Unload.
+sudo "${MY_DIR}/vboxdrv.sh" stop ## @todo this removes the udev rules, which is very very unhelpful.
+
+if [ -z "${OPT_UNLOAD_ONLY}" ]; then
+    # Build and load.
+    set -x
+    MAKE_JOBS=`grep vendor_id /proc/cpuinfo | wc -l`
+    if [ "${MAKE_JOBS}" -le "0" ]; then MAKE_JOBS=1; fi
+    make "-j${MAKE_JOBS}" -C "${MY_DIR}/src/vboxdrv" "$@"
+
+    echo "Installing SUPDrv (aka VBoxDrv/vboxdrv)"
+    sudo /sbin/insmod "${MY_DIR}/src/vboxdrv/vboxdrv.ko"
+fi
+
