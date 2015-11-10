e155e8ebfc5cf3f0d29bbff2eb363344d59d3d75
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2015-11-10 22:53:17 +0000

    Forgot to add Intel_Core_i7_6700K.h before comitting it. duh.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@58653 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/VMM/VMMR3/CPUMR3Db.cpp
+++ src/VBox/VMM/VMMR3/CPUMR3Db.cpp
@@ -176,6 +176,7 @@ typedef struct CPUMDBENTRY
 /** @} */
 
 
+#include "cpus/Intel_Core_i7_6700K.h"
 #include "cpus/Intel_Core_i7_5600U.h"
 #include "cpus/Intel_Core_i7_3960X.h"
 #include "cpus/Intel_Core_i5_3570.h"
