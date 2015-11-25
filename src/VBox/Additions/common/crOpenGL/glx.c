96dfcb3c662ebd7d9747f95018bcd014b6d91681
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2015-11-25 11:14:32 +0000

    Additions/common/3D: added Oracle copyright to glx.c.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@58846 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Additions/common/crOpenGL/glx.c
+++ src/VBox/Additions/common/crOpenGL/glx.c
@@ -1,4 +1,23 @@
-/* Copyright (c) 2001, Stanford University
+/* $Id: dri_drv.c 100879 2015-06-09 14:26:20Z bird $ */
+
+/** @file
+ * VBox OpenGL GLX implementation
+ */
+
+/*
+ * Copyright (C) 2009-2015 Oracle Corporation
+ *
+ * This file is part of VirtualBox Open Source Edition (OSE), as
+ * available from http://www.virtualbox.org. This file is free software;
+ * you can redistribute it and/or modify it under the terms of the GNU
+ * General Public License (GPL) as published by the Free Software
+ * Foundation, in version 2 as it comes in the "COPYING" file of the
+ * VirtualBox OSE distribution. VirtualBox OSE is distributed in the
+ * hope that it will be useful, but WITHOUT ANY WARRANTY of any kind.
+ * --------------------------------------------------------------------
+ * Original copyright notice:
+ *
+ * Copyright (c) 2001, Stanford University
  * All rights reserved
  *
  * See the file LICENSE.txt for information on redistributing this software.
