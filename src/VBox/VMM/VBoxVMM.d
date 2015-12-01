274ade46b51c4d519a9dc9baafe60367abcaad5c
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2015-12-01 14:25:16 +0000

    VBoxVMM.d: touch
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@58939 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/VMM/VBoxVMM.d
+++ src/VBox/VMM/VBoxVMM.d
@@ -100,6 +100,7 @@ provider vboxvmm
     probe int__software(struct VMCPU *a_pVCpu, struct CPUMCTX *a_pCtx, uint8_t a_iInterrupt);
     /** Hardware interrupt being dispatched. */
     probe int__hardware(struct VMCPU *a_pVCpu, struct CPUMCTX *a_pCtx, uint8_t a_iInterrupt, uint32_t a_uTag, uint32_t a_idSource);
+
 };
 
 #pragma D attributes Evolving/Evolving/Common provider vboxvmm provider
