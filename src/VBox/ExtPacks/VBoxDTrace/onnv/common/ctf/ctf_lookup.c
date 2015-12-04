31482061c901eda386c3d1a7128809a79312495b
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2015-12-04 15:21:52 +0000

    ctf_lookup.c: Fixed read beyond end of string.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@58990 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/ExtPacks/VBoxDTrace/onnv/common/ctf/ctf_lookup.c
+++ src/VBox/ExtPacks/VBoxDTrace/onnv/common/ctf/ctf_lookup.c
@@ -135,7 +135,12 @@ ctf_lookup_by_name(ctf_file_t *fp, const char *name)
 
 		for (lp = fp->ctf_lookups; lp->ctl_prefix != NULL; lp++) {
 			if (lp->ctl_prefix[0] == '\0' ||
+#ifdef VBOX
+			    (   strncmp(p, lp->ctl_prefix, (size_t)(q - p)) == 0
+			     && lp->ctl_len <= (size_t)(q - p)) /* don't push 'p' to far and crash */ ) {
+#else
 			    strncmp(p, lp->ctl_prefix, (size_t)(q - p)) == 0) {
+#endif
 				for (p += lp->ctl_len; isspace(*p); p++)
 					continue; /* skip prefix and next ws */
 
