cf49e542425e0f28be6f3dd375f4084864a8be0c
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2015-12-04 14:23:17 +0000

    VBoxDTraceR0.cpp: Forgot to commit this when adding the settings serial number yesterday.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@58986 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/ExtPacks/VBoxDTrace/VBoxDTraceR0.cpp
+++ src/VBox/ExtPacks/VBoxDTrace/VBoxDTraceR0.cpp
@@ -1512,6 +1512,7 @@ static int      vboxDtPOps_Enable(void *pvProv, dtrace_id_t idProbe, void *pvPro
                 pProbeLocEn->fEnabled = 1;
                 ASMAtomicIncU32(&pProv->pacProbeEnabled[idxProbe]);
                 ASMAtomicIncU32(&pProv->pDesc->cProbesEnabled);
+                ASMAtomicIncU32(&pProv->pDesc->uSettingsSerialNo);
             }
         }
         else
@@ -1522,6 +1523,7 @@ static int      vboxDtPOps_Enable(void *pvProv, dtrace_id_t idProbe, void *pvPro
                 pProv->paR0ProbeLocs[idxProbeLoc].fEnabled = 1;
                 ASMAtomicIncU32(&pProv->paR0Probes[idxProbe].cEnabled);
                 ASMAtomicIncU32(&pProv->pDesc->cProbesEnabled);
+                ASMAtomicIncU32(&pProv->pDesc->uSettingsSerialNo);
             }
 
             /* Update user mode structure. */
@@ -1559,6 +1561,7 @@ static void     vboxDtPOps_Disable(void *pvProv, dtrace_id_t idProbe, void *pvPr
                 pProbeLocEn->fEnabled = 0;
                 ASMAtomicDecU32(&pProv->pacProbeEnabled[idxProbe]);
                 ASMAtomicDecU32(&pProv->pDesc->cProbesEnabled);
+                ASMAtomicIncU32(&pProv->pDesc->uSettingsSerialNo);
             }
         }
         else
@@ -1569,6 +1572,7 @@ static void     vboxDtPOps_Disable(void *pvProv, dtrace_id_t idProbe, void *pvPr
                 pProv->paR0ProbeLocs[idxProbeLoc].fEnabled = 0;
                 ASMAtomicDecU32(&pProv->paR0Probes[idxProbe].cEnabled);
                 ASMAtomicDecU32(&pProv->pDesc->cProbesEnabled);
+                ASMAtomicIncU32(&pProv->pDesc->uSettingsSerialNo);
             }
 
             /* Update user mode structure. */
