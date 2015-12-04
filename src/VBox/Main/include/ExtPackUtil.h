1fa6791de24092548baa68a7ebf747141ab957ee
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2015-12-04 14:23:49 +0000

    VBoxDTrace: Extpack conversion fix.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@58988 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Main/include/ExtPackUtil.h
+++ src/VBox/Main/include/ExtPackUtil.h
@@ -18,7 +18,9 @@
 #ifndef ____H_EXTPACKUTIL
 #define ____H_EXTPACKUTIL
 
-#include <iprt/cpp/ministring.h>
+#ifdef __cplusplus
+# include <iprt/cpp/ministring.h>
+#endif
 #include <iprt/fs.h>
 #include <iprt/vfs.h>
 
@@ -58,6 +60,8 @@
  * Play short and safe. */
 #define VBOX_EXTPACK_MAX_MEMBER_NAME_LENGTH 128
 
+
+#ifdef __cplusplus
 
 /**
  * Plug-in descriptor.
@@ -131,7 +135,7 @@ int                 VBoxExtPackValidateTarball(RTFILE hTarballFile, const char *
                                                const char *pszTarball, const char *pszTarballDigest,
                                                char *pszError, size_t cbError,
                                                PRTMANIFEST phValidManifest, PRTVFSFILE phXmlFile, RTCString *pStrDigest);
-
+#endif /* __cplusplus */
 
 #endif
 
