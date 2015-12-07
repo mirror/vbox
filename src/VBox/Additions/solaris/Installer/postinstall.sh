99cab43ed3a8a71b3a1e1edf6ae64d6ad3ee4754
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2015-12-07 13:03:12 +0000

    bugref:8153: vboxvideo: ABI issues / software rendering on Linux guest: do set-up of our OpenGL library during Additions installation later, as we can only test whether pass-through is enabled after the kernel module has been loaded.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@59023 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Additions/solaris/Installer/postinstall.sh
+++ src/VBox/Additions/solaris/Installer/postinstall.sh
@@ -334,10 +334,6 @@ fi
 # be added to this group.
 groupadd vboxsf >/dev/null 2>&1
 
-# Set up our OpenGL pass-through library.
-ln -sf $vboxadditions_path/vbox_vendor_select /lib/opengl/ogl_select
-/lib/svc/method/ogl-select start
-
 # Move the pointer integration module to kernel/drv & remove the unused module name from pkg and file from disk
 
 # Finalize
@@ -395,6 +391,9 @@ if test "$currentzone" = "global"; then
     fi
 fi
 
+# Set up our OpenGL pass-through library.
+ln -sf $vboxadditions_path/vbox_vendor_select /lib/opengl/ogl_select
+test "$currentzone" = "global" && /lib/svc/method/ogl-select start
 
 echo "Done."
 if test $retval -eq 0; then
