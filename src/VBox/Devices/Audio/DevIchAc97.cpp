c71e428a4edb25fa4a47e034bfc5c61a2eed9ce1
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2015-12-11 09:53:54 +0000

    DevIchAc97.cpp: Removed debug logging.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@59085 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Devices/Audio/DevIchAc97.cpp
+++ src/VBox/Devices/Audio/DevIchAc97.cpp
@@ -1194,23 +1194,16 @@ static DECLCALLBACK(void) ichac97Timer(PPDMDEVINS pDevIns, PTMTIMER pTimer, void
 
     pThis->uTimerTS = uTicksNow;
 
-    LogFlowFunc(("pDevIns=%p\n", pDevIns));
-
     RTListForEach(&pThis->lstDrv, pDrv, AC97DRIVER, Node)
     {
         cbIn = cbOut = 0;
         rc = pDrv->pConnector->pfnQueryStatus(pDrv->pConnector,
                                               &cbIn, &cbOut, NULL /* cSamplesLive */);
-        LogFlowFunc(("rc1=%Rrc\n", rc));
         if (RT_SUCCESS(rc))
             rc = pDrv->pConnector->pfnPlayOut(pDrv->pConnector, NULL /* cSamplesPlayed */);
 
-        LogFlowFunc(("rc2=%Rrc\n", rc));
-
         uint32_t cSamplesMin  = (int)((2 * uTicksElapsed * pDrv->Out.pStrmOut->Props.uHz + uTicksPerSec) / uTicksPerSec / 2);
         uint32_t cbSamplesMin = AUDIOMIXBUF_S2B(&pDrv->Out.pStrmOut->MixBuf, cSamplesMin);
-
-        LogFlowFunc(("rc3=%Rrc\n", rc));
 
         LogFlowFunc(("LUN#%RU8: rc=%Rrc, cbOut=%RU32, cSamplesMin=%RU32, cbSamplesMin=%RU32\n",
                      pDrv->uLUN, rc, cbOut, cSamplesMin, cbSamplesMin));
