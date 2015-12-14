7a5e2b868bb7bc1e7f4118ba0c0f852f16e9ce63
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2015-12-14 09:05:09 +0000

    FE/Qt: when sending the Ctrl+Break down/up keyboard sequence, don't omit sending the extended scan code (0xe0) -- thanks yjh for pointing this out
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@59108 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Frontends/VirtualBox/src/runtime/UIMachineLogic.cpp
+++ src/VBox/Frontends/VirtualBox/src/runtime/UIMachineLogic.cpp
@@ -1448,11 +1448,13 @@ void UIMachineLogic::sltTypeCABS()
 
 void UIMachineLogic::sltTypeCtrlBreak()
 {
-    static QVector<LONG> sequence(4);
+    static QVector<LONG> sequence(6);
     sequence[0] = 0x1d;        /* Ctrl down */
-    sequence[1] = 0x46;        /* Break down */
-    sequence[2] = 0x46 | 0x80; /* Break up */
-    sequence[3] = 0x1d | 0x80; /* Ctrl up */
+    sequence[1] = 0xe0;        /* Extended flag */
+    sequence[2] = 0x46;        /* Break down */
+    sequence[3] = 0xe0;        /* Extended flag */
+    sequence[4] = 0x46 | 0x80; /* Break up */
+    sequence[5] = 0x1d | 0x80; /* Ctrl up */
     keyboard().PutScancodes(sequence);
     AssertWrapperOk(keyboard());
 }
