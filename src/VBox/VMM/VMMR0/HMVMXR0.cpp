f4af0d72bc6cfe92787323ced3c02805d50f142b
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2015-12-15 21:30:54 +0000

    doxygen fix
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@59141 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/VMM/VMMR0/HMVMXR0.cpp
+++ src/VBox/VMM/VMMR0/HMVMXR0.cpp
@@ -138,7 +138,7 @@
  *
  * We need to intercept all exceptions manually except:
  * - \#NM, \#MF handled in hmR0VmxLoadSharedCR0().
- * - \#AC and #DB are always intercepted to prevent the CPU from deadlocking
+ * - \#AC and \#DB are always intercepted to prevent the CPU from deadlocking
  *   due to bugs in Intel CPUs.
  * - \#PF need not be intercepted even in real-mode if we have Nested Paging
  * support.
