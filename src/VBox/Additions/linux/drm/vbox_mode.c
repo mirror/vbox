17f08808a54fc52f19593869e764b74123c639bb
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2015-12-26 15:56:00 +0000

    bugref:8087: Additions/x11: support non-root X server: call drm_kms_helper_hotplug_event() directly from the KMS driver rather than drm_helper_hpd_irq_event().  The second only accepts a hot-plug event as genuine if a screen is disconnected and then re-connected.  Our screens are swapped without any intermediate disconnection.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@59225 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Additions/linux/drm/vbox_mode.c
+++ src/VBox/Additions/linux/drm/vbox_mode.c
@@ -531,7 +531,7 @@ ssize_t vbox_connector_write_sysfs(struct device *pDev,
         pVBoxConnector->modeHint.cX = (uint16_t)cX;
         pVBoxConnector->modeHint.cY = (uint16_t)cY;
     }
-    drm_helper_hpd_irq_event(pVBoxConnector->base.dev);
+    drm_kms_helper_hotplug_event(pVBoxConnector->base.dev);
     if (pVBox->fbdev)
         drm_fb_helper_hotplug_event(&pVBox->fbdev->helper);
     LogFunc(("vboxvideo: %d\n", __LINE__));
@@ -581,10 +581,6 @@ int vbox_connector_init(struct drm_device *pDev, unsigned cScreen,
 #else
     drm_connector_register(pConnector);
 #endif
-
-    /* The connector supports hot-plug detection: we promise to call
-     * "drm_helper_hpd_irq_event" when hot-plugging occurs. */
-    pConnector->polled = DRM_CONNECTOR_POLL_HPD;
 
     drm_mode_connector_attach_encoder(pConnector, pEncoder);
 
