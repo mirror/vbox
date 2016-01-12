c894fc2d3533297b59f4a9b1c075fc2c55dc92b1
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2016-01-12 16:19:31 +0000

    ThreadTask: Review/@todo
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@59318 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Main/src-all/ThreadTask.cpp
+++ src/VBox/Main/src-all/ThreadTask.cpp
@@ -19,6 +19,9 @@
 #include "VirtualBoxBase.h"
 #include "ThreadTask.h"
 
+/** @todo r=bird: DOCUMENTATION!! 'ING DOCUMENTATION!! THIS 'ING FUNCTION
+ *        CONSUMES THE this OBJECTED AND YOU AREN'T EXACTLY 'ING TELLING
+ *        ANYBODY.  THAT IS REALLY NICE OF YOU. */
 HRESULT ThreadTask::createThread(PRTTHREAD pThread, RTTHREADTYPE enmType)
 {
     HRESULT rc = S_OK;
@@ -39,16 +42,21 @@ HRESULT ThreadTask::createThread(PRTTHREAD pThread, RTTHREADTYPE enmType)
     }
     catch(HRESULT eRC)
     {
+        /** @todo r=bird: only happens in your above throw call.
+         *  Makes you wonder why you don't just do if (RT_SUCCESS(vrc)) return S_OK;
+         *  else {delete pTask; return E_FAIL;}   */
         rc = eRC;
         delete this;
     }
     catch(std::exception& )
     {
+        /** @todo r=bird: can't happen, RTThreadCreate+Utf8Str doesn't do this. */
         rc = E_FAIL;
         delete this;
     }
     catch(...)
     {
+        /** @todo r=bird: can't happen, RTThreadCreate+Utf8Str doesn't do this. */
         rc = E_FAIL;
         delete this;
     }
@@ -71,6 +79,14 @@ HRESULT ThreadTask::createThread(PRTTHREAD pThread, RTTHREADTYPE enmType)
     {
         pTask->handler();
     }
+    /** @todo r=bird: This next stuff here is utterly and totally pointless, as
+     *        the handler() method _must_ and _shall_ do this, otherwise there is
+     *        no 'ing way we can know about it, since you (a) you always return
+     *        VINF_SUCCESS (aka '0'), and (b) the thread isn't waitable even if
+     *        you wanted to return anything.  It is much preferred to _crash_ and
+     *        _burn_ if we fail to catch stuff than quietly ignore it, most
+     *        likely the state of the objects involved is butchered by this.  At
+     *        least AssertLogRel()! Gee. */
     catch(HRESULT eRC)
     {
         rc = eRC;
