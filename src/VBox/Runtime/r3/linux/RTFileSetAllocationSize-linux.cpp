0d773bcb073d76cda0e569c19d8bbb66445848b5
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2016-01-25 14:56:07 +0000

    IPRT: Linux specific implementation for RTFileSetAllocationSize()
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@59461 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Runtime/r3/linux/RTFileSetAllocationSize-linux.cpp
+++ src/VBox/Runtime/r3/linux/RTFileSetAllocationSize-linux.cpp
@@ -1,6 +1,6 @@
 /* $Id$ */
 /** @file
- * IPRT - RTFileSetAllocationSize, generic implementation.
+ * IPRT - RTFileSetAllocationSize, linux implementation.
  */
 
 /*
@@ -28,24 +28,57 @@
 /*********************************************************************************************************************************
 *   Header Files                                                                                                                 *
 *********************************************************************************************************************************/
-#include <iprt/assert.h>
-#include <iprt/err.h>
+#define LOG_GROUP RTLOGGROUP_FILE
 #include <iprt/file.h>
-
 #include "internal/iprt.h"
 
+#include <iprt/assert.h>
+#include <iprt/err.h>
+
+#include <dlfcn.h>
+#include <errno.h>
+#include <unistd.h>
+#include <sys/fcntl.h>
+
+/**
+ * The Linux specific fallocate() method.
+ */
+typedef int (*PFNLNXFALLOCATE) (int iFd, int fMode, off_t offStart, off_t cb);
+/** Flag to specify that the file size should not be extended. */
+#define LNX_FALLOC_FL_KEEP_SIZE 1
 
 RTDECL(int) RTFileSetAllocationSize(RTFILE hFile, uint64_t cbSize, uint32_t fFlags)
 {
-    /*
-     * Quick validation.
-     */
     AssertReturn(hFile != NIL_RTFILE, VERR_INVALID_PARAMETER);
     AssertReturn(!(fFlags & ~RTFILE_ALLOC_SIZE_F_VALID), VERR_INVALID_PARAMETER);
 
-    NOREF(cbSize);
+    /*
+     * Validate offset.
+     */
+    if (    sizeof(off_t) < sizeof(cbSize)
+        &&  (cbSize >> 32) != 0)
+    {
+        AssertMsgFailed(("64-bit filesize not supported! cbSize=%lld\n", cbSize));
+        return VERR_NOT_SUPPORTED;
+    }
 
-    return VERR_NOT_SUPPORTED;
+    int rc = VINF_SUCCESS;
+    PFNLNXFALLOCATE pfnLnxFAllocate = (PFNLNXFALLOCATE)(uintptr_t)dlsym(RTLD_DEFAULT, "fallocate");
+    if (VALID_PTR(pfnLnxFAllocate))
+    {
+        int fLnxFlags = (fFlags & RTFILE_ALLOC_SIZE_F_KEEP_SIZE) ? LNX_FALLOC_FL_KEEP_SIZE : 0;
+        int rcLnx = pfnLnxFAllocate(RTFileToNative(hFile), fLnxFlags, 0, cbSize);
+        if (rcLnx != 0)
+        {
+            if (errno == EOPNOTSUPP)
+                rc = VERR_NOT_SUPPORTED;
+            else
+                rc = RTErrConvertFromErrno(errno);
+        }
+    }
+    else
+        rc = VERR_NOT_SUPPORTED;
+
+    return rc;
 }
 RT_EXPORT_SYMBOL(RTFileSetAllocationSize);
-
