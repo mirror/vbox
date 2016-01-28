0ddcd43480f7df26019cd59d4fff8e0ba2613511
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2016-01-28 14:41:26 +0000

    IPRT: Smaller cleanups to the Linux and Posix specific RTFileSetAllocationSize() implementations
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@59512 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Runtime/r3/posix/RTFileSetAllocationSize-posix.cpp
+++ src/VBox/Runtime/r3/posix/RTFileSetAllocationSize-posix.cpp
@@ -49,16 +49,9 @@ RTDECL(int) RTFileSetAllocationSize(RTFILE hFile, uint64_t cbSize, uint32_t fFla
 {
     AssertReturn(hFile != NIL_RTFILE, VERR_INVALID_PARAMETER);
     AssertReturn(!(fFlags & ~RTFILE_ALLOC_SIZE_F_VALID), VERR_INVALID_PARAMETER);
-
-    /*
-     * Validate offset.
-     */
-    if (    sizeof(off_t) < sizeof(cbSize)
-        &&  RT_HIDWORD(cbSize) != 0)
-    {
-        AssertMsgFailed(("64-bit filesize not supported! cbSize=%lld\n", cbSize));
-        return VERR_NOT_SUPPORTED;
-    }
+    AssertMsgReturn(sizeof(off_t) >= sizeof(cbSize) &&  RT_HIDWORD(cbSize) == 0,
+                    ("64-bit filesize not supported! cbSize=%lld\n", cbSize),
+                    VERR_NOT_SUPPORTED);
 
     if (fFlags & RTFILE_ALLOC_SIZE_F_KEEP_SIZE)
         return VERR_NOT_SUPPORTED;
