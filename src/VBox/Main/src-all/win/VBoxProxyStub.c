fa86b845a5e90bd13b49458fe5fd0356837db1eb
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2016-02-03 00:01:45 +0000

    VBoxProxyStub.c: duh^2
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@59566 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Main/src-all/win/VBoxProxyStub.c
+++ src/VBox/Main/src-all/win/VBoxProxyStub.c
@@ -22,6 +22,7 @@
 /*********************************************************************************************************************************
 *   Header Files                                                                                                                 *
 *********************************************************************************************************************************/
+#define LOG_GROUP LOG_GROUP_MAIN
 #define PROXY_DELEGATION                                                                             /* see generated dlldata.c */
 #include <iprt/nt/nt-and-windows.h>
 #include <rpcproxy.h>
@@ -29,6 +30,7 @@
 #include <stdio.h>
 
 #include "VirtualBox.h"
+#include <VBox/log.h>
 #include <iprt/alloca.h>
 #include <iprt/assert.h>
 #include <iprt/ctype.h>
@@ -90,7 +92,7 @@
 #ifdef RT_STRICT
 # define VBPS_LOGREL_NO_ASSERT(a_Expr) (a_Expr)
 #else
-# define VBPS_LOGREL_NO_ASSERT false
+# define VBPS_LOGREL_NO_ASSERT(a_Expr) false
 #endif
 
 
