ac677e1088974148240b2368c2bd1a1666f8ff2d
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2016-02-04 16:40:36 +0000

    Additions/installer/autorun.sh: do also test for mate-terminal (ticketref:15112)
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@59583 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Additions/linux/installer/autorun.sh
+++ src/VBox/Additions/linux/installer/autorun.sh
@@ -31,8 +31,8 @@ mywhich() {
 # May clobber $gtx*
 # Calls mywhich
 getxterm() {
-    # gnome-terminal uses -e differently to other emulators
-    for gxti in "konsole --title -e" "gnome-terminal --title -x" "xterm -T -e"; do
+    # gnome-terminal and mate-terminal use -e differently to other emulators
+    for gxti in "konsole --title -e" "gnome-terminal --title -x" "mate-terminal --title -x" "xterm -T -e"; do
         set $gxti
         gxtpath="`mywhich $1`"
         case "$gxtpath" in ?*)
