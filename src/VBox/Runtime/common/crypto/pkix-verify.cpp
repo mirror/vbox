af02108babac2859a51640ae478dd4861634491d
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2016-02-15 22:36:49 +0000

    IPRT/pkix-verify.cpp: rhel5 (openssl < 1.0.0) build fix.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@59695 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Runtime/common/crypto/pkix-verify.cpp
+++ src/VBox/Runtime/common/crypto/pkix-verify.cpp
@@ -38,6 +38,9 @@
 #ifdef IPRT_WITH_OPENSSL
 # include "internal/iprt-openssl.h"
 # include "openssl/evp.h"
+# ifndef OPENSSL_VERSION_NUMBER
+#  error "Missing OPENSSL_VERSION_NUMBER!"
+# endif
 #endif
 
 
@@ -226,7 +229,8 @@ RTDECL(int) RTCrPkixPubKeyVerifySignedDigest(PCRTASN1OBJID pAlgorithm, PCRTASN1D
 
     RTCrPkixSignatureRelease(hSignature);
 
-#ifdef IPRT_WITH_OPENSSL
+#if defined(IPRT_WITH_OPENSSL) \
+  && (OPENSSL_VERSION_NUMBER > 0x10000000L) /* 0.9.8 doesn't seem to have EVP_PKEY_CTX_set_signature_md. */
     /*
      * Validate using OpenSSL EVP.
      */
