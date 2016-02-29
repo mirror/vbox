c4d83430f5db1865183a23f852331eca7a65d7cc
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2016-02-29 12:05:44 +0000

    FE/Qt: ​bugref:8115: Mac OS X: VM settings: System page: Hiding hardware virtualization settings when raw-mode is not supported.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@59870 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Frontends/VirtualBox/src/settings/machine/UIMachineSettingsSystem.cpp
+++ src/VBox/Frontends/VirtualBox/src/settings/machine/UIMachineSettingsSystem.cpp
@@ -727,6 +727,13 @@ void UIMachineSettingsSystem::prepareTabAcceleration()
     m_pComboParavirtProvider->addItem(gpConverter->toString(KParavirtProvider_Minimal), QVariant(KParavirtProvider_Minimal));
     m_pComboParavirtProvider->addItem(gpConverter->toString(KParavirtProvider_HyperV), QVariant(KParavirtProvider_HyperV));
     m_pComboParavirtProvider->addItem(gpConverter->toString(KParavirtProvider_KVM), QVariant(KParavirtProvider_KVM));
+
+    /* Hide hardware virtualization settings when raw-mode is not supported: */
+#ifndef VBOX_WITH_RAW_MODE
+    m_pLabelVirtualization->setVisible(false);
+    m_pCheckBoxVirtualization->setVisible(false);
+    m_pCheckBoxNestedPaging->setVisible(false);
+#endif /* !VBOX_WITH_RAW_MODE */
 }
 
 void UIMachineSettingsSystem::prepareValidation()
