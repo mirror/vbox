b228ca5daa759b312766b15512e7f633058d6407
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2016-03-14 17:15:51 +0000

    recompiler: shifting of negative values is undefined (thanks PVS)
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@60022 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/recompiler/target-i386/translate.c
+++ src/recompiler/target-i386/translate.c
@@ -2705,7 +2705,7 @@ static void gen_push_T1(DisasContext *s)
         if (s->ss32 && !s->addseg)
             gen_op_mov_reg_A0(1, R_ESP);
         else
-            gen_stack_update(s, (-2) << s->dflag);
+            gen_stack_update(s, -(2 << s->dflag));
     }
 }
 
@@ -2758,7 +2758,7 @@ static void gen_pusha(DisasContext *s)
 {
     int i;
     gen_op_movl_A0_reg(R_ESP);
-    gen_op_addl_A0_im(-16 <<  s->dflag);
+    gen_op_addl_A0_im(-(16 <<  s->dflag));
     if (!s->ss32)
         gen_op_andl_A0_ffff();
     tcg_gen_mov_tl(cpu_T[1], cpu_A0);
