ac5eb5e428e6b79655e372e18e1a275d2d67ad8d
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2016-03-21 12:20:33 +0000

    IEM: another iret todo.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@60118 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/VMM/VMMAll/IEMAllCImpl.cpp.h
+++ src/VBox/VMM/VMMAll/IEMAllCImpl.cpp.h
@@ -3109,6 +3109,9 @@ IEM_CIMPL_DEF_1(iemCImpl_iret_prot, IEMMODE, enmEffOpSize)
             rcStrict = iemMemStackPopContinueSpecial(pIemCpu, 8, &uFrame.pv, &uNewRsp);
             if (rcStrict != VINF_SUCCESS)
                 return rcStrict;
+/** @todo We might be popping a 32-bit ESP from the IRET frame, but whether
+ *        16-bit or 32-bit are being loaded into SP depends on the D/B
+ *        bit of the popped SS selector it turns out. */
             uNewESP = uFrame.pu32[0];
             uNewSS  = (uint16_t)uFrame.pu32[1];
         }
