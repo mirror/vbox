a0dc6de55dd33d116477ca14b9e72768b3ed04d9
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2016-03-21 17:45:35 +0000

    Installer/linux: refined library check
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@60128 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Installer/linux/install.sh
+++ src/VBox/Installer/linux/install.sh
@@ -328,7 +328,7 @@ if [ "$ACTION" = "install" ]; then
 
     # Create legacy symlinks if necesary for Qt5/xcb stuff.
     if [ -d $INSTALLATION_DIR/legacy ]; then
-        if ! /sbin/ldconfig -p | grep -q "\<libxcb\.so\>"; then
+        if ! /sbin/ldconfig -p | grep -q "\<libxcb\.so\.1\>"; then
             for f in `ls -1 $INSTALLATION_DIR/legacy/`; do
                 ln -s $INSTALLATION_DIR/legacy/$f $INSTALLATION_DIR/$f
                 echo $INSTALLATION_DIR/$f >> $CONFIG_DIR/$CONFIG_FILES
