9f966867119aa810e449db6bded9c78be290a140
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2016-03-29 15:57:49 +0000

    Devices/PC/DevPit-i8254.cpp: fix PC speaker passthrough
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@60248 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Devices/PC/DevPit-i8254.cpp
+++ src/VBox/Devices/PC/DevPit-i8254.cpp
@@ -296,7 +296,7 @@ static int pitTryDeviceOpenSanitizeIoctl(const char *pszPath, int flags)
     {
         int errno_eviocgsnd0 = 0;
         int errno_kiocsound = 0;
-        if (ioctl(fd, EVIOCGSND(0)) != -1)
+        if (ioctl(fd, EVIOCGSND(0)) == -1)
         {
             errno_eviocgsnd0 = errno;
             if (ioctl(fd, KIOCSOUND, 1) == -1)
@@ -1404,8 +1404,11 @@ static DECLCALLBACK(int)  pitConstruct(PPDMDEVINS pDevIns, int iInstance, PCFGMN
 #else
         LogRel(("PIT: speaker: emulation deactivated\n"));
 #endif
-        RTStrFree(pszPassthroughSpeakerDevice);
-        pszPassthroughSpeakerDevice = NULL;
+        if (pszPassthroughSpeakerDevice)
+        {
+            MMR3HeapFree(pszPassthroughSpeakerDevice);
+            pszPassthroughSpeakerDevice = NULL;
+        }
     }
     pThis->channels[0].irq = u8Irq;
     for (i = 0; i < RT_ELEMENTS(pThis->channels); i++)
