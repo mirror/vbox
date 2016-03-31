81d0ec0fe83f034bc71759bc8b99505b1e6de90b
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2016-03-31 10:05:48 +0000

    Additions/VBoxGuest: RT_SUCCESS / NT_SUCCESS mixup (thanks Parfait)
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@60272 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Additions/common/VBoxGuest/VBoxGuest-win.cpp
+++ src/VBox/Additions/common/VBoxGuest/VBoxGuest-win.cpp
@@ -528,7 +528,7 @@ NTSTATUS vgdrvNtInit(PDRIVER_OBJECT pDrvObj, PDEVICE_OBJECT pDevObj, PUNICODE_ST
     }
 #endif
 
-    if (RT_SUCCESS(rcNt))
+    if (NT_SUCCESS(rcNt))
     {
         ULONG uValue = 0;
         NTSTATUS rcNt2 = vgdrvNtRegistryReadDWORD(RTL_REGISTRY_SERVICES, L"VBoxGuest", L"LoggingEnabled", &uValue);
