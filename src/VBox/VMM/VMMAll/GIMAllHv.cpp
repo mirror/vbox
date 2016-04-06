82b4ea2c9860d36d7e264df9cb147dea635c7a1c
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2016-04-06 11:06:52 +0000

    VMM/GIM/HyperV: EOI MSR is read-only.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@60351 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/VMM/VMMAll/GIMAllHv.cpp
+++ src/VBox/VMM/VMMAll/GIMAllHv.cpp
@@ -435,9 +435,6 @@ VMM_INT_DECL(VBOXSTRICTRC) gimHvReadMsr(PVMCPU pVCpu, uint32_t idMsr, PCCPUMMSRR
         case MSR_GIM_HV_TPR:
             return PDMApicReadMsr(pVCpu, MSR_IA32_X2APIC_TPR, puValue);
 
-        case MSR_GIM_HV_EOI:
-            return PDMApicReadMsr(pVCpu, MSR_IA32_X2APIC_EOI, puValue);
-
         case MSR_GIM_HV_ICR:
             return PDMApicReadMsr(pVCpu, MSR_IA32_X2APIC_ICR, puValue);
 
@@ -513,6 +510,9 @@ VMM_INT_DECL(VBOXSTRICTRC) gimHvReadMsr(PVMCPU pVCpu, uint32_t idMsr, PCCPUMMSRR
             return VERR_CPUM_RAISE_GP_0;
         }
 
+        /* Read-only MSRs: */
+        case MSR_GIM_HV_EOI:
+        /* Reserved/unknown MSRs: */
         default:
         {
 #ifdef IN_RING3
