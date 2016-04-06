ce987d6aa8cd1c8fb96e863ad9ebc213cef082b4
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2016-04-06 13:01:12 +0000

    bugref:8087: Additions/x11: support non-root X server: provide EDID blocks with size-unique serial numbers to the kernel graphics driver in the hope that Unity will stop trying to force particular resolutions.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@60357 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Additions/linux/drm/vbox_mode.c
+++ src/VBox/Additions/linux/drm/vbox_mode.c
@@ -428,6 +428,66 @@ static struct drm_encoder *vbox_encoder_init(struct drm_device *dev, unsigned i)
     return &vbox_encoder->base;
 }
 
+/** Generate EDID data with a mode-unique serial number for the virtual
+ *  monitor to try to persuade Unity that different modes correspond to
+ *  different monitors and it should not try to force the same resolution on
+ *  them. */
+static void vbox_set_edid(struct drm_connector *connector, int width,
+                          int height)
+{
+    enum { EDID_SIZE = 128 };
+    unsigned char edid[EDID_SIZE] = {
+        0x00, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0x00, /* header */
+        0x58, 0x58, /* manufacturer (VBX) */
+        0x00, 0x00, /* product code */
+        0x00, 0x00,0x00, 0x00, /* serial number goes here */
+        0x01, /* week of manufacture */
+        0x00, /* year of manufacture */
+        0x01, 0x03, /* EDID version */
+        0x80, /* capabilities - digital */
+        0x00, /* horiz. res in cm, zero for projectors */
+        0x00, /* vert. res in cm */
+        0x78, /* display gamma (120 == 2.2). */
+        0xEE, /* features (standby, suspend, off, RGB, standard colour space,
+               * preferred timing mode) */
+        0xEE, 0x91, 0xA3, 0x54, 0x4C, 0x99, 0x26, 0x0F, 0x50, 0x54,
+            /* chromaticity for standard colour space. */
+        0x00, 0x00, 0x00, /* no default timings */
+        0x01, 0x01, 0x01, 0x01, 0x01, 0x01, 0x01, 0x01, 0x01, 0x01, 0x01, 0x01,
+        0x01, 0x01, 0x01, 0x01, /* no standard timings */
+        0x00, 0x00, 0x00, 0x06, 0x00, 0x00, 0x06, 0x00, 0x02, 0x02, 0x02, 0x02,
+        0x00, 0x00, 0x00, 0x00, 0x00, 0x00, /* descriptor block 1 goes here */
+        0x00, 0x00, 0x00, 0xFD, 0x00, /* descriptor block 2, monitor ranges */
+        0x00, 0xC8, 0x00, 0xC8, 0x64, 0x00, 0x0A, 0x20, 0x20, 0x20, 0x20, 0x20,
+        0x20, /* 0-200Hz vertical, 0-200KHz horizontal, 1000MHz pixel clock */
+        0x00, 0x00, 0x00, 0xFC, 0x00, /* descriptor block 3, monitor name */
+        'V', 'B', 'O', 'X', ' ', 'm', 'o', 'n', 'i', 't', 'o', 'r', '\n',
+        0x00, 0x00, 0x00, 0x10, 0x00, /* descriptor block 4: dummy data */
+        0x0A, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20,
+        0x20,
+        0x00, /* number of extensions */
+        0x00 /* checksum goes here */
+    };
+    int clock = (width + 6) * (height + 6) * 60 / 10000;
+    unsigned i;
+    unsigned sum = 0;
+
+    edid[12] = width & 0xff;
+    edid[13] = width >> 8;
+    edid[14] = height & 0xff;
+    edid[15] = height >> 8;
+    edid[54] = clock & 0xff;
+    edid[55] = clock >> 8;
+    edid[56] = width & 0xff;
+    edid[58] = (width >> 4) & 0xf0;
+    edid[59] = height & 0xff;
+    edid[61] = (height >> 4) & 0xf0;
+    for (i = 0; i < EDID_SIZE - 1; ++i)
+        sum += edid[i];
+    edid[EDID_SIZE - 1] = (0x100 - (sum & 0xFF)) & 0xFF;
+    drm_mode_connector_update_edid_property(connector, (struct edid *)edid);
+}
+
 static int vbox_get_modes(struct drm_connector *connector)
 {
     struct vbox_connector *vbox_connector = NULL;
@@ -459,6 +519,7 @@ static int vbox_get_modes(struct drm_connector *connector)
         drm_mode_probed_add(connector, mode);
         ++num_modes;
     }
+    vbox_set_edid(connector, preferred_width, preferred_height);
     return num_modes;
 }
 
