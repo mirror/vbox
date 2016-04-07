776b7c6f70d93430f90330263414a9e77581a75c
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2016-04-07 14:28:14 +0000

    forgotten file
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@60374 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Additions/common/VBoxService/VBoxServiceCpuHotPlug.cpp
+++ src/VBox/Additions/common/VBoxService/VBoxServiceCpuHotPlug.cpp
@@ -317,13 +317,16 @@ static int vgsvcCpuHotPlugGetACPIDevicePath(char **ppszPath, uint32_t idCpuCore,
 
                         for (unsigned i = 0; i < RT_ELEMENTS(g_apszTopologyPath); i++)
                         {
-                            int64_t i64Core    = RTLinuxSysFsReadIntFile(10, "%s/%s/topology/core_id",
-                                                                         pszPathCurr, g_apszTopologyPath[i]);
-                            int64_t i64Package = RTLinuxSysFsReadIntFile(10, "%s/%s/topology/physical_package_id",
-                                                                         pszPathCurr, g_apszTopologyPath[i]);
+                            int64_t i64Core    = 0;
+                            int64_t i64Package = 0;
 
-                            if (   i64Core != -1
-                                && i64Package != -1)
+                            int rc2 = RTLinuxSysFsReadIntFile(10, &i64Core, "%s/%s/topology/core_id",
+                                                              pszPathCurr, g_apszTopologyPath[i]);
+                            if (RT_SUCCESS(rc2))
+                                rc2 = RTLinuxSysFsReadIntFile(10, &i64Package, "%s/%s/topology/physical_package_id",
+                                                              pszPathCurr, g_apszTopologyPath[i]);
+
+                            if (RT_SUCCESS(rc2))
                             {
                                 idCore = (uint32_t)i64Core;
                                 idPackage = (uint32_t)i64Package;
