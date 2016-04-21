a36b28042849581790a79a3d71de0bc20c96a94e
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2016-04-21 13:49:01 +0000

    Main/Medium: don't skip this important assertion doing sanity checking
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@60628 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Main/src-server/MediumImpl.cpp
+++ src/VBox/Main/src-server/MediumImpl.cpp
@@ -7854,13 +7854,11 @@ HRESULT Medium::i_taskMergeHandler(Medium::MergeTask &task)
                  * or LockedRead (parent medium) state if it is not the target.
                  * If it is the target it must be in the LockedWrite state.
                  */
-#ifndef DEBUG_andy /* See see @bugref{8172}. */
                 Assert(   (   pMedium != pTarget
                            && (   pMedium->m->state == MediumState_Deleting
                                || pMedium->m->state == MediumState_LockedRead))
                        || (   pMedium == pTarget
                            && pMedium->m->state == MediumState_LockedWrite));
-#endif
                 /*
                  * Medium must be the target, in the LockedRead state
                  * or Deleting state where it is not allowed to be attached
