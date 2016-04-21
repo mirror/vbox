48a291b934276c100d543f38ceba4a1a394e6165
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2016-04-21 16:06:58 +0000

    ValidationKit/usb: Format the port as a hexadecimal number because the filter requires it that way
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@60634 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/ValidationKit/tests/usb/tdUsb1.py
+++ src/VBox/ValidationKit/tests/usb/tdUsb1.py
@@ -319,7 +319,7 @@ class tdUsbBenchmark(vbox.TestDriver):                                      # py
             if fRc is True:
                 iBusId, _ = oUsbGadget.getGadgetBusAndDevId();
                 fRc = oSession.addUsbDeviceFilter('Compliance device', sVendorId = '0525', sProductId = 'a4a0', \
-                                                  sPort = str(iBusId));
+                                                  sPort = format(iBusId, 'x'));
                 if fRc is True:
 
                     # Wait a moment to let the USB device appear
