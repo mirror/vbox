53f92062fab91865888e0dd2623478917a5cd30c
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2016-04-27 08:02:31 +0000

    Main/USBProxyBackendUsbIp.cpp: Fixes for Windows, RTPoll returns with both the read and error events set so we have to check for available data first before handling the error
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@60713 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Main/src-server/generic/USBProxyBackendUsbIp.cpp
+++ src/VBox/Main/src-server/generic/USBProxyBackendUsbIp.cpp
@@ -411,7 +411,6 @@ int USBProxyBackendUsbIp::captureDevice(HostUSBDevice *aDevice)
      */
     Assert(aDevice->i_getUnistate() == kHostUSBDeviceState_Capturing);
     devLock.release();
-    interruptWait();
 
     return VINF_SUCCESS;
 }
@@ -430,7 +429,6 @@ int USBProxyBackendUsbIp::releaseDevice(HostUSBDevice *aDevice)
      */
     Assert(aDevice->i_getUnistate() == kHostUSBDeviceState_ReleasingToHost);
     devLock.release();
-    interruptWait();
 
     return VINF_SUCCESS;
 }
@@ -503,10 +501,11 @@ int USBProxyBackendUsbIp::wait(RTMSINTERVAL aMillies)
             }
             else if (uIdReady == USBIP_POLL_ID_SOCKET)
             {
-                if (fEventsRecv & RTPOLL_EVT_ERROR)
-                    rc = VERR_NET_SHUTDOWN;
-                else
+                if (fEventsRecv & RTPOLL_EVT_READ)
                     rc = receiveData();
+                else if (fEventsRecv & RTPOLL_EVT_ERROR)
+                    rc = VERR_NET_SHUTDOWN;
+
                 if (RT_SUCCESS(rc))
                 {
                     /*
@@ -787,16 +786,26 @@ void USBProxyBackendUsbIp::advanceState(USBIPRECVSTATE enmRecvState)
  */
 int USBProxyBackendUsbIp::receiveData()
 {
+    int rc = VINF_SUCCESS;
     size_t cbRecvd = 0;
-    int rc = RTTcpReadNB(m->hSocket, m->pbRecvBuf, m->cbResidualRecv, &cbRecvd);
-    if (RT_SUCCESS(rc))
+
+    do
     {
-        m->cbResidualRecv -= cbRecvd;
-        m->pbRecvBuf      += cbRecvd;
-        /* In case we received everything for the current state process the data. */
-        if (!m->cbResidualRecv)
-            rc = processData();
-    }
+        rc = RTTcpReadNB(m->hSocket, m->pbRecvBuf, m->cbResidualRecv, &cbRecvd);
+        if (RT_SUCCESS(rc))
+        {
+            m->cbResidualRecv -= cbRecvd;
+            m->pbRecvBuf      += cbRecvd;
+            /* In case we received everything for the current state process the data. */
+            if (!m->cbResidualRecv)
+            {
+                rc = processData();
+                if (   RT_SUCCESS(rc)
+                    && m->enmRecvState == kUsbIpRecvState_None)
+                    break;
+            }
+        }
+    } while (RT_SUCCESS(rc) && cbRecvd > 0);
 
     return rc;
 }
