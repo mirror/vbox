c775b1e5d3acb14c398347949cefe63d59f93a35
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2016-05-09 13:15:43 +0000

    TRPMRCHandlerA.asm: Missed the other v8086 iret.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@60892 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/VMM/VMMRC/TRPMRCHandlersA.asm
+++ src/VBox/VMM/VMMRC/TRPMRCHandlersA.asm
@@ -487,6 +487,10 @@ ALIGNCODE(16)
     add     esp, 4
 %endif
 
+    ; For v8086 mode we must branch off before we enable write protection.
+    test    dword [ebx + CPUMCTXCORE.eflags], X86_EFL_VM
+    jnz     .gc_V86_return
+
     ; enable WP
     mov     eax, cr0                    ;; @todo try elimiate this read.
     or      eax, X86_CR0_WRITE_PROTECT
@@ -514,9 +518,6 @@ ALIGNCODE(16)
     mov     eax, [ebx + CPUMCTXCORE.eip]
     mov     [%$STK_EIP], eax
 
-    test    dword [ebx + CPUMCTXCORE.eflags], X86_EFL_VM
-    jnz     .gc_V86_return
-
     mov     ax, [ebx + CPUMCTXCORE.gs.Sel]
     TRPM_NP_GP_HANDLER NAME(trpmRCTrapInGeneric), TRPM_TRAP_IN_MOV_GS
     mov     gs, ax
@@ -543,22 +544,16 @@ ALIGNCODE(16)
 
 ALIGNCODE(16)
 .gc_V86_return:
-    mov     eax, dword [ebx + CPUMCTXCORE.es.Sel]
-    mov     [%$STK_V86_ES], eax
-    mov     eax, dword [ebx + CPUMCTXCORE.ds.Sel]
-    mov     [%$STK_V86_DS], eax
-    mov     eax, dword [ebx + CPUMCTXCORE.fs.Sel]
-    mov     [%$STK_V86_FS], eax
-    mov     eax, dword [ebx + CPUMCTXCORE.gs.Sel]
-    mov     [%$STK_V86_GS], eax
+    ;
+    ; We may be returning to V8086 while having entered from protected mode!
+    ; So, we have to push the whole stack frame.  There's code in CPUMRC that
+    ; does exactly that, so call it instead of duplicating it.
+    ;
+    push    ebx
+    extern  NAME(CPUMGCCallV86Code)
+    call    NAME(CPUMGCCallV86Code)
+    int3                                ; doesn't return...
 
-    ; finally restore our scratch register eax and ebx.
-    pop     ebx
-    pop     eax
-    add     esp, 16 + 8                 ; skip segregs, error code, and vector number.
-
-    TRPM_NP_GP_HANDLER NAME(trpmRCTrapInGeneric), TRPM_TRAP_IN_IRET | TRPM_TRAP_IN_V86
-    iret
 
     ;
     ; Trap in Hypervisor, try to handle it.
@@ -950,6 +945,7 @@ ti_GenericInterrupt:
     add     esp, 4
 %endif
 
+    ; For v8086 mode we must branch off before we enable write protection.
     test    dword [ebx + CPUMCTXCORE.eflags], X86_EFL_VM
     jnz     .gc_V86_return
 
