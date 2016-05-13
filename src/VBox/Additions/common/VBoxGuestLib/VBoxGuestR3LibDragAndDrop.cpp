b2b8bfc9e85de65f235907e712ad5b6520cb171a
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2016-05-13 16:59:51 +0000

    DnD/VbglR3: Report back proper error in VbglR3DnDConnect().
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@60979 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Additions/common/VBoxGuestLib/VBoxGuestR3LibDragAndDrop.cpp
+++ src/VBox/Additions/common/VBoxGuestLib/VBoxGuestR3LibDragAndDrop.cpp
@@ -1348,9 +1348,6 @@ VBGLR3DECL(int) VbglR3DnDConnect(PVBGLR3GUESTDNDCMDCTX pCtx)
     if (RT_SUCCESS(rc))
     {
         rc = Info.result;
-        if (rc == VERR_HGCM_SERVICE_NOT_FOUND)
-            return VINF_PERMISSION_DENIED; /* HGCM service not available - bail out early. */
-
         if (RT_SUCCESS(rc))
         {
             /* Set the default protocol version to use. */
@@ -1360,6 +1357,9 @@ VBGLR3DECL(int) VbglR3DnDConnect(PVBGLR3GUESTDNDCMDCTX pCtx)
             pCtx->uClientID = Info.u32ClientID;
         }
     }
+
+    if (RT_FAILURE(rc))
+        return rc;
 
     /*
      * Get the VM's session ID.
