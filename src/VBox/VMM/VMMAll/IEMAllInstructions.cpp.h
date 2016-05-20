888962abc8008281a27afba32966d2bca61f912e
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2016-05-20 15:04:29 +0000

    IEM: Enabled iemOp_movntq_Mq_Pq__movntdq_Mdq_Vdq on non-REM systems.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@61107 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/VMM/VMMAll/IEMAllInstructions.cpp.h
+++ src/VBox/VMM/VMMAll/IEMAllInstructions.cpp.h
@@ -6869,7 +6869,7 @@ FNIEMOP_STUB(iemOp_cvttpd2dq_Vdq_Wdp__cvtdq2pd_Vdq_Wpd__cvtpd2dq_Vdq_Wpd);
 
 
 /** Opcode 0x0f 0xe7. */
-#if 0 /** @todo enable tomorrow */
+#ifndef VBOX_WITH_REM /** @todo enable tomorrow */
 FNIEMOP_DEF(iemOp_movntq_Mq_Pq__movntdq_Mdq_Vdq)
 {
     IEMOP_MNEMONIC(!(pIemCpu->fPrefixes & IEM_OP_PRF_SIZE_OP) ? "movntq mr,r" : "movntdq mr,r");
