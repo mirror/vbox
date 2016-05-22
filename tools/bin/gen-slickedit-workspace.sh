f94839e1f484879886aab3043b1b45d7e75ff3a1
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2016-05-22 23:24:54 +0000

    gen-slickedit-workspace.sh: openssl version changed
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@61114 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- tools/bin/gen-slickedit-workspace.sh
+++ tools/bin/gen-slickedit-workspace.sh
@@ -1090,7 +1090,7 @@ my_generate_project "bldprogs"      "src/bldprogs"                          --be
 my_generate_project "zlib"          "src/libs/zlib-1.2.8"                   --begin-incs "include"                                          --end-includes "src/libs/zlib-1.2.8/*.c" "src/libs/zlib-1.2.8/*.h"
 my_generate_project "liblzf"        "src/libs/liblzf-3.4"                   --begin-incs "include"                                          --end-includes "src/libs/liblzf-3.4"
 my_generate_project "libpng"        "src/libs/libpng-1.2.54"                --begin-incs "include"                                          --end-includes "src/libs/libpng-1.2.54/*.c" "src/libs/libpng-1.2.54/*.h"
-my_generate_project "openssl"       "src/libs/openssl-1.0.1s"               --begin-incs "include" "src/libs/openssl-1.0.1s/crypto"         --end-includes "src/libs/openssl-1.0.1s"
+my_generate_project "openssl"       "src/libs/openssl-1.0.1t"               --begin-incs "include" "src/libs/openssl-1.0.1t/crypto"         --end-includes "src/libs/openssl-1.0.1t"
 my_generate_project "curl"          "src/libs/curl-7.47.0"                  --begin-incs "include" "src/libs/curl-7.47.0/include"           --end-includes "src/libs/curl-7.47.0"
 my_generate_project "kStuff"        "src/libs/kStuff"                       --begin-incs "include" "src/libs/kStuff/kStuff/include"         --end-includes "src/libs/kStuff"
 
