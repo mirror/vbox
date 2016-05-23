83f3d669929b91494a2d892768378c7c0f6ddfe9
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2016-05-23 22:31:25 +0000

    CPUMRZ.cpp: oops.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@61147 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/VMM/VMMRZ/CPUMRZ.cpp
+++ src/VBox/VMM/VMMRZ/CPUMRZ.cpp
@@ -132,7 +132,6 @@ VMMRZ_INT_DECL(void)    CPUMRZFpuStateActualizeSseForRead(PVMCPU pVCpu)
 {
 #if defined(VBOX_WITH_KERNEL_USING_XMM) && HC_ARCH_BITS == 64
     NOREF(pVCpu);
-#error "do NOT commit this"
 #else
     if (pVCpu->cpum.s.fUseFlags & CPUM_USED_FPU_GUEST)
     {
@@ -145,10 +144,7 @@ VMMRZ_INT_DECL(void)    CPUMRZFpuStateActualizeSseForRead(PVMCPU pVCpu)
         }
         else
 # endif
-        {
-RTLogPrintf("calling cpumRZSaveGuestSseRegisters\n");
             cpumRZSaveGuestSseRegisters(&pVCpu->cpum.s);
-        }
     }
 #endif
 }
