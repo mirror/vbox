502802b07ec3c598608917c4bf7221da9dc43bd3
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2016-05-26 17:01:21 +0000

    FE/Qt: Selector UI: Details pane: Preview element: Yet another Qt5 migration consequence, the usual fix for the traclucent content being blend-out by the backing-store.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@61215 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Frontends/VirtualBox/src/selector/graphics/details/UIGMachinePreview.cpp
+++ src/VBox/Frontends/VirtualBox/src/selector/graphics/details/UIGMachinePreview.cpp
@@ -392,6 +392,18 @@ void UIGMachinePreview::paint(QPainter *pPainter, const QStyleOptionGraphicsItem
         /* Move image to viewport center: */
         QRect imageRect(QPoint(0, 0), m_pPreviewImg->size());
         imageRect.moveCenter(m_vRect.center());
+
+#ifdef VBOX_WS_MAC
+# if QT_VERSION >= 0x050000
+        /* Set composition-mode to opaque: */
+        pPainter->setCompositionMode(QPainter::CompositionMode_Source);
+        /* Replace translucent background with black one: */
+        pPainter->fillRect(imageRect, QColor(Qt::black));
+        /* Return default composition-mode back: */
+        pPainter->setCompositionMode(QPainter::CompositionMode_SourceAtop);
+# endif /* QT_VERSION >= 0x050000 */
+#endif /* VBOX_WS_MAC */
+
         /* Draw preview image: */
         pPainter->drawImage(imageRect.topLeft(), *m_pPreviewImg);
     }
