21720214c614f534eef0eb433de1ed11e11ad73d
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2016-05-26 01:57:33 +0000

    NAT: Missed part of r107489 - provide more convenience macros.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@61202 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Devices/Network/slirp/ctl.h
+++ src/VBox/Devices/Network/slirp/ctl.h
@@ -26,7 +26,17 @@
 #define CTL_GUEST       15
 #define CTL_BROADCAST   255
 
+
+#define CTL_CHECK_NETWORK(x) (((x) & RT_H2N_U32(pData->netmask)) == pData->special_addr.s_addr)
+
 #define CTL_CHECK(x, ctl) (   ((RT_N2H_U32((x)) & ~pData->netmask) == (ctl)) \
-                           && (((x) & RT_H2N_U32(pData->netmask)) == pData->special_addr.s_addr))
+                           && CTL_CHECK_NETWORK(x))
+
+#define CTL_CHECK_MINE(x) (   CTL_CHECK(x, CTL_ALIAS)      \
+                           || CTL_CHECK(x, CTL_DNS)        \
+                           || CTL_CHECK(x, CTL_TFTP))
+
+#define CTL_CHECK_BROADCAST(x) CTL_CHECK((x), ~pData->netmask)
+
 
 #endif /* _SLIRP_CTL_H_ */
