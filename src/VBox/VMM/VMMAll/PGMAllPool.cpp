c721f23fc583fe251b19ff4e79648b54bf9bf3d4
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2016-05-26 14:14:27 +0000

    pgmPoolMonitorIsReused: Subject non-REP MOVSWDQ and STOSWDQ to the new tests too.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@61212 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/VMM/VMMAll/PGMAllPool.cpp
+++ src/VBox/VMM/VMMAll/PGMAllPool.cpp
@@ -790,18 +790,20 @@ DECLINLINE(bool) pgmPoolMonitorIsReused(PVM pVM, PVMCPU pVCpu, PCPUMCTXCORE pReg
                 Log(("pgmPoolMonitorIsReused: OP_STOSQ\n"));
                 return true;
             }
-            return false;
-    }
+            break;
 
-    /*
-     * Anything having ESP on the left side means stack writes.
-     */
-    if (    (    (pDis->Param1.fUse & DISUSE_REG_GEN32)
-             ||  (pDis->Param1.fUse & DISUSE_REG_GEN64))
-        &&  (pDis->Param1.Base.idxGenReg == DISGREG_ESP))
-    {
-        Log4(("pgmPoolMonitorIsReused: ESP\n"));
-        return true;
+        default:
+            /*
+             * Anything having ESP on the left side means stack writes.
+             */
+            if (    (    (pDis->Param1.fUse & DISUSE_REG_GEN32)
+                     ||  (pDis->Param1.fUse & DISUSE_REG_GEN64))
+                &&  (pDis->Param1.Base.idxGenReg == DISGREG_ESP))
+            {
+                Log4(("pgmPoolMonitorIsReused: ESP\n"));
+                return true;
+            }
+            break;
     }
 
     /*
