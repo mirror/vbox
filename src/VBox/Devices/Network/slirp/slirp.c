79eba6df4ca00409f189a028c60dac55130e8743
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2016-05-26 00:20:20 +0000

    NAT: slirp_input - g/c unused variable.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@61200 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Devices/Network/slirp/slirp.c
+++ src/VBox/Devices/Network/slirp/slirp.c
@@ -1382,7 +1382,6 @@ void slirp_input(PNATState pData, struct mbuf *m, size_t cbBuf)
     int proto;
     static bool fWarnedIpv6;
     struct ethhdr *eh;
-    uint8_t au8Ether[ETH_ALEN];
 
     m->m_len = cbBuf;
     if (cbBuf < ETH_HLEN)
@@ -1391,11 +1390,9 @@ void slirp_input(PNATState pData, struct mbuf *m, size_t cbBuf)
         m_freem(pData, m);
         return;
     }
+
     eh = mtod(m, struct ethhdr *);
     proto = RT_N2H_U16(eh->h_proto);
-
-    memcpy(au8Ether, eh->h_source, ETH_ALEN);
-
     switch(proto)
     {
         case ETH_P_ARP:
