dd30af73ea1d5870c0bb1e3b52c3465dc20a7ae1
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2016-05-27 12:59:26 +0000

    FE/Qt: Fixing GUI warnings: But we want that one hide.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@61243 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Frontends/VirtualBox/src/selector/graphics/chooser/UIGChooserItemGroup.h
+++ src/VBox/Frontends/VirtualBox/src/selector/graphics/chooser/UIGChooserItemGroup.h
@@ -149,7 +149,10 @@ private:
     int minimumHeightHint(bool fOpenedGroup) const;
     int minimumWidthHint() const;
     int minimumHeightHint() const;
+#pragma clang diagnostic push
+# pragma clang diagnostic ignored "-Woverloaded-virtual"
     QSizeF minimumSizeHint(bool fOpenedGroup) const;
+#pragma clang diagnostic pop
     QSizeF sizeHint(Qt::SizeHint which, const QSizeF &constraint = QSizeF()) const;
 
     /* Helpers: Drag&drop stuff: */
