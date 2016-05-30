9a8392836b16493130630cce0e97680c0f546683
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2016-05-30 15:19:06 +0000

    Main/HostDnsService: dumpHostDnsInformation - print domain before the search list.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@61302 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Main/src-server/HostDnsService.cpp
+++ src/VBox/Main/src-server/HostDnsService.cpp
@@ -426,12 +426,13 @@ void HostDnsMonitorProxy::updateInfo()
 static void dumpHostDnsInformation(const HostDnsInformation& info)
 {
     dumpHostDnsStrVector("server", info.servers);
-    dumpHostDnsStrVector("search string", info.searchList);
 
     if (!info.domain.empty())
         LogRel(("  domain: %s\n", info.domain.c_str()));
     else
         LogRel(("  no domain set\n"));
+
+    dumpHostDnsStrVector("search string", info.searchList);
 }
 
 
