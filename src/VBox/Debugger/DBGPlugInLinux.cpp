82583ee586ca9b00b4da668385a32c64993f341c
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2016-06-08 14:09:27 +0000

    Debugger/Digger/Linux: Fix for buffer overflow return code
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@61578 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Debugger/DBGPlugInLinux.cpp
+++ src/VBox/Debugger/DBGPlugInLinux.cpp
@@ -419,16 +419,17 @@ static int dbgDiggerLinuxLogBufferQueryAscii(PDBGDIGGERLINUX pThis, PUVM pUVM, R
     }
 
     /** @todo: Try to parse where the single messages start to make use of cMessages. */
-    memcpy(&pszBuf[0], pbLogBuf, RT_MIN(cbBuf, cbLogBuf));
+    size_t cchLength = RTStrNLen((const char *)pbLogBuf, cbLogBuf);
+    memcpy(&pszBuf[0], pbLogBuf, RT_MIN(cbBuf, cchLength));
 
     /* Done with the buffer. */
     RTMemFree(pbLogBuf);
 
     /* Set return size value. */
     if (pcbActual)
-        *pcbActual = RT_MIN(cbBuf, cbLogBuf);
+        *pcbActual = RT_MIN(cbBuf, cchLength);
 
-    return cbBuf <= cbLogBuf ? VINF_SUCCESS : VERR_BUFFER_OVERFLOW;
+    return cbBuf <= cchLength ? VERR_BUFFER_OVERFLOW : VINF_SUCCESS;
 }
 
 /**
