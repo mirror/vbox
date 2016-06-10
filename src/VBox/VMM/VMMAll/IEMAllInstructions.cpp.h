d18036ff2eeb2559d40c794002b97205a5341fa1
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2016-06-10 15:19:48 +0000

    iemOp_movups_Wps_Vps__movupd_Wpd_Vpd__movss_Wss_Vss__movsd_Vsd_Wsd: ignore irrelevant prefixes like REX*.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@61660 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/VMM/VMMAll/IEMAllInstructions.cpp.h
+++ src/VBox/VMM/VMMAll/IEMAllInstructions.cpp.h
@@ -1510,7 +1510,8 @@ FNIEMOP_STUB(iemOp_movups_Vps_Wps__movupd_Vpd_Wpd__movss_Vss_Wss__movsd_Vsd_Wsd)
 FNIEMOP_DEF(iemOp_movups_Wps_Vps__movupd_Wpd_Vpd__movss_Wss_Vss__movsd_Vsd_Wsd)
 {
     /* Quick hack. Need to restructure all of this later some time. */
-    if (pIemCpu->fPrefixes == 0)
+    uint8_t const fRelevantPrefix = pIemCpu->fPrefixes & (IEM_OP_PRF_SIZE_OP | IEM_OP_PRF_REPNZ | IEM_OP_PRF_REPZ);
+    if (fRelevantPrefix == 0)
     {
         IEMOP_MNEMONIC("movups Wps,Vps");
         uint8_t bRm; IEM_OPCODE_GET_NEXT_U8(&bRm);
@@ -1549,7 +1550,7 @@ FNIEMOP_DEF(iemOp_movups_Wps_Vps__movupd_Wpd_Vpd__movss_Wss_Vss__movsd_Vsd_Wsd)
             IEM_MC_END();
         }
     }
-    else if (pIemCpu->fPrefixes == IEM_OP_PRF_REPNZ)
+    else if (fRelevantPrefix == IEM_OP_PRF_REPNZ)
     {
         IEMOP_MNEMONIC("movsd Wsd,Vsd");
         uint8_t bRm; IEM_OPCODE_GET_NEXT_U8(&bRm);
