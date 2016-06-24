afb175bbbfed2e0874cd12ea71a94e385dd2436b
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2016-06-24 16:17:15 +0000

    FE/Qt: bugref:5978: Runtime UI: Mini-toolbar: Workaround for Unity (Compiz) which sometimes forgets to move Qt::Tool mini-toolbar to full-screen.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@61884 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Frontends/VirtualBox/src/widgets/UIMiniToolBar.h
+++ src/VBox/Frontends/VirtualBox/src/widgets/UIMiniToolBar.h
@@ -65,6 +65,9 @@ signals:
 
 public:
 
+    /** Proposes default set of window flags for particular platform. */
+    static Qt::WindowFlags defaultWindowFlags();
+
     /** Constructor, passes @a pParent to the QWidget constructor.
       * @param geometryType determines the geometry type,
       * @param alignment    determines the alignment type,
