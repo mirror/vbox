0a7a34b1b7640a301777dddffd18630edca1c203
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2016-07-14 11:30:50 +0000

    RTDbgModSymbolAdd: VERR_INVALID_POINTER for NULL pszSymbol.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@62244 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Runtime/common/dbg/dbgmod.cpp
+++ src/VBox/Runtime/common/dbg/dbgmod.cpp
@@ -1756,7 +1756,7 @@ RTDECL(int) RTDbgModSymbolAdd(RTDBGMOD hDbgMod, const char *pszSymbol, RTDBGSEGI
      */
     PRTDBGMODINT pDbgMod = hDbgMod;
     RTDBGMOD_VALID_RETURN_RC(pDbgMod, VERR_INVALID_HANDLE);
-    AssertPtrReturn(pszSymbol, VERR_DBG_SYMBOL_NAME_OUT_OF_RANGE);
+    AssertPtrReturn(pszSymbol, VERR_INVALID_POINTER);
     size_t cchSymbol = strlen(pszSymbol);
     AssertReturn(cchSymbol, VERR_DBG_SYMBOL_NAME_OUT_OF_RANGE);
     AssertReturn(cchSymbol < RTDBG_SYMBOL_NAME_LENGTH, VERR_DBG_SYMBOL_NAME_OUT_OF_RANGE);
