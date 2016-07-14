08399723588ec3a8bfbf61f2cc51487287d1d1a1
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2016-07-14 11:28:38 +0000

    rtDbgModExportsAddSymbolCallback: Make fake name for NONAME ordinal exports.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@62243 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Runtime/common/dbg/dbgmodexports.cpp
+++ src/VBox/Runtime/common/dbg/dbgmodexports.cpp
@@ -65,6 +65,13 @@ static DECLCALLBACK(int) rtDbgModExportsAddSymbolCallback(RTLDRMOD hLdrMod, cons
 
     if (Value >= pArgs->uImageBase)
     {
+        char szOrdinalNm[48];
+        if (!pszSymbol)
+        {
+            RTStrPrintf(szOrdinalNm, sizeof(szOrdinalNm), "Ordinal%u", uSymbol);
+            pszSymbol = szOrdinalNm;
+        }
+
         int rc = RTDbgModSymbolAdd(pArgs->pDbgMod, pszSymbol, RTDBGSEGIDX_RVA, Value - pArgs->uImageBase,
                                    0 /*cb*/, 0 /* fFlags */, NULL /*piOrdinal*/);
         Log(("Symbol #%05u %#018RTptr %s [%Rrc]\n", uSymbol, Value, pszSymbol, rc)); NOREF(rc);
