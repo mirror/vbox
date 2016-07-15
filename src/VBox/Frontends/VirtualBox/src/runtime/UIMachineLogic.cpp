ea3edb227ce890184c3e06b6ae14c2e77d40547d
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2016-07-15 15:47:53 +0000

    FE/Qt: bugref:8502: Use the proper API to get the screen-shot while under the separate UI frontend.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@62285 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Frontends/VirtualBox/src/runtime/UIMachineLogic.cpp
+++ src/VBox/Frontends/VirtualBox/src/runtime/UIMachineLogic.cpp
@@ -2720,7 +2720,21 @@ void UIMachineLogic::takeScreenshot(const QString &strFile, const QString &strFo
         uMaxWidth  += width;
         uMaxHeight  = RT_MAX(uMaxHeight, height);
         QImage shot = QImage(width, height, QImage::Format_RGB32);
-        display().TakeScreenShot(i, shot.bits(), shot.width(), shot.height(), KBitmapFormat_BGR0);
+        /* For separate process: */
+        if (vboxGlobal().isSeparateProcess())
+        {
+            /* Take screen-data to array first: */
+            const QVector<BYTE> screenData = display().TakeScreenShotToArray(i, shot.width(), shot.height(), KBitmapFormat_BGR0);
+            /* And copy that data to screen-shot if it is Ok: */
+            if (display().isOk() && !screenData.isEmpty())
+                memcpy(shot.bits(), screenData.data(), shot.width() * shot.height() * 4);
+        }
+        /* For the same process: */
+        else
+        {
+            /* Take the screen-shot directly: */
+            display().TakeScreenShot(i, shot.bits(), shot.width(), shot.height(), KBitmapFormat_BGR0);
+        }
         images << shot;
     }
     /* Create a image which will hold all sub images vertically. */
