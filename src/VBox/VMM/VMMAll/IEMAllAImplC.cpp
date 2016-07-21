7761e328e2d4ada7c9b849d20b953284c779bce1
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2016-07-21 20:56:44 +0000

    iemAImpl_div_u64: Documented flag behavior of Intel Skylake 6700K, which is what we implement.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@62413 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/VMM/VMMAll/IEMAllAImplC.cpp
+++ src/VBox/VMM/VMMAll/IEMAllAImplC.cpp
@@ -1212,6 +1212,7 @@ IEM_DECL_IMPL_DEF(void, iemAImpl_imul_two_u64,(uint64_t *puDst, uint64_t uSrc, u
 
 IEM_DECL_IMPL_DEF(int, iemAImpl_div_u64,(uint64_t *pu64RAX, uint64_t *pu64RDX, uint64_t u64Divisor, uint32_t *pfEFlags))
 {
+    /* Note! Skylake leaves all flags alone. */
     if (   u64Divisor != 0
         && *pu64RDX < u64Divisor)
     {
