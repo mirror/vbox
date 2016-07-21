7c575fe89a1f03bd01ff6cf266b6ac12dea4ead5
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2016-07-21 15:08:00 +0000

    3D: Skipping guest windows having class 'ApplicationFrameWindow' which occupy all Desktop in Win10. bugref:8496
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@62405 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Additions/WINNT/VBoxTray/VBoxSeamless.cpp
+++ src/VBox/Additions/WINNT/VBoxTray/VBoxSeamless.cpp
@@ -196,99 +196,104 @@ BOOL CALLBACK VBoxEnumFunc(HWND hwnd, LPARAM lParam)
 
     dwStyle   = GetWindowLong(hwnd, GWL_STYLE);
     dwExStyle = GetWindowLong(hwnd, GWL_EXSTYLE);
-    if (   !(dwStyle & WS_VISIBLE)
-        ||  (dwStyle & WS_CHILD))
+
+    if ( !(dwStyle & WS_VISIBLE) || (dwStyle & WS_CHILD))
         return TRUE;
 
-    Log(("VBoxTray: VBoxEnumFunc %x\n", hwnd));
+    LogFlow(("VBoxTray: VBoxEnumFunc %x\n", hwnd));
     /* Only visible windows that are present on the desktop are interesting here */
-    if (GetWindowRect(hwnd, &rectWindow))
+    if (!GetWindowRect(hwnd, &rectWindow))
     {
-        char szWindowText[256];
-        szWindowText[0] = 0;
-        OSVERSIONINFO OSinfo;
-        HWND hStart = NULL;
-        GetWindowText(hwnd, szWindowText, sizeof(szWindowText));
-        OSinfo.dwOSVersionInfoSize = sizeof (OSinfo);
-        GetVersionEx (&OSinfo);
-        if (OSinfo.dwMajorVersion >= 6)
+        return TRUE;
+    }
+
+    char szWindowText[256];
+    char szWindowClass[256];
+    OSVERSIONINFO OSinfo;
+    HWND hStart = NULL;
+
+    szWindowText[0] = 0;
+    szWindowClass[0] = 0;
+
+    GetWindowText(hwnd, szWindowText, sizeof(szWindowText));
+    GetClassName(hwnd, szWindowClass, sizeof(szWindowClass));
+
+    OSinfo.dwOSVersionInfoSize = sizeof (OSinfo);
+    GetVersionEx (&OSinfo);
+
+    if (OSinfo.dwMajorVersion >= 6)
+    {
+        hStart = ::FindWindowEx(GetDesktopWindow(), NULL, "Button", "Start");
+
+        if ( hwnd == hStart && !strcmp(szWindowText, "Start") )
         {
-            hStart = ::FindWindowEx(GetDesktopWindow(), NULL, "Button", "Start");
-            if (  hwnd == hStart && szWindowText != NULL
-                && !(strcmp(szWindowText, "Start"))
-               )
-            {
-                /* for vista and above. To solve the issue of small bar above
-                 * the Start button when mouse is hovered over the start button in seamless mode.
-                 * Difference of 7 is observed in Win 7 platform between the dimensionsof rectangle with Start title and its shadow.
-                 */
-                rectWindow.top += 7;
-                rectWindow.bottom -=7;
-            }
+            /* for vista and above. To solve the issue of small bar above
+             * the Start button when mouse is hovered over the start button in seamless mode.
+             * Difference of 7 is observed in Win 7 platform between the dimensions of rectangle with Start title and its shadow.
+            */
+            rectWindow.top += 7;
+            rectWindow.bottom -=7;
         }
-        rectVisible = rectWindow;
+    }
 
-#ifdef LOG_ENABLED
-        DWORD pid = 0;
-        DWORD tid = GetWindowThreadProcessId(hwnd, &pid);
-#endif
+    rectVisible = rectWindow;
 
-        /* Filter out Windows XP shadow windows */
-        /** @todo still shows inside the guest */
-        if (   szWindowText[0] == 0
-            && (
-                    (dwStyle == (WS_POPUP|WS_VISIBLE|WS_CLIPSIBLINGS)
-                            && dwExStyle == (WS_EX_LAYERED|WS_EX_TOOLWINDOW|WS_EX_TRANSPARENT|WS_EX_TOPMOST))
-                 || (dwStyle == (WS_POPUP|WS_VISIBLE|WS_DISABLED|WS_CLIPSIBLINGS|WS_CLIPCHILDREN)
-                            && dwExStyle == (WS_EX_TOOLWINDOW | WS_EX_TRANSPARENT | WS_EX_LAYERED | WS_EX_NOACTIVATE))
-                 || (dwStyle == (WS_POPUP|WS_VISIBLE|WS_CLIPSIBLINGS|WS_CLIPCHILDREN)
-                                       && dwExStyle == (WS_EX_TOOLWINDOW))
-                            ))
+    /* Filter out Windows XP shadow windows 
+    /** @todo still shows inside the guest */
+    if ( szWindowText[0] == 0 &&
+            (dwStyle == (WS_POPUP | WS_VISIBLE | WS_CLIPSIBLINGS)
+                && dwExStyle == (WS_EX_LAYERED | WS_EX_TOOLWINDOW | WS_EX_TRANSPARENT | WS_EX_TOPMOST))
+            || (dwStyle == (WS_POPUP | WS_VISIBLE | WS_DISABLED | WS_CLIPSIBLINGS | WS_CLIPCHILDREN)
+                && dwExStyle == (WS_EX_TOOLWINDOW | WS_EX_TRANSPARENT | WS_EX_LAYERED | WS_EX_NOACTIVATE))
+            || (dwStyle == (WS_POPUP | WS_VISIBLE | WS_CLIPSIBLINGS | WS_CLIPCHILDREN)
+                && dwExStyle == (WS_EX_TOOLWINDOW)) )
+    {
+        Log(("VBoxTray: Filter out shadow window style=%x exstyle=%x\n", dwStyle, dwExStyle));
+        Log(("VBoxTray: Enum hwnd=%x rect (%d,%d) (%d,%d) (filtered)\n", hwnd, rectWindow.left, rectWindow.top, rectWindow.right, rectWindow.bottom));
+        Log(("VBoxTray: title=%s style=%x exStyle=%x\n", szWindowText, dwStyle, dwExStyle));
+        return TRUE;
+    }
+
+    /** Such a windows covers the whole screen making desktop background*/
+    if (strcmp(szWindowText, "Program Manager") && strcmp(szWindowClass, "ApplicationFrameWindow"))
+    {
+        Log(("VBoxTray: Enum hwnd=%x rect (%d,%d)-(%d,%d) [%d x %d](applying)\n", hwnd, 
+            rectWindow.left, rectWindow.top, rectWindow.right, rectWindow.bottom,
+            rectWindow.left - rectWindow.right, rectWindow.bottom - rectWindow.top));
+        Log(("VBoxTray: title=%s style=%x exStyle=%x\n", szWindowText, dwStyle, dwExStyle));
+
+        HRGN hrgn = CreateRectRgn(0, 0, 0, 0);
+
+        int ret = GetWindowRgn(hwnd, hrgn);
+
+        if (ret == ERROR)
         {
-            Log(("VBoxTray: Filter out shadow window style=%x exstyle=%x\n", dwStyle, dwExStyle));
-            Log(("VBoxTray: Enum hwnd=%x rect (%d,%d) (%d,%d) (filtered)\n", hwnd, rectWindow.left, rectWindow.top, rectWindow.right, rectWindow.bottom));
-            Log(("VBoxTray: title=%s style=%x exStyle=%x\n", szWindowText, dwStyle, dwExStyle));
-            Log(("VBoxTray: pid=%d tid=%d\n", pid, tid));
-            return TRUE;
-        }
-
-        /** @todo will this suffice? The Program Manager window covers the whole screen */
-        if (strcmp(szWindowText, "Program Manager"))
-        {
-            Log(("VBoxTray: Enum hwnd=%x rect (%d,%d) (%d,%d) (applying)\n", hwnd, rectWindow.left, rectWindow.top, rectWindow.right, rectWindow.bottom));
-            Log(("VBoxTray: title=%s style=%x exStyle=%x\n", szWindowText, dwStyle, dwExStyle));
-            Log(("VBoxTray: pid=%d tid=%d\n", pid, tid));
-
-            HRGN hrgn = CreateRectRgn(0,0,0,0);
-
-            int ret = GetWindowRgn(hwnd, hrgn);
-
-            if (ret == ERROR)
-            {
-                Log(("VBoxTray: GetWindowRgn failed with rc=%d\n", GetLastError()));
-                SetRectRgn(hrgn, rectVisible.left, rectVisible.top, rectVisible.right, rectVisible.bottom);
-            }
-            else
-            {
-                /* this region is relative to the window origin instead of the desktop origin */
-                OffsetRgn(hrgn, rectWindow.left, rectWindow.top);
-            }
-            if (lpParam->hrgn)
-            {
-                /* create a union of the current visible region and the visible rectangle of this window. */
-                CombineRgn(lpParam->hrgn, lpParam->hrgn, hrgn, RGN_OR);
-                DeleteObject(hrgn);
-            }
-            else
-                lpParam->hrgn = hrgn;
+            Log(("VBoxTray: GetWindowRgn failed with rc=%d, adding antire rect\n", GetLastError()));
+            SetRectRgn(hrgn, rectVisible.left, rectVisible.top, rectVisible.right, rectVisible.bottom);
         }
         else
         {
-            Log(("VBoxTray: Enum hwnd=%x rect (%d,%d) (%d,%d) (ignored)\n", hwnd, rectWindow.left, rectWindow.top, rectWindow.right, rectWindow.bottom));
-            Log(("VBoxTray: title=%s style=%x\n", szWindowText, dwStyle));
-            Log(("VBoxTray: pid=%d tid=%d\n", pid, tid));
+            /* this region is relative to the window origin instead of the desktop origin */
+            OffsetRgn(hrgn, rectWindow.left, rectWindow.top);
         }
+
+        if (lpParam->hrgn)
+        {
+            /* create a union of the current visible region and the visible rectangle of this window. */
+            CombineRgn(lpParam->hrgn, lpParam->hrgn, hrgn, RGN_OR);
+            DeleteObject(hrgn);
+        }
+        else
+            lpParam->hrgn = hrgn;
     }
+    else
+    {
+        Log(("VBoxTray: Enum hwnd=%x rect (%d,%d)-(%d,%d) [%d x %d](ignored)\n", hwnd,
+            rectWindow.left, rectWindow.top, rectWindow.right, rectWindow.bottom,
+            rectWindow.left - rectWindow.right, rectWindow.bottom - rectWindow.top));
+        Log(("VBoxTray: title=%s style=%x exStyle=%x\n", szWindowText, dwStyle, dwExStyle));
+    }
+
     return TRUE; /* continue enumeration */
 }
 
@@ -319,20 +324,22 @@ void VBoxSeamlessCheckWindows(bool fForce)
             {
                 lpEscapeData->escapeCode = VBOXESC_SETVISIBLEREGION;
                 LPRGNDATA lpRgnData = VBOXDISPIFESCAPE_DATA(lpEscapeData, RGNDATA);
+
                 memset(lpRgnData, 0, cbSize);
                 cbSize = GetRegionData(param.hrgn, cbSize, lpRgnData);
+
                 if (cbSize)
                 {
-#ifdef DEBUG
                     RECT *lpRect = (RECT *)&lpRgnData->Buffer[0];
-                    Log(("VBoxTray: New visible region: \n"));
+                    LogRel(("VBoxTray: New visible region: \n"));
 
-                    for (DWORD i=0;i<lpRgnData->rdh.nCount;i++)
+                    for (DWORD i = 0; i < lpRgnData->rdh.nCount; i++)
                     {
-                        Log(("VBoxTray: visible rect (%d,%d)(%d,%d)\n", lpRect[i].left, lpRect[i].top, lpRect[i].right, lpRect[i].bottom));
+                        LogRel(("VBoxTray: visible rect (%d,%d)(%d,%d)\n", lpRect[i].left, lpRect[i].top, lpRect[i].right, lpRect[i].bottom));
                     }
-#endif
+
                     LPRGNDATA lpCtxRgnData = VBOXDISPIFESCAPE_DATA(pCtx->lpEscapeData, RGNDATA);
+
                     if (fForce
                         ||  !pCtx->lpEscapeData
                         ||  (lpCtxRgnData->rdh.dwSize + lpCtxRgnData->rdh.nRgnSize != cbSize)
