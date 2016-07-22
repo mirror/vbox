506eae9eb1aee90476124eeeb7d1f50ca164c7a4
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2016-07-22 18:36:43 +0000

    (C) 2016
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@62485 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Main/webservice/webtest.cpp
+++ src/VBox/Main/webservice/webtest.cpp
@@ -3,7 +3,7 @@
  *      demo webservice client in C++. This mimics some of the
  *      functionality of VBoxManage for testing purposes.
  *
- * Copyright (C) 2006-2014 Oracle Corporation
+ * Copyright (C) 2006-2016 Oracle Corporation
  *
  * This file is part of VirtualBox Open Source Edition (OSE), as
  * available from http://www.virtualbox.org. This file is free software;
