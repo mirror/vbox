46818e7354917e33503d1d111fd981d933693cd1
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2016-07-22 09:58:34 +0000

    NetBSD: Commit patches 001 - 003 contributed by Kamil Rytarowski on vbox-dev, thanks
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@62416 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Runtime/r3/posix/utf8-posix.cpp
+++ src/VBox/Runtime/r3/posix/utf8-posix.cpp
@@ -173,8 +173,8 @@ static int rtstrConvertCached(const void *pvInput, size_t cbInput, const char *p
         iconv_t hIconv = (iconv_t)*phIconv;
         if (hIconv == (iconv_t)-1)
         {
-#ifdef RT_OS_SOLARIS
-            /* Solaris doesn't grok empty codeset strings, so help it find the current codeset. */
+#if defined(RT_OS_SOLARIS) || defined(RT_OS_NETBSD)
+            /* Some systems don't grok empty codeset strings, so help it find the current codeset. */
             if (!*pszInputCS)
                 pszInputCS = rtStrGetLocaleCodeset();
             if (!*pszOutputCS)
