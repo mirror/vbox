53c8308f41c1b06f30762e9cb1dfc056be138407
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2016-07-28 15:32:52 +0000

    unused parameter warnings.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@62627 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Runtime/testcase/tstRTR0Thread.cpp
+++ src/VBox/Runtime/testcase/tstRTR0Thread.cpp
@@ -57,6 +57,7 @@ typedef TSTRTR0THREADDATA *PTSTRTR0THREADDATA;
 static DECLCALLBACK(int) tstRTR0ThreadCallback(RTTHREAD hThread, void *pvUser)
 {
     PTSTRTR0THREADDATA pData = (PTSTRTR0THREADDATA)pvUser;
+    RT_NOREF1(hThread);
     if (RT_LIKELY(pData))
     {
         if (pData->uMagic == TSTRTR0THREADDATA_MAGIC)
@@ -81,6 +82,7 @@ DECLEXPORT(int) TSTRTR0ThreadSrvReqHandler(PSUPDRVSESSION pSession, uint32_t uOp
                                                    uint64_t u64Arg, PSUPR0SERVICEREQHDR pReqHdr)
 {
     RTR0TESTR0_SRV_REQ_PROLOG_RET(pReqHdr);
+    RT_NOREF2(pSession, u64Arg);
 
     /*
      * The big switch.
