72f6da1795af3075161c566a0cbe2f0c8666eb19
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2016-07-29 09:01:44 +0000

    HDA: Re-enabled speakers for OS X guests.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@62672 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Devices/Audio/DevIchHdaCodec.cpp
+++ src/VBox/Devices/Audio/DevIchHdaCodec.cpp
@@ -1176,7 +1176,10 @@ static DECLCALLBACK(int) stac9220ResetNode(PHDACODEC pThis, uint8_t uNID, PCODEC
         {
             pNode->port.u32F07_param = CODEC_F07_IN_ENABLE;
             pNode->port.u32F08_param = 0;
-            pNode->port.u32F09_param = CODEC_MAKE_F09_ANALOG(true /* fPresent */, CODEC_F09_ANALOG_NA);
+            /* If Line in is reported as enabled, OS X sees no speakers! Windows does
+             * not care either way, although Linux does.
+             */
+            pNode->port.u32F09_param = CODEC_MAKE_F09_ANALOG(0, 0);
 
             pNode->port.node.au32F00_param[0x9] = CODEC_MAKE_F00_09(CODEC_F00_09_TYPE_PIN_COMPLEX, 0, 0)
                                                 | CODEC_F00_09_CAP_UNSOL
