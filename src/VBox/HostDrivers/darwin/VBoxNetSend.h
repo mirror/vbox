fc2c5a7198ffdd7b9281d49fa97995bcfc4fe869
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2016-08-15 23:19:14 +0000

    HostDrivers: warnings (clang)
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@63515 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/HostDrivers/darwin/VBoxNetSend.h
+++ src/VBox/HostDrivers/darwin/VBoxNetSend.h
@@ -75,7 +75,7 @@ DECLINLINE(int) VBoxNetSendDummy(ifnet_t pIfNet)
 
         mbuf_pkthdr_setrcvif(pPkt, pIfNet); /* will crash without this. */
 
-        errno_t err = ifnet_input(pIfNet, pPkt, NULL);
+        err = ifnet_input(pIfNet, pPkt, NULL);
         if (err)
         {
             rc = RTErrConvertFromErrno(err);
