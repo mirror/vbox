fc928cf05e6d0cd67df814a599c8edb223f590dd
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2016-08-16 08:46:12 +0000

    nit
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@63527 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Devices/Audio/DrvHostDSound.cpp
+++ src/VBox/Devices/Audio/DrvHostDSound.cpp
@@ -13,7 +13,6 @@
  * Foundation, in version 2 as it comes in the "COPYING" file of the
  * VirtualBox OSE distribution. VirtualBox OSE is distributed in the
  * hope that it will be useful, but WITHOUT ANY WARRANTY of any kind.
- *
  */
 
 
