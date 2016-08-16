f0ea3e31d9f9fc7e9e9be1bd0be40646cb9807de
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2016-08-16 08:41:33 +0000

    nit
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@63525 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/VMM/VMMAll/APICAll.cpp
+++ src/VBox/VMM/VMMAll/APICAll.cpp
@@ -7,7 +7,7 @@
  * Copyright (C) 2016 Oracle Corporation
  *
  * This file is part of VirtualBox Open Source Edition (OSE), as
- * available from http://www.virtualbox.org. This file is free software
+ * available from http://www.virtualbox.org. This file is free software;
  * you can redistribute it and/or modify it under the terms of the GNU
  * General Public License (GPL) as published by the Free Software
  * Foundation, in version 2 as it comes in the "COPYING" file of the
