29a82497053ab78ab8c6570e0ac22eb40c7b10c5
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2016-08-26 12:06:13 +0000

    3D: Skip the second VBOXCMDVBVA_RESIZE which hides disabled Virtual Displays, bugref:8387
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@63652 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Additions/WINNT/Graphics/Video/mp/wddm/VBoxMPWddm.cpp
+++ src/VBox/Additions/WINNT/Graphics/Video/mp/wddm/VBoxMPWddm.cpp
@@ -647,8 +647,11 @@ void vboxWddmGhDisplayCheckSetInfoEx(PVBOXMP_DEVEXT pDevExt, bool fReportTargets
         vboxWddmGhDisplayCheckSetInfoFromSourceEx(pDevExt, pSource, fReportTargets);
     }
 
+    /* Skip the second VBOXCMDVBVA_RESIZE which hides disabled Virtual Displays
     if (fReportTargets)
+    {
         vboxWddmGhDisplayCheckSetInfoForDisabledTargets(pDevExt);
+    }*/
 }
 
 void vboxWddmGhDisplayCheckSetInfo(PVBOXMP_DEVEXT pDevExt)
@@ -1031,7 +1034,7 @@ NTSTATUS DxgkDdiAddDevice(
     vboxVDbgBreakFv();
 
     NTSTATUS Status = STATUS_SUCCESS;
-    PVBOXMP_DEVEXT pDevExt;
+    PVBOXMP_DEVEXT pDevExt = NULL;
 
     WCHAR RegKeyBuf[512];
     ULONG cbRegKeyBuf = sizeof (RegKeyBuf);
