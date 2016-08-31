320e8d332ec5f597e13a433b99d7dbddc3bf5a3e
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2016-08-31 07:29:51 +0000

    Dev/Net/Virtio: Move packet dumper into common header and use it in virtio.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@63669 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Devices/Network/DevVirtioNet.cpp
+++ src/VBox/Devices/Network/DevVirtioNet.cpp
@@ -33,6 +33,7 @@
 # include <iprt/mem.h>
 # include <iprt/uuid.h>
 #endif /* IN_RING3 */
+#include <VBox/VBoxPktDmp.h>
 #include "VBoxDD.h"
 #include "../VirtIO/Virtio.h"
 
@@ -322,9 +323,13 @@ DECLINLINE(void) vnetCsRxLeave(PVNETSTATE pThis)
 DECLINLINE(void) vnetPacketDump(PVNETSTATE pThis, const uint8_t *pbPacket, size_t cb, const char *pszText)
 {
 # ifdef DEBUG
+#  if 0
     Log(("%s %s packet #%d (%d bytes):\n",
          INSTANCE(pThis), pszText, ++pThis->u32PktNo, cb));
     Log3(("%.*Rhxd\n", cb, pbPacket));
+#  else
+    vboxEthPacketDump(INSTANCE(pThis), pszText, pbPacket, (uint32_t)cb);
+#  endif
 # else
     RT_NOREF4(pThis, pbPacket, cb, pszText);
 # endif
