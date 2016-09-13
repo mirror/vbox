d359886483e2249d740f8bada5deba28619eb39a
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2016-09-13 08:19:12 +0000

    bugref:8087: Additions/x11: support non-root X server: better work-around in the kernel vboxvideo driver for clients that do not send dirty rectangle information.  However it only currently works for the first guest screen.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@63807 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Additions/linux/drm/vbox_drv.c
+++ src/VBox/Additions/linux/drm/vbox_drv.c
@@ -241,6 +241,16 @@ static int vbox_master_set(struct drm_device *dev,
      * do not advertise dynamic modes on the first query and send a
      * tentative hotplug notification after that to see if they query again. */
     vbox->initial_mode_queried = false;
+    mutex_lock(&vbox->hw_mutex);
+    /* Disable VBVA when someone releases master in case the next person tries
+     * to do VESA. */
+    /** @todo work out if anyone is likely to and whether it will even work. */
+    /* Update: we also disable it because if the new master does not do dirty
+     * rectangle reporting (e.g. old versions of Plymouth) then at least the
+     * first screen will still be updated.  We enable it as soon as we
+     * receive a dirty rectangle report. */
+    vbox_disable_accel(vbox);
+    mutex_unlock(&vbox->hw_mutex);
     return 0;
 }
 
@@ -254,15 +264,9 @@ static void vbox_master_drop(struct drm_device *dev,
 #endif
 {
     struct vbox_private *vbox = dev->dev_private;
+    /* See vbox_master_set() */
     vbox->initial_mode_queried = false;
     mutex_lock(&vbox->hw_mutex);
-    /* Disable VBVA when someone releases master in case the next person tries
-     * to do VESA. */
-    /** @todo work out if anyone is likely to and whether it will even work. */
-    /* Update: we also disable it because if the new master does not do dirty
-     * rectangle reporting (e.g. old versions of Plymouth) then at least the
-     * first screen will still be updated.  We enable it as soon as we
-     * receive a dirty rectangle report. */
     vbox_disable_accel(vbox);
     mutex_unlock(&vbox->hw_mutex);
 }
