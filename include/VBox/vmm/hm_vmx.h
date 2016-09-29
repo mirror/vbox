4f6cf21848234fe240a53c07ca7d9d9333082a6b
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2016-09-29 12:28:13 +0000

    VMM/HMVMXR0: Macro for true ctls MSR bit.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@64089 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- include/VBox/vmm/hm_vmx.h
+++ include/VBox/vmm/hm_vmx.h
@@ -1108,6 +1108,9 @@ whose vector indexed a bit set in the EOI-exit bitmap. */
 #define MSR_IA32_VMX_BASIC_INFO_VMCS_MEM_TYPE(a)                (((a) >> 50) & 0xF)
 /** Whether the processor provides additional information for exits due to INS/OUTS. */
 #define MSR_IA32_VMX_BASIC_INFO_VMCS_INS_OUTS(a)                RT_BOOL((a) & RT_BIT_64(54))
+/** Whether default 1 bits in control MSRs (pin/proc/exit/entry) may be
+ *  cleared to 0 and that 'true' control MSRs are supported. */ */
+#define MSR_IA32_VMX_BASIC_INFO_TRUE_CONTROLS(a)                RT_BOOL((a) & RT_BIT_64(55))
 /** @} */
 
 
