a1149d6ca949b5225a98c3332add3f05dd98ffeb
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2016-09-30 13:46:23 +0000

    CFGM: when dumping some size, print both MB and GB values for huge values
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@64110 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/VMM/VMMR3/CFGM.cpp
+++ src/VBox/VMM/VMMR3/CFGM.cpp
@@ -3243,14 +3243,14 @@ static void cfgmR3Dump(PCFGMNODE pRoot, unsigned iLevel, PCDBGFINFOHLP pHlp)
                     || (   pLeaf->cchName >= 2
                         && !RTStrNCmp(pLeaf->szName, "cb", 2)) )
                 {
+                    if (pLeaf->Value.Integer.u64 > _2M)
+                        pHlp->pfnPrintf(pHlp, ", %'lld MB", pLeaf->Value.Integer.u64 / _1M);
+                    else if (pLeaf->Value.Integer.u64 > _2K)
+                        pHlp->pfnPrintf(pHlp, ", %'lld KB", pLeaf->Value.Integer.u64 / _1K);
                     if (pLeaf->Value.Integer.u64 > _2G)
                         pHlp->pfnPrintf(pHlp, ", %'lld.%lld GB",
                                               pLeaf->Value.Integer.u64 / _1G,
                                               (pLeaf->Value.Integer.u64 % _1G) / (_1G / 10));
-                    else if (pLeaf->Value.Integer.u64 > _2M)
-                        pHlp->pfnPrintf(pHlp, ", %'lld MB", pLeaf->Value.Integer.u64 / _1M);
-                    else if (pLeaf->Value.Integer.u64 > _2K)
-                        pHlp->pfnPrintf(pHlp, ", %'lld KB", pLeaf->Value.Integer.u64 / _1K);
                 }
                 pHlp->pfnPrintf(pHlp, ")\n");
                 break;
