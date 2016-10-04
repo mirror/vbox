de917421abc50594c489594f07409af9e9b4eb06
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2016-10-04 12:05:46 +0000

    Main/NATEngine: Reject rules with names that contain whitespace, slash (CFGM node path separator) or comma (VBoxManage natpf argument is comma-separated).  ticketref:16002.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@64142 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Main/src-server/NATEngineImpl.cpp
+++ src/VBox/Main/src-server/NATEngineImpl.cpp
@@ -21,6 +21,7 @@
 #include "MachineImpl.h"
 #include "GuestOSTypeImpl.h"
 
+#include <iprt/ctype.h>
 #include <iprt/string.h>
 #include <iprt/cpp/utils.h>
 
@@ -259,8 +260,26 @@ HRESULT NATEngine::addRedirect(const com::Utf8Str &aName, NATProtocol_T aProto, 
         default:
             return E_INVALIDARG;
     }
+
     if (name.isEmpty())
         name = Utf8StrFmt("%s_%d_%d", proto, aHostPort, aGuestPort);
+    else
+    {
+        const char *s;
+        char c;
+
+        for (s = name.c_str(); (c = *s) != '\0'; ++s)
+        {
+            if (RT_C_IS_SPACE(c))
+                return setError(E_INVALIDARG,
+                                tr("Whitespace in NAT rule name"));
+
+            if (   c == '/'  /* CFGM node path separator */
+                || c == ',') /* VBoxManage natpf<N> argument is csv */
+                return setError(E_INVALIDARG,
+                                tr("'%c' - invalid character in NAT rule name"), c);
+        }
+    }
 
     settings::NATRulesMap::iterator it;
     for (it = mData->m->mapRules.begin(); it != mData->m->mapRules.end(); ++it)
