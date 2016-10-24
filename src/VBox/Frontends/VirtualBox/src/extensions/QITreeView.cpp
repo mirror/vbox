5a59024d624cb0aa1a02b89512f156d70ac77bac
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2016-10-24 16:35:58 +0000

    FE/Qt: bugref:6899: Accessibility support (step 101): Doxy for QITreeView.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@64401 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Frontends/VirtualBox/src/extensions/QITreeView.cpp
+++ src/VBox/Frontends/VirtualBox/src/extensions/QITreeView.cpp
@@ -19,11 +19,11 @@
 # include <precomp.h>
 #else  /* !VBOX_WITH_PRECOMPILED_HEADERS */
 
-/* Global includes */
+/* Qt includes: */
 # include <QMouseEvent>
 # include <QPainter>
 
-/* Local includes */
+/* GUI includes: */
 # include "QITreeView.h"
 
 #endif /* !VBOX_WITH_PRECOMPILED_HEADERS */
@@ -32,42 +32,57 @@
 QITreeView::QITreeView (QWidget *aParent)
     : QTreeView (aParent)
 {
+    /* Mark header hidden: */
     setHeaderHidden (true);
+    /* Mark root hidden: */
     setRootIsDecorated (false);
 }
 
 void QITreeView::currentChanged (const QModelIndex &aCurrent, const QModelIndex &aPrevious)
 {
+    /* Notify listeners about it: */
     emit currentItemChanged (aCurrent, aPrevious);
+    /* Call to base-class: */
     QTreeView::currentChanged (aCurrent, aPrevious);
 }
 
 void QITreeView::drawBranches (QPainter *aPainter, const QRect &aRect, const QModelIndex &aIndex) const
 {
+    /* Notify listeners about it: */
     emit drawItemBranches (aPainter, aRect, aIndex);
+    /* Call to base-class: */
     QTreeView::drawBranches (aPainter, aRect, aIndex);
 }
 
 void QITreeView::mouseMoveEvent (QMouseEvent *aEvent)
 {
+    /* Reject event initially: */
     aEvent->setAccepted (false);
+    /* Notify listeners about event allowing them to handle it: */
     emit mouseMoved (aEvent);
+    /* Call to base-class only if event was not yet accepted: */
     if (!aEvent->isAccepted())
         QTreeView::mouseMoveEvent (aEvent);
 }
 
 void QITreeView::mousePressEvent (QMouseEvent *aEvent)
 {
+    /* Reject event initially: */
     aEvent->setAccepted (false);
+    /* Notify listeners about event allowing them to handle it: */
     emit mousePressed (aEvent);
+    /* Call to base-class only if event was not yet accepted: */
     if (!aEvent->isAccepted())
         QTreeView::mousePressEvent (aEvent);
 }
 
 void QITreeView::mouseDoubleClickEvent (QMouseEvent *aEvent)
 {
+    /* Reject event initially: */
     aEvent->setAccepted (false);
+    /* Notify listeners about event allowing them to handle it: */
     emit mouseDoubleClicked (aEvent);
+    /* Call to base-class only if event was not yet accepted: */
     if (!aEvent->isAccepted())
         QTreeView::mouseDoubleClickEvent (aEvent);
 }
--- src/VBox/Frontends/VirtualBox/src/extensions/QITreeView.h
+++ src/VBox/Frontends/VirtualBox/src/extensions/QITreeView.h
@@ -18,39 +18,58 @@
 #ifndef __QITreeView_h__
 #define __QITreeView_h__
 
-/* Global includes */
+/* Qt includes: */
 #include <QTreeView>
 
-/*
- * QITreeView class which extends standard QITreeView's functionality.
- */
+
+/** QTreeView subclass extending standard functionality. */
 class QITreeView: public QTreeView
 {
     Q_OBJECT;
 
 public:
 
+    /** Constructs table-view passing @a aParent to the base-class. */
     QITreeView (QWidget *aParent = 0);
 
 signals:
 
+    /** Notifies listeners about index changed from @a aPrevious to @a aCurrent.*/
     void currentItemChanged (const QModelIndex &aCurrent, const QModelIndex &aPrevious);
+
+    /** Notifies listeners about painting of item branches.
+      * @param  aPainter  Brings the painter to draw branches.
+      * @param  aRect     Brings the rectangle embedding branches.
+      * @param  aIndex    Brings the index of the item for which branches will be painted. */
     void drawItemBranches (QPainter *aPainter, const QRect &aRect, const QModelIndex &aIndex) const;
+
+    /** Notifies listeners about mouse moved @a aEvent. */
     void mouseMoved (QMouseEvent *aEvent);
+    /** Notifies listeners about mouse pressed @a aEvent. */
     void mousePressed (QMouseEvent *aEvent);
+    /** Notifies listeners about mouse double-clicked @a aEvent. */
     void mouseDoubleClicked (QMouseEvent *aEvent);
 
 protected slots:
 
+    /** Handles index changed from @a aPrevious to @a aCurrent.*/
     void currentChanged (const QModelIndex &aCurrent, const QModelIndex &aPrevious);
 
 protected:
 
+    /** Handles painting of item branches.
+      * @param  aPainter  Brings the painter to draw branches.
+      * @param  aRect     Brings the rectangle embedding branches.
+      * @param  aIndex    Brings the index of the item for which branches will be painted. */
     void drawBranches (QPainter *aPainter, const QRect &aRect, const QModelIndex &aIndex) const;
+
+    /** Handles mouse move @a aEvent. */
     void mouseMoveEvent (QMouseEvent *aEvent);
+    /** Handles mouse press @a aEvent. */
     void mousePressEvent (QMouseEvent *aEvent);
+    /** Handles mouse double-click @a aEvent. */
     void mouseDoubleClickEvent (QMouseEvent *aEvent);
 };
 
-#endif /* __QITreeView_h__ */
+#endif /* !__QITreeView_h__ */
 
