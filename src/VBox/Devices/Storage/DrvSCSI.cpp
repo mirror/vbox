e8e6c9ca93ca2cdda3013bddc03091e4e71e5555
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2016-10-27 14:54:04 +0000

    DrvSCSI: Don't report that all data was transfered if the status indicates an error + todo to implement that residual counters properly
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@64441 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Devices/Storage/DrvSCSI.cpp
+++ src/VBox/Devices/Storage/DrvSCSI.cpp
@@ -782,9 +782,10 @@ static DECLCALLBACK(int) drvscsiIoReqFree(PPDMIMEDIAEX pInterface, PDMMEDIAEXIOR
 /** @interface_method_impl{PDMIMEDIAEX,pfnIoReqQueryResidual} */
 static DECLCALLBACK(int) drvscsiIoReqQueryResidual(PPDMIMEDIAEX pInterface, PDMMEDIAEXIOREQ hIoReq, size_t *pcbResidual)
 {
-    RT_NOREF2(pInterface, hIoReq);
+    RT_NOREF1(pInterface);
+    PDRVSCSIREQ pReq = (PDRVSCSIREQ)hIoReq;
 
-    *pcbResidual = 0; /** @todo: Implement. */
+    *pcbResidual = *pReq->pu8ScsiSts == SCSI_STATUS_OK ? 0 : pReq->cbBuf; /** @todo: Implement properly. */
     return VINF_SUCCESS;
 }
 
