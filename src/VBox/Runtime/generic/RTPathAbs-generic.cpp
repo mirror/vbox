2a36b42209bb25baf7ef8463c56c5d571435e925
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2016-11-10 15:11:20 +0000

    RTPathAbs-generic.cpp: Have to remove '.' too, like the it says on the package, or NT fails to parse the resulting path (RTDir*, RTFsQuery*,++)
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@64634 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Runtime/generic/RTPathAbs-generic.cpp
+++ src/VBox/Runtime/generic/RTPathAbs-generic.cpp
@@ -290,7 +290,8 @@ RTDECL(int) RTPathAbs(const char *pszPath, char *pszAbsPath, size_t cchAbsPath)
      */
     for (;;)
     {
-        if (   pszCur[0] == '.'
+        char const chFirst = pszCur[0];
+        if (   chFirst == '.'
             && pszCur[1] == '.'
             && (!pszCur[2] || pszCur[2] == RTPATH_SLASH))
         {
@@ -314,6 +315,21 @@ RTDECL(int) RTPathAbs(const char *pszPath, char *pszAbsPath, size_t cchAbsPath)
             Assert(pszPrev[-1] == RTPATH_SLASH);
             memmove(pszPrev, pszCur + 3, strlen(pszCur + 3) + 1);
             pszCur = pszPrev - 1;
+        }
+        else if (   chFirst == '.'
+                 && (!pszCur[1] || pszCur[1] == RTPATH_SLASH))
+        {
+            /* remove unnecessary '.' */
+            if (!pszCur[1])
+            {
+                if (pszCur != pszTop)
+                    pszCur[-1] = '\0';
+                else
+                    *pszCur = '\0';
+                break;
+            }
+            memmove(pszCur, pszCur + 2, strlen(pszCur + 2) + 1);
+            continue;
         }
         else
         {
