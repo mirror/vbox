dddccd5ca3c511a3986e38c88a72b27da5ff9379
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2016-11-11 16:11:23 +0000

    Audio: Reverted part of r111751 to fix unit test failure.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@64652 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Devices/Audio/AudioMixBuffer.cpp
+++ src/VBox/Devices/Audio/AudioMixBuffer.cpp
@@ -1013,8 +1013,22 @@ static int audioMixBufMixTo(PPDMAUDIOMIXBUF pDst, PPDMAUDIOMIXBUF pSrc, uint32_t
     uint32_t cDstAvail    = pDst->cSamples - pDst->cUsed;
     uint32_t offDstWrite  = pDst->offWrite;
 
+    // Updated code causes tstAudioMixBuffer to fail. Problem is failing the call
+    // when source has no available data, which can apparently happen. See #8521.
+#if 0
     if (!cSrcAvail)
+    {
         return VERR_NO_DATA;
+    }
+#else
+    if (   !cSrcAvail
+        || !cDstAvail)
+    {
+        if (pcProcessed)
+            *pcProcessed = 0;
+        return VINF_SUCCESS;
+    }
+#endif
 
     AUDMIXBUF_LOG(("cSrcSamples=%RU32, cSrcAvail=%RU32 -> cDstAvail=%RU32\n", cSrcSamples,  cSrcAvail, cDstAvail));
 
