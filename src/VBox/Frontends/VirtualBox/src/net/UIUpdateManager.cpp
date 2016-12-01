3a4041404c6f726bbdfec0950ee6b288335420a6
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2016-12-01 13:53:10 +0000

    FE/Qt: added VBOX_NEW_VERSION_TEST which can be used to test the update check
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@64772 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Frontends/VirtualBox/src/net/UIUpdateManager.cpp
+++ src/VBox/Frontends/VirtualBox/src/net/UIUpdateManager.cpp
@@ -55,6 +55,11 @@
 #endif /* !VBOX_WITH_PRECOMPILED_HEADERS */
 
 
+#if 0
+/* enable to test the version update check */
+#define VBOX_NEW_VERSION_TEST "0.0.0_0 http://unknown.unknown.org/0.0.0/VirtualBox-0.0.0-0-unknown.pkg"
+#endif
+
 /* Forward declarations: */
 class UIUpdateStep;
 
@@ -227,6 +232,9 @@ private:
         /* Deserialize incoming data: */
         QString strResponseData(pReply->readAll());
 
+#ifdef VBOX_NEW_VERSION_TEST
+        strResponseData = VBOX_NEW_VERSION_TEST;
+#endif
         /* Newer version of necessary package found: */
         if (strResponseData.indexOf(QRegExp("^\\d+\\.\\d+\\.\\d+(_[0-9A-Z]+)? \\S+$")) == 0)
         {
@@ -519,7 +527,11 @@ void UIUpdateManager::sltCheckIfUpdateIsNecessary(bool fForceCall /* = false */)
     VBoxUpdateData currentData(gEDataManager->applicationUpdateData());
 
     /* If update is really necessary: */
-    if (fForceCall || currentData.isNeedToCheck())
+    if (
+#ifdef VBOX_NEW_VERSION_TEST
+        true ||
+#endif
+        fForceCall || currentData.isNeedToCheck())
     {
         /* Prepare update queue: */
         new UIUpdateStepVirtualBox(m_pQueue, fForceCall);
