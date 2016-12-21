f0e2c79fff8d6ee0b37a2d1f5b4bf00beaefac8c
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2016-12-21 12:50:29 +0000

    build fix
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@64977 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Main/src-client/GuestDnDTargetImpl.cpp
+++ src/VBox/Main/src-client/GuestDnDTargetImpl.cpp
@@ -570,7 +570,7 @@ void GuestDnDTarget::i_sendDataThreadTask(SendDataTask *pTask)
     if (FAILED(autoCaller.rc()))
         return;
 
-    vrc = pThis->i_sendData(pTask->getCtx(), RT_INDEFINITE_WAIT /* msTimeout */);
+    int vrc = pThis->i_sendData(pTask->getCtx(), RT_INDEFINITE_WAIT /* msTimeout */);
 /** @todo
  *
  *  r=bird: What happens with @a vrc?
