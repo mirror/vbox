203fc20c3ca6bd90dfc1c3c18b50e5c72afaa843
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2016-12-23 14:18:23 +0000

    Audio/DevHDA.cpp: VBOX_WITH_HDA_ASYNC_IO -> VBOX_WITH_AUDIO_HDA_ASYNC_IO, logging.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@65003 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Devices/Audio/DevHDA.cpp
+++ src/VBox/Devices/Audio/DevHDA.cpp
@@ -4650,7 +4650,7 @@ static int hdaStreamUpdate(PHDASTATE pThis, PHDASTREAM pStream)
              * Process backends.
              */
 
-#ifdef VBOX_WITH_HDA_ASYNC_IO
+#ifdef VBOX_WITH_AUDIO_HDA_ASYNC_IO
             /* Let the asynchronous thread know that there is some new data to process. */
             hdaStreamAsyncIONotify(pThis, pStream);
 #else
@@ -6149,6 +6149,10 @@ static DECLCALLBACK(int) hdaConstruct(PPDMDEVINS pDevIns, int iInstance, PCFGMNO
         return rc;
 
     RTListInit(&pThis->lstDrv);
+
+#ifdef VBOX_WITH_AUDIO_HDA_ASYNC_IO
+    LogRel(("HDA: Asynchronous I/O enabled\n"));
+#endif
 
     uint8_t uLUN;
     for (uLUN = 0; uLUN < UINT8_MAX; ++uLUN)
