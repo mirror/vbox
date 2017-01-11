31e3acfdae3c31dd87666c9b2ab05881af59451a
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2017-01-11 13:00:02 +0000

    burn fix
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@65244 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/VMM/VMMR3/VMMGuruMeditation.cpp
+++ src/VBox/VMM/VMMR3/VMMGuruMeditation.cpp
@@ -476,7 +476,7 @@ VMMR3DECL(void) VMMR3FatalDump(PVM pVM, PVMCPU pVCpu, int rcErr)
 #ifdef VMM_R0_SWITCH_STACK
                     uint32_t const   iLast   = VMM_STACK_SIZE / sizeof(uintptr_t);
                     uint32_t         iAddr   = (uint32_t)(  pVCpu->vmm.s.CallRing3JmpBufR0.SavedEsp
-                                                          - MMHyperCCToR0(pVM, pVCpu->vmm.s.pbEMTStackR3) / sizeof(uintptr_t);
+                                                          - MMHyperCCToR0(pVM, pVCpu->vmm.s.pbEMTStackR3)) / sizeof(uintptr_t);
                     if (iAddr > iLast)
                         iAddr = 0;
 #else
