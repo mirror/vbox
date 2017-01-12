56f1460606a7c4b0da32da974b7e57dee5e5a651
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2017-01-12 10:20:08 +0000

    FE/Qt: bugref:6899: Accessibility support (step 162): Very basic accessibility interface for QIStatusBar.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@65253 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Frontends/VirtualBox/src/extensions/QIStatusBar.cpp
+++ src/VBox/Frontends/VirtualBox/src/extensions/QIStatusBar.cpp
@@ -18,14 +18,48 @@
 #ifdef VBOX_WITH_PRECOMPILED_HEADERS
 # include <precomp.h>
 #else
+/* Qt includes: */
+# include <QAccessibleWidget>
 /* GUI includes: */
 # include "QIStatusBar.h"
 #endif
 
 
+/** QAccessibleWidget extension used as an accessibility interface for QIStatusBar. */
+class QIAccessibilityInterfaceForQIStatusBar : public QAccessibleWidget
+{
+public:
+
+    /** Returns an accessibility interface for passed @a strClassname and @a pObject. */
+    static QAccessibleInterface *pFactory(const QString &strClassname, QObject *pObject)
+    {
+        /* Creating QIStatusBar accessibility interface: */
+        if (pObject && strClassname == QLatin1String("QIStatusBar"))
+            return new QIAccessibilityInterfaceForQIStatusBar(qobject_cast<QWidget*>(pObject));
+
+        /* Null by default: */
+        return 0;
+    }
+
+    /** Constructs an accessibility interface passing @a pWidget to the base-class. */
+    QIAccessibilityInterfaceForQIStatusBar(QWidget *pWidget)
+        : QAccessibleWidget(pWidget, QAccessible::ToolBar)
+    {
+        // We are not interested in status-bar text as it's a mean of
+        // accessibility in case when accessibility is disabled.
+        // Since accessibility is enabled in our case, we wish
+        // to pass control token to our sub-elements.
+        // So we are using QAccessible::ToolBar.
+    }
+};
+
+
 QIStatusBar::QIStatusBar(QWidget *pParent)
     : QStatusBar(pParent)
 {
+    /* Install QIStatusBar accessibility interface factory: */
+    QAccessible::installFactory(QIAccessibilityInterfaceForQIStatusBar::pFactory);
+
     /* Make sure we remember the last one status message: */
     connect(this, SIGNAL(messageChanged(const QString &)),
             this, SLOT(sltRememberLastMessage(const QString &)));
