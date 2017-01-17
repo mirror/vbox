e1727b4b1d0470bf24d770ea0eb5ff6df37cc3e3
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2017-01-17 15:54:47 +0000

    pylint
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@65351 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/ValidationKit/testmanager/webui/wuicontentbase.py
+++ src/VBox/ValidationKit/testmanager/webui/wuicontentbase.py
@@ -51,6 +51,7 @@ class WuiHtmlBase(object): # pylint: disable=R0903
         pass;
 
     def toHtml(self):
+
         """
         Must be overridden by sub-classes.
         """
@@ -736,7 +737,7 @@ class WuiListContentBase(WuiContentBase):
     """
 
     def __init__(self, aoEntries, iPage, cItemsPerPage, tsEffectiveDate, sTitle, sId = None, fnDPrint = None,
-                 oDisp = None, aiSelectedSortColumns = None):
+                 oDisp = None, aiSelectedSortColumns = None): # pylint: disable=too-many-arguments
         WuiContentBase.__init__(self, fnDPrint = fnDPrint, oDisp = oDisp);
         self._aoEntries         = aoEntries; ## @todo should replace this with a Logic object and define methods for querying.
         self._iPage             = iPage;
@@ -1036,7 +1037,7 @@ class WuiListContentWithActionBase(WuiListContentBase):
     """
 
     def __init__(self, aoEntries, iPage, cItemsPerPage, tsEffectiveDate, sTitle, sId = None, fnDPrint = None,
-                 oDisp = None, aiSelectedSortColumns = None):
+                 oDisp = None, aiSelectedSortColumns = None):   # pylint: disable=too-many-arguments
         WuiListContentBase.__init__(self, aoEntries, iPage, cItemsPerPage, tsEffectiveDate, sTitle, sId = sId,
                                     fnDPrint = fnDPrint, oDisp = oDisp, aiSelectedSortColumns = aiSelectedSortColumns);
         self._aoActions     = None; # List of [ oValue, sText, sHover ] provided by the child class.
