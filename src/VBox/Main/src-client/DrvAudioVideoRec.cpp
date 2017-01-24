418a6d63ae743d88b8de447f02ae257fe0981996
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2017-01-24 10:45:47 +0000

    VideoRec: Set container type.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@65416 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Main/src-client/DrvAudioVideoRec.cpp
+++ src/VBox/Main/src-client/DrvAudioVideoRec.cpp
@@ -291,6 +291,8 @@ static int avRecSinkInit(PDRVAUDIOVIDEOREC pThis, PAVRECSINK pSink, PAVRECCONTAI
 
     if (RT_SUCCESS(rc))
     {
+        pSink->Con.Parms.enmType     = pConParms->enmType;
+
         pSink->Codec.Parms.uHz       = uHz;
         pSink->Codec.Parms.cChannels = pCodecParms->cChannels;
         pSink->Codec.Parms.uBitrate  = pCodecParms->uBitrate;
