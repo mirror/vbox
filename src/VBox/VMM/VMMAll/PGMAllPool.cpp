381170872554d227e99dd60c6410ea035dcdc5f8
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2017-01-26 21:44:44 +0000

    pgmR3PoolReset: A few missing members (harmless).
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@65471 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/VMM/VMMAll/PGMAllPool.cpp
+++ src/VBox/VMM/VMMAll/PGMAllPool.cpp
@@ -5476,7 +5476,6 @@ void pgmR3PoolReset(PVM pVM)
         pPage->iModifiedPrev = NIL_PGMPOOL_IDX;
         pPage->iMonitoredNext = NIL_PGMPOOL_IDX;
         pPage->iMonitoredPrev = NIL_PGMPOOL_IDX;
-        pPage->cModifications = 0;
         pPage->GCPhys     = NIL_RTGCPHYS;
         pPage->enmKind    = PGMPOOLKIND_FREE;
         pPage->enmAccess  = PGMPOOLACCESS_DONTCARE;
@@ -5490,8 +5489,12 @@ void pgmR3PoolReset(PVM pVM)
         pPage->fCached    = false;
         pPage->fReusedFlushPending = false;
         pPage->iUserHead  = NIL_PGMPOOL_USER_INDEX;
+        pPage->cPresent = 0;
+        pPage->iFirstPresent = NIL_PGMPOOL_PRESENT_INDEX;
+        pPage->cModifications = 0;
         pPage->iAgeNext   = NIL_PGMPOOL_IDX;
         pPage->iAgePrev   = NIL_PGMPOOL_IDX;
+        pPage->idxDirtyEntry = 0;
         pPage->GCPtrLastAccessHandlerRip = NIL_RTGCPTR;
         pPage->GCPtrLastAccessHandlerFault = NIL_RTGCPTR;
         pPage->cLastAccessHandler = 0;
