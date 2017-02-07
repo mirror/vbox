82d62b7b66e8c4d1c717f63bf63ab3edb1670e0a
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2017-02-07 14:07:49 +0000

    gcc 7: Devices/Network: fall thru
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@65662 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Devices/Network/slirp/libalias/alias_ftp.c
+++ src/VBox/Devices/Network/slirp/libalias/alias_ftp.c
@@ -340,6 +340,7 @@ ParseFtpPortCommand(struct libalias *la, char *sptr, int dlen)
                 break;
             else
                 state++;
+            /* fall thru */
         case 1:
         case 3:
         case 5:
