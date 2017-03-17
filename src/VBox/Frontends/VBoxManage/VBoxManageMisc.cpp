0578eed36f7c6152a531e70f06bebce999d54eff
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2017-03-17 15:47:45 +0000

    VBoxManage: when installing the ExtPack, show the license and ask the user for confirmation (if required)
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@66157 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Frontends/VBoxManage/VBoxManageMisc.cpp
+++ src/VBox/Frontends/VBoxManage/VBoxManageMisc.cpp
@@ -1161,6 +1161,26 @@ RTEXITCODE handleExtPack(HandlerArg *a)
         ComPtr<IExtPackFile> ptrExtPackFile;
         CHECK_ERROR2I_RET(ptrExtPackMgr, OpenExtPackFile(bstrTarball.raw(), ptrExtPackFile.asOutParam()), RTEXITCODE_FAILURE);
         CHECK_ERROR2I_RET(ptrExtPackFile, COMGETTER(Name)(bstrName.asOutParam()), RTEXITCODE_FAILURE);
+        BOOL fShowLicense = true;
+        CHECK_ERROR2I_RET(ptrExtPackFile, COMGETTER(ShowLicense)(&fShowLicense), RTEXITCODE_FAILURE);
+        if (fShowLicense)
+        {
+            Bstr bstrLicense;
+            CHECK_ERROR2I_RET(ptrExtPackFile,
+                              QueryLicense(Bstr("").raw() /* PreferredLocale */,
+                                           Bstr("").raw() /* PreferredLanguage */,
+                                           Bstr("txt").raw() /* Format */,
+                                           bstrLicense.asOutParam()), RTEXITCODE_FAILURE);
+            RTPrintf("%ls\n", bstrLicense.raw());
+            RTPrintf("Do you agree to these license terms and conditions (y/n)? " );
+            ch = RTStrmGetCh(g_pStdIn);
+            RTPrintf("\n");
+            if (ch != 'y' && ch != 'Y')
+            {
+                RTPrintf("Installation of \"%ls\" aborted.\n", bstrName.raw());
+                return RTEXITCODE_SUCCESS;
+            }
+        }
         ComPtr<IProgress> ptrProgress;
         CHECK_ERROR2I_RET(ptrExtPackFile, Install(fReplace, NULL, ptrProgress.asOutParam()), RTEXITCODE_FAILURE);
         hrc = showProgress(ptrProgress);
