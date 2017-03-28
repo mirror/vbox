f2c489739bcad9bf8803df1a2e564959cc0475a8
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2017-03-28 12:48:56 +0000

    strformatrt.cpp: Reduced the ring-0 address code a little.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@66300 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Runtime/common/string/strformatrt.cpp
+++ src/VBox/Runtime/common/string/strformatrt.cpp
@@ -475,20 +475,16 @@ DECLHIDDEN(size_t) rtstrFormatRt(PFNRTSTROUTPUT pfnOutput, void *pvArgOutput, co
                 /*
                  * For now don't show the address.
                  */
-                cch = 0;
                 if (fFlags & RTSTR_F_OBFUSCATE_PTR)
                 {
-                    if (fFlags & RTSTR_F_SPECIAL)
-                        cch += pfnOutput(pvArgOutput, RT_STR_TUPLE("0x"));
-
-# if R0_ARCH_BITS == 32
-                    cch += pfnOutput(pvArgOutput, RT_STR_TUPLE("XXXXXXXX"));
-# elif R0_ARCH_BITS == 64
-                    cch += pfnOutput(pvArgOutput, RT_STR_TUPLE("XXXXXXXXXXXXXXXX"));
+# if R0_ARCH_BITS == 64
+                    static const char s_szObfuscated[] = "0xXXXXXXXX";
 # else
-#  error implement me!
+                    static const char s_szObfuscated[] = "0xXXXXXXXXXXXXXXXX";
 # endif
-                    return cch;
+                    if (fFlags & RTSTR_F_SPECIAL)
+                        return pfnOutput(pvArgOutput, s_szObfuscated, sizeof(s_szObfuscated) - 1);
+                    return pfnOutput(pvArgOutput, &s_szObfuscated[2], sizeof(s_szObfuscated) - 1 - 2);
                 }
 #endif
 
