52093dfded411c8225c39e70dd68a7ce2648c223
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2017-03-28 10:37:16 +0000

    VMMSwitcher: RKv
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@66288 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/VMM/VMMR3/VMMSwitcher.cpp
+++ src/VBox/VMM/VMMR3/VMMSwitcher.cpp
@@ -286,7 +286,7 @@ int vmmR3SwitcherInit(PVM pVM)
             }
             while (i-- > 0)
             {
-                LogRel(("VMM: Core code alloc attempt #%d: pvR3=%p pvR0=%p HCPhys=%RHp\n",
+                LogRel(("VMM: Core code alloc attempt #%d: pvR3=%p pvR0=%RKv HCPhys=%RHp\n",
                         i, paBadTries[i].pvR3, paBadTries[i].pvR0, paBadTries[i].HCPhys));
                 SUPR3ContFree(paBadTries[i].pvR3, paBadTries[i].cb >> PAGE_SHIFT);
             }
@@ -324,7 +324,7 @@ int vmmR3SwitcherInit(PVM pVM)
         {
             pVM->vmm.s.pvCoreCodeRC = GCPtr;
             MMR3HyperReserve(pVM, PAGE_SIZE, "fence", NULL);
-            LogRel(("VMM: CoreCode: R3=%RHv R0=%RHv RC=%RRv Phys=%RHp cb=%#x\n",
+            LogRel(("VMM: CoreCode: R3=%RHv R0=%RKv RC=%RRv Phys=%RHp cb=%#x\n",
                     pVM->vmm.s.pvCoreCodeR3, pVM->vmm.s.pvCoreCodeR0, pVM->vmm.s.pvCoreCodeRC, pVM->vmm.s.HCPhysCoreCode, pVM->vmm.s.cbCoreCode));
 
             /*
