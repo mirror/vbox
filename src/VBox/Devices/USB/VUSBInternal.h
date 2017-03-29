4d303a8e229c01989361e8da2935e3a823eff62f
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2017-03-29 19:46:09 +0000

    EHCI: Improved transfer type determination heuristic.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@66347 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Devices/USB/VUSBInternal.h
+++ src/VBox/Devices/USB/VUSBInternal.h
@@ -157,6 +157,8 @@ typedef struct vusb_pipe
     RTCRITSECT          CritSectCtrl;
     /** Count of active async transfers. */
     volatile uint32_t   async;
+    /** Last scheduled frame - only valid for isochronous endpoints. */
+    uint32_t            uLastFrame;
 } VUSBPIPE;
 /** Pointer to a VUSB pipe structure. */
 typedef VUSBPIPE *PVUSBPIPE;
