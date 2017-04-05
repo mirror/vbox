865084f70cecbffd8239301acd71c451fc8ed6eb
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2017-04-05 12:02:41 +0000

    vsheriff: should be placed here but still does not work :-/
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@66430 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/ValidationKit/testmanager/batch/virtual_test_sheriff.py
+++ src/VBox/ValidationKit/testmanager/batch/virtual_test_sheriff.py
@@ -651,7 +651,9 @@ class VirtualTestSheriff(object): # pylint: disable=R0903
     katSimpleInstallUninstallMainLogReasons = [
         # ( Whether to stop on hit, reason tuple, needle text. )
         ( False, ktReason_Host_LeftoverService,
-		'SERVICE_NAME: vbox' ),
+          'SERVICE_NAME: vbox' ),
+        ( True,  ktReason_Host_Modprobe_Failed, 
+          'modprobe vboxdrv failed' )
     ];
 
     kdatSimpleInstallUninstallMainLogReasonsPerOs = {
@@ -837,7 +839,6 @@ class VirtualTestSheriff(object): # pylint: disable=R0903
         ( True,  ktReason_Unknown_File_Not_Found, # lump it in with file-not-found for now.
           'Error: failed to start machine. Error message: Not supported. (VERR_NOT_SUPPORTED)' ),
         ( False, ktReason_Unknown_VM_Crash,                         'txsDoConnectViaTcp: Machine state: Aborted' ),
-        ( True,  ktReason_Host_Modprobe_Failed,                     'modprobe vboxdrv failed' )
     ];
 
     ## Things we search a VBoxHardening.log file for to figure out why something went bust.
