d569db9313b74799153f7347bc57284566185679
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2017-04-27 14:59:05 +0000

    bugref:8524: Additions/linux: play nicely with distribution-installed Additions Split up HGSMIBase.cpp into two files: HGSMIGuest.cpp which contains code needed for guest-to-host communication and HGSMIHost.cpp which contains code for host-to-guest communication.  Currently only the Windows Additions use the second.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@66694 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Additions/common/VBoxVideo/HGSMIGuest.cpp
+++ src/VBox/Additions/common/VBoxVideo/HGSMIGuest.cpp
@@ -1,7 +1,6 @@
 /* $Id$ */
 /** @file
- * VirtualBox Video driver, common code - HGSMI initialisation and helper
- * functions.
+ * VirtualBox Video driver, common code - HGSMI guest-to-host communication.
  */
 
 /*
@@ -29,105 +28,6 @@
 #include <VBoxVideoGuest.h>
 #include <VBoxVideoVBE.h>
 #include <VBoxVideoIPRT.h>
-
-/**
- * Initialise the host context structure.
- *
- * @param  pCtx               the context structure to initialise
- * @param  pvBaseMapping      where the basic HGSMI structures are mapped at
- * @param  offHostFlags       the offset of the host flags into the basic HGSMI
- *                            structures
- * @param  pvHostAreaMapping  where the area for the host heap is mapped at
- * @param  offVRAMHostArea    offset of the host heap area into VRAM
- * @param  cbHostArea         size in bytes of the host heap area
- */
-DECLHIDDEN(void) VBoxHGSMISetupHostContext(PHGSMIHOSTCOMMANDCONTEXT pCtx,
-                                           void *pvBaseMapping,
-                                           uint32_t offHostFlags,
-                                           void *pvHostAreaMapping,
-                                           uint32_t offVRAMHostArea,
-                                           uint32_t cbHostArea)
-{
-    uint8_t *pu8HostFlags = ((uint8_t *)pvBaseMapping) + offHostFlags;
-    pCtx->pfHostFlags = (HGSMIHOSTFLAGS *)pu8HostFlags;
-    /** @todo should we really be using a fixed ISA port value here? */
-    pCtx->port        = (RTIOPORT)VGA_PORT_HGSMI_HOST;
-    HGSMIAreaInitialize(&pCtx->areaCtx, pvHostAreaMapping, cbHostArea,
-                         offVRAMHostArea);
-}
-
-
-/** Send completion notification to the host for the command located at offset
- * @a offt into the host command buffer. */
-static void HGSMINotifyHostCmdComplete(PHGSMIHOSTCOMMANDCONTEXT pCtx, HGSMIOFFSET offt)
-{
-    VBVO_PORT_WRITE_U32(pCtx->port, offt);
-}
-
-
-/**
- * Inform the host that a command has been handled.
- *
- * @param  pCtx   the context containing the heap to be used
- * @param  pvMem  pointer into the heap as mapped in @a pCtx to the command to
- *                be completed
- */
-DECLHIDDEN(void) VBoxHGSMIHostCmdComplete(PHGSMIHOSTCOMMANDCONTEXT pCtx,
-                                          void *pvMem)
-{
-    HGSMIBUFFERHEADER *pHdr = HGSMIBufferHeaderFromData(pvMem);
-    HGSMIOFFSET offMem = HGSMIPointerToOffset(&pCtx->areaCtx, pHdr);
-    Assert(offMem != HGSMIOFFSET_VOID);
-    if(offMem != HGSMIOFFSET_VOID)
-    {
-        HGSMINotifyHostCmdComplete(pCtx, offMem);
-    }
-}
-
-
-/** Submit an incoming host command to the appropriate handler. */
-static void hgsmiHostCmdProcess(PHGSMIHOSTCOMMANDCONTEXT pCtx,
-                                HGSMIOFFSET offBuffer)
-{
-    int rc = HGSMIBufferProcess(&pCtx->areaCtx, &pCtx->channels, offBuffer);
-    Assert(!RT_FAILURE(rc));
-    if(RT_FAILURE(rc))
-    {
-        /* failure means the command was not submitted to the handler for some reason
-         * it's our responsibility to notify its completion in this case */
-        HGSMINotifyHostCmdComplete(pCtx, offBuffer);
-    }
-    /* if the cmd succeeded it's responsibility of the callback to complete it */
-}
-
-/** Get the next command from the host. */
-static HGSMIOFFSET hgsmiGetHostBuffer(PHGSMIHOSTCOMMANDCONTEXT pCtx)
-{
-    return VBVO_PORT_READ_U32(pCtx->port);
-}
-
-
-/** Get and handle the next command from the host. */
-static void hgsmiHostCommandQueryProcess(PHGSMIHOSTCOMMANDCONTEXT pCtx)
-{
-    HGSMIOFFSET offset = hgsmiGetHostBuffer(pCtx);
-    AssertReturnVoid(offset != HGSMIOFFSET_VOID);
-    hgsmiHostCmdProcess(pCtx, offset);
-}
-
-
-/** Drain the host command queue. */
-DECLHIDDEN(void) VBoxHGSMIProcessHostQueue(PHGSMIHOSTCOMMANDCONTEXT pCtx)
-{
-    while (pCtx->pfHostFlags->u32HostFlags & HGSMIHOSTFLAGS_COMMANDS_PENDING)
-    {
-        if (!ASMAtomicCmpXchgBool(&pCtx->fHostCmdProcessing, true, false))
-            return;
-        hgsmiHostCommandQueryProcess(pCtx);
-        ASMAtomicWriteBool(&pCtx->fHostCmdProcessing, false);
-    }
-}
-
 
 /**
  * Set up the HGSMI guest-to-host command context.
@@ -238,10 +138,18 @@ DECLHIDDEN(bool) VBoxHGSMIIsSupported(void)
 }
 
 
-/** Inform the host of the location of the host flags in VRAM via an HGSMI
- * command. */
-static int vboxHGSMIReportFlagsLocation(PHGSMIGUESTCOMMANDCONTEXT pCtx,
-                                        HGSMIOFFSET offLocation)
+/**
+ * Inform the host of the location of the host flags in VRAM via an HGSMI
+ * command.
+ * @returns  IPRT status value.
+ * @returns  VERR_NOT_IMPLEMENTED  if the host does not support the command.
+ * @returns  VERR_NO_MEMORY        if a heap allocation fails.
+ * @param    pCtx                  the context of the guest heap to use.
+ * @param    offLocation           the offset chosen for the flags withing guest
+ *                                 VRAM.
+ */
+DECLHIDDEN(int) VBoxHGSMIReportFlagsLocation(PHGSMIGUESTCOMMANDCONTEXT pCtx,
+                                             HGSMIOFFSET offLocation)
 {
     HGSMIBUFFERLOCATION *p;
     int rc = VINF_SUCCESS;
@@ -267,26 +175,15 @@ static int vboxHGSMIReportFlagsLocation(PHGSMIGUESTCOMMANDCONTEXT pCtx,
 
 
 /**
- * Inform the host of the location of the host flags in VRAM via an HGSMI
- * command.
+ * Notify the host of HGSMI-related guest capabilities via an HGSMI command.
  * @returns  IPRT status value.
  * @returns  VERR_NOT_IMPLEMENTED  if the host does not support the command.
  * @returns  VERR_NO_MEMORY        if a heap allocation fails.
  * @param    pCtx                  the context of the guest heap to use.
- * @param    offLocation           the offset chosen for the flags withing guest
- *                                 VRAM.
+ * @param    fCaps                 the capabilities to report, see VBVACAPS.
  */
-DECLHIDDEN(int) VBoxHGSMIReportFlagsLocation(PHGSMIGUESTCOMMANDCONTEXT pCtx,
-                                             HGSMIOFFSET offLocation)
-{
-    return vboxHGSMIReportFlagsLocation(pCtx, offLocation);
-}
-
-
-/** Notify the host of HGSMI-related guest capabilities via an HGSMI command.
- */
-static int vboxHGSMISendCapsInfo(PHGSMIGUESTCOMMANDCONTEXT pCtx,
-                                 uint32_t fCaps)
+DECLHIDDEN(int) VBoxHGSMISendCapsInfo(PHGSMIGUESTCOMMANDCONTEXT pCtx,
+                                      uint32_t fCaps)
 {
     VBVACAPS *pCaps;
     int rc = VINF_SUCCESS;
@@ -309,48 +206,6 @@ static int vboxHGSMISendCapsInfo(PHGSMIGUESTCOMMANDCONTEXT pCtx,
         }
         /* Free the IO buffer. */
         VBoxHGSMIBufferFree(pCtx, pCaps);
-    }
-    else
-        rc = VERR_NO_MEMORY;
-    return rc;
-}
-
-
-/**
- * Notify the host of HGSMI-related guest capabilities via an HGSMI command.
- * @returns  IPRT status value.
- * @returns  VERR_NOT_IMPLEMENTED  if the host does not support the command.
- * @returns  VERR_NO_MEMORY        if a heap allocation fails.
- * @param    pCtx                  the context of the guest heap to use.
- * @param    fCaps                 the capabilities to report, see VBVACAPS.
- */
-DECLHIDDEN(int) VBoxHGSMISendCapsInfo(PHGSMIGUESTCOMMANDCONTEXT pCtx,
-                                      uint32_t fCaps)
-{
-    return vboxHGSMISendCapsInfo(pCtx, fCaps);
-}
-
-
-/** Tell the host about the location of the area of VRAM set aside for the host
- * heap. */
-static int vboxHGSMIReportHostArea(PHGSMIGUESTCOMMANDCONTEXT pCtx,
-                                   uint32_t u32AreaOffset, uint32_t u32AreaSize)
-{
-    VBVAINFOHEAP *p;
-    int rc = VINF_SUCCESS;
-
-    /* Allocate the IO buffer. */
-    p = (VBVAINFOHEAP *)VBoxHGSMIBufferAlloc(pCtx,
-                                       sizeof (VBVAINFOHEAP), HGSMI_CH_VBVA,
-                                       VBVA_INFO_HEAP);
-    if (p)
-    {
-        /* Prepare data to be sent to the host. */
-        p->u32HeapOffset = u32AreaOffset;
-        p->u32HeapSize   = u32AreaSize;
-        rc = VBoxHGSMIBufferSubmit(pCtx, p);
-        /* Free the IO buffer. */
-        VBoxHGSMIBufferFree(pCtx, p);
     }
     else
         rc = VERR_NO_MEMORY;
@@ -397,95 +252,6 @@ DECLHIDDEN(void) VBoxHGSMIGetBaseMappingInfo(uint32_t cbVRAM,
     if (poffHostFlags)
         *poffHostFlags =   VBVA_ADAPTER_INFORMATION_SIZE
                          - sizeof(HGSMIHOSTFLAGS);
-}
-
-
-/**
- * Get the information needed to map the area used by the host to send back
- * requests.
- *
- * @param  pCtx                the context containing the heap to use
- * @param  cbVRAM              how much video RAM is allocated to the device
- * @param  offVRAMBaseMapping  the offset of the basic communication structures
- *                             into the guest's VRAM
- * @param  poffVRAMHostArea    where to store the offset into VRAM of the host
- *                             heap area
- * @param  pcbHostArea         where to store the size of the host heap area
- */
-DECLHIDDEN(void) VBoxHGSMIGetHostAreaMapping(PHGSMIGUESTCOMMANDCONTEXT pCtx,
-                                             uint32_t cbVRAM,
-                                             uint32_t offVRAMBaseMapping,
-                                             uint32_t *poffVRAMHostArea,
-                                             uint32_t *pcbHostArea)
-{
-    uint32_t offVRAMHostArea = offVRAMBaseMapping, cbHostArea = 0;
-
-    AssertPtrReturnVoid(poffVRAMHostArea);
-    AssertPtrReturnVoid(pcbHostArea);
-    VBoxQueryConfHGSMI(pCtx, VBOX_VBVA_CONF32_HOST_HEAP_SIZE, &cbHostArea);
-    if (cbHostArea != 0)
-    {
-        uint32_t cbHostAreaMaxSize = cbVRAM / 4;
-        /** @todo what is the idea of this? */
-        if (cbHostAreaMaxSize >= VBVA_ADAPTER_INFORMATION_SIZE)
-        {
-            cbHostAreaMaxSize -= VBVA_ADAPTER_INFORMATION_SIZE;
-        }
-        if (cbHostArea > cbHostAreaMaxSize)
-        {
-            cbHostArea = cbHostAreaMaxSize;
-        }
-        /* Round up to 4096 bytes. */
-        cbHostArea = (cbHostArea + 0xFFF) & ~0xFFF;
-        offVRAMHostArea = offVRAMBaseMapping - cbHostArea;
-    }
-
-    *pcbHostArea = cbHostArea;
-    *poffVRAMHostArea = offVRAMHostArea;
-    // LogFunc(("offVRAMHostArea = 0x%08X, cbHostArea = 0x%08X\n",
-    //          offVRAMHostArea, cbHostArea));
-}
-
-
-/**
- * Tell the host about the ways it can use to communicate back to us via an
- * HGSMI command
- *
- * @returns  iprt status value
- * @param  pCtx                  the context containing the heap to use
- * @param  offVRAMFlagsLocation  where we wish the host to place its flags
- *                               relative to the start of the VRAM
- * @param  fCaps                 additions HGSMI capabilities the guest
- *                               supports
- * @param  offVRAMHostArea       offset into VRAM of the host heap area
- * @param  cbHostArea            size in bytes of the host heap area
- */
-DECLHIDDEN(int) VBoxHGSMISendHostCtxInfo(PHGSMIGUESTCOMMANDCONTEXT pCtx,
-                                         HGSMIOFFSET offVRAMFlagsLocation,
-                                         uint32_t fCaps,
-                                         uint32_t offVRAMHostArea,
-                                         uint32_t cbHostArea)
-{
-    // Log(("VBoxVideo::vboxSetupAdapterInfo\n"));
-
-    /* setup the flags first to ensure they are initialized by the time the
-     * host heap is ready */
-    int rc = vboxHGSMIReportFlagsLocation(pCtx, offVRAMFlagsLocation);
-    AssertRC(rc);
-    if (RT_SUCCESS(rc) && fCaps)
-    {
-        /* Inform about caps */
-        rc = vboxHGSMISendCapsInfo(pCtx, fCaps);
-        AssertRC(rc);
-    }
-    if (RT_SUCCESS (rc))
-    {
-        /* Report the host heap location. */
-        rc = vboxHGSMIReportHostArea(pCtx, offVRAMHostArea, cbHostArea);
-        AssertRC(rc);
-    }
-    // Log(("VBoxVideo::vboxSetupAdapterInfo finished rc = %d\n", rc));
-    return rc;
 }
 
 
