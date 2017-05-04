37383f9c0f2fb69d3534af4164a7106dd24f6e9a
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2017-05-04 08:56:02 +0000

    testboxscript: storage server migration step 1
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@66773 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/ValidationKit/testboxscript/testboxscript_real.py
+++ src/VBox/ValidationKit/testboxscript/testboxscript_real.py
@@ -932,7 +932,8 @@ class TestBoxScript(object):
                               dest=sPrefix + 'ServerType',   metavar='<nfs|cifs>', default=None,
                               help='The type of server, cifs or nfs. If empty (default), we won\'t try mount anything.');
             parser.add_option('--' + sLower + '-server-name',
-                              dest=sPrefix + 'ServerName',   metavar='<server>',   default='solserv.de.oracle.com',
+                              dest=sPrefix + 'ServerName',   metavar='<server>',
+                              default='solserv.de.oracle.com' if sLower == 'builds' else 'teststor.de.oracle.com',
                               help='The name of the server with the builds.');
             parser.add_option('--' + sLower + '-server-share',
                               dest=sPrefix + 'ServerShare',  metavar='<share>',    default=sLower,
