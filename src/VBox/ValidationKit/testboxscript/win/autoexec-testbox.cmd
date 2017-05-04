39c280a41186b9d8c5b2e18cd3a3c06657db2666
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2017-05-04 16:13:23 +0000

    testboxscript: very useful improvement I found on the testboxes
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@66797 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/ValidationKit/testboxscript/win/autoexec-testbox.cmd
+++ src/VBox/ValidationKit/testboxscript/win/autoexec-testbox.cmd
@@ -1,5 +1,8 @@
 @echo "$Id$"
 @echo on
+setlocal EnableExtensions
+set exe=python.exe
+for /f %%x in ('tasklist /NH /FI "IMAGENAME eq %exe%"') do if %%x == %exe% goto end
 %SystemDrive%\Python27\python.exe %SystemDrive%\testboxscript\testboxscript\testboxscript.py --testrsrc-server-type=cifs --builds-server-type=cifs
 pause
-
+:end
