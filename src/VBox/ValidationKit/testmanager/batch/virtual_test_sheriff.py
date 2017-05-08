f3e66d7d7dc6d0d5f96f250f09fd0333bd8fb2a6
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2017-05-08 12:37:47 +0000

    vsheriff: teach about BSOD / DRIVER_IRQL_NOT_LESS_OR_EQUAL
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@66817 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/ValidationKit/testmanager/batch/virtual_test_sheriff.py
+++ src/VBox/ValidationKit/testmanager/batch/virtual_test_sheriff.py
@@ -453,6 +453,7 @@ class VirtualTestSheriff(object): # pylint: disable=R0903
     ## @{
     ktReason_BSOD_Recovery                             = ( 'BSOD',              'Recovery' );
     ktReason_BSOD_Automatic_Repair                     = ( 'BSOD',              'Automatic Repair' );
+    ktReason_BSOD_000000D1                             = ( 'BSOD',              '0x000000D1' );
     ktReason_BSOD_C0000225                             = ( 'BSOD',              '0xC0000225 (boot)' );
     ktReason_Guru_Generic                              = ( 'Guru Meditations',  'Generic Guru Meditation' );
     ktReason_Guru_VERR_IEM_INSTR_NOT_IMPLEMENTED       = ( 'Guru Meditations',  'VERR_IEM_INSTR_NOT_IMPLEMENTED' );
@@ -889,6 +890,8 @@ class VirtualTestSheriff(object): # pylint: disable=R0903
         ( True,  ktReason_BSOD_Recovery,                    '576f8e38d62b311cac7e3dc3436a0d0b9bd8cfd7fa9c43aafa95631520a45eac' ),
         ( True,  ktReason_BSOD_Automatic_Repair,            'c6a72076cc619937a7a39cfe9915b36d94cee0d4e3ce5ce061485792dcee2749' ),
         ( True,  ktReason_BSOD_Automatic_Repair,            '26c4d8a724ff2c5e1051f3d5b650dbda7b5fdee0aa3e3c6059797f7484a515df' ),
+        ( True,  ktReason_BSOD_000000D1,                    '134621281f00a3f8aeeb7660064bffbf6187ed56d5852142328d0bcb18ef0ede' ),
+        ( True,  ktReason_BSOD_000000D1,                    '279f11258150c9d2fef041eca65501f3141da8df39256d8f6377e897e3b45a93' ),
         ( True,  ktReason_BSOD_C0000225,                    'bd13a144be9dcdfb16bc863ff4c8f02a86e263c174f2cd5ffd27ca5f3aa31789' ),
         ( True,  ktReason_BSOD_C0000225,                    '8348b465e7ee9e59dd4e785880c57fd8677de05d11ac21e786bfde935307b42f' ),
         ( True,  ktReason_BSOD_C0000225,                    '1316e1fc818a73348412788e6910b8c016f237d8b4e15b20caf4a866f7a7840e' ),
