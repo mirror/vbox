4708c55ba4609c8738efe6970b3ea558f2673446
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2017-05-08 17:01:34 +0000

    state_snapshot.c: build fix
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@66835 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/GuestHost/OpenGL/state_tracker/state_snapshot.c
+++ src/VBox/GuestHost/OpenGL/state_tracker/state_snapshot.c
@@ -1,5 +1,4 @@
 /* $Id$ */
-
 /** @file
  * VBox Context state saving/loading used by VM snapshot
  */
@@ -29,8 +28,9 @@
 #include <iprt/types.h>
 #include <iprt/err.h>
 #include <VBox/err.h>
+#include <VBox/log.h>
 
-/* @todo
+/** @todo
  * We have two ways of saving/loading states.
  *
  * First which is being used atm, just pure saving/loading of structures.
