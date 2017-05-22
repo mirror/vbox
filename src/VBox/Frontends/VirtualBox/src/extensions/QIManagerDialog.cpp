2c5b600679092db04f08adafc54b26b9e44a81ec
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2017-05-22 14:44:36 +0000

    FE/Qt: Extensions: QIManagerDialog, a new QMainWindow extension to be used with various managers like Virtual Media and Host Network one.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@67017 cfe28804-0f27-0410-a406-dd0f0b0b656f

---
+++ src/VBox/Frontends/VirtualBox/src/extensions/QIManagerDialog.cpp
@@ -0,0 +1,193 @@
+/* $Id$ */
+/** @file
+ * VBox Qt GUI - QIManagerDialog class implementation.
+ */
+
+/*
+ * Copyright (C) 2009-2017 Oracle Corporation
+ *
+ * This file is part of VirtualBox Open Source Edition (OSE), as
+ * available from http://www.virtualbox.org. This file is free software;
+ * you can redistribute it and/or modify it under the terms of the GNU
+ * General Public License (GPL) as published by the Free Software
+ * Foundation, in version 2 as it comes in the "COPYING" file of the
+ * VirtualBox OSE distribution. VirtualBox OSE is distributed in the
+ * hope that it will be useful, but WITHOUT ANY WARRANTY of any kind.
+ */
+
+#ifdef VBOX_WITH_PRECOMPILED_HEADERS
+# include <precomp.h>
+#else  /* !VBOX_WITH_PRECOMPILED_HEADERS */
+
+/* Qt includes: */
+# include <QMenuBar>
+# include <QPushButton>
+
+/* GUI includes: */
+# include "QIDialogButtonBox.h"
+# include "QIManagerDialog.h"
+# include "UIDesktopWidgetWatchdog.h"
+# ifdef VBOX_WS_MAC
+#  include "UIToolBar.h"
+#  include "UIWindowMenuManager.h"
+# endif /* VBOX_WS_MAC */
+# include "UIMessageCenter.h"
+# include "VBoxGlobal.h"
+
+#endif /* !VBOX_WITH_PRECOMPILED_HEADERS */
+
+
+/*********************************************************************************************************************************
+*   Class QIManagerDialogFactory implementation.                                                                                 *
+*********************************************************************************************************************************/
+
+void QIManagerDialogFactory::prepare(QIManagerDialog *&pDialog, QWidget *pCenterWidget /* = 0 */)
+{
+    create(pDialog, pCenterWidget);
+    pDialog->prepare();
+}
+
+void QIManagerDialogFactory::cleanup(QIManagerDialog *&pDialog)
+{
+    pDialog->cleanup();
+    delete pDialog;
+    pDialog = 0;
+}
+
+
+/*********************************************************************************************************************************
+*   Class QIManagerDialog implementation.                                                                                        *
+*********************************************************************************************************************************/
+
+QIManagerDialog::QIManagerDialog(QWidget *pCenterWidget)
+    : pCenterWidget(pCenterWidget)
+    , m_pWidget(0)
+    , m_pWidgetMenu(0)
+#ifdef VBOX_WS_MAC
+    , m_pWidgetToolbar(0)
+#endif
+    , m_pButtonBox(0)
+{
+}
+
+void QIManagerDialog::closeEvent(QCloseEvent *pEvent)
+{
+    /* Ignore the event itself: */
+    pEvent->ignore();
+    /* But tell the listener to close us: */
+    emit sigClose();
+}
+
+void QIManagerDialog::prepare()
+{
+    /* Tell the application we are not that important: */
+    setAttribute(Qt::WA_QuitOnClose, false);
+
+    /* Invent initial size: */
+    QSize proposedSize;
+    const int iHostScreen = gpDesktop->screenNumber(pCenterWidget);
+    if (iHostScreen >= 0 && iHostScreen < gpDesktop->screenCount())
+    {
+        /* On the basis of current host-screen geometry if possible: */
+        const QRect screenGeometry = gpDesktop->screenGeometry(iHostScreen);
+        if (screenGeometry.isValid())
+            proposedSize = screenGeometry.size() * 2 / 5;
+    }
+    /* Fallback to default size if we failed: */
+    if (proposedSize.isNull())
+        proposedSize = QSize(800, 600);
+    /* Resize to initial size: */
+    resize(proposedSize);
+
+    /* Prepare dialog: */
+    prepareDialog();
+    /* Prepare central-widget: */
+    prepareCentralWidget();
+    /* Prepare menu-bar: */
+    prepareMenuBar();
+#ifdef VBOX_WS_MAC
+    /* Prepare toolbar: */
+    prepareToolBar();
+#endif
+
+    /* Center according requested widget: */
+    VBoxGlobal::centerWidget(this, pCenterWidget, false);
+}
+
+void QIManagerDialog::prepareCentralWidget()
+{
+    /* Create central-widget: */
+    setCentralWidget(new QWidget);
+    AssertPtrReturnVoid(centralWidget());
+    {
+        /* Create main-layout: */
+        new QVBoxLayout(centralWidget());
+        AssertPtrReturnVoid(centralWidget()->layout());
+        {
+            /* Configure layout: */
+            centralWidget()->layout()->setContentsMargins(5, 5, 5, 5);
+            centralWidget()->layout()->setSpacing(10);
+
+            /* Prepare widget: */
+            prepareWidget();
+            /* Prepare button-box: */
+            prepareButtonBox();
+        }
+    }
+}
+
+void QIManagerDialog::prepareButtonBox()
+{
+    /* Create button-box: */
+    m_pButtonBox = new QIDialogButtonBox;
+    AssertPtrReturnVoid(m_pButtonBox);
+    {
+        /* Configure button-box: */
+        m_pButtonBox->setStandardButtons(QDialogButtonBox::Help | QDialogButtonBox::Close);
+        m_pButtonBox->button(QDialogButtonBox::Close)->setShortcut(Qt::Key_Escape);
+        connect(m_pButtonBox, SIGNAL(helpRequested()), &msgCenter(), SLOT(sltShowHelpHelpDialog()));
+        connect(m_pButtonBox, &QIDialogButtonBox::rejected, this, &QIManagerDialog::sigClose);
+
+        /* Add into layout: */
+        centralWidget()->layout()->addWidget(m_pButtonBox);
+    }
+}
+
+void QIManagerDialog::prepareMenuBar()
+{
+    /* Add widget menu: */
+    menuBar()->addMenu(m_pWidgetMenu);
+
+#ifdef VBOX_WS_MAC
+    /* Prepare 'Window' menu: */
+    AssertPtrReturnVoid(gpWindowMenuManager);
+    menuBar()->addMenu(gpWindowMenuManager->createMenu(this));
+    gpWindowMenuManager->addWindow(this);
+#endif
+}
+
+#ifdef VBOX_WS_MAC
+void QIManagerDialog::prepareToolBar()
+{
+    /* Enable unified toolbar on macOS: */
+    addToolBar(m_pWidgetToolbar);
+    m_pWidgetToolbar->enableMacToolbar();
+}
+#endif
+
+void QIManagerDialog::cleanupMenuBar()
+{
+#ifdef VBOX_WS_MAC
+    /* Cleanup 'Window' menu: */
+    AssertPtrReturnVoid(gpWindowMenuManager);
+    gpWindowMenuManager->removeWindow(this);
+    gpWindowMenuManager->destroyMenu(this);
+#endif
+}
+
+void QIManagerDialog::cleanup()
+{
+    /* Cleanup menu-bar: */
+    cleanupMenuBar();
+}
+
---
+++ src/VBox/Frontends/VirtualBox/src/extensions/QIManagerDialog.h
@@ -0,0 +1,151 @@
+/* $Id$ */
+/** @file
+ * VBox Qt GUI - QIManagerDialog class declaration.
+ */
+
+/*
+ * Copyright (C) 2009-2017 Oracle Corporation
+ *
+ * This file is part of VirtualBox Open Source Edition (OSE), as
+ * available from http://www.virtualbox.org. This file is free software;
+ * you can redistribute it and/or modify it under the terms of the GNU
+ * General Public License (GPL) as published by the Free Software
+ * Foundation, in version 2 as it comes in the "COPYING" file of the
+ * VirtualBox OSE distribution. VirtualBox OSE is distributed in the
+ * hope that it will be useful, but WITHOUT ANY WARRANTY of any kind.
+ */
+
+#ifndef ___QIManagerDialog_h___
+#define ___QIManagerDialog_h___
+
+/* Qt includes: */
+#include <QMainWindow>
+
+/* Forward declarations: */
+class QIDialogButtonBox;
+class QIManagerDialog;
+#ifdef VBOX_WS_MAC
+class UIToolBar;
+#endif
+
+
+/** Manager dialog factory insterface. */
+class QIManagerDialogFactory
+{
+public:
+
+    /** Constructs Manager dialog factory. */
+    QIManagerDialogFactory() {}
+    /** Destructs Manager dialog factory. */
+    virtual ~QIManagerDialogFactory() {}
+
+    /** Prepares Manager dialog @a pDialog instance.
+      * @param  pCenterWidget  Brings the widget reference to center according to. */
+    virtual void prepare(QIManagerDialog *&pDialog, QWidget *pCenterWidget = 0);
+    /** Cleanups Manager dialog @a pDialog instance. */
+    virtual void cleanup(QIManagerDialog *&pDialog);
+
+protected:
+
+    /** Creates derived @a pDialog instance.
+      * @param  pCenterWidget  Brings the widget reference to center according to. */
+    virtual void create(QIManagerDialog *&pDialog, QWidget *pCenterWidget) = 0;
+};
+
+
+/** QMainWindow sub-class used as various manager dialogs. */
+class QIManagerDialog : public QMainWindow
+{
+    Q_OBJECT;
+
+signals:
+
+    /** Notifies listener about dialog should be closed. */
+    void sigClose();
+
+protected:
+
+    /** Constructs Manager dialog.
+      * @param  pCenterWidget  Brings the widget reference to center according to. */
+    QIManagerDialog(QWidget *pCenterWidget);
+
+    /** @name Prepare/cleanup cascade.
+      * @{ */
+        /** Prepares dialog. */
+        virtual void prepareDialog() {}
+        /** Prepares widget. */
+        virtual void prepareWidget() {}
+    /** @} */
+
+    /** @name Widget stuff.
+      * @{ */
+        /** Defines the @a pWidget instance. */
+        void setWidget(QWidget *pWidget) { m_pWidget = pWidget; }
+        /** Defines the @a pWidgetMenu instance. */
+        void setWidgetMenu(QMenu *pWidgetMenu) { m_pWidgetMenu = pWidgetMenu; }
+#ifdef VBOX_WS_MAC
+        /** Defines the @a pWidgetToolbar instance. */
+        void setWidgetToolbar(UIToolBar *pWidgetToolbar) { m_pWidgetToolbar = pWidgetToolbar; }
+#endif
+    /** @} */
+
+    /** @name Event-handling stuff.
+      * @{ */
+        /** Handles close @a pEvent. */
+        void closeEvent(QCloseEvent *pEvent);
+    /** @} */
+
+private:
+
+    /** @name Prepare/cleanup cascade.
+      * @{ */
+        /** Prepares all. */
+        void prepare();
+        /** Prepares central-widget. */
+        void prepareCentralWidget();
+        /** Prepares button-box. */
+        void prepareButtonBox();
+        /** Prepares menu-bar. */
+        void prepareMenuBar();
+#ifdef VBOX_WS_MAC
+        /** Prepares toolbar. */
+        void prepareToolBar();
+#endif
+
+        /** Cleanup menu-bar. */
+        void cleanupMenuBar();
+        /** Cleanups all. */
+        void cleanup();
+    /** @} */
+
+    /** @name General stuff.
+      * @{ */
+        /** Holds the widget reference to center Host Network Manager according. */
+        QWidget *pCenterWidget;
+    /** @} */
+
+    /** @name Widget stuff.
+      * @{ */
+        /** Holds the widget instance. */
+        QWidget *m_pWidget;
+
+        /** Holds the widget menu instance. */
+        QMenu     *m_pWidgetMenu;
+#ifdef VBOX_WS_MAC
+        /** Holds the widget toolbar instance. */
+        UIToolBar *m_pWidgetToolbar;
+#endif
+    /** @} */
+
+    /** @name Button-box stuff.
+      * @{ */
+        /** Holds the dialog button-box instance. */
+        QIDialogButtonBox *m_pButtonBox;
+    /** @} */
+
+    /** Allow factory access to private/protected members: */
+    friend class QIManagerDialogFactory;
+};
+
+#endif /* !___QIManagerDialog_h___ */
+
