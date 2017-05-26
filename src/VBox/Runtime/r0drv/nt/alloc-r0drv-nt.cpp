7dac438391fd908bf93b2a8d9dc54e400451878c
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2017-05-26 10:11:32 +0000

    bugref:8727: Enabling NonPagedPoolNx in rtR0MemAllocEx for Windows 8 and up
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@67111 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Runtime/r0drv/nt/alloc-r0drv-nt.cpp
+++ src/VBox/Runtime/r0drv/nt/alloc-r0drv-nt.cpp
@@ -45,7 +45,7 @@ DECLHIDDEN(int) rtR0MemAllocEx(size_t cb, uint32_t fFlags, PRTMEMHDR *ppHdr)
 {
     if (!(fFlags & RTMEMHDR_FLAG_ANY_CTX))
     {
-#if 0 /* This allegedly makes the driver verifier happier... */
+#if 1 /* This allegedly makes the driver verifier happier... */
         POOL_TYPE enmPoolType = NonPagedPool;
         if (!(fFlags & RTMEMHDR_FLAG_EXEC) && g_uRtNtVersion >= RTNT_MAKE_VERSION(8,0))
             enmPoolType = NonPagedPoolNx;
