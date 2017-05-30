99708817df99c919b85f74364c75003adebde4d4
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2017-05-30 12:48:38 +0000

    DevVGA.cpp/vga_mem_writeb: should return VINF_IOM_R3_MMIO_WRITE not _READ. fixes ring-0 assertions in strict builds.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@67140 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Devices/Graphics/DevVGA.cpp
+++ src/VBox/Devices/Graphics/DevVGA.cpp
@@ -1315,7 +1315,7 @@ static int vga_mem_writeb(PVGASTATE pThis, RTGCPHYS addr, uint32_t val)
     /* VMSVGA keeps the VGA and SVGA framebuffers separate unlike this boch-based
        VGA implementation, so we fake it by going to ring-3 and using a heap buffer.  */
     if (!pThis->svga.fEnabled) { /*likely*/ }
-    else                       return VINF_IOM_R3_MMIO_READ;
+    else                       return VINF_IOM_R3_MMIO_WRITE;
 #endif
 
     addr &= 0x1ffff;
