3c1b7e243ec18ab53698cbaa54bbbe329bc004dd
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2017-06-01 08:15:19 +0000

    bugref:8524: Additions/linux: play nicely with distribution-installed Additions ttm_bo_default_io_mem_pfn() is not available before Linux 4.12
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@67191 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Additions/linux/drm/vbox_ttm.c
+++ src/VBox/Additions/linux/drm/vbox_ttm.c
@@ -270,7 +270,7 @@ struct ttm_bo_driver vbox_bo_driver = {
     .verify_access = vbox_bo_verify_access,
     .io_mem_reserve = &vbox_ttm_io_mem_reserve,
     .io_mem_free = &vbox_ttm_io_mem_free,
-#if LINUX_VERSION_CODE >= KERNEL_VERSION(4, 11, 0)
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(4, 12, 0)
     .io_mem_pfn = ttm_bo_default_io_mem_pfn,
 #endif
 #if LINUX_VERSION_CODE >= KERNEL_VERSION(4, 7, 0) && LINUX_VERSION_CODE < KERNEL_VERSION(4, 11, 0)
