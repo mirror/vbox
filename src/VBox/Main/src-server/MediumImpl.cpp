4f345a8c03610a04360522eeec5a4604842e6bc4
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2017-06-01 12:20:32 +0000

    Main: More hacking on OPC exporting.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@67205 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Main/src-server/MediumImpl.cpp
+++ src/VBox/Main/src-server/MediumImpl.cpp
@@ -6102,11 +6102,10 @@ HRESULT Medium::i_fixParentUuidOfChildren(MediumLockList *pChildrenToReparent)
 HRESULT Medium::i_addRawToFss(const char *aFilename, SecretKeyStore *pKeyStore,
                               RTVFSFSSTREAM hVfsFssDst /*, const ComObjPtr<Progress> &aProgress*/)
 {
-
-    //RT_NOREF(aProgress); /** @todo fix progress object. */
+     /** @todo fix progress object. */
 
     /*
-     * Build the source lock list.
+     * Build the source lock list and lock the images.
      */
     MediumLockList SourceMediumLockList;
     HRESULT rc = i_createMediumLockList(true /* fFailIfInaccessible */,
@@ -6114,6 +6113,8 @@ HRESULT Medium::i_addRawToFss(const char *aFilename, SecretKeyStore *pKeyStore,
                                         false /* fMediumLockWriteAll */,
                                         NULL,
                                         SourceMediumLockList);
+    if (SUCCEEDED(rc))
+        rc = SourceMediumLockList.Lock();
     if (FAILED(rc))
         return rc;
 
