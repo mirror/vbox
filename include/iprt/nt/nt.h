758885130c992a2f0f876c84bf2595913e9869dc
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2017-06-09 13:11:12 +0000

    tabs to spaces
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@67318 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- include/iprt/nt/nt.h
+++ include/iprt/nt/nt.h
@@ -2264,23 +2264,23 @@ typedef SYSTEM_SESSION_PROCESS_INFORMATION *PSYSTEM_SESSION_PROCESS_INFORMATION;
 /** Input to SystemModuleInformation. */
 typedef struct _RTL_PROCESS_MODULE_INFORMATION
 {
-	HANDLE Section;
-	PVOID  MappedBase;
-	PVOID  ImageBase;
-	ULONG  ImageSize;
-	ULONG  Flags;
-	USHORT LoadOrderIndex;
-	USHORT InitOrderIndex;
-	USHORT LoadCount;
-	USHORT OffsetToFileName;
-	UCHAR  FullPathName[256];
+    HANDLE Section;
+    PVOID  MappedBase;
+    PVOID  ImageBase;
+    ULONG  ImageSize;
+    ULONG  Flags;
+    USHORT LoadOrderIndex;
+    USHORT InitOrderIndex;
+    USHORT LoadCount;
+    USHORT OffsetToFileName;
+    UCHAR  FullPathName[256];
 } RTL_PROCESS_MODULE_INFORMATION;
 typedef RTL_PROCESS_MODULE_INFORMATION *PRTL_PROCESS_MODULE_INFORMATION;
 
 typedef struct _RTL_PROCESS_MODULES
 {
-	ULONG  NumberOfModules;
-	RTL_PROCESS_MODULE_INFORMATION Modules[1];
+    ULONG  NumberOfModules;
+    RTL_PROCESS_MODULE_INFORMATION Modules[1];
 } RTL_PROCESS_MODULES;
 typedef RTL_PROCESS_MODULES *PRTL_PROCESS_MODULES;
 
