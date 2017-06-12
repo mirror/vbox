dca6d795cbd003c06eddf5d7c322ca39897f4379
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2017-06-12 10:20:16 +0000

    HDA codec: SPDIF out does not do presence detect (fixes assertion with Snow Leopard guest).
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@67335 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Devices/Audio/HDACodec.cpp
+++ src/VBox/Devices/Audio/HDACodec.cpp
@@ -1267,8 +1267,7 @@ static DECLCALLBACK(int) stac9220ResetNode(PHDACODEC pThis, uint8_t uNID, PCODEC
                                                   | CODEC_F00_09_CAP_DIGITAL
                                                   | CODEC_F00_09_CAP_CONNECTION_LIST
                                                   | CODEC_F00_09_CAP_STEREO;
-            pNode->digout.node.au32F00_param[0xC] = CODEC_F00_0C_CAP_OUTPUT
-                                                  | CODEC_F00_0C_CAP_PRESENCE_DETECT;
+            pNode->digout.node.au32F00_param[0xC] = CODEC_F00_0C_CAP_OUTPUT;
 
             /* Connection list entries. */
             pNode->digout.node.au32F00_param[0xE] = CODEC_MAKE_F00_0E(CODEC_F00_0E_LIST_NID_SHORT, 3 /* Entries */);
