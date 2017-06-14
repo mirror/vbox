a371dc7ebe5882a6dda32fb462003969ff9e1703
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2017-06-14 13:16:54 +0000

    bugref:8524: Additions/linux: play nicely with distribution-installed Additions Additions: linux/drm: Change vbox_prime.c to kernel coding style
    This is the result of running linux/scripts/Lindent + manual cleanups.
    After this the file passes linux/scripts/checkpatch -f
    except for the LINUX_VERSION_CODE checks.
    
    This patch contains no functional changes, only coding style fixes,
    including changing uintXX_t types to uXX.
    
    Signed-off-by: Hans de Goede <hdegoede@redhat.com>
    
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@67405 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Additions/linux/drm/vbox_prime.c
+++ src/VBox/Additions/linux/drm/vbox_prime.c
@@ -25,55 +25,55 @@
 
 #include "vbox_drv.h"
 
-/* Based on qxl_prime.c:
+/*
+ * Based on qxl_prime.c:
  * Empty Implementations as there should not be any other driver for a virtual
- * device that might share buffers with vboxvideo */
+ * device that might share buffers with vboxvideo
+ */
 
 int vbox_gem_prime_pin(struct drm_gem_object *obj)
 {
-    WARN_ONCE(1, "not implemented");
-    return -ENOSYS;
+	WARN_ONCE(1, "not implemented");
+	return -ENOSYS;
 }
 
 void vbox_gem_prime_unpin(struct drm_gem_object *obj)
 {
-    WARN_ONCE(1, "not implemented");
+	WARN_ONCE(1, "not implemented");
 }
-
 
 struct sg_table *vbox_gem_prime_get_sg_table(struct drm_gem_object *obj)
 {
-    WARN_ONCE(1, "not implemented");
-    return ERR_PTR(-ENOSYS);
+	WARN_ONCE(1, "not implemented");
+	return ERR_PTR(-ENOSYS);
 }
 
-struct drm_gem_object *vbox_gem_prime_import_sg_table(
-        struct drm_device *dev,
+struct drm_gem_object *vbox_gem_prime_import_sg_table(struct drm_device *dev,
 #if LINUX_VERSION_CODE < KERNEL_VERSION(3, 18, 0)
-        size_t size,
+						      size_t size,
 #else
-        struct dma_buf_attachment *attach,
+						      struct dma_buf_attachment
+						      *attach,
 #endif
-        struct sg_table *table)
+						      struct sg_table *table)
 {
-    WARN_ONCE(1, "not implemented");
-    return ERR_PTR(-ENOSYS);
+	WARN_ONCE(1, "not implemented");
+	return ERR_PTR(-ENOSYS);
 }
 
 void *vbox_gem_prime_vmap(struct drm_gem_object *obj)
 {
-    WARN_ONCE(1, "not implemented");
-    return ERR_PTR(-ENOSYS);
+	WARN_ONCE(1, "not implemented");
+	return ERR_PTR(-ENOSYS);
 }
 
 void vbox_gem_prime_vunmap(struct drm_gem_object *obj, void *vaddr)
 {
-    WARN_ONCE(1, "not implemented");
+	WARN_ONCE(1, "not implemented");
 }
 
-int vbox_gem_prime_mmap(struct drm_gem_object *obj,
-        struct vm_area_struct *area)
+int vbox_gem_prime_mmap(struct drm_gem_object *obj, struct vm_area_struct *area)
 {
-    WARN_ONCE(1, "not implemented");
-    return -ENOSYS;
+	WARN_ONCE(1, "not implemented");
+	return -ENOSYS;
 }
