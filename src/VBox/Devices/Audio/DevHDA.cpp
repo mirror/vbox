359482ef391a2e8d0ebb4f0e09ea307a49ee24e7
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2017-06-19 13:23:12 +0000

    Audio/DevHDA: Build fix if VBOX_WITH_AUDIO_HDA_ASYNC_IO is not defined.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@67475 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Devices/Audio/DevHDA.cpp
+++ src/VBox/Devices/Audio/DevHDA.cpp
@@ -5564,12 +5564,12 @@ static void hdaStreamUpdate(PHDASTATE pThis, PHDASTREAM pStream, bool fInTimer)
         if (fInTimer)
         {
 #endif
-            const uint32_t cbReadable = hdaStreamGetUsed(pStream);
-            if (cbReadable)
+            const uint32_t cbToTransfer = hdaStreamGetUsed(pStream);
+            if (cbToTransfer)
             {
                 /* When running synchronously, do the DMA data transfers here.
                  * Otherwise this will be done in the stream's async I/O thread. */
-                rc2 = hdaStreamTransfer(pThis, pStream, cbReadable);
+                rc2 = hdaStreamTransfer(pThis, pStream, cbToTransfer);
                 AssertRC(rc2);
             }
 #ifdef VBOX_WITH_AUDIO_HDA_ASYNC_IO
