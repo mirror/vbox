596e0aa9f85064a8e3ad7ca953a7b0e81cb85e65
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2017-06-19 20:22:01 +0000

    DrvHostPulseAudio: more debug
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@67483 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Devices/Audio/DrvHostPulseAudio.cpp
+++ src/VBox/Devices/Audio/DrvHostPulseAudio.cpp
@@ -999,6 +999,7 @@ static void paEnumSourceCb(pa_context *pCtx, const pa_source_info *pInfo, int eo
 
 static void paEnumServerCb(pa_context *pCtx, const pa_server_info *pInfo, void *pvUserData)
 {
+    LogRel(("DEBUG: paEnumServerCb\n"));
     if (!pCtx)
         LogRel(("DEBUG: paEnumServerCb return because of !pCtx\n"));
     AssertPtrReturnVoid(pCtx);
@@ -1029,6 +1030,7 @@ static void paEnumServerCb(pa_context *pCtx, const pa_server_info *pInfo, void *
     }
 
 //    LogRel(("DEBUG: pa_threaded_mainloop_signal() from paEnumServerCb\n"));
+    LogRel(("DEBUG: paEnumServerCb signal mainloop\n"));
     pa_threaded_mainloop_signal(pThis->pMainLoop, 0);
 }
 
