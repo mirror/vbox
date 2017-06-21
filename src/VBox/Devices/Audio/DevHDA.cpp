0f61aef4bda565d8e9dee694b82eec626b4ae56f
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2017-06-21 13:40:44 +0000

    Audio/DevHDA.cpp: Comment.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@67541 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Devices/Audio/DevHDA.cpp
+++ src/VBox/Devices/Audio/DevHDA.cpp
@@ -6088,7 +6088,7 @@ static DECLCALLBACK(int) hdaSaveExec(PPDMDEVINS pDevIns, PSSMHANDLE pSSM)
     SSMR3PutU32(pSSM, RT_ELEMENTS(pThis->au32Regs));
     SSMR3PutMem(pSSM, pThis->au32Regs, sizeof(pThis->au32Regs));
 
-    /* Load controller-specifc internals. */
+    /* Save controller-specifc internals. */
     SSMR3PutU64(pSSM, pThis->u64WalClk);
     SSMR3PutU8(pSSM, pThis->u8IRQL);
 
