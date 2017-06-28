16d8bb6ecf019ccf7ae0f0bc6ebc2c8f38ccd346
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2017-06-28 15:33:29 +0000

    FE/Qt: removed empty defaultcodec element from qt_sr.ts
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@67666 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Frontends/VirtualBox/nls/qt_sr.ts
+++ src/VBox/Frontends/VirtualBox/nls/qt_sr.ts
@@ -1,6 +1,5 @@
 ﻿<?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS><TS version="1.1" language="fr_FR">
-<defaultcodec></defaultcodec>
 <context>
     <name>MAC_APPLICATION_MENU</name>
     <message>
