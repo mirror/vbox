06a2453cc0b0e6dae7b4f1a2551ab5507baf8406
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2017-07-14 09:27:42 +0000

    VSCSI: Added virtual SSC device. WIP, disabled by default.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@67961 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Devices/Storage/VSCSI/VSCSILun.cpp
+++ src/VBox/Devices/Storage/VSCSI/VSCSILun.cpp
@@ -4,7 +4,7 @@
  */
 
 /*
- * Copyright (C) 2006-2016 Oracle Corporation
+ * Copyright (C) 2006-2017 Oracle Corporation
  *
  * This file is part of VirtualBox Open Source Edition (OSE), as
  * available from http://www.virtualbox.org. This file is free software;
@@ -28,6 +28,8 @@
 extern VSCSILUNDESC g_VScsiLunTypeSbc;
 /** MMC descriptor */
 extern VSCSILUNDESC g_VScsiLunTypeMmc;
+/** SSC descriptor */
+extern VSCSILUNDESC g_VScsiLunTypeSsc;
 
 /**
  * Array of supported SCSI LUN types.
@@ -36,6 +38,9 @@ static PVSCSILUNDESC g_aVScsiLunTypesSupported[] =
 {
     &g_VScsiLunTypeSbc,
     &g_VScsiLunTypeMmc,
+#ifdef VBOX_WITH_VSCSI_SSC
+    &g_VScsiLunTypeSsc,
+#endif
 };
 
 VBOXDDU_DECL(int) VSCSILunCreate(PVSCSILUN phVScsiLun, VSCSILUNTYPE enmLunType,
