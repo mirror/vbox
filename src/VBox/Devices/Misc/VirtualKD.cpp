30f0ee371640b9342975abc539707ddf706527b5
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2017-07-14 14:36:12 +0000

    bugref:8877: VirtualKD: build fix
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@67976 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Devices/Misc/VirtualKD.cpp
+++ src/VBox/Devices/Misc/VirtualKD.cpp
@@ -111,14 +111,13 @@ static DECLCALLBACK(int) vkdPortWrite(PPDMDEVINS pDevIns, void *pvUser, RTIOPORT
             || !RequestHeader.cbData)
             return VINF_SUCCESS;
 
-        size_t cbData = RT_MIN(RequestHeader.cbData, sizeof(pThis->abCmdBody));
+        unsigned cbData = RT_MIN(RequestHeader.cbData, sizeof(pThis->abCmdBody));
         rc = PDMDevHlpPhysRead(pDevIns, (RTGCPHYS)(u32 + sizeof(RequestHeader)), pThis->abCmdBody, cbData);
         if (!RT_SUCCESS(rc))
             return VINF_SUCCESS;
 
         char *pReply = NULL;
-        unsigned cbReply;
-        cbReply = pThis->pKDClient->OnRequest(pThis->abCmdBody, cbData, &pReply);
+        unsigned cbReply = pThis->pKDClient->OnRequest(pThis->abCmdBody, cbData, &pReply);
 
         if (!pReply)
             cbReply = 0;
