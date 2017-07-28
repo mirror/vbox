b2cac88117b65b1d5316c7250f0c888e3dc9f81d
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2017-07-28 14:36:33 +0000

    Main/ApplianceImport: bugref:8930: don't enable OHCI if xHCI is enabled
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@68158 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Main/src-server/ApplianceImplImport.cpp
+++ src/VBox/Main/src-server/ApplianceImplImport.cpp
@@ -3465,20 +3465,21 @@ void Appliance::i_importVBoxMachine(ComObjPtr<VirtualSystemDescription> &vsdescT
     {
         /** @todo r=klaus add support for arbitrary USB controller types, this can't handle
          *  multiple controllers due to its design anyway */
-        /* usually the OHCI controller is enabled already, need to check */
+        /* Usually the OHCI controller is enabled already, need to check. But
+         * do this only if there is no xHCI controller. */
         bool fOHCIEnabled = false;
+        bool fXHCIEnabled = false;
         settings::USBControllerList &llUSBControllers = config.hardwareMachine.usbSettings.llUSBControllers;
         settings::USBControllerList::iterator it;
         for (it = llUSBControllers.begin(); it != llUSBControllers.end(); ++it)
         {
             if (it->enmType == USBControllerType_OHCI)
-            {
                 fOHCIEnabled = true;
-                break;
-            }
+            if (it->enmType == USBControllerType_XHCI)
+                fXHCIEnabled = true;
         }
 
-        if (!fOHCIEnabled)
+        if (!fXHCIEnabled && !fOHCIEnabled)
         {
             settings::USBController ctrl;
             ctrl.strName = "OHCI";
