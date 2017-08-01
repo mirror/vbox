4715fc6dc6dd1cb6389fcdbe7a1da862a3ef2f2d
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2017-08-01 11:22:27 +0000

    Runtime/r0drv/linux: openSUSE Leap 42.3 tweak for a Linux 4.4 kernel with Linux 4.10 get_user_pages() interface (2nd fix)
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@68217 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Runtime/r0drv/linux/memobj-r0drv-linux.c
+++ src/VBox/Runtime/r0drv/linux/memobj-r0drv-linux.c
@@ -1027,9 +1027,9 @@ DECLHIDDEN(int) rtR0MemObjNativeEnterPhys(PPRTR0MEMOBJINTERNAL ppMem, RTHCPHYS P
 
 /* openSUSE Leap 42.3 detection :-/ */
 #if    LINUX_VERSION_CODE >= KERNEL_VERSION(4, 4, 0) \
-    && LINUX_VERSION_CODE <  KERNEL_VERSION(4, 9, 0) \
+    && LINUX_VERSION_CODE <  KERNEL_VERSION(4, 6, 0) \
     && defined(FAULT_FLAG_REMOTE)
-# define GET_USER_PAGES_API     KERNEL_VERSION(4, 10, 0)
+# define GET_USER_PAGES_API     KERNEL_VERSION(4, 10, 0) /* no typo! */
 #else
 # define GET_USER_PAGES_API     LINUX_VERSION_CODE
 #endif
