2e276e1fe908f85db669ec7309d33508be070deb
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2017-08-08 08:23:53 +0000

    FE/Qt: bugref:8400: Medium details widget: More suitable error message for dynamical medium shrinking case (s.a. r117448).
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@68330 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Frontends/VirtualBox/src/medium/UIMediumDetailsWidget.cpp
+++ src/VBox/Frontends/VirtualBox/src/medium/UIMediumDetailsWidget.cpp
@@ -692,8 +692,9 @@ void UIMediumDetailsWidget::retranslateValidation(QWidget *pWidget /* = 0 */)
 //        m_pErrorPaneDescription->setToolTip(tr("Cannot change medium description from <b>%1</b> to <b>%2</b>.")
 //                                               .arg(m_oldData.m_options.m_strDescription).arg(m_newData.m_options.m_strDescription));
     if (!pWidget || pWidget == m_pErrorPaneSize)
-        m_pErrorPaneSize->setToolTip(tr("Cannot change medium size from <b>%1</b> to <b>%2</b> as storage shrinking is currently forbidden.")
-                                         .arg(m_oldData.m_options.m_uLogicalSize).arg(m_newData.m_options.m_uLogicalSize));
+        m_pErrorPaneSize->setToolTip(tr("Cannot change medium size from <b>%1</b> to <b>%2</b> as storage shrinking is currently not implemented.")
+                                        .arg(vboxGlobal().formatSize(m_oldData.m_options.m_uLogicalSize))
+                                        .arg(vboxGlobal().formatSize(m_newData.m_options.m_uLogicalSize)));
 }
 
 void UIMediumDetailsWidget::updateButtonStates()
