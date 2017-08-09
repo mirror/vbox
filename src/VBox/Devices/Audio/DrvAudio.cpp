e440eb7ed04a07c18ec902a8530e1267753a113a
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2017-08-09 15:14:54 +0000

    DrvAudio: Increased temporary audio frame buffer to 4K in drvAudioStreamPlay[Raw|NonInterleaved]. Should decrease number of roundtrips needed.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@68356 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Devices/Audio/DrvAudio.cpp
+++ src/VBox/Devices/Audio/DrvAudio.cpp
@@ -1290,7 +1290,7 @@ static int drvAudioStreamPlayNonInterleaved(PDRVAUDIO pThis,
 
         if (cfToPlay)
         {
-            uint8_t auBuf[256]; /** @todo Get rid of this here. */
+            uint8_t auBuf[_4K]; /** @todo Get rid of this here. */
 
             uint32_t cbLeft  = AUDIOMIXBUF_F2B(&pHstStream->MixBuf, cfToPlay);
             uint32_t cbChunk = sizeof(auBuf);
@@ -1384,7 +1384,7 @@ static int drvAudioStreamPlayRaw(PDRVAUDIO pThis,
         if (cfToPlay > cfWritable) /* More frames available than we can write? Limit. */
             cfToPlay = cfWritable;
 
-        PDMAUDIOFRAME aFrameBuf[256]; /** @todo Get rid of this here. */
+        PDMAUDIOFRAME aFrameBuf[_4K]; /** @todo Get rid of this here. */
 
         uint32_t cfLeft = cfToPlay;
         while (cfLeft)
