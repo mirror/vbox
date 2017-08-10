6258bcf91141749f23c17ee33dd4b7a6f3f3dacc
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2017-08-10 13:15:16 +0000

    DevATA: bugref:8965: enable IDE0/IDE1 at device creation time as this is not properly done by the EFI if ICH9 is used
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@68369 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Devices/Storage/DevATA.cpp
+++ src/VBox/Devices/Storage/DevATA.cpp
@@ -7371,6 +7371,18 @@ static DECLCALLBACK(int) ataR3Construct(PPDMDEVINS pDevIns, int iInstance, PCFGM
             AssertMsgFailed(("Unsupported IDE chipset type: %d\n", pThis->u8Type));
     }
 
+    /** @todo
+     * This is the job of the BIOS / EFI!
+     *
+     * The same is done in DevPCI.cpp / pci_bios_init_device() but there is no
+     * corresponding function in DevPciIch9.cpp. The EFI has corresponding code
+     * in OvmfPkg/Library/PlatformBdsLib/BdsPlatform.c: NotifyDev() but this
+     * function assumes that the IDE controller is located at PCI 00:01.1 which
+     * is not true if the ICH9 chipset is used.
+     */
+    PCIDevSetWord(&pThis->dev, 0x40, 0x8000); /* enable IDE0 */
+    PCIDevSetWord(&pThis->dev, 0x42, 0x8000); /* enable IDE1 */
+
     PCIDevSetCommand(   &pThis->dev, PCI_COMMAND_IOACCESS | PCI_COMMAND_MEMACCESS | PCI_COMMAND_BUSMASTER);
     PCIDevSetClassProg( &pThis->dev, 0x8a); /* programming interface = PCI_IDE bus master is supported */
     PCIDevSetClassSub(  &pThis->dev, 0x01); /* class_sub = PCI_IDE */
