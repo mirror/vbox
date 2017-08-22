03d879ff628b3e16db715ac12845e057c6a8676f
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2017-08-22 05:38:05 +0000

    Additions/VBoxGuest-solaris: Hide interrupt and hilevel priority message on bootup but still log it to syslog.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@68495 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Additions/common/VBoxGuest/VBoxGuest-solaris.c
+++ src/VBox/Additions/common/VBoxGuest/VBoxGuest-solaris.c
@@ -845,7 +845,7 @@ static int vgdrvSolarisAddIRQ(dev_info_t *pDip)
                         {
                             /* Check if the interrupt priority is scheduler level or above, if so we need to use a high-level
                                and low-level interrupt handlers with corresponding mutexes. */
-                            cmn_err(CE_NOTE,  "vboxguest: uIntrPriority=%d hilevel_pri=%d\n", uIntrPriority, ddi_intr_get_hilevel_pri());
+                            cmn_err(CE_CONT, "!vboxguest: uIntrPriority=%d hilevel_pri=%d\n", uIntrPriority, ddi_intr_get_hilevel_pri());
                             if (uIntrPriority >= ddi_intr_get_hilevel_pri())
                             {
                                 /* Initialize the high-level mutex. */
