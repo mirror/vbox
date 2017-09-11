69c8d1b2444e95a0f338028512a388d099591243
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2017-09-11 10:16:17 +0000

    Build fix.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@68713 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Devices/Audio/DevIchAc97.cpp
+++ src/VBox/Devices/Audio/DevIchAc97.cpp
@@ -412,6 +412,9 @@ typedef struct AC97STATE
 #ifndef VBOX_WITH_AUDIO_AC97_CALLBACKS
     /** The timer for pumping data thru the attached LUN drivers. */
     PTMTIMERR3              pTimer;
+# if HC_ARCH_BITS == 32
+    uint32_t                Padding0;
+# endif
     /** Flag indicating whether the timer is active or not. */
     bool                    fTimerActive;
     uint8_t                 u8Padding1[7];
