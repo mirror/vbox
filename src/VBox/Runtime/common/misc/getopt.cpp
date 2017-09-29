2650757a87642a54cd29ccdd0bfed0377542a3a6
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2017-09-29 16:13:21 +0000

    IPRT/RTGetOpt: Added 32-bit and 64-bit pair types. [build fix]
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@68937 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Runtime/common/misc/getopt.cpp
+++ src/VBox/Runtime/common/misc/getopt.cpp
@@ -433,10 +433,10 @@ static int rtGetOptProcessValue(uint32_t fFlags, const char *pszValue, PRTGETOPT
                             while (RT_C_IS_BLANK(*pszValue)); \
                         if (pszValue != pszNext) \
                         { \
-                            unsigned uBase =    pszValue[0] == '0' \
-                                             && (pszValue[1] == 'x' || pszValue[1] == 'X') \
-                                             && RT_C_IS_XDIGIT(pszValue[2]) \
-                                           ? 16 : a_ConvBase; \
+                            uBase =    pszValue[0] == '0' \
+                                    && (pszValue[1] == 'x' || pszValue[1] == 'X') \
+                                    && RT_C_IS_XDIGIT(pszValue[2]) \
+                                  ? 16 : a_ConvBase; \
                             rc = a_fnConv(pszValue, &pszNext, uBase, &Value2); \
                             if (rc == VINF_SUCCESS) \
                             { /* likely */ } \
