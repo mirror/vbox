552a5c507a7e0994e946469cdf4023951a00df7c
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2017-10-06 08:39:09 +0000

    VMM/IEM: Adjust assertion to allow VINF_EM_TRIPLE_FAULT in iemExecStatusCodeFiddling.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@68997 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/VMM/VMMAll/IEMAll.cpp
+++ src/VBox/VMM/VMMAll/IEMAll.cpp
@@ -14799,6 +14799,7 @@ DECL_FORCE_INLINE(VBOXSTRICTRC) iemExecStatusCodeFiddling(PVMCPU pVCpu, VBOXSTRI
                       || rcStrict == VINF_EM_RAW_EMULATE_INSTR
                       || rcStrict == VINF_EM_RAW_TO_R3
                       || rcStrict == VINF_EM_RAW_EMULATE_IO_BLOCK
+                      || rcStrict == VINF_EM_TRIPLE_FAULT
                       /* raw-mode / virt handlers only: */
                       || rcStrict == VINF_EM_RAW_EMULATE_INSTR_GDT_FAULT
                       || rcStrict == VINF_EM_RAW_EMULATE_INSTR_TSS_FAULT
