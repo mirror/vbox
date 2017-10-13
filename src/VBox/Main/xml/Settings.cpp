70a78c39bc932a2fb039db037838f59e80d19615
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2017-10-13 12:41:17 +0000

    Settings: restore the lost code for writing maxSize attribute for video capture
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@69070 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Main/xml/Settings.cpp
+++ src/VBox/Main/xml/Settings.cpp
@@ -4135,6 +4135,7 @@ void MachineConfigFile::readHardware(const xml::ElementNode &elmHardware,
             pelmHwChild->getAttributeValue("rate",      hw.ulVideoCaptureRate);
             pelmHwChild->getAttributeValue("fps",       hw.ulVideoCaptureFPS);
             pelmHwChild->getAttributeValue("maxTime",   hw.ulVideoCaptureMaxTime);
+            pelmHwChild->getAttributeValue("maxSize",   hw.ulVideoCaptureMaxSize);
             pelmHwChild->getAttributeValue("options",   hw.strVideoCaptureOptions);
         }
         else if (pelmHwChild->nameEquals("RemoteDisplay"))
