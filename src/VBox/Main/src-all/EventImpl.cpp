1b1cf75f53d97301bcb54690de9996101130b176
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2017-10-25 19:16:02 +0000

    Main/Event: yell loudly if forcefully unregistering a passive event listener due to piling up too many events
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@69323 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Main/src-all/EventImpl.cpp
+++ src/VBox/Main/src-all/EventImpl.cpp
@@ -861,6 +861,7 @@ HRESULT ListenerRecord::enqueue(IEvent *aEvent)
     if (queueSize > 1000 || (queueSize > 500 && sinceRead > 60 * 1000))
     {
         ::RTCritSectLeave(&mcsQLock);
+        LogRel(("Event: forcefully unregistering passive event listener %p due to excessive queue size\n", this));
         return E_ABORT;
     }
 
