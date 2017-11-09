8d5c196bfe61308fc9b54f7df58abaec7bb118b0
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2017-11-09 18:31:09 +0000

    GuestHost: HGSMIMemAlloc uninit check
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@69634 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/GuestHost/HGSMI/HGSMIMemAlloc.cpp
+++ src/VBox/GuestHost/HGSMI/HGSMIMemAlloc.cpp
@@ -561,10 +561,14 @@ void HGSMIMAUninit(HGSMIMADATA *pMA)
 {
     HGSMIMABLOCK *pIter;
     HGSMIMABLOCK *pNext;
-    RTListForEachSafe(&pMA->listBlocks, pIter, pNext, HGSMIMABLOCK, nodeBlock)
+    /* If it has been initialized. */
+    if (pMA->listBlocks.pNext)
     {
-        RTListNodeRemove(&pIter->nodeBlock);
-        hgsmiMABlockFree(pMA, pIter);
+        RTListForEachSafe(&pMA->listBlocks, pIter, pNext, HGSMIMABLOCK, nodeBlock)
+        {
+            RTListNodeRemove(&pIter->nodeBlock);
+            hgsmiMABlockFree(pMA, pIter);
+        }
     }
 
     RT_ZERO(*pMA);
