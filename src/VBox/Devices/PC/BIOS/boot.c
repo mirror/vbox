5904a7b4eb71902232e2c7d532f06427a72b090a
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2017-11-15 12:54:20 +0000

    BIOS: Slightly adjusted boot sector validity test (fixes darwinx86-602.iso).
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@69703 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Devices/PC/BIOS/boot.c
+++ src/VBox/Devices/PC/BIOS/boot.c
@@ -323,7 +323,7 @@ uint32_t BIOSCALL int19_function(uint8_t bseqnr)
     // NB: It is somewhat common for failed OS installs to have the
     // 0x55AA signature and a valid partition table but zeros in the
     // rest of the boot sector. We do a quick check by comparing the first
-    // two words of boot sector; if identical, the boot sector is
+    // and third word of boot sector; if identical, the boot sector is
     // extremely unlikely to be valid.
     if (bootdrv != 0) bootchk = 0;
     else bootchk = 1; /* disable 0x55AA signature check on drive A: */
@@ -334,7 +334,7 @@ uint32_t BIOSCALL int19_function(uint8_t bseqnr)
         bootchk = 1;
 #endif // BX_ELTORITO_BOOT
 
-    if (read_word(bootseg,0) == read_word(bootseg,2)
+    if (read_word(bootseg,0) == read_word(bootseg,4)
       || (bootchk == 0 && read_word(bootseg,0x1fe) != 0xaa55))
     {
         print_boot_failure(bootcd, bootlan, bootdrv, 0, lastdrive);
