45c7a398c6b24a20ac7faf0b98895768e0d572bd
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2017-12-07 13:34:16 +0000

    VBoxGuest-netbsd.c: shuffle the code around to make the narration organized more like that of a typical NetBSD driver.  Mostly moves autoconf machinery to the beginning.  While here fix a couple of indentation botches.  No functional changes intended.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@69979 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Additions/common/VBoxGuest/VBoxGuest-netbsd.c
+++ src/VBox/Additions/common/VBoxGuest/VBoxGuest-netbsd.c
@@ -24,11 +24,6 @@
  * terms and conditions of either the GPL or the CDDL or both.
  */
 
-/** @todo r=bird: This must merge with SUPDrv-netbsd.c before long. The two
- * source files should only differ on prefixes and the extra bits wrt to the
- * pci device. I.e. it should be diffable so that fixes to one can easily be
- * applied to the other. */
-
 
 /*********************************************************************************************************************************
 *   Header Files                                                                                                                 *
@@ -120,6 +115,20 @@ struct vboxguest_session
 *   Internal Functions                                                                                                           *
 *********************************************************************************************************************************/
 /*
+ * Driver(9) autoconf machinery.
+ */
+static int VBoxGuestNetBSDMatch(device_t parent, cfdata_t match, void *aux);
+static void VBoxGuestNetBSDAttach(device_t parent, device_t self, void *aux);
+static int VBoxGuestNetBSDDetach(device_t self, int flags);
+
+/*
+ * IRQ related functions.
+ */
+static int  VBoxGuestNetBSDAddIRQ(vboxguest_softc *sc, struct pci_attach_args *pa);
+static void VBoxGuestNetBSDRemoveIRQ(vboxguest_softc *sc);
+static int  VBoxGuestNetBSDISR(void *pvState);
+
+/*
  * Character device file handlers.
  */
 static int VBoxGuestNetBSDOpen(dev_t device, int flags, int fmt, struct lwp *process);
@@ -127,15 +136,6 @@ static int VBoxGuestNetBSDClose(struct file *fp);
 static int VBoxGuestNetBSDIOCtl(struct file *fp, u_long cmd, void *addr);
 static int VBoxGuestNetBSDIOCtlSlow(struct vboxguest_session *session, u_long command, void *data);
 static int VBoxGuestNetBSDPoll(struct file *fp, int events);
-static void VBoxGuestNetBSDAttach(device_t, device_t, void*);
-static int VBoxGuestNetBSDDetach(device_t pDevice, int flags);
-
-/*
- * IRQ related functions.
- */
-static void VBoxGuestNetBSDRemoveIRQ(vboxguest_softc *sc);
-static int  VBoxGuestNetBSDAddIRQ(vboxguest_softc *pvState, struct pci_attach_args *pa);
-static int  VBoxGuestNetBSDISR(void *pvState);
 
 
 /*********************************************************************************************************************************
@@ -179,6 +179,240 @@ static struct selinfo       g_SelInfo;
 
 CFDRIVER_DECL(vboxguest, DV_DULL, NULL);
 extern struct cfdriver vboxguest_cd;
+
+
+CFATTACH_DECL_NEW(vboxguest, sizeof(vboxguest_softc),
+    VBoxGuestNetBSDMatch, VBoxGuestNetBSDAttach, VBoxGuestNetBSDDetach, NULL);
+
+
+static int VBoxGuestNetBSDMatch(device_t parent, cfdata_t match, void *aux)
+{
+    const struct pci_attach_args *pa = aux;
+
+    if (   PCI_VENDOR(pa->pa_id) == VMMDEV_VENDORID
+        && PCI_PRODUCT(pa->pa_id) == VMMDEV_DEVICEID)
+    {
+        return 1;
+    }
+
+    return 0;
+}
+
+
+static void VBoxGuestNetBSDAttach(device_t parent, device_t self, void *aux)
+{
+    int rc = VINF_SUCCESS;
+    int iResId = 0;
+    vboxguest_softc *vboxguest;
+    struct pci_attach_args *pa = aux;
+    bus_space_tag_t iot, memt;
+    bus_space_handle_t ioh, memh;
+    bus_dma_segment_t seg;
+    int ioh_valid, memh_valid;
+
+    cUsers = 0;
+
+    aprint_normal(": VirtualBox Guest\n");
+
+    vboxguest = device_private(self);
+    vboxguest->sc_dev = self;
+
+    /*
+     * Initialize IPRT R0 driver, which internally calls OS-specific r0 init.
+     */
+    rc = RTR0Init(0);
+    if (RT_FAILURE(rc))
+    {
+        LogFunc(("RTR0Init failed.\n"));
+        aprint_error_dev(vboxguest->sc_dev, "RTR0Init failed\n");
+        return;
+    }
+
+    vboxguest->pc = pa->pa_pc;
+
+    /*
+     * Allocate I/O port resource.
+     */
+    ioh_valid = (pci_mapreg_map(pa, PCI_MAPREG_START, PCI_MAPREG_TYPE_IO, 0,
+                                &vboxguest->io_tag, &vboxguest->io_handle,
+                                &vboxguest->uIOPortBase, &vboxguest->io_regsize) == 0);
+
+    if (ioh_valid)
+    {
+
+        /*
+         * Map the MMIO region.
+         */
+        memh_valid = (pci_mapreg_map(pa, PCI_MAPREG_START+4, PCI_MAPREG_TYPE_MEM, BUS_SPACE_MAP_LINEAR,
+                                     &vboxguest->iVMMDevMemResId, &vboxguest->VMMDevMemHandle,
+                                     &vboxguest->pMMIOBase, &vboxguest->VMMDevMemSize) == 0);
+        if (memh_valid)
+        {
+            /*
+             * Call the common device extension initializer.
+             */
+            rc = VGDrvCommonInitDevExt(&g_DevExt, vboxguest->uIOPortBase,
+                                       bus_space_vaddr(vboxguest->iVMMDevMemResId,
+                                                       vboxguest->VMMDevMemHandle),
+                                       vboxguest->VMMDevMemSize,
+#if ARCH_BITS == 64
+                                       VBOXOSTYPE_NetBSD_x64,
+#else
+                                       VBOXOSTYPE_NetBSD,
+#endif
+                                       VMMDEV_EVENT_MOUSE_POSITION_CHANGED);
+            if (RT_SUCCESS(rc))
+            {
+                /*
+                 * Add IRQ of VMMDev.
+                 */
+                rc = VBoxGuestNetBSDAddIRQ(vboxguest, pa);
+                if (RT_SUCCESS(rc))
+                {
+                    vboxguest->vboxguest_state |= VBOXGUEST_STATE_INITOK;
+                    return;
+                }
+                VGDrvCommonDeleteDevExt(&g_DevExt);
+            }
+            else
+            {
+                aprint_error_dev(vboxguest->sc_dev, "init failed\n");
+            }
+            bus_space_unmap(vboxguest->iVMMDevMemResId, vboxguest->VMMDevMemHandle, vboxguest->VMMDevMemSize);
+        }
+        else
+        {
+            aprint_error_dev(vboxguest->sc_dev, "MMIO mapping failed\n");
+        }
+        bus_space_unmap(vboxguest->io_tag, vboxguest->io_handle, vboxguest->io_regsize);
+    }
+    else
+    {
+        aprint_error_dev(vboxguest->sc_dev, "IO mapping failed\n");
+    }
+
+    RTR0Term();
+    return;
+}
+
+
+/**
+ * Sets IRQ for VMMDev.
+ *
+ * @returns NetBSD error code.
+ * @param   vboxguest  Pointer to the state info structure.
+ * @param   pa  Pointer to the PCI attach arguments.
+ */
+static int VBoxGuestNetBSDAddIRQ(vboxguest_softc *vboxguest, struct pci_attach_args *pa)
+{
+    int iResId = 0;
+    int rc = 0;
+    const char *intrstr;
+#if __NetBSD_Prereq__(6, 99, 39)
+    char intstrbuf[100];
+#endif
+
+    LogFlow((DEVICE_NAME ": %s\n", __func__));
+
+    if (pci_intr_map(pa, &vboxguest->ih))
+    {
+        aprint_error_dev(vboxguest->sc_dev, "couldn't map interrupt.\n");
+        return VERR_DEV_IO_ERROR;
+    }
+
+    intrstr = pci_intr_string(vboxguest->pc, vboxguest->ih
+#if __NetBSD_Prereq__(6, 99, 39)
+                              , intstrbuf, sizeof(intstrbuf)
+#endif
+                              );
+    aprint_normal_dev(vboxguest->sc_dev, "interrupting at %s\n", intrstr);
+
+    vboxguest->pfnIrqHandler = pci_intr_establish(vboxguest->pc, vboxguest->ih, IPL_BIO, VBoxGuestNetBSDISR, vboxguest);
+    if (vboxguest->pfnIrqHandler == NULL)
+    {
+        aprint_error_dev(vboxguest->sc_dev, "couldn't establish interrupt\n");
+        return VERR_DEV_IO_ERROR;
+    }
+
+    return VINF_SUCCESS;
+}
+
+
+static int VBoxGuestNetBSDDetach(device_t self, int flags)
+{
+    vboxguest_softc *vboxguest;
+    vboxguest = device_private(self);
+
+    LogFlow((DEVICE_NAME ": %s\n", __func__));
+
+    if (cUsers > 0)
+        return EBUSY;
+
+    if ((vboxguest->vboxguest_state & VBOXGUEST_STATE_INITOK) == 0)
+        return 0;
+
+    /*
+     * Reverse what we did in VBoxGuestNetBSDAttach.
+     */
+
+    VBoxGuestNetBSDRemoveIRQ(vboxguest);
+
+    VGDrvCommonDeleteDevExt(&g_DevExt);
+
+    bus_space_unmap(vboxguest->iVMMDevMemResId, vboxguest->VMMDevMemHandle, vboxguest->VMMDevMemSize);
+    bus_space_unmap(vboxguest->io_tag, vboxguest->io_handle, vboxguest->io_regsize);
+
+    RTR0Term();
+
+    return 0;
+}
+
+
+/**
+ * Removes IRQ for VMMDev.
+ *
+ * @param   vboxguest  Opaque pointer to the state info structure.
+ */
+static void VBoxGuestNetBSDRemoveIRQ(vboxguest_softc *vboxguest)
+{
+    LogFlow((DEVICE_NAME ": %s\n", __func__));
+
+    if (vboxguest->pfnIrqHandler)
+    {
+        pci_intr_disestablish(vboxguest->pc, vboxguest->pfnIrqHandler);
+    }
+}
+
+
+/**
+ * Interrupt service routine.
+ *
+ * @returns Whether the interrupt was from VMMDev.
+ * @param   pvState Opaque pointer to the device state.
+ */
+static int VBoxGuestNetBSDISR(void *pvState)
+{
+    LogFlow((DEVICE_NAME ": %s: pvState=%p\n", __func__, pvState));
+
+    bool fOurIRQ = VGDrvCommonISR(&g_DevExt);
+
+    return fOurIRQ ? 0 : 1;
+}
+
+
+/*
+ * Called by VGDrvCommonISR() if mouse position changed
+ */
+void VGDrvNativeISRMousePollEvent(PVBOXGUESTDEVEXT pDevExt)
+{
+    LogFlow((DEVICE_NAME ": %s\n", __func__));
+
+    /*
+     * Wake up poll waiters.
+     */
+    selnotify(&g_SelInfo, 0, 0);
+}
+
 
 /**
  * File open handler
@@ -411,221 +645,6 @@ static int VBoxGuestNetBSDPoll(struct file *fp, int events)
     return events_processed;
 }
 
-static int VBoxGuestNetBSDDetach(device_t self, int flags)
-{
-    vboxguest_softc *vboxguest;
-    vboxguest = device_private(self);
-
-    LogFlow((DEVICE_NAME ": %s\n", __func__));
-
-    if (cUsers > 0)
-        return EBUSY;
-
-    if ((vboxguest->vboxguest_state & VBOXGUEST_STATE_INITOK) == 0)
-        return 0;
-
-    /*
-     * Reverse what we did in VBoxGuestNetBSDAttach.
-     */
-
-    VBoxGuestNetBSDRemoveIRQ(vboxguest);
-
-    VGDrvCommonDeleteDevExt(&g_DevExt);
-
-    bus_space_unmap(vboxguest->iVMMDevMemResId, vboxguest->VMMDevMemHandle, vboxguest->VMMDevMemSize);
-    bus_space_unmap(vboxguest->io_tag, vboxguest->io_handle, vboxguest->io_regsize);
-
-    RTR0Term();
-
-    return 0;
-}
-
-/**
- * Interrupt service routine.
- *
- * @returns Whether the interrupt was from VMMDev.
- * @param   pvState Opaque pointer to the device state.
- */
-static int VBoxGuestNetBSDISR(void *pvState)
-{
-    LogFlow((DEVICE_NAME ": %s: pvState=%p\n", __func__, pvState));
-
-    bool fOurIRQ = VGDrvCommonISR(&g_DevExt);
-
-    return fOurIRQ ? 0 : 1;
-}
-
-void VGDrvNativeISRMousePollEvent(PVBOXGUESTDEVEXT pDevExt)
-{
-    LogFlow((DEVICE_NAME ": %s\n", __func__));
-
-    /*
-     * Wake up poll waiters.
-     */
-    selnotify(&g_SelInfo, 0, 0);
-}
-
-/**
- * Sets IRQ for VMMDev.
- *
- * @returns NetBSD error code.
- * @param   vboxguest  Pointer to the state info structure.
- * @param   pa  Pointer to the PCI attach arguments.
- */
-static int VBoxGuestNetBSDAddIRQ(vboxguest_softc *vboxguest, struct pci_attach_args *pa)
-{
-    int iResId = 0;
-    int rc = 0;
-    const char *intrstr;
-#if __NetBSD_Prereq__(6, 99, 39)
-    char intstrbuf[100];
-#endif
-
-    LogFlow((DEVICE_NAME ": %s\n", __func__));
-
-    if (pci_intr_map(pa, &vboxguest->ih))
-    {
-        aprint_error_dev(vboxguest->sc_dev, "couldn't map interrupt.\n");
-        return VERR_DEV_IO_ERROR;
-    }
-
-    intrstr = pci_intr_string(vboxguest->pc, vboxguest->ih
-#if __NetBSD_Prereq__(6, 99, 39)
-                              , intstrbuf, sizeof(intstrbuf)
-#endif
-                              );
-    aprint_normal_dev(vboxguest->sc_dev, "interrupting at %s\n", intrstr);
-
-    vboxguest->pfnIrqHandler = pci_intr_establish(vboxguest->pc, vboxguest->ih, IPL_BIO, VBoxGuestNetBSDISR, vboxguest);
-    if (vboxguest->pfnIrqHandler == NULL)
-    {
-        aprint_error_dev(vboxguest->sc_dev, "couldn't establish interrupt\n");
-        return VERR_DEV_IO_ERROR;
-    }
-
-    return VINF_SUCCESS;
-}
-
-/**
- * Removes IRQ for VMMDev.
- *
- * @param   vboxguest  Opaque pointer to the state info structure.
- */
-static void VBoxGuestNetBSDRemoveIRQ(vboxguest_softc *vboxguest)
-{
-    LogFlow((DEVICE_NAME ": %s\n", __func__));
-
-    if (vboxguest->pfnIrqHandler)
-    {
-        pci_intr_disestablish(vboxguest->pc, vboxguest->pfnIrqHandler);
-    }
-}
-
-static void VBoxGuestNetBSDAttach(device_t parent, device_t self, void *aux)
-{
-    int rc = VINF_SUCCESS;
-    int iResId = 0;
-    vboxguest_softc *vboxguest;
-    struct pci_attach_args *pa = aux;
-    bus_space_tag_t iot, memt;
-    bus_space_handle_t ioh, memh;
-    bus_dma_segment_t seg;
-    int ioh_valid, memh_valid;
-
-    cUsers = 0;
-
-    aprint_normal(": VirtualBox Guest\n");
-
-    vboxguest = device_private(self);
-    vboxguest->sc_dev = self;
-
-    /*
-     * Initialize IPRT R0 driver, which internally calls OS-specific r0 init.
-     */
-    rc = RTR0Init(0);
-    if (RT_FAILURE(rc))
-    {
-        LogFunc(("RTR0Init failed.\n"));
-        aprint_error_dev(vboxguest->sc_dev, "RTR0Init failed\n");
-        return;
-    }
-
-    vboxguest->pc = pa->pa_pc;
-
-    /*
-     * Allocate I/O port resource.
-     */
-    ioh_valid = (pci_mapreg_map(pa, PCI_MAPREG_START, PCI_MAPREG_TYPE_IO, 0, &vboxguest->io_tag, &vboxguest->io_handle, &vboxguest->uIOPortBase, &vboxguest->io_regsize) == 0);
-
-    if (ioh_valid)
-    {
-
-        /*
-         * Map the MMIO region.
-         */
-        memh_valid = (pci_mapreg_map(pa, PCI_MAPREG_START+4, PCI_MAPREG_TYPE_MEM, BUS_SPACE_MAP_LINEAR, &vboxguest->iVMMDevMemResId, &vboxguest->VMMDevMemHandle, &vboxguest->pMMIOBase, &vboxguest->VMMDevMemSize) == 0);
-        if (memh_valid)
-        {
-            /*
-             * Call the common device extension initializer.
-             */
-            rc = VGDrvCommonInitDevExt(&g_DevExt, vboxguest->uIOPortBase,
-                    bus_space_vaddr(vboxguest->iVMMDevMemResId,
-                            vboxguest->VMMDevMemHandle),
-                                       vboxguest->VMMDevMemSize,
-#if ARCH_BITS == 64
-                                       VBOXOSTYPE_NetBSD_x64,
-#else
-                                       VBOXOSTYPE_NetBSD,
-#endif
-                                       VMMDEV_EVENT_MOUSE_POSITION_CHANGED);
-            if (RT_SUCCESS(rc))
-            {
-                /*
-                 * Add IRQ of VMMDev.
-                 */
-                rc = VBoxGuestNetBSDAddIRQ(vboxguest, pa);
-                if (RT_SUCCESS(rc))
-                {
-                    vboxguest->vboxguest_state |= VBOXGUEST_STATE_INITOK;
-                    return;
-                }
-                VGDrvCommonDeleteDevExt(&g_DevExt);
-            }
-            else
-            {
-                aprint_error_dev(vboxguest->sc_dev, "init failed\n");
-            }
-            bus_space_unmap(vboxguest->iVMMDevMemResId, vboxguest->VMMDevMemHandle, vboxguest->VMMDevMemSize);
-        }
-        else
-        {
-            aprint_error_dev(vboxguest->sc_dev, "MMIO mapping failed\n");
-        }
-        bus_space_unmap(vboxguest->io_tag, vboxguest->io_handle, vboxguest->io_regsize);
-    }
-    else
-    {
-        aprint_error_dev(vboxguest->sc_dev, "IO mapping failed\n");
-    }
-
-    RTR0Term();
-    return;
-}
-
-static int
-VBoxGuestNetBSDMatch(device_t parent, cfdata_t match, void *aux)
-{
-    const struct pci_attach_args *pa = aux;
-
-    if (   PCI_VENDOR(pa->pa_id) == VMMDEV_VENDORID
-        && PCI_PRODUCT(pa->pa_id) == VMMDEV_DEVICEID)
-    {
-        return 1;
-    }
-
-    return 0;
-}
 
 /**
  * @note This code is duplicated on other platforms with variations, so please
@@ -670,8 +689,6 @@ int VBOXCALL VBoxGuestIDC(void *pvSession, uintptr_t uReq, PVBGLREQHDR pReqHdr, 
     return rc;
 }
 
-CFATTACH_DECL_NEW(vboxguest, sizeof(vboxguest_softc),
-    VBoxGuestNetBSDMatch, VBoxGuestNetBSDAttach, VBoxGuestNetBSDDetach, NULL);
 
 MODULE(MODULE_CLASS_DRIVER, vboxguest, "pci");
 
