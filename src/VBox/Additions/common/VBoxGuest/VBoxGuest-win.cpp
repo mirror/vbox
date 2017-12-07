dba3fc70969e909d4e3d1694050e696f67db0019
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2017-12-07 20:58:40 +0000

    I can't read.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@69999 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Additions/common/VBoxGuest/VBoxGuest-win.cpp
+++ src/VBox/Additions/common/VBoxGuest/VBoxGuest-win.cpp
@@ -534,7 +534,7 @@ NTSTATUS vgdrvNtInit(PDRIVER_OBJECT pDrvObj, PDEVICE_OBJECT pDevObj, PUNICODE_ST
 
     if (NT_SUCCESS(rcNt))
     {
-        ULONG uValue = 0;
+        ULONG uValue = 0x123;
         NTSTATUS rcNt2 = vgdrvNtRegistryReadDWORD(RTL_REGISTRY_SERVICES, L"VBoxGuest", L"LoggingEnabled", &uValue);
         if (NT_SUCCESS(rcNt2))
         {
@@ -542,8 +542,6 @@ NTSTATUS vgdrvNtInit(PDRIVER_OBJECT pDrvObj, PDEVICE_OBJECT pDevObj, PUNICODE_ST
             if (pDevExt->Core.fLoggingEnabled)
                 LogRelFunc(("Logging to host log enabled (%#x)", uValue));
         }
-        else
-            pDevExt->Core.fLoggingEnabled = true;
 
         /* Ready to rumble! */
         LogRelFunc(("Device is ready!\n"));
