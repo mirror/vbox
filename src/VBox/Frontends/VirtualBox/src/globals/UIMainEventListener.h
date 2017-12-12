c8d23b5be5d24130d5e3701a36ac6055335e44ce
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2017-12-12 12:06:50 +0000

    FE/Qt: bugref:9038: Another small fix for r119623: Adjusting doxy (s.a. r119626).
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@70079 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Frontends/VirtualBox/src/globals/UIMainEventListener.h
+++ src/VBox/Frontends/VirtualBox/src/globals/UIMainEventListener.h
@@ -87,8 +87,8 @@ signals:
     /** Notifies about mouse capability change to @a fSupportsAbsolute, @a fSupportsRelative, @a fSupportsMultiTouch and @a fNeedsHostCursor. */
     void sigMouseCapabilityChange(bool fSupportsAbsolute, bool fSupportsRelative, bool fSupportsMultiTouch, bool fNeedsHostCursor);
     /** Notifies about guest request to change the cursor position to @a uX * @a uY.
-      * @note If @a fData is NOT set this merely notifies that the guest supports this. */
-    void sigCursorPositionChange(bool fData, unsigned long uX, unsigned long uY);
+      * @param  fContainsData  Brings whether the @a uX and @a uY values are valid and could be used by the GUI now. */
+    void sigCursorPositionChange(bool fContainsData, unsigned long uX, unsigned long uY);
     /** Notifies about keyboard LEDs change for @a fNumLock, @a fCapsLock and @a fScrollLock. */
     void sigKeyboardLedsChangeEvent(bool fNumLock, bool fCapsLock, bool fScrollLock);
     /** Notifies about machine @a state change. */
