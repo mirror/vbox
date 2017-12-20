274e870f50c4bb31e04979be1401e6b3e50f9e1a
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2017-12-20 18:12:25 +0000

    Build fix.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@70248 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Devices/Audio/HDAStream.h
+++ src/VBox/Devices/Audio/HDAStream.h
@@ -195,6 +195,7 @@ typedef struct HDASTREAM
     /** Current channel index.
      *  For a stereo stream, this is u8Channel + 1. */
     uint8_t                  u8Channel;
+    uint8_t                  Padding0[6];
     /** DMA base address (SDnBDPU - SDnBDPL). */
     uint64_t                 u64BDLBase;
     /** Cyclic Buffer Length (SDnCBL).
