ef2fc1bad03c114d264b6925f4322aafd46da93c
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2018-01-05 15:24:56 +0000

    Audio/VBoxMMNotificationClient.cpp: Set m_fRegisteredClient if we successfully registered a notification client.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@70470 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Devices/Audio/VBoxMMNotificationClient.cpp
+++ src/VBox/Devices/Audio/VBoxMMNotificationClient.cpp
@@ -5,7 +5,7 @@
  */
 
 /*
- * Copyright (C) 2017 Oracle Corporation
+ * Copyright (C) 2017-2018 Oracle Corporation
  *
  * This file is part of VirtualBox Open Source Edition (OSE), as
  * available from http://www.virtualbox.org. This file is free software;
@@ -63,6 +63,8 @@ HRESULT VBoxMMNotificationClient::Initialize(void)
         hr = m_pEnum->RegisterEndpointNotificationCallback(this);
         if (SUCCEEDED(hr))
         {
+            m_fRegisteredClient = true;
+
             hr = AttachToDefaultEndpoint();
         }
     }
