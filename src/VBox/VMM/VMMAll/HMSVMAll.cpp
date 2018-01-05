cdda9a7376ac8e9d870b392e8814e207323bf321
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2018-01-05 09:23:54 +0000

    VMM/HM: Comment.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@70463 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/VMM/VMMAll/HMSVMAll.cpp
+++ src/VBox/VMM/VMMAll/HMSVMAll.cpp
@@ -179,6 +179,9 @@ VMM_INT_DECL(void) HMSvmNstGstVmExitNotify(PVMCPU pVCpu, PCPUMCTX pCtx)
  *
  * @returns @c true if VGIF is enabled, @c false otherwise.
  * @param   pVM         The cross context VM structure.
+ *
+ * @remarks This value returned by this functions is expected by the callers not
+ *          to change throughout the lifetime of the VM.
  */
 VMM_INT_DECL(bool) HMSvmIsVGifActive(PVM pVM)
 {
