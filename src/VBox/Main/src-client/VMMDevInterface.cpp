fb9c4a418b7eb7f76b9a45502114203536e6ef30
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2018-01-16 15:36:41 +0000

    Main: Minor HGCM fixes, ?bugref:9064
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@70599 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Main/src-client/VMMDevInterface.cpp
+++ src/VBox/Main/src-client/VMMDevInterface.cpp
@@ -616,9 +616,15 @@ static DECLCALLBACK(int) iface_hgcmConnect(PPDMIHGCMCONNECTOR pInterface, PVBOXH
         return VERR_INVALID_PARAMETER;
     }
 
+    /* Check if service name is a string terminated by zero*/
+    size_t cchInfo = 0;
+    if (RTStrNLenEx(pServiceLocation->u.host.achName, sizeof(pServiceLocation->u.host.achName), &cchInfo) != VINF_SUCCESS)
+    {
+        return VERR_INVALID_PARAMETER;
+    }
+
     if (!pDrv->pVMMDev || !pDrv->pVMMDev->hgcmIsActive())
         return VERR_INVALID_STATE;
-
     return HGCMGuestConnect(pDrv->pHGCMPort, pCmd, pServiceLocation->u.host.achName, pu32ClientID);
 }
 
