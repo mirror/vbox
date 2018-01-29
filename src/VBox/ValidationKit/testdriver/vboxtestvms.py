97f74e4b61bdd544ca5450af8bc2c39a0d0b21ff
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2018-01-29 12:32:16 +0000

    vboxtestvms.py: upates
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@70791 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/ValidationKit/testdriver/vboxtestvms.py
+++ src/VBox/ValidationKit/testdriver/vboxtestvms.py
@@ -1105,19 +1105,24 @@ class TestVmManager(object):
         #       sKind = 'Windows10_64', acCpusSup = range(1, 33), fIoApic = True, sFirmwareType = 'efi', sChipsetType = 'ich9'),
 
         # DOS and Old Windows.
-        AncientTestVm('tst-dos20',              sKind = 'DOS',  sHd = '5.2/great-old-ones/t-dos20/t-dos20.vdi'),
+        AncientTestVm('tst-dos20',              sKind = 'DOS',
+                      sHd = '5.2/great-old-ones/t-dos20/t-dos20.vdi'),
         AncientTestVm('tst-dos401-win30me',     sKind = 'DOS',
                       sHd = '5.2/great-old-ones/t-dos401-win30me/t-dos401-win30me.vdi',                 cMBRamMax = 4),
         AncientTestVm('tst-dos401-emm386-win30me', sKind = 'DOS',
                       sHd = '5.2/great-old-ones/t-dos401-emm386-win30me/t-dos401-emm386-win30me.vdi',   cMBRamMax = 4),
+        AncientTestVm('tst-dos50-win31',        sKind = 'DOS',
+                      sHd = '5.2/great-old-ones/t-dos50-win31/t-dos50-win31.vdi'),
+        AncientTestVm('tst-dos50-emm386-win31', sKind = 'DOS',
+                      sHd = '5.2/great-old-ones/t-dos50-emm386-win31/t-dos50-emm386-win31.vdi'),
+        AncientTestVm('tst-dos622',             sKind = 'DOS',
+                      sHd = '5.2/great-old-ones/t-dos622/t-dos622.vdi'),
+        AncientTestVm('tst-dos622-emm386',      sKind = 'DOS',
+                      sHd = '5.2/great-old-ones/t-dos622-emm386/t-dos622-emm386.vdi'),
+        AncientTestVm('tst-dos71',              sKind = 'DOS',
+                      sHd = '5.2/great-old-ones/t-dos71/t-dos71.vdi'),
 
-        AncientTestVm('tst-dos622',             sKind = 'DOS',  sHd = '5.2/great-old-ones/t-dos622/t-dos622.vdi'),
-        AncientTestVm('tst-dos622-emm386',      sKind = 'DOS',  sHd = '5.2/great-old-ones/t-dos622-emm386/t-dos622-emm386.vdi'),
-
-
-        AncientTestVm('tst-dos71',              sKind = 'DOS',  sHd = '5.2/great-old-ones/t-dos71/t-dos71.vdi'),
-
-        AncientTestVm('tst-dos5-win311a',       sKind = 'DOS',  sHd = '5.2/great-old-ones/t-dos5-win311a/t-dos5-win311a.vdi'),
+        #AncientTestVm('tst-dos5-win311a',       sKind = 'DOS',  sHd = '5.2/great-old-ones/t-dos5-win311a/t-dos5-win311a.vdi'),
     );
 
 
