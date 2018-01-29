95f61abf4f8ea61fe289dcda049f626f1172059b
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2018-01-29 10:57:10 +0000

    linux/shared folders: use indent to switch Linux-only files to kernel style. bugref:9109: Shared folders: update to match in-kernel code more closely
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@70786 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Additions/linux/sharedfolders/vbsfmount.h
+++ src/VBox/Additions/linux/sharedfolders/vbsfmount.h
@@ -25,50 +25,48 @@
 #define VBSF_MOUNT_SIGNATURE_BYTE_1 '\376'
 #define VBSF_MOUNT_SIGNATURE_BYTE_2 '\375'
 
-struct vbsf_mount_info_new
-{
-    /*
-     * The old version of the mount_info struct started with a
-     * char name[MAX_HOST_NAME] field, where name cannot be '\0'.
-     * So the new version of the mount_info struct starts with a
-     * nullchar field which is always 0 so that we can detect and
-     * reject the old structure being passed.
-     */
-    char nullchar;
-    char signature[3];          /* signature */
-    int  length;                /* length of the whole structure */
-    char name[MAX_HOST_NAME];   /* share name */
-    char nls_name[MAX_NLS_NAME];/* name of an I/O charset */
-    int  uid;                   /* user ID for all entries, default 0=root */
-    int  gid;                   /* group ID for all entries, default 0=root */
-    int  ttl;                   /* time to live */
-    int  dmode;                 /* mode for directories if != 0xffffffff */
-    int  fmode;                 /* mode for regular files if != 0xffffffff */
-    int  dmask;                 /* umask applied to directories */
-    int  fmask;                 /* umask applied to regular files */
+struct vbsf_mount_info_new {
+	/*
+	 * The old version of the mount_info struct started with a
+	 * char name[MAX_HOST_NAME] field, where name cannot be '\0'.
+	 * So the new version of the mount_info struct starts with a
+	 * nullchar field which is always 0 so that we can detect and
+	 * reject the old structure being passed.
+	 */
+	char nullchar;
+	char signature[3];	/* signature */
+	int length;		/* length of the whole structure */
+	char name[MAX_HOST_NAME];	/* share name */
+	char nls_name[MAX_NLS_NAME];	/* name of an I/O charset */
+	int uid;		/* user ID for all entries, default 0=root */
+	int gid;		/* group ID for all entries, default 0=root */
+	int ttl;		/* time to live */
+	int dmode;		/* mode for directories if != 0xffffffff */
+	int fmode;		/* mode for regular files if != 0xffffffff */
+	int dmask;		/* umask applied to directories */
+	int fmask;		/* umask applied to regular files */
 };
 
-struct vbsf_mount_opts
-{
-    int  uid;
-    int  gid;
-    int  ttl;
-    int  dmode;
-    int  fmode;
-    int  dmask;
-    int  fmask;
-    int  ronly;
-    int  sloppy;
-    int  noexec;
-    int  nodev;
-    int  nosuid;
-    int  remount;
-    char nls_name[MAX_NLS_NAME];
-    char *convertcp;
+struct vbsf_mount_opts {
+	int uid;
+	int gid;
+	int ttl;
+	int dmode;
+	int fmode;
+	int dmask;
+	int fmask;
+	int ronly;
+	int sloppy;
+	int noexec;
+	int nodev;
+	int nosuid;
+	int remount;
+	char nls_name[MAX_NLS_NAME];
+	char *convertcp;
 };
 
 /** Completes the mount operation by adding the new mount point to mtab if required. */
 int vbsfmount_complete(const char *host_name, const char *mount_point,
-                       unsigned long flags, struct vbsf_mount_opts *opts);
+		       unsigned long flags, struct vbsf_mount_opts *opts);
 
-#endif /* vbsfmount.h */
+#endif				/* vbsfmount.h */
