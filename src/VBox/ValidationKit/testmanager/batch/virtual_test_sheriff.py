51dc78e7cbb5532d43bace84e988063ccf0f75d6
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2018-02-01 10:31:00 +0000

    ValidationKit: adapt vsheriff to renamed "misc" unit tests (the collection of all compiled unit tests)
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@70847 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/ValidationKit/testmanager/batch/virtual_test_sheriff.py
+++ src/VBox/ValidationKit/testmanager/batch/virtual_test_sheriff.py
@@ -143,7 +143,7 @@ class VirtualTestSheriffCaseFile(object):
     def isVBoxUnitTest(self):
         """ Test case classification: The unit test doing all our testcase/*.cpp stuff. """
         return self.isVBoxTest() \
-           and self.oTestCase.sName.lower() == 'unit tests';
+           and (self.oTestCase.sName.lower() == 'unit tests' or self.oTestCase.sName.lower() == 'misc: unit tests');
 
     def isVBoxInstallTest(self):
         """ Test case classification: VirtualBox Guest installation test. """
