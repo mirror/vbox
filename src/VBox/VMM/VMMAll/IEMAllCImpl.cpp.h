41b2bb22149a59e54d22e006b7f5b539df9c107a
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2018-02-06 10:53:13 +0000

    VMM/IEM: Don't look at nested SVM state when it isn't there.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@70879 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/VMM/VMMAll/IEMAllCImpl.cpp.h
+++ src/VBox/VMM/VMMAll/IEMAllCImpl.cpp.h
@@ -5088,11 +5088,14 @@ IEM_CIMPL_DEF_2(iemCImpl_mov_Rd_Cd, uint8_t, iGReg, uint8_t, iCrReg)
         case 8:
         {
 #ifdef VBOX_WITH_NESTED_HWVIRT
-            PCSVMVMCBCTRL pVmcbCtrl = &pCtx->hwvirt.svm.CTX_SUFF(pVmcb)->ctrl;
-            if (pVmcbCtrl->IntCtrl.n.u1VIntrMasking)
+            if (CPUMIsGuestInSvmNestedHwVirtMode(pCtx))
             {
-                crX = pVmcbCtrl->IntCtrl.n.u8VTPR;
-                break;
+                PCSVMVMCBCTRL pVmcbCtrl = &pCtx->hwvirt.svm.CTX_SUFF(pVmcb)->ctrl;
+                if (pVmcbCtrl->IntCtrl.n.u1VIntrMasking)
+                {
+                    crX = pVmcbCtrl->IntCtrl.n.u8VTPR;
+                    break;
+                }
             }
 #endif
             uint8_t uTpr;
