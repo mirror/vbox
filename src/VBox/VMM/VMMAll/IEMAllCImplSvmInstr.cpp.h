642ab40c7a194c3159087813464c61effa92e19a
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2018-02-19 09:21:43 +0000

    VMM/IEM: Nested Hw.virt: Fix decode-assist fetching 15 bytes for SVM intercepted page-faults. Still a couple of todos left.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@71048 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/VMM/VMMAll/IEMAllCImplSvmInstr.cpp.h
+++ src/VBox/VMM/VMMAll/IEMAllCImplSvmInstr.cpp.h
@@ -962,11 +962,15 @@ IEM_STATIC VBOXSTRICTRC iemHandleSvmEventIntercept(PVMCPU pVCpu, PCPUMCTX pCtx, 
             PSVMVMCBCTRL  pVmcbCtrl = &pCtx->hwvirt.svm.CTX_SUFF(pVmcb)->ctrl;
             uint8_t const offOpCode = pVCpu->iem.s.offOpcode;
             uint8_t const cbCurrent = pVCpu->iem.s.cbOpcode - pVCpu->iem.s.offOpcode;
-            if (   cbCurrent > 0
-                && cbCurrent < sizeof(pVmcbCtrl->abInstr))
+            if (cbCurrent >= SVM_CTRL_GUEST_INSTR_BYTES_MAX)
             {
-                Assert(cbCurrent <= sizeof(pVCpu->iem.s.abOpcode));
-                memcpy(&pVmcbCtrl->abInstr[0], &pVCpu->iem.s.abOpcode[offOpCode], cbCurrent);
+                Assert(cbCurrent <= RT_ELEMENTS(pVCpu->iem.s.abOpcode));
+                memcpy(&pVmcbCtrl->abInstr[0], &pVCpu->iem.s.abOpcode[offOpCode], SVM_CTRL_GUEST_INSTR_BYTES_MAX);
+            }
+            else
+            {
+                /** @todo fetch 15 bytes from CS:RIP and stop fetching on exceptions or CS
+                 *        limit is exceeded. */
             }
 #endif
         }
