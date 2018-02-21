9f291d5137b1a0d0be47b24e38f748d844717354
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2018-02-21 12:54:01 +0000

    VMM,SUPDrv: More NEM/win page hacking. bugref:9044 [build fix]
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@71083 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/VMM/VMMR3/VMM.cpp
+++ src/VBox/VMM/VMMR3/VMM.cpp
@@ -2494,12 +2494,12 @@ VMMR3DECL(int) VMMR3CallR0(PVM pVM, uint32_t uOperation, uint64_t u64Arg, PSUPVM
  * Wrapper for SUPR3CallVMMR0Ex which will deal with VINF_VMM_CALL_HOST returns.
  *
  * @returns VBox status code.
- * @param   pVM         The cross context VM structure.
- * @param   pVCpu       The cross context VM structure.
- * @param   uOperation  Operation to execute.
- * @param   u64Arg      Constant argument.
- * @param   pReqHdr     Pointer to a request header. See SUPR3CallVMMR0Ex for
- *                      details.
+ * @param   pVM             The cross context VM structure.
+ * @param   pVCpu           The cross context VM structure.
+ * @param   enmOperation    Operation to execute.
+ * @param   u64Arg          Constant argument.
+ * @param   pReqHdr         Pointer to a request header. See SUPR3CallVMMR0Ex for
+ *                          details.
  */
 VMMR3_INT_DECL(int) VMMR3CallR0Emt(PVM pVM, PVMCPU pVCpu, VMMR0OPERATION enmOperation, uint64_t u64Arg, PSUPVMMR0REQHDR pReqHdr)
 {
