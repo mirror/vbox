d0c22ffc180478b2c7364bec6c97796a599e2dae
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2018-03-02 10:03:22 +0000

    FE/Qt bugref:6699 No C++11 keywords yet!
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@71170 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Frontends/VirtualBox/src/runtime/information/guestctrl/UIGuestControlFileTable.h
+++ src/VBox/Frontends/VirtualBox/src/runtime/information/guestctrl/UIGuestControlFileTable.h
@@ -48,15 +48,15 @@ public:
     explicit UIGuestControlFileModel(QObject *parent = 0);
     ~UIGuestControlFileModel();
 
-    QVariant data(const QModelIndex &index, int role) const override;
-    Qt::ItemFlags flags(const QModelIndex &index) const override;
+    QVariant data(const QModelIndex &index, int role) const /* override */;
+    Qt::ItemFlags flags(const QModelIndex &index) const /* override */;
     QVariant headerData(int section, Qt::Orientation orientation,
-                        int role = Qt::DisplayRole) const override;
+    int role = Qt::DisplayRole) const /* override */;
     QModelIndex index(int row, int column,
-                      const QModelIndex &parent = QModelIndex()) const override;
-    QModelIndex parent(const QModelIndex &index) const override;
-    int rowCount(const QModelIndex &parent = QModelIndex()) const override;
-    int columnCount(const QModelIndex &parent = QModelIndex()) const override;
+    const QModelIndex &parent = QModelIndex()) const /* override */;
+    QModelIndex parent(const QModelIndex &index) const /* override */;
+    int rowCount(const QModelIndex &parent = QModelIndex()) const /* override */;
+    int columnCount(const QModelIndex &parent = QModelIndex()) const /* override */;
     void signalUpdate();
 
 private:
