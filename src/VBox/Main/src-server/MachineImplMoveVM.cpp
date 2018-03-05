3957d23f54c177b53e0fab8705f4b2b3b119496d
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2018-03-05 10:55:30 +0000

    bugref:8345. The trailing blanks were removed.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@71197 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Main/src-server/MachineImplMoveVM.cpp
+++ src/VBox/Main/src-server/MachineImplMoveVM.cpp
@@ -554,7 +554,7 @@ void MachineMoveVM::i_MoveVMThreadTask(MachineMoveVM* task)
      * A way to handle shareable disk:
      * Collect the shareable disks attched to the VM.
      * Get the machines whom the shareable disks attach to.
-     * Return an error if the state of any VM doesn't allow to move a shareable disk and 
+     * Return an error if the state of any VM doesn't allow to move a shareable disk and
      * this disk is located in the VM's folder (it means the disk is intended for "moving").
      */
 
@@ -647,7 +647,7 @@ void MachineMoveVM::i_MoveVMThreadTask(MachineMoveVM* task)
         }
 
         /*
-         * Moving Machine settings file 
+         * Moving Machine settings file
          * The settings file are moved after all disks and snapshots because this file should be updated
          * with actual information and only then should be moved.
          */
