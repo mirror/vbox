1b20dd1fef43fba4d6c8b8de2c1faeac5a1269c6
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2018-03-05 15:53:15 +0000

    VBoxNetDHCP: Add explicit comment to previous about nameserver 0.0.0.0 being valid.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@71205 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/NetworkServices/DHCP/VBoxNetDHCP.cpp
+++ src/VBox/NetworkServices/DHCP/VBoxNetDHCP.cpp
@@ -645,6 +645,11 @@ int VBoxNetDhcp::hostDnsServers(const ComHostPtr& host,
 
         if (addr.u == INADDR_ANY)
         {
+            /*
+             * This doesn't seem to be very well documented except for
+             * RTFS of res_init.c, but INADDR_ANY is a valid value for
+             * for "nameserver".
+             */
             addr.u = RT_H2N_U32_C(INADDR_LOOPBACK);
         }
 
