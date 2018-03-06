c0529262ab8f349c7c7fdfeca2944d169efb04e8
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2018-03-06 13:17:55 +0000

    Doxygen build fix.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@71242 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Main/src-client/GuestSessionImplTasks.cpp
+++ src/VBox/Main/src-client/GuestSessionImplTasks.cpp
@@ -652,7 +652,7 @@ SessionTaskCopyDirTo::~SessionTaskCopyDirTo(void)
  * @param   strSource               Source directory on the host to copy to the guest.
  * @param   strFilter               DOS-style wildcard filter (?, *).  Optional.
  * @param   strDest                 Destination directory on the guest.
- * @param   fRecursive,             Whether to recursively copy the directory contents or not.
+ * @param   fRecursive              Whether to recursively copy the directory contents or not.
  * @param   fFollowSymlinks         Whether to follow symlinks or not.
  * @param   strSubDir               Current sub directory to handle. Needs to NULL and only
  *                                  is needed for recursion.
