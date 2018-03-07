5a10e3d5cd0eaa70eb855d1bc9c49f9e4c78995c
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2018-03-07 11:38:49 +0000

    Build fix.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@71253 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Main/src-client/GuestSessionImpl.cpp
+++ src/VBox/Main/src-client/GuestSessionImpl.cpp
@@ -2451,7 +2451,7 @@ HRESULT GuestSession::fileCopyFromGuest(const com::Utf8Str &aSource, const com::
     if (RT_UNLIKELY((aDest.c_str()) == NULL || *(aDest.c_str()) == '\0'))
         return setError(E_INVALIDARG, tr("No destination specified"));
 
-    uint32_t fFlags = FileCopyFlag_None;
+    FileCopyFlag_T fFlags = FileCopyFlag_None;
     if (aFlags.size())
     {
         for (size_t i = 0; i < aFlags.size(); i++)
@@ -2526,7 +2526,7 @@ HRESULT GuestSession::fileCopyToGuest(const com::Utf8Str &aSource, const com::Ut
     if (RT_UNLIKELY((aDest.c_str()) == NULL || *(aDest.c_str()) == '\0'))
         return setError(E_INVALIDARG, tr("No destination specified"));
 
-    uint32_t fFlags = FileCopyFlag_None;
+    FileCopyFlag_T fFlags = FileCopyFlag_None;
     if (aFlags.size())
     {
         for (size_t i = 0; i < aFlags.size(); i++)
