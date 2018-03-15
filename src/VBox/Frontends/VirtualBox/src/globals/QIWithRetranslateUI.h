4dcc46867c0d7ea347ba904275487b992be1aa79
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2018-03-15 14:27:38 +0000

    FE/Qt: bugref:9049: A bit of refactoring and cleanup for QIWithRetranslateUI templates.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@71354 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Frontends/VirtualBox/src/globals/QIWithRetranslateUI.h
+++ src/VBox/Frontends/VirtualBox/src/globals/QIWithRetranslateUI.h
@@ -4,7 +4,7 @@
  */
 
 /*
- * Copyright (C) 2008-2017 Oracle Corporation
+ * Copyright (C) 2008-2018 Oracle Corporation
  *
  * This file is part of VirtualBox Open Source Edition (OSE), as
  * available from http://www.virtualbox.org. This file is free software;
@@ -15,27 +15,34 @@
  * hope that it will be useful, but WITHOUT ANY WARRANTY of any kind.
  */
 
-#ifndef __QIWithRetranslateUI_h
-#define __QIWithRetranslateUI_h
+#ifndef ___QIWithRetranslateUI_h___
+#define ___QIWithRetranslateUI_h___
 
-/* Global includes */
+/* Qt includes: */
 #include <QApplication>
 #include <QEvent>
-#include <QObject>
 #include <QGraphicsWidget>
+#include <QObject>
+#include <QWidget>
 
+
+/** Template for automatic language translations of underlying QWidget. */
 template <class Base>
-class QIWithRetranslateUI: public Base
+class QIWithRetranslateUI : public Base
 {
 public:
 
+    /** Constructs translatable widget passing @a pParent to the base-class. */
     QIWithRetranslateUI(QWidget *pParent = 0) : Base(pParent) {}
 
 protected:
 
+    /** Handles standard Qt change @a pEvent. */
     virtual void changeEvent(QEvent *pEvent)
     {
-        Base::changeEvent (pEvent);
+        /* Call to base-class: */
+        Base::changeEvent(pEvent);
+        /* Handle LanguageChange events: */
         switch (pEvent->type())
         {
             case QEvent::LanguageChange:
@@ -49,21 +56,28 @@ protected:
         }
     }
 
+    /** Handles translation event. */
     virtual void retranslateUi() = 0;
 };
 
+
+/** Template for automatic language translations of underlying QWidget with certain flags. */
 template <class Base>
-class QIWithRetranslateUI2: public Base
+class QIWithRetranslateUI2 : public Base
 {
 public:
 
+    /** Constructs translatable widget passing @a pParent and @a fFlags to the base-class. */
     QIWithRetranslateUI2(QWidget *pParent = 0, Qt::WindowFlags fFlags = 0) : Base(pParent, fFlags) {}
 
 protected:
 
+    /** Handles standard Qt change @a pEvent. */
     virtual void changeEvent(QEvent *pEvent)
     {
-        Base::changeEvent (pEvent);
+        /* Call to base-class: */
+        Base::changeEvent(pEvent);
+        /* Handle LanguageChange events: */
         switch (pEvent->type())
         {
             case QEvent::LanguageChange:
@@ -77,14 +91,18 @@ protected:
         }
     }
 
+    /** Handles translation event. */
     virtual void retranslateUi() = 0;
 };
 
+
+/** Template for automatic language translations of underlying QObject. */
 template <class Base>
-class QIWithRetranslateUI3: public Base
+class QIWithRetranslateUI3 : public Base
 {
 public:
 
+    /** Constructs translatable widget passing @a pParent to the base-class. */
     QIWithRetranslateUI3(QObject *pParent = 0)
         : Base(pParent)
     {
@@ -93,9 +111,10 @@ public:
 
 protected:
 
+    /** Pre-handles standard Qt @a pEvent for passed @a pObject. */
     virtual bool eventFilter(QObject *pObject, QEvent *pEvent)
     {
-        /* Handle events for qApp only: */
+        /* Handle LanguageChange events for qApp only: */
         if (pObject == qApp)
         {
             switch (pEvent->type())
@@ -108,14 +127,18 @@ protected:
         return Base::eventFilter(pObject, pEvent);
     }
 
+    /** Handles translation event. */
     virtual void retranslateUi() = 0;
 };
 
+
+/** Template for automatic language translations of underlying QGraphicsWidget. */
 template <class Base>
-class QIWithRetranslateUI4: public Base
+class QIWithRetranslateUI4 : public Base
 {
 public:
 
+    /** Constructs translatable widget passing @a pParent to the base-class. */
     QIWithRetranslateUI4(QGraphicsWidget *pParent = 0)
         : Base(pParent)
     {
@@ -124,23 +147,23 @@ public:
 
 protected:
 
+    /** Pre-handles standard Qt @a pEvent for passed @a pObject. */
     virtual bool eventFilter(QObject *pObject, QEvent *pEvent)
     {
+        /* Handle LanguageChange events: */
         switch (pEvent->type())
         {
-            case QEvent::LanguageChange:
-            {
-                retranslateUi();
-                break;
-            }
-            default:
-                break;
+            case QEvent::LanguageChange: retranslateUi(); break;
+            default: break;
         }
+        /* Call to base-class: */
         return Base::eventFilter(pObject, pEvent);
     }
 
+    /** Handles translation event. */
     virtual void retranslateUi() = 0;
 };
 
-#endif /* __QIWithRetranslateUI_h */
+
+#endif /* !___QIWithRetranslateUI_h___ */
 
