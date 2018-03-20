1fb6fb7fbe60acc721f6b95873574b7d8dd333aa
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2018-03-20 13:17:08 +0000

    FE/Qt: bugref:9049: Tiny cleanup for QIMainWindow.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@71400 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Frontends/VirtualBox/src/extensions/QIMainWindow.h
+++ src/VBox/Frontends/VirtualBox/src/extensions/QIMainWindow.h
@@ -4,7 +4,7 @@
  */
 
 /*
- * Copyright (C) 2016-2017 Oracle Corporation
+ * Copyright (C) 2016-2018 Oracle Corporation
  *
  * This file is part of VirtualBox Open Source Edition (OSE), as
  * available from http://www.virtualbox.org. This file is free software;
@@ -44,5 +44,5 @@ protected:
     QRect m_geometry;
 };
 
-#endif /* ___QIMainWindow_h___ */
+#endif /* !___QIMainWindow_h___ */
 
