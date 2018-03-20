8e61dafb8da467466bfe73db5b584168ff93bff8
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2018-03-20 10:37:27 +0000

    bugref:8345. Removed the trailing whitespaces.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@71390 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/ValidationKit/tests/api/tdMoveVM1.py
+++ src/VBox/ValidationKit/tests/api/tdMoveVM1.py
@@ -287,13 +287,13 @@ class SubTstDrvMoveVM1(base.SubTestDriverBase):
             fRc = oSession.close() and fRc
             if fRc is False:
                 reporter.log('Couldn\'t close machine session')
- 
+
             #Run VM and get new Session object
             oSession = self.oTstDrv.startVm(oMachine)
- 
+
             #some time interval should be here for not closing VM just after start
             time.sleep(1)
- 
+
             if oMachine.state != self.oTstDrv.oVBoxMgr.constants.MachineState_Running:
                 reporter.log("Machine '%s' is not Running" % (oMachine.name))
                 fRc = False
@@ -302,16 +302,16 @@ class SubTstDrvMoveVM1(base.SubTestDriverBase):
             fRc = fRc and oSession.saveState()
             self.oTstDrv.terminateVmBySession(oSession)
 
-            if fRc: 
+            if fRc:
                 sLoc = sMoveLoc + os.sep + oMachine.name + os.sep
                 sMoveLoc = os.path.join(sOrigLoc, 'moveFolder_4th_scenario')
                 os.mkdir(sMoveLoc, 0o775)
- 
+
                 #create a new Session object for moving VM
                 oSession = self.oTstDrv.openSession(oMachine)
                 fRc = self.moveVMToLocation(sMoveLoc, oSession.o.machine) and fRc
 
-                # cleaning up: get rid of saved state 
+                # cleaning up: get rid of saved state
                 fRc = fRc and oSession.o.machine.discardSavedState(True)
                 if fRc is False:
                     reporter.log('Failed to discard the saved state of machine')
