bd800c302d04368cc58e483f85ba678ad7b72fa9
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2018-03-21 13:19:58 +0000

    FE/Qt: bugref:9049: Preventive include fix for UIDnDHandler.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@71430 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Frontends/VirtualBox/src/runtime/UIDnDHandler.cpp
+++ src/VBox/Frontends/VirtualBox/src/runtime/UIDnDHandler.cpp
@@ -4,7 +4,7 @@
  */
 
 /*
- * Copyright (C) 2011-2017 Oracle Corporation
+ * Copyright (C) 2011-2018 Oracle Corporation
  *
  * This file is part of VirtualBox Open Source Edition (OSE), as
  * available from http://www.virtualbox.org. This file is free software;
@@ -21,11 +21,12 @@
 
 /* Qt includes: */
 #include <QApplication>
+#include <QDrag>
 #include <QKeyEvent>
 #include <QStringList>
 #include <QTimer>
-#include <QDrag>
 #include <QUrl>
+#include <QWidget>
 
 /* VirtualBox interface declarations: */
 #ifndef VBOX_WITH_XPCOM
