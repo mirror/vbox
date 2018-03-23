b28a49e1a1ba458869961764afcc72e4a55140d9
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2018-03-23 06:35:32 +0000

    VMM/HMSVMR0: Comment nit.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@71476 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/VMM/VMMR0/HMSVMR0.cpp
+++ src/VBox/VMM/VMMR0/HMSVMR0.cpp
@@ -1444,9 +1444,8 @@ static void hmR0SvmLoadSharedCR0(PVMCPU pVCpu, PSVMVMCB pVmcb, PCPUMCTX pCtx)
     }
 
     /*
-     * Use the #MF style of legacy-FPU error reporting for now as IEM needs work if we want to fully
-     * emulate it properly. AMD-V has MSRs that lets us isolate the host from it, but IEM/REM needs
-     * work, see @bugref{7243#c103}.
+     * Use the #MF style of legacy-FPU error reporting for now. Although AMD-V has MSRs that lets us
+     * isolate the host from it, IEM/REM still needs work to emulate it properly. see @bugref{7243#c103}.
      */
     if (!(uGuestCr0 & X86_CR0_NE))
     {
