decaf10c01f8f736992338f2c6d99fc922877e03
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2018-04-06 08:56:24 +0000

    VBoxGuest: VBGL_HGCM_HDR_INIT_TIMED, consistent use of VbglHGCMParm helpers.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@71695 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Additions/common/VBoxGuest/lib/VBoxGuestR3LibGuestProp.cpp
+++ src/VBox/Additions/common/VBoxGuest/lib/VBoxGuestR3LibGuestProp.cpp
@@ -862,9 +862,9 @@ VBGLR3DECL(int) VbglR3GuestPropWait(HGCMCLIENTID idClient,
     VBGL_HGCM_HDR_INIT_TIMED(&Msg.hdr, idClient, GUEST_PROP_FN_GET_NOTIFICATION, 4, cMillies);
 
     VbglHGCMParmPtrSetString(&Msg.patterns, pszPatterns);
-    Msg.buffer.SetPtr(pvBuf, cbBuf);
-    Msg.timestamp.SetUInt64(u64Timestamp);
-    Msg.size.SetUInt32(0);
+    VbglHGCMParmPtrSet(&Msg.buffer, pvBuf, cbBuf);
+    VbglHGCMParmUInt64Set(&Msg.timestamp, u64Timestamp);
+    VbglHGCMParmUInt32Set(&Msg.size, 0);
 
     int rc = VbglR3HGCMCall(&Msg.hdr, sizeof(Msg));
 
