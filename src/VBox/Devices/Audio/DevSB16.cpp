a3a9b3a89d1fa04739df92f9ebfbc879385841e0
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2018-04-07 22:25:41 +0000

    DevSB16.cpp,DevHDA.cpp: Just put markers on the PDMDEV_CHECK_VERSIONS_RETURN macro invoations so nobody puts stuff before them again (see ac97).
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@71742 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Devices/Audio/DevSB16.cpp
+++ src/VBox/Devices/Audio/DevSB16.cpp
@@ -2470,7 +2470,7 @@ static DECLCALLBACK(void) sb16PowerOff(PPDMDEVINS pDevIns)
  */
 static DECLCALLBACK(int) sb16Destruct(PPDMDEVINS pDevIns)
 {
-    PDMDEV_CHECK_VERSIONS_RETURN(pDevIns);
+    PDMDEV_CHECK_VERSIONS_RETURN_QUIET(pDevIns); /* this shall come first */
     PSB16STATE pThis = PDMINS_2_DATA(pDevIns, PSB16STATE);
 
     LogFlowFuncEnter();
@@ -2490,7 +2490,7 @@ static DECLCALLBACK(int) sb16Destruct(PPDMDEVINS pDevIns)
 static DECLCALLBACK(int) sb16Construct(PPDMDEVINS pDevIns, int iInstance, PCFGMNODE pCfg)
 {
     RT_NOREF(iInstance);
-    PDMDEV_CHECK_VERSIONS_RETURN(pDevIns);
+    PDMDEV_CHECK_VERSIONS_RETURN(pDevIns); /* this shall come first */
     PSB16STATE pThis = PDMINS_2_DATA(pDevIns, PSB16STATE);
 
     /*
