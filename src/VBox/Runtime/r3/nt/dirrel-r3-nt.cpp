2ef21f76d24ca4f751d31affa058a33ffb1d3dfe
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2018-04-12 10:17:37 +0000

    IPRT/dirrel-r3-nt.cpp: RTDirRelDirOpenFiltered and RTDirRelDirCreate must pass sane DOS paths to rtDirOpenRelativeOrHandle or RTPathAbs may fail.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@71844 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Runtime/r3/nt/dirrel-r3-nt.cpp
+++ src/VBox/Runtime/r3/nt/dirrel-r3-nt.cpp
@@ -186,6 +186,48 @@ RTDECL(int)  RTDirRelFileOpen(RTDIR hDir, const char *pszRelFilename, uint64_t f
  */
 
 
+/**
+ * Helper for cooking up a path string for rtDirOpenRelativeOrHandle.
+ *
+ * @returns IPRT status code.
+ * @param   pszDst              The destination buffer.
+ * @param   cbDst               The size of the destination buffer.
+ * @param   pThis               The directory this is relative to.
+ * @param   pNtPath             The NT path with a possibly relative path.
+ * @param   fRelative           Whether @a pNtPath is relative or not.
+ * @param   pszPath             The input path.
+ */
+static int rtDirRelJoinPathForDirOpen(char *pszDst, size_t cbDst, PRTDIRINTERNAL pThis,
+                                      PUNICODE_STRING pNtPath, bool fRelative, const char *pszPath)
+{
+    int rc;
+    if (fRelative)
+    {
+        size_t cchRel = 0;
+        rc = RTUtf16CalcUtf8LenEx(pNtPath->Buffer, pNtPath->Length / sizeof(RTUTF16), &cchRel);
+        AssertRC(rc);
+        if (RT_SUCCESS(rc))
+        {
+            if (pThis->cchPath + cchRel < cbDst)
+            {
+                size_t cchBase = pThis->cchPath;
+                memcpy(pszDst, pThis->pszPath, cchBase);
+                pszDst += cchBase;
+                cbDst  -= cchBase;
+                rc = RTUtf16ToUtf8Ex(pNtPath->Buffer, pNtPath->Length / sizeof(RTUTF16), &pszDst, cbDst, NULL);
+            }
+            else
+                rc = VERR_FILENAME_TOO_LONG;
+        }
+    }
+    else
+    {
+        /** @todo would be better to convert pNtName to DOS/WIN path here,
+         *        as it is absolute and doesn't need stuff resolved. */
+        rc = RTPathJoin(pszDst, cbDst, pThis->pszPath, pszPath);
+    }
+    return rc;
+}
 
 RTDECL(int) RTDirRelDirOpen(RTDIR hDir, const char *pszDir, RTDIR *phDir)
 {
@@ -209,7 +251,11 @@ RTDECL(int) RTDirRelDirOpenFiltered(RTDIR hDir, const char *pszDirAndFilter, RTD
                                       pThis->enmInfoClass == FileMaximumInformation);
     if (RT_SUCCESS(rc))
     {
-        rc = rtDirOpenRelativeOrHandle(phDir, pszDirAndFilter, enmFilter, fFlags, (uintptr_t)hRoot, &NtName);
+        char szAbsDirAndFilter[RTPATH_MAX];
+        rc = rtDirRelJoinPathForDirOpen(szAbsDirAndFilter, sizeof(szAbsDirAndFilter), pThis,
+                                        &NtName, hRoot != NULL, pszDirAndFilter);
+        if (RT_SUCCESS(rc))
+            rc = rtDirOpenRelativeOrHandle(phDir, szAbsDirAndFilter, enmFilter, fFlags, (uintptr_t)hRoot, &NtName);
         RTNtPathFree(&NtName, NULL);
     }
     return rc;
@@ -293,8 +339,12 @@ RTDECL(int) RTDirRelDirCreate(RTDIR hDir, const char *pszRelPath, RTFMODE fMode,
             }
             else
             {
-                rc = rtDirOpenRelativeOrHandle(phSubDir, pszRelPath, RTDIRFILTER_NONE, 0 /*fFlags*/,
-                                               (uintptr_t)hNewDir, NULL /*pvNativeRelative*/);
+                char szAbsDirAndFilter[RTPATH_MAX];
+                rc = rtDirRelJoinPathForDirOpen(szAbsDirAndFilter, sizeof(szAbsDirAndFilter), pThis,
+                                                &NtName, hRoot != NULL, pszRelPath);
+                if (RT_SUCCESS(rc))
+                    rc = rtDirOpenRelativeOrHandle(phSubDir, pszRelPath, RTDIRFILTER_NONE, 0 /*fFlags*/,
+                                                   (uintptr_t)hNewDir, NULL /*pvNativeRelative*/);
                 if (RT_FAILURE(rc))
                     NtClose(hNewDir);
             }
