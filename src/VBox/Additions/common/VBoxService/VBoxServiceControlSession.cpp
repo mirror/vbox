ffcab2c19779a40a9f1eba22b665d3133d2da364
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2018-05-03 16:20:44 +0000

    Guest Control/VBoxService: Resolved a @todo in vgsvcGstCtrlSessionHandlePathRename() (now using a dedicated flag for the actual RTPathRename call).
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@72098 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Additions/common/VBoxService/VBoxServiceControlSession.cpp
+++ src/VBox/Additions/common/VBoxService/VBoxServiceControlSession.cpp
@@ -574,17 +574,18 @@ static int vgsvcGstCtrlSessionHandlePathRename(PVBOXSERVICECTRLSESSION pSession,
 
         if (RT_SUCCESS(rc))
         {
-/** @todo r=bird: shouldn't you use a different variable here for the IPRT flags??? */
+            unsigned fPathRenameFlags = 0;
+
             if (fFlags & PATHRENAME_FLAG_NO_REPLACE)
-                fFlags |= RTPATHRENAME_FLAGS_NO_REPLACE;
+                fPathRenameFlags |= RTPATHRENAME_FLAGS_NO_REPLACE;
 
             if (fFlags & PATHRENAME_FLAG_REPLACE)
-                fFlags |= RTPATHRENAME_FLAGS_REPLACE;
+                fPathRenameFlags |= RTPATHRENAME_FLAGS_REPLACE;
 
             if (fFlags & PATHRENAME_FLAG_NO_SYMLINKS)
-                fFlags |= RTPATHRENAME_FLAGS_NO_SYMLINKS;
+                fPathRenameFlags |= RTPATHRENAME_FLAGS_NO_SYMLINKS;
 
-            rc = RTPathRename(szSource, szDest, fFlags);
+            rc = RTPathRename(szSource, szDest, fPathRenameFlags);
         }
 
         /* Report back in any case. */
