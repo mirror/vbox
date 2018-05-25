9ede61ea67304c6b4527b1dd331190b93f0a2c2f
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2018-05-25 11:34:03 +0000

    FE/Qt: bugref:9044: Did a mockup of the NEM indicator icon (turtle / tortoise with a V on the shell).
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@72342 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Frontends/VirtualBox/src/runtime/UIIndicatorsPool.cpp
+++ src/VBox/Frontends/VirtualBox/src/runtime/UIIndicatorsPool.cpp
@@ -936,8 +936,8 @@ public:
         setStateIcon(KVMExecutionEngine_NotSet, UIIconPool::iconSet(":/vtx_amdv_disabled_16px.png"));
         setStateIcon(KVMExecutionEngine_RawMode, UIIconPool::iconSet(":/vtx_amdv_disabled_16px.png"));
         setStateIcon(KVMExecutionEngine_HwVirt, UIIconPool::iconSet(":/vtx_amdv_16px.png"));
-        /** @todo new indicator icon, something that looks very very slow (at least on Windows)... @bugref{9044} */
-        setStateIcon(KVMExecutionEngine_NativeApi, UIIconPool::iconSet(":/vtx_amdv_disabled_16px.png"));
+        /** @todo New indicator icon, vm_execution_engine_native_api_16px.png, V inside a turtle / tortoise.  @bugref{9044} */
+        setStateIcon(KVMExecutionEngine_NativeApi, UIIconPool::iconSet(":/vm_execution_engine_native_api_16px.png"));
         /* Translate finally: */
         retranslateUi();
     }
