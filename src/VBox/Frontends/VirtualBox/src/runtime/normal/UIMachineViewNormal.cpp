f3392375a01739b02ada185a859be1878a72bfa8
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2018-06-06 06:10:18 +0000

    LogRel nit.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@72458 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Frontends/VirtualBox/src/runtime/normal/UIMachineViewNormal.cpp
+++ src/VBox/Frontends/VirtualBox/src/runtime/normal/UIMachineViewNormal.cpp
@@ -183,7 +183,7 @@ void UIMachineViewNormal::resendSizeHint()
 void UIMachineViewNormal::adjustGuestScreenSize()
 {
     /* Should we adjust guest-screen size? Logging paranoia is required here to reveal the truth. */
-    LogRel(("GUI: UIMachineViewNormal::adjustGuestScreenSize: Adjust guest-screen size if necessary.\n"));
+    LogRel(("GUI: UIMachineViewNormal::adjustGuestScreenSize: Adjust guest-screen size if necessary\n"));
     bool fAdjust = false;
 
     /* Step 1: Is the guest-screen of another size than necessary? */
