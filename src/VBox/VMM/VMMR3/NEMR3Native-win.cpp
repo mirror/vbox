a9dbfd58570c066b325b191d3f043ab8e0e91f42
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2018-06-26 14:00:10 +0000

    NEM/win: Turn VINF_PGM_CHANGE_MODE into VINF_SUCCESS too after calling PGMChangeMode in ring-3. bugref:9044
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@72695 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/VMM/VMMR3/NEMR3Native-win.cpp
+++ src/VBox/VMM/VMMR3/NEMR3Native-win.cpp
@@ -2237,7 +2237,9 @@ VBOXSTRICTRC nemR3NativeRunGC(PVM pVM, PVMCPU pVCpu)
                 LogFlow(("nemR3NativeRunGC: calling PGMChangeMode...\n"));
                 int rc = PGMChangeMode(pVCpu, CPUMGetGuestCR0(pVCpu), CPUMGetGuestCR4(pVCpu), CPUMGetGuestEFER(pVCpu));
                 AssertRCReturn(rc, rc);
-                if (rcStrict == VINF_NEM_CHANGE_PGM_MODE || rcStrict == VINF_NEM_FLUSH_TLB)
+                if (   rcStrict == VINF_NEM_CHANGE_PGM_MODE
+                    || rcStrict == VINF_PGM_CHANGE_MODE
+                    || rcStrict == VINF_NEM_FLUSH_TLB)
                 {
                     if (   !VM_FF_IS_PENDING(pVM, VM_FF_HIGH_PRIORITY_POST_MASK | VM_FF_HP_R0_PRE_HM_MASK)
                         && !VMCPU_FF_IS_PENDING(pVCpu,   (VMCPU_FF_HIGH_PRIORITY_POST_MASK | VMCPU_FF_HP_R0_PRE_HM_MASK)
