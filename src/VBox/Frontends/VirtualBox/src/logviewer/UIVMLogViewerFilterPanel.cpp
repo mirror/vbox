425092499f30136ad61c464ccbf6a2b8dc0fcb83
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2018-06-29 06:53:38 +0000

    FE/Qt: Some tooltip modifications
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@72733 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Frontends/VirtualBox/src/logviewer/UIVMLogViewerFilterPanel.cpp
+++ src/VBox/Frontends/VirtualBox/src/logviewer/UIVMLogViewerFilterPanel.cpp
@@ -531,8 +531,8 @@ void UIVMLogViewerFilterPanel::prepareConnections()
 void UIVMLogViewerFilterPanel::retranslateUi()
 {
     UIVMLogViewerPanel::retranslateUi();
-    m_pFilterComboBox->setToolTip(UIVMLogViewerWidget::tr("Enter filtering string here."));
-    m_pAddFilterTermButton->setToolTip(UIVMLogViewerWidget::tr("Add filter term. (Enter)"));
+    m_pFilterComboBox->setToolTip(UIVMLogViewerWidget::tr("Select or enter a term which will be used in filtering the log text."));
+    m_pAddFilterTermButton->setToolTip(UIVMLogViewerWidget::tr("Add the filter term to the set of filter terms. (Enter)"));
     m_pResultLabel->setText(UIVMLogViewerWidget::tr("Showing %1/%2").arg(m_iFilteredLineCount).arg(m_iUnfilteredLineCount));
     m_pFilterTermsLineEdit->setToolTip(UIVMLogViewerWidget::tr("The filter terms list. Select one to remove or click the button on the right side to remove them all."));
     m_pRadioButtonContainer->setToolTip(UIVMLogViewerWidget::tr("The type of boolean operator for filter operation."));
