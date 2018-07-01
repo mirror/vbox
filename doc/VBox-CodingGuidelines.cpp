26cd3951afe6bba906222ca6fb8653a4e0b682c7
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2018-07-01 11:29:08 +0000

    doc/VBox-CodingGuidelines.cpp: Added links to local coding guidelines around VBox.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@72781 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- doc/VBox-CodingGuidelines.cpp
+++ doc/VBox-CodingGuidelines.cpp
@@ -20,9 +20,9 @@
  * The VBox Coding guidelines are followed by all of VBox with the exception of
  * qemu. Qemu is using whatever the frenchman does.
  *
- * There are a few compulsory rules and a bunch of optional ones. The following
- * sections will describe these in details. In addition there is a section of
- * Subversion 'rules'.
+ * There are a few compulsory rules and a bunch of optional ones.  The following
+ * sections will describe these in details.  In addition there is a section of
+ * Subversion 'rules' and one with links to local coding guidelines.
  *
  *
  *
@@ -904,5 +904,29 @@ int main()
  * </ul>
  *
  * (Inspired by mozilla tree rules.)
+ *
+ *
+ * @section sec_vbox_guideline_local        Local Coding Guidelines
+ *
+ * VirtualBox is a large project, so there are parts with additional guidelines
+ * that extends the above ones.
+ *
+ * <ul>
+ *
+ *   <li> src/VBox/VMM/: @ref pg_vmm_guideline
+ *        (src/VBox/VMM/Docs-CodingGuidelines.cpp)
+ *
+ *   <li> src/VBox/ValidationKit/: @ref pg_validationkit_guideline
+ *        (src/VBox/ValidationKit/ValidationKitCodingGuidelines.cpp)
+ *
+ *   <li> src/VBox/Runtime/: @ref sec_vbox_guideline_optional is mandatory.
+ *
+ *   <li> src/VBox/Frontends/VirtualBox/: @ref sec_vbox_guideline_compulsory_cppqtgui
+ *
+ *   <li> src/VBox/Main: @ref sec_vbox_guideline_compulsory_cppmain
+ *
+ *
+ * </ul>
+ *
  */
 
