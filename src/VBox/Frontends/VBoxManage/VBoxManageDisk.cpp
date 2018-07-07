ef80e5ccedfc4d5ea694df4fa1d56233e0b6328f
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2018-07-07 16:46:43 +0000

    VBoxManage: mediumio build fix
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@72952 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Frontends/VBoxManage/VBoxManageDisk.cpp
+++ src/VBox/Frontends/VBoxManage/VBoxManageDisk.cpp
@@ -2050,7 +2050,7 @@ static RTEXITCODE mediumIOOpenMediumForIO(HandlerArg *pHandler, PCMEDIUMIOCOMMON
      */
     ComPtr<IMedium> ptrMedium;
     HRESULT hrc = openMedium(pHandler, pCommonOpts->pszFilenameOrUuid, pCommonOpts->enmDeviceType,
-                             (AccessMode_T)(fWritable ? AccessMode_ReadWrite : AccessMode_ReadOnly),
+                             fWritable ? (AccessMode_T)AccessMode_ReadWrite : (AccessMode_T)AccessMode_ReadOnly,
                              ptrMedium, false /* fForceNewUuidOnOpen */, false /* fSilent */);
     if (SUCCEEDED(hrc))
     {
