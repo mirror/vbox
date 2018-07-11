eee56f08043476ffe3782bf72f6cd139c1bb8db0
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2018-07-11 11:20:50 +0000

    Storage/VDVfs.cpp: Fix for snapshot setups.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@73061 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Storage/VDVfs.cpp
+++ src/VBox/Storage/VDVfs.cpp
@@ -674,7 +674,7 @@ static DECLCALLBACK(int) vdVfsChain_Instantiate(PCRTVFSCHAINELEMENTREG pProvider
             else
             {
                 uint32_t cChain = pElement->cArgs;
-                if (pElement->cArgs == 2 && pElement->paArgs[pElement->cArgs - 1].uProvider != 0)
+                if (pElement->cArgs >= 2 && pElement->paArgs[pElement->cArgs - 1].uProvider != 0)
                     cChain--;
                 uint32_t const  fFinal    = (uint32_t)pElement->uProvider;
                 uint32_t const  fReadOnly = (fFinal & ~(VD_OPEN_FLAGS_READONLY | VD_OPEN_FLAGS_NORMAL)) | VD_OPEN_FLAGS_READONLY;
