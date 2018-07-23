c7fdecee3e4dd915c21f4042f8788143c22b8c16
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2018-07-23 12:15:31 +0000

    FE/Qt: bugref:9152: Export Appliance wizard: Expert page: forgot something in r123924.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@73317 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Frontends/VirtualBox/src/wizards/exportappliance/UIWizardExportAppPageExpert.h
+++ src/VBox/Frontends/VirtualBox/src/wizards/exportappliance/UIWizardExportAppPageExpert.h
@@ -45,6 +45,8 @@ class UIWizardExportAppPageExpert : public UIWizardPage,
     Q_PROPERTY(MACAddressPolicy macAddressPolicy READ macAddressPolicy WRITE setMACAddressPolicy);
     Q_PROPERTY(bool manifestSelected READ isManifestSelected WRITE setManifestSelected);
     Q_PROPERTY(bool includeISOsSelected READ isIncludeISOsSelected WRITE setIncludeISOsSelected);
+    Q_PROPERTY(CCloudUserProfileList profiles READ profiles);
+    Q_PROPERTY(QString profile READ profile);
     Q_PROPERTY(ExportAppliancePointer applianceWidget READ applianceWidget);
 
 public:
