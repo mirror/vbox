5138542e0bd7e13a4c89e943f3c86c46c87c3e5c
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2018-07-31 09:08:32 +0000

    Audio/DrvAudio: Renaming nit.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@73405 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Devices/Audio/DrvAudio.cpp
+++ src/VBox/Devices/Audio/DrvAudio.cpp
@@ -2991,16 +2991,16 @@ static DECLCALLBACK(uint32_t) drvAudioStreamGetReadable(PPDMIAUDIOCONNECTOR pInt
         return 0;
     }
 
-    uint32_t cReadable = 0;
+    uint32_t cfReadable = 0;
 
     PPDMAUDIOSTREAM pGstStream = pHstStream->pPair;
     if (pGstStream)
-        cReadable = AudioMixBufLive(&pGstStream->MixBuf);
+        cfReadable = AudioMixBufLive(&pGstStream->MixBuf);
 
-    Log3Func(("[%s] cbReadable=%RU32 (%zu bytes)\n", pHstStream->szName, cReadable,
-              AUDIOMIXBUF_F2B(&pGstStream->MixBuf, cReadable)));
+    Log3Func(("[%s] cfReadable=%RU32 (%zu bytes)\n", pHstStream->szName, cfReadable,
+              AUDIOMIXBUF_F2B(&pGstStream->MixBuf, cfReadable)));
 
-    uint32_t cbReadable = AUDIOMIXBUF_F2B(&pGstStream->MixBuf, cReadable);
+    uint32_t cbReadable = AUDIOMIXBUF_F2B(&pGstStream->MixBuf, cfReadable);
 
     rc2 = RTCritSectLeave(&pThis->CritSect);
     AssertRC(rc2);
