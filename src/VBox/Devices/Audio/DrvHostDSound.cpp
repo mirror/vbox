2567c82d0f0834bd49c079406da7fec5def2d697
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2018-08-07 13:01:59 +0000

    Audio/DrvHostDSound.cpp: Logging.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@73546 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Devices/Audio/DrvHostDSound.cpp
+++ src/VBox/Devices/Audio/DrvHostDSound.cpp
@@ -676,7 +676,7 @@ static HRESULT directSoundPlayOpen(PDRVHOSTDSOUND pThis, PDSOUNDSTREAM pStreamDS
         bd.dwBufferBytes = DrvAudioHlpFramesToBytes(pCfgReq->Backend.cfBufferSize, &pCfgReq->Props);
 
         DSLOG(("DSound: Requested playback buffer is %RU64ms (%ld bytes)\n",
-               pCfgReq->Backend.cfBufferSize, DrvAudioHlpBytesToMilli(bd.dwBufferBytes, &pCfgReq->Props), bd.dwBufferBytes));
+               DrvAudioHlpBytesToMilli(bd.dwBufferBytes, &pCfgReq->Props), bd.dwBufferBytes));
 
         hr = IDirectSound8_CreateSoundBuffer(pThis->pDS, &bd, &pDSB, NULL);
         if (FAILED(hr))
