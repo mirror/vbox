2ffd832518eb22fe804459dfb7e3d2293c302e93
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2018-08-13 09:40:01 +0000

    Audio/Main: Also insert the debugging enabled / path values into the AC'97 device config. See @todo.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@73629 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Main/src-client/ConsoleImpl2.cpp
+++ src/VBox/Main/src-client/ConsoleImpl2.cpp
@@ -2812,6 +2812,9 @@ int Console::i_configConstructorInner(PUVM pUVM, PVM pVM, AutoWriteLock *pAlock)
             Utf8Str strDebugPathOut;
             GetExtraDataBoth(virtualBox, pMachine, "VBoxInternal2/Audio/Debug/PathOut", &strDebugPathOut);
 
+            /** @todo Implement an audio device class, similar to the audio backend class, to construct the common stuff
+             *        without duplicating (more) code. */
+
             switch (audioController)
             {
                 case AudioControllerType_AC97:
@@ -2864,9 +2867,8 @@ int Console::i_configConstructorInner(PUVM pUVM, PVM pVM, AutoWriteLock *pAlock)
                     InsertConfigInteger(pInst,    "Trusted",               1); /* boolean */
                     hrc = pBusMgr->assignPCIDevice(strAudioDevice.c_str(), pInst);          H();
                     InsertConfigNode   (pInst,    "Config",                &pCfg);
-
-                        InsertConfigInteger(pCfg, "DebugEnabled", fDebugEnabled);
-                        InsertConfigString (pCfg, "DebugPathOut", strDebugPathOut);
+                    InsertConfigInteger(pCfg,     "DebugEnabled",          fDebugEnabled);
+                    InsertConfigString (pCfg,     "DebugPathOut",          strDebugPathOut);
                     break;
                 }
                 default: AssertFailedBreak();
