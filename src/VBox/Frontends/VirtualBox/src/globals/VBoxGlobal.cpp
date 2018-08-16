c7b69d18b18135d098a3a6e150e37188b98765a4
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2018-08-16 08:40:38 +0000

    FE/Qt: bugref: 9040 Create manager UI log files with `delete share` flag to enable log rotation on Windows
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@73702 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Frontends/VirtualBox/src/globals/VBoxGlobal.cpp
+++ src/VBox/Frontends/VirtualBox/src/globals/VBoxGlobal.cpp
@@ -4101,9 +4101,9 @@ void VBoxGlobal::prepare()
                               RTLOGFLAGS_PREFIX_TIME_PROG,
                               "all",
                               "VBOX_GUI_SELECTORWINDOW_RELEASE_LOG",
-                              RTLOGDEST_FILE,
+                              RTLOGDEST_FILE | RTLOGDEST_F_NO_DENY,
                               UINT32_MAX,
-                              1,
+                              10,
                               60 * 60,
                               _1M,
                               NULL /*pErrInfo*/);
