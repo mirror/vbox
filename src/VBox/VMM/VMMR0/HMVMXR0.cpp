cda0ed09704f6babefff27ed8a3fea5ec25677eb
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2018-08-18 04:48:52 +0000

    VMM/HVMXMR0: const nit.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@73754 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/VMM/VMMR0/HMVMXR0.cpp
+++ src/VBox/VMM/VMMR0/HMVMXR0.cpp
@@ -2757,7 +2757,7 @@ VMMR0DECL(int) VMXR0SetupVM(PVM pVM)
 #endif
 
     /* At least verify VMX is enabled, since we can't check if we're in VMX root mode without #GP'ing. */
-    RTCCUINTREG uHostCR4 = ASMGetCR4();
+    RTCCUINTREG const uHostCR4 = ASMGetCR4();
     if (RT_UNLIKELY(!(uHostCR4 & X86_CR4_VMXE)))
         return VERR_VMX_NOT_IN_VMX_ROOT_MODE;
 
