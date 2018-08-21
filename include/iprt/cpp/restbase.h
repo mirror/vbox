fc845bd1ed8b573df038c66e5ab2434a53461a86
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2018-08-21 15:00:15 +0000

    iprt/cpp/restbase.h: Some serialization and deserialization ideas. bugref:9167
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@73803 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- include/iprt/cpp/restbase.h
+++ include/iprt/cpp/restbase.h
@@ -27,8 +27,12 @@
 #define ___iprt_cpp_restbase_h
 
 #include <iprt/types.h>
+#include <iprt/assert.h>
 #include <iprt/err.h>
 #include <iprt/http.h>
+#include <iprt/json.h>
+#include <iprt/stdarg.h>
+#include <iprt/cpp/ministring.h>
 #include <iprt/cpp/utils.h>
 
 
@@ -39,14 +43,111 @@
 
 
 /**
- * Base class for REST data objects.
+ * Abstract base class for serializing data objects.
+ */
+class RTCRestOutputBase
+{
+public:
+    RTCRestOutputBase();
+    virtual ~RTCRestOutputBase();
+
+    /**
+     * RTStrPrintf like function (see @rel pg_rt_str_format).
+     *
+     * @returns Number of bytes outputted.
+     * @param   uDepth      The indentation level.
+     * @param   pszFormat   The format string.
+     * @param   ...         Argument specfied in @a pszFormat.
+     */
+    size_t         printf(unsigned uIndent, const char *pszFormat, ...) RT_IPRT_FORMAT_ATTR(2, 3)
+    {
+        va_list va;
+        va_start(va, pszFormat);
+        size_t cchWritten = this->vprintf(uIndent, pszFormat, va);
+        va_end(va);
+        return cchWritten;
+    }
+
+    /**
+     * RTStrPrintfV like function (see @rel pg_rt_str_format).
+     *
+     * @returns Number of bytes outputted.
+     * @param   uDepth      The indentation level.
+     * @param   pszFormat   The format string.
+     * @param   va          Argument specfied in @a pszFormat.
+     */
+    virtual size_t vprintf(unsigned uIndent, const char *pszFormat, va_list va) RT_IPRT_FORMAT_ATTR(2, 0) = 0;
+};
+
+
+/**
+ * Serialize to a string object.
+ */
+class RTCRestOutputToString : public RTCRestOutputBase, public RTCNonCopyable
+{
+public:
+    /**
+     * Creates an instance that appends to @a a_pDst.
+     * @param   a_pDst      Pointer to the destination string object.
+     *                      NULL is not accepted and will assert.
+     */
+    RTCRestOutputToString(RTCString *a_pDst);
+    virtual ~RTCRestOutputToString();
+
+    size_t vprintf(unsigned uIndent, const char *pszFormat, va_list va);
+
+    /**
+     * Finalizes the output and releases the string object to the caller.
+     *
+     * @returns The released string object.  NULL if we ran out of memory or if
+     *          called already.
+     *
+     * @remark  This sets m_pDst to NULL and the object cannot be use for any
+     *          more output afterwards.
+     */
+    virtual RTCString *finalize(void);
+
+
+protected:
+    /** Pointer to the destination string.  NULL after finalize().   */
+    RTCString  *m_pDst;
+    /** Set if we ran out of memory and should ignore subsequent calls. */
+    bool        m_fOutOfMemory;
+};
+
+
+/**
+ * Abstract base class for REST data objects.
  */
 class RTCSRestObjectBase
 {
 public:
     RTCSRestObjectBase() {}
     virtual ~RTCSRestObjectBase() {}
+
     /** @todo Add some kind of state? */
+
+    /**
+     * Serialize the object as JSON.
+     *
+     * @returns a_rDst
+     * @param   a_rDst      The destination for the serialization.
+     * @param   uIndent     The indentation level.  Increment by 1 for child
+     *                      objects.
+     */
+    virtual RTCRestOutputBase &serializeAsJson(RTCRestOutputBase &a_rDst, unsigned uIndent) = 0;
+
+    /**
+     * Deserialize object from the given JSON iterator.
+     *
+     * @returns IPRT status code.
+     * @param   hJsonIt     The JSON iterator for this object.
+     * @param   pErrInfo    Where to return additional error information.
+     *                      Optional.
+     *
+     * @todo Take a RTJSONVAL?
+     */
+    virtual int deserializeFromJson(RTJSONIT hJsonIt, PRTERRINFO pErrInfo) = 0;
 };
 
 
