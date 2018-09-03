78e01fe233dafaa606509a70679e115d48c16d9f
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2018-09-03 09:12:30 +0000

    tdMoveMedium1,tdMoveVM1: Shut up pylint warnings about importing things in 2.7.9 compatible manner.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@74031 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/ValidationKit/tests/api/tdMoveMedium1.py
+++ src/VBox/ValidationKit/tests/api/tdMoveMedium1.py
@@ -204,6 +204,6 @@ class SubTstDrvMoveMedium1(base.SubTestDriverBase):
 
 if __name__ == '__main__':
     sys.path.append(os.path.dirname(os.path.abspath(__file__)))
-    from tdApi1 import tdApi1
+    from tdApi1 import tdApi1; # pylint: disable=relative-import
     sys.exit(tdApi1([SubTstDrvMoveMedium1]).main(sys.argv))
 
