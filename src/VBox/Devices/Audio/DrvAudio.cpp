190cc2546718a2446b421284bbca489cf3bd52bf
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2018-09-03 09:43:19 +0000

    Audio/DrvAudio: Also check for free input streams on stream creation and bail out if it's not the case in drvAudioStreamCreate().
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@74033 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Devices/Audio/DrvAudio.cpp
+++ src/VBox/Devices/Audio/DrvAudio.cpp
@@ -2400,11 +2400,14 @@ static DECLCALLBACK(int) drvAudioStreamCreate(PPDMIAUDIOCONNECTOR pInterface,
         }
 
         /* Note: cbHstStrm will contain the size of the data the backend needs to operate on. */
-        size_t cbHstStrm = 0;
+        size_t cbHstStrm;
         if (pCfgHost->enmDir == PDMAUDIODIR_IN)
         {
             if (!pThis->In.cStreamsFree)
-                LogFunc(("Warning: No more input streams free to use\n"));
+            {
+                LogFlowFunc(("Maximum number of host input streams reached\n"));
+                RC_BREAK(VERR_AUDIO_NO_FREE_INPUT_STREAMS);
+            }
 
             cbHstStrm = pThis->BackendCfg.cbStreamIn;
         }
