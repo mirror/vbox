3bfab835ce7f865fb5cf18f591654fa2589ba323
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2018-09-06 15:10:20 +0000

    IPRT/rest: When we generate X-Date header we use RTTimeToRfc2822. RFC2822 (RFC5322) marks "GMT" as obsolete, but RFC7231 explicitly specifies literal "GMT" in the grammar (7.1.1.1), so replace final "-0000" or "+0000" with "GMT" to make the server happy.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@74122 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Runtime/common/rest/RTCRestClientApiBaseOci.cpp
+++ src/VBox/Runtime/common/rest/RTCRestClientApiBaseOci.cpp
@@ -62,6 +62,23 @@ static int ociSignRequestEnsureDateOrXDate(RTHTTP hHttp)
     ssize_t cch = RTTimeToRfc2822(RTTimeExplode(&Now, RTTimeNow(&NowSpec)), szDate, sizeof(szDate));
     AssertRCReturn((int)cch, (int)cch);
 
+    /*
+     * RFC2822 (RFC5322) marks "GMT" as obsolete,
+     * but RFC7231 explicitly specifies in the grammar:
+     *
+     *   GMT = %x47.4D.54 ; "GMT", case-sensitive
+     */
+    if (   cch >= 5
+        && (szDate[cch-5] == '-' || szDate[cch-5] == '+')
+        && strcmp(&szDate[cch-5+1], "0000") == 0)
+    {
+        szDate[cch-5+0] = 'G';
+        szDate[cch-5+1] = 'M';
+        szDate[cch-5+2] = 'T';
+        szDate[cch-5+3] = '\0';
+        cch -= 2;
+    }
+
     return RTHttpAddHeader(hHttp, "x-date", szDate, cch, RTHTTPADDHDR_F_BACK);
 }
 
