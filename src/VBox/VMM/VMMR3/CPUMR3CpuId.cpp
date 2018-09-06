9ae974e34e9bb9205801a8b745a203f7599b0029
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2018-09-06 02:43:53 +0000

    VMM/CPUM: Nested VMX: bugref:9180 Disallow nested hwvirt on 32-bit hosts.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@74097 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/VMM/VMMR3/CPUMR3CpuId.cpp
+++ src/VBox/VMM/VMMR3/CPUMR3CpuId.cpp
@@ -3948,8 +3948,8 @@ static int cpumR3CpuIdReadConfig(PVM pVM, PCPUMCPUIDCONFIG pConfig, PCFGMNODE pC
     {
         /** @cfgm{/CPUM/NestedHWVirt, bool, false}
          * Whether to expose the hardware virtualization (VMX/SVM) feature to the guest.
-         * The default is false, and when enabled requires nested paging and AMD-V or
-         * unrestricted guest mode.
+         * The default is false, and when enabled requires a 64-bit CPU with support for
+         * nested-paging and AMD-V or unrestricted guest mode.
          */
         rc = CFGMR3QueryBoolDef(pCpumCfg, "NestedHWVirt", &pConfig->fNestedHWVirt, false);
         AssertLogRelRCReturn(rc, rc);
@@ -3965,6 +3965,13 @@ static int cpumR3CpuIdReadConfig(PVM pVM, PCPUMCPUIDCONFIG pConfig, PCFGMNODE pC
             LogRel(("CPUM: WARNING! Can't turn on nested VT-x/AMD-V when NEM is used!\n"));
             pConfig->fNestedHWVirt = false;
         }
+
+#if HC_ARCH_BITS == 32
+        /* We don't support nested hardware virtualization on 32-bit hosts. */
+        if (pConfig->fNestedHWVirt)
+            return VMSetError(pVM, VERR_CPUM_INVALID_HWVIRT_CONFIG, RT_SRC_POS,
+                              "Cannot enable nested VT-x/AMD-V on a 32-bit host\n");
+#endif
     }
 
     /*
