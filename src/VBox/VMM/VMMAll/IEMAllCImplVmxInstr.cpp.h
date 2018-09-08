5459bb8df6e1aad77c2e5007f91ef72c4eda4fc6
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2018-09-08 10:49:05 +0000

    VMM/IEM: Nested VMX: bugref:9180 vmlaunch/vmresume bits. Granularity bit checks consistency.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@74152 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/VMM/VMMAll/IEMAllCImplVmxInstr.cpp.h
+++ src/VBox/VMM/VMMAll/IEMAllCImplVmxInstr.cpp.h
@@ -2539,10 +2539,8 @@ IEM_STATIC int iemVmxVmentryCheckGuestSegRegs(PVMCPU pVCpu, const char *pszInstr
             }
 
             /* Granularity. */
-            if (   (   (SelReg.u32Limit & 0xfff) != 0xfff
-                    && uGranularity == 0)
-                || (   (SelReg.u32Limit & 0xfff00000)
-                    && uGranularity == 1))
+            if (   ((SelReg.u32Limit & 0x00000fff) == 0x00000fff || !uGranularity)
+                && ((SelReg.u32Limit & 0xfff00000) == 0x00000000 ||  uGranularity))
             { /* likely */ }
             else
             {
@@ -2706,10 +2704,8 @@ IEM_STATIC int iemVmxVmentryCheckGuestSegRegs(PVMCPU pVCpu, const char *pszInstr
             else
                 IEM_VMX_VMENTRY_FAILED_RET(pVCpu, pszInstr, pszFailure, kVmxVInstrDiag_Vmentry_GuestSegAttrLdtrPresent);
 
-            if (   (   (Ldtr.u32Limit & 0xfff) != 0xfff
-                    && Ldtr.Attr.n.u1Granularity == 0)
-                || (   (Ldtr.u32Limit & 0xfff00000)
-                    && Ldtr.Attr.n.u1Granularity == 1))
+            if (   ((Ldtr.u32Limit & 0x00000fff) == 0x00000fff || !Ldtr.Attr.n.u1Granularity)
+                && ((Ldtr.u32Limit & 0xfff00000) == 0x00000000 ||  Ldtr.Attr.n.u1Granularity))
             { /* likely */ }
             else
                 IEM_VMX_VMENTRY_FAILED_RET(pVCpu, pszInstr, pszFailure, kVmxVInstrDiag_Vmentry_GuestSegAttrLdtrGran);
@@ -2770,10 +2766,8 @@ IEM_STATIC int iemVmxVmentryCheckGuestSegRegs(PVMCPU pVCpu, const char *pszInstr
         else
             IEM_VMX_VMENTRY_FAILED_RET(pVCpu, pszInstr, pszFailure, kVmxVInstrDiag_Vmentry_GuestSegAttrTrPresent);
 
-        if (   (   (Tr.u32Limit & 0xfff) != 0xfff
-                && Tr.Attr.n.u1Granularity == 0)
-            || (   (Tr.u32Limit & 0xfff00000)
-                && Tr.Attr.n.u1Granularity == 1))
+        if (   ((Tr.u32Limit & 0x00000fff) == 0x00000fff || !Tr.Attr.n.u1Granularity)
+            && ((Tr.u32Limit & 0xfff00000) == 0x00000000 ||  Tr.Attr.n.u1Granularity))
         { /* likely */ }
         else
             IEM_VMX_VMENTRY_FAILED_RET(pVCpu, pszInstr, pszFailure, kVmxVInstrDiag_Vmentry_GuestSegAttrTrGran);
