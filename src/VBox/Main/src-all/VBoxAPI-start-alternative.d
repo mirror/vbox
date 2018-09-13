35ccaa7e1d7662bf6d302912ee72117612e6beca
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2018-09-13 13:29:14 +0000

    Main: Attempt at making dtrace 1.6.2 happy. bugref:9152
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@74238 cfe28804-0f27-0410-a406-dd0f0b0b656f

---
+++ src/VBox/Main/src-all/VBoxAPI-start-alternative.d
@@ -0,0 +1,29 @@
+/* $Id$ */
+/** @file
+ * VBoxAPI - Static dtrace probes.
+ */
+
+/*
+ * Copyright (C) 2015-2017 Oracle Corporation
+ *
+ * This file is part of VirtualBox Open Source Edition (OSE), as
+ * available from http://www.virtualbox.org. This file is free software;
+ * you can redistribute it and/or modify it under the terms of the GNU
+ * General Public License (GPL) as published by the Free Software
+ * Foundation, in version 2 as it comes in the "COPYING" file of the
+ * VirtualBox OSE distribution. VirtualBox OSE is distributed in the
+ * hope that it will be useful, but WITHOUT ANY WARRANTY of any kind.
+ */
+
+#pragma D attributes Evolving/Evolving/Common provider vboxapi provider
+#pragma D attributes Private/Private/Unknown  provider vboxapi module
+#pragma D attributes Private/Private/Unknown  provider vboxapi function
+#pragma D attributes Evolving/Evolving/Common provider vboxapi name
+#pragma D attributes Evolving/Evolving/Common provider vboxapi args
+
+provider vboxapi
+{
+    /* Manually defined probes: */
+    probe machine__state__changed(void *a_pMachine, int a_enmNewState, int a_enmOldState, const char *pszMachineUuid);
+
+    /* The following probes are automatically generated and changes with the API: */
