e04b74ee9bfb98c29e851c97d4835c5b03104a88
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2018-09-19 09:50:35 +0000

    IPRT/cipher: fixed assertion in encryption code. bugref:9246
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@74362 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Runtime/common/crypto/cipher-openssl.cpp
+++ src/VBox/Runtime/common/crypto/cipher-openssl.cpp
@@ -234,7 +234,7 @@ RTDECL(int) RTCrCipherEncrypt(RTCRCIPHER hCipher, void const *pvKey, size_t cbKe
             {
                 Assert(cbEncrypted1 <= (ssize_t)cbNeeded);
                 int cbEncrypted2 = 0;
-                rcOssl = EVP_DecryptFinal(pCipherCtx, (unsigned char *)pvEncrypted + cbEncrypted1, &cbEncrypted1);
+                rcOssl = EVP_DecryptFinal(pCipherCtx, (unsigned char *)pvEncrypted + cbEncrypted1, &cbEncrypted2);
                 if (rcOssl > 0)
                 {
                     Assert(cbEncrypted1 + cbEncrypted2 == (ssize_t)cbNeeded);
