cd2425275e091434f026859461623c232e1e80e3
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2018-09-21 16:47:56 +0000

    Additions/linux: remove attempt to suppress a log message. The build logs for kernel modules start with a test for autoconf.h which includes the text of an error message in the text.  This historically confused users who thought it was a real error, so we tried to suppress it from our logs, but the egrep command failed on Ubuntu 14.04.  Since though we now only include the build log in case of error anyway there is no longer a risk that users will think that a successful build failed, so just remove the suppression.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@74412 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Additions/linux/installer/vboxadd.sh
+++ src/VBox/Additions/linux/installer/vboxadd.sh
@@ -130,10 +130,9 @@ log()
 
 module_build_log()
 {
-    setup_log
-    echo "${1}" | egrep -v \
-        "^test -e include/generated/autoconf.h|^echo >&2|^/bin/false)$" \
-        >> "${LOG}"
+    log "Error building the module.  Build output follows."
+    echo ""
+    echo "${1}" >> "${LOG}"
 }
 
 dev=/dev/vboxguest
@@ -344,7 +343,6 @@ setup_modules()
         --module-source $MODULE_SRC/vboxguest \
         --no-print-directory install 2>&1`; then
         # If check_module_dependencies.sh fails it prints a message itself.
-        log "Error building the module:"
         module_build_log "$myerr"
         "${INSTALL_DIR}"/other/check_module_dependencies.sh 2>&1 &&
             info "Look at $LOG to find out what went wrong"
@@ -355,7 +353,6 @@ setup_modules()
         --use-module-symvers /tmp/vboxguest-Module.symvers \
         --module-source $MODULE_SRC/vboxsf \
         --no-print-directory install 2>&1`; then
-        log "Error building the module:"
         module_build_log "$myerr"
         info  "Look at $LOG to find out what went wrong"
         return 0
@@ -365,7 +362,6 @@ setup_modules()
         --use-module-symvers /tmp/vboxguest-Module.symvers \
         --module-source $MODULE_SRC/vboxvideo \
         --no-print-directory install 2>&1`; then
-        log "Error building the module:"
         module_build_log "$myerr"
         info "Look at $LOG to find out what went wrong"
     fi
