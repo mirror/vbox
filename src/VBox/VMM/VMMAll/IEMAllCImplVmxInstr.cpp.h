fce2de82ae3695e6fe4b55b3829c9d92ea2979e3
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2018-10-06 06:34:04 +0000

    VMM/IEM: Nested VMX: bugref:9180 Comment nit.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@74634 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/VMM/VMMAll/IEMAllCImplVmxInstr.cpp.h
+++ src/VBox/VMM/VMMAll/IEMAllCImplVmxInstr.cpp.h
@@ -3190,8 +3190,6 @@ IEM_STATIC VBOXSTRICTRC iemVmxVmexitInstrMovToCr3(PVMCPU pVCpu, uint64_t uNewCr3
  * VMX VM-exit handler for VM-exits due to 'Mov GReg,CR8' (CR8 read).
  *
  * @returns VBox strict status code.
- * @retval VINF_VMX_INTERCEPT_NOT_ACTIVE if the Mov instruction did not cause a
- *         VM-exit.
  * @param   pVCpu       The cross context virtual CPU structure.
  * @param   iGReg       The general register to which the CR8 value is being stored.
  * @param   cbInstr     The instruction length in bytes.
