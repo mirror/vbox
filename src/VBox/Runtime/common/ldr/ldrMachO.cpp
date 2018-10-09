e009e624a81652cfba6d4e5ae0c354e15a68cd56
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2018-10-09 21:12:39 +0000

    IPRT/ldr: More hacking Mach-O code signatures. bugref:9232
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@74727 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Runtime/common/ldr/ldrMachO.cpp
+++ src/VBox/Runtime/common/ldr/ldrMachO.cpp
@@ -4275,7 +4275,7 @@ static int rtldrMachO_VerifySignatureDecode(PRTLDRMODMACHO pThis, PRTLDRMACHOSIG
             if (   cbSelf >= RT_UOFFSET_AFTER(RTCRAPLCSCODEDIRECTORY, fExecSeg)
                 && (   pThis->offSeg0ForCodeSign != RT_BE2H_U64(pCodeDir->offExecSeg)
                     || pThis->cbSeg0ForCodeSign != RT_BE2H_U64(pCodeDir->cbExecSeg)
-                    || pThis->fSeg0ForCodeSign != RT_BE2H_U64(pCodeDir->fFlags)) )
+                    || pThis->fSeg0ForCodeSign != RT_BE2H_U64(pCodeDir->fExecSeg)) )
                 return RTErrInfoSetF(pErrInfo, VERR_LDRVI_BAD_CERT_FORMAT,
                                      "Slot #%u: Segment #0 info mismatch: @%#RX64 LB %#RX64 flags=%#RX64; expected @%#RX64 LB %#RX64 flags=%#RX64",
                                      iSlot, RT_BE2H_U64(pCodeDir->offExecSeg), RT_BE2H_U64(pCodeDir->cbExecSeg),
