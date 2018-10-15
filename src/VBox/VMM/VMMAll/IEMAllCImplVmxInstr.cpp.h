959c6600a8b98164fb7ac56049cbc9b35eb13614
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2018-10-15 08:50:46 +0000

    VMM/IEM: Nested VMX: bugref:9180 Updated VM-exit todo list.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@74836 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/VMM/VMMAll/IEMAllCImplVmxInstr.cpp.h
+++ src/VBox/VMM/VMMAll/IEMAllCImplVmxInstr.cpp.h
@@ -18,7 +18,6 @@
 
 #ifdef VBOX_WITH_NESTED_HWVIRT_VMX
 /** @todo NSTVMX: The following VM-exit intercepts are pending:
- *  VMX_EXIT_EXT_INT ("acknowledge interrupt on exit" behavior pending)
  *  VMX_EXIT_INIT_SIGNAL
  *  VMX_EXIT_SIPI
  *  VMX_EXIT_IO_SMI
