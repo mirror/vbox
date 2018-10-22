f409c6043fe83e97eb91887d854a347c834bd059
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2018-10-22 10:56:47 +0000

    VideoRec/Main: Append screen ID to dumped video frames (enabled debug builds only).
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@74971 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Main/src-client/VideoRec.cpp
+++ src/VBox/Main/src-client/VideoRec.cpp
@@ -1894,8 +1894,11 @@ int VideoRecSendVideoFrame(PVIDEORECCONTEXT pCtx, uint32_t uScreen, uint32_t x, 
         bmpDIBHdr.u32XPelsPerMeter = 5000;
         bmpDIBHdr.u32YPelsPerMeter = 5000;
 
+        char szFileName[RTPATH_MAX];
+        RTStrPrintf2(szFileName, sizeof(szFileName), "/tmp/VideoRecFrame-%RU32.bmp", uScreen);
+
         RTFILE fh;
-        int rc2 = RTFileOpen(&fh, "/tmp/VideoRecFrame.bmp",
+        int rc2 = RTFileOpen(&fh, szFileName,
                              RTFILE_O_CREATE_REPLACE | RTFILE_O_WRITE | RTFILE_O_DENY_NONE);
         if (RT_SUCCESS(rc2))
         {
