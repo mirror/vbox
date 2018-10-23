f3198efa8bbe008578876b22ca4dd3e8edfaabdd
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2018-10-23 10:59:33 +0000

    VideoRec/Main: Don't use the logging facility in EBMLWriter.h.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@74991 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Main/include/EBMLWriter.h
+++ src/VBox/Main/include/EBMLWriter.h
@@ -4,7 +4,7 @@
  */
 
 /*
- * Copyright (C) 2013-2017 Oracle Corporation
+ * Copyright (C) 2013-2018 Oracle Corporation
  *
  * This file is part of VirtualBox Open Source Edition (OSE), as
  * available from http://www.virtualbox.org. This file is free software;
@@ -22,12 +22,6 @@
 #include <VBox/com/string.h> /* For Utf8Str. */
 
 using namespace com;
-
-#ifdef LOG_GROUP
-# undef LOG_GROUP
-#endif
-#define LOG_GROUP LOG_GROUP_MAIN_DISPLAY
-#include "LoggingNew.h"
 
 #include <list>
 #include <map>
