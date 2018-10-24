508bfc543f49d4082e1cd1a46f6b99aac690d8a3
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2018-10-24 11:26:34 +0000

    FE/Qt: UIActionPoolSelector: Snapshot properties icon fix.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@75026 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Frontends/VirtualBox/src/globals/UIActionPoolSelector.cpp
+++ src/VBox/Frontends/VirtualBox/src/globals/UIActionPoolSelector.cpp
@@ -1780,8 +1780,8 @@ public:
     UIActionMenuSelectorSnapshotToggleProperties(UIActionPool *pParent)
         : UIActionToggle(pParent)
     {
-        setIcon(UIIconPool::iconSetFull(":/snapshot_show_details_16px.png", ":/snapshot_show_details_16px.png",
-                                        ":/snapshot_show_details_disabled_16px.png", ":/snapshot_show_details_disabled_16px.png"));
+        setIcon(UIIconPool::iconSetFull(":/snapshot_show_details_32px.png", ":/snapshot_show_details_16px.png",
+                                        ":/snapshot_show_details_disabled_32px.png", ":/snapshot_show_details_disabled_16px.png"));
     }
 
 protected:
