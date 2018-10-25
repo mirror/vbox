9879a36231b9a7e2ec54eec450ddc33fb36aa46c
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2018-10-25 14:29:11 +0000

    FE/Qt: bugref:9254: UIScaleFactorEditor: A bit of style-fixes.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@75074 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Frontends/VirtualBox/src/widgets/UIScaleFactorEditor.cpp
+++ src/VBox/Frontends/VirtualBox/src/widgets/UIScaleFactorEditor.cpp
@@ -29,6 +29,7 @@
 
 /* GUI includes: */
 # include "QIAdvancedSlider.h"
+# include "VBoxGlobal.h"
 # include "UIScaleFactorEditor.h"
 
 #endif /* !VBOX_WITH_PRECOMPILED_HEADERS */
@@ -236,6 +237,8 @@ void UIScaleFactorEditor::prepare()
     if (m_pScaleSpinBox)
     {
         m_pMainLayout->addWidget(m_pScaleSpinBox, 0, 3);
+        vboxGlobal().setMinimumWidthAccordingSymbolCount(m_pScaleSpinBox, 7);
+        m_pScaleSpinBox->setSuffix("%");
         m_pScaleSpinBox->setMinimum(100);
         m_pScaleSpinBox->setMaximum(200);
         connect(m_pScaleSpinBox ,static_cast<void(QSpinBox::*)(int)>(&QSpinBox::valueChanged),
