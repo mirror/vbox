8c709dada4fccd6bc05eea79888976d1a8f16df1
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2018-10-25 10:17:55 +0000

    FE/Qt: bugref:9241: VirtualBox Manager: Few new icons.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@75060 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Frontends/VirtualBox/src/globals/UIActionPool.cpp
+++ src/VBox/Frontends/VirtualBox/src/globals/UIActionPool.cpp
@@ -1132,7 +1132,7 @@ public:
 
     /** Constructs action passing @a pParent to the base-class. */
     UIActionSimplePreferences(UIActionPool *pParent)
-        : UIActionSimple(pParent, ":/global_settings_16px.png", ":/global_settings_16px.png", true)
+        : UIActionSimple(pParent, ":/global_settings_32px.png", ":/global_settings_16px.png", true)
     {
         setMenuRole(QAction::PreferencesRole);
         retranslateUi();
--- src/VBox/Frontends/VirtualBox/src/globals/UIActionPoolSelector.cpp
+++ src/VBox/Frontends/VirtualBox/src/globals/UIActionPoolSelector.cpp
@@ -173,7 +173,7 @@ public:
 
     /** Constructs action passing @a pParent to the base-class. */
     UIActionSimpleSelectorFileShowImportApplianceWizard(UIActionPool *pParent)
-        : UIActionSimple(pParent, ":/import_16px.png", ":/import_16px.png")
+        : UIActionSimple(pParent, ":/import_32px.png", ":/import_16px.png")
     {}
 
 protected:
@@ -210,7 +210,7 @@ public:
 
     /** Constructs action passing @a pParent to the base-class. */
     UIActionSimpleSelectorFileShowExportApplianceWizard(UIActionPool *pParent)
-        : UIActionSimple(pParent, ":/export_16px.png", ":/export_16px.png")
+        : UIActionSimple(pParent, ":/export_32px.png", ":/export_16px.png")
     {}
 
 protected:
@@ -377,7 +377,7 @@ public:
 
     /** Constructs action passing @a pParent to the base-class. */
     UIActionSimpleSelectorGroupPerformAddMachine(UIActionPool *pParent)
-        : UIActionSimple(pParent, ":/vm_add_16px.png", ":/vm_add_16px.png")
+        : UIActionSimple(pParent, ":/vm_add_32px.png", ":/vm_add_16px.png")
     {}
 
 protected:
@@ -565,7 +565,7 @@ public:
 
     /** Constructs action passing @a pParent to the base-class. */
     UIActionSimpleSelectorMachinePerformAdd(UIActionPool *pParent)
-        : UIActionSimple(pParent, ":/vm_add_16px.png", ":/vm_add_16px.png")
+        : UIActionSimple(pParent, ":/vm_add_32px.png", ":/vm_add_16px.png")
     {}
 
 protected:
