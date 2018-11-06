29a0e31da9e02ac0f737d94086ea8ccbcf9141b8
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2018-11-06 12:58:09 +0000

    Main/Settings: Comment nit.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@75283 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Main/xml/Settings.cpp
+++ src/VBox/Main/xml/Settings.cpp
@@ -4406,7 +4406,7 @@ void MachineConfigFile::readHardware(const xml::ElementNode &elmHardware,
             pelmHwChild->getAttributeValue("enabled",   hw.captureSettings.fEnabled);
 
             /* Right now I don't want to bump the settings version, so just convert the enabled
-             * screens to the former uint6t_t bit array and vice versa. */
+             * screens to the former uint64t_t bit array and vice versa. */
             uint64_t u64VideoCaptureScreens;
             pelmHwChild->getAttributeValue("screens",   u64VideoCaptureScreens);
 
@@ -5821,7 +5821,7 @@ void MachineConfigFile::buildHardwareXML(xml::ElementNode &elmParent,
             pelmVideoCapture->setAttribute("enabled", hw.captureSettings.fEnabled);
 
         /* Right now I don't want to bump the settings version, so just convert the enabled
-         * screens to the former uint6t_t bit array and vice versa. */
+         * screens to the former uint64t_t bit array and vice versa. */
         uint64_t u64VideoCaptureScreens = 0;
         CaptureScreenMap::const_iterator itScreen = hw.captureSettings.mapScreens.begin();
         while (itScreen != hw.captureSettings.mapScreens.end())
