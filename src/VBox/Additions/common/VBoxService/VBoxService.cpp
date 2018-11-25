53d517eb034d086b0ec5b13a9d5b38a2901dd999
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2018-11-25 11:46:26 +0000

    VBoxService: Fixed incorrect (and unncessary) assertion in vgsvcArgString.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@75713 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Additions/common/VBoxService/VBoxService.cpp
+++ src/VBox/Additions/common/VBoxService/VBoxService.cpp
@@ -531,7 +531,7 @@ int VGSvcArgUInt32(int argc, char **argv, const char *psz, int *pi, uint32_t *pu
 static int vgsvcArgString(int argc, char **argv, const char *psz, int *pi, char *pszBuf, size_t cbBuf)
 {
     AssertPtrReturn(pszBuf, VERR_INVALID_POINTER);
-    AssertPtrReturn(cbBuf, VERR_INVALID_PARAMETER);
+    AssertReturn(cbBuf, VERR_INVALID_PARAMETER);
 
     if (*psz == ':' || *psz == '=')
         psz++;
@@ -1078,7 +1078,7 @@ int main(int argc, char **argv)
                 case 'l':
                 {
                     rc = vgsvcArgString(argc, argv, psz + 1, &i,
-                                              g_szLogFile, sizeof(g_szLogFile));
+                                        g_szLogFile, sizeof(g_szLogFile));
                     if (rc)
                         return rc;
                     psz = NULL;
@@ -1088,7 +1088,7 @@ int main(int argc, char **argv)
                 case 'p':
                 {
                     rc = vgsvcArgString(argc, argv, psz + 1, &i,
-                                              g_szPidFile, sizeof(g_szPidFile));
+                                        g_szPidFile, sizeof(g_szPidFile));
                     if (rc)
                         return rc;
                     psz = NULL;
