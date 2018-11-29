bb55a21da88421b74b7621f13dc3ae79d26e0cb5
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2018-11-29 11:21:19 +0000

    VMM/HMVMXR0: Doxygen nit.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@75810 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/VMM/VMMR0/HMVMXR0.cpp
+++ src/VBox/VMM/VMMR0/HMVMXR0.cpp
@@ -7590,6 +7590,7 @@ static uint32_t hmR0VmxEvaluatePendingEvent(PVMCPU pVCpu)
  * Sets a pending-debug exception to be delivered to the guest if the guest is
  * single-stepping in the VMCS.
  *
+ * @returns VBox status code.
  * @param   pVCpu           The cross context virtual CPU structure.
  */
 DECLINLINE(int) hmR0VmxSetPendingDebugXcptVmcs(PVMCPU pVCpu)
