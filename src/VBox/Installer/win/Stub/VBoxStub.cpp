3238c5724d1934121e004dbad0f7a114fd89e1b7
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2018-11-29 12:57:29 +0000

    Installer/win/VBoxStub: 32-bit hosts are no longer officially supported. Use 5.2.x if you still have one.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@75812 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Installer/win/Stub/VBoxStub.cpp
+++ src/VBox/Installer/win/Stub/VBoxStub.cpp
@@ -374,7 +374,6 @@ static int ExtractFile(const char *pszResourceName,
  *
  * @param   pPackage            Pointer to a VBOXSTUBPKG struct that contains the resource.
  * @param   pszTempFile         The full file path + name to extract the resource to.
- *
  */
 static int Extract(const PVBOXSTUBPKG  pPackage,
                    const char         *pszTempFile)
@@ -387,21 +386,20 @@ static int Extract(const PVBOXSTUBPKG  pPackage,
  * Detects whether we're running on a 32- or 64-bit platform and returns the result.
  *
  * @returns TRUE if we're running on a 64-bit OS, FALSE if not.
- *
  */
 static BOOL IsWow64(void)
 {
-    BOOL bIsWow64 = TRUE;
+    BOOL fIsWow64 = TRUE;
     fnIsWow64Process = (LPFN_ISWOW64PROCESS)GetProcAddress(GetModuleHandle(TEXT("kernel32")), "IsWow64Process");
     if (NULL != fnIsWow64Process)
     {
-        if (!fnIsWow64Process(GetCurrentProcess(), &bIsWow64))
+        if (!fnIsWow64Process(GetCurrentProcess(), &fIsWow64))
         {
             /* Error in retrieving process type - assume that we're running on 32bit. */
             return FALSE;
         }
     }
-    return bIsWow64;
+    return fIsWow64;
 }
 
 
@@ -411,7 +409,6 @@ static BOOL IsWow64(void)
  * @returns @c true if we need to handle the specified package, @c false if not.
  *
  * @param   pPackage            Pointer to a VBOXSTUBPKG struct that contains the resource.
- *
  */
 static bool PackageIsNeeded(PVBOXSTUBPKG pPackage)
 {
@@ -1084,6 +1081,17 @@ int WINAPI WinMain(HINSTANCE  hInstance,
 #endif
         RTPrintf("Additional MSI parameters: %s\n",
                  szMSIArgs[0] ? szMSIArgs : "<None>");
+    }
+
+    /*
+     * 32-bit is not officially supported any more.
+     */
+    if (   !fExtractOnly
+        && !g_fSilent
+        && !IsWow64())
+    {
+        rcExit = ShowError("32-bit Windows hosts are not supported by this VirtualBox release.");
+        vrc = VERR_NOT_SUPPORTED;
     }
 
     if (RT_SUCCESS(vrc))
