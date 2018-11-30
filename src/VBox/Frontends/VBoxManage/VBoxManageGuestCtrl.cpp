f732c775118611d73df115425a7cb01a7a49b849
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2018-11-30 12:06:02 +0000

    VBoxManage/guestcontrol: fix
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@75845 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Frontends/VBoxManage/VBoxManageGuestCtrl.cpp
+++ src/VBox/Frontends/VBoxManage/VBoxManageGuestCtrl.cpp
@@ -1845,7 +1845,8 @@ static RTEXITCODE gctlHandleCopy(PGCTLCMDCTX pCtx, int argc, char **argv, bool f
 
         RT_NOREF(strFilter);
         if (strFilter.isNotEmpty())
-            rcExit = RTMsgErrorExitFailure("Skipping '%s/%s' because wildcard expansion isn't implemented yet\n");
+            rcExit = RTMsgErrorExitFailure("Skipping '%s/%s' because wildcard expansion isn't implemented yet\n",
+                                           srcSrc.c_str(),  srcFilter.c_src());
         else if (fHostToGuest)
         {
             /*
