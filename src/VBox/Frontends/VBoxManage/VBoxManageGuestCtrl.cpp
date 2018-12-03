a8cbc5bbd858cc67fd73d62205e375de2b4ee995
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2018-12-03 21:55:52 +0000

    Main,Frontends: s/FileName/Filename/g (API); Took a shot a completing the code for populating IGuestFsObjInfo. bugref:9320 [build fix]
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@75927 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Frontends/VBoxManage/VBoxManageGuestCtrl.cpp
+++ src/VBox/Frontends/VBoxManage/VBoxManageGuestCtrl.cpp
@@ -2594,7 +2594,7 @@ static DECLCALLBACK(RTEXITCODE) gctlHandleStat(PGCTLCMDCTX pCtx, int argc, char 
                 do *pszAttribs++ = '\0';
                 while (*pszAttribs == ' ');
             else
-                pszAttribs = "";
+                pszAttribs = strchr(pszMode, '\0');
             if (uDeviceNo != 0)
                 RTPrintf("  Mode: %-16s Attrib: %-17s Dev ID: %#RX32\n", pszMode, pszAttribs, uDeviceNo);
             else
