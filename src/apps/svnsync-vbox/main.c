a3d7ce2b7f22168fb5002772a94efaa8641fcc2d
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2018-12-06 19:56:16 +0000

    svnsync-vbox: handle tricky cases better: deleting of files which are changed and renamed in a directory which also got renamed (only way is to ignore the error, found no way to detect) and also handle adding files which get renamed in the same changeset (i.e. newly exported and renamed)
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@76026 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/apps/svnsync-vbox/main.c
+++ src/apps/svnsync-vbox/main.c
@@ -1549,8 +1549,9 @@ delete_entry(const char *path,
   if (prev_process && !ignore_everything)
   {
     eb->changeset_live = TRUE;
-    SVN_ERR(eb->wrapped_editor->delete_entry(path, base_revision,
-                                             pb->wrapped_node_baton, pool));
+    /* Deliberately ignore error, it's the only safe solution. */
+    eb->wrapped_editor->delete_entry(path, base_revision,
+                                     pb->wrapped_node_baton, pool);
   }
 
   return SVN_NO_ERROR;
@@ -1956,7 +1957,7 @@ open_file(const char *path,
                            pb->process_default, pb->process_recursive, path,
                            eb->current, &fb->process, NULL, NULL, pool));
     if (file_added_this_changeset)
-      fb->prev_process = fb->process;
+      fb->prev_process = FALSE;
     fb->prev_process_default = FALSE;
     fb->process_default = FALSE;
     DX(fprintf(stderr, "  %s (prev %s)\n", fb->process ? "EXPORT" : "IGNORE", fb->prev_process ? "EXPORT" : "IGNORE");)
