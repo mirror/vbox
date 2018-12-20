9ae5d69e34987fdebeb0ba3ca4f839fd90c17209
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2018-12-20 14:22:40 +0000

    DevVGA-SVGA3d-glLdr.h: Temporary hack for system glext.h using different header guard macro and defining GL_GLEXT_VERSION differently.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@76315 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Devices/Graphics/DevVGA-SVGA3d-glLdr.h
+++ src/VBox/Devices/Graphics/DevVGA-SVGA3d-glLdr.h
@@ -60,6 +60,12 @@ typedef void (APIENTRYP PFNGLGETPROGRAMIVARBPROC) (GLenum target, GLenum pname, 
 # define VBOX_VMSVGA3D_GL_HACK_LEVEL 0x103
 #endif
 
+#ifndef __glext_h__
+# undef GL_GLEXT_VERSION    /** @todo r=bird: We include GL/glext.h above which also defines this and we'll end up with
+                             * a clash if the system one does not use the same header guard as ours.  So, I'm wondering
+                             * whether this include is really needed, and if it is, whether we should use a unique header
+                             * guard macro on it, so we'll have the same problems everywhere... */
+#endif
 #include "vmsvga_glext/glext.h"
 
 
