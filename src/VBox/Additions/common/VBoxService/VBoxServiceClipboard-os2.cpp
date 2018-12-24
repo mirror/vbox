ff7237f9162da2793b696567f5384be5b8e52a10
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2018-12-24 15:16:48 +0000

    iprt/string.h: Dropped including utf16.h and let those who need it include it themselves.  bugref:9344
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@76441 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Additions/common/VBoxService/VBoxServiceClipboard-os2.cpp
+++ src/VBox/Additions/common/VBoxService/VBoxServiceClipboard-os2.cpp
@@ -42,14 +42,15 @@
 #define INCL_ERRORS
 #include <os2.h>
 
-#include <iprt/thread.h>
-#include <iprt/string.h>
-#include <iprt/semaphore.h>
-#include <iprt/time.h>
+#include <iprt/asm.h>
+#include <iprt/assert.h>
 #include <iprt/mem.h>
 #include <iprt/param.h>
-#include <iprt/assert.h>
-#include <iprt/asm.h>
+#include <iprt/semaphore.h>
+#include <iprt/string.h>
+#include <iprt/thread.h>
+#include <iprt/time.h>
+#include <iprt/utf16.h>
 #include <VBox/VBoxGuestLib.h>
 #include <VBox/HostServices/VBoxClipboardSvc.h>
 #include "VBoxServiceInternal.h"
