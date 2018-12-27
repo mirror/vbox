7ad362cc732f50c9e7467930dc2572df55d5f8da
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2018-12-27 17:05:26 +0000

    FE/Qt: 6699: Change some function access modifiers.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@76499 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Frontends/VirtualBox/src/guestctrl/UIFileManagerHostTable.h
+++ src/VBox/Frontends/VirtualBox/src/guestctrl/UIFileManagerHostTable.h
@@ -34,14 +34,14 @@ class UIFileManagerHostTable : public UIFileManagerTable
 public:
 
     UIFileManagerHostTable(UIActionPool *pActionPool, QWidget *pParent = 0);
+    static KFsObjType  fileType(const QFileInfo &fsInfo);
+
+protected:
+
     /** Scans the directory with the path @strPath and inserts items to the
      *  tree under the @p parent. */
     static void scanDirectory(const QString& strPath, UICustomFileSystemItem *parent,
                               QMap<QString, UICustomFileSystemItem*> &fileObjects);
-
-protected:
-
-    static KFsObjType  fileType(const QFileInfo &fsInfo);
     void            retranslateUi() /* override */;
     virtual void    readDirectory(const QString& strPath, UICustomFileSystemItem *parent, bool isStartDir = false) /* override */;
     virtual void    deleteByItem(UICustomFileSystemItem *item) /* override */;
