d9bae82060e4a518aeca2c3558ad0080997d00f3
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2019-01-02 11:42:07 +0000

    Linux installer: Fixed missing symlinks to VirtualBoxVM.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@76608 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Installer/linux/install.sh
+++ src/VBox/Installer/linux/install.sh
@@ -313,6 +313,9 @@ if [ "$ACTION" = "install" ]; then
 
     # Create symlinks to start binaries
     ln -sf $INSTALLATION_DIR/VBox.sh /usr/bin/VirtualBox
+    if [ -f $INSTALLATION_DIR/VirtualBoxVM ]; then
+        ln -sf $INSTALLATION_DIR/VBox.sh /usr/bin/VirtualBoxVM
+    fi
     ln -sf $INSTALLATION_DIR/VBox.sh /usr/bin/VBoxManage
     ln -sf $INSTALLATION_DIR/VBox.sh /usr/bin/VBoxSDL
     ln -sf $INSTALLATION_DIR/VBox.sh /usr/bin/VBoxVRDP
@@ -335,6 +338,9 @@ if [ "$ACTION" = "install" ]; then
 
     # Convenience symlinks. The creation fails if the FS is not case sensitive
     ln -sf VirtualBox /usr/bin/virtualbox > /dev/null 2>&1
+    if [ -f $INSTALLATION_DIR/VirtualBoxVM ]; then
+        ln -sf VirtualBox /usr/bin/virtualboxvm > /dev/null 2>&1
+    fi
     ln -sf VBoxManage /usr/bin/vboxmanage > /dev/null 2>&1
     ln -sf VBoxSDL /usr/bin/vboxsdl > /dev/null 2>&1
     ln -sf VBoxHeadless /usr/bin/vboxheadless > /dev/null 2>&1
