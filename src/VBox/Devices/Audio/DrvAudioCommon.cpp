92e040ab50d3482c59ec691f8ba096371e2e7c32
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2019-01-17 13:52:49 +0000

    Audio/DrvAudioCommon: Comment nits.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@76858 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Devices/Audio/DrvAudioCommon.cpp
+++ src/VBox/Devices/Audio/DrvAudioCommon.cpp
@@ -927,8 +927,8 @@ void DrvAudioHlpPCMPropsPrint(const PPDMAUDIOPCMPROPS pProps)
  * Converts PCM properties to a audio stream configuration.
  *
  * @return  IPRT status code.
- * @param   pProps              Pointer to PCM properties to convert.
- * @param   pCfg                Pointer to audio stream configuration to store result into.
+ * @param   pProps              PCM properties to convert.
+ * @param   pCfg                Stream configuration to store result into.
  */
 int DrvAudioHlpPCMPropsToStreamCfg(const PPDMAUDIOPCMPROPS pProps, PPDMAUDIOSTREAMCFG pCfg)
 {
@@ -944,7 +944,7 @@ int DrvAudioHlpPCMPropsToStreamCfg(const PPDMAUDIOPCMPROPS pProps, PPDMAUDIOSTRE
 /**
  * Initializes a stream configuration with its default values.
  *
- * @param   pCfg                Pointer to stream to initialize.
+ * @param   pCfg                Stream configuration to initialize.
  */
 void DrvAudioHlpStreamCfgInit(PPDMAUDIOSTREAMCFG pCfg)
 {
