8f532484aed0ec552115132defccb8738bdb57f5
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2019-02-01 10:22:42 +0000

    VMReq.cpp: clang compile fix, use ASMAtomicWriteNullPtr instead
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@77099 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/VMM/VMMR3/VMReq.cpp
+++ src/VBox/VMM/VMMR3/VMReq.cpp
@@ -669,7 +669,7 @@ VMMR3DECL(int) VMR3ReqAlloc(PUVM pUVM, PVMREQ *ppReq, VMREQTYPE enmType, VMCPUID
             Assert(pReq->enmType == VMREQTYPE_INVALID);
             Assert(pReq->enmState == VMREQSTATE_FREE);
             Assert(pReq->pUVM == pUVM);
-            ASMAtomicXchgSize(&pReq->pNext, NULL);
+            ASMAtomicWriteNullPtr(&pReq->pNext);
             pReq->enmState = VMREQSTATE_ALLOCATED;
             pReq->iStatus  = VERR_VM_REQUEST_STATUS_STILL_PENDING;
             pReq->fFlags   = VMREQFLAGS_VBOX_STATUS;
