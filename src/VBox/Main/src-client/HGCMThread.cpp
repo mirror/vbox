f71cbccfe25fcafc0f1f2e4f23d62b41525ba709
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2019-02-06 21:14:23 +0000

    Main/HGCMThread: Don't clear the thread handle when the thread exits as the thread was created waitable and the handle is required to properly wait on it for termination (first and foremost memory leak fix but who knows, might also fix some HGCM related races where the service thread didn't terminate and the service was already destroyed)
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@77191 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Main/src-client/HGCMThread.cpp
+++ src/VBox/Main/src-client/HGCMThread.cpp
@@ -201,8 +201,6 @@ static DECLCALLBACK(int) hgcmWorkerThreadFunc(RTTHREAD hThreadSelf, void *pvUser
 
     pThread->m_fu32ThreadFlags |= HGCMMSG_TF_TERMINATED;
 
-    pThread->m_hThread = NIL_RTTHREAD;
-
     LogFlow(("MAIN::hgcmWorkerThreadFunc: completed HGCM thread %p\n", pThread));
 
     return rc;
@@ -258,7 +256,10 @@ int HGCMThread::WaitForTermination(void)
     LogFlowFunc(("\n"));
 
     if (m_hThread != NIL_RTTHREAD)
+    {
         rc = RTThreadWait(m_hThread, 5000, NULL);
+        m_hThread = NIL_RTTHREAD;
+    }
 
     LogFlowFunc(("rc = %Rrc\n", rc));
     return rc;
