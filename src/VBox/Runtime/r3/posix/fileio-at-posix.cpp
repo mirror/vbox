0595e6f4cb469a6c507d61ecff9c14f3dfab563f
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2019-02-08 00:07:03 +0000

    IPRT: Added r3/posix/fileio-at-posix.cpp for more efficient and safer RTFileWriteAt/ReadAt implementations.  bugref:9172
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@77210 cfe28804-0f27-0410-a406-dd0f0b0b656f

---
+++ src/VBox/Runtime/r3/posix/fileio-at-posix.cpp
@@ -0,0 +1,97 @@
+/* $Id$ */
+/** @file
+ * IPRT - File I/O, RTFileReadAt and RTFileWriteAt, posix.
+ */
+
+/*
+ * Copyright (C) 2006-2019 Oracle Corporation
+ *
+ * This file is part of VirtualBox Open Source Edition (OSE), as
+ * available from http://www.virtualbox.org. This file is free software;
+ * you can redistribute it and/or modify it under the terms of the GNU
+ * General Public License (GPL) as published by the Free Software
+ * Foundation, in version 2 as it comes in the "COPYING" file of the
+ * VirtualBox OSE distribution. VirtualBox OSE is distributed in the
+ * hope that it will be useful, but WITHOUT ANY WARRANTY of any kind.
+ *
+ * The contents of this file may alternatively be used under the terms
+ * of the Common Development and Distribution License Version 1.0
+ * (CDDL) only, as it comes in the "COPYING.CDDL" file of the
+ * VirtualBox OSE distribution, in which case the provisions of the
+ * CDDL are applicable instead of those of the GPL.
+ *
+ * You may elect to license modified versions of this file under the
+ * terms and conditions of either the GPL or the CDDL or both.
+ */
+
+
+/*********************************************************************************************************************************
+*   Header Files                                                                                                                 *
+*********************************************************************************************************************************/
+#include <errno.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+#include "internal/iprt.h"
+#include <iprt/file.h>
+
+#include <iprt/err.h>
+#include <iprt/log.h>
+
+
+
+RTDECL(int)  RTFileReadAt(RTFILE hFile, RTFOFF off, void *pvBuf, size_t cbToRead, size_t *pcbRead)
+{
+    ssize_t cbRead = pread(RTFileToNative(hFile), pvBuf, cbToRead, off);
+    if (cbRead >= 0)
+    {
+        if (pcbRead)
+            /* caller can handle partial read. */
+            *pcbRead = cbRead;
+        else
+        {
+            /* Caller expects all to be read. */
+            while ((ssize_t)cbToRead > cbRead)
+            {
+                ssize_t cbReadPart = pread(RTFileToNative(hFile), (char*)pvBuf + cbRead, cbToRead - cbRead, off + cbRead);
+                if (cbReadPart <= 0)
+                {
+                    if (cbReadPart == 0)
+                        return VERR_EOF;
+                    return RTErrConvertFromErrno(errno);
+                }
+                cbRead += cbReadPart;
+            }
+        }
+        return VINF_SUCCESS;
+    }
+
+    return RTErrConvertFromErrno(errno);
+}
+
+
+RTDECL(int)  RTFileWriteAt(RTFILE hFile, RTFOFF off, const void *pvBuf, size_t cbToWrite, size_t *pcbWritten)
+{
+    ssize_t cbWritten = pwrite(RTFileToNative(hFile), pvBuf, cbToWrite, off);
+    if (cbWritten >= 0)
+    {
+        if (pcbWritten)
+            /* caller can handle partial write. */
+            *pcbWritten = cbWritten;
+        else
+        {
+            /* Caller expects all to be write. */
+            while ((ssize_t)cbToWrite > cbWritten)
+            {
+                ssize_t cbWrittenPart = pwrite(RTFileToNative(hFile), (const char *)pvBuf + cbWritten, cbToWrite - cbWritten,
+                                               off + cbWritten);
+                if (cbWrittenPart <= 0)
+                    return RTErrConvertFromErrno(errno);
+                cbWritten += cbWrittenPart;
+            }
+        }
+        return VINF_SUCCESS;
+    }
+    return RTErrConvertFromErrno(errno);
+}
+
