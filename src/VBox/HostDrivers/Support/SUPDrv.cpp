4dad619943385871ca4b17d0fa57fe5827b3cbe1
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2019-02-22 04:38:58 +0000

    SUPDrv: Use atomics while updating host MSR-cached global variable.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@77423 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/HostDrivers/Support/SUPDrv.cpp
+++ src/VBox/HostDrivers/Support/SUPDrv.cpp
@@ -4602,7 +4602,7 @@ SUPR0DECL(int) SUPR0GetHwvirtMsrs(PSUPHWVIRTMSRS pMsrs, uint32_t fCaps, bool fFo
      * already cached, simply copy the cached MSRs and we're done.
      */
     if (   !fForce
-        && g_fHwvirtMsrsCached)
+        && ASMAtomicReadBool(&g_fHwvirtMsrsCached))
     {
         memcpy(pMsrs, &g_HwvirtMsrs, sizeof(*pMsrs));
         RTThreadPreemptRestore(&PreemptState);
@@ -4657,12 +4657,12 @@ SUPR0DECL(int) SUPR0GetHwvirtMsrs(PSUPHWVIRTMSRS pMsrs, uint32_t fCaps, bool fFo
                 if (fProcCtls2Allowed1 & VMX_PROC_CTLS2_VMFUNC)
                     g_HwvirtMsrs.u.vmx.u64VmFunc = ASMRdMsr(MSR_IA32_VMX_VMFUNC);
             }
-            g_fHwvirtMsrsCached = true;
+            ASMAtomicWriteBool(&g_fHwvirtMsrsCached, true);
         }
         else if (fCaps & SUPVTCAPS_AMD_V)
         {
             g_HwvirtMsrs.u.svm.u64MsrHwcr = ASMRdMsr(MSR_K8_HWCR);
-            g_fHwvirtMsrsCached = true;
+            ASMAtomicWriteBool(&g_fHwvirtMsrsCached, true);
         }
         else
         {
