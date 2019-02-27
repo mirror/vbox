9190cda2dee86e88685fef47d8544060dd55680a
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2019-02-27 13:25:21 +0000

    Runtime/fuzz: build fix
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@77484 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Runtime/common/fuzz/fuzz.cpp
+++ src/VBox/Runtime/common/fuzz/fuzz.cpp
@@ -352,7 +352,7 @@ static void *rtFuzzCtxMemoryAlloc(PRTFUZZCTXINT pThis, size_t cb)
     {
         size_t cbIgn = 0;
         pMemHdr->cb = cb;
-        ASMAtomicAddSize(&pThis->cbMemTotal, cb + sizeof(RTFUZZMEMHDR), &cbIgn); RT_NOREF(cbIgn);
+        size_t cbIgn = ASMAtomicAddZ(&pThis->cbMemTotal, cb + sizeof(RTFUZZMEMHDR)); RT_NOREF(cbIgn);
         return pMemHdr + 1;
     }
 
@@ -371,8 +371,7 @@ static void rtFuzzCtxMemoryFree(PRTFUZZCTXINT pThis, void *pv)
 {
     PRTFUZZMEMHDR pMemHdr = ((PRTFUZZMEMHDR)pv) - 1;
 
-    size_t cbIgn = 0;
-    ASMAtomicSubSize(&pThis->cbMemTotal, pMemHdr->cb + sizeof(RTFUZZMEMHDR), &cbIgn); RT_NOREF(cbIgn);
+    size_t cbIgn = ASMAtomicSubZ(&pThis->cbMemTotal, pMemHdr->cb + sizeof(RTFUZZMEMHDR)); RT_NOREF(cbIgn);
     RTMemFree(pMemHdr);
 }
 
