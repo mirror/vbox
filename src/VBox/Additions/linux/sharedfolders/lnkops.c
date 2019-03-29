0c4f9b8e3b35eaee598e6bac844231167d30df13
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2019-03-29 13:12:50 +0000

    linux/vboxsf: vbsf_put_link signature fix for 4.2, 4.3 and 4.4 (removed in 4.5). bugref:9172
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@77950 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Additions/linux/sharedfolders/lnkops.c
+++ src/VBox/Additions/linux/sharedfolders/lnkops.c
@@ -261,7 +261,9 @@ static int         vbsf_follow_link(struct dentry *dentry, struct nameidata *nd)
  *
  * For kernel before 2.6.8 memory isn't being kept around.
  */
-#  if LINUX_VERSION_CODE >= KERNEL_VERSION(2, 6, 13)
+#  if LINUX_VERSION_CODE >= KERNEL_VERSION(4, 2, 0)
+static void vbsf_put_link(struct dentry *dentry, void *cookie)
+#  elif LINUX_VERSION_CODE >= KERNEL_VERSION(2, 6, 13)
 static void vbsf_put_link(struct dentry *dentry, struct nameidata *nd, void *cookie)
 #  else
 static void vbsf_put_link(struct dentry *dentry, struct nameidata *nd)
