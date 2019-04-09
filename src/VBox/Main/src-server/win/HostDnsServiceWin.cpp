6f38f4b5ff3e6d6b388d2b6c1ae7fc6f7e4e3538
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2019-04-09 14:25:04 +0000

    Main/HostDnsService: Pass fResume = FALSE to SetWaitableTimer() so that DNS change triggered by suspend doesn't cancel that suspend. ticketref:18549
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@78065 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Main/src-server/win/HostDnsServiceWin.cpp
+++ src/VBox/Main/src-server/win/HostDnsServiceWin.cpp
@@ -183,7 +183,7 @@ int HostDnsServiceWin::monitorThreadProc(void)
                 delay.QuadPart = -2 * 1000 * 1000 * 10LL; /* relative: 2s */
 
                 BOOL ok = SetWaitableTimer(m->haDataEvent[DATA_TIMER], &delay,
-                                           0, NULL, NULL, TRUE);
+                                           0, NULL, NULL, FALSE);
                 if (ok)
                 {
                     m->fTimerArmed = true;
