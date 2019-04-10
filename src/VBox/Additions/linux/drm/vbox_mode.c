fb3622e6235d296babac39352422375a4718c80a
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2019-04-10 06:47:16 +0000

    Additions/linux/vboxvideo: attempt to fix a crash in Linux Mint guests. bugref:4567: Linux kernel module maintanance. ticketref:18443: Kernel Oops in vboxvideo.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@78067 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Additions/linux/drm/vbox_mode.c
+++ src/VBox/Additions/linux/drm/vbox_mode.c
@@ -177,6 +177,8 @@ static bool vbox_set_up_input_mapping(struct vbox_private *vbox)
 			if (to_vbox_crtc(crtci)->crtc_id != 0)
 				continue;
 
+			if (!CRTC_FB(crtci))
+				break;
 			vbox->single_framebuffer = true;
 			vbox->input_mapping_width = CRTC_FB(crtci)->width;
 			vbox->input_mapping_height = CRTC_FB(crtci)->height;
