566003cd7c4866a6bc8decef184e14137536acde
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2019-04-25 12:51:33 +0000

    VMM/HMVMXR0: Nested VMX: bugref:9180 Hack to workaround BSOD w/ release builds on Windows 10 64-bit hosts.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@78295 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/VMM/VMMR0/HMVMXR0.cpp
+++ src/VBox/VMM/VMMR0/HMVMXR0.cpp
@@ -7313,6 +7313,18 @@ static int hmR0VmxImportGuestState(PVMCPU pVCpu, PCVMXVMCSINFO pVmcsInfo, uint64
     uint64_t u64Val;
     uint32_t u32Val;
 
+    /*
+     * Note! This is hack to workaround a mysterious BSOD observed with release builds
+     *       on Windows 10 64-bit hosts. Profile and debug builds are not affected and
+     *       neither are other host platforms.
+     *
+     *       Committing this temporarily as it prevents BSOD.
+     */
+#ifdef RT_OS_WINDOWS
+    if (pVM == 0 || pVM == (void *)(uintptr_t)-1)
+        return VERR_HM_IPE_1;
+#endif
+
     STAM_PROFILE_ADV_START(&pVCpu->hm.s.StatImportGuestState, x);
 
     /*
