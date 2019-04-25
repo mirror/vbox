be5bea465b0ac3ad67994190d98fa9d195c17803
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2019-04-25 23:57:05 +0000

    winnt/vboxsf: Hide the RTFS_DOS_NT_REPARSE_POINT attribute as we don't provide any related following information and this attribute would just confuse the guest if it was ever passed to us by the host.  bugref:9172
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@78301 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Additions/WINNT/SharedFolders/driver/vbsfhlp.c
+++ src/VBox/Additions/WINNT/SharedFolders/driver/vbsfhlp.c
@@ -61,7 +61,8 @@ uint32_t VBoxToNTFileAttributes(uint32_t fIprtMode)
     AssertCompile((RTFS_DOS_NT_NOT_CONTENT_INDEXED >> RTFS_DOS_SHIFT) == FILE_ATTRIBUTE_NOT_CONTENT_INDEXED);
     AssertCompile((RTFS_DOS_NT_ENCRYPTED           >> RTFS_DOS_SHIFT) == FILE_ATTRIBUTE_ENCRYPTED);
 
-    uint32_t fNtAttribs = (fIprtMode & (RTFS_DOS_MASK_NT & ~(RTFS_DOS_NT_OFFLINE | RTFS_DOS_NT_DEVICE))) >> RTFS_DOS_SHIFT;
+    uint32_t fNtAttribs = (fIprtMode & (RTFS_DOS_MASK_NT & ~(RTFS_DOS_NT_OFFLINE | RTFS_DOS_NT_DEVICE | RTFS_DOS_NT_REPARSE_POINT)))
+                       >> RTFS_DOS_SHIFT;
     return fNtAttribs ? fNtAttribs : FILE_ATTRIBUTE_NORMAL;
 }
 
@@ -74,6 +75,7 @@ uint32_t VBoxToNTFileAttributes(uint32_t fIprtMode)
 uint32_t NTToVBoxFileAttributes(uint32_t fNtAttribs)
 {
     uint32_t fIprtMode = (fNtAttribs << RTFS_DOS_SHIFT) & RTFS_DOS_MASK_NT;
+    fIprtMode &= ~(RTFS_DOS_NT_OFFLINE | RTFS_DOS_NT_DEVICE | RTFS_DOS_NT_REPARSE_POINT);
     return fIprtMode ? fIprtMode : RTFS_DOS_NT_NORMAL;
 }
 
