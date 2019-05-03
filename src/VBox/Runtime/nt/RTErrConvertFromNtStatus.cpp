523ea3066497e3b46d7155cab123a2d908af6d3a
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2019-05-03 11:30:04 +0000

    tstRTSymlink: Use the enmType parameter so recent w10 stop failing.  Also, translate the STATUS_FILE_IS_A_DIRECTORY we get when using the wrong symlink type.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@78373 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Runtime/nt/RTErrConvertFromNtStatus.cpp
+++ src/VBox/Runtime/nt/RTErrConvertFromNtStatus.cpp
@@ -85,6 +85,7 @@ RTDECL(int)  RTErrConvertFromNtStatus(long lNativeCode)
         case STATUS_SHARING_VIOLATION:      return VERR_SHARING_VIOLATION;
         case STATUS_NO_MEDIA_IN_DEVICE:     return VERR_DRIVE_IS_EMPTY;
         case STATUS_ACCESS_VIOLATION:       return VERR_INVALID_POINTER;
+        case STATUS_FILE_IS_A_DIRECTORY:    return VERR_IS_A_DIRECTORY;
 
         case STATUS_REPARSE_POINT_NOT_RESOLVED:
                                             return VERR_TOO_MANY_SYMLINKS;
