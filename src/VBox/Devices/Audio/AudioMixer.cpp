12301d260b9b96c032e05b12944e4bef3ba6556e
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2019-05-14 15:25:22 +0000

    Audio: Doxygen build fix.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@78510 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Devices/Audio/AudioMixer.cpp
+++ src/VBox/Devices/Audio/AudioMixer.cpp
@@ -110,9 +110,9 @@ static int audioMixerStreamUpdateStatus(PAUDMIXSTREAM pMixStream);
 /**
  * Converts a mixer sink status to a string.
  *
- * @returns Stringified mixer sink flags. Must be free'd with RTStrFree().
+ * @returns Stringified mixer sink status flags. Must be free'd with RTStrFree().
  *          "NONE" if no flags set.
- * @param   fFlags              Mixer sink flags to convert.
+ * @param   fStatus             Mixer sink status to convert.
  */
 static char *dbgAudioMixerSinkStatusToStr(AUDMIXSINKSTS fStatus)
 {
