a2404b966b9933935b42ce38bff53f792fe7fd9c
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2019-05-16 19:23:28 +0000

    winnt/vboxsf: More words describing the FileAllocationInformation setting scenario.  bugref:9172
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@78551 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Additions/WINNT/SharedFolders/driver/info.cpp
+++ src/VBox/Additions/WINNT/SharedFolders/driver/info.cpp
@@ -2178,9 +2178,9 @@ NTSTATUS VBoxMRxSetFileInfo(IN PRX_CONTEXT RxContext)
          *       must use our own cached file size value.  (Cause of broken test
          *       of opening w/ truncation.)
          *
-         * P.S.  When opening a file with the trunction flag set, it seems to be
-         *       the NT I/O manager doing the truncation after opening the file
-         *       as a separate setting FileAllocationInformation operation (no
+         * P.S.  When opening a file with the TRUNCATE_EXISTING disposition,
+         *       kernel32.dll translate it to FILE_OPEN and do the truncating
+         *       separately with a set FileAllocationInformation operation (no
          *       EOF or VDL setting).
          */
         case FileAllocationInformation:
