7a546000996243d8f0a639b6ff69661a2d2abee4
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2019-05-27 08:53:59 +0000

    VMM/HMVMXR0: Nested VMX: bugref:9180 Mov-to-CR0/CR4 VM-exit handling.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@78777 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/VMM/VMMR0/HMVMXR0.cpp
+++ src/VBox/VMM/VMMR0/HMVMXR0.cpp
@@ -12843,6 +12843,27 @@ DECLINLINE(VBOXSTRICTRC) hmR0VmxHandleExitNested(PVMCPU pVCpu, PVMXTRANSIENT pVm
             switch (uAccessType)
             {
                 case VMX_EXIT_QUAL_CRX_ACCESS_WRITE:
+                {
+                    PCVMXVVMCS pVmcsNstGst = pVCpu->cpum.GstCtx.hwvirt.vmx.CTX_SUFF(pVmcs);
+                    Assert(pVmcsNstGst);
+                    uint8_t const iCrReg   = VMX_EXIT_QUAL_CRX_REGISTER(pVmxTransient->uExitQual);
+                    uint8_t const iGReg    = VMX_EXIT_QUAL_CRX_GENREG(pVmxTransient->uExitQual);
+                    Assert(iGReg < RT_ELEMENTS(pVCpu->cpum.GstCtx.aGRegs));
+                    uint64_t const uNewCrx = pVCpu->cpum.GstCtx.aGRegs[iGReg].u64;
+                    if (CPUMIsGuestVmxMovToCr0Cr4InterceptSet(pVCpu, &pVCpu->cpum.GstCtx, iCrReg, uNewCrx))
+                    {
+                        VMXVEXITINFO ExitInfo;
+                        RT_ZERO(ExitInfo);
+                        ExitInfo.uReason = VMX_EXIT_MOV_CRX;
+                        ExitInfo.cbInstr = pVmxTransient->cbInstr;
+                        ExitInfo.u64Qual = pVmxTransient->uExitQual;
+                        rcStrict = IEMExecVmxVmexitInstrWithInfo(pVCpu, &ExitInfo);
+                    }
+                    else
+                        rcStrict = hmR0VmxExitMovCRx(pVCpu, pVmxTransient);
+                    break;
+                }
+
                 case VMX_EXIT_QUAL_CRX_ACCESS_READ:
                 {
                     /** @todo NSTVMX: Implement me. */
