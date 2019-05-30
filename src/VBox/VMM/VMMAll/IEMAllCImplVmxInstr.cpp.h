00ffcff7d660267fb9d1f00385f9e7c208b25b59
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2019-05-30 05:22:30 +0000

    VMM/IEM: Nested VMX: bugref:9180 Use CPUMIsGuestVmxVmreadVmwriteInterceptSet.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@78864 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/VMM/VMMAll/IEMAllCImplVmxInstr.cpp.h
+++ src/VBox/VMM/VMMAll/IEMAllCImplVmxInstr.cpp.h
@@ -7735,48 +7735,6 @@ IEM_STATIC bool iemVmxIsRdmsrWrmsrInterceptSet(PCVMCPU pVCpu, uint32_t uExitReas
 
 
 /**
- * Checks whether a VMREAD or VMWRITE instruction for the given VMCS field is
- * intercepted (causes a VM-exit) or not.
- *
- * @returns @c true if the instruction is intercepted, @c false otherwise.
- * @param   pVCpu           The cross context virtual CPU structure.
- * @param   u64FieldEnc     The VMCS field encoding.
- * @param   uExitReason     The VM-exit reason (VMX_EXIT_VMREAD or
- *                          VMX_EXIT_VMREAD).
- */
-IEM_STATIC bool iemVmxIsVmreadVmwriteInterceptSet(PCVMCPU pVCpu, uint32_t uExitReason, uint64_t u64FieldEnc)
-{
-    Assert(IEM_VMX_IS_NON_ROOT_MODE(pVCpu));
-    Assert(   uExitReason == VMX_EXIT_VMREAD
-           || uExitReason == VMX_EXIT_VMWRITE);
-
-    /* Without VMCS shadowing, all VMREAD and VMWRITE instructions are intercepted. */
-    if (!IEM_GET_GUEST_CPU_FEATURES(pVCpu)->fVmxVmcsShadowing)
-        return true;
-
-    /*
-     * If any reserved bit in the 64-bit VMCS field encoding is set, the VMREAD/VMWRITE is intercepted.
-     * This excludes any reserved bits in the valid parts of the field encoding (i.e. bit 12).
-     */
-    if (u64FieldEnc & VMX_VMCS_ENC_RSVD_MASK)
-        return true;
-
-    /* Finally, consult the VMREAD/VMWRITE bitmap whether to intercept the instruction or not. */
-    uint32_t const u32FieldEnc = RT_LO_U32(u64FieldEnc);
-    Assert(u32FieldEnc >> 3 < VMX_V_VMREAD_VMWRITE_BITMAP_SIZE);
-    Assert(pVCpu->cpum.GstCtx.hwvirt.vmx.CTX_SUFF(pvVmreadBitmap));
-    uint8_t const *pbBitmap = uExitReason == VMX_EXIT_VMREAD
-                            ? (uint8_t const *)pVCpu->cpum.GstCtx.hwvirt.vmx.CTX_SUFF(pvVmreadBitmap)
-                            : (uint8_t const *)pVCpu->cpum.GstCtx.hwvirt.vmx.CTX_SUFF(pvVmwriteBitmap);
-    pbBitmap += (u32FieldEnc >> 3);
-    if (*pbBitmap & RT_BIT(u32FieldEnc & 7))
-        return true;
-
-    return false;
-}
-
-
-/**
  * VMREAD common (memory/register) instruction execution worker
  *
  * @returns Strict VBox status code.
@@ -7793,7 +7751,7 @@ IEM_STATIC VBOXSTRICTRC iemVmxVmreadCommon(PVMCPU pVCpu, uint8_t cbInstr, uint64
 {
     /* Nested-guest intercept. */
     if (   IEM_VMX_IS_NON_ROOT_MODE(pVCpu)
-        && iemVmxIsVmreadVmwriteInterceptSet(pVCpu, VMX_EXIT_VMREAD, u64FieldEnc))
+        && CPUMIsGuestVmxVmreadVmwriteInterceptSet(pVCpu, VMX_EXIT_VMREAD, u64FieldEnc))
     {
         if (pExitInfo)
             return iemVmxVmexitInstrWithInfo(pVCpu, pExitInfo);
@@ -8009,7 +7967,7 @@ IEM_STATIC VBOXSTRICTRC iemVmxVmwrite(PVMCPU pVCpu, uint8_t cbInstr, uint8_t iEf
 {
     /* Nested-guest intercept. */
     if (   IEM_VMX_IS_NON_ROOT_MODE(pVCpu)
-        && iemVmxIsVmreadVmwriteInterceptSet(pVCpu, VMX_EXIT_VMWRITE, u64FieldEnc))
+        && CPUMIsGuestVmxVmreadVmwriteInterceptSet(pVCpu, VMX_EXIT_VMWRITE, u64FieldEnc))
     {
         if (pExitInfo)
             return iemVmxVmexitInstrWithInfo(pVCpu, pExitInfo);
