a829f3efc174163f7bc77c262fea9555153c4a87
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2019-06-22 01:17:15 +0000

    Fixed copyright
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@79290 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Devices/Storage/DevVirtioSCSI.cpp
+++ src/VBox/Devices/Storage/DevVirtioSCSI.cpp
@@ -1,15 +1,20 @@
 /* $Id$ $Revision$ $Date$ $Author$ */
 /** @file
- * VBox Sample Device.
+ * VBox storage devices - Virtio SCSI Driver
+ *
  */
 
 /*
- * Copyright (C) 2009-2019 Oracle Corporation
+ * Copyright (C) 2006-2019 Oracle Corporation
  *
- * Oracle Corporation confidential
- * All rights reserved
+ * This file is part of VirtualBox Open Source Edition (OSE), as
+ * available from http://www.virtualbox.org. This file is free software;
+ * you can redistribute it and/or modify it under the terms of the GNU
+ * General Public License (GPL) as published by the Free Software
+ * Foundation, in version 2 as it comes in the "COPYING" file of the
+ * VirtualBox OSE distribution. VirtualBox OSE is distributed in the
+ * hope that it will be useful, but WITHOUT ANY WARRANTY of any kind.
  */
-
 
 /*********************************************************************************************************************************
 *   Header Files                                                                                                                 *
