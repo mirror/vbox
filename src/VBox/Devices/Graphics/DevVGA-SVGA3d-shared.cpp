9b36a8de037a3ef9ca0d3b3ea890691adb51ffa1
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2019-07-15 11:55:51 +0000

    Devices/Graphics: UYVY and YUY2 formats
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@79791 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Devices/Graphics/DevVGA-SVGA3d-shared.cpp
+++ src/VBox/Devices/Graphics/DevVGA-SVGA3d-shared.cpp
@@ -394,10 +394,15 @@ uint32_t vmsvga3dSurfaceFormatSize(SVGA3dSurfaceFormat format,
         *pcyBlock = 1;
         return 1;
 
-    case SVGA3D_NV12:
+    case SVGA3D_UYVY:
         *pcxBlock = 1;
         *pcyBlock = 1;
-        return 1;
+        return 2;
+
+    case SVGA3D_YUY2:
+        *pcxBlock = 1;
+        *pcyBlock = 1;
+        return 2;
 
     case SVGA3D_V16U16:
         *pcxBlock = 1;
