93d94b206aa2912892523e1070e06c7b566df71b
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2019-07-16 09:55:32 +0000

    VMM/CPUM: Nested VMX: bugref:9180 Fix determining the VMX max physical address width (reported in IA32_VMX_BASIC MSR). It's derived from long mode (whether the guest is a 64-bit capable CPU or not) and if we clear/change it, we adjust this as well.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@79804 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/VMM/VMMR3/CPUMR3CpuId.cpp
+++ src/VBox/VMM/VMMR3/CPUMR3CpuId.cpp
@@ -4751,6 +4751,7 @@ VMMR3_INT_DECL(void) CPUMR3SetGuestCpuIdFeature(PVM pVM, CPUMCPUIDFEATURE enmFea
             /* Valid for both Intel and AMD. */
             pVM->cpum.s.aGuestCpuIdPatmExt[1].uEdx = pLeaf->uEdx |= X86_CPUID_EXT_FEATURE_EDX_LONG_MODE;
             pVM->cpum.s.GuestFeatures.fLongMode = 1;
+            pVM->cpum.s.GuestFeatures.cVmxMaxPhysAddrWidth = pVM->cpum.s.GuestFeatures.cMaxPhysAddrWidth;
             LogRel(("CPUM: SetGuestCpuIdFeature: Enabled LONG MODE\n"));
             break;
 
@@ -5070,6 +5071,7 @@ VMMR3_INT_DECL(void) CPUMR3ClearGuestCpuIdFeature(PVM pVM, CPUMCPUIDFEATURE enmF
             if (pLeaf)
                 pVM->cpum.s.aGuestCpuIdPatmExt[1].uEdx = pLeaf->uEdx &= ~X86_CPUID_EXT_FEATURE_EDX_LONG_MODE;
             pVM->cpum.s.GuestFeatures.fLongMode = 0;
+            pVM->cpum.s.GuestFeatures.cVmxMaxPhysAddrWidth = 32;
             break;
 
         case CPUMCPUIDFEATURE_LAHF:
