0c059759e3f90023d154d919544aa321eebe4017
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2019-07-19 18:48:53 +0000

    FE/Qt: bugref:7720: Fixing typos/comments in r132309.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@79902 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Frontends/VirtualBox/src/manager/details/UIDetailsElement.cpp
+++ src/VBox/Frontends/VirtualBox/src/manager/details/UIDetailsElement.cpp
@@ -440,21 +440,21 @@ void UIDetailsElement::sltHandleAnchorClicked(const QString &strAnchor)
             QPointer<QIDialogContainer> pPopup = new QIDialogContainer(0, Qt::Popup);
             if (pPopup)
             {
-                /* Prepare name editor: */
+                /* Prepare editor: */
                 UINameAndSystemEditor *pEditor = new UINameAndSystemEditor(pPopup, true, false, false);
                 if (pEditor)
                 {
                     pEditor->setName(strData.section(',', 0, 0));
 
-                    /* Add to container: */
+                    /* Add to popup: */
                     pPopup->setWidget(pEditor);
                 }
 
-                /* Adjust dialog geometry: */
+                /* Adjust popup geometry: */
                 pPopup->move(QCursor::pos());
                 pPopup->adjustSize();
 
-                /* Execute the dialog: */
+                /* Execute popup, change machine name if confirmed: */
                 if (pPopup->exec() == QDialog::Accepted)
                     uiCommon().setMachineName(machine(), pEditor->name());
 
