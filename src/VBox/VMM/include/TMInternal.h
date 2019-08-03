f3a53bb4c0f7c94743be53b06a68ec82d81b8ecb
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2019-08-03 13:52:22 +0000

    VMM/TM: Added half an hour of load percentage history.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@80117 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/VMM/include/TMInternal.h
+++ src/VBox/VMM/include/TMInternal.h
@@ -304,7 +304,11 @@ typedef struct TMCPULOADSTATE
     /** The percent of the period spent on other things. */
     uint8_t                 cPctOther;
     /** Explicit alignment padding */
-    uint8_t                 au8Alignment[5];
+    uint8_t                 au8Alignment[1];
+    /** Index into aHistory of the current entry. */
+    uint16_t volatile       idxHistory;
+    /** Number of valid history entries before idxHistory. */
+    uint16_t volatile       cHistoryEntries;
 
     /** Previous cNsTotal value. */
     uint64_t                cNsPrevTotal;
@@ -312,6 +316,15 @@ typedef struct TMCPULOADSTATE
     uint64_t                cNsPrevExecuting;
     /** Previous cNsHalted value. */
     uint64_t                cNsPrevHalted;
+    /** Data for the last 30 min (given an interval of 1 second). */
+    struct
+    {
+        uint8_t             cPctExecuting;
+        /** The percent of the period spent halted. */
+        uint8_t             cPctHalted;
+        /** The percent of the period spent on other things. */
+        uint8_t             cPctOther;
+    }                       aHistory[30*60];
 } TMCPULOADSTATE;
 AssertCompileSizeAlignment(TMCPULOADSTATE, 8);
 AssertCompileMemberAlignment(TMCPULOADSTATE, cNsPrevTotal, 8);
