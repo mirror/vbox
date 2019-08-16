8c4956cc719cd4e79e7032b6cde539143d575d05
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2019-08-16 06:03:08 +0000

    VMM/HMSVMR0: Don't return VINF_EM_RAW_GUEST_TRAP after setting #BP pending for injection, as we may not properly consider the pending event in ring-3.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@80316 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/VMM/VMMR0/HMSVMR0.cpp
+++ src/VBox/VMM/VMMR0/HMSVMR0.cpp
@@ -7666,9 +7666,10 @@ HMSVM_EXIT_DECL hmR0SvmExitXcptBP(PVMCPUCC pVCpu, PSVMTRANSIENT pSvmTransient)
         Event.n.u3Type   = SVM_EVENT_EXCEPTION;
         Event.n.u8Vector = X86_XCPT_BP;
         hmR0SvmSetPendingEvent(pVCpu, &Event, 0 /* GCPtrFaultAddress */);
+        rc = VINF_SUCCESS;
     }
 
-    Assert(rc == VINF_SUCCESS || rc == VINF_EM_RAW_GUEST_TRAP || rc == VINF_EM_DBG_BREAKPOINT);
+    Assert(rc == VINF_SUCCESS || rc == VINF_EM_DBG_BREAKPOINT);
     return rc;
 }
 
