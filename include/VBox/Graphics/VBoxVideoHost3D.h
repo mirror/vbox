b6d01f781a8eefb652f0f1e693efd454899ed15a
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2019-08-28 11:46:50 +0000

    VBoxVideoHost3D.h: Doxygen fixes
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@80472 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- include/VBox/Graphics/VBoxVideoHost3D.h
+++ include/VBox/Graphics/VBoxVideoHost3D.h
@@ -77,6 +77,9 @@ typedef struct VBOXCRCMDCTL_CALLOUT_LIST
 
 struct VBOXCRCMDCTL;
 
+/**
+ * Probably not used anymore (just to keep doxygen happy for now).
+ */
 typedef DECLCALLBACKPTR(int, PFNVBOXCRCLIENT_CALLOUT)(HVBOXCRCLIENT hClient, struct VBOXCRCMDCTL* pCmd, VBOXCRCMDCTL_CALLOUT_LISTENTRY *pEntry, PFNVBOXCRCMDCTL_CALLOUT_CB pfnCb);
 
 typedef struct VBOXCRCLIENT_INFO
@@ -178,6 +181,10 @@ typedef DECLCALLBACKPTR(uint8_t*, PFNVBOXCRCMDCTL_REMAINING_HOST_COMMAND)(HVBOXC
 typedef struct VBOXCRCMDCTL_HGCMENABLE_DATA
 {
     HVBOXCRCMDCTL_REMAINING_HOST_COMMAND hRHCmd;
+
+    /**
+     * Probably not used anymore (just to keep doxygen happy for now).
+     */
     PFNVBOXCRCMDCTL_REMAINING_HOST_COMMAND pfnRHCmd;
 } VBOXCRCMDCTL_HGCMENABLE_DATA;
 
@@ -190,7 +197,15 @@ typedef DECLCALLBACKPTR(void, PFNVBOXCRCMDCTL_NOTIFY_TERMINATING_DONE)(HVBOXCRCM
 typedef struct VBOXCRCMDCTL_HGCMDISABLE_DATA
 {
     HVBOXCRCMDCTL_NOTIFY_TERMINATING hNotifyTerm;
+
+    /**
+     * Probably not used anymore (just to keep doxygen happy for now).
+     */
     PFNVBOXCRCMDCTL_NOTIFY_TERMINATING pfnNotifyTerm;
+
+    /**
+     * Probably not used anymore (just to keep doxygen happy for now).
+     */
     PFNVBOXCRCMDCTL_NOTIFY_TERMINATING_DONE pfnNotifyTermDone;
 } VBOXCRCMDCTL_HGCMDISABLE_DATA;
 
