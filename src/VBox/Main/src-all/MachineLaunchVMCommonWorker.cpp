56455c383b3a6e4abaa64acbf0dd30326705c3c2
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2019-09-05 22:18:23 +0000

    Main/MachineLaunchVMCommonWorker: Simplified darwin specific code for kicking off VirtualVM or override.  Was making too many unnecessary string copies.  [fix]  bugref:9341
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@80613 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Main/src-all/MachineLaunchVMCommonWorker.cpp
+++ src/VBox/Main/src-all/MachineLaunchVMCommonWorker.cpp
@@ -92,9 +92,8 @@ int MachineLaunchVMCommonWorker(const Utf8Str &aNameOrId,
     char szPath[RTPATH_MAX];
     int vrc = RTPathAppPrivateArch(szPath, sizeof(szPath));
     AssertRCReturn(vrc, vrc);
-    vrc = RTPathEnsureTrailingSeparator(szPath, sizeof(szPath));
-    AssertRCReturn(vrc, vrc);
-    size_t cbBufLeft = strlen(szPath);
+    size_t cchBufLeft = RTPathEnsureTrailingSeparator(szPath, sizeof(szPath));
+    AssertReturn(cchBufLeft > 0, VERR_FILENAME_TOO_LONG);
     char *pszNamePart = &szPath[cbBufLeft]; NOREF(pszNamePart);
     cbBufLeft = sizeof(szPath) - cbBufLeft;
 
