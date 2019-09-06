b4f1e2fe5c29ebce7365b1d1e8b2e0dfc34612c4
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2019-09-06 20:20:20 +0000

    DevRTC: Enabled new style code. [release build fix]  bugref:9218
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@80646 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Devices/PC/DevRTC.cpp
+++ src/VBox/Devices/PC/DevRTC.cpp
@@ -684,7 +684,7 @@ static DECLCALLBACK(void) rtcTimerSecond(PPDMDEVINS pDevIns, PTMTIMER pTimer, vo
     Assert(PDMDevHlpTimerIsLockOwner(pDevIns, pThis->hPeriodicTimer));
     Assert(PDMDevHlpCritSectIsOwner(pDevIns, pDevIns->CTX_SUFF(pCritSectRo)));
     Assert(pTimer == PDMDevHlpTimerToPtr(pDevIns, pThis->hSecondTimer));
-    RT_NOREF(pvUser);
+    RT_NOREF(pvUser, pTimer);
 
     /* if the oscillator is not in normal operation, we do not update */
     if ((pThis->cmos_data[RTC_REG_A] & 0x70) != 0x20)
