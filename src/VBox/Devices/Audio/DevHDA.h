afadab7383939190e54a7b57572bb7d15d2b2768
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2019-09-09 20:13:38 +0000

    DevHDA: Some more v-align nit.  This is prep work for converting it to the new PDM model.  bugref:9218
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@80684 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Devices/Audio/DevHDA.h
+++ src/VBox/Devices/Audio/DevHDA.h
@@ -47,9 +47,9 @@
  */
 typedef struct HDAMIXERSINK
 {
-    R3PTRTYPE(PHDASTREAM)  pStream;
+    R3PTRTYPE(PHDASTREAM)   pStream;
     /** Pointer to the actual audio mixer sink. */
-    R3PTRTYPE(PAUDMIXSINK) pMixSink;
+    R3PTRTYPE(PAUDMIXSINK)  pMixSink;
 } HDAMIXERSINK, *PHDAMIXERSINK;
 
 /**
@@ -58,8 +58,8 @@ typedef struct HDAMIXERSINK
 typedef struct HDATAG
 {
     /** Own stream tag. */
-    uint8_t               uTag;
-    uint8_t               Padding[7];
+    uint8_t                 uTag;
+    uint8_t                 Padding[7];
     /** Pointer to associated stream. */
     R3PTRTYPE(PHDASTREAM) pStream;
 } HDATAG, *PHDATAG;
@@ -70,31 +70,31 @@ typedef struct HDASTATEDBGINFO
 #ifdef DEBUG
     /** Timestamp (in ns) of the last timer callback (hdaTimer).
      * Used to calculate the time actually elapsed between two timer callbacks. */
-    uint64_t                           tsTimerLastCalledNs;
+    uint64_t                tsTimerLastCalledNs;
     /** IRQ debugging information. */
     struct
     {
         /** Timestamp (in ns) of last processed (asserted / deasserted) IRQ. */
-        uint64_t                       tsProcessedLastNs;
+        uint64_t            tsProcessedLastNs;
         /** Timestamp (in ns) of last asserted IRQ. */
-        uint64_t                       tsAssertedNs;
+        uint64_t            tsAssertedNs;
         /** How many IRQs have been asserted already. */
-        uint64_t                       cAsserted;
+        uint64_t            cAsserted;
         /** Accumulated elapsed time (in ns) of all IRQ being asserted. */
-        uint64_t                       tsAssertedTotalNs;
+        uint64_t            tsAssertedTotalNs;
         /** Timestamp (in ns) of last deasserted IRQ. */
-        uint64_t                       tsDeassertedNs;
+        uint64_t            tsDeassertedNs;
         /** How many IRQs have been deasserted already. */
-        uint64_t                       cDeasserted;
+        uint64_t            cDeasserted;
         /** Accumulated elapsed time (in ns) of all IRQ being deasserted. */
-        uint64_t                       tsDeassertedTotalNs;
+        uint64_t            tsDeassertedTotalNs;
     } IRQ;
 #endif
     /** Whether debugging is enabled or not. */
-    bool                               fEnabled;
+    bool                    fEnabled;
     /** Path where to dump the debug output to.
      *  Defaults to VBOX_AUDIO_DEBUG_DUMP_PCM_DATA_PATH. */
-    char                               szOutPath[RTPATH_MAX + 1];
+    char                    szOutPath[RTPATH_MAX + 1];
 } HDASTATEDBGINFO, *PHDASTATEDBGINFO;
 
 /**
@@ -103,84 +103,84 @@ typedef struct HDASTATEDBGINFO
 typedef struct HDASTATE
 {
     /** The PCI device structure. */
-    PDMPCIDEV                          PciDev;
+    PDMPCIDEV               PciDev;
     /** R3 Pointer to the device instance. */
-    PPDMDEVINSR3                       pDevInsR3;
+    PPDMDEVINSR3            pDevInsR3;
     /** R0 Pointer to the device instance. */
-    PPDMDEVINSR0                       pDevInsR0;
+    PPDMDEVINSR0            pDevInsR0;
     /** R0 Pointer to the device instance. */
-    PPDMDEVINSRC                       pDevInsRC;
+    PPDMDEVINSRC            pDevInsRC;
     /** Padding for alignment. */
-    uint32_t                           u32Padding;
+    uint32_t                u32Padding;
     /** Critical section protecting the HDA state. */
-    PDMCRITSECT                        CritSect;
+    PDMCRITSECT             CritSect;
     /** The base interface for LUN\#0. */
-    PDMIBASE                           IBase;
-    RTGCPHYS                           MMIOBaseAddr;
+    PDMIBASE                IBase;
+    RTGCPHYS                MMIOBaseAddr;
     /** The HDA's register set. */
-    uint32_t                           au32Regs[HDA_NUM_REGS];
+    uint32_t                au32Regs[HDA_NUM_REGS];
     /** Internal stream states. */
-    HDASTREAM                          aStreams[HDA_MAX_STREAMS];
+    HDASTREAM               aStreams[HDA_MAX_STREAMS];
     /** Mapping table between stream tags and stream states. */
-    HDATAG                             aTags[HDA_MAX_TAGS];
+    HDATAG                  aTags[HDA_MAX_TAGS];
     /** CORB buffer base address. */
-    uint64_t                           u64CORBBase;
+    uint64_t                u64CORBBase;
     /** RIRB buffer base address. */
-    uint64_t                           u64RIRBBase;
+    uint64_t                u64RIRBBase;
     /** DMA base address.
      *  Made out of DPLBASE + DPUBASE (3.3.32 + 3.3.33). */
-    uint64_t                           u64DPBase;
+    uint64_t                u64DPBase;
     /** Pointer to CORB buffer. */
-    R3PTRTYPE(uint32_t *)              pu32CorbBuf;
+    R3PTRTYPE(uint32_t *)   pu32CorbBuf;
     /** Size in bytes of CORB buffer. */
-    uint32_t                           cbCorbBuf;
+    uint32_t                cbCorbBuf;
     /** Padding for alignment. */
-    uint32_t                           u32Padding1;
+    uint32_t                u32Padding1;
     /** Pointer to RIRB buffer. */
-    R3PTRTYPE(uint64_t *)              pu64RirbBuf;
+    R3PTRTYPE(uint64_t *)   pu64RirbBuf;
     /** Size in bytes of RIRB buffer. */
-    uint32_t                           cbRirbBuf;
+    uint32_t                cbRirbBuf;
     /** DMA position buffer enable bit. */
-    bool                               fDMAPosition;
+    bool                    fDMAPosition;
     /** Flag whether the R0 and RC parts are enabled. */
-    bool                               fRZEnabled;
+    bool                    fRZEnabled;
     /** Reserved. */
-    bool                               fPadding1b;
+    bool                    fPadding1b;
     /** Number of active (running) SDn streams. */
-    uint8_t                            cStreamsActive;
+    uint8_t                 cStreamsActive;
     /** The stream timers for pumping data thru the attached LUN drivers. */
-    PTMTIMERR3                         pTimer[HDA_MAX_STREAMS];
+    PTMTIMERR3              pTimer[HDA_MAX_STREAMS];
 #ifdef VBOX_WITH_STATISTICS
-    STAMPROFILE                        StatTimer;
-    STAMPROFILE                        StatIn;
-    STAMPROFILE                        StatOut;
-    STAMCOUNTER                        StatBytesRead;
-    STAMCOUNTER                        StatBytesWritten;
+    STAMPROFILE             StatTimer;
+    STAMPROFILE             StatIn;
+    STAMPROFILE             StatOut;
+    STAMCOUNTER             StatBytesRead;
+    STAMCOUNTER             StatBytesWritten;
 #endif
     /** Pointer to HDA codec to use. */
-    R3PTRTYPE(PHDACODEC)               pCodec;
+    R3PTRTYPE(PHDACODEC)    pCodec;
     /** List of associated LUN drivers (HDADRIVER). */
-    RTLISTANCHORR3                     lstDrv;
+    RTLISTANCHORR3          lstDrv;
     /** The device' software mixer. */
-    R3PTRTYPE(PAUDIOMIXER)             pMixer;
+    R3PTRTYPE(PAUDIOMIXER)  pMixer;
     /** HDA sink for (front) output. */
-    HDAMIXERSINK                       SinkFront;
+    HDAMIXERSINK            SinkFront;
 #ifdef VBOX_WITH_AUDIO_HDA_51_SURROUND
     /** HDA sink for center / LFE output. */
-    HDAMIXERSINK                       SinkCenterLFE;
+    HDAMIXERSINK            SinkCenterLFE;
     /** HDA sink for rear output. */
-    HDAMIXERSINK                       SinkRear;
+    HDAMIXERSINK            SinkRear;
 #endif
     /** HDA mixer sink for line input. */
-    HDAMIXERSINK                       SinkLineIn;
+    HDAMIXERSINK            SinkLineIn;
 #ifdef VBOX_WITH_AUDIO_HDA_MIC_IN
     /** Audio mixer sink for microphone input. */
-    HDAMIXERSINK                       SinkMicIn;
+    HDAMIXERSINK            SinkMicIn;
 #endif
     /** Last updated wall clock (WALCLK) counter. */
-    uint64_t                           u64WalClk;
+    uint64_t                u64WalClk;
     /** Response Interrupt Count (RINTCNT). */
-    uint16_t                           u16RespIntCnt;
+    uint16_t                u16RespIntCnt;
     /** Position adjustment (in audio frames).
      *
      *  This is not an official feature of the HDA specs, but used by
@@ -191,28 +191,28 @@ typedef struct HDASTATE
      *  a stream is ahead from its actual reading/writing position when
      *  starting a stream.
      */
-    uint16_t                           cPosAdjustFrames;
+    uint16_t                cPosAdjustFrames;
     /** Whether the position adjustment is enabled or not. */
-    bool                               fPosAdjustEnabled;
+    bool                    fPosAdjustEnabled;
 #ifdef VBOX_STRICT
     /** Wall clock (WALCLK) stale count.
-     *  This indicates the number of set wall clock
-     *  values which did not actually move the counter forward (stale). */
-    uint8_t                            u8WalClkStaleCnt;
-    uint8_t                            Padding1[2];
+     *  This indicates the number of set wall clock values which did not actually
+     *  move the counter forward (stale). */
+    uint8_t                 u8WalClkStaleCnt;
+    uint8_t                 Padding1[2];
 #else
-    uint8_t                            Padding1[3];
+    uint8_t                 Padding1[3];
 #endif
     /** Current IRQ level. */
-    uint8_t                            u8IRQL;
+    uint8_t                 u8IRQL;
     /** The device timer Hz rate. Defaults to HDA_TIMER_HZ_DEFAULT. */
-    uint16_t                           uTimerHz;
+    uint16_t                uTimerHz;
     /** Padding for alignment. */
-    uint8_t                            au8Padding3[3];
-    HDASTATEDBGINFO                    Dbg;
+    uint8_t                 au8Padding3[3];
+    HDASTATEDBGINFO         Dbg;
     /** This is for checking that the build was correctly configured in all contexts.
-     * This is set to HDASTATE_ALIGNMENT_CHECK_MAGIC.  */
-    uint64_t                            uAlignmentCheckMagic;
+     *  This is set to HDASTATE_ALIGNMENT_CHECK_MAGIC. */
+    uint64_t                uAlignmentCheckMagic;
 } HDASTATE, *PHDASTATE;
 
 /** Value for HDASTATE:uAlignmentCheckMagic. */
