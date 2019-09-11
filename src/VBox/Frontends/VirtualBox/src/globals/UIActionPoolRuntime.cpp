e1860d05900638e47f7fa6403603dccc2f403c68
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2019-09-11 10:03:25 +0000

    FE/Qt: Fixed compilation if VBOX_WITH_EXTPACK is not defined.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@80723 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Frontends/VirtualBox/src/globals/UIActionPoolRuntime.cpp
+++ src/VBox/Frontends/VirtualBox/src/globals/UIActionPoolRuntime.cpp
@@ -3544,7 +3544,7 @@ void UIActionPoolRuntime::updateConfiguration()
     }
 
     /* Recache extension-pack related action restrictions: */
-#if VBOX_WITH_EXTPACK
+#ifdef VBOX_WITH_EXTPACK
     CExtPack extPack = uiCommon().virtualBox().GetExtensionPackManager().Find(GUI_ExtPackName);
 #else
     CExtPack extPack;
