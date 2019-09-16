c20e4bb60d7fff6a2b03121027b9127107d0b70b
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2019-09-16 13:40:38 +0000

    Main: bugref:9341: Fixed doxygen issues
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@80826 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Main/src-all/MachineLaunchVMCommonWorker.cpp
+++ src/VBox/Main/src-all/MachineLaunchVMCommonWorker.cpp
@@ -55,21 +55,21 @@
  *          is buggy, so I wouldn't rely on any exact meaning here!
  * @retval  VERR_INTERNAL_ERROR when something wrong.
  *
- * @param   aNameOrId        The Machine name or id interface the VM will start for.
- * @param   aComment         The comment for new VM process.
- * @param   aFrontend        The desired frontend for started VM.
- * @param   aEnvironment     Additional environment variables=value pairs
- *                           separated by newlines for new VM process.
- * @param   aExtraArg        Extra argument for the VM process.  Ignored if
- *                           empty string.
- * @param   aFilename        Start new VM using specified filename. Only filename
- *                           without path is allowed. Default filename is used if
- *                           empty.
- * @param   aFlags           Flags for RTProcCreateEx functions family if
- *                           required (RTPROC_FLAGS_XXX).
- * @param   aExtraData       Additional data for RTProcCreateX functions family
- *                           if required.  Content is defined by the flags.
- * @param   aPid             The PID of created process is returned here
+ * @param   aNameOrId            The Machine name or id interface the VM will start for.
+ * @param   aComment             The comment for new VM process.
+ * @param   aFrontend            The desired frontend for started VM.
+ * @param   aEnvironmentChanges  Additional environment variables in the putenv style
+ *                               (VAR=VAL for setting, VAR for unsetting) for new VM process.
+ * @param   aExtraArg            Extra argument for the VM process.  Ignored if
+ *                               empty string.
+ * @param   aFilename            Start new VM using specified filename. Only filename
+ *                               without path is allowed. Default filename is used if
+ *                               empty.
+ * @param   aFlags               Flags for RTProcCreateEx functions family if
+ *                               required (RTPROC_FLAGS_XXX).
+ * @param   aExtraData           Additional data for RTProcCreateX functions family
+ *                               if required.  Content is defined by the flags.
+ * @param   aPid                 The PID of created process is returned here
  */
 int MachineLaunchVMCommonWorker(const Utf8Str &aNameOrId,
                                 const Utf8Str &aComment,
