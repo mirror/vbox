bcd573c6ebcd52762c3df28fb458d3d2cb8c8e13
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2019-09-16 12:06:08 +0000

    EFI/FirmwareNew/Logo.c: SCM fix
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@80822 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Devices/EFI/FirmwareNew/VBoxPkg/Logo/Logo.c
+++ src/VBox/Devices/EFI/FirmwareNew/VBoxPkg/Logo/Logo.c
@@ -1,10 +1,33 @@
+/* $Id$ */
 /** @file
-  Logo DXE Driver, install Edkii Platform Logo protocol.
+ * Logo DXE Driver, install Edkii Platform Logo protocol.
+ */
 
-Copyright (c) 2016 - 2017, Intel Corporation. All rights reserved.<BR>
-SPDX-License-Identifier: BSD-2-Clause-Patent
-
-**/
+/*
+ * Copyright (C) 2019 Oracle Corporation
+ *
+ * This file is part of VirtualBox Open Source Edition (OSE), as
+ * available from http://www.virtualbox.org. This file is free software;
+ * you can redistribute it and/or modify it under the terms of the GNU
+ * General Public License (GPL) as published by the Free Software
+ * Foundation, in version 2 as it comes in the "COPYING" file of the
+ * VirtualBox OSE distribution. VirtualBox OSE is distributed in the
+ * hope that it will be useful, but WITHOUT ANY WARRANTY of any kind.
+ *
+ * The contents of this file may alternatively be used under the terms
+ * of the Common Development and Distribution License Version 1.0
+ * (CDDL) only, as it comes in the "COPYING.CDDL" file of the
+ * VirtualBox OSE distribution, in which case the provisions of the
+ * CDDL are applicable instead of those of the GPL.
+ *
+ * You may elect to license modified versions of this file under the
+ * terms and conditions of either the GPL or the CDDL or both.
+ * ---------------------------------------------------------------------------
+ * This code is based on:
+ *
+ * Copyright (c) 2016 - 2017, Intel Corporation. All rights reserved.<BR>
+ * SPDX-License-Identifier: BSD-2-Clause-Patent
+ */
 #include <Uefi.h>
 #include <Protocol/HiiDatabase.h>
 #include <Protocol/GraphicsOutput.h>
