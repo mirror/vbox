edfd0c621b61615ec6d308bb2a8145ca68818b68
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2019-09-18 21:10:26 +0000

    EFI/FirmwareNew: Implement support for the DataHub protocol which was removed in newer EDK versions and enable the VBoxAppleSim DXE driver again as it is required for macOS to boot [scm fix]
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@80903 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Devices/EFI/FirmwareNew/VBoxPkg/VBoxAppleSim/DataHub.c
+++ src/VBox/Devices/EFI/FirmwareNew/VBoxPkg/VBoxAppleSim/DataHub.c
@@ -81,7 +81,7 @@ DataHubLogData (
 
     pEntry->RecHdr.Version    = EFI_DATA_RECORD_HEADER_VERSION;
     pEntry->RecHdr.HeaderSize = sizeof(EFI_DATA_RECORD_HEADER);
-    pEntry->RecHdr.RecordSize = RawDataSize + sizeof(EFI_DATA_RECORD_HEADER); 
+    pEntry->RecHdr.RecordSize = RawDataSize + sizeof(EFI_DATA_RECORD_HEADER);
     CopyMem(&pEntry->RecHdr.DataRecordGuid, DataRecordGuid, sizeof(pEntry->RecHdr.DataRecordGuid));
     CopyMem(&pEntry->RecHdr.ProducerName, ProducerName, sizeof(pEntry->RecHdr.ProducerName));
     pEntry->RecHdr.DataRecordClass = DataRecordClass;
@@ -190,7 +190,7 @@ InitializeDataHub (
 {
     EFI_STATUS              Status;
 
-   
+
     InitializeListHead(&mDataHubInstance.LstEntries);
     EfiInitializeLock (&mDataHubInstance.Lck, TPL_NOTIFY);
 
--- src/VBox/Devices/EFI/FirmwareNew/VBoxPkg/VBoxAppleSim/DataHub.h
+++ src/VBox/Devices/EFI/FirmwareNew/VBoxPkg/VBoxAppleSim/DataHub.h
@@ -68,7 +68,7 @@ EFI_STATUS
 
   Arguments:
     This         - Protocol instance pointer.
-    
+
 
   Returns:
     EFI_SUCCESS     - Data was logged.
@@ -92,7 +92,7 @@ EFI_STATUS
 
   Arguments:
     This         - Protocol instance pointer.
-    
+
 
   Returns:
     EFI_SUCCESS     - Data was logged.
@@ -117,7 +117,7 @@ EFI_STATUS
 
   Arguments:
     This         - Protocol instance pointer.
-    
+
 
   Returns:
     EFI_SUCCESS     - The filter driver event was registered.
@@ -139,7 +139,7 @@ EFI_STATUS
 
   Arguments:
     This         - Protocol instance pointer.
-    
+
 
   Returns:
     EFI_SUCCESS     - The filter driver represented by FilterEvent was shut off.
