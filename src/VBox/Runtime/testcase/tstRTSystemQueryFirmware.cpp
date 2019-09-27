6651f9cadd0aca5d6019a6384c80c509f83ce6df
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2019-09-27 21:57:20 +0000

    tstRTSystemQueryFirmware: build fix,
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@81064 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Runtime/testcase/tstRTSystemQueryFirmware.cpp
+++ src/VBox/Runtime/testcase/tstRTSystemQueryFirmware.cpp
@@ -64,7 +64,7 @@ int main()
                 RTTestPrintf(hTest, RTTESTLVL_INFO, "  Firmware type: Unknown\n");
                 break;
             default:
-                RTTestFailed(hTest, "RTSystemFirmwareQueryType return invalid type: %d (%#x)", enmType);
+                RTTestFailed(hTest, "RTSystemFirmwareQueryType return invalid type: %d (%#x)", enmType, enmType);
                 break;
         }
     }
