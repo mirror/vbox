535490a42d23614428fc7ce72254914c404c0faa
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2019-10-21 20:13:22 +0000

    DevE1000: Converted to new model (wrt critsects, I/O, and pointers at least). bugref:9218
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@81432 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Devices/Network/DevE1000.cpp
+++ src/VBox/Devices/Network/DevE1000.cpp
@@ -8113,12 +8113,18 @@ static DECLCALLBACK(int)  e1kRZConstruct(PPDMDEVINS pDevIns)
     PE1KSTATE   pThis   = PDMINS_2_DATA(pDevIns, PE1KSTATE);
     PE1KSTATECC pThisCC = PDMINS_2_DATA_CC(pDevIns, PE1KSTATECC);
 
+    /* Initialize context specific state data: */
     pThisCC->CTX_SUFF(pDevIns)      = pDevIns;
     /** @todo @bugref{9218} ring-0 driver stuff */
     pThisCC->CTX_SUFF(pDrv)         = NULL;
     pThisCC->CTX_SUFF(pTxSg)        = NULL;
 
-    int rc = PDMDevHlpMmioSetUpContext(pDevIns, pThis->hMmioRegion, e1kMMIOWrite, e1kMMIORead, NULL /*pvUser*/);
+    /* Configure critical sections the same way: */
+    int rc = PDMDevHlpSetDeviceCritSect(pDevIns, PDMDevHlpCritSectGetNop(pDevIns));
+    AssertRCReturn(rc, rc);
+
+    /* Set up MMIO and I/O port callbacks for this context: */
+    rc = PDMDevHlpMmioSetUpContext(pDevIns, pThis->hMmioRegion, e1kMMIOWrite, e1kMMIORead, NULL /*pvUser*/);
     AssertRCReturn(rc, rc);
 
     rc = PDMDevHlpIoPortSetUpContext(pDevIns, pThis->hIoPorts, e1kIOPortOut, e1kIOPortIn, NULL /*pvUser*/);
@@ -8137,7 +8143,7 @@ const PDMDEVREG g_DeviceE1000 =
     /* .u32version = */             PDM_DEVREG_VERSION,
     /* .uReserved0 = */             0,
     /* .szName = */                 "e1000",
-    /* .fFlags = */                 PDM_DEVREG_FLAGS_DEFAULT_BITS | PDM_DEVREG_FLAGS_RC | PDM_DEVREG_FLAGS_R0,
+    /* .fFlags = */                 PDM_DEVREG_FLAGS_DEFAULT_BITS | PDM_DEVREG_FLAGS_RC | PDM_DEVREG_FLAGS_R0 | PDM_DEVREG_FLAGS_NEW_STYLE,
     /* .fClass = */                 PDM_DEVREG_CLASS_NETWORK,
     /* .cMaxInstances = */          ~0U,
     /* .uSharedVersion = */         42,
