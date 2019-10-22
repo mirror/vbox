2aa80a78ce27b0904ce353517bdb6fc646a824de
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2019-10-22 13:03:57 +0000

    EFI: Prepared an assembly file for linking the .fd files into VBoxDD2.dll/dylib/so.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@81450 cfe28804-0f27-0410-a406-dd0f0b0b656f

---
+++ src/VBox/Devices/EFI/DevEFI-binaries.asm
@@ -0,0 +1,40 @@
+; $Id$
+;; @file
+; DevEFI - firmware binaries.
+;
+
+;
+; Copyright (C) 2011-2019 Oracle Corporation
+;
+; This file is part of VirtualBox Open Source Edition (OSE), as
+; available from http://www.virtualbox.org. This file is free software;
+; you can redistribute it and/or modify it under the terms of the GNU
+; General Public License (GPL) as published by the Free Software
+; Foundation, in version 2 as it comes in the "COPYING" file of the
+; VirtualBox OSE distribution. VirtualBox OSE is distributed in the
+; hope that it will be useful, but WITHOUT ANY WARRANTY of any kind.
+;
+
+
+;*********************************************************************************************************************************
+;*  Header Files                                                                                                                 *
+;*********************************************************************************************************************************
+%include "VBox/asmdefs.mac"
+%include "VBox/err.mac"
+%include "iprt/x86.mac"
+
+
+BEGINCONST
+EXPORTEDNAME g_abEfiFirmware32
+        incbin "VBoxEFI32.fd"
+end_32_firmware:
+EXPORTEDNAME g_cbEfiFirmware32
+        dd  end_32_firmware - NAME(g_abEfiFirmware32)
+
+ALIGNDATA(64)
+EXPORTEDNAME g_abEfiFirmware64
+        incbin "VBoxEFI64.fd"
+end_64_firmware:
+EXPORTEDNAME g_cbEfiFirmware64
+        dd  end_64_firmware - NAME(g_abEfiFirmware64)
+
