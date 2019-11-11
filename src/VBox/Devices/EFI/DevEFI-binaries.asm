49ad133c4575b5e41186cc0f56c5a595ba7d3ad9
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2019-11-11 20:44:57 +0000

    Devices/EFI: Fix linker warnings for ELF binaries
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@81779 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Devices/EFI/DevEFI-binaries.asm
+++ src/VBox/Devices/EFI/DevEFI-binaries.asm
@@ -38,3 +38,14 @@ end_64_firmware:
 EXPORTEDNAME g_cbEfiFirmware64
         dd  end_64_firmware - NAME(g_abEfiFirmware64)
 
+%ifdef ASM_FORMAT_ELF
+size g_abEfiFirmware32 end_32_firmware - NAME(g_abEfiFirmware32)
+type g_abEfiFirmware32 object
+size g_cbEfiFirmware32 4
+type g_cbEfiFirmware32 object
+
+size g_abEfiFirmware64 end_32_firmware - NAME(g_abEfiFirmware64)
+type g_abEfiFirmware64 object
+size g_cbEfiFirmware64 4
+type g_cbEfiFirmware64 object
+%endif
