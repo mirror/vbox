de5f5460d22c563e687b4e5a3469458406142b22
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2019-11-20 10:30:36 +0000

    VMM/HMVMXR0: Nested VMX: bugref:9180 Unused variable build fix.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@82018 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/VMM/VMMR0/HMVMXR0.cpp
+++ src/VBox/VMM/VMMR0/HMVMXR0.cpp
@@ -4917,11 +4917,10 @@ static void hmR0VmxExportGuestApicTpr(PVMCPUCC pVCpu, PCVMXTRANSIENT pVmxTransie
  *
  * @returns Guest's interruptibility-state.
  * @param   pVCpu           The cross context virtual CPU structure.
- * @param   pVmxTransient   The VMX-transient structure.
  *
  * @remarks No-long-jump zone!!!
  */
-static uint32_t hmR0VmxGetGuestIntrState(PVMCPUCC pVCpu, PCVMXTRANSIENT pVmxTransient)
+static uint32_t hmR0VmxGetGuestIntrState(PVMCPUCC pVCpu)
 {
     /*
      * Check if we should inhibit interrupt delivery due to instructions like STI and MOV SS.
@@ -8578,7 +8577,7 @@ static VBOXSTRICTRC hmR0VmxEvaluatePendingEvent(PVMCPUCC pVCpu, PCVMXTRANSIENT p
     /** @todo r=ramshankar: Move this outside this function to the caller. */
     {
         /* Get the current interruptibility-state of the guest or nested-guest (this updates FFs). */
-        uint32_t const fIntrState = hmR0VmxGetGuestIntrState(pVCpu, pVmxTransient);
+        uint32_t const fIntrState = hmR0VmxGetGuestIntrState(pVCpu);
 
 #ifdef VBOX_STRICT
         /* Validate. */
