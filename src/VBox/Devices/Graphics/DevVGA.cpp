b707fb06ed3c91d0b32d4e9ef127983a79f1f861
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2019-11-21 09:01:45 +0000

    DevVGA: Doxygen fix. bugref:9218
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@82066 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Devices/Graphics/DevVGA.cpp
+++ src/VBox/Devices/Graphics/DevVGA.cpp
@@ -5613,7 +5613,7 @@ static DECLCALLBACK(void) vgaPortReportHostCursorCapabilities(PPDMIDISPLAYPORT p
 }
 
 /**
- * @callback_method_impl{FNTMTIMERFNTMTIMERDEV, VGA Refresh Timer}
+ * @callback_method_impl{FNTMTIMERDEV, VGA Refresh Timer}
  */
 static DECLCALLBACK(void) vgaTimerRefresh(PPDMDEVINS pDevIns, PTMTIMER pTimer, void *pvUser)
 {
