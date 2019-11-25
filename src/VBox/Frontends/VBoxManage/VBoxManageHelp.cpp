1d02062a7e6064e16e41325b5a8d43befb04d894
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2019-11-25 20:42:38 +0000

    Frontends/VBoxManage: add forgotten --system-uuid-le option to help output
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@82198 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Frontends/VBoxManage/VBoxManageHelp.cpp
+++ src/VBox/Frontends/VBoxManage/VBoxManageHelp.cpp
@@ -582,6 +582,7 @@ void printUsage(USAGECATEGORY enmCommand, uint64_t fSubcommandScope, PRTSTREAM p
                      "                            [--biosapic disabled|apic|x2apic]\n"
                      "                            [--biossystemtimeoffset <msec>]\n"
                      "                            [--biospxedebug on|off]\n"
+                     "                            [--system-uuid-le on|off]\n"
                      "                            [--boot<1-4> none|floppy|dvd|disk|net>]\n"
                      "                            [--nic<1-N> none|null|nat|bridged|intnet"
 #if defined(VBOX_WITH_NETFLT)
