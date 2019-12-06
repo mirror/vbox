0925cce71ccfa50bc56a91dd2972366d31dfdef0
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2019-12-06 16:32:09 +0000

    RepackGA scripts: updating repacking scripts. bugref:9587
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@82468 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Installer/win/Scripts/Combined-3-RepackAdditions.cmd
+++ src/VBox/Installer/win/Scripts/Combined-3-RepackAdditions.cmd
@@ -36,7 +36,7 @@ set _MY_OPT_UNTAR_DIR=%_MY_SCRIPT_DIR%\..\..\..\
 for %%i in (%_MY_OPT_UNTAR_DIR%) do set _MY_OPT_UNTAR_DIR=%%~fi
 set _MY_OPT_BUILD_TYPE=@KBUILD_TYPE@
 set _MY_OPT_OUTDIR=%_MY_OPT_UNTAR_DIR%\output
-set _MY_OPT_SRC_DIR=%_MY_OPT_UNTAR_DIR%\..\src
+set _MY_OPT_SRC_DIR=%_MY_SCRIPT_DIR%\resources\
 
 :argument_loop
 if ".%1" == "."             goto no_more_arguments
@@ -190,14 +190,14 @@ echo **************************************************************************
 echo * AMD64: Unpacking signed drivers...
 echo **************************************************************************
 cd /d "%_MY_REPACK_DIR_AMD64%" || goto end_failed
-call "%_MY_REPACK_DIR_AMD64%\UnpackBlessedDrivers.cmd" -b "%_MY_BINDIR_AMD64%" -i "%_MY_OPT_SIGNED_AMD64%" -n || goto end_failed
+call "%_MY_REPACK_DIR_AMD64%\UnpackBlessedDrivers.cmd" -b "%_MY_BINDIR_AMD64%" -i "%_MY_OPT_SIGNED_AMD64%" -n -v || goto end_failed
 echo .
 
 echo **************************************************************************
 echo * X86: Unpacking signed drivers...
 echo **************************************************************************
 cd /d "%_MY_REPACK_DIR_X86%" || goto end_failed
-call "%_MY_REPACK_DIR_X86%\UnpackBlessedDrivers.cmd" -b "%_MY_BINDIR_X86%" -i "%_MY_OPT_SIGNED_X86%" -n || goto end_failed
+call "%_MY_REPACK_DIR_X86%\UnpackBlessedDrivers.cmd" -b "%_MY_BINDIR_X86%" -i "%_MY_OPT_SIGNED_X86%" -n -v || goto end_failed
 echo .
 
 
@@ -208,26 +208,29 @@ echo **************************************************************************
 echo * Building amd64 installer
 echo **************************************************************************
 
+del %_MY_OPT_UNTAR_DIR%\win.amd64\release\bin\additions\VBoxWindowsAdditions-amd64.exe
+cp %_MY_REPACK_DIR_AMD64%\..\obj\uninst.exe %_MY_REPACK_DIR_AMD64%
+
 rem TBD: that has to be converted to invoke auto-generated .cmd
 
 %KBUILD_BIN_PATH%\kmk_redirect.exe -C %_MY_OPT_SRC_DIR%/VBox/Additions/WINNT/Installer ^
         -E "PATH_OUT=%_MY_REPACK_DIR_AMD64%\.." ^
-        -E "PATH_TARGET=%_MY_REPACK_DIR_AMD64%\..\obj" ^
-        -E "PATH_TARGET_X86=%_MY_REPACK_DIR_AMD64%\..\obj" ^
+        -E "PATH_TARGET=%_MY_REPACK_DIR_AMD64%" ^
+        -E "PATH_TARGET_X86=%_MY_REPACK_DIR_X86%\resources" ^
         -E "VBOX_PATH_ADDITIONS_WIN_X86=%_MY_REPACK_DIR_AMD64%\..\bin\additions" ^
         -E "VBOX_PATH_DIFX=%KBUILD_DEVTOOLS%\win.x86\DIFx\v2.1-r3" ^
         -E "VBOX_VENDOR=Oracle Corporation" -E "VBOX_VENDOR_SHORT=Oracle" -E "VBOX_PRODUCT=Oracle VM VirtualBox" ^
         -E "VBOX_C_YEAR=@VBOX_C_YEAR@" -E "VBOX_VERSION_STRING=@VBOX_VERSION_STRING@" -E "VBOX_VERSION_STRING_RAW=@VBOX_VERSION_STRING_RAW@" ^
         -E "VBOX_VERSION_MAJOR=@VBOX_VERSION_MAJOR@" -E "VBOX_VERSION_MINOR=@VBOX_VERSION_MINOR@" -E "VBOX_VERSION_BUILD=0" -E "VBOX_SVN_REV=@VBOX_SVN_REV@" ^
-        -E "VBOX_WINDOWS_ADDITIONS_ICON_FILE=%_MY_OPT_SRC_DIR%\VBox\Artwork\win\NonOSE\VirtualBoxGA-vista.ico" ^
-        -E "VBOX_NSIS_ICON_FILE=%_MY_OPT_SRC_DIR%\VBox\Artwork\win\NonOSE\VirtualBoxGA-nsis.ico" ^
+        -E "VBOX_WINDOWS_ADDITIONS_ICON_FILE=%_MY_OPT_SRC_DIR%\VirtualBoxGA-vista.ico" ^
+        -E "VBOX_NSIS_ICON_FILE=%_MY_OPT_SRC_DIR%\VirtualBoxGA-nsis.ico" ^
         -E "VBOX_WITH_GUEST_INSTALL_HELPER=1" -E "VBOX_WITH_GUEST_INSTALLER_UNICODE=1" -E "VBOX_WITH_LICENSE_INSTALL_RTF=1" ^
-        -E "VBOX_WITH_WDDM=1" -E "VBOX_WITH_MESA3D=1" -E "VBOX_BRAND_WIN_ADD_INST_DLGBMP=%_MY_OPT_SRC_DIR%\VBox\Additions\WINNT\Installer\welcome.bmp" ^
-        -E "VBOX_BRAND_LICENSE_RTF=%_MY_OPT_SRC_DIR%\..\doc\License-gpl-2.0.rtf"  -E "BUILD_TYPE=release" -E "BUILD_TARGET_ARCH=amd64" ^
+        -E "VBOX_WITH_WDDM=1" -E "VBOX_WITH_MESA3D=1" -E "VBOX_BRAND_WIN_ADD_INST_DLGBMP=%_MY_OPT_SRC_DIR%\welcome.bmp" ^
+        -E "VBOX_BRAND_LICENSE_RTF=%_MY_OPT_SRC_DIR%\License-gpl-2.0.rtf"  -E "BUILD_TYPE=release" -E "BUILD_TARGET_ARCH=amd64" ^
         --  %KBUILD_DEVTOOLS%/win.x86/nsis/v3.04-log/makensis.exe /NOCD /V2 ^
                 "/DVBOX_SIGN_ADDITIONS=1" ^
                 "/DEXTERNAL_UNINSTALLER=1" ^
-                "%_MY_OPT_SRC_DIR%\VBox\Additions\WINNT\Installer\VBoxGuestAdditions.nsi"
+                "%_MY_OPT_SRC_DIR%\VBoxGuestAdditions.nsi"
 
 rem
 rem Building amd64 installer
@@ -236,26 +239,29 @@ echo **************************************************************************
 echo * Building x86 installer
 echo **************************************************************************
 
+del %_MY_OPT_UNTAR_DIR%\win.x86\release\bin\additions\VBoxWindowsAdditions-x86.exe
+cp %_MY_REPACK_DIR_X86%\..\obj\uninst.exe %_MY_REPACK_DIR_X86%\
+
 rem TBD: that has to be converted to invoke auto-generated .cmd
 
 %KBUILD_BIN_PATH%\kmk_redirect.exe -C %_MY_OPT_SRC_DIR%/VBox/Additions/WINNT/Installer ^
         -E "PATH_OUT=%_MY_REPACK_DIR_X86%\.." ^
-        -E "PATH_TARGET=%_MY_REPACK_DIR_X86%\..\obj" ^
-        -E "PATH_TARGET_X86=%_MY_REPACK_DIR_X86%\..\obj" ^
+        -E "PATH_TARGET=%_MY_REPACK_DIR_X86%" ^
+        -E "PATH_TARGET_X86=%_MY_REPACK_DIR_X86%\resources" ^
         -E "VBOX_PATH_ADDITIONS_WIN_X86=%_MY_REPACK_DIR_X86%\..\bin\additions" ^
         -E "VBOX_PATH_DIFX=%KBUILD_DEVTOOLS%\win.x86\DIFx\v2.1-r3" ^
         -E "VBOX_VENDOR=Oracle Corporation" -E "VBOX_VENDOR_SHORT=Oracle" -E "VBOX_PRODUCT=Oracle VM VirtualBox" ^
         -E "VBOX_C_YEAR=@VBOX_C_YEAR@" -E "VBOX_VERSION_STRING=@VBOX_VERSION_STRING@" -E "VBOX_VERSION_STRING_RAW=@VBOX_VERSION_STRING_RAW@" ^
         -E "VBOX_VERSION_MAJOR=@VBOX_VERSION_MAJOR@" -E "VBOX_VERSION_MINOR=@VBOX_VERSION_MINOR@" -E "VBOX_VERSION_BUILD=0" -E "VBOX_SVN_REV=@VBOX_SVN_REV@" ^
-        -E "VBOX_WINDOWS_ADDITIONS_ICON_FILE=%_MY_OPT_SRC_DIR%\VBox\Artwork\win\NonOSE\VirtualBoxGA-vista.ico" ^
-        -E "VBOX_NSIS_ICON_FILE=%_MY_OPT_SRC_DIR%\VBox\Artwork\win\NonOSE\VirtualBoxGA-nsis.ico" ^
+        -E "VBOX_WINDOWS_ADDITIONS_ICON_FILE=%_MY_OPT_SRC_DIR%\VirtualBoxGA-vista.ico" ^
+        -E "VBOX_NSIS_ICON_FILE=%_MY_OPT_SRC_DIR%\VirtualBoxGA-nsis.ico" ^
         -E "VBOX_WITH_GUEST_INSTALL_HELPER=1" -E "VBOX_WITH_GUEST_INSTALLER_UNICODE=1" -E "VBOX_WITH_LICENSE_INSTALL_RTF=1" ^
-        -E "VBOX_WITH_WDDM=1" -E "VBOX_WITH_MESA3D=1" -E "VBOX_BRAND_WIN_ADD_INST_DLGBMP=%_MY_OPT_SRC_DIR%\VBox\Additions\WINNT\Installer\welcome.bmp" ^
-        -E "VBOX_BRAND_LICENSE_RTF=%_MY_OPT_SRC_DIR%\..\doc\License-gpl-2.0.rtf"  -E "BUILD_TYPE=release" -E "BUILD_TARGET_ARCH=x86" ^
+        -E "VBOX_WITH_WDDM=1" -E "VBOX_WITH_MESA3D=1" -E "VBOX_BRAND_WIN_ADD_INST_DLGBMP=%_MY_OPT_SRC_DIR%\welcome.bmp" ^
+        -E "VBOX_BRAND_LICENSE_RTF=%_MY_OPT_SRC_DIR%\License-gpl-2.0.rtf"  -E "BUILD_TYPE=release" -E "BUILD_TARGET_ARCH=x86" ^
         --  %KBUILD_DEVTOOLS%/win.x86/nsis/v3.04-log/makensis.exe /NOCD /V2 ^
                 "/DVBOX_SIGN_ADDITIONS=1" ^
                 "/DEXTERNAL_UNINSTALLER=1" ^
-                "%_MY_OPT_SRC_DIR%\VBox\Additions\WINNT\Installer\VBoxGuestAdditions.nsi"
+                "%_MY_OPT_SRC_DIR%\VBoxGuestAdditions.nsi"
 
 rem
 rem Making .iso
@@ -279,9 +285,9 @@ rem TBD: that has to be converted to invoke auto-generated .cmd
         /cert/vbox-sha256.cer=%_MY_SCRIPT_DIR%../bin/additions/vbox-sha256.cer ^
         /VBoxWindowsAdditions-x86.exe=%_MY_OPT_UNTAR_DIR%/win.x86/release/bin/additions/VBoxWindowsAdditions-x86.exe ^
         /VBoxWindowsAdditions.exe=%_MY_OPT_UNTAR_DIR%/win.x86/release/bin/additions/VBoxWindowsAdditions.exe ^
-        /AUTORUN.INF=%_MY_OPT_SRC_DIR%/VBox/Additions/WINNT/Installer/ISO/AUTORUN.INF ^
+        /AUTORUN.INF=%_MY_OPT_SRC_DIR%/AUTORUN.INF ^
         /cert/VBoxCertUtil.exe=%_MY_SCRIPT_DIR%../bin/additions/VBoxCertUtil.exe ^
-        /NT3x/Readme.txt=%_MY_OPT_SRC_DIR%/VBox/Additions/WINNT/Installer/ISO/NT3xReadme.txt ^
+        /NT3x/Readme.txt=%_MY_OPT_SRC_DIR%/NT3xReadme.txt ^
         /NT3x/VBoxGuest.sys=%_MY_OPT_UNTAR_DIR%/win.x86/release/bin/additions/VBoxGuest.sys ^
         /NT3x/VBoxGuest.cat=%_MY_OPT_UNTAR_DIR%/win.x86/release/bin/additions/VBoxGuest.cat ^
         /NT3x/VBoxGuest.inf=%_MY_OPT_UNTAR_DIR%/win.x86/release/bin/additions/VBoxGuest.inf ^
