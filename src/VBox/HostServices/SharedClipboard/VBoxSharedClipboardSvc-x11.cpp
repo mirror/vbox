0e4e3051e2772aed29bafead886faf22a575f77a
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2019-12-10 12:25:32 +0000

    VBoxSharedClipboardSvc-x11.cpp: Forgot to set pcbActual in ShClSvcImplReadData with the result that the guest didn't really see any data. bugref:9437
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@82535 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/HostServices/SharedClipboard/VBoxSharedClipboardSvc-x11.cpp
+++ src/VBox/HostServices/SharedClipboard/VBoxSharedClipboardSvc-x11.cpp
@@ -202,10 +202,9 @@ int ShClSvcImplReadData(PSHCLCLIENT pClient,
                 rc = ShClEventWait(&pClient->EventSrc, idEvent, 30 * 1000, &pPayload);
                 if (RT_SUCCESS(rc))
                 {
-                    memcpy(pData->pvData,  pPayload->pvData, RT_MIN(pData->cbData, pPayload->cbData));
-                    pData->cbData = pPayload->cbData;
-
-                    Assert(pData->cbData == pPayload->cbData); /* Sanity. */
+                    memcpy(pData->pvData, pPayload->pvData, RT_MIN(pData->cbData, pPayload->cbData));
+                    pData->cbData = (uint32_t)pPayload->cbData; /** @todo r=bird: Just ditch this data block wrapper, it made you forget to set pcbActual! */
+                    *pcbActual = (uint32_t)pPayload->cbData;
                 }
             }
 
