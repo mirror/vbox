3821ffc84f5797cfc38b547dcdf0a298dc3f2158
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2019-12-13 05:48:21 +0000

    VMM/IEM: Nested VMX: bugref:9180 VMX_EXIT_APIC_ACCESS emulation is done, remove from todo list.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@82575 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/VMM/VMMAll/IEMAllCImplVmxInstr.cpp.h
+++ src/VBox/VMM/VMMAll/IEMAllCImplVmxInstr.cpp.h
@@ -173,7 +173,6 @@
  *  VMX_EXIT_RSM
  *  VMX_EXIT_MONITOR (APIC access VM-exit caused by MONITOR pending)
  *  VMX_EXIT_ERR_MACHINE_CHECK (we never need to raise this?)
- *  VMX_EXIT_APIC_ACCESS
  *  VMX_EXIT_EPT_VIOLATION
  *  VMX_EXIT_EPT_MISCONFIG
  *  VMX_EXIT_INVEPT
