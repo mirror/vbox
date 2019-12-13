2ac4e60d69b0c37e786cdc3484a9be7a7d395a58
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2019-12-13 04:29:46 +0000

    VMM/HMVMXR0: Space and obsolete todo.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@82572 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/VMM/VMMR0/HMVMXR0.cpp
+++ src/VBox/VMM/VMMR0/HMVMXR0.cpp
@@ -8965,8 +8965,8 @@ VMMR0DECL(void) VMXR0ThreadCtxCallback(RTTHREADCTXEVENT enmEvent, PVMCPUCC pVCpu
                initializing VT-x if necessary (onlined CPUs, local init etc.) */
             int rc = hmR0EnterCpu(pVCpu);
             AssertRC(rc);
-            Assert(   (pVCpu->hm.s.fCtxChanged & (HM_CHANGED_HOST_CONTEXT | HM_CHANGED_VMX_HOST_GUEST_SHARED_STATE))
-                   ==                            (HM_CHANGED_HOST_CONTEXT | HM_CHANGED_VMX_HOST_GUEST_SHARED_STATE));
+            Assert((pVCpu->hm.s.fCtxChanged &  (HM_CHANGED_HOST_CONTEXT | HM_CHANGED_VMX_HOST_GUEST_SHARED_STATE))
+                                            == (HM_CHANGED_HOST_CONTEXT | HM_CHANGED_VMX_HOST_GUEST_SHARED_STATE));
 
             /* Load the active VMCS as the current one. */
             PVMXVMCSINFO pVmcsInfo = hmGetVmxActiveVmcsInfo(pVCpu);
@@ -10340,9 +10340,6 @@ static VBOXSTRICTRC hmR0VmxPreRunGuest(PVMCPUCC pVCpu, PVMXTRANSIENT pVmxTransie
     /*
      * Virtualize memory-mapped accesses to the physical APIC (may take locks).
      */
-    /** @todo Doing this from ring-3 after VM setup phase causes a
-     *        VERR_IOM_MMIO_RANGE_NOT_FOUND guru while booting Visa 64 SMP VM. No
-     *        idea why atm. */
     PVMCC pVM = pVCpu->CTX_SUFF(pVM);
     if (   !pVCpu->hm.s.vmx.u64GstMsrApicBase
         && (pVM->hm.s.vmx.Msrs.ProcCtls2.n.allowed1 & VMX_PROC_CTLS2_VIRT_APIC_ACCESS)
