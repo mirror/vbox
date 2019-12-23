f0a646fa96727b384afd8d5c58a88e65a2bd9434
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2019-12-23 20:50:12 +0000

    TestManager/report.py: Corrected the wording in getPeriodDesc for when starting in the past.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@82647 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/ValidationKit/testmanager/core/report.py
+++ src/VBox/ValidationKit/testmanager/core/report.py
@@ -206,11 +206,12 @@ class ReportModelBase(ModelLogicBase): # pylint: disable=too-few-public-methods
         """
         if iPeriod == 0:
             return 'now' if self.tsNow is None else 'then';
+        sTerm = 'ago' if self.tsNow is None else 'earlier';
         if self.cHoursPerPeriod == 24:
-            return '%dd ago' % (iPeriod, );
+            return '%dd %s' % (iPeriod, sTerm, );
         if (iPeriod * self.cHoursPerPeriod) % 24 == 0:
-            return '%dd ago' % (iPeriod * self.cHoursPerPeriod / 24, );
-        return '%dh ago' % (iPeriod * self.cHoursPerPeriod, );
+            return '%dd %s' % (iPeriod * self.cHoursPerPeriod / 24, sTerm, );
+        return '%dh %s' % (iPeriod * self.cHoursPerPeriod, sTerm);
 
     def getStraightPeriodDesc(self, iPeriod):
         """
