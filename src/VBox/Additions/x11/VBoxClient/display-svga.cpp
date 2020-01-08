4343f79315825bbec83978894c1cc0e79534997a
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2020-01-08 10:52:32 +0000

    VMSVGA/X11: bugref:9637. Caching monitor (display) information so that we can send the whole data (not just delta) to drm.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@82668 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Additions/x11/VBoxClient/display-svga.cpp
+++ src/VBox/Additions/x11/VBoxClient/display-svga.cpp
@@ -95,6 +95,8 @@ struct DRMCONTEXT
     RTFILE hDevice;
 };
 
+struct DRMVMWRECT aRects[VMW_MAX_HEADS];
+
 static void drmConnect(struct DRMCONTEXT *pContext)
 {
     unsigned i;
@@ -183,8 +185,6 @@ static int run(struct VBCLSERVICE **ppInterface, bool fDaemonised)
     struct DRMCONTEXT drmContext = { NIL_RTFILE };
     unsigned i;
     int rc;
-    struct DRMVMWRECT aRects[VMW_MAX_HEADS];
-    unsigned cHeads;
     /* Do not acknowledge the first event we query for to pick up old events,
      * e.g. from before a guest reboot. */
     bool fAck = false;
@@ -216,28 +216,30 @@ static int run(struct VBCLSERVICE **ppInterface, bool fDaemonised)
             VBClLogFatalError("Display change request contained, rc=%Rrc\n", rc);
         if (cDisplaysOut > 0)
         {
-            cHeads = 0;
             for (i = 0; i < cDisplaysOut && i < VMW_MAX_HEADS; ++i)
             {
                 if (!(aDisplays[i].fDisplayFlags & VMMDEV_DISPLAY_DISABLED))
                 {
-                    if ((i == 0) || (aDisplays[i].fDisplayFlags & VMMDEV_DISPLAY_ORIGIN))
+                    uint32_t idDisplay = aDisplays[i].idDisplay;
+                    if (idDisplay < VMW_MAX_HEADS)
                     {
-                        aRects[cHeads].x = aDisplays[i].xOrigin;
-                        aRects[cHeads].y = aDisplays[i].yOrigin;
-                    } else {
-                        aRects[cHeads].x = aRects[cHeads - 1].x + aRects[cHeads - 1].w;
-                        aRects[cHeads].y = aRects[cHeads - 1].y;
+                        if ((idDisplay == 0) || (aDisplays[i].fDisplayFlags & VMMDEV_DISPLAY_ORIGIN))
+                        {
+                            aRects[idDisplay].x = aDisplays[i].xOrigin;
+                            aRects[idDisplay].y = aDisplays[i].yOrigin;
+                        } else {
+                            aRects[idDisplay].x = aRects[idDisplay - 1].x + aRects[idDisplay - 1].w;
+                            aRects[idDisplay].y = aRects[idDisplay - 1].y;
+                        }
+                        aRects[idDisplay].w = aDisplays[i].cx;
+                        aRects[idDisplay].h = aDisplays[i].cy;
                     }
-                    aRects[cHeads].w = aDisplays[i].cx;
-                    aRects[cHeads].h = aDisplays[i].cy;
-                    ++cHeads;
                 }
             }
-            for (i = 0; i < cHeads; ++i)
+            for (i = 0; i < cDisplaysOut; ++i)
                 printf("Head %u: %dx%d, (%d, %d)\n", i, (int)aRects[i].w, (int)aRects[i].h,
                        (int)aRects[i].x, (int)aRects[i].y);
-            drmSendHints(&drmContext, aRects, cHeads);
+            drmSendHints(&drmContext, aRects, VMW_MAX_HEADS);
         }
         do
         {
