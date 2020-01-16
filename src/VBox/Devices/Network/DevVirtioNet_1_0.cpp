030e42492091fa2aa5463218b78a60b6c227923d
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2020-01-16 12:37:12 +0000

    Fix burn
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@82780 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Devices/Network/DevVirtioNet_1_0.cpp
+++ src/VBox/Devices/Network/DevVirtioNet_1_0.cpp
@@ -61,13 +61,8 @@
 
 /*
  * GSO = Generic Segmentation Offload
- * TSO = TCP Segmentation Offload
- * UDP = UDP Fragmentation Offset
+ * TSO = TCP Segmentation Offload */
 
-/*********************************************************************************************************************************
-*   Defined Constants And Macros                                                                                                 *
-*********************************************************************************************************************************/
-/** The current saved state version. */
 #define VIRTIONET_SAVED_STATE_VERSION          UINT32_C(1)
 #define VIRTIONET_MAX_QPAIRS                   512
 #define VIRTIONET_MAX_QUEUES                   (VIRTIONET_MAX_QPAIRS * 2 + 1)
@@ -1109,6 +1104,8 @@ static int virtioNetR3HandleRxPacket(PPDMDEVINS pDevIns, PVIRTIONET pThis, PVIRT
         int rc = virtioCoreR3QueueGet(pDevIns, &pThis->Virtio, RXQIDX(0), &pDescChain, true);
 
         AssertRC(rc == VINF_SUCCESS || rc == VERR_NOT_AVAILABLE, rc);
+
+        /** @todo  Find a better way to deal with this */
 
         AssertMsgReturn(rc == VINF_SUCCESS && pDescChain->cbPhysSend,
                         ("Not enough Rx buffers in queue to accomodate ethernet packet\n"),
