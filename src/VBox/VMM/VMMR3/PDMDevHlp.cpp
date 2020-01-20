3bf66032fd226eb40798960d859b66d52f05a09b
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2020-01-20 17:02:25 +0000

    VMM/PDMDevHlp: Remove space in timer description to fix debug assertion in STAM later on due to ordering constraints
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@82801 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/VMM/VMMR3/PDMDevHlp.cpp
+++ src/VBox/VMM/VMMR3/PDMDevHlp.cpp
@@ -414,7 +414,7 @@ static DECLCALLBACK(int) pdmR3DevHlp_TMTimerCreate(PPDMDEVINS pDevIns, TMCLOCK e
 
     if (pDevIns->iInstance > 0) /** @todo use a string cache here later. */
     {
-         char *pszDesc2 = MMR3HeapAPrintf(pVM, MM_TAG_PDM_DEVICE_DESC, "%s [%u]", pszDesc, pDevIns->iInstance);
+         char *pszDesc2 = MMR3HeapAPrintf(pVM, MM_TAG_PDM_DEVICE_DESC, "%s[%u]", pszDesc, pDevIns->iInstance);
          if (pszDesc2)
              pszDesc = pszDesc2;
     }
@@ -439,7 +439,7 @@ static DECLCALLBACK(int) pdmR3DevHlp_TimerCreate(PPDMDEVINS pDevIns, TMCLOCK enm
 
     if (pDevIns->iInstance > 0) /** @todo use a string cache here later. */
     {
-         char *pszDesc2 = MMR3HeapAPrintf(pVM, MM_TAG_PDM_DEVICE_DESC, "%s [%u]", pszDesc, pDevIns->iInstance);
+         char *pszDesc2 = MMR3HeapAPrintf(pVM, MM_TAG_PDM_DEVICE_DESC, "%s[%u]", pszDesc, pDevIns->iInstance);
          if (pszDesc2)
              pszDesc = pszDesc2;
     }
