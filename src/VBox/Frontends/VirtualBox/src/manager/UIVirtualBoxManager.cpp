d74bd13a3aa389009c7e08009a263594c0ee2ff9
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2020-01-31 15:02:06 +0000

    FE/Qt: bugref:9653: Build fix for r135945.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@82945 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Frontends/VirtualBox/src/manager/UIVirtualBoxManager.cpp
+++ src/VBox/Frontends/VirtualBox/src/manager/UIVirtualBoxManager.cpp
@@ -2044,7 +2044,7 @@ bool UIVirtualBoxManager::isAtLeastOneItemSupportsShortcuts(const QList<UIVirtua
         if (   pItem->accessible()
 #ifdef VBOX_WS_MAC
             /* On Mac OS X this are real alias files, which don't work with the old legacy xml files. */
-            && pItem->settingsFile().endsWith(".vbox", Qt::CaseInsensitive)
+            && pItem->toLocal()->settingsFile().endsWith(".vbox", Qt::CaseInsensitive)
 #endif
             )
             return true;
