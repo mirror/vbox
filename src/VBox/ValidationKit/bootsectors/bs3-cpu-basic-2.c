c24f6259f56a1add1cae9e5534ab583c42b1e52e
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2020-02-07 13:35:21 +0000

    bs3-cpu-basic-2.c: Re-enable all tests.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@83021 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/ValidationKit/bootsectors/bs3-cpu-basic-2.c
+++ src/VBox/ValidationKit/bootsectors/bs3-cpu-basic-2.c
@@ -76,7 +76,7 @@ BS3_DECL(void) Main_rm()
      * Do tests driven from 16-bit code.
      */
     NOREF(g_aModeTest); NOREF(g_aModeByOneTests); /* for when commenting out bits */
-#if 0
+#if 1
     Bs3TestDoModes_rm(g_aModeTest, RT_ELEMENTS(g_aModeTest));
     Bs3TestDoModesByOne_rm(g_aModeByOneTests, RT_ELEMENTS(g_aModeByOneTests), 0);
 #endif
@@ -87,6 +87,6 @@ BS3_DECL(void) Main_rm()
     Bs3SwitchTo32BitAndCallC_rm(bs3CpuBasic2_Do32BitTests_pe32, 0);
 
     Bs3TestTerm();
-//for (;;) { ASMHalt(); }
+for (;;) { ASMHalt(); }
 }
 
