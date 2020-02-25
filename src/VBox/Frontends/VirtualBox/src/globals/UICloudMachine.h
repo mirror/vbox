a54e0a223e9116a46ad1c022dd02127761a6e11d
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2020-02-25 11:09:17 +0000

    FE/Qt: bugref:9653: Burn fix for r136262.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@83145 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Frontends/VirtualBox/src/globals/UICloudMachine.h
+++ src/VBox/Frontends/VirtualBox/src/globals/UICloudMachine.h
@@ -15,8 +15,8 @@
  * hope that it will be useful, but WITHOUT ANY WARRANTY of any kind.
  */
 
-#ifndef FEQT_INCLUDED_SRC_manager_UICloudMachine_h
-#define FEQT_INCLUDED_SRC_manager_UICloudMachine_h
+#ifndef FEQT_INCLUDED_SRC_globals_UICloudMachine_h
+#define FEQT_INCLUDED_SRC_globals_UICloudMachine_h
 #ifndef RT_WITHOUT_PRAGMA_ONCE
 # pragma once
 #endif
