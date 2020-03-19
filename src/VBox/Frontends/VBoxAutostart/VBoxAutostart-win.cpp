f0c395212d0c5cec06d53ec6fa4cf0e6b360b609
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2020-03-19 12:54:50 +0000

    Main: bugref:9341: Fixed: the VM always started for specified user during autostart regardless the config file.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@83327 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Frontends/VBoxAutostart/VBoxAutostart-win.cpp
+++ src/VBox/Frontends/VBoxAutostart/VBoxAutostart-win.cpp
@@ -97,6 +97,7 @@ static int autostartGetProcessDomainUser(com::Utf8Str &aUser)
         return RTErrConvertFromWin32(GetLastError());
     rc = RTUtf16ToUtf8(wszUsername, &pszUser);
     aUser = pszUser;
+    aUser.toLower();
     RTStrFree(pszUser);
     return rc;
 }
@@ -110,6 +111,7 @@ static int autostartGetLocalDomain(com::Utf8Str &aDomain)
     char *pszDomain = NULL;
     int rc = RTUtf16ToUtf8(pwszDomain, &pszDomain);
     aDomain = pszDomain;
+    aDomain.toLower();
     RTStrFree(pszDomain);
     return rc;
 }
@@ -131,6 +133,8 @@ static int autostartGetDomainAndUser(const com::Utf8Str &aDomainAndUser, com::Ut
         }
         aDomain = aDomainAndUser.substr(0, offDelim);
         aUser   = aDomainAndUser.substr(offDelim + 1);
+        aDomain.toLower();
+        aUser.toLower();
         return VINF_SUCCESS;
     }
 
@@ -150,12 +154,16 @@ static int autostartGetDomainAndUser(const com::Utf8Str &aDomainAndUser, com::Ut
         }
         aDomain = aDomainAndUser.substr(offDelim + 1);
         aUser   = aDomainAndUser.substr(0, offDelim);
+        aDomain.toLower();
+        aUser.toLower();
         return VINF_SUCCESS;
     }
 
     // only user is specified
     int rc = autostartGetLocalDomain(aDomain);
     aUser = aDomainAndUser;
+    aDomain.toLower();
+    aUser.toLower();
     return rc;
 }
 
