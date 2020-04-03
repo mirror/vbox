569fe79f6bbf829c06e212da4a9161b46cda7d7d
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2020-04-03 15:41:58 +0000

    Guest Control/VBoxService: Strip truncation bit in vgsvcGstCtrlSessionHandleFileOpen() when opening in read-only mode. bugref:9320
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@83534 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Additions/common/VBoxService/VBoxServiceControlSession.cpp
+++ src/VBox/Additions/common/VBoxService/VBoxServiceControlSession.cpp
@@ -321,6 +321,10 @@ static int vgsvcGstCtrlSessionHandleFileOpen(PVBOXSERVICECTRLSESSION pSession, P
                 if (RT_SUCCESS(rc))
                 {
                     fFlags |= (uCreationMode << RTFILE_O_CREATE_MODE_SHIFT) & RTFILE_O_CREATE_MODE_MASK;
+                    /* If we're opening a file in read-only mode, strip truncation mode.
+                     * rtFileRecalcAndValidateFlags() will validate it anyway, but avoid asserting in debug builds. */
+                    if (fFlags & RTFILE_O_READ) 
+                        fFlags &= ~RTFILE_O_TRUNCATE;
                     rc = RTFileOpen(&pFile->hFile, pFile->szName, fFlags);
                     if (RT_SUCCESS(rc))
                     {
