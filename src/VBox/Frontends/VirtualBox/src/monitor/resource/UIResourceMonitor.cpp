2b6b0d6baae1bac4de20980c373c944a764def72
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2020-04-07 08:22:06 +0000

    FE/Qt: bugref:9686. build fix
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@83601 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Frontends/VirtualBox/src/monitor/resource/UIResourceMonitor.cpp
+++ src/VBox/Frontends/VirtualBox/src/monitor/resource/UIResourceMonitor.cpp
@@ -1117,7 +1117,7 @@ void UIResourceMonitorModel::sltTimeout()
     for (int i = 0; i < (int)VMResourceMonitorColumn_Max; ++i)
     {
         for (int j = 0; j < m_itemList.size(); ++j)
-            if (m_columnDataMaxLength.value(i, 0) < m_itemList[j].m_columnData[i])
+            if (m_columnDataMaxLength.value(i, 0) < m_itemList[j].m_columnData[i].length())
                 m_columnDataMaxLength[i] = m_itemList[j].m_columnData[i].length();
     }
     emit sigDataUpdate();
