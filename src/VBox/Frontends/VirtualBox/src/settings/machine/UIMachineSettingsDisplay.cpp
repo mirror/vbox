1f3d02abf51423e0a4d0bdcd03e68d80cd8673e8
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2020-04-08 17:40:29 +0000

    FE/Qt: bugref:9691: UIMachineSettingsDisplay: Hiding 2D Video Acceleration check-box as this functionality no more stable.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@83625 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Frontends/VirtualBox/src/settings/machine/UIMachineSettingsDisplay.cpp
+++ src/VBox/Frontends/VirtualBox/src/settings/machine/UIMachineSettingsDisplay.cpp
@@ -818,11 +818,8 @@ void UIMachineSettingsDisplay::polishPage()
 #else
     m_pCheckbox3D->hide();
 #endif
-#ifdef VBOX_WITH_VIDEOHWACCEL
-    m_pCheckbox2DVideo->setEnabled(isMachineOffline() && VBox2DHelpers::isAcceleration2DVideoAvailable());
-#else
     m_pCheckbox2DVideo->hide();
-#endif
+    m_pPlaceholder2DVideo->hide();
 
     /* Polish 'Remote Display' availability: */
     m_pTabWidget->setTabEnabled(1, oldDisplayData.m_fRemoteDisplayServerSupported);
