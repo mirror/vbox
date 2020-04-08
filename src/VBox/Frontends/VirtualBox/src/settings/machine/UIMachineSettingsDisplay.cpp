a8d4bd62abf69d57d0e347049758d22034b2bfa0
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2020-04-08 18:06:46 +0000

    FE/Qt: bugref:9691: UIMachineSettingsDisplay: Missed bit for r137058.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@83627 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Frontends/VirtualBox/src/settings/machine/UIMachineSettingsDisplay.cpp
+++ src/VBox/Frontends/VirtualBox/src/settings/machine/UIMachineSettingsDisplay.cpp
@@ -871,11 +871,6 @@ void UIMachineSettingsDisplay::sltHandleGraphicsControllerComboChange()
 {
     /* Update Video RAM requirements: */
     m_pVideoMemoryEditor->setGraphicsControllerType(m_pGraphicsControllerEditor->value());
-#ifdef VBOX_WITH_VIDEOHWACCEL
-    m_pLayout2DVideo->setCurrentWidget(  graphicsControllerTypeCurrent() == KGraphicsControllerType_VBoxVGA
-                                       ? m_pCheckbox2DVideo
-                                       : m_pPlaceholder2DVideo);
-#endif
 
     /* Revalidate: */
     revalidate();
