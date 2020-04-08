c83f04a9786ecadc500f1611034bdb0144b1ab09
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2020-04-08 16:26:17 +0000

    Validation Kit/testdriver: Explicitly set tarfile.GNU_FORMAT when creating tarballs, as with Python 3.8 the default format (tarfile.DEFAULT_FORMAT) has been changed to tarfile.PAX_FORMAT, which our extraction code (vts_tar) currently can't handle.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@83623 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/ValidationKit/testdriver/testfileset.py
+++ src/VBox/ValidationKit/testdriver/testfileset.py
@@ -513,7 +513,10 @@ class TestFileSet(object):
 
         # Open the tarball:
         try:
-            oTarFile = tarfile.open(sTarFileHst, 'w:gz');
+            # Make sure to explicitly set GNU_FORMAT here, as with Python 3.8 the default format (tarfile.DEFAULT_FORMAT)
+            # has been changed to tarfile.PAX_FORMAT, which our extraction code (vts_tar) currently can't handle.
+            ## @todo Remove tarfile.GNU_FORMAT and use tarfile.PAX_FORMAT as soon as we have PAX support.
+            oTarFile = tarfile.open(sTarFileHst, 'w:gz', format = tarfile.GNU_FORMAT);
         except:
             return reporter.errorXcpt('Failed to open new tar file: %s' % (sTarFileHst,));
 
