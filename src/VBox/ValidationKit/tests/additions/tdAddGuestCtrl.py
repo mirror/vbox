7a086b2d64d061ed0ab0e977cc41f09512e0f683
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2020-04-09 08:16:41 +0000

    Guest Control/Validation Kit: Pylint.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@83641 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/ValidationKit/tests/additions/tdAddGuestCtrl.py
+++ src/VBox/ValidationKit/tests/additions/tdAddGuestCtrl.py
@@ -1584,11 +1584,11 @@ class SubTstDrvAddGuestCtrl(base.SubTestDriverBase):
         """
         if oTestVm.isWindows():
             sPathSC = os.path.join(self.getGuestSystemDir(oTestVm), 'sc.exe');
-            if fStart:
-                return self.oTstDrv.txsRunTest(oTxsSession, 'Starting VBoxService with verbose logging', 30 * 1000,
+            if fStart is True:
+                return self.oTstDrv.txsRunTest(oTxsSession, 'Starting VBoxService with verbose logging', 30 * 1000, \
                                                sPathSC, (sPathSC, 'start', 'VBoxService'));
             else:
-                return self.oTstDrv.txsRunTest(oTxsSession, 'Stopping VBoxService', 30 * 1000,
+                return self.oTstDrv.txsRunTest(oTxsSession, 'Stopping VBoxService', 30 * 1000, \
                                                sPathSC, (sPathSC, 'stop', 'VBoxService'));
         else:
             reporter.log('Controlling VBoxService not supported for this guest yet');
