7e0a0ff178a81bbffd3d6f66ed5d37a22c350264
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2020-04-18 14:17:26 +0000

    VBoxBalloonCtrl: VC++ 14.1 adjustments. Only include libc++ headers where needed.  Elimited stupid std::string usage in --help, reformatted --help output.  [build fix] bugref:8489
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@83800 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Frontends/VBoxBalloonCtrl/VBoxWatchdog.cpp
+++ src/VBox/Frontends/VBoxBalloonCtrl/VBoxWatchdog.cpp
@@ -52,6 +52,7 @@
 #include <iprt/system.h>
 #include <iprt/time.h>
 
+#include <algorithm>
 #include <signal.h>
 
 #include "VBoxWatchdogInternal.h"
@@ -848,7 +849,7 @@ static void displayHelp(const char *pszImage)
 
     for (unsigned i = 0; i < RT_ELEMENTS(g_aOptions); i++)
     {
-        const char *pcszDescr = "";
+        const char *pcszDescr;
         switch (g_aOptions[i].iShort)
         {
             case GETOPTDEF_WATCHDOG_DISABLE_MODULE:
@@ -887,6 +888,8 @@ static void displayHelp(const char *pszImage)
             case 'I':
                 pcszDescr = "Maximum time interval to trigger log rotation (seconds).";
                 break;
+            default:
+                AssertFailedBreakStmt(pcszDescr = "");
         }
 
         if (g_aOptions[i].iShort < 1000)
