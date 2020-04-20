befff5b5af5cc04163c884a625f274a6c79b678e
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2020-04-20 17:15:49 +0000

    Validation Kit/Guest Additions: Use asyncMkDirPath() instead of asyncMkDir() here.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@83866 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/ValidationKit/tests/additions/tdAddBasic1.py
+++ src/VBox/ValidationKit/tests/additions/tdAddBasic1.py
@@ -357,7 +357,7 @@ class tdAddBasic1(vbox.TestDriver):                                         # py
         or oTestVm.sKind.startswith('Ubuntu'):
 
             if fNeedsProxy:
-                fRc = oTxsSession.syncMkDir("/etc/apt/apt.conf.d/", 0o755, fIgnoreErrors = True);
+                fRc = oTxsSession.asyncMkDirPath("/etc/apt/apt.conf.d/", 0o755);
                 if fRc:
                     fRc = oTxsSession.syncUploadString('Acquire::http::Proxy \"' + sHttpProxy + '\"\r\n'
                                                        'Acquire::https::Proxy \"' + sHttpsProxy + '\"',
