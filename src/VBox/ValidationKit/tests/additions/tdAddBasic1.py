76a9fe985fda488030175aafe0d01ece9268dbeb
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2020-04-24 17:20:24 +0000

    Validation Kit/Additions: Resolved a todo: Check the overall Linux Guest Additions installer status.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@83977 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/ValidationKit/tests/additions/tdAddBasic1.py
+++ src/VBox/ValidationKit/tests/additions/tdAddBasic1.py
@@ -349,8 +349,8 @@ class tdAddBasic1(vbox.TestDriver):                                         # py
         # Make sure to add "--nox11" to the makeself wrapper in order to not getting any blocking
         # xterm window spawned.
         fRc = self.txsRunTest(oTxsSession, 'VBoxLinuxAdditions.run', 30 * 60 * 1000,
-                                '/bin/sh', ('/bin/sh', '${CDROM}/VBoxLinuxAdditions.run', '--nox11'));
-        ## @todo We need to figure out why the result is != 0 when running the .run installer. For now just ignore it.
+                              '/bin/sh', ('/bin/sh', '${CDROM}/VBoxLinuxAdditions.run', '--nox11'),
+                              fCheckSessionStatus = True);
         if not fRc:
             reporter.error('Installing Linux Additions failed (isSuccess=%s, iResult=%d, see log file for details)'
                             % (oTxsSession.isSuccess(), oTxsSession.getResult()));
