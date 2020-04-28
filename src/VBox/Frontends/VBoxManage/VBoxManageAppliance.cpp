8c72b739ba4259e9914072d0f250d37b9dffac59
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2020-04-28 10:06:54 +0000

    bugref:9699. small fix.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@84033 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Frontends/VBoxManage/VBoxManageAppliance.cpp
+++ src/VBox/Frontends/VBoxManage/VBoxManageAppliance.cpp
@@ -2034,7 +2034,7 @@ RTEXITCODE handleSignAppliance(HandlerArg *arg)
                         RTCrDigestRelease(hDigest);
                         RTCrKeyRelease(hPrivateKey);
                         if (rc == VERR_CR_PKIX_SIGNATURE_MISMATCH)
-                            return RTMsgErrorExit(RTEXITCODE_FAILURE, "The manifest signature does not match", rc);
+                            return RTMsgErrorExit(RTEXITCODE_FAILURE, "The manifest signature does not match");
 
                         return RTMsgErrorExit(RTEXITCODE_FAILURE, "Error validating the manifest signature (%Rrc, %s)",
                                               rc, ErrInfo.pszMsg);
