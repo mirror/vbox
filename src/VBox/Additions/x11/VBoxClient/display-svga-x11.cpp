c803c5d81a4e3fbdbff508d232a372ac8679d7f7
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2020-05-11 12:08:56 +0000

    bugref:9637. Listening to RRScreenChangeNotify event seems to be sufficient
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@84252 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Additions/x11/VBoxClient/display-svga-x11.cpp
+++ src/VBox/Additions/x11/VBoxClient/display-svga-x11.cpp
@@ -562,14 +562,10 @@ static void monitorRandREvents()
     switch (eventTypeOffset)
     {
         case RRScreenChangeNotify:
-            VBClLogInfo("RRScreenChangeNotify\n");
+            VBClLogInfo("RRScreenChangeNotify event received\n");
             queryMonitorPositions();
             break;
-        case RRNotify:
-            VBClLogInfo("RRNotify\n");
-            break;
         default:
-            VBClLogInfo("Unknown RR event: %d\n", event.type);
             break;
     }
 }
@@ -807,11 +803,9 @@ static void x11Connect()
     x11Context.fWmwareCtrlExtention = XQueryExtension(x11Context.pDisplay, "VMWARE_CTRL",
                                                       &x11Context.hVMWCtrlMajorOpCode, &dummy, &dummy);
     if (!x11Context.fWmwareCtrlExtention)
-        VBClLogError("VMWARE's ctrl extension is not available!\n");
+        VBClLogError("VMWARE's ctrl extension is not available! Multi monitor management is not possible\n");
     else
-    {
         VBClLogInfo("VMWARE's ctrl extension is available. Major Opcode is %d.\n", x11Context.hVMWCtrlMajorOpCode);
-    }
 
     /* Check Xrandr stuff. */
     bool fSuccess = false;
@@ -839,10 +833,6 @@ static void x11Connect()
     }
     x11Context.rootWindow = DefaultRootWindow(x11Context.pDisplay);
     x11Context.hEventMask = RRScreenChangeNotifyMask;
-    if (x11Context.hRandRMinor >= 2)
-        x11Context.hEventMask |= RRCrtcChangeNotifyMask
-                               | RROutputChangeNotifyMask
-                               | RROutputPropertyNotifyMask;
 
     /* Select the XEvent types we want to listen to. */
 #ifdef WITH_DISTRO_XRAND_XINERAMA
