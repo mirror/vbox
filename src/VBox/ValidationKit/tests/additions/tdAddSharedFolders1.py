3475edb4963d7e2bfa501370a445432075e8134c
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2020-05-11 15:30:44 +0000

    Validation Kit/tdAddSharedFolders1.py: Also check the session status while running FsPerf.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@84257 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/ValidationKit/tests/additions/tdAddSharedFolders1.py
+++ src/VBox/ValidationKit/tests/additions/tdAddSharedFolders1.py
@@ -280,11 +280,12 @@ class SubTstDrvAddSharedFolders1(base.SubTestDriverBase):
             # Run FsPerf:
             reporter.log2('Starting guest FsPerf (%s)...' % (asArgs,));
             sFsPerfPath = self._locateGstFsPerf(oTxsSession);
-            fRc = self.oTstDrv.txsRunTest(oTxsSession, 'FsPerf', 30 * 60 * 1000, sFsPerfPath, asArgs);
+            fRc = self.oTstDrv.txsRunTest(oTxsSession, 'FsPerf', 30 * 60 * 1000, sFsPerfPath, asArgs,
+                                          fCheckSessionStatus = True);
             reporter.log2('FsPerf -> %s' % (fRc,));
             if not fRc:
                 # Do a bit of diagnosis to find out why this failed.
-                if     not  oTestVm.isWindows() \
+                if     not oTestVm.isWindows() \
                    and not oTestVm.isOS2():
                     oTxsSession.syncExec("/bin/ls", ("/bin/ls", "-al", sFsPerfPath), fIgnoreErrors = True);
                     oTxsSession.syncExec("/bin/ls", ("/bin/ls", "-al", "-R", "/opt"), fIgnoreErrors = True);
