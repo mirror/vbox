20c871fd7fe3600ee213e49fd70cacb832940bf6
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2020-05-12 16:37:49 +0000

    IPRT/Base64: Allow the end-of-line style to be specified via the fFlags parameter when encoding. ?[added assertion] bugref:9699
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@84274 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Runtime/common/string/base64.cpp
+++ src/VBox/Runtime/common/string/base64.cpp
@@ -506,6 +506,7 @@ RTDECL(int) RTBase64EncodeEx(const void *pvData, size_t cbData, uint32_t fFlags,
     size_t const    cchEol = g_acchEolStyles[fFlags & RTBASE64_FLAGS_EOL_STYLE_MASK];
     char const      chEol0 = g_aachEolStyles[fFlags & RTBASE64_FLAGS_EOL_STYLE_MASK][0];
     char const      chEol1 = g_aachEolStyles[fFlags & RTBASE64_FLAGS_EOL_STYLE_MASK][1];
+    Assert(cchEol == (chEol0 != '\0' ? 1U : 0U) + (chEol1 != '\0' ? 1U : 0U));
 
     /*
      * Process whole "trios" of input data.
