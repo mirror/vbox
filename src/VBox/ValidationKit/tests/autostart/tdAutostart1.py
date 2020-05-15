1e2b5521493033dacb22a5b9d13257f0d7840c96
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2020-05-15 18:22:55 +0000

    Main: bugref:9341: Fixed pylint issues
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@84323 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/ValidationKit/tests/autostart/tdAutostart1.py
+++ src/VBox/ValidationKit/tests/autostart/tdAutostart1.py
@@ -189,13 +189,14 @@ class tdAutostartOs(object):
                 asFiles = (s for s in asFiles
                            if oRegExp.match(os.path.basename(s))
                            and os.path.exists(sTestBuildDir + '/' + s));
-                asFiles = sorted(asFiles, reverse = True, key = lambda s: os.path.getmtime(os.path.join(sTestBuildDir, s)));
+                asFiles = sorted(asFiles, reverse = True, 
+                                 key = lambda s, sTstBuildDir = sTestBuildDir: os.path.getmtime(os.path.join(sTstBuildDir, s)));
                 if asFiles:
                     return sTestBuildDir + '/' + asFiles[0];
             except:
                 pass;
 
-        reporter.error('Failed to find a file matching "%s" in %s.' % (sRegExp, sTestBuildDir));
+        reporter.error('Failed to find a file matching "%s" in %s.' % (sRegExp, ','.join(asTestBuildDirs)));
         return None;
 
     def _createAutostartCfg(self, sDefaultPolicy = 'allow', asUserAllow = (), asUserDeny = ()):
