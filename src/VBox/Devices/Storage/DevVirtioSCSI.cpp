6cc86cea4a5471b8fa8ba3acfbbf0cdd7b5111d3
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2020-05-19 06:47:48 +0000

    Fix burns
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@84357 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Devices/Storage/DevVirtioSCSI.cpp
+++ src/VBox/Devices/Storage/DevVirtioSCSI.cpp
@@ -703,7 +703,10 @@ static uint8_t virtioScsiEstimateCdbLen(uint8_t uCmd, uint8_t cbMax)
 #endif /* LOG_ENABLED */
 
 
-/* @todo Figure out how to implement this with R0 changes. Not used by current linux driver */
+/*
+ * @todo Figure out how to implement this with R0 changes. Not used by current linux driver
+ */
+
 #if 0
 static int virtioScsiR3SendEvent(PPDMDEVINS pDevIns, PVIRTIOSCSI pThis, uint16_t uTarget, uint32_t uEventType, uint32_t uReason)
 {
