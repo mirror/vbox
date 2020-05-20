e0dd18b01e5e8e88fabb3e83f289455191c6c75e
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2020-05-20 14:29:30 +0000

    VBoxMMNotificationClient.cpp: Use newly added wrapper for the problematic header (endpointvolume.h).  bugref:8489
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@84408 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Devices/Audio/VBoxMMNotificationClient.cpp
+++ src/VBox/Devices/Audio/VBoxMMNotificationClient.cpp
@@ -19,19 +19,10 @@
 #include "VBoxMMNotificationClient.h"
 
 #include <iprt/win/windows.h>
-
-#pragma warning(push)
-#pragma warning(disable: 4201)
-#ifndef DOXYGEN_RUNNING
-# if RT_MSC_PREREQ(RT_MSC_VER_VC141)
-#  pragma warning(disable: 4091) /* v7.1\include\ksmedia.h(4356): warning C4091: 'typedef ': ignored on left of '<unnamed-enum-KSEVENT_DYNAMIC_FORMAT_CHANGE>' when no variable is declared */
-# endif
-#endif
 #include <mmdeviceapi.h>
-#include <endpointvolume.h>
-#pragma warning(pop)
+#include <iprt/win/endpointvolume.h>
 
-#ifdef LOG_GROUP
+#ifdef LOG_GROUP  /** @todo r=bird: wtf? Put it before all other includes like you're supposed to. */
 # undef LOG_GROUP
 #endif
 #define LOG_GROUP LOG_GROUP_DRV_HOST_AUDIO
