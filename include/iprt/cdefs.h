92288c35c96dcafac349418c6b59a20a69a2580f
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2020-05-25 15:19:43 +0000

    iprt/cdefs.h: The __extension__ trick works for gcc 8.4 as well.  bugref:9746
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@84511 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- include/iprt/cdefs.h
+++ include/iprt/cdefs.h
@@ -2563,7 +2563,7 @@
 #if RT_MSC_PREREQ(RT_MSC_VER_VS2005) /** @todo Probably much much earlier. */ \
  || (defined(__cplusplus) && (   RT_GNUC_PREREQ(6, 1) \
                               && (  !RT_GNUC_PREREQ(7, 0) /* gcc-7 warns again */ \
-                                  || RT_GNUC_PREREQ(9, 0) /* gcc-9 works with __extension__, the ones in between needs testing */ ))) \
+                                  || RT_GNUC_PREREQ(8, 0) /* gcc-8 works with __extension__, the ones in between needs testing */ ))) \
  || defined(__WATCOMC__) /* openwatcom 1.9 supports it, we don't care about older atm. */ \
  || RT_CLANG_PREREQ_EX(3, 4, 0) /* Only tested clang v3.4, support is probably older. */
 # define RT_FLEXIBLE_ARRAY
@@ -2584,7 +2584,7 @@
  * A trick to make GNU C++ quietly accept flexible arrays in C++ code when
  * pedantic warnings are enabled.  Put this on the line before the flexible
  * array. */
-#if (RT_GNUC_PREREQ(9, 0) && defined(__cplusplus)) || defined(DOXGYEN_RUNNING)
+#if (RT_GNUC_PREREQ(8, 0) && defined(__cplusplus)) || defined(DOXGYEN_RUNNING)
 # define RT_FLEXIBLE_ARRAY_EXTENSION            RT_GCC_EXTENSION
 #else
 # define RT_FLEXIBLE_ARRAY_EXTENSION
