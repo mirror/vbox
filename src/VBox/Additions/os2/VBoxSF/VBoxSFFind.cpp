7f1bb9529021ac02a542b32fa9ea0aae511f0b91
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2020-05-25 08:45:14 +0000

    os2/VBoxSF: 8.3 detection fixes.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@84484 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Additions/os2/VBoxSF/VBoxSFFind.cpp
+++ src/VBox/Additions/os2/VBoxSF/VBoxSFFind.cpp
@@ -79,7 +79,7 @@ static bool vboxSfOs2IsUtf16Name8dot3(PRTUTF16 pwszName, size_t cwcName, char *p
                     break;
                 if (wc == '.')
                     return false;
-                if (off >= offMax)
+                if (off > offMax)
                     return false;
             }
             break;
@@ -91,7 +91,7 @@ static bool vboxSfOs2IsUtf16Name8dot3(PRTUTF16 pwszName, size_t cwcName, char *p
     }
 
     /*
-     * Conver to the native code page.
+     * Convert to the native code page.
      */
     APIRET rc = SafeKernStrFromUcs(NULL, pszTmp, pwszName, cbTmp, cwcName);
     if (rc != NO_ERROR)
@@ -119,7 +119,7 @@ static bool vboxSfOs2IsUtf16Name8dot3(PRTUTF16 pwszName, size_t cwcName, char *p
         char ch = *pszTmp++;
         if (ch == '\0')
             return true;
-        if (ch != '.')
+        if (ch == '.')
             return false;
         if (cch >= 3)
             return false;
