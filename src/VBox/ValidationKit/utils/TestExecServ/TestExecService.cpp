97028026fcbc20ddaefefb9ad1299a2ff6849b7a
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2020-06-10 15:28:53 +0000

    Validation Kit/TXS: Added support for creating (packing) files / directories on the guest.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@84763 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/ValidationKit/utils/TestExecServ/TestExecService.cpp
+++ src/VBox/ValidationKit/utils/TestExecServ/TestExecService.cpp
@@ -824,6 +824,60 @@ static int txsDoExpandString(PCTXSPKTHDR pPktHdr)
 }
 
 /**
+ * Packs a tar file / directory.
+ *
+ * @returns IPRT status code from send.
+ * @param   pPktHdr             The pack file packet.
+ */
+static int txsDoPackFile(PCTXSPKTHDR pPktHdr)
+{
+    int rc;
+    char *pszFile = NULL;
+    char *pszSource = NULL;
+
+    /* Packet cursor. */
+    const char *pch = (const char *)(pPktHdr + 1);
+
+    if (txsIsStringValid(pPktHdr, "file", pch, &pszFile, &pch, &rc))
+    {
+        if (txsIsStringValid(pPktHdr, "source", pch, &pszSource, &pch, &rc))
+        {
+            char *pszSuff = RTPathSuffix(pszFile);
+
+            const char *apszArgs[7];
+            unsigned cArgs = 0;
+
+            apszArgs[cArgs++] = "RTTar";
+            apszArgs[cArgs++] = "--create";
+
+            apszArgs[cArgs++] = "--file";
+            apszArgs[cArgs++] = pszFile;
+
+            if (   pszSuff
+                && (   !RTStrICmp(pszSuff, ".gz")
+                    || !RTStrICmp(pszSuff, ".tgz")))
+                apszArgs[cArgs++] = "--gzip";
+
+            apszArgs[cArgs++] = pszSource;
+
+            RTEXITCODE rcExit = RTZipTarCmd(cArgs, (char **)apszArgs);
+            if (rcExit != RTEXITCODE_SUCCESS)
+                rc = VERR_GENERAL_FAILURE; /** @todo proper return code. */
+            else
+                rc = VINF_SUCCESS;
+
+            rc = txsReplyRC(pPktHdr, rc, "RTZipTarCmd(\"%s\",\"%s\")",
+                            pszFile, pszSource);
+
+            RTStrFree(pszSource);
+        }
+        RTStrFree(pszFile);
+    }
+
+    return rc;
+}
+
+/**
  * Unpacks a tar file.
  *
  * @returns IPRT status code from send.
@@ -3008,6 +3062,8 @@ static RTEXITCODE txsMainLoop(void)
             rc = txsDoPutFile(pPktHdr, true /*fHasMode*/);
         else if (txsIsSameOpcode(pPktHdr, "GET FILE"))
             rc = txsDoGetFile(pPktHdr);
+        else if (txsIsSameOpcode(pPktHdr, "PKFILE"))
+            rc = txsDoPackFile(pPktHdr);
         else if (txsIsSameOpcode(pPktHdr, "UNPKFILE"))
             rc = txsDoUnpackFile(pPktHdr);
         /* Misc: */
