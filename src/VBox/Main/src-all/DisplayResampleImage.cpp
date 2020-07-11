c4456121a8529f906f3d21a741267df6189702e4
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2020-07-11 10:51:30 +0000

    Main/DisplayResampleImage.cpp: sign error/warning fix for Clang 11.  bugref:9790
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@85210 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Main/src-all/DisplayResampleImage.cpp
+++ src/VBox/Main/src-all/DisplayResampleImage.cpp
@@ -88,7 +88,6 @@ void BitmapScale32 (uint8_t *dst,
                 {
                     FIXEDPOINT xportion;
                     FIXEDPOINT pcontribution;
-                    int p;
                     if (FIXEDPOINT_FLOOR (sx) == FIXEDPOINT_FLOOR (sx1))
                     {
                         xportion = INT_TO_FIXEDPOINT(1) - FIXEDPOINT_FRACTION(sx);
@@ -110,7 +109,7 @@ void BitmapScale32 (uint8_t *dst,
                         pcontribution = xportion * yportion;
                     }
                     /* Color depth specific code begin */
-                    p = *(uint32_t *)(pu8SrcLine + FIXEDPOINT_TO_INT(sx) * 4);
+                    int32_t p = *(int32_t *)(pu8SrcLine + FIXEDPOINT_TO_INT(sx) * 4);
                     /* Color depth specific code end */
                     red   += trueColorGetRed (p) * pcontribution;
                     green += trueColorGetGreen (p) * pcontribution;
