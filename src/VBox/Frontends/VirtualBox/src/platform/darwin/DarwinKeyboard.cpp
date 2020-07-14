4af6a0637060c9cb20b2364be2520eb5e9c82c01
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2020-07-14 09:46:56 +0000

    FE/Qt: Fixed bogus if statement in darwinUsbHidSubscribeInterestNotifications.  bugref:9790
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@85326 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Frontends/VirtualBox/src/platform/darwin/DarwinKeyboard.cpp
+++ src/VBox/Frontends/VirtualBox/src/platform/darwin/DarwinKeyboard.cpp
@@ -1753,9 +1753,10 @@ static int darwinUsbHidSubscribeInterestNotifications(VBoxHidsState_t *pHidState
             CFRunLoopAddSource(CFRunLoopGetCurrent(), IONotificationPortGetRunLoopSource(pHidState->pNotificationPrortRef), kCFRunLoopDefaultMode);
 
             rc = IOServiceAddMatchingNotification(pHidState->pNotificationPrortRef, kIOMatchedNotification,
-                pDictionary, darwinUsbHidDeviceMatchCb, pHidState, &pHidState->pUsbHidDeviceMatchNotify);
+                                                  pDictionary, darwinUsbHidDeviceMatchCb, pHidState,
+                                                  &pHidState->pUsbHidDeviceMatchNotify);
 
-            if (rc == kIOReturnSuccess && &pHidState->pUsbHidDeviceMatchNotify != NULL)
+            if (rc == kIOReturnSuccess && pHidState->pUsbHidDeviceMatchNotify != IO_OBJECT_NULL)
             {
                 darwinUsbHidDeviceMatchCb(pHidState, pHidState->pUsbHidDeviceMatchNotify);
                 LogRel2(("Successfully subscribed to IOUSBInterface IOService match notifications\n"));
