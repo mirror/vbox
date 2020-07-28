16d5f5a068ad6329fd9e3c0cb628176ae2a0e692
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2020-07-28 15:57:10 +0000

    DevPciIch9.cpp: indent fix
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@85490 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Devices/Bus/DevPciIch9.cpp
+++ src/VBox/Devices/Bus/DevPciIch9.cpp
@@ -1894,7 +1894,7 @@ static void ich9pciBiosInitDeviceBARs(PPDMDEVINS pDevIns, PDEVPCIROOT pPciRoot, 
         uint8_t u8ResourceType = devpciR3GetByte(pPciDev, u32Address);
 
         bool fPrefetch =    (u8ResourceType & ((uint8_t)(PCI_ADDRESS_SPACE_MEM_PREFETCH | PCI_ADDRESS_SPACE_IO)))
-                      == PCI_ADDRESS_SPACE_MEM_PREFETCH;
+                         == PCI_ADDRESS_SPACE_MEM_PREFETCH;
         bool f64Bit =    (u8ResourceType & ((uint8_t)(PCI_ADDRESS_SPACE_BAR64 | PCI_ADDRESS_SPACE_IO)))
                       == PCI_ADDRESS_SPACE_BAR64;
         bool fIsPio = ((u8ResourceType & PCI_ADDRESS_SPACE_IO) == PCI_ADDRESS_SPACE_IO);
