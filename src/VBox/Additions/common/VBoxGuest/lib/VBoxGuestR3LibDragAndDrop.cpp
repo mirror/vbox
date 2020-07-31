62b04e5cb236edad4774727613cb273b7513ca97
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2020-07-31 17:14:22 +0000

    DnD/VbglR3: Fixes for sending raw meta data to host.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@85587 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Additions/common/VBoxGuest/lib/VBoxGuestR3LibDragAndDrop.cpp
+++ src/VBox/Additions/common/VBoxGuest/lib/VBoxGuestR3LibDragAndDrop.cpp
@@ -1666,7 +1666,7 @@ static int vbglR3DnDGHSendRawData(PVBGLR3GUESTDNDCMDCTX pCtx, void *pvData, size
     VBOXDNDDATAHDR dataHdr;
     RT_ZERO(dataHdr);
 
-    /* For raw data only the total size is required to be specified. */
+    dataHdr.cbMeta  = (uint32_t)cbData;
     dataHdr.cbTotal = cbData;
 
     return vbglR3DnDGHSendDataInternal(pCtx, pvData, cbData, &dataHdr);
@@ -1756,6 +1756,8 @@ VBGLR3DECL(int) VbglR3DnDGHSendData(PVBGLR3GUESTDNDCMDCTX pCtx, const char *pszF
 
     LogFlowFunc(("pszFormat=%s, pvData=%p, cbData=%RU32\n", pszFormat, pvData, cbData));
 
+    LogRel2(("DnD: Sending %RU32 bytes meta data in format '%s'\n", cbData, pszFormat));
+
     int rc;
     if (DnDMIMEHasFileURLs(pszFormat, strlen(pszFormat)))
     {
@@ -1775,18 +1777,20 @@ VBGLR3DECL(int) VbglR3DnDGHSendData(PVBGLR3GUESTDNDCMDCTX pCtx, const char *pszF
                 rc = vbglR3DnDGHSendTransferData(pCtx, &lstTransfer);
             DnDTransferListDestroy(&lstTransfer);
         }
-
-        if (RT_FAILURE(rc))
-            LogRel(("DnD: Sending guest meta data to host failed with %Rrc\n", rc));
     }
     else
         rc = vbglR3DnDGHSendRawData(pCtx, pvData, cbData);
 
     if (RT_FAILURE(rc))
     {
-        int rc2 = VbglR3DnDGHSendError(pCtx, rc);
-        if (RT_FAILURE(rc2))
-            LogFlowFunc(("Unable to send error (%Rrc) to host, rc=%Rrc\n", rc, rc2));
+        LogRel(("DnD: Sending data failed with rc=%Rrc\n", rc));
+
+        if (rc != VERR_CANCELLED)
+        {
+            int rc2 = VbglR3DnDGHSendError(pCtx, rc);
+            if (RT_FAILURE(rc2))
+                LogFlowFunc(("Unable to send error (%Rrc) to host, rc=%Rrc\n", rc, rc2));
+        }
     }
 
     return rc;
