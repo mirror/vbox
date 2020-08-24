dfcd8e962fcadb6caa6a0d26d263ad228b6eeabe
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2020-08-24 17:27:48 +0000

    Main: bugref:9224: fixed scm issues
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@85878 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Runtime/common/dvm/dvmmbr.cpp
+++ src/VBox/Runtime/common/dvm/dvmmbr.cpp
@@ -242,7 +242,7 @@ static DECLCALLBACK(int) rtDvmFmtMbrProbe(PCRTDVMDISK pDisk, uint32_t *puScore)
                 && pbMbr[510] == 0x55
                 && pbMbr[511] == 0xaa)
                 *puScore = RTDVM_MATCH_SCORE_SUPPORTED; /* Not perfect because GPTs have a protective MBR. */
-            /* @todo this could easily confuser a DOS, OS/2 or NT boot sector with a MBR... */
+            /** @todo this could easily confuser a DOS, OS/2 or NT boot sector with a MBR... */
             RTMemTmpFree(pbMbr);
         }
         else
