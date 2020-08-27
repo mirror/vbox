5bbf20c00c92fc447f93ab23dca1401f7b756567
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2020-08-27 13:46:55 +0000

    Runtime/dbgkrnlinfo-r0drv-nt.cpp: Split up rtR0DbgKrnlNtInit() and make it reallocate the module information to the maximum and search again for a module symbol when not being found on the first try, fixes VERR_NEM_MISSING_KERNEL_API_2 when using Hyper-V on systems with a lot of loaded kernel modules, bugref:9044 comment 66 until 68 [scm fix]
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@85906 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Runtime/r0drv/nt/dbgkrnlinfo-r0drv-nt.cpp
+++ src/VBox/Runtime/r0drv/nt/dbgkrnlinfo-r0drv-nt.cpp
@@ -375,7 +375,7 @@ static bool rtR0DbgKrnlNtParseModule(PRTDBGNTKRNLMODINFO pModInfo, uint8_t const
  * @retval  VERR_MODULE_NOT_FOUND if @a pModInfo wasn't found.
  *
  * @param   pInfo               Pointer to the module information.
- * @param   cModules            Number of valid module entries in the module information pointer. 
+ * @param   cModules            Number of valid module entries in the module information pointer.
  * @param   pModInfo            Custom module to search for.  Optional.
  */
 static int rtR0DbgKrnlNtSearchForModuleWorker(PRTL_PROCESS_MODULES pInfo, uint32_t cModules, PRTDBGNTKRNLMODINFO pModInfo)
