fd97d779c3ff10360a0d1eaa72d15e2b6053175d
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2020-08-28 10:35:13 +0000

    DnD/X11: Committed too much in r140114.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@85926 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Additions/x11/VBoxClient/draganddrop.cpp
+++ src/VBox/Additions/x11/VBoxClient/draganddrop.cpp
@@ -1667,13 +1667,6 @@ int DragInstance::hgEnter(const RTCList<RTCString> &lstFormats, uint32_t dndList
         rc = wndXDnDSetActionList(m_wndProxy.hWnd, lstActions);
         AssertRCBreak(rc);
 
-        Atom atmEnabled = 1;
-        XChangeProperty(m_pDisplay, m_wndProxy.hWnd, xAtom(XA_XdndActionAsk), XA_ATOM, 32, PropModeReplace,
-                        reinterpret_cast<unsigned char*>(&atmEnabled), 1);
-
-        XChangeProperty(m_pDisplay, m_wndProxy.hWnd, xAtom(XA_XdndActionDescription), XA_STRING, 8, PropModeReplace,
-                        (const unsigned char *)"VBox DnD", sizeof("VBox DnD"));
-
         /* Set the DnD selection owner to our window. */
         /** @todo Don't use CurrentTime -- according to ICCCM section 2.1. */
         XSetSelectionOwner(m_pDisplay, xAtom(XA_XdndSelection), m_wndProxy.hWnd, CurrentTime);
