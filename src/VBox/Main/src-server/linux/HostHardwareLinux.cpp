797c64760eb5328c0f4b1abd236c8ab87981adb6
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2020-08-28 16:46:50 +0000

    Main/HostHardwareLinux.cpp: doxygen fix. bugref:9224
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@85932 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Main/src-server/linux/HostHardwareLinux.cpp
+++ src/VBox/Main/src-server/linux/HostHardwareLinux.cpp
@@ -268,7 +268,7 @@ static bool isCdromDevNum(dev_t Number) RT_NOTHROW_DEF
  * @param pszVendor  where to store the vendor id string on success (optional)
  * @param cbVendor   the size of the @a pszVendor buffer
  * @param pszModel   where to store the product id string on success (optional)
- * @param cchModel   the size of the @a pszModel buffer
+ * @param cbModel    the size of the @a pszModel buffer
  * @note check documentation on the SCSI INQUIRY command and the Linux kernel
  *       SCSI headers included above if you want to understand what is going
  *       on in this method.
