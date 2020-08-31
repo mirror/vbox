5b81d4e27073e5f1581e100a545f9a23dda59735
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2020-08-31 20:12:01 +0000

    tstRTInlineAsm.cpp: Display the extended APIC IDs too.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@85963 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Runtime/testcase/tstRTInlineAsm.cpp
+++ src/VBox/Runtime/testcase/tstRTInlineAsm.cpp
@@ -414,6 +414,8 @@ void tstASMCpuId(void)
                 RTTestIPrintf(RTTESTLVL_ALWAYS, " %d", iBit);
         RTTestIPrintf(RTTESTLVL_ALWAYS, "\n");
     }
+    if (ASMCpuId_EAX(0) >= 0xb)
+        RTTestIPrintf(RTTESTLVL_ALWAYS, "APIC ID(Ext 0b):                 %#010x\n", ASMGetApicIdExt0B());
 
     /*
      * Extended.
@@ -651,6 +653,8 @@ void tstASMCpuId(void)
                        s.uEAX & 0xff, s.uEAX & 0xff,
                        s.uEBX, s.uEBX);
      }
+     if (ASMCpuId_EAX(UINT32_C(0x80000000)) >= UINT32_C(0x8000001E))
+         RTTestIPrintf(RTTESTLVL_ALWAYS, "APIC ID(Ext 8000001b):           %#010x\n", ASMGetApicIdExt8000001E());
 }
 
 # if 0
