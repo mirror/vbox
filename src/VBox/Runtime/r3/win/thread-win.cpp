56cd2ab23e5e30514670eef346bf4387112dd996
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2020-09-18 13:38:58 +0000

    IPRT/win/thread: Use new SetThreadDescription to set thread names.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@86171 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Runtime/r3/win/thread-win.cpp
+++ src/VBox/Runtime/r3/win/thread-win.cpp
@@ -144,7 +144,6 @@ DECLHIDDEN(void) rtThreadNativeInformDebugger(PRTTHREADINT pThread)
  */
 static void rtThreadWinTellDebuggerThreadName(uint32_t idThread, const char *pszName)
 {
-    /// @todo check out SetThreadDescription from W10-1607
     struct
     {
         uint32_t    uType;
