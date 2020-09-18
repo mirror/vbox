0ed286881ec68b36d5cd32fae0b289a3f80943ea
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2020-09-18 10:01:32 +0000

    Debugger/DBGCRemoteKd: Start supporting 32bit guests [build fix]
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@86167 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Debugger/DBGCRemoteKd.cpp
+++ src/VBox/Debugger/DBGCRemoteKd.cpp
@@ -2648,7 +2648,7 @@ static int dbgcKdCtxPktManipulate64WriteBkpt(PKDCTX pThis, PCKDPACKETMANIPULATE6
     WriteBkpt64.u64PtrBkpt = pPktManip->u.WriteBkpt.u64PtrBkpt;
 
     DBGFADDRESS BpAddr;
-    DBGFR3AddrFromFlat(pThis->Dbgc.pUVM, &BpAddr, KD_PTR_GET(pPktManip->u.WriteBkpt.u64PtrBkpt));
+    DBGFR3AddrFromFlat(pThis->Dbgc.pUVM, &BpAddr, KD_PTR_GET(pThis, pPktManip->u.WriteBkpt.u64PtrBkpt));
     int rc = DBGFR3BpSetInt3(pThis->Dbgc.pUVM, pThis->Dbgc.idCpu, &BpAddr,
                              1 /*iHitTrigger*/, UINT64_MAX /*iHitDisable*/, &WriteBkpt64.u32HndBkpt);
     if (RT_FAILURE(rc))
