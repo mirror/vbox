adcaad92900942432510d300d97e0b6a91a2027a
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2020-09-24 10:06:36 +0000

    xpcom/xptcall: Implemented XPTC_InvokeByIndex in assembly (YASM) as the original mozilla code is making too many assumptions about how the compiler will compile the code. This already broke with gcc 8.2 and asan (bugref:8019) and now Clang breaks it (bugref:9790) when stack is needed for passing parameters. Code has received minimal testing, the floating point stuff isn't tested as we don't use that (I hope). [file header] bugref:9790
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@86258 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/libs/xpcom18a4/xpcom/reflect/xptcall/src/md/unix/xptcinvoke_amd64_vbox.asm
+++ src/libs/xpcom18a4/xpcom/reflect/xptcall/src/md/unix/xptcinvoke_amd64_vbox.asm
@@ -1,3 +1,24 @@
+; $Id$
+;; @file
+; XPCOM - Implementation XPTC_InvokeByIndex in assembly.
+;
+; This solves the problem of Clang and gcc (sometimes) not playing along with
+; the alloca() based trick to pass stack parameters.  We first had trouble
+; when enabling asan with gcc 8.2, then Clang 11 on mac had similar issues
+; (at least for profile builds).
+;
+
+;
+; Copyright (C) 2020 Oracle Corporation
+;
+; This file is part of VirtualBox Open Source Edition (OSE), as
+; available from http://www.virtualbox.org. This file is free software;
+; you can redistribute it and/or modify it under the terms of the GNU
+; General Public License (GPL) as published by the Free Software
+; Foundation, in version 2 as it comes in the "COPYING" file of the
+; VirtualBox OSE distribution. VirtualBox OSE is distributed in the
+; hope that it will be useful, but WITHOUT ANY WARRANTY of any kind.
+;
 
 
 ;*********************************************************************************************************************************
