e737d1760b10d2315161d983a9da3ae163ef740a
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2020-09-25 20:36:35 +0000

    xpcom/xptcall: r12 & rbx corruption fix when there are five parameters or less (regression of r140554).
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@86293 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/libs/xpcom18a4/xpcom/reflect/xptcall/src/md/unix/xptcinvoke_amd64_vbox.asm
+++ src/libs/xpcom18a4/xpcom/reflect/xptcall/src/md/unix/xptcinvoke_amd64_vbox.asm
@@ -130,7 +130,7 @@ BEGINPROC_EXPORTED XPTC_InvokeByIndex
         call    r11                     ; note! stack is aligned here.
 
 .fast_return:
-        lea     rsp, [rbp - 8*3]
+        lea     rsp, [rbp - 8*2]
         pop     r12
         pop     rbx
         leave
