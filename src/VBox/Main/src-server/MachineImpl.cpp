ab59232844817f1ed2bde4d5631293cd52bcfa87
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2020-10-08 17:00:21 +0000

    VBoxSVC/MachineImpl.cpp: Fixed use-after-free problem in Machine::i_prepareSaveSettings where a Utf8Str::printf was fed its old value as one of the format argument. bugref:9841
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@86498 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Main/src-server/MachineImpl.cpp
+++ src/VBox/Main/src-server/MachineImpl.cpp
@@ -9604,10 +9604,11 @@ HRESULT Machine::i_prepareSaveSettings(bool *pfNeedsGlobalSaveSettings)
             if (newConfigFile != configFile)
             {
                 /* get the path to old settings file in renamed directory */
+                Assert(mData->m_strConfigFileFull == configFile);
                 configFile.printf("%s%c%s",
                                   newConfigDir.c_str(),
                                   RTPATH_DELIMITER,
-                                  RTPathFilename(configFile.c_str()));
+                                  RTPathFilename(mData->m_strConfigFileFull.c_str()));
                 if (!fSettingsFileIsNew)
                 {
                     /* perform real rename only if the machine is not new */
