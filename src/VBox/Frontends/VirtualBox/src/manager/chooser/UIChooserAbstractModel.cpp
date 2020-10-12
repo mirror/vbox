63144f218e84ed3fbb9b26db0c33b1c6d47b1724
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2020-10-12 10:20:49 +0000

    FE/Qt: bugref:9653: Enable Cloud VMs listing for trunk.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@86539 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Frontends/VirtualBox/src/manager/chooser/UIChooserAbstractModel.cpp
+++ src/VBox/Frontends/VirtualBox/src/manager/chooser/UIChooserAbstractModel.cpp
@@ -797,7 +797,6 @@ void UIChooserAbstractModel::prepare()
 
 void UIChooserAbstractModel::prepareConnections()
 {
-#ifdef VBOX_GUI_WITH_CLOUD_VMS
     /* Cloud thread-pool connections: */
     connect(uiCommon().threadPoolCloud(), &UIThreadPool::sigTaskComplete,
             this, &UIChooserAbstractModel::sltHandleCloudListMachinesTaskComplete);
@@ -805,7 +804,6 @@ void UIChooserAbstractModel::prepareConnections()
      * this is to be replaced by corresponding Main API event later. */
     connect(&uiCommon(), &UICommon::sigCloudMachineRegistered,
             this, &UIChooserAbstractModel::sltCloudMachineRegistered);
-#endif
 
     /* Setup global connections: */
     connect(gVBoxEvents, &UIVirtualBoxEventHandler::sigMachineStateChange,
@@ -887,7 +885,6 @@ void UIChooserAbstractModel::reloadLocalTree()
 
 void UIChooserAbstractModel::reloadCloudTree()
 {
-#ifdef VBOX_GUI_WITH_CLOUD_VMS
     LogRelFlow(("UIChooserAbstractModel: Loading cloud providers/profiles...\n"));
 
     /* Wipe out existing cloud providers first.
@@ -987,7 +984,6 @@ void UIChooserAbstractModel::reloadCloudTree()
     }
 
     LogRelFlow(("UIChooserAbstractModel: Cloud providers/profiles loaded.\n"));
-#endif /* VBOX_GUI_WITH_CLOUD_VMS */
 }
 
 void UIChooserAbstractModel::addLocalMachineIntoTheTree(const CMachine &comMachine,
