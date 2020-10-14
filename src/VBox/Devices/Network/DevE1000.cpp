361ffc7a5ed52d725c3ad0589c0930be5a206118
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2020-10-14 12:47:10 +0000

    Dev/E1000: (bugref:9829) Fixed division-by-zero issues
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@86574 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Devices/Network/DevE1000.cpp
+++ src/VBox/Devices/Network/DevE1000.cpp
@@ -3947,6 +3947,9 @@ DECLINLINE(unsigned) e1kTxDLoadMore(PPDMDEVINS pDevIns, PE1KSTATE pThis)
     AssertCompile(E1K_TXD_CACHE_SIZE < (256 * sizeof(pThis->nTxDFetched)));
     unsigned nDescsToFetch      = RT_MIN(nDescsAvailable, E1K_TXD_CACHE_SIZE - pThis->nTxDFetched);
     unsigned nDescsTotal        = TDLEN / sizeof(E1KTXDESC);
+    Assert(nDescsTotal != 0);
+    if (nDescsTotal == 0)
+        return 0;
     unsigned nFirstNotLoaded    = (TDH + pThis->nTxDFetched) % nDescsTotal;
     unsigned nDescsInSingleRead = RT_MIN(nDescsToFetch, nDescsTotal - nFirstNotLoaded);
     E1kLog3(("%s e1kTxDLoadMore: nDescsAvailable=%u nDescsToFetch=%u nDescsTotal=%u nFirstNotLoaded=0x%x nDescsInSingleRead=%u\n",
@@ -5462,7 +5465,10 @@ static void e1kDumpTxDCache(PPDMDEVINS pDevIns, PE1KSTATE pThis)
     {
         if (i == pThis->iTxDCurrent)
             LogRel(("E1000: >>> "));
-        LogRel(("E1000: %RGp: %R[e1ktxd]\n", e1kDescAddr(TDBAH, TDBAL, tdh++ % cDescs), &pThis->aTxDescriptors[i]));
+        if (cDescs)
+            LogRel(("E1000: %RGp: %R[e1ktxd]\n", e1kDescAddr(TDBAH, TDBAL, tdh++ % cDescs), &pThis->aTxDescriptors[i]));
+        else
+            LogRel(("E1000: <lost>: %R[e1ktxd]\n", &pThis->aTxDescriptors[i]));
     }
 }
 
@@ -7344,9 +7350,13 @@ static DECLCALLBACK(void) e1kInfo(PPDMDEVINS pDevIns, PCDBGFINFOHLP pHlp, const 
     {
         if (i == pThis->iRxDCurrent)
             pHlp->pfnPrintf(pHlp, ">>> ");
-        pHlp->pfnPrintf(pHlp, "%RGp: %R[e1krxd]\n",
-                        e1kDescAddr(RDBAH, RDBAL, rdh++ % cDescs),
-                        &pThis->aRxDescriptors[i]);
+        if (cDescs)
+            pHlp->pfnPrintf(pHlp, "%RGp: %R[e1krxd]\n",
+                            e1kDescAddr(RDBAH, RDBAL, rdh++ % cDescs),
+                            &pThis->aRxDescriptors[i]);
+        else
+            pHlp->pfnPrintf(pHlp, "<lost>: %R[e1krxd]\n",
+                            &pThis->aRxDescriptors[i]);
     }
 #endif /* E1K_WITH_RXD_CACHE */
 
@@ -7373,9 +7383,13 @@ static DECLCALLBACK(void) e1kInfo(PPDMDEVINS pDevIns, PCDBGFINFOHLP pHlp, const 
     {
         if (i == pThis->iTxDCurrent)
             pHlp->pfnPrintf(pHlp, ">>> ");
-        pHlp->pfnPrintf(pHlp, "%RGp: %R[e1ktxd]\n",
-                        e1kDescAddr(TDBAH, TDBAL, tdh++ % cDescs),
-                        &pThis->aTxDescriptors[i]);
+        if (cDescs)
+            pHlp->pfnPrintf(pHlp, "%RGp: %R[e1ktxd]\n",
+                            e1kDescAddr(TDBAH, TDBAL, tdh++ % cDescs),
+                            &pThis->aTxDescriptors[i]);
+        else
+            pHlp->pfnPrintf(pHlp, "<lost>: %R[e1ktxd]\n",
+                            &pThis->aTxDescriptors[i]);
     }
 #endif /* E1K_WITH_TXD_CACHE */
 
