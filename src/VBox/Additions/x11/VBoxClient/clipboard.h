489db634f260fba7aab61d4ebca7a3e66533e89f
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2020-11-24 13:46:03 +0000

    Shared Clipboard/Transfers: Initial working PoC for host -> guest transfers [build fix]. bugref:9437
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@86968 cfe28804-0f27-0410-a406-dd0f0b0b656f

---
+++ src/VBox/Additions/x11/VBoxClient/clipboard.h
@@ -0,0 +1,32 @@
+/** $Id$ */
+/** @file
+ * Guest Additions - X11 Shared Clipboard - Main header.
+ */
+
+/*
+ * Copyright (C) 2020 Oracle Corporation
+ *
+ * This file is part of VirtualBox Open Source Edition (OSE), as
+ * available from http://www.virtualbox.org. This file is free software;
+ * you can redistribute it and/or modify it under the terms of the GNU
+ * General Public License (GPL) as published by the Free Software
+ * Foundation, in version 2 as it comes in the "COPYING" file of the
+ * VirtualBox OSE distribution. VirtualBox OSE is distributed in the
+ * hope that it will be useful, but WITHOUT ANY WARRANTY of any kind.
+ */
+
+/**
+ * Struct keeping a Shared Clipboard context.
+ */
+struct SHCLCONTEXT
+{
+    /** Client command context */
+    VBGLR3SHCLCMDCTX CmdCtx;
+#ifdef VBOX_WITH_SHARED_CLIPBOARD_TRANSFERS
+    /** Associated transfer data. */
+    SHCLTRANSFERCTX  TransferCtx;
+#endif
+    /** X11 clipboard context. */
+    SHCLX11CTX       X11;
+};
+
