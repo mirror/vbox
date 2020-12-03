7f958bef89c70fc0f6122d541e08a9c3368214fa
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2020-12-03 13:47:23 +0000

    Installer/solaris: Big cleanup of SysV packging, and first cut (so far the result will not be usable, permissions and so on need fixing) of IPS packaging. bugref:5999
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@87035 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Installer/solaris/makepackage.sh
+++ src/VBox/Installer/solaris/makepackage.sh
@@ -18,16 +18,22 @@
 
 #
 # Usage:
-#       makepackage.sh [--hardened] $(PATH_TARGET)/install packagename {$(KBUILD_TARGET_ARCH)|neutral} $(VBOX_SVN_REV) [VBIPackageName]
+#       makepackage.sh [--hardened] [--ips] $(PATH_TARGET)/install packagename {$(KBUILD_TARGET_ARCH)|neutral} $(VBOX_SVN_REV)
 
 
 # Parse options.
 HARDENED=""
-while test $# -ge 1;
+IPS_PACKAGE=""
+PACKAGE_SPEC="prototype"
+while [ $# -ge 1 ];
 do
     case "$1" in
         --hardened)
             HARDENED=1
+            ;;
+        --ips)
+            IPS_PACKAGE=1
+            PACKAGE_SPEC="virtualbox.p5m"
             ;;
     *)
         break
@@ -37,211 +43,253 @@ do
 done
 
 if [ -z "$4" ]; then
-    echo "Usage: $0 installdir packagename x86|amd64 svnrev [VBIPackage]"
+    echo "Usage: $0 installdir packagename x86|amd64 svnrev"
     echo "-- packagename must not have any extension (e.g. VirtualBox-SunOS-amd64-r28899)"
     exit 1
 fi
 
-PKG_BASE_DIR=$1
-VBOX_INSTALLED_DIR=$1/opt/VirtualBox
-VBOX_PKGFILE=$2.pkg
-VBOX_ARCHIVE=$2.tar.gz
+PKG_BASE_DIR="$1"
+PACKAGE_SPEC="$PKG_BASE_DIR/$PACKAGE_SPEC"
+VBOX_INSTALLED_DIR=/opt/VirtualBox
+if [ -n "$IPS_PACKAGE" ]; then
+    VBOX_PKGFILE="$2".p5p
+else
+    VBOX_PKGFILE="$2".pkg
+fi
 # VBOX_PKG_ARCH is currently unused.
-VBOX_PKG_ARCH=$3
-VBOX_SVN_REV=$4
+VBOX_PKG_ARCH="$3"
+VBOX_SVN_REV="$4"
 
-VBOX_PKGNAME=SUNWvbox
-VBOX_GGREP=/usr/sfw/bin/ggrep
-VBOX_AWK=/usr/bin/awk
-#VBOX_GTAR=/usr/sfw/bin/gtar
+if [ -n "$IPS_PACKAGE" ] ; then
+    VBOX_PKGNAME=system/virtualbox
+else
+    VBOX_PKGNAME=SUNWvbox
+fi
+# need GNU grep because Solaris egrep does not support word matching
+VBOX_GGREP=/usr/gnu/bin/grep
+# need dynamic regex support which isn't available in S11 /usr/bin/awk
+VBOX_AWK=/usr/xpg4/bin/awk
 
 # check for GNU grep we use which might not ship with all Solaris
-if test ! -f "$VBOX_GGREP" && test ! -h "$VBOX_GGREP"; then
+if [ ! -f "$VBOX_GGREP" ] && [ ! -h "$VBOX_GGREP" ]; then
     echo "## GNU grep not found in $VBOX_GGREP."
     exit 1
 fi
 
-# check for GNU tar we use which might not ship with all Solaris
-#if test ! -f "$VBOX_GTAR" && test ! -h "$VBOX_GTAR"; then
-#    echo "## GNU tar not found in $VBOX_GTAR."
-#    exit 1
-#fi
-
 # bail out on non-zero exit status
 set -e
 
-# Fixup filelist using awk, the parameters must be in awk syntax
-# params: filename condition action
-filelist_fixup()
+if [ -n "$IPS_PACKAGE" ]; then
+
+package_spec_create()
 {
-  "$VBOX_AWK" 'NF == 6 && '"$2"' { '"$3"' } { print }' "$1" > "tmp-$1"
-  mv -f "tmp-$1" "$1"
+    > "$PACKAGE_SPEC"
 }
 
-dirlist_fixup()
+package_spec_append_info()
 {
-  "$VBOX_AWK" 'NF == 6 && $1 == "d" && '"$2"' { '"$3"' } { print }' "$1" > "tmp-$1"
-  mv -f "tmp-$1" "$1"
+    : # provided by vbox-ips.mog
 }
 
-hardlink_fixup()
+package_spec_append_content()
 {
-  "$VBOX_AWK" 'NF == 3 && $1=="l" && '"$2"' { '"$3"' } { print }' "$1" > "tmp-$1"
-  mv -f "tmp-$1" "$1"
+    pkgsend generate "$1" | pkgfmt >> "$PACKAGE_SPEC"
 }
 
-symlink_fixup()
+package_spec_append_hardlink()
 {
-  "$VBOX_AWK" 'NF == 3 && $1=="s" && '"$2"' { '"$3"' } { print }' "$1" > "tmp-$1"
-  mv -f "tmp-$1" "$1"
-}
-
-create_hardlink()
-{
-    if test -f "$VBOX_INSTALLED_DIR/amd64/$1" || test -f "$VBOX_INSTALLED_DIR/i386/$1"; then
-        ln -f ./VBoxISAExec "$VBOX_INSTALLED_DIR/$1"
+    if [ -f "$3$4/amd64/$2" -o -f "$3$4/i386/$2" ]; then
+        echo "hardlink path=$4/$2 target=$1" >> "$PACKAGE_SPEC"
     fi
 }
 
-# Prepare file list
-cd "$PKG_BASE_DIR"
-echo 'i pkginfo=./vbox.pkginfo' > prototype
-echo 'i checkinstall=./checkinstall.sh' >> prototype
-echo 'i postinstall=./postinstall.sh' >> prototype
-echo 'i preremove=./preremove.sh' >> prototype
-echo 'i space=./vbox.space' >> prototype
-if test -f "./vbox.copyright"; then
-    echo 'i copyright=./vbox.copyright' >> prototype
+package_spec_fixup_content()
+{
+    :
+}
+
+package_create()
+{
+    pkgmogrify -DVBOX_PKGNAME="$VBOX_PKGNAME" "$PACKAGE_SPEC" vbox-ips.mog | pkgfmt > "$PACKAGE_SPEC.1"
+
+    pkgdepend generate -m -d "$1" "$PACKAGE_SPEC.1" | pkgfmt > "$PACKAGE_SPEC.2"
+
+    pkgdepend resolve -m -v "$PACKAGE_SPEC.2"
+
+    pkglint "$PACKAGE_SPEC.2.res"
+
+    rm -rf "$1/vbox-repo"
+    pkgrepo create "$1/vbox-repo"
+    pkgrepo -s "$1/vbox-repo" set publisher/prefix=virtualbox
+
+    # Create package in local file repository
+    pkgsend -s "$1/vbox-repo" publish -d "$1" "$PACKAGE_SPEC.2.res"
+
+    pkgrepo -s "$1/vbox-repo" info
+    pkgrepo -s "$1/vbox-repo" list
+
+    # Convert into package archive
+    pkgrecv -s "$1/vbox-repo" -a -d "$2" -m latest "$3"
+    #rm -rf "$1/vbox-repo"
+}
+
+else
+
+package_spec_create()
+{
+    > "$PACKAGE_SPEC"
+}
+
+package_spec_append_info()
+{
+    echo 'i pkginfo=vbox.pkginfo' >> "$PACKAGE_SPEC"
+    echo 'i checkinstall=checkinstall.sh' >> "$PACKAGE_SPEC"
+    echo 'i postinstall=postinstall.sh' >> "$PACKAGE_SPEC"
+    echo 'i preremove=preremove.sh' >> "$PACKAGE_SPEC"
+    echo 'i space=vbox.space' >> "$PACKAGE_SPEC"
+}
+
+# Our package is a non-relocatable package.
+#
+# pkgadd will take care of "relocating" them when they are used for remote installations using
+# $PKG_INSTALL_ROOT and not $BASEDIR. Seems this little subtlety led to it's own page:
+# https://docs.oracle.com/cd/E19253-01/820-4042/package-2/index.html
+
+package_spec_append_content()
+{
+    cd "$1"
+    # Exclude directories to not cause install-time conflicts with existing system directories
+    find . ! -type d | "$VBOX_GGREP" -v -wE 'prototype|makepackage\.sh|vbox\.pkginfo|postinstall\.sh|checkinstall\.sh|preremove\.sh|vbox\.space|virtualbox\.p5m.*' | LC_COLLATE=C sort | pkgproto >> "$PACKAGE_SPEC"
+    cd -
+    "$VBOX_AWK" 'NF == 3 && $1 == "s" && $2 == "none" { $3="/"$3 } { print }' "$PACKAGE_SPEC" > "$PACKAGE_SPEC.tmp"
+    mv -f "$PACKAGE_SPEC.tmp" "$PACKAGE_SPEC"
+    "$VBOX_AWK" 'NF == 6 && ($1 == "f" || $1 == "l") && ($2 == "none" || $2 == "manifest") { $3="/"$3 } { print }' "$PACKAGE_SPEC" > "$PACKAGE_SPEC.tmp"
+    mv -f "$PACKAGE_SPEC.tmp" "$PACKAGE_SPEC"
+
+    cd "$1"
+    # Include opt/VirtualBox and subdirectories as we want uninstall to clean up directory structure.
+    # Include var/svc for manifest class action script does not create them.
+    find . -type d | "$VBOX_GGREP" -E 'opt/VirtualBox|var/svc/manifest/application/virtualbox' | LC_COLLATE=C sort | pkgproto >> "$PACKAGE_SPEC"
+    cd -
+    "$VBOX_AWK" 'NF == 6 && $1 == "d" && $2 == "none" { $3="/"$3 } { print }' "$PACKAGE_SPEC" > "$PACKAGE_SPEC.tmp"
+    mv -f "$PACKAGE_SPEC.tmp" "$PACKAGE_SPEC"
+}
+
+package_spec_append_hardlink()
+{
+    if [ -f "$3$4/amd64/$2" -o -f "$3$4/i386/$2" ]; then
+        echo "l none $4/$2=$1" >> "$PACKAGE_SPEC"
+    fi
+}
+
+# Fixup filelist using awk, the parameters must be in awk syntax
+# params: filename condition action
+package_spec_fixup_filelist()
+{
+    "$VBOX_AWK" 'NF == 6 && '"$1"' { '"$2"' } { print }' "$PACKAGE_SPEC" > "$PACKAGE_SPEC.tmp"
+    mv -f "$PACKAGE_SPEC.tmp" "$PACKAGE_SPEC"
+}
+
+package_spec_fixup_dirlist()
+{
+    "$VBOX_AWK" 'NF == 6 && $1 == "d" && '"$1"' { '"$2"' } { print }' "$PACKAGE_SPEC" > "$PACKAGE_SPEC.tmp"
+    mv -f "$PACKAGE_SPEC.tmp" "$PACKAGE_SPEC"
+}
+
+package_spec_fixup_content()
+{
+    # fix up file permissions (owner/group)
+    # don't grok for class-specific files (like sed, if any)
+    package_spec_fixup_filelist '$2 == "none"'                                                                  '$5 = "root"; $6 = "bin"'
+
+    # HostDriver vboxdrv
+    package_spec_fixup_filelist '$3 == "/platform/i86pc/kernel/drv/vboxdrv"'                                    '$6 = "sys"'
+    package_spec_fixup_filelist '$3 == "/platform/i86pc/kernel/drv/amd64/vboxdrv"'                              '$6 = "sys"'
+
+    # NetFilter vboxflt
+    package_spec_fixup_filelist '$3 == "/platform/i86pc/kernel/drv/vboxflt"'                                    '$6 = "sys"'
+    package_spec_fixup_filelist '$3 == "/platform/i86pc/kernel/drv/amd64/vboxflt"'                              '$6 = "sys"'
+
+    # NetFilter vboxbow
+    package_spec_fixup_filelist '$3 == "/platform/i86pc/kernel/drv/vboxbow"'                                    '$6 = "sys"'
+    package_spec_fixup_filelist '$3 == "/platform/i86pc/kernel/drv/amd64/vboxbow"'                              '$6 = "sys"'
+
+    # NetAdapter vboxnet
+    package_spec_fixup_filelist '$3 == "/platform/i86pc/kernel/drv/vboxnet"'                                    '$6 = "sys"'
+    package_spec_fixup_filelist '$3 == "/platform/i86pc/kernel/drv/amd64/vboxnet"'                              '$6 = "sys"'
+
+    # USBMonitor vboxusbmon
+    package_spec_fixup_filelist '$3 == "/platform/i86pc/kernel/drv/vboxusbmon"'                                 '$6 = "sys"'
+    package_spec_fixup_filelist '$3 == "/platform/i86pc/kernel/drv/amd64/vboxusbmon"'                           '$6 = "sys"'
+
+    # USB Client vboxusb
+    package_spec_fixup_filelist '$3 == "/platform/i86pc/kernel/drv/vboxusb"'                                    '$6 = "sys"'
+    package_spec_fixup_filelist '$3 == "/platform/i86pc/kernel/drv/amd64/vboxusb"'                              '$6 = "sys"'
+
+    # Manifest class action scripts
+    package_spec_fixup_filelist '$3 == "/var/svc/manifest/application/virtualbox/virtualbox-webservice.xml"'    '$2 = "manifest";$6 = "sys"'
+    package_spec_fixup_filelist '$3 == "/var/svc/manifest/application/virtualbox/virtualbox-balloonctrl.xml"'   '$2 = "manifest";$6 = "sys"'
+    package_spec_fixup_filelist '$3 == "/var/svc/manifest/application/virtualbox/virtualbox-zoneaccess.xml"'    '$2 = "manifest";$6 = "sys"'
+
+    # Use 'root' as group so as to match attributes with the previous installation and prevent a conflict. Otherwise pkgadd bails out thinking
+    # we're violating directory attributes of another (non existing) package
+    package_spec_fixup_dirlist '$3 == "/var/svc/manifest/application/virtualbox"'                               '$6 = "root"'
+
+    # Hardening requires some executables to be marked setuid.
+    if [ -n "$HARDENED" ]; then
+        package_spec_fixup_filelist '(   $3 == "/opt/VirtualBox/amd64/VirtualBoxVM" \
+                                      || $3 == "/opt/VirtualBox/amd64/VBoxHeadless" \
+                                      || $3 == "/opt/VirtualBox/amd64/VBoxSDL" \
+                                      || $3 == "/opt/VirtualBox/i386/VirtualBox" \
+                                      || $3 == "/opt/VirtualBox/i386/VBoxHeadless" \
+                                      || $3 == "/opt/VirtualBox/i386/VBoxSDL" )'                                '$4 = "4755"'
+    fi
+
+    # Other executables that need setuid root (hardened or otherwise)
+    package_spec_fixup_filelist '(   $3 == "/opt/VirtualBox/amd64/VBoxNetAdpCtl" \
+                                  || $3 == "/opt/VirtualBox/i386/VBoxNetAdpCtl" \
+                                  || $3 == "/opt/VirtualBox/amd64/VBoxNetDHCP" \
+                                  || $3 == "/opt/VirtualBox/i386/VBoxNetDHCP" \
+                                  || $3 == "/opt/VirtualBox/amd64/VBoxNetNAT" \
+                                  || $3 == "/opt/VirtualBox/i386/VBoxNetNAT" )'                                 '$4 = "4755"'
+
+    echo " --- start of $PACKAGE_SPEC  ---"
+    cat "$PACKAGE_SPEC"
+    echo " --- end of $PACKAGE_SPEC --- "
+}
+
+package_create()
+{
+    # Create the package instance
+    pkgmk -o -f "$PACKAGE_SPEC" -r "$1"
+
+    # Translate into package datastream
+    pkgtrans -s -o /var/spool/pkg "$1/$2" "$3"
+
+    rm -rf "/var/spool/pkg/$2"
+}
+
 fi
 
-# Create relative hardlinks for executables to either the 32-bit or 64-bit subfolders
-cd "$VBOX_INSTALLED_DIR"
-create_hardlink VBoxManage
-create_hardlink VBoxSDL
-create_hardlink VBoxAutostart
-create_hardlink vboxwebsrv
-create_hardlink VBoxZoneAccess
-create_hardlink VBoxSVC
-create_hardlink VBoxBugReport
-create_hardlink VBoxBalloonCtrl
-create_hardlink VBoxTestOGL
-create_hardlink VirtualBox
-create_hardlink VirtualBoxVM
-create_hardlink vbox-img
-create_hardlink VBoxHeadless
 
-# Exclude directories to not cause install-time conflicts with existing system directories
-cd "$PKG_BASE_DIR"
-find . ! -type d | $VBOX_GGREP -v -wE 'prototype|makepackage.sh|vbox.pkginfo|postinstall.sh|checkinstall.sh|preremove.sh|ReadMe.txt|vbox.space|vbox.depend|vbox.copyright|VirtualBoxKern' | pkgproto >> prototype
+# Prepare package spec
+package_spec_create
 
-# Include opt/VirtualBox and subdirectories as we want uninstall to clean up directory structure.
-# Include var/svc for manifest class action script does not create them.
-find . -type d | $VBOX_GGREP -E 'opt/VirtualBox|var/svc/manifest/application/virtualbox' | pkgproto >> prototype
+# Metadata
+package_spec_append_info "$PKG_BASE_DIR"
 
-# fix up file permissions (owner/group)
-# don't grok for class-specific files (like sed, if any)
-filelist_fixup prototype '$2 == "none"'                                                                '$5 = "root"; $6 = "bin"'
+# File and direcory list
+package_spec_append_content "$PKG_BASE_DIR"
 
-# don't include autoresponse and LICENSE from the base folder into / of
-# the package, it goes into .tar.gz and another one already exists
-# in /opt/VirtualBox
-sed '/f none autoresponse/d' prototype > prototype2
-mv -f prototype2 prototype
-sed '/f none LICENSE/d' prototype > prototype2
-mv -f prototype2 prototype
+# Add hardlinks for executables to launch the 32-bit or 64-bit executable
+for f in VBoxManage VBoxSDL VBoxAutostart vboxwebsrv VBoxZoneAccess VBoxSVC VBoxBugReport VBoxBalloonCtrl VBoxTestOGL VirtualBox VirtualBoxVM vbox-img VBoxHeadless; do
+    package_spec_append_hardlink VBoxISAExec    $f  "$PKG_BASE_DIR" "$VBOX_INSTALLED_DIR"
+done
 
-# HostDriver vboxdrv
-filelist_fixup prototype '$3 == "platform/i86pc/kernel/drv/vboxdrv"'                                   '$6 = "sys"'
-filelist_fixup prototype '$3 == "platform/i86pc/kernel/drv/amd64/vboxdrv"'                             '$6 = "sys"'
+package_spec_fixup_content
 
-# NetFilter vboxflt
-filelist_fixup prototype '$3 == "platform/i86pc/kernel/drv/vboxflt"'                                   '$6 = "sys"'
-filelist_fixup prototype '$3 == "platform/i86pc/kernel/drv/amd64/vboxflt"'                             '$6 = "sys"'
-
-# NetFilter vboxbow
-filelist_fixup prototype '$3 == "platform/i86pc/kernel/drv/vboxbow"'                                   '$6 = "sys"'
-filelist_fixup prototype '$3 == "platform/i86pc/kernel/drv/amd64/vboxbow"'                             '$6 = "sys"'
-
-# NetAdapter vboxnet
-filelist_fixup prototype '$3 == "platform/i86pc/kernel/drv/vboxnet"'                                   '$6 = "sys"'
-filelist_fixup prototype '$3 == "platform/i86pc/kernel/drv/amd64/vboxnet"'                             '$6 = "sys"'
-
-# USBMonitor vboxusbmon
-filelist_fixup prototype '$3 == "platform/i86pc/kernel/drv/vboxusbmon"'                                '$6 = "sys"'
-filelist_fixup prototype '$3 == "platform/i86pc/kernel/drv/amd64/vboxusbmon"'                          '$6 = "sys"'
-
-# USB Client vboxusb
-filelist_fixup prototype '$3 == "platform/i86pc/kernel/drv/vboxusb"'                                   '$6 = "sys"'
-filelist_fixup prototype '$3 == "platform/i86pc/kernel/drv/amd64/vboxusb"'                             '$6 = "sys"'
-
-# Manifest class action scripts
-filelist_fixup prototype '$3 == "var/svc/manifest/application/virtualbox/virtualbox-webservice.xml"'    '$2 = "manifest";$6 = "sys"'
-filelist_fixup prototype '$3 == "var/svc/manifest/application/virtualbox/virtualbox-balloonctrl.xml"'   '$2 = "manifest";$6 = "sys"'
-filelist_fixup prototype '$3 == "var/svc/manifest/application/virtualbox/virtualbox-zoneaccess.xml"'    '$2 = "manifest";$6 = "sys"'
-
-# Use 'root' as group so as to match attributes with the previous installation and prevent a conflict. Otherwise pkgadd bails out thinking
-# we're violating directory attributes of another (non existing) package
-dirlist_fixup prototype  '$3 == "var/svc/manifest/application/virtualbox"'                              '$6 = "root"'
-
-# Hardening requires some executables to be marked setuid.
-if test -n "$HARDENED"; then
-    $VBOX_AWK 'NF == 6 \
-        && (    $3 == "opt/VirtualBox/amd64/VirtualBoxVM" \
-            ||  $3 == "opt/VirtualBox/amd64/VBoxHeadless" \
-            ||  $3 == "opt/VirtualBox/amd64/VBoxSDL" \
-            ||  $3 == "opt/VirtualBox/i386/VirtualBox" \
-            ||  $3 == "opt/VirtualBox/i386/VBoxHeadless" \
-            ||  $3 == "opt/VirtualBox/i386/VBoxSDL" \
-            ) \
-       { $4 = "4755" } { print }' prototype > prototype2
-    mv -f prototype2 prototype
-fi
-
-# Other executables that need setuid root (hardened or otherwise)
-$VBOX_AWK 'NF == 6 \
-    && (    $3 == "opt/VirtualBox/amd64/VBoxNetAdpCtl" \
-        ||  $3 == "opt/VirtualBox/i386/VBoxNetAdpCtl" \
-        ||  $3 == "opt/VirtualBox/amd64/VBoxNetDHCP" \
-        ||  $3 == "opt/VirtualBox/i386/VBoxNetDHCP" \
-        ||  $3 == "opt/VirtualBox/amd64/VBoxNetNAT" \
-        ||  $3 == "opt/VirtualBox/i386/VBoxNetNAT" \
-        ) \
-   { $4 = "4755" } { print }' prototype > prototype2
-mv -f prototype2 prototype
-
-# Our package is a non-relocatable package. pkgadd will take care of "relocating" them when they are used for
-# remote installations using $PKG_INSTALL_ROOT and not $BASEDIR. Seems this little subtlety led to it's own page:
-# http://docs.sun.com/app/docs/doc/820-4042/package-2?a=view
-filelist_fixup prototype  '$2 == "none"'                              '$3="/"$3'
-filelist_fixup prototype  '$2 == "manifest"'                          '$3="/"$3'
-symlink_fixup  prototype  '$2 == "none"'                              '$3="/"$3'
-hardlink_fixup prototype  '$2 == "none"'                              '$3="/"$3'
-
-echo " --- start of prototype  ---"
-cat prototype
-echo " --- end of prototype --- "
-
-# Explicitly set timestamp to shutup warning
-VBOXPKG_TIMESTAMP=vbox`date '+%Y%m%d%H%M%S'`_r$VBOX_SVN_REV
-
-# Create the package instance
-pkgmk -p $VBOXPKG_TIMESTAMP -o -r .
-
-# Translate into package datastream
-pkgtrans -s -o /var/spool/pkg "`pwd`/$VBOX_PKGFILE" "$VBOX_PKGNAME"
-
-# $5 if exists would contain the path to the VBI package to include in the .tar.gz
-#if [ -f LICENSE ]; then
-#    VBOX_LICENSEFILE=LICENSE
-#fi
-#if test -f "$5"; then
-#    $VBOX_GTAR zcvf "$VBOX_ARCHIVE" $VBOX_LICENSEFILE "$VBOX_PKGFILE" "$5" autoresponse ReadMe.txt
-#else
-#    $VBOX_GTAR zcvf "$VBOX_ARCHIVE" $VBOX_LICENSEFILE "$VBOX_PKGFILE" autoresponse ReadMe.txt
-#fi
+package_create "$PKG_BASE_DIR" "$VBOX_PKGFILE" "$VBOX_PKGNAME" "$VBOX_SVN_REV"
 
 echo "## Package file created successfully!"
-rm -rf "/var/spool/pkg/$VBOX_PKGNAME"
 
 exit $?
-
