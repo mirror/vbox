2650fa8b884845290e82f1c9255d01f384a0fdf9
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2020-12-29 13:08:16 +0000

    VMM/DBGFR3FlowTrace: First commit of a new DBGF framework to collect guest state information during execution, allowing visualization of guest execution flow, bugref:8650 [scm]
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@87138 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- include/VBox/vmm/dbgfflowtrace.h
+++ include/VBox/vmm/dbgfflowtrace.h
@@ -23,8 +23,11 @@
  * terms and conditions of either the GPL or the CDDL or both.
  */
 
-#ifndef ___VBox_vmm_dbgfflowtrace_h
-#define ___VBox_vmm_dbgfflowtrace_h
+#ifndef VBOX_INCLUDED_vmm_dbgfflowtrace_h
+#define VBOX_INCLUDED_vmm_dbgfflowtrace_h
+#ifndef RT_WITHOUT_PRAGMA_ONCE
+# pragma once
+#endif
 
 #include <VBox/types.h>
 #include <VBox/vmm/dbgf.h>
