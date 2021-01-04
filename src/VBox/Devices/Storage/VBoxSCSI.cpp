2723f55833444f748b4ec24daef28b1fd63c709b
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2021-01-04 13:20:13 +0000

    Devices/VBoxSCSI: Check against the correct limit, bugref:9897
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@87165 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Devices/Storage/VBoxSCSI.cpp
+++ src/VBox/Devices/Storage/VBoxSCSI.cpp
@@ -425,11 +425,11 @@ int vboxscsiReadString(PPDMDEVINS pDevIns, PVBOXSCSI pVBoxSCSI, uint8_t iRegiste
     uint32_t cbTransfer = *pcTransfers * cb;
     if (pVBoxSCSI->cbBufLeft > 0)
     {
-        Assert(cbTransfer <= pVBoxSCSI->cbBuf);
-        if (cbTransfer > pVBoxSCSI->cbBuf)
+        Assert(cbTransfer <= pVBoxSCSI->cbBufLeft);
+        if (cbTransfer > pVBoxSCSI->cbBufLeft)
         {
-            memset(pbDst + pVBoxSCSI->cbBuf, 0xff, cbTransfer - pVBoxSCSI->cbBuf);
-            cbTransfer = pVBoxSCSI->cbBuf;  /* Ignore excess data (not supposed to happen). */
+            memset(pbDst + pVBoxSCSI->cbBuf, 0xff, cbTransfer - pVBoxSCSI->cbBufLeft);
+            cbTransfer = pVBoxSCSI->cbBufLeft;  /* Ignore excess data (not supposed to happen). */
         }
 
         /* Copy the data and adance the buffer position. */
