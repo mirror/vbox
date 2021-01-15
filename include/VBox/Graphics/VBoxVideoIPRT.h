93445d0f0422a842918b479b75df67cb12d284f8
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2021-01-15 11:13:04 +0000

    VBoxVideoIPRT.h: Don't include asm-amd64-x86.h unless we're targeting either of those - causes trouble compiling Main, so someone is including too much. bugref:9898
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@87261 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- include/VBox/Graphics/VBoxVideoIPRT.h
+++ include/VBox/Graphics/VBoxVideoIPRT.h
@@ -42,7 +42,7 @@
 #include <iprt/stdint.h>
 #include <iprt/types.h>
 
-#if !defined VBOX_XPDM_MINIPORT && !defined RT_OS_OS2
+#if !defined VBOX_XPDM_MINIPORT && !defined RT_OS_OS2 && (defined(RT_ARCH_AMD64) || defined(RT_ARCH_X86))
 # include <iprt/asm-amd64-x86.h>
 #endif
 
