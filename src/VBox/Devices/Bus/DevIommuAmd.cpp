9fbc08067e479e190eef92b0c5479e968e3687bc
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2021-01-28 13:26:28 +0000

    AMD IOMMU: bugref:9654 IOTLB cache bits.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@87458 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Devices/Bus/DevIommuAmd.cpp
+++ src/VBox/Devices/Bus/DevIommuAmd.cpp
@@ -627,10 +627,11 @@ static PIOWALKRESULT iommuAmdIotlbLookup(PIOMMU pThis, uint64_t uDomainId, uint6
  * Adds an IOTLB entry corresponding to the given I/O page walk result.
  *
  * @param   pThis           The IOMMU device state.
+ * @param   uDomainId       The domain ID.
  * @param   uIova           The I/O virtual address being accessed.
  * @param   pWalkResult     The I/O page walk result of the access.
  */
-static void iommuAmdIotlbAdd(PIOMMU pThis, uint64_t uIova, PCIOWALKRESULT pWalkResult)
+static void iommuAmdIotlbAdd(PIOMMU pThis, uint16_t uDomainId, uint64_t uIova, PCIOWALKRESULT pWalkResult)
 {
     Assert(!(uIova & X86_PAGE_4K_OFFSET_MASK));
     Assert(pWalkResult);
@@ -661,8 +662,8 @@ static void iommuAmdIotlbAdd(PIOMMU pThis, uint64_t uIova, PCIOWALKRESULT pWalkR
     RT_ZERO(*pIotlbe);
 
     /* Update the entry with the result of the page walk. */
-    pIotlbe->Core.Key     = uIova;
-    pIotlbe->Core.KeyLast = uIova + RT_BIT_64(pWalkResult->cShift) - 1;
+    pIotlbe->Core.Key     = iommuAmdIotlbConstructKey(uDomainId, uIova);
+    pIotlbe->Core.KeyLast = iommuAmdIotlbConstructKey(uDomainId, uIova + RT_BIT_64(pWalkResult->cShift) - 1);
     pIotlbe->WalkResult   = *pWalkResult;
 
     /* Add the entry to the cache. */
