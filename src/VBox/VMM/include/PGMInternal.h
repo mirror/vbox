e0e95d368c9a24bfe2de4286d5bd46174b869ce3
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2021-01-29 17:38:47 +0000

    VMM/PGM: Corrected todo on aGCPhysGstPaePDs & aGstPaePdpeRegs.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@87485 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/VMM/include/PGMInternal.h
+++ src/VBox/VMM/include/PGMInternal.h
@@ -3764,7 +3764,8 @@ typedef struct PGMCPU
     /** The values of the 4 PDPE CPU registers (PAE).
      * @todo Not really maintained by PGM atm, only by VT-x in EPT mode. Should
      *       load on cr3 load and use instead of guest memory version like real
-     *       HW. */
+     *       HW.  We probably should move these to the CPUMCTX and treat them
+     *       like the rest of the register wrt exporting to VT-x and import back. */
     X86PDPE                         aGstPaePdpeRegs[4];
     /** The physical addresses of the monitored guest page directories (PAE). */
     RTGCPHYS                        aGCPhysGstPaePDsMonitored[4];
