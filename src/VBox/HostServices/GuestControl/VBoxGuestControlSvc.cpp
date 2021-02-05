2c16858769fe0dd5f105d2843a57f3e06e5b3549
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2021-02-05 12:55:26 +0000

    VBoxGuestControlSvc: Better assertion text and explanatory comment.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@87624 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/HostServices/GuestControl/VBoxGuestControlSvc.cpp
+++ src/VBox/HostServices/GuestControl/VBoxGuestControlSvc.cpp
@@ -1128,8 +1128,12 @@ int GstCtrlService::clientMakeMeMaster(ClientState *pClient, VBOXHGCMCALLHANDLE 
     ASSERT_GUEST_RETURN(cParms == 0, VERR_WRONG_PARAMETER_COUNT);
 
     uint32_t fRequestor = mpHelpers->pfnGetRequestor(hCall);
+    /* The next assertion triggers upgrading GAs on some linux guests. Problem is that VBoxService is
+       restarted after installation but the kernel module hasn't been reloaded, so things are out
+       of wack.  Just reboot. */
     ASSERT_GUEST_LOGREL_MSG_RETURN(fRequestor != VMMDEV_REQUESTOR_LEGACY,
-                                   ("Outdated VBoxGuest w/o requestor support. Please update!\n"),
+                                   ("Guest is using outdated VBoxGuest w/o requestor support.\n"
+                                    "Please update guest additions (or restart guest if you just did)!\n"),
                                    VERR_VERSION_MISMATCH);
     ASSERT_GUEST_LOGREL_MSG_RETURN(!(fRequestor & VMMDEV_REQUESTOR_USER_DEVICE), ("fRequestor=%#x\n", fRequestor),
                                    VERR_ACCESS_DENIED);
