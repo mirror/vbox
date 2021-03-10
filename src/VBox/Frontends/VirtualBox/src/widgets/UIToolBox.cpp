646809372cd6eb05984d7c19d372286bb8e6f0fe
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2021-03-10 11:17:15 +0000

    FE/Qt: bugref:9515: UIToolBox: A fix for HiDPI support for the stuff implemented in r142402; Base pixmap has devicePixelRatio initialized from windowHandle(), transformed pixmap should have it copied.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@88066 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Frontends/VirtualBox/src/widgets/UIToolBox.cpp
+++ src/VBox/Frontends/VirtualBox/src/widgets/UIToolBox.cpp
@@ -251,15 +251,16 @@ void UIToolBoxPage::setExpandCollapseIcon()
         return;
     }
     const int iMetric = QApplication::style()->pixelMetric(QStyle::PM_SmallIconSize);
-    if (m_fExpanded)
+    QPixmap basePixmap = m_expandCollapseIcon.pixmap(windowHandle(), QSize(iMetric, iMetric));
+    if (!m_fExpanded)
+        m_pExpandCollapseIconLabel->setPixmap(basePixmap);
+    else
     {
         QTransform transform;
         transform.rotate(90);
-        m_pExpandCollapseIconLabel->setPixmap(m_expandCollapseIcon.pixmap(windowHandle(), QSize(iMetric, iMetric)).transformed(transform));
-    }
-    else
-    {
-        m_pExpandCollapseIconLabel->setPixmap(m_expandCollapseIcon.pixmap(windowHandle(), QSize(iMetric, iMetric)));
+        QPixmap transformedPixmap = basePixmap.transformed(transform);
+        transformedPixmap.setDevicePixelRatio(basePixmap.devicePixelRatio());
+        m_pExpandCollapseIconLabel->setPixmap(transformedPixmap);
     }
 }
 
