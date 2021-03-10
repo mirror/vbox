cea5f87c7f885d72cf824ecc8941b426da2747db
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2021-03-10 13:16:37 +0000

    FE/Qt: bugref:9950. Small label fix.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@88068 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Frontends/VirtualBox/src/wizards/newvm/UIWizardNewVMPageBasic1.cpp
+++ src/VBox/Frontends/VirtualBox/src/wizards/newvm/UIWizardNewVMPageBasic1.cpp
@@ -537,8 +537,7 @@ void UIWizardNewVMPageBasic1::retranslateUi()
                                                       "in which case you will have to select a valid installation medium."));
 
     if (m_pNameOSTypeLabel)
-        m_pNameOSTypeLabel->setText(UIWizardNewVM::tr("Please choose a descriptive name and destination folder for the new virtual machine "
-                                             "and select the type of operating system you intend to install on it. "
+        m_pNameOSTypeLabel->setText(UIWizardNewVM::tr("Please choose a descriptive name and destination folder for the new virtual machine. "
                                              "The name you choose will be used throughout VirtualBox "
                                              "to identify this machine."));
     if (m_pISOPathSelectorLabel)
