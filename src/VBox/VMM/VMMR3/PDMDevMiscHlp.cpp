94677ff2df23d3764eaf2cabb1ebde971dfa602d
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2021-03-10 19:25:11 +0000

    AMD IOMMU: Doxygen fixes. bugref:9654
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@88078 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/VMM/VMMR3/PDMDevMiscHlp.cpp
+++ src/VBox/VMM/VMMR3/PDMDevMiscHlp.cpp
@@ -141,18 +141,18 @@ static DECLCALLBACK(void) pdmR3IoApicHlp_Unlock(PPDMDEVINS pDevIns)
 
 
 /** @interface_method_impl{PDMIOAPICHLP,pfnIommuMsiRemap} */
-static DECLCALLBACK(int) pdmR3IoApicHlp_IommuMsiRemap(PPDMDEVINS pDevIns, uint16_t uDeviceId, PCMSIMSG pMsiIn, PMSIMSG pMsiOut)
+static DECLCALLBACK(int) pdmR3IoApicHlp_IommuMsiRemap(PPDMDEVINS pDevIns, uint16_t idDevice, PCMSIMSG pMsiIn, PMSIMSG pMsiOut)
 {
     PDMDEV_ASSERT_DEVINS(pDevIns);
     LogFlow(("pdmR3IoApicHlp_IommuRemapMsi: caller='%s'/%d: pMsiIn=(%#RX64, %#RU32)\n", pDevIns->pReg->szName,
              pDevIns->iInstance, pMsiIn->Addr.u64, pMsiIn->Data.u32));
 
 #ifdef VBOX_WITH_IOMMU_AMD
-    int rc = pdmIommuMsiRemap(pDevIns, uDeviceId, pMsiIn, pMsiOut);
+    int rc = pdmIommuMsiRemap(pDevIns, idDevice, pMsiIn, pMsiOut);
     if (RT_SUCCESS(rc) || rc != VERR_IOMMU_NOT_PRESENT)
         return rc;
 #else
-    RT_NOREF(pDevIns, uDeviceId);
+    RT_NOREF(pDevIns, idDevice);
 #endif
 
     *pMsiOut = *pMsiIn;
