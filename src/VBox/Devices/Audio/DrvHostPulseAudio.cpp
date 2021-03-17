32f675507c7bf2d218c7954ee641d65c6a9d3855
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2021-03-17 16:43:36 +0000

    DrvHostPulseAudio.cpp: Another attempt at fixing bogus pCfgAcq->Backend.cFramesBufferSize assignment in paCreateStreamIn (probably harmless). bugref:9890
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@88161 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Devices/Audio/DrvHostPulseAudio.cpp
+++ src/VBox/Devices/Audio/DrvHostPulseAudio.cpp
@@ -876,7 +876,9 @@ static int paCreateStreamIn(PDRVHOSTPULSEAUDIO pThis, PPULSEAUDIOSTREAM  pStream
     pCfgAcq->Props.cChannels   = pStreamPA->SampleSpec.channels;
 
     pCfgAcq->Backend.cFramesPeriod       = PDMAUDIOSTREAMCFG_B2F(pCfgAcq, pStreamPA->BufAttr.fragsize);
-    pCfgAcq->Backend.cFramesBufferSize   = pCfgAcq->Backend.cFramesPeriod;
+    pCfgAcq->Backend.cFramesBufferSize   = pStreamPA->BufAttr.maxlength != UINT32_MAX /* paranoia */
+                                         ? PDMAUDIOSTREAMCFG_B2F(pCfgAcq, pStreamPA->BufAttr.maxlength)
+                                         : pCfgAcq->Backend.cFramesPeriod * 2 /* whatever */;
     pCfgAcq->Backend.cFramesPreBuffering = pCfgAcq->Backend.cFramesPeriod;
 
     LogFlowFuncLeaveRC(rc);
