32f788467fc9e87ae4117cfa19906ae7a30b5778
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2021-03-18 12:02:12 +0000

    GA/Win/Installer: GA installer should ignore ERROR_NO_SUCH_DEVINST, bugref:9945
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@88190 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Additions/WINNT/Installer/VBoxDrvInst.cpp
+++ src/VBox/Additions/WINNT/Installer/VBoxDrvInst.cpp
@@ -366,6 +366,10 @@ int VBoxInstallDriver(const BOOL fInstall, const _TCHAR *pszDriverPath, BOOL fSi
                         _tprintf(_T("ERROR: The driver package type is not supported of INF %ws!\n"), szDriverInf);
                         break;
 
+                    case ERROR_NO_SUCH_DEVINST:
+                        _tprintf(_T("INFO: The driver package was installed but no matching devices found in the device tree (ERROR_NO_SUCH_DEVINST).\n"));
+                    break;
+
                     default:
                     {
                         /* Try error lookup with GetErrorMsg(). */
@@ -375,7 +379,14 @@ int VBoxInstallDriver(const BOOL fInstall, const _TCHAR *pszDriverPath, BOOL fSi
                         break;
                     }
                 }
-                hr = HRESULT_FROM_WIN32(dwRet);
+
+                if (dwRet == ERROR_NO_SUCH_DEVINST)
+                {
+                    /* GA installer should ignore this error code and continue */
+                    hr = S_OK;
+                }
+                else
+                    hr = HRESULT_FROM_WIN32(dwRet);
             }
             g_pfnDIFXAPISetLogCallback(NULL, NULL);
             if (phFile)
