016df169fb42af20727b9f4b0a7d4dad3aaef250
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2021-03-18 01:43:19 +0000

    DevHDA: Rewrote the wall clock code to base it on the virtual sync clock with a start offset and holding it back according to DMA progress.  Introduced new saved state version. [build fix] bugref:9890
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@88171 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Devices/Audio/HDAStream.cpp
+++ src/VBox/Devices/Audio/HDAStream.cpp
@@ -1050,19 +1050,6 @@ static uint32_t hdaR3StreamGetFree(PHDASTREAMR3 pStreamR3)
 }
 
 /**
- * Converts frame count to wall clock ticks (WALCLK).
- *
- * @returns Wall clock ticks.
- * @param   pStream     The stream to do the conversion for.
- * @param   cFrames     Number of audio frames.
- */
-DECLINLINE(uint64_t) hdaR3StreamFramesToWalClk(PHDASTREAM pStreamShared, uint32_t cFrames)
-{
-    const uint32_t uHz = RT_MAX(pStreamShared->State.Cfg.Props.uHz, 1 /* prevent div/0 */);
-    return ASMMultU32ByU32DivByU32(cFrames, 24000000 /* 24 MHz wall clock (WALCLK). */ , uHz);
-}
-
-/**
  * Get the current address and number of bytes left in the current BDLE.
  *
  * @returns The current physical address.
