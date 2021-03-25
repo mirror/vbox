372e7c15aef0cb98abd472eae178eb5e53eddf2c
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2021-03-25 17:50:38 +0000

    IPRT/rtSemEventMultiPosixWaitTimed: Fixed wrong milli->nano overflow check.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@88294 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Runtime/r3/posix/semeventmulti-posix.cpp
+++ src/VBox/Runtime/r3/posix/semeventmulti-posix.cpp
@@ -456,7 +456,7 @@ static int rtSemEventMultiPosixWaitTimed(struct RTSEMEVENTMULTIINTERNAL *pThis, 
      * Convert uTimeout to a relative value in nano seconds.
      */
     if (fFlags & RTSEMWAIT_FLAGS_MILLISECS)
-        uTimeout = uTimeout < UINT64_MAX / UINT32_C(1000000) * UINT32_C(1000000)
+        uTimeout = uTimeout < UINT64_MAX / UINT32_C(1000000)
                  ? uTimeout * UINT32_C(1000000)
                  : UINT64_MAX;
     if (uTimeout == UINT64_MAX) /* unofficial way of indicating an indefinite wait */
