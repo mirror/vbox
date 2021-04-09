3e34dc8b9a8bacdaa0cd1f90848409db958d708b
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2021-04-09 13:04:05 +0000

    Audio: Doxygen fix. bygref:9890
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@88435 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Devices/Audio/AudioMixBuffer.cpp
+++ src/VBox/Devices/Audio/AudioMixBuffer.cpp
@@ -2391,7 +2391,6 @@ int AudioMixBufWriteCirc(PAUDIOMIXBUF pMixBuf, const void *pvBuf, uint32_t cbBuf
  * @retval  VERR_BUFFER_OVERFLOW no space is available for writing anymore.
  * @param   pMixBuf     Pointer to mixing buffer to write to.
  * @param   pSrcProps   The source format.
- * @param   enmFmt      Audio format supplied in the buffer.
  * @param   pvBuf       Pointer to audio buffer to be written.
  * @param   cbBuf       Size (in bytes) of audio buffer.
  * @param   pcWritten   Returns number of audio frames written. Optional.
