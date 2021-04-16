c122e44834dab46a21e775568d055c8133ef5773
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2021-04-16 16:26:42 +0000

    Additions: Linux: vboxsf: print revision number as well as a version string on module load.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@88570 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Additions/linux/sharedfolders/vfsmod.c
+++ src/VBox/Additions/linux/sharedfolders/vfsmod.c
@@ -1677,15 +1677,15 @@ static int __init init(void)
                  */
                 rc = register_filesystem(&g_vboxsf_fs_type);
                 if (rc == 0) {
-                    printk(KERN_INFO "vboxsf: Successfully loaded version " VBOX_VERSION_STRING "\n");
+                    printk(KERN_INFO "vboxsf: Successfully loaded version " VBOX_VERSION_STRING " r" __stringify(VBOX_SVN_REV) "\n");
 #ifdef VERMAGIC_STRING
-                    LogRel(("vboxsf: Successfully loaded version " VBOX_VERSION_STRING " on %s (LINUX_VERSION_CODE=%#x)\n",
+                    LogRel(("vboxsf: Successfully loaded version " VBOX_VERSION_STRING " r" __stringify(VBOX_SVN_REV) " on %s (LINUX_VERSION_CODE=%#x)\n",
                             VERMAGIC_STRING, LINUX_VERSION_CODE));
 #elif defined(UTS_RELEASE)
-                    LogRel(("vboxsf: Successfully loaded version " VBOX_VERSION_STRING " on %s (LINUX_VERSION_CODE=%#x)\n",
+                    LogRel(("vboxsf: Successfully loaded version " VBOX_VERSION_STRING " r" __stringify(VBOX_SVN_REV) " on %s (LINUX_VERSION_CODE=%#x)\n",
                             UTS_RELEASE, LINUX_VERSION_CODE));
 #else
-                    LogRel(("vboxsf: Successfully loaded version " VBOX_VERSION_STRING " (LINUX_VERSION_CODE=%#x)\n", LINUX_VERSION_CODE));
+                    LogRel(("vboxsf: Successfully loaded version " VBOX_VERSION_STRING " r" __stringify(VBOX_SVN_REV) " (LINUX_VERSION_CODE=%#x)\n", LINUX_VERSION_CODE));
 #endif
                     return 0;
                 }
