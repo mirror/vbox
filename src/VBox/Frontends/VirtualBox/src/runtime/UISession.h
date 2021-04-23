e14206eb5f60d56d4e1871289f2dec85067c98a9
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2021-04-23 16:50:13 +0000

    FE/Qt: bugref:8161: A bit of cleanup for UISession.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@88685 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Frontends/VirtualBox/src/runtime/UISession.h
+++ src/VBox/Frontends/VirtualBox/src/runtime/UISession.h
@@ -61,31 +61,6 @@ class QMenuBar;
 class QIcon;
 #endif /* !VBOX_WS_MAC */
 
-/* CConsole callback event types: */
-enum UIConsoleEventType
-{
-    UIConsoleEventType_MousePointerShapeChange = QEvent::User + 1,
-    UIConsoleEventType_MouseCapabilityChange,
-    UIConsoleEventType_KeyboardLedsChange,
-    UIConsoleEventType_StateChange,
-    UIConsoleEventType_AdditionsStateChange,
-    UIConsoleEventType_NetworkAdapterChange,
-    /* Not used: UIConsoleEventType_SerialPortChange, */
-    /* Not used: UIConsoleEventType_ParallelPortChange, */
-    /* Not used: UIConsoleEventType_StorageControllerChange, */
-    UIConsoleEventType_MediumChange,
-    /* Not used: UIConsoleEventType_CPUChange, */
-    UIConsoleEventType_VRDEServerChange,
-    UIConsoleEventType_VRDEServerInfoChange,
-    UIConsoleEventType_USBControllerChange,
-    UIConsoleEventType_USBDeviceStateChange,
-    UIConsoleEventType_SharedFolderChange,
-    UIConsoleEventType_RuntimeError,
-    UIConsoleEventType_CanShowWindow,
-    UIConsoleEventType_ShowWindow,
-    UIConsoleEventType_MAX
-};
-
 class UISession : public QObject
 {
     Q_OBJECT;
