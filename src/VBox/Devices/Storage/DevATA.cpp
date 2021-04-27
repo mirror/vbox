3f0edb11de9d281550c7561c8eee5a83e67e2640
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2021-04-27 11:59:39 +0000

    DevATA: Fixed misleading INITIALIZE DEVICE PARAMETERS logging (the head number programmed into registers is the highest valid head number, starting with head zero).
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@88730 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Devices/Storage/DevATA.cpp
+++ src/VBox/Devices/Storage/DevATA.cpp
@@ -4198,7 +4198,7 @@ static bool ataR3InitDevParmSS(PPDMDEVINS pDevIns, PATACONTROLLER pCtl, PATADEVS
     RT_NOREF(pDevR3);
     LogFlowFunc(("\n"));
     LogRel(("ATA: LUN#%d: INITIALIZE DEVICE PARAMETERS: %u logical sectors, %u heads\n",
-            s->iLUN, s->uATARegNSector, s->uATARegSelect & 0x0f));
+            s->iLUN, s->uATARegNSector, (s->uATARegSelect & 0x0f) + 1));
     ataR3LockLeave(pDevIns, pCtl);
     RTThreadSleep(pCtl->msDelayIRQ);
     ataR3LockEnter(pDevIns, pCtl);
