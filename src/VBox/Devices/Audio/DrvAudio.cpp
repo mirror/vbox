6ceaf5b9f1094442d405882f298bc36d889b899e
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2021-04-29 01:20:46 +0000

    Audio: Do not pause disabled streams as that may cause some simplistic backends (like ALSA) to enable the stream upon resume. bugref:9890
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@88762 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Devices/Audio/DrvAudio.cpp
+++ src/VBox/Devices/Audio/DrvAudio.cpp
@@ -1977,7 +1977,6 @@ static int drvAudioStreamControlInternal(PDRVAUDIO pThis, PDRVAUDIOSTREAM pStrea
     switch (enmStreamCmd)
     {
         case PDMAUDIOSTREAMCMD_ENABLE:
-        {
             if (!(pStreamEx->Core.fStatus & PDMAUDIOSTREAM_STS_ENABLED))
             {
                 /* Is a pending disable outstanding? Then disable first. */
@@ -2011,10 +2010,8 @@ static int drvAudioStreamControlInternal(PDRVAUDIO pThis, PDRVAUDIOSTREAM pStrea
                 }
             }
             break;
-        }
 
         case PDMAUDIOSTREAMCMD_DISABLE:
-        {
             if (pStreamEx->Core.fStatus & PDMAUDIOSTREAM_STS_ENABLED)
             {
                 /*
@@ -2050,11 +2047,10 @@ static int drvAudioStreamControlInternal(PDRVAUDIO pThis, PDRVAUDIOSTREAM pStrea
                 }
             }
             break;
-        }
 
         case PDMAUDIOSTREAMCMD_PAUSE:
-        {
-            if (!(pStreamEx->Core.fStatus & PDMAUDIOSTREAM_STS_PAUSED))
+            if (   (pStreamEx->Core.fStatus & (PDMAUDIOSTREAM_STS_ENABLED | PDMAUDIOSTREAM_STS_PAUSED))
+                ==                             PDMAUDIOSTREAM_STS_ENABLED)
             {
                 rc = drvAudioStreamControlInternalBackend(pThis, pStreamEx, PDMAUDIOSTREAMCMD_PAUSE);
                 if (RT_SUCCESS(rc))
@@ -2064,12 +2060,11 @@ static int drvAudioStreamControlInternal(PDRVAUDIO pThis, PDRVAUDIOSTREAM pStrea
                 }
             }
             break;
-        }
 
         case PDMAUDIOSTREAMCMD_RESUME:
-        {
             if (pStreamEx->Core.fStatus & PDMAUDIOSTREAM_STS_PAUSED)
             {
+                Assert(pStreamEx->Core.fStatus & PDMAUDIOSTREAM_STS_ENABLED);
                 rc = drvAudioStreamControlInternalBackend(pThis, pStreamEx, PDMAUDIOSTREAMCMD_RESUME);
                 if (RT_SUCCESS(rc))
                 {
@@ -2078,7 +2073,6 @@ static int drvAudioStreamControlInternal(PDRVAUDIO pThis, PDRVAUDIOSTREAM pStrea
                 }
             }
             break;
-        }
 
         default:
             rc = VERR_NOT_IMPLEMENTED;
