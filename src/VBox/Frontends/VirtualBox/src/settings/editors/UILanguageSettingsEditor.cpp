96ac40842b9fa04d71a245a7f2d091f3e01df2fe
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2021-04-30 12:14:29 +0000

    FE/Qt: bugref:10003: Reworking UILanguageSettingsEditor to have native colors.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@88794 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Frontends/VirtualBox/src/settings/editors/UILanguageSettingsEditor.cpp
+++ src/VBox/Frontends/VirtualBox/src/settings/editors/UILanguageSettingsEditor.cpp
@@ -291,7 +291,7 @@ void UILanguageSettingsEditor::sltHandleItemPainting(QTreeWidgetItem *pItem, QPa
     {
         /* We are drawing a separator line in the tree: */
         const QRect rect = m_pTreeWidget->visualItemRect(pLanguageItem);
-        pPainter->setPen(m_pTreeWidget->palette().color(QPalette::Mid));
+        pPainter->setPen(m_pTreeWidget->palette().color(QPalette::Window));
         pPainter->drawLine(rect.x(), rect.y() + rect.height() - 1,
                            rect.x() + rect.width(), rect.y() + rect.height() - 1);
     }
