063c95e784ef0404667890108a94fcc1e5652c07
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2021-05-07 15:06:47 +0000

    Audio/VaKit: Fixed SCM warning. bugref:10008
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@88930 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Devices/Audio/VBoxDDVKAT.h
+++ src/VBox/Devices/Audio/VBoxDDVKAT.h
@@ -1,7 +1,33 @@
+/* $Id$ */
+/** @file
+ * Validation Kit Audio Test (VKAT) header for handling the device drivers.
+ *
+ ** @todo Move / rename this header into ValidationKit/utils/audio/whatever.h?
+ */
 
+/*
+ * Copyright (C) 2021 Oracle Corporation
+ *
+ * This file is part of VirtualBox Open Source Edition (OSE), as
+ * available from http://www.virtualbox.org. This file is free software;
+ * you can redistribute it and/or modify it under the terms of the GNU
+ * General Public License (GPL) as published by the Free Software
+ * Foundation, in version 2 as it comes in the "COPYING" file of the
+ * VirtualBox OSE distribution. VirtualBox OSE is distributed in the
+ * hope that it will be useful, but WITHOUT ANY WARRANTY of any kind.
+ *
+ * The contents of this file may alternatively be used under the terms
+ * of the Common Development and Distribution License Version 1.0
+ * (CDDL) only, as it comes in the "COPYING.CDDL" file of the
+ * VirtualBox OSE distribution, in which case the provisions of the
+ * CDDL are applicable instead of those of the GPL.
+ *
+ * You may elect to license modified versions of this file under the
+ * terms and conditions of either the GPL or the CDDL or both.
+ */
 
-#ifndef VBoxDDVKAT_h__
-#define VBoxDDVKAT_h__
+#ifndef VBOX_INCLUDED_SRC_build_VBoxDDVKAT_h
+#define VBOX_INCLUDED_SRC_build_VBoxDDVKAT_h
 
 #include <VBox/vmm/pdmifs.h>
 
@@ -79,4 +105,4 @@ extern const PDMDRVREG g_DrvVKATPulseAudio;
 extern const PDMDRVREG g_DrvVKATAlsa;
 extern const PDMDRVREG g_DrvVKATOss;
 
-#endif /* VBoxDDVKAT_h__ */
+#endif /* VBOX_INCLUDED_SRC_build_VBoxDDVKAT_h */
