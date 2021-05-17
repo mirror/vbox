e152e4bf035a22c1787adf710d66dff079651a31
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2021-05-17 10:02:15 +0000

    TM: todo
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@89087 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/VMM/VMMAll/TMAllCpu.cpp
+++ src/VBox/VMM/VMMAll/TMAllCpu.cpp
@@ -587,6 +587,7 @@ VMM_INT_DECL(int) TMCpuTickSetLastSeen(PVMCPUCC pVCpu, uint64_t u64LastSeenTick)
     VMCPU_ASSERT_EMT(pVCpu);
 
     LogFlow(("TMCpuTickSetLastSeen %RX64\n", u64LastSeenTick));
+    /** @todo deal with wraparound!   */
     if (pVCpu->tm.s.u64TSCLastSeen < u64LastSeenTick)
         pVCpu->tm.s.u64TSCLastSeen = u64LastSeenTick;
     return VINF_SUCCESS;
