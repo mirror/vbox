936a6f860fc9abfdc1ef3cc9e127b461538893d5
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2021-05-19 21:32:08 +0000

    DevIchAc97: Don't disable during reset unless it was running. Mixer will assert. bugref:9890
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@89186 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Devices/Audio/DevIchAc97.cpp
+++ src/VBox/Devices/Audio/DevIchAc97.cpp
@@ -2889,8 +2889,9 @@ ichac97IoPortNabmWrite(PPDMDEVINS pDevIns, void *pvUser, RTIOPORT offPort, uint3
 
                             /* Make sure that Run/Pause Bus Master bit (RPBM) is cleared (0). */
                             Assert((pRegs->cr & AC97_CR_RPBM) == 0);
+                            if (pRegs->cr & AC97_CR_RPBM)
+                                ichac97R3StreamEnable(pDevIns, pThis, pThisCC, pStream, pStreamCC, false /* fEnable */);
 
-                            ichac97R3StreamEnable(pDevIns, pThis, pThisCC, pStream, pStreamCC, false /* fEnable */);
                             ichac97R3StreamReset(pThis, pStream, pStreamCC);
 
                             ichac97StreamUpdateSR(pDevIns, pThis, pStream, AC97_SR_DCH); /** @todo Do we need to do that? */
