9e5851f434c4a6ca9060d5a6a6b3f3ac8e95c013
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2021-05-28 08:46:17 +0000

    AudioMixer: Removed AudioMixBufAcquireReadBlock, AudioMixBufReleaseReadBlock and AudioMixBufFinish. [build fix] bugref:9890
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@89336 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Devices/Audio/AudioMixBuffer.cpp
+++ src/VBox/Devices/Audio/AudioMixBuffer.cpp
@@ -64,7 +64,9 @@
 
 #ifdef DEBUG
 static void audioMixBufDbgPrintInternal(PAUDIOMIXBUF pMixBuf, const char *pszFunc);
+# ifdef UNUSED
 static bool audioMixBufDbgValidate(PAUDIOMIXBUF pMixBuf);
+# endif
 #endif
 
 /*
@@ -1066,6 +1068,7 @@ static void audioMixBufDbgPrintSingle(PAUDIOMIXBUF pMixBuf, const char *pszFunc,
          pMixBuf->pszName, pMixBuf->offRead, pMixBuf->offWrite, pMixBuf->cMixed, pMixBuf->cUsed, pMixBuf->cFrames));
 }
 
+# ifdef UNUSED
 /**
  * Validates a single mixing buffer.
  *
@@ -1102,6 +1105,7 @@ static bool audioMixBufDbgValidate(PAUDIOMIXBUF pMixBuf)
 
     return fValid;
 }
+# endif
 
 /**
  * Prints statistics and status of the full chain of a mixing buffer to the logger,
