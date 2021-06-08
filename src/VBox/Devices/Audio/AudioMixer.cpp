be320df60bf1d024d73daa39ff6bf104c8b52fe7
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2021-06-08 08:11:48 +0000

    AudioMixer: Must use RTMemAllocZVar when doing variable length allocation for the mixer and sink, or the electric fence heap will misalign the blocks causing critsect trouble. bugref:9890
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@89558 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Devices/Audio/AudioMixer.cpp
+++ src/VBox/Devices/Audio/AudioMixer.cpp
@@ -174,7 +174,7 @@ int AudioMixerCreate(const char *pszName, uint32_t fFlags, PAUDIOMIXER *ppMixer)
     AssertPtrReturn(ppMixer, VERR_INVALID_POINTER);
 
     int         rc;
-    PAUDIOMIXER pMixer = (PAUDIOMIXER)RTMemAllocZ(sizeof(AUDIOMIXER) + cchName + 1);
+    PAUDIOMIXER pMixer = (PAUDIOMIXER)RTMemAllocZVar(sizeof(AUDIOMIXER) + cchName + 1);
     if (pMixer)
     {
         rc = RTCritSectInit(&pMixer->CritSect);
@@ -378,7 +378,7 @@ int AudioMixerCreateSink(PAUDIOMIXER pMixer, const char *pszName, PDMAUDIODIR en
     /*
      * Allocate the data and initialize the critsect.
      */
-    PAUDMIXSINK pSink = (PAUDMIXSINK)RTMemAllocZ(sizeof(AUDMIXSINK) + cchName + 1);
+    PAUDMIXSINK pSink = (PAUDMIXSINK)RTMemAllocZVar(sizeof(AUDMIXSINK) + cchName + 1);
     if (pSink)
     {
         rc = RTCritSectInit(&pSink->CritSect);
