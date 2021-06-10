673f2d5b67d79bf1888663033a682486dd4aec53
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2021-06-10 23:04:30 +0000

    DevHda: Debug info nits. bugref:9890
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@89611 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Devices/Audio/DevHda.cpp
+++ src/VBox/Devices/Audio/DevHda.cpp
@@ -4182,7 +4182,7 @@ static void hdaR3DbgPrintBDL(PPDMDEVINS pDevIns, PHDASTATE pThis, PCDBGFINFOHLP 
 
         cbTotal += bd.u32BufSize;
     }
-    pHlp->pfnPrintf(pHlp, "  Total: %#RX64 bytes (%RU64), %u ms\n", cbTotal, cbTotal,
+    pHlp->pfnPrintf(pHlp, "  Total: %#RX64 bytes (%RU64), %RU64 ms\n", cbTotal, cbTotal,
                     PDMAudioPropsBytesToMilli(pGuestProps, (uint32_t)cbTotal));
     if (cbTotal != u32CBL)
         pHlp->pfnPrintf(pHlp, "  Warning: %#RX64 bytes does not match CBL (%#RX64)!\n", cbTotal, u32CBL);
@@ -4231,7 +4231,7 @@ static DECLCALLBACK(void) hdaR3DbgInfoStream(PPDMDEVINS pDevIns, PCDBGFINFOHLP p
 }
 
 /**
- * @callback_method_impl{FNDBGFHANDLERDEV, hdabdle}
+ * @callback_method_impl{FNDBGFHANDLERDEV, hdabdl}
  */
 static DECLCALLBACK(void) hdaR3DbgInfoBDL(PPDMDEVINS pDevIns, PCDBGFINFOHLP pHlp, const char *pszArgs)
 {
