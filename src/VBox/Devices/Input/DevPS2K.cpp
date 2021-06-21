cb0d3522fe4f465722230d6fa817910b9a421bf0
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2021-06-21 10:41:28 +0000

    Typo.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@89812 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Devices/Input/DevPS2K.cpp
+++ src/VBox/Devices/Input/DevPS2K.cpp
@@ -953,7 +953,7 @@ static int ps2kR3ProcessKeyEvent(PPDMDEVINS pDevIns, PPS2K pThis, uint32_t u32Hi
  *
  * Note: The throttling is currently only done for keyboard data, not command responses.
  * The throttling could and perhaps should be done for any data (including command
- * response) scoming from PS/2 devices, both keyboard and auxiliary. That is not currently
+ * responses) coming from PS/2 devices, both keyboard and auxiliary. That is not currently
  * done because it would needlessly slow things down.
  */
 static DECLCALLBACK(void) ps2kR3ThrottleTimer(PPDMDEVINS pDevIns, TMTIMERHANDLE hTimer, void *pvUser)
