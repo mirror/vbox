73c0a12445cb726d7f2f320bbda7174f7f039875
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2021-06-24 19:59:39 +0000

    Frontends/VBoxManage/VBoxInternalManage.cpp: fix forgotten field name adjustments in debug code
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@89905 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Frontends/VBoxManage/VBoxInternalManage.cpp
+++ src/VBox/Frontends/VBoxManage/VBoxInternalManage.cpp
@@ -1802,9 +1802,9 @@ static RTEXITCODE CmdCreateRawVMDK(int argc, char **argv, ComPtr<IVirtualBox> aV
         for (unsigned i = 0; i < RawDescriptor.cPartDescs; i++)
         {
             RTPrintf("%2u  %14RU64 %14RU64 %14RU64 %#18p %s\n", i,
-                     RawDescriptor.pPartDescs[i].uStart,
+                     RawDescriptor.pPartDescs[i].offStartInVDisk,
                      RawDescriptor.pPartDescs[i].cbData,
-                     RawDescriptor.pPartDescs[i].uStartOffset,
+                     RawDescriptor.pPartDescs[i].offStartInDevice,
                      RawDescriptor.pPartDescs[i].pvPartitionData,
                      RawDescriptor.pPartDescs[i].pszRawDevice);
         }
