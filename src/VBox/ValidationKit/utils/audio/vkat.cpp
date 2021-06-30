95e50842c045d81736b00ddf6706f571e212c975
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2021-06-30 07:57:43 +0000

    Audio/ValKit: Closing fixes for audioVerifyOne(). bugref:10008
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@89964 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/ValidationKit/utils/audio/vkat.cpp
+++ src/VBox/ValidationKit/utils/audio/vkat.cpp
@@ -870,38 +870,39 @@ static int audioVerifyOne(const char *pszPathSetA, const char *pszPathSetB)
     AUDIOTESTSET SetA, SetB;
     int rc = audioVerifyOpenTestSet(pszPathSetA, &SetA);
     if (RT_SUCCESS(rc))
-        rc = audioVerifyOpenTestSet(pszPathSetB, &SetB);
-
-    if (RT_SUCCESS(rc))
     {
-        AUDIOTESTERRORDESC errDesc;
-        rc = AudioTestSetVerify(&SetA, &SetB, &errDesc);
+        rc = audioVerifyOpenTestSet(pszPathSetB, &SetB);
         if (RT_SUCCESS(rc))
         {
-            RTTestPrintf(g_hTest, RTTESTLVL_ALWAYS, "%RU32 errors occurred while verifying\n", AudioTestErrorDescCount(&errDesc));
-            if (AudioTestErrorDescFailed(&errDesc))
+            AUDIOTESTERRORDESC errDesc;
+            rc = AudioTestSetVerify(&SetA, &SetB, &errDesc);
+            if (RT_SUCCESS(rc))
             {
-                /** @todo Use some AudioTestErrorXXX API for enumeration here later. */
-                PAUDIOTESTERRORENTRY pErrEntry;
-                RTListForEach(&errDesc.List, pErrEntry, AUDIOTESTERRORENTRY, Node)
-                    RTTestFailed(g_hTest, pErrEntry->szDesc);
+                RTTestPrintf(g_hTest, RTTESTLVL_ALWAYS, "%RU32 errors occurred while verifying\n", AudioTestErrorDescCount(&errDesc));
+                if (AudioTestErrorDescFailed(&errDesc))
+                {
+                    /** @todo Use some AudioTestErrorXXX API for enumeration here later. */
+                    PAUDIOTESTERRORENTRY pErrEntry;
+                    RTListForEach(&errDesc.List, pErrEntry, AUDIOTESTERRORENTRY, Node)
+                        RTTestFailed(g_hTest, pErrEntry->szDesc);
+                }
+                else
+                    RTTestPrintf(g_hTest, RTTESTLVL_ALWAYS, "Verification successful\n");
+
+                AudioTestErrorDescDestroy(&errDesc);
             }
             else
-                RTTestPrintf(g_hTest, RTTESTLVL_ALWAYS, "Verification successful\n");
+                RTTestFailed(g_hTest, "Verification failed with %Rrc", rc);
 
-            AudioTestErrorDescDestroy(&errDesc);
+            if (!g_fDrvAudioDebug) /* Don't wipe stuff when debugging. Can be useful for introspecting data. */
+                AudioTestSetWipe(&SetB);
+            AudioTestSetClose(&SetB);
         }
-        else
-            RTTestFailed(g_hTest, "Verification failed with %Rrc", rc);
+
+        if (!g_fDrvAudioDebug) /* Ditto. */
+            AudioTestSetWipe(&SetA);
+        AudioTestSetClose(&SetA);
     }
-
-    if (!g_fDrvAudioDebug) /* Don't wipe stuff when debugging. Can be useful for introspecting data. */
-        AudioTestSetWipe(&SetA);
-    AudioTestSetClose(&SetA);
-
-    if (!g_fDrvAudioDebug) /* Ditto. */
-        AudioTestSetWipe(&SetB);
-    AudioTestSetClose(&SetB);
 
     RTTestSubDone(g_hTest);
 
