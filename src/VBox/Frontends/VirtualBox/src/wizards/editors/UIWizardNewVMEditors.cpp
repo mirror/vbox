f7b6c4f361972c61ad6fc410ed8720db9c4d5ae3
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2021-07-13 07:26:44 +0000

    FE/Qt: bugref:9996. Moving group box creation to a separate class
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@90164 cfe28804-0f27-0410-a406-dd0f0b0b656f

---
+++ src/VBox/Frontends/VirtualBox/src/wizards/editors/UIWizardNewVMEditors.cpp
@@ -0,0 +1,80 @@
+/* $Id$ */
+/** @file
+ * VBox Qt GUI - UIUserNamePasswordEditor class implementation.
+ */
+
+/*
+ * Copyright (C) 2006-2020 Oracle Corporation
+ *
+ * This file is part of VirtualBox Open Source Edition (OSE), as
+ * available from http://www.virtualbox.org. This file is free software;
+ * you can redistribute it and/or modify it under the terms of the GNU
+ * General Public License (GPL) as published by the Free Software
+ * Foundation, in version 2 as it comes in the "COPYING" file of the
+ * VirtualBox OSE distribution. VirtualBox OSE is distributed in the
+ * hope that it will be useful, but WITHOUT ANY WARRANTY of any kind.
+ */
+
+/* Qt includes: */
+#include <QVBoxLayout>
+
+/* GUI includes: */
+#include "UIUserNamePasswordEditor.h"
+#include "UIWizardNewVMEditors.h"
+
+/* Other VBox includes: */
+#include "iprt/assert.h"
+
+UIUserNamePasswordGroupBox::UIUserNamePasswordGroupBox(QWidget *pParent /* = 0 */)
+    :QGroupBox(pParent)
+{
+    prepare();
+}
+
+void UIUserNamePasswordGroupBox::prepare()
+{
+    QVBoxLayout *pUserNameContainerLayout = new QVBoxLayout(this);
+    m_pUserNamePasswordEditor = new UIUserNamePasswordEditor;
+    AssertReturnVoid(m_pUserNamePasswordEditor);
+    m_pUserNamePasswordEditor->setLabelsVisible(true);
+    m_pUserNamePasswordEditor->setSizePolicy(QSizePolicy::Minimum, QSizePolicy::Fixed);
+    pUserNameContainerLayout->addWidget(m_pUserNamePasswordEditor);
+
+    connect(m_pUserNamePasswordEditor, &UIUserNamePasswordEditor::sigPasswordChanged,
+            this, &UIUserNamePasswordGroupBox::sigPasswordChanged);
+    connect(m_pUserNamePasswordEditor, &UIUserNamePasswordEditor::sigUserNameChanged,
+            this, &UIUserNamePasswordGroupBox::sigUserNameChanged);
+}
+
+QString UIUserNamePasswordGroupBox::userName() const
+{
+    if (m_pUserNamePasswordEditor)
+        return m_pUserNamePasswordEditor->userName();
+    return QString();
+}
+
+void UIUserNamePasswordGroupBox::setUserName(const QString &strUserName)
+{
+    if (m_pUserNamePasswordEditor)
+        m_pUserNamePasswordEditor->setUserName(strUserName);
+}
+
+QString UIUserNamePasswordGroupBox::password() const
+{
+    if (m_pUserNamePasswordEditor)
+        return m_pUserNamePasswordEditor->password();
+    return QString();
+}
+
+void UIUserNamePasswordGroupBox::setPassword(const QString &strPassword)
+{
+    if (m_pUserNamePasswordEditor)
+        m_pUserNamePasswordEditor->setPassword(strPassword);
+}
+
+bool UIUserNamePasswordGroupBox::isComplete()
+{
+    if (m_pUserNamePasswordEditor)
+        return m_pUserNamePasswordEditor->isComplete();
+    return false;
+}
---
+++ src/VBox/Frontends/VirtualBox/src/wizards/editors/UIWizardNewVMEditors.h
@@ -0,0 +1,68 @@
+/* $Id$ */
+/** @file
+ * VBox Qt GUI - UIWizardNewVMEditors class declaration.
+ */
+
+/*
+ * Copyright (C) 2006-2020 Oracle Corporation
+ *
+ * This file is part of VirtualBox Open Source Edition (OSE), as
+ * available from http://www.virtualbox.org. This file is free software;
+ * you can redistribute it and/or modify it under the terms of the GNU
+ * General Public License (GPL) as published by the Free Software
+ * Foundation, in version 2 as it comes in the "COPYING" file of the
+ * VirtualBox OSE distribution. VirtualBox OSE is distributed in the
+ * hope that it will be useful, but WITHOUT ANY WARRANTY of any kind.
+ */
+
+#ifndef FEQT_INCLUDED_SRC_wizards_editors_UIWizardNewVMEditors_h
+#define FEQT_INCLUDED_SRC_wizards_editors_UIWizardNewVMEditors_h
+#ifndef RT_WITHOUT_PRAGMA_ONCE
+# pragma once
+#endif
+
+/* Qt includes: */
+#include <QIcon>
+#include <QGroupBox>
+
+/* Local includes: */
+#include "QIWithRetranslateUI.h"
+
+/* Forward declarations: */
+class QGridLayout;
+class QLabel;
+class QILineEdit;
+class UIPasswordLineEdit;
+class UIUserNamePasswordEditor;
+
+class UIUserNamePasswordGroupBox : public QGroupBox
+{
+    Q_OBJECT;
+
+signals:
+
+    void sigUserNameChanged(const QString &strUserName);
+    void sigPasswordChanged(const QString &strPassword);
+
+public:
+
+    UIUserNamePasswordGroupBox(QWidget *pParent = 0);
+
+    /** @name Wrappers for UIUserNamePasswordEditor
+     * @{ */
+       QString userName() const;
+       void setUserName(const QString &strUserName);
+
+       QString password() const;
+       void setPassword(const QString &strPassword);
+       bool isComplete();
+    /** @} */
+
+private:
+
+    void prepare();
+
+    UIUserNamePasswordEditor *m_pUserNamePasswordEditor;
+};
+
+#endif /* !FEQT_INCLUDED_SRC_wizards_editors_UIWizardNewVMEditors_h */
