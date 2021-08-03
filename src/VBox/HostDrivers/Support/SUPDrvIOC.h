0d56aef4fe4fe30008531f6d5c73eeabc1b62bb3
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2021-08-03 09:19:13 +0000

    SUP: Export RTSemEventIsSignalSafe and RTSemEventMultiIsSignalSafe - IOC version change. bugref:6695
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@90489 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/HostDrivers/Support/SUPDrvIOC.h
+++ src/VBox/HostDrivers/Support/SUPDrvIOC.h
@@ -222,7 +222,7 @@ typedef SUPREQHDR *PSUPREQHDR;
  * @todo Pending work on next major version change:
  *          - Remove pvVMMR0 from SUPLDRLOAD.
  */
-#define SUPDRV_IOC_VERSION                              0x00300002
+#define SUPDRV_IOC_VERSION                              0x00300003
 
 /** SUP_IOCTL_COOKIE. */
 typedef struct SUPCOOKIE
