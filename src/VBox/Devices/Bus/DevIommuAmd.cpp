15798f90bf72c19c25123818110cf49f37ffec76
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2021-08-04 05:17:28 +0000

    AMD IOMMU: bugref:9654 Allow invalidations for page sizes larger than 4 GB, since address bits 51:32 can be used to encode page sizes greater that 4 GB.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@90505 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Devices/Bus/DevIommuAmd.cpp
+++ src/VBox/Devices/Bus/DevIommuAmd.cpp
@@ -5022,9 +5022,8 @@ static int iommuAmdR3CmdProcess(PPDMDEVINS pDevIns, PCCMD_GENERIC_T pCmd, RTGCPH
                  * Validate invalidation size.
                  * See AMD IOMMU spec. 2.2.3 "I/O Page Tables for Host Translations".
                  */
-                if (   cShift == 12 /* 4K  */ || cShift == 13 /* 8K  */
-                    || cShift == 14 /* 16K */ || cShift == 20 /* 1M  */
-                    || cShift == 22 /* 4M  */ || cShift == 32 /* 4G  */)
+                if (   cShift >= 12 /* 4 KB */
+                    && cShift <= 51 /* 2 PB */)
                 {
                     /* Remove the range of I/O virtual addresses requesting to be invalidated. */
                     size_t const cbIova = RT_BIT_64(cShift);
