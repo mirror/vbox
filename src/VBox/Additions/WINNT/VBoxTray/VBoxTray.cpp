25b1b9c8d638077416e28946ce83952580c4ac15
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2021-08-17 20:17:01 +0000

    VBoxTray: Reverted r146329 as it seems this one is different as it acutally installs the g_pReleaseLogger as the debug logger.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@90710 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Additions/WINNT/VBoxTray/VBoxTray.cpp
+++ src/VBox/Additions/WINNT/VBoxTray/VBoxTray.cpp
@@ -579,10 +579,11 @@ static int vboxTrayLogCreate(void)
     int rc = RTLogCreateEx(&g_pLoggerRelease, fFlags,
 #ifdef DEBUG
                            "all.e.l.f",
+                           "VBOXTRAY_LOG",
 #else
                            "all",
-#endif
                            "VBOXTRAY_RELEASE_LOG",
+#endif
                            RT_ELEMENTS(s_apszGroups), s_apszGroups, UINT32_MAX, RTLOGDEST_STDOUT,
                            vboxTrayLogHeaderFooter, g_cHistory, g_uHistoryFileSize, g_uHistoryFileTime,
                            RTErrInfoInitStatic(&ErrInfo), NULL /*pszFilenameFmt*/);
