e767ad2c018c794bd0e3f8367bfe5a0bceac7fcf
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2021-08-17 10:59:20 +0000

    FE/Qt: bugref:10087: Runtime UI / UIMouseHandler: Couple of bug-fixes for touchpad-based scrolling.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@90701 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Frontends/VirtualBox/src/runtime/UIMouseHandler.cpp
+++ src/VBox/Frontends/VirtualBox/src/runtime/UIMouseHandler.cpp
@@ -652,7 +652,10 @@ bool UIMouseHandler::eventFilter(QObject *pWatched, QEvent *pEvent)
                                                          kEventQueueOptionsNone) != NULL)
                         return true;
 #endif /* VBOX_WS_MAC */
+
+                    /* This event should be also processed using next 'case': */
                 }
+                RT_FALL_THRU();
                 case QEvent::MouseButtonRelease:
                 {
                     /* Get mouse-event: */
@@ -732,7 +735,12 @@ bool UIMouseHandler::eventFilter(QObject *pWatched, QEvent *pEvent)
                     if (pEvent->type() == QEvent::MouseButtonPress)
                         machineLogic()->keyboardHandler()->finaliseCaptureKeyboard();
 #endif /* VBOX_WS_X11 */
-                    m_iLastMouseWheelDelta = 0;
+
+                    /* For various mouse click related events
+                     * we also reset last mouse wheel delta: */
+                    if (pEvent->type() != QEvent::MouseMove)
+                        m_iLastMouseWheelDelta = 0;
+
                     if (mouseEvent(pMouseEvent->type(), uScreenId,
                                    pMouseEvent->pos(), pMouseEvent->globalPos(),
                                    pMouseEvent->buttons(), 0, Qt::Horizontal))
@@ -758,7 +766,9 @@ bool UIMouseHandler::eventFilter(QObject *pWatched, QEvent *pEvent)
                     m_iLastMouseWheelDelta += pWheelEvent->delta();
                     if (qAbs(m_iLastMouseWheelDelta) >= 120)
                     {
-                        iDelta = m_iLastMouseWheelDelta;
+                        /* Rounding iDelta to the nearest multiple of 120: */
+                        iDelta = m_iLastMouseWheelDelta / 120;
+                        iDelta *= 120;
                         m_iLastMouseWheelDelta = m_iLastMouseWheelDelta % 120;
                     }
                     if (mouseEvent(pWheelEvent->type(), uScreenId,
