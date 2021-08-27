f86413e9207de1c3c55c6a140b96c557c01ba47d
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2021-08-27 12:41:27 +0000

    VMM: Darwin/clang build fix. bugref:10086
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@90952 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/VMM/VMMR3/VMM.cpp
+++ src/VBox/VMM/VMMR3/VMM.cpp
@@ -815,18 +815,6 @@ static DECLCALLBACK(int) vmmR3LogFlusher(RTTHREAD hThreadSelf, void *pvUser)
     pVM->vmm.s.LogFlusherItem.u32 = UINT32_MAX;
 
     /*
-     * Logger getter functions.
-     */
-    typedef DECLCALLBACKPTR(PRTLOGGER, PFNGETLOGGER, ());
-    static const PFNGETLOGGER s_apfnLogGetters[] =
-    {
-        RTLogGetDefaultInstance,
-        RTLogRelGetDefaultInstance,
-    };
-    Assert(RTLogGetDefaultInstance == s_apfnLogGetters[VMMLOGGER_IDX_REGULAR]);
-    Assert(RTLogRelGetDefaultInstance == s_apfnLogGetters[VMMLOGGER_IDX_RELEASE]);
-
-    /*
      * The work loop.
      */
     for (;;)
@@ -863,7 +851,8 @@ static DECLCALLBACK(int) vmmR3LogFlusher(RTTHREAD hThreadSelf, void *pvUser)
                             LogAlways(("*FLUSH* idCpu=%u idxLogger=%u idxBuffer=%u cbToFlush=%#x fFlushed=%RTbool cbDropped=%#x\n",
                                        Item.s.idCpu, Item.s.idxLogger, Item.s.idxBuffer, cbToFlush,
                                        pShared->AuxDesc.fFlushedIndicator, pShared->cbDropped));
-                            PRTLOGGER const pLogger = s_apfnLogGetters[Item.s.idxLogger]();
+                            PRTLOGGER const pLogger = Item.s.idxLogger == VMMLOGGER_IDX_REGULAR
+                                                    ? RTLogGetDefaultInstance() : RTLogRelGetDefaultInstance();
                             if (pLogger)
                                 RTLogBulkWrite(pLogger, pchBufR3, cbToFlush);
                             LogAlways(("*FLUSH DONE*\n"));
