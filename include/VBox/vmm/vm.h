8e708b812e57af1d7638d74dedd6f445fbbb686f
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2021-08-27 14:03:28 +0000

    VMM: VMCPU structure padding adjustment for windows. bugref:10086
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@90959 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- include/VBox/vmm/vm.h
+++ include/VBox/vmm/vm.h
@@ -218,7 +218,7 @@ typedef struct VMCPU
 #ifdef VMM_INCLUDED_SRC_include_VMMInternal_h
         struct VMMCPU       s;
 #endif
-        uint8_t             padding[960];       /* multiple of 64 */
+        uint8_t             padding[1152];       /* multiple of 64 */
     } vmm;
 
     /** PDM part. */
@@ -281,7 +281,7 @@ typedef struct VMCPU
     STAMPROFILEADV          aStatAdHoc[8];                          /* size: 40*8 = 320 */
 
     /** Align the following members on page boundary. */
-    uint8_t                 abAlignment2[3128];
+    uint8_t                 abAlignment2[2936];
 
     /** PGM part. */
     union VMCPUUNIONPGM
