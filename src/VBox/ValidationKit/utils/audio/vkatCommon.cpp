c4eed621f5f8f7b1dc77db60bda2dfadcf6f2b15
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2021-09-09 17:09:38 +0000

    Audio/Validation Kit: Some more diagnostics to find out why some testbox guests refuse to play any test tones. ​bugref:10008
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@91184 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/ValidationKit/utils/audio/vkatCommon.cpp
+++ src/VBox/ValidationKit/utils/audio/vkatCommon.cpp
@@ -305,7 +305,8 @@ int audioTestPlayTone(PAUDIOTESTENV pTstEnv, PAUDIOTESTSTREAM pStream, PAUDIOTES
             }
 
             uint32_t       cbPlayed   = 0;
-            uint32_t const cbCanWrite = AudioTestMixStreamGetWritable(&pStream->Mix);
+            AudioTestMixStreamGetWritable(&pStream->Mix);
+            uint32_t const cbCanWrite = PDMAudioPropsMilliToBytes(pMix->pProps, pStream->Cfg.Device.cMsSchedulingHint);
             if (cbCanWrite)
             {
                 uint32_t const cbToGenerate = RT_MIN(RT_MIN(cbToPlayTotal - cbPlayedTotal, sizeof(abBuf)), cbCanWrite);
