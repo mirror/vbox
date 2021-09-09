63da1fd3e4b501ce742c34590c8b0ac72295e64e
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2021-09-09 06:05:34 +0000

    Audio/Validation Kit: Some more diagnostics to find out why some testbox guests refuse to play any test tones [build fix]. ​bugref:10008
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@91173 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Devices/Audio/DrvHostAudioPulseAudio.cpp
+++ src/VBox/Devices/Audio/DrvHostAudioPulseAudio.cpp
@@ -155,12 +155,10 @@ typedef struct DRVHSTAUDPASTREAM
     pa_operation           *pTriggerOp;
     /** Internal byte offset. */
     uint64_t                offInternal;
-#ifdef LOG_ENABLED
     /** Creation timestamp (in microsecs) of stream playback / recording. */
     pa_usec_t               tsStartUs;
     /** Timestamp (in microsecs) when last read from / written to the stream. */
     pa_usec_t               tsLastReadWrittenUs;
-#endif
     /** Number of occurred audio data underflows. */
     uint32_t                cUnderflows;
     /** Pulse sample format and attribute specification. */
