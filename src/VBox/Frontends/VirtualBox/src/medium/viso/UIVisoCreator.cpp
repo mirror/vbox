70cb30010d3a30411c8c23422576e066d90c3025
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2021-09-17 11:23:17 +0000

    FE/Qt: bugref:9080. Adding help button to VISO creator.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@91296 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Frontends/VirtualBox/src/medium/viso/UIVisoCreator.cpp
+++ src/VBox/Frontends/VirtualBox/src/medium/viso/UIVisoCreator.cpp
@@ -26,6 +26,7 @@
 #include "UIDesktopWidgetWatchdog.h"
 #include "UIExtraDataManager.h"
 #include "UIIconPool.h"
+#include "UIMessageCenter.h"
 #include "QIToolBar.h"
 #include "UIVisoHostBrowser.h"
 #include "UIVisoCreator.h"
@@ -323,9 +324,15 @@ void UIVisoCreator::prepareWidgets()
     m_pButtonBox = new QIDialogButtonBox;
     if (m_pButtonBox)
     {
-        m_pButtonBox->setStandardButtons(QDialogButtonBox::Cancel | QDialogButtonBox::Ok);
+        m_pButtonBox->setStandardButtons(QDialogButtonBox::Help | QDialogButtonBox::Cancel | QDialogButtonBox::Ok);
         m_pButtonBox->button(QDialogButtonBox::Cancel)->setShortcut(Qt::Key_Escape);
         m_pMainLayout->addWidget(m_pButtonBox, 4, 3, 1, 5, Qt::AlignRight);
+
+        connect(m_pButtonBox->button(QIDialogButtonBox::Help), &QPushButton::pressed,
+                &(msgCenter()), &UIMessageCenter::sltHandleHelpRequest);
+        m_pButtonBox->button(QDialogButtonBox::Help)->setShortcut(QKeySequence::HelpContents);
+
+        uiCommon().setHelpKeyword(m_pButtonBox->button(QIDialogButtonBox::Help), "viso");
     }
 }
 
