c657ac9e3b0f36b7034a8392c54a99f5449e1451
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2021-09-22 15:17:10 +0000

    Runtime/common/vfs/vfsmemory.cpp: Quick and dirty implementation for the SetSize callback when the caller wants to grow the file. Used for bugref:10098
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@91330 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Runtime/common/vfs/vfsmemory.cpp
+++ src/VBox/Runtime/common/vfs/vfsmemory.cpp
@@ -691,7 +691,17 @@ static DECLCALLBACK(int) rtVfsMemFile_QuerySize(void *pvThis, uint64_t *pcbFile)
  */
 static DECLCALLBACK(int) rtVfsMemFile_SetSize(void *pvThis, uint64_t cbFile, uint32_t fFlags)
 {
-    NOREF(pvThis); NOREF(cbFile); NOREF(fFlags);
+    AssertReturn(RTVFSFILE_SIZE_F_IS_VALID(fFlags), VERR_INVALID_PARAMETER);
+
+    PRTVFSMEMFILE pThis = (PRTVFSMEMFILE)pvThis;
+    if (   (fFlags & RTVFSFILE_SIZE_F_ACTION_MASK) == RTVFSFILE_SIZE_F_NORMAL
+        && (RTFOFF)cbFile >= pThis->Base.ObjInfo.cbObject)
+    {
+        /* Growing is just a matter of increasing the size of the object. */
+        pThis->Base.ObjInfo.cbObject = cbFile;
+        return VINF_SUCCESS;
+    }
+
     AssertMsgFailed(("Lucky you! You get to implement this (or bug bird about it).\n"));
     return VERR_NOT_IMPLEMENTED;
 }
