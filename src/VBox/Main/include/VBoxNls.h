49b6820cfd47e117f7e3294f6d84a32a1a0b037e
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2021-09-27 14:10:37 +0000

    Main: bugref:1909: Fixed scm issues
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@91402 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Main/include/VBoxNls.h
+++ src/VBox/Main/include/VBoxNls.h
@@ -1,9 +1,10 @@
+/* $Id$ */
 /** @file
  * VBox NLS.
  */
 
 /*
- * Copyright (C) 2006-2020 Oracle Corporation
+ * Copyright (C) 2020 Oracle Corporation
  *
  * This file is part of VirtualBox Open Source Edition (OSE), as
  * available from http://www.virtualbox.org. This file is free software;
@@ -12,19 +13,11 @@
  * Foundation, in version 2 as it comes in the "COPYING" file of the
  * VirtualBox OSE distribution. VirtualBox OSE is distributed in the
  * hope that it will be useful, but WITHOUT ANY WARRANTY of any kind.
- *
- * The contents of this file may alternatively be used under the terms
- * of the Common Development and Distribution License Version 1.0
- * (CDDL) only, as it comes in the "COPYING.CDDL" file of the
- * VirtualBox OSE distribution, in which case the provisions of the
- * CDDL are applicable instead of those of the GPL.
- *
- * You may elect to license modified versions of this file under the
- * terms and conditions of either the GPL or the CDDL or both.
  */
 
-#ifndef VBOX_INCLUDED_VBoxNls_h
-#define VBOX_INCLUDED_VBoxNls_h
+
+#ifndef MAIN_INCLUDED_VBoxNls_h
+#define MAIN_INCLUDED_VBoxNls_h
 #ifndef RT_WITHOUT_PRAGMA_ONCE
 # pragma once
 #endif
@@ -58,5 +51,5 @@ struct ctx \
 };
 #endif
 
-#endif /* !VBOX_INCLUDED_VBoxNls_h */
+#endif /* !MAIN_INCLUDED_VBoxNls_h */
 
