e3aa84299ea5c9acefae15262a1f706f64bc7809
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2021-09-27 10:46:15 +0000

    Main/ExtPackManagerImpl: Use RTPathXxxx function instead of RTStrPrintf2 to construct paths! bugref:1909
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@91388 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Main/src-all/ExtPackManagerImpl.cpp
+++ src/VBox/Main/src-all/ExtPackManagerImpl.cpp
@@ -820,14 +820,16 @@ HRESULT ExtPack::initWithDir(VirtualBox *a_pVirtualBox, VBOXEXTPACKCTX a_enmCont
     if (m->pReg != NULL && m->pReg->pszNlsBaseName != NULL)
     {
         char szPath[RTPATH_MAX];
-        ssize_t cchOkay = RTStrPrintf2(szPath, sizeof(szPath), "%s%s%s", a_pszDir,
-                                       RTPATH_SLASH_STR "nls" RTPATH_SLASH_STR,
-                                       m->pReg->pszNlsBaseName);
-        if (cchOkay > 0)
+        rc = RTPathJoin(szPath, sizeof(szPath), a_pszDir, "nls");
+        if (RT_SUCCESS(rc))
         {
-            rc = VirtualBoxTranslator::registerTranslation(szPath, false, &m->pTrComponent);
-            if (RT_FAILURE(rc))
-                m->pTrComponent = NULL;
+            rc = RTPathAppend(szPath, sizeof(szPath), m->pReg->pszNlsBaseName);
+            if (RT_SUCCESS(rc))
+            {
+                rc = VirtualBoxTranslator::registerTranslation(szPath, false, &m->pTrComponent);
+                if (RT_FAILURE(rc))
+                    m->pTrComponent = NULL;
+            }
         }
     }
 #endif
