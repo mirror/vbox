00fb47e96dee76ff7c183dec3be867731ca578cb
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2021-09-29 13:02:49 +0000

    FE/Qt: bugref:9996: Implementing UIToolBox::setPageEnabled.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@91455 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Frontends/VirtualBox/src/widgets/UIToolBox.cpp
+++ src/VBox/Frontends/VirtualBox/src/widgets/UIToolBox.cpp
@@ -411,8 +411,7 @@ QSize UIToolBox::minimumSizeHint() const
 
 void UIToolBox::setPageEnabled(int iIndex, bool fEnabled)
 {
-    Q_UNUSED(fEnabled);
-    Q_UNUSED(iIndex);
+    m_pages.value(iIndex)->setEnabled(fEnabled);
 }
 
 void UIToolBox::setPageTitle(int iIndex, const QString &strTitle)
