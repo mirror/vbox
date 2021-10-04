bc52234859bcc15897083f8f164292f9b6026a9f
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2021-10-04 12:51:41 +0000

    FE/Qt: bugref:9996: Forgot something in r147232.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@91541 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Frontends/VirtualBox/src/widgets/UIApplianceExportEditorWidget.cpp
+++ src/VBox/Frontends/VirtualBox/src/widgets/UIApplianceExportEditorWidget.cpp
@@ -24,14 +24,12 @@
 #include "UIMessageCenter.h"
 
 
-/*********************************************************************************************************************************
-*   Class ExportSortProxyModel implementation.                                                                                   *
-*********************************************************************************************************************************/
-
-class ExportSortProxyModel: public UIApplianceSortProxyModel
+/** UIApplianceSortProxyModel subclass for Export Appliance wizard. */
+class ExportSortProxyModel : public UIApplianceSortProxyModel
 {
 public:
 
+    /** Constructs proxy model passing @a pParent to the base-class. */
     ExportSortProxyModel(QObject *pParent = 0)
         : UIApplianceSortProxyModel(pParent)
     {
