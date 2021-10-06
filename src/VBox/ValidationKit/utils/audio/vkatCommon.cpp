a5b02931623913c66ce991662bc9018109e74f72
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2021-10-06 17:35:59 +0000

    Audio/Validation Kit: Implemented setting the system's master volume to 100% on OSS. Untested [build fix]. ​​bugref:10008
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@91602 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/ValidationKit/utils/audio/vkatCommon.cpp
+++ src/VBox/ValidationKit/utils/audio/vkatCommon.cpp
@@ -39,6 +39,7 @@
 # include "DrvHostAudioAlsaStubs.h"
 #endif
 #ifdef VBOX_WITH_AUDIO_OSS
+# include <errno.h>
 # include <fcntl.h>
 # include <sys/ioctl.h>
 # include <sys/mman.h>
@@ -256,7 +257,7 @@ int audioTestSetMasterVolume(unsigned uVolPercent)
         return rc;
 #endif
 
-   RT_NOREF(uVolPercent);
+   RT_NOREF(rc, uVolPercent);
     /** @todo Port other platforms. */
    return VERR_NOT_SUPPORTED;
 }
