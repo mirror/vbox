9bd648ffddcb3364230d2f45e29f64f1ce3d0264
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2021-10-11 08:49:23 +0000

    FE/Qt: 9974. scm fix.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@91657 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Frontends/VirtualBox/src/runtime/UIBootTimeErrorDialog.h
+++ src/VBox/Frontends/VirtualBox/src/runtime/UIBootTimeErrorDialog.h
@@ -15,8 +15,8 @@
  * hope that it will be useful, but WITHOUT ANY WARRANTY of any kind.
  */
 
-#ifndef FEQT_INCLUDED_SRC_medium_UIBootTimeErrorDialog_h
-#define FEQT_INCLUDED_SRC_medium_UIBootTimeErrorDialog_h
+#ifndef FEQT_INCLUDED_SRC_runtime_UIBootTimeErrorDialog_h
+#define FEQT_INCLUDED_SRC_runtime_UIBootTimeErrorDialog_h
 #ifndef RT_WITHOUT_PRAGMA_ONCE
 # pragma once
 #endif
@@ -102,4 +102,4 @@ private:
     CMachine              m_comMachine;
 };
 
-#endif /* !FEQT_INCLUDED_SRC_medium_UIBootTimeErrorDialog_h */
+#endif /* !FEQT_INCLUDED_SRC_runtime_UIBootTimeErrorDialog_h */
