5ad126faad26215a5c233ab8c4e822a7ed76d362
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2021-10-17 17:48:02 +0000

    IPRT/strprintf: Prep for splitting out the ellipsis functions. bugref:10124
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@91787 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Runtime/common/string/strprintf.cpp
+++ src/VBox/Runtime/common/string/strprintf.cpp
@@ -98,21 +98,24 @@ static DECLCALLBACK(size_t) strbufoutput(void *pvArg, const char *pachChars, siz
 }
 
 
-RTDECL(size_t) RTStrPrintf(char *pszBuffer, size_t cchBuffer, const char *pszFormat, ...)
+RTDECL(size_t) RTStrPrintfV(char *pszBuffer, size_t cchBuffer, const char *pszFormat, va_list args)
 {
-    /* Explicitly inline RTStrPrintfV + RTStrPrintfExV here because this is a frequently use API. */
     STRBUFARG Arg;
-    va_list args;
-    size_t cbRet;
-
     AssertMsgReturn(cchBuffer, ("Excellent idea! Format a string with no space for the output!\n"), 0);
     Arg.psz = pszBuffer;
     Arg.cch = cchBuffer - 1;
+    return RTStrFormatV(strbufoutput, &Arg, NULL, NULL, pszFormat, args);
+}
+RT_EXPORT_SYMBOL(RTStrPrintfV);
 
-    va_start(args, pszFormat);
-    cbRet = RTStrFormatV(strbufoutput, &Arg, NULL, NULL, pszFormat, args);
-    va_end(args);
 
+RTDECL(size_t) RTStrPrintf(char *pszBuffer, size_t cchBuffer, const char *pszFormat, ...)
+{
+    size_t  cbRet;
+    va_list va;
+    va_start(va, pszFormat);
+    cbRet = RTStrPrintfV(pszBuffer, cchBuffer, pszFormat, va);
+    va_end(va);
     return cbRet;
 }
 RT_EXPORT_SYMBOL(RTStrPrintf);
@@ -128,12 +131,6 @@ RTDECL(size_t) RTStrPrintfExV(PFNSTRFORMAT pfnFormat, void *pvArg, char *pszBuff
 }
 RT_EXPORT_SYMBOL(RTStrPrintfExV);
 
-
-RTDECL(size_t) RTStrPrintfV(char *pszBuffer, size_t cchBuffer, const char *pszFormat, va_list args)
-{
-    return RTStrPrintfExV(NULL, NULL, pszBuffer, cchBuffer, pszFormat, args);
-}
-RT_EXPORT_SYMBOL(RTStrPrintfV);
 
 
 RTDECL(size_t) RTStrPrintfEx(PFNSTRFORMAT pfnFormat, void *pvArg, char *pszBuffer, size_t cchBuffer, const char *pszFormat, ...)
