2f646794b58bc733f9d5051010e827d47b172532
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2021-10-21 13:49:59 +0000

    VMM,Devices: Eliminate direct calls to DBGFR3* and use the device helper callbacks, bugref:10074 [build fix]
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@91950 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Devices/VirtIO/Virtio.cpp
+++ src/VBox/Devices/VirtIO/Virtio.cpp
@@ -701,7 +701,7 @@ void vpciR3DumpState(PPDMDEVINS pDevIns, PVPCISTATE pThis, const char *pcszCalle
     }
 }
 # else
-#  define vpciR3DumpState(x, s)  do {} while (0)
+#  define vpciR3DumpState(d, x, s)  do {} while (0)
 # endif
 
 /**
