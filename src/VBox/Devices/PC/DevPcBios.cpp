6566e617847f58a9a5e53feae79f158927fbb37d
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2021-10-21 15:05:23 +0000

    VMM,DevPciBios: Add helper callback for PDMR3QueryLun and make use of it, also replace CPUMGetGuestMicroarch with PDMDevHlpCpuGetGuestMicroarch, bugref:10074 [build fix]
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@91961 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Devices/PC/DevPcBios.cpp
+++ src/VBox/Devices/PC/DevPcBios.cpp
@@ -1499,7 +1499,7 @@ static DECLCALLBACK(int)  pcbiosConstruct(PPDMDEVINS pDevIns, int iInstance, PCF
     /*
      * Get the CPU arch so we can load the appropriate ROMs.
      */
-    CPUMMICROARCH const enmMicroarch = pVM ? PDMDevHlpCpuGetGuestMicroarch(pDevIns) : kCpumMicroarch_Intel_P6;
+    CPUMMICROARCH const enmMicroarch = PDMDevHlpCpuGetGuestMicroarch(pDevIns);
 
     if (pThis->pszPcBiosFile)
     {
