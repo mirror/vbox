ff4c7e692a25becce27a3ec28d3b2269b9f7579d
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2021-10-29 09:30:22 +0000

    VMNet: bugref:9932 Explicit handling of interface start timeout
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@92140 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Devices/Network/DrvVMNet.m
+++ src/VBox/Devices/Network/DrvVMNet.m
@@ -327,7 +327,7 @@ static vmnet_return_t drvVMNetAttach(PDRVVMNET pThis)
 {
     xpc_object_t interface_desc;
     dispatch_semaphore_t operation_done;
-    __block vmnet_return_t vmnet_status = VMNET_SUCCESS;
+    __block vmnet_return_t vmnet_status = VMNET_FAILURE;
     __block size_t max_packet_size = 0;
     //__block RTMAC MacAddress;
 
@@ -399,7 +399,11 @@ static vmnet_return_t drvVMNetAttach(PDRVVMNET pThis)
         }
         dispatch_semaphore_signal(operation_done);
     });
-    dispatch_semaphore_wait(operation_done, dispatch_time(DISPATCH_TIME_NOW, RT_NS_10SEC));
+    if (dispatch_semaphore_wait(operation_done, dispatch_time(DISPATCH_TIME_NOW, RT_NS_10SEC)))
+    {
+        LogRel(("VMNet: Failed to start VMNET interface due to time out!\n"));
+        return VMNET_FAILURE;
+    }
 
     if (vmnet_status != VMNET_SUCCESS)
         return vmnet_status;
