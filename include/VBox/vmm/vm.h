a84efe18b5181df9ad60ce900ef7d1e04c5facb6
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2021-11-11 11:05:15 +0000

    include/VBox/vmm/vm.h: Reserve some more space for the NEM part of VMCPU, taking form the alignment, bugref:9044 [fix]
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@92358 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- include/VBox/vmm/vm.h
+++ include/VBox/vmm/vm.h
@@ -191,7 +191,7 @@ typedef struct VMCPU
 #ifdef VMM_INCLUDED_SRC_include_NEMInternal_h
         struct NEMCPU       s;
 #endif
-        uint8_t             padding[2560];      /* multiple of 64 */
+        uint8_t             padding[4608];      /* multiple of 64 */
     } nem;
 
     /** TRPM part. */
@@ -281,7 +281,7 @@ typedef struct VMCPU
     STAMPROFILEADV          aStatAdHoc[8];                          /* size: 40*8 = 320 */
 
     /** Align the following members on page boundary. */
-    uint8_t                 abAlignment2[696];
+    uint8_t                 abAlignment2[2744];
 
     /** PGM part. */
     union VMCPUUNIONPGM
