5940bcf42bb4c5068c27d0e4f65472fb47c85c10
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2021-11-14 16:48:22 +0000

    FE/Qt: bugref:10141. Typo
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@92416 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Frontends/VirtualBox/src/runtime/UIBootFailureDialog.cpp
+++ src/VBox/Frontends/VirtualBox/src/runtime/UIBootFailureDialog.cpp
@@ -92,7 +92,7 @@ void UIBootFailureDialog::retranslateUi()
 
     if (m_pLabel)
         m_pLabel->setText(tr("The virtual machine failed to boot. That might be caused by a missing operating system "
-                             "or misconfigured boot order. Mounting an operation system install DVD might solve this problem. "
+                             "or misconfigured boot order. Mounting an operating system install DVD might solve this problem. "
                              "Selecting an ISO file will attempt to mount it after the dialog is closed."));
 
     if (m_pBootImageLabel)
