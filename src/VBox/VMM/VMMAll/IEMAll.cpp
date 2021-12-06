7c4c2f9a5e893acfe773ca1d417f41fd1810eb9e
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2021-12-06 09:48:13 +0000

    VMM: Nested VMX: bugref:10092 Assert that we don't support accessed/dirty bit with EPT paging yet.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@92757 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/VMM/VMMAll/IEMAll.cpp
+++ src/VBox/VMM/VMMAll/IEMAll.cpp
@@ -8251,8 +8251,8 @@ iemMemPageTranslateAndCheckAccess(PVMCPUCC pVCpu, RTGCPTR GCPtrMem, uint32_t fAc
     {
         int rc2 = PGMGstModifyPage(pVCpu, GCPtrMem, 1, fAccessedDirty, ~(uint64_t)fAccessedDirty);
         AssertRC(rc2);
-        /** @todo Nested VMX: Accessed/dirty bit updates might fail and cause EPT
-         *        violations/EPT misconfigs. */
+        /** @todo Nested VMX: Accessed/dirty bit currently not supported, asserted below. */
+        Assert(!(CPUMGetGuestIa32VmxEptVpidCap(pVCpu) & VMX_BF_EPT_VPID_CAP_ACCESS_DIRTY_MASK));
     }
 
     RTGCPHYS const GCPhys = Walk.GCPhys | (GCPtrMem & PAGE_OFFSET_MASK);
