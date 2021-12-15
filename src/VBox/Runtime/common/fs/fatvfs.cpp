66d3ad76df10ccb747667d3bd98bbf84cdfa4324
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2021-12-15 08:59:15 +0000

    IPRT/fatvfs: GCC 11 build fix.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@92914 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Runtime/common/fs/fatvfs.cpp
+++ src/VBox/Runtime/common/fs/fatvfs.cpp
@@ -914,7 +914,7 @@ static int rtFsFatClusterMap_FlushWorker(PRTFSFATVOL pThis, uint32_t const iFirs
     int      rc      = VINF_SUCCESS;
     uint64_t off     = UINT64_MAX;
     uint64_t offEdge = UINT64_MAX;
-    RTSGSEG  aSgSegs[8];
+    RTSGSEG  aSgSegs[8] = { 0 }; /* Initialization required for GCC >= 11. */
     RTSGBUF  SgBuf;
     RTSgBufInit(&SgBuf, aSgSegs, RT_ELEMENTS(aSgSegs));
     SgBuf.cSegs = 0; /** @todo RTSgBuf API is stupid, make it smarter. */
