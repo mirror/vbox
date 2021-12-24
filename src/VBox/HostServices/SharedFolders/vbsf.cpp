bc2318192349d5a899424afdcbf208ff5129d4fb
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2021-12-24 16:11:55 +0000

    SharedFoldersSvc: Copy out the short name to help run DOS and WinOS2 programs on Windows hosts.  Seems we never returned these, always setting cucShortName to zero. ticketref:19453
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@93075 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/HostServices/SharedFolders/vbsf.cpp
+++ src/VBox/HostServices/SharedFolders/vbsf.cpp
@@ -1753,8 +1753,15 @@ int vbsfDirList(SHFLCLIENTDATA *pClient, SHFLROOT root, SHFLHANDLE Handle, SHFLS
         pDirEntry->Info.Attr.fMode |= 0111;
 #endif
         vbfsCopyFsObjInfoFromIprt(&pSFDEntry->Info, &pDirEntry->Info);
-        pSFDEntry->cucShortName = 0;
 
+        /* The shortname (only used by OS/2 atm): */
+        Assert(pDirEntry->cwcShortName < RT_ELEMENTS(pSFDEntry->uszShortName));
+        Assert(pDirEntry->wszShortName[pDirEntry->cwcShortName] == '\0');
+        pSFDEntry->cucShortName = pDirEntry->cwcShortName;
+        if (pDirEntry->cwcShortName)
+            memcpy(pSFDEntry->uszShortName, pDirEntry->wszShortName, sizeof(pSFDEntry->uszShortName));
+
+        /* The name: */
         if (fUtf8)
         {
             void *src, *dst;
@@ -1809,6 +1816,7 @@ int vbsfDirList(SHFLCLIENTDATA *pClient, SHFLROOT root, SHFLHANDLE Handle, SHFLS
             cbNeeded = RT_OFFSETOF(SHFLDIRINFO, name.String) + pSFDEntry->name.u16Size;
         }
 
+        /* Advance */
         pSFDEntry   = (PSHFLDIRINFO)((uintptr_t)pSFDEntry + cbNeeded);
         *pcbBuffer += cbNeeded;
         cbBufferOrg-= cbNeeded;
