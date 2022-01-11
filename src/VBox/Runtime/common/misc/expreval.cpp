7fef73dd50e9107850bbf0696095c96d1a6fdce5
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2022-01-11 01:09:45 +0000

    iprt/RTExprEval: Early code for a 'simple' expression evaluator a la /bin/expr and such. [scm+doxygen] bugref:9781
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@93174 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Runtime/common/misc/expreval.cpp
+++ src/VBox/Runtime/common/misc/expreval.cpp
@@ -34,7 +34,7 @@
 #include <iprt/asm.h>
 #include <iprt/assert.h>
 #include <iprt/ctype.h>
-#include <iprt/errcore.h>
+#include <iprt/err.h>
 #include <iprt/mem.h>
 #include <iprt/path.h>
 #include <iprt/string.h>
