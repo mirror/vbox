e59d31246e683ceb7b39009fafde565df5b2968c
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2022-01-12 15:17:28 +0000

    Additions/VbglR3: Use RTStrCopy() in VbglR3HGCMConnect().
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@93201 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Additions/common/VBoxGuest/lib/VBoxGuestR3LibHGCM.cpp
+++ src/VBox/Additions/common/VBoxGuest/lib/VBoxGuestR3LibHGCM.cpp
@@ -44,13 +44,17 @@
  */
 VBGLR3DECL(int) VbglR3HGCMConnect(const char *pszServiceName, HGCMCLIENTID *pidClient)
 {
+    AssertPtrReturn(pszServiceName, VERR_INVALID_POINTER);
+    AssertPtrReturn(pidClient,      VERR_INVALID_POINTER);
+
     VBGLIOCHGCMCONNECT Info;
     RT_ZERO(Info);
     VBGLREQHDR_INIT(&Info.Hdr, HGCM_CONNECT);
     Info.u.In.Loc.type = VMMDevHGCMLoc_LocalHost_Existing;
-    strcpy(Info.u.In.Loc.u.host.achName, pszServiceName);
-
-    int rc = vbglR3DoIOCtl(VBGL_IOCTL_HGCM_CONNECT, &Info.Hdr, sizeof(Info));
+    int rc = RTStrCopy(Info.u.In.Loc.u.host.achName, sizeof(Info.u.In.Loc.u.host.achName), pszServiceName);
+    if (RT_FAILURE(rc))
+        return rc;
+    rc = vbglR3DoIOCtl(VBGL_IOCTL_HGCM_CONNECT, &Info.Hdr, sizeof(Info));
     if (RT_SUCCESS(rc))
         *pidClient = Info.u.Out.idClient;
     return rc;
