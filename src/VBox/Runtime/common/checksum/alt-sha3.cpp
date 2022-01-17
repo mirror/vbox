bf9a566fe1849b0980fdd8234183ee7ebf8bb74c
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2022-01-17 19:23:46 +0000

    IPRT/sha3: Fix field name inconsistency for big endian architectures.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@93281 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Runtime/common/checksum/alt-sha3.cpp
+++ src/VBox/Runtime/common/checksum/alt-sha3.cpp
@@ -96,7 +96,7 @@ static void rtSha3Keccak(RTSHA3ALTPRIVATECTX *pState)
 #ifdef RT_BIG_ENDIAN
     /* This sucks a performance wise on big endian systems, sorry.  We just
        needed something simple that works on AMD64 and x86. */
-    for (size_t i = 0; i < RT_ELEMENTS(pState->aState); i++)
+    for (size_t i = 0; i < RT_ELEMENTS(pState->au64); i++)
         pState->au64[i] = RT_LE2H_U64(pState->au64[i]);
 #endif
 
@@ -276,8 +276,8 @@ static void rtSha3Keccak(RTSHA3ALTPRIVATECTX *pState)
     }
 
 #ifdef RT_BIG_ENDIAN
-    for (size_t i = 0; i < RT_ELEMENTS(pState->au64State); i++)
-        pState->au64State[i] = RT_H2LE_U64(pState->au64State[i]);
+    for (size_t i = 0; i < RT_ELEMENTS(pState->au64); i++)
+        pState->au64[i] = RT_H2LE_U64(pState->au64[i]);
 #endif
 }
 
