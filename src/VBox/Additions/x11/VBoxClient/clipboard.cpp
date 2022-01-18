ea7a5199951911fb2254f2f44846c14ba8a62344
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2022-01-18 16:06:06 +0000

    Shared Clipboard/X11: Renamed mandatory (own) X11 callbacks to better point out the data flow, added some docs [build fix].
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@93327 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Additions/x11/VBoxClient/clipboard.cpp
+++ src/VBox/Additions/x11/VBoxClient/clipboard.cpp
@@ -57,7 +57,6 @@ SHCLFUSECTX g_FuseCtx;
 #endif
 
 
-/** @copydoc ShClX11RequestDataCallback */
 DECLCALLBACK(int) ShClX11RequestDataCallback(PSHCLCONTEXT pCtx, SHCLFORMAT uFmt, void **ppv, uint32_t *pcb)
 {
     LogFlowFunc(("pCtx=%p, uFmt=%#x\n", pCtx, uFmt));
@@ -143,7 +142,6 @@ struct CLIPREADCBREQ
     SHCLFORMAT Format;
 };
 
-/** @copydoc ShClX11ReportFormatsCallback */
 DECLCALLBACK(void) ShClX11ReportFormatsCallback(PSHCLCONTEXT pCtx, SHCLFORMATS fFormats)
 {
     RT_NOREF(pCtx);
@@ -155,7 +153,6 @@ DECLCALLBACK(void) ShClX11ReportFormatsCallback(PSHCLCONTEXT pCtx, SHCLFORMATS f
     LogFlowFuncLeaveRC(rc2);
 }
 
-/** @copydoc ShClX11ReportDataCallback */
 DECLCALLBACK(void) ShClX11ReportDataCallback(PSHCLCONTEXT pCtx, int rcCompletion,
                                              CLIPREADCBREQ *pReq, void *pv, uint32_t cb)
 {
