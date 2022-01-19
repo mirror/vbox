c295450a58416a01ace098207617c0034a3e1b8e
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2022-01-19 08:38:19 +0000

    Main/UnattendedImpl: bugref:10182. Adding the string aarch64 to linux architecture detection code. Needed to detect arch. of OL 7.8 Server for example.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@93339 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Main/src-server/UnattendedImpl.cpp
+++ src/VBox/Main/src-server/UnattendedImpl.cpp
@@ -816,10 +816,11 @@ HRESULT Unattended::i_innerDetectIsoOSWindows(RTVFS hVfsIso, DETECTBUFFER *pBuf,
  */
 static bool detectLinuxArch(const char *pszArch, VBOXOSTYPE *penmOsType, VBOXOSTYPE enmBaseOsType)
 {
-    if (   RTStrNICmp(pszArch, RT_STR_TUPLE("amd64"))  == 0
-        || RTStrNICmp(pszArch, RT_STR_TUPLE("x86_64")) == 0
-        || RTStrNICmp(pszArch, RT_STR_TUPLE("x86-64")) == 0 /* just in case */
-        || RTStrNICmp(pszArch, RT_STR_TUPLE("x64"))    == 0 /* ditto */ )
+    if (   RTStrNICmp(pszArch, RT_STR_TUPLE("amd64"))   == 0
+        || RTStrNICmp(pszArch, RT_STR_TUPLE("x86_64"))  == 0
+        || RTStrNICmp(pszArch, RT_STR_TUPLE("x86-64"))  == 0 /* just in case */
+        || RTStrNICmp(pszArch, RT_STR_TUPLE("x64"))     == 0 /* ditto */
+        || RTStrNICmp(pszArch, RT_STR_TUPLE("aarch64")) == 0 /* as seen in OL 7.8 Server */)
     {
         *penmOsType = (VBOXOSTYPE)(enmBaseOsType | VBOXOSTYPE_x64);
         return true;
@@ -852,10 +853,11 @@ static bool detectLinuxArch(const char *pszArch, VBOXOSTYPE *penmOsType, VBOXOST
  */
 static bool detectLinuxArchII(const char *pszArch, VBOXOSTYPE *penmOsType, VBOXOSTYPE enmBaseOsType)
 {
-    if (   RTStrIStr(pszArch, "amd64")  != NULL
-        || RTStrIStr(pszArch, "x86_64") != NULL
-        || RTStrIStr(pszArch, "x86-64") != NULL /* just in case */
-        || RTStrIStr(pszArch, "x64")    != NULL /* ditto */ )
+    if (   RTStrIStr(pszArch, "amd64")   != NULL
+        || RTStrIStr(pszArch, "x86_64")  != NULL
+        || RTStrIStr(pszArch, "x86-64")  != NULL /* just in case */
+        || RTStrIStr(pszArch, "x64")     != NULL /* ditto */
+        || RTStrIStr(pszArch, "aarch64") != NULL /* as seen in OL 7.8 Server */)
     {
         *penmOsType = (VBOXOSTYPE)(enmBaseOsType | VBOXOSTYPE_x64);
         return true;
