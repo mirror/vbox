5db8d2f465175fac5a8498ac34e603069bc0b4e1
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2022-01-21 14:20:57 +0000

    SharedClipboard/win: scm fix. bugref:10160
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@93399 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/GuestHost/SharedClipboard/clipboard-win.cpp
+++ src/VBox/GuestHost/SharedClipboard/clipboard-win.cpp
@@ -576,7 +576,7 @@ int SharedClipboardWinConvertCFHTMLToMIME(const char *pszSource, const uint32_t 
  * For more format details, check out:
  * https://docs.microsoft.com/en-us/previous-versions/windows/internet-explorer/ie-developer/platform-apis/aa767917(v=vs.85)
  *
- * @returns VBox status code. 
+ * @returns VBox status code.
  * @param   pszSource   Source buffer that contains utf-16 string in mime html format
  * @param   cb          Size of source buffer in bytes
  * @param   ppszOutput  Where to return the allocated output buffer to put converted UTF-8
