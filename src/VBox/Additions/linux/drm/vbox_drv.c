6e0305d7aacee4abdda85cf85adbfe25685764e2
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2022-01-24 16:26:56 +0000

    Linux Additions: vboxvideo: initial support for kernel 5.17-rc1, bugref:10190.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@93416 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Additions/linux/drm/vbox_drv.c
+++ src/VBox/Additions/linux/drm/vbox_drv.c
@@ -50,6 +50,19 @@
 #include "version-generated.h"
 #include "revision-generated.h"
 
+/** Detect whether kernel mode setting is OFF. */
+#if defined(CONFIG_VGA_CONSOLE)
+# if RTLNX_VER_MIN(5,17,0)
+#  define VBOX_VIDEO_NOMODESET() drm_firmware_drivers_only() && vbox_modeset == -1
+# elif RTLNX_VER_MIN(4,7,0)
+#  define VBOX_VIDEO_NOMODESET() vgacon_text_force() && vbox_modeset == -1
+# else /* < 4.7.0 */
+# define VBOX_VIDEO_NOMODESET() 0
+# endif /* < 4.7.0 */
+#else /* !CONFIG_VGA_CONSOLE */
+# define VBOX_VIDEO_NOMODESET() 0
+#endif /* !CONFIG_VGA_CONSOLE */
+
 static int vbox_modeset = -1;
 
 MODULE_PARM_DESC(modeset, "Disable/Enable modesetting");
@@ -398,15 +411,13 @@ static struct drm_driver driver = {
 static int __init vbox_init(void)
 {
 	printk("vboxvideo: loading version " VBOX_VERSION_STRING " r" __stringify(VBOX_SVN_REV) "\n");
-#if defined(CONFIG_VGA_CONSOLE) || RTLNX_VER_MIN(4,7,0)
-	if (vgacon_text_force() && vbox_modeset == -1)
+	if (VBOX_VIDEO_NOMODESET())
 	{
 		printk("vboxvideo: kernel is running with *nomodeset* parameter,\n");
 		printk("vboxvideo: please consider either to remove it or load driver\n");
 		printk("vboxvideo: with parameter modeset=1, unloading\n");
 		return -EINVAL;
 	}
-#endif
 
 	if (vbox_modeset == 0)
 	{
