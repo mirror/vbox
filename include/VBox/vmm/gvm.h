caa7c4a1489499ce9645f457c627189bb36b80a4
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2022-02-03 21:11:33 +0000

    VMM: Embedded the zero and mmio-dummy pages into the VM structure and realigned it for a 16384 page size. [build fix] bugref:9898
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@93594 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- include/VBox/vmm/gvm.h
+++ include/VBox/vmm/gvm.h
@@ -301,7 +301,7 @@ typedef struct GVM
 #ifdef VBOX_WITH_NEM_R0
     uint8_t         abPadding2[16384 - 64 - 4352 - 1024 - 256 - 256 - 64 - 2176 - 640 - 512 - 64 - 1024 - 128 - 704 - sizeof(PGVMCPU) * VMM_MAX_CPU_COUNT];
 #else
-    uint8_t         abPadding2[16384 - 64 - 4352 - 1024 - 256 -     - 64 - 2176 - 640 - 512 - 64 - 1024 - 128 - 704 - sizeof(PGVMCPU) * VMM_MAX_CPU_COUNT];
+    uint8_t         abPadding2[16384 - 64 - 4352 - 1024 - 256 -       64 - 2176 - 640 - 512 - 64 - 1024 - 128 - 704 - sizeof(PGVMCPU) * VMM_MAX_CPU_COUNT];
 #endif
 
     /** For simplifying CPU enumeration in VMMAll code. */
