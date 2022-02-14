0ba32c878fc5bae348ea85cb8d55294a01bc186e
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2022-02-14 14:33:32 +0000

    VMM/HM: Need to postpone registering the debug info handlers further, bugref:9044
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@93729 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/VMM/VMMR3/HM.cpp
+++ src/VBox/VMM/VMMR3/HM.cpp
@@ -509,24 +509,6 @@ VMMR3_INT_DECL(int) HMR3Init(PVM pVM)
      *        VERR_SVM_IN_USE. */
     if (pVM->fHMEnabled)
     {
-        /*
-         * Register info handlers.
-         */
-        rc = DBGFR3InfoRegisterInternalEx(pVM, "hm", "Dumps HM info.", hmR3Info, DBGFINFO_FLAGS_ALL_EMTS);
-        AssertRCReturn(rc, rc);
-
-        rc = DBGFR3InfoRegisterInternalEx(pVM, "hmeventpending", "Dumps the pending HM event.", hmR3InfoEventPending,
-                                          DBGFINFO_FLAGS_ALL_EMTS);
-        AssertRCReturn(rc, rc);
-
-        rc = DBGFR3InfoRegisterInternalEx(pVM, "svmvmcbcache", "Dumps the HM SVM nested-guest VMCB cache.",
-                                          hmR3InfoSvmNstGstVmcbCache, DBGFINFO_FLAGS_ALL_EMTS);
-        AssertRCReturn(rc, rc);
-
-        rc = DBGFR3InfoRegisterInternalEx(pVM, "lbr", "Dumps the HM LBR info.", hmR3InfoLbr, DBGFINFO_FLAGS_ALL_EMTS);
-        AssertRCReturn(rc, rc);
-
-
         uint32_t fCaps;
         rc = SUPR3QueryVTCaps(&fCaps);
         if (RT_SUCCESS(rc))
@@ -653,6 +635,26 @@ VMMR3_INT_DECL(int) HMR3Init(PVM pVM)
             || pVM->bMainExecutionEngine == VM_EXEC_ENGINE_RAW_MODE
             || pVM->bMainExecutionEngine == VM_EXEC_ENGINE_HW_VIRT /* paranoia */)
             return VM_SET_ERROR(pVM, rc, "Misconfigured VM: No guest execution engine available!");
+    }
+
+    if (pVM->fHMEnabled)
+    {
+        /*
+         * Register info handlers now that HM is used for sure.
+         */
+        rc = DBGFR3InfoRegisterInternalEx(pVM, "hm", "Dumps HM info.", hmR3Info, DBGFINFO_FLAGS_ALL_EMTS);
+        AssertRCReturn(rc, rc);
+
+        rc = DBGFR3InfoRegisterInternalEx(pVM, "hmeventpending", "Dumps the pending HM event.", hmR3InfoEventPending,
+                                          DBGFINFO_FLAGS_ALL_EMTS);
+        AssertRCReturn(rc, rc);
+
+        rc = DBGFR3InfoRegisterInternalEx(pVM, "svmvmcbcache", "Dumps the HM SVM nested-guest VMCB cache.",
+                                          hmR3InfoSvmNstGstVmcbCache, DBGFINFO_FLAGS_ALL_EMTS);
+        AssertRCReturn(rc, rc);
+
+        rc = DBGFR3InfoRegisterInternalEx(pVM, "lbr", "Dumps the HM LBR info.", hmR3InfoLbr, DBGFINFO_FLAGS_ALL_EMTS);
+        AssertRCReturn(rc, rc);
     }
 
     Assert(pVM->bMainExecutionEngine != VM_EXEC_ENGINE_NOT_SET);
