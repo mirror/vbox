cab1383010790e7b3b3f2e4f12d5e30ab3af60ec
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2022-02-14 11:14:51 +0000

    Guest Control/Validation Kit: Logging nits.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@93719 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/ValidationKit/tests/additions/tdAddGuestCtrl.py
+++ src/VBox/ValidationKit/tests/additions/tdAddGuestCtrl.py
@@ -2902,12 +2902,13 @@ class SubTstDrvAddGuestCtrl(base.SubTestDriverBase):
             if fRc:
                 reporter.log('All blocking processes were terminated successfully');
 
-            # There still should be 20 processes because we just terminated the 10 blocking ones above.
             try:    cProcs = len(self.oTstDrv.oVBoxMgr.getArray(oGuestSession, 'processes'));
             except: fRc    = reporter.errorXcpt();
             else:
-                if cProcs != cProcsPerGroup * 2:
-                    fRc = reporter.error('Got %d total processes, expected %d (final)' % (cProcs, cProcsPerGroup));
+                # There still should be 20 processes because we just terminated the 10 blocking ones above.
+                cProcsExpected = cProcsPerGroup * 2;
+                if cProcs != cProcsExpected:
+                    fRc = reporter.error('Got %d total processes, expected %d (final)' % (cProcs, cProcsExpected));
                 reporter.log2('Final guest session processes count: %d' % (cProcs,));
 
         if not fRc:
