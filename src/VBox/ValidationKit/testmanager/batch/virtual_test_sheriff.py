54ecbe83ecb3f953ca2cfc8346460a973efdcd3c
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2022-02-15 23:20:01 +0000

    vsheriff: GA failure reasoning fix.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@93774 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/ValidationKit/testmanager/batch/virtual_test_sheriff.py
+++ src/VBox/ValidationKit/testmanager/batch/virtual_test_sheriff.py
@@ -1203,7 +1203,7 @@ class VirtualTestSheriff(object): # pylint: disable=too-few-public-methods
         """
         enmReason = None;
         sParentName = oFailedResult.oParent.sName if oFailedResult.oParent else '';
-        if oFailedResult.sName == 'VBoxWindowsAdditions.exe' >= 0:
+        if oFailedResult.sName == 'VBoxWindowsAdditions.exe':
             enmReason = self.ktReason_Add_Installer_Win_Failed;
         # guest control:
         elif sParentName == 'Guest Control' and oFailedResult.sName == 'Preparations':
