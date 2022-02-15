310985f1369cb98f5a710aec68b580131c61fb0c
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2022-02-15 21:57:50 +0000

    vsheriff: Another windows install failure reason.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@93767 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/ValidationKit/testmanager/batch/virtual_test_sheriff.py
+++ src/VBox/ValidationKit/testmanager/batch/virtual_test_sheriff.py
@@ -638,6 +638,7 @@ class VirtualTestSheriff(object): # pylint: disable=too-few-public-methods
     ktReason_Host_DriverNotCompilable                  = ( 'Host',              'Driver not compilable' );
     ktReason_Host_InstallationFailed                   = ( 'Host',              'Installation failed' );
     ktReason_Host_InstallationWantReboot               = ( 'Host',              'Installation want reboot' );
+    ktReason_Host_InvalidPackage                       = ( 'Host',              'ERROR_INSTALL_PACKAGE_INVALID' );
     ktReason_Host_NotSignedWithBuildCert               = ( 'Host',              'Not signed with build cert' );
     ktReason_Host_DoubleFreeHeap                       = ( 'Host',              'Double free or corruption' );
     ktReason_Host_LeftoverService                      = ( 'Host',              'Leftover service' );
@@ -876,6 +877,7 @@ class VirtualTestSheriff(object): # pylint: disable=too-few-public-methods
             # ( Whether to stop on hit, reason tuple, needle text. )
             ( True,  ktReason_Host_InstallationWantReboot, 'ERROR_SUCCESS_REBOOT_REQUIRED' ),
             ( False, ktReason_Host_InstallationFailed, 'Installation error.' ),
+            ( True,  ktReason_Host_InvalidPackage, 'Uninstaller failed, exit code: 1620' ),
         ],
     };
 
