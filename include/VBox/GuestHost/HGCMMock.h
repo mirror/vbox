2c14b850d2816b37ba6b754865db4208aa89feef
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2022-02-28 11:21:37 +0000

    Validation Kit/HGCM: Split out the HGCM testing (mocking) framework into an own header, added documentation, more code for generalizing this [build fix].
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@93972 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- include/VBox/GuestHost/HGCMMock.h
+++ include/VBox/GuestHost/HGCMMock.h
@@ -19,7 +19,17 @@
  * Foundation, in version 2 as it comes in the "COPYING" file of the
  * VirtualBox OSE distribution. VirtualBox OSE is distributed in the
  * hope that it will be useful, but WITHOUT ANY WARRANTY of any kind.
+ *
+ * The contents of this file may alternatively be used under the terms
+ * of the Common Development and Distribution License Version 1.0
+ * (CDDL) only, as it comes in the "COPYING.CDDL" file of the
+ * VirtualBox OSE distribution, in which case the provisions of the
+ * CDDL are applicable instead of those of the GPL.
+ *
+ * You may elect to license modified versions of this file under the
+ * terms and conditions of either the GPL or the CDDL or both.
  */
+
 #ifndef VBOX_INCLUDED_HGCMMock_h
 #define VBOX_INCLUDED_HGCMMock_h
 #ifndef RT_WITHOUT_PRAGMA_ONCE
@@ -47,8 +57,6 @@
 /*********************************************************************************************************************************
 *  Definitions.                                                                                                                  *
 *********************************************************************************************************************************/
-
-RT_C_DECLS_BEGIN
 
 #if defined(IN_RING3) /* Only R3 parts implemented so far. */
 
@@ -771,7 +779,5 @@ VBGLR3DECL(int) VbglR3HGCMCall(PVBGLIOCHGCMCALL pInfo, size_t cbInfo)
 }
 
 #endif /* IN_RING3 */
-
-RT_C_DECLS_END
 
 #endif /* !VBOX_INCLUDED_HGCMMock_h */
