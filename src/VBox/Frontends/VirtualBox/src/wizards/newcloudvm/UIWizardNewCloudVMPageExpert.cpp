3d6aa77c428a6affba2feb22e454ef4a79e2f0c6
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2022-03-11 09:42:17 +0000

    FE/Qt: bugref:9996: Fixing regression in New Cloud VM wizard; Two NLS tags misplaced.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@94167 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Frontends/VirtualBox/src/wizards/newcloudvm/UIWizardNewCloudVMPageExpert.cpp
+++ src/VBox/Frontends/VirtualBox/src/wizards/newcloudvm/UIWizardNewCloudVMPageExpert.cpp
@@ -213,8 +213,8 @@ void UIWizardNewCloudVMPageExpert::retranslateUi()
     /* Translate source tab-bar: */
     if (m_pSourceTabBar)
     {
-        m_pSourceTabBar->setTabText(0, UIWizardNewCloudVM::tr("&Boot Volumes"));
-        m_pSourceTabBar->setTabText(1, UIWizardNewCloudVM::tr("&Images"));
+        m_pSourceTabBar->setTabText(0, UIWizardNewCloudVM::tr("&Images"));
+        m_pSourceTabBar->setTabText(1, UIWizardNewCloudVM::tr("&Boot Volumes"));
     }
 
     /* Translate profile stuff: */
