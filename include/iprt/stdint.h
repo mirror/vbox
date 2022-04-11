31888d65aea624e23bc40e8e3862d805307a1687
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2022-04-11 00:15:07 +0000

    include/iprt/stdint.h: Supply [u]int_fast[xx]_t and [u]int_least[xx]_t types for SoftFloat ring-0 useage. bugref:9898
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@94546 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- include/iprt/stdint.h
+++ include/iprt/stdint.h
@@ -185,6 +185,56 @@ typedef int64_t             intmax_t;
 typedef uint64_t            uintmax_t;
 #   endif
 
+    /* smallest minimum-width integer types - assumes to be the same as above! */
+typedef int8_t              int_least8_t;
+typedef uint8_t             uint_least8_t;
+#   define INT_LEAST8_MIN   INT8_MIN
+#   define INT_LEAST8_MAX   INT8_MAX
+#   define UINT_LEAST8_MAX  UINT8_MAX
+typedef int16_t             int_least16_t;
+typedef uint16_t            uint_least16_t;
+#   define INT_LEAST16_MIN  INT16_MIN
+#   define INT_LEAST16_MAX  INT16_MAX
+#   define UINT_LEAST16_MAX UINT16_MAX
+typedef int32_t             int_least32_t;
+typedef uint32_t            uint_least32_t;
+#   define INT_LEAST32_MIN  INT32_MIN
+#   define INT_LEAST32_MAX  INT32_MAX
+#   define UINT_LEAST32_MAX UINT32_MAX
+typedef int64_t             int_least64_t;
+typedef uint64_t            uint_least64_t;
+#   define INT_LEAST64_MIN  INT64_MIN
+#   define INT_LEAST64_MAX  INT64_MAX
+#   define UINT_LEAST64_MAX UINT64_MAX
+
+    /* fastest minimum-width integer types */
+typedef signed char         int_fast8_t;
+typedef unsigned char       uint_fast8_t;
+#   define INT_FAST8_MIN    INT8_MIN
+#   define INT_FAST8_MAX    INT8_MAX
+#   define UINT_FAST8_MAX   UINT8_MAX
+typedef signed int          int_fast16_t;
+typedef unsigned int        uint_fast16_t;
+#   if ARCH_BITS == 16
+#    define INT_FAST16_MIN  INT16_MIN
+#    define INT_FAST16_MAX  INT16_MAX
+#    define UINT_FAST16_MAX UINT16_MAX
+#   else
+#    define INT_FAST16_MIN  INT32_MIN
+#    define INT_FAST16_MAX  INT32_MAX
+#    define UINT_FAST16_MAX UINT32_MAX
+#   endif
+typedef int32_t             int_fast32_t;
+typedef uint32_t            uint_fast32_t;
+#   define INT_FAST32_MIN   INT32_MIN
+#   define INT_FAST32_MAX   INT32_MAX
+#   define UINT_FAST32_MAX  UINT32_MAX
+typedef int64_t             int_fast64_t;
+typedef uint64_t            uint_fast64_t;
+#   define INT_FAST64_MIN   INT64_MIN
+#   define INT_FAST64_MAX   INT64_MAX
+#   define UINT_FAST64_MAX  UINT64_MAX
+
 #  else
 #   error "PORTME: Add architecture. Don't forget to check the [U]INTx_C() and [U]INTMAX_MIN/MAX macros."
 #  endif
