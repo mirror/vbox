0623b33d88b7f59f6b94d8e5ff1df7c5c464918b
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2022-04-28 18:25:09 +0000

    Main/NvramStore: Add support for encrypted NVRAM files (untested), bugref:9955 [build fix]
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@94745 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Main/src-all/NvramStoreImpl.cpp
+++ src/VBox/Main/src-all/NvramStoreImpl.cpp
@@ -869,7 +869,11 @@ int NvramStore::i_retainCryptoIf(PCVBOXCRYPTOIF *ppCryptoIf)
 #ifdef VBOX_COM_INPROC
     return m->pParent->i_retainCryptoIf(ppCryptoIf);
 #else
-    return m->pParent->i_getVirtualBox()->i_retainCryptoIf(ppCryptoIf);
+    HRESULT hrc = m->pParent->i_getVirtualBox()->i_retainCryptoIf(ppCryptoIf);
+    if (SUCCEEDED(hrc))
+        return VINF_SUCCESS;
+
+    return VERR_COM_IPRT_ERROR;
 #endif
 }
 
@@ -879,7 +883,11 @@ int NvramStore::i_releaseCryptoIf(PCVBOXCRYPTOIF pCryptoIf)
 #ifdef VBOX_COM_INPROC
     return m->pParent->i_releaseCryptoIf(pCryptoIf);
 #else
-    return m->pParent->i_getVirtualBox()->i_releaseCryptoIf(pCryptoIf);
+    HRESULT hrc = m->pParent->i_getVirtualBox()->i_releaseCryptoIf(pCryptoIf);
+    if (SUCCEEDED(hrc))
+        return VINF_SUCCESS;
+
+    return VERR_COM_IPRT_ERROR;
 #endif
 }
 
