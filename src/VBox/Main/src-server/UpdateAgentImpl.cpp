4a6fc4861cc958e7cf9424a7151151f60c980a71
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2022-04-28 13:26:10 +0000

    Main/Update check: Prevent overwriting the agent's default repo URL on loading settings when XML settings are empty. bugref:7983
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@94736 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Main/src-server/UpdateAgentImpl.cpp
+++ src/VBox/Main/src-server/UpdateAgentImpl.cpp
@@ -650,7 +650,8 @@ HRESULT UpdateAgent::i_loadSettings(const settings::UpdateAgent &data)
     m->fEnabled          = data.fEnabled;
     m->enmChannel        = data.enmChannel;
     m->uCheckFreqSeconds = data.uCheckFreqSeconds;
-    m->strRepoUrl        = data.strRepoUrl;
+    if (data.strRepoUrl.isNotEmpty()) /* Prevent overwriting the agent's default URL when XML settings are empty. */
+        m->strRepoUrl    = data.strRepoUrl;
     m->enmProxyMode      = data.enmProxyMode;
     m->strProxyUrl       = data.strProxyUrl;
     m->strLastCheckDate  = data.strLastCheckDate;
@@ -866,6 +867,8 @@ HRESULT HostUpdateAgent::checkFor(ComPtr<IProgress> &aProgress)
 DECLCALLBACK(HRESULT) HostUpdateAgent::i_checkForUpdateTask(UpdateAgentTask *pTask)
 {
     RT_NOREF(pTask);
+
+    AssertReturn(m->strRepoUrl.isNotEmpty(), E_INVALIDARG);
 
     // Following the sequence of steps in UIUpdateStepVirtualBox::sltStartStep()
     // Build up our query URL starting with the configured repository.
