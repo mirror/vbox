3b00116b71953a7adf94109ad8b1412baf7dfaa1
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2022-04-29 08:14:37 +0000

    libs/softfloat: Consolidate the build/*/platform.h stuff into a single one for the VBox targets. bugref:9898
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@94753 cfe28804-0f27-0410-a406-dd0f0b0b656f

---
+++ src/libs/softfloat-3e/build/vbox/platform.h
@@ -0,0 +1,76 @@
+/* $Id$ */
+/** @file
+ * Platform Header for all VirtualBox targets.
+ */
+
+/*
+ * Copyright (C) 2022 Oracle Corporation
+ *
+ * This file is part of VirtualBox Open Source Edition (OSE), as
+ * available from http://www.virtualbox.org. This file is free software;
+ * you can redistribute it and/or modify it under the terms of the GNU
+ * General Public License (GPL) as published by the Free Software
+ * Foundation, in version 2 as it comes in the "COPYING" file of the
+ * VirtualBox OSE distribution. VirtualBox OSE is distributed in the
+ * hope that it will be useful, but WITHOUT ANY WARRANTY of any kind.
+ */
+
+#ifndef VBOX_INCLUDED_SRC_vbox_platform_h
+#define VBOX_INCLUDED_SRC_vbox_platform_h
+#ifndef RT_WITHOUT_PRAGMA_ONCE
+# pragma once
+#endif
+
+/* For RT_LITTLE_ENDIAN, RT_ARCH_XXX and more: */
+#include <iprt/cdefs.h>
+
+/* Build config: */
+#define SOFTFLOAT_FAST_INT64            /**< We use functions guarded by this, so must be defined regardless of truthiness. */
+#define SOFTFLOAT_ROUND_ODD             /** @todo Skip this? */
+
+/* IPRT should detect endianness correctly: */
+#ifdef RT_LITTLE_ENDIAN
+# define LITTLEENDIAN 1
+#endif
+
+/* Compiler/host configuration bits: */
+#define SOFTFLOAT_FAST_DIV32TO16
+#if ARCH_BITS > 32 || defined(RT_ARCH_X86)
+# define SOFTFLOAT_FAST_DIV64TO32
+#endif
+
+/* See DECLINLINE for guidance: */
+#ifdef __GNUC__
+# define INLINE                         static __inline__
+#elif defined(__cplusplus)
+# define INLINE                         static inline
+#elif defined(_MSC_VER)
+# define INLINE                         static _inline
+#else
+# error "Port me!"
+#endif
+
+/* Include GCC optimizations: */
+#ifdef __GNUC__
+# define SOFTFLOAT_BUILTIN_CLZ          1
+# if ARCH_BITS > 32
+#  define SOFTFLOAT_INTRINSIC_INT128    1
+# endif
+# include "opts-GCC.h"
+#endif
+/** @todo do Visual C++ optimization / generic IPRT based ones. */
+
+/* We've eliminated the global variables and need no TLS variable tricks. */
+#ifndef THREAD_LOCAL
+# if 1
+#  define THREAD_LOCAL
+# else
+#  ifdef _MSC_VER
+#   define THREAD_LOCAL                 __declspec(thread)
+#  else
+#   define THREAD_LOCAL                 __thread
+#  endif
+# endif
+#endif
+
+#endif /* !VBOX_INCLUDED_SRC_vbox_platform_h */
