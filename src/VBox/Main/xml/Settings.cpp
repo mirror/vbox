489657534a315f7e47f5584641155defe1ce1551
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2022-05-03 06:59:52 +0000

    Main/Setting: Some fixes to the log file key store and ID naming, bugref:9955
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@94788 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Main/xml/Settings.cpp
+++ src/VBox/Main/xml/Settings.cpp
@@ -6138,8 +6138,8 @@ void MachineConfigFile::readMachine(const xml::ElementNode &elmMachine)
         elmMachine.getAttributeValue("stateKeyStore", strStateKeyStore);
         elmMachine.getAttributeValuePath("stateFile", strStateFile);
 
-        elmMachine.getAttributeValue("strLogKeyId", strLogKeyId);
-        elmMachine.getAttributeValue("strLogKeyStore", strLogKeyStore);
+        elmMachine.getAttributeValue("logKeyId", strLogKeyId);
+        elmMachine.getAttributeValue("logKeyStore", strLogKeyStore);
 
         if (elmMachine.getAttributeValue("currentSnapshot", str))
             parseUUID(uuidCurrentSnapshot, str, &elmMachine);
@@ -8041,9 +8041,9 @@ void MachineConfigFile::buildMachineXML(xml::ElementNode &elmMachine,
         if (strStateKeyStore.length())
             elmMachine.setAttribute("stateKeyStore", strStateKeyStore);
         if (strLogKeyId.length())
-            elmMachine.setAttribute("strLogKeyId", strLogKeyId);
+            elmMachine.setAttribute("logKeyId", strLogKeyId);
         if (strLogKeyStore.length())
-            elmMachine.setAttribute("strLogKeyStore", strLogKeyStore);
+            elmMachine.setAttribute("logKeyStore", strLogKeyStore);
     }
     if (    strStateFile.length()
          && !(fl & BuildMachineXML_SuppressSavedState)
