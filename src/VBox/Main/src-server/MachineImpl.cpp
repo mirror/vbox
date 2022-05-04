2da08f671ac22ec162f381fdc4884282541df0c2
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2022-05-04 08:53:29 +0000

    Main/MachineImpl: build fixes, bugref:9955
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@94814 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Main/src-server/MachineImpl.cpp
+++ src/VBox/Main/src-server/MachineImpl.cpp
@@ -429,7 +429,7 @@ HRESULT Machine::init(VirtualBox *aParent,
             }
 
             HRESULT hrc2 = aParent->i_releaseCryptoIf(pCryptoIf);
-            Assert(hrc2 == S_OK);
+            Assert(hrc2 == S_OK); RT_NOREF(hrc2);
 
             if (FAILED(hrc))
                 return hrc; /* Error is set. */
@@ -732,7 +732,7 @@ HRESULT Machine::initFromSettings(VirtualBox *aParent,
     if (pCryptoIf)
     {
         HRESULT hrc2 = aParent->i_releaseCryptoIf(pCryptoIf);
-        Assert(hrc2 == S_OK);
+        Assert(hrc2 == S_OK); RT_NOREF(hrc2);
     }
 #endif
 
