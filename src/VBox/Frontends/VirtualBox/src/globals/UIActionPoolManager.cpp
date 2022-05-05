eefebcccf89ecb3469dba7ae59d92ce2ff68b4e8
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2022-05-05 08:54:30 +0000

    FE/Qt: bugref:10081. Adding icons for the clear action. These will be replaced when we get the icons.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@94836 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Frontends/VirtualBox/src/globals/UIActionPoolManager.cpp
+++ src/VBox/Frontends/VirtualBox/src/globals/UIActionPoolManager.cpp
@@ -2763,12 +2763,12 @@ public:
         : UIActionSimple(pParent)
     {
         setShortcutContext(Qt::WidgetWithChildrenShortcut);
-        setIcon(0, UIIconPool::iconSetFull(":/hd_remove_32px.png",          ":/hd_remove_16px.png",
-                                           ":/hd_remove_disabled_32px.png", ":/hd_remove_disabled_16px.png"));
-        setIcon(1, UIIconPool::iconSetFull(":/cd_remove_32px.png",          ":/cd_remove_16px.png",
-                                           ":/cd_remove_disabled_32px.png", ":/cd_remove_disabled_16px.png"));
-        setIcon(2, UIIconPool::iconSetFull(":/fd_remove_32px.png",          ":/fd_remove_16px.png",
-                                           ":/fd_remove_disabled_32px.png", ":/fd_remove_disabled_16px.png"));
+        setIcon(0, UIIconPool::iconSetFull(":/hd_clear_32px.png",          ":/hd_clear_16px.png",
+                                           ":/hd_clear_disabled_32px.png", ":/hd_clear_disabled_16px.png"));
+        setIcon(1, UIIconPool::iconSetFull(":/cd_clear_32px.png",          ":/cd_clear_16px.png",
+                                           ":/cd_clear_disabled_32px.png", ":/cd_clear_disabled_16px.png"));
+        setIcon(2, UIIconPool::iconSetFull(":/fd_clear_32px.png",          ":/fd_clear_16px.png",
+                                           ":/fd_clear_disabled_32px.png", ":/fd_clear_disabled_16px.png"));
     }
 
 protected:
