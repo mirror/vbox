4b24f36d615d8020c87f681dce2a9aaa6651f932
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2022-05-10 09:45:57 +0000

    Devices/Network/lwip-new/ipv4/ip_frag.c: Missing paranthesis around ternary operator in macro, bugref:3409
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@94977 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Devices/Network/lwip-new/src/core/ipv4/ip_frag.c
+++ src/VBox/Devices/Network/lwip-new/src/core/ipv4/ip_frag.c
@@ -99,10 +99,21 @@ PACK_STRUCT_END
 #  include "arch/epstruct.h"
 #endif
 
+#ifndef VBOX
 #define IP_ADDRESSES_AND_ID_MATCH(iphdrA, iphdrB)  \
   (ip_addr_cmp(&(iphdrA)->src, &(iphdrB)->src) && \
    ip_addr_cmp(&(iphdrA)->dest, &(iphdrB)->dest) && \
    IPH_ID(iphdrA) == IPH_ID(iphdrB)) ? 1 : 0
+#else
+/*
+ * Put parantheses around the whole construct so the ternary operator works
+ * as anticipated.
+ */
+#define IP_ADDRESSES_AND_ID_MATCH(iphdrA, iphdrB)  \
+  ((ip_addr_cmp(&(iphdrA)->src, &(iphdrB)->src) && \
+    ip_addr_cmp(&(iphdrA)->dest, &(iphdrB)->dest) && \
+    IPH_ID(iphdrA) == IPH_ID(iphdrB)) ? 1 : 0)
+#endif
 
 /* global variables */
 static struct ip_reassdata *reassdatagrams;
