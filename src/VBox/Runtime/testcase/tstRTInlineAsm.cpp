b86cdf18a1c8cf9d131e04b23de6a2b0315e42a1
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2022-05-24 13:38:17 +0000

    IPRT/tstRTInlineAsm: Fixed bug in the ASMAtomicCmpXchgPtrVoid tests in tstASMAtomicCmpXchgU32Worker.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@95074 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Runtime/testcase/tstRTInlineAsm.cpp
+++ src/VBox/Runtime/testcase/tstRTInlineAsm.cpp
@@ -1448,6 +1448,7 @@ DECLINLINE(void) tstASMAtomicCmpXchgU32Worker(uint32_t volatile *pu32)
     CHECK_OP_AND_VAL_EX(bool, "%d", "%d", pi32, ASMAtomicCmpXchgS32(pi32, INT32_MAX, 19), true, INT32_MAX);
 
 #if ARCH_BITS == 32
+    *pu32 = 29;
     void * volatile *ppv = (void * volatile *)pu32;
     CHECK_OP_AND_VAL_EX(bool, "%d", "%p", ppv, ASMAtomicCmpXchgPtrVoid(ppv, NULL, (void *)(intptr_t)-29), false, (void *)(intptr_t)29);
     CHECK_OP_AND_VAL_EX(bool, "%d", "%p", ppv, ASMAtomicCmpXchgPtrVoid(ppv, NULL, (void *)(intptr_t)29), true, NULL);
