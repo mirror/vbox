d96a6a6c1ab59669a72ff707ae8649662124b894
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2022-05-25 07:02:46 +0000

    SUPDrv-linux: bugref:10209 Use kernel_fpu_begin/end for Linux kernels 5.18 and newer.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@95089 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/HostDrivers/Support/linux/SUPDrv-linux.c
+++ src/VBox/HostDrivers/Support/linux/SUPDrv-linux.c
@@ -1732,7 +1732,14 @@ SUPR0_EXPORT_SYMBOL(SUPR0GetKernelFeatures);
 SUPR0DECL(bool) SUPR0FpuBegin(bool fCtxHook)
 {
     RT_NOREF(fCtxHook);
+#if RTLNX_VER_MIN(5,18,0)
+    kernel_fpu_begin();
+    /* if (fCtxHook) */
+        preempt_enable();  /* HACK ALERT! undo the implicit preempt_disable() in kernel_fpu_begin(). */
+    return true;
+#else
     return false;
+#endif
 }
 SUPR0_EXPORT_SYMBOL(SUPR0FpuBegin);
 
@@ -1740,6 +1747,11 @@ SUPR0_EXPORT_SYMBOL(SUPR0FpuBegin);
 SUPR0DECL(void) SUPR0FpuEnd(bool fCtxHook)
 {
     RT_NOREF(fCtxHook);
+#if RTLNX_VER_MIN(5,18,0)
+    /* if (fCtxHook) */
+        preempt_disable();  /* HACK ALERT! undo the implicit preempt_enable() in SUPR0FpuBegin(). */
+    kernel_fpu_end();
+#endif
 }
 SUPR0_EXPORT_SYMBOL(SUPR0FpuEnd);
 
