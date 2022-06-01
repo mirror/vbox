e0c9cf5977f42da5091e0e8a02f13f719e1fcd56
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2022-06-01 08:09:21 +0000

    Main/Unattended: bugref:9781. Detect OpenSUSE distro name and black list it for now.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@95154 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Main/src-server/UnattendedImpl.cpp
+++ src/VBox/Main/src-server/UnattendedImpl.cpp
@@ -1365,6 +1365,12 @@ static bool detectLinuxDistroName(const char *pszOsAndVersion, VBOXOSTYPE *penmO
         else
             fRet = false;
     }
+    else if (   RTStrNICmp(pszOsAndVersion, RT_STR_TUPLE("OpenSUSE")) == 0
+             && !RT_C_IS_ALNUM(pszOsAndVersion[8]))
+    {
+        *penmOsType = (VBOXOSTYPE)((*penmOsType & VBOXOSTYPE_x64) | VBOXOSTYPE_OpenSUSE);
+        pszOsAndVersion = RTStrStripL(pszOsAndVersion + 8);
+    }
     else if (   RTStrNICmp(pszOsAndVersion, RT_STR_TUPLE("Oracle")) == 0
              && !RT_C_IS_ALNUM(pszOsAndVersion[6]))
     {
@@ -3646,7 +3652,12 @@ HRESULT Unattended::getIsUnattendedInstallSupported(BOOL *aIsUnattendedInstallSu
             return S_OK;
         }
     }
-
+    /* Skip all OpenSUSE variants for now. */
+    if ((mEnmOsType & 0xff000) == VBOXOSTYPE_OpenSUSE)
+    {
+        *aIsUnattendedInstallSupported = false;
+        return S_OK;
+    }
     *aIsUnattendedInstallSupported = true;
     return S_OK;
 }
