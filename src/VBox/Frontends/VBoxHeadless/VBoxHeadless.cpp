98559a1399cf01d05f546535351bfe001511ff1d
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2022-06-05 20:36:24 +0000

    VBoxHeadless: build fixes. bugref:9898
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@95205 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Frontends/VBoxHeadless/VBoxHeadless.cpp
+++ src/VBox/Frontends/VBoxHeadless/VBoxHeadless.cpp
@@ -422,7 +422,7 @@ static void HandleSignal(int sig)
         aSegs[cSegs++].iov_base = (char *)"\n";
     for (int i = 0; i < cSegs; i++)
         aSegs[i].iov_len = strlen((const char *)aSegs[i].iov_base);
-    (void)writev(2, aSegs, cSegs);
+    ssize_t ignored = writev(2, aSegs, cSegs); RT_NOREV_PV(ignored);
 # else
     LogRel(("VBoxHeadless: received signal %d\n", sig)); /** @todo r=bird: This is not at all safe. */
 # endif
@@ -1503,7 +1503,7 @@ extern "C" DECLEXPORT(int) TrustedMain(int argc, char **argv, char **envp)
         {
             if (g_fTerminateFE)
             {
-                LogRel(("VBoxHeadless: processEventQueue: %Rrc, termination requested\n", vrc));
+                LogRel(("VBoxHeadless: processEventQueue: termination requested\n"));
                 break;
             }
 
