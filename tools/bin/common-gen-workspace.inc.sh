7c0a3dde7a81d096fcb8e1577c3e2a0a6d00df81
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2022-07-09 19:29:40 +0000

    tools/bin/common-gen-workspace.inc.sh: The RDP client is no longer with us.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@95581 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- tools/bin/common-gen-workspace.inc.sh
+++ tools/bin/common-gen-workspace.inc.sh
@@ -345,7 +345,6 @@ my_generate_all_projects()
     my_generate_project "Net-NetLib"    "src/VBox/NetworkServices/NetLib"       --begin-incs "include" "src/VBox/NetworkServices/NetLib"        --end-includes "src/VBox/NetworkServices/NetLib"
 
     # src/VBox/RDP
-    my_generate_project "RDP-Client"    "src/VBox/RDP/client-1.8.4"             --begin-incs "include" "src/VBox/RDP/client-1.8.4"              --end-includes "src/VBox/RDP/client-1.8.4"
     my_generate_project "RDP-Server"    "src/VBox/RDP/server"                   --begin-incs "include" "src/VBox/RDP/server"                    --end-includes "src/VBox/RDP/server"
     my_generate_project "RDP-WebClient" "src/VBox/RDP/webclient"                --begin-incs "include" "src/VBox/RDP/webclient"                 --end-includes "src/VBox/RDP/webclient"
     my_generate_project "RDP-Misc"      "src/VBox/RDP"                          --begin-incs "include"                                          --end-includes "src/VBox/RDP/auth" "src/VBox/RDP/tscpasswd" "src/VBox/RDP/x11server"
