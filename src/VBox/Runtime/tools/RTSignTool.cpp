1a6aea8d9a445b4f4877fe007eda40d056154ee0
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2022-07-17 18:48:37 +0000

    RTSignTool: Search the user/system stores for intermedate CAs to the signing certificate and automatically include them in the signing output. [build fix] bugref:8691
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@95683 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Runtime/tools/RTSignTool.cpp
+++ src/VBox/Runtime/tools/RTSignTool.cpp
@@ -830,7 +830,11 @@ public:
          * We don't need to consider RTCRCERTCTX::pTaInfo here as we're not
          * after trust anchors, only intermediate certificates.
          */
+#ifdef RT_OS_WINDOWS
         PCRTCRX509CERTIFICATE pChildCert = pPrev ? pPrev->pCert : pCertificateReal ? pCertificateReal : pCertificate;
+#else
+        PCRTCRX509CERTIFICATE pChildCert = pPrev ? pPrev->pCert : pCertificate;
+#endif
         AssertReturnStmt(pChildCert, RTCrCertCtxRelease(pPrev), NULL);
 
         RTCRSTORECERTSEARCH Search;
@@ -879,7 +883,11 @@ public:
             /* Add the signing certificate: */
             RTERRINFOSTATIC ErrInfo;
             rc = RTCrStoreCertAddX509(hRetStore, RTCRCERTCTX_F_ENC_X509_DER | RTCRCERTCTX_F_ADD_IF_NOT_FOUND,
+#ifdef RT_OS_WINDOWS
                                       (PRTCRX509CERTIFICATE)(pCertificateReal ? pCertificateReal : pCertificate),
+#else
+                                      (PRTCRX509CERTIFICATE)pCertificate,
+#endif
                                       RTErrInfoInitStatic(&ErrInfo));
             if (RT_SUCCESS(rc))
             {
