30a7eb8ff2b8060666529e631d8249f13c3e90c4
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2022-07-20 12:43:25 +0000

    Reverting r152428: FE/Qt: bugref:10244. Hiding audio related UI elements if VBOX_WITH_AUDIO_RECORDING is unset.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@95754 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Frontends/VirtualBox/src/settings/editors/UIRecordingSettingsEditor.cpp
+++ src/VBox/Frontends/VirtualBox/src/settings/editors/UIRecordingSettingsEditor.cpp
@@ -478,13 +478,10 @@ void UIRecordingSettingsEditor::prepareWidgets()
                 {
                     if (m_pLabelMode)
                         m_pLabelMode->setBuddy(m_pComboMode);
-#ifdef VBOX_WITH_AUDIO_RECORDING
                     m_pComboMode->addItem(QString(), QVariant::fromValue(UISettingsDefs::RecordingMode_VideoAudio));
                     m_pComboMode->addItem(QString(), QVariant::fromValue(UISettingsDefs::RecordingMode_VideoOnly));
                     m_pComboMode->addItem(QString(), QVariant::fromValue(UISettingsDefs::RecordingMode_AudioOnly));
-#else
-                    m_pComboMode->addItem(QString(), QVariant::fromValue(UISettingsDefs::RecordingMode_VideoOnly));
-#endif
+
                     pLayoutSettings->addWidget(m_pComboMode, 0, 1, 1, 3);
                 }
 
@@ -745,36 +742,33 @@ void UIRecordingSettingsEditor::prepareWidgets()
                             pLayoutRecordingAudioQuality->addWidget(m_pSliderAudioQuality);
                         }
                         /* Prepare recording audio quality scale layout: */
-                        QHBoxLayout *pLayoutRecordingAudioQualityScale = new QHBoxLayout;
-                        if (pLayoutRecordingAudioQualityScale)
+                        QHBoxLayout *pLayoutRecordingAudioQialityScale = new QHBoxLayout;
+                        if (pLayoutRecordingAudioQialityScale)
                         {
-                            pLayoutRecordingAudioQualityScale->setContentsMargins(0, 0, 0, 0);
+                            pLayoutRecordingAudioQialityScale->setContentsMargins(0, 0, 0, 0);
 
                             /* Prepare recording audio quality min label: */
                             m_pLabelAudioQualityMin = new QLabel(m_pWidgetAudioQualitySettings);
                             if (m_pLabelAudioQualityMin)
-                                pLayoutRecordingAudioQualityScale->addWidget(m_pLabelAudioQualityMin);
-                            pLayoutRecordingAudioQualityScale->addStretch();
+                                pLayoutRecordingAudioQialityScale->addWidget(m_pLabelAudioQualityMin);
+                            pLayoutRecordingAudioQialityScale->addStretch();
                             /* Prepare recording audio quality med label: */
                             m_pLabelAudioQualityMed = new QLabel(m_pWidgetAudioQualitySettings);
                             if (m_pLabelAudioQualityMed)
-                                pLayoutRecordingAudioQualityScale->addWidget(m_pLabelAudioQualityMed);
-                            pLayoutRecordingAudioQualityScale->addStretch();
+                                pLayoutRecordingAudioQialityScale->addWidget(m_pLabelAudioQualityMed);
+                            pLayoutRecordingAudioQialityScale->addStretch();
                             /* Prepare recording audio quality max label: */
                             m_pLabelAudioQualityMax = new QLabel(m_pWidgetAudioQualitySettings);
                             if (m_pLabelAudioQualityMax)
-                                pLayoutRecordingAudioQualityScale->addWidget(m_pLabelAudioQualityMax);
+                                pLayoutRecordingAudioQialityScale->addWidget(m_pLabelAudioQualityMax);
 
-                            pLayoutRecordingAudioQuality->addLayout(pLayoutRecordingAudioQualityScale);
+                            pLayoutRecordingAudioQuality->addLayout(pLayoutRecordingAudioQialityScale);
                         }
                     }
 
                     pLayoutSettings->addWidget(m_pWidgetAudioQualitySettings, 7, 1, 2, 1);
                 }
-#ifndef VBOX_WITH_AUDIO_RECORDING
-                m_pWidgetAudioQualitySettings->hide();
-                m_pLabelAudioQuality->hide();
-#endif
+
                 /* Prepare recording size hint label: */
                 m_pLabelSizeHint = new QLabel(pWidgetSettings);
                 if (m_pLabelSizeHint)
