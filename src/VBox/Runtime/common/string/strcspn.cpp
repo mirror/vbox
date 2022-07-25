4ac093cd4875c27c0e1e0e229cafe4beb258a200
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2022-07-25 13:25:01 +0000

    IPRT/string: Added simple strstr, strrchr and strcspn implementations for IPRT_NO_CRT mode.  bugref:10261
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@95814 cfe28804-0f27-0410-a406-dd0f0b0b656f

---
+++ src/VBox/Runtime/common/string/strcspn.cpp
@@ -0,0 +1,52 @@
+/* $Id$ */
+/** @file
+ * IPRT - CRT Strings, strcspn().
+ */
+
+/*
+ * Copyright (C) 2006-2022 Oracle Corporation
+ *
+ * This file is part of VirtualBox Open Source Edition (OSE), as
+ * available from http://www.virtualbox.org. This file is free software;
+ * you can redistribute it and/or modify it under the terms of the GNU
+ * General Public License (GPL) as published by the Free Software
+ * Foundation, in version 2 as it comes in the "COPYING" file of the
+ * VirtualBox OSE distribution. VirtualBox OSE is distributed in the
+ * hope that it will be useful, but WITHOUT ANY WARRANTY of any kind.
+ *
+ * The contents of this file may alternatively be used under the terms
+ * of the Common Development and Distribution License Version 1.0
+ * (CDDL) only, as it comes in the "COPYING.CDDL" file of the
+ * VirtualBox OSE distribution, in which case the provisions of the
+ * CDDL are applicable instead of those of the GPL.
+ *
+ * You may elect to license modified versions of this file under the
+ * terms and conditions of either the GPL or the CDDL or both.
+ */
+
+
+/*********************************************************************************************************************************
+*   Header Files                                                                                                                 *
+*********************************************************************************************************************************/
+#include <iprt/string.h>
+
+
+/**
+ * strpbrk with a offset return instead of a pointer.
+ */
+size_t strcspn(const char *pszString, const char *pszBreakChars)
+{
+    const char * const pszStringStart = pszString;
+    int                chCur;
+    while ((chCur = *pszString++) != '\0')
+    {
+        int            chBreak;
+        const char    *psz = pszBreakChars;
+        while ((chBreak = *psz++) != '\0')
+            if (chBreak == chCur)
+                return (size_t)(pszString - pszStringStart + 1);
+
+    }
+    return (size_t)(pszString - pszStringStart);
+}
+
---
+++ src/VBox/Runtime/common/string/strstr.cpp
@@ -0,0 +1,61 @@
+/* $Id$ */
+/** @file
+ * IPRT - CRT Strings, strstr().
+ */
+
+/*
+ * Copyright (C) 2006-2022 Oracle Corporation
+ *
+ * This file is part of VirtualBox Open Source Edition (OSE), as
+ * available from http://www.virtualbox.org. This file is free software;
+ * you can redistribute it and/or modify it under the terms of the GNU
+ * General Public License (GPL) as published by the Free Software
+ * Foundation, in version 2 as it comes in the "COPYING" file of the
+ * VirtualBox OSE distribution. VirtualBox OSE is distributed in the
+ * hope that it will be useful, but WITHOUT ANY WARRANTY of any kind.
+ *
+ * The contents of this file may alternatively be used under the terms
+ * of the Common Development and Distribution License Version 1.0
+ * (CDDL) only, as it comes in the "COPYING.CDDL" file of the
+ * VirtualBox OSE distribution, in which case the provisions of the
+ * CDDL are applicable instead of those of the GPL.
+ *
+ * You may elect to license modified versions of this file under the
+ * terms and conditions of either the GPL or the CDDL or both.
+ */
+
+
+/*********************************************************************************************************************************
+*   Header Files                                                                                                                 *
+*********************************************************************************************************************************/
+#include <iprt/string.h>
+
+
+/**
+ * Find the location of @a pszSubStr in @a pszString.
+ *
+ * @returns Pointer to first occurence of the substring in @a pszString, NULL if
+ *          not found.
+ * @param   pszString   Zero terminated string to search.
+ * @param   pszSubStr   The substring to search for.
+ */
+char *strstr(const char *pszString, const char *pszSubStr)
+{
+    char const  ch0Sub = *pszSubStr;
+    pszString = strchr(pszString, ch0Sub);
+    if (pszString)
+    {
+        size_t const cchSubStr = strlen(pszSubStr);
+        do
+        {
+            if (strncmp(pszString, pszSubStr, cchSubStr) == 0)
+                return (char *)pszString;
+            if (ch0Sub)
+                pszString = strchr(pszString + 1, ch0Sub);
+            else
+                break;
+        } while (pszString != NULL);
+    }
+    return NULL;
+}
+
