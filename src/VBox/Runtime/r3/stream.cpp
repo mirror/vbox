d920e5f23359525332c092b399eef76b48e64ebe
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2022-08-02 07:27:38 +0000

    IPRT/stream.cpp: Re-did the mode string handling in RTStrmOpen, switching to use fdopen instead of fopen, adding a few new options. [build fix] bugref:10261
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@95980 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Runtime/r3/stream.cpp
+++ src/VBox/Runtime/r3/stream.cpp
@@ -487,7 +487,7 @@ RTR3DECL(int) RTStrmOpen(const char *pszFilename, const char *pszMode, PRTSTREAM
                         ? RTFILE_O_CREATE_REPLACE | RTFILE_O_WRITE
                         : RTFILE_O_CREATE         | RTFILE_O_WRITE; break;
         case 'r': fOpen = RTFILE_O_OPEN           | RTFILE_O_READ; break;
-        default:  AssertMsgReturn(chMode, ("No main mode (a|r|w) specified in '%s'!\n", pszMode), VERR_INVALID_FLAGS);
+        default:  AssertMsgFailedReturn(("No main mode (a|r|w) specified in '%s'!\n", pszMode), VERR_INVALID_FLAGS);
     }
     AssertMsgReturn(!fExclusive || chMode == 'w', ("the 'x' flag is only allowed with 'w'! (%s)\n", pszMode),
                     VERR_INVALID_FLAGS);
