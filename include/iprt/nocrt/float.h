bd4d27750cc611106e26b2236b5caae36ad56247
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2022-08-03 09:25:58 +0000

    include/iprt/nocrt: More on ostream, limits. Stubbed std::sort. bugref:10261
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@95993 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- include/iprt/nocrt/float.h
+++ include/iprt/nocrt/float.h
@@ -42,11 +42,18 @@
  */
 #if defined(RT_ARCH_X86) || defined(RT_ARCH_AMD64) || defined(RT_ARCH_ARM64)
 
-# define FLT_MAX        (3.40282347E+38F)
-# define FLT_MIN        (1.17549435E-38F)
-# define FLT_MAX_EXP    (128)
-# define FLT_MIN_EXP    (-125)
-# define FLT_EPSILON    (1.192092896E-07F)
+# define FLT_MAX            (3.40282347E+38F)
+# define FLT_MIN            (1.17549435E-38F)
+# define FLT_TRUE_MIN       (1.40129846E-45F)
+# define FLT_MAX_EXP        (128)
+# define FLT_MIN_EXP        (-125)
+# define FLT_MAX_10_EXP     (38)
+# define FLT_MIN_10_EXP     (-37)
+# define FLT_EPSILON        (1.192092896E-07F)
+# define FLT_DIG            (6)
+# define FLT_DECIMAL_DIG    (9)
+# define FLT_MANT_DIG       (24)
+# define FLT_HAS_SUBNORM    (1)
 
 #endif
 
@@ -55,23 +62,54 @@
  */
 #if defined(RT_ARCH_X86) || defined(RT_ARCH_AMD64) || defined(RT_ARCH_ARM64)
 
-# define DBL_MAX        (1.7976931348623157E+308)
-# define DBL_MIN        (2.2250738585072014E-308)
-# define DBL_MAX_EXP    (1024)
-# define DBL_MIN_EXP    (-1021)
-# define DBL_EPSILON    (2.2204460492503131E-16)
+# define DBL_MAX            (1.7976931348623157E+308)
+# define DBL_MIN            (2.2250738585072014E-308)
+# define DBL_TRUE_MIN       (4.9406564584124654E-324)
+# define DBL_MAX_EXP        (1024)
+# define DBL_MIN_EXP        (-1021)
+# define DBL_MAX_10_EXP     (308)
+# define DBL_MIN_10_EXP     (-307)
+# define DBL_EPSILON        (2.2204460492503131E-16)
+# define DBL_DIG            (15)
+# define DBL_DECIMAL_DIG    (17)
+# define DBL_MANT_DIG       (53)
+# define DBL_HAS_SUBNORM    (1)
 
 #endif
 
 /*
  * long double
  */
-#if (defined(RT_ARCH_X86) || defined(RT_ARCH_AMD64)) && defined(RT_OS_WINDOWS)
-# define LDBL_MAX        DBL_MAX
-# define LDBL_MIN        DBL_MIN
-# define LDBL_MAX_EXP    DBL_MAX_EXP
-# define LDBL_MIN_EXP    DBL_MIN_EXP
-# define LDBL_EPSIOLON   DBL_EPSIOLON
+#if ((defined(RT_ARCH_X86) || defined(RT_ARCH_AMD64)) && defined(RT_OS_WINDOWS)) || defined(RT_ARCH_ARM64) /*?*/
+
+# define LDBL_MAX           DBL_MAX
+# define LDBL_MIN           DBL_MIN
+# define LDBL_TRUE_MIN      DBL_TRUE_MIN
+# define LDBL_MAX_EXP       DBL_MAX_EXP
+# define LDBL_MIN_EXP       DBL_MIN_EXP
+# define LDBL_MAX_10_EXP    DBL_MAX_10_EXP
+# define LDBL_MIN_10_EXP    DBL_MIN_10_EXP
+# define LDBL_EPSILON       DBL_EPSILON
+# define LDBL_DIG           DBL_DIG
+# define LDBL_DECIMAL_DIG   DBL_DECIMAL_DIG
+# define LDBL_MANT_DIG      DBL_MANT_DIG
+# define LDBL_HAS_SUBNORM   DBL_HAS_SUBNORM
+
+#elif defined(RT_ARCH_X86) || defined(RT_ARCH_AMD64)
+
+# define LDBL_MAX           (1.1897314953572317650E+4932L)
+# define LDBL_MIN           (3.3621031431120935063E-4932L)
+# define LDBL_TRUE_MIN      (3.6451995318824746025E-4951L)
+# define LDBL_MAX_EXP       (-16381)
+# define LDBL_MIN_EXP       (16384)
+# define LDBL_MAX_10_EXP    (4932)
+# define LDBL_MIN_10_EXP    (-4931)
+# define LDBL_EPSILON       (1.0842021724855044340E-19L)
+# define LDBL_DIG           (18)
+# define LDBL_DECIMAL_DIG   (21)
+# define LDBL_MANT_DIG      (64)
+# define LDBL_HAS_SUBNORM   (1)
+
 #endif
 
 
