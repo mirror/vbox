708a720bcef614bf550f591ded5d8fbc6e800e4d
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2022-08-08 11:53:27 +0000

    IPRT/nocrt: very basic sqrt and sqrtf implementations. bugref:10261
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@96112 cfe28804-0f27-0410-a406-dd0f0b0b656f

---
+++ src/VBox/Runtime/common/math/sqrtf.asm
@@ -0,0 +1,55 @@
+; $Id$
+;; @file
+; IPRT - No-CRT sqrtf - AMD64 & X86.
+;
+
+;
+; Copyright (C) 2022 Oracle Corporation
+;
+; This file is part of VirtualBox Open Source Edition (OSE), as
+; available from http://www.virtualbox.org. This file is free software;
+; you can redistribute it and/or modify it under the terms of the GNU
+; General Public License (GPL) as published by the Free Software
+; Foundation, in version 2 as it comes in the "COPYING" file of the
+; VirtualBox OSE distribution. VirtualBox OSE is distributed in the
+; hope that it will be useful, but WITHOUT ANY WARRANTY of any kind.
+;
+; The contents of this file may alternatively be used under the terms
+; of the Common Development and Distribution License Version 1.0
+; (CDDL) only, as it comes in the "COPYING.CDDL" file of the
+; VirtualBox OSE distribution, in which case the provisions of the
+; CDDL are applicable instead of those of the GPL.
+;
+; You may elect to license modified versions of this file under the
+; terms and conditions of either the GPL or the CDDL or both.
+;
+
+%define RT_ASM_WITH_SEH64
+%include "iprt/asmdefs.mac"
+
+BEGINCODE
+
+;;
+; Square root - the fast not entirely C standard confirming version.
+;
+; @returns  st(0) / xmm0
+; @param    r32     [rbp + 08h] / xmm0
+RT_NOCRT_BEGINPROC sqrtf
+        push    xBP
+        SEH64_PUSH_xBP
+        mov     xBP, xSP
+        SEH64_SET_FRAME_xBP 0
+        SEH64_END_PROLOGUE
+
+%ifdef RT_ARCH_AMD64
+        sqrtss  xmm0, xmm0
+%else
+        fld     dword [xBP + xCB*2]
+        fsqrt
+        ;; @todo rounding?
+%endif
+
+        leave
+        ret
+ENDPROC   RT_NOCRT(sqrtf)
+
