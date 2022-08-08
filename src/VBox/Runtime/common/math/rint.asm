fcd268aa7eb2d46c4aa43118e9634ff6ae033d41
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2022-08-08 21:29:03 +0000

    IPRT/nocrt: lround, lroundf, lroundl, rint, rintf, round, roundf and roundl. bugref:10261
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@96117 cfe28804-0f27-0410-a406-dd0f0b0b656f

---
+++ src/VBox/Runtime/common/math/rint.asm
@@ -0,0 +1,64 @@
+; $Id$
+;; @file
+; IPRT - No-CRT rint - AMD64 & X86.
+;
+
+;
+; Copyright (C) 2022 Oracle Corporation
+;
+; This file is part of VirtualBox Open Source Edition (OSE), as
+; available from http://www.virtualbox.org. This file is free software;
+; you can redistribute it and/or modify it under the terms of the GNU
+; General Public License (GPL) as published by the Free Software
+; Foundation, in version 2 as it comes in the "COPYING" file of the
+; VirtualBox OSE distribution. VirtualBox OSE is distributed in the
+; hope that it will be useful, but WITHOUT ANY WARRANTY of any kind.
+;
+; The contents of this file may alternatively be used under the terms
+; of the Common Development and Distribution License Version 1.0
+; (CDDL) only, as it comes in the "COPYING.CDDL" file of the
+; VirtualBox OSE distribution, in which case the provisions of the
+; CDDL are applicable instead of those of the GPL.
+;
+; You may elect to license modified versions of this file under the
+; terms and conditions of either the GPL or the CDDL or both.
+;
+
+%define RT_ASM_WITH_SEH64
+%include "iprt/asmdefs.mac"
+
+BEGINCODE
+
+;;
+; Round to integer value according to current rounding mode.
+;
+; ASSUME FCW and MXCSR are in sync for AMD64.
+;
+; @returns  st(0) / xmm0
+; @param    rd      [rbp + 08h] / xmm0
+RT_NOCRT_BEGINPROC rint
+        push    xBP
+        SEH64_PUSH_xBP
+        mov     xBP, xSP
+        SEH64_SET_FRAME_xBP 0
+%ifdef RT_ARCH_AMD64
+        sub     xSP, 10h
+        SEH64_ALLOCATE_STACK 10h
+%endif
+        SEH64_END_PROLOGUE
+
+%ifdef RT_ARCH_AMD64
+        movsd   qword [xSP], xmm0
+        fld     qword [xSP]
+        frndint
+        fstp    qword [xSP]
+        movsd   xmm0, qword [xSP]
+%else
+        fld     qword [xBP + xCB*2]
+        frndint
+%endif
+
+        leave
+        ret
+ENDPROC   RT_NOCRT(rint)
+
---
+++ src/VBox/Runtime/common/math/round.cpp
@@ -0,0 +1,59 @@
+/* $Id$ */
+/** @file
+ * IPRT - No-CRT - round().
+ */
+
+/*
+ * Copyright (C) 2022 Oracle Corporation
+ *
+ * This file is part of VirtualBox Open Source Edition (OSE), as
+ * available from http://www.virtualbox.org. This file is free software;
+ * you can redistribute it and/or modify it under the terms of the GNU
+ * General Public License (GPL) as published by the Free Software
+ * Foundation, in version 2 as it comes in the "COPYING" file of the
+ * VirtualBox OSE distribution. VirtualBox OSE is distributed in the
+ * hope that it will be useful, but WITHOUT ANY WARRANTY of any kind.
+ *
+ * The contents of this file may alternatively be used under the terms
+ * of the Common Development and Distribution License Version 1.0
+ * (CDDL) only, as it comes in the "COPYING.CDDL" file of the
+ * VirtualBox OSE distribution, in which case the provisions of the
+ * CDDL are applicable instead of those of the GPL.
+ *
+ * You may elect to license modified versions of this file under the
+ * terms and conditions of either the GPL or the CDDL or both.
+ */
+
+
+/*********************************************************************************************************************************
+*   Header Files                                                                                                                 *
+*********************************************************************************************************************************/
+#define IPRT_NO_CRT_FOR_3RD_PARTY
+#include "internal/nocrt.h"
+#include <iprt/nocrt/math.h>
+
+
+#undef round
+double RT_NOCRT(round)(double rd)
+{
+    if (isfinite(rd))
+    {
+        double const rdIn = rd;
+        if (rd >= 0.0)
+        {
+            rd = RT_NOCRT(ceil)(rd);
+            if (rd - rdIn > 0.5)
+                rd -= 1.0;
+        }
+        else
+        {
+            rd = RT_NOCRT(ceil)(-rd);
+            if (rd + rdIn > 0.5)
+                rd -= 1.0;
+            rd = -rd;
+        }
+    }
+    return rd;
+}
+RT_ALIAS_AND_EXPORT_NOCRT_SYMBOL(round);
+
