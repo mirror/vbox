f592e342fc93473000b85656149cd642ecdbc76f
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2022-08-17 09:15:05 +0000

    VMM/IEM: Start implementing floating point SSE instructions using addps, added some new infrastructure bits (mostly untested), bugref:9898 [build fix]
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@96250 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/VMM/VMMAll/IEMAllAImplC.cpp
+++ src/VBox/VMM/VMMAll/IEMAllAImplC.cpp
@@ -13808,7 +13808,7 @@ DECLINLINE(PRTFLOAT32U) iemFpSoftF32ToIprt(PRTFLOAT32U pr32Dst, float32_t const 
     return pr32Dst;
 }
 
-
+#if 0 /* Currently unused. */
 /**
  * Converts from the packed IPRT 64-bit (single precision) floating point format to
  * the SoftFloat 64-bit floating point format (float64_t).
@@ -13834,7 +13834,7 @@ DECLINLINE(PRTFLOAT64U) iemFpSoftF64ToIprt(PRTFLOAT64U pr64Dst, float64_t const 
     pr64Dst->u = r64XSrc.v;
     return pr64Dst;
 }
-
+#endif
 
 /** Initializer for the SoftFloat state structure. */
 # define IEM_SOFTFLOAT_STATE_INITIALIZER_FROM_MXCSR(a_Mxcsr) \
