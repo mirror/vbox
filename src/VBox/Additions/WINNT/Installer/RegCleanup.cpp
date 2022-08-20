d9ecab74f4b91ccad561bde0d7756ac64d0c3cb0
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2022-08-20 02:49:33 +0000

    Add/NT/Inst: Cleaned up RegCleanup, switched it to RegDeleteKeyW, removing unnecessary includes and printf dependency. bugref:10261
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@96372 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Additions/WINNT/Installer/RegCleanup.cpp
+++ src/VBox/Additions/WINNT/Installer/RegCleanup.cpp
@@ -1,6 +1,15 @@
 /* $Id$ */
 /** @file
- * delinvalid - remove "InvalidDisplay" key on NT4
+ * RegCleanup - Remove "InvalidDisplay" and "NewDisplay" keys on NT4,
+ *              run via HKLM/.../Windows/CurrentVersion/RunOnce.
+ *
+ * Delete the "InvalidDisplay" key which causes the display applet to be
+ * started on every boot. For some reason this key isn't removed after
+ * setting the proper resolution and even not when * doing a driver reinstall.
+ * Removing it doesn't seem to do any harm.  The key is inserted by windows on
+ * first reboot after installing the VBox video driver using the VirtualBox
+ * utility. It's not inserted when using the Display applet for installation.
+ * There seems to be a subtle problem with the VirtualBox util.
  */
 
 /*
@@ -15,36 +24,14 @@
  * hope that it will be useful, but WITHOUT ANY WARRANTY of any kind.
  */
 
-/*
- Purpose:
-
- Delete the "InvalidDisplay" key which causes the display
- applet to be started on every boot. For some reason this key
- isn't removed after setting the proper resolution and even not when
- doing a driver reinstall. Removing it doesn't seem to do any harm.
- The key is inserted by windows on first reboot after installing
- the VBox video driver using the VirtualBox utility.
- It's not inserted when using the Display applet for installation.
- There seems to be a subtle problem with the VirtualBox util.
- */
-
 
 /*********************************************************************************************************************************
 *   Header Files                                                                                                                 *
 *********************************************************************************************************************************/
-//#define _UNICODE
-
 #include <iprt/win/windows.h>
-#include <iprt/win/setupapi.h>
-#include <regstr.h>
-#include <DEVGUID.h>
-#include <stdio.h>
-
-#include "tchar.h"
-#include "string.h"
 
 
-BOOL isNT4(void)
+static BOOL isNT4(void)
 {
     OSVERSIONINFO OSversion;
 
@@ -53,37 +40,37 @@ BOOL isNT4(void)
 
     switch (OSversion.dwPlatformId)
     {
-    case VER_PLATFORM_WIN32s:
-    case VER_PLATFORM_WIN32_WINDOWS:
-        return FALSE;
-    case VER_PLATFORM_WIN32_NT:
-        if (OSversion.dwMajorVersion == 4)
-            return TRUE;
-        else return FALSE;
-    default:
-        break;
+        case VER_PLATFORM_WIN32s:
+        case VER_PLATFORM_WIN32_WINDOWS:
+            return FALSE;
+        case VER_PLATFORM_WIN32_NT:
+            if (OSversion.dwMajorVersion == 4)
+                return TRUE;
+            return FALSE;
+        default:
+            break;
     }
     return FALSE;
 }
 
+
 int main()
 {
-    int rc = 0;
-
     /* This program is only for installing drivers on NT4 */
     if (!isNT4())
     {
-        printf("This program only runs on NT4\n");
-        return -1;
+        DWORD cbIgn;
+        WriteFile(GetStdHandle(STD_ERROR_HANDLE), RT_STR_TUPLE("This program only runs on NT4\r\n"), &cbIgn, NULL);
+        return 1;
     }
 
     /* Delete the "InvalidDisplay" key which causes the display
-     applet to be started on every boot. For some reason this key
-     isn't removed after setting the proper resolution and even not when
-     doing a driverreinstall.  */
-    RegDeleteKey(HKEY_LOCAL_MACHINE, TEXT("SYSTEM\\CurrentControlSet\\Control\\GraphicsDrivers\\InvalidDisplay"));
-    RegDeleteKey(HKEY_LOCAL_MACHINE, TEXT("SYSTEM\\CurrentControlSet\\Control\\GraphicsDrivers\\NewDisplay"));
+       applet to be started on every boot. For some reason this key
+       isn't removed after setting the proper resolution and even not when
+       doing a driverreinstall.  */
+    RegDeleteKeyW(HKEY_LOCAL_MACHINE, L"SYSTEM\\CurrentControlSet\\Control\\GraphicsDrivers\\InvalidDisplay");
+    RegDeleteKeyW(HKEY_LOCAL_MACHINE, L"SYSTEM\\CurrentControlSet\\Control\\GraphicsDrivers\\NewDisplay");
 
-    return rc;
+    return 0;
 }
 
