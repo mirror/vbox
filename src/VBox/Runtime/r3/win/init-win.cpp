0fa8a59ea6946026369c05809f212f9dad3225a7
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2022-09-05 19:34:59 +0000

    IPRT: Now the GetVersionExW fallback should be correct. bugref:10261
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@96606 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Runtime/r3/win/init-win.cpp
+++ src/VBox/Runtime/r3/win/init-win.cpp
@@ -358,7 +358,7 @@ static void rtR3InitWindowsVersion(void)
              */
             RT_ZERO(g_WinOsInfoEx);
             g_WinOsInfoEx.dwOSVersionInfoSize = sizeof(OSVERSIONINFOW);
-            if (!pfnGetVersionExW && pfnGetVersionExW((POSVERSIONINFOW)&g_WinOsInfoEx))
+            if (pfnGetVersionExW && pfnGetVersionExW((POSVERSIONINFOW)&g_WinOsInfoEx))
                 Assert(g_WinOsInfoEx.dwPlatformId != VER_PLATFORM_WIN32_NT || g_WinOsInfoEx.dwMajorVersion < 5);
             else
             {
