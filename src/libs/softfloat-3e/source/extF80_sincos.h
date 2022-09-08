477931a1167f26cd6a7814dc8f3905e62efaa5c1
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2022-09-08 22:34:06 +0000

    libs/softfloat-3e: Build fixes and cleanups for the sincos code. [scm fix] bugref:9898
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@96665 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/libs/softfloat-3e/source/extF80_sincos.h
+++ src/libs/softfloat-3e/source/extF80_sincos.h
@@ -25,6 +25,12 @@
  * SPDX-License-Identifier: GPL-3.0-only
  */
 
+#ifndef VBOX_INCLUDED_SRC_source_extF80_sincos_h
+#define VBOX_INCLUDED_SRC_source_extF80_sincos_h
+#ifndef RT_WITHOUT_PRAGMA_ONCE
+# pragma once
+#endif
+
 /** The pi/2 constant as 128-bit floating point value.
  * base-10: 1.57079632679489661923132169163975139
  * base-16: 1.921fb54442d18469898cc51701b8
@@ -720,3 +726,5 @@ const RTFLOAT128U g_ar128FsincosCORDICConsts2[] =
      * base-2 : 1.0011011011101001110110110101000010000110101111001011010011001111111010111111001000010010010101111011000000001010e-1 */
     RTFLOAT128U_INIT_C(0, 0x36e9db5086bc, 0xb4cfebf21257b00a, 0x3ffe),
 };
+
+#endif /* !VBOX_INCLUDED_SRC_source_extF80_sincos_h */
