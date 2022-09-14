6a6c08ff847ddd597cc3af76c9a24ce159d6e8b4
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2022-09-14 07:53:44 +0000

    VMM/testcase/tstIEMAImpl: Implement basic cvtsd2si testcase, ​bugref:9898
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@96727 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/VMM/testcase/tstIEMAImplDataSseBinary.asm
+++ src/VBox/VMM/testcase/tstIEMAImplDataSseBinary.asm
@@ -100,5 +100,7 @@ IEM_TEST_DATA cvtsd2ss_u128_r64,    "tstIEMAImplDataSseBinary-cvtsd2ss_u128_r64.
 IEM_TEST_DATA sqrtsd_u128_r64,      "tstIEMAImplDataSseBinary-sqrtsd_u128_r64.bin"
 
 IEM_TEST_DATA cvttsd2si_i32_r64,    "tstIEMAImplDataSseBinary-cvttsd2si_i32_r64.bin"
+IEM_TEST_DATA cvtsd2si_i32_r64,     "tstIEMAImplDataSseBinary-cvtsd2si_i32_r64.bin"
 
 IEM_TEST_DATA cvttsd2si_i64_r64,    "tstIEMAImplDataSseBinary-cvttsd2si_i64_r64.bin"
+IEM_TEST_DATA cvtsd2si_i64_r64,     "tstIEMAImplDataSseBinary-cvtsd2si_i64_r64.bin"
