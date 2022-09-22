fd44a7fe9b57413c81b80cf56d4f1bc612ed6be9
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2022-09-22 23:33:03 +0000

    libs/dxvk-native-1.9.2a: Refer to libvulkan using the versioned library name (libvulkan.so.1) since the unversioned one is only available if the development package is installed.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@96838 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/libs/dxvk-native-1.9.2a/src/vulkan/vulkan_loader.cpp
+++ src/libs/dxvk-native-1.9.2a/src/vulkan/vulkan_loader.cpp
@@ -7,7 +7,7 @@
 # ifdef RT_OS_DARWIN
 #  define VBOX_VULKAN_LIBRARY_NAME "libMoltenVK"
 # else
-#  define VBOX_VULKAN_LIBRARY_NAME "libvulkan"
+#  define VBOX_VULKAN_LIBRARY_NAME "libvulkan.so.1"
 # endif
 #endif
 
@@ -38,45 +38,45 @@ namespace dxvk::vk {
     return NULL;
 #endif
   }
-  
-  
+
+
   InstanceLoader::InstanceLoader(bool owned, VkInstance instance)
   : m_instance(instance), m_owned(owned) { }
 
-  
+
   PFN_vkVoidFunction InstanceLoader::sym(const char* name) const {
     return dxvk::vk::GetInstanceProcAddr(m_instance, name);
   }
-  
-  
+
+
   DeviceLoader::DeviceLoader(bool owned, VkInstance instance, VkDevice device)
   : m_getDeviceProcAddr(reinterpret_cast<PFN_vkGetDeviceProcAddr>(
       dxvk::vk::GetInstanceProcAddr(instance, "vkGetDeviceProcAddr"))),
     m_device(device), m_owned(owned) { }
-  
-  
+
+
   PFN_vkVoidFunction DeviceLoader::sym(const char* name) const {
     return m_getDeviceProcAddr(m_device, name);
   }
-  
-  
+
+
   LibraryFn::LibraryFn() { }
   LibraryFn::~LibraryFn() { }
-  
-  
+
+
   InstanceFn::InstanceFn(bool owned, VkInstance instance)
   : InstanceLoader(owned, instance) { }
   InstanceFn::~InstanceFn() {
     if (m_owned)
       this->vkDestroyInstance(m_instance, nullptr);
   }
-  
-  
+
+
   DeviceFn::DeviceFn(bool owned, VkInstance instance, VkDevice device)
   : DeviceLoader(owned, instance, device) { }
   DeviceFn::~DeviceFn() {
     if (m_owned)
       this->vkDestroyDevice(m_device, nullptr);
   }
-  
+
 }
