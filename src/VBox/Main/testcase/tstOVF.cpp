5c8a70948ec1f3edd6b246bec301cf362ead9aa7
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2022-09-24 01:55:24 +0000

    fixed tstOVF so it no longer leaves a mess after failure, removed tstOVF from ValidationKit blacklist
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@96849 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Main/testcase/tstOVF.cpp
+++ src/VBox/Main/testcase/tstOVF.cpp
@@ -231,9 +231,21 @@ void importOVF(const char *pcszPrefix,
                 case VirtualSystemDescriptionType_SoundCard:
                     pcszType = "sound";
                 break;
+				
+				case VirtualSystemDescriptionType_SettingsFile:
+                    pcszType = "settings";
+                break;
+				
+				case VirtualSystemDescriptionType_BaseFolder:
+                    pcszType = "basefolder";
+                break;
+				
+				case VirtualSystemDescriptionType_PrimaryGroup:
+                    pcszType = "primarygroup";
+                break;
 
                 default:
-                    throw MyError(E_UNEXPECTED, "Invalid VirtualSystemDescriptionType\n");
+                    throw MyError(E_UNEXPECTED, "Invalid VirtualSystemDescriptionType (enum)\n");
                 break;
             }
 
@@ -287,6 +299,12 @@ void copyDummyDiskImage(const char *pcszPrefix,
     RTPathExecDir(szDst, sizeof(szDst));
     RTPathAppend(szDst, sizeof(szDst), pcszDest);
     RTPrintf("%s: copying ovf-dummy.vmdk to \"%s\"...\n", pcszPrefix, pcszDest);
+
+	if (FILE *file = fopen(pcszDest, "r"))
+	{
+		fclose(file);
+		RTFileDelete(pcszDest);
+	}
 
     int vrc = RTFileCopy(szSrc, szDst);
     if (RT_FAILURE(vrc)) throw MyError(0, Utf8StrFmt("Cannot copy ovf-dummy.vmdk to %s: %Rra\n", pcszDest, vrc).c_str());
