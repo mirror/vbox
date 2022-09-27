1ccf2ed716c74a14b58d011cf26dca54c5fd6c89
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2022-09-27 11:15:30 +0000

    IEM: Removed wrong/redundant CR0.EM checks in FXSAVE/FXRSTOR.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@96896 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/VMM/VMMAll/IEMAllCImpl.cpp
+++ src/VBox/VMM/VMMAll/IEMAllCImpl.cpp
@@ -8461,8 +8461,6 @@ IEM_CIMPL_DEF_3(iemCImpl_fxsave, uint8_t, iEffSeg, RTGCPTR, GCPtrEff, IEMMODE, e
     /*
      * Raise exceptions.
      */
-    if (pVCpu->cpum.GstCtx.cr0 & X86_CR0_EM)
-        return iemRaiseUndefinedOpcode(pVCpu);
     if (pVCpu->cpum.GstCtx.cr0 & (X86_CR0_TS | X86_CR0_EM))
         return iemRaiseDeviceNotAvailable(pVCpu);
 
@@ -8555,8 +8553,6 @@ IEM_CIMPL_DEF_3(iemCImpl_fxrstor, uint8_t, iEffSeg, RTGCPTR, GCPtrEff, IEMMODE, 
     /*
      * Raise exceptions.
      */
-    if (pVCpu->cpum.GstCtx.cr0 & X86_CR0_EM)
-        return iemRaiseUndefinedOpcode(pVCpu);
     if (pVCpu->cpum.GstCtx.cr0 & (X86_CR0_TS | X86_CR0_EM))
         return iemRaiseDeviceNotAvailable(pVCpu);
 
