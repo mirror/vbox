b552922e2aa379729f70b5673b831065d4fca60c
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2022-10-13 08:52:20 +0000

    Revert r154066, bugref:9044
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@97125 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/VMM/VMMR3/NEMR3Native-darwin.cpp
+++ src/VBox/VMM/VMMR3/NEMR3Native-darwin.cpp
@@ -543,9 +543,7 @@ DECLINLINE(int) nemR3DarwinMap(PVM pVM, RTGCPHYS GCPhys, void *pvRam, size_t cb,
         hrc = hv_vm_map_space(pVM->nem.s.uVmAsid, pvRam, GCPhys, cb, fHvMemProt);
     else
         hrc = hv_vm_map(pvRam, GCPhys, cb, fHvMemProt);
-    if (hrc != HV_SUCCESS)
-        LogRel(("Failed to map %RGp LB %zu fProt=%#x/%#x hrc=%\n", GCPhys, cb, fPageProt, fHvMemProt));
-    return VINF_SUCCESS; //nemR3DarwinHvSts2Rc(hrc);
+    return nemR3DarwinHvSts2Rc(hrc);
 }
 
 
