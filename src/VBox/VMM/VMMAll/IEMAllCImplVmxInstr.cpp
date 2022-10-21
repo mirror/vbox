d9e5831db3c07a3c1c15e77490ba94df9e82ce59
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2022-10-21 11:33:21 +0000

    VMM/IEM: Nested VMX: bugref:10092 Use the same log level as the VM-exit logging along with 'vmentry' prefix.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@97263 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/VMM/VMMAll/IEMAllCImplVmxInstr.cpp
+++ src/VBox/VMM/VMMAll/IEMAllCImplVmxInstr.cpp
@@ -7919,10 +7919,10 @@ static VBOXSTRICTRC iemVmxVmlaunchVmresume(PVMCPUCC pVCpu, uint8_t cbInstr, VMXI
 # endif
 
                         /* Finally, done. */
-                        LogFlow(("%s: cs:rip=%04x:%08RX64 cr0=%#RX64 (%#RX64) cr4=%#RX64 (%#RX64) efer=%#RX64 (%#RX64)\n",
-                                 pszInstr, pVCpu->cpum.GstCtx.cs.Sel, pVCpu->cpum.GstCtx.rip, pVCpu->cpum.GstCtx.cr0,
-                                 pVmcs->u64Cr0ReadShadow.u, pVCpu->cpum.GstCtx.cr4, pVmcs->u64Cr4ReadShadow.u,
-                                 pVCpu->cpum.GstCtx.msrEFER, pVmcs->u64GuestEferMsr.u));
+                        Log2(("vmentry: %s: cs:rip=%04x:%08RX64 cr0=%#RX64 (%#RX64) cr4=%#RX64 (%#RX64) efer=%#RX64 (%#RX64)\n",
+                              pszInstr, pVCpu->cpum.GstCtx.cs.Sel, pVCpu->cpum.GstCtx.rip, pVCpu->cpum.GstCtx.cr0,
+                              pVmcs->u64Cr0ReadShadow.u, pVCpu->cpum.GstCtx.cr4, pVmcs->u64Cr4ReadShadow.u,
+                              pVCpu->cpum.GstCtx.msrEFER, pVmcs->u64GuestEferMsr.u));
                         return VINF_SUCCESS;
                     }
                     return iemVmxVmexit(pVCpu, VMX_EXIT_ERR_MSR_LOAD | VMX_EXIT_REASON_ENTRY_FAILED, pVmcs->u64RoExitQual.u);
