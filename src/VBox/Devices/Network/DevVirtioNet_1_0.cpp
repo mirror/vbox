891f705a8f4d5f9758103b5018704815619b5867
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2022-10-25 08:50:06 +0000

    Devices/DevVirtioNet_1_0: Initialize some virtqbuf members to fend off debug only assertions later on
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@97293 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Devices/Network/DevVirtioNet_1_0.cpp
+++ src/VBox/Devices/Network/DevVirtioNet_1_0.cpp
@@ -1990,6 +1990,10 @@ static int virtioNetR3CopyRxPktToGuest(PPDMDEVINS pDevIns, PVIRTIONET pThis, PVI
     RT_NOREF(pThisCC);
 #ifdef VIRTIO_VBUF_ON_STACK
     VIRTQBUF_T VirtqBuf;
+
+    VirtqBuf.u32Magic  = VIRTQBUF_MAGIC;
+    VirtqBuf.cRefs     = 1;
+
     PVIRTQBUF pVirtqBuf = &VirtqBuf;
     int rc = virtioCoreR3VirtqAvailBufGet(pDevIns, &pThis->Virtio, pRxVirtq->uIdx, pVirtqBuf, true);
 #else /* !VIRTIO_VBUF_ON_STACK */
@@ -2726,6 +2730,10 @@ static int virtioNetR3TransmitPkts(PPDMDEVINS pDevIns, PVIRTIONET pThis, PVIRTIO
     int rc;
 #ifdef VIRTIO_VBUF_ON_STACK
     VIRTQBUF_T VirtqBuf;
+
+    VirtqBuf.u32Magic  = VIRTQBUF_MAGIC;
+    VirtqBuf.cRefs     = 1;
+
     PVIRTQBUF pVirtqBuf = &VirtqBuf;
     while ((rc = virtioCoreR3VirtqAvailBufPeek(pVirtio->pDevInsR3, pVirtio, pTxVirtq->uIdx, pVirtqBuf)) == VINF_SUCCESS)
 #else /* !VIRTIO_VBUF_ON_STACK */
