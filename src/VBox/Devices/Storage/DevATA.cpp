b8558b9f30e866d3d7a12f5f124922a0605f4c50
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2022-11-04 15:00:33 +0000

    DevATA: Fix a mixed up logical operator ('&&' accidentally used instead of '||') causing VMs with an empty CDROM drive attached to an IDE controller to hang on boot waiting for a READ CAPACITY request to the non-existent CD to finish (regression from r153527).
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@97403 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Devices/Storage/DevATA.cpp
+++ src/VBox/Devices/Storage/DevATA.cpp
@@ -3861,7 +3861,7 @@ static void atapiR3ParseCmdVirtualATAPI(PPDMDEVINS pDevIns, PATACONTROLLER pCtl,
                 atapiR3CmdErrorSimple(pCtl, s, SCSI_SENSE_UNIT_ATTENTION, SCSI_ASC_MEDIUM_MAY_HAVE_CHANGED); /* media changed */
                 break;
             }
-            if (!pDevR3->pDrvMount && !pDevR3->pDrvMount->pfnIsMounted(pDevR3->pDrvMount))
+            if (!pDevR3->pDrvMount || !pDevR3->pDrvMount->pfnIsMounted(pDevR3->pDrvMount))
             {
                 atapiR3CmdErrorSimple(pCtl, s, SCSI_SENSE_NOT_READY, SCSI_ASC_MEDIUM_NOT_PRESENT);
                 break;
