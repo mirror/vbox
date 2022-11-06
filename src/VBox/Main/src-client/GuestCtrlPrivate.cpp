44016273bce810d349e0f627b84d23a2aa5cb695
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2022-11-06 09:34:15 +0000

    Guest Control/Main: More (temporary) logging. ​bugref:10286
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@97413 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Main/src-client/GuestCtrlPrivate.cpp
+++ src/VBox/Main/src-client/GuestCtrlPrivate.cpp
@@ -1753,6 +1753,10 @@ int GuestPath::BuildDestinationPath(const Utf8Str &strSrcPath, PathStyle_T enmSr
         strDstPath += pszSrcName;
     }
 
+    LogRel(("Guest Control: Building destination path for '%s' -> '%s'\n",
+            strSrcPath.c_str(), GuestBase::pathStyleToStr(enmSrcPathStyle),
+            strDstPath.c_str(), GuestBase::pathStyleToStr(enmDstPathStyle)));
+
     return VINF_SUCCESS;
 }
 
@@ -1891,9 +1895,9 @@ int GuestPath::Translate(Utf8Str &strPath, PathStyle_T enmSrcPathStyle, PathStyl
 
     strTranslated.jolt();
 
-    LogRel2(("Guest Control: Translating '%s' (%s) -> '%s' (%s): %Rrc\n",
-             strPath.c_str(), GuestBase::pathStyleToStr(enmSrcPathStyle),
-             strTranslated.c_str(), GuestBase::pathStyleToStr(enmDstPathStyle), vrc));
+    LogRel(("Guest Control: Translating '%s' (%s) -> '%s' (%s): %Rrc\n",
+            strPath.c_str(), GuestBase::pathStyleToStr(enmSrcPathStyle),
+            strTranslated.c_str(), GuestBase::pathStyleToStr(enmDstPathStyle), vrc));
 
     if (RT_SUCCESS(vrc))
         strPath = strTranslated;
