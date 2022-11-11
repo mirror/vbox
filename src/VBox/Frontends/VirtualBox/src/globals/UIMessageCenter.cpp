6b21010b045af878bdc7841c13bff2ade882c12c
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2022-11-11 12:39:59 +0000

    FE/Qt: bugref:9458: Fixing regression in r154531.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@97513 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Frontends/VirtualBox/src/globals/UIMessageCenter.cpp
+++ src/VBox/Frontends/VirtualBox/src/globals/UIMessageCenter.cpp
@@ -795,14 +795,16 @@ bool UIMessageCenter::confirmSettingsReloading(QWidget *pParent /* = 0 */) const
         return false;
     setWarningShown("confirmSettingsReloading", true);
 
-    return questionBinary(pParent, MessageType_Question,
-                          tr("<p>The machine settings were changed while you were editing them. "
-                             "You currently have unsaved setting changes.</p>"
-                             "<p>Would you like to reload the changed settings or to keep your own changes?</p>"),
-                          0 /* auto-confirm id */,
-                          tr("Reload settings"), tr("Keep changes"));
+    const bool fResult = questionBinary(pParent, MessageType_Question,
+                                        tr("<p>The machine settings were changed while you were editing them. "
+                                           "You currently have unsaved setting changes.</p>"
+                                           "<p>Would you like to reload the changed settings or to keep your own changes?</p>"),
+                                        0 /* auto-confirm id */,
+                                        tr("Reload settings"), tr("Keep changes"));
 
     setWarningShown("confirmSettingsReloading", false);
+
+    return fResult;
 }
 
 int UIMessageCenter::confirmRemovingOfLastDVDDevice(QWidget *pParent /* = 0*/) const
