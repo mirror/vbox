bb1305e5bd808ce48bdb3a61f422460b2ab0c654
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2022-11-14 17:52:27 +0000

    Frontends/VBoxManage: The appliance import code populates the IVirtualSystemDescription instance corresponding to the amount of guest memory using two different allocation units: OVFValues[] in bytes and VBoxValues[] in megabytes.  The SDK documents the allocation unit as being in bytes for both fields so update the code to consistently use bytes for VBoxValues[] just as is done for OVFValues[].  Follow-up changes to handle 'VBoxManage import --memory <>'.  bugref:10314
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@97536 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Frontends/VBoxManage/VBoxManageAppliance.cpp
+++ src/VBox/Frontends/VBoxManage/VBoxManageAppliance.cpp
@@ -753,9 +753,14 @@ RTEXITCODE handleImportAppliance(HandlerArg *arg)
                                 uint32_t ulMemMB;
                                 if (VINF_SUCCESS == strOverride.toInt(ulMemMB))
                                 {
+                                    /* 'VBoxManage import --memory' size is in megabytes */
+                                    RTPrintf(Appliance::tr("%2u: Guest memory specified with --memory: %RU32 MB\n"),
+                                             a, ulMemMB);
+
+                                    /* IVirtualSystemDescription guest memory size is in bytes */
+                                    uint64_t ullMemBytes = (uint64_t)ulMemMB * _1M;
+                                    strOverride = Utf8StrFmt("%RU64", ullMemBytes);
                                     bstrFinalValue = strOverride;
-                                    RTPrintf(Appliance::tr("%2u: Guest memory specified with --memory: %ls MB\n"),
-                                             a, bstrFinalValue.raw());
                                 }
                                 else
                                     return errorSyntax(Appliance::tr("Argument to --memory option must be a non-negative number."));
