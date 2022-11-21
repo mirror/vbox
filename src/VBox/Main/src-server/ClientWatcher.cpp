b4811451bccb130c71508a2b457a0321faf24baf
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2022-11-21 15:51:36 +0000

    Main/ClientWatcher: Resolve signals to strings on non-Windows, to make reading the logs (also for the testboxes) a bit easier [build fix].
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@97633 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Main/src-server/ClientWatcher.cpp
+++ src/VBox/Main/src-server/ClientWatcher.cpp
@@ -213,11 +213,11 @@ uint32_t VirtualBox::ClientWatcher::reapProcesses(void)
                                     pid, pid, Status.iStatus, Status.iStatus));
                             break;
                         case RTPROCEXITREASON_SIGNAL:
-                            LogRel(("Reaper: Pid %d (%x) was signalled: %s (%d / %#x)\n",
+                            LogRel(("Reaper: Pid %d (%x) was signalled: %s (%d / %#x)\n"
 #if defined(RT_OS_WINDOWS) || defined(RT_OS_OS2)
-                                    pid, pid, "", Status.iStatus, Status.iStatus));
+                                    , pid, pid, "", Status.iStatus, Status.iStatus));
 #else
-                                    pid, pid, strsignal(Status.iStatus), Status.iStatus, Status.iStatus));
+                                    , pid, pid, strsignal(Status.iStatus), Status.iStatus, Status.iStatus));
 #endif
                             break;
                     }
