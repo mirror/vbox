cc8ad4b159b7d8d21eed1befed00db3aef40b176
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2022-12-05 08:17:08 +0000

    DnD/VBoxClient: Check for a potentially valid window handle in DragInstance::wndX11GetNameA() before calling the X server.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@97738 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Additions/x11/VBoxClient/draganddrop.cpp
+++ src/VBox/Additions/x11/VBoxClient/draganddrop.cpp
@@ -2813,17 +2813,17 @@ int DragInstance::proxyWinHide(void)
  * The returned pointer must be freed using RTStrFree().
  *
  * @returns Pointer to the allocated window name.
+ * @retval  NULL on allocation failure.
+ * @retval  "<No name>" if window name was not found / invalid window handle.
  * @param   wndThis                 Window to retrieve name for.
- *
- * @remark If the window title is not available, the text
- *         "<No name>" will be returned.
  */
 char *DragInstance::wndX11GetNameA(Window wndThis) const
 {
     char *pszName = NULL;
 
     XTextProperty propName;
-    if (XGetWMName(m_pDisplay, wndThis, &propName))
+    if (   wndThis != None
+        && XGetWMName(m_pDisplay, wndThis, &propName))
     {
         if (propName.value)
             pszName = RTStrDup((char *)propName.value); /** @todo UTF8? */
