bb7a92d2b3cba781cb2f0c9812a877bb360498b2
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2023-01-11 05:41:42 +0000

    Devices/Graphics: logging. bugref:9830
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@98050 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Devices/Graphics/DevVGA-SVGA3d-dx-shader.cpp
+++ src/VBox/Devices/Graphics/DevVGA-SVGA3d-dx-shader.cpp
@@ -2360,6 +2360,9 @@ static int dxbcCreateIOSGNBlob(DXShaderInfo const *pInfo, DXBCHeader *pHdr, uint
     if (!dxbcByteWriterCanWrite(w, sizeof(DXBCBlobHeader) + cbBlob))
         return VERR_NO_MEMORY;
 
+    Log6(("Create signature type %c%c%c%c (0x%RX32)\n",
+          RT_BYTE1(u32BlobType), RT_BYTE2(u32BlobType), RT_BYTE3(u32BlobType), RT_BYTE4(u32BlobType), u32BlobType));
+
     DXBCBlobHeader *pHdrBlob = (DXBCBlobHeader *)dxbcByteWriterPtr(w);
     pHdrBlob->u32BlobType = u32BlobType;
     // pHdrBlob->cbBlob = 0;
@@ -2390,6 +2393,10 @@ static int dxbcCreateIOSGNBlob(DXShaderInfo const *pInfo, DXBCHeader *pHdr, uint
         dst->enmComponentType = srcEntry->componentType;
         dst->idxRegister      = srcEntry->registerIndex;
         dst->u.mask           = srcEntry->mask;
+
+        Log6(("  [%u]: %s[%u] sv %u type %u reg %u mask %X\n",
+              iSignatureEntry, srcSemantic->pcszSemanticName, dst->idxSemantic,
+              dst->enmSystemValue, dst->enmComponentType, dst->idxRegister, dst->u.mask));
 
         if (dst->offElementName == 0)
         {
