bc237055f13273d35ebf3d63f598a804245e6a76
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2023-01-31 11:28:31 +0000

    VMM/IEM: Nested VMX: bugref:10318 Pause-loop exiting is exposed to the guest, don't assert on it.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@98360 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/VMM/VMMAll/IEMAllCImplVmxInstr.cpp
+++ src/VBox/VMM/VMMAll/IEMAllCImplVmxInstr.cpp
@@ -6555,7 +6555,6 @@ static int iemVmxVmentryCheckCtls(PVMCPUCC pVCpu, const char *pszInstr) RT_NOEXC
         Assert(!(pVmcs->u32ProcCtls2 & VMX_PROC_CTLS2_PML));                /* We don't support PML yet. */
         Assert(!(pVmcs->u32ProcCtls2 & VMX_PROC_CTLS2_VMFUNC));             /* We don't support VM functions yet. */
         Assert(!(pVmcs->u32ProcCtls2 & VMX_PROC_CTLS2_EPT_XCPT_VE));        /* We don't support EPT-violation #VE yet. */
-        Assert(!(pVmcs->u32ProcCtls2 & VMX_PROC_CTLS2_PAUSE_LOOP_EXIT));    /* We don't support Pause-loop exiting yet. */
         Assert(!(pVmcs->u32ProcCtls2 & VMX_PROC_CTLS2_TSC_SCALING));        /* We don't support TSC-scaling yet. */
 
         /* VMCS shadowing. */
