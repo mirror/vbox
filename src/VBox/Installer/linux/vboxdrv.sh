e4cd77fdd595b444f9671b069430163bf03b8adc
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2023-02-14 15:37:37 +0000

    Linux: rcvboxdrv: Allow to bypass forced kernel modules signature validation, bugref:10287.
    Added a tweak which allows to bypass modules signature validation for distributions
    which do not provide tools for that. Administrator should explicitly add:
    
      VBOX_BYPASS_MODULES_SIGNATURE_CHECK="1"
    
    into /etc/vbox/vbox.cfg in order to enable this tweak.
    
    
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@98565 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Installer/linux/vboxdrv.sh
+++ src/VBox/Installer/linux/vboxdrv.sh
@@ -388,6 +388,15 @@ module_signed()
     mod="$1"
     [ -n "$mod" ] || return
 
+    # Be nice with distributions which do not provide tools which we
+    # use in order to verify module signature. This variable needs to
+    # be explicitly set by administrator. This script will look for it
+    # in /etc/vbox/vbox.cfg. Make sure that you know what you do!
+    if [ "$VBOX_BYPASS_MODULES_SIGNATURE_CHECK" = "1" ]; then
+        echo "1"
+        return
+    fi
+
     extraction_tool=/lib/modules/"$(uname -r)"/build/scripts/extract-module-sig.pl
     mod_path=$(module_path "$mod" 2>/dev/null)
     openssl_tool=$(which openssl 2>/dev/null)
