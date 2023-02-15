5510bd1c013340551fe0a5f3afab641f5bae767b
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2023-02-15 16:48:37 +0000

    Additions: X11: Make legacy display resize service to gracefully shutdown, bugref:10359.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@98594 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Additions/x11/VBoxClient/display.cpp
+++ src/VBox/Additions/x11/VBoxClient/display.cpp
@@ -31,6 +31,7 @@
 #include <iprt/file.h>
 #include <iprt/mem.h>
 #include <iprt/string.h>
+#include <iprt/asm.h>
 
 #include <X11/Xlib.h>
 #include <X11/Xatom.h>
@@ -145,7 +146,7 @@ static void doResize(struct DISPLAYSTATE *pState)
 
 /** Main loop: handle display hot-plug events, property updates (which can
  *  signal VT switches hot-plug in old X servers). */
-static void runDisplay(struct DISPLAYSTATE *pState)
+static void runDisplay(struct DISPLAYSTATE *pState, bool volatile *pfShutdown)
 {
     Display *pDisplay = pState->pDisplay;
     long cValue = 1;
@@ -165,7 +166,7 @@ static void runDisplay(struct DISPLAYSTATE *pState)
     /* Interrupting this cleanly will be more work than making it robust
      * against spontaneous termination, especially as it will never get
      * properly tested, so I will go for the second. */
-    while (true)
+    while (!ASMAtomicReadBool(pfShutdown))
     {
         XEvent event;
         struct pollfd PollFd;
@@ -280,13 +281,23 @@ static DECLCALLBACK(int) init(void)
  */
 static DECLCALLBACK(int) run(bool volatile *pfShutdown)
 {
-    RT_NOREF(pfShutdown); /** @todo Probably very wrong not to check pfShutdown... Especially given no pfnStop implementation. */
     struct DISPLAYSTATE *pSelf = &g_DisplayState;
 
     if (!pSelf->mfInit)
         return VERR_WRONG_ORDER;
-    runDisplay(pSelf);
-    return VERR_INTERNAL_ERROR;  /* "Should never reach here." */
+
+    runDisplay(pSelf, pfShutdown);
+
+    return VINF_SUCCESS;
+}
+
+/**
+ * @interface_method_impl{VBCLSERVICE,pfnStop}
+ */
+static DECLCALLBACK(void) stop(void)
+{
+    /* Nothing to do here. Implement empty callback, so
+     * main thread can set pfShutdown=true on process termination. */
 }
 
 VBCLSERVICE g_SvcDisplayLegacy =
@@ -299,6 +310,6 @@ VBCLSERVICE g_SvcDisplayLegacy =
     NULL,                               /* pfnOption */
     init,                               /* pfnInit */
     run,                                /* pfnWorker */
-    NULL,                               /* pfnStop */
+    stop,                               /* pfnStop */
     NULL,                               /* pfnTerm */
 };
