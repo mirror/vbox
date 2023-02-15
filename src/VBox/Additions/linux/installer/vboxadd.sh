71bea93bfcbf71747bd489edb3fb08a9463cc95b
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2023-02-15 17:25:30 +0000

    Additions: Linux: rcvboxadd: Do not rebuild kernel modules on system shutdown, bugref:10359.
    This commit changes previous behavior when Guest Additions' kernel modules rebuild
    was triggered on system shutdown ("rcvboxadd stop"). This is probably no longer needed.
    Script will check, as before, if modules rebuild is needed on system start
    ("rcvboxadd start") and will attempt to reload them (as well as restart
    corresponding services) after rebuild is completed.
    
    
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@98595 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Additions/linux/installer/vboxadd.sh
+++ src/VBox/Additions/linux/installer/vboxadd.sh
@@ -70,21 +70,6 @@ SERVICE="VirtualBox Guest Additions"
 QUIET=
 test -z "${TARGET_VER}" && TARGET_VER=`uname -r`
 
-# Marker to ignore a particular kernel version which was already installed.
-#
-# This is needed in order to prevent modules rebuild on system start and do
-# that on system shutdown instead. Modern Linux distributions might attempt
-# to run Additions service in async mode. As a result, on system boot, modules
-# not-by-us will be loaded while we will try to build our modules. This marker is:
-#
-#   created -- in scope of setup_modules() when actual modules are built.
-#   checked -- in scope of stop() when system goes shutdown and if marker
-#              for certain kernel version does not exist, modules rebuild
-#              will be triggered for this kernel version.
-#   removed -- in scope of cleanup_modules() when modules are removed from
-#              system for all installed kernels.
-SKIPFILE_BASE=/var/lib/VBoxGuestAdditions/skip
-
 export VBOX_KBUILD_TYPE
 export USERNAME
 
@@ -330,7 +315,6 @@ cleanup_modules()
         rm -rf "/var/lib/dkms/${i}"*
     done
     rm -f /etc/depmod.d/vboxvideo-upstream.conf
-    rm -f "$SKIPFILE_BASE"-*
 }
 
 # Secure boot state.
@@ -545,10 +529,6 @@ setup_modules()
     sign_modules "${KERN_VER}"
 
     update_initramfs "${KERN_VER}"
-
-    # We have just built modules for KERN_VER kernel. Create a marker to indicate
-    # that modules for this kernel version should not be rebuilt on system shutdown.
-    touch "$SKIPFILE_BASE"-"$KERN_VER"
 
     return 0
 }
@@ -872,20 +852,15 @@ for your Linux distribution."
         test -d /sys &&
             ps -A -o comm | grep -q '/*udevd$' 2>/dev/null ||
             no_udev=1
-        running_vboxguest || {
+        check_running_module_version "vboxguest" || {
             rm -f $dev || {
                 fail "Cannot remove $dev"
             }
             rm -f $userdev || {
                 fail "Cannot remove $userdev"
             }
-            $MODPROBE vboxguest >/dev/null 2>&1 ||
-                fail "modprobe vboxguest failed"
-            case "$no_udev" in 1)
-                sleep .5;;
-            esac
-            $MODPROBE vboxsf > /dev/null 2>&1 ||
-                info "modprobe vboxsf failed"
+            # Assuming modules were just (re-)built, try to reload everything.
+            reload
         }
         case "$no_udev" in 1)
             do_vboxguest_non_udev;;
@@ -898,15 +873,7 @@ for your Linux distribution."
 stop()
 {
     begin "Stopping."
-    if test -z "${INSTALL_NO_MODULE_BUILDS}"; then
-        # We want to build modules for newly installed kernels on shutdown, so
-        # check which we marked at start-up.
-        for setupi in /lib/modules/*; do
-            KERN_VER="${setupi##*/}"
-            # For a full setup, mark kernels we do not want to build.
-            test -f "$SKIPFILE_BASE"-"$KERN_VER" || setup_modules "$KERN_VER"
-        done
-    fi
+
     if test -r /etc/ld.so.conf.d/00vboxvideo.conf; then
         rm /etc/ld.so.conf.d/00vboxvideo.conf
         ldconfig
@@ -1082,7 +1049,7 @@ reload()
         # Check if we just loaded modules of correct version. Skip vboxvideo
         # since it's not loaded for very old guests.
         for mod in vboxguest vboxsf; do
-            check_running_module_version "$mod" || fail "currently loaded module $mod version ($(running_module_version "$mod") does not match to VirtualBox Guest Additions installation version ($VBOX_VERSION $VBOX_REVISION))"
+            check_running_module_version "$mod" || fail "currently loaded module $mod version ($(running_module_version "$mod")) does not match to VirtualBox Guest Additions installation version ($VBOX_VERSION $VBOX_REVISION)"
         done
         # Take reported version of running Guest Additions from running vboxguest module (as a paranoia check).
         info "kernel modules and services $(running_module_version "vboxguest") reloaded"
