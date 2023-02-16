790053969ea1913aeb677656d9354b45cb49064d
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2023-02-16 00:54:55 +0000

    Add/VBoxDrvInst: Logging correction.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@98598 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Additions/WINNT/Installer/VBoxDrvInst.cpp
+++ src/VBox/Additions/WINNT/Installer/VBoxDrvInst.cpp
@@ -244,7 +244,7 @@ static void __cdecl VBoxDIFxLogCallback(DIFXAPI_LOG enmEvent, DWORD dwError, PCW
         RTStrCopy(szBuf, sizeof(szBuf), pszEvent);
         RTStrCat(szBuf, sizeof(szBuf), ": ");
         size_t offVal = strlen(szBuf);
-        RTStrFormatU32(&szBuf[offVal], sizeof(szBuf) - offVal, dwError, 10, 0, 0, 0);
+        RTStrFormatU32(&szBuf[offVal], sizeof(szBuf) - offVal, dwError, 16, 0, 0, dwError ? RTSTR_F_SPECIAL : 0);
         RTStrCat(szBuf, sizeof(szBuf), " - ");
         DWORD dwIgn;
         WriteFile(hLogFile, szBuf, (DWORD)strlen(szBuf), &dwIgn, NULL);
