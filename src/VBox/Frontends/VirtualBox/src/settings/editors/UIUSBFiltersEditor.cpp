b988e3369eb4ef4d239930fbfb3fa75c5bdb0b24
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2023-03-20 11:15:27 +0000

    FE/Qt: bugref:10343: Machine settings / USB page: While adding USB device filter on the basis of existing USB device we should (should we?) init port field as well.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@99062 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Frontends/VirtualBox/src/settings/editors/UIUSBFiltersEditor.cpp
+++ src/VBox/Frontends/VirtualBox/src/settings/editors/UIUSBFiltersEditor.cpp
@@ -402,9 +402,7 @@ void UIUSBFiltersEditor::sltAddFilterConfirmed(QAction *pAction)
      * device itself; for this reason only a few people will want to use it
      * in the filter since the same device plugged into a different socket
      * will not match the filter in this case. */
-#if 0
-    newFilterData.m_strPort = QString().sprintf("%04hX", comUsb.GetPort());
-#endif
+    newFilterData.m_strPort = QString::asprintf("%#06hX", comUsb.GetPort());
     newFilterData.m_strManufacturer = comUsb.GetManufacturer();
     newFilterData.m_strProduct = comUsb.GetProduct();
     newFilterData.m_strSerialNumber = comUsb.GetSerialNumber();
