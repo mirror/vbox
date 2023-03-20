6d9492a2b3b9ab12e5da422f890d1254fd7d9057
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2023-03-20 12:01:53 +0000

    manual/Makefile.kmk: Cleaning up html and qhelp outputting. The latter doesn't need an extra copy of the Docbook version of the images, since DITA-OT copies the necessary images to qhelp/topics/images/.  bugref:10348 bugref:10302
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@99064 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- doc/manual/htmlhelp-qthelp.py
+++ doc/manual/htmlhelp-qthelp.py
@@ -1,22 +1,12 @@
 #!/usr/bin/python3
-
+# -*- coding: utf-8 -*-
 # $Id$
-## @file
-# A python script to create a .qhp file out of a given htmlhelp
-# folder. Lots of things about the said folder is assumed. Please
-# see the code and inlined comments.
 
-import sys, getopt
-import os.path
-import re
-import logging
-
-if sys.version_info >= (3, 0):
-    from html.parser import HTMLParser
-else:
-    from HTMLParser import HTMLParser
-
-
+"""
+A python script to create a .qhp file out of a given htmlhelp
+folder. Lots of things about the said folder is assumed. Please
+see the code and inlined comments.
+"""
 
 __copyright__ = \
 """
@@ -41,67 +31,75 @@ along with this program; if not, see <https://www.gnu.org/licenses>.
 SPDX-License-Identifier: GPL-3.0-only
 """
 
+import getopt
+import logging
+import os.path
+import re
+import sys
+
+if sys.version_info[0] >= 3:
+    from html.parser import HTMLParser
+else:
+    from HTMLParser import HTMLParser
+
 # number of opened and not yet closed section tags of toc section
 open_section_tags = 0
 
 html_files = []
 
-# use html_parser stuff to collect <a name tags
 def create_keywords_section(folder):
+    """
+    use html_parser stuff to collect <a name ...> tags
+    """
     keywords_section_lines = ['<keywords>']
     for html_file_name in html_files:
         full_html_path = os.path.join(folder, html_file_name)
         file_content = open(full_html_path, encoding='utf-8').read()
 
         class html_parser(HTMLParser):
-          def __init__(self):
-            HTMLParser.__init__(self)
-            self.a_tag=[]
-          def handle_starttag(self, tag, attributes):
-            if tag != 'div' and tag != 'a':
-              return
-            if tag == 'a':
-              for a in attributes:
-                if a[0] == 'name':
-                  self.a_tag.append(a[1])
+            def __init__(self):
+                HTMLParser.__init__(self)
+                self.a_tag = []
+            def handle_starttag(self, tag, attrs):
+                if tag != 'div' and tag != 'a':
+                    return
+                if tag == 'a':
+                    for a in attrs:
+                        if a[0] == 'name':
+                            self.a_tag.append(a[1])
 
         parser = html_parser()
         parser.feed(file_content)
         for k in parser.a_tag:
             line = '<keyword name="' + k + '" id="' + k + '" ref="' + html_file_name + '#' + k + '"/>'
-            keywords_section_lines.append(line);
+            keywords_section_lines.append(line)
     keywords_section_lines.append('</keywords>')
     return keywords_section_lines
 
-# find the png files under /images folder and create a part of the
-# qhelp project file with <file> tags
 def create_image_list(folder):
-    image_folder_name = 'images'
-    image_files_list = []
-    # Look for 'images' sub folder
-    subdirs = [x[0] for x in os.walk(folder)]
-    full_folder_path = os.path.join(folder, image_folder_name)
-    if full_folder_path not in subdirs:
-        logging.error('Image subfolder "%s" is not found under "%s".', image_folder_name, folder)
-        return image_files_list;
-    png_files = []
-    for f in os.listdir(full_folder_path):
-        png_files.append(image_folder_name + '/' + f)
-        image_files_list.append('<file>images/' + f + '</file>')
-    return image_files_list
+    """
+    find the png files under topics/images folder and create a part of the
+    qhelp project file with <file> tags
+    """
+    sFullImageFolderPath = os.path.join(folder, 'topics', 'images');
+    if not os.path.isdir(sFullImageFolderPath):
+        logging.error('Image subfolder "topics/images" is not found under "%s"!', folder)
+        sys.exit(1);
+    return ['<file>topics/images/%s</file>' % sFile for sFile in os.listdir(sFullImageFolderPath)];
 
-# open files list and read the list of html files from there
 def create_html_list(folder, list_file):
+    """
+    open files list and read the list of html files from there
+    """
     global html_files
     html_file_lines = []
     if not list_file in os.listdir(folder):
         logging.error('Could not find the file "%s" in "%s"', list_file, folder)
         return html_file_lines
     full_path = os.path.join(folder, list_file)
-    file = open(full_path, encoding='utf-8')
+    with open(full_path, encoding='utf-8') as file:
+        lines = file.readlines()
 
-    lines = file.readlines()
-    file.close()
     # first search for the [FILES] marker then collect .html lines
     marker_found = 0
     for line in lines:
@@ -125,28 +123,30 @@ def create_files_section(folder, list_file):
 
 def parse_param_tag(line):
     label = 'value="'
-    start = line.find(label);
+    start = line.find(label)
     if start == -1:
         return ''
-    start +=  len(label)
+    start += len(label)
     end = line.find('"', start)
     if end == -1:
-        return '';
+        return ''
     return line[start:end]
 
-# look at next two lines. they are supposed to look like the following
-#      <param name="Name" value="Oracle VM VirtualBox">
-#      <param name="Local" value="index.html">
-# parse out value fields and return
-# title="Oracle VM VirtualBox" ref="index.html
 def parse_object_tag(lines, index):
-    result=''
+    """
+    look at next two lines. they are supposed to look like the following
+         <param name="Name" value="Oracle VM VirtualBox">
+         <param name="Local" value="index.html">
+    parse out value fields and return
+    title="Oracle VM VirtualBox" ref="index.html
+    """
+    result = ''
     if index + 2 > len(lines):
-        logging.warning('Not enough tags after this one "%s"',lines[index])
+        logging.warning('Not enough tags after this one "%s"', lines[index])
         return result
-    if not re.match(r'^\s*<param', lines[index + 1], re.IGNORECASE) or \
-       not re.match(r'^\s*<param', lines[index + 2], re.IGNORECASE):
-        logging.warning('Skipping the line "%s" since next two tags are supposed to be param tags',  lines[index])
+    if    not re.match(r'^\s*<param', lines[index + 1], re.IGNORECASE) \
+       or not re.match(r'^\s*<param', lines[index + 2], re.IGNORECASE):
+        logging.warning('Skipping the line "%s" since next two tags are supposed to be param tags', lines[index])
         return result
     title = parse_param_tag(lines[index + 1])
     ref = parse_param_tag(lines[index + 2])
@@ -158,9 +158,12 @@ def parse_object_tag(lines, index):
         logging.warning('Title or ref part is empty for the tag "%s"', lines[index])
     return result
 
-# parse any string other than staring with <OBJECT
-# decide if <session tag should be closed
 def parse_non_object_tag(lines, index):
+    """
+    parse any string other than staring with <OBJECT
+    decide if <section> tag should be closed
+    """
+
     if index + 1 > len(lines):
         return ''
     global open_section_tags
@@ -177,7 +180,7 @@ def parse_non_object_tag(lines, index):
     return ''
 
 def parse_line(lines, index):
-    result=''
+    result = ''
 
     # if the line starts with <OBJECT
     if re.match(r'^\s*<OBJECT', lines[index], re.IGNORECASE):
@@ -186,20 +189,22 @@ def parse_line(lines, index):
         result = parse_non_object_tag(lines, index)
     return result
 
-# parse TOC file. assuming all the relevant information
-# is stored in tags and attributes. whatever is outside of
-# <... > pairs is filtered out. we also assume < ..> are not nested
-# and each < matches to a >
 def create_toc(folder, toc_file):
+    """
+    parse TOC file. assuming all the relevant information
+    is stored in tags and attributes. whatever is outside of
+    <... > pairs is filtered out. we also assume < ..> are not nested
+    and each < matches to a >
+    """
     toc_string_list = []
     content = [x[2] for x in os.walk(folder)]
     if toc_file not in content[0]:
         logging.error('Could not find toc file "%s" under "%s"', toc_file, folder)
         return toc_string_list
     full_path = os.path.join(folder, toc_file)
-    file = open(full_path, encoding='utf-8')
-    content = file.read()
-    file.close()
+    with open(full_path, encoding='utf-8') as file:
+        content = file.read()
+
     # convert the file string into a list of tags there by eliminating whatever
     # char reside outside of tags.
     char_pos = 0
@@ -218,70 +223,70 @@ def create_toc(folder, toc_file):
     # content = re.sub(r'>.*?<', r'>\n<', content)
     # lines = content.split('\n')
     toc_string_list.append('<toc>')
-    index = 0
-    for tag in tag_list:
+    for index, _ in enumerate(tag_list):
         str = parse_line(tag_list, index)
         if str:
             toc_string_list.append(str)
-        index += 1
     toc_string_list.append('</toc>')
     toc_string = '\n'.join(toc_string_list)
 
     return toc_string_list
 
-def usage(arg):
+def usage(iExitCode):
     print('htmlhelp-qthelp.py -d <helphtmlfolder> -o <outputfilename>')
-    sys.exit()
+    return iExitCode
 
 def main(argv):
+    # Parse arguments.
     helphtmlfolder = ''
     output_filename = ''
     list_file = ''
     toc_file = ''
     try:
-        opts, args = getopt.getopt(sys.argv[1:],"hd:o:f:t:")
+        opts, _ = getopt.getopt(argv[1:], "hd:o:f:t:")
     except getopt.GetoptError as err:
-        print(err)
-        usage(2)
+        logging.error(str(err))
+        return usage(2)
     for opt, arg in opts:
         if opt == '-h':
-            usage(0)
-        elif opt in ("-d"):
+            return usage(0)
+        if opt == "-d":
             helphtmlfolder = arg
             print(helphtmlfolder)
-        elif opt in ("-f"):
+        elif opt == "-f":
             list_file = arg
-        elif opt in ("-t"):
+        elif opt == "-t":
             toc_file = arg
             print(toc_file)
-        elif opt in ("-o"):
-             output_filename = arg
+        elif opt == "-o":
+            output_filename = arg
     # check supplied helphtml folder argument
     if not helphtmlfolder:
         logging.error('No helphtml folder is provided. Exiting')
-        usage(2)
+        return usage(2)
     if not os.path.exists(helphtmlfolder):
         logging.error('folder "%s" does not exist. Exiting', helphtmlfolder)
-        usage(2)
+        return usage(2)
     helphtmlfolder = os.path.normpath(helphtmlfolder)
 
     # check supplied output file name
     if not output_filename:
         logging.error('No filename for output is given. Exiting')
-        usage(2)
+        return usage(2)
 
-    out_xml_lines = ['<?xml version="1.0" encoding="UTF-8"?>', \
-                     '<QtHelpProject version="1.0">' , \
-                     '<namespace>org.virtualbox</namespace>', \
-                     '<virtualFolder>doc</virtualFolder>', \
+    out_xml_lines = ['<?xml version="1.0" encoding="UTF-8"?>',
+                     '<QtHelpProject version="1.0">',
+                     '<namespace>org.virtualbox</namespace>',
+                     '<virtualFolder>doc</virtualFolder>',
                      '<filterSection>']
-    out_xml_lines += create_toc(helphtmlfolder, toc_file) + create_files_section(helphtmlfolder, list_file)
+    out_xml_lines += create_toc(helphtmlfolder, toc_file)
+    out_xml_lines += create_files_section(helphtmlfolder, list_file)
     out_xml_lines += create_keywords_section(helphtmlfolder)
     out_xml_lines += ['</filterSection>', '</QtHelpProject>']
 
-    out_file = open(output_filename, 'wb')
-    out_file.write('\n'.join(out_xml_lines).encode('utf8'))
-    out_file.close()
+    with open(output_filename, 'wb') as out_file:
+        out_file.write('\n'.join(out_xml_lines).encode('utf8'))
+    return 0
 
 if __name__ == '__main__':
-    main(sys.argv[1:])
+    sys.exit(main(sys.argv))
