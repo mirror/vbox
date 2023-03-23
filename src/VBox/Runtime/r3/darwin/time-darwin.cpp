8e72c45e58bb75ff7d43e9ffa55845b1b2ada66f
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2023-03-23 08:23:39 +0000

    Revert r156473 as clock_gettime_nsec_np(CLOCK_UPTIME_RAW) is apparently doing the same as we do just not as efficient (calls mach_timebase_info() every time)
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@99127 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Runtime/r3/darwin/time-darwin.cpp
+++ src/VBox/Runtime/r3/darwin/time-darwin.cpp
@@ -53,7 +53,6 @@
 /*********************************************************************************************************************************
 *   Global Variables                                                                                                             *
 *********************************************************************************************************************************/
-#if defined(RT_ARCH_AMD64) || defined(RT_ARCH_X86)
 static struct mach_timebase_info    g_Info = { 0, 0 };
 static double                       g_rdFactor = 0.0;
 static bool                         g_fFailedToGetTimeBaseInfo = false;
@@ -76,7 +75,6 @@ static void rtTimeDarwinLazyInit(void)
         Assert(g_Info.denom == 0 && g_Info.numer == 0 && g_rdFactor == 0.0);
     }
 }
-#endif
 
 
 /**
@@ -85,7 +83,6 @@ static void rtTimeDarwinLazyInit(void)
  */
 DECLINLINE(uint64_t) rtTimeGetSystemNanoTS(void)
 {
-#if defined(RT_ARCH_AMD64) || defined(RT_ARCH_X86)
     /* Lazy init. */
     if (RT_UNLIKELY(g_Info.denom == 0 && !g_fFailedToGetTimeBaseInfo))
         rtTimeDarwinLazyInit();
@@ -103,9 +100,6 @@ DECLINLINE(uint64_t) rtTimeGetSystemNanoTS(void)
     gettimeofday(&tv, NULL);
     return (uint64_t)tv.tv_sec  * RT_NS_1SEC_64
          + (uint64_t)(tv.tv_usec * RT_NS_1US);
-#else
-    return clock_gettime_nsec_np(CLOCK_UPTIME_RAW);
-#endif
 }
 
 
