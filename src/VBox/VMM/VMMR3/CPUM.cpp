329d5c12ea40ce359e95640dd0f5ab3fc6147680
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2023-03-29 15:12:04 +0000

    Disassembler,VMM,Runtime: Get rid of deprecated DISCPUSTATE types (preparation for architecture specific separation in order to support ARMv8), bugref:10394 [build fix, missing file]
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@99212 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/VMM/VMMR3/CPUM.cpp
+++ src/VBox/VMM/VMMR3/CPUM.cpp
@@ -4439,7 +4439,7 @@ VMMR3DECL(int) CPUMR3DisasmInstrCPU(PVM pVM, PVMCPU pVCpu, PCPUMCTX pCtx, RTGCPT
     uint32_t cbInstr;
 #ifndef LOG_ENABLED
     RT_NOREF_PV(pszPrefix);
-    rc = DISInstrWithReader(GCPtrPC, enmDisCpuMode, cpumR3DisasInstrRead, &State, pCpu, &cbInstr);
+    rc = DISInstrWithReader(GCPtrPC, enmDisCpuMode, cpumR3DisasInstrRead, &State, pDis, &cbInstr);
     if (RT_SUCCESS(rc))
     {
 #else
