caa1ecb9cdf2b665402ee06c17747cae7f4fc13e
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2023-03-30 12:09:55 +0000

    FE/Qt: bugref:6669: Build fix for r156591.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@99219 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Frontends/VirtualBox/src/manager/UIVirtualBoxManager.cpp
+++ src/VBox/Frontends/VirtualBox/src/manager/UIVirtualBoxManager.cpp
@@ -1001,11 +1001,9 @@ void UIVirtualBoxManager::sltOpenWizard(WizardType enmType)
                     const QUuid uId = m_pWidget->currentSnapshotId();
                     if (!uId.isNull() && comMachine.isNotNull())
                     {
-                        printf("Looking for snapshot with ID = {%s}\n", uId.toString().toUtf8().constData());
                         comSnapshot = comMachine.FindSnapshot(uId.toString());
                         if (comSnapshot.isNotNull())
                         {
-                            printf("Snapshot found, acquiring machine ..\n");
                             const CMachine comSnapshotMachine = comSnapshot.GetMachine();
                             if (comSnapshotMachine.isNotNull())
                                 comMachine = comSnapshotMachine;
