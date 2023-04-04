5bad4cc135e770510fe1e1e431cef60f1fd12d57
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2023-04-04 16:47:54 +0000

    Guest Control/Validation Kit: Introduced a process create wrapper function, as the process creation code was getting way too convoluted with all those API changes and differences [pylint].
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@99285 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/ValidationKit/tests/additions/tdAddGuestCtrl.py
+++ src/VBox/ValidationKit/tests/additions/tdAddGuestCtrl.py
@@ -5565,10 +5565,10 @@ class tdAddGuestCtrl(vbox.TestDriver):                                         #
         aWaitFor = [ vboxcon.GuestSessionWaitForFlag_Start ];
         _ = oGuestSession.waitForArray(aWaitFor, 30 * 1000);
 
-        sCmd = self.getGuestSystemShell(oTestVm);
-        asArgs = [ sCmd, '/C', 'dir', '/S', 'c:\\windows' ];
-        aEnv = [];
-        afFlags = [];
+        #sCmd = self.getGuestSystemShell(oTestVm);
+        #asArgs = [ sCmd, '/C', 'dir', '/S', 'c:\\windows' ];
+        #aEnv = [];
+        #afFlags = [];
 
         # Fix this once being used (again).
         #for _ in xrange(100):
