0e6e9dbfcd61fb6b03a90c92d53c40a284fb23fc
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2023-04-06 16:02:43 +0000

    VMM/IEM: Nested VMX: bugref:10318 Fixed race while registering the virtual VMX APIC-access page by multiple VCPUs.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@99318 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/VMM/VMMAll/IEMAllCImplVmxInstr.cpp
+++ src/VBox/VMM/VMMAll/IEMAllCImplVmxInstr.cpp
@@ -7180,16 +7180,12 @@ static int iemVmxVmentryLoadGuestVmcsRefState(PVMCPUCC pVCpu, const char *pszIns
          *
          * See Intel spec. 24.11.4 "Software Access to Related Structures".
          */
-        if (!PGMHandlerPhysicalIsRegistered(pVCpu->CTX_SUFF(pVM), GCPhysApicAccess))
-        {
-            PVMCC pVM = pVCpu->CTX_SUFF(pVM);
-            int rc = PGMHandlerPhysicalRegister(pVM, GCPhysApicAccess, GCPhysApicAccess | X86_PAGE_4K_OFFSET_MASK,
-                                                pVM->iem.s.hVmxApicAccessPage, 0 /*uUser*/, NULL /*pszDesc*/);
-            if (RT_SUCCESS(rc))
-            { /* likely */ }
-            else
-                IEM_VMX_VMENTRY_FAILED_RET(pVCpu, pszInstr, pszFailure, kVmxVDiag_Vmentry_AddrApicAccessHandlerReg);
-        }
+        PVMCC pVM = pVCpu->CTX_SUFF(pVM);
+        int rc = PGMHandlerPhysicalRegisterVmxApicAccessPage(pVM, GCPhysApicAccess, pVM->iem.s.hVmxApicAccessPage);
+        if (RT_SUCCESS(rc))
+        { /* likely */ }
+        else
+            IEM_VMX_VMENTRY_FAILED_RET(pVCpu, pszInstr, pszFailure, kVmxVDiag_Vmentry_AddrApicAccessHandlerReg);
     }
 
     /*
