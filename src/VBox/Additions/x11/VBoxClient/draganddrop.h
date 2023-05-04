609e2e089dd40dd8692a2ab2eaa89f07af423e9a
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2023-05-04 10:09:59 +0000

    Guest Additions/VBoxClient: Moved the X11-specific code for drag'n drop into an own submodule and using the actual service as a (runtime) wrapper (for also the Wayland-specific code later) [build fix]. bugref:10427
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@99599 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Additions/x11/VBoxClient/draganddrop.h
+++ src/VBox/Additions/x11/VBoxClient/draganddrop.h
@@ -114,11 +114,11 @@ public:
         RT_ZERO(m_dndCtx);
     }
 
-    virtual int  init(void) override;
-    virtual int  worker(bool volatile *pfShutdown) override;
-    virtual void reset(void) override;
-    virtual void stop(void) override;
-    virtual int  term(void) override;
+    virtual int  init(void);
+    virtual int  worker(bool volatile *pfShutdown);
+    virtual void reset(void);
+    virtual void stop(void);
+    virtual int  term(void);
 
 private:
 
