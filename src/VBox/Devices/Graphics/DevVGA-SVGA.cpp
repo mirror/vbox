bbabdd12df49a55ca6a185c7cff0afff990b9ad7
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2023-05-17 08:20:47 +0000

    Devices/Graphics: correct assertion
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@99824 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Devices/Graphics/DevVGA-SVGA.cpp
+++ src/VBox/Devices/Graphics/DevVGA-SVGA.cpp
@@ -7057,10 +7057,12 @@ static void vmsvgaR3PowerOnDevice(PPDMDEVINS pDevIns, PVGASTATE pThis, PVGASTATE
         uint32_t u32FIFOCaps = 0;
         vmsvgaR3GetCaps(pThis, pThisCC, &u32DeviceCaps, &u32DeviceCaps2, &u32FIFOCaps);
 
-        /* Capabilities should not change normally. */
-        Assert(   pThis->svga.u32DeviceCaps == u32DeviceCaps
-               && pThis->svga.u32DeviceCaps2 == u32DeviceCaps2
-               && pThisCC->svga.pau32FIFO[SVGA_FIFO_CAPABILITIES] == u32FIFOCaps);
+        /* Capabilities should not change normally.
+         * However the saved state might have a subset of currently implemented caps.
+         */
+        Assert(   (pThis->svga.u32DeviceCaps & u32DeviceCaps) == pThis->svga.u32DeviceCaps
+               && (pThis->svga.u32DeviceCaps2 & u32DeviceCaps2) == pThis->svga.u32DeviceCaps2
+               && (pThisCC->svga.pau32FIFO[SVGA_FIFO_CAPABILITIES] & u32FIFOCaps) == pThisCC->svga.pau32FIFO[SVGA_FIFO_CAPABILITIES]);
     }
 #endif
 
