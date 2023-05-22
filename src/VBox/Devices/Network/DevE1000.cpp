d56ebff21a6b46c39c6c0ba705ecdb0dbb1fd076
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2023-05-22 10:32:57 +0000

    Devices/DevE1000: Don't crash if there is no status driver attached, bugref:10383
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@99886 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Devices/Network/DevE1000.cpp
+++ src/VBox/Devices/Network/DevE1000.cpp
@@ -8260,9 +8260,12 @@ static DECLCALLBACK(int) e1kR3Construct(PPDMDEVINS pDevIns, int iInstance, PCFGM
     /* Status driver */
     PPDMIBASE pBase;
     rc = PDMDevHlpDriverAttach(pDevIns, PDM_STATUS_LUN, &pThisCC->IBase, &pBase, "Status Port");
-    if (RT_FAILURE(rc))
+    if (RT_SUCCESS(rc))
+        pThisCC->pLedsConnector = PDMIBASE_QUERY_INTERFACE(pBase, PDMILEDCONNECTORS);
+    else if (rc == VERR_PDM_NO_ATTACHED_DRIVER)
+        rc = VINF_SUCCESS;
+    else
         return PDMDEV_SET_ERROR(pDevIns, rc, N_("Failed to attach the status LUN"));
-    pThisCC->pLedsConnector = PDMIBASE_QUERY_INTERFACE(pBase, PDMILEDCONNECTORS);
 
     /* Network driver */
     rc = PDMDevHlpDriverAttach(pDevIns, 0, &pThisCC->IBase, &pThisCC->pDrvBase, "Network Port");
