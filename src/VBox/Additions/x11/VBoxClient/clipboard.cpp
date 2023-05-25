0b687e6298b96b505a0f362635fd0ef6dff31332
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2023-05-25 11:44:58 +0000

    Guest Additions/VBoxClient: Also use the X clipboard implementation for XWayland desktop environments (at least for now). ​bugref:10427
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@99977 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Additions/x11/VBoxClient/clipboard.cpp
+++ src/VBox/Additions/x11/VBoxClient/clipboard.cpp
@@ -82,7 +82,9 @@ static DECLCALLBACK(int) vbclShClWorker(bool volatile *pfShutdown)
 
     int rc = VINF_SUCCESS;
 
-    if (VBClGetDisplayServerType() == VBGHDISPLAYSERVERTYPE_X11)
+    if (   VBClGetDisplayServerType() == VBGHDISPLAYSERVERTYPE_X11
+        /* If Wayland w/ X fallback support is installed (also called XWayland), prefer using the X clipboard for now. */
+        || VBClGetDisplayServerType() == VBGHDISPLAYSERVERTYPE_XWAYLAND)
     {
         rc = VBClX11ClipboardInit();
         if (RT_SUCCESS(rc))
@@ -93,12 +95,10 @@ static DECLCALLBACK(int) vbclShClWorker(bool volatile *pfShutdown)
             rc = VBClX11ClipboardMain();
         }
     }
-#if 0
-    else (VBClGetSessionType() == GHDISPLAYSERVERTYPE_WAYLAND)
+    else if (VBClGetDisplayServerType() == VBGHDISPLAYSERVERTYPE_WAYLAND)
     {
-
+        VBClLogError("Shared Clipboard for Wayland not supported yet!\n");
     }
-#endif
 
     if (RT_FAILURE(rc))
         VBClLogError("Service terminated abnormally with %Rrc\n", rc);
