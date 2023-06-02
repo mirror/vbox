d463dcc730034401fcbff1fbe4955976b13f457c
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2023-06-02 09:06:45 +0000

    Runtime/RTFdt: Fix for converting the memory reservations when writing the DTB, bugref:10401 bugref:3409
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@100048 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Runtime/common/misc/fdt.cpp
+++ src/VBox/Runtime/common/misc/fdt.cpp
@@ -1066,8 +1066,8 @@ static int rtFdtDumpAsDtb(PRTFDTINT pThis, RTVFSIOSTREAM hVfsIos, PRTERRINFO pEr
     {
         DTBFDTRSVENTRY MemRsv;
 
-        MemRsv.PhysAddrStart = RT_H2BE_U32(pThis->paMemRsv[i].PhysAddrStart);
-        MemRsv.cbArea        = RT_H2BE_U32(pThis->paMemRsv[i].cbArea);
+        MemRsv.PhysAddrStart = RT_H2BE_U64(pThis->paMemRsv[i].PhysAddrStart);
+        MemRsv.cbArea        = RT_H2BE_U64(pThis->paMemRsv[i].cbArea);
         rc = RTVfsIoStrmWrite(hVfsIos, &MemRsv, sizeof(MemRsv), true /*fBlocking*/, NULL /*pcbWritten*/);
         if (RT_FAILURE(rc))
             return RTErrInfoSetF(pErrInfo, rc, "Failed to write memory reservation entry %u (%u bytes) to I/O stream",
