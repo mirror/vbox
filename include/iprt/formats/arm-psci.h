8eb505afd4dd77530101b72e7d0ce25dd6046bd0
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2023-06-07 17:55:58 +0000

    include/iprt/formats: Add headers for ARM SMCCC (SMC Calling Convention) and PSCI (Power State Coordination Interface) definitions required for CPU management and SMP, bugref:10454 [doxygen]
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@100103 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- include/iprt/formats/arm-psci.h
+++ include/iprt/formats/arm-psci.h
@@ -59,7 +59,7 @@
 #define ARM_PSCI_STS_DENIED                         INT32_C(-3)
 /** CPU is already online. */
 #define ARM_PSCI_STS_ALREADY_ON                     INT32_C(-4)
-/** @todo. */
+/** Operation is already pending. */
 #define ARM_PSCI_STS_ON_PENDING                     INT32_C(-5)
 /** PSCI implementation encountered an internal failure for the operation. */
 #define ARM_PSCI_STS_INTERNAL_FAILURE               INT32_C(-6)
