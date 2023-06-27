559a70cf5dfd795448e86f6ae1afbc7819918292
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2023-06-27 17:42:04 +0000

    FE/Qt: bugref:6699. Build fix.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@100303 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Frontends/VirtualBox/src/guestctrl/UIFileManagerTable.cpp
+++ src/VBox/Frontends/VirtualBox/src/guestctrl/UIFileManagerTable.cpp
@@ -390,10 +390,10 @@ UIFileManagerTable::UIFileManagerTable(UIActionPool *pActionPool, QWidget *pPare
     , m_pActionPool(pActionPool)
     , m_pToolBar(0)
     , m_pMainLayout(0)
+    , m_pNavigationWidget(0)
     , m_pModel(0)
     , m_pView(0)
     , m_pProxyModel(0)
-    , m_pNavigationWidget(0)
     , m_pathSeparator('/')
     , m_pToolBarLayout(0)
 {
