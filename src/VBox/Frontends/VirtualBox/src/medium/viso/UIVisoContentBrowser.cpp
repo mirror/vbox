c21b7423c7f337d716df2f22886edcc1132abb60
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2023-07-14 14:56:22 +0000

    FE/Qt: bugref:9080. Processing the list of removed items from a VISO file.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@100586 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Frontends/VirtualBox/src/medium/viso/UIVisoContentBrowser.cpp
+++ src/VBox/Frontends/VirtualBox/src/medium/viso/UIVisoContentBrowser.cpp
@@ -906,7 +906,13 @@ void UIVisoContentBrowser::processRemovedEntries(const QStringList &removedEntri
 {
     foreach (const QString &strPath, removedEntries)
     {
-        QStringList path = UIPathOperations::pathTrail(strPath);
+        QFileInfo fileInfo(strPath);
+        UICustomFileSystemItem *pItem = searchItemByPath(strPath);
+        if (pItem)
+        {
+            pItem->setRemovedFromViso(true);
+            createVisoEntry(strPath,  QString(), true);
+        }
     }
 }
 
@@ -954,7 +960,6 @@ void UIVisoContentBrowser::scanHostDirectory(UICustomFileSystemItem *directoryIt
             /* Do not recurse into sysmlinks since it may end up infinite recursion: */
             if (fRecursive && enmType == KFsObjType_Directory && !newItem->isUpDirectory())
                 scanHostDirectory(newItem, true);
-
         }
         directoryItem->setIsOpened(true);
     }
