3e85586e16ca2d480d560dfdd0765570bb332422
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2023-07-27 16:55:16 +0000

    FE/Qt: bugref:10450: Linux: Ignore QT_SCALE_FACTOR env var set by some DE, like Mate; This var used in Qt5 as override, but in Qt6 as additional multiplier, no idea why; As a result we have x4 scale-factor when x2 is set.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@100718 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Frontends/VirtualBox/src/main.cpp
+++ src/VBox/Frontends/VirtualBox/src/main.cpp
@@ -477,6 +477,16 @@ extern "C" DECLEXPORT(int) TrustedMain(int argc, char **argv, char ** /*envp*/)
         QApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
 #  endif
 # endif
+#else
+# ifdef VBOX_WS_NIX
+        // WORKAROUND:
+        // Some Desktop Environments forcing this variable which was useful in Qt5 case.
+        // But in Qt6 case this variable is additional multiplier to scale-factor
+        // calculated automatic way, which means for x2 the effective factor will be 2x2=4.
+        // No idea how to proceed, probably we will ignore this variable for now, by
+        // setting it to be always equal to 1.
+        qputenv("QT_SCALE_FACTOR", "1");
+# endif
 #endif
 
         /* Create application: */
