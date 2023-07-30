29c340b52a4886c69037373dd7d11971c17921da
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2023-07-30 10:33:22 +0000

    FE/Qt: ​bugref:10444, ​bugref:10465, ​bugref:10466. Redirect all ext.pack un/installation events whether event source is a IProgress or IVirtualBox to corresponding UICommon signals to unify code paths.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@100738 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Frontends/VirtualBox/src/extensionpackmanager/UIExtensionPackManager.cpp
+++ src/VBox/Frontends/VirtualBox/src/extensionpackmanager/UIExtensionPackManager.cpp
@@ -385,12 +385,17 @@ void UIExtensionPackManagerWidget::prepare()
 {
     /* Prepare self: */
     uiCommon().setHelpKeyword(this, "ext-pack-manager");
+    /* Connect un/install event we get from IVirtualBox to UICommon since all
+     * ext pack installation events should go through its signal. Several
+     * other objects listen to those signals for menu update etc: */
+    connect(gVBoxEvents, &UIVirtualBoxEventHandler::sigExtensionPackInstalled,
+            &uiCommon(), &UICommon::sigExtensionPackInstalled);
+    connect(gVBoxEvents, &UIVirtualBoxEventHandler::sigExtensionPackUninstalled,
+            &uiCommon(), &UICommon::sigExtensionPackUninstalled);
+
+
     connect(&uiCommon(), &UICommon::sigExtensionPackInstalled,
             this, &UIExtensionPackManagerWidget::sltHandleExtensionPackInstalled);
-    connect(gVBoxEvents, &UIVirtualBoxEventHandler::sigExtensionPackInstalled,
-            this, &UIExtensionPackManagerWidget::sltHandleExtensionPackInstalled);
-    connect(gVBoxEvents, &UIVirtualBoxEventHandler::sigExtensionPackUninstalled,
-            this, &UIExtensionPackManagerWidget::sltHandleExtensionPackUninstalled);
 
     /* Prepare stuff: */
     prepareActions();
