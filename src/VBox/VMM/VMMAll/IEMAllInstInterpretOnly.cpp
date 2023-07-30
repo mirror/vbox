861a009d9fa49e6980dd42a803a1b3dc10470d71
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2023-07-30 18:22:25 +0000

    VMM/IEM: Preparations for splitting up IEMAllInstInterpretOnly.cpp into several files. bugref:10369
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@100739 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/VMM/VMMAll/IEMAllInstInterpretOnly.cpp
+++ src/VBox/VMM/VMMAll/IEMAllInstInterpretOnly.cpp
@@ -25,67 +25,7 @@
  * SPDX-License-Identifier: GPL-3.0-only
  */
 
-
-/*********************************************************************************************************************************
-*   Header Files                                                                                                                 *
-*********************************************************************************************************************************/
-#ifndef LOG_GROUP /* defined when included by tstIEMCheckMc.cpp */
-# define LOG_GROUP LOG_GROUP_IEM
-#endif
-#define VMCPU_INCL_CPUM_GST_CTX
-#include <VBox/vmm/iem.h>
-#include <VBox/vmm/cpum.h>
-#include <VBox/vmm/apic.h>
-#include <VBox/vmm/pdm.h>
-#include <VBox/vmm/pgm.h>
-#include <VBox/vmm/iom.h>
-#include <VBox/vmm/em.h>
-#include <VBox/vmm/hm.h>
-#include <VBox/vmm/nem.h>
-#include <VBox/vmm/gim.h>
-#ifdef VBOX_WITH_NESTED_HWVIRT_SVM
-# include <VBox/vmm/em.h>
-# include <VBox/vmm/hm_svm.h>
-#endif
-#ifdef VBOX_WITH_NESTED_HWVIRT_VMX
-# include <VBox/vmm/hmvmxinline.h>
-#endif
-#include <VBox/vmm/tm.h>
-#include <VBox/vmm/dbgf.h>
-#include <VBox/vmm/dbgftrace.h>
-#ifndef TST_IEM_CHECK_MC
-# include "IEMInternal.h"
-#endif
-#include <VBox/vmm/vmcc.h>
-#include <VBox/log.h>
-#include <VBox/err.h>
-#include <VBox/param.h>
-#include <VBox/dis.h>
-#include <iprt/asm-math.h>
-#include <iprt/assert.h>
-#include <iprt/string.h>
-#include <iprt/x86.h>
-
-#ifndef TST_IEM_CHECK_MC
-# include "IEMInline.h"
-# include "IEMOpHlp.h"
-# include "IEMMc.h"
-#endif
-
-
-/*********************************************************************************************************************************
-*   Defined Constants And Macros                                                                                                 *
-*********************************************************************************************************************************/
-#define g_apfnOneByteMap    g_apfnIemInterpretOnlyOneByteMap
-
-
-
-/*
- * Include common bits.
- */
-#include "IEMAllInstCommonBodyMacros.h"
-#include "IEMAllInstCommon.cpp.h"
-
+#include "IEMAllIntprTables.h"
 
 /*
  * Include the tables.
---
+++ src/VBox/VMM/VMMAll/IEMAllIntprTables.h
@@ -0,0 +1,88 @@
+/* $Id$ */
+/** @file
+ * IEM - Instruction Decoding and Emulation, Interpreter Tables Common Header.
+ */
+
+/*
+ * Copyright (C) 2011-2023 Oracle and/or its affiliates.
+ *
+ * This file is part of VirtualBox base platform packages, as
+ * available from https://www.virtualbox.org.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation, in version 3 of the
+ * License.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, see <https://www.gnu.org/licenses>.
+ *
+ * SPDX-License-Identifier: GPL-3.0-only
+ */
+
+
+/*********************************************************************************************************************************
+*   Header Files                                                                                                                 *
+*********************************************************************************************************************************/
+#ifndef LOG_GROUP /* defined when included by tstIEMCheckMc.cpp */
+# define LOG_GROUP LOG_GROUP_IEM
+#endif
+#define VMCPU_INCL_CPUM_GST_CTX
+#include <VBox/vmm/iem.h>
+#include <VBox/vmm/cpum.h>
+#include <VBox/vmm/apic.h>
+#include <VBox/vmm/pdm.h>
+#include <VBox/vmm/pgm.h>
+#include <VBox/vmm/iom.h>
+#include <VBox/vmm/em.h>
+#include <VBox/vmm/hm.h>
+#include <VBox/vmm/nem.h>
+#include <VBox/vmm/gim.h>
+#ifdef VBOX_WITH_NESTED_HWVIRT_SVM
+# include <VBox/vmm/em.h>
+# include <VBox/vmm/hm_svm.h>
+#endif
+#ifdef VBOX_WITH_NESTED_HWVIRT_VMX
+# include <VBox/vmm/hmvmxinline.h>
+#endif
+#include <VBox/vmm/tm.h>
+#include <VBox/vmm/dbgf.h>
+#include <VBox/vmm/dbgftrace.h>
+#ifndef TST_IEM_CHECK_MC
+# include "IEMInternal.h"
+#endif
+#include <VBox/vmm/vmcc.h>
+#include <VBox/log.h>
+#include <VBox/err.h>
+#include <VBox/param.h>
+#include <VBox/dis.h>
+#include <iprt/asm-math.h>
+#include <iprt/assert.h>
+#include <iprt/string.h>
+#include <iprt/x86.h>
+
+#ifndef TST_IEM_CHECK_MC
+# include "IEMInline.h"
+# include "IEMOpHlp.h"
+# include "IEMMc.h"
+#endif
+
+
+/*********************************************************************************************************************************
+*   Defined Constants And Macros                                                                                                 *
+*********************************************************************************************************************************/
+#define g_apfnOneByteMap    g_apfnIemInterpretOnlyOneByteMap
+
+
+
+/*
+ * Include common bits.
+ */
+#include "IEMAllInstCommonBodyMacros.h"
+#include "IEMAllInstCommon.cpp.h"
+
