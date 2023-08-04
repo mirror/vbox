e34935f406858c2afd087300f1a9a52f999b34d1
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2023-08-04 11:15:22 +0000

    ValKit: Use SATA Controller for tst-win11-64-efi VM
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@100796 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/ValidationKit/testdriver/vboxtestvms.py
+++ src/VBox/ValidationKit/testdriver/vboxtestvms.py
@@ -2083,7 +2083,8 @@ class TestVmManager(object):
 
         # W11
         TestVm('tst-win11-64-efi',           kfGrpStdSmoke,       sHd = '7.0/win11/win11-64-edited.vdi',
-               sKind = 'Windows11_64', acCpusSup = range(1, 33), fIoApic = True, sFirmwareType = 'efi'),
+               sKind = 'Windows11_64', acCpusSup = range(1, 33), fIoApic = True, sFirmwareType = 'efi', 
+               sHddControllerType = 'SATA Controller'),
 
         # Nested hardware-virtualization
         TestVm('tst-nsthwvirt-ubuntu-64',   kfGrpStdSmoke,       sHd = '5.3/nat/nsthwvirt-ubuntu64/t-nsthwvirt-ubuntu64.vdi',
