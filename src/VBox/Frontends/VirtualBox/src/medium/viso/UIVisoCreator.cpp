fd6095a86b5a83078be37faeb8d0933980d217f3
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2023-08-07 10:47:49 +0000

    FE/Qt: bugref:10496, bugref:9080. Update settings widget when save as action is used.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@100816 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Frontends/VirtualBox/src/medium/viso/UIVisoCreator.cpp
+++ src/VBox/Frontends/VirtualBox/src/medium/viso/UIVisoCreator.cpp
@@ -75,6 +75,7 @@ public:
 
     UIVisoSettingWidget(QWidget *pParent);
     virtual void retranslateUi();
+    void setVisoName(const QString &strName);
     void setSettings(const UIVisoCreatorWidget::Settings &settings);
     UIVisoCreatorWidget::Settings settings() const;
 
@@ -201,6 +202,12 @@ void UIVisoSettingWidget::prepareConnections()
         connect(m_pShowHiddenObjectsCheckBox, &QCheckBox::toggled, this, &UIVisoSettingWidget::sigSettingsChanged);
 }
 
+void UIVisoSettingWidget::setVisoName(const QString &strName)
+{
+    if (m_pVisoNameLineEdit)
+        m_pVisoNameLineEdit->setText(strName);
+}
+
 void UIVisoSettingWidget::setSettings(const UIVisoCreatorWidget::Settings &settings)
 {
     if (m_pVisoNameLineEdit)
@@ -284,6 +291,12 @@ void UIVisoCreatorWidget::setVisoName(const QString& strName)
         return;
     m_settings.m_strVisoName = strName;
     emit sigVisoNameChanged(m_settings.m_strVisoName);
+    if (m_pSettingsWidget)
+    {
+        m_pSettingsWidget->blockSignals(true);
+        m_pSettingsWidget->setVisoName(strName);
+        m_pSettingsWidget->blockSignals(false);
+    }
 }
 
 void UIVisoCreatorWidget::setVisoFilePath(const QString& strPath)
