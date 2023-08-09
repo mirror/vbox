031be08f68327bc3ce4ef1a84e5557237eae28b2
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2023-08-09 15:08:16 +0000

    include/iprt/x86.h: A small Solaris-specific build warning remediation. Undefine MSR_AMD_VIRT_SPEC_CTL to avoid warnings about '"MSR_AMD_VIRT_SPEC_CTL" redefined' when compiling SUPLib.cpp and SUPLibLdr.cpp.  Both of these files include <iprt/alloca.h> which includes <stdlib.h> which pulls in numerous other header files before penultimately including <sys/privregs.h> which ultimately pulls in <sys/controlregs.h> which has its own definition of MSR_AMD_VIRT_SPEC_CTL as of S11.4 SRU60/st_147.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@100837 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- include/iprt/x86.h
+++ include/iprt/x86.h
@@ -49,11 +49,12 @@
 #endif
 
 /** Workaround for Solaris sys/regset.h defining CS, DS and sys/controlregs.h
- * defining MSR_IA32_FLUSH_CMD */
+ * defining MSR_IA32_FLUSH_CMD and MSR_AMD_VIRT_SPEC_CTL */
 #ifdef RT_OS_SOLARIS
 # undef CS
 # undef DS
 # undef MSR_IA32_FLUSH_CMD
+# undef MSR_AMD_VIRT_SPEC_CTL
 #endif
 
 /** @defgroup grp_rt_x86   x86 Types and Definitions
