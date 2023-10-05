ffddb1f877809c514a50839013c770179f6c82f3
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2023-10-05 22:49:50 +0000

    iprt/string.h: RT_BCOPY_UNFORTIFIED nits. bugref:10209 ticketref:21410
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@101366 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- include/iprt/string.h
+++ include/iprt/string.h
@@ -133,6 +133,33 @@ RT_C_DECLS_END
 #endif
 
 
+/** @defgroup grp_rt_str    RTStr - String Manipulation
+ * Mostly UTF-8 related helpers where the standard string functions won't do.
+ * @ingroup grp_rt
+ * @{
+ */
+
+RT_C_DECLS_BEGIN
+
+
+/**
+ * The maximum string length.
+ */
+#define RTSTR_MAX       (~(size_t)0)
+
+
+/** @def RTSTR_TAG
+ * The default allocation tag used by the RTStr allocation APIs.
+ *
+ * When not defined before the inclusion of iprt/string.h, this will default to
+ * the pointer to the current file name.  The string API will make of use of
+ * this as pointer to a volatile but read-only string.
+ */
+#if !defined(RTSTR_TAG) || defined(DOXYGEN_RUNNING)
+# define RTSTR_TAG      (__FILE__)
+#endif
+
+
 /**
  * Byte zero the specified object.
  *
@@ -192,53 +219,31 @@ RT_C_DECLS_END
         memcpy((a_pDst), (void const *)a_pvVolatileSrc_BCopy_Volatile, (a_cbToCopy)); \
     } while (0)
 
-/**
- * Wrapper for the Linux kernel memcpy.
+/** @def RT_BCOPY_UNFORTIFIED
+ * For copying a number of bytes from/to variable length structures.
  *
- * Use when copying to variable length structures, it prevents a fortified
- * memcpy (linux 5.18+) from complaining about "field-spanning writes".
+ * This is for working around false positives ("field-spanning writes") in the
+ * linux kernel's fortified memcpy (v5.18+) when copying from/to
+ * RT_FLEXIBLE_ARRAY fields and similar tricks going beyond the strict
+ * definition of a target or source structure.
  *
- * @see @ticketref{21410}, @bugref{10209}
+ * @param   a_pDst          Pointer to the destination buffer.
+ * @param   a_pSrc          Pointer to the source buffer.
+ * @param   a_cbToCopy      Number of bytes to copy.
+ * @see @bugref{10209}, @ticketref{21410}
  */
 #if defined(RT_OS_LINUX) && defined(__KERNEL__)
 # if (RTLNX_VER_MIN(5,18,0) || RTLNX_RHEL_RANGE(9,3, 9,99)) \
   && !defined(__NO_FORTIFY) \
   && defined(__OPTIMIZE__) \
   && defined(CONFIG_FORTIFY_SOURCE)
-#  define RT_BCOPY_UNFORTIFIED      __underlying_memcpy
-# else /* __NO_FORTIFY && !__OPTIMIZE__ && !CONFIG_FORTIFY_SOURCE */
-#  define RT_BCOPY_UNFORTIFIED      memcpy
-# endif /* !__NO_FORTIFY && __OPTIMIZE__ && CONFIG_FORTIFY_SOURCE */
-#else /* !RT_OS_LINUX && !__KERNEL__ */
-# define RT_BCOPY_UNFORTIFIED       memcpy
-#endif
-
-
-/** @defgroup grp_rt_str    RTStr - String Manipulation
- * Mostly UTF-8 related helpers where the standard string functions won't do.
- * @ingroup grp_rt
- * @{
- */
-
-RT_C_DECLS_BEGIN
-
-
-/**
- * The maximum string length.
- */
-#define RTSTR_MAX       (~(size_t)0)
-
-
-/** @def RTSTR_TAG
- * The default allocation tag used by the RTStr allocation APIs.
- *
- * When not defined before the inclusion of iprt/string.h, this will default to
- * the pointer to the current file name.  The string API will make of use of
- * this as pointer to a volatile but read-only string.
- */
-#if !defined(RTSTR_TAG) || defined(DOXYGEN_RUNNING)
-# define RTSTR_TAG      (__FILE__)
-#endif
+#  define RT_BCOPY_UNFORTIFIED(a_pDst, a_pSrc, a_cbToCopy)  __underlying_memcpy((a_pDst), (a_pSrc), (a_cbToCopy))
+# else
+#  define RT_BCOPY_UNFORTIFIED(a_pDst, a_pSrc, a_cbToCopy)  memcpy((a_pDst), (a_pSrc), (a_cbToCopy))
+# endif
+#else  /* !RT_OS_LINUX && !__KERNEL__ */
+# define RT_BCOPY_UNFORTIFIED(a_pDst, a_pSrc, a_cbToCopy)   memcpy((a_pDst), (a_pSrc), (a_cbToCopy))
+#endif /* !RT_OS_LINUX && !__KERNEL__ */
 
 
 #ifdef IN_RING3
