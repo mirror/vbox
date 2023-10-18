a133a2e0b5c2714e0a9014ae4a943503c5c27dd2
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2023-10-18 11:11:54 +0000

    FE/Qt: bugref:10513: Forgot something in r159526.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@101493 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Frontends/VirtualBox/src/settings/UIAdvancedSettingsDialog.cpp
+++ src/VBox/Frontends/VirtualBox/src/settings/UIAdvancedSettingsDialog.cpp
@@ -1132,6 +1132,7 @@ void UIAdvancedSettingsDialog::prepareSelector()
 
     /* Make sure there is a serious spacing between selector and pages: */
     m_pLayoutMain->setColumnMinimumWidth(1, 20);
+    m_pLayoutMain->setRowStretch(1, 1);
 
     /* Prepare mode checkbox: */
     m_pCheckBoxMode = new UIModeCheckBox(centralWidget());
