1ae6e2398bdfa254b61075e92f363ebcda066b0b
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2023-10-20 15:09:20 +0000

    HostDrivers/Support: Reverted previous change (r159602) as it is wrong. bugref:8864
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@101526 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/HostDrivers/Support/linux/SUPDrv-linux.c
+++ src/VBox/HostDrivers/Support/linux/SUPDrv-linux.c
@@ -1685,7 +1685,8 @@ SUPR0DECL(uint32_t) SUPR0GetKernelFeatures(void)
     uint32_t fFlags = 0;
 #ifdef CONFIG_PAX_KERNEXEC
     fFlags |= SUPKERNELFEATURES_GDT_READ_ONLY;
-#elif RTLNX_VER_MIN(4,12,0)
+#endif
+#if RTLNX_VER_MIN(4,12,0)
     fFlags |= SUPKERNELFEATURES_GDT_NEED_WRITABLE;
 #endif
 #if defined(VBOX_STRICT) || defined(VBOX_WITH_EFLAGS_AC_SET_IN_VBOXDRV)
