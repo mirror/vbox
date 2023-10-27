4c0188bba512532a3eb7b7c07bcf816b4b13e6f0
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2023-10-27 17:37:59 +0000

    libssh: fixing build. bugref:10539
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@101638 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Devices/Network/DrvCloudTunnel.cpp
+++ src/VBox/Devices/Network/DrvCloudTunnel.cpp
@@ -754,7 +754,9 @@ static int drvCloudTunnelReceiveCallback(ssh_session session, ssh_channel channe
     return len;
 }
 
-static int channelWriteWontblockCallback(ssh_session, ssh_channel, unsigned int, void *)
+// for libssh 0.9.6
+static int channelWriteWontblockCallback(ssh_session, ssh_channel, size_t, void *)
+//static int channelWriteWontblockCallback(ssh_session, ssh_channel, unsigned int, void *)
 {
     return 0;
 }
