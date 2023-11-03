70a933808582e36ae674450b1818bc1d41d2da46
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2023-11-03 15:55:10 +0000

    libs/xpcom: Remove prsystem.{c,h} as they aren't used anywhere, bugref:10545 [build fix]
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@101751 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/libs/xpcom18a4/nsprpub/pr/include/prsystem.h
+++ src/libs/xpcom18a4/nsprpub/pr/include/prsystem.h
@@ -35,40 +35,31 @@
  *
  * ***** END LICENSE BLOCK ***** */
 
-#ifndef nspr_h___
-#define nspr_h___
+#ifndef prsystem_h___
+#define prsystem_h___
 
-#include "pratom.h"
-#include "prbit.h"
-#include "prclist.h"
-#include "prcmon.h"
-#include "prcvar.h"
-#include "prdtoa.h"
-#include "prenv.h"
-#include "prerror.h"
-#include "prinet.h"
-#include "prinit.h"
-#include "prinrval.h"
-#include "prio.h"
-#include "pripcsem.h"
-#include "prlink.h"
-#include "prlock.h"
-#include "prlog.h"
-#include "prlong.h"
-#include "prmem.h"
-#include "prmon.h"
-#include "prmwait.h"
-#include "prnetdb.h"
-#include "prprf.h"
-#include "prproces.h"
-#include "prrwlock.h"
-#include "prshm.h"
-#include "prshma.h"
-#include "prsystem.h"
-#include "prthread.h"
-#include "prtime.h"
-#include "prtpool.h"
-#include "prtrace.h"
+/*
+** API to NSPR functions returning system info.
+*/
 #include "prtypes.h"
 
-#endif /* nspr_h___ */
+#ifdef VBOX_WITH_XPCOM_NAMESPACE_CLEANUP
+#define PR_GetPageSize VBoxNsprPR_GetPageSize
+#define PR_GetPageShift VBoxNsprPR_GetPageShift
+#endif /* VBOX_WITH_XPCOM_NAMESPACE_CLEANUP */
+
+PR_BEGIN_EXTERN_C
+
+/*
+** Return the number of bytes in a page
+*/
+NSPR_API(PRInt32) PR_GetPageSize(void);
+
+/*
+** Return log2 of the size of a page
+*/
+NSPR_API(PRInt32) PR_GetPageShift(void);
+
+PR_END_EXTERN_C
+
+#endif /* prsystem_h___ */
