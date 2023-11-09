2ce6917ebdb62199518f6620fd164f80425dad30
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2023-11-09 17:01:02 +0000

    FE/Qt: bugref:10513: VM Settings / System page / Motherboard tab: Adjust default size-hint for Boot table; Initially it's empty and fix allows to get rid of unwanted flicker.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@102042 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Frontends/VirtualBox/src/settings/editors/UIBootOrderEditor.cpp
+++ src/VBox/Frontends/VirtualBox/src/settings/editors/UIBootOrderEditor.cpp
@@ -224,11 +224,29 @@ QSize UIBootListWidget::sizeHint() const
 
 QSize UIBootListWidget::minimumSizeHint() const
 {
+    /* Try to get actual item count: */
+    int iItemCount = topLevelItemCount();
+    /* [By default] calculate for 4 items: */
+    if (iItemCount < 1)
+        iItemCount = 4;
+
+    /* Try to get actual size-hints for column/row: */
+    int iColumnHint = sizeHintForColumn(0);
+    int iRowHint = sizeHintForRow(0);
+    /* [By default] calculate for 15 chars: */
+    if (iColumnHint < 1 || iRowHint < 1)
+    {
+        QFontMetrics fm(font());
+        iColumnHint = 15 * fm.averageCharWidth();
+        iRowHint = fm.height() + 2 * 3; /// @todo <= tree-widget item margin
+    }
+
+    /* Take into account table frame as well: */
     const int iH = 2 * frameWidth();
     const int iW = iH;
-    return QSize(sizeHintForColumn(0) + iW,
-                 sizeHintForRow(0) * topLevelItemCount() + iH);
 
+    /* Calculate tree-widget hint finally: */
+    return QSize(iColumnHint + iW, iRowHint * iItemCount + iH);
 }
 
 void UIBootListWidget::retranslateUi()
