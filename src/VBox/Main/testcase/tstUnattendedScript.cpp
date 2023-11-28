2031d7be7d17c1c99d8788700d902b0e60091a2b
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2023-11-28 09:46:37 +0000

    Main/tstUnattendedScript: Override RTCrShaCryptGenerateSalt() from IPRT to generate predictable salts to compare the script outputs [build fix]. bugref:10551
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@102354 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Main/testcase/tstUnattendedScript.cpp
+++ src/VBox/Main/testcase/tstUnattendedScript.cpp
@@ -665,7 +665,7 @@ bool Unattended::i_getAvoidUpdatesOverNetwork() const
 }
 
 /* Override RTCrShaCryptGenerateSalt() from IPRT to generate predictable salts to compare the script outputs. */
-RTR3DECL(int) RTCrShaCryptGenerateSalt(char szSalt[RT_SHACRYPT_MAX_SALT_LEN + 1], size_t cchSalt)
+int RTCrShaCryptGenerateSalt(char szSalt[RT_SHACRYPT_MAX_SALT_LEN + 1], size_t cchSalt)
 {
     RT_NOREF(cchSalt);
     RTStrPrintf(szSalt, RT_SHACRYPT_MAX_SALT_LEN, "testcase123");
