45d9ef313a3da3eb386d1b9fd8c9867b6b059749
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2023-12-08 11:47:59 +0000

    FE/Qt/UIFileManagerTable: More flower box fixes.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@102537 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Frontends/VirtualBox/src/guestctrl/UIFileManagerTable.cpp
+++ src/VBox/Frontends/VirtualBox/src/guestctrl/UIFileManagerTable.cpp
@@ -112,7 +112,7 @@ protected:
 
 
 /*********************************************************************************************************************************
-*   UStringInputDialog definition.                                                                                          *
+*   UStringInputDialog definition.                                                                                               *
 *********************************************************************************************************************************/
 
 /** A QIDialog child including a line edit whose text exposed when the dialog is accepted */
@@ -327,6 +327,7 @@ UIDirectoryStatistics::UIDirectoryStatistics()
 {
 }
 
+
 /*********************************************************************************************************************************
 *   UIFileDeleteConfirmationDialog implementation.                                                                               *
 *********************************************************************************************************************************/
@@ -378,7 +379,7 @@ bool UIFileDeleteConfirmationDialog::askDeleteConfirmationNextTime() const
 
 
 /*********************************************************************************************************************************
-*   UIFileManagerTable implementation.                                                                                      *
+*   UIFileManagerTable implementation.                                                                                           *
 *********************************************************************************************************************************/
 const unsigned UIFileManagerTable::m_iKiloByte = 1024; /**< Our kilo bytes are a power of two! (bird) */
 
