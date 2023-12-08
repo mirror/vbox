f90be10f4a00b04ac8636e547049e0d9c9218933
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2023-12-08 14:45:15 +0000

    Devices/DevQemuFwCfg: Fixed ASAN warning (as error).
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@102544 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Devices/PC/DevQemuFwCfg.cpp
+++ src/VBox/Devices/PC/DevQemuFwCfg.cpp
@@ -721,7 +721,7 @@ static DECLCALLBACK(int) qemuFwCfgR3ReadFileDir(PDEVQEMUFWCFG pThis, PCQEMUFWCFG
         pThis->u.CfgFile.cbFile   = RT_H2BE_U32(pEntry->cbFile);
         pThis->u.CfgFile.uCfgItem = RT_H2BE_U16(pEntry->Cfg.uCfgItem);
         pThis->u.CfgFile.u16Rsvd  = 0;
-        strncpy(&pThis->u.CfgFile.szFilename[0], pEntry->Cfg.pszItem, sizeof(pThis->u.CfgFile.szFilename));
+        strncpy(pThis->u.CfgFile.szFilename, pEntry->Cfg.pszItem, sizeof(pThis->u.CfgFile.szFilename));
         pThis->u.CfgFile.szFilename[QEMU_FW_CFG_ITEM_FILE_NAME_MAX] = '\0';
 
         memcpy(pvBuf, &pThis->u.ab[off], cbToRead);
