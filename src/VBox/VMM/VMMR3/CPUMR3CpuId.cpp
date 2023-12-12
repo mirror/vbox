11bbf9074ea3196c368881e6c5c087e445a2c802
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2023-12-12 14:59:05 +0000

    VMM/CPUM: Disabled MTRR read/write stuff as grub seems to guru with a standard debian 9 VM on my host. bugref:10498
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@102588 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/VMM/VMMR3/CPUMR3CpuId.cpp
+++ src/VBox/VMM/VMMR3/CPUMR3CpuId.cpp
@@ -3286,7 +3286,8 @@ int cpumR3InitCpuIdAndMsrs(PVM pVM, PCCPUMMSRS pHostMsrs)
         {
             /* Check if MTRR read+write support is enabled. */
             bool fEnableMtrrWrite;
-            rc = CFGMR3QueryBoolDef(pCpumCfg, "MTRRWrite", &fEnableMtrrWrite, true);
+            rc = CFGMR3QueryBoolDef(pCpumCfg, "MTRRWrite", &fEnableMtrrWrite,
+                                    false /** @todo true - 2023-12-12 bird: does not work yet, so disabled it */);
             AssertRCReturn(rc, rc);
             if (fEnableMtrrWrite)
             {
