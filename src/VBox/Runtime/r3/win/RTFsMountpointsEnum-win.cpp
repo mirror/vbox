245851cd4b70111f7f23ad85d5261119545ed449
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2023-12-20 17:19:03 +0000

    IPRT/RTFsMountpointsEnum: Dynamically resolve some Windows volume APIs which are not in NT 3.51 [build fix]. ​bugref:10415
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@102659 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Runtime/r3/win/RTFsMountpointsEnum-win.cpp
+++ src/VBox/Runtime/r3/win/RTFsMountpointsEnum-win.cpp
@@ -39,6 +39,7 @@
 *   Header Files                                                                                                                 *
 *********************************************************************************************************************************/
 #include "internal/iprt.h"
+#include "internal-r3-win.h"
 #include <iprt/file.h>
 
 #include <iprt/assert.h>
@@ -56,12 +57,12 @@
 *********************************************************************************************************************************/
 /* kernel32.dll: */
 typedef HANDLE (WINAPI *PFNFINDFIRSTVOLUMEW)(LPWSTR, DWORD);
-typedef HANDLE (WINAPI *PFNFINDNEXTVOLUMEW)(HANDLE, LPWSTR, DWORD);
-typedef HANDLE (WINAPI *PFNFINDVOLUMECLOSE)(HANDLE);
-typedef HANDLE (WINAPI *PFNGETVOLUMEPATHNAMESFORVOLUMENAMEW)(LPCWSTR, LPWCH, DWORD, PDWORD);
+typedef BOOL (WINAPI *PFNFINDNEXTVOLUMEW)(HANDLE, LPWSTR, DWORD);
+typedef BOOL (WINAPI *PFNFINDVOLUMECLOSE)(HANDLE);
+typedef BOOL (WINAPI *PFNGETVOLUMEPATHNAMESFORVOLUMENAMEW)(LPCWSTR, LPWCH, DWORD, PDWORD);
 typedef HANDLE (WINAPI *PFNFINDFIRSTVOLUMEMOUNTPOINTW)(LPCWSTR, LPWSTR, DWORD);
-typedef HANDLE (WINAPI *PFNFINDNEXTVOLUMEMOUNTPOINTW)(HANDLE, LPWSTR, DWORD);
-typedef HANDLE (WINAPI *PFNFINDVOLUMEMOUNTPOINTCLOSE)(HANDLE);
+typedef BOOL (WINAPI *PFNFINDNEXTVOLUMEMOUNTPOINTW)(HANDLE, LPWSTR, DWORD);
+typedef BOOL (WINAPI *PFNFINDVOLUMEMOUNTPOINTCLOSE)(HANDLE);
 
 
 /*********************************************************************************************************************************
