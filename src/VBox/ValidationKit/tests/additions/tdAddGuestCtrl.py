f0111a43b14afb91fb72f06c735401b00a980f95
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2024-01-17 10:44:21 +0000

    Validation Kit/tdAddGuestCtrl: Use testDriver::getGuestWinDir() for building up the path to VBoxControl. Should fix running on NT4 guests.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@102915 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/ValidationKit/tests/additions/tdAddGuestCtrl.py
+++ src/VBox/ValidationKit/tests/additions/tdAddGuestCtrl.py
@@ -3139,7 +3139,7 @@ class SubTstDrvAddGuestCtrl(base.SubTestDriverBase):
         """
 
         # Paths:
-        sVBoxControl    = None; ## @todo Get path of installed Guest Additions. Later.
+        sVBoxControl    = None; # Only available on supported Windows guests.
         sShell          = self.oTstDrv.getGuestSystemShell(oTestVm);
         sShellOpt       = '/C' if oTestVm.isWindows() or oTestVm.isOS2() else '-c';
         sSystemDir      = self.oTstDrv.getGuestSystemDir(oTestVm);
@@ -3148,7 +3148,7 @@ class SubTstDrvAddGuestCtrl(base.SubTestDriverBase):
         if oTestVm.isWindows() or oTestVm.isOS2():
             sImageOut = self.oTstDrv.getGuestSystemShell(oTestVm);
             if oTestVm.isWindows():
-                sVBoxControl = "C:\\Program Files\\Oracle\\VirtualBox Guest Additions\\VBoxControl.exe";
+                sVBoxControl = oTestVm.pathJoin(self.oTstDrv.getGuestWinDir(oTestVm), 'VBoxControl.exe');
         else:
             # Really old guests (like OL 6) have their coreutils in /bin instead of /usr/bin.
             if self.oTstDrv.txsIsFile(oSession, oTxsSession, "/bin/ls", fIgnoreErrors = True):
