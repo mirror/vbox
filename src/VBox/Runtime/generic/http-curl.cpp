08ee82d3b1fe76d90aec284131a2741d3977219c
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2024-01-17 10:51:49 +0000

    Runtime/http-curl.cpp: Make RTHttpSetProxy() accept a NULL pcszProxy parameter indicating complete disabling of any proxy settings
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@102916 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Runtime/generic/http-curl.cpp
+++ src/VBox/Runtime/generic/http-curl.cpp
@@ -2079,7 +2079,6 @@ RTR3DECL(int) RTHttpSetProxy(RTHTTP hHttp, const char *pcszProxy, uint32_t uPort
 {
     PRTHTTPINTERNAL pThis = hHttp;
     RTHTTP_VALID_RETURN(pThis);
-    AssertPtrReturn(pcszProxy, VERR_INVALID_PARAMETER);
     AssertReturn(!pThis->fBusy, VERR_WRONG_ORDER);
 
     /*
@@ -2089,6 +2088,9 @@ RTR3DECL(int) RTHttpSetProxy(RTHTTP hHttp, const char *pcszProxy, uint32_t uPort
      * leave that to cURL.  (A bit afraid of breaking user settings.)
      */
     pThis->fUseSystemProxySettings = false;
+    if (!pcszProxy)
+        return rtHttpUpdateAutomaticProxyDisable(pThis);
+
     return rtHttpUpdateProxyConfig(pThis, CURLPROXY_HTTP, pcszProxy, uPort ? uPort : 1080, pcszProxyUser, pcszProxyPwd);
 }
 
