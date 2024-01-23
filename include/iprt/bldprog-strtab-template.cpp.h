c061108118c888640dd12282ad68b5f8dd12b303
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2024-01-23 22:13:47 +0000

    iprt/bldprog-strtab-template.cpp.h: Fix comments to clang v15 doesn't barf on invalid UTF-8 source comments. [build fix]
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@103004 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- include/iprt/bldprog-strtab-template.cpp.h
+++ include/iprt/bldprog-strtab-template.cpp.h
@@ -1205,13 +1205,13 @@ static void BldProgStrTab_WriteStringTable(PBLDPROGSTRTAB pThis, FILE *pOut,
     {
         if (BldProgBitIsSet(pThis->bmUsedChars, i)
 #  ifdef BLDPROG_STRTAB_PURE_ASCII
-            ? pThis->aCompDict[i].cchString != 1
+            ? pThis->aCompDict[i].cchString != 1U
 #  else
-            ? pThis->aCompDict[i].cchString != (i != 0xff ? 1 : 0)
+            ? pThis->aCompDict[i].cchString != (i != 0xffU ? 1U : 0U)
 #  endif
-            : pThis->aCompDict[i].cchString < 1)
+            : pThis->aCompDict[i].cchString < 1U)
             abort();
-        if (pThis->aCompDict[i].cchString > 1)
+        if (pThis->aCompDict[i].cchString > 1U)
             BldProgStrTab_CheckStrTabString(pThis, &pThis->aCompDict[i]);
     }
 # endif
