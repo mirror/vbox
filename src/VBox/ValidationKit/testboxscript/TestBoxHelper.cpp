7680473850f4a8f772ac3c76d60bef3a0be0e5d6
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2024-02-01 10:29:15 +0000

    ValidationKit/{common,testboxscript}: Add a has native API support flag when signing on indicating whether the testbox supports testing through NEM (Hyper-V on Windows, KVM on Linux and Hypervisor.framework on macOS). This is the first part not touching the actual testmanager, bugref:10592 [build and scm fix]
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@103158 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/ValidationKit/testboxscript/TestBoxHelper.cpp
+++ src/VBox/ValidationKit/testboxscript/TestBoxHelper.cpp
@@ -66,7 +66,7 @@
 # include <iprt/nt/nt-and-windows.h>
 # include <iprt/ldr.h>
 
-extern "C" HRESULT WINAPI 
+extern "C" HRESULT WINAPI
 WHvGetCapability(UINT32 CapabilityCode, VOID *CapabilityBuffer, UINT32 CapabilityBufferSizeInBytes, UINT32 *WrittenSizeInBytes);
 #elif defined(RT_OS_LINUX)
 # include <sys/stat.h>
@@ -527,7 +527,7 @@ static bool isNativeApiSupported(void)
 
 #elif defined(RT_OS_LINUX)
     /* Check by opening /dev/kvm. */
-    int fdKvm = open("/dev/kvm", O_RDWR | O_CLOEXEC);
+    int fdKvm = open("/dev/kvm", O_RDWR);
     if (fdKvm >= 0)
     {
         close(fdKvm);
