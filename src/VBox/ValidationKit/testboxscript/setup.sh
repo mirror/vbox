81c869cd076bb628a5a086c6549df89299b4dec6
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2024-02-05 14:55:29 +0000

    adding python versions
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@103205 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/ValidationKit/testboxscript/setup.sh
+++ src/VBox/ValidationKit/testboxscript/setup.sh
@@ -672,7 +672,7 @@ import sys;\
 x = sys.version_info[0] == 3 or (sys.version_info[0] == 2 and (sys.version_info[1] >= 6 or (sys.version_info[1] == 5 and sys.version_info[2] >= 1)));\
 sys.exit(not x);\
 ";
-    for python in python2.7 python2.6 python2.5 python;
+    for python in python[2:3].[0:]  #python3.11 python3.10 python3.9 python2.6 python2.5 python;
     do
         python=`which ${python} 2> /dev/null`
         if [ -n "${python}" -a -x "${python}" ]; then
