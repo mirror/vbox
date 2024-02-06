4cd338f1354bafdd83c10359eb9caf826431b391
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2024-02-06 16:03:24 +0000

    IPRT/process-win: Fixed regression from r152441, leading to start Windows processes from services in session 0 instead of session >=1.
    This was due to reworking the code to solely use the "W-variants" (Unicode) of the Windows Process32 APIs. rtProcWinFindTokenByProcess() now uses  PROCESSENTRY32W instead of PROCESSENTRY32, but it was forgotten to also change the dwSize member to sizeof(PROCESSENTRY32W). This in turn led to a debug assertion, showing a ERROR_BAD_LENGTH. The function itself, however, just returned false in release builds, which in turn messes up the process token handling.
    
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@103230 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Runtime/r3/win/process-win.cpp
+++ src/VBox/Runtime/r3/win/process-win.cpp
@@ -730,7 +730,7 @@ static bool rtProcWinFindTokenByProcess(const char * const *papszNames, PSID pSi
             for (size_t i = 0; papszNames[i] && !fFound; i++)
             {
                 PROCESSENTRY32W ProcEntry;
-                ProcEntry.dwSize = sizeof(PROCESSENTRY32);
+                ProcEntry.dwSize = sizeof(PROCESSENTRY32W);
                 ProcEntry.szExeFile[0] = '\0';
                 if (g_pfnProcess32FirstW(hSnap, &ProcEntry))
                 {
