62def89ee6c6aed6d352dcb258f77daf4b8de3b2
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2024-02-07 11:52:35 +0000

    Guest Control/GuestSessionImplTasks: Fixed implicit fallthrough for copying files to guest and the destination file already exists (found by Parfait). bugref:3409
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@103244 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Main/src-client/GuestSessionImplTasks.cpp
+++ src/VBox/Main/src-client/GuestSessionImplTasks.cpp
@@ -2312,6 +2312,7 @@ int GuestSessionTaskCopyTo::Run(void)
                                             Utf8StrFmt(tr("Destination \"%s\" on guest already exists and is a file"),
                                                        strDstRootAbs.c_str()));
                         vrc = VERR_IS_A_FILE;
+                        break;
                     }
 
                     case FsObjType_Symlink:
