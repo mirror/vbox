c5ed41156f3f4e291d357260ce5d2f447b62527a
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2024-02-19 10:05:01 +0000

    Debugger: Fix some harmless warnings, bugref:3409
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@103422 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Debugger/DBGCEmulateCodeView.cpp
+++ src/VBox/Debugger/DBGCEmulateCodeView.cpp
@@ -3845,6 +3845,7 @@ static RTGCPHYS dbgcGetGuestPageMode(PDBGC pDbgc, bool *pfPAE, bool *pfLME, bool
 #if defined(VBOX_VMM_TARGET_ARMV8)
     AssertReleaseFailed();
     RT_NOREF(pDbgc, pfPAE, pfLME, pfPSE, pfPGE, pfNXE);
+    *pfPAE = *pfLME = *pfPSE = *pfPGE = *pfNXE = false;
     return ~(RTGCPHYS)0;
 #else
     PVMCPU      pVCpu = VMMR3GetCpuByIdU(pDbgc->pUVM, pDbgc->idCpu);
@@ -3882,6 +3883,7 @@ static RTHCPHYS dbgcGetShadowPageMode(PDBGC pDbgc, bool *pfPAE, bool *pfLME, boo
 #if defined(VBOX_VMM_TARGET_ARMV8)
     RT_NOREF(pDbgc, pfPAE, pfLME, pfPSE, pfPGE, pfNXE);
     AssertReleaseFailed();
+    *pfPAE = *pfLME = *pfPSE = *pfPGE = *pfNXE = false;
     return ~(RTHCPHYS)0;
 #else
     PVMCPU pVCpu = VMMR3GetCpuByIdU(pDbgc->pUVM, pDbgc->idCpu);
