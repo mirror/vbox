aa42c83955ff260b3fd9a6f6933e8f462f8134cc
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2024-03-13 12:56:50 +0000

    VMM/IEM: Fixed missing variable index unpacking in iemNativeRegMoveAndFreeAndFlushAtCall. bugref:10370
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@103827 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/VMM/VMMAll/IEMAllN8veRecompiler.cpp
+++ src/VBox/VMM/VMMAll/IEMAllN8veRecompiler.cpp
@@ -4610,7 +4610,7 @@ iemNativeRegMoveAndFreeAndFlushAtCall(PIEMRECOMPILERSTATE pReNative, uint32_t of
                     IEMNATIVE_ASSERT_VAR_IDX(pReNative, idxVar);
                     PIEMNATIVEVAR const pVar   = &pReNative->Core.aVars[IEMNATIVE_VAR_IDX_UNPACK(idxVar)];
                     Assert(pVar->idxReg == idxReg);
-                    if (!(RT_BIT_32(idxVar) & fKeepVars))
+                    if (!(RT_BIT_32(IEMNATIVE_VAR_IDX_UNPACK(idxVar)) & fKeepVars))
                     {
                         Log12(("iemNativeRegMoveAndFreeAndFlushAtCall: idxVar=%#x enmKind=%d idxReg=%d\n",
                                idxVar, pVar->enmKind, pVar->idxReg));
