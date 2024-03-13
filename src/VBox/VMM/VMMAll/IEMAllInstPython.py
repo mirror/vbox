c401b99b023a21f1e3d8fb4a884a63be8b018c8d
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2024-03-13 08:50:08 +0000

    VMM/IEM: Implement native emitter for IEM_MC_BROADCAST_YREG_U32_ZX_VLMAX(), bugref:10614
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@103817 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/VMM/VMMAll/IEMAllInstPython.py
+++ src/VBox/VMM/VMMAll/IEMAllInstPython.py
@@ -2964,7 +2964,7 @@ g_dMcStmtParsers = {
     'IEM_MC_BROADCAST_XREG_U8_ZX_VLMAX':                         (McBlock.parseMcGeneric,           True,  True,  False, ),
     'IEM_MC_BROADCAST_YREG_U128_ZX_VLMAX':                       (McBlock.parseMcGeneric,           True,  True,  False, ),
     'IEM_MC_BROADCAST_YREG_U16_ZX_VLMAX':                        (McBlock.parseMcGeneric,           True,  True,  False, ),
-    'IEM_MC_BROADCAST_YREG_U32_ZX_VLMAX':                        (McBlock.parseMcGeneric,           True,  True,  False, ),
+    'IEM_MC_BROADCAST_YREG_U32_ZX_VLMAX':                        (McBlock.parseMcGeneric,           True,  True,  g_fNativeSimd),
     'IEM_MC_BROADCAST_YREG_U64_ZX_VLMAX':                        (McBlock.parseMcGeneric,           True,  True,  False, ),
     'IEM_MC_BROADCAST_YREG_U8_ZX_VLMAX':                         (McBlock.parseMcGeneric,           True,  True,  False, ),
     'IEM_MC_BSWAP_LOCAL_U16':                                    (McBlock.parseMcGeneric,           False, False, True,  ),
