c95967c33361061f13b29639396924f72b1b5849
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2024-03-21 09:37:32 +0000

    ValKit/bs3kit,bs3-cpu-instr-3: Don't set g_uBs3TrapEipHint automatically in Bs3TrapSetJmpAndRestoreWithExtCtx as we don't know the actual trap/exit EIP and it may have a different value in the top word from pCtxRestore->rip. Instead the caller has to set it like with Bs3RegCtxRestore. bugref:9898
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@103980 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/ValidationKit/bootsectors/bs3kit/bs3-cmn-TrapSetJmpAndRestoreWithExtCtx.c
+++ src/VBox/ValidationKit/bootsectors/bs3kit/bs3-cmn-TrapSetJmpAndRestoreWithExtCtx.c
@@ -49,12 +49,7 @@ BS3_CMN_DEF(void, Bs3TrapSetJmpAndRestoreWithExtCtx,(PCBS3REGCTX pCtxRestore, PC
        (only a potential issue in 64-bit mode). */
     Bs3ExtCtxRestoreEx(pExtCtxRestore);
     if (Bs3TrapSetJmp(pTrapFrame))
-    {
-#if TMPL_BITS == 32
-        g_uBs3TrapEipHint = pCtxRestore->rip.u32;
-#endif
         Bs3RegCtxRestore(pCtxRestore, BS3REGCTXRESTORE_F_NO_V86_ASSIST);
-    }
     g_fBs3TrapNoV86Assist = false;
     Bs3ExtCtxSaveEx(pExtCtxTrap);
 }
