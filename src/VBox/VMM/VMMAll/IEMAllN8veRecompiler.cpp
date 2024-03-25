bc674f28addc15d6aef2eccfd30cffe1c10ffb3d
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2024-03-25 21:08:40 +0000

    VMM/IEM: Mark the SIMD register as dirty based on the load size in iemNativeSimdRegAllocTmpForGuestSimdReg() so it doesn't need to be done in each native emitter, add some additional debug info about dirty SIMD registers, bugref:10614 [build fix]
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@104049 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/VMM/VMMAll/IEMAllN8veRecompiler.cpp
+++ src/VBox/VMM/VMMAll/IEMAllN8veRecompiler.cpp
@@ -6473,7 +6473,7 @@ iemNativeSimdRegAllocTmpForGuestSimdReg(PIEMRECOMPILERSTATE pReNative, uint32_t 
         if (   enmIntendedUse == kIemNativeGstRegUse_ForFullWrite
             || enmIntendedUse == kIemNativeGstRegUse_ForUpdate)
         {
-# ifdef IEMNATIVE_WITH_TB_DEBUG_INFO
+# if defined(IEMNATIVE_WITH_TB_DEBUG_INFO) && defined(IEMNATIVE_WITH_DELAYED_REGISTER_WRITEBACK)
             iemNativeDbgInfoAddNativeOffset(pReNative, *poff);
             iemNaitveDbgInfoAddGuestRegDirty(pReNative, true /*fSimdReg*/, enmGstSimdReg, idxSimdReg);
 # endif
@@ -6509,7 +6509,7 @@ iemNativeSimdRegAllocTmpForGuestSimdReg(PIEMRECOMPILERSTATE pReNative, uint32_t 
     if (   enmIntendedUse == kIemNativeGstRegUse_ForFullWrite
         || enmIntendedUse == kIemNativeGstRegUse_ForUpdate)
     {
-# ifdef IEMNATIVE_WITH_TB_DEBUG_INFO
+# if defined(IEMNATIVE_WITH_TB_DEBUG_INFO) && defined(IEMNATIVE_WITH_DELAYED_REGISTER_WRITEBACK)
         iemNativeDbgInfoAddNativeOffset(pReNative, *poff);
         iemNaitveDbgInfoAddGuestRegDirty(pReNative, true /*fSimdReg*/, enmGstSimdReg, idxRegNew);
 # endif
