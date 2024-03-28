4f6d08819712969d43ba7d5fa564a48b95253ff6
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2024-03-28 00:16:26 +0000

    VMM/IEM: #undef IEMNATIVE_WITH_DELAYED_REGISTER_WRITEBACK build fix. bugref:10629
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@104098 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/VMM/VMMAll/IEMAllN8veRecompiler.cpp
+++ src/VBox/VMM/VMMAll/IEMAllN8veRecompiler.cpp
@@ -9165,7 +9165,8 @@ iemNativeEmitLeaGprByGstRegRef(PIEMRECOMPILERSTATE pReNative, uint32_t off, uint
  *                              this will still flush pending writes in call volatile registers if false.
  */
 DECL_HIDDEN_THROW(uint32_t)
-iemNativeEmitCallCommon(PIEMRECOMPILERSTATE pReNative, uint32_t off, uint8_t cArgs, uint8_t cHiddenArgs, bool fFlushPendingWrites /*= true*/)
+iemNativeEmitCallCommon(PIEMRECOMPILERSTATE pReNative, uint32_t off, uint8_t cArgs, uint8_t cHiddenArgs,
+                        bool fFlushPendingWrites /*= true*/)
 {
 #ifdef VBOX_STRICT
     /*
@@ -9184,6 +9185,7 @@ iemNativeEmitCallCommon(PIEMRECOMPILERSTATE pReNative, uint32_t off, uint8_t cAr
 #endif
 
     /* We don't know what the called function makes use of, so flush any pending register writes. */
+    RT_NOREF(fFlushPendingWrites);
 #ifdef IEMNATIVE_WITH_DELAYED_REGISTER_WRITEBACK
     if (fFlushPendingWrites)
 #endif
