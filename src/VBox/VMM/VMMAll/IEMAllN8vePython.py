77195109d0656d7e8c2ac267bf74286f309b26af
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2024-04-03 13:24:35 +0000

    Committed too much, bugref:10641
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@104137 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/VMM/VMMAll/IEMAllN8vePython.py
+++ src/VBox/VMM/VMMAll/IEMAllN8vePython.py
@@ -618,7 +618,7 @@ g_dUnsupportedMcStmtStats = {}
 g_dUnsupportedMcStmtLastOneStats = {}
 
 ### Statistics: List of variations (value) with aimpl_[^0] calls that is only missing this one statement (key).
-g_dUnsupportedMcStmtLastOneAImplStats = {}
+#g_dUnsupportedMcStmtLastOneAImplStats = {}
 
 
 def analyzeVariantForNativeRecomp(oVariation,
@@ -652,32 +652,31 @@ def analyzeVariantForNativeRecomp(oVariation,
             else:
                 g_dUnsupportedMcStmtLastOneStats[sStmt] = [oVariation,];
 
-    if (    len(dUnsupportedStmts) in (1,2)
-        and iai.McStmt.findStmtByNames(aoStmts,
-                                       { 'IEM_MC_CALL_AIMPL_3': 1,
-                                         'IEM_MC_CALL_AIMPL_4': 1,
-                                         #'IEM_MC_CALL_VOID_AIMPL_0': 1, - can't test results... ?
-                                         'IEM_MC_CALL_VOID_AIMPL_1': 1,
-                                         'IEM_MC_CALL_VOID_AIMPL_2': 1,
-                                         'IEM_MC_CALL_VOID_AIMPL_3': 1,
-                                         'IEM_MC_CALL_VOID_AIMPL_4': 1,
-                                         'IEM_MC_CALL_FPU_AIMPL_1': 1,
-                                         'IEM_MC_CALL_FPU_AIMPL_2': 1,
-                                         'IEM_MC_CALL_FPU_AIMPL_3': 1,
-                                         'IEM_MC_CALL_MMX_AIMPL_2': 1,
-                                         'IEM_MC_CALL_MMX_AIMPL_3': 1,
-                                         'IEM_MC_CALL_SSE_AIMPL_2': 1,
-                                         'IEM_MC_CALL_SSE_AIMPL_3': 1,
-                                         'IEM_MC_CALL_AVX_AIMPL_2': 1,
-                                         'IEM_MC_CALL_AVX_AIMPL_3': 1,
-                                         'IEM_MC_CALL_AVX_AIMPL_4': 1,
-                                         'IEM_MC_CALL_AVX_AIMPL_NEW_3': 1,
-                                        })):
-        for sStmt in dUnsupportedStmts:
-            if sStmt in g_dUnsupportedMcStmtLastOneAImplStats:
-                g_dUnsupportedMcStmtLastOneAImplStats[sStmt].append(oVariation);
-            else:
-                g_dUnsupportedMcStmtLastOneAImplStats[sStmt] = [oVariation,];
+    #if (    len(dUnsupportedStmts) in (1,2)
+    #    and iai.McStmt.findStmtByNames(aoStmts,
+    #                                   { 'IEM_MC_CALL_AIMPL_3': 1,
+    #                                     'IEM_MC_CALL_AIMPL_4': 1,
+    #                                     #'IEM_MC_CALL_VOID_AIMPL_0': 1, - can't test results... ?
+    #                                     'IEM_MC_CALL_VOID_AIMPL_1': 1,
+    #                                     'IEM_MC_CALL_VOID_AIMPL_2': 1,
+    #                                     'IEM_MC_CALL_VOID_AIMPL_3': 1,
+    #                                     'IEM_MC_CALL_VOID_AIMPL_4': 1,
+    #                                     #'IEM_MC_CALL_FPU_AIMPL_1': 1,
+    #                                     #'IEM_MC_CALL_FPU_AIMPL_2': 1,
+    #                                     #'IEM_MC_CALL_FPU_AIMPL_3': 1,
+    #                                     #'IEM_MC_CALL_MMX_AIMPL_2': 1,
+    #                                     #'IEM_MC_CALL_MMX_AIMPL_3': 1,
+    #                                     #'IEM_MC_CALL_SSE_AIMPL_2': 1,
+    #                                     #'IEM_MC_CALL_SSE_AIMPL_3': 1,
+    #                                     #'IEM_MC_CALL_AVX_AIMPL_2': 1,
+    #                                     #'IEM_MC_CALL_AVX_AIMPL_3': 1,
+    #                                     #'IEM_MC_CALL_AVX_AIMPL_4': 1,
+    #                                    })):
+    #    for sStmt in dUnsupportedStmts:
+    #        if sStmt in g_dUnsupportedMcStmtLastOneAImplStats:
+    #            g_dUnsupportedMcStmtLastOneAImplStats[sStmt].append(oVariation);
+    #        else:
+    #            g_dUnsupportedMcStmtLastOneAImplStats[sStmt] = [oVariation,];
 
     return None;
 
@@ -720,7 +719,7 @@ def analyzeThreadedFunctionsForNativeRecomp(aoThreadedFuncs, sHostArch): # type 
           % (cNative * 100.0 / cTotal, cNative, cTotal), file = sys.stderr);
     if g_dUnsupportedMcStmtLastOneStats:
         asTopKeys = sorted(g_dUnsupportedMcStmtLastOneStats, reverse = True,
-                           key = lambda sSortKey: len(g_dUnsupportedMcStmtLastOneStats[sSortKey]));
+                           key = lambda sSortKey: len(g_dUnsupportedMcStmtLastOneStats[sSortKey]))[:16];
         print('todo:', file = sys.stderr);
         print('todo: Top %s variations with one unsupported statement dependency:' % (len(asTopKeys),),
               file = sys.stderr);
@@ -733,7 +732,7 @@ def analyzeThreadedFunctionsForNativeRecomp(aoThreadedFuncs, sHostArch): # type 
                      , file = sys.stderr);
 
         asTopKeys = sorted(g_dUnsupportedMcStmtStats, reverse = True,
-                           key = lambda sSortKey: g_dUnsupportedMcStmtStats[sSortKey]);
+                           key = lambda sSortKey: g_dUnsupportedMcStmtStats[sSortKey])[:16];
         print('todo:', file = sys.stderr);
         print('todo: Top %d most used unimplemented statements:' % (len(asTopKeys),), file = sys.stderr);
         cchMaxKey = max([len(sKey) for sKey in asTopKeys]);
@@ -749,18 +748,18 @@ def analyzeThreadedFunctionsForNativeRecomp(aoThreadedFuncs, sHostArch): # type 
                   file = sys.stderr);
         print('todo:', file = sys.stderr);
 
-    if g_dUnsupportedMcStmtLastOneAImplStats:
-        asTopKeys = sorted(g_dUnsupportedMcStmtLastOneAImplStats, reverse = True,
-                           key = lambda sSortKey: len(g_dUnsupportedMcStmtLastOneAImplStats[sSortKey]));
-        print('todo:', file = sys.stderr);
-        print('todo: Top %s variations with AIMPL call and 1-2 unsupported statement dependencies:' % (len(asTopKeys),),
-              file = sys.stderr);
-        cchMaxKey = max([len(sKey) for sKey in asTopKeys]);
-        for sKey in asTopKeys:
-            print('todo: %*s = %s (%s%s)'
-                  % (cchMaxKey, sKey, len(g_dUnsupportedMcStmtLastOneAImplStats[sKey]),
-                     ', '.join([oVar.getShortName() for oVar in g_dUnsupportedMcStmtLastOneAImplStats[sKey][:5]]),
-                     ',...' if len(g_dUnsupportedMcStmtLastOneAImplStats[sKey]) >= 5 else '', )
-                     , file = sys.stderr);
+    #if g_dUnsupportedMcStmtLastOneAImplStats:
+    #    asTopKeys = sorted(g_dUnsupportedMcStmtLastOneAImplStats, reverse = True,
+    #                       key = lambda sSortKey: len(g_dUnsupportedMcStmtLastOneAImplStats[sSortKey]))[:16];
+    #    print('todo:', file = sys.stderr);
+    #    print('todo: Top %s variations with AIMPL call and 1-2 unsupported statement dependencies:' % (len(asTopKeys),),
+    #          file = sys.stderr);
+    #    cchMaxKey = max([len(sKey) for sKey in asTopKeys]);
+    #    for sKey in asTopKeys:
+    #        print('todo: %*s = %s (%s%s)'
+    #              % (cchMaxKey, sKey, len(g_dUnsupportedMcStmtLastOneAImplStats[sKey]),
+    #                 ', '.join([oVar.getShortName() for oVar in g_dUnsupportedMcStmtLastOneAImplStats[sKey][:5]]),
+    #                 ',...' if len(g_dUnsupportedMcStmtLastOneAImplStats[sKey]) >= 5 else '', )
+    #                 , file = sys.stderr);
 
     return True;
