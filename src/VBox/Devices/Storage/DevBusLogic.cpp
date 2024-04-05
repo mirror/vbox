a4d6a90a3babcaf8b7fcbcd3612c45a8c94a9288
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2024-04-05 09:08:56 +0000

    Devices/BusLogic: Respond with a invalid command parameter error if the CDB length for a SCSI I/O request is invalid, bugref:10613
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@104171 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Devices/Storage/DevBusLogic.cpp
+++ src/VBox/Devices/Storage/DevBusLogic.cpp
@@ -3221,7 +3221,8 @@ static int buslogicR3DeviceSCSIRequestSetup(PPDMDEVINS pDevIns, PBUSLOGIC pThis,
     blPhysReadMeta(pDevIns, pThis, GCPhysAddrCCB, &CCBGuest, sizeof(CCB32));
 
     uTargetIdCCB = pThis->fMbxIs24Bit ? CCBGuest.o.uTargetId : CCBGuest.n.uTargetId;
-    if (RT_LIKELY(uTargetIdCCB < RT_ELEMENTS(pThisCC->aDeviceStates)))
+    if (   RT_LIKELY(uTargetIdCCB < RT_ELEMENTS(pThisCC->aDeviceStates))
+        && CCBGuest.c.cbCDB <= RT_ELEMENTS(CCBGuest.c.abCDB))
     {
         PBUSLOGICDEVICE pTgtDev = &pThisCC->aDeviceStates[uTargetIdCCB];
 
@@ -3442,7 +3443,7 @@ static void buslogicR3ProcessBiosReq(PPDMDEVINS pDevIns, PBUSLOGIC pThis, PBUSLO
     PESCMD pCmd = (PESCMD)pThis->aCommandBuffer;
 
     if (RT_LIKELY(   pCmd->uTargetId < RT_ELEMENTS(pThisCC->aDeviceStates)
-                  && pCmd->cbCDB <= 16))
+                  && pCmd->cbCDB <= RT_ELEMENTS(pCmd->abCDB)))
     {
         PBUSLOGICDEVICE pTgtDev = &pThisCC->aDeviceStates[pCmd->uTargetId];
 
