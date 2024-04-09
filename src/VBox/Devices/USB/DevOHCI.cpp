a88495467f31a39c7cd880bd11205097ec847f75
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2024-04-09 11:55:04 +0000

    DevOHCI: Take more care when adding items to in-flight tracker (see bugref:10612).
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@104250 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Devices/USB/DevOHCI.cpp
+++ src/VBox/Devices/USB/DevOHCI.cpp
@@ -910,6 +910,7 @@ static void                 ohciR3PhysReadCacheInvalidate(POHCIPAGECACHE pPageCa
 static DECLCALLBACK(void)   ohciR3RhXferCompletion(PVUSBIROOTHUBPORT pInterface, PVUSBURB pUrb);
 static DECLCALLBACK(bool)   ohciR3RhXferError(PVUSBIROOTHUBPORT pInterface, PVUSBURB pUrb);
 
+static bool                 ohciR3IsTdInFlight(POHCICC pThisCC, uint32_t GCPhysTD);
 static int                  ohciR3InFlightFind(POHCICC pThisCC, uint32_t GCPhysTD);
 # if defined(VBOX_STRICT) || defined(LOG_ENABLED)
 static int                  ohciR3InDoneQueueFind(POHCICC pThisCC, uint32_t GCPhysTD);
@@ -1941,6 +1942,13 @@ DECLINLINE(int) ohciR3InFlightFindFree(POHCICC pThisCC, const int iStart)
  */
 static void ohciR3InFlightAdd(POHCI pThis, POHCICC pThisCC, uint32_t GCPhysTD, PVUSBURB pUrb)
 {
+    if (ohciR3IsTdInFlight(pThisCC, GCPhysTD))
+    {
+        PPDMDEVINS pDevIns = pThisCC->pDevInsR3;
+        ohciR3RaiseUnrecoverableError(pDevIns, pThis, 10);
+        return;
+    }
+
     int i = ohciR3InFlightFindFree(pThisCC, (GCPhysTD >> 4) % RT_ELEMENTS(pThisCC->aInFlight));
     if (i >= 0)
     {
