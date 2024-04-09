91fcdbde03e8b9b94632ebb8dca182e784e0568f
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2024-04-09 09:56:09 +0000

    Installer/linux: The UI is always split now. bugref:9049
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@104249 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Installer/linux/install.sh
+++ src/VBox/Installer/linux/install.sh
@@ -322,9 +322,7 @@ if [ "$ACTION" = "install" ]; then
 
     # Create symlinks to start binaries
     ln -sf $INSTALLATION_DIR/VBox.sh /usr/bin/VirtualBox
-    if [ -f $INSTALLATION_DIR/VirtualBoxVM ]; then
-        ln -sf $INSTALLATION_DIR/VBox.sh /usr/bin/VirtualBoxVM
-    fi
+    ln -sf $INSTALLATION_DIR/VBox.sh /usr/bin/VirtualBoxVM
     ln -sf $INSTALLATION_DIR/VBox.sh /usr/bin/VBoxManage
     ln -sf $INSTALLATION_DIR/VBox.sh /usr/bin/VBoxSDL
     ln -sf $INSTALLATION_DIR/VBox.sh /usr/bin/VBoxVRDP
@@ -365,9 +363,7 @@ if [ "$ACTION" = "install" ]; then
 
     # Convenience symlinks. The creation fails if the FS is not case sensitive
     ln -sf VirtualBox /usr/bin/virtualbox > /dev/null 2>&1
-    if [ -f $INSTALLATION_DIR/VirtualBoxVM ]; then
-        ln -sf VirtualBoxVM /usr/bin/virtualboxvm > /dev/null 2>&1
-    fi
+    ln -sf VirtualBoxVM /usr/bin/virtualboxvm > /dev/null 2>&1
     ln -sf VBoxManage /usr/bin/vboxmanage > /dev/null 2>&1
     ln -sf VBoxSDL /usr/bin/vboxsdl > /dev/null 2>&1
     ln -sf VBoxHeadless /usr/bin/vboxheadless > /dev/null 2>&1
