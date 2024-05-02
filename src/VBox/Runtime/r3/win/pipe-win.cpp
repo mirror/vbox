0bf793df3580506715416081afe335084c0cc92c
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2024-05-02 11:22:33 +0000

    Runtime/r3/pipe-win.cpp: Fix statement assigning the return value in AssertStmt(), bugref:3409
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@104476 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Runtime/r3/win/pipe-win.cpp
+++ src/VBox/Runtime/r3/win/pipe-win.cpp
@@ -508,13 +508,13 @@ RTDECL(int)  RTPipeFromNative(PRTPIPE phPipe, RTHCINTPTR hNativePipe, uint32_t f
                 AssertStmt(   Info.NamedPipeState == FILE_PIPE_CONNECTED_STATE
                            || Info.NamedPipeState == FILE_PIPE_CLOSING_STATE
                            || Info.NamedPipeState == FILE_PIPE_DISCONNECTED_STATE,
-                           VERR_INVALID_HANDLE);
+                           rc = VERR_INVALID_HANDLE);
                 AssertStmt(      Info.NamedPipeConfiguration
                               == (   Info.NamedPipeEnd == FILE_PIPE_SERVER_END
                                   ? (pThis->fRead ? FILE_PIPE_INBOUND  : FILE_PIPE_OUTBOUND)
                                   : (pThis->fRead ? FILE_PIPE_OUTBOUND : FILE_PIPE_INBOUND) )
                            || Info.NamedPipeConfiguration == FILE_PIPE_FULL_DUPLEX,
-                           VERR_INVALID_HANDLE);
+                           rc = VERR_INVALID_HANDLE);
                 if (   RT_SUCCESS(rc)
                     && hNative2 == INVALID_HANDLE_VALUE
                     && !g_pfnSetHandleInformation(hNative,
