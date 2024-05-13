9c2ef85d27efebf18f26468007102aed6254f799
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2024-05-13 15:18:26 +0000

    Devices/Misc/DevFlashCFI.cpp: Return rc on error, bugref:3409
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@104602 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Devices/Misc/DevFlashCFI.cpp
+++ src/VBox/Devices/Misc/DevFlashCFI.cpp
@@ -599,8 +599,12 @@ static DECLCALLBACK(int) flashR3Construct(PPDMDEVINS pDevIns, int iInstance, PCF
         if (RT_SUCCESS(rc))
         {
             if (cbFlash <= pThis->cbFlashSize)
+            {
                 rc = pThis->Lun0.pDrvVfs->pfnReadAll(pThis->Lun0.pDrvVfs, FLASH_CFI_VFS_NAMESPACE, pThis->pszFlashFile,
                                                      pThis->pbFlash, pThis->cbFlashSize);
+                if (RT_FAILURE(rc))
+                    return PDMDEV_SET_ERROR(pDevIns, rc, N_("Failed to read flash content from VFS layer\n"));
+            }
             else
                 return PDMDEV_SET_ERROR(pDevIns, VERR_BUFFER_OVERFLOW, N_("Configured flash size is too small to fit the saved NVRAM content"));
         }
