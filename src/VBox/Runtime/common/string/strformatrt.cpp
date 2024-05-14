8e0204579e221d969b5f3e49967e4548ee60c6b7
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2024-05-14 11:53:30 +0000

    IPRT/strformatrt: Fixed warnings. ​bugref:3409
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@104624 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Runtime/common/string/strformatrt.cpp
+++ src/VBox/Runtime/common/string/strformatrt.cpp
@@ -819,7 +819,7 @@ DECLHIDDEN(size_t) rtstrFormatRt(PFNRTSTROUTPUT pfnOutput, void *pvArgOutput, co
                             return rtStrFormatBadPointer(0, pfnOutput, pvArgOutput, cchWidth, fFlags, psz,
                                                          szBuf, RT_STR_TUPLE("!BadFnNm"));
 
-                        while ((ch = *psz) != '\0' && ch != '(')
+                        while ((ch = *psz) != '\0')
                         {
                             if (RT_C_IS_BLANK(ch))
                             {
@@ -1527,7 +1527,6 @@ DECLHIDDEN(size_t) rtstrFormatRt(PFNRTSTROUTPUT pfnOutput, void *pvArgOutput, co
                     char const  chAddSafe = chVariant == 'p' ? '/'
                                           : chVariant == 'q' ? '+' /* '+' in queries is problematic, so no escape. */
                                           :                    '~' /* whatever */;
-                    size_t      cchOutput = 0;
                     const char *pszStr    = va_arg(*pArgs, char *);
                     ssize_t     cchStr;
                     ssize_t     offCur;
