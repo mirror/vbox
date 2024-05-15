a9f54f2d4729ab953c0847583e8ab915f00f4a5e
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2024-05-15 18:06:33 +0000

    Additions/common/VBoxService/VBoxServiceVMInfo.cpp: Don't call RTStrToUInt32() multiple times in RT_CLAMP(), bugref:3409
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@104644 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Additions/common/VBoxService/VBoxServiceVMInfo.cpp
+++ src/VBox/Additions/common/VBoxService/VBoxServiceVMInfo.cpp
@@ -312,7 +312,8 @@ static DECLCALLBACK(int) vbsvcVMInfoInit(void)
         if (RT_SUCCESS(rc2))
         {
             AssertPtr(pszValue);
-            g_uVMInfoUserIdleThresholdMS = RT_CLAMP(RTStrToUInt32(pszValue), 1000, UINT32_MAX - 1);
+            g_uVMInfoUserIdleThresholdMS = RTStrToUInt32(pszValue);
+            g_uVMInfoUserIdleThresholdMS = RT_CLAMP(g_uVMInfoUserIdleThresholdMS, 1000, UINT32_MAX - 1);
             RTStrFree(pszValue);
         }
     }
