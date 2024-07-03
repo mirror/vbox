0d60dc54fe3764e9e52255e005d0a31165a76e5a
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2024-07-03 20:22:58 +0000

    ValKit/bs3-cpu-weird-1: Passes on real hw: Intel 6700k.  bugref:10315
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@105126 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/ValidationKit/bootsectors/bs3-cpu-weird-1.c
+++ src/VBox/ValidationKit/bootsectors/bs3-cpu-weird-1.c
@@ -55,14 +55,15 @@ FNBS3TESTDOMODE BS3_CMN_FAR_NM(bs3CpuWeird1_Popf);
 /*********************************************************************************************************************************
 *   Global Variables                                                                                                             *
 *********************************************************************************************************************************/
+/**
+ * All tests in r163755 passed running directly on an Intel 6700k.
+ */
 static const BS3TESTMODEBYONEENTRY g_aModeByOneTests[] =
 {
-#if 1
     { "dbg+inhibit+ringxfer",   BS3_CMN_FAR_NM(bs3CpuWeird1_DbgInhibitRingXfer),    0 },
     { "pc wrapping",            BS3_CMN_FAR_NM(bs3CpuWeird1_PcWrapping),            0 },
     { "push/pop",               BS3_CMN_FAR_NM(bs3CpuWeird1_PushPop),               0 },
     { "push/pop sreg",          BS3_CMN_FAR_NM(bs3CpuWeird1_PushPopSReg),           0 },
-#endif
     { "popf",                   BS3_CMN_FAR_NM(bs3CpuWeird1_Popf),                  0 },
 };
 
