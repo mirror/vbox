2289c7ba79695b3153739a533c417c9c6fea90e4
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2024-07-24 18:35:43 +0000

    GA/Windows: Sending VBOXESC_CONFIGURETARGETS from VBoxService (draft version). bugref:10714
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@105495 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Additions/common/VBoxService/VBoxServiceDisplayConfig.cpp
+++ src/VBox/Additions/common/VBoxService/VBoxServiceDisplayConfig.cpp
@@ -30,17 +30,98 @@
 #include "VBoxServiceInternal.h"
 #include "VBoxServiceUtils.h"
 
+#undef NTDDI_VERSION
+#define NTDDI_VERSION NTDDI_LONGHORN
+#include <iprt/win/d3dkmthk.h>
+
+#define VBOX_WITH_WDDM
+#include <VBoxDisplay.h>
+
 
 /*********************************************************************************************************************************
 *   Global Variables                                                                                                             *
 *********************************************************************************************************************************/
+
+static PFND3DKMT_ENUMADAPTERS g_pfnD3DKMTEnumAdapters;
+static PFND3DKMT_OPENADAPTERFROMLUID g_pfnD3DKMTOpenAdapterFromLuid;
+static PFND3DKMT_CLOSEADAPTER g_pfnD3DKMTCloseAdapter;
+static PFND3DKMT_ESCAPE g_pfnD3DKMTEscape;
 
 /**
  * @interface_method_impl{VBOXSERVICE,pfnInit}
  */
 static DECLCALLBACK(int) vgsvcDisplayConfigInit(void)
 {
+    RTLDRMOD hLdrMod;
+    int rc = RTLdrLoadSystem("gdi32.dll", true /*fNoUnload*/, &hLdrMod);
+    if (RT_SUCCESS(rc))
+    {
+        rc = RTLdrGetSymbol(hLdrMod, "D3DKMTEnumAdapters", (void **)&g_pfnD3DKMTEnumAdapters);
+        if (RT_SUCCESS(rc))
+            rc = RTLdrGetSymbol(hLdrMod, "D3DKMTOpenAdapterFromLuid", (void **)&g_pfnD3DKMTOpenAdapterFromLuid);
+        if (RT_SUCCESS(rc))
+            rc = RTLdrGetSymbol(hLdrMod, "D3DKMTCloseAdapter", (void **)&g_pfnD3DKMTCloseAdapter);
+        if (RT_SUCCESS(rc))
+            rc = RTLdrGetSymbol(hLdrMod, "D3DKMTEscape", (void **)&g_pfnD3DKMTEscape);
+        AssertRC(rc);
+        RTLdrClose(hLdrMod);
+    }
+
+    VGSvcVerbose(2, "DXGK d3dkmthk callbacks are %s\n", RT_SUCCESS(rc) ? "Ok" : "Fail");
+
     return VINF_SUCCESS;
+}
+
+void ReplugDisplays(uint32_t cDisplays, VMMDevDisplayDef *paDisplays)
+{
+    RT_NOREF2(cDisplays, paDisplays);
+    D3DKMT_HANDLE hAdapter;
+    D3DKMT_ENUMADAPTERS EnumAdapters = {0};
+    NTSTATUS rcNt;
+    ULONG id = 0;
+
+    EnumAdapters.NumAdapters = RT_ELEMENTS(EnumAdapters.Adapters);
+    rcNt = g_pfnD3DKMTEnumAdapters(&EnumAdapters);
+
+    VGSvcVerbose(2, "D3DKMTEnumAdapters  rcNt=%#x NumAdapters=%d\n", rcNt, EnumAdapters.NumAdapters);
+
+    for(id = 0; id < EnumAdapters.NumAdapters; id++)
+    {
+        D3DKMT_ADAPTERINFO *pAdapterInfo = &EnumAdapters.Adapters[id];
+        VGSvcVerbose(2, "#%d: NumOfSources=%d hAdapter=0x%p Luid(%u, %u)\n", id,
+            pAdapterInfo->NumOfSources, pAdapterInfo->hAdapter, pAdapterInfo->AdapterLuid.HighPart, pAdapterInfo->AdapterLuid.LowPart);
+    }
+
+    D3DKMT_OPENADAPTERFROMLUID OpenAdapterData;
+    RT_ZERO(OpenAdapterData);
+    OpenAdapterData.AdapterLuid = EnumAdapters.Adapters[0].AdapterLuid;
+    rcNt = g_pfnD3DKMTOpenAdapterFromLuid(&OpenAdapterData);
+    VGSvcVerbose(2, "D3DKMTOpenAdapterFromLuid  rcNt=%#x hAdapter=0x%p\n", rcNt, OpenAdapterData.hAdapter);
+
+    hAdapter = OpenAdapterData.hAdapter;
+
+    if (hAdapter)
+    {
+        VBOXDISPIFESCAPE EscapeHdr = {0};
+        EscapeHdr.escapeCode = VBOXESC_CONFIGURETARGETS;
+        EscapeHdr.u32CmdSpecific = 0;
+
+        D3DKMT_ESCAPE EscapeData = {0};
+        EscapeData.hAdapter = hAdapter;
+        EscapeData.Type = D3DKMT_ESCAPE_DRIVERPRIVATE;
+        EscapeData.Flags.HardwareAccess = 1;
+        EscapeData.pPrivateDriverData = &EscapeHdr;
+        EscapeData.PrivateDriverDataSize = sizeof (EscapeHdr);
+
+        rcNt = g_pfnD3DKMTEscape(&EscapeData);
+        VGSvcVerbose(2, "D3DKMTEscape rcNt=%#x\n", rcNt);
+
+        D3DKMT_CLOSEADAPTER CloseAdapter;
+        CloseAdapter.hAdapter = hAdapter;
+
+        rcNt = g_pfnD3DKMTCloseAdapter(&CloseAdapter);
+        VGSvcVerbose(2, "D3DKMTCloseAdapter rcNt=%#x\n", rcNt);
+    }
 }
 
 /**
@@ -80,6 +161,7 @@ DECLCALLBACK(int) vgsvcDisplayConfigWorker(bool volatile *pfShutdown)
             VGSvcVerbose(2, "VbglR3GetDisplayChangeRequestMulti rc=%Rrc cDisplays=%d\n", rc, cDisplays);
             if (cDisplays > 0)
             {
+                ReplugDisplays(cDisplays, &aDisplays[0]);
                 VGSvcVerbose(2, "Display[0] (%dx%d)\n", aDisplays[0].cx, aDisplays[0].cy);
             }
         }
