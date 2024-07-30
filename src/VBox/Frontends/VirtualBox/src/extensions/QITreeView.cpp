1ac9cb83cb1cc9bd2a385cd69a3f3b0b45228ad4
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2024-07-30 11:55:10 +0000

    FE/Qt: bugref:10681: QITreeView: Reimplement function returning item rectangle for accessibility interface; Include item children into cumulative item rectangle; This is the subsequent step, s.a. r164182 and r164210.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@105541 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Frontends/VirtualBox/src/extensions/QITreeView.cpp
+++ src/VBox/Frontends/VirtualBox/src/extensions/QITreeView.cpp
@@ -362,8 +362,30 @@ QString QIAccessibilityInterfaceForQITreeView::text(QAccessible::Text /* enmText
 
 QRect QITreeViewItem::rect() const
 {
-    /* Redirect call to parent-tree: */
-    return parentTree() ? parentTree()->visualRect(modelIndex()) : QRect();
+    /* We can only ask the parent-tree for a rectangle: */
+    if (parentTree())
+    {
+        /* Acquire parent-tree model: */
+        const QAbstractItemModel *pModel = parentTree()->model();
+        AssertPtrReturn(pModel, QRect());
+
+        /* Acquire zero-column rectangle: */
+        QModelIndex itemIndex = modelIndex();
+        QRect rect = parentTree()->visualRect(itemIndex);
+        /* Enumerate all the remaining columns: */
+        for (int i = 1; i < pModel->columnCount(); ++i)
+        {
+            /* Acquire enumerated column rectangle: */
+            QModelIndex itemIndexI = pModel->index(itemIndex.row(), i, itemIndex.parent());
+            QRegion cumulativeRegion(rect);
+            cumulativeRegion += parentTree()->visualRect(itemIndexI);
+            rect = cumulativeRegion.boundingRect();
+        }
+        /* Total rect finally: */
+        return rect;
+    }
+    /* Empty rect by default: */
+    return QRect();
 }
 
 QModelIndex QITreeViewItem::modelIndex() const
