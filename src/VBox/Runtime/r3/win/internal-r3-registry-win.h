093016440256af42e59ecc0b3454a707eacf906d
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2024-08-21 17:31:52 +0000

    IPRT: Added RTSystemQueryNtFeatureEnabled() API to query Windows (NT) features + testcase. Also includes internal code for querying Windows registry values [build fix, forgot a file]. bugref:10753
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@105787 cfe28804-0f27-0410-a406-dd0f0b0b656f

---
+++ src/VBox/Runtime/r3/win/internal-r3-registry-win.h
@@ -0,0 +1,60 @@
+/* $Id$ */
+/** @file
+ * IPRT - Registy functions for Windows.
+ */
+
+/*
+ * Copyright (C) 2024 Oracle and/or its affiliates.
+ *
+ * This file is part of VirtualBox base platform packages, as
+ * available from https://www.virtualbox.org.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation, in version 3 of the
+ * License.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, see <https://www.gnu.org/licenses>.
+ *
+ * The contents of this file may alternatively be used under the terms
+ * of the Common Development and Distribution License Version 1.0
+ * (CDDL), a copy of it is provided in the "COPYING.CDDL" file included
+ * in the VirtualBox distribution, in which case the provisions of the
+ * CDDL are applicable instead of those of the GPL.
+ *
+ * You may elect to license modified versions of this file under the
+ * terms and conditions of either the GPL or the CDDL or both.
+ *
+ * SPDX-License-Identifier: GPL-3.0-only OR CDDL-1.0
+ */
+
+#ifndef IPRT_INCLUDED_SRC_r3_win_registry_win_h
+#define IPRT_INCLUDED_SRC_r3_win_registry_win_h
+#ifndef RT_WITHOUT_PRAGMA_ONCE
+# pragma once
+#endif
+
+#include "internal/iprt.h"
+#include <iprt/types.h>
+
+
+/**
+ * Queries a DWORD value from a Windows registry key.
+ *
+ * @returns IPRT status code.
+ * @retval  VERR_FILE_NOT_FOUND if the value has not been found.
+ * @param   hKey                    Registry handle to use.
+ * @param   pszKey                  Registry key to query \a pszName in.
+ * @param   pszName                 Name of the value to query.
+ * @param   pdwValue                Where to return the actual value on success.
+ */
+RTDECL(int) RTSystemWinRegistryQueryDWORD(HKEY hKey, const char *pszKey, const char *pszName, DWORD *pdwValue);
+
+
+#endif /* !IPRT_INCLUDED_SRC_r3_win_registry_win_h */
