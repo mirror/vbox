67ed8efb174da0f410ab2db6e4b8977bd79ac221
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2024-09-10 12:04:06 +0000

    Disassembler/ArmV8: Updates and start on floating point and SIMD instructions, bugref:10394 [scm]
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@106005 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Disassembler/DisasmCore-armv8.cpp
+++ src/VBox/Disassembler/DisasmCore-armv8.cpp
@@ -766,7 +766,7 @@ static int disArmV8ParseImmHImmB(PDISSTATE pDis, uint32_t u32Insn, PCDISARMV8OPC
 
     Assert(pInsnParm->cBits == 7);
     uint32_t u32ImmRaw = disArmV8ExtractBitVecFromInsn(u32Insn, pInsnParm->idxBitStart, pInsnParm->cBits);
-    if (!(u32ImmRaw & RT_BIT_32(6))) /* immh == 0xxx is reserved for the scalar variant. */ 
+    if (!(u32ImmRaw & RT_BIT_32(6))) /* immh == 0xxx is reserved for the scalar variant. */
         return VERR_DIS_INVALID_OPCODE;
 
     pParam->uValue = 2 * 64 - u32ImmRaw;
